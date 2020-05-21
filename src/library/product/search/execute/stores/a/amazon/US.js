39fb673afdcd06fa7fe88df75cf2ddc02cd61caa
mjgp2 <matthew.painter@import.io>
2020-05-21 23:05:27 +0100

    Need JS on at the moment

--- src/library/product/search/execute/stores/a/amazon/US.js
+++ src/library/product/search/execute/stores/a/amazon/US.js
@@ -7,7 +7,6 @@ module.exports = {
   },
   implementation: async ({ keywords }, { country, store }, context, { goto }) => {
     const url = `https://www.amazon.com/s?k=${encodeURIComponent(keywords)}&ref=nb_sb_noss_2`;
-    await context.setJavaScriptEnabled(false);
     await goto({ url });
   },
 };
