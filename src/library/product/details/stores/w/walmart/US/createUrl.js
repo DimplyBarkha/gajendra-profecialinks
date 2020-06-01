6d8ad49fa03c3468c188d745dea44ab4d4a7c2ae
Bindu Datla <bindu.datla@import.io>
2020-06-01 18:43:11 -0400

    Initial setup for walmart

---
+++ src/library/product/details/stores/w/walmart/US/createUrl.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'walmart.com',
+    prefix: null,
+    country: 'US',
+    store: 'walmart',
+  },
+};
