44d60b3eaf1b815a67e60170c35614ba6a2e592f
SushmaM <sushmam@sushmas-mbp.lan>
2020-06-03 11:59:36 -0700

    Complete search extractor without deploying due to issue

--- src/library/product/search/stores/w/walgreens/US/execute.js
+++ src/library/product/search/stores/w/walgreens/US/execute.js
@@ -1,3 +1,28 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('inputs!!!');
+  console.log(inputs);
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url });
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/search/execute',
@@ -5,8 +30,9 @@ module.exports = {
     country: 'US',
     store: 'walgreens',
     domain: 'walgreens.com',
-    url: 'https://www.walgreens.com/search/results.jsp?Ntt=${searchTerms}',
+    url: 'https://www.walgreens.com/search/results.jsp?Ntt={searchTerms}',
     loadedSelector: 'div.wag-product-card-details',
     noResultsXPath: '//h1[@id="zero-result-alert"]|//div[@id="noProducts-Drug"]',
   },
+  implementation,
 };
--- src/library/product/search/stores/w/walgreens/US/extract.js
+++ src/library/product/search/stores/w/walgreens/US/extract.js
@@ -1,4 +1,49 @@
+async function implementation (
+  // @ts-ignore
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(() => {
+    function addHiddenDiv (i, productCards, productInfo) {
+      // @ts-ignore
+      pageNum = document.getElementById('pag-counter') !== null ? parseInt(document.getElementById('pag-counter').value) : 1;
+      const newDiv = document.createElement('div');
+      newDiv.id = i;
+      newDiv.className = 'extra-info';
+      newDiv.textContent = pageNum === 1 ? i + 1 : ((pageNum - 1) * numberOfProductsPerPage) + 1;
+      newDiv.style.display = 'none';
+      console.log(productInfo);
+      newDiv.dataset.url = 'https://www.walgreens.com' + productInfo[i].productInfo.productURL;
+      newDiv.dataset.gtin = productInfo[i].productInfo.upc;
+      newDiv.dataset.wic = productInfo[i].productInfo.wic;
+      productCards.item(i).appendChild(newDiv);
+    }
 
+    const productCards = document.getElementsByClassName('wag-product-card-details');
+    // @ts-ignore
+    let pageNum = document.getElementById('pag-counter') !== null ? parseInt(document.getElementById('pag-counter').value) : 1;
+    // @ts-ignore
+    const numberOfProductsPerPage = parseInt(document.getElementById('pagenav-items-per-page').value);
+
+    // @ts-ignore
+    const productInfo = window.__APP_INITIAL_STATE__.searchResult.searchData.products;
+
+    let i = 0;
+    while (i < productCards.length) {
+      if (productCards.item(i).querySelectorAll('.extra-info').length > 0) {
+        // @ts-ignore
+        document.getElementById(i).remove();
+      }
+      addHiddenDiv(i, productCards, productInfo);
+      i++;
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -7,4 +52,5 @@ module.exports = {
     transform: null,
     domain: 'walgreens.com',
   },
+  implementation,
 };
