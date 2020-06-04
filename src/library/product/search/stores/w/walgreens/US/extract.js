7a6489b70a271fa7f0775ac15a59596b69cb1c46
SushmaM <sushmam@sushmas-mbp.lan>
2020-06-04 15:53:02 -0700

    Fix data attributes in extract js due to data error

--- src/library/product/search/stores/w/walgreens/US/extract.js
+++ src/library/product/search/stores/w/walgreens/US/extract.js
@@ -7,38 +7,32 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+
   await context.evaluate(() => {
-    function addHiddenDiv (i, productCards, productInfo) {
+    // @ts-ignore
+    const numberOfProductsPerPage = parseInt(document.getElementById('pagenav-items-per-page').value);
+
+    function addHiddenDiv (i, productCards) {
       // @ts-ignore
-      pageNum = document.getElementById('pag-counter') !== null ? parseInt(document.getElementById('pag-counter').value) : 1;
+      const pageNum = document.getElementById('pag-counter') !== null ? parseInt(document.getElementById('pag-counter').value) : 1;
       const newDiv = document.createElement('div');
       newDiv.id = i;
       newDiv.className = 'extra-info';
-      newDiv.textContent = pageNum === 1 ? i + 1 : ((pageNum - 1) * numberOfProductsPerPage) + 1;
+      newDiv.textContent = pageNum === 1 ? i + 1 : ((pageNum - 1) * numberOfProductsPerPage) + 1 + i;
       newDiv.style.display = 'none';
-      console.log(productInfo);
-      newDiv.dataset.url = 'https://www.walgreens.com' + productInfo[i].productInfo.productURL;
-      newDiv.dataset.gtin = productInfo[i].productInfo.upc;
-      newDiv.dataset.wic = productInfo[i].productInfo.wic;
+      newDiv.dataset.id = productCards[i].querySelector('a').getAttribute('id').split('_sku')[1];
+      newDiv.dataset.url = 'https://www.walgreens.com' + productCards[i].querySelector('a').getAttribute('href');
       productCards.item(i).appendChild(newDiv);
     }
 
     const productCards = document.getElementsByClassName('wag-product-card-details');
-    // @ts-ignore
-    let pageNum = document.getElementById('pag-counter') !== null ? parseInt(document.getElementById('pag-counter').value) : 1;
-    // @ts-ignore
-    const numberOfProductsPerPage = parseInt(document.getElementById('pagenav-items-per-page').value);
-
-    // @ts-ignore
-    const productInfo = window.__APP_INITIAL_STATE__.searchResult.searchData.products;
-
     let i = 0;
     while (i < productCards.length) {
       if (productCards.item(i).querySelectorAll('.extra-info').length > 0) {
         // @ts-ignore
         document.getElementById(i).remove();
       }
-      addHiddenDiv(i, productCards, productInfo);
+      addHiddenDiv(i, productCards);
       i++;
     }
   });
