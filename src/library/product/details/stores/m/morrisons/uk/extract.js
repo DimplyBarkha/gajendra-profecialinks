584360b64bc997e860290955d13f43cf0612c871
Manoj <manoj.d@osmosys.asia>
2020-06-05 21:51:49 +0530

    updated column names and fixed issues

--- src/library/product/details/stores/m/morrisons/uk/extract.js
+++ src/library/product/details/stores/m/morrisons/uk/extract.js
@@ -9,6 +9,14 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await new Promise(resolve => setTimeout(resolve, 100000));
+    await context.evaluate(async function () {
+      const desc = document.querySelector('.bop-productDetails > .gn-accordionElement > div.gn-accordionElement__content.gn-accordionElement__content--expanded > .gn-accordionElement__wrapper');
+      let text = '';
+      if (desc) {
+        text = desc.innerText.replace(/[\r\n]+/gm, ' ');
+      }
+      document.querySelector('body').setAttribute('description', text);
+    });
     await context.extract(productDetails);
   },
 };
