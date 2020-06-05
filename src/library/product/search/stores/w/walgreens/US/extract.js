18edc211a71ce85cae788a0aaf46adbd66a7fc86
sushma-mangalapally <65569027+sushma-mangalapally@users.noreply.github.com>
2020-06-04 23:27:23 -0700

    Mangalapally walgreens search (#12)
    * Complete search extractor without deploying due to issue
    
    * Edit inputs
    
    * Add line for no results for input
    
    * Fix data attributes in extract js due to data error
    
    * Fix data attributes in extract js due to data error
    
    * Add thumbnails link to extractor
    
    Co-authored-by: SushmaM <sushmam@sushmas-mbp.lan>

--- src/library/product/search/stores/w/walgreens/US/extract.js
+++ src/library/product/search/stores/w/walgreens/US/extract.js
@@ -1,4 +1,44 @@
+async function implementation (
+  // @ts-ignore
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
 
+  await context.evaluate(() => {
+    // @ts-ignore
+    const numberOfProductsPerPage = parseInt(document.getElementById('pagenav-items-per-page').value);
+
+    function addHiddenDiv (i, productCards) {
+      // @ts-ignore
+      const pageNum = document.getElementById('pag-counter') !== null ? parseInt(document.getElementById('pag-counter').value) : 1;
+      const newDiv = document.createElement('div');
+      newDiv.id = i;
+      newDiv.className = 'extra-info';
+      newDiv.textContent = pageNum === 1 ? i + 1 : ((pageNum - 1) * numberOfProductsPerPage) + 1 + i;
+      newDiv.style.display = 'none';
+      newDiv.dataset.id = productCards[i].querySelector('a').getAttribute('id').split('_sku')[1];
+      newDiv.dataset.url = 'https://www.walgreens.com' + productCards[i].querySelector('a').getAttribute('href');
+      newDiv.dataset.thumbnail = productCards[i].querySelector('img').getAttribute('src').slice(2);
+      productCards.item(i).appendChild(newDiv);
+    }
+
+    const productCards = document.getElementsByClassName('wag-product-card-details');
+    let i = 0;
+    while (i < productCards.length) {
+      if (productCards.item(i).querySelectorAll('.extra-info').length > 0) {
+        // @ts-ignore
+        document.getElementById(i).remove();
+      }
+      addHiddenDiv(i, productCards);
+      i++;
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -7,4 +47,5 @@ module.exports = {
     transform: null,
     domain: 'walgreens.com',
   },
+  implementation,
 };
