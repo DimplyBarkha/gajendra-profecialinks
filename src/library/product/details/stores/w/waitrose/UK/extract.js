432477e1dc952a9550f22b93ec0bb2344b47b88a
bhavani.a <bhavani.a@osmosys.asia>
2020-06-05 18:21:01 +0530

    resolved the issues

--- src/library/product/details/stores/w/waitrose/UK/extract.js
+++ src/library/product/details/stores/w/waitrose/UK/extract.js
@@ -7,4 +7,17 @@ module.exports = {
     transform: null,
     domain: 'waitrose.com',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      const availableStatus = document.querySelector('button.add-to-cart-button__full-button') ? 'In stock' : 'Out of stock';
+      document.querySelector('body').setAttribute('availableStatus', 'In Stock');
+
+      const breadcrumb = [...document.querySelectorAll('ul[class="crumbs___SEVo6"]>li')]
+      if(breadcrumb) {
+        const categoryRanking = breadcrumb.map(i => i.innerText ).join('> ');
+        document.querySelector('body').setAttribute('categoryRanking', categoryRanking);
+      }
+    });
+    await context.extract(productDetails);
+  },
 };
