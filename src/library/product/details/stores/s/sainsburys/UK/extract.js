cd6008c5a7b0b1ce503bc496abc11e589ff2a007
Ankit Kumar <ankit.k@osmosys.asia>
2020-06-05 19:06:59 +0530

    added scripts to handle breadcrumbs

--- src/library/product/details/stores/s/sainsburys/UK/extract.js
+++ src/library/product/details/stores/s/sainsburys/UK/extract.js
@@ -7,4 +7,22 @@ module.exports = {
     transform: null,
     domain: 'sainsburys.co.uk',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      console.log('EXECUTING INSIDE BROWSER');
+      const elements = document.querySelectorAll('ol[class*="breadcrumbs"]>li');
+      let text;
+      const elementLength = elements.length;
+      console.log(elementLength);
+      for (let i = 0; i < elementLength; i++) {
+        if (i === 1) {
+          text = elements[i].textContent;
+          continue;
+        }
+        text = `${text} > ${elements[i].textContent}`;
+      }
+      document.querySelector('body').setAttribute('breadcrumbs', text);
+    });
+    await context.extract(productDetails);
+  },
 };
