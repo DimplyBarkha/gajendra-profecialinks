e5afb8d953d892ea559b8fb25199d08c09d1a384
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-06-05 11:52:03 -0700

    Target Search

--- src/library/product/search/stores/t/target/US/execute.js
+++ src/library/product/search/stores/t/target/US/execute.js
@@ -1,3 +1,43 @@
+/**
+ *
+ * @param { { keywords: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+
+ async function implementation (
+   inputs,
+   parameters,
+   context,
+   dependencies,
+ ) {
+
+   function stall(ms) {
+      return new Promise((res, rej) => {
+        setTimeout(() => {
+          res();
+        }, ms)
+      })
+    }
+
+   const url = "https://target.com";
+   await dependencies.goto({ url });
+   await context.setInputValue("input#search", inputs.keywords);
+   await stall(2000);
+   await context.evaluate(function() {
+      let link = document.querySelector(".TypeaheadItemLink-sc-125kxr2-0");
+      if (link != null ) {
+          link.click();
+      }
+    });
+   await context.waitForXPath("//ul//li");
+   return context.evaluate(function () {
+     return document.querySelectorAll('li').length > 0;
+   });
+   console.log('extracting..');
+
+ }
 
 module.exports = {
   implements: 'product/search/execute',
@@ -9,4 +49,5 @@ module.exports = {
     loadedSelector: 'div[data-test="productGridContainer"] li',
     noResultsXPath: '//h1[contains(.,"no results found")]',
   },
+  implementation
 };
--- src/library/product/search/stores/t/target/US/extract.js
+++ src/library/product/search/stores/t/target/US/extract.js
@@ -1,4 +1,110 @@
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    console.log("Do pagination");
+    let clickNextBtn = () => {
+      let nextBtn = document.querySelector('a[data-test="next"]');
+      if (nextBtn != null && !nextBtn.hasAttribute("disabled") ) {
+          nextBtn.click();
+      }
+    }
+
+    function stall(ms) {
+      return new Promise((res, rej) => {
+        setTimeout(() => {
+          res();
+        }, ms)
+      })
+    }
+
+    let counter = 1;
+    var rank = 1;
+    while(true) {
+      await stall(1000);
+      await context.evaluate(async function() {
+
+        function stall(ms) {
+          return new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+
+        function addHiddenDiv(el, myClass, content) {
+            let newDiv = document.createElement("div");
+            newDiv.setAttribute("class", myClass);
+            newDiv.textContent = content;
+            newDiv.style.display = "none";
+            el.appendChild(newDiv);
+        }
+
+        function isElementInViewport (el) {
+          if(el) {
+            let rect = el.getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+          }
+          return false;
+        }
+
+        let scrollTop = 0;
+        while(!isElementInViewport(document.querySelector('a[data-test="next"]'))) {
+          await stall(500);
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          if(scrollTop == 20000) {
+            break;
+          }
+        }
+
+        let itemContainers = document.querySelectorAll('li.Col-favj32-0.bZxgbc.h-padding-a-none');
+        let rank = 1;
+        for(let itemContainer of itemContainers) {
+          addHiddenDiv(itemContainer, 'itemId', itemContainer.querySelector('a').getAttribute('href').split("?")[0].split('/')[4]);
+          //addHiddenDiv(itemContainer, 'thumbnailInfo', itemContainer.querySelector('source').getAttribute('srcset'));
+          let pageNum = document.querySelector('button[data-test="select"]') ? document.querySelector('button[data-test="select"]').innerText.split(" ")[1] : 1;
+          totalRank = ((pageNum - 1) * 24) + rank;
+          addHiddenDiv(itemContainer, 'rank', totalRank);
+          rank++;
+        }
+
+
+      });
+
+      await stall(1000);
+      return await context.extract(productDetails, { transform });
+      await stall(500);
+      let hasNextBtn = await context.evaluate(function() {
+        let nextBtn = document.querySelector('a[data-test="next"]');
+        if (nextBtn && !nextBtn.hasAttribute("disabled") ) {
+            return true;
+        }
+        return false;
+      });
+      if(!hasNextBtn) {
+        break;
+      }
+      await stall(500);
+      await context.evaluate(clickNextBtn);
+      if(counter == 100) {
+        break;
+      }
+      counter++;
+    }
+
+}
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -7,4 +113,5 @@ module.exports = {
     transform: null,
     domain: 'target.com',
   },
+  implementation
 };
