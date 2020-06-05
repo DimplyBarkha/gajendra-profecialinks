46dc69d724b6ed125894bc5cbcab9c2ba925a0e7
Manas Ranjan <38726977+manasnilaroutOSM@users.noreply.github.com>
2020-06-05 21:28:54 +0530

    Fixed JS code.

--- src/library/product/details/stores/w/waitrose/UK/extract.js
+++ src/library/product/details/stores/w/waitrose/UK/extract.js
@@ -12,9 +12,11 @@ module.exports = {
       const availableStatus = document.querySelector('button.add-to-cart-button__full-button') ? 'In stock' : 'Out of stock';
       document.querySelector('body').setAttribute('availableStatus', 'In Stock');
 
-      const breadcrumb = [...document.querySelectorAll('ul[class="crumbs___SEVo6"]>li')]
-      if(breadcrumb) {
-        const categoryRanking = breadcrumb.map(i => i.innerText ).join('> ');
+      const breadcrumb = document.querySelectorAll('ul[class="crumbs___SEVo6"]>li');
+
+      if (breadcrumb) {
+        let categoryRanking;
+        breadcrumb.forEach(el => { categoryRanking = (categoryRanking ? `${categoryRanking} > ` : '') + el.textContent });
         document.querySelector('body').setAttribute('categoryRanking', categoryRanking);
       }
     });
