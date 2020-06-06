6bb9dff01f2d77d4acc87a56b9c9bb931c11a10c
bhavani.a <bhavani.a@osmosys.asia>
2020-06-06 15:55:27 +0530

    fixed the issue with adding category and availabikty

--- src/library/product/details/stores/w/waitrose/UK/extract.js
+++ src/library/product/details/stores/w/waitrose/UK/extract.js
@@ -9,15 +9,15 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      const availableStatus = document.querySelector('button.add-to-cart-button__full-button') ? 'In stock' : 'Out of stock';
-      document.querySelector('body').setAttribute('availableStatus', 'In Stock');
+      const availableStatus = document.querySelector('button[data-origincomponent="AddToTrolley"]') ? 'In stock' : 'Out of stock';
+      document.querySelector('body').setAttribute('available_status', availableStatus);
 
       const breadcrumb = document.querySelectorAll('ul[class="crumbs___SEVo6"]>li');
 
       if (breadcrumb) {
         let categoryRanking;
         breadcrumb.forEach(el => { categoryRanking = (categoryRanking ? `${categoryRanking} > ` : '') + el.textContent });
-        document.querySelector('body').setAttribute('categoryRanking', categoryRanking);
+        document.querySelector('body').setAttribute('category_ranking', categoryRanking);
       }
     });
     await context.extract(productDetails);
