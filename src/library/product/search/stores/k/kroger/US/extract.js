2ca3aaed504da7d10562eea9b1954f29c2f95aee
Jonathan Campbell <45703069+jon-campbell1@users.noreply.github.com>
2020-06-05 22:06:53 -0700

    Update extract.js

--- src/library/product/search/stores/k/kroger/US/extract.js
+++ src/library/product/search/stores/k/kroger/US/extract.js
@@ -1,4 +1,4 @@
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -7,46 +7,26 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-
   async function getElementsOnPage() {
     return await context.evaluate(() => {
       return document.getElementsByClassName('kds-Text--m text-default-800 mt-12 mb-4 font-500').length;
     });
   }
 
-  const currentElCount = await getElementsOnPage();
+  let currentElCount = await getElementsOnPage();
 
-  const totalElCount = currentElCount;
-
-<<<<<<< HEAD
-  // add indices for RankOrganic
-  await context.evaluate(async (totalElCount) => {
-    const items = document.querySelectorAll('div.ProductCard-promoContainer.-mx-12');
-
-    items.forEach((item, index) => {
-      const ranking = document.createElement('li');
-      ranking.classList.add('current-rank');
-      ranking.textContent = index + 1 + totalElCount;
-      ranking.style.display = 'none';
-      item.append(ranking);
-    });
-  }, JSON.parse(totalElCount));
-=======
+  let totalElCount = currentElCount;
 
 
 
->>>>>>> b57c2cbbd0fe9c99e7519535e10dbfae49b501dc
 
-  // end my code
+
 
   return await context.extract(productDetails, { transform });
 }
 
-<<<<<<< HEAD
-=======
 const { transform } = require('../../../../shared');
 
->>>>>>> b57c2cbbd0fe9c99e7519535e10dbfae49b501dc
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -54,6 +34,5 @@ module.exports = {
     store: 'kroger',
     transform: transform,
     domain: 'kroger.com',
-  },
-  implementation,
+  }, implementation
 };
