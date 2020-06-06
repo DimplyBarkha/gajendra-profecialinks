b59aab7b56cf2891bd00f3c81a57196b64b3aa98
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-06-05 17:16:44 -0700

    updated extract.yaml for search and core

---
+++ src/library/product/search/stores/a/amazonFresh/US/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonFresh',
+    transform: null,
+    domain: 'amazon.com',
+  },
+};
