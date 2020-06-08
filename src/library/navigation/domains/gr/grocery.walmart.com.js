b20189ca8756c57e1e442e389eebd44cc9428c0a
sashidatla <57485741+sashidatla@users.noreply.github.com>
2020-06-07 23:59:13 -0400

    Walmart og search (#33)
    * initial setup
    
    * changed yaml
    
    * training issue for id and productUrl
    
    * fixed extraction issues
    
    * merged
    
    * added transform
    
    * changes from review
    
    * Fixed Issue with navigation implementation

--- src/library/navigation/domains/gr/grocery.walmart.com.js
+++ src/library/navigation/domains/gr/grocery.walmart.com.js
@@ -10,21 +10,9 @@ module.exports = {
   },
 
   implementation: async (inputs, parameterValues, context, dependencies) => {
-    // CSS
-    const cssProductPrice = 'div[data-automation-id="productPageTile"] div[class*="titleContainer"] [data-automation-id="salePrice"]';
-    const cssProductMainImage = 'div[data-automation-id="productPageTile"] div[class*="imageContainer"] img[data-automation-id="image"] , div[data-automation-id="productPageTile"] div[class*="imageContainer"] img[data-tl-id="ProductPage-primary-image"]';
-
     // USING OPT TAGS > anti_fingerprint), to avoid blocking
     // #[!opt!]{"anti_fingerprint":false}[/!opt!]
     const url = `${inputs.url}#[!opt!]{"anti_fingerprint":false}[/!opt!]`;
     await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
-    // await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true , captureRequests: true});
-    try {
-      await context.waitForSelector(cssProductPrice, { timeout: 10000 });
-      await context.waitForSelector(cssProductMainImage, { timeout: 10000 });
-    } catch (e) {
-      console.log('Handling Wait Timeout - Optional');
-      console.log(e);
-    }
   },
 };
