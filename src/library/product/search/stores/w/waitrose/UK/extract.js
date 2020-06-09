9d7c3aabc9d4db35e87713c851a504c65e71a35d
Sindhusha balla <sindhusha.b@osmosys.asia>
2020-06-09 17:00:19 +0530

    Added pagination support in extract.js

--- src/library/product/search/stores/w/waitrose/UK/extract.js
+++ src/library/product/search/stores/w/waitrose/UK/extract.js
@@ -7,4 +7,29 @@ module.exports = {
     transform: null,
     domain: 'waitrose.com',
   },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    // @TODO - Get totalResults from DOM
+    const totalResults = 89
+    const resultPerLoad = 48;
+    let maxClicks = Math.round(89 / 48) - 1;
+    while (maxClicks > 0) {
+      console.log('Clicked');
+      // @TODO - Add a check for load more selector also i.e click only exists
+      await context.click('div.loadMoreWrapper___UneG1 > button');
+      await timeout(30000);
+      maxClicks--;
+    }
+    async function timeout (ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    return await context.extract(productDetails, { transform });
+
+  }
 };
