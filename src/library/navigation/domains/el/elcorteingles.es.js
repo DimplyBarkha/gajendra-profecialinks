f63b9acdb9a06c882be84a13e43fae807bb45c28
Lakshmi-Korlapati <lakshmi.k@osmosys.asia>
2020-06-09 22:33:21 +0530

    Updated the code for extractor, navigation file

--- src/library/navigation/domains/el/elcorteingles.es.js
+++ src/library/navigation/domains/el/elcorteingles.es.js
@@ -7,6 +7,14 @@ module.exports = {
     country: 'ES',
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
-    await context.goto(url, { timeout: 100000, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url, {
+        first_request_timeout: 100000,
+        block_ads: false,
+        load_all_resources: true,
+        images_enabled: true,
+        timeout: 100000,
+        waitUntil: 'load',
+        checkBlocked: true,
+      });
   },
 };
