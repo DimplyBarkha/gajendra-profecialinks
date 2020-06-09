191f912ad8b38030b2e590402052f45aac58ffd9
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-06-09 15:36:29 -0700

    Removed Kroger File

--- src/library/product/details/stores/k/kroger/US/createUrl.js
+++ src/library/product/details/stores/k/kroger/US/createUrl.js
@@ -1,4 +1,4 @@
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -7,18 +7,19 @@ async function implementation (
   const { id } = inputs;
   const { domain, prefix } = parameters;
 
-  // add leading zeroes if needed
-  let extendedId = id.toString().split('');
+// add leading zeroes if needed
+  let extendedId = id.toString().split("");
 
-  while (extendedId.length < 13) {
-    extendedId.unshift('0');
-    console.log(extendedId);
+  while(extendedId.length<13){
+    extendedId.unshift("0")
+    console.log(extendedId)
   }
 
-  extendedId = extendedId.join('');
+  extendedId = extendedId.join("")
 
   return `https://www.kroger.com/search?query=${extendedId}&searchType=natural&fulfillment=all`;
 }
+
 
 module.exports = {
   implements: 'product/details/createUrl',
@@ -27,6 +28,5 @@ module.exports = {
     prefix: null,
     country: 'US',
     store: 'kroger',
-  },
-  implementation,
+  }, implementation
 };
--- src/library/product/search/stores/k/kroger/US/extract.js
+++ src/library/product/search/stores/k/kroger/US/extract.js
@@ -1,4 +1,4 @@
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -7,15 +7,20 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  async function getElementsOnPage () {
+  async function getElementsOnPage() {
     return await context.evaluate(() => {
       return document.getElementsByClassName('kds-Text--m text-default-800 mt-12 mb-4 font-500').length;
     });
   }
 
-  const currentElCount = await getElementsOnPage();
+  let currentElCount = await getElementsOnPage();
 
-  const totalElCount = currentElCount;
+  let totalElCount = currentElCount;
+
+
+
+
+
 
   return await context.extract(productDetails, { transform });
 }
@@ -29,6 +34,5 @@ module.exports = {
     store: 'kroger',
     transform: transform,
     domain: 'kroger.com',
-  },
-  implementation,
+  }, implementation
 };
