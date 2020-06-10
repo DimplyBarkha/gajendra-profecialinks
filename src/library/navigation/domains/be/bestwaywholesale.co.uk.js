b7be810c84724777d31dfa1692b1868422669b3e
Sindhusha balla <sindhusha.b@osmosys.asia>
2020-06-10 15:01:23 +0530

    Added login support & retrieved stock availability

--- src/library/navigation/domains/be/bestwaywholesale.co.uk.js
+++ src/library/navigation/domains/be/bestwaywholesale.co.uk.js
@@ -6,4 +6,29 @@ module.exports = {
     country: 'UK',
     store: 'bestwaywholesale',
   },
+  implementation: async ({ url }, parameterValues, context, dependencies) => {
+    let loginPageResponse = await context.goto('https://www.bestwaywholesale.co.uk/login-auth', {
+      timeout: 10000,
+      waitUntil: 'load',
+      checkBlocked: true,
+      js_enabled: true,
+      css_enabled: false
+    });
+
+    if (loginPageResponse.status !== 200) {
+      console.log('Blocked: ' + loginPageResponse.status);
+      return;
+    }
+    await context.evaluate(async function () {
+      const ACCOUNT_ID = '804999333';
+      document.getElementById('account_number').value = ACCOUNT_ID;
+    });
+    await context.clickAndWaitForNavigation('input[name="submit"]', {}, { timeout: 30000 });
+    await context.evaluate(async function () {
+      const ACCOUNT_PWD = 'bestway804';
+      document.getElementById('password').value = ACCOUNT_PWD;
+    });
+    await context.clickAndWaitForNavigation('#btn-login', {}, { timeout: 30000 });
+    await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true })
+  }
 };
