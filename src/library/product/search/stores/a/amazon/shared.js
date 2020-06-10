4eaacb329ff2cbb46e107957ca2daa3391f69598
David Skoglund <david.skoglund@import.io>
2020-06-10 14:36:08 -0700

    95. Updated transform,adjusted schema, needs  more changes.

--- src/library/product/search/stores/a/amazon/shared.js
+++ src/library/product/search/stores/a/amazon/shared.js
@@ -4,7 +4,10 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
+const transform = (data, context) => {
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
   for (const { group } of data) {
     for (const row of group) {
       if (row.aggregateRatingText) {
@@ -14,6 +17,14 @@ const transform = (data) => {
           },
         ];
       }
+      rankCounter = rankCounter + 1;
+      if (!row.sponsored) {
+        orgRankCounter = orgRankCounter + 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+      context.setState({ rankCounter });
+      context.setState({ orgRankCounter });
     }
   }
   return data;
