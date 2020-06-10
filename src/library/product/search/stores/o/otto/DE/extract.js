2d085b4000d7df8af08c66ca9a39f30648368abf
nikita.kumari <nikita.kumari@synerzip.com>
2020-06-10 17:11:06 +0530

    Code clean up

--- src/library/product/search/stores/o/otto/DE/extract.js
+++ src/library/product/search/stores/o/otto/DE/extract.js
@@ -1,4 +1,4 @@
-async function implementation(
+async function implementation (
   // @ts-ignore
   // @ts-ignore
   // @ts-ignore
@@ -11,7 +11,7 @@ async function implementation(
   const { productDetails } = dependencies;
   await context.evaluate(async () => {
     // @ts-ignore
-    function addHiddenDiv(id, content, index) {
+    function addHiddenDiv (id, content, index) {
       const newDiv = document.createElement('div');
       newDiv.id = id;
       newDiv.textContent = content;
@@ -19,24 +19,23 @@ async function implementation(
       const originalDiv = document.querySelectorAll("article[class*='product'] span[class*='name']")[index];
       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
     }
-    async function tet() {
-     let prevScroll = document.documentElement.scrollTop
+    async function infiniteScroll () {
+      let prevScroll = document.documentElement.scrollTop;
       while (true) {
-        window.scrollBy(0, document.documentElement.clientHeight)
-        await new Promise(r => setTimeout(r, 500));
-        let currentScroll = document.documentElement.scrollTop
+        window.scrollBy(0, document.documentElement.clientHeight);
+        await new Promise(resolve => setTimeout(resolve, 500));
+        const currentScroll = document.documentElement.scrollTop;
         if (currentScroll === prevScroll) {
-          break
+          break;
         }
-        prevScroll = currentScroll
+        prevScroll = currentScroll;
       }
     }
-    await tet();
+    await infiniteScroll();
     const product = document.querySelectorAll("script[type*='application/ld+json']");
-    console.log("Number of pages", product.length);
     for (let i = 0; i < product.length; i++) {
       // @ts-ignore
-      let node = (product[i]) ? JSON.parse(product[i].innerText) : '';
+      const node = (product[i]) ? JSON.parse(product[i].innerText) : '';
       if (node) {
         // @ts-ignore
         const aggregateRating = (node.aggregateRating && node.aggregateRating[0].ratingValue) ? node.aggregateRating[0].ratingValue : '';
@@ -45,10 +44,18 @@ async function implementation(
         // @ts-ignore
         const price = (node.offers && node.offers[0].price) ? node.offers[0].price : '';
         const url = window.location.href;
-        if (aggregateRating) addHiddenDiv('ii_aggregateRating', aggregateRating, i);
-        if (reviewCount) addHiddenDiv('ii_reviewCount', reviewCount, i);
-        if (price) addHiddenDiv('ii_price', price, i);
-        if (url) addHiddenDiv('ii_url', url, i);
+        if (aggregateRating) {
+          addHiddenDiv('ii_aggregateRating', aggregateRating, i);
+        };
+        if (reviewCount) {
+          addHiddenDiv('ii_reviewCount', reviewCount, i);
+        };
+        if (price) {
+          addHiddenDiv('ii_price', price, i);
+        };
+        if (url) {
+          addHiddenDiv('ii_url', url, i);
+        };
       }
     }
   });
