c2ef7e3e34328d4f331c2b7fa8da5d0c4a82682f
David Skoglund <david.skoglund@import.io>
2020-06-10 16:55:45 -0700

    #97.Edited extractor to handle transfrom logic. Checked schema fields.

--- src/library/product/search/stores/a/amazonFresh/US/extract.js
+++ src/library/product/search/stores/a/amazonFresh/US/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'amazonFresh',
-    transform: null,
+    transform: transform,
     domain: 'amazon.com',
   },
 };
---
+++ src/library/product/search/stores/a/amazonFresh/shared.js
@@ -0,0 +1,33 @@
+
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data, context) => {
+    const state = context.getState();
+    let orgRankCounter = state.orgRankCounter || 0;
+    let rankCounter = state.rankCounter || 0;
+    for (const { group } of data) {
+      for (const row of group) {
+        if (row.aggregateRatingText) {
+          row.aggregateRating = [
+            {
+              text: row.aggregateRatingText[0].text.replace(/ \D.*/, ''),
+            },
+          ];
+        }
+        rankCounter = rankCounter + 1;
+        if (!row.sponsored) {
+          orgRankCounter = orgRankCounter + 1;
+          row.rankOrganic = [{ text: orgRankCounter }];
+        }
+        row.rank = [{ text: rankCounter }];
+        context.setState({ rankCounter });
+        context.setState({ orgRankCounter });
+      }
+    }
+    return data;
+  };
+  
+  module.exports = { transform };
