fdb62a981a331c3b2bc96d4ff2ee672522469fcd
SushmaM <sushmam@sushmas-mbp.lan>
2020-06-10 11:12:45 -0700

    Add missing fields

--- src/library/product/search/stores/w/walgreens/US/extract.js
+++ src/library/product/search/stores/w/walgreens/US/extract.js
@@ -7,11 +7,15 @@ async function implementation (
   context,
   dependencies,
 ) {
+  console.log('awka');
+  console.log(inputs);
+  console.log(parameters);
+  console.log(dependencies);
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await context.evaluate(() => {
-    function addHiddenDiv (i, productCards) {
+  await context.evaluate(async function () {
+    function addHiddenDiv (i, productCards, productInfo) {
       const newDiv = document.createElement('div');
       newDiv.id = i;
       newDiv.className = 'extra-info';
@@ -34,7 +38,52 @@ async function implementation (
           newDiv.dataset.price = price.replace(re, '$1.$2');
         }
       }
+
+      console.log('HERE');
+      console.log(productInfo);
+      console.log(productInfo[i]);
+      console.log(productInfo[i].productInfo);
+      console.log(productInfo[i].productInfo.wic);
+
+      if (productInfo !== null) {
+        newDiv.dataset.id = productInfo[i].productInfo.wic;
+        newDiv.dataset.upc = productInfo[i].productInfo.upc;
+      }
       productCards.item(i).appendChild(newDiv);
+    }
+
+    const refURL = window.location.href;
+    const pageNum = parseInt(document.querySelector('.pagination').querySelector('input').value);
+
+    let productInfo = null;
+
+    const response = await fetch('https://www.walgreens.com/productsearch/v1/products/search', {
+      headers: {
+        accept: 'application/json, text/plain, */*',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json; charset=UTF-8',
+        pragma: 'no-cache',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+      },
+      referrer: refURL,
+      referrerPolicy: 'no-referrer-when-downgrade',
+      body: '{"p":' + pageNum + ',"s":24,"view":"allView","geoTargetEnabled":false,"abtest":["tier2","showNewCategories"],"deviceType":"desktop","q":"' + window.__APP_INITIAL_STATE__.search.searchString + '","requestType":"search","sort":"relevance","couponStoreId":"4372"}',
+      method: 'POST',
+      mode: 'cors',
+    });
+
+    if (response && response.status === 404) {
+      console.log('Product Not Found!!!!');
+    }
+
+    if (response && response.status === 200) {
+      console.log('Product Found!!!!');
+      const data = await response.json();
+      console.log(data);
+      productInfo = data.products;
     }
 
     const productCards = document.getElementsByClassName('wag-product-card-details');
@@ -44,7 +93,7 @@ async function implementation (
         // @ts-ignore
         document.getElementById(i).remove();
       }
-      addHiddenDiv(i, productCards);
+      addHiddenDiv(i, productCards, productInfo);
       i++;
     }
   });
