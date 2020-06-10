b17a01106219d33fe67d6705fb7ffb0904ca6d3d
Sindhusha balla <sindhusha.b@osmosys.asia>
2020-06-10 16:10:07 +0530

    added wait selector to fix nightmare error

--- src/library/navigation/domains/be/bestwaywholesale.co.uk.js
+++ src/library/navigation/domains/be/bestwaywholesale.co.uk.js
@@ -7,12 +7,12 @@ module.exports = {
     store: 'bestwaywholesale',
   },
   implementation: async ({ url }, parameterValues, context, dependencies) => {
-    let loginPageResponse = await context.goto('https://www.bestwaywholesale.co.uk/login-auth', {
+    const loginPageResponse = await context.goto('https://www.bestwaywholesale.co.uk/login-auth', {
       timeout: 10000,
       waitUntil: 'load',
       checkBlocked: true,
       js_enabled: true,
-      css_enabled: false
+      css_enabled: false,
     });
 
     if (loginPageResponse.status !== 200) {
@@ -23,12 +23,13 @@ module.exports = {
       const ACCOUNT_ID = '804999333';
       document.getElementById('account_number').value = ACCOUNT_ID;
     });
-    await context.clickAndWaitForNavigation('input[name="submit"]', {}, { timeout: 30000, waitUntil: 'load'});
+    await context.clickAndWaitForNavigation('input[name="submit"]', {}, { timeout: 30000, waitUntil: 'load' });
     await context.evaluate(async function () {
       const ACCOUNT_PWD = 'bestway804';
       document.getElementById('password').value = ACCOUNT_PWD;
     });
-    await context.clickAndWaitForNavigation('#btn-login', {}, { timeout: 50000, waitUntil: 'load' });
-    await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true })
-  }
+    await context.clickAndWaitForNavigation('#btn-login', {}, { timeout: 30000 });
+    await context.waitForSelector('div.main', { timeout: 50000 });
+    await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+  },
 };
--- src/library/product/details/stores/b/bestwaywholesale/UK/extract.js
+++ src/library/product/details/stores/b/bestwaywholesale/UK/extract.js
@@ -32,9 +32,9 @@ module.exports = {
       addMultipleNutritionalEle(multiNutriObj);
 
       // Add availability to the DOM
-      let available = getEleByXpath(`//div/p[contains(@class,"prodstock") or contains(.,'available')]`);
-      if(available) {
-        if(available.includes('In stock')) {
+      const available = getEleByXpath('//div/p[contains(@class,"prodstock") or contains(.,\'available\')]');
+      if (available) {
+        if (available.includes('In stock')) {
           addEleToDoc('availabilityText', 'In Stock');
         } else {
           addEleToDoc('availabilityText', 'Out of Stock');
