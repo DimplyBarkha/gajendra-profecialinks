744e3aa8e4683e6a4ea3ef727b42415b8f43592a
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-06-11 10:35:45 -0700

    cvs core search

--- src/library/product/details/stores/c/cvs/format.js
+++ src/library/product/details/stores/c/cvs/format.js
@@ -44,18 +44,22 @@ const transform = (data) => {
   const cleanUp = (data, context) => {
     let dataStr = JSON.stringify(data);
     console.log('INSIDE OF CLEANUP');
-    dataStr = dataStr.replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[^\x00-\x7F]/g, '');
+    if(dataStr) {
+      dataStr = dataStr.replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
+        .replace(/&amp;nbsp;/g, ' ')
+        .replace(/&amp;#160/g, ' ')
+        .replace(/\\u00A0/g, ' ')
+        .replace(/\s{2,}/g, ' ')
+        .replace(/"\s{1,}/g, '"')
+        .replace(/\s{1,}"/g, '"')
+        .replace(/^ +| +$|( )+/g, ' ')
+        // eslint-disable-next-line no-control-regex
+        .replace(/[^\x00-\x7F]/g, '');
 
-    return JSON.parse(dataStr);
+      return JSON.parse(dataStr);
+    } else {
+      return data
+    }
   };
 
   return cleanUp(data);
