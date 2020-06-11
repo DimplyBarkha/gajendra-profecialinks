aa68c33b021a3dcaeb7f0c9510ef3e802b290a4c
David Skoglund <david.skoglund@import.io>
2020-06-10 17:21:14 -0700

    #95. Updated extractor to  handle transform logic.

--- src/library/product/search/stores/a/amazon/US/extract.js
+++ src/library/product/search/stores/a/amazon/US/extract.js
@@ -1,10 +1,10 @@
 const { transform } = require('../shared');
-
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'amazon',
-    transform,
+    transform: transform,
+    domain: 'amazon.com',
   },
 };
