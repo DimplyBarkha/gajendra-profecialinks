b15b12e498fd3279bce363e26af0723e8c3995e3
vikas kyatannawar <49167922+vikas-kyatannawar@users.noreply.github.com>
2020-06-13 06:20:56 +0530

    removed unused code (#158)

--- src/library/product/search/stores/t/tesco/UK/execute.js
+++ src/library/product/search/stores/t/tesco/UK/execute.js
@@ -1,17 +1,3 @@
-async function addRankAndOrganicRank (context) {
-  // add rank and rank_organic attribute
-  await context.evaluate(() => {
-    const rowSelector = 'div.product-list--page.product-list--current-page > div > ul[data-auto="product-list"] > li';
-    const rows = Array.from(document.querySelectorAll(rowSelector));
-    const pageNumber = Number(document.querySelector('[name="current-results-page"] > span:not([class])').textContent);
-    const offSet = (pageNumber - 1) * 24;
-    for (let key = 0; key < rows.length; key++) {
-      const rank = (key + 1 + offSet).toString();
-      rows[key].setAttribute('rank', rank);
-      rows[key].setAttribute('rank_organic', rank);
-    }
-  });
-};
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -21,6 +7,5 @@ module.exports = {
     url: 'https://www.tesco.com/groceries/en-GB/search?query={searchTerms}',
     loadedSelector: 'div.product-image__container',
     noResultsXPath: '//div[@class="empty-section--heading"][contains(text(),"We didn\'t find anything for")]',
-    interactionFn: addRankAndOrganicRank,
   },
 };
