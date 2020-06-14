c026ebe77c45d41e9c9ccf6a007ba2d08238ae8f
David Skoglund <david.skoglund@import.io>
2020-06-13 18:29:22 -0700

    95. adjusted paginate.

--- src/library/product/search/stores/a/amazon/US/paginate.js
+++ src/library/product/search/stores/a/amazon/US/paginate.js
@@ -4,9 +4,12 @@ module.exports = {
     country: 'US',
     store: 'amazon',
     domain: 'amazon.com',
-    nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
-    mutationSelector: 'span[cel_widget_id="UPPER-RESULT_INFO_BAR"] div>span[dir="auto"]:first-of-type',
-    spinnerSelector: 'div.s-result-list-placeholder:not([class*="hidden"])',
+    // nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
+    // mutationSelector: 'span[cel_widget_id="UPPER-RESULT_INFO_BAR"] div>span[dir="auto"]:first-of-type',
+    // spinnerSelector: 'div.s-result-list-placeholder:not([class*="hidden"])',
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+    openSearchDefinition: {
+      template: 'https://www.amazon.com/s?k={searchTerms}&ref=nb_sb_noss_2&page={page}',
+    },
   },
 };
