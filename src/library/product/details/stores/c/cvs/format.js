c218a67483cdf09b8e600a2cd77e41ebfa87fc15
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-06-16 11:44:10 -0700

    added changes toformat.js for cvs

--- src/library/product/details/stores/c/cvs/format.js
+++ src/library/product/details/stores/c/cvs/format.js
@@ -5,25 +5,23 @@
  */
 const transform = (data) => {
   const cleanUp = (data, context) => {
-    let dataStr = JSON.stringify(data);
-    console.log('INSIDE OF CLEANUP');
-    if (dataStr) {
-      dataStr = dataStr.replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
-        .replace(/&amp;nbsp;/g, ' ')
-        .replace(/&amp;#160/g, ' ')
-        .replace(/\\u00A0/g, ' ')
-        .replace(/\s{2,}/g, ' ')
-        .replace(/"\s{1,}/g, '"')
-        .replace(/\s{1,}"/g, '"')
-        .replace(/^ +| +$|( )+/g, ' ')
-        // eslint-disable-next-line no-control-regex
-        .replace(/[^\x00-\x7F]/g, '');
-
-      return JSON.parse(dataStr);
-    } else {
-      return data;
-    }
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[^\x00-\x7F]/g, '');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
   };
+  
   for (const { group } of data) {
     for (let row of group) {
       try {
