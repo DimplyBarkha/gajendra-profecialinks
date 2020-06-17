5131e707eeac6598cda2e28745942882b5f3a28d
Nishat Bagwan <nishat.bagwan@synerzip.com>
2020-06-17 17:10:19 +0530

    #124. Changes for flaconi de core extended data

--- src/library/product/details/stores/f/flaconi/DE/extract.js
+++ src/library/product/details/stores/f/flaconi/DE/extract.js
@@ -30,10 +30,13 @@ module.exports = {
 
       const directionelement1 = document.evaluate("//div[contains(@class, 'instruction-content')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
       const directionelement2 = document.evaluate("//div[contains(@class, 'instruction')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      const bulletInfo = document.evaluate("//ul[@class='product-properties-list']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
       if (directionelement1) {
-        addElementToDocument('fl_directioninfo', directionelement1.innerText);
+        const decriptionContent = bulletInfo ? `${directionelement1.innerText} ${bulletInfo.innerText}` : directionelement1.innerText;
+        addElementToDocument('fl_directioninfo', decriptionContent);
       } else if (directionelement2) {
-        addElementToDocument('fl_directioninfo', directionelement2.innerText);
+        const decriptionContent = bulletInfo ? `${directionelement2.innerText} ${bulletInfo.innerText}` : directionelement2.innerText;
+        addElementToDocument('fl_directioninfo', decriptionContent);
       }
 
       const colorlement = document.evaluate("//ul[@id='makeup-color-list']/li[1]//span/@style", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
@@ -44,6 +47,19 @@ module.exports = {
       const videoarr = document.querySelectorAll('div.lazyYoutube > a[title=""]');
       if (videoarr && videoarr.length) {
         addVideoElementToDocument('pd_video', videoarr);
+      }
+      const brandElement = document.querySelector('h1 a.brand') ? (document.querySelector('h1 a.brand').innerText).trim() : '';
+      const nameElement = document.querySelector('h1 span.series') ? (document.querySelector('h1 span.series').innerText).trim() : '';
+      const describeElement = document.querySelector('h1 span.gender') ? (document.querySelector('h1 span.gender').innerText).trim() : '';
+      const str = brandElement + ' ' + nameElement + ' ' + describeElement;
+      addElementToDocument('fl_imageAlt', `${brandElement} ${nameElement} ${describeElement}`);
+      addElementToDocument('fl_extended', `${brandElement} Start ${describeElement}`);
+      addElementToDocument('fl_name', `${nameElement} ${describeElement}`);
+      const listPriceElement = document.evaluate("//ul[contains(@class,'product-list')]/li[contains(@class,'selected')or contains(@class,'visible')]//div[@class='price-box']/span[@class='old-price']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (listPriceElement && listPriceElement.innerText) {
+        const replaceChars = { '.': ',', ',': '.' };
+        const priceData = (listPriceElement.innerText).replace(/[./,]/g, function (match) { return replaceChars[match]; });
+        addElementToDocument('fl_listPrice', priceData);
       }
     });
     await context.extract(productDetails);
