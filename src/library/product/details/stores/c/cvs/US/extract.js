2ae51ea042c11875ccc3558a7acca383e6c7ce6a
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-06-17 11:36:33 -0700

    started updates to variant ids

--- src/library/product/details/stores/c/cvs/US/extract.js
+++ src/library/product/details/stores/c/cvs/US/extract.js
@@ -194,22 +194,6 @@ module.exports = {
         }
       }
 
-      const videoLinks = document.evaluate("//a[contains(@href,'VideoDialog')]/@href", document.cloneNode(true), null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
-      let videoLink = videoLinks.iterateNext();
-      while (videoLink) {
-        const resp = await fetch('https://www.parfumdreams.de' + videoLink.value, { method: 'GET' });
-        const respText = await resp.text();
-        if (respText) {
-          const startString = '<source type="video/mp4" src="';
-          const startIdx = respText.indexOf(startString);
-          let link = respText.substring(`${startIdx}${startString.length}`);
-          const endIdx = link.indexOf('">');
-          link = link.substring(0, endIdx);
-          addElementToDocument('pd_video', link);
-        }
-        videoLink = videoLinks.iterateNext();
-      }
-
       addHiddenDiv('ii_url', window.location.href);
       addHiddenDiv('ii_sku', skuFromUrl);
 
@@ -223,20 +207,45 @@ module.exports = {
       collectManufDesc();
     }, skuFromUrl);
 
-    async function collectVideo () {
-      // const secret = await context.evaluate(function () {
-      //   const ele = document.querySelector('video');
-      //   if (ele) {
-      //     const eleSrc = ele.getAttribute('src');
-      //     console.log('RETURNING SOURCE');
-      //     return eleSrc;
-      //   } else {
-      //     return 'COULD NOT FIND!!!!!!!!!!!!!!!!!!!!';
-      //   }
-      // }, [], 'iframe[title="Product Videos"]');
-      // console.log(secret);
-    }
-    collectVideo();
+  //   async function buttonCheck() {
+  //     return await context.evaluate(function() {
+  //        const buttons = document.querySelectorAll('div.swatch-scroll > div.css-1dbjc4n')
+  //        if(buttons != null) {
+  //          const variants = [];
+  //          buttons.forEach((variantBtn) => {
+  //           variants.push(variantBtn)
+  //          })
+  //            return variants;
+  //        } else {
+  //            return false
+  //        }
+  //    });
+  // }
+  
+      
+  
+  //   async function variantClick() {
+  //     const variantNums = [];
+  //     // const button = document.querySelector('div.css-1dbjc4n.r-1yd45rl.r-rs99b7.r-1loqt21.r-1bq2mok.r-1inuy60.r-1m04atk.r-1pyaxff.r-glunga.r-1otgn73.r-eafdt9.r-1i6wzkk.r-lrvibr')
+  //     // debugger
+  //     // context.click('div.css-1dbjc4n.r-1yd45rl.r-rs99b7.r-1loqt21.r-1bq2mok.r-1inuy60.r-1m04atk.r-1pyaxff.r-glunga.r-1otgn73.r-eafdt9.r-1i6wzkk.r-lrvibr')
+  //     if(buttonCheck()){
+  //       const btns = await buttonCheck()
+  //       debugger
+  //       btns.forEach((btn) => {
+  //         context.click(btn);
+  //         new Promise(resolve => setTimeout(resolve, 5000));
+  //       })
+  //     }
+  //   }
+  //   // const variantCollections = document.querySelectorAll('div.swatch-scroll > div.css-1dbjc4n');
+  //   // if(variantCollections) {
+  //   //   variantClick('div.swatch-scroll > div.css-1dbjc4n')
+  //   // }
+  //     await variantClick()
+    
+
+
 
     return await context.extract(productDetails, { transform: transformParam });
   },
