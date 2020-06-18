64fd9eab00b764c89538323a8c64f6c634686329
David Skoglund <david.skoglund@import.io>
2020-06-17 21:57:41 -0700

    #96. added extract.js logic to match core amazon.

--- src/library/product/details/stores/a/amazonFresh/US/extract.js
+++ src/library/product/details/stores/a/amazonFresh/US/extract.js
@@ -19,10 +19,10 @@ async function implementation (
     const variants = await context.evaluate(function () {
       const variantList = [];
       const elements = document.querySelectorAll('li[data-defaultasin]');
-      for (var i = 0; i < elements.length; i++) {
+      for (let i = 0; i < elements.length; i++) {
         console.log(i);
         const element = elements[i];
-        if (element === null) {
+        if (element == null) {
           continue;
         }
         const vasin = element.getAttribute('data-defaultasin');
@@ -36,21 +36,88 @@ async function implementation (
     return variants;
   };
 
+  async function buttonCheck () {
+    return await context.evaluate(function () {
+      const button = document.querySelector('#olpLinkWidget_feature_div span[data-action="show-all-offers-display"] a');
+      if (button != null) {
+        return 'true';
+      } else {
+        return 'false';
+      }
+    });
+  }
+
+  async function getLbb () {
+    const sellersShowButton = '#olpLinkWidget_feature_div span[data-action="show-all-offers-display"] a';
+    if (await buttonCheck() === 'true') {
+      const [response] = await Promise.all([
+        context.waitForNavigation({ timeout: 20000 }),
+        context.click(sellersShowButton),
+      ]);
+      const otherSellersDiv = 'div#all-offers-display div#aod-offer div[id*="aod-price"]';
+      await context.waitForSelector(otherSellersDiv, { timeout: 20000 });
+      return await context.evaluate(function () {
+        function addHiddenDiv (id, content) {
+          const newDiv = document.createElement('div');
+          newDiv.id = id;
+          newDiv.textContent = content;
+          newDiv.style.display = 'none';
+          document.body.appendChild(newDiv);
+        }
+        const firstCheck = document.querySelector('div#shipsFromSoldByInsideBuyBox_feature_div');
+        const otherSellers = document.querySelectorAll('div#aod-offer');
+        const price = document.querySelector('span#price_inside_buybox');
+        if (firstCheck && price) {
+          const priceText = parseFloat((price.innerText).slice(1));
+          if (firstCheck.innerText !== 'Ships from and sold by Amazon.com.' && otherSellers) {
+            otherSellers.forEach((seller) => {
+              const sellerPrice = seller.querySelector('span.a-offscreen').innerText;
+              const priceNum = parseFloat(sellerPrice.slice(1));
+              const shipsFrom = seller.querySelector('div#aod-offer-shipsFrom div.a-column.a-span9.a-span-last');
+              const soldBy = seller.querySelector('div#aod-offer-soldBy div.a-column.a-span9.a-span-last');
+              if (shipsFrom.innerText === 'Amazon.com' && soldBy.innerText === 'Amazon.com' && priceNum > priceText) {
+                addHiddenDiv('ii_lbb', 'YES');
+                addHiddenDiv('ii_lbbPrice', `${priceNum}`);
+              }
+            });
+          }
+        }
+      });
+    }
+  }
+
+  async function addUrl () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    let url = window.location.href;
+    const splits = url ? url.split('/') : [];
+    url = (splits.length > 0) ? splits[splits.length - 1] : '';
+    addHiddenDiv('added-asin', url);
+  }
+
+
+  const allVariants = [...new Set(await getVariants())];
+  await getLbb();
+  await context.evaluate(addUrl);
   console.log('getting variants');
-  const allVariants = await getVariants();
-  // if( allVariants.length > 1 ) {
-  //  allVariants.shift();
-  // }
   await context.extract(productDetails, { transform, type: 'APPEND' });
-  console.log(allVariants);
-  // start at 1 to skip the first variant which is this page
-  for (let i = 1; i < allVariants.length; i++) {
+  console.log('#### of Variants:', allVariants.length);
+  console.log('#### Variants:', allVariants);
+  for (let i = 0; i < allVariants.length; i++) {
     const id = allVariants[i];
     const url = await dependencies.createUrl({ id });
     await dependencies.goto({ url });
-
+    await context.evaluate(addUrl);
+    await getLbb();
     await context.extract(productDetails, { transform, type: 'APPEND' });
     const pageVariants = await getVariants();
+    console.log('#### of Variants:', allVariants.length);
+    console.log('#### Variants:', allVariants);
     for (let j = 0; j < pageVariants.length; j++) {
       const pageVariant = pageVariants[j];
       if (allVariants.indexOf(pageVariant) === -1) {
@@ -68,7 +135,7 @@ module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
-    store: 'amazonFresh',
+    store: 'amazon',
     transform: transform,
     domain: 'amazon.com',
   },
