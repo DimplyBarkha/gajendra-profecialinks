d8854ccecc99b275e6df834ec53f9f98cae15c93
Sanket Take <sanket.take@synerzip.com>
2020-06-19 16:39:13 +0530

    #189. Created extractor and ran it for 1 field.

---
+++ src/library/product/details/stores/a/amazonPrimePantry/US/execute.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonPrimePantry',
+    domain: 'amazon.com',
+    loadedSelector: '#title',
+    noResultsXPath: null,
+  },
+};
