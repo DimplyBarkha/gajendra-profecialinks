785243c9d9211085d60c53d11bbebef1d01906b1
Vikas Kyatannawar <vikas.k@osmosys.asia>
2020-06-19 13:02:35 +0530

    saving changes

--- src/library/navigation/domains/pr/primenow.amazon.com.js
+++ src/library/navigation/domains/pr/primenow.amazon.com.js
@@ -1,21 +1,44 @@
 
+// module.exports = {
+//   implements: 'navigation/goto',
+//   parameterValues: {
+//     domain: 'primenow.amazon.com',
+//     timeout: null,
+//     country: 'US',
+//     store: 'amazonPrimeNow_75204',
+//   },
+//   implementation: async (inputs, parameterValues, context, dependencies) => {
+// const url = `${inputs.url}`;
+// await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+// await context.waitForSelector('input#lsPostalCode');
+// await context.setInputValue('input#lsPostalCode', '75204');
+// await context.click('input.a-button-input');
+// await context.waitForNavigation();
+// await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+// await context.waitForNavigation();
+// await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+//   },
+// };
+
 module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'primenow.amazon.com',
     timeout: null,
     country: 'US',
-    store: 'amazonPrimeNow_75204',
+    store: 'amazonPrimeNow',
   },
+  // For navigating from home page to search page because we have to enter the zip code in  home page.
   implementation: async (inputs, parameterValues, context, dependencies) => {
     const url = `${inputs.url}`;
+    const zip = await context.primeZipCode.apply(context) || '75204';
     await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
     await context.waitForSelector('input#lsPostalCode');
-    await context.setInputValue('input#lsPostalCode', '75204');
+    await context.setInputValue('input#lsPostalCode', zip);
     await context.click('input.a-button-input');
     await context.waitForNavigation();
     await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
     await context.waitForNavigation();
     await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
   },
-};
+}
--- src/library/product/details/stores/a/amazonPrimeNow_75204/US/execute.js
+++ src/library/product/details/stores/a/amazonPrimeNow_75204/US/execute.js
@@ -1,11 +1,34 @@
 
+// module.exports = {
+//   implements: 'product/details/execute',
+//   parameterValues: {
+//     country: 'US',
+//     store: 'amazonPrimeNow_75204',
+//     domain: 'primenow.amazon.com',
+//   },
+// };
+
+const { implementation } = require('../../../../execute');
+
+async function localImplementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const primeZipCode = parameters.store.match(/\d{5}/g)[0];
+
+  context.primeZipCode = () => primeZipCode;
+
+  return await implementation(inputs, parameters, context, dependencies);
+}
+
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
     country: 'US',
     store: 'amazonPrimeNow_75204',
     domain: 'primenow.amazon.com',
-    loadedSelector: null,
-    noResultsXPath: null,
   },
+  implementation: localImplementation,
 };
