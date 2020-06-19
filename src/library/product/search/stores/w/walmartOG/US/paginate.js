7fd03bb92c009e4e685231770eb8582555855db7
Sashi Datla <sashi.datla@import.io>
2020-06-19 09:53:47 -0400

    extraction issues singleValue and regex

--- src/library/product/search/stores/w/walmartOG/US/paginate.js
+++ src/library/product/search/stores/w/walmartOG/US/paginate.js
@@ -14,3 +14,22 @@ module.exports = {
     domain: 'grocery.walmart.com',
   },
 };
+
+/*
+
+// Issue with pagination
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'US',
+    store: 'walmartOG',
+    nextLinkSelector: 'svg[data-automation-id="nextButton"]',
+    mutationSelector: null,
+    spinnerSelector: 'div[data-automation-id="spinner"]',
+    loadedSelector: 'div[data-automation-id="productsList"] div[data-automation-id="productTile"],div[data-automation-id="productsListPage"] [data-automation-id="noResultsSearchTerm"]',
+    openSearchDefinition: null,
+    domain: 'grocery.walmart.com',
+  },
+
+};
+  */
