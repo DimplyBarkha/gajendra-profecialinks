6fb9937edb4a0f071042efe286a970c83be78cfe
Mamatha Gundapaneni <mamatha.g@osmosys.asia>
2020-06-19 15:32:54 +0530

    Updated navigation file

--- src/library/navigation/domains/pr/primenow.amazon.com.js
+++ src/library/navigation/domains/pr/primenow.amazon.com.js
@@ -9,10 +9,13 @@ module.exports = {
   },
   implementation: async (inputs, parameterValues, context, dependencies) => {
     const url = `${inputs.url}`;
-    await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
-    await context.setInputValue('div.postalCodeColumn input#lsPostalCode', '98005');
-    await context.click('span.a-button-inner input.a-button-input');
     await context.goto(url, { timeout: 100000, waitUntil: 'load', checkBlocked: true });
+    await context.waitForNavigation();
+    await context.setInputValue('#lsPostalCode', '98005');
+    await context.click('span.a-button-inner input.a-button-input');
+    await context.waitForNavigation();
+    await context.goto(url);
+    await context.waitForNavigation();
     await context.waitForSelector('span#productTitle');
   },
 };
