964b69d51e5a550d532b0b37c71a9bfcf46593b9
SushmaM <sushmam@sushmas-mbp.lan>
2020-06-18 19:56:06 -0700

    Remove any script that runs survey

--- src/library/product/search/stores/c/cvs/US/extract.js
+++ src/library/product/search/stores/c/cvs/US/extract.js
@@ -1,5 +1,27 @@
 const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
 
+  await context.evaluate(async function () {
+    const nodes = document.querySelectorAll('script');
+    const nodesArr = Array.prototype.slice.call(nodes);
+    console.log(nodesArr);
+
+    for (let i = 0; i < nodesArr.length; i++) {
+      if ((nodesArr[i].hasAttribute('src') && nodesArr[i].getAttribute('src').indexOf('foresee') !== -1)) {
+        nodesArr[i].src = '/';
+        break;
+      }
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +30,5 @@ module.exports = {
     transform: transform,
     domain: 'cvs.com',
   },
+  implementation,
 };
