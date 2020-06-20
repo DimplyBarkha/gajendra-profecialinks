1c50d70cbd2cdb9443418e25157b14a9f2f09ea7
Lakshmi-Korlapati <lakshmi.k@osmosys.asia>
2020-06-20 19:21:26 +0530

    Added the other fields

--- src/library/product/details/stores/a/amazonPharmapacks/US/extract.js
+++ src/library/product/details/stores/a/amazonPharmapacks/US/extract.js
@@ -12,23 +12,6 @@ module.exports = {
         if(document.querySelector('[data-action="main-image-click"]')) {
             document.querySelector('[data-action="main-image-click"]').click();
         }
-      let productInfo = Array.from(document.querySelectorAll('#feature-bullets > ul > li > span')).map(elm => {
-        if (!elm.querySelector('#replacementPartsFitmentBulletInner')) {
-          const value = elm.textContent.trim();
-          return `${value}`;
-        }
-      }).filter(elm => elm).join(' || ');
-      productInfo = `${productInfo} ${document.querySelector('#productDescription>p') ? document.querySelector('#productDescription>p').textContent : ''}`;
-      document.body.setAttribute('additional_product_info', productInfo);
-
-      // Get additional feature bullets
-      const featureBullets = Array.from(document.querySelectorAll('#feature-bullets > ul > li > span')).map(elm => {
-        if (!elm.querySelector('#replacementPartsFitmentBulletInner')) {
-          const value = elm.textContent.trim();
-          return `${value}`;
-        }
-      }).filter(elm => elm).join(' || ');
-      document.body.setAttribute('feature_bullets', featureBullets);
     });
     await context.extract(productDetails);
   },
