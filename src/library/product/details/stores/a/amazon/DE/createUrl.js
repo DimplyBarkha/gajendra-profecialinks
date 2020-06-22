b849128d2cd3b021a5ee841ffbb3fe9c642ff46e
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-06-22 20:55:57 +0530

    Updated shema for the core extractor

--- src/library/product/details/stores/a/amazon/DE/createUrl.js
+++ src/library/product/details/stores/a/amazon/DE/createUrl.js
@@ -8,4 +8,16 @@ module.exports = {
     country: 'DE',
     store: 'amazon',
   },
+  implementation,
 };
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('URL+++' + `https://www.amazon.de/dp/${inputs.id}`);
+  return `https://www.amazon.de/dp/${inputs.id}`;
+  // TODO: Check for not found?
+}
