5b9703faa63bc8dd5cbbfec79bcabe23d342234d
satyam <satyam.rathod@synerzip.com>
2020-06-22 10:04:55 +0530

    #116 transfromations added for pricePerUnit and availability Text

--- src/library/product/details/stores/m/medikamente-per-klick/format.js
+++ src/library/product/details/stores/m/medikamente-per-klick/format.js
@@ -20,6 +20,20 @@ const transform = (data) => {
           alternateImage.text = `https://www.medikamente-per-klick.de${alternateImage.text}`.replace('/klein/', '/mittel/').replace('_k.', '_m.');
         });
       }
+      if (row.pricePerUnit) {
+        row.pricePerUnit.forEach(pricePerUnit => {
+          pricePerUnit.text = pricePerUnit.text.replace(/\./g, '').replace(',', '.');
+        });
+      }
+      if (row.availabilityText) {
+        row.availabilityText.forEach(availabilityText => {
+          if (availabilityText.text.includes('in_stock')) {
+            availabilityText.text = 'In Stock';
+          } else if (availabilityText.text.includes('out_of_stock')) {
+            availabilityText.text = 'Out of Stock';
+          }
+        });
+      }
     }
   }
   return data;
