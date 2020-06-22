f30c16e83d577a5f67bae103b478202f88c8e77b
Manas Ranjan <manas.n@osmosys.asia>
2020-06-22 22:42:30 +0530

    Added amazon prime source.

---
+++ src/library/product/details/stores/a/amazonPrimeNow/US/createUrl.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'primenow.amazon.com',
+    prefix: null,
+    url: null,
+    country: 'US',
+    store: 'amazonPrimeNow',
+  },
+};
---
+++ src/library/product/details/stores/a/amazonPrimeNow/US/details.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonPrimeNow',
+    domain: 'primenow.amazon.com',
+  },
+};
