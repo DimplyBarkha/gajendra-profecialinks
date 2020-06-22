eeeca7d4e89428661b27ebe3e8e8569e92e7b209
Sanket Take <sanket.take@synerzip.com>
2020-06-22 10:33:08 +0530

    #189. Added 5 fields including description and their transforms.

--- src/library/product/details/stores/a/amazonPrimePantry/shared.js
+++ src/library/product/details/stores/a/amazonPrimePantry/shared.js
@@ -1,20 +1,46 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const cleanUp = (data, context) => {
+    let dataStr = JSON.stringify(data);
+    console.log('INSIDE OF CLEANUP');
+    dataStr = dataStr.replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[^\x00-\x7F]/g, '');
+
+    return JSON.parse(dataStr);
+  };
   for (const { group } of data) {
-    for (const row of group) {
-      if (row.specifications) {
+    for (let row of group) {
+      if (row.nameExtended) {
         let text = '';
-        row.specifications.forEach(item => {
+        row.nameExtended.forEach(item => {
+          text += `${item.text.replace('Amazon.com:', '').replace(': Prime Pantry', '')}`;
+        });
+        row.nameExtended = [
+          {
+            text: cleanUp(text)
+          },
+        ];
+      }
+      if (row.description) {
+        let text = '';
+        row.description.forEach(item => {
           text += `${item.text.replace(/\n \n/g, ':')} || `;
         });
-        row.specifications = [
+        row.description = [
           {
-            text: text.slice(0, -4),
+            text: cleanUp(text.slice(0, -4)),
           },
         ];
       }
@@ -22,5 +48,4 @@ const transform = (data) => {
   }
   return data;
 };
-
 module.exports = { transform };
