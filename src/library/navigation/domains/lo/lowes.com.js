17e522432cfe74918a462bfdf786d09fd1c2e16e
Shreyam <shreyam.g@osmosys.asia>
2020-06-22 21:17:38 +0530

    lowes-us-core

---
+++ src/library/navigation/domains/lo/lowes.com.js
@@ -0,0 +1,9 @@
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+  domain: 'lowes.com',
+  timeout: '30000',
+  country: 'US',
+  store: 'lowes',
+  },
+  };
---
+++ src/library/product/details/stores/l/lowes/US/details.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'US',
+    store: 'lowes',
+    domain: 'lowes.com',
+  },
+};
