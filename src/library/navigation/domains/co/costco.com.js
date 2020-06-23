38c73e14e571cc040577fceccbef5d4bee358708
Ankit Kumar <ankit.k@osmosys.asia>
2020-06-23 17:06:02 +0530

    updated aggreegaterating to aggregaterating2 for workbench processing. updated script for page scrolling issue

--- src/library/navigation/domains/co/costco.com.js
+++ src/library/navigation/domains/co/costco.com.js
@@ -9,7 +9,7 @@ module.exports = {
   // For Setting up the location Zip code via cookie.
   implementation: async (inputs, parameterValues, context, dependencies) => {
     const url = `${inputs.url}`;
-    await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
     await context.evaluate(async function () {
       const zipCode = '98188';
       const cookieString = `invCheckPostalCode=${zipCode}; expires=Sun, 1 Jan 2099 00:00:00 UTC; path=/`;
@@ -17,9 +17,9 @@ module.exports = {
     });
     await context.waitForSelector('div.thumbnail p.description');
     // For scrolling down to the bottom of page to ensure full loaded page.
+    await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
     await context.evaluate(async function () {
       window.scrollTo(0, document.body.scrollHeight);
     });
-    await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
   },
 };
