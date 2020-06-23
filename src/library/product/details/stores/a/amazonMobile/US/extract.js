31458bbbd487f35a58b94574c538de4319e7c907
irfan shaikh <irfan.shaikh@synerzip.com>
2020-06-23 18:51:27 +0530

    #191. Added transform for five fields.

--- src/library/product/details/stores/a/amazonMobile/US/extract.js
+++ src/library/product/details/stores/a/amazonMobile/US/extract.js
@@ -1,10 +1,23 @@
+const { transform } = require('../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'amazonMobile',
-    transform: null,
+    transform,
     domain: 'amazon.com',
+    implementation: async (inputs,
+      parameters,
+      context,
+      dependencies) => {
+      const { transform } = parameters;
+      const { productDetails } = dependencies;
+      await context.evaluate(async function () {
+        const element = document.getElementById('detail-bullets');
+        element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+      });
+      return await context.extract(productDetails, { transform });
+    },
   },
 };
