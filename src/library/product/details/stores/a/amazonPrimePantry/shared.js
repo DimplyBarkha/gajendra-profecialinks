fa645ab44ab4360de5c29367fbf7de1c50d5e7b4
Sanket Take <sanket.take@synerzip.com>
2020-06-25 09:48:23 +0530

    #189. modified field for asin, imagesAlt

--- src/library/product/details/stores/a/amazonPrimePantry/shared.js
+++ src/library/product/details/stores/a/amazonPrimePantry/shared.js
@@ -43,11 +43,6 @@ const transform = (data) => {
           },
         ];
       }
-      if (row.alternateImages) {
-        row.alternateImages.forEach(item => {
-          item.text = `${item.text.replace('._SS40_', '')}`;
-        });
-      }
       if (row.ratingCount) {
         row.ratingCount.forEach(item => {
           item.text = `${item.text.replace(',', '')}`;
