9e81270bd36a1739b34bc1f42e2ded021fd61b36
satyam <satyam.rathod@synerzip.com>
2020-06-25 13:48:09 +0530

    #227 1.Added xpath for all fields in yaml. 2.added code for ranking in extract js

--- src/library/product/search/stores/h/homedepot/US/execute.js
+++ src/library/product/search/stores/h/homedepot/US/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'US',
     store: 'homedepot',
     domain: 'homedepot.com',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.homedepot.com/s/{searchTerms}',
+    loadedSelector: 'div[data-section="gridview"] > div[data-component="productpod"]',
+    noResultsXPath: '//h1[contains(@class,"results-nrf-hero__text")]',
   },
 };
--- src/library/product/search/stores/h/homedepot/US/extract.js
+++ src/library/product/search/stores/h/homedepot/US/extract.js
@@ -1,3 +1,30 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    document.querySelectorAll('div[data-section="gridview"]>div[data-component="productpod"]').forEach(node => {
+      const noOfResultPerPage = +node.getAttribute('data-podpageresultscount');
+      const prevPage = +node.getAttribute('data-pagenumber') - 1;
+      const nodePos = +node.getAttribute('data-pos') + 1;
+      let rank;
+      if (prevPage >= 1) {
+        rank = prevPage * noOfResultPerPage + nodePos;
+      } else {
+        rank = nodePos;
+      }
+      const newDiv = document.createElement('span');
+      newDiv.className = 'rank_org';
+      newDiv.textContent = rank.toString();
+      newDiv.style.display = 'none';
+      node.appendChild(newDiv);
+    });
+  });
+  return await context.extract(productDetails);
+}
 
 module.exports = {
   implements: 'product/search/extract',
@@ -7,4 +34,5 @@ module.exports = {
     transform: null,
     domain: 'homedepot.com',
   },
+  implementation,
 };
