bfdfbf6fc6d58c8adef62c7d8358dca4e42d23be
sushma-mangalapally <sushma.mangalapally@import.io>
2020-06-25 08:53:08 -0700

    Got rid of other unused code and clean up search query

--- src/library/product/search/stores/w/walgreens/US/extract.js
+++ src/library/product/search/stores/w/walgreens/US/extract.js
@@ -49,43 +49,13 @@ async function implementation (
       }
     }
 
-    async function fetchProducts (pageNum) {
-      const refURL = window.location.href;
-      const search = document.querySelector('.c-list-view-settings__list-info').querySelectorAll('strong')[1].textContent;
-
-      const response = await fetch('https://search.unbxd.io/ed39b885dbb44eca3c2782623eb2ba9d/www-foodservicedirect-com-805741548149387/search?q=' + search + '&rows=32&format=json&f.categoryPath.nameId=false&filter=product_websites:%22base%22&start=' + pageNum, {
-        headers: {
-          accept: '*/*',
-          'accept-language': 'en-US,en;q=0.9',
-          'sec-fetch-dest': 'script',
-          'sec-fetch-mode': 'no-cors',
-          'sec-fetch-site': 'cross-site',
-        },
-        referrer: refURL,
-        referrerPolicy: 'no-referrer-when-downgrade',
-        body: null,
-        method: 'GET',
-        mode: 'cors',
-      });
-
-      if (response && response.status === 404) {
-        console.log('Product Not Found!!!!');
-      }
-
-      if (response && response.status === 200) {
-        console.log('Product Found!!!!');
-        const data = await response.json();
-        addAllHiddenDiv(data.response.products);
-        return data;
-      }
-    }
-
     const refURL = window.location.href;
     let productInfo = null;
     const pageNum = (document.querySelector('.pagination') && document.querySelector('.pagination').querySelector('input')) ? parseInt(document.querySelector('.pagination').querySelector('input').value) : 1;
-    const searchQuery = (document.querySelector('.product-search-fullview') && document.querySelector('.product-search-fullview').querySelector('h1')) ? document.querySelector('.product-search-fullview').querySelector('h1').textContent : window.__APP_INITIAL_STATE__.search.searchString;
+    const searchQuery = window.__APP_INITIAL_STATE__.search.searchString ? window.__APP_INITIAL_STATE__.search.searchString : ((document.querySelector('.product-search-fullview') && document.querySelector('.product-search-fullview').querySelector('h1')) ? document.querySelector('.product-search-fullview').querySelector('h1').textContent.replace(/"/g, '') : '');
     let bodyParser = '{"p":' + pageNum + ',"s":24,"view":"allView","geoTargetEnabled":false,"abtest":["tier2","showNewCategories"],"deviceType":"desktop","q":"' + searchQuery + '","requestType":"search","sort":"relevance","couponStoreId":"4372"}';
     console.log(window.__APP_INITIAL_STATE__.search);
+    // For redirect link
     if (Object.keys(window.__APP_INITIAL_STATE__.search).length === 0) {
       console.log('OVER HERE');
       const conID = (window.__APP_INITIAL_STATE__.searchResult.filterInfo.id);
@@ -122,19 +92,15 @@ async function implementation (
       productInfo = data.products;
     }
 
-    // function addAllHiddenDiv (productInfo) {
-      const productCards = document.getElementsByClassName('wag-product-card-details');
-      let i = 0;
-      while (i < productCards.length) {
-        if (productCards.item(i).querySelectorAll('.extra-info').length > 0) {
-          document.getElementById(i.toString()).remove();
-        }
-        addHiddenDiv(i, productCards, productInfo);
-        i++;
+    const productCards = document.getElementsByClassName('wag-product-card-details');
+    let i = 0;
+    while (i < productCards.length) {
+      if (productCards.item(i).querySelectorAll('.extra-info').length > 0) {
+        document.getElementById(i.toString()).remove();
       }
-    // }
-
-    // fetchProducts()
+      addHiddenDiv(i, productCards, productInfo);
+      i++;
+    }
   });
   return await context.extract(productDetails, { transform });
 }
