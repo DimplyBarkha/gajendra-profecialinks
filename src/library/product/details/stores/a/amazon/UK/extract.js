e688c40ec390d01435480a192c91780a94c51d3f
nikita.kumari <nikita.kumari@synerzip.com>
2020-06-25 15:27:01 +0530

    Scroll to images #190

--- src/library/product/details/stores/a/amazon/UK/extract.js
+++ src/library/product/details/stores/a/amazon/UK/extract.js
@@ -1,5 +1,25 @@
 const { transform } = require('./format');
-
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async () => {
+    var element = document.querySelectorAll("div[cel_widget_id*='aplus'] img");
+    if (element) {
+      element.forEach(async (node) => {
+        node && node.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+        await new Promise((resolve) => {
+          setTimeout(resolve, 20);
+        });
+      });
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,4 +28,5 @@ module.exports = {
     transform,
     domain: 'amazon.co.uk',
   },
+  implementation,
 };
