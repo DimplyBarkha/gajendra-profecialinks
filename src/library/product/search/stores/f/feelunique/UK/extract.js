05ebb7ab4a5c56a527da51e4158a466ea56fc7fe
sourabhk <sourabh.karmarkar@synerzip.com>
2020-06-25 14:03:42 +0530

    #226 Added correction code for feelunique.com search extractor

--- src/library/product/search/stores/f/feelunique/UK/extract.js
+++ src/library/product/search/stores/f/feelunique/UK/extract.js
@@ -7,15 +7,23 @@ async function implementation (
   dependencies,
 ) {
   const { productDetails } = dependencies;
-  await context.evaluate(async function () {
-    function addElementToDocument (doc, key, value) {
-      const catElement = document.createElement('div');
-      catElement.id = key;
-      catElement.textContent = value;
-      catElement.style.display = 'none';
-      doc.appendChild(catElement);
-    }
-  });
+  // await context.evaluate(() => {
+  //   function addHiddenDiv (id, content, productDivRef) {
+  //     const newDiv = document.createElement('div');
+  //     newDiv.id = id;
+  //     newDiv.textContent = content;
+  //     newDiv.style.display = 'none';
+  //     productDivRef.parentNode.insertBefore(newDiv, productDivRef);
+  //   }
+  //   const product = document.querySelectorAll('section[class*="products-grid"] article figure');
+  //   const rankSelector = document.querySelector('div[id="pagination"] li[class="current"]');
+  //   // @ts-ignore
+  //   let rank = rankSelector ? Number(rankSelector.innerText) : 0;
+  //   if (rank) rank = (rank - 1) * 60 + 1;
+  //   for (let i = 0; i < product.length; i++) {
+  //     addHiddenDiv(`ii_rankOrganic_${rank}`, rank++, product[i]);
+  //   }
+  // });
   return await context.extract(productDetails, { transform: parameters.transform });
 }
 
--- src/library/product/search/stores/f/feelunique/transform.js
+++ src/library/product/search/stores/f/feelunique/transform.js
@@ -4,14 +4,29 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  let rank = 1;
   for (const { group } of data) {
     for (const row of group) {
       if (row.productUrl) {
         row.productUrl.forEach(productUrl => {
-          console.log('productUrl ---> ', productUrl);
           if (!productUrl.text.includes('https://www.feelunique.com')) {
             productUrl.text = `https://www.feelunique.com${productUrl.text}`;
           }
+        });
+      }
+
+      if (row.thumbnail) {
+        row.thumbnail.forEach(thumbnail => {
+          if (!thumbnail.text.includes('https')) {
+            thumbnail.text = `https:${thumbnail.text}`;
+          }
+        });
+      }
+
+      if (row.rankOrganic) {
+        row.rankOrganic.forEach(rankOrganic => {
+          rankOrganic.text = rank;
+          rank++;
         });
       }
     }
