ef093dfb5c428acb4987aadca1d7e0a21f0fb559
Vikas Kyatannawar <vikas.k@osmosys.asia>
2020-06-25 13:56:48 +0530

    updated goto

--- src/library/navigation/domains/be/bestwaywholesale.co.uk.js
+++ src/library/navigation/domains/be/bestwaywholesale.co.uk.js
@@ -21,12 +21,20 @@ module.exports = {
       }
       await context.waitForNavigation();
     };
+    const redirectToProductPage = async function (context) {
+      const isProductPage = await context.evaluate((selector) => !!document.querySelector(selector), 'div.productpagedetail');
+      if (!isProductPage) {
+        await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+        await context.waitForNavigation();
+        await clickPopup(context);
+      }
+    };
 
     await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
 
     await context.waitForSelector('div.productpagedetail', { timeout: 50000 });
 
-    const isLoggedIn = await context.evaluate((selector) => !!document.querySelector(selector), 'a.signin');
+    const isLoggedIn = await context.evaluate((selector) => !!document.querySelector(selector), 'p.prodlogin');
 
     if (!isLoggedIn) {
       console.log('Aleady logged in');
@@ -36,35 +44,31 @@ module.exports = {
 
     await clickPopup(context);
 
-    await context.clickAndWaitForNavigation('a.signin', {}, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    await context.goto('https://www.bestwaywholesale.co.uk/login-auth', { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    await context.waitForNavigation();
 
     const loginLoaded = await context.evaluate((selector) => !!document.querySelector(selector), '#account_number');
 
     if (!loginLoaded) {
+      await redirectToProductPage();
       return;
     }
 
     const ACCOUNT_ID = '804999333';
     await context.setInputValue('#account_number', ACCOUNT_ID);
 
-    await context.clickAndWaitForNavigation('input[name="submit"]', {}, { timeout: 50000, waitUntil: 'load' });
-
+    await context.click('input[name="submit"]');
+    await context.waitForNavigation();
     await context.waitForFunction(function () {
       // @ts-ignore
       return document.querySelector('#username').value === 'supplierlogin@bestway.co.uk';
     }, { timeout: 10000 });
     const ACCOUNT_PWD = 'bestway804';
     await context.setInputValue('#password', ACCOUNT_PWD);
-    await context.clickAndWaitForNavigation('#btn-login', {}, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    await context.click('#btn-login');
+    await context.waitForNavigation();
 
     await clickPopup(context);
-
-    const isProductPage = await context.evaluate((selector) => !!document.querySelector(selector), 'div.productpagedetail');
-
-    if (!isProductPage) {
-      await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
-      await context.waitForNavigation();
-      await clickPopup(context);
-    }
+    await redirectToProductPage();
   },
 };
