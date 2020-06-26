2045f6883372c964d83accd43b2d8b348ed97704
alscotty <alawrencescott@gmail.com>
2020-06-26 15:35:36 -0700

    new paginate xpaths

--- src/library/product/search/stores/k/kroger/US/paginate.js
+++ src/library/product/search/stores/k/kroger/US/paginate.js
@@ -1,70 +1,3 @@
-/**
- *
- * @param {{
- *  keywords: string,
- *  page: number,
- *  offset: number,
- * }} inputs
- * @param {{
- *  nextLinkSelector: string,
- *  mutationSelector: string,
- *  loadedSelector: string,
- *  spinnerSelector: string,
- *  openSearchDefinition: { template: string, indexOffset?: number, pageOffset?: number }
- * }} parameters
- * @param { ImportIO.IContext } context
- * @param { Record<string, any> } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { keywords, page, offset } = inputs;
-  const { nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
-
-  if (nextLinkSelector) {
-    const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector), nextLinkSelector);
-    if (!hasNextLink) {
-      return false;
-    }
-  }
-
-  const { pager } = dependencies;
-  const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
-  if (success) {
-    return true;
-  }
-
-  let url = await context.evaluate(function () {
-    /** @type { HTMLLinkElement } */
-    const next = document.querySelector('head link[rel="next"]');
-    if (!next) {
-      return false;
-    }
-    return next.href;
-  });
-
-  if (!url && openSearchDefinition) {
-    url = openSearchDefinition.template
-      .replace('{searchTerms}', encodeURIComponent(keywords))
-      .replace('{page}', (page + (openSearchDefinition.pageOffset || 0)).toString())
-      .replace('{offset}', (offset + (openSearchDefinition.indexOffset || 0)).toString());
-  }
-
-  if (!url) {
-    return false;
-  }
-
-  console.log('Going to url', url);
-  await context.goto(url.toString());
-  if (parameters.loadedSelector) {
-    await context.waitForSelector(parameters.loadedSelector);
-  }
-  return true;
-}
-
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
@@ -72,12 +5,13 @@ module.exports = {
     store: 'kroger',
     // loadedSelector: '.PaginateItems, p[class*="no-query-results"]',
     loadedSelector: '.PaginateItems',
-    nextLinkSelector: 'li.Pagination-item.Pagination-next a',
-    noResultsXpath: '//p[contains(@class,"no-query-results")]',
+    // nextLinkSelector: 'li.Pagination-item.Pagination-next a',
+    // noResultsXpath: '//p[contains(@class,"no-query-results")]',
+    // noResultsXpath: '//label[contains(@class,"kds-LoadingSpinner")]',
+    noResultsXpath: 'count(//div[contains(@class,"AutoGrid-cell")])=0',
     openSearchDefinition: {
       template: 'https://www.kroger.com/pl/all/00?fulfillment=all&query={searchTerms}&page={page}&searchType=natural',
     },
     domain: 'kroger.com',
-    implementation,
   },
 };
