00e3163f2346c4f68ad3530668a00201298c666a
nagajyothi.cp <nagajyothi.cp@osmosys.asia>
2020-06-26 15:07:27 +0530

    Updated schema

--- src/library/product/details/stores/a/amazonRuna/US/extract.js
+++ src/library/product/details/stores/a/amazonRuna/US/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require('../shared');
+const { transform } = require('../../amazonPharmapacks/US/shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -6,13 +6,5 @@ module.exports = {
     store: 'amazonRuna',
     transform,
     domain: 'amazon.com',
-  },
-  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
-    await context.evaluate(async function () {
-      if (document.querySelector('[data-action="main-image-click"]')) {
-        document.querySelector('[data-action="main-image-click"]').click();
-      }
-    });
-    await context.extract(productDetails);
   },
 };
