9514a62392c1ed7f65d8985ec8dbc356a2cac71c
sushma-mangalapally <sushma.mangalapally@import.io>
2020-06-26 16:24:22 -0700

    Create transform file for Walmart OG core

---
+++ src/library/product/details/stores/w/walmartOG/US/transform.js
@@ -0,0 +1,46 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  for (const { group } of data) {
+    for (const row of group) {
+      try {
+        console.log('description!!!');
+        console.log(row.description);
+        if (row.description) {
+          row.description.forEach(item => {
+            if (item.text.match('\\n')) {
+              item.text = item.text.replace(/\s\n/g, '||').trim();
+            }
+          });
+        }
+        console.log(row.description);
+        if (row.additionalDescBulletInfo && row.additionalDescBulletInfo[0].text.length > 1) {
+          row.additionalDescBulletInfo[0].text = row.additionalDescBulletInfo[0].text.startsWith(' || ') ? row.additionalDescBulletInfo[0].text : ' || ' + row.additionalDescBulletInfo[0].text;
+        }
+
+        Object.keys(row).forEach(header => row[header].forEach(el => {
+          el.text = clean(el.text);
+        }));
+      } catch (exception) { console.log('Error in transform', exception); }
+    }
+  }
+  return data;
+};
+
+module.exports = { transform };
