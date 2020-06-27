b4a994ab839dd54100ffca2ab477e8d8907b947c
Akansha <akanshad.wajge@gmail.com>
2020-06-27 22:50:49 +0530

    rank transform added (#235)

--- src/library/product/search/stores/a/amazonLg/US/extract.js
+++ src/library/product/search/stores/a/amazonLg/US/extract.js
@@ -1,8 +1,10 @@
+const { transform } = require('./shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'amazonLg',
+    transform: transform,
     domain: 'amazon.com',
   },
   implementation,
@@ -24,34 +26,10 @@ async function implementation (
       node.appendChild(newDiv);
     }
     const url = window.location.href;
-    const pageNum = url.match(/\d+/g) ? url.match(/\d+/g).map(Number) : '';
-    let rank = pageNum ? pageNum[0] : '';
-    console.log('pageNumber: ', rank);
-    const plength = document.querySelectorAll('span[cel_widget_id="MAIN-SEARCH_RESULTS"]');
-    const productLength = plength ? plength.length : '';
-    console.log('productLength: ', productLength);
-    if (!rank || (rank === 1)) {
-      rank = 0;
-      // @ts-ignore
-      await localStorage.setItem('rank', rank);
-    } else if (rank === 1) {
-      // @ts-ignore
-      rank = await localStorage.getItem(productLength);
-    } else if (rank > 1) {
-      // @ts-ignore
-      rank = await localStorage.getItem('rank');
-      rank = await Number(rank);
-    }
     document.querySelectorAll('span[cel_widget_id="MAIN-SEARCH_RESULTS"]').forEach(node => {
-      // @ts-ignore
-      ++rank;
-      addHiddenDiv(node, `ii_rank_${rank}`, rank);
       addHiddenDiv(node, 'ii_url', url);
       addHiddenDiv(node, 'ii_pageTimestamp', (new Date()).toISOString().replace(/[TZ]/g, ' '));
-      console.log('rank: ', rank);
     });
-    // @ts-ignore
-    await localStorage.setItem('rank', rank);
   });
-  return await context.extract(productDetails);
+  return await context.extract(productDetails, { transform });
 }
