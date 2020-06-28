32ff2f6532c6ed4056fe54e054b25b83f7e2dc4d
nikita.kumari <nikita.kumari@synerzip.com>
2020-06-28 15:01:43 +0530

    Updated few xpaths ans selector(#239)

--- src/library/product/search/stores/a/amazon/UK/execute.js
+++ src/library/product/search/stores/a/amazon/UK/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'UK',
     store: 'amazon',
     domain: 'amazon.co.uk',
-    url: 'https://www.amazon.co.uk/s?k={searchTerms}',
+    url: 'https://www.amazon.co.uk/s?k={searchTerms}&ref=nb_sb_noss',
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
     noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")]',
   },
--- src/library/product/search/stores/a/amazon/UK/paginate.js
+++ src/library/product/search/stores/a/amazon/UK/paginate.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'UK',
     store: 'amazon',
-    nextLinkSelector: 'ul.a-pagination > li.a-last a',
+    nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
     // mutationSelector: 'span[cel_widget_id="UPPER-RESULT_INFO_BAR"] div>span[dir="auto"]:first-of-type',
     spinnerSelector: null,
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
