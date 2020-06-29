4f438e8d97dbe70e89810447f5f4d287c7b40d33
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-06-29 14:00:12 +0530

    Updated parameters

--- src/library/product/details/stores/a/auchandrive/FR/execute.js
+++ src/library/product/details/stores/a/auchandrive/FR/execute.js
@@ -1,33 +1,35 @@
-async function implementation(
+async function implementation (
   inputs,
   parameters,
   context,
   dependencies,
 ) {
   const { url, id } = inputs;
-  const cookies = [
-    { name: "gtm_store_info", value: "ALLAR SERVICES EDF|98731" },
-    { name: "auchanCook", value: "98731|" }
-  ];
+  // const cookies = [
+  //   { name: "gtm_store_info", value: "ALLAR SERVICES EDF|98731" },
+  //   { name: "auchanCook", value: "98731|" }
+  // ];
 
   const gotoOptions = {
     timeout: 10000,
     waitUntil: 'load',
     checkBlocked: true,
-    cookies
+    // cookies
   };
 
   // // Need to load the webpage first to start a session
-  await context.goto('https://www.auchandrive.fr/recherche/');
-  await context.setCookie(...cookies);
-
+  // await context.goto('https://www.auchandrive.fr/recherche/');
+  // await context.setCookie(...cookies);
+  let productUrl = '';
   if (id) {
-    const newUrl = 'https://www.auchandrive.fr/recherche/' + id;
+    const newUrl = 'https://www.auchandrive.fr/recherche/' + id + '#[!opt!]{"cookie_jar":[{"name":"connect.sid","value":"s%3AZXKHRVjDk2txSj8MzXQky8VZuhmTubgs.M6oBh6HGNeRWLesfG%2Bcu6No9ij6ejEqB6mVRhSQGHJs"}]}[/!opt!]';
     await context.goto(newUrl, gotoOptions);
     return await context.clickAndWaitForNavigation('a.product-item__main', undefined, { waitUntil: 'networkidle0' });
+  } else {
+    productUrl = url + '#[!opt!]{"cookie_jar":[{"name":"connect.sid","value":"s%3AZXKHRVjDk2txSj8MzXQky8VZuhmTubgs.M6oBh6HGNeRWLesfG%2Bcu6No9ij6ejEqB6mVRhSQGHJs"}]}[/!opt!]';
   }
 
-  await context.goto(url, gotoOptions);
+  await context.goto(productUrl, gotoOptions);
 
   // TODO: Check for not found?
 }
@@ -38,5 +40,5 @@ module.exports = {
     store: 'auchandrive',
     domain: 'auchandrive.fr',
   },
-  implementation
+  implementation,
 };
