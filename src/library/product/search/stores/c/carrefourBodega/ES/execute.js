cf113c31a0a3185585f6dcb4521b90f4b35ccf46
nikita.kumari <nikita.kumari@synerzip.com>
2020-06-29 17:34:57 +0530

    Created Extractor Basic setup(#261)

---
+++ src/library/product/search/stores/c/carrefourBodega/ES/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'carrefourBodega',
+    domain: 'carrefour.es',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
