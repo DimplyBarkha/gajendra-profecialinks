7064ec56cbbfa3aff580747644ad3fb60a18a098
Akansha <akanshad.wajge@gmail.com>
2020-06-29 22:00:08 +0530

    noResultsXPath added (#260)

--- src/library/product/search/stores/a/amazonMweb/FR/paginate.js
+++ src/library/product/search/stores/a/amazonMweb/FR/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
     mutationSelector: null,
     spinnerSelector: null,
-    noResultsXPath: null, // noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")]',
+    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")]',
     openSearchDefinition: null,
     domain: 'amazon.fr',
     zipcode: '',
