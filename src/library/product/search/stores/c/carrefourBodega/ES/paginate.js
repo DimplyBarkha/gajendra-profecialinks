77515a90c3396bf89c22a5bcd46a6561ba6ea83a
nikita.kumari <nikita.kumari@synerzip.com>
2020-06-29 18:33:29 +0530

    Updated xpath, input file, mutation selector(#261)

--- src/library/product/search/stores/c/carrefourBodega/ES/paginate.js
+++ src/library/product/search/stores/c/carrefourBodega/ES/paginate.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'ES',
     store: 'carrefourBodega',
     nextLinkSelector: "div[class='js-next-preference']  a[title*='Ver m']",
-    mutationSelector: "div[class*='item-page']",
+    mutationSelector: "div[class*='content-product ']",
     spinnerSelector: null,
     // loadedSelector: "div[class*='item-page'] article.item",
     noResultsXPath: "//h1[@id='search_results' and contains(.,'(0)')]",
