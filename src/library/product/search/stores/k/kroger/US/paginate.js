dc7164e3099dafcb18de6467c21aa8b1dfe5f9ec
alscotty <alawrencescott@gmail.com>
2020-06-29 10:06:34 -0700

    fix search dedups

--- src/library/product/search/stores/k/kroger/US/paginate.js
+++ src/library/product/search/stores/k/kroger/US/paginate.js
@@ -3,15 +3,12 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'kroger',
-    // loadedSelector: '.PaginateItems, p[class*="no-query-results"]',
     loadedSelector: '.PaginateItems',
-    // nextLinkSelector: 'li.Pagination-item.Pagination-next a',
-    // noResultsXpath: '//p[contains(@class,"no-query-results")]',
-    // noResultsXpath: '//label[contains(@class,"kds-LoadingSpinner")]',
-    noResultsXpath: 'count(//div[contains(@class,"AutoGrid-cell")])=0',
-    openSearchDefinition: {
-      template: 'https://www.kroger.com/pl/all/00?fulfillment=all&query={searchTerms}&page={page}&searchType=natural',
-    },
+    nextLinkSelector: 'li.Pagination-item.Pagination-next:not(.is-disabled)> a',
+    // openSearchDefinition: {
+    //   template: 'https://www.kroger.com/pl/all/00?fulfillment=all&query={searchTerms}&page={page}&searchType=natural',
+    // },
+    openSearchDefinition: null,
     domain: 'kroger.com',
   },
 };
