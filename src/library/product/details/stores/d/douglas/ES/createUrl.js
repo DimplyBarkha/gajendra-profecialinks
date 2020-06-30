00adca474cb3c7def31e6119bff504b717fc747e
Nilesh <nilesh.b@osmosys.asia>
2020-06-30 21:27:16 +0530

    Fixed all the issue.

---
+++ src/library/product/details/stores/d/douglas/ES/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'douglas.es',
+    prefix: null,
+    url: null,
+    country: 'ES',
+    store: 'douglas',
+    zipcode: '',
+  },
+};
