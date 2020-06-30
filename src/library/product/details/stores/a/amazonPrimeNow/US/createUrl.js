defa08b37c70c7f91533490b4fdcd0b0da5437ce
Mamatha Gundapaneni <mamatha.g@osmosys.asia>
2020-06-30 22:57:32 +0530

    Added amazon prime source

---
+++ src/library/product/details/stores/a/amazonPrimeNow/US/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'primenow.amazon.com',
+    prefix: null,
+    url: 'https://primenow.amazon.com/dp/{id}',
+    country: 'US',
+    store: 'amazonPrimeNow',
+    zipcode: '75204',
+  },
+};
---
+++ src/library/product/details/stores/a/amazonPrimeNow/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonPrimeNow',
+    transform: null,
+    domain: 'primenow.amazon.com',
+    zipcode: '75204',
+  },
+};
