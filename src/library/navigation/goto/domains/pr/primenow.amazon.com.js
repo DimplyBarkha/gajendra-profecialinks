0c43cdcbecd1c848f856dcdec2dcde6c9de139c9
Manoj <manoj.d@osmosys.asia>
2020-07-01 12:36:16 +0530

    updated goto

--- src/library/navigation/goto/domains/pr/primenow.amazon.com.js
+++ src/library/navigation/goto/domains/pr/primenow.amazon.com.js
@@ -2,7 +2,6 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'primenow.amazon.com',
-    timeout: null,
     country: 'US',
     store: 'amazonPrimeNow',
   },
