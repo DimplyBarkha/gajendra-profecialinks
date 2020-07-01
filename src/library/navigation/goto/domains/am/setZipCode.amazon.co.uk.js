64db52562b7da15eb4159b23b22477907b192a0b
nikita.kumari <nikita.kumari@synerzip.com>
2020-07-01 18:18:23 +0530

    Zipcode Changes

--- src/library/navigation/goto/domains/am/setZipCode.amazon.co.uk.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.co.uk.js
@@ -1,4 +1,25 @@
-
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { zipcode } = inputs;
+  await context.waitForSelector('#nav-packard-glow-loc-icon');
+  await context.click('#nav-packard-glow-loc-icon');
+  await context.waitForSelector('input#GLUXZipUpdateInput');
+  try {
+    await context.click('a#GLUXChangePostalCodeLink');
+  } catch (error) {
+    console.log('Element not visible');
+  }
+  await context.setInputValue('input#GLUXZipUpdateInput', zipcode);
+  await context.waitForSelector('#GLUXZipUpdate input');
+  await context.click('#GLUXZipUpdate input');
+  // await context.waitForSelector('button[name="glowDoneButton"]');
+  // await context.click('button[name="glowDoneButton"]');
+  await context.waitForNavigation();
+}
 module.exports = {
   implements: 'navigation/goto/setZipCode',
   parameterValues: {
@@ -7,4 +28,5 @@ module.exports = {
     store: 'amazon',
     zipcode: 'SW1P 3EU',
   },
+  implementation,
 };
