6cb28c5f95dea485ed41024693f463c5a7cded7b
sourabhk <sourabh.karmarkar@synerzip.com>
2020-07-01 10:56:34 +0530

    #241 Added captcha solve code and minor xpath corrections

--- src/library/product/details/stores/a/amazon/FR/createUrl.js
+++ src/library/product/details/stores/a/amazon/FR/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'amazon.fr',
     prefix: 'dp',
-    url: 'https://amazon.com/dp/{id}',
+    url: 'https://amazon.fr/dp/{id}',
     country: 'FR',
     store: 'amazon',
   },
