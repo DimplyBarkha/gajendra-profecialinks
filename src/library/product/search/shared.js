e6def8e68811716f51395beed337b2a025a17968
Bindu Datla <bindu.datla@import.io>
2020-07-01 12:53:25 -0400

    updated shared.js

--- src/library/product/search/shared.js
+++ src/library/product/search/shared.js
@@ -23,7 +23,7 @@ const transform = (data, context) => {
   const productCodes = state.productCodes || [];
   for (const { group } of data) {
     for (const row of group) {
-      if (row.id && productCodes.indexOf(row.id[0].text) === -1) {
+      if (row.id && row.id[0] && productCodes.indexOf(row.id[0].text) === -1) {
         productCodes.push(row.id[0].text);
         rankCounter = rankCounter + 1;
         if (!row.sponsored) {
@@ -33,7 +33,7 @@ const transform = (data, context) => {
         row.rank = [{ text: rankCounter }];
       } else {
         console.log(`${row.id[0].text} : ${row.name[0].text}`);
-        row.id = [];
+        row.id = [{ text: '' }];
       }
       Object.keys(row).forEach(header => row[header].forEach(el => {
         el.text = clean(el.text);
