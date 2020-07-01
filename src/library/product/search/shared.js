d0c7a994b3ab19367896df8286673ad0f8dcd62b
Adam Scott <alawrencescott@gmail.com>
2020-07-01 15:04:37 -0700

    Scott kroger search (#233)
    * update xpaths
    
    * pending work on searchUrl
    
    * add search url manually
    
    * updated xpaths
    
    * add aggregateRating2 field
    
    * increased zip code timeouts
    
    * new files
    
    * deleted unneeded files
    
    * edit paginate to avoid dups
    
    * fix zip on search
    
    * removed zip for search
    
    * fixed paginate errors
    
    * updated upc xpath
    
    * new paginate xpaths
    
    * fix search dedups
    
    * resolve module dependency error
    
    * fix zip input
    
    * adding zip code
    
    * updated setzipcode
    
    * final zipcode fixes for search
    
    * add zip to extractor.yaml
    
    * added new transform
    
    * better syntax per PR
    
    * refactor zip code logic
    
    * moved import
    
    Co-authored-by: Bindu Datla <bindu.datla@import.io>

--- src/library/product/search/shared.js
+++ src/library/product/search/shared.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
@@ -14,29 +13,36 @@ const transform = (data, context) => {
     .replace(/"\s{1,}/g, '"')
     .replace(/\s{1,}"/g, '"')
     .replace(/^ +| +$|( )+/g, ' ')
-  // eslint-disable-next-line no-control-regex
+    // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-
   const state = context.getState();
   let orgRankCounter = state.orgRankCounter || 0;
   let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
   for (const { group } of data) {
     for (const row of group) {
-      rankCounter = rankCounter + 1;
-      if (!row.sponsored) {
-        orgRankCounter = orgRankCounter + 1;
-        row.rankOrganic = [{ text: orgRankCounter }];
+      if (row.id && row.id[0] && productCodes.indexOf(row.id[0].text) === -1) {
+        productCodes.push(row.id[0].text);
+        rankCounter += 1;
+        if (!row.sponsored) {
+          orgRankCounter += 1;
+          row.rankOrganic = [{ text: orgRankCounter }];
+        }
+        row.rank = [{ text: rankCounter }];
+      } else {
+        console.log(`${row.id[0].text} : ${row.name[0].text}`);
+        row.id = [{ text: '' }];
       }
-      row.rank = [{ text: rankCounter }];
-      context.setState({ rankCounter });
-      context.setState({ orgRankCounter });
       Object.keys(row).forEach(header => row[header].forEach(el => {
         el.text = clean(el.text);
       }));
     }
   }
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  console.log(productCodes);
   return data;
 };
-
 module.exports = { transform };
