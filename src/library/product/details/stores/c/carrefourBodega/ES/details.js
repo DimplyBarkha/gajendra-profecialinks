2b8b460e1df14588092afd5a723be9d06df51625
nikita.kumari <nikita.kumari@synerzip.com>
2020-07-01 17:17:21 +0530

    Extractor basic setup(#291)

---
+++ src/library/product/details/stores/c/carrefourBodega/ES/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'ES',
+    store: 'carrefourBodega',
+    domain: 'carrefour.es',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/c/carrefourBodega/ES/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'ES',
+    store: 'carrefourBodega',
+    transform: null,
+    domain: 'carrefour.es',
+    zipcode: '',
+  },
+};
