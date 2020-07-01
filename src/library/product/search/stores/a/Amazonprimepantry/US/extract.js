9351c678d24dfd7bb07fe26452602067e607a56e
satyam <satyam.rathod@synerzip.com>
2020-07-01 16:45:57 +0530

    #290 initial commit with loaded selector

---
+++ src/library/product/search/stores/a/Amazonprimepantry/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'Amazonprimepantry',
+    transform: null,
+    domain: 'amazon.com',
+    zipcode: '10001',
+  },
+};
