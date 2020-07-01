759cde8a6d79a43cb12c0b88d53e7f03ba3d8e6d
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-01 21:23:32 +0530

    updated url

--- src/library/product/search/stores/d/douglas/ES/execute.js
+++ src/library/product/search/stores/d/douglas/ES/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     ccountry: 'ES',
     store: 'douglas',
     domain: 'douglas.es',
-    url: 'https://api.empathybroker.com/search/v1/query/douglas/search?lang=es&origin=linked&promoted.start=0&scope=desktop&tariff=V005&start=0&rows=500&q={searchTerms}#[!opt!]{"type":"json"}[/!opt!]',
+    url: 'https://api.empathybroker.com/search/v1/query/douglas/search?lang=es&origin=linked&promoted.start=0&scope=desktop&tariff=V005&start=0&rows=24&q={searchTerms}#[!opt!]{"type":"json"}[/!opt!]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/d/douglas/ES/paginate.js
+++ src/library/product/search/stores/d/douglas/ES/paginate.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'douglas',
     openSearchDefinition: {
       pageOffset: 24,
-      template: 'https://api.empathybroker.com/search/v1/query/douglas/search?lang=es&origin=linked&promoted.start={page}&scope=desktop&start=0&rows=24&tariff=V005&q={searchTerms}#[!opt!]{"type":"json"}[/!opt!]',
+      template: 'https://api.empathybroker.com/search/v1/query/douglas/search?lang=es&origin=linked&promoted.start=0&scope=desktop&start={page}&rows=24&tariff=V005&q={searchTerms}#[!opt!]{"type":"json"}[/!opt!]',
     },
     domain: 'douglas.es',
     zipcode: '',
