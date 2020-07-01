91c52921ed357ed46022dafc01116523a531989a
nikita.kumari <nikita.kumari@synerzip.com>
2020-07-01 14:27:23 +0530

    Sponsored  xpath corrected(#239)

--- src/library/product/search/stores/a/amazon/UK/execute.js
+++ src/library/product/search/stores/a/amazon/UK/execute.js
@@ -5,6 +5,7 @@ module.exports = {
     country: 'UK',
     store: 'amazon',
     domain: 'amazon.co.uk',
+    zipcode: 'SW1P 3EU',
     url: 'https://www.amazon.co.uk/s?k={searchTerms}&ref=nb_sb_noss',
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
     noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")]',
