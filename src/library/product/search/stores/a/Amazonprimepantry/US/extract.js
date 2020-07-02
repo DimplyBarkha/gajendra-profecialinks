815df6088722e7ec39689d45a9457990f8853520
satyam <satyam.rathod@synerzip.com>
2020-07-02 12:21:24 +0530

    #290 added code for zipcode. Added all the required fields to yaml. Added pagination

--- src/library/product/search/stores/a/Amazonprimepantry/US/extract.js
+++ src/library/product/search/stores/a/Amazonprimepantry/US/extract.js
@@ -1,4 +1,26 @@
 const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function (inputs) {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+
+    const searchUrl = window.location.href.replace(/%20/g, ' ');
+    addHiddenDiv('search-url', searchUrl);
+  });
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
@@ -9,4 +31,5 @@ module.exports = {
     domain: 'amazon.com',
     zipcode: '10001',
   },
+  implementation,
 };
