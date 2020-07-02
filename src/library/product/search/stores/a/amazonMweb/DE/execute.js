51b9f8b554e72c5007ff4110bec224912eaddf12
Akansha <akanshad.wajge@gmail.com>
2020-07-02 20:29:55 +0530

    searchUrl fixed

--- src/library/product/search/stores/a/amazonMweb/DE/execute.js
+++ src/library/product/search/stores/a/amazonMweb/DE/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'amazonMweb',
     domain: 'amazon.de',
     timeout: 8000,
-    url: 'https://www.amazon.de/s?k={searchTerms}',
+    url: 'https://www.amazon.de/s?k={searchTerms}&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss',
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
     noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")]',
     zipcode: '',
