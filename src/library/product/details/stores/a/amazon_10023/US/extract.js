b136b99ec64551b57ac9aca596dbc44bcfaee821
irfan shaikh <irfan.shaikh@synerzip.com>
2020-07-02 12:34:37 +0530

    #238. Added remaining xpath and inputs.

--- src/library/product/details/stores/a/amazon_10023/US/extract.js
+++ src/library/product/details/stores/a/amazon_10023/US/extract.js
@@ -9,4 +9,21 @@ module.exports = {
     domain: 'amazon.com',
     zipcode: '10023',
   },
+  implementation: async ({ url }, { transform }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      let manufacturerDescription = document.querySelector('div[id="aplus"] div[data-cel-widget="aplus"]');
+      manufacturerDescription = manufacturerDescription ? manufacturerDescription.innerText.trim() : '';
+      if (manufacturerDescription) {
+        addElementToDocument('pd_enhancedContent', manufacturerDescription);
+      }
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
