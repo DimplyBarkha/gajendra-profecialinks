2c41e5f902b440e0d00f8c32bdf5722b68e30ead
sourabhk <sourabh.karmarkar@synerzip.com>
2020-07-02 14:12:27 +0530

    #296 Added extractor logic for amazon fr search

---
+++ src/library/product/search/stores/a/amazon/FR/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'FR',
+    store: 'amazon',
+    domain: 'amazon.fr',
+    url: 'https://www.amazon.fr/s?k={searchTerms}&ref=nb_sb_noss',
+    loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Aucun résultat")]',
+    zipcode: '75019',
+  },
+};
---
+++ src/library/product/search/stores/a/amazon/FR/extract.js
@@ -0,0 +1,36 @@
+
+const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  async function addUrl () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const url = window.location.href;
+    addHiddenDiv('added-searchurl', url);
+  }
+  await context.evaluate(addUrl);
+  return await context.extract(productDetails, { transform });
+}
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'FR',
+    store: 'amazon',
+    transform,
+    domain: 'amazon.fr',
+    zipcode: '75019',
+  },
+  implementation,
+};
