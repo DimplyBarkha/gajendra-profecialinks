a5fa9e46b65b087a0ea5c1bed16a2a8b1db6a96d
Bindu Datla <bindu.datla@import.io>
2020-07-02 19:01:02 -0400

    removed zipcode value, and moved the zipcode code to navigation

--- src/library/navigation/goto/domains/ta/setZipCode.target.com.js
+++ src/library/navigation/goto/domains/ta/setZipCode.target.com.js
@@ -1,10 +1,40 @@
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { url, zipcode } = inputs;
+  // const { country, domain, store } = parameters;
+
+  await context.waitForXPath('//button[@data-test="storeId-utilityNavBtn"]');
+  await context.evaluate(async function () {
+    function stall (ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+    document.getElementById('storeId-utilityNavBtn').click();
+    await stall(1000);
+  });
+  await context.setInputValue('#zipOrCityState', zipcode);
+  await context.evaluate(async function () {
+    document.querySelector('button[data-test="storeLocationSearch-button"]').click();
+  });
+  await context.waitForXPath("//button[@data-test='storeId-listItem-setStore']");
+  await context.evaluate(function () {
+    document.querySelectorAll('button[data-test="storeId-listItem-setStore"]')[0].click();
+  });
+}
 module.exports = {
   implements: 'navigation/goto/setZipCode',
   parameterValues: {
     country: 'US',
     domain: 'target.com',
-    store: 'target',
-    zipcode: '48374',
+    store: 'target',   
   },
+  implementation
 };
--- src/library/product/search/stores/t/target/US/execute.js
+++ src/library/product/search/stores/t/target/US/execute.js
@@ -1,54 +1,3 @@
-/**
- *
- * @param { { keywords: string, zipcode: string } } inputs
- * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { goto: ImportIO.Action} } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
-  await dependencies.goto({ url, zipcode: inputs.zipcode });
-  await context.waitForXPath('//button[@data-test="storeId-utilityNavBtn"]');
-  await context.evaluate(async function () {
-    function stall (ms) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve();
-        }, ms);
-      });
-    }
-    document.getElementById('storeId-utilityNavBtn').click();
-    await stall(1000);
-  });
-  await context.setInputValue('#zipOrCityState', inputs.zipcode);
-  await context.evaluate(async function () {
-    document.querySelector('button[data-test="storeLocationSearch-button"]').click();
-  });
-  await context.waitForXPath("//button[@data-test='storeId-listItem-setStore']");
-  await context.evaluate(function () {
-    document.querySelectorAll('button[data-test="storeId-listItem-setStore"]')[0].click();
-  });
-
-  if (parameters.loadedSelector) {
-    await context.waitForFunction(function (sel, xp) {
-      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
-  }
-  console.log('Checking no results', parameters.noResultsXPath);
-  return await context.evaluate(function (xp) {
-    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-    console.log(xp, r);
-    const e = r.iterateNext();
-    console.log(e);
-    return !e;
-  }, parameters.noResultsXPath);
-}
-
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -58,6 +7,5 @@ module.exports = {
     url: 'https://www.target.com/s?searchTerm={searchTerms}',
     loadedSelector: 'ul li',
     noResultsXPath: '//h1[contains(.,"no results found")]',
-  },
-  implementation,
+  },  
 };
