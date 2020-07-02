c5614222f6a87ad8c1e450d65ef6bbbe9ee1039f
nikita.kumari <nikita.kumari@synerzip.com>
2020-07-02 16:11:04 +0530

    Updated xpaths and transformation(#291)

--- src/library/product/details/stores/c/carrefourBodega/ES/extract.js
+++ src/library/product/details/stores/c/carrefourBodega/ES/extract.js
@@ -1,3 +1,38 @@
+/**
+ *
+ * @param { { url?: string,  id?: string} } inputs
+ * @param { Record<string, any> } parameters
+ * @param { ImportIO.IContext } context
+ * @param { Record<string, any> } dependencies
+ */
+async function implementation (
+  // @ts-ignore
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  context.evaluate(() => {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    let node = (document.querySelector("div[class='pdp'] button").getAttribute('data-datalayer')) ? document.querySelector("div[class='pdp'] button").getAttribute('data-datalayer') : '';
+    node = (node && JSON.parse(node)) ? JSON.parse(node) : '';
+    // @ts-ignore
+    node = (node && node.ecommerce && node.ecommerce.add && node.ecommerce.add.products && node.ecommerce.add.products[0].id && JSON.parse(node.ecommerce.add.products[0].id)[0]) ? JSON.parse(node.ecommerce.add.products[0].id)[0] : '';
+    if (node) {
+      addHiddenDiv('ii_gtin', node);
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
+
 const { transform } = require('./format');
 module.exports = {
   implements: 'product/details/extract',
@@ -8,4 +43,5 @@ module.exports = {
     domain: 'carrefour.es',
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/details/stores/c/carrefourBodega/ES/format.js
+++ src/library/product/details/stores/c/carrefourBodega/ES/format.js
@@ -7,13 +7,13 @@
 const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
-      if (row.gtin) {
-        row.gtin.forEach(item => {
-          let data = JSON.parse(item.text);
-          data = (data.ecommerce && data.ecommerce.add && data.ecommerce.add.products && data.ecommerce.add.products[0].id && JSON.parse(data.ecommerce.add.products[0].id)[0]) ? JSON.parse(data.ecommerce.add.products[0].id)[0] : '';
-          item.text = data;
-        });
-      }
+      // if (row.gtin) {
+      //   row.gtin.forEach(item => {
+      //     let data = JSON.parse(item.text);
+      //     data = (data.ecommerce && data.ecommerce.add && data.ecommerce.add.products && data.ecommerce.add.products[0].id && JSON.parse(data.ecommerce.add.products[0].id)[0]) ? JSON.parse(data.ecommerce.add.products[0].id)[0] : '';
+      //     item.text = data;
+      //   });
+      // }
       if (row.nameExtended) {
         row.nameExtended.forEach(item => {
           item.text = item.text.replace(/\n/g, '').replace(/\s{2,}/g, ' ');
@@ -38,6 +38,25 @@ const transform = (data) => {
         row.directions.forEach(item => {
           item.text = item.text.replace(/\n/g, '').replace(/\s{2,}/g, ' ');
         });
+      }
+      if (row.sku) {
+        row.sku.forEach(item => {
+          item.text = item.text.replace(/.*R-(.*)b\/p/, '$1');
+        });
+      }
+      if (row.variantId) {
+        row.variantId.forEach(item => {
+          item.text = item.text.replace(/.*R-(.*)b\/p/, '$1');
+        });
+      }
+      if (row.category) {
+        // const text = [];
+        // row.category.forEach((item, index, array) => {
+        //   if (array.length - 1 !== index) {
+        //     text.push({ text: `${item.text}` });
+        //   }
+        // });
+        row.category = row.category.slice(0, row.category.length - 1);
       }
     }
   }
