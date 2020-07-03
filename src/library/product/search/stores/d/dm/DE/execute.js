a5d7dc8c715f5cbe825cdc4f0a0712c1df3bd2a9
Manoj <manoj.d@osmosys.asia>
2020-07-03 21:35:29 +0530

    updated noresult selector

--- src/library/product/search/stores/d/dm/DE/execute.js
+++ src/library/product/search/stores/d/dm/DE/execute.js
@@ -7,6 +7,6 @@ module.exports = {
     domain: 'dm.de',
     url: 'https://www.dm.de/search?query={searchTerms}&searchType=product',
     loadedSelector: '[data-dmid="product-tile-container"]',
-    noResultsXPath: '//div[@data-dmid="content-search-count-container"] | //h2[@data-dmid="fallback-tips-headline"]',
+    noResultsXPath: '//div[@data-dmid="content-search-count-container"] | //h2[@data-dmid="fallback-tips-headline"] | //div[contains(@data-dmid,"search-container")]//p[contains(.,"Leider ist ein technischer Fehler aufgetreten")] | //div[@data-dmid="faq-search-container"]',
   },
 };
