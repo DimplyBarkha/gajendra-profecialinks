e2a8213d407ac0d8344c903b7c728877659cca3c
Manoj <manoj.d@osmosys.asia>
2020-07-03 15:22:03 +0530

    updated goto

--- src/library/navigation/goto/domains/dm/dm.de.js
+++ src/library/navigation/goto/domains/dm/dm.de.js
@@ -5,6 +5,16 @@ module.exports = {
     domain: 'dm.de',
     country: 'DE',
     store: 'dm',
-    timeout: 30000,
+  },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    await context.goto(url, {
+      first_request_timeout: 100000,
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 100000,
+      waitUntil: 'load',
+      checkBlocked: true,
+    });
   },
 };
