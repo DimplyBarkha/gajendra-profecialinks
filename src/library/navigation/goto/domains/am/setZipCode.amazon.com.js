2ba555e886b9682c7ba7886b62c387c77d1595a3
Sanket Take <sanket.take@synerzip.com>
2020-07-04 19:30:15 +0530

    #189. Testing stage. Minor modifications

--- src/library/navigation/goto/domains/am/setZipCode.amazon.com.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.com.js
@@ -19,8 +19,8 @@ async function implementation(
   await context.click('#GLUXZipUpdate input');
   await context.waitForSelector('button[name="glowDoneButton"]');
   await context.click('button[name="glowDoneButton"]');
-  await context.waitForMutuation('#glow-ingress-line2', { timeout: 10000 });
   await context.waitForNavigation();
+  await new Promise(resolve => setTimeout(resolve, 5261));
 }
 
 module.exports = {
