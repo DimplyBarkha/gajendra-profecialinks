acf9528ecf21671552b03263819f629caef3f2f7
Sanket Take <sanket.take@synerzip.com>
2020-07-05 15:57:08 +0530

    #189. Modified cleanup function

--- src/library/product/details/stores/a/amazonPrimePantry/shared.js
+++ src/library/product/details/stores/a/amazonPrimePantry/shared.js
@@ -7,15 +7,17 @@ const transform = (data) => {
   const cleanUp = (data, context) => {
     let dataStr = JSON.stringify(data);
     dataStr = dataStr.replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
+      .replace(/\r\n|\r|\n/g, ' ')
       .replace(/&amp;nbsp;/g, ' ')
       .replace(/&amp;#160/g, ' ')
-      .replace(/\\u00A0/g, ' ')
+      .replace(/\u00A0/g, ' ')
       .replace(/\s{2,}/g, ' ')
       .replace(/"\s{1,}/g, '"')
       .replace(/\s{1,}"/g, '"')
       .replace(/^ +| +$|( )+/g, ' ')
       // eslint-disable-next-line no-control-regex
-      .replace(/[^\x00-\x7F]/g, '');
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
 
     return JSON.parse(dataStr);
   };
