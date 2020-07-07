ad4775f9b85c0ffb086708036fa77e257060071a
David Skoglund <david.skoglund@import.io>
2020-07-06 21:29:18 -0700

    #295. Added temp search extractor.

--- src/library/product/search/stores/a/amazon/US/execute.js
+++ src/library/product/search/stores/a/amazon/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     store: 'amazon',
     domain: 'amazon.com',
     url: 'https://www.amazon.com/s?k={searchTerms}&ref=nb_sb_noss_2',
-    loadedSelector: 'section.ebx-empathy-x__body',
+    loadedSelector: 'div[class*="result-list"]>div[data-asin]>div>span[cel_widget_id="MAIN-SEARCH_RESULTS"]',
     noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")]',
   },
 };
