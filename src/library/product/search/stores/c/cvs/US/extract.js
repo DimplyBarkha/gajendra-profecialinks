dfc31d4cdff1eb94bfe0101d042f77b8a4a9238f
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-07-06 20:16:39 -0700

    updates to cvs search

--- src/library/product/search/stores/c/cvs/US/extract.js
+++ src/library/product/search/stores/c/cvs/US/extract.js
@@ -10,29 +10,6 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await context.evaluate(function () {
-
-    function addHiddenDiv (id, content) {
-      const newDiv = document.createElement('div');
-      newDiv.id = id;
-      newDiv.textContent = content;
-      newDiv.style.display = 'none';
-      document.body.appendChild(newDiv);
-    }
-    addHiddenDiv(`ii_url`, window.location.href);
-    
-    
-  });
-  // const addSku = await context.evaluate(function () {
-  //   const skuScript  = '//script[contains(.,"productIndexData")]'
-  //   if(skuScript) {
-  //     var element = document.evaluate( skuScript, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-  //     if( element.snapshotLength > 0 ) {
-  //         console.log(element.snapshotItem(1).textContent)
-  //     }
-  //   }
-  // }) 
-
   console.log("Do pagination");
   let resultCounter = 0;
   await new Promise(r => setTimeout(r, 8000));
@@ -60,31 +37,70 @@ async function implementation (
   }
 
 
+
+
+  async function addToDom() {
+
+    return await context.evaluate(function () {
+      function addHiddenDiv (id, content, doc) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        if(doc){
+          // debugger
+          doc.appendChild(newDiv)
+        } else {
+          document.body.appendChild(newDiv);
+        }
+      }
+
+      function addSku () {
+        let textNode = document.evaluate('//script[contains(.,"initialState")]', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null)
+        let text = textNode.iterateNext().textContent;
+        let textcontent = text.match(/var productIndexData = {.*}}/g)[0].replace('var productIndexData = ','').trimEnd();
+        let parsed = JSON.parse(textcontent);
+        let products = document.querySelectorAll('div.css-1dbjc4n.r-18u37iz.r-tzz3ar div.css-1dbjc4n.r-1pi2tsx a')
+        let i = 0;
+        let skus = [];
+        debugger
+        while(i < parsed.products.length){
+          let prodSku = parsed.products[i].defaultSku
+          skus.push(prodSku)
+          addHiddenDiv(`ii_sku`, prodSku, products[i]);
+          i++;
+        }
+        console.log(skus)
+      }
+
+      // addHiddenDiv(`ii_url`, window.location.href);
+      // addSku();
+    });
+}
+
   async function continuedClickToShowResults() {
 
-          let moreButton = 'div.css-1dbjc4n.r-1awozwy.r-14lw9ot.r-rs99b7'
-          let numberPageResults = 0;
-          let count = 0
-          while (numberPageResults <= 150 && count < 200) {
-           
-              if(await buttonCheck()) {
-                  await context.click(moreButton)
-                  await new Promise(r => setTimeout(r, 10000));
-                  numberPageResults = await getNumberResultsOnPage();
-                  console.log(numberPageResults + " items on page")
-                  // return await context.extract(productDetails, { transform });
-                  count++
-              } else {
-                  break;
-              }
-          }
-
+    let moreButton = 'div.css-1dbjc4n.r-1awozwy.r-14lw9ot.r-rs99b7';
+    let numberPageResults = 0;
+    let count = 0;
+    while (numberPageResults <= 150 && count < 200) {
+      
+        if(await buttonCheck()) {
+            await context.click(moreButton);
+            await new Promise(r => setTimeout(r, 10000));
+            numberPageResults = await getNumberResultsOnPage();
+            console.log(numberPageResults + " items on page");
+            // return await context.extract(productDetails, { transform });
+            // await addToDom();
+            count++
+        } else {
+            break;
+        }
+    }
   }
 
-
-
-  await continuedClickToShowResults()
-
+  await continuedClickToShowResults();
+  await addToDom()
 
   return await context.extract(productDetails, { transform });
 }
