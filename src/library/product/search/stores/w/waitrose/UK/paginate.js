dd8729c3c4c82898be70f5b58253af2d3a334feb
nagajyothi.cp <nagajyothi.cp@osmosys.asia>
2020-07-07 12:38:05 +0530

    Updated code

--- src/library/product/search/stores/w/waitrose/UK/paginate.js
+++ src/library/product/search/stores/w/waitrose/UK/paginate.js
@@ -5,10 +5,7 @@ module.exports = {
     country: 'UK',
     store: 'waitrose',
     nextLinkSelector: 'div.loadMoreWrapper___UneG1 > button',
-    mutationSelector: null,
     spinnerSelector: 'div[class="spinner___1nvln lg___1_BF1 dark___WsJWk"]',
-    loadedSelector: null,
-    openSearchDefinition: null,
     domain: 'waitrose.com',
   },
 };
