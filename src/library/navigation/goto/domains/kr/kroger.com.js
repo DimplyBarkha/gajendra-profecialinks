66e49b2e9b5a8739a1cd84418b9979952abcd76b
Bindu Datla <bindu.datla@import.io>
2020-07-06 21:15:56 -0400

    updated opt tags

--- src/library/navigation/goto/domains/kr/kroger.com.js
+++ src/library/navigation/goto/domains/kr/kroger.com.js
@@ -9,10 +9,11 @@ module.exports = {
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.goto(url, {first_request_timeout: 60000, timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    const optUrl =  `${url}#[!opt!]{"anti_fingerprint":false, "first_request_timeout": 60, "proxy":{"use_relay_proxy": false}}[/!opt!]`;
+    await context.goto(optUrl, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode });
+      await dependencies.setZipCode({ url: optUrl, zipcode: zipcode });
     }
   },
 };
