34af7f1f63adac34c9a37bc201608af429d1505c
satyam <satyam.rathod@synerzip.com>
2020-07-07 10:35:28 +0530

    #303 added all xpath for data fields. Added transforms for ranking

--- src/library/product/search/stores/h/homedepot_lg/US/extract.js
+++ src/library/product/search/stores/h/homedepot_lg/US/extract.js
@@ -1,11 +1,49 @@
-
+const { transform } = require('../format');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const searchUrl = window.location.href.replace(/%20/g, ' ');
+    addHiddenDiv('search-url', searchUrl);
+    // if(price-format__main-price)
+    document.querySelectorAll('div[class="product-result__wrapped-results"] div.product-pod').forEach(node => {
+      let priceBlock = node.querySelector('div.price-format__main-price');
+      if (priceBlock) {
+        // @ts-ignore
+        priceBlock = priceBlock.children;
+        const currency = (priceBlock[0] && priceBlock[0].innerText) || '';
+        let price = (priceBlock[1] && priceBlock[1].innerText) || '';
+        const decimalPrice = (priceBlock[2] && priceBlock[2].innerText) || '';
+        price = decimalPrice ? `${currency}${price}.${decimalPrice}` : `${currency}${price}`;
+        const newDiv = document.createElement('span');
+        newDiv.className = 'price_main';
+        newDiv.textContent = price.toString();
+        newDiv.style.display = 'none';
+        node.appendChild(newDiv);
+      }
+    });
+  });
+  return await context.extract(productDetails, { transform: parameters.transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'homedepot_lg',
-    transform: null,
+    transform,
     domain: 'homedepot.com',
     zipcode: '',
   },
+  implementation,
 };
---
+++ src/library/product/search/stores/h/homedepot_lg/format.js
@@ -0,0 +1,49 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data, context) => {
+  const state = context.getState();
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.productUrl) {
+        row.productUrl.forEach(productUrl => {
+          productUrl.text = `https://www.homedepot.com${productUrl.text}`;
+        });
+      }
+      if (row.id) {
+        row.id.forEach(id => {
+          if (id.text.includes('/')) {
+            id.text = id.text.replace(/.*\/(.*)$/, '$1');
+          }
+        });
+      }
+      if (row.aggregateRating2) {
+        row.aggregateRating2.forEach(item => {
+          if (item.text.includes('width')) {
+            const starWidth = item.text.replace(/.*?(\d+\.?\d*).*/, '$1');
+            if (starWidth) {
+              const ratings = ((starWidth * 5) / 100).toFixed(1);
+              item.text = ratings;
+            }
+          }
+        });
+      }
+      if (row.id && row.id[0] && productCodes.indexOf(row.id[0].text) === -1) {
+        productCodes.push(row.id[0].text);
+        rankCounter += 1;
+        row.rankOrganic = [{ text: rankCounter }];
+      } else {
+        row.id = [{ text: '' }];
+      }
+    }
+  }
+  context.setState({ rankCounter });
+  context.setState({ productCodes });
+  return data;
+};
+
+module.exports = { transform };
