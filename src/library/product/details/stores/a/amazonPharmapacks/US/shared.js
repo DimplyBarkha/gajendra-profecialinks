bb03d71621e0cea9ef72d8f1cecb6359783868b8
nagajyothi.cp <nagajyothi.cp@osmosys.asia>
2020-07-09 13:15:42 +0530

    Updated code

--- src/library/product/details/stores/a/amazonPharmapacks/US/shared.js
+++ src/library/product/details/stores/a/amazonPharmapacks/US/shared.js
@@ -72,6 +72,17 @@ const transform = (data) => {
           }
         });
       }
+      if (row.variantAsins) {
+        let text = '';
+        row.variantAsins.forEach(item => {
+          text += `${item.text} | `;
+        });
+        row.variantAsins = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+      }
     }
   }
   return data;
