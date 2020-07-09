ea981beafc98a977f7f33efe4e1d66e4184814b9
irfan shaikh <irfan.shaikh@synerzip.com>
2020-07-09 12:09:46 +0530

    #191. Added 'specifications' field fetch logic

--- src/library/product/details/stores/a/amazonMobile/US/extract.js
+++ src/library/product/details/stores/a/amazonMobile/US/extract.js
@@ -16,8 +16,19 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(async function () {
+      function addEleToDoc (key, value) {
+        const prodEle = document.createElement('div');
+        prodEle.id = key;
+        prodEle.textContent = value;
+        prodEle.style.display = 'none';
+        document.body.appendChild(prodEle);
+      }
       const element = document.getElementById('detail-bullets');
       element && element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+      const specifications = document.querySelectorAll('div[id="detail-bullets"]  ul  li ,  table[id="productDetails_techSpec"]  tbody  tr');
+      const data = [];
+      specifications && specifications.forEach(item => data.push(item.innerText.trim()));
+      addEleToDoc('pd_spec1', data.join(''));
     });
     return await context.extract(productDetails, { transform });
   },
--- src/library/product/details/stores/a/amazonMobile/shared.js
+++ src/library/product/details/stores/a/amazonMobile/shared.js
@@ -64,6 +64,17 @@ const transform = (data) => {
           item.text = cleanUp(item.text);
         });
       }
+      if (row.specifications) {
+        let text = '';
+        row.specifications.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        });
+        row.specifications = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+      }
     }
   }
   return data;
