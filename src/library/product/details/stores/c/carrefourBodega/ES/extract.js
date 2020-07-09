20a0e1226824b1ac33af003f5eec65b7323f8978
nikita.kumari <nikita.kumari@synerzip.com>
2020-07-09 16:41:23 +0530

    gtin transformation(#291)

--- src/library/product/details/stores/c/carrefourBodega/ES/extract.js
+++ src/library/product/details/stores/c/carrefourBodega/ES/extract.js
@@ -7,6 +7,7 @@
  */
 async function implementation (
   // @ts-ignore
+  // @ts-ignore
   inputs,
   parameters,
   context,
@@ -22,10 +23,19 @@ async function implementation (
       newDiv.style.display = 'none';
       document.body.appendChild(newDiv);
     }
-    let node = (document.querySelector("div[class='pdp'] button").getAttribute('data-datalayer')) ? document.querySelector("div[class='pdp'] button").getAttribute('data-datalayer') : '';
-    node = (node && JSON.parse(node)) ? JSON.parse(node) : '';
+    let node = document.evaluate("//script[contains(.,'dataLayer = ')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    // eslint-disable-next-line no-useless-escape
     // @ts-ignore
-    node = (node && node.ecommerce && node.ecommerce.add && node.ecommerce.add.products && node.ecommerce.add.products[0].id && JSON.parse(node.ecommerce.add.products[0].id)[0]) ? JSON.parse(node.ecommerce.add.products[0].id)[0] : '';
+    // node = node ? node.text.replace(/\r\n|\n|\r/gm, '').replace('dataLayer = ', '').replace(';', '').replace('[', '').slice(0, -1) : {};
+    node = node ? node.textContent.replace(/\r\n|\n|\r/gm, '').replace(/.*"productEAN":\["(\d+).*/gm, '$1') : '';
+    console.log("nooode",node);
+    // @ts-ignore
+    // node = node ? JSON.parse(node) : {};
+    // let value = node && node.productEAN && node.productEAN[0] ? node.productEAN[0] : '';
+    // let node = (document.querySelector("div[class='pdp'] button").getAttribute('data-datalayer')) ? document.querySelector("div[class='pdp'] button").getAttribute('data-datalayer') : '';
+    // node = (node && JSON.parse(node)) ? JSON.parse(node) : '';
+    // @ts-ignore
+    // node = (node && node.ecommerce && node.ecommerce.add && node.ecommerce.add.products && node.ecommerce.add.products[0].id && JSON.parse(node.ecommerce.add.products[0].id)[0]) ? JSON.parse(node.ecommerce.add.products[0].id)[0] : '';
     if (node) {
       addHiddenDiv('ii_gtin', node);
     }
