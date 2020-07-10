94aa16cfbeae7ea887c71b5fd7ddeef2f1ac2372
alscotty <alawrencescott@gmail.com>
2020-07-10 11:36:23 -0700

    add for workbench test, npm lint fixes

--- src/library/product/search/stores/a/amazonFresh/US/search.js
+++ src/library/product/search/stores/a/amazonFresh/US/search.js
@@ -1,9 +1,9 @@
 module.exports = {
   implements: 'product/search',
-  parameterValues: { 
-    country: 'US', 
-    domain: 'amazon.com', 
-    store: 'amazonFresh', 
-    zipcode: '90210'
-    },
+  parameterValues: {
+    country: 'US',
+    domain: 'amazon.com',
+    store: 'amazonFresh',
+    zipcode: '90210',
+  },
 };
