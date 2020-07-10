4842522ef5ecce6365a77aa6481f70269448a23e
alscotty <alawrencescott@gmail.com>
2020-07-10 11:04:52 -0700

    added inputs for testing

--- src/library/navigation/goto/domains/am/setZipCode.amazon.com.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.com.js
@@ -5,13 +5,13 @@ const implementation = async (inputs, parameters, context, dependencies) => {
 
   const changeZip = async (wantedZip) => {
     await context.click('span#glow-ingress-line2.nav-line-2');
-    await new Promise((resolve, reject) => setTimeout(resolve, 4000));
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
 
     await context.setInputValue('input[aria-label="or enter a US zip code"]', wantedZip);
-    await new Promise((resolve, reject) => setTimeout(resolve, 4000));
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
 
     await context.click('input[aria-labelledby="GLUXZipUpdate-announce"]');
-    await new Promise((resolve, reject) => setTimeout(resolve, 4000));
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
 
     await context.click('button[name="glowDoneButton"]');
   };
@@ -19,7 +19,7 @@ const implementation = async (inputs, parameters, context, dependencies) => {
   try {
     await changeZip(zipcode);
   } catch (exception) {
-    console.log('Failed to change zipcode..!');
+    throw new Error('Failed to update zipcode!');
   }
 };
 
@@ -28,7 +28,6 @@ module.exports = {
   parameterValues: {
     country: 'US',
     domain: 'amazon.com',
-    // store: 'amazonApparel',
     store: 'amazonFresh',
     zipcode: '90210',
   },
