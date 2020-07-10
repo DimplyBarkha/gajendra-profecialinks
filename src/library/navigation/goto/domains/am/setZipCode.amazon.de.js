281b120d032e664955cda34c4f7eece7bb63851b
nehashende <neha.shende@synerzip.com>
2020-07-10 15:51:07 +0530

    #187. Zipcode and xpath changes.

--- src/library/navigation/goto/domains/am/setZipCode.amazon.de.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.de.js
@@ -7,30 +7,21 @@ async function implementation (
   const { zipcode } = inputs;
   await context.waitForSelector('#nav-packard-glow-loc-icon');
   await context.click('#nav-packard-glow-loc-icon');
-  console.log('Opening zipcode dialog');
   await context.waitForSelector('input#GLUXZipUpdateInput');
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
   try {
     await context.click('a#GLUXChangePostalCodeLink');
   } catch (error) {
     console.log('Element not visible');
   }
-  console.log('Setting zipcode');
   await context.setInputValue('input#GLUXZipUpdateInput', zipcode);
   await context.waitForSelector('#GLUXZipUpdate input');
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
   await context.click('#GLUXZipUpdate input');
-  console.log('Zipcode set');
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-  await context.waitForSelector('button[name="glowDoneButton"]');
-  console.log('Closing zipcode dialog');
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-  await context.click('button[name="glowDoneButton"]');
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-  console.log('Zipcode dialog closed');
-  // await context.evaluate(() => {
-  //   window.location.reload();
-  // });
+  try {
+    await context.waitForSelector('button[name="glowDoneButton"]');
+    await context.click('button[name="glowDoneButton"]');
+  } catch (error) {
+    console.log('Done button not found');
+  }
   await context.waitForNavigation();
 }
 
