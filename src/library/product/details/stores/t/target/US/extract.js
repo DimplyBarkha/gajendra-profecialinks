d99b4eca0b1612243019642db9891ce0dc0fa530
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-07-11 22:24:28 -0400

    Red Sky API Integration

--- src/library/product/details/stores/t/target/US/extract.js
+++ src/library/product/details/stores/t/target/US/extract.js
@@ -65,6 +65,46 @@ async function implementation (
       return newDiv;
     }
 
+    let scrollTop = 500;
+    while (true) {
+      window.scroll(0, scrollTop);
+      await stall(200);
+      scrollTop += 500;
+      if (scrollTop === 5000) {
+        break;
+      }
+    }
+
+    await stall(50);
+    const manufacturerCTA = document.querySelector('.Button-bwu3xu-0.styles__ShowMoreButton-zpxf66-2.h-padding-t-tight');
+    if (manufacturerCTA) {
+      manufacturerCTA.click();
+    }
+    await stall(50);
+    const element = document.querySelector('#salsify-ec-iframe');
+    if (element) {
+      console.log('found iframe');
+      console.log(element.getAttribute('src'));
+      await fetch(element.getAttribute('src'))
+        .then(async function (response) {
+          const sometext = await response.text();
+          const startText = '<body>';
+          const endText = '</body>';
+          const startIx = sometext.indexOf(startText);
+          const endIx = sometext.indexOf(endText, startIx);
+          console.log('start: ' + startIx);
+          console.log('end: ' + endIx);
+          const bodyContent = sometext.substring(startIx + startText.length, endIx);
+          const wrapper = document.createElement('div');
+          wrapper.id = 'frameContents';
+          wrapper.innerHTML = bodyContent;
+          document.body.appendChild(wrapper);
+          console.log('tried to append iframe content');
+        });
+    } else {
+      console.log('did not find iframe');
+    }
+
     async function getProductInfo(variant, productName, variantCount = null) {
       const newDiv = createListItem();
 
@@ -103,18 +143,6 @@ async function implementation (
         addHiddenDiv(newDiv, 'productUrl', variant.buy_url || variant.enrichment.buy_url);
       }
 
-      if (variant.selling_channel ) {
-        if(variant.selling_channel.includes('Sold Online')) {
-          addHiddenDiv(newDiv, 'availability', 'In Stock');
-        } else if (variant.selling_channel.includes('in Stores')) {
-          addHiddenDiv(newDiv, 'availability', 'In Store Only');
-        } else {
-          addHiddenDiv(newDiv, 'availability', 'Out of Stock');
-        }
-      } else {
-        addHiddenDiv(newDiv, 'availability', 'Out of Stock');
-      }
-
       let description = '';
       if (variant.product_description && variant.product_description.soft_bullets && variant.product_description.soft_bullets.bullets && variant.product_description.soft_bullets.bullets.length) {
         description = '|| ' + variant.product_description.soft_bullets.bullets.join(' || ') + ' ';
@@ -147,14 +175,39 @@ async function implementation (
       }
 
       if (variant.product_description && variant.product_description.bullet_description) {
+        const materials = [];
         variant.product_description.bullet_description.forEach(e => {
           if (e.includes('<B>Weight:</B>') || e.includes('<B>Net weight:</B>')) {
             addHiddenDiv(newDiv, 'netWeight', e.split('</B>')[1].trim());
           }
-          if (e.includes('<B>Number of')) {
+          if (e.includes('Number of')) {
             addHiddenDiv(newDiv, 'packSize', e.split('</B>')[1].trim());
           }
+          if (e.includes('Dimensions')) {
+            addHiddenDiv(newDiv, 'specifications', e.split('</B>')[1].trim());
+          }
+          if (e.includes('Warranty')) {
+            addHiddenDiv(newDiv, 'warranty', e.split('</B>')[1].trim());
+          }
+          if (e.includes('Storage')) {
+            addHiddenDiv(newDiv, 'storage', e.replace(/(<([^>]+)>)/ig, ''));
+          }
+          if (e.includes('Contains:')) {
+            addHiddenDiv(newDiv, 'allergyAdvice', e.split('</B>')[1].trim());
+          }
+          if (e.includes('Alcohol content:')) {
+            addHiddenDiv(newDiv, 'alcoholContent', e.split('</B>')[1].trim());
+          }
+          if (e.includes('Suggested Age:')) {
+            addHiddenDiv(newDiv, 'ageSuitability', e.split('</B>')[1].trim());
+          }
+          if (e.includes('Material:') || e.includes('material:')) {
+            materials.push(e.split('</B>')[1].trim());
+          }
         });
+        if (materials.length) {
+          addHiddenDiv(newDiv, 'materials', materials.join(', '));
+        }
       }
 
       if (variant.package_dimensions) {
@@ -178,6 +231,10 @@ async function implementation (
         addHiddenDiv(newDiv, 'disclaimer', variant.disclaimer.description);
       }
 
+      if (variant.country_of_origin) {
+        addHiddenDiv(newDiv, 'countryOfOrigin', variant.country_of_origin);
+      }
+
       if (variant.enrichment && variant.enrichment.drug_facts) {
         if(variant.enrichment.drug_facts.direction && variant.enrichment.drug_facts.direction.general_directions && variant.enrichment.drug_facts.direction.general_directions.length) {
           addHiddenDiv(newDiv, 'directions', variant.enrichment.drug_facts.direction.general_directions[0]);
@@ -189,6 +246,10 @@ async function implementation (
 
       if (variantCount) {
         addHiddenDiv(newDiv, 'variantCount', variantCount);
+      }
+
+      if(variant.proposition_65_warning_text) {
+        addHiddenDiv(newDiv, 'prop65Warning', variant.proposition_65_warning_text);
       }
 
       if (variant.enrichment
@@ -236,10 +297,53 @@ async function implementation (
               addHiddenDiv(newDiv, 'dietaryFibrePerServing', e.quantity || e.percentage);
               addHiddenDiv(newDiv, 'dietaryFibrePerServingUom', e.unit_of_measurement || '%');
             }
+            if (e.name === 'Sugars') {
+              addHiddenDiv(newDiv, 'totalSugarsPerServing', e.quantity || e.percentage);
+              addHiddenDiv(newDiv, 'totalSugarsPerServingUom', e.unit_of_measurement || '%');
+            }
+            if (e.name === 'Protein') {
+              addHiddenDiv(newDiv, 'proteinPerServing', e.quantity || e.percentage);
+              addHiddenDiv(newDiv, 'proteinPerServingUom', e.unit_of_measurement || '%');
+            }
+            if (e.name === 'Vitamin A') {
+              addHiddenDiv(newDiv, 'vitaminAPerServing', e.quantity || e.percentage);
+              addHiddenDiv(newDiv, 'vitaminAPerServingUom', e.unit_of_measurement || '%');
+            }
+            if (e.name === 'Vitamin C') {
+              addHiddenDiv(newDiv, 'vitaminCPerServing', e.quantity || e.percentage);
+              addHiddenDiv(newDiv, 'vitaminCPerServingUom', e.unit_of_measurement || '%');
+            }
+            if (e.name === 'Calcium') {
+              addHiddenDiv(newDiv, 'calciumPerServing', e.quantity || e.percentage);
+              addHiddenDiv(newDiv, 'calciumPerServingUom', e.unit_of_measurement || '%');
+            }
+            if (e.name === 'Iron') {
+              addHiddenDiv(newDiv, 'ironPerServing', e.quantity || e.percentage);
+              addHiddenDiv(newDiv, 'ironPerServingUom', e.unit_of_measurement || '%');
+            }
+            if (e.name === 'Magnesium') {
+              addHiddenDiv(newDiv, 'magnesiumPerServing', e.quantity || e.percentage);
+              addHiddenDiv(newDiv, 'magnesiumPerServingUom', e.unit_of_measurement || '%');
+            }
           });
+      }
 
+      if (parentData.product.item && parentData.product.item.child_items) {
+        const variants = [];
+        parentData.product.item.child_items.forEach(e => {
+          variants.push(e.tcin);
+        });
+        addHiddenDiv(newDiv, 'firstVariant', variants[0]);
+        addHiddenDiv(newDiv, 'variants', variants.join(' | '));
+      }
 
+      if (variant.variation && variant.variation.flexible_themes) {
+        const variationInfo = [];
+        for (let key in variant.variation.flexible_themes) {
+          variationInfo.push(variant.variation.flexible_themes[key])
         }
+        addHiddenDiv(newDiv, 'variationInfo', variationInfo.join(' '));
+      }
 
       document.querySelector('div[data-test="breadcrumb"]').querySelectorAll('span[itemprop="name"]').forEach((e, ind) => {
         if (ind > 0) {
@@ -247,21 +351,139 @@ async function implementation (
         }
       });
 
+      let terms = 'No';
+      if (document && document.querySelector('a[href="/c/terms-conditions/-/N-4sr7l"]')) {
+        terms = 'Yes';
+      }
+      addHiddenDiv(newDiv, 'terms', terms);
+
+      let privacy = 'No';
+      if (document && document.querySelector('a[href="/c/target-privacy-policy/-/N-4sr7p"]')) {
+        privacy = 'Yes';
+      }
+      addHiddenDiv(newDiv, 'privacy', privacy);
+      addHiddenDiv(newDiv, 'customerServiceAvailability', 'Yes');
+
+      const zoom = document.querySelector('.ZoomedImage__Zoomed-sc-1j8d1oa-0.dwtKdC');
+      if (zoom) {
+        addHiddenDiv(newDiv, 'zoom', 'Yes');
+      } else {
+        addHiddenDiv(newDiv, 'zoom', 'No');
+      }
+
+      let rotate = document.querySelector('button[data-test="button-model-viewer"]');
+      if (!rotate) {
+        document.querySelectorAll('.wc-demoted').forEach(e => {
+          console.log('h3', e.innerText);
+          if (e && e.innerText && e.innerText === '360° View') {
+            rotate = true;
+          }
+        });
+      }
+      if (rotate) {
+        addHiddenDiv(newDiv, 'rotate', 'Yes');
+      } else {
+        addHiddenDiv(newDiv, 'rotate', 'No');
+      }
+
+      const manufacturerCTA = document.querySelector('.Button-bwu3xu-0.styles__ShowMoreButton-zpxf66-2.h-padding-t-tight');
+      const frameContents = document.getElementById('frameContents');
+      if (frameContents) {
+        addHiddenDiv(newDiv, 'manufacturerDesc', frameContents.innerText);
+        const manufacturerImgs = [];
+        frameContents.querySelectorAll('img').forEach(e => {
+          manufacturerImgs.push(e.getAttribute('src'));
+        });
+        addHiddenDiv(newDiv, 'manufacturerImgs', manufacturerImgs.join(' | '));
+      } else if (manufacturerCTA) {
+        const manufacturerImgs = [];
+        if (document.getElementById('wcframable1-0') && document.getElementById('wcframable1-0').contentWindow) {
+          const frameContents = document.getElementById('wcframable1-0').contentWindow.document.body;
+          frameContents.querySelectorAll('img').forEach(e => {
+            manufacturerImgs.push(e.src);
+          });
+        }
+        if (document.getElementById('wc-power-page') && document.getElementById('wc-power-page').innerText) {
+          console.log('wcframes', document.querySelectorAll('.wc-fragment').length);
+          const manufacturerDesc = [];
+          document.querySelectorAll('.wc-fragment').forEach(e => {
+            if (e.querySelector('.wc-pct-data')) {
+              e.querySelectorAll('tr').forEach(tr => {
+                if (tr.innerText && !manufacturerDesc.includes(tr.innerText)) {
+                  manufacturerDesc.push(tr.innerText);
+                }
+              });
+            } else {
+              manufacturerDesc.push(e.innerText);
+            }
+          });
+          addHiddenDiv(newDiv, 'manufacturerDesc', manufacturerDesc.join(' ').replace(/See product page/g, '').replace(/\d options available/g, ''));
+          document.querySelectorAll('img.wc-media.wc-image').forEach(e => {
+            manufacturerImgs.push(e.src);
+          });
+          const aplusBody = document.querySelector('.wc-product-image-row');
+          if (aplusBody) {
+            aplusBody.querySelectorAll('.wc-pct-product-image').forEach(e => {
+              manufacturerImgs.push(e.src);
+            });
+          }
+          addHiddenDiv(newDiv, 'manufacturerImgs', manufacturerImgs.join(' | '));
+        }
+      }
+
+      await fetch('https://redsky.target.com/redsky_aggregations/v1/web/pdp_fulfillment_v1?key=eb2551e4accc14f38cc42d32fbc2b2ea&tcin=' + variant.tcin + '&store_id=1465&zip=54166&state=WI&latitude=44.780&longitude=-88.540&pricing_store_id=1465&fulfillment_test_mode=grocery_opu_team_member_test')
+      .then(data => data.json())
+      .then(availabilityData => {
+        let inStore = false;
+        let deliver = false;
+        if(availabilityData
+          && availabilityData.data
+          && availabilityData.data.product
+          && availabilityData.data.product.fulfillment) {
+
+            if (availabilityData.data.product.fulfillment.store_options
+                && availabilityData.data.product.fulfillment.store_options.length
+                && availabilityData.data.product.fulfillment.store_options[0].in_store_only.availability_status === 'IN_STOCK') {
+                inStore = true;
+            }
+
+            if (availabilityData.data.product.fulfillment.shipping_options
+                && availabilityData.data.product.fulfillment.shipping_options.availability_status === 'IN_STOCK') {
+                deliver = true;
+            }
+
+          }
+
+          if (deliver) {
+            addHiddenDiv(newDiv, 'availability', 'In Stock');
+          } else if (inStore) {
+            addHiddenDiv(newDiv, 'availability', 'In Store Only');
+          }
+          if (!deliver && !inStore) {
+            addHiddenDiv(newDiv, 'availability', 'Out of stock');
+          }
+
+      });
+
       await fetch('https://redsky.target.com/web/pdp_location/v1/tcin/' + variant.tcin + '?pricing_store_id=1465&key=eb2551e4accc14f38cc42d32fbc2b2ea')
       .then(data => data.json())
       .then(variantData => {
-        if(variantData.price && variantData.price.current_retail) {
-          addHiddenDiv(newDiv, 'price', variantData.price.current_retail);
-        }
-        if(variantData.price && variantData.price.reg_retail) {
-          addHiddenDiv(newDiv, 'regPrice', variantData.price.reg_retail);
+        if (variantData.price) {
+          if (variantData.price.current_retail) {
+            addHiddenDiv(newDiv, 'price', variantData.price.current_retail);
+          }
+          if (variantData.price.reg_retail) {
+            addHiddenDiv(newDiv, 'regPrice', variantData.price.reg_retail);
+          }
+          if (variantData.price.save_dollar) {
+            addHiddenDiv(newDiv, 'promotion', "Save $" + variantData.price.save_dollar.toFixed(2) + ' ' + variantData.price.save_percent + '%' + ' off')
+          }
         }
       });
 
       await fetch('https://redsky.target.com/v3/pdp/tcin/' + variant.tcin + '?excludes=taxonomy%2Cavailable_to_promise_store%2Cavailable_to_promise_network&key=eb2551e4accc14f38cc42d32fbc2b2ea&pricing_store_id=1465&storeId=1465&fulfillment_test_mode=grocery_opu_team_member_test')
       .then(data => data.json())
       .then(ratingData => {
-        console.log('ratingDataz', ratingData.rating_and_review_statistics);
         if (ratingData.product
           && ratingData.product.rating_and_review_statistics
           && ratingData.product.rating_and_review_statistics.result
@@ -273,8 +495,6 @@ async function implementation (
           addHiddenDiv(newDiv, 'aggregateRatingText', ratingData.product.rating_and_review_statistics.result[variant.tcin].coreStats.AverageOverallRating + " out of 5");
         }
       });
-
-
 
     }
 
@@ -288,15 +508,15 @@ async function implementation (
     .then(data => data.json())
     .then(async function (res) {
       parentData = res;
-      if (res.product.item.parent_items) {
+      if (res.product.item.parent_items && !isNaN(res.product.item.parent_items)) {
 
         const parentId = res.product.item.parent_items;
         await fetch("https://redsky.target.com/v3/pdp/tcin/" + parentId + "?excludes=taxonomy%2Cbulk_ship%2Cawesome_shop%2Cquestion_answer_statistics%2Crating_and_review_reviews%2Crating_and_review_statistics%2Cdeep_red_labels%2Cin_store_location%2Cavailable_to_promise_store%2Cavailable_to_promise_network&key=eb2551e4accc14f38cc42d32fbc2b2ea&pricing_store_id=1465&storeId=1465&fulfillment_test_mode=grocery_opu_team_member_test")
         .then(data => data.json())
-        .then(async function (res) {
-          parentData = res;
-          for (let variant of res.product.item.child_items) {
-            getProductInfo(variant, res.product.item.product_description.title, res.product.item.child_items.length);
+        .then(async function (parentRes) {
+          parentData = parentRes;
+          for (let variant of parentRes.product.item.child_items) {
+            getProductInfo(variant, parentRes.product.item.product_description.title, parentRes.product.item.child_items.length);
           }
         });
 
@@ -309,7 +529,7 @@ async function implementation (
       }
     });
 
-    await stall(500);
+    await stall(5000);
 
   });
 
