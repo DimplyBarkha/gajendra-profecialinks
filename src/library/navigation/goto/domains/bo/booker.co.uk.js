74660646eb068cb83d1afd46d45edd153e5a38fb
Manas Ranjan <manas.n@osmosys.asia>
2020-07-13 17:34:07 +0530

    Updated navigation a bit again.

--- src/library/navigation/goto/domains/bo/booker.co.uk.js
+++ src/library/navigation/goto/domains/bo/booker.co.uk.js
@@ -8,23 +8,43 @@ module.exports = {
   },
   // For navigating from home page to search page because search page is redirecting to home page.
   implementation: async ({ url }, parameterValues, context, dependencies) => {
-    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    await context.goto('https://www.booker.co.uk/home.aspx', { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+
     await context.evaluate(() => {
-      document.querySelector('input[name="OutsideHomePageControl$cmdPostCode"]').click();
+      if (document.querySelector('input[name="OutsideHomePageControl$cmdPostCode"]')) {
+        document.querySelector('input[name="OutsideHomePageControl$cmdPostCode"]').click();
+      }
     });
 
-    await context.waitForSelector('input[name="BLC$txtPostcode"]', { timeout: 30000 });
+    try {
+      await context.waitForSelector('input[name="BLC$txtPostcode"]', { timeout: 30000 });
+    } catch (err) {
+      console.log('Selector did not load.');
+    }
+
     await context.evaluate(() => {
-      document.querySelector('input[name="BLC$txtPostcode"]').value = 'SY23 3JQ';
-      document.querySelector('#BLC_cmdLookupPostcode').click();
+      if (document.querySelector('input[name="BLC$txtPostcode"]')) {
+        document.querySelector('input[name="BLC$txtPostcode"]').value = 'SY23 3JQ';
+        document.querySelector('#BLC_cmdLookupPostcode').click();
+      }
     });
 
-    await context.waitForSelector('input[id="cmdProceed"]', { timeout: 30000 });
+    try {
+      await context.waitForSelector('input[id="cmdProceed"]', { timeout: 30000 });
+    } catch (err) {
+      console.log('Selector did not load.');
+    }
     await context.evaluate(() => {
-      document.querySelector('input[src="/images/buttons/lookup.gif"]').click();
+      if (document.querySelector('input[src="/images/buttons/lookup.gif"]')) {
+        document.querySelector('input[src="/images/buttons/lookup.gif"]').click();
+      }
     });
 
-    await context.waitForSelector('input[name="BranchInfo$cmdBrowseSite"]', { timeout: 30000 });
+    try {
+      await context.waitForSelector('input[name="BranchInfo$cmdBrowseSite"]', { timeout: 30000 });
+    } catch (err) {
+      console.log('Selector did not load.');
+    }
     await context.evaluate(() => {
       document.querySelector('#BranchInfo_cmdBrowseSite').click();
     });
