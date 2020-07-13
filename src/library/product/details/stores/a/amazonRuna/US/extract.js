f6a96c852a5b8ef35741b87070e126e44f4eca23
Manas Ranjan <manas.n@osmosys.asia>
2020-07-13 22:20:54 +0530

    Updated navigation.

--- src/library/product/details/stores/a/amazonRuna/US/extract.js
+++ src/library/product/details/stores/a/amazonRuna/US/extract.js
@@ -8,21 +8,33 @@ module.exports = {
     domain: 'amazon.com',
   },
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
-    await context.evaluate(async (selector) => {
-      const delay = t => new Promise(resolve => setTimeout(resolve, t));
+    const scrollToContent = async (selector) => {
+      await context.evaluate(async (selectorToWaitFor) => {
+        const delay = t => new Promise(resolve => setTimeout(resolve, t));
 
-      console.log('SCROLLING TO PAGE');
-      const element = document.querySelector(selector);
+        console.log('SCROLLING TO PAGE');
+        const element = document.querySelector(selectorToWaitFor);
 
-      if (!element) {
-        console.log('Element not found');
+        if (!element) {
+          console.log('Element not found');
+          return;
+        }
+
+        element.scrollIntoView(false);
+        await delay(1500);
         return;
-      }
+      }, selector);
+    }
 
-      element.scrollIntoView();
-      await delay(3000);
-      return;
-    }, 'div[data-cel-widget="aplus_feature_div"]');
+    await scrollToContent('.askDetailPageSearchWidgetSection');
+
+    try {
+      await context.waitForSelector('#aplus', { timeout: 30000 });
+    } catch (err) {
+      console.log(`Manufacturer details did not load.`);
+    }
+
+    await scrollToContent('div[data-cel-widget="aplus_feature_div"]');
 
     return await context.extract(productDetails, { transform });
   },
