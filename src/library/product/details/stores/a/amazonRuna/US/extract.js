bc94c7938966f0706e315f3ca4280a6c74253bc4
Manas Ranjan <manas.n@osmosys.asia>
2020-07-13 13:39:50 +0530

    Added a wait for scrolling to manufacturation content.

--- src/library/product/details/stores/a/amazonRuna/US/extract.js
+++ src/library/product/details/stores/a/amazonRuna/US/extract.js
@@ -7,4 +7,23 @@ module.exports = {
     transform,
     domain: 'amazon.com',
   },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    await context.evaluate(async (selector) => {
+      const delay = t => new Promise(resolve => setTimeout(resolve, t));
+
+      console.log('SCROLLING TO PAGE');
+      const element = document.querySelector(selector);
+
+      if (!element) {
+        console.log('Element not found');
+        return;
+      }
+
+      element.scrollIntoView();
+      await delay(3000);
+      return;
+    }, 'div[data-cel-widget="aplus_feature_div"]');
+
+    return await context.extract(productDetails, { transform });
+  },
 };
