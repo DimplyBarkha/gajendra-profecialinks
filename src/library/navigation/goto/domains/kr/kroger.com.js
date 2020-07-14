284278727316884af3a4147f4f5951c6472a3c25
Bindu Datla <bindu.datla@import.io>
2020-07-14 17:46:37 -0400

    testing goto blank

--- src/library/navigation/goto/domains/kr/kroger.com.js
+++ src/library/navigation/goto/domains/kr/kroger.com.js
@@ -10,7 +10,8 @@ module.exports = {
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
     await context.setBlockAds(false);
-    const optUrl = `${url}#[!opt!]{"anti_fingerprint":false, "first_request_timeout": 60, "proxy":{"use_relay_proxy": false}}[/!opt!]`;    
+    const optUrl = `${url}#[!opt!]{"anti_fingerprint":false, "first_request_timeout": 60, "proxy":{"use_relay_proxy": false}}[/!opt!]`;
+    //await context.goto('about:blank');
     await context.goto(optUrl, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
