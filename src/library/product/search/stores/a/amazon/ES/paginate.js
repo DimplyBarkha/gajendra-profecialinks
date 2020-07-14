bdc1b92fc3f1381660b70bdeb857d5021b409c21
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-14 22:04:29 +0530

    updated pagination

--- src/library/product/search/stores/a/amazon/ES/paginate.js
+++ src/library/product/search/stores/a/amazon/ES/paginate.js
@@ -4,10 +4,11 @@ module.exports = {
   parameterValues: {
     country: 'ES',
     store: 'amazon',
-    nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
-    mutationSelector: 'span[cel_widget_id="UPPER-RESULT_INFO_BAR"] div>span[dir="auto"]:first-of-type',
-    spinnerSelector: 'div.s-result-list-placeholder:not([class*="hidden"])',
+    openSearchDefinition: {
+      template: 'https://www.amazon.es/s?k={searchTerms}&__mk_es_ES=ÅMÅŽÕÑ&ref=nb_sb_noss_2&page={page}',
+    },
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No hay resultados")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])]',
     domain: 'amazon.es',
     zipcode: '',
   },
