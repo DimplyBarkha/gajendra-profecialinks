729a82aa57d16d698aca0a59ae6bded27aec9236
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-07-14 14:39:53 +0530

    Issue num: #333

---
+++ src/library/product/search/stores/a/amazonMsCategory/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'amazonMsCategory',
+    transform: null,
+    domain: 'amazon.de',
+    zipcode: '',
+  },
+};
