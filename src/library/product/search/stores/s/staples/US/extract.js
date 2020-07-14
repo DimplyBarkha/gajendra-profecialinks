c52ac67ac2f1b7875e814a723e4ec79f67bebb2b
nikita.kumari <nikita.kumari@synerzip.com>
2020-07-14 18:50:48 +0530

    Codebase setup(#338)

---
+++ src/library/product/search/stores/s/staples/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'staples',
+    transform: null,
+    domain: 'staples.com',
+    zipcode: '',
+  },
+};
