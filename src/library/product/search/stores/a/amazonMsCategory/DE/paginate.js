f1daccf01f3edbdbedc2f997be8f00caf17554f2
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-07-14 18:35:10 +0530

    Resolved issues

--- src/library/product/search/stores/a/amazonMsCategory/DE/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/DE/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//div[contains(text(),"ersonalisierte Empfehlungen anzeigen")]/text() | //img[contains(@alt,"Dogs of Amazon")]',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol/li)]',
     openSearchDefinition: {
       template: 'https://www.amazon.de/gp/bestsellers/*/{searchTerms}?_encoding=UTF8&pg={page}',
     },
