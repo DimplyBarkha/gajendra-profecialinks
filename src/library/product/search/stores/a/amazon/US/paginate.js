ecdd18b41fe8fcb62f7ccd271a36c5b6431cbb23
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-14 21:56:17 +0530

    updated pagination

--- src/library/product/search/stores/a/amazon/US/paginate.js
+++ src/library/product/search/stores/a/amazon/US/paginate.js
@@ -4,9 +4,10 @@ module.exports = {
     country: 'US',
     store: 'amazon',
     domain: 'amazon.com',
-    nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
-    mutationSelector: 'span[cel_widget_id="UPPER-RESULT_INFO_BAR"] div>span[dir="auto"]:first-of-type',
-    spinnerSelector: 'div.s-result-list-placeholder:not([class*="hidden"])',
+    openSearchDefinition: {
+      template: 'https://www.amazon.com/s?k={searchTerms}&ref=nb_sb_noss_2&page={page}',
+    },
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])]',
   },
 };
