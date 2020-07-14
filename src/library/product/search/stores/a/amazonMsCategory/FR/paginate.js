6ee8c1b7bdf993b7ee16d0d5cba99a3368de5fcf
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-14 16:49:56 +0530

    updated no results xpath

--- src/library/product/search/stores/a/amazonMsCategory/FR/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/FR/paginate.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FR',
     store: 'amazonMsCategory',
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Vous recherchez une page")]',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Vous recherchez une page")] | /html[not(//ol/li)]',
     openSearchDefinition: {
       template: 'https://www.amazon.fr/gp/bestsellers/*/{searchTerms}?pg={page}#[!opt!]{"force200":true}[/!opt!]',
     },
