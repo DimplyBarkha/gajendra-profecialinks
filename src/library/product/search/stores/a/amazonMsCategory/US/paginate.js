3169808bedfea647fd108a6f98ef6278c6e2ed19
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-14 10:48:20 +0530

    replaced next link selector with openSearchDefinition

--- src/library/product/search/stores/a/amazonMsCategory/US/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/US/paginate.js
@@ -1,16 +1,17 @@
-const { util } = require('chai');
 
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'US',
     store: 'amazonMsCategory',
-    nextLinkSelector: 'ul.a-pagination>li.a-last:not(.a-disabled)',
+    nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
     noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")]',
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.amazon.com/gp/bestsellers/*/{searchTerms}?_encoding=UTF8&pg={page}',
+    },
     domain: 'amazon.com',
   },
 };
