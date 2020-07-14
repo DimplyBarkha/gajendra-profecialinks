93b3ff14232e44c6c2c90fae6b2a7a9273933c84
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-14 11:30:36 +0530

    #335 built extractor

---
+++ src/library/product/search/stores/a/amazonMsCategory/UK/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'amazonMsCategory',
+    transform: null,
+    domain: 'amazon.co.uk',
+    zipcode: '',
+  },
+};
