aae9651930e405f61857dde487c640ab31ec54ed
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-14 16:46:29 +0530

    updated no results xpath

--- src/library/product/search/stores/a/amazonMsCategory/IT/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/IT/paginate.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'IT',
     store: 'amazonMsCategory',
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")]',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")] | /html[not(//ol/li)]',
     openSearchDefinition: {
       template: 'https://www.amazon.it/gp/bestsellers/*/{searchTerms}?pg={page}#[!opt!]{"force200":true}[/!opt!]',
     },
