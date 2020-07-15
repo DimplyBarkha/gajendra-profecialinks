1088339e2f40678ded55493698868f8323d2ac9e
Manas Ranjan <manas.n@osmosys.asia>
2020-07-15 20:36:30 +0530

    Updated extract code.

--- src/library/product/details/stores/a/amazonRuna/US/extract.js
+++ src/library/product/details/stores/a/amazonRuna/US/extract.js
@@ -1,5 +1,4 @@
 const { transform } = require('../../amazonPharmapacks/US/shared');
-const { productPrimeCheck } = require('../../amazonPharmapacks/US/checkPrime');
 
 module.exports = {
   implements: 'product/details/extract',
@@ -66,7 +65,64 @@ module.exports = {
 
     await scrollToContent('div[data-cel-widget="aplus_feature_div"]');
 
-    await context.evaluate(productPrimeCheck);
+    await context.evaluate(async () => {
+      let primeValue = 'No';
+      const merchantAnchors = document.querySelectorAll('#merchant-info a');
+      const buyBoxSpans = document.querySelectorAll('#buybox span');
+      const metaNames = document.querySelectorAll('meta[name]');
+    
+      const findMatchingString = (nodeList) => {
+        return new Promise((resolve, reject) => {
+          for (const node of nodeList) {
+            const text = node.textContent;
+    
+            if (text.match(/sold by amazon/ig)) {
+              return resolve('Yes - Shipped & Sold');
+            } else if (text.match(/fulfilled by amazon/ig)) {
+              return resolve('Yes - Fulfilled');
+            } else if (text.match(/prime pantry/ig)) {
+              return resolve('Prime Pantry');
+            }
+          }
+    
+          return resolve(undefined);
+        });
+      };
+    
+      if (document.querySelector('i#burjActionPanelAddOnBadge.a-icon.a-icon-addon')) {
+        primeValue = 'Add-On';
+      }
+    
+      if (document.querySelector('body').innerHTML.match(/Exclusively for Prime Members/ig)) {
+        return 'Prime Exclusive';
+      }
+    
+      if (merchantAnchors && merchantAnchors.length) {
+        const res = await findMatchingString(merchantAnchors);
+    
+        if (res) {
+          primeValue = res;
+        }
+      }
+    
+      if (buyBoxSpans && buyBoxSpans.length) {
+        const res = await findMatchingString(buyBoxSpans);
+    
+        if (res) {
+          primeValue = res;
+        }
+      }
+    
+      if (metaNames && metaNames.length) {
+        const res = await findMatchingString(metaNames);
+    
+        if (res) {
+          primeValue = res;
+        }
+      }
+    
+      document.querySelector('body').setAttribute('primeValue', primeValue);
+    });
 
     return await context.extract(productDetails, { transform });
   },
