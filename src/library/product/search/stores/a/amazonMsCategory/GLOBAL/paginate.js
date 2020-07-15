1d12e70a309bb67e7454c584b4cbfbc310b8db51
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-15 16:55:53 +0530

    #311 Added GLOBAL extractor

---
+++ src/library/product/search/stores/a/amazonMsCategory/GLOBAL/paginate.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonMsCategory',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol/li)]',
+    openSearchDefinition: {
+      template: 'https://www.amazon.com/gp/bestsellers/*/{searchTerms}?_encoding=UTF8&pg={page}',
+    },
+    domain: 'amazon.com',
+  },
+};
