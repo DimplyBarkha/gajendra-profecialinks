bcc5e26583def76f8c776ac3ce2d5bc1d1e070bd
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-07-15 12:48:16 +0530

    Issue #341

--- src/library/product/search/stores/a/amazonMsReviews/US/execute.js
+++ src/library/product/search/stores/a/amazonMsReviews/US/execute.js
@@ -7,6 +7,6 @@ module.exports = {
     domain: 'amazon.com',
     url: 'https://www.amazon.com/product-reviews/{searchTerms}/?sortBy=recent',
     loadedSelector: 'div[data-hook=review]',
-    noResultsXPath: null,
+    noResultsXPath: '//div[contains(@class, "no-reviews-section")]', 
   },
 };
