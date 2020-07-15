a46458b2909fabc8d50cf03ea2bfc9b9e424c98d
Sindhusha balla <sindhusha.b@osmosys.asia>
2020-07-15 20:28:48 +0530

    Removed checkprime file

--- src/library/product/details/stores/a/amazonPharmapacks/US/extract.js
+++ src/library/product/details/stores/a/amazonPharmapacks/US/extract.js
@@ -1,5 +1,4 @@
 const { transform } = require('./shared');
-const { productPrimeCheck } = require('../../amazonPharmapacks/US/checkPrime');
 
 module.exports = {
   implements: 'product/details/extract',
@@ -10,6 +9,66 @@ module.exports = {
     domain: 'amazon.com',
   },
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    const productPrimeCheck = async () => {
+      console.log('EXECUTING PRIME RELATED CODE.');
+      let primeValue = 'No';
+      const merchantAnchors = document.querySelectorAll('#merchant-info a');
+      const buyBoxSpans = document.querySelectorAll('#buybox span');
+      const metaNames = document.querySelectorAll('meta[name]');
+
+      const findMatchingString = (nodeList) => {
+        return new Promise((resolve, reject) => {
+          for (const node of nodeList) {
+            const text = node.textContent;
+
+            if (text.match(/sold by amazon/ig)) {
+              return resolve('Yes - Shipped & Sold');
+            } else if (text.match(/fulfilled by amazon/ig)) {
+              return resolve('Yes - Fulfilled');
+            } else if (text.match(/prime pantry/ig)) {
+              return resolve('Prime Pantry');
+            }
+          }
+
+          return resolve(undefined);
+        });
+      };
+
+      if (document.querySelector('i#burjActionPanelAddOnBadge.a-icon.a-icon-addon')) {
+        primeValue = 'Add-On';
+      }
+
+      if (document.querySelector('body').innerHTML.match(/Exclusively for Prime Members/ig)) {
+        return 'Prime Exclusive';
+      }
+
+      if (merchantAnchors && merchantAnchors.length) {
+        const res = await findMatchingString(merchantAnchors);
+
+        if (res) {
+          primeValue = res;
+        }
+      }
+
+      if (buyBoxSpans && buyBoxSpans.length) {
+        const res = await findMatchingString(buyBoxSpans);
+
+        if (res) {
+          primeValue = res;
+        }
+      }
+
+      if (metaNames && metaNames.length) {
+        const res = await findMatchingString(metaNames);
+
+        if (res) {
+          primeValue = res;
+        }
+      }
+
+      document.querySelector('body').setAttribute('primeValue', primeValue);
+    };
+
     const scrollToContent = async (selector) => {
       await context.evaluate(async (selectorToScrollTo) => {
         function scrollToSmoothly (pos, time) {
@@ -48,6 +107,9 @@ module.exports = {
           });
         }
         const elem = document.querySelector(selectorToScrollTo);
+        if (!elem) {
+          return;
+        }
         await scrollToSmoothly(elem.offsetTop);
       }, selector);
     };
