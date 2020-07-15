b4fb265e287f2aab1a81b20a1eab6cabd9f63d22
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-07-15 09:45:38 +0530

    Commiting the changes

--- src/library/product/search/stores/a/amazonMsCategory/DE/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/DE/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol/li)]',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Suchen Sie bestimmte Informationen")]| /html[not(//ol/li)]',
     openSearchDefinition: {
       template: 'https://www.amazon.de/gp/bestsellers/*/{searchTerms}?_encoding=UTF8&pg={page}',
     },
