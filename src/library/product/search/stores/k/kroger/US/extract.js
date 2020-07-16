ddef23ddb20a89f9d2c842e314e6c8515b0f6750
alscotty <alawrencescott@gmail.com>
2020-07-15 17:09:14 -0700

    add custom implementations to their own file for search

--- src/library/product/search/stores/k/kroger/US/extract.js
+++ src/library/product/search/stores/k/kroger/US/extract.js
@@ -1,42 +1,5 @@
 const { transform } = require('../../../../shared');
-
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-
-  await context.evaluate(() => {
-    console.log('new page!');
-    // potential page 2 confirmation popup:
-    const confirmButton = document.querySelector('button[data-testid="DynamicTooltip-Button-confirm"]');
-    if (confirmButton) {
-      confirmButton.click();
-    }
-
-    const searchEl = document.querySelector('span.kds-Text--l');
-    let searchTerms;
-    if (searchEl) {
-      const searchText = searchEl.textContent;
-      const idx = searchText.indexOf(':');
-      searchTerms = searchText.slice(idx + 2);
-    }
-
-    const url = `https://www.kroger.com/search?query=${searchTerms}&searchType=natural`;
-
-    const searchUrlDiv = document.createElement('div');
-    searchUrlDiv.classList.add('my-search-url');
-    searchUrlDiv.style.display = 'none';
-    searchUrlDiv.textContent = url;
-
-    document.body.appendChild(searchUrlDiv);
-  });
-
-  return await context.extract(productDetails, { transform });
-}
+const { implementation } = require('./sharedSearchExtract');
 
 module.exports = {
   implements: 'product/search/extract',
