302de38e060490aaf450979c8b2af679ebe5a3be
alscotty <alawrencescott@gmail.com>
2020-07-16 12:34:43 -0700

    one more fix to searchUrl

--- src/library/product/search/stores/a/amazonFresh/US/extract.js
+++ src/library/product/search/stores/a/amazonFresh/US/extract.js
@@ -10,14 +10,19 @@ async function implementation (
   const { productDetails } = dependencies;
   await context.waitForXPath('//div/@data-asin');
 
-  await context.evaluate(()=>{
-    const searchUrl = window.location.href;
-    const urlDiv = document.createElement('div');
-    urlDiv.id = 'my-search-url';
-    urlDiv.style.display = 'none';
-    urlDiv.textContent = searchUrl;
-    document.body.appendChild(urlDiv);
-  })
+  await context.evaluate(() => {
+    const searchEl = document.querySelector('div#my-search-url');
+    if (searchEl) {
+      searchEl.textContent = window.location.href;
+    } else {
+      const searchUrl = window.location.href;
+      const urlDiv = document.createElement('div');
+      urlDiv.id = 'my-search-url';
+      urlDiv.style.display = 'none';
+      urlDiv.textContent = searchUrl;
+      document.body.appendChild(urlDiv);
+    }
+  });
 
   return await context.extract(productDetails, { transform });
 }
