e53034f7133860361b68564f7446bf686b7b66d1
satyam <satyam.rathod@synerzip.com>
2020-07-16 12:17:42 +0530

    #347 added all xpath and required transformations

--- src/library/product/details/stores/h/homedepot_lg/US/extract.js
+++ src/library/product/details/stores/h/homedepot_lg/US/extract.js
@@ -1,11 +1,148 @@
+const { transform } = require('../format');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'homedepot_lg',
-    transform: null,
+    transform,
     domain: 'homedepot.com',
     zipcode: '',
+  },
+  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+    const RegularSite = await context.evaluate(async function () {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+      const nameExtendedSelector = document.querySelector('meta[itemprop="name"]');
+      if (!nameExtendedSelector) {
+        const nameExtendedSelectorNew = document.querySelector('meta[property="og:title"]');
+        const nameExtended = nameExtendedSelectorNew ? nameExtendedSelectorNew.getAttribute('content') : '';
+        const newDiv = document.createElement('meta');
+        newDiv.setAttribute('itemprop', 'name');
+        newDiv.content = nameExtended;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+
+      // availability Check
+      let availabilityText = 'In Stock';
+      const discontinuedMsg = document.evaluate('//span[contains(text(),"Discontinued")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (discontinuedMsg) {
+        availabilityText = 'Discontinued';
+      }
+      const addToCartCheck = document.evaluate('//div[@class="buybelt-wrapper"]//*[normalize-space(text())="Add to Cart"] | //div[@class="buybox"]//*[normalize-space(text())="Add to Cart"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (addToCartCheck) {
+        // @ts-ignore
+        if (addToCartCheck.parentElement && addToCartCheck.parentElement.disabled) {
+          availabilityText = 'Out Of Stock';
+        }
+      } else {
+        availabilityText = 'Out Of Stock';
+      }
+
+      addHiddenDiv('custom_availability_text', availabilityText);
+      const regularSite = document.querySelector('div#thumbnails');
+      async function addVideoUrl (videoId) {
+        const videoJson = await fetch(`https://edge.api.brightcove.com/playback/v1/accounts/66036796001/videos/${videoId}`, {
+          headers: {
+            accept: 'application/json;pk=BCpkADawqM1R4SCVtAbuCMoV8_qAjR72HCIdahKkP_EVYDN5CZrBJIcAnpRv8i1e-btnRvDxRYedYyE9G44ucRGlPepdnvI7oXLRIPxAXyFvLd3W9D1-bEwmIoo',
+            'accept-language': 'en-US,en;q=0.9',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+          },
+          referrer: window.location.origin,
+          referrerPolicy: 'no-referrer-when-downgrade',
+          body: null,
+          method: 'GET',
+          mode: 'cors',
+          credentials: 'omit',
+        }).then(r => r.json());
+        try {
+          const regex = new RegExp(`^https.*${videoId}$`);
+          const videoUrl = videoJson.sources.map(({ src }) => src && regex.test(src) && src).filter(Boolean)[0];
+          addHiddenDiv(`video_url${videoId}`, videoUrl);
+        } catch (error) {
+          console.log('video not found');
+        }
+      }
+      if (!regularSite) {
+        const imgExpander = document.querySelector('span.mediagallery__thumbnailicons--count');
+        if (imgExpander) {
+          // @ts-ignore
+          imgExpander.click();
+        } else {
+          // @ts-ignore
+          for (const [index, img] of document.querySelectorAll('div[class="mediagallery__thumbnails"] img').entries()) {
+            // @ts-ignore
+            if (!img.src.includes('videoId')) {
+              // @ts-ignore
+              addHiddenDiv(`altImage${index}`, img.src);
+            } else {
+              const videoId = img.getAttribute('src').replace(/.*videoId=(.*$)/, '$1');
+              await addVideoUrl(videoId);
+            }
+          }
+        }
+        return !!imgExpander;
+      } else {
+        // @ts-ignore
+        for (const video of document.querySelectorAll('div#thumbnails a[data-target="VIDEO"]:not(.media__thumbnail-additional)')) {
+          const videoId = video.getAttribute('data-media_id');
+          await addVideoUrl(videoId);
+        };
+      }
+    });
+    if (RegularSite) {
+      try {
+        await context.waitForXPath('//div[@class="overlay__side-content__product-images"]//img');
+        await context.evaluate(async function () {
+          function addHiddenDiv (id, content) {
+            const newDiv = document.createElement('div');
+            newDiv.id = id;
+            newDiv.textContent = content;
+            newDiv.style.display = 'none';
+            document.body.appendChild(newDiv);
+          }
+          async function addVideoUrl (videoId) {
+            const videoJson = await fetch(`https://edge.api.brightcove.com/playback/v1/accounts/66036796001/videos/${videoId}`, {
+              headers: {
+                accept: 'application/json;pk=BCpkADawqM1R4SCVtAbuCMoV8_qAjR72HCIdahKkP_EVYDN5CZrBJIcAnpRv8i1e-btnRvDxRYedYyE9G44ucRGlPepdnvI7oXLRIPxAXyFvLd3W9D1-bEwmIoo',
+                'accept-language': 'en-US,en;q=0.9',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'cross-site',
+              },
+              referrer: window.location.origin,
+              referrerPolicy: 'no-referrer-when-downgrade',
+              body: null,
+              method: 'GET',
+              mode: 'cors',
+              credentials: 'omit',
+            }).then(r => r.json());
+            try {
+              const regex = new RegExp(`^https.*${videoId}$`);
+              const videoUrl = videoJson.sources.map(({ src }) => src && regex.test(src) && src).filter(Boolean)[0];
+              addHiddenDiv(`video_url${videoId}`, videoUrl);
+            } catch (error) {
+              console.log('video not found');
+            }
+          }
+          // @ts-ignore
+          for (const img of document.querySelectorAll('div[class="overlay__side-content__alt-media"] img')) {
+            const videoId = img.getAttribute('src').replace(/.*videoId=(.*$)/, '$1');
+            await addVideoUrl(videoId);
+          };
+        });
+      } catch (error) {
+        console.log('Error');
+      }
+    }
+    return await context.extract(productDetails, { transform });
   },
 };
---
+++ src/library/product/details/stores/h/homedepot_lg/format.js
@@ -0,0 +1,72 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.alternateImages) {
+        const secondaryImages = [];
+        row.alternateImages.forEach(alternateImage => {
+          if (!alternateImage.text.includes('videoId')) {
+            alternateImage.text = alternateImage.text.replace(/\d+(\..*)$/, '1000$1');
+            secondaryImages.push(alternateImage);
+          }
+        });
+        row.alternateImages = secondaryImages;
+        row.secondaryImageTotal = [{
+          text: secondaryImages.length,
+        }];
+      }
+      if (row.specifications) {
+        row.specifications.forEach(item => {
+          item.text = item.text.replace(/\n \n \n \n \n/g, ' ').replace(/\n \n \n \n/g, ' | ').replace('Specifications', '').replace(/\n \n/g, ' : ').trim();
+        });
+      }
+      if (row.shippingDimensions) {
+        row.shippingDimensions.forEach(item => {
+          item.text = item.text.replace(/\n \n \n \n/g, ' | ').replace(/(\n\s?){1,2}/g, ' : ').trim();
+        });
+      }
+      if (row.descriptionBullets) {
+        row.descriptionBullets.forEach(item => {
+          if (item.text === '0') {
+            item.text = '';
+          }
+        });
+      }
+      if (row.description) {
+        row.description.forEach(item => {
+          item.text = item.text.replace(/(\s?\n)+/g, ' || ').trim();
+        });
+      }
+      if (row.nameExtended) {
+        row.nameExtended.forEach(item => {
+          item.text = item.text.replace(/-\s*The Home Depot$/, '').trim();
+        });
+      }
+      if (row.warnings) {
+        row.warnings.forEach(item => {
+          item.text = item.text.replace(/see\s*/i, '').trim();
+        });
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          if (item.text.includes('Product')) {
+            const price = item.text.replace(/.*"price":(.*?),.*/, '$1');
+            item.text = `$${price}`;
+          }
+        });
+      }
+      if (row.mpc) {
+        row.mpc.forEach(item => {
+          item.text = item.text.replace(/.*?#/, '').trim();
+        });
+      }
+    }
+  }
+  return data;
+};
+
+module.exports = { transform };
