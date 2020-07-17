ce88355ee69b0d8090213ac379dc0dde0491e87a
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-17 16:46:54 +0530

    #331 added price transform

--- src/library/product/search/stores/a/amazonMsCategory/IT/extract.js
+++ src/library/product/search/stores/a/amazonMsCategory/IT/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'IT',
     store: 'amazonMsCategory',
-    transform: null,
+    transform,
     domain: 'amazon.it',
     zipcode: '',
   },
