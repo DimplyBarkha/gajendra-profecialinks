48e9c0cd20cb2795e7fe8a58d07f3dca3ebf3233
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-17 14:29:56 +0530

    #351. Created extractors

---
+++ src/library/product/details/stores/a/amazonMsPromos/CA/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'amazon.ca',
+    prefix: null,
+    url: 'https://www.amazon.com/gp/goldbox?ref_=nav_cs_gb',
+    country: 'CA',
+    store: 'amazonMsPromos',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/amazonMsPromosDealId/CA/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'CA',
+    store: 'amazonMsPromosDealId',
+    domain: 'amazon.ca',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
