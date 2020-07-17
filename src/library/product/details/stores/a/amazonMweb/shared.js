edd5e3f6e38eaf44da4affd1c1d5149856602644
Akansha <akanshad.wajge@gmail.com>
2020-07-17 17:30:36 +0530

    QA fixes (#260)

--- src/library/product/details/stores/a/amazonMweb/shared.js
+++ src/library/product/details/stores/a/amazonMweb/shared.js
@@ -33,30 +33,68 @@ const transform = (data) => {
           },
         ];
       }
-      if (row.aggregateRating2) {
-        let text = '';
-        row.aggregateRating2.forEach(item => {
-          text += `${item.text.replace(',', '.')}`;
+      if (row.aggregateRating) {
+        row.aggregateRating.forEach(aggregateRating => {
+          aggregateRating.text = aggregateRating.text.replace('sur', '').trim().replace(',', '.');
         });
-        row.aggregateRating2 = [
-          {
-            text: cleanUp(text),
-          },
-        ];
+      }
+      if (row.variantCount) {
+        row.variantCount.forEach(variantCount => {
+          if (variantCount.text < 2) {
+            variantCount.text = 1;
+          }
+        });
       }
       if (row.price) {
         row.price.forEach(price => {
           price.text = price.text.replace('.', '').replace(',', '.').trim();
         });
       }
-      if (row.description) {
-        row.description.forEach(description => {
-          description.text = cleanUp(description.text);
+      if (row.specifications) {
+        let text = '';
+        row.specifications.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        });
+        row.specifications = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+      }
+      if (row.availabilityText) {
+        row.availabilityText.forEach(availabilityText => {
+          availabilityText.text = availabilityText.text.trim();
         });
       }
       if (row.manufacturerDescription) {
-        row.manufacturerDescription.forEach(manufacturerDescription => {
-          manufacturerDescription.text = cleanUp(manufacturerDescription.text);
+        let text = '';
+        row.manufacturerDescription.forEach(item => {
+          item.text = `${item.text.replace(/([\<img].*[\"\>])/g, ' ').trim()}  `;
+          text += `${item.text.replace(/\n \n/g, ' ').trim()}  `;
+        });
+        row.manufacturerDescription = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+      }
+      if (row.manufacturerImages) {
+        row.manufacturerImages.forEach(manufacturerImages => {
+          if (manufacturerImages.text.includes('grey-pixel.gif')) {
+            manufacturerImages.text = '';
+          } else {
+            manufacturerImages.text = manufacturerImages.text.replace('._AC_US40_', '');
+          }
+        });
+      }
+      if (row.alternateImages) {
+        row.alternateImages.forEach(alternateImages => {
+          alternateImages.text = alternateImages.text.replace('._AC_US40_', '');
+        });
+      }
+      if (row.brandText) {
+        row.brandText.forEach(brandText => {
+          brandText.text = brandText.text.replace('Marque :', '').trim();
         });
       }
     }
