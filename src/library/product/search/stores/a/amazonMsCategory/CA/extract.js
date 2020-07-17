6ab4ff4fa856420523d00b4fc22df2f2bdb5e506
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-17 16:34:39 +0530

    #334 added price transform

--- src/library/product/search/stores/a/amazonMsCategory/CA/extract.js
+++ src/library/product/search/stores/a/amazonMsCategory/CA/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'CA',
     store: 'amazonMsCategory',
-    transform: null,
+    transform,
     domain: 'amazon.ca',
     zipcode: '',
   },
