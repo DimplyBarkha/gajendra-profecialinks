07fb122275076b39bfede766acff944c7aa68faa
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-17 15:10:03 +0530

    #354. created extractor

---
+++ src/library/product/details/stores/a/amazonMsPromosDealId/ES/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'amazonMsPromosDealId',
+    domain: 'amazon.es',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
