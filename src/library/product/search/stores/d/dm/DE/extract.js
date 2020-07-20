fc75c22431545b2689dc3833399cde80f5cdf025
Manoj <manoj.d@osmosys.asia>
2020-07-20 12:20:22 +0530

    updated code

--- src/library/product/search/stores/d/dm/DE/extract.js
+++ src/library/product/search/stores/d/dm/DE/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require('../shared');
+const { transform } = require('../../../../shared');
 
 async function implementation (
   inputs,
@@ -12,8 +12,21 @@ async function implementation (
     document.querySelectorAll('[data-dmid="product-tile-rating"]').forEach(elm => {
       const ratingStars = [...elm.querySelectorAll('img[data-dmid="filled-star"]')].map(rate => Number(rate.getAttribute('alt') ? rate.getAttribute('alt').match(/\d+/)[0] : '0'));
       const rating = ratingStars.reduce((a, b) => a + b, 0) / 100;
-      const ratingValue = rating.toString().replace(".", ",");
+      const ratingValue = rating.toString().replace('.', ',');
       elm.setAttribute('rating', ratingValue);
+    });
+    document.querySelectorAll('div[data-dmid="product-description"]').forEach(elm => {
+      const brand = elm.querySelector('span[data-dmid="product-brand"]');
+      const name = elm.querySelector('a[data-dmid="dm-link"]').getAttribute('title');
+      const size = elm.querySelector('a[data-dmid="dm-link"] > span');
+      const brandName = brand.innerHTML;
+      if (size.innerHTML.includes('...')) {
+	      const fullName = brandName.concat(' ', name, ' ', size.innerHTML.replace(new RegExp('(.+,\\s)(\\d+\\s(ml|g|St|Wl|l))', 'g'), ', $2'));
+        elm.setAttribute('name', fullName);
+      } else {
+	      const fullName = brandName.concat(' ', size.innerHTML);
+	      elm.setAttribute('name', fullName);
+      }
     });
   });
   return await context.extract(productDetails, { transform });
