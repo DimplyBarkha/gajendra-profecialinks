9839fd8bd2628105a87655c0c6103791f32f5328
David Skoglund <david.skoglund@import.io>
2020-07-21 19:58:15 -0700

    #311. deployed to demo.

---
+++ src/library/product/details/stores/a/amazonMsCategory/US/createUrl.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'amazon.com',
+    prefix: null,
+    url: null,
+    country: 'US',
+    store: 'amazonMsCategory',
+  },
+};
---
+++ src/library/product/details/stores/a/amazonMsCategory/US/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonMsCategory',
+    transform: null,
+    domain: 'amazon.com',
+  },
+};
