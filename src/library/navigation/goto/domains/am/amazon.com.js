133d6a0f624dafc6f66f71bc7c1e5440c7d710bc
Satyam Rathod <satyam.rathod@synerzip.com>
2020-07-22 16:01:38 +0530

    #252 amazon captcha code updated

--- src/library/navigation/goto/domains/am/amazon.com.js
+++ src/library/navigation/goto/domains/am/amazon.com.js
@@ -50,6 +50,7 @@ module.exports = {
 
     const solveCaptchaIfNecessary = async () => {
       console.log('Checking for CAPTCHA');
+      await new Promise(resolve => setTimeout(resolve, 1000));
       while (await isCaptcha() === 'true' && captchas < MAX_CAPTCHAS) {
         captchas++;
         if (backconnect) {
@@ -64,16 +65,16 @@ module.exports = {
           console.log('We failed to solve the CAPTCHA');
           return context.reportBlocked(lastResponseData.code, 'Blocked: Could not solve CAPTCHA, attempts=' + captchas);
         }
-        return false;
+        return 'false';
       }
-      return true;
+      return 'true';
     };
     const run = async () => {
       // do we perhaps want to go to the homepage for amazon first?
       lastResponseData = await context.goto(url, {
-        timeout: 10000,
+        timeout: 60000,
         waitUntil: 'load',
-        checkBlocked: true,
+        checkBlocked: false,
         js_enabled: true,
         css_enabled: false,
         random_move_mouse: true,
@@ -89,14 +90,14 @@ module.exports = {
 
         console.log('Waiting for page to reload on homepage');
         context.waitForNavigation();
-        if (!await solveCaptchaIfNecessary()) {
+        if (await solveCaptchaIfNecessary() === 'false') {
           hasCaptcha = true;
           return;
         }
 
         console.log('Go to some random page');
         const clickedOK = await context.evaluate(async function () { //* [contains(@id,'contextualIngressPtLabel_deliveryShortLine')]/spa
-          const randomLinkEls = document.evaluate("a[href*='/dp/']", document, null, XPathResult.ANY_TYPE, null);
+          const randomLinkEls = document.evaluate("//a[contains(@href,'/dp/')]", document, null, XPathResult.ANY_TYPE, null);
           const randomLinkEl = randomLinkEls.iterateNext();
           if (randomLinkEl) {
             // @ts-ignore
@@ -116,7 +117,7 @@ module.exports = {
 
         console.log('Going back to desired page');
         lastResponseData = await context.goto(url, {
-          timeout: 10000,
+          timeout: 60000,
           waitUntil: 'load',
           checkBlocked: true,
           js_enabled: true,
@@ -141,7 +142,7 @@ module.exports = {
         return context.reportBlocked(lastResponseData.status, 'Blocked: ' + lastResponseData.status);
       }
 
-      if (!await solveCaptchaIfNecessary) {
+      if (await solveCaptchaIfNecessary() === 'false') {
         hasCaptcha = true;
         return;
       }
@@ -151,9 +152,8 @@ module.exports = {
       }
 
       const wrongLocale = await context.evaluate(async function () {
-        const detailsLocaleEl = document.evaluate("//*[contains(@id,'contextualIngressPtLabel_deliveryShortLine')]/span", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        const searchLocaleEl = document.evaluate("//span[@id='glow-ingress-line1']//*[contains(text(),':')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        if (!!detailsLocaleEl.snapshotLength || !!searchLocaleEl.snapshotLength) {
+        const localeEl = document.evaluate("//div[contains(@id, 'glow-toaster-body') and not(//*[contains(text(), 'Amazon Fresh')])]/following-sibling::div[@class='glow-toaster-footer']//input[@data-action-type='SELECT_LOCATION']", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        if (localeEl.snapshotLength > 0) {
           return 'true';
         } else {
           return 'false';
@@ -170,11 +170,10 @@ module.exports = {
         // return extractorContext.raiseError('WRONG_GEO', 'Incorrect locale detected');
       }
     };
-
     try {
       await run();
     } finally {
-      // needs to be non-fat arrow
+    // needs to be non-fat arrow
       await context.evaluate((captchaCount, duration, js, hasCaptcha) => {
         const captchasElt = document.createElement('meta');
         captchasElt.name = 'captchas';
@@ -194,9 +193,9 @@ module.exports = {
         document.head.appendChild(javascriptElt);
         // js_enabled
       }, [captchas, Date.now() - start, hasCaptcha]);
-    }
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode });
+      if (zipcode) {
+        await dependencies.setZipCode({ url: url, zipcode: zipcode });
+      }
     }
   },
 };
