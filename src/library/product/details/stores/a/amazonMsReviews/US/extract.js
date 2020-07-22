30005b6132e96e0eb89abb0552c8b19a616db55f
David Skoglund <david.skoglund@import.io>
2020-07-22 07:46:39 -0700

    #315. deploying to demo.

---
+++ src/library/product/details/stores/a/amazonMsReviews/US/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonMsReviews',
+    transform: null,
+    domain: 'amazon.com',
+  },
+};
