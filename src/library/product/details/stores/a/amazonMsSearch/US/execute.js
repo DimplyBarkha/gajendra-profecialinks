4e857e0ec9a150c8782ee18078e9f51c89f98891
David Skoglund <david.skoglund@import.io>
2020-07-21 20:14:56 -0700

    #295. deployed to demo.

---
+++ src/library/product/details/stores/a/amazonMsSearch/US/execute.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonMsSearch',
+    domain: 'amazon.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+  },
+};
