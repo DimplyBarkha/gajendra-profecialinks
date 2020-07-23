f7d4def7440f6d6209f6091a8b9edaa7b8bdd3c7
David Skoglund <david.skoglund@import.io>
2020-07-23 15:04:44 -0700

    #341. Updated noResultsXpath, and added function to check for no pagination. May be redundant

--- src/library/product/search/stores/a/amazonMsReviews/CA/execute.js
+++ src/library/product/search/stores/a/amazonMsReviews/CA/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.ca',
     url: 'https://www.amazon.ca/product-reviews/{searchTerms}/?sortBy=recent',
     loadedSelector: 'div[data-hook=review]',
-    noResultsXPath: '//div[contains(@class, "no-reviews-section")]',
+    noResultsXPath: '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")]',
     zipcode: '',
   },
 };
