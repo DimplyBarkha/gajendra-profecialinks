805ac2109db6fa8f1d74cad703b39cd7ef2d64a7
Nilesh <nilesh.b@osmosys.asia>
2020-07-24 19:05:53 +0530

    Fixed all the issue.

--- src/library/product/details/stores/s/sainsburys/shared.js
+++ src/library/product/details/stores/s/sainsburys/shared.js
@@ -9,43 +9,39 @@ const transform = (data) => {
       if (row.caloriesPerServing) {
         let text = '';
         row.caloriesPerServing.forEach(item => {
-          if(item.text.endsWith("/")){
-            text += item.text.slice(0,-1)
-          }else{
+          if (item.text.endsWith("/")) {
+            text += item.text.slice(0, -1)
+          } else {
             text += item.text;
           }
         });
-        row.caloriesPerServing = [
-          {
-            text: text,
-          },
-        ];
+        row.caloriesPerServing = [{
+          text: text.replace(/\n/g, '').replace('//','/').replace(/(.+)kJ(.+)kcal/g,'$1$2').replace(/\s{1,}/g, '').trim()
+        }, ];
       }
       if (row.allergyAdvice) {
         let text = '';
         row.allergyAdvice.forEach(item => {
           text += item.text.replace(/\n/g, '');
         });
-        row.allergyAdvice = [
-          {
-            text: text,
-          },
-        ];
+        row.allergyAdvice = [{
+          text: text,
+        }, ];
       }
       if (row.description) {
         let text = '';
         row.description.forEach(item => {
           text = row.description.map(elm => elm.text).join(' ').replace(/\s{2,}/g, ' ').replace(/\n\\n/g, '').trim();;
         });
-        row.description = [
-          {
-            text: text,
-          },
-        ];
+        row.description = [{
+          text: text,
+        }, ];
       }
     }
   }
   return data;
 };
 
-module.exports = { transform };
+module.exports = {
+  transform
+};
