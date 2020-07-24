154d776f897ba8973b3c9d7920f9244718c77890
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-07-24 20:01:08 +0530

    createUrl changes(#188)

--- src/library/product/details/stores/a/amazonLg/US/createUrl.js
+++ src/library/product/details/stores/a/amazonLg/US/createUrl.js
@@ -5,7 +5,8 @@ module.exports = {
     prefix: 'dp',
     country: 'US',
     store: 'amazonLg',
-    url: 'https://amazon.com/dp/{id}?th=1&psc=1',
+    // url: 'https://amazon.com/dp/{id}?th=1&psc=1',
+    url: 'https://amazon.com/dp/{id}?th=1&language=en_US&showDetailProductDesc=1#productDescription_feature_div',
     zipcode: '',
   },
 };
