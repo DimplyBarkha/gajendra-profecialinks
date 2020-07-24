79d04eec6589239adfb388543b7a33e9c022a711
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-07-24 10:12:00 +0530

    fixed PR issues

--- src/library/navigation/goto/domains/am/setZipCode.amazon.de.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.de.js
@@ -1,10 +1,9 @@
 module.exports = {
-    implements: 'navigation/goto/setZipCode',
-    parameterValues: {
-      country: 'DE',
-      domain: 'amazon.de',
-      store: 'amazonApparel',
-      zipcode: '',
-    },
-  };
-  
+	implements: 'navigation/goto/setZipCode',
+	parameterValues: {
+		country: 'DE',
+		domain: 'amazon.de',
+		store: 'amazonApparel',
+		zipcode: '',
+	},
+};
