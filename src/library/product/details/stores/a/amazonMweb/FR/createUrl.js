5f66a87581e897eafffc08c2be8a6f2d05c50872
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-07-24 16:47:54 +0530

    createUrl changes (#246)

--- src/library/product/details/stores/a/amazonMweb/FR/createUrl.js
+++ src/library/product/details/stores/a/amazonMweb/FR/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'amazon.fr',
     prefix: 'dp',
-    url: 'https://amazon.fr/dp/{id}',
+    url: 'https://amazon.fr/dp/{id}?showDetailProductDesc=1',
     country: 'FR',
     store: 'amazonMweb',
   },
