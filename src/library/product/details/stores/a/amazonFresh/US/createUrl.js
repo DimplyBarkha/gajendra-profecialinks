2419c9892573d15afac0e3835eae146f2f97b38c
sushma-mangalapally <sushma.mangalapally@import.io>
2020-07-24 21:42:10 -0700

    Add opt tag to url

--- src/library/product/details/stores/a/amazonFresh/US/createUrl.js
+++ src/library/product/details/stores/a/amazonFresh/US/createUrl.js
@@ -5,6 +5,7 @@ module.exports = {
     domain: 'amazon.com',
     country: 'US',
     store: 'amazonFresh',
-    url: 'https://amazon.com/dp/product/{id}/?fpw=alm&s=amazonfresh&fpw=fresh#[!opt!]{"load_all_resources":true}[/!opt!]',
+    url: 'https://amazon.com/dp/product/{id}/?fpw=alm&s=amazonfresh&fpw=fresh#[!opt!]{  "headers": {"cache-control": "max-age=0"}}[/!opt!]',
+    // url: 'https://amazon.com/dp/product/{id}/?fpw=alm&s=amazonfresh&fpw=fresh#[!opt!]{"load_all_resources":true, "block_ads":false}[/!opt!]',
   },
 };
