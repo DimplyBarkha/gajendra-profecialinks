96425fec2529720b4ffe2d7d7b0b74c334b38de9
David Skoglund <david.skoglund@import.io>
2020-07-25 17:45:03 -0700

    #343. updated noResultsXpath after 404 issue.

--- src/library/product/search/stores/a/amazonMsReviews/ES/execute.js
+++ src/library/product/search/stores/a/amazonMsReviews/ES/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.es',
     url: 'https://www.amazon.es/product-reviews/{searchTerms}/?sortBy=recent',
     loadedSelector: 'div[data-hook=review]',
-    noResultsXPath: '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")]',
+    noResultsXPath: '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "404")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")]',
     zipcode: '',
   },
 };
