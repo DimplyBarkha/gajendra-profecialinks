bac5f6b21f5ce5c4e916dda2dd61bb5ff46cfe13
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-27 11:03:44 +0530

    #311 updated noResultsXpath, proxy and deployed with CLI 0.3.5

--- src/library/product/search/stores/a/amazonMsCategory/US/execute.js
+++ src/library/product/search/stores/a/amazonMsCategory/US/execute.js
@@ -6,7 +6,6 @@ module.exports = {
     store: 'amazonMsCategory',
     domain: 'amazon.com',
     url: 'https://www.amazon.com/gp/bestsellers/{searchTerms}',
-    loadedSelector: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)]',
-    noResultsXPath: null,
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
   },
 };
--- src/library/product/search/stores/a/amazonMsCategory/US/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/US/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)]',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
     openSearchDefinition: {
       template: 'https://www.amazon.com/gp/bestsellers/{searchTerms}?pg={page}',
     },
