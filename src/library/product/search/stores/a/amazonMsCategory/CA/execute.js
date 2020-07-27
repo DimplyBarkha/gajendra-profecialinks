91bf894e289c89793a79e6b8a8db8bbd539f1fc7
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-27 10:53:38 +0530

    #334 updated noResultsXpath, proxy and deployed with CLI 0.3.5

--- src/library/product/search/stores/a/amazonMsCategory/CA/execute.js
+++ src/library/product/search/stores/a/amazonMsCategory/CA/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.ca',
     url: 'https://www.amazon.ca/gp/bestsellers/*/{searchTerms}?_encoding=UTF8',
     loadedSelector: null,
-    noResultsXPath: null,
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/a/amazonMsCategory/CA/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/CA/paginate.js
@@ -9,7 +9,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")]',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
     openSearchDefinition: {
       template: 'https://www.amazon.ca/gp/bestsellers/*/{searchTerms}?_encoding=UTF8&pg={page}#[!opt!]{"force200":true}[/!opt!]',
     },
