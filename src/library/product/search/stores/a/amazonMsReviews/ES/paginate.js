d6df20d882106660a0b91aa67ea362c5fc1d57e2
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-27 12:09:22 +0530

    #343 updated noResultsXpath, proxy and deployed with CLI 0.3.5

--- src/library/product/search/stores/a/amazonMsReviews/ES/paginate.js
+++ src/library/product/search/stores/a/amazonMsReviews/ES/paginate.js
@@ -12,7 +12,7 @@ async function implementation (
 ) {
   const { _date, page, keywords } = inputs;
   const loadedSelector = 'div[data-hook=review]';
-  const noResultsXPath = '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")]';
+  const noResultsXPath = '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")]';
   const openSearchDefinition = {
     template: 'https://www.amazon.es/product-reviews/{searchTerms}?sortBy=recent&pageNumber={page}',
   };
