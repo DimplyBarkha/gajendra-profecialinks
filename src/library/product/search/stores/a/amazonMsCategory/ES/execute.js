9bdc66dd06203e625af8ff279e55cf36a266d4ea
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-27 10:57:01 +0530

    #336 updated noResultsXpath, proxy and deployed with CLI 0.3.5

--- src/library/product/search/stores/a/amazonMsCategory/ES/execute.js
+++ src/library/product/search/stores/a/amazonMsCategory/ES/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.es',
     url: 'https://www.amazon.es/gp/bestsellers/*/{searchTerms}',
     loadedSelector: null,
-    noResultsXPath: null,
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")] | /html[not(//ol/li)]',
     zipcode: '',
   },
 };
