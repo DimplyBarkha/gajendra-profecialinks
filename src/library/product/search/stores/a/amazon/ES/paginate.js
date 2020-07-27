64300f02185b9495fdd90cb4d7a34958588033b7
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-27 11:37:59 +0530

    #329 updated noResultsXpath, proxy and deployed with CLI 0.3.5

--- src/library/product/search/stores/a/amazon/ES/paginate.js
+++ src/library/product/search/stores/a/amazon/ES/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
       template: 'https://www.amazon.es/s?k={searchTerms}&__mk_es_ES=ÅMÅŽÕÑ&ref=nb_sb_noss_2&page={page}',
     },
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
-    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No hay resultados")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])]',
+    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No hay resultados")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")]',
     domain: 'amazon.es',
     zipcode: '',
   },
