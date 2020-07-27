8292e12086f437b01e2a5e54ddea8925fb812a36
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-27 11:36:31 +0530

    #330 updated noResultsXpath, proxy and deployed with CLI 0.3.5

--- src/library/product/search/stores/a/amazon/DE/execute.js
+++ src/library/product/search/stores/a/amazon/DE/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.de',
     url: 'https://www.amazon.de/s?k={searchTerms}&__mk_de_DE=ÅMÅŽÕÑ&ref=nb_sb_noss_2',
     loadedSelector: 'div[class*="result-list"]>div[data-asin]>div>span[cel_widget_id="MAIN-SEARCH_RESULTS"]',
-    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Keine Ergebnisse")]',
+    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Keine Ergebnisse")] | //img[contains(@alt,"Dogs of Amazon")]  | //*[contains(text(),"Suchen Sie bestimmte Informationen")]',
     zipcode: '',
   },
 };
