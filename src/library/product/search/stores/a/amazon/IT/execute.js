c8472c1ecff2ed850955591f71e637f7f3c1edc8
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-27 11:41:19 +0530

    #325 updated noResultsXpath, proxy and deployed with CLI 0.3.5

--- src/library/product/search/stores/a/amazon/IT/execute.js
+++ src/library/product/search/stores/a/amazon/IT/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.it',
     url: 'https://www.amazon.it/s?k={searchTerms}&__mk_it_IT=ÅMÅŽÕÑ&ref=nb_sb_noss_2',
     loadedSelector: 'div[class*="result-list"]>div[data-asin]>div>span[cel_widget_id="MAIN-SEARCH_RESULTS"]',
-    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Nessun risultato")]',
+    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Nessun risultato")] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/a/amazon/IT/paginate.js
+++ src/library/product/search/stores/a/amazon/IT/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
       template: 'https://www.amazon.it/s?k={searchTerms}&__mk_it_IT=ÅMÅŽÕÑ&ref=nb_sb_noss_2&page={page}',
     },
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
-    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Nessun risultato")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])]',
+    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Nessun risultato")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")]',
     domain: 'amazon.it',
     zipcode: '',
   },
