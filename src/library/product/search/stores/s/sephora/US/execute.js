484c540a0830e4e43af15e8250637c7b8d3560b9
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-07-28 09:32:58 -0700

    created sephora search

---
+++ src/library/product/search/stores/s/sephora/US/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'US',
+    store: 'sephora',
+    domain: 'sephora.com',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/s/sephora/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'sephora',
+    transform: null,
+    domain: 'sephora.com',
+    zipcode: '',
+  },
+};
