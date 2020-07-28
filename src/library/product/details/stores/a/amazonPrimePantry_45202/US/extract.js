4af34b7863cf29ed3c6ccaab1ef005a8f15a6b8d
Satyam Rathod <satyam.rathod@synerzip.com>
2020-07-28 20:25:31 +0530

    #299 fixed bugs raised from UAT

--- src/library/product/details/stores/a/amazonPrimePantry_45202/US/extract.js
+++ src/library/product/details/stores/a/amazonPrimePantry_45202/US/extract.js
@@ -12,17 +12,18 @@ module.exports = {
   implementation: async (inputs,
     parameters,
     context,
-    dependencies, ) => {
+    dependencies,
+  ) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(async function () {
       await new Promise(resolve => setTimeout(resolve, 2814));
-      const element = document.getElementById("aplus");
+      const element = document.getElementById('aplus');
       if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
+        element.scrollIntoView({ behavior: 'smooth' });
         await new Promise(resolve => setTimeout(resolve, 2197));
       }
-    })
+    });
     return await context.extract(productDetails, { transform });
-  }
+  },
 };
