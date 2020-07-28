a6422f507dc00970f7cb600e4107a123dbc0bf6e
David Skoglund <david.skoglund@import.io>
2020-07-28 12:46:26 -0700

    #341. Updated goto to fix evaluate xpath.

--- src/library/product/search/stores/a/amazonMsReviews/CA/execute.js
+++ src/library/product/search/stores/a/amazonMsReviews/CA/execute.js
@@ -7,11 +7,7 @@ module.exports = {
     domain: 'amazon.ca',
     url: 'https://www.amazon.ca/product-reviews/{searchTerms}/?sortBy=recent',
     loadedSelector: 'div[data-hook=review]',
-<<<<<<< HEAD
     noResultsXPath: '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")]',
-=======
-    noResultsXPath: '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "404")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")] | //*[contains(text(),"Looking for something?")]',
->>>>>>> 629e1b1b62d680ba8a0685dfa9ee060dd339629a
     zipcode: '',
   },
 };
--- src/library/product/search/stores/a/amazonMsReviews/CA/paginate.js
+++ src/library/product/search/stores/a/amazonMsReviews/CA/paginate.js
@@ -12,11 +12,7 @@ async function implementation (
 ) {
   const { _date, page, keywords } = inputs;
   const loadedSelector = 'div[data-hook=review]';
-<<<<<<< HEAD
-  const noResultsXPath = '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")]';
-=======
   const noResultsXPath = '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //*[contains(text(),"Looking for something?")]';
->>>>>>> 629e1b1b62d680ba8a0685dfa9ee060dd339629a
   const openSearchDefinition = {
     template: 'https://www.amazon.ca/product-reviews/{searchTerms}?sortBy=recent&pageNumber={page}',
   };
