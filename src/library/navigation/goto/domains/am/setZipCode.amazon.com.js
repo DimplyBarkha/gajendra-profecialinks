ab9177a99cfdb8010998b2a90e82b14376462411
Satyam Rathod <satyam.rathod@synerzip.com>
2020-07-28 11:10:59 +0530

    #364 added all the required xpath and deployed to the platform

--- src/library/navigation/goto/domains/am/setZipCode.amazon.com.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.com.js
@@ -1,10 +1,34 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { zipcode } = inputs;
+  console.log('///////////////////////..............................................................................',zipcode);
+  await context.waitForSelector('#nav-packard-glow-loc-icon');
+  await context.click('#nav-packard-glow-loc-icon');
+  await context.waitForSelector('input#GLUXZipUpdateInput');
+  try {
+    await context.click('a#GLUXChangePostalCodeLink');
+  } catch (error) {
+    console.log('Element not visible');
+  }
+  await context.setInputValue('input#GLUXZipUpdateInput', zipcode);
+  await context.waitForSelector('#GLUXZipUpdate input');
+  await context.click('#GLUXZipUpdate input');
+  await context.waitForSelector('button[name="glowDoneButton"]');
+  await context.click('button[name="glowDoneButton"]');
+  await context.waitForNavigation();
+}
 
 module.exports = {
   implements: 'navigation/goto/setZipCode',
   parameterValues: {
     country: 'US',
     domain: 'amazon.com',
-    store: 'amazonApparel',
+    store: 'amazonPrimePantry',
     zipcode: '',
   },
+  implementation,
 };
