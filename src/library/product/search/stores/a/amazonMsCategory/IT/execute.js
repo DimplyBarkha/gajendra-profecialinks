496f7b9d1c8e3fea5a898717c67a06147a9857ee
David Skoglund <david.skoglund@import.io>
2020-07-29 11:51:00 -0700

    #331, updated goto with latest 503 logic.

--- src/library/product/search/stores/a/amazonMsCategory/IT/execute.js
+++ src/library/product/search/stores/a/amazonMsCategory/IT/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.it',
     url: 'https://www.amazon.it/gp/bestsellers/*/{searchTerms}',
     loadedSelector: null,
-    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")] | /html[not(//ol/li)]',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")] | /html[not(//ol/li)] | //a[contains(@href,"ref=cs_503_link")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/a/amazonMsCategory/IT/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/IT/paginate.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'IT',
     store: 'amazonMsCategory',
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")] | /html[not(//ol/li)]',
+    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")] | /html[not(//ol/li)] | //a[contains(@href,"ref=cs_503_link")]',
     openSearchDefinition: {
       template: 'https://www.amazon.it/gp/bestsellers/*/{searchTerms}?pg={page}#[!opt!]{"force200":true}[/!opt!]',
     },
