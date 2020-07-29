af68af1f1e385c4ef5cc6873f51bbbcf8c6ac67d
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-07-29 14:50:25 -0700

    sephora search updates

--- src/library/product/search/stores/s/sephora/US/extract.js
+++ src/library/product/search/stores/s/sephora/US/extract.js
@@ -1,5 +1,5 @@
-const { transform } = require('../../../../shared');
-// const { transform } = require('../format');
+// const { transform } = require('../../../../shared');
+const { transform } = require('../format');
 
 
 module.exports = {
@@ -25,18 +25,23 @@ async function implementation (
 
   await context.evaluate(async function () {
     let scrollTop = 0;
-    while (scrollTop !== 9000) {
+    while (scrollTop !== 10000) {
       // await stall(2500);
+      await new Promise(resolve => setTimeout(resolve, 5000));
+
       scrollTop += 1000;
       window.scroll(0, scrollTop);
+      await new Promise(resolve => setTimeout(resolve, 2000));
+
       console.log("SCROLLING")
-      if (scrollTop === 9000) {
+      if (scrollTop === 10000) {
         break;
       }
     }
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    // debugger
   });
 
-  await new Promise(resolve => setTimeout(resolve, 10000));
 
 
 
--- src/library/product/search/stores/s/sephora/format.js
+++ src/library/product/search/stores/s/sephora/format.js
@@ -1,15 +1,10 @@
+
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  // const cleanUp = (data, context) => {
-  //   data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-  //     el.text = clean(el.text);
-  //   }))));
-  //   return data;
-  // };
   const clean = text => text.toString()
     .replace(/\r\n|\r|\n/g, ' ')
     .replace(/&amp;nbsp;/g, ' ')
@@ -19,106 +14,54 @@ const transform = (data, context) => {
     .replace(/"\s{1,}/g, '"')
     .replace(/\s{1,}"/g, '"')
     .replace(/^ +| +$|( )+/g, ' ')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
     // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '');
-
-
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
   for (const { group } of data) {
-    for (let row of group) {
+    for (const row of group) {
       try {
-        if (row.weightGross) {
-          let text = row.weightGross[0].text;
-          let split = text.split(' ')
-          row.weightGross = [{ text: `${split[0]}` }];
-        }
         if(row.id){
           let text = row.id[0].text
-          console.log("FORMAT123")
-          console.log("TEXT HERE2" + " " + text)
+          
           let sNum = text.match(/(s[0-9]+)/g)
           if(sNum[0]){
-            row.id[0].text = sNum[0]
+            let num = sNum[0].match(/[0-9]+/g)
+            if(num[0]){
+              console.log("TEXT HERE" + " " + num[0])
+              // row.id[0].text = text
+
+              row.id[0].text = num[0]
+            }
           }
 
         }
-
-
-
-        // row = cleanUp(row);
+        if (row.id && row.id[0] && productCodes.indexOf(row.id[0].text) === -1) {
+          productCodes.push(row.id[0].text);
+          rankCounter += 1;
+          if (!row.sponsored) {
+            orgRankCounter += 1;
+            row.rankOrganic = [{ text: orgRankCounter }];
+          }
+          row.rank = [{ text: rankCounter }];
+        } else {
+          row.id = [{ text: '' }];
+        }
         Object.keys(row).forEach(header => row[header].forEach(el => {
           el.text = clean(el.text);
         }));
-      } catch (exception) {
-        console.log(exception);
+      } catch {
+        console.log("ERROR in format.js")
       }
     }
   }
-  // context.setState({ variantArray });
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  console.log(productCodes);
   return data;
 };
 module.exports = { transform };
-
-
-
-// /**
-//  *
-//  * @param {ImportIO.Group[]} data
-//  * @returns {ImportIO.Group[]}
-//  */
-// const transform = (data, context) => {
-//   const clean = text => text.toString()
-//     .replace(/\r\n|\r|\n/g, ' ')
-//     .replace(/&amp;nbsp;/g, ' ')
-//     .replace(/&amp;#160/g, ' ')
-//     .replace(/\u00A0/g, ' ')
-//     .replace(/\s{2,}/g, ' ')
-//     .replace(/"\s{1,}/g, '"')
-//     .replace(/\s{1,}"/g, '"')
-//     .replace(/^ +| +$|( )+/g, ' ')
-//     // eslint-disable-next-line no-control-regex
-//     .replace(/[\x00-\x1F]/g, '')
-//     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-//   const state = context.getState();
-//   let orgRankCounter = state.orgRankCounter || 0;
-//   let rankCounter = state.rankCounter || 0;
-//   const productCodes = state.productCodes || [];
-//   for (const { group } of data) {
-//     for (const row of group) {
-//       try {
-//         if(row.id){
-//           let text = row.id[0].text
-          
-//           // console.log("TEXT HERE" + " " + text)
-//           let sNum = text.match(/(s[0-9]+)/g)
-//           if(sNum[0]){
-//             row.id[0].text = sNum[0]
-//           }
-
-//         }
-//         if (row.id && row.id[0] && productCodes.indexOf(row.id[0].text) === -1) {
-//           productCodes.push(row.id[0].text);
-//           rankCounter += 1;
-//           if (!row.sponsored) {
-//             orgRankCounter += 1;
-//             row.rankOrganic = [{ text: orgRankCounter }];
-//           }
-//           row.rank = [{ text: rankCounter }];
-//         } else {
-//           row.id = [{ text: '' }];
-//         }
-//         Object.keys(row).forEach(header => row[header].forEach(el => {
-//           el.text = clean(el.text);
-//         }));
-//       } catch {
-//         console.log("ERROR in format.js")
-//       }
-//     }
-//   }
-//   context.setState({ rankCounter });
-//   context.setState({ orgRankCounter });
-//   context.setState({ productCodes });
-//   console.log(productCodes);
-//   return data;
-// };
-// module.exports = { transform };
