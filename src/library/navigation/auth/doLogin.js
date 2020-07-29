dea7f4713f182b9583259d50e7915f61535088d7
Sindhusha balla <sindhusha.b@osmosys.asia>
2020-07-29 20:30:48 +0530

    Added auth extractor template

---
+++ src/library/navigation/auth/doLogin.js
@@ -0,0 +1,74 @@
+
+/**
+ *
+ * @param { { username: string, password: string } } inputs
+ * @param { { domain: any, usernameSelector: string, passwordSelector: string, buttonSelector: string, loggedInSelector: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { someAction: ImportIO.Action, someFunction: () => void, someExtraction: string } } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { username, password } = inputs;
+  const { usernameSelector, passwordSelector, buttonSelector, loggedInSelector } = parameters;
+
+  await context.setInputValue(usernameSelector, username);
+  await context.setInputValue(passwordSelector, password);
+  await context.click(buttonSelector);
+
+  if (loggedInSelector) {
+    await context.waitForFunction(function (sel) {
+      return Boolean(document.querySelector(sel));
+    }, { timeout: 10000 }, loggedInSelector);
+  }
+}
+
+module.exports = {
+  parameters: [
+    {
+      name: 'domain',
+      description: 'domain of target website',
+      optional: false,
+    },
+    {
+      name: 'usernameSelector',
+      description: '',
+      optional: false,
+    },
+    {
+      name: 'passwordSelector',
+      description: '',
+      optional: false,
+    },
+    {
+      name: 'buttonSelector',
+      description: '',
+      optional: false,
+    },
+    {
+      name: 'loggedInSelector',
+      description: '',
+      optional: true,
+    },
+  ],
+  inputs: [
+    {
+      name: 'username',
+      description: '',
+      type: 'string',
+      optional: false,
+    },
+    {
+      name: 'password',
+      description: '',
+      type: 'string',
+      optional: false,
+    },
+  ],
+  dependencies: {},
+  path: './domains/${domain[0:2]}/${domain}/doLogin',
+  implementation,
+};
