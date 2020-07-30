204fa8be7a1b6f806ece4d6ea554380a25669022
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-30 10:34:33 +0530

    #341 updated 404 check

--- src/library/navigation/goto/domains/am/amazon.ca.js
+++ src/library/navigation/goto/domains/am/amazon.ca.js
@@ -2,9 +2,11 @@
 module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
-    country: 'CA',
     domain: 'amazon.ca',
-    store: 'amazonMsReviews',
+    timeout: null,
+    country: 'CA',
+    store: 'amazon',
+    zipcode: '',
   },
   implementation: async ({ url }, parameterValues, context, dependencies) => {
     const memory = {};
@@ -70,7 +72,7 @@ module.exports = {
       let status = 200;
       if (document.querySelector('a img[src*="503.png"], a[href*="ref=cs_503_link"]')) {
         status = 503;
-      } else if (document.querySelector('a[href*="dogsofamazon"'))  {
+      } else if (document.evaluate("//script[contains(text(),'PageNotFound')]", document.body, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength > 0) {
         status = 404;
       }
       return { status };
