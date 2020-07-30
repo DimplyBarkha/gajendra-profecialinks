341975a61a5b1c5d8852300b4ee165e659825a64
David Skoglund <david.skoglund@import.io>
2020-07-30 15:05:53 -0700

    #341. updated 404 detection.

--- src/library/navigation/goto/domains/am/amazon.ca.js
+++ src/library/navigation/goto/domains/am/amazon.ca.js
@@ -71,7 +71,7 @@ module.exports = {
       let status = 200;
       if (document.querySelector('a img[src*="503.png"], a[href*="ref=cs_503_link"]')) {
         status = 503;
-      } else if (document.evaluate("//script[contains(text(),'PageNotFound')]", document.body, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength > 0) {
+      } else if (document.evaluate("//script[contains(text(),'PageNotFound')]", document.body, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength > 0 || !!document.querySelector('a[href*="dogsofamazon"],img[alt*="unde"],img[alt*="Dogs"],img[alt*="hein"]') ) {
         status = 404;
       }
       return { status };
