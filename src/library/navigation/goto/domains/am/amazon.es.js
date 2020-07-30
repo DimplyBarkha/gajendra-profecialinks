306fd9b738096f53475212219b05ff5bd22b3266
vikas kyatannawar <vikas.k@osmosys.asia>
2020-07-30 11:02:23 +0530

    #354 updated 404 check

--- src/library/navigation/goto/domains/am/amazon.es.js
+++ src/library/navigation/goto/domains/am/amazon.es.js
@@ -72,7 +72,7 @@ module.exports = {
       let status = 200;
       if (document.querySelector('a img[src*="503.png"], a[href*="ref=cs_503_link"]')) {
         status = 503;
-      } else if (document.evaluate("//b[contains(text(), 'Buscas algo?')]", document.body, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength>0)  {
+      } else if (document.evaluate("//script[contains(text(),'PageNotFound')]", document.body, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength > 0) {
         status = 404;
       }
       return { status };
