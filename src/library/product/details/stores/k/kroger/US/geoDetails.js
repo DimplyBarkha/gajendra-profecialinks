293c4b7df089cc5b48c058b960f3e17cf9fa3431
alscotty <alawrencescott@gmail.com>
2020-07-31 11:06:31 -0700

    start geo

---
+++ src/library/product/details/stores/k/kroger/US/geoDetails.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/geo',
+  parameterValues: {
+    country: 'US',
+    domain: 'kroger.com',
+    store: 'kroger',
+    zipcode: '',
+    useDefault: null,
+  },
+};
---
+++ src/library/product/details/stores/k/kroger/US/prepare.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/geo/prepare',
+  parameterValues: {
+    country: 'US',
+    domain: 'kroger.com',
+    store: 'kroger',
+    zipcode: '',
+  },
+};
