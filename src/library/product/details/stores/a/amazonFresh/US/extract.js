685e84d18edeb21ff88d45e571ae466f8c173f69
sushma-mangalapally <sushma.mangalapally@import.io>
2020-07-31 18:04:08 -0700

    Add back variant loop

--- src/library/product/details/stores/a/amazonFresh/US/extract.js
+++ src/library/product/details/stores/a/amazonFresh/US/extract.js
@@ -224,35 +224,142 @@ async function implementation (
     await new Promise(r => setTimeout(r, 5000));
   }
 
-  async function addUrl () {
-    function addHiddenDiv (id, content) {
-      const newDiv = document.createElement('div');
-      newDiv.id = id;
-      newDiv.textContent = content;
-      newDiv.style.display = 'none';
-      document.body.appendChild(newDiv);
-    }
-    const url = window.location.href;
-    const splits = url ? url.split('/') : [];
-    const asinRaw = (splits.length > 0) ? splits[splits.length - 2] : '';
-    addHiddenDiv('added-url', url);
-    addHiddenDiv('added-asin', asinRaw);
-    addHiddenDiv('added-parentInput', parentInput);
+  async function addUrl (parentInput) {
+    await context.evaluate(async function (parentInput) {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+      const url = window.location.href;
+      const splits = url ? url.split('/') : [];
+      const asinRaw = (splits.length > 0) ? splits[splits.length - 2] : '';
+      addHiddenDiv('added-url', url);
+      addHiddenDiv('added-asin', asinRaw);
+      if (parentInput) {
+        addHiddenDiv('added-parentInput', parentInput);
+      }
+    });
   }
 
   // await setLocale();
   // @ts-ignore
-
+  async function getVariants () {
+    const variants = await context.evaluate(function () {
+      const variantList = [];
+      const variantCards = document.querySelectorAll('li[data-defaultasin]');
+      const variantDropdown = document.querySelectorAll('[id*="variation"] option');
+      const variantBooks = document.querySelectorAll('[id*="Swatches"]>ul>li a[id][href*="dp"]');
+      // const parentVariant = document.evaluate("//script[contains(@type,'a-state') and contains(text(), 'parentAsin')]", document, null, XPathResult.ANY_TYPE, null) ? document.evaluate("//script[contains(@type,'a-state') and contains(text(), 'parentAsin')]", document, null, XPathResult.ANY_TYPE, null).iterateNext() : null;
+      // if(parentVariant){
+      //   const regex = /parentAsin\"\:\"([A-Za-z0-9]{10,})/s;
+      //   let vasinRaw = parentVariant.innerText;
+      //   const vasin = vasinRaw.match(regex) ? vasinRaw.match(regex)[1] : '';
+      //   if(vasin !== ''){
+      //     variantList.push(vasin);
+      //     }
+      // }
+      if (variantBooks) {
+        for (let i = 0; i < variantBooks.length; i++) {
+          const element = variantBooks[i];
+          if (element == null) {
+            continue;
+          }
+          const vasinRaw = element.getAttribute('href');
+          if (vasinRaw !== '') {
+            const regex = /\/dp\/([A-Za-z0-9]{10,})/s;
+            const vasin = vasinRaw.match(regex) ? vasinRaw.match(regex)[1] : '';
+            if (vasin !== '') {
+              variantList.push(vasin);
+            }
+          }
+        }
+      }
+      if (variantDropdown) {
+        for (let i = 0; i < variantDropdown.length; i++) {
+          const element = variantDropdown[i];
+          if (element == null) {
+            continue;
+          }
+          const vasinRaw = element.getAttribute('value');
+          if (vasinRaw !== '') {
+            const regex = /[0-9]{1,},([0-9a-zA-Z]{10,})/s;
+            const vasin = vasinRaw.match(regex) ? vasinRaw.match(regex)[1] : '';
+            if (vasin !== '') {
+              variantList.push(vasin);
+            }
+          }
+        }
+      }
+      if (variantCards) {
+        for (let i = 0; i < variantCards.length; i++) {
+          const element = variantCards[i];
+          if (element == null) {
+            continue;
+          }
+          const vasin = element.getAttribute('data-defaultasin');
+          if (vasin !== '') {
+            variantList.push(vasin);
+          }
+        }
+      }
+      return variantList;
+    });
+    return variants;
+  };
   await new Promise(resolve => setTimeout(resolve, 5000));
   await setLocale();
   console.log('getting variants');
-  // const allVariants = [...new Set(await getVariants())];
+  const allVariants = [...new Set(await getVariants())];
   await new Promise(resolve => setTimeout(resolve, 5000));
   await loadAllResources();
-  await context.evaluate(addUrl);
+  // const productID = inputs.id;
+  addUrl(parentInput);
   console.log('autoscroll end');
+  await context.extract(productDetails, { transform, type: 'APPEND' });
+  console.log('#### of Variants:', allVariants.length);
+  console.log('#### Variants:', allVariants);
+  for (let i = 0; i < allVariants.length; i++) {
+    const id = allVariants[i];
+    const url = await dependencies.createUrl({ id });
+    await dependencies.goto({ url });
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    await setLocale();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    if (allVariants.length >= 5) {
+      await loadAllResources(10000);
+    } else {
+      await loadAllResources();
+    }
+    console.log('autoscroll end');
+    addUrl(parentInput);
+    // await new Promise(resolve => setTimeout(resolve, 2000));
+    await context.extract(productDetails, { transform, type: 'APPEND' });
+    const pageVariants = await getVariants();
+    console.log('#### of Variants:', allVariants.length);
+    console.log('#### Variants:', allVariants);
+    for (let j = 0; j < pageVariants.length; j++) {
+      const pageVariant = pageVariants[j];
+      if (allVariants.indexOf(pageVariant) === -1) {
+        allVariants.push(pageVariant);
+        console.log('new variant: ' + pageVariant);
+        console.log(allVariants);
+      }
+    }
+  }
+  // return await context.extract(productDetails, { transform });
+  // await new Promise(resolve => setTimeout(resolve, 5000));
+  // await setLocale();
+  // console.log('getting variants');
+  // // const allVariants = [...new Set(await getVariants())];
+  // await new Promise(resolve => setTimeout(resolve, 5000));
+  // await loadAllResources();
+  // await context.evaluate(addUrl);
+  // console.log('autoscroll end');
 
-  return await context.extract(productDetails, { transform });
+  // return await context.extract(productDetails, { transform });
 }
 
 module.exports = {
@@ -265,4 +372,3 @@ module.exports = {
   },
   implementation,
 };
-
