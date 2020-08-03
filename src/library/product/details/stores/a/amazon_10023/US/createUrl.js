f5af9a9fed479025bf7918fbcad081113ad3ab07
irfan shaikh <irfan.shaikh@synerzip.com>
2020-08-03 13:25:22 +0530

    #238. Updated Url

--- src/library/product/details/stores/a/amazon_10023/US/createUrl.js
+++ src/library/product/details/stores/a/amazon_10023/US/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'amazon.com',
     prefix: 'dp',
-    url: 'https://amazon.com/dp/{id}?th=1&language=en_US&showDetailProductDesc=1#navFooter',
+    url: 'https://amazon.com/dp/{id}?th=1&language=en_US&showDetailProductDesc=1',
     country: 'US',
     store: 'amazon_10023',
     zipcode: '10023',
