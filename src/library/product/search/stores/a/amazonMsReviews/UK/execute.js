d543cefa42a14ba2f3e0d8ef1a664bb21419b9e6
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-04 10:18:20 +0530

    #342. Updated no results Xpath

--- src/library/product/search/stores/a/amazonMsReviews/UK/execute.js
+++ src/library/product/search/stores/a/amazonMsReviews/UK/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.co.uk',
     url: 'https://www.amazon.co.uk/product-reviews/{searchTerms}/?sortBy=recent',
     loadedSelector: 'div[data-hook=review]',
-    noResultsXPath: '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "404")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")] | //*[contains(text(),"Looking for something?")]',
+    noResultsXPath: '/html[//script[contains(text(),\'pageType: "CustomerReviews"\')]][not(//div[@data-hook="review"])] | //div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "404")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")] | //*[contains(text(),"Looking for something?")]',
     zipcode: '',
   },
 };
