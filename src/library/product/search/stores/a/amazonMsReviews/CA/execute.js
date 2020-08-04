3afa9321fcbf567fdf2903abdc0e962c4b311774
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-04 10:20:06 +0530

    #341. Updated no results Xpath

--- src/library/product/search/stores/a/amazonMsReviews/CA/execute.js
+++ src/library/product/search/stores/a/amazonMsReviews/CA/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.ca',
     url: 'https://www.amazon.ca/product-reviews/{searchTerms}/?sortBy=recent',
     loadedSelector: 'div[data-hook=review]',
-    noResultsXPath: '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //*[contains(text(),"Looking for something?")] | //img[contains(@alt,  "Sorry")] ',
+    noResultsXPath: '/html[//script[contains(text(),\'pageType: "CustomerReviews"\')]][not(//div[@data-hook="review"])] | //div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //*[contains(text(),"Looking for something?")] | //img[contains(@alt,  "Sorry")] ',
     zipcode: '',
   },
 };
