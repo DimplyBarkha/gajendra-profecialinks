413c6d8524da04cdf07ff65ef3ffcad48c4aa0d2
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 10:37:53 +0530

    #295. updated no results xpath with more 503 and 404

--- src/library/product/search/stores/a/amazon/GLOBAL/paginate.js
+++ src/library/product/search/stores/a/amazon/GLOBAL/paginate.js
@@ -8,6 +8,6 @@ module.exports = {
       template: 'https://www.amazon.com/s?k={searchTerms}&ref=nb_sb_noss_2&page={page}',
     },
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
-    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])]',
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])]',
   },
 };
