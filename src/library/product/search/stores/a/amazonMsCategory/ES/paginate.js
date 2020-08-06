794993b45144165f0685b96a64ad3168c3480019
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 18:51:17 +0530

    #336. updated empty response as 200 and no results Xpath

--- src/library/product/search/stores/a/amazonMsCategory/ES/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/ES/paginate.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'ES',
     store: 'amazonMsCategory',
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")] | /html[not(//ol/li)]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "zeitgeist"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")] | /html[not(//ol/li)]',
     openSearchDefinition: {
       template: 'https://www.amazon.es/gp/bestsellers/*/{searchTerms}?&pg={page}#[!opt!]{"force200":true}[/!opt!]',
     },
