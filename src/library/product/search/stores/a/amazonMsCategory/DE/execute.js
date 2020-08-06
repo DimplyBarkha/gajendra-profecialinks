4783c6e18d8da57d4badd2a857aa1cb0c00a16b3
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 18:48:49 +0530

    #333. updated empty response as 200 and no results Xpath

--- src/library/product/search/stores/a/amazonMsCategory/DE/execute.js
+++ src/library/product/search/stores/a/amazonMsCategory/DE/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.de',
     url: 'https://www.amazon.de/gp/bestsellers/*/{searchTerms}?_encoding=UTF8',
     loadedSelector: null,
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol/li)] | //*[contains(text(),"Suchen Sie bestimmte Informationen")]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "zeitgeist"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol/li)] | //*[contains(text(),"Suchen Sie bestimmte Informationen")]',
     zipcode: '',
   },
 };
