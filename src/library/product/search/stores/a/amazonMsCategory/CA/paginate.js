f8b6a234fbac83ddefe35e3337aa48b72a101563
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 18:43:57 +0530

    #334. updated empty response as 200 and no results Xpath

--- src/library/product/search/stores/a/amazonMsCategory/CA/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/CA/paginate.js
@@ -1,5 +1,3 @@
-const { util } = require('chai');
-
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
@@ -9,7 +7,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "zeitgeist"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
     openSearchDefinition: {
       template: 'https://www.amazon.ca/gp/bestsellers/*/{searchTerms}?_encoding=UTF8&pg={page}#[!opt!]{"force200":true}[/!opt!]',
     },
