4bac69e3f6724931ca1838329de0fb44f56d8dab
sushma-mangalapally <sushma.mangalapally@import.io>
2020-08-06 14:31:14 -0700

    Remove opt tags

--- src/library/product/details/stores/a/amazonFresh/US/createUrl.js
+++ src/library/product/details/stores/a/amazonFresh/US/createUrl.js
@@ -5,7 +5,6 @@ module.exports = {
     domain: 'amazon.com',
     country: 'US',
     store: 'amazonFresh',
-    url: 'https://amazon.com/dp/product/{id}/?fpw=alm&s=amazonfresh&fpw=fresh#[!opt!]{  "headers": {"cache-control": "max-age=0"}}[/!opt!]',
-    // url: 'https://amazon.com/dp/product/{id}/?fpw=alm&s=amazonfresh&fpw=fresh#[!opt!]{"load_all_resources":true, "block_ads":false}[/!opt!]',
+    url: 'https://amazon.com/dp/product/{id}/?fpw=alm&s=amazonfresh&fpw=fresh',
   },
 };
