475f4eacee36a01ad71926e17911ca709971764f
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 19:10:17 +0530

    #349. updated empty response as 200 and no results Xpath

--- src/library/product/details/stores/a/amazonMsPromos/US/execute.js
+++ src/library/product/details/stores/a/amazonMsPromos/US/execute.js
@@ -6,6 +6,6 @@ module.exports = {
     store: 'amazonMsPromos',
     domain: 'amazon.com',
     loadedSelector: null,
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "GoldBox"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")]',
   },
 };
