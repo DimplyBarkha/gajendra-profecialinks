3f1e580cf6af504caea4de0624c7e20662255561
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 10:42:01 +0530

    #327. updated no results xpath with more 503 and 404

--- src/library/product/search/stores/a/amazon/FR/paginate.js
+++ src/library/product/search/stores/a/amazon/FR/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
       template: 'https://www.amazon.fr/s?k={searchTerms}&__mk_fr_FR=ÅMÅŽÕÑ&ref=nb_sb_noss_2&page={page}',
     },
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
-    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Aucun résultat")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Vous recherchez une page")]',
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Aucun résultat")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Vous recherchez une page")]',
     domain: 'amazon.fr',
     zipcode: '',
   },
