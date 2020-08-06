28e5d2178d4541e7806421db02526e79f415051c
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 10:55:12 +0530

    #336. updated no results xpath with more 503 and 404

--- src/library/product/search/stores/a/amazonMsCategory/ES/execute.js
+++ src/library/product/search/stores/a/amazonMsCategory/ES/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.es',
     url: 'https://www.amazon.es/gp/bestsellers/*/{searchTerms}',
     loadedSelector: null,
-    noResultsXPath: '//img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")] | /html[not(//ol/li)]',
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")] | /html[not(//ol/li)]',
     zipcode: '',
   },
 };
