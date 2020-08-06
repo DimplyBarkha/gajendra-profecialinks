cf96894cb7b305a63ce278857a2f3bba110e0ea6
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 10:44:06 +0530

    #329. updated no results xpath with more 503 and 404

--- src/library/product/search/stores/a/amazon/ES/execute.js
+++ src/library/product/search/stores/a/amazon/ES/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.es',
     url: 'https://www.amazon.es/s?k={searchTerms}&__mk_es_ES=ÅMÅŽÕÑ&ref=nb_sb_noss_2',
     loadedSelector: 'div[class*="result-list"]>div[data-asin]>div>span[cel_widget_id="MAIN-SEARCH_RESULTS"]',
-    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No hay resultados")] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")]',
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No hay resultados")] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")]',
     zipcode: '',
   },
 };
