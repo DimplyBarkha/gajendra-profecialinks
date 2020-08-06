9a666a2d324ed727ccfe56c4d3f538a4058b7944
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 15:36:21 +0530

    #295. updated no results Xpath

--- src/library/product/search/stores/a/amazon/US/execute.js
+++ src/library/product/search/stores/a/amazon/US/execute.js
@@ -6,6 +6,6 @@ module.exports = {
     domain: 'amazon.com',
     url: 'https://www.amazon.com/s?k={searchTerms}&ref=nb_sb_noss_2',
     loadedSelector: 'div[class*="result-list"]>div[data-asin]>div>span[cel_widget_id="MAIN-SEARCH_RESULTS"]',
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
   },
 };
