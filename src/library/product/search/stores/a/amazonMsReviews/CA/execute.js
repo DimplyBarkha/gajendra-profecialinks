9fcde65b2e1f80f2a2dc406e613449228b7938c5
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 19:01:25 +0530

    #341. updated empty response as 200 and no results Xpath

--- src/library/product/search/stores/a/amazonMsReviews/CA/execute.js
+++ src/library/product/search/stores/a/amazonMsReviews/CA/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.ca',
     url: 'https://www.amazon.ca/product-reviews/{searchTerms}/?sortBy=recent',
     loadedSelector: 'div[data-hook=review]',
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | /html[//script[contains(text(),\'pageType: "CustomerReviews"\')]][not(//div[@data-hook="review"])] | //div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //*[contains(text(),"Looking for something?")] | //img[contains(@alt,  "Sorry")] ',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "CustomerReviews"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | /html[//script[contains(text(),\'pageType: "CustomerReviews"\')]][not(//div[@data-hook="review"])] | //div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //*[contains(text(),"Looking for something?")] | //img[contains(@alt,  "Sorry")] ',
     zipcode: '',
   },
 };
