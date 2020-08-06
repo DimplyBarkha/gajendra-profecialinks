246961acaad9eeff4cfb49773e1389b058cd0d73
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 11:45:35 +0530

    #349. updated no results xpath with more 503 and 404

--- src/library/product/details/stores/a/amazonMsPromos/US/execute.js
+++ src/library/product/details/stores/a/amazonMsPromos/US/execute.js
@@ -6,6 +6,6 @@ module.exports = {
     store: 'amazonMsPromos',
     domain: 'amazon.com',
     loadedSelector: null,
-    noResultsXPath: null,
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")]',
   },
 };
--- src/library/product/details/stores/a/amazonMsPromosDealId/US/execute.js
+++ src/library/product/details/stores/a/amazonMsPromosDealId/US/execute.js
@@ -5,5 +5,6 @@ module.exports = {
     country: 'US',
     store: 'amazonMsPromosDealId',
     domain: 'amazon.com',
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")]',
   },
 };
