7ed49e54cca5d20104b0d1ae462607e956470bc1
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 11:54:20 +0530

    #350. updated no results xpath with more 503 and 404

--- src/library/product/details/stores/a/amazonMsPromosDealId/UK/execute.js
+++ src/library/product/details/stores/a/amazonMsPromosDealId/UK/execute.js
@@ -5,8 +5,7 @@ module.exports = {
     country: 'UK',
     store: 'amazonMsPromosDealId',
     domain: 'amazon.co.uk',
-    loadedSelector: null,
-    noResultsXPath: null,
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")]',
     zipcode: '',
   },
 };
