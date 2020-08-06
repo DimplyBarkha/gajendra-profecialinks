93b4f947077f74ac01a692787074cdda039fb945
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 10:13:44 +0530

    #340. updated no results xpath with more 503 and 404

--- src/library/product/search/stores/a/amazonMsReviews/DE/execute.js
+++ src/library/product/search/stores/a/amazonMsReviews/DE/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.de',
     url: 'https://www.amazon.de/product-reviews/{searchTerms}/?sortBy=recent',
     loadedSelector: 'div[data-hook=review]',
-    noResultsXPath: '/html[//script[contains(text(),\'pageType: "CustomerReviews"\')]][not(//div[@data-hook="review"])] | //div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "404")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")] | //*[contains(text(),"Suchen Sie bestimmte Informationen")]',
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | /html[//script[contains(text(),\'pageType: "CustomerReviews"\')]][not(//div[@data-hook="review"])] | //div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "404")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")] | //*[contains(text(),"Suchen Sie bestimmte Informationen")]',
     zipcode: '',
   },
 };
