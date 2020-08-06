9cd22a094595245907221a84692855198edbfc59
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-08-06 09:04:20 -0700

    sephora core updates

--- src/library/product/details/stores/s/sephora/US/extract.js
+++ src/library/product/details/stores/s/sephora/US/extract.js
@@ -19,7 +19,6 @@ module.exports = {
       }
       addHiddenDiv(`ii_parentInput`, parentInput);
 
-      let prodText = {};
       let flag = true;
       let i = 0;
       while(flag){
@@ -33,64 +32,9 @@ module.exports = {
         }
         i++
       }
-
-
-      // let directions = '//span[contains(., "How to Use")]'
-      // const element = document.querySelectorAll("script[type='application/ld+json']");
-      // let variantObj;
-      // let variantSkuArray = [];
-      // if(element.length > 0) {
-      //   for(let i = 0; i < element.length; i++){
-      //     let variantText = element[i].innerText;
-      //     if(variantText.includes("sku")){
-      //       let varObj = JSON.parse(variantText);
-      //       if(varObj){
-      //         variantObj = varObj;
-      //       }
-      //     }
-      //   }
-      // }
-      // if(variantObj){
-      //   if(variantObj.offers){
-      //     for(let j = 0; j < variantObj.offers.length; j++){
-      //       if(variantObj.offers[j].sku){
-      //         // addHiddenDiv(`ii_variants`, variantObj.offers[j].sku);
-
-      //         variantSkuArray.push(variantObj.offers[j].sku)
-      //       }
-      //     }
-      //   }
-      // }
-      // if(variantSkuArray.length){
-      //   return variantSkuArray
-      // } else {
-      //   return null
-      // }
     }, parentInput);
 
-    // if(variantArray){
-    //   for(let i = 0; i < variantArray.length; i++){
-    //     await context.goto(`https://www.sephora.com/search?keyword=${variantArray[i]}`, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
-    //     await new Promise(resolve => setTimeout(resolve, 5000));
-    //     const addSkuArray = await context.evaluate(function (variants) {
-    //       let variantStr = variants.join(" | ")
-    //         function addHiddenDiv (id, content) {
-    //           const newDiv = document.createElement('div');
-    //           newDiv.id = id;
-    //           newDiv.textContent = content;
-    //           newDiv.style.display = 'none';
-    //           document.body.appendChild(newDiv);
-    //         }
-    //         addHiddenDiv(`ii_variants`, variantStr);
-            
-            
-    //       }, variantArray);
-    //       await context.extract(productDetails);
-    //     }
-    //   } else {
-    //   }
-      
-      // addHiddenDiv(`ii_url`, window.location.href);
+
       return await context.extract(productDetails);
     },
 };
