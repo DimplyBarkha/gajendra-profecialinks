ea5a417ebe2fbc323d226c9ac22be427735fd495
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 15:43:49 +0530

    #329. updated no results Xpath

--- src/library/product/search/stores/a/amazon/ES/paginate.js
+++ src/library/product/search/stores/a/amazon/ES/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
       template: 'https://www.amazon.es/s?k={searchTerms}&__mk_es_ES=ÅMÅŽÕÑ&ref=nb_sb_noss_2&page={page}',
     },
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No hay resultados")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No hay resultados")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")]',
     domain: 'amazon.es',
     zipcode: '',
   },
