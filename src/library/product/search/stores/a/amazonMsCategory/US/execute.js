5973c18e7260ec112b7851260e7c46b593df578c
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-06 18:46:00 +0530

    #311. updated empty response as 200 and no results Xpath

--- src/library/product/search/stores/a/amazonMsCategory/US/execute.js
+++ src/library/product/search/stores/a/amazonMsCategory/US/execute.js
@@ -6,6 +6,6 @@ module.exports = {
     store: 'amazonMsCategory',
     domain: 'amazon.com',
     url: 'https://www.amazon.com/gp/bestsellers/{searchTerms}',
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "zeitgeist"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
   },
 };
--- src/library/product/search/stores/a/amazonMsCategory/US/paginate.js
+++ src/library/product/search/stores/a/amazonMsCategory/US/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ol>li span[class*="item"]>a:nth-child(1)',
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "zeitgeist"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
     openSearchDefinition: {
       template: 'https://www.amazon.com/gp/bestsellers/{searchTerms}?pg={page}',
     },
