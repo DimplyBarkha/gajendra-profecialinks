90e7e2dc6c746e18cc16f2ab19da82c642fae30e
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-08-10 16:51:56 -0700

    sephora core updates

--- src/library/product/details/stores/s/sephora/US/extract.js
+++ src/library/product/details/stores/s/sephora/US/extract.js
@@ -10,7 +10,65 @@ module.exports = {
   },
   implementation: async ({ parentInput }, { country, domain, transform: transformParam }, context, { productDetails }) => {
 
-    const variantArray = await context.evaluate(function (parentInput) {
+    const itemUrl = await context.evaluate(function() {
+      let itemCheck = '//div[@data-comp="ProductGrid "]//a'
+      var checkElement = document.evaluate( itemCheck, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      if( checkElement.snapshotLength > 0 ) {
+        let url = checkElement.snapshotItem(0).href
+        let splits = url.split("&")
+        return splits[0]
+      } else {
+        return null
+      }
+    })
+    if(itemUrl){
+      await context.goto(itemUrl, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
+    }
+
+    // const html = await context.evaluate(async function getEnhancedContent() {
+
+    //   let videoEle = document.querySelector('#linkJSON');
+    //   let videoIdForUrl = [];
+    //   if(videoEle){
+    //     let videoObj = JSON.parse(videoEle.innerText);
+    //     let videoIds = videoObj[4].props.currentProduct.productVideos
+    //     if(videoIds){
+    //       videoIds.forEach(obj => {
+    //         videoIdForUrl.push(obj.videoUrl)
+    //       })
+    //     }
+    //   }
+
+
+    //   async function fetchRetry(url, n) {
+    //     function handleErrors(response) {
+    //       if (response.status === 200){
+    //         return response;
+    //       } else {
+    //         debugger
+    //         console.log("FETCH FAILED")
+    //         if (n === 1) return "Nothing Found";
+    //         return fetchRetry(url, n - 1);
+    //       }
+    //     }
+    //     let fetched = fetch(url, {
+        
+    //       "referrer": "https://www.sephora.com/product/top-secrets-instant-matte-pore-refiner-P443340?skuId=2225589&keyword=2225589",
+    //       "body": null,
+    //       "method": "GET",
+    //     }).then(handleErrors).then(response => response.text()).catch(error => {
+    //       debugger
+    //         console.log("FETCH FAILED")
+    //         if (n === 1) return "Nothing Found";
+    //         return fetchRetry(url, n - 1);
+
+    //     });
+    //     return fetched;
+    //   }
+    //   fetchRetry(`https://edge.api.brightcove.com/playback/v1/accounts/6072792324001/videos/${videoIdForUrl[0]}`, 10)
+    // })
+
+    const variantArray = await context.evaluate(function (parentInput, html) {
       function addHiddenDiv (id, content) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
@@ -18,6 +76,7 @@ module.exports = {
         newDiv.style.display = 'none';
         document.body.appendChild(newDiv);
       }
+
       addHiddenDiv(`ii_url`, window.location.href);
       addHiddenDiv(`ii_parentInput`, parentInput);
 
@@ -31,6 +90,34 @@ module.exports = {
           break;
         }
       }
+      const element = document.querySelectorAll("script[type='application/ld+json']");
+      let variantObj;
+      let variantSkuArray = [];
+      if(element.length > 0) {
+        for(let i = 0; i < element.length; i++){
+          let variantText = element[i].innerText;
+          if(variantText.includes("sku")){
+            let varObj = JSON.parse(variantText);
+            if(varObj){
+              variantObj = varObj;
+            }
+          }
+        }
+      }
+      if(variantObj){
+        if(variantObj.offers){
+          for(let j = 0; j < variantObj.offers.length; j++){
+            if(variantObj.offers[j].sku){
+              variantSkuArray.push(variantObj.offers[j].sku)
+            }
+          }
+        }
+      }
+      if(variantSkuArray.length){
+        let variantsStr = variantSkuArray.join(" | ")
+        addHiddenDiv(`ii_variants`, variantsStr);
+      }
+
 
       let flag = true;
       let i = 0;
@@ -45,8 +132,23 @@ module.exports = {
         }
         i++
       }
+
+      let variantInfo = '//span[contains(@data-at,"item_variation_type")]'
+      var vInfo = document.evaluate( variantInfo, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      if( vInfo.snapshotLength > 0 ) {
+        for(let i = 0; i < vInfo.snapshotLength; i++) {
+          let info = vInfo.snapshotItem(i).textContent;
+          let splits = info.split(":");
+          if(info.includes("COLOR")){
+            addHiddenDiv(`ii_color`, splits[1]);
+          }
+          addHiddenDiv(`ii_variantInfo`, splits[1]);
+        }
+      }
+
     }, parentInput);
-      await new Promise(resolve => setTimeout(resolve, 1000));
+
+      await new Promise(resolve => setTimeout(resolve, 5000));
 
 
       return await context.extract(productDetails, { transform: transformParam });
