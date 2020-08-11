2862fe1ebd9a0a88f6efe7afd5ba948550258696
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-08-11 15:05:57 -0700

    sephora search ca updates

---
+++ src/library/product/search/stores/s/sephora/CA/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'CA',
+    store: 'sephora',
+    domain: 'sephora.ca',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/s/sephora/CA/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'CA',
+    store: 'sephora',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'sephora.ca',
+    zipcode: '',
+  },
+};
