db1a855cd749474eef838c3196b13872ff317771
Manas Ranjan <manas.n@osmosys.asia>
2020-08-12 11:09:14 +0530

    ASDA GEO initial push.

---
+++ src/library/product/details/stores/a/asda/UK/prepare.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/geo/prepare',
+  parameterValues: {
+    country: 'UK',
+    domain: 'groceries.asda.com',
+    store: 'asda',
+    zipcode: '',
+  },
+};
