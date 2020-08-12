78bbcbacc48e4bc09effbf492bd5a6507dcc95ef
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-08-12 11:24:58 -0700

    sephora core us updates

--- src/library/product/details/stores/s/sephora/US/extract.js
+++ src/library/product/details/stores/s/sephora/US/extract.js
@@ -11,6 +11,15 @@ module.exports = {
   implementation: async ({ parentInput }, { country, domain, transform: transformParam }, context, { productDetails }) => {
 
     const itemUrl = await context.evaluate(function() {
+      let resultsCheck = '(//h1//text()[not(parent::b)])[1]'
+      var checkResults = document.evaluate( resultsCheck, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      if(checkResults.snapshotLength > 0){
+        let checkNone = checkResults.snapshotItem(0).textContent;
+        if(checkNone === "0 Product results:"){
+          throw new Error("notFound");
+        }
+      }
+
       let itemCheck = '//div[@data-comp="ProductGrid "]//a'
       var checkElement = document.evaluate( itemCheck, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
       if( checkElement.snapshotLength > 0 ) {
@@ -25,70 +34,68 @@ module.exports = {
       await context.goto(itemUrl, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
     }
 
-    const html = await context.evaluate(async function getEnhancedContent() {
+    // const html = await context.evaluate(async function getEnhancedContent() {
 
-      let videoEle = document.querySelector('#linkJSON');
-      let videoIdForUrl = [];
-      if(videoEle){
-        let videoObj = JSON.parse(videoEle.innerText);
-        let videoIds = videoObj[4].props.currentProduct.productVideos
-        if(videoIds){
-          videoIds.forEach(obj => {
-            videoIdForUrl.push(obj.videoUrl)
-          })
-        }
-      }
-      // let videoClick = '(//div[@data-comp="Carousel "])[1]//img[contains(@src, "Video")]'
-      // var videoLinks = document.evaluate( videoClick, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-      // if( videoLinks.snapshotLength > 0 ) {
-      //   for(let i = 0; i < videoLinks.snapshotLength; i++) {
-      //     let info = videoLinks.snapshotItem(i).textContent;
-      //     if(info.includes("COLOR")){
+    //   let videoEle = document.querySelector('#linkJSON');
+    //   let videoIdForUrl = [];
+    //   if(videoEle){
+    //     let videoObj = JSON.parse(videoEle.innerText);
+    //     let videoIds = videoObj[4].props.currentProduct.productVideos
+    //     if(videoIds){
+    //       videoIds.forEach(obj => {
+    //         videoIdForUrl.push(obj.videoUrl)
+    //       })
+    //     }
+    //   }
+    //   // let videoClick = '(//div[@data-comp="Carousel "])[1]//img[contains(@src, "Video")]'
+    //   // var videoLinks = document.evaluate( videoClick, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    //   // if( videoLinks.snapshotLength > 0 ) {
+    //   //   for(let i = 0; i < videoLinks.snapshotLength; i++) {
+    //   //     let info = videoLinks.snapshotItem(i).textContent;
+    //   //     if(info.includes("COLOR")){
     
-      //   }
-      // }
+    //   //   }
+    //   // }
 
-      async function fetchRetry(url, n) {
-        function handleErrors(response) {
-          if (response.status === 200){
-            return response;
-          } else {
-            debugger
-            console.log("FETCH FAILED")
-            if (n === 1) return "Nothing Found";
-            return fetchRetry(url, n - 1);
-          }
-        }
-        let fetched = fetch(url, {
-          "headers": {
-            "accept": "*/*",
-            "accept-language": "en-US,en;q=0.9",
-            "accept-encoding": "gzip, deflate, br",
-            "cache-control": "no-cache",
-            "origin": "https://www.sephora.com",
-            "authority": "manifest.prod.boltdns.net",
-            "pragma": "no-cache",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "cross-site"
-          },
-          "referrer": "https://www.sephora.com/ca/en/product/highliner-gel-crayon-P379434?icid2=products%20grid:p379434:product&skuId=1880079",
-          "referrerPolicy": "no-referrer-when-downgrade",
-          "body": null,
-          "method": "GET",
-          "mode": "cors",
-          "credentials": "same-origin"
-        }).then(handleErrors).then(response => response.text()).catch(error => {
-          debugger
-            console.log("FETCH FAILED")
-            if (n === 1) return "Nothing Found";
-            return fetchRetry(url, n - 1);
+    //   async function fetchRetry(url, n) {
+    //     function handleErrors(response) {
+    //       if (response.status === 200){
+    //         return response;
+    //       } else {
+    //         console.log("FETCH FAILED")
+    //         if (n === 1) return "Nothing Found";
+    //         return fetchRetry(url, n - 1);
+    //       }
+    //     }
+    //     let fetched = fetch(url, {
+    //       "headers": {
+    //         "accept": "*/*",
+    //         "accept-language": "en-US,en;q=0.9",
+    //         "accept-encoding": "gzip, deflate, br",
+    //         "cache-control": "no-cache",
+    //         "origin": "https://www.sephora.com",
+    //         "authority": "manifest.prod.boltdns.net",
+    //         "pragma": "no-cache",
+    //         "sec-fetch-dest": "empty",
+    //         "sec-fetch-mode": "cors",
+    //         "sec-fetch-site": "cross-site"
+    //       },
+    //       "referrer": "https://www.sephora.com/ca/en/product/highliner-gel-crayon-P379434?icid2=products%20grid:p379434:product&skuId=1880079",
+    //       "referrerPolicy": "no-referrer-when-downgrade",
+    //       "body": null,
+    //       "method": "GET",
+    //       "mode": "cors",
+    //       "credentials": "same-origin"
+    //     }).then(handleErrors).then(response => response.text()).catch(error => {
+    //         console.log("FETCH FAILED")
+    //         if (n === 1) return "Nothing Found";
+    //         return fetchRetry(url, n - 1);
 
-        });
-        return fetched;
-      }
-      return fetchRetry(`https://edge.api.brightcove.com/playback/v1/accounts/6072792324001/videos/${videoIdForUrl[0]}`, 10)
-    })
+    //     });
+    //     return fetched;
+    //   }
+    //   return fetchRetry(`https://edge.api.brightcove.com/playback/v1/accounts/6072792324001/videos/${videoIdForUrl[0]}`, 10)
+    // });
 
     const variantArray = await context.evaluate(function (parentInput, html) {
       function addHiddenDiv (id, content) {
@@ -158,17 +165,17 @@ module.exports = {
       let variantInfo = '//span[contains(@data-at,"item_variation_type")]'
       var vInfo = document.evaluate( variantInfo, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
       if( vInfo.snapshotLength > 0 ) {
-        for(let i = 0; i < vInfo.snapshotLength; i++) {
-          let info = vInfo.snapshotItem(i).textContent;
+        // for(let i = 0; i < vInfo.snapshotLength; i++) {
+          let info = vInfo.snapshotItem(0).textContent;
           let splits = info.split(":");
           if(info.includes("COLOR")){
             addHiddenDiv(`ii_color`, splits[1]);
           }
           addHiddenDiv(`ii_variantInfo`, splits[1]);
-        }
+        // }
       }
 
-    }, parentInput, html);
+    }, parentInput);
 
       await new Promise(resolve => setTimeout(resolve, 5000));
 
