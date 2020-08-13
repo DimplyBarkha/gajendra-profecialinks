7dff2c15ad8888e4c74fd6c5a969a0f125e366b4
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-08-13 16:07:56 -0700

    sephora core au updates

---
+++ src/library/product/details/stores/s/sephora/AU/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'sephora.com.au',
+    prefix: null,
+    url: null,
+    country: 'AU',
+    store: 'sephora',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/s/sephora/AU/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'AU',
+    store: 'sephora',
+    transform: null,
+    domain: 'sephora.com.au',
+    zipcode: '',
+  },
+};
