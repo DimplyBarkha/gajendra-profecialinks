f033a1f2f41552079967b4f63995aaecc03d18ef
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-08-13 15:33:07 -0700

    sephora search us updates

--- src/library/product/search/stores/s/sephora/US/paginate.js
+++ src/library/product/search/stores/s/sephora/US/paginate.js
@@ -7,7 +7,7 @@ module.exports = {
     nextLinkSelector: 'nav[aria-label="Pagination"] button[aria-label="Next"]:not([disabled])',
     // mutationSelector: null,
     // spinnerSelector: 'div[data-comp="Interstice Loader "]:not([style="display:none"])',
-    loadedSelector: 'div[data-comp="ProductGrid "] a',
+    loadedSelector: ['div[data-comp="ProductGrid "] a', 'div[data-at="product_tabs_section"]'],
     // noResultsXPath: '//h1[contains(., "0 Product results")]',
     // openSearchDefinition: null,
     domain: 'sephora.com',
