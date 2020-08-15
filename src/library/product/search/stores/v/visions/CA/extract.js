6212a72b5de59f85fcda1205f6afd829200424f9
nikita.kumari <nikita.kumari@synerzip.com>
2020-08-15 18:01:53 +0530

    Added search Url(#402)

--- src/library/product/search/stores/v/visions/CA/extract.js
+++ src/library/product/search/stores/v/visions/CA/extract.js
@@ -9,6 +9,18 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
   // await new Promise((resolve) => setTimeout(resolve, 10000));
+  async function addUrl () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const url = window.location.href;
+    addHiddenDiv('added-searchurl', url);
+  }
+  await context.evaluate(addUrl);
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
