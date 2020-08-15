f55358a5fb9ac09bccecad95dab757669ae87d4d
Bindu Datla <bindu.datla@import.io>
2020-08-14 21:05:00 -0400

    updated to check for videos

--- src/library/product/details/stores/s/sephora/US/variantsExtract.js
+++ src/library/product/details/stores/s/sephora/US/variantsExtract.js
@@ -8,7 +8,7 @@ module.exports = {
     domain: 'sephora.com',
     zipcode: '',
   },
-  implementation
+  implementation,
 };
 
 async function implementation (
@@ -30,59 +30,56 @@ async function implementation (
     }
     const element = document.querySelectorAll("script[type='application/ld+json']");
     let variantObj;
-    let variantSkuArray = [];
-    if(element.length > 0) {
-      for(let i = 0; i < element.length; i++){
-        let variantText = element[i].innerText;
-        if(variantText.includes("sku")){
-          let varObj = JSON.parse(variantText);
-          if(varObj){
+    const variantSkuArray = [];
+    if (element.length > 0) {
+      for (let i = 0; i < element.length; i++) {
+        const variantText = element[i].innerText;
+        if (variantText.includes('sku')) {
+          const varObj = JSON.parse(variantText);
+          if (varObj) {
             variantObj = varObj;
           }
         }
       }
     }
-    if(variantObj){
-      if(variantObj.offers){
-        for(let j = 0; j < variantObj.offers.length; j++){
-          if(variantObj.offers[j].sku){
+    if (variantObj) {
+      if (variantObj.offers) {
+        for (let j = 0; j < variantObj.offers.length; j++) {
+          if (variantObj.offers[j].sku) {
             // addHiddenDiv(`ii_variants`, variantObj.offers[j].sku);
 
-            variantSkuArray.push(variantObj.offers[j].sku)
+            variantSkuArray.push(variantObj.offers[j].sku);
           }
         }
       }
     }
-    if(variantSkuArray.length){
+    if (variantSkuArray.length) {
       // debugger
-      return variantSkuArray
+      return variantSkuArray;
     } else {
-      return null
+      return null;
     }
   });
 
-  if(variantArray){
-    for(let i = 0; i < variantArray.length; i++){
+  if (variantArray) {
+    for (let i = 0; i < variantArray.length; i++) {
       // await context.goto(`https://www.sephora.com/search?keyword=${variantArray[i]}`, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
       // await new Promise(resolve => setTimeout(resolve, 5000));
       const addSkuArray = await context.evaluate(function (variantArray, i) {
         // let variantStr = variants.join(" | ")
-          function addHiddenDiv (id, content) {
-            const newDiv = document.createElement('div');
-            newDiv.id = id;
-            newDiv.textContent = content;
-            newDiv.style.display = 'none';
-            document.body.appendChild(newDiv);
-          }
-          addHiddenDiv(`ii_variantUrl`, `https://www.sephora.com/search?keyword=${variantArray[i]}`);
+        function addHiddenDiv (id, content) {
+          const newDiv = document.createElement('div');
+          newDiv.id = id;
+          newDiv.textContent = content;
+          newDiv.style.display = 'none';
+          document.body.appendChild(newDiv);
+        }
+        addHiddenDiv('ii_variantUrl', `https://www.sephora.com/search?keyword=${variantArray[i]}`);
 
-          addHiddenDiv(`ii_variant`, `${variantArray[i]}`);
-
-      
+        addHiddenDiv('ii_variant', `${variantArray[i]}`);
       }, variantArray, i);
     }
-  } 
-
+  }
 
   return await context.extract(variants, { transform });
 }
