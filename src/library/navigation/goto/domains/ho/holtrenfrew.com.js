ee6d9a52daee537f21fdb4361fded556e2b4049d
Saurabh Ghewari <saurabhghewari@synerzip.com>
2020-08-15 18:54:28 +0530

    #401 Search extractor with pagination done.

--- src/library/navigation/goto/domains/ho/holtrenfrew.com.js
+++ src/library/navigation/goto/domains/ho/holtrenfrew.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'holtrenfrew.com',
-    timeout: 60000,
+    timeout: 120000,
     country: 'CA',
     zipcode: '',
     store: 'holtrenfrew',
