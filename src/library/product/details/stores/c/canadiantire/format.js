60938a1623ebe422434ca859b3d50a2d3a160ec3
Satyam Rathod <satyam.rathod@synerzip.com>
2020-08-17 15:09:20 +0530

    #385 data level issues fixed

--- src/library/product/details/stores/c/canadiantire/format.js
+++ src/library/product/details/stores/c/canadiantire/format.js
@@ -8,7 +8,7 @@ const transform = (data) => {
     for (const row of group) {
       if (row.specifications) {
         row.specifications = [{
-          text: row.specifications.reduce((item, currItem) => `${item} | ${currItem.text.trim()}`, '').replace(/(\n\s?)+/g, '').slice(3).trim(),
+          text: row.specifications.reduce((item, currItem) => `${item} || ${currItem.text.trim()}`, '').replace(/(\n\s?)+/g, '').slice(3).trim(),
         }];
       };
       if (row.listPrice && !row.price) {
@@ -16,9 +16,23 @@ const transform = (data) => {
           text: row.listPrice[0].text.trim(),
         }];
       };
-      if (row.description && row.description[1]) {
-        const text = row.description[1].text.replace(/(\s*\n\s*)+/g, ' || ');
-        row.description = [{ text: `${row.description[0].text} ${text}` }];
+      if (row.shippingDimensions) {
+        row.shippingDimensions = [{
+          text: row.shippingDimensions.reduce((item, currItem) => `${item} | ${currItem.text.trim()}`, '').replace(/(\s?\n\s?)+/g, ' ').slice(3).trim(),
+        }];
+      };
+      if (row.additionalDescBulletInfo) {
+        row.additionalDescBulletInfo = [{
+          text: row.additionalDescBulletInfo.reduce((item, currItem) => `${item} | ${currItem.text.trim()}`, '').slice(1).trim(),
+        }];
+      };
+      if (row.description) {
+        if (row.description.length > 1) {
+          const text = row.description[1].text.replace(/(\s*\n\s*)+/g, ' || ');
+          row.description = [{ text: `${row.description[0].text} ${text}` }];
+        } else if (/FEATURES/i.test(row.description[0].text)) {
+          row.description[0].text = row.description[0].text.replace(/(\s*\n\s*)+/g, ' || ');
+        }
       };
       if (row.availabilityText && row.availabilityText[0]) {
         const text = row.availabilityText[0].text;
