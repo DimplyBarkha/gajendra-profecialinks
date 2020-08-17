dbd34ed614ffd4216c8c631e3bf2268cb4032c88
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-08-17 15:21:53 -0400

    Fixes

---
+++ src/library/product/details/stores/e/euronics/IE/extract.js
@@ -0,0 +1,33 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  function stall(ms) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, ms)
+    })
+  }
+
+  await context.waitForXPath('//a[@class="img-link-block"]');
+  await context.click('.img-link-block');
+  await stall(5000);
+  return await context.extract(productDetails, { transform });
+}
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'IE',
+    store: 'euronics',
+    transform: null,
+    domain: 'euronics.ie',
+  },
+  implementation,
+};
