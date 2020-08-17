50a2d8b8195e90dc048e940029efa72fb0a110b1
Saurabh Ghewari <saurabhghewari@synerzip.com>
2020-08-17 14:06:11 +0530

    #398 Timeout for details updated.

--- src/library/navigation/goto/domains/ho/holtrenfrew.com.js
+++ src/library/navigation/goto/domains/ho/holtrenfrew.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'holtrenfrew.com',
-    timeout: 120000,
+    timeout: 30000,
     country: 'CA',
     store: 'holtrenfrew',
     zipcode: '',
