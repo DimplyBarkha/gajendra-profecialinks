78d58e2c7d84d50197578b759d5f86dfeaee448a
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-08-18 13:18:30 +0530

    timeout increased(#403)

--- src/library/navigation/goto/domains/be/bestbuy.ca.js
+++ src/library/navigation/goto/domains/be/bestbuy.ca.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'bestbuy.ca',
-    timeout: 30000,
+    timeout: 90000,
     country: 'CA',
     store: 'bestbuy',
     zipcode: '',
