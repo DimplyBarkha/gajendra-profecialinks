d9ae5889611c2296eb50ae7622f04921e16cb2d8
Manoj Dey <manoj.d@OSMLIN16>
2020-08-18 16:23:18 +0530

    updated code

--- src/library/product/search/stores/t/tesco/UK/extract.js
+++ src/library/product/search/stores/t/tesco/UK/extract.js
@@ -1,5 +1,34 @@
 const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function stall (ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
 
+    let scrollTop = 500;
+    while (true) {
+      window.scroll(0, scrollTop);
+      await stall(1000);
+      scrollTop += 500;
+      if (scrollTop === 10000) {
+        break;
+      }
+    }
+    
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -58,4 +87,5 @@ module.exports = {
   //     throw new Error('Error when calling API');
   //   }
   // },
+  implementation,
 };
