19147e9df13e983c41887304a13464120adc61ee
Abhisek <abhisek.d@osmosys.asia>
2020-08-18 23:14:19 +0530

    Adding the first set of files

---
+++ src/library/product/details/stores/e/elcorteingles_electronica/ES/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'elcorteingles_electronica',
+    domain: 'elcorteingles.es',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
