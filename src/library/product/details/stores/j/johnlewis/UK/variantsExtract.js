eecc8a48b43fb41299c643a1332db90a02f8d6b9
nehashende <neha.shende@synerzip.com>
2020-08-18 20:53:00 +0530

    #412. Added variant code.

---
+++ src/library/product/details/stores/j/johnlewis/UK/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'UK',
+    store: 'johnlewis',
+    transform: null,
+    domain: 'johnlewis.com',
+    zipcode: '',
+  },
+};
