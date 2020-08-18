8f2e49bf46787d492ba985dab577dd006e88827e
sourabhk <sourabh.karmarkar@synerzip.com>
2020-08-18 12:38:01 +0530

    #413 Added extractor robot

---
+++ src/library/product/details/stores/b/boulanger/FR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'FR',
+    store: 'boulanger',
+    transform: null,
+    domain: 'boulanger.com',
+    zipcode: '',
+  },
+};
