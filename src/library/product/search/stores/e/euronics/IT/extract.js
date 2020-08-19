9fd23a40be0654ef97e52dd05f13e5357e553f14
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-08-19 13:06:41 -0400

    IT Search Updates

--- src/library/product/search/stores/e/euronics/IT/extract.js
+++ src/library/product/search/stores/e/euronics/IT/extract.js
@@ -8,12 +8,32 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
+  function stall (ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, ms);
+    });
+  }
+
   const results = [];
-  const resultsArr = [];
+  let resultsArr = [];
 
   while (true) {
 
-     let resultsArr = await context.evaluate(async function(resultsArr) {
+    await context.waitForXPath('//div[@class="productCard j-productCard j-comparisonItem "]');
+    await stall(3000);
+    const hasNextButton = await context.evaluate(function() {
+      let nextBtn = null;
+      document.querySelectorAll('.pageNumber__item').forEach((el, ind) => {
+        if(el.classList.contains('active')) {
+          nextBtn = document.querySelectorAll('.pageNumber__item')[ind + 1];
+        }
+      });
+      return nextBtn;
+    });
+
+     resultsArr = await context.evaluate(async function(resultsArr) {
 
       function stall (ms) {
         return new Promise((resolve, reject) => {
@@ -30,12 +50,37 @@ async function implementation (
         el.appendChild(div);
       }
 
+      document.querySelectorAll('.j-comparisonItem').forEach(el => {
+        if (resultsArr.length >= 150) {
+           el.remove();
+         } else {
+           resultsArr.push(true);
+        }
+        addHiddenDiv(el, 'rank', resultsArr.length);
+      });
+
+      return resultsArr;
+
     }, resultsArr);
+
+    console.log('totalResults', resultsArr.length);
 
     let extract = await context.extract(productDetails, { transform });
     results.push(extract);
 
+    await stall(1000);
 
+    if (hasNextButton && resultsArr.length < 150) {
+      await context.evaluate(function() {
+        document.querySelectorAll('.pageNumber__item').forEach((el, ind) => {
+          if(el.classList.contains('active')) {
+            return document.querySelectorAll('.pageNumber__item')[ind + 1].querySelector('a').click();
+          }
+        });
+      });
+    } else {
+      break;
+    }
 
   }
 
