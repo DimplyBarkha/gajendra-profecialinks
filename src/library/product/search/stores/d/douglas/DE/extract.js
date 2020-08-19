97d6c39bcfe211d8df407d841403d454c0440f90
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-08-20 00:30:52 +0530

    fixed issues with name image and product codes

--- src/library/product/search/stores/d/douglas/DE/extract.js
+++ src/library/product/search/stores/d/douglas/DE/extract.js
@@ -1,5 +1,5 @@
 const { transform } = require('./shared');
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -13,6 +13,31 @@ async function implementation (
       rating.parentElement.parentElement.innerText = (width / 20).toString();
     });
   });
+
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall(ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
+
+
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
