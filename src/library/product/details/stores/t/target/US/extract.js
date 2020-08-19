6aecbab51e9ed7d971f239327de17404a973d0ce
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-08-19 12:11:10 -0400

    Fixes

--- src/library/product/details/stores/t/target/US/extract.js
+++ src/library/product/details/stores/t/target/US/extract.js
@@ -634,7 +634,41 @@ async function implementation (
       }
       addHiddenDiv(newDiv, 'videos', videos.filter(onlyUnique).join(' | '));
 
-      let inStore = false;
+      await fetch('https://redsky.target.com/redsky_aggregations/v1/web/pdp_fulfillment_v1?key=eb2551e4accc14f38cc42d32fbc2b2ea&tcin=' + variant.tcin + '&store_id=1465&zip=54166&state=WI&latitude=44.780&longitude=-88.540&pricing_store_id=1465&fulfillment_test_mode=grocery_opu_team_member_test')
+      .then(data => data.json())
+      .then(availabilityData => {
+        let inStore = false;
+        let deliver = false;
+        if (availabilityData &&
+        availabilityData.data &&
+        availabilityData.data.product &&
+        availabilityData.data.product.fulfillment) {
+          if (availabilityData.data.product.fulfillment.store_options &&
+              availabilityData.data.product.fulfillment.store_options.length) {
+                availabilityData.data.product.fulfillment.store_options.forEach(store => {
+                  if(store.in_store_only.availability_status === 'IN_STOCK' || store.in_store_only.availability_status.includes('LIMITED_STOCK')) {
+                    inStore = true;
+                  }
+                });
+          }
+
+          if (availabilityData.data.product.fulfillment.shipping_options &&
+              (availabilityData.data.product.fulfillment.shipping_options.availability_status === 'IN_STOCK' || availabilityData.data.product.fulfillment.shipping_options.availability_status.includes('LIMITED_STOCK'))) {
+            deliver = true;
+          }
+        }
+
+        if (deliver) {
+          addHiddenDiv(newDiv, 'availability', 'In Stock');
+        } else if (inStore) {
+          addHiddenDiv(newDiv, 'availability', 'In Store Only');
+        }
+        if (!deliver && !inStore) {
+          addHiddenDiv(newDiv, 'availability', 'Out of stock');
+        }
+      });
+
+      /*let inStore = false;
       let deliver = false;
 
       const inStoreOnlyMessage = document.querySelector('div[data-test="inStoreOnlyMessage"]') || document.querySelector('div[data-test="orderPickupMessage"]');
@@ -659,7 +693,7 @@ async function implementation (
       }
       if (!deliver && !inStore) {
         addHiddenDiv(newDiv, 'availability', 'Out of stock');
-      }
+      }*/
 
       // &pricing_store_id=1465&storeId=1465
       let priceSuccess = false;
