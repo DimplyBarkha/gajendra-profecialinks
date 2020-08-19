f616f9bd54fb2bf1cc35a774baf173041d9d9af7
nehashende <neha.shende@synerzip.com>
2020-08-19 14:24:42 +0530

    #422. Initial commit for source JohnLewis search.

---
+++ src/library/product/search/stores/j/johnlewis/UK/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'johnlewis',
+    domain: 'johnlewis.com',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/j/johnlewis/UK/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'johnlewis',
+    transform: null,
+    domain: 'johnlewis.com',
+    zipcode: '',
+  },
+};
