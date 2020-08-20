96f19f5ba155ce5144b3e4a914bbe1a88e85e442
sourabhk <sourabh.karmarkar@synerzip.com>
2020-08-20 20:25:51 +0530

    #428 Added xpaths and corrections

--- src/library/product/details/stores/j/joycemayne/AU/extract.js
+++ src/library/product/details/stores/j/joycemayne/AU/extract.js
@@ -1,11 +1,54 @@
-const { transform } = require("../format");
+const { transform } = require('../format');
+
+/**
+ *
+ * @param { { url?: string,  id?: string} } inputs
+ * @param { Record<string, any> } parameters
+ * @param { ImportIO.IContext } context
+ * @param { Record<string, any> } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+
+    // If images are present in description then add to manufacturerDescription else add to description
+    const manufacturerImageFlag = document.querySelector('div[class="box-description cms"] img');
+    const descriptionSelector = document.querySelector('div[class="box-description cms"]');
+    let description = descriptionSelector ? descriptionSelector.innerText : '';
+    description = description ? description.replace(/(\n\s*){1,}/g, ' || ') : '';
+    description = description ? description.replace(/\|\| Key Features/gm, 'Key Features') : '';
+    if (manufacturerImageFlag) {
+      addHiddenDiv('added-manufacturerDesc', description);
+    } else {
+      addHiddenDiv('added-description', description);
+    }
+  });
+  await new Promise(resolve => setTimeout(resolve, 10000));
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
-  implements: "product/details/extract",
+  implements: 'product/details/extract',
   parameterValues: {
-    country: "AU",
-    store: "joycemayne",
+    country: 'AU',
+    store: 'joycemayne',
     transform,
-    domain: "joycemayne.com.au",
-    zipcode: "",
+    domain: 'joycemayne.com.au',
+    zipcode: '',
   },
+  implementation,
 };
--- src/library/product/details/stores/j/joycemayne/format.js
+++ src/library/product/details/stores/j/joycemayne/format.js
@@ -21,15 +21,40 @@ const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
       if (row.description) {
-        row.description.forEach((item) => {
-          item.text = cleanUp(item.text);
+        row.description.forEach((descriptionItem) => {
+          descriptionItem.text = cleanUp(descriptionItem.text);
         });
       }
       if (row.termsAndConditions) {
-        row.termsAndConditions.forEach((item) => {
-          item.text = cleanUp(item.text);
+        row.termsAndConditions.forEach((termsAndConditionsItem) => {
+          termsAndConditionsItem.text = cleanUp(termsAndConditionsItem.text);
         });
       }
+      if (row.descriptionBullets) {
+        row.descriptionBullets.forEach((descriptionBulletsItem) => {
+          if (descriptionBulletsItem.text === 0 || descriptionBulletsItem.text === '0') {
+            descriptionBulletsItem.text = '';
+          }
+        });
+      }
+      if (row.specifications) {
+        row.specifications[0].text = row.specifications[0].text.replace(/(\n\s*){4,}/g, ' || ').replace(/(\n\s*){2,}/g, ' : ');
+      }
+      const shippingDimensionsArray = [];
+      if (row.shippingDimensions) {
+        row.shippingDimensions.forEach((shippingDimensionsItem) => {
+          shippingDimensionsItem.text = shippingDimensionsItem.text.replace(/(\n\s*){2,}/g, ' : ');
+          shippingDimensionsArray.push(shippingDimensionsItem.text);
+        });
+      }
+      row.shippingDimensions = [{ text: shippingDimensionsArray.join(' | ') }];
+      const additionalDescBulletInfoArray = [];
+      if (row.additionalDescBulletInfo) {
+        row.additionalDescBulletInfo.forEach((additionalDescBulletInfoItem) => {
+          additionalDescBulletInfoArray.push(additionalDescBulletInfoItem.text);
+        });
+      }
+      row.additionalDescBulletInfo = [{ text: additionalDescBulletInfoArray.join(' | ') }];
     }
   }
   return data;
