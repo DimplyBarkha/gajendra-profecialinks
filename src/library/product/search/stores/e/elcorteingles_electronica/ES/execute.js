fe206a7f6ebbfff534e6406ee2fcade1641147ef
Nilesh <nilesh.b@osmosys.asia>
2020-08-20 19:51:04 +0530

    Added no results xpath.

--- src/library/product/search/stores/e/elcorteingles_electronica/ES/execute.js
+++ src/library/product/search/stores/e/elcorteingles_electronica/ES/execute.js
@@ -7,6 +7,6 @@ module.exports = {
     domain: 'elcorteingles.es',
     url: 'https://www.elcorteingles.es/search/1/?s={searchTerms}',
     loadedSelector: 'ul.c12.products_list._four',
-    noResultsXPath: 'div.products_list-container._no_products.vp',
+    noResultsXPath: '//div[@class="products_list-container _no_products vp"] | //a[@class="product_detail-brand"]',
   },
 };
