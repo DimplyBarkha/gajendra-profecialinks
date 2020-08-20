fca2580920e582dd276afc0cc43737f84f8c9074
Ankit Kumar <ankit.k@osmosys.asia>
2020-08-20 18:04:08 +0530

    Added timeout as and parameter in navigation/goto

--- src/library/navigation/goto/domains/wa/waitrose.com.js
+++ src/library/navigation/goto/domains/wa/waitrose.com.js
@@ -5,9 +5,10 @@ module.exports = {
     domain: 'waitrose.com',
     country: 'UK',
     store: 'waitrose',
+    timeout: 100000,
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
-    await context.goto(url, { timeout: 100000, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url, { waitUntil: 'load', checkBlocked: true });
     await context.evaluate(async () => {
       if (document.querySelector('[data-test="accept-all"]')) {
         document.querySelector('[data-test="accept-all"]').click();
