d3b28cf44726dafcff51447a19dcfa63366274f6
Guillaume Vial <guillaumevial@Guiguis-mac.local>
2020-08-20 11:28:04 +0100

    init

---
+++ src/library/product/details/stores/d/dyson/UK/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'dyson.co.uk',
+    prefix: null,
+    url: null,
+    country: 'UK',
+    store: 'dyson',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/d/dyson/UK/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'dyson',
+    transform: null,
+    domain: 'dyson.co.uk',
+    zipcode: '',
+  },
+};
