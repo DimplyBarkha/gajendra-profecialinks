9f5fee9b0ad3d436a9274aea97ebc9d80c4729f9
Guillaume Vial <guillaumevial@Guiguis-mac.local>
2020-08-20 12:46:26 +0100

    added xpath

--- src/library/product/details/stores/d/dyson/UK/extract.js
+++ src/library/product/details/stores/d/dyson/UK/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'UK',
     store: 'dyson',
-    transform: null,
+    transform,
     domain: 'dyson.co.uk',
     zipcode: '',
   },
