ab0f6f62f55c0c69e9fae793a19192f66e4d6fde
Bindu Datla <bindu.datla@import.io>
2020-08-19 22:07:59 -0400

    updated to increase timeout and add blockAds

--- src/library/navigation/goto/domains/me/medimax.de.js
+++ src/library/navigation/goto/domains/me/medimax.de.js
@@ -3,9 +3,18 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'medimax.de',
-    timeout: null,
+    timeout: 20000,
     country: 'DE',
     store: 'medimax',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
