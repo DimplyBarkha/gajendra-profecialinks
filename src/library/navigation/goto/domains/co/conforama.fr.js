342c0fde55f001b0c2f8feac7d89f2272e8b0746
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-08-21 21:27:25 +0530

    captcha(#421)

--- src/library/navigation/goto/domains/co/conforama.fr.js
+++ src/library/navigation/goto/domains/co/conforama.fr.js
@@ -3,9 +3,17 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'conforama.fr',
-    timeout: 80000,
+    timeout: 8000000,
     country: 'FR',
     store: 'conforama',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+    await context.solveCaptcha({
+    type: 'RECAPTCHA',
+    inputElement: '.g-recaptcha'
+  }); 
+  
   },
 };
