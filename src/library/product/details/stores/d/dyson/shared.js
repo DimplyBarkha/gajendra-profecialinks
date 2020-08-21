b2648cfc41e43209594c4e539e783d2ee778ce02
Guillaume Vial <guillaumevial@Guiguis-mac.local>
2020-08-21 14:48:25 +0100

    Qa fix

--- src/library/product/details/stores/d/dyson/shared.js
+++ src/library/product/details/stores/d/dyson/shared.js
@@ -13,7 +13,7 @@ module.exports.implementation = async function implementation (
       catElement.id = key;
       catElement.textContent = value;
       catElement.style.display = 'none';
-      document.appendChild(catElement);
+      document.body.appendChild(catElement);
     }
 
     const getSel = (sel, prop) => {
@@ -22,7 +22,11 @@ module.exports.implementation = async function implementation (
       return el || '';
     };
 
-    const getXpath =
+    const getXpath = (xpath, prop) => {
+      const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+      if (prop && elem && elem.singleNodeValue) return elem.singleNodeValue[prop];
+      return elem ? elem.singleNodeValue : '';
+    };
 
     const description = document.querySelectorAll('.product-hero__text-wrapper ul>li');
     addElementToDocument('added_description', description.length);
@@ -47,18 +51,65 @@ module.exports.implementation = async function implementation (
         let found = false;
         let index = 0;
         let brand;
-        while (index < words.length || !found) {
-          phrase.push(words[index]);
-          brand = phrase.join(' ') + tm;
-          found = window.find(brand);
+        while (index < words.length && !found) {
+          const word = words[index];
+          phrase.push(word);
+          console.log(phrase)
+          if (phrase.join(' ').toLowerCase().trim() !== 'dyson') {
+            brand = phrase.join(' ') + tm;
+            found = window.find(brand);
+          }
+          index++;
         }
         if (found) setBrand(brand);
       }
     } if (!brandText) {
-      
+      const lastChance = getXpath("//div[@data-title][contains(normalize-space(@data-title),'™ ')]/@data-title");
+      // Remove leading 'The'
+      const cleaned = lastChance ? lastChance.split('The ') : [];
+      if (cleaned.length > 0) setBrand(cleaned[1]);
+      else setBrand(lastChance);
     }
-    // div[@data-title][contains(normalize-space(@data-title),'™ ')]/@data-title
-    addElementToDocument('added_brandText', brandText);
+
+    addElementToDocument('added_brandtext', brandText);
+
+    // add the sku
+    if (window.dataLayer && window.dataLayer.primaryProduct) {
+      const json = window.dataLayer.primaryProduct;
+      addElementToDocument('added_sku', json.globalProductSKU);
+      // get the colour as well
+      if (json.color) addElementToDocument('added_color', json.color);
+      else {
+        const prodID = json.globalProductID;
+        if (prodID) {
+          const colorMapping = {
+            Pu: 'Purple',
+            Rd: 'Red',
+            Ir: 'Iron',
+            Co: 'Copper',
+            Bu: 'Blue',
+            Gd: 'Gold',
+            Bk: 'Black',
+            Nk: 'Nickel',
+          };
+          const shortColors = prodID.split(' ').slice(-1);
+          if (shortColors.length > 0) {
+            const colors = shortColors[0].split('/')
+              .map(col => {
+                if (col.length === 3 && col[0] === 'S') {
+                  return colorMapping[col.slice(1)] || col.slice(1);
+                }
+                return colorMapping[col] || col;
+              });
+            addElementToDocument('added_color', colors.join('-'));
+          }
+        }
+      }
+    }
+
+    // count the additional info addeed bullets
+    const addiDescrp = document.querySelectorAll('.product-specification ul>li');
+    addElementToDocument('added_description', addiDescrp.length);
   });
   return await context.extract(productDetails, { transform: parameters.transform });
 };
