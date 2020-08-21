10f9e1f6582bf503c21d25d7487bc39a4b3b0342
sourabhk <sourabh.karmarkar@synerzip.com>
2020-08-21 10:25:04 +0530

    #429 Added searchURL bugfix

--- src/library/product/search/stores/j/joycemayne/AU/extract.js
+++ src/library/product/search/stores/j/joycemayne/AU/extract.js
@@ -1,5 +1,28 @@
 const { transform } = require('../format');
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    // Adding search url to DOM
+    const searchURL = window.location.href;
+    addHiddenDiv('added_search_url', searchURL);
+  });
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,4 +32,5 @@ module.exports = {
     domain: 'joycemayne.com.au',
     zipcode: '',
   },
+  implementation,
 };
