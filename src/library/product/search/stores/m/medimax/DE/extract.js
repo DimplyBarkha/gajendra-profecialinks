30a2fe0e419b758a1f4237b39b9abe636af7bff2
Satyam Rathod <satyam.rathod@synerzip.com>
2020-08-21 13:57:14 +0530

    #414 search url updated

--- src/library/product/search/stores/m/medimax/DE/extract.js
+++ src/library/product/search/stores/m/medimax/DE/extract.js
@@ -12,6 +12,23 @@ async function implementation (
   } catch (err) {
     console.log(err);
   }
+  await context.evaluate(async function (inputs) {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+
+    const searchUrlDom = document.getElementById('search-url');
+    const searchUrl = window.location.href.replace(/%20/g, ' ');
+    if (searchUrlDom && searchUrlDom.innerText) {
+      searchUrlDom.innerText = searchUrl;
+    } else {
+      addHiddenDiv('search-url', searchUrl);
+    }
+  });
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
