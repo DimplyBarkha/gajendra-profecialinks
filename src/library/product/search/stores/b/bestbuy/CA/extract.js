db8455827ab88db1f9aa308fbd8be4af4180988e
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-08-21 19:01:23 +0530

    fix QA1(#403)

--- src/library/product/search/stores/b/bestbuy/CA/extract.js
+++ src/library/product/search/stores/b/bestbuy/CA/extract.js
@@ -18,14 +18,45 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await context.waitForXPath('//div[@class="productPricingContainer_3gTS3"]//meta[@itemprop="price"]');
+  // await context.waitForXPath('//div[@class="productPricingContainer_3gTS3"]//meta[@itemprop="price"]');
+  // await context.evaluate(async () => {
+  //     const element = document.querySelector('footer.globalFooter_Kvg_F ');
+  //     if (element) {
+  //       element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+  //       await new Promise((resolve) => setTimeout(resolve, 10000));
+  //     }
+  //     await new Promise((resolve) => setTimeout(resolve, 10000));
+  // });
   await context.evaluate(async () => {
-      const element = document.querySelector('footer.globalFooter_Kvg_F ');
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-        await new Promise((resolve) => setTimeout(resolve, 10000));
+    // window.scrollTo(0,9999);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 6000));
+      // await context.waitForSelector('div[data-test="product-thumb"] a div img');
+    }catch(error){
+      console.log(error)
+    }
+    async function infiniteScroll () {
+      let prevScroll = document.documentElement.scrollTop;
+      while (true) {
+        window.scrollBy(0, document.documentElement.clientHeight);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const currentScroll = document.documentElement.scrollTop;
+        if (currentScroll === prevScroll) {
+          break;
+        }
+        prevScroll = currentScroll;
       }
-  });
+    }
+    await infiniteScroll();
+  })
+
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 6000));
+    // await context.waitForSelector('div[data-test="product-thumb"] a div img');
+  } catch (error) {
+    console.log('error: ', error);
+    
+  }
   return await context.extract(productDetails, { transform });
 }
 
