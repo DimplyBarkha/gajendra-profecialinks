1f5ab6c4e94b24da521fef1539dab5596c91af64
sourabhk <sourabh.karmarkar@synerzip.com>
2020-08-21 15:33:42 +0530

    #433 Added extractor robot and structure

---
+++ src/library/product/details/stores/c/cdiscount/FR/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'FR',
+    store: 'cdiscount',
+    domain: 'cdiscount.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/c/cdiscount/FR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'FR',
+    store: 'cdiscount',
+    transform: null,
+    domain: 'cdiscount.com',
+    zipcode: '',
+  },
+};
