55e4b48997ce676bd8cd07f335cc6e09220fa0f5
sushma-mangalapally <sushma.mangalapally@import.io>
2020-08-21 11:11:24 -0700

    Update xpath

--- src/library/product/search/stores/e/expert/DE/extract.js
+++ src/library/product/search/stores/e/expert/DE/extract.js
@@ -1,3 +1,5 @@
+const { transform } = require('../../../../shared');
+
 async function implementation (
   inputs,
   parameters,
@@ -6,6 +8,7 @@ async function implementation (
 ) {
   const { productDetails } = dependencies;
   // await context.waitForSelector('.widget-Popup--container-outer---view-popup', { timeout: 20000 }); 
+  // await new Promise((resolve, reject) => setTimeout(resolve, 6000));
   await context.evaluate(async function () {
     if (document.querySelector('.widget-Popup--container-outer---view-popup')) {
       document.querySelector('.widget-Popup--container-outer---view-popup').click();
@@ -13,13 +16,6 @@ async function implementation (
     if (document.querySelector('i.widget-Popup--close')) {
       document.querySelector('i.widget-Popup--close').click();
     }
-    console.log('hello!')
-    // function addHiddenDiv (i, productCards, productInfo) {
-    //   const newDiv = document.createElement('div');
-    //   newDiv.id = i;
-    //   newDiv.className = 'extra-info';
-    //   newDiv.style.display = 'none';
-    // }
 
     function addHiddenDiv (id, content) {
       const newDiv = document.createElement('div');
@@ -45,6 +41,7 @@ async function implementation (
     // }
     // localStorage.setItem('prodCount', `${lastProductPosition + arr.length}`);
   });
+  // await context.waitForSelector('div.referenced.list div[class^="widget-ArticleList--article"] div[class~="widget-Container--subContent"] div.widget-Root--subContent');
   return await context.extract(productDetails);
 }
 module.exports = {
@@ -52,7 +49,7 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'expert',
-    transform: null,
+    transform: transform,
     domain: 'expert.de',
     zipcode: '',
   },
