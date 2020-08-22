e1610a74a03177a47cf59a97b040afdcb77e30bb
sushma-mangalapally <sushma.mangalapally@import.io>
2020-08-21 17:00:07 -0700

    Add spain folder

---
+++ src/library/product/details/stores/m/mediamarkt/ES/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'mediamarkt',
+    domain: 'mediamarkt.es',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/m/mediamarkt/ES/extract.js
@@ -0,0 +1,21 @@
+const { transform } = require('../transform');
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'NL',
+    store: 'mediamarkt',
+    transform: transform,
+    domain: 'mediamarkt.nl',
+    zipcode: '',
+  },
+
+  // implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+  //   await context.evaluate(async function () {
+  //     if (document.querySelector('gdpr-cookie-layer--show') && document.querySelector('button.gdpr-cookie-layer__btn--submit')) {
+  //       document.querySelector('button.gdpr-cookie-layer__btn--submit').click();
+  //     }
+  //   });
+  //   await context.extract(productDetails);
+  // },
+};
