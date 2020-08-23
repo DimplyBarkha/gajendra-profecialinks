5d27c9c90921ac097348f38ba470dd893ade8087
Abhisek <abhisek.d@osmosys.asia>
2020-08-23 21:34:13 +0530

    Changes xpath

--- src/library/navigation/goto/domains/el/elcorteingles.es.js
+++ src/library/navigation/goto/domains/el/elcorteingles.es.js
@@ -7,7 +7,7 @@ module.exports = {
     country: 'ES',
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
-    url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+    url = `${url}`;
     await context.goto(url, {
       block_ads: false,
       load_all_resources: true,
