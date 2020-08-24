d952ad6ac895526748e64e19b260b0a0a79bc360
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-08-24 19:39:53 +0530

    added variant extractor

--- src/library/product/details/stores/d/douglas/DE/variantsExtract.js
+++ src/library/product/details/stores/d/douglas/DE/variantsExtract.js
@@ -1,4 +1,36 @@
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { createUrl, variants } = dependencies;
+  await context.evaluate(function () {
+    function addHiddenDiv(id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+      return newDiv;
+    }
+    let url = window.location.href;
+    const variantContainer = document.querySelector('div.rd__product-details__picker__list');
+    console.log('came to variant')
+    console.log(addHiddenDiv('ajay', "em ledu"))
+    if (variantContainer) {
+      console.log('variant container found')
+      const variants = variantContainer.querySelectorAll('div.rd__product-details__picker__list__item');
+      for (var i = 0; i < variants.length; i++) {
+        variants[i].click();
+        addHiddenDiv('variantUrl', window.location.href)
+        console.log(window.location.href);
+      }
+    }
 
+  });
+  return await context.extract(variants);
+}
 module.exports = {
   implements: 'product/details/variants/variantsExtract',
   parameterValues: {
@@ -8,4 +40,5 @@ module.exports = {
     domain: 'douglas.de',
     zipcode: '',
   },
+  implementation,
 };
