75f9b3587a849c1ead8cd82f68f544b989a3c5fd
alscotty <alawrencescott@gmail.com>
2020-08-24 15:41:23 -0700

    lint fixes

--- src/library/navigation/goto/domains/wa/setZipCode.walmart.com.js
+++ src/library/navigation/goto/domains/wa/setZipCode.walmart.com.js
@@ -19,17 +19,17 @@ async function implementation (
     return;
   }
 
-  const currentZip = await context.evaluate((selector) => document.querySelector(selector) ? document.querySelector(selector).innerText : '', zipSelector);     
-   
+  const currentZip = await context.evaluate((selector) => document.querySelector(selector) ? document.querySelector(selector).innerText : '', zipSelector);
+
   console.log(`Current Location : ${currentZip}`);
   if (currentZip && !currentZip.includes(zipcode)) {
     await context.click(zipSelector);
     await context.waitForSelector('div#vh-location-form', { timeout: 10000 });
     await context.setInputValue('div#vh-location-form input[data-tl-id="zipcode-form-input"]', zipcode);
     const updateLocationSelector = 'div#vh-location-form button[data-tl-id="zipcode-form-submit-button"]';
-    await context.waitForSelector(updateLocationSelector, { timeout: 10000 });   
+    await context.waitForSelector(updateLocationSelector, { timeout: 10000 });
     await context.clickAndWaitForNavigation(updateLocationSelector, {}, { timeout: 20000 });
-    await context.click('body');    
+    await context.click('body');
   }
 }
 
