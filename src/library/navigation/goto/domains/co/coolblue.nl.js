4a0a8b090caf42d0af2973c832d693a3c24cf169
Ankit Kumar <ankit.k@osmosys.asia>
2020-08-25 03:37:55 +0530

    initial commit, generated extractor with few columns

---
+++ src/library/navigation/goto/domains/co/coolblue.nl.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'coolblue.nl',
+    timeout: null,
+    zipcode: '',
+    country: 'NL',
+    store: 'coolblue',
+  },
+};
---
+++ src/library/product/details/stores/c/coolblue/NL/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'NL',
+    store: 'coolblue',
+    transform: null,
+    domain: 'coolblue.nl',
+    zipcode: '',
+  },
+};
