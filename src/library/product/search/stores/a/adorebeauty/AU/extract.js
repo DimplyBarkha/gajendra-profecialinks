19cc4fa904f8d3b6cb13e7b67c008e49abbba648
Saurabh Ghewari <saurabhghewari@synerzip.com>
2020-08-24 15:26:36 +0530

    #436 Updated regex for thumbnail. Added pagination for load more products.

--- src/library/product/search/stores/a/adorebeauty/AU/extract.js
+++ src/library/product/search/stores/a/adorebeauty/AU/extract.js
@@ -1,3 +1,28 @@
+/**
+ *
+ * @param { { } } inputs
+ * @param { Record<string, any> } parameters
+ * @param { ImportIO.IContext } context
+ * @param { Record<string, any> } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async () => {
+    let loadMore = document.querySelector('button[class="flex items-center mx-auto text-center uppercase text-white bg-black my-4 px-4 py-3 rounded-sm"]');
+    while(loadMore && loadMore.getAttribute('disabled') !== "disabled"){
+      await loadMore.click();
+      await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+      loadMore = document.querySelector('button[class="flex items-center mx-auto text-center uppercase text-white bg-black my-4 px-4 py-3 rounded-sm"]');
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
@@ -8,4 +33,5 @@ module.exports = {
     domain: 'adorebeauty.com.au',
     zipcode: '',
   },
+  implementation
 };
