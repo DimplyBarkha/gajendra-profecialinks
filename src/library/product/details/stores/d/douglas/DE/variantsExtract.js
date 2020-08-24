d013122d6b56d434de56d425bcad495cdfbcf99a
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-08-24 14:35:41 +0530

    added variant extractor for douglas.de

---
+++ src/library/product/details/stores/d/douglas/DE/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'DE',
+    store: 'douglas',
+    transform: null,
+    domain: 'douglas.de',
+    zipcode: '',
+  },
+};
