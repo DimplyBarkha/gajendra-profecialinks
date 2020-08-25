c5d3795a2fe4fd22c98262d4fb79793c32c85554
Saurabh Ghewari <saurabhghewari@synerzip.com>
2020-08-25 21:15:15 +0530

    #436 Added logic to get the ID from adore beauty. Also updated inputs file.

--- src/library/product/search/stores/a/adorebeauty/AU/extract.js
+++ src/library/product/search/stores/a/adorebeauty/AU/extract.js
@@ -16,31 +16,36 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  // await context.evaluate(async () => {
-  //   let loadMore = document.querySelector('button[class="flex items-center mx-auto text-center uppercase text-white bg-black my-4 px-4 py-3 rounded-sm"]');
-  //   while(loadMore && loadMore.getAttribute('disabled') !== "disabled"){
-  //     await loadMore.click();
-  //     await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-  //     loadMore = document.querySelector('button[class="flex items-center mx-auto text-center uppercase text-white bg-black my-4 px-4 py-3 rounded-sm"]');
-  //   }
-  // });
+  
   await context.evaluate(async () => {
-    let btnView = document.querySelectorAll('button[class="md:hidden group-hover:inline-flex w-1/2 text-15 font-montserrat bg-white hover:bg-black text-black hover:text-white border border-black py-3 px-4 mr-2"]');
-    let btnViewLength = btnView.length;
-    async function clickButtonAndGetData() {
-      btnViewLength = btnViewLength - 1;
-      await btnView[btnViewLength].click();
-      let productId = document.getElementsByTagName("META")[7].content;
-      console.log(productId);
-      let modalClose = document.querySelector('div[class="dialog__close"]');
-      await modalClose.click();
-      if(btnViewLength) {
-        clickButtonAndGetData();
-      }
+    let loadMore = document.querySelector('button[class="flex items-center mx-auto text-center uppercase text-white bg-black my-4 px-4 py-3 rounded-sm"]');
+    while(loadMore && loadMore.getAttribute('disabled') !== "disabled"){
+      await loadMore.click();
+      await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+      loadMore = document.querySelector('button[class="flex items-center mx-auto text-center uppercase text-white bg-black my-4 px-4 py-3 rounded-sm"]');
+    }
+  });
+
+  await context.evaluate(async () => {
+
+    function addHiddenDiv (id, content, parent) {
+      const newDiv = document.createElement('div');
+      newDiv.className = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      parent.appendChild(newDiv);
     }
 
-    if(btnViewLength) {
-      clickButtonAndGetData();
+    let recordSelector = document.querySelectorAll('li[class="ais-InfiniteHits-item m-2"]');
+    
+    for (const record of recordSelector) {
+      let btnView = record.querySelector('button.text-black');
+      btnView.click();
+      await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+      let productId = document.querySelector("meta[itemprop='productID']").content;
+      let modalClose = document.querySelector('div[class="dialog__close"]');
+      await modalClose.click();
+      addHiddenDiv("myProductId", productId, record);
     }
   });
 
