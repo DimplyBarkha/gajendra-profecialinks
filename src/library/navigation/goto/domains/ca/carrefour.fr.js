368b141243f6e0a2d608a85fb8ddd06584f460d4
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-08-25 20:31:05 +0530

    captcha 7 passed

--- src/library/navigation/goto/domains/ca/carrefour.fr.js
+++ src/library/navigation/goto/domains/ca/carrefour.fr.js
@@ -9,11 +9,21 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    await context.goto(url, { timeout: 90000, waitUntil: 'load', checkBlocked: false });
-    await context.solveCaptcha({
-    type: 'RECAPTCHA',
-    inputElement: '.g-recaptcha'
-  }); 
+    await context.goto(url, { timeout: 9000000, waitUntil: 'load', checkBlocked: false });
+    try {
+      await context.waitForSelector('.g-recaptcha');
+      await context.solveCaptcha({
+        type: 'RECAPTCHA',
+        // inputElement: '.g-recaptcha' rc-anchor-container
+        inputElement: '#rc-anchor-container'
+      }); 
+    } catch (error) {
+      
+    }
+    
   await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+  // await context.waitForFunction(function (sel, xp) {
+  //   return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+  // }, {"timeout":90000}, "div[id=\"product-detail-page\"]", "//div[@class=\"error\"]")
   },
   };
