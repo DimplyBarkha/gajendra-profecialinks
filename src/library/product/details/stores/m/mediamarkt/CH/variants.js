994e07270e463e66cc8ad2c71bfbe124904de114
sushma-mangalapally <sushma.mangalapally@import.io>
2020-08-25 15:15:24 -0700

    Add changes to xpath and variant files

---
+++ src/library/product/details/stores/m/mediamarkt/CH/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'CH',
+    store: 'mediamarkt',
+    domain: 'mediamarkt.ch',
+    zipcode: '',
+  },
+};
