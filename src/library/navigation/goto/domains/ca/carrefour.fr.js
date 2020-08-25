31c7877d5edcd465e00261372453bebcebdf2169
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-08-25 12:20:00 +0530

    cpatcha initial code (#456)

--- src/library/navigation/goto/domains/ca/carrefour.fr.js
+++ src/library/navigation/goto/domains/ca/carrefour.fr.js
@@ -8,4 +8,12 @@ module.exports = {
     store: 'carrefour',
     zipcode: '',
   },
-};
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    await context.goto(url, { timeout: 90000, waitUntil: 'load', checkBlocked: false });
+    await context.solveCaptcha({
+    type: 'RECAPTCHA',
+    inputElement: '.g-recaptcha'
+  }); 
+  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+  },
+  };
