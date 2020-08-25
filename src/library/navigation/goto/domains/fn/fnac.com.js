7a7347d7e6300e6b1b9028ad927d0fe630aaeedc
sourabh-karmarkar <sourabh.karmarkar@synerzip.com>
2020-08-25 14:18:53 +0530

    #449 created extractor and added xpaths

---
+++ src/library/navigation/goto/domains/fn/fnac.com.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'fnac.com',
+    timeout: 60000,
+    country: 'FR',
+    store: 'fnac',
+    zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    /**
+    * Adding goto in try catch as the webpage keeps on loading indefinitely
+    * even if we give huge time out and eventually throws timeout error
+    */
+    try {
+      await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    } catch (e) {
+      console.log('page load time out ----> ', e);
+    }
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
+};
