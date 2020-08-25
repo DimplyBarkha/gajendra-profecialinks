860c9fb0c52f343027569712d7719a6d1525ca03
Guillaume Vial <guillaumevial@Guiguis-mac.local>
2020-08-25 23:13:53 +0100

    deployed US and AU

--- src/library/product/details/stores/d/dyson/AU/extract.js
+++ src/library/product/details/stores/d/dyson/AU/extract.js
@@ -1,11 +1,14 @@
+const { transform } = require('../../../../shared');
+const { implementation } = require('../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AU',
     store: 'dyson',
-    transform: null,
+    transform,
     domain: 'dyson.com.au',
     zipcode: '',
   },
+  implementation,
 };
