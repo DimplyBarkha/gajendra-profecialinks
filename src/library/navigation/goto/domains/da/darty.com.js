ba3f6977f31c8a2e7960a0a9011431877011bf02
Satyam Rathod <satyam.rathod@synerzip.com>
2020-08-25 10:35:48 +0530

    #420 captcha code added

--- src/library/navigation/goto/domains/da/darty.com.js
+++ src/library/navigation/goto/domains/da/darty.com.js
@@ -3,9 +3,40 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'darty.com',
-    timeout: null,
+    timeout: 20000,
     country: 'FR',
     store: 'darty',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, {
+      antiCaptchaOptions: {
+        type: 'GEETEST',
+        libPath: '',
+      },
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+    });
+
+    try {
+      await context.waitForSelector('div.product_body');
+    } catch (error) {
+      try {
+        await context.waitForSelector('div.geetest_btn');
+      } catch (error) {
+        
+      }
+      await context.solveCaptcha({
+        type: 'GEETEST',
+        inputElement: 'div.geetest_btn',
+      });
+    }
+
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
