d9d11651dd7a112989437560115e39f008ad89c1
Ankit Kumar <ankit.k@osmosys.asia>
2020-08-26 01:13:45 +0530

    updated extract.yaml and added sript to handle alternate images

--- src/library/product/details/stores/c/coolblue/NL/extract.js
+++ src/library/product/details/stores/c/coolblue/NL/extract.js
@@ -8,4 +8,23 @@ module.exports = {
     domain: 'coolblue.nl',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      // click on the image to get the alternate image info
+      const mainImageselector = document.querySelector('ul.product-media-gallery__wrapper>li');
+      if (mainImageselector) {
+        console.log('main image selector found');
+        mainImageselector.click();
+        console.log('main image selector Clicked');
+      }
+      // Get categories info
+      const breacrumb = Array.from(document.querySelectorAll('ol.breadcrumbs li')).map(elm => {
+        const value = elm.textContent.trim();
+        return `${value}`;
+      }).filter(elm => elm);
+      document.body.setAttribute('category', breacrumb.join('>'));
+      console.log('category set');
+    });
+    await context.extract(productDetails);
+  },
 };
