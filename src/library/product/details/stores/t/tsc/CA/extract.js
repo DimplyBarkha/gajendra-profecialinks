ba1dbf4316cee01e917048c7d1b14ab36a8f9d5d
sourabhk <sourabh.karmarkar@synerzip.com>
2020-08-25 13:00:37 +0530

    #397 Added QA2 bugfixes

--- src/library/product/details/stores/t/tsc/CA/extract.js
+++ src/library/product/details/stores/t/tsc/CA/extract.js
@@ -26,6 +26,33 @@ async function implementation (
       newDiv.style.display = 'none';
       document.body.appendChild(newDiv);
     }
+
+    // Function to fetch color and add to DOM
+    function fetchColor () {
+      let colorSelector = document.evaluate('//label[@class="style-swatch-label style-selected"]/@title', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      let color = '';
+      if (!colorSelector) {
+        colorSelector = document.querySelector('div[id="infoTabContent"] div[id="tab0"]');
+        const colorSelectorText = colorSelector ? colorSelector.innerText : '';
+        if (colorSelectorText.includes('Colour')) {
+          color = colorSelectorText.replace(/\n/gm, '').replace(/.*Colour: ?(\w+\/?\w+).*/gm, '$1');
+        }
+      } else {
+        color = colorSelector.value;
+      }
+      addHiddenDiv('added_color', color);
+    }
+
+    // Function to fetch gtin from script tag as not available directly on DOM.
+    function fetchGtinFromScript () {
+      const scriptDataTagSelector = document.evaluate('//script[@id="jsonld"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      const scriptTagData = scriptDataTagSelector ? scriptDataTagSelector.innerText : '';
+      const gtin = scriptTagData ? scriptTagData.replace(/\n/gm, '').replace(/.*gtin12.*?(\d+).*/gm, '$1') : '';
+      if (gtin.length < 15) {
+        addHiddenDiv('added_gtin', gtin);
+      }
+    }
+
     const brandSelector = document.querySelector('div[data-id="productData"]');
     const brandJSON = brandSelector ? brandSelector.getAttribute('data-value') : '';
     const brandObj = JSON.parse(brandJSON);
@@ -53,11 +80,13 @@ async function implementation (
     for (let i = 0; i < descriptionArray.length; i++) {
       const descObj = descriptionArray[i];
       if (descObj.includes('•')) {
-        bulletInfoArray.push(descObj.replace('•', '').trim());
+        bulletInfoArray.push(descObj.replace('•', ' || ').trim());
       }
     }
-    const additionalDescBulletInfo = bulletInfoArray.join(' || ');
+    const additionalDescBulletInfo = bulletInfoArray.join('');
     addHiddenDiv('added-additionalDescBulletInfo', additionalDescBulletInfo);
+    fetchGtinFromScript();
+    fetchColor();
   });
 
   await new Promise(resolve => setTimeout(resolve, 20000));
--- src/library/product/details/stores/t/tsc/format.js
+++ src/library/product/details/stores/t/tsc/format.js
@@ -19,6 +19,19 @@ const transform = (data) => {
 
   for (const { group } of data) {
     for (const row of group) {
+
+      if (row.warranty) {
+        if (row.warranty.length > 1) {
+          row.warranty = [row.warranty[row.warranty.length - 1]];
+        }
+      }
+
+      if (row.price) {
+        row.price.forEach(priceItem => {
+          priceItem.text = priceItem.text.replace(',', '');
+        });
+      }
+
       // ManufacturerImages may contain full URL or not hence appending https:
       if (row.manufacturerImages) {
         row.manufacturerImages.forEach(imageItem => {
@@ -27,9 +40,11 @@ const transform = (data) => {
           }
         });
       }
+
       if (row.alternateImages) {
         row.secondaryImageTotal = [{ text: row.alternateImages.length }];
       }
+
       // Adding || to nameExtended bullets and nameExtended bullet count
       if (row.description) {
         row.description.forEach(descItem => {
@@ -39,6 +54,7 @@ const transform = (data) => {
           descItem.text = cleanUp(descItem.text);
         });
       }
+
       // If availabilityText contains SOLD OUT then it will be OUT OF STOCK else IN STOCK
       if (row.availabilityText) {
         row.availabilityText.forEach(avItem => {
@@ -51,25 +67,50 @@ const transform = (data) => {
       } else {
         row.availabilityText = [{ text: 'IN STOCK' }];
       }
+
       // Adding yes or no if image zoom exists
       if (row.imageZoomFeaturePresent) {
         row.imageZoomFeaturePresent = [{ text: 'YES' }];
       } else {
         row.imageZoomFeaturePresent = [{ text: 'NO' }];
       }
+
       // Adding nameExtended
       if (row.name && row.brandText && row.variantInformation) {
         row.nameExtended = [{ text: row.brandText[0].text + ' - ' + row.name[0].text + ' - ' + row.variantInformation[0].text }];
       } else if (row.name && row.brandText) {
         row.nameExtended = [{ text: row.brandText[0].text + ' - ' + row.name[0].text }];
       }
+
       if (row.countryOfOrigin) {
         row.countryOfOrigin.forEach(countryOfOriginItem => {
           countryOfOriginItem.text = cleanUp(countryOfOriginItem.text);
-          if (countryOfOriginItem.text.includes('Country of origin')) {
-            countryOfOriginItem.text = countryOfOriginItem.text.replace(/.*?Country of origin: ?(\w*).*/gm, '$1');
+          if (countryOfOriginItem.text.toLowerCase().includes('country of origin')) {
+            countryOfOriginItem.text = countryOfOriginItem.text.replace(/.*?Country of origin: ?(\w*).*/gmi, '$1');
           } else {
             countryOfOriginItem.text = '';
+          }
+        });
+      }
+
+      if (row.weightNet) {
+        row.weightNet.forEach(weightNetItem => {
+          weightNetItem.text = cleanUp(weightNetItem.text);
+          if (weightNetItem.text.includes('Weight')) {
+            weightNetItem.text = weightNetItem.text.replace(/.*Weight.*?: ?(.*?) ?•.*/gm, '$1');
+          } else {
+            weightNetItem.text = '';
+          }
+        });
+      }
+
+      if (row.manufacturerDescription) {
+        row.manufacturerDescription.forEach(manufacturerDescriptionItem => {
+          if (manufacturerDescriptionItem.text.toLowerCase().includes('user manual') || manufacturerDescriptionItem.text.toLowerCase().includes('owner\'s guide')) {
+            manufacturerDescriptionItem.text = '';
+          } else {
+            manufacturerDescriptionItem.text = cleanUp(manufacturerDescriptionItem.text);
+            manufacturerDescriptionItem.text = manufacturerDescriptionItem.text.replace(/•/gm, ' || ');
           }
         });
       }
