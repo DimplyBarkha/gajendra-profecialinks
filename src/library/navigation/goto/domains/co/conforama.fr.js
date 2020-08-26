289b2dff3e61d49f5d4c85b97c944fa314d91992
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-08-26 08:08:23 +0530

    capatcha boolean logic code (#438)

--- src/library/navigation/goto/domains/co/conforama.fr.js
+++ src/library/navigation/goto/domains/co/conforama.fr.js
@@ -3,28 +3,35 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'conforama.fr',
-    timeout: 9000000,
+    timeout: 90000,
     country: 'FR',
     store: 'conforama',
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    await context.goto(url, { timeout: 9000000, waitUntil: 'load', checkBlocked: false });
+    await context.goto(url, { timeout: 90000, waitUntil: 'load', checkBlocked: false, first_request_timeout: 90000});
     try {
-      await context.waitForSelector('.g-recaptcha');
-      await context.solveCaptcha({
-        type: 'RECAPTCHA',
-        // inputElement: '.g-recaptcha' rc-anchor-container
-        inputElement: '#rc-anchor-container'
-      }); 
+      // await context.waitForSelector('.g-recaptcha');
+      const hasCaptcha = await context.evaluate(function (xp) {
+        const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+        console.log(xp, r);
+        const e = r.iterateNext();
+        console.log(e);
+        return !e;
+      }, '//*[@class="g-recaptcha"]');
+
+      if(hasCaptcha){
+        await context.solveCaptcha({
+          type: 'RECAPTCHA',
+          inputElement: '.g-recaptcha'
+        }); 
+      }
+
     } catch (error) {
       
     }
     
   await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-  // await context.waitForFunction(function (sel, xp) {
-  //   return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-  // }, {"timeout":90000}, "div[id=\"product-detail-page\"]", "//div[@class=\"error\"]")
   },
   
 };
