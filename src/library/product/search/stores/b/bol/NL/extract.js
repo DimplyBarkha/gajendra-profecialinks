3ba4566799c58b240c22c0a03eb669f18fda97e7
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-08-27 00:18:36 +0530

    Fixed all the issues with the extractor

--- src/library/product/search/stores/b/bol/NL/extract.js
+++ src/library/product/search/stores/b/bol/NL/extract.js
@@ -1,18 +1,5 @@
-
-module.exports = {
-  implements: 'product/search/extract',
-  parameterValues: {
-    country: 'NL',
-    store: 'bol',
-    transform: null,
-    domain: 'bol.com',
-    zipcode: '',
-  },
-  implementation
-};
-
-// const { transform } = require('../../../../shared');
-async function implementation (
+const { transform } = require('./shared');
+async function implementation(
   inputs,
   parameters,
   context,
@@ -20,8 +7,46 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await context.evaluate(async function () {
 
+  await context.evaluate(async function () {
+    const cookieEle = document.querySelector("button[data-test='consent-modal-confirm-btn']");
+    if (cookieEle) {
+      document.querySelector("button[data-test='consent-modal-confirm-btn']").click();
+    }
   });
+
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall(ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
   return await context.extract(productDetails, { transform });
 }
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'NL',
+    store: 'bol',
+    transform,
+    domain: 'bol.com',
+    zipcode: '',
+  },
+  implementation
+};
---
+++ src/library/product/search/stores/b/bol/NL/shared.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
+  for (const { group } of data) {
+    for (const row of group) {
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
+      if (row.price) {
+        const currentPrice = row.price[0].text;
+        row.price = [{ text: currentPrice.replace('.', ',') }];
+      }
+      if (row.aggregateRating2) {
+        const aggregateRating2 = row.aggregateRating2[0].text;
+        row.aggregateRating2 = [{ text: aggregateRating2.replace('.', ',') }];
+      }
+
+    }
+  }
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  return data;
+};
+module.exports = { transform };
