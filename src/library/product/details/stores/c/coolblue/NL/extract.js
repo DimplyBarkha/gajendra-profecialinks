2aeb40397281743333b14c1c58e4e522117f857c
Ankit Kumar <ankit.k@osmosys.asia>
2020-08-26 20:00:48 +0530

    updated script to handle not extractable xpaths

--- src/library/product/details/stores/c/coolblue/NL/extract.js
+++ src/library/product/details/stores/c/coolblue/NL/extract.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      // click on the image to get the alternate image info
+      // click on the image to get the alternate image info (no need to revert this as all the data is available in this situation too)
       const mainImageselector = document.querySelector('ul.product-media-gallery__wrapper>li');
       if (mainImageselector) {
         console.log('main image selector found');
@@ -24,6 +24,8 @@ module.exports = {
       }).filter(elm => elm);
       document.body.setAttribute('category', breacrumb.join('>'));
       console.log('category set');
+      // checking if one of the not extracted xpath is loaded in time (wait for selector says its available)
+      console.log(`THIS IS LOADED : ${document.querySelector('body > footer > div.grid-wrapper-xs--layout-wrapper.grid-wrapper-xs--padding.grid-wrapper-m--padding.grid-section-xs--gap-1.grid-section-m--gap-8 > ul > li:nth-child(3) > div.is-hidden-until-size-m > a > h5').textContent}`);
     });
     await context.extract(productDetails);
   },
