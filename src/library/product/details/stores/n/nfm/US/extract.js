eaacf6632b94a129000ef5ec43fe8f62b4df4551
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-08-26 13:21:21 +0530

    #459. Extractor code updated for source nfm.

--- src/library/product/details/stores/n/nfm/US/extract.js
+++ src/library/product/details/stores/n/nfm/US/extract.js
@@ -1,12 +1,12 @@
-const { transform } = require("./format");
+const { transform } = require('./format');
 module.exports = {
-  implements: "product/details/extract",
+  implements: 'product/details/extract',
   parameterValues: {
-    country: "US",
-    store: "nfm",
+    country: 'US',
+    store: 'nfm',
     transform,
-    domain: "nfm.com",
-    zipcode: "",
+    domain: 'nfm.com',
+    zipcode: '',
   },
   implementation: async (inputs, parameters, context, dependencies) => {
     const { transform } = parameters;
--- src/library/product/details/stores/n/nfm/US/format.js
+++ src/library/product/details/stores/n/nfm/US/format.js
@@ -6,50 +6,28 @@
 const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
-      console.log("in row category", row.category);
-      // console.log("row.additionalDescBulletInfo", row.additionalDescBulletInfo);
-      console.log("row.descriptionBullets", row.descriptionBullets);
-      console.log("row.description", row.description);
-      console.log("row.largeImageCount", row.largeImageCount);
-      console.log("row.ratingCount", row.ratingCount);
-      console.log("row.specifications", row.specifications);
-      console.log("row.aggregateRatingText", row.aggregateRatingText);
-      console.log("row.nameExtended", row.nameExtended);
       if (row.category) {
-        let breadcrumb = row.category[0].text;
-        breadcrumb =
-          breadcrumb.split(" ")[0] === "Home"
-            ? breadcrumb.replace("Home ", "")
-            : breadcrumb;
-        breadcrumb = breadcrumb.replace(/\n \n/g, "|").trim();
-        breadcrumb =
-          breadcrumb.charAt(0) === "|"
-            ? breadcrumb.replace("| ", "")
-            : breadcrumb;
-        row.category = [
+        if (row.category[0].text === 'Home') {
+          row.category.shift();
+        }
+      }
+      if (row.additionalDescBulletInfo) {
+        let text = '';
+        row.additionalDescBulletInfo.forEach((el) => {
+          text += el.text.replace(/\n \n/g, '||');
+        });
+        row.additionalDescBulletInfo = [
           {
-            text: breadcrumb,
-            xpath: row.category[0].xpath,
+            text: text.slice(0, -3).trim(),
           },
         ];
       }
-      // if (row.additionalDescBulletInfo) {
-      //   let text = "";
-      //   row.additionalDescBulletInfo.forEach((el) => {
-      //     text += el.text + " || ";
-      //   });
-      //   row.additionalDescBulletInfo = [
-      //     {
-      //       text: text.slice(0, -3).trim(),
-      //     },
-      //   ];
-      // }
       if (row.descriptionBullets) {
-        let info = row.descriptionBullets[0].text;
-        row.descriptionBullets[0].text = info.split("\n \n").length;
+        const info = row.additionalDescBulletInfo[0].text;
+        row.descriptionBullets[0].text = info.split('||').length;
       }
       if (row.description) {
-        row.description[0].text = row.description[0].text.replace(/\n/g, "");
+        row.description[0].text = row.description[0].text.replace(/\n/g, '');
       }
       if (row.largeImageCount) {
         row.largeImageCount = [
@@ -60,7 +38,7 @@ const transform = (data) => {
       }
       if (row.ratingCount) {
         let pr = row.ratingCount[0].text;
-        pr = pr.replace(/^\((.+)\)$/, "$1");
+        pr = pr.replace(/^\((.+)\)$/, '$1');
         row.ratingCount = [
           {
             text: pr,
@@ -69,9 +47,9 @@ const transform = (data) => {
         ];
       }
       if (row.specifications) {
-        let text = "";
+        let text = '';
         row.specifications.forEach((item) => {
-          text += `${item.text.replace(/\n \n/g, "")} || `;
+          text += `${item.text.replace(/\n \n/g, '')} || `;
         });
         row.specifications = [
           {
@@ -81,24 +59,13 @@ const transform = (data) => {
       }
       if (row.aggregateRatingText) {
         let ag = row.aggregateRatingText[0].text;
-        ag = ag.split(" ")[0] === "Rated" ? ag.replace("Rated ", "") : ag;
+        ag = ag.split(' ')[0] === 'Rated' ? ag.replace('Rated ', '') : ag;
         row.aggregateRatingText = [
           {
             text: ag,
             xpath: row.aggregateRatingText[0].xpath,
           },
         ];
-      }
-      if (row.nameExtended) {
-        let firstWord = row.nameExtended[0].text;
-        firstWord = firstWord.split(" ");
-        row.nameExtended[0].text = row.nameExtended
-          ? firstWord[0] +
-            " - " +
-            row.nameExtended[0].text +
-            " - " +
-            row.color[0].text
-          : row.nameExtended[0].text;
       }
     }
   }
