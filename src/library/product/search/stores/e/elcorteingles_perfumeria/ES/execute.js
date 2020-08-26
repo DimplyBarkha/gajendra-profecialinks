e67fe38c9df20c67680d3dfd9e30dd88cc64d2fb
sheril.p <SHERIL.P@OSMOSYS.ASIA>
2020-08-26 10:15:48 +0530

    updated cose and full inputs

--- src/library/product/search/stores/e/elcorteingles_perfumeria/ES/execute.js
+++ src/library/product/search/stores/e/elcorteingles_perfumeria/ES/execute.js
@@ -4,7 +4,7 @@ module.exports = {
     country: 'ES',
     store: 'elcorteingles_perfumeria',
     domain: 'elcorteingles.es',
-    url: 'https://www.elcorteingles.es/perfumeria/search/?s={searchTerms}',
+    url: 'https://www.elcorteingles.es/perfumeria/search/2/?s={searchTerms}&stype=text_box',
     loadedSelector: 'img.js_preview_image.lazyloaded',
     noResultsXPath: '//div[@class="products_list-container _no_products vp"] | //html[not(//div[@id="products-list"]/ul/li)]',
     zipcode: '',
