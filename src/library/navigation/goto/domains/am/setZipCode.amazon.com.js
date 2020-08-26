e233909fdc94372437b01750a96f2b5489ffe1c1
alscotty <alawrencescott@gmail.com>
2020-08-26 14:50:55 -0700

    testing with API zip

--- src/library/navigation/goto/domains/am/setZipCode.amazon.com.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.com.js
@@ -30,9 +30,52 @@ const implementation = async (inputs, parameters, context, dependencies) => {
     // try one more time if it fails:
     console.log(exception);
     await changeZip(zipcode);
-    // throw new Error('Failed to update zipcode!');
   }
 };
+
+// const implementation = async (inputs, parameters, context, dependencies) => {
+//   const { zipcode } = inputs;
+//   const country = 'US';
+
+//   const changeLocation = () => context.evaluate(async (country, zip) => {
+
+//     const body = zip
+//       ? `locationType=LOCATION_INPUT&zipCode=${zip}&storeContext=generic&deviceType=web&pageType=Gateway&actionSource=glow&almBrandId=undefined`
+//       : `locationType=COUNTRY&district=${country}&countryCode=${country}&storeContext=generic&deviceType=web&pageType=Gateway&actionSource=glow&almBrandId=undefined`;
+
+//     const response = await fetch('/gp/delivery/ajax/address-change.html', {
+//       headers: {
+//         accept: 'text/html,*/*',
+//         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+//         'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+//         'x-requested-with': 'XMLHttpRequest',
+//       },
+//       body,
+//       method: 'POST',
+//       mode: 'cors',
+//       credentials: 'include',
+//     });
+//     return response.status;
+
+//   },country,zipcode);
+
+//   console.log('HIT API ZIP CHANGERRRR');
+
+//   if (await changeLocation() !== 200) {
+//     throw new Error(`Cannot change location (${changeLocation})`);
+//   }
+
+//   function clearPopup () { context.evaluate(()=>{
+//       let popup = document.evaluate("count(//span[@class='a-button-inner']/span[@class='a-button-text' and contains(text(),'Close')])>0", document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue;
+//       let el = document.querySelector('span.a-button-inner span.a-button-text');
+//       if (popup && el){
+//         el.click()
+//       }
+//     });
+//   } 
+//   clearPopup();
+
+// };
 
 module.exports = {
   implements: 'navigation/goto/setZipCode',
