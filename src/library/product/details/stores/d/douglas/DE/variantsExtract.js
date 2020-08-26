fe6a9f709b736653d04ebfb4d033a8beacc2b9b4
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-08-26 15:23:15 +0530

    Fixed variant extractor

--- src/library/product/details/stores/d/douglas/DE/variantsExtract.js
+++ src/library/product/details/stores/d/douglas/DE/variantsExtract.js
@@ -9,6 +9,7 @@ async function implementation(
     function addHiddenDiv(id, content) {
       const newDiv = document.createElement('div');
       newDiv.id = id;
+      newDiv.title = content.substring(content.lastIndexOf('_') + 1, content.lastIndexOf('.'));
       newDiv.textContent = content;
       newDiv.style.display = 'none';
       document.body.appendChild(newDiv);
@@ -16,15 +17,11 @@ async function implementation(
     }
     let url = window.location.href;
     const variantContainer = document.querySelector('div.rd__product-details__picker__list');
-    console.log('came to variant')
-    console.log(addHiddenDiv('ajay', "em ledu"))
     if (variantContainer) {
-      console.log('variant container found')
       const variants = variantContainer.querySelectorAll('div.rd__product-details__picker__list__item');
       for (var i = 0; i < variants.length; i++) {
         variants[i].click();
         addHiddenDiv('variantUrl', window.location.href)
-        console.log(window.location.href);
       }
     }
 
