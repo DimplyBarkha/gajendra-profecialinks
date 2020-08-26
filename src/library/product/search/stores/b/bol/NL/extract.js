eb8160cd79b6434feb75b4e31dc7f609008081e3
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-08-26 16:05:42 +0530

    added extractoion in extract.js

--- src/library/product/search/stores/b/bol/NL/extract.js
+++ src/library/product/search/stores/b/bol/NL/extract.js
@@ -8,4 +8,20 @@ module.exports = {
     domain: 'bol.com',
     zipcode: '',
   },
+  implementation
 };
+
+// const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+
+  });
+  return await context.extract(productDetails, { transform });
+}
