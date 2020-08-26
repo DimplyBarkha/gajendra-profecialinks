b53535e00709e08330d88623677971a983356516
sushma-mangalapally <sushma.mangalapally@import.io>
2020-08-26 14:31:17 -0700

    Fix no results xpath and add proxy for core variants

--- src/library/product/details/stores/a/amazonFresh/US/execute.js
+++ src/library/product/details/stores/a/amazonFresh/US/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'amazonFresh',
     domain: 'amazon.com',
     loadedSelector: 'span[data-component-type="s-product-image"]',
-    noResultsXPath: '//span[contains(@class, "messaging-messages-no-results")]',
+    noResultsXPath: '//span[contains(@class, "messaging-messages-no-results")]|//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "404")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")] | //*[contains(text(),"Looking for something?")] | //img[contains(@alt,"Sorry! Something went wrong on our end. ")] | //div[@id="g"]//img[contains(@alt,"Dogs")]',
     // loadedSelector: '#productTitle',
     // noResultsXPath: '//div[contains(@class, "page-content") and not(//div[contains(@class, "reviews-content")])] | //div[contains(@class, "no-reviews-section")] | //a[contains(@href, "404")] | //a[contains(@href, "dogsofamazon")] | //b[contains(@class, "h1") and contains(text(), "particolare")] | //b[contains(@class, "h1") and contains(text(), "Buscas algo")] | //img[contains(@alt, "fetch that page")] | //*[contains(text(),"Looking for something?")] | //img[contains(@alt,"Sorry! Something went wrong on our end. ")] | //div[@id="g"]//img[contains(@alt,"Dogs")]',
   },
