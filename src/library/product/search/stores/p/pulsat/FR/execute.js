712dfea44474d5e3a9c482d2ef2e2981659d6c6b
nikita.kumari <nikita.kumari@synerzip.com>
2020-08-26 16:44:10 +0530

    LoadSelector fixes #473

--- src/library/product/search/stores/p/pulsat/FR/execute.js
+++ src/library/product/search/stores/p/pulsat/FR/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'pulsat',
     domain: 'pulsat.fr',
     url: 'https://www.pulsat.fr/catalogsearch/result/?q={searchTerms}',
-    loadedSelector: 'div[class="item product-list-col"] article',
-    noResultsXPath: '//div[@class="alert alert-info"]//text()',
+    loadedSelector: 'div[class*="product-list-col"] article',
+    noResultsXPath: "//div[@class='alert alert-info' and contains(.,'Aucun résultat trouvé')]",
     zipcode: '',
   },
 };
