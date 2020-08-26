efb5d74266d134fbc31a30130a3bd1147dbb2339
Gauri Nerkar <gaurin@synerzipune.local>
2020-08-26 17:27:52 +0530

    #478 elgigante se Initial commit

---
+++ src/library/navigation/goto/domains/el/elgiganten.se.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'elgiganten.se',
+    timeout: null,
+    country: 'SE',
+    store: 'elgiganten',
+    zipcode: '',
+  },
+};
