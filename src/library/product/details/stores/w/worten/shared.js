93b790ad1fd6c4b2b9c87b113139e6f607670d01
Manoj Dey <manoj.d@OSMLIN16>
2020-08-26 21:41:51 +0530

    updated code

---
+++ src/library/product/details/stores/w/worten/shared.js
@@ -0,0 +1,61 @@
+
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+    // Default transform function
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+
+    for (const { group } of data) {
+      for (const row of group) {
+        if (row.description) {
+            let text = '';
+            row.description.forEach(item => {
+              text += item.text.replace(/\\n/g, ' ');
+            });
+            row.description = [
+              {
+                text: text,
+              },
+            ];
+        }
+        if (row.aggregateRating) {
+            let text = '';
+            row.aggregateRating.forEach(item => {
+              text += item.text.replace(new RegExp('(.+)\\.(.+)', 'g'), '$1,$2');
+            });
+            row.aggregateRating = [
+              {
+                text: text,
+              },
+            ];
+        }
+        if (row.specifications) {
+          let text = '';
+          row.specifications.forEach(item => {
+            text = row.specifications.map(elm => elm.text).join(' ').replace(/●/g, '||');
+          });
+          row.specifications = [{ text }];
+        }
+      }
+    }
+    return data;
+  };
+
+  module.exports = { transform };
