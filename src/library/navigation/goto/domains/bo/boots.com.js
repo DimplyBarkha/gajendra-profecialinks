33dc74a300776f71609d5b86ca91946d1725c998
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-27 22:39:14 +0530

    #458. refactored goto implementation

--- src/library/navigation/goto/domains/bo/boots.com.js
+++ src/library/navigation/goto/domains/bo/boots.com.js
@@ -9,10 +9,12 @@ module.exports = {
     timeout: 50000,
   },
   implementation: async ({ url }, { country, domain, timeout }, context, dependencies) => {
-    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true });
-    await context.waitForNavigation({ waitUntil: 'networkidle0' });
     const newUrl = await context.evaluate(function (url) {
-      const searchTerm = url.match(/https:\/\/www.boots.com\/sitesearch?searchTerm=(.+)/) && url.match(/https:\/\/www.boots.com\/sitesearch?searchTerm=(.+)/)[1].toLowerCase();
+      const searchTerm =
+        url.match(/https:\/\/www.boots.com\/sitesearch\?searchTerm=(.+)/) &&
+        url
+          .match(/https:\/\/www.boots.com\/sitesearch\?searchTerm=(.+)/)[1]
+          .toLowerCase();
       if (
         window.location.pathname.toLowerCase() === '/dyson' ||
         (searchTerm &&
@@ -20,14 +22,12 @@ module.exports = {
           searchTerm.match(/\w+/).length === 1 &&
           searchTerm.match(/dyson/))
       ) {
-        return window.location.origin + '/dyson/dyson-shop-all';
+        return 'https://www.boots.com/dyson/dyson-shop-all';
       } else {
         return false;
       };
     }, url);
-
-    if (newUrl) {
-      await context.goto(newUrl, { timeout, waitUntil: 'load', checkBlocked: true });
-    }
+    url = newUrl || url;
+    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true });
   },
 };
--- src/library/navigation/goto/domains/bo/boots.ie.js
+++ src/library/navigation/goto/domains/bo/boots.ie.js
@@ -9,9 +9,12 @@ module.exports = {
     timeout: 50000,
   },
   implementation: async ({ url }, { country, domain, timeout }, context, dependencies) => {
-    await context.goto(url, { timeout, waitUntil: 'networkidle0', checkBlocked: true });
     const newUrl = await context.evaluate(function (url) {
-      const searchTerm = url.match(/https:\/\/www.boots.com\/sitesearch?searchTerm=(.+)/) && url.match(/https:\/\/www.boots.com\/sitesearch?searchTerm=(.+)/)[1].toLowerCase();
+      const searchTerm =
+        url.match(/https:\/\/www.boots.ie\/sitesearch\?searchTerm=(.+)/) &&
+        url
+          .match(/https:\/\/www.boots.ie\/sitesearch\?searchTerm=(.+)/)[1]
+          .toLowerCase();
       if (
         window.location.pathname.toLowerCase() === '/dyson' ||
         (searchTerm &&
@@ -19,14 +22,12 @@ module.exports = {
           searchTerm.match(/\w+/).length === 1 &&
           searchTerm.match(/dyson/))
       ) {
-        return window.location.origin + '/dyson/dyson-shop-all';
+        return 'https://www.boots.ie/dyson/dyson-shop-all';
       } else {
         return false;
       };
     }, url);
-
-    if (newUrl) {
-      await context.goto(newUrl, { timeout, waitUntil: 'load', checkBlocked: true });
-    }
+    url = newUrl || url;
+    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true });
   },
 };
