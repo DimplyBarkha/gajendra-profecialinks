d767a23f38ad61f0f6d7a7568cf7bf296e70b5f5
sushma-mangalapally <sushma.mangalapally@import.io>
2020-08-27 09:17:36 -0700

    Add search

---
+++ src/library/product/search/stores/m/mediamarkt/NL/extract.js
@@ -0,0 +1,28 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+  await context.waitForSelector('button.gdpr-cookie-layer__btn--submit');
+
+  await context.evaluate(async function () {
+    if (document.querySelector('gdpr-cookie-layer--show') && document.querySelector('button.gdpr-cookie-layer__btn--submit')) {
+      document.querySelector('button.gdpr-cookie-layer__btn--submit').click();
+    }
+  });
+  return await context.extract(productDetails);
+}
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'NL',
+    store: 'mediamarkt',
+    transform: null,
+    domain: 'mediamarkt.nl',
+    zipcode: '',
+  },
+  implementation,
+};
