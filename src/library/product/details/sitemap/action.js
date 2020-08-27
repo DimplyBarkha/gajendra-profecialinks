d2e3a95faa8d988d3bcce7a43db7e5b210c78fbe
Guillaume Vial <guillaumevial@Guiguis-mac.local>
2020-08-27 13:07:16 +0100

    added sitemap template

---
+++ src/library/product/details/sitemap/action.js
@@ -0,0 +1,60 @@
+
+/**
+ *
+ * @param { { url: any, URL: any } } inputs
+ * @param { { country: any, domain: any, store: any } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { execute: ImportIO.Action, extract: ImportIO.Action } } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { URL, url } = inputs;
+  const { execute, extract } = dependencies;
+  await execute({ url: url || URL });
+
+  await extract({ url: url || URL });
+}
+
+module.exports = {
+  parameters: [
+    {
+      name: 'country',
+      description: '',
+      optional: false,
+    },
+    {
+      name: 'domain',
+      description: '',
+      optional: false,
+    },
+    {
+      name: 'store',
+      description: '',
+      optional: false,
+    },
+  ],
+  inputs: [
+    {
+      name: 'url',
+      description: '',
+      type: 'string',
+      optional: true,
+    },
+    {
+      name: 'URL',
+      description: '',
+      type: 'string',
+      optional: true,
+    },
+  ],
+  dependencies: {
+    execute: 'action:product/details/execute',
+    extract: 'action:product/details/variants/sitemap',
+  },
+  path: './stores/${store[0:1]}/${store}/${country}/sitemap',
+  implementation,
+};
---
+++ src/library/product/details/stores/d/dyson/UK/sitemap.js
@@ -0,0 +1,51 @@
+
+/**
+ *
+ * @param { { url: any, id: any } } inputs
+ * @param { { country: any, transform: any, store: any } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { Record<string, any> } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { sitemap } = dependencies;
+  return await context.extract(sitemap, { transform });
+}
+
+module.exports = {
+  parameters: [
+    {
+      name: 'country',
+      description: '',
+      optional: false,
+    },
+    {
+      name: 'store',
+      description: '',
+      optional: false,
+    },
+    {
+      name: 'transform',
+      description: 'transform function for the extraction',
+      optional: true,
+    },
+  ],
+  inputs: [
+    {
+      name: 'url',
+      description: '',
+      type: 'string',
+      optional: false,
+    },
+  ],
+  dependencies: {
+    sitemap: 'extraction:product/details/stores/${store[0:1]}/${store}/${country}/sitemap',
+  },
+  path: '../stores/${store[0:1]}/${store}/${country}/sitemap',
+  implementation,
+};
