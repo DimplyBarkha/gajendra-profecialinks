8622ce9cbee6aaa15dd5ef0853853dadf821f8b1
Guillaume Vial <guillaumevial@Guiguis-mac.local>
2020-08-27 13:12:17 +0100

    sitemap

---
+++ src/library/product/details/stores/d/dyson/UK/sitemapExtract.js
@@ -0,0 +1,9 @@
+module.exports = {
+  implements: 'product/details/sitemap/sitemapExtract',
+  parameterValues: {
+    country: 'UK',
+    store: 'dyson',
+    transform: null,
+    domain: 'dyson.co.uk',
+  },
+};
