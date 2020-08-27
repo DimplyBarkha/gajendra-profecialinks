98c91fcdecac08404a58c75b34b851c7facaa1af
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-08-27 11:05:06 -0700

    sephora us serach updates

--- src/library/product/details/stores/s/sephora/US/extract.js
+++ src/library/product/details/stores/s/sephora/US/extract.js
@@ -10,29 +10,29 @@ module.exports = {
   },
   implementation: async ({ parentInput }, { country, domain, transform: transformParam }, context, { productDetails }) => {
 
-    // const itemUrl = await context.evaluate(function() {
-    //   let resultsCheck = '(//h1//text()[not(parent::b)])[1]'
-    //   var checkResults = document.evaluate( resultsCheck, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-    //   if(checkResults.snapshotLength > 0){
-    //     let checkNone = checkResults.snapshotItem(0).textContent;
-    //     if(checkNone === "0 Product results:"){
-    //       throw new Error("notFound");
-    //     }
-    //   }
+    const itemUrl = await context.evaluate(function() {
+      let resultsCheck = '(//h1//text()[not(parent::b)])[1]'
+      var checkResults = document.evaluate( resultsCheck, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      if(checkResults.snapshotLength > 0){
+        let checkNone = checkResults.snapshotItem(0).textContent;
+        if(checkNone === "0 Product results:"){
+          throw new Error("notFound");
+        }
+      }
 
-    //   let itemCheck = '//div[@data-comp="ProductGrid "]//a'
-    //   var checkElement = document.evaluate( itemCheck, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-    //   if( checkElement.snapshotLength > 0 ) {
-    //     let url = checkElement.snapshotItem(0).href
-    //     let splits = url.split("&")
-    //     return splits[0]
-    //   } else {
-    //     return null
-    //   }
-    // })
-    // if(itemUrl){
-    //   await context.goto(itemUrl, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
-    // }
+      let itemCheck = '//div[@data-comp="ProductGrid "]//a'
+      var checkElement = document.evaluate( itemCheck, document, null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      if( checkElement.snapshotLength > 0 ) {
+        let url = checkElement.snapshotItem(0).href
+        let splits = url.split("&")
+        return splits[0]
+      } else {
+        return null
+      }
+    })
+    if(itemUrl){
+      await context.goto(itemUrl, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
+    }
 
 
 
--- src/library/product/search/stores/s/sephora/US/extract.js
+++ src/library/product/search/stores/s/sephora/US/extract.js
@@ -23,16 +23,16 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  const scrollFunc = await context.evaluate(async function () {
+  const scrollFunc = await context.waitForFunction(async function () {
     let scrollTop = 0;
     while (scrollTop !== 20000) {
       // await stall(2500);
       // await new Promise(resolve => setTimeout(resolve, 5000));
       scrollTop += 1000;
       window.scroll(0, scrollTop);
-      await new Promise(resolve => setTimeout(resolve, 2000));
-
+      // await new Promise(resolve => setTimeout(resolve, 5000));
       console.log("SCROLLING");
+      
       if (scrollTop === 20000) {
         break;
       }
