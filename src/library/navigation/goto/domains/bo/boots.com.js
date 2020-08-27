1b382de4de06eb29ed55b581378f7c63a93f2c59
vikas kyatannawar <vikas.k@osmosys.asia>
2020-08-27 14:52:47 +0530

    #458. updated goto conditions

--- src/library/navigation/goto/domains/bo/boots.com.js
+++ src/library/navigation/goto/domains/bo/boots.com.js
@@ -12,8 +12,7 @@ module.exports = {
     await context.waitForNavigation({ waitUntil: 'networkidle0' });
     const newUrl = await context.evaluate(function (url) {
       const searchTerm = url.match(/https:\/\/www.boots.com\/sitesearch?searchTerm=(.+)/) && url.match(/https:\/\/www.boots.com\/sitesearch?searchTerm=(.+)/)[1].toLowerCase();
-      if (!searchTerm) return;
-      if (window.location.pathname.toLowerCase() === '/dyson' || (searchTerm.match(/\w+/) && searchTerm.match(/\w+/).length === 1 && searchTerm.match(/dyson/))) {
+      if (window.location.pathname.toLowerCase() === '/dyson' || (searchTerm && searchTerm.match(/\w+/) && searchTerm.match(/\w+/).length === 1 && searchTerm.match(/dyson/))) {
         return window.location.origin + '/dyson/dyson-shop-all';
       } else { return false; };
     }, url);
