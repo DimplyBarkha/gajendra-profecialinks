71479d729afad7d3b9124b3b42b06f01913b7c07
sourabhk <sourabh.karmarkar@synerzip.com>
2020-08-28 21:37:27 +0530

    #433 Added code to fetch webcollage content for fetching enhanced_content

--- src/library/navigation/goto/domains/cd/cdiscount.com.js
+++ src/library/navigation/goto/domains/cd/cdiscount.com.js
@@ -10,13 +10,10 @@ module.exports = {
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.setLoadImages(true);
-    await context.setLoadAllResources(true);
-    await context.setJavaScriptEnabled(true);
-    await context.setBlockAds(false);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, js_enabled: true, css_enabled: false, random_move_mouse: true });
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, load_all_resources: true, block_ads: false, anti_fingerprint: true, js_enabled: true, css_enabled: false, random_move_mouse: true });
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
     }
+    console.log('goto finished ------------------->');
   },
 };
--- src/library/product/details/stores/c/cdiscount/FR/extract.js
+++ src/library/product/details/stores/c/cdiscount/FR/extract.js
@@ -16,7 +16,51 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await context.evaluate(async function (url) {
+  // Using variant SKU array from 'prodSkus' to preform fetch for manufacturer information for each variant
+  async function collectManuf (variants) {
+    // Must first goto the scontent.webcollage to get credentials for next fetch
+    await context.goto('https://scontent.webcollage.net#[!opt!]{"type":"js","init_js":""}[/!opt!]', { timeout: 20000, waitUntil: 'load', checkBlocked: true });
+    const manufArray = [];
+    // Looping through each variant SKU
+    for (let i = 0; i < variants.length; i++) {
+      const html = await context.evaluate(async function getEnhancedContent (variants, i) {
+        // Recursive retry for fetches failures
+        async function fetchRetry (url, n) {
+          function handleErrors (response) {
+            if (response.status === 200) {
+              return response;
+            } else {
+              console.log('FETCH FAILED');
+              if (n === 1) return 'Nothing Found';
+              return fetchRetry(url, n - 1);
+            }
+          }
+          const fetched = fetch(url).then(handleErrors).then(response => response.text()).catch(function () {
+            console.log('FETCH FAILED');
+            if (n === 1) return 'Nothing Found';
+            return fetchRetry(url, n - 1);
+          });
+          return fetched;
+        }
+        return await fetchRetry(`https://scontent.webcollage.net/cdiscount-fr/power-page?ird=true&channel-product-id=${variants[i]}`, 10);
+      }, variants, i);
+
+      const regex = /html: "(.+)"\n\s\s\}\n\};/s;
+      let text = 'Not Found';
+      console.log('html -------> ', html);
+      // Checking the output of fetchRetry, if the output contains an "html" object it is collected and stored in the manufArray
+      if (html.match(regex)) {
+        text = html.match(regex)[1];
+      }
+      // Trimming the object so that it can be appended to the DOM
+      text = text.replace(/html: /g, '');
+      manufArray.push(text);
+    }
+    return manufArray;
+  }
+  const htmlList = await collectManuf(['DYS5025155028155']);
+
+  await context.evaluate(async function (url, htmlList) {
 
     function addHiddenDiv (id, content) {
       const newDiv = document.createElement('div');
@@ -25,7 +69,11 @@ async function implementation (
       newDiv.style.display = 'none';
       document.body.appendChild(newDiv);
     }
+    console.log('page is loaded successfully....executing scrolling code');
 
+    addHiddenDiv('added-htmlList', htmlList[0]);
+
+    /*
     // Scrolling till specifications as manufacturer images are loaded on website after scrolling down
     await new Promise(resolve => setTimeout(resolve, 10000));
     async function scrollToLoadAplusImages () {
@@ -41,6 +89,8 @@ async function implementation (
       }
     }
     await scrollToLoadAplusImages();
+    */
+    console.log('scrolling code execution complete.....');
 
     // If images are present in description then add to manufacturerDescription else add to description
     const manufacturerImageFlag = document.querySelector('div[id="inpage_container"] img');
@@ -53,7 +103,7 @@ async function implementation (
     } else {
       addHiddenDiv('added-description', description);
     }
-  }, inputs.url);
+  }, inputs.url, htmlList);
   await new Promise(resolve => setTimeout(resolve, 10000));
   return await context.extract(productDetails, { transform });
 }
