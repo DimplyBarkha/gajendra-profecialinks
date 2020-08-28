3715a78404a9fdf5974650ff7d99adb130293f46
Abhisek <abhisek.d@osmosys.asia>
2020-08-28 17:45:58 +0530

    Pushing PT

---
+++ src/library/product/details/stores/e/elcorteingles_electronica/PT/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'elcorteingles.es',
+    prefix: null,
+    url: null,
+    country: 'PT',
+    store: 'elcorteingles_electronica',
+    zipcode: '',
+  },
+};
