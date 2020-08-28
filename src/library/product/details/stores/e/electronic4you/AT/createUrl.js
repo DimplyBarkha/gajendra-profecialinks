b5efafb3d0c9e55173218ab6f57bf9c8c56f4da0
Karolina <karolina.urbanska@tdcm.io>
2020-08-28 15:49:47 +0200

    Created core extractor for electronic4you.at

---
+++ src/library/product/details/stores/e/electronic4you/AT/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'electronic4you.at',
+    prefix: null,
+    url: null,
+    country: 'AT',
+    store: 'electronic4you',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/e/electronic4you/AT/extract.js
@@ -0,0 +1,50 @@
+
+const { cleanUp } = require('../../../../shared');
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'AT',
+    store: 'electronic4you',
+    transform: cleanUp,
+    domain: 'electronic4you.at',
+    zipcode: '',
+  },
+  // @ts-ignore
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const descBullets = document.querySelector('div.short-description ul')
+        // @ts-ignore
+        ? document.querySelector('div.short-description ul').innerText : '';
+      if (descBullets) {
+        addElementToDocument('desc_bullets', descBullets.replace(/\n|•/g, '').replace(/\s{2,}/g, ' '));
+      }
+      const techDetails = document.querySelector('table#product-attribute-specs-table tbody')
+        // @ts-ignore
+        ? document.querySelector('table#product-attribute-specs-table tbody').innerText : '';
+      if (techDetails) {
+        addElementToDocument('desc_techDetails', techDetails.replace(/\n|•/g, '').replace(/\s{2,}/g, ' '));
+      }
+      const manufacturerDesc1 = document.querySelector('div.features-list-product-page')
+        // @ts-ignore
+        ? document.querySelector('div.features-list-product-page').innerText : '';
+      if (manufacturerDesc1) {
+        addElementToDocument('desc_manufacturer1', manufacturerDesc1.replace(/\n|•/g, '').replace(/\s{2,}/g, ' '));
+      }
+      const manufacturerDesc2 = document.querySelector('div.dyson-content')
+        // @ts-ignore
+        ? document.querySelector('div.dyson-content').innerText : '';
+      if (manufacturerDesc2) {
+        addElementToDocument('desc_manufacturer2', manufacturerDesc2.replace(/\n|•/g, '').replace(/\s{2,}/g, ' '));
+      }
+    });
+    await context.extract(productDetails);
+  },
+};
