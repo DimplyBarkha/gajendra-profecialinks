24a41d4e4bf7c6e03757c3937f0e6e739cb67af4
nikita.kumari <nikita.kumari@synerzip.com>
2020-08-30 18:22:37 +0530

    Updated noResultXpath #509

--- src/library/product/search/stores/q/qvc/GB/execute.js
+++ src/library/product/search/stores/q/qvc/GB/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'qvcuk.com',
     url: 'https://www.qvcuk.com/catalog/search.html?keyword={searchTerms}',
     loadedSelector: "div[class*='productGallery'] div[class*='galleryItem']",
-    noResultsXPath: "//div[@class='searchResults' and contains(.,'0 results')]",
+    noResultsXPath: "//div[@class='searchResults' and contains(.,'0 results')] | //*[not(//div[contains(@class,'productGallery')]//div[contains(@class,'galleryItem')])]",
     zipcode: '',
   },
 };
