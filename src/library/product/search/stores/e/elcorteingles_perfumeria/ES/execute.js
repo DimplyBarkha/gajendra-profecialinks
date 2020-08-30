ed4e12bb86302e6c90a46df14dfc296bc515b86a
Abhisek <abhisek.d@osmosys.asia>
2020-08-30 21:22:14 +0530

    Added details

--- src/library/product/search/stores/e/elcorteingles_perfumeria/ES/execute.js
+++ src/library/product/search/stores/e/elcorteingles_perfumeria/ES/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'ES',
     store: 'elcorteingles_perfumeria',
     domain: 'elcorteingles.es',
-    url: 'https://www.elcorteingles.es/search/1/?s={searchTerms}',
+    url: 'https://www.elcorteingles.es/perfumeria-general/search/1/?v={searchTerms}',
     loadedSelector: 'ul.c12.products_list._four',
     noResultsXPath: '//div[@class="products_list-container _no_products vp"] | //a[@class="product_detail-brand"]',
     zipcode: '',
