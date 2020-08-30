cae268b8c10a958d26a910f319c6325c42e64b49
Saurabh Ghewari <saurabhghewari@synerzip.com>
2020-08-30 15:17:12 +0530

    #506 Created new core extractor.

---
+++ src/library/navigation/goto/domains/ha/harveynorman.com.au/.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'harveynorman.com.au/',
+    timeout: null,
+    country: 'AU',
+    store: 'harveynorman',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/h/harveynorman/AU/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'AU',
+    store: 'harveynorman',
+    transform: null,
+    domain: 'harveynorman.com.au/',
+    zipcode: '',
+  },
+};
