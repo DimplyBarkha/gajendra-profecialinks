9f99ada5de7781021307b598baebc6f6bc671dda
sushma-mangalapally <sushma.mangalapally@import.io>
2020-08-30 17:59:13 -0700

    Add extractors for Sweden

--- src/library/product/search/stores/m/mediamarkt/SE/execute.js
+++ src/library/product/search/stores/m/mediamarkt/SE/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'SE',
     store: 'mediamarkt',
     domain: 'mediamarkt.se',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.mediamarkt.se/sv/search.html?query={searchTerms}&searchProfile=onlineshop&channel=mmsesv',
+    loadedSelector: 'div.product-wrapper',
+    noResultsXPath: '//div[contains(@id, "search_no_result")]',
     zipcode: '',
   },
 };
