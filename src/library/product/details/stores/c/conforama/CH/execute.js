75c609ed3ef1c4fb463ccc34779f84ba99b6ced1
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-08-31 21:33:08 +0530

    initial setup (#515)

---
+++ src/library/product/details/stores/c/conforama/CH/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'CH',
+    store: 'conforama',
+    domain: 'conforama.ch',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/c/conforama/CH/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'CH',
+    store: 'conforama',
+    transform: null,
+    domain: 'conforama.ch',
+    zipcode: '',
+  },
+};
