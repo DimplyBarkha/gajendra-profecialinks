29b5ac9eea30169f059952002be95a1b9e38499c
Abhisek <abhisek.d@osmosys.asia>
2020-08-31 14:43:06 +0530

    Added changes

--- src/library/product/details/stores/e/elcorteingles_electronica/ES/execute.js
+++ src/library/product/details/stores/e/elcorteingles_electronica/ES/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'ES',
     store: 'elcorteingles_electronica',
     domain: 'elcorteingles.es',
-    loadedSelector: 'a.product_detail-brand',
+    loadedSelector: 'div.js-sticky-control',
     noResultsXPath: '//div[contains(@class,"artwork image")]',
     zipcode: '',
   },
