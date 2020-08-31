e9adfa6934f0f0ec8b84c387fbe318fd69a9fe0d
Abhisek <abhisek.d@osmosys.asia>
2020-08-31 13:58:16 +0530

    Adding search

---
+++ src/library/product/search/stores/b/bedbathandbeyond/CA/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'CA',
+    store: 'bedbathandbeyond',
+    transform: null,
+    domain: 'bedbathandbeyond.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/b/bedbathandbeyond/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'bedbathandbeyond',
+    transform: null,
+    domain: 'bedbathandbeyond.com',
+    zipcode: '',
+  },
+};
