52bd6c88ced4f659d1afd714d4bbc1bd597b039d
Gauri Nerkar <gaurin@synerzipune.local>
2020-08-31 16:21:10 +0530

    #512 Added xpaths and required transform

---
+++ src/library/product/search/stores/e/elgiganten/SE/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'SE',
+    store: 'elgiganten',
+    domain: 'elgiganten.se',
+    url: 'https://www.elgiganten.se/search?SearchTerm={searchTerm}&search=&searchResultTab=',
+    loadedSelector: 'div.mini-product',
+    noResultsXPath: '//h2[contains(text(),"Tyvärr kunde vi inte hitta någonting som matchade din sökning")]',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/e/elgiganten/SE/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'SE',
+    store: 'elgiganten',
+    nextLinkSelector: '',
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: 'div.mini-product',
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'elgiganten.se',
+    zipcode: '',
+  },
+};
