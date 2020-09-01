aafdf030c81512a3c23269b61d4cbd0e28d40f0b
nehashende <neha.shende@synerzip.com>
2020-09-01 13:27:26 +0530

    #519. Initial commit for source Galaxus core.

---
+++ src/library/product/details/stores/g/galaxus/CH/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'galaxus.ch',
+    prefix: null,
+    url: null,
+    country: 'CH',
+    store: 'galaxus',
+    zipcode: '',
+  },
+};
