cf7cbc740051925d848d3c502fcaaa6bc7e5d68a
nikita.kumari <nikita.kumari@synerzip.com>
2020-09-01 14:39:55 +0530

    Xpaths and transforms #497

---
+++ src/library/product/details/stores/q/qvc/US/shared.js
@@ -0,0 +1,83 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.specifications) {
+        let text = '';
+        row.specifications.forEach((item, index) => {
+          text += item.text.trim() + ' || ';
+        });
+        row.specifications = [
+          {
+            text: text.slice(0, -3),
+          },
+        ];
+      }
+      if (row.alternateImages) {
+        row.alternateImages.forEach(item => {
+          item.text = item.text.replace(/(.*)/, 'https:$1');
+        });
+      }
+      if (row.image) {
+        row.image.forEach(item => {
+          item.text = item.text.replace(/(.*)/, 'https:$1');
+        });
+      }
+      if (row.mpc) {
+        row.mpc.forEach(item => {
+          item.text = item.text.replace(/.*"mpn":\["(.*?)"\].*/gm, '$1').trim();
+        });
+      }
+      if (row.description) {
+        row.description.forEach(item => {
+          item.text = item.text.replace(/\n/gm, '').replace(/\s{2,}/gm, ' ').trim();
+        });
+        if (row.additionalDescBulletInfo) {
+          for (const bullet of row.additionalDescBulletInfo) {
+            for (const item of row.description) {
+              item.text = item.text.replace(bullet.text, `|| ${bullet.text}`);
+            }
+          }
+        }
+      }
+      if (row.brandText) {
+        row.brandText.forEach(item => {
+          item.text = item.text.replace(/.*"brand_name":\["(.*?)"\].*/gm, '$1').trim();
+        });
+      }
+      if (row.manufacturerDescription) {
+        row.manufacturerDescription.forEach(item => {
+          item.text = item.text.replace(/\n/gm, '').replace(/\s{2,}/gm, ' ').trim();
+        });
+      }
+      if (row.variantCount) {
+        row.variantCount.forEach(item => {
+          // eslint-disable-next-line eqeqeq
+          if (item.text == 0) {
+            item.text = 1;
+          }
+        });
+      }
+      if (row.technicalInformationPdfPresent) {
+        row.technicalInformationPdfPresent.forEach(item => {
+          // eslint-disable-next-line eqeqeq
+          if (item.text != 'No') {
+            item.text = item.text.replace(/(.*)/, 'Yes');
+          }
+        });
+      }
+      if (row.nameExtended) {
+        row.nameExtended.forEach(item => {
+          item.text = item.text.trim();
+        });
+      }
+    }
+  }
+  return data;
+};
+
+module.exports = { transform };
