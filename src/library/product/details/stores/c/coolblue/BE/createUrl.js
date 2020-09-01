10618c0543a9a2ce3ea01504f0cf80dcde6556c6
Ankit Kumar <ankit.k@osmosys.asia>
2020-09-01 19:42:43 +0530

    added extractor coolblue BE core variant

---
+++ src/library/product/details/stores/c/coolblue/BE/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'coolblue.be',
+    prefix: null,
+    url: null,
+    country: 'BE',
+    store: 'coolblue',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/c/coolblue/BE/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'BE',
+    store: 'coolblue',
+    transform: null,
+    domain: 'coolblue.be',
+    zipcode: '',
+  },
+};
