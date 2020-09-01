c59454b243bfb9c1cd6959f7df11ceb67c63f41a
Pashya <pashya@profecialinks.com>
2020-09-01 23:32:06 +0530

    coolblue.nl

---
+++ src/library/product/search/stores/c/coolblue/NL/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'NL',
+    store: 'coolblue',
+    domain: 'coolblue.nl',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/c/coolblue/NL/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'NL',
+    store: 'coolblue',
+    transform: null,
+    domain: 'coolblue.nl',
+    zipcode: '',
+  },
+};
