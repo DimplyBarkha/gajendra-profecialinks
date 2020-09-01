234f86833b0a9062c37b127016abd8df6310a325
Mamatha Gundapaneni <mamatha.g@osmosys.asia>
2020-09-01 18:27:07 +0530

    Added shared file

--- src/library/product/details/stores/t/target/AU/extract.js
+++ src/library/product/details/stores/t/target/AU/extract.js
@@ -1,10 +1,12 @@
 
+const { transform } = require('./shared');
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AU',
     store: 'target',
-    transform: null,
+    transform,
     domain: 'target.com.au',
     zipcode: '',
   },
---
+++ src/library/product/details/stores/t/target/AU/shared.js
@@ -0,0 +1,66 @@
+
+/**
+*
+* @param {ImportIO.Group[]} data
+* @returns {ImportIO.Group[]}
+*/
+const transform = (data) => {
+  for (const { group } of data) {
+    for (const row of group) {
+
+      if (row.description) {
+        let text = '';
+        row.description.forEach(item => {
+          text = text + (text ? ' ' : '') + item.text;
+        });
+        row.description = [{ text }];
+      }
+
+      if (row.manufacturerDescription) {
+        let text = '';
+        row.manufacturerDescription.forEach(item => {
+          text = text + (text ? ' ' : '') + item.text;
+        });
+        row.manufacturerDescription = [{ text }];
+      }
+
+      if (row.brandText) {
+        let text = '';
+        row.brandText.forEach(item => {
+          text += item.text.replace(/\"/g, '');
+        });
+        row.brandText = [{
+          text: text,
+        },
+        ];
+      }
+
+      if ((!row.listPrice || !row.listPrice.length) && row.price) {
+        row.listPrice = row.price;
+      }
+    }
+  }
+
+  // Clean up data
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+    el.text = el.text.trim();
+  }))));
+
+  return data;
+};
+
+module.exports = { transform };
