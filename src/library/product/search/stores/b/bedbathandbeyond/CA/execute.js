9f37f3fa4c41b6154d1cffe59282fcc4c03bc546
Abhisek <abhisek.d@osmosys.asia>
2020-09-01 18:43:45 +0530

    added xapths

--- src/library/product/search/stores/b/bedbathandbeyond/CA/execute.js
+++ src/library/product/search/stores/b/bedbathandbeyond/CA/execute.js
@@ -5,9 +5,7 @@ module.exports = {
     country: 'CA',
     store: 'bedbathandbeyond',
     domain: 'bedbathandbeyond.com',
-    url: 'https://www.bedbathandbeyond.ca/store/s/{searchTerms}?ta=typeahead',
-    loadedSelector: 'section.productSearch div.grid-container .tealium-product-grid',
-    noResultsXPath: null,
+    url: 'https://www.bedbathandbeyond.ca/api/apps/bedbath/query/s1?web3feo=abc&start=72&q={searchTerms}&rows=24&site=BedBathCanada&wt=json&uids=null,666aac62-5b1c-4787-b84c-a4538ccf68ff&qlist=dysin,horm,dyson&currencyCode=USD&country=US&noFacet=true&rT=xtCompat&badge_ids=0000&isBrowser=true#[!opt!]{"type":"json"}[/!opt!]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/b/bedbathandbeyond/CA/paginate.js
+++ src/library/product/search/stores/b/bedbathandbeyond/CA/paginate.js
@@ -4,12 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'CA',
     store: 'bedbathandbeyond',
-    nextLinkSelector: null,
-    mutationSelector: null,
-    spinnerSelector: null,
-    loadedSelector: null,
-    noResultsXPath: null,
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      offset: 24,
+      template: 'https://www.bedbathandbeyond.ca/api/apps/bedbath/query/s1?web3feo=abc&start={offset}&q={searchTerms}&rows=24&site=BedBathCanada&wt=json&uids=null,666aac62-5b1c-4787-b84c-a4538ccf68ff&qlist=dysin,horm,dyson&currencyCode=USD&country=US&noFacet=true&rT=xtCompat&badge_ids=0000&isBrowser=true#[!opt!]{"type":"json"}[/!opt!]'
+    },
     domain: 'bedbathandbeyond.com',
     zipcode: '',
   },
