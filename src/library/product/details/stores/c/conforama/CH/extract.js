92eb95a9325f6ff072716beb7546e2740eece155
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-02 17:00:20 +0530

    Fields added(#515)

--- src/library/product/details/stores/c/conforama/CH/extract.js
+++ src/library/product/details/stores/c/conforama/CH/extract.js
@@ -1,11 +1,65 @@
-
+const { transform } = require('../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'CH',
     store: 'conforama',
-    transform: null,
+    transform,
     domain: 'conforama.ch',
     zipcode: '',
   },
+  implementation,
 };
+
+async function implementation (
+  // @ts-ignore
+  // @ts-ignore
+  // @ts-ignore
+  { parentInput },
+  parameters,
+  context,
+  dependencies,
+) {
+  // @ts-ignore
+  const { transform } = parameters;
+  // @ts-ignore
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async (parentInput) => {
+    let descriptionTab  = document.querySelector('h2[id="description"]');
+    // @ts-ignore
+    descriptionTab = descriptionTab ? descriptionTab.click() : '';
+    function addElementToDocument (key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+     let description = document.querySelector("div[id='tabs-1']");
+    // @ts-ignore
+    description = description ? description.innerHTML : '';
+    let descArr = [];
+    // @ts-ignore
+    if(description !== ''){
+      // @ts-ignore
+      description = description ? description.replace(/<li>/gm, ' || ').replace(/<.*?>/gm, '').replace(/\n/gm, ' ').replace(/•/gm, ' ||').replace(/\s{2,}/, ' ').replace('Description du produit','').replace(/La marque vous parle/g,'').trim() : '';
+    }
+    addElementToDocument('description',description);
+    function findJsonData (scriptSelector, startString, endString) {
+       
+      const xpath = `//script[contains(.,'${scriptSelector}')]`;
+      let element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      // @ts-ignore
+     let elementTxt = (element !== null) ? element.textContent : ''
+      return elementTxt;
+    };
+    let brandJson = findJsonData ('"brand": {','{ "@context":','}');
+    // @ts-ignore
+    brandJson = brandJson ? JSON.parse(brandJson).brand : '';
+    // @ts-ignore
+    let brandText = brandJson ? brandJson.name : '';
+    addElementToDocument('brandText',brandText);
+    });
+    return await context.extract(productDetails, { transform });
+    }
---
+++ src/library/product/details/stores/c/conforama/shared.js
@@ -0,0 +1,35 @@
+/**
+*
+* @param {ImportIO.Group[]} data
+* @returns {ImportIO.Group[]}
+*/
+const transform = (data, context) => {
+    for (const { group } of data) {
+      for (const row of group) {
+        if (row.specifications) {
+          let text = '';
+          row.specifications.forEach(item => {
+            text += `${item.text.replace(/\n \n/g, ' : ')} || `;
+          });
+          row.specifications = [
+            {
+              text: text,
+            },
+          ];
+        }
+        if (row.image) {
+          row.image.forEach(item => {
+            if(item.text.includes('https:')){
+             item.text = item.text;
+            }else{
+             item.text = 'https:'+item.text; 
+            }
+          });
+        }
+      }
+    }
+    return data;
+  };
+  
+  module.exports = { transform };
+  
