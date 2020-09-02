4fcc68f4d867423dfa9cb1fd5baa812149041845
Ankit Kumar <ankit.k@osmosys.asia>
2020-09-02 15:21:09 +0530

    added script to handle products with no variants

--- src/library/product/details/stores/c/coolblue/BE/variantsExtract.js
+++ src/library/product/details/stores/c/coolblue/BE/variantsExtract.js
@@ -8,4 +8,27 @@ module.exports = {
     domain: 'coolblue.be',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { variants }) => {
+    await context.evaluate(async function () {
+      //  if no variants available , pull sku and product page url
+
+      //  Setting SKU
+      let sku = document.querySelector('main#main-content').getAttribute('data-product-id');
+      const url = document.querySelector('head > link:nth-child(7)').href;
+      const variantAvailable = document.querySelector('div.selectable-card input');
+      if (!variantAvailable && sku) {
+        console.log('variants not available, appending SKU');
+        sku = `collection-item-${sku}`;
+        const div = document.createElement('div');
+        div.className = 'selectable-card';
+        const getInput = document.createElement('input');
+        getInput.id = sku;
+        div.appendChild(getInput);
+        document.body.appendChild(div);
+        getInput.setAttribute('value', url);
+        console.log('variants not available________appended SKU');
+      }
+    });
+    await context.extract(variants);
+  },
 };
