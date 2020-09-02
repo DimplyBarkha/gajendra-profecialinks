86fbac73a2e9a7d55f022304e3c20c4704c2f563
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-02 11:02:44 +0530

    Updated extract.yaml for rank and rankOrganic

--- src/library/product/search/stores/p/placedestendances/FR/extract.js
+++ src/library/product/search/stores/p/placedestendances/FR/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'FR',
     store: 'placedestendances',
-    transform: null,
+    transform: transform,
     domain: 'placedestendances.com',
     zipcode: '',
   },
