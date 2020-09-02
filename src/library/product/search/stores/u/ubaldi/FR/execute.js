452663cabf54c40a19a6a73a834b098ed1bc4bfb
Vivek Gondil <gondilvivek@gmail.com>
2020-09-02 19:43:15 +0530

    Coded custum implimentation and xpaths, deployed

--- src/library/product/search/stores/u/ubaldi/FR/execute.js
+++ src/library/product/search/stores/u/ubaldi/FR/execute.js
@@ -1,3 +1,28 @@
+async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
+) {
+    console.log('params', parameters);
+    const inputKeyWord = inputs.keywords.replace(' ', '-');
+    const url = parameters.url.replace('{searchTerms}', inputKeyWord);
+    await dependencies.goto({ url, zipcode: inputs.zipcode });
+    if (parameters.loadedSelector) {
+        await context.waitForFunction(function(sel, xp) {
+            return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+        }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    }
+    console.log('Checking no results', parameters.noResultsXPath);
+    return await context.evaluate(function(xp) {
+        const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+        console.log(xp, r);
+        const e = r.iterateNext();
+        console.log(e);
+        return !e;
+    }, parameters.noResultsXPath);
+}
+
 module.exports = {
     implements: 'product/search/execute',
     parameterValues: {
@@ -9,4 +34,5 @@ module.exports = {
         noResultsXPath: 'div.recherche-vide',
         zipcode: '',
     },
+    implementation,
 };
--- src/library/product/search/stores/u/ubaldi/FR/paginate.js
+++ src/library/product/search/stores/u/ubaldi/FR/paginate.js
@@ -1,16 +1,67 @@
+async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
+) {
+    const { keywords, page, offset } = inputs;
+    const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
+    const { pager } = dependencies;
 
+    const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
+    if (success) {
+        return true;
+    }
+
+    let url = await context.evaluate(function() {
+        /** @type { HTMLLinkElement } */
+        const next = document.querySelector('head link[rel="next"]');
+        if (!next) {
+            return false;
+        }
+        return next.href;
+    });
+
+    if (!url) {
+        return false;
+    }
+
+    console.log('Going to url', url);
+    await dependencies.goto({ url });
+    if (loadedSelector) {
+
+        await context.waitForSelector('#scroll_loading_point_up');
+        console.log("Removing");
+        await context.evaluate(() => {
+            document.getElementById('scroll_loading_point_up').remove();
+        })
+        await context.waitForFunction(function(sel, xp) {
+            return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+        }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+    }
+
+    console.log('Checking no results', noResultsXPath);
+    return await context.evaluate(function(xp) {
+        const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+        console.log(xp, r);
+        const e = r.iterateNext();
+        console.log(e);
+        return !e;
+    }, noResultsXPath);
+}
 module.exports = {
-  implements: 'product/search/paginate',
-  parameterValues: {
-    country: 'FR',
-    store: 'ubaldi',
-    nextLinkSelector: null,
-    mutationSelector: null,
-    spinnerSelector: null,
-    loadedSelector: null,
-    noResultsXPath: null,
-    openSearchDefinition: null,
-    domain: 'ubaldi.com',
-    zipcode: '',
-  },
+    implements: 'product/search/paginate',
+    parameterValues: {
+        country: 'FR',
+        store: 'ubaldi',
+        nextLinkSelector: null,
+        mutationSelector: null,
+        spinnerSelector: null,
+        loadedSelector: null,
+        noResultsXPath: '//div[@class="recherche-vide"]',
+        openSearchDefinition: null,
+        domain: 'ubaldi.com',
+        zipcode: '',
+    },
+
 };
