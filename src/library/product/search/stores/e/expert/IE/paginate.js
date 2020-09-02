5040d32218b09d3e399e5df6d24e956980c5ac31
sushma-mangalapally <sushma.mangalapally@import.io>
2020-09-02 10:42:31 -0700

    Add changes for paginate

--- src/library/product/search/stores/e/expert/IE/paginate.js
+++ src/library/product/search/stores/e/expert/IE/paginate.js
@@ -1,18 +1,90 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { keywords, page, offset } = inputs;
+  const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
 
+  if (nextLinkSelector) {
+    const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector), nextLinkSelector);
+    if (!hasNextLink) {
+      return false;
+    }
+  } else {
+    const foundNextButton = await context.evaluate(function () {
+      const hiddenNextButton = document.querySelector('li.pages-item-next a[title="Next"]');
+      if (hiddenNextButton) {
+        hiddenNextButton.click();
+        return true;
+      }
+    });
+
+    if (foundNextButton) {
+      return true;
+    }
+    return false;
+  }
+
+  const { pager } = dependencies;
+  const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
+  if (success) {
+    return true;
+  }
+
+  let url = await context.evaluate(function () {
+    /** @type { HTMLLinkElement } */
+    const next = document.querySelector('head link[rel="next"]');
+    if (!next) {
+      return false;
+    }
+    return next.href;
+  });
+
+  if (!url && openSearchDefinition) {
+    url = openSearchDefinition.template
+      .replace('{searchTerms}', encodeURIComponent(keywords))
+      .replace('{page}', (page + (openSearchDefinition.pageOffset || 0)).toString())
+      .replace('{offset}', (offset + (openSearchDefinition.indexOffset || 0)).toString());
+  }
+
+  if (!url) {
+    return false;
+  }
+
+  console.log('Going to url', url);
+  await dependencies.goto({ url });
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'IE',
     store: 'expert',
+    // nextLinkSelector: 'li.pages-item-next a[title="Next"]',
     nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div.product-item-info',
     noResultsXPath: '//*[contains(text(), "no results")]',
-    openSearchDefinition: {
-      template: 'https://www.expert.ie/catalogsearch/result/index/?p={page}&q={searchTerms}'
-    },
+    // openSearchDefinition: {
+    //   template: 'https://www.expert.ie/catalogsearch/result/index/?p={page}&q={searchTerms}'
+    // },
     domain: 'expert.ie',
     zipcode: '',
   },
+  implementation,
 };
