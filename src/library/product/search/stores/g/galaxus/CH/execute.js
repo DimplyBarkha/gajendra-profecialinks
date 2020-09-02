5a66f82bf12c35706d903d4af02fbe91dd60109a
nehashende <neha.shende@synerzip.com>
2020-09-02 21:52:20 +0530

    #523. Tried error 403 solving methods.

--- src/library/product/search/stores/g/galaxus/CH/execute.js
+++ src/library/product/search/stores/g/galaxus/CH/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'CH',
     store: 'galaxus',
     domain: 'galaxus.ch',
-    url: 'https://www.galaxus.ch/Search?q={searchTerms}',
-    loadedSelector: 'article.panelProduct a.styled__StyledLink-sc-5q25ol-0',
-    noResultsXPath: "//h2[contains(@class, 'ZZ5g')] = 'Nothing found for'",
+    url: 'https://www.galaxus.ch/de/search?q={searchTerms}',
+    loadedSelector: null,//'article',
+    noResultsXPath: null,//"//h2[contains(@class, 'ZZ5g')] = 'Nothing found for'",
     zipcode: '',
   },
 };
