74c90c0f6919c8849f14b628465cfbcbb420b0c2
Pashya <pashya@profecialinks.com>
2020-09-03 03:37:34 +0530

    CoolBlue.nl variants code for extractor

---
+++ src/library/product/details/stores/c/coolblue/NL/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'NL',
+    store: 'coolblue',
+    transform: null,
+    domain: 'coolblue.nl',
+    zipcode: '',
+  },
+};
