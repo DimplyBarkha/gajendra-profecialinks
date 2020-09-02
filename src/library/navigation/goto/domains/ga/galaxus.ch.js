f637abe49d063a945d8c96a3b3fefa58ec217444
nehashende <neha.shende@synerzip.com>
2020-09-02 12:23:56 +0530

    #523. Initial commit for source Galaxus search.

---
+++ src/library/navigation/goto/domains/ga/galaxus.ch.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'galaxus.ch',
+    timeout: null,
+    country: 'CH',
+    store: 'galaxus',
+    zipcode: '',
+  },
+};
