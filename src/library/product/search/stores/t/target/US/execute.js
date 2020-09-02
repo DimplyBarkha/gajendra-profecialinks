1a0c08d2084abf31b40176520007a5bc844363af
Mamatha Gundapaneni <mamatha.g@osmosys.asia>
2020-09-02 18:40:06 +0530

    Added no results xpath field

--- src/library/product/search/stores/t/target/US/execute.js
+++ src/library/product/search/stores/t/target/US/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'target.com',
     url: 'https://www.target.com/s?searchTerm={searchTerms}',
     loadedSelector: 'div[data-test="productGridContainer"] li:nth-last-child(1)',
-    noResultsXPath: '//h1[contains(.,"no results found")]',
+    noResultsXPath: '//h1[contains(.,"no results found")] | //img[@alt="no_or_low_results_icon"]',
     zipcode: '',
   },
 };
