2a57f724b63cf4ea7235d3895583a125fe74d89c
Karolina <karolina.urbanska@tdcm.io>
2020-09-02 13:46:49 +0200

    Improved some fileds and generated tests

--- src/library/product/details/stores/b/bellaffair/AT/extract.js
+++ src/library/product/details/stores/b/bellaffair/AT/extract.js
@@ -23,6 +23,22 @@ module.exports = {
       if (pdfPresent) {
         addElementToDocument('pdfPresent', 'Yes');
       } else addElementToDocument('pdfPresent', 'No');
+      const aggregateRating = document.querySelector('meta[itemprop="ratingValue"]')
+        ? document.querySelector('meta[itemprop="ratingValue"]').getAttribute('content').replace(/\./g, ',') : '';
+      if (aggregateRating) addElementToDocument('aggregateRating', aggregateRating);
+      const description = document.querySelector('div.description div.left')
+        // @ts-ignore
+        ? document.querySelector('div.description div.left').innerText.replace(/\n/g, ' ') : '';
+      if (description) addElementToDocument('description', description);
+      const manufacturerDescription = document.querySelector('div[itemprop="description"]')
+        ? document.querySelector('div[itemprop="description"]')
+          // @ts-ignore
+          .innerText.replace(/\n{2,}/g, '\n') : '';
+      if (manufacturerDescription) addElementToDocument('manufacturerDescription', manufacturerDescription);
+      if (description) addElementToDocument('description', description);
+      const shippingInfoXpath = document.evaluate('//div[@class="block third-block"][p[contains(text(),"Versandkosten")]]', document, null, XPathResult.STRING_TYPE, null);
+      const shippingInfo = shippingInfoXpath ? shippingInfoXpath.stringValue.replace(/\n{2,}/g, '\n') : '';
+      if (shippingInfo) addElementToDocument('shippingInfo', shippingInfo);
     });
     await context.extract(productDetails);
   },
