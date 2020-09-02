5ac90cbf5d6aa7268c86b7a373e327d8380a374c
sourabhk <sourabh.karmarkar@synerzip.com>
2020-09-02 18:30:36 +0530

    #518 Added load more product results logic

--- src/library/product/search/stores/s/schubiger-online/CH/extract.js
+++ src/library/product/search/stores/s/schubiger-online/CH/extract.js
@@ -9,12 +9,33 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+  let { results } = inputs;
+  results = results || 150;
 
   // If cookie pop up appears then clicking on accept button
   await context.evaluate(async function () {
     const cookieButtonSelector = document.querySelector('button[class*="accept-all-btn"]');
     cookieButtonSelector && cookieButtonSelector.click();
   });
+
+  // Clicking on load more button until the specified number of results are loaded
+  async function loadResults (results) {
+    await context.evaluate(async function (results) {
+      let loadMoreButtonSelector = document.querySelector('div[class*="load-more-button"] a[role="button"]');
+      while (loadMoreButtonSelector) {
+        loadMoreButtonSelector.click();
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        loadMoreButtonSelector = document.querySelector('div[class*="load-more-button"] a[role="button"]');
+        // Check if number of products exceeds the results value
+        const productLengthSelector = document.querySelectorAll('div[class*="c-product-grid__item-wrapper"] div[class*="grid__item"]');
+        const numberOfProducts = productLengthSelector.length;
+        if (numberOfProducts >= results) {
+          break;
+        }
+      }
+    }, results);
+  }
+  await loadResults(results);
 
   await context.evaluate(async function () {
     function addHiddenDiv (id, content) {
