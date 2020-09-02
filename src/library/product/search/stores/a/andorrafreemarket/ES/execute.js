abf704013cf2da6d610653aa840ffd26f58bb5ad
nirmal-gspann <nirmal.genji@gspann.com>
2020-09-02 13:53:30 +0530

    Search for Andorra Free Market ES

---
+++ src/library/product/search/stores/a/andorrafreemarket/ES/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'andorrafreemarket',
+    domain: 'andorrafreemarket.com',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/a/andorrafreemarket/ES/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'ES',
+    store: 'andorrafreemarket',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'andorrafreemarket.com',
+    zipcode: '',
+  },
+};
