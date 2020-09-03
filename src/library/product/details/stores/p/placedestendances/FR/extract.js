fda73a30cce3bf47972c7d3ee6931f0f443b30c2
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-03 10:40:01 +0530

    Updated extract.yaml

--- src/library/product/details/stores/p/placedestendances/FR/extract.js
+++ src/library/product/details/stores/p/placedestendances/FR/extract.js
@@ -8,4 +8,16 @@ module.exports = {
     domain: 'placedestendances.com/fr/fr',
     zipcode: '',
   },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(async function () {
+      await new Promise(r=>setTimeout(r,8000));
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+  },
 };
