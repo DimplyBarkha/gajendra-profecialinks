dc7c997a0f7b20cf6dc6842ba8ea454061161243
GirishSolanki <girish.solanki@kcsitglobal.com>
2020-09-03 21:58:38 +0530

    pushed changes to appliancesonline

--- src/library/product/search/stores/a/appliancesonline/AU/paginate.js
+++ src/library/product/search/stores/a/appliancesonline/AU/paginate.js
@@ -4,8 +4,11 @@ module.exports = {
     country: 'AU',
     store: 'appliancesonline',
     domain: 'appliancesonline.com.au',
-    //nextLinkSelector: 'div.pagination > pagination-template > ul > li.ng-star-inserted>a.next',
-    nextLinkSelector: 'div.pagination > pagination-template > ul > li.ng-star-inserted:not([class*="active"]):last-child > a.ng-star-inserted',
+    nextLinkSelector: null,
+    //nextLinkSelector: 'div.pagination > pagination-template > ul > li.ng-star-inserted:not([class*="active"]):last-child > a.ng-star-inserted',
     loadedSelector: 'div.grid-container-flex',
+    openSearchDefinition: {
+      template: 'https://www.appliancesonline.com.au/search/{searchTerms}?currentpage={page}&sortkey=highestrated',
+    },
   },
 };
