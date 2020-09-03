05f39bd15b1d661e72c0461fb3cfa0ebe8335be1
nehashende <neha.shende@synerzip.com>
2020-09-03 20:35:06 +0530

    #529. Initial commit for source Newegg search.

---
+++ src/library/navigation/goto/domains/ne/setZipCode.newegg.com.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'US',
+    domain: 'newegg.com',
+    store: 'newegg',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/n/newegg/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'newegg',
+    transform: null,
+    domain: 'newegg.com',
+    zipcode: '',
+  },
+};
