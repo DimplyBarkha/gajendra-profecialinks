f68ec6fb91f948ac59c078004895e138f8bd4c2a
Prasanna Barate <prasanna.barate@synerzip.com>
2020-09-03 15:33:02 +0530

    Added timeout for search page.

--- src/library/product/search/stores/e/emax/AE/extract.js
+++ src/library/product/search/stores/e/emax/AE/extract.js
@@ -9,4 +9,8 @@ module.exports = {
     domain: 'emaxme.com',
     zipcode: '',
   },
+  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+    await new Promise(resolve => setTimeout(resolve, 10000))
+    return await context.extract(productDetails, { transform });
+  },
 };
