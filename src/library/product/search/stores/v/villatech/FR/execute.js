7779395a6e0bdfeda2afa2494829ccf909e0f469
nikita.kumari <nikita.kumari@synerzip.com>
2020-09-03 12:33:41 +0530

    Updated noresultxpaths

--- src/library/product/search/stores/v/villatech/FR/execute.js
+++ src/library/product/search/stores/v/villatech/FR/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'villatech.fr',
     url: 'https://www.villatech.fr/catalogsearch/result/?q={searchTerms}',
     loadedSelector: 'section.products-list div.product-list-col',
-    noResultsXPath: '//div[@class="results"]//div[contains(text(),"Aucun résultat trouvé")]',
+    noResultsXPath: '//div[@class="results"]//div[contains(text(),"Aucun résultat trouvé")] | //article[@class="product"]',
     zipcode: '',
   },
 };
