4d9252bacc7dc859914be8882f026e3050d02401
nehashende <neha.shende@synerzip.com>
2020-09-03 15:26:42 +0530

    #430. Manual QA issues fixed.

--- src/library/navigation/goto/domains/ar/arnotts.ie.js
+++ src/library/navigation/goto/domains/ar/arnotts.ie.js
@@ -8,4 +8,9 @@ module.exports = {
     store: 'arnotts',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    await context.setBlockAds(false);
+    const timeout = parameters.timeout ? parameters.timeout : 30000;
+    await context.goto(url, { first_request_timeout: 60000, anti_fingerprint: true, timeout, waitUntil: 'load', checkBlocked: true });
+  },
 };
