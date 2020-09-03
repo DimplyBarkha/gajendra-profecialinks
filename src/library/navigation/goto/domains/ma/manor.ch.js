65ec8f426c4481a74b45d41bafb59d8abf052eb5
Gajender Ramola <ramola106@gmail.com>
2020-09-03 19:45:41 +0530

    committed manor.ch details extractor

---
+++ src/library/navigation/goto/domains/ma/manor.ch.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'manor.ch',
+    timeout: 100000,
+    country: 'CH',
+    store: 'manor',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/details/stores/m/manor/transform.js
@@ -0,0 +1,67 @@
+
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+    for (const { group } of data) {
+      for (const row of group) {        
+        if (row.color) {
+          row.color.forEach(item => {
+            var myRegexp = /Farbe\s*:\s*(.+)/g;
+            var match = myRegexp.exec(item.text);
+            if(match){
+            if(match.length){
+            item.text = match[1].trim();
+            }else{
+            item.text = "";
+            }
+            }
+          });        
+        }
+        if (row.description) {
+          row.description.forEach(item => {
+            item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
+          });
+        }
+        if (row.brandLink) {
+          row.brandLink.forEach(item => {
+            item.text = 'https://www.manor.ch'+item.text
+          });
+        }        
+        if (row.alternateImages) {
+            row.alternateImages.forEach(item => {
+              item.text = 'https://www.manor.ch'+item.text
+            });
+          }
+
+        if (row.manufacturerImages) {
+          row.manufacturerImages.forEach(item => {
+            item.text = 'https://www.manor.ch'+item.text
+          });
+        }
+        if (row.image) {
+          row.image.forEach(item => {
+            item.text = 'https://www.manor.ch'+item.text
+          });
+        }
+        if (row.warranty) {
+          row.warranty.forEach(item => {
+            item.text = item.text.replace(/Garantie/ig, ' ').trim();
+          });
+        }
+        if (row.variants) {
+          let info = [];
+          row.variants.forEach(item => {
+            info.push(item.text.replace(/(.+\/)/g, '').trim());
+          });
+          row.variants = [{'text':info.join(' | '),'xpath':row.variants[0].xpath}];        
+        }        
+      }
+    }
+    return data;
+  };
+  
+  module.exports = { transform };
+  
