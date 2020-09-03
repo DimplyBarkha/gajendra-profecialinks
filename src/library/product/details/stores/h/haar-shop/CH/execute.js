c736f607f06a0a420fa1f571728d6cb655ec29b5
Nandakishor Valakunde <nvalakunde@gmail.com>
2020-09-03 23:19:19 +0530

    Initial Commit
    Initial Commit

---
+++ src/library/product/details/stores/h/haar-shop/CH/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'CH',
+    store: 'haar-shop',
+    domain: 'haar-shop.ch',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
