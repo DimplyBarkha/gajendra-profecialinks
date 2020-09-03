536773868c0edce84871de4186b71fae18237b7a
Saurabh Ghewari <saurabhghewari@synerzip.com>
2020-09-03 15:20:48 +0530

    #526 Added loadedSelector and tested on by setting proxy on local machine.

---
+++ src/library/product/details/stores/f/fnac/ES/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'fnac',
+    domain: 'fnac.es',
+    loadedSelector: 'div.productPageTop',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/f/fnac/ES/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'ES',
+    store: 'fnac',
+    transform: null,
+    domain: 'fnac.es',
+    zipcode: '',
+  },
+};
