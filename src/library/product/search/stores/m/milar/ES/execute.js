a61b3cfdbde72424e647fc7becddadfa24f9986d
jignesh013 <jignesh@profecialinks.com>
2020-09-03 19:59:02 +0530

    Fixing the dynamic search issue

--- src/library/product/search/stores/m/milar/ES/execute.js
+++ src/library/product/search/stores/m/milar/ES/execute.js
@@ -1,13 +1,52 @@
 
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters, inputs);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
+  await context.evaluate(function (inputs) {
+    document.querySelector('input#txtBuscador').value = inputs.keywords;
+    document.querySelector('form.form-search').submit();
+  }, inputs);
+  context.waitForNavigation();
+  await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+}
+
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
     country: 'ES',
     store: 'milar',
     domain: 'milar.es',
-    url: 'https://www.milar.es/tablet-samsung-galaxy-tab-a19-32gb-silve.html',
+    url: 'https://www.milar.es',
     loadedSelector: 'div#df-results__dfclassic',
     noResultsXPath: '//p[contains(text(),"No Records")]',
     zipcode: '',
   },
+  implementation,
 };
