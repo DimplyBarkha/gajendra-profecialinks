e90898d9e8a47ab917641b6efe2b5b4853267496
GirishSolanki <girish.solanki@kcsitglobal.com>
2020-09-03 15:44:27 +0530

    pushed changes behalf of jatin

--- src/library/navigation/goto/domains/ap/appliancesonline.com.au.js
+++ src/library/navigation/goto/domains/ap/appliancesonline.com.au.js
@@ -3,6 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'appliancesonline.com.au',
+    timeout: 50000,
     country: 'AU',
     store: 'appliancesonline',
     zipcode: '',
--- src/library/product/search/stores/a/appliancesonline/AU/paginate.js
+++ src/library/product/search/stores/a/appliancesonline/AU/paginate.js
@@ -4,9 +4,8 @@ module.exports = {
     country: 'AU',
     store: 'appliancesonline',
     domain: 'appliancesonline.com.au',
-    nextLinkSelector: 'div.pagination > pagination-template > ul > li.next',
-    mutationSelector: null,
-    spinnerSelector: null,
+    //nextLinkSelector: 'div.pagination > pagination-template > ul > li.ng-star-inserted>a.next',
+    nextLinkSelector: 'div.pagination > pagination-template > ul > li.ng-star-inserted:not([class*="active"]):last-child > a.ng-star-inserted',
     loadedSelector: 'div.grid-container-flex',
   },
 };
