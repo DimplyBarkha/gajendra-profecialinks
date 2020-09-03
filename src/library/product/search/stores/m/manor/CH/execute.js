c09fac11488c99c982ad36105100e9603d309689
Gajender Ramola <ramola106@gmail.com>
2020-09-03 23:34:48 +0530

    committed for search

---
+++ src/library/product/search/stores/m/manor/CH/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'CH',
+    store: 'manor',
+    domain: 'manor.ch',
+    url: 'https://www.manor.ch/search/text#/q/{searchTerms}',
+    loadedSelector: 'div#epoq_resultrow',
+    noResultsXPath: '//span[contains(text(),"Leider konnten wir kein Ergebnis für Sie finden.")]',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/search/stores/m/manor/CH/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'CH',
+    store: 'manor',
+    transform: null,
+    domain: 'manor.ch',
+    zipcode: "''",
+  },
+};
