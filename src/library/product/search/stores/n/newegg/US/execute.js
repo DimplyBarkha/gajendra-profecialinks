a576b470759d3e9f957bfcbcaa73fe08b5dbe0ba
nehashende <neha.shende@synerzip.com>
2020-09-03 20:55:08 +0530

    #529. Added extractor logic.

--- src/library/product/search/stores/n/newegg/US/execute.js
+++ src/library/product/search/stores/n/newegg/US/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'US',
     store: 'newegg',
     domain: 'newegg.com',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.newegg.com/p/pl?d={searchTerms}',
+    loadedSelector: 'div.items-grid-view > div > div > a img',
+    noResultsXPath: '//span[@class="result-message-error"]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/n/newegg/US/extract.js
+++ src/library/product/search/stores/n/newegg/US/extract.js
@@ -1,11 +1,26 @@
-
+const { transform } = require('./transform');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'newegg',
-    transform: null,
+    transform,
     domain: 'newegg.com',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, store, transform: transformParam }, context, dependencies) => {
+    async function addUrl () {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+      const url = window.location.href;
+      addHiddenDiv('added-searchurl', url);
+    }
+    await context.evaluate(addUrl);
+    return await context.extract(dependencies.productDetails, { transform: transformParam });
   },
 };
