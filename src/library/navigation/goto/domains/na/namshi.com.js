dfac3c9e28cd7bd826b280d17181b98c19701bfe
jeet kothiyal (jeet) <jeetkothiyal@gmail.com>
2020-09-03 22:58:09 +0530

    DS-Core-Namshi_AE

---
+++ src/library/navigation/goto/domains/na/namshi.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'namshi.com',
+    timeout: null,
+    country: 'AE',
+    store: 'namshi',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/details/stores/n/namshi/AE/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'AE',
+    store: 'namshi',
+    domain: 'namshi.com',
+    zipcode: "''",
+  },
+};
