9fc0076ba6f3f12edc19b579284350a5be63a690
nehashende <neha.shende@synerzip.com>
2020-09-04 14:18:28 +0530

    #535. Initial commit for source Noel Leeming core.

---
+++ src/library/product/details/stores/n/noelleeming/NZ/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'noelleeming.co.nz',
+    prefix: null,
+    url: null,
+    country: 'NZ',
+    store: 'noelleeming',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/n/noelleeming/NZ/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'NZ',
+    store: 'noelleeming',
+    transform: null,
+    domain: 'noelleeming.co.nz',
+    zipcode: '',
+  },
+};
