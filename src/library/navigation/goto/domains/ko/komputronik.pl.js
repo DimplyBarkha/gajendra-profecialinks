06423743a1b13447f2a5580d654bb91ea3ca7333
sivasoarlogic <siva@soarlogic.com>
2020-09-04 14:10:32 +0530

    DS-Core-komputronik_PL
    rebase

---
+++ src/library/navigation/goto/domains/ko/komputronik.pl.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'komputronik.pl',
+    timeout: 100000,
+    country: 'PL',
+    store: 'komputronik',
+    zipcode: "''",
+  },
+};
