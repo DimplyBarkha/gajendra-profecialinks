108e04899322d929604a3f8cf054616e4024d077
kcs-rudra-bhatt <58464196+kcs-rudra-bhatt@users.noreply.github.com>
2020-09-04 14:37:43 +0530

    Changes made for Gigantti.fi website - Core

---
+++ src/library/product/details/stores/g/gigantti/FI/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'gigantti.fi',
+    prefix: null,
+    url: null,
+    country: 'FI',
+    store: 'gigantti',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/g/gigantti/FI/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'FI',
+    store: 'gigantti',
+    transform: null,
+    domain: 'gigantti.fi',
+    zipcode: '',
+  },
+};
