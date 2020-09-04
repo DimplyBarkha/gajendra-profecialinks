13041aaaf22b959453374e14e151656de453b903
Prasanna Barate <prasanna.barate@synerzip.com>
2020-09-04 19:50:06 +0530

    Fixed secondary image issue.

--- src/library/product/details/stores/e/emax/AE/format.js
+++ src/library/product/details/stores/e/emax/AE/format.js
@@ -6,26 +6,6 @@
 const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
-      // if (row.image) {
-      //   row.image = row.image.slice(0, 1);
-      // }
-      // if (row.alternateImages) {
-      //   row.alternateImages = row.alternateImages.map((ele) => {
-      //     ele.text = ele.text.replace('90x90', '700x700');
-      //     return ele;
-      //   });
-      //   row.alternateImages.shift();
-      // }
-      // if (row.largeImageCount) {
-      //   row.largeImageCount = [
-      //     {
-      //       text: row.alternateImages.length,
-      //     },
-      //   ];
-      // }
-      // if (row.price) {
-      //   row.price = row.price.slice(0, 1);
-      // }
       if (row.brandText) {
         let firstWord = row.nameExtended[0].text;
         firstWord = firstWord.split(' ');
@@ -35,15 +15,18 @@ const transform = (data) => {
           },
         ];
       }
+      if (row.alternateImages) {
+        const enlargePrefix = row.alternateImagesEnlargePath && row.alternateImagesEnlargePath[0].text ? row.alternateImagesEnlargePath[0].text : ''
+        row.alternateImages.forEach(item => {
+          item.text = enlargePrefix ? enlargePrefix + item.text.replace(/(.*\/)(.*\/.*\/.*)/, '$2') : item.text
+        });
+      }
       if (row.description) {
         row.description[0].text = row.description[0].text.replace(/\n \n/g, '').replace(/\n /g, ' ');
       }
       if (row.additionalDescBulletInfo) {
         row.additionalDescBulletInfo[0].text = row.additionalDescBulletInfo[0].text.replace(/\n \n/g, ' || ');
       }
-      // if (row.descriptionBullets) {
-      //   row.descriptionBullets[0].text = (row.additionalDescBulletInfo[0].text.split('||').length - 1) + 1;
-      // }
       if (row.firstVariant) {
         row.firstVariant[0].text = row.firstVariant[0].text.replace(/\n/g, ' ');
       }
@@ -51,16 +34,11 @@ const transform = (data) => {
         row.specifications.forEach(item => {
           item.text = item.text.replace('More Information \n \n \n', '').replace(/\n \n \n \n/g, ' || ').replace(/\n \n/g, ' : ');
         });
-        // row.specifications[0].text = row.specifications[0].text.replace('More Information \n \n \n', '');
-        // row.specifications[0].text = row.specifications[0].text.replace(/\n \n \n \n/g, ' || ');
-        // row.specifications[0].text = row.specifications[0].text.replace(/\n \n/g, ' : ');
       }
       if (row.warranty) {
         row.warranty.forEach(item => {
           item.text = item.text.replace(/\n/g, ' | ');
         });
-        // row.warranty[0].text = row.warranty[0].text.replace('OEM Warrenty \n', '');
-        // row.warranty[0].text = row.warranty[0].text.replace(/\n/g, ' | ');
       }
       if (row.availabilityText) {
         row.availabilityText = row.availabilityText.length ? [{ text: 'In Stock' }] : [{ text: 'Out of Stock' }];
