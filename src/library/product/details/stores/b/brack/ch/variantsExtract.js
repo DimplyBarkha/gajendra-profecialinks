a3e9d8560e74ce17f5af6712c15a9b307d51f42a
Aditi-ux <aditi@profecialinks.com>
2020-09-05 00:44:34 +0530

    Extraction Variants Brack.ch

---
+++ src/library/product/details/stores/b/brack/ch/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'CH',
+    store: 'brack',
+    transform: null,
+    domain: 'brack.ch',
+    zipcode: "''",
+  },
+};
