7c4a20cc200866a4da854e29f329d3c22c1e2a11
GirishSolanki <girish.solanki@kcsitglobal.com>
2020-09-04 16:01:07 +0530

    pushed changes to dev branch

--- src/library/product/search/stores/d/davidjones/AU/extract.js
+++ src/library/product/search/stores/d/davidjones/AU/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'AU',
     store: 'davidjones',
-    transform: null,
+    transform: transform,
     domain: 'davidjones.com',
     zipcode: '',
   },
--- src/library/product/search/stores/d/davidjones/AU/paginate.js
+++ src/library/product/search/stores/d/davidjones/AU/paginate.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'AU',
     store: 'davidjones',
-    nextLinkSelector: 'button.sli_load_more_button',    
+    nextLinkSelector: 'div.sli_infinite_wrapper > button.sli_load_more_button',    
     loadedSelector: '#page-content',
     domain: 'davidjones.com',
     zipcode: '',
