76a93f6684ac90b8c4dd258b026c924c573234bd
Satyam <satyam.rathod@synerzip.com>
2020-09-04 13:38:52 +0530

    #532 updated review regex

--- src/library/product/search/stores/b/bestbuy/US/execute.js
+++ src/library/product/search/stores/b/bestbuy/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'bestbuy',
     domain: 'bestbuy.com',
-    url: 'https://www.bestbuy.com/site/searchpage.jsp?st={searchTerms}',
+    url: 'https://www.bestbuy.com/site/searchpage.jsp?st={searchTerms}&intl=nosplash',
     loadedSelector: 'li.sku-item[data-sku-id]',
     noResultsXPath: '//h3[@class="no-results-message"]',
     zipcode: '',
