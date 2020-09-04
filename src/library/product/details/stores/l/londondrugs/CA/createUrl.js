d321a2f872ba89fdc4a8be0a0ee880e7fa67ce14
Aakash Sheoran <akash.s@osmosys.asia>
2020-09-04 13:40:59 +0530

    Completed core extractor

---
+++ src/library/product/details/stores/l/londondrugs/CA/createUrl.js
@@ -0,0 +1,11 @@
+module.exports = {
+  implements: "product/details/createUrl",
+  parameterValues: {
+    domain: "londondrugs.com",
+    prefix: null,
+    url: "https://www.londondrugs.com/{id}.html",
+    country: "CA",
+    store: "londondrugs",
+    zipcode: "",
+  },
+};
---
+++ src/library/product/details/stores/l/londondrugs/CA/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'CA',
+    store: 'londondrugs',
+    domain: 'londondrugs.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
