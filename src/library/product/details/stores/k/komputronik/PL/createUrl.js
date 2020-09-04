597d3a9de2318ee657eea1625812409ea40a1adb
sivasoarlogic <siva@soarlogic.com>
2020-09-04 14:10:32 +0530

    DS-Core-komputronik_PL

---
+++ src/library/product/details/stores/k/komputronik/PL/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'komputronik.pl',
+    prefix: null,
+    url: null,
+    country: 'PL',
+    store: 'komputronik',
+    zipcode: "''",
+  },
+};
