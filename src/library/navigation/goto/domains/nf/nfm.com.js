8b2ffe8260e8e85a5fd773c25a53bd950bbe014e
nehashende <neha.shende@synerzip.com>
2020-09-04 13:31:23 +0530

    #460. Added residential proxy.

--- src/library/navigation/goto/domains/nf/nfm.com.js
+++ src/library/navigation/goto/domains/nf/nfm.com.js
@@ -7,9 +7,4 @@ module.exports = {
     store: 'nfm',
     zipcode: '',
   },
-  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    await context.setBlockAds(false);
-    const timeout = parameters.timeout ? parameters.timeout : 30000;
-    await context.goto(url, { first_request_timeout: 60000, anti_fingerprint: true, timeout, waitUntil: 'load', checkBlocked: true });
-  },
 };
