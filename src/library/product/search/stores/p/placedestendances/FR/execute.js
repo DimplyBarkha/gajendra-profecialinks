f309a1395d6e7b0c259ac7b156a722bc36e3b2d4
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-04 14:20:49 +0530

    Updated xpath for no results

--- src/library/product/search/stores/p/placedestendances/FR/execute.js
+++ src/library/product/search/stores/p/placedestendances/FR/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'FR',
     store: 'placedestendances',
     domain: 'placedestendances.com',
-    //url: 'https://www.placedestendances.com/fr/fr/{searchTerms}',
     url: 'https://www.placedestendances.com/fr/fr/{searchTerms}',
     loadedSelector: 'div.lien_voir_tout_container',
+    noResultsXPath: '//div[@class="titrefindeco" and contains(text(),"Oups")]',
     zipcode: '',
   },
 };
