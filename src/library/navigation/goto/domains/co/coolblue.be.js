f6bc15427ccfee97030b551561374378a74d43df
Pashya <pashya@profecialinks.com>
2020-09-06 03:31:31 +0530

    variants extractor for coolblue BE

---
+++ src/library/navigation/goto/domains/co/coolblue.be.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'coolblue.be',
+    timeout: 100000,
+    country: 'BE',
+    store: 'coolblue',
+    zipcode: '',
+  },
+};
