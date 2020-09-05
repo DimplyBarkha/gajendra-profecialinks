1cf544c401510f0536db925b95a2ab7884b3530e
Aditi-ux <aditi@profecialinks.com>
2020-09-05 18:16:29 +0530

    Updates

--- src/library/product/details/stores/u/ubaldi/format.js
+++ src/library/product/details/stores/u/ubaldi/format.js
@@ -6,9 +6,15 @@
 const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
+      if (row.variants) {
+        const variantArray = row.variants.map((item) => {
+          return item.text;
+        });
+        row.variants = [{ text: variantArray.join('|'), xpath: row.variants[0].xpath }];
+      }
       if (row.additionalDescBulletInfo) {
         const additionalDescBulletInfoArray = row.additionalDescBulletInfo.map((item) => {
-          return typeof (item.text) === 'string' ? item.text.replace(/\n/gm, '| ') : '';
+          return typeof (item.text) === 'string' ? item.text.replace(/\n/gm, '| ') : '|';
         });
         row.additionalDescBulletInfo = [{ text: additionalDescBulletInfoArray.join('|'), xpath: row.additionalDescBulletInfo[0].xpath }];
       }
@@ -18,24 +24,6 @@ const transform = (data) => {
         });
         row.specifications = [{ text: specificationsArray.join('|'), xpath: row.specifications[0].xpath }];
       }
-    //   if (row.variants) {
-    //     const variantArray = row.variants.map((item) => {
-    //       return item.text;
-    //     });
-    //     row.variants = [{ text: variantArray.join('|'), xpath: row.variants[0].xpath }];
-    //   }
-    //   if (row.shippingInfo) {
-    //     const shippingInfoArray = row.shippingInfo.map((item) => {
-    //       return typeof (item.text) === 'string' ? item.text.replace(/\n/gm, ' ') : '';
-    //     });
-    //     row.shippingInfo = [{ text: shippingInfoArray.join('|'), xpath: row.shippingInfo[0].xpath }];
-    //   }
-    //   if (row.specifications) {
-    //     const specificationsArr = row.specifications.map((item) => {
-    //       return typeof (item.text) === 'string' ? item.text.replace(/\n/gm, '| ') : '';
-    //     });
-    //     row.specifications = [{ text: specificationsArr.join('|'), xpath: row.specifications[0].xpath }];
-    //   }
     }
   }
 
--- src/library/product/details/stores/u/ubaldi/fr/extract.js
+++ src/library/product/details/stores/u/ubaldi/fr/extract.js
@@ -1,46 +1,4 @@
 
-// async function implementation (
-//   inputs,
-//   parameters,
-//   context,
-//   dependencies,
-// ) {
-//   await context.evaluate(async function () {
-//     const legalDisclaimerClick = document.getElementsByClassName('fa-garanties-legales stat open-popin')[0];
-//     if (legalDisclaimerClick !== undefined) {
-//       legalDisclaimerClick.click();
-//     }
-//   });
-//   await context.evaluate(async function () {
-//     const closeLegalDisclaimer = document.getElementsByClassName('btn btn-default btn-icone bi-suppression bi-gauche')[0];
-//     closeLegalDisclaimer.click();
-//   });
-
-//   const applyScroll = async function (context) {
-//     await context.evaluate(async function () {
-//       let scrollTop = 0;
-//       while (scrollTop !== 20000) {
-//         await stall(500);
-//         scrollTop += 1000;
-//         window.scroll(0, scrollTop);
-//         if (scrollTop === 20000) {
-//           await stall(5000);
-//           break;
-//         }
-//       }
-//       function stall (ms) {
-//         return new Promise((resolve, reject) => {
-//           setTimeout(() => {
-//             resolve();
-//           }, ms);
-//         });
-//       }
-//     });
-//   };
-
-//   await applyScroll(context);
-// }
-
 const { transform } = require('../format');
 module.exports = {
   implements: 'product/details/extract',
@@ -51,5 +9,37 @@ module.exports = {
     domain: 'ubaldi.com',
     zipcode: "''",
   },
-  // implementation,
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    // await context.waitForSelector('#flix-iframe0', { timeout: 10000 });
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const colorlement = document.evaluate("//div[contains(@class,'fa-liste-decli')]//*/@style", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (colorlement && colorlement.value.indexOf('background-color') > -1) {
+        const colorCode = colorlement.value.match(/\d+/g);
+        addElementToDocument('fl_colorcode', colorCode);
+      }
+
+      async function collectVideo () {
+        const secret = await context.evaluate(function () {
+          const ele = document.querySelector('video');
+          if (ele) {
+            const eleSrc = ele.getAttribute('src');
+            console.log('RETURNING SOURCE');
+            return eleSrc;
+          } else {
+            return 'COULD NOT FIND!!!!!!!!!!!!!!!!!!!!';
+          }
+        }, [], 'iframe[title="Flix-media-video-0"]');
+        console.log(secret);
+      }
+      collectVideo();
+    });
+    await context.extract(productDetails);
+  },
 };
