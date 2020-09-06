1801aa82aeca7257c7b7c6fdabe76b1db663b8df
Gajender Ramola <ramola106@gmail.com>
2020-09-06 15:04:59 +0530

    add some more keys

--- src/library/navigation/goto/domains/ma/manor.ch.js
+++ src/library/navigation/goto/domains/ma/manor.ch.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'manor.ch',
-    timeout: null,
+    timeout: 1000000,
     country: 'CH',
     store: 'manor',
     zipcode: "''",
