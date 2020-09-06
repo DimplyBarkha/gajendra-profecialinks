895daf328c797fa3e48bbe4b38a00c56e44cade7
jignesh013 <jignesh@profecialinks.com>
2020-09-06 16:01:36 +0530

    Fixing pagintation issue

--- src/library/product/search/stores/a/andorrafreemarket/ES/paginate.js
+++ src/library/product/search/stores/a/andorrafreemarket/ES/paginate.js
@@ -4,11 +4,11 @@ module.exports = {
   parameterValues: {
     country: 'ES',
     store: 'andorrafreemarket',
-    nextLinkSelector: 'div[class="category-products"]>div[class="toolbar"]>div[class="pager"]>div>ol>li[class="next"]',
+    nextLinkSelector: 'div[class="category-products"]>div[class="toolbar"]>div[class="pager"]>div>ol>li[class="next"] a.next',
     mutationSelector: 'div[class="category-products"]>div[class="toolbar"]>div[class="sorter"]>p[class="amount"]',
     spinnerSelector: null,
     loadedSelector: 'div[class="category-products"]',
-    noResultsXPath: '//div[contains(@class, "main-container")]//div[contains(@class, "col-main")]//div[@class="page-title"]//h1[contains(text(), "Resultados")]',
+    noResultsXPath: '//p[contains(@class,"empty-catalog")]',
     openSearchDefinition: null,
     domain: 'andorrafreemarket.com',
     zipcode: '',
