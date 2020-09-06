5dda6f8fda9d6a26fee40b7f0be27dcd55b7042c
sukhvinder <sukhvinder@profecialinks.com>
2020-09-06 11:28:44 +0530

    search module

---
+++ src/library/product/search/stores/n/noon/SA/paginate.js
@@ -0,0 +1,15 @@
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'SA',
+    store: 'noon',
+    nextLinkSelector: 'li.next',
+    mutationSelector: null,
+    spinnerSelector: 'div.jsx-3230834092 loader',
+    loadedSelector: 'div.jsx-3152181095 .productList .gridView',
+    noResultsXPath:  '//div[@class="jsx-671104679 jsx-3349621030 container"]',
+    openSearchDefinition: null,
+    domain: 'noon.com',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/search/stores/n/noon/tranform.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.prodcutUrl) {
+        row.prodcutUrl.forEach(item => {
+          item.text = "https://www.noon.com" + item.text;
+        });
+      }
+    }
+  }
+  return data;
+};
+
+module.exports = { transform };
