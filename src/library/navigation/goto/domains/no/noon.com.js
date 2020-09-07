21bb61fb310bb7615b7326a8fd4e652d18fd69f6
sukhvinder <sukhvinder@profecialinks.com>
2020-09-07 17:45:20 +0530

    commited files

--- src/library/navigation/goto/domains/no/noon.com.js
+++ src/library/navigation/goto/domains/no/noon.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'noon.com',
-    timeout: null,
+    timeout: 50000,
     country: 'SA',
     store: 'noon',
     zipcode: '',
