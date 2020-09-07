01e80c3767c96f5f17c3bdd746f6ae5a3259ea63
peter <pravinr@synerzipune.local>
2020-09-07 17:27:12 +0530

    onlinetrade RU variant code

---
+++ src/library/product/details/stores/o/onlinetrade/RU/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'RU',
+    store: 'onlinetrade',
+    domain: 'onlinetrade.ru',
+    zipcode: '',
+  },
+};
