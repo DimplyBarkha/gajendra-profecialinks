8fb57593dec8de8e255d9c1eca293ba58594c8b1
Vishal Patro <vishal@profecialinks.com>
2020-09-08 02:07:48 +0530

    Updated extracter and added the deployment yamls
    Signed-off-by: Vishal Patro <vishal@profecialinks.com>

--- src/library/product/details/stores/p/perfecthair/format.js
+++ src/library/product/details/stores/p/perfecthair/format.js
@@ -6,10 +6,18 @@
 const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
+      if (row.description) {
+        row.description.map(item => {
+          item.text = item.text.replace(/\n/gm, " | ");
+        });
+      }
       if (row.productOtherInformation) {
         row.productOtherInformation.map(item => {
           item.text = item.text.replace(/\n /, " ").replace(/\n /gm, "");
         });
+      }
+      if (row.specifications) {
+        row.specifications = [{ text: row.specifications.map(item => item.text.replace(/\n/, "").replace(/\n /gm, "")).join(" | ") }]
       }
     }
   }
