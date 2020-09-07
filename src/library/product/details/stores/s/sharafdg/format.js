099d9a323912a30a27f2ca1405ccd7475f80170d
jignesh013 <jignesh@profecialinks.com>
2020-09-07 22:42:58 +0530

    Changed specification logic

--- src/library/product/details/stores/s/sharafdg/format.js
+++ src/library/product/details/stores/s/sharafdg/format.js
@@ -9,11 +9,13 @@ const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
       if (row.specifications) {
-        console.log('ROW SPECIFICATIONS =>', row.specifications);
-        const specifications = row.specifications.map((item) => {
-          return item.text;
-        });
-        row.specifications = [{ text: specifications.join('||') }];
+        const formattedSpecification = [];
+        for (let i = 0; i < row.specifications.length; i++) {
+          if (i % 2 === 0 && row.specifications[i + 1]) {
+            formattedSpecification.push(`${row.specifications[i].text}:${row.specifications[i + 1].text}`);
+          }
+        }
+        row.specifications = [{ text: formattedSpecification.join(' | ') }];
       }
       if (row.additionalDescBulletInfo) {
         const additionalDescBulletInfo = row.additionalDescBulletInfo.map((item) => {
@@ -24,6 +26,21 @@ const transform = (data) => {
     }
   }
 
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
   return data;
 };
 
