923802918da55c24d3178b49535bf72da608e391
Syed Abuthahir <sayed@profecialinks.com>
2020-09-07 13:29:17 +0530

    small changes done

--- src/library/product/details/stores/m/milar/ES/transform.js
+++ src/library/product/details/stores/m/milar/ES/transform.js
@@ -3,11 +3,31 @@
 const transform = (data) => {
   for (const { group} of data) {
       for (const row of group) {
+
+            if (row.pageTimestamp) {
+                var d = new Date();
+                row.pageTimestamp = d.toISOString();
+                console.log(row.pageTimestamp,"ppppppp")
+            }
+
+            if (row.technicalInformationPdfPresent) {
+                row.technicalInformationPdfPresent = 'No';
+            }
+
+            if (row.Image360Present) {
+                row.Image360Present = 'No';
+            }
+
           if (row.additionalDescBulletInfo) {
               row.additionalDescBulletInfo.forEach(item => {
                   item.text  = item.text.replace(/(\s*\n\s\n\s*)/g, ' | ').trim();
               });
           }
+
+
+          if (row.descriptionBullets) {
+            row.descriptionBullets = row.descriptionBullets.length
+        }
 
           if (row.specifications) {
             row.specifications.forEach(item => {
