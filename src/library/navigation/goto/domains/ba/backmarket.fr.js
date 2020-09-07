cd1cd11bb2fbe3ac5675b808dd44f7e698e3c27c
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-07 15:30:51 +0530

    404 handled in goto(#437)

--- src/library/navigation/goto/domains/ba/backmarket.fr.js
+++ src/library/navigation/goto/domains/ba/backmarket.fr.js
@@ -8,4 +8,19 @@ module.exports = {
     store: 'backmarket',
     zipcode: '',
   },
+  implementation: async (inputs, parameterValues, context, dependencies) => {
+    const url = `${inputs.url}`;
+    await context.goto(url, { timeout: 90000, waitUntil: 'load', checkBlocked: true });
+    try {
+      await context.evaluate(async function () {
+        let notLoaded = document.evaluate('//h3[contains(text(), "pas été trouvée. ")] | //h3[contains(text(), "- Ce lien n’a pas encore été reconditionné")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if(notLoaded){
+         throw "PAGE NOT FOUND !!";
+        }
+      })
+    } catch (error) {
+      console.log('error: ', error); 
+    }
+    
+  },
 };
