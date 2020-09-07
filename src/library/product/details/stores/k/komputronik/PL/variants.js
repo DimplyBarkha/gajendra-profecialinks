b7beabb93c9d6c0153ffc7698a39779de4311f8b
sivasoarlogic <siva@soarlogic.com>
2020-09-07 14:46:20 +0530

    DS-Core-Komputronik_PL

---
+++ src/library/product/details/stores/k/komputronik/PL/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'PL',
+    store: 'komputronik',
+    domain: 'komputronik.pl',
+    zipcode: "''",
+  },
+};
