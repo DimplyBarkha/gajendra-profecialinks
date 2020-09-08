99336b801b56f785d41762c0a280f01c04e4c49e
Gajender Ramola <ramola106@gmail.com>
2020-09-08 12:27:37 +0530

    scrape id

--- src/library/product/search/stores/m/manor/transform.js
+++ src/library/product/search/stores/m/manor/transform.js
@@ -10,6 +10,16 @@ const transform = (data) => {
         for (const row of group) {            
             row.rankOrganic = [{'text':rank,'xpath':''}];
             rank = rank + 1;
+            if (row.productUrl) {
+                row.productUrl.forEach(item => {
+                    var arr = item.text.split('/');
+                    var url_len = arr.length
+                    var item_id = arr[url_len-1]
+                    row.id.forEach(item => { 
+                        item.text = item_id
+                    });
+                });
+            }
         }        
     }
     return data;
