52cd2c7e359a99aba66d455bc76ba0f60b8bceca
judhisahoo2009 <judhisahoo@hotmail.com>
2020-09-08 20:08:38 +0530

    pushing

--- src/library/product/details/stores/m/mediaexpert/variantsTransform.js
+++ src/library/product/details/stores/m/mediaexpert/variantsTransform.js
@@ -7,51 +7,9 @@
 const transform = (data) => {
     for (const { group } of data) {
       for (const row of group) {
-        if(row.image){
-          row.image.forEach(item => {
+        if(row.variantUrl){
+          row.variantUrl.forEach(item => {
             item.text='https://www.mediaexpert.pl'+item.text;
-          });
-        }
-
-        if(row.alternateImages){
-          row.alternateImages.forEach(item => {
-            item.text='https://www.mediaexpert.pl'+item.text;
-          });
-        }
-
-        if (row.manufacture) {
-          row.manufacture.forEach(item => {
-            var myRegexp = /producenta\/importera\s+\n(.+?)\s*\n/g;
-            var match = myRegexp.exec(item.text);
-            if(match.length){
-                item.text = match[1].trim();
-            }else{
-                item.text = "";
-            }
-          });
-        }
-
-        if (row.brandText) {
-          row.brandText.forEach(item => {
-            var myRegexp = /producenta\/importera\s+\n(.+?)\s*\n/g;
-            var match = myRegexp.exec(item.text);
-            if(match.length){
-                item.text = match[1].trim();
-            }else{
-                item.text = "";
-            }
-          });
-        }
-
-        if (row.warranty) {
-          row.warranty.forEach(item => {
-            var myRegexp = /Gwarancja\s+\n\s*(.+)/g;
-            var match = myRegexp.exec(item.text);
-            if(match.length){
-                item.text = match[1].trim();
-            }else{
-                item.text = "";
-            }
           });
         }
       }
