8c89e371b44e5f1b9ce0900b8b244aba6a0b1efc
unknown <ganesh.working@gmail.com>
2020-09-09 03:56:24 +0530

    core part is done

--- src/library/product/details/stores/o/ozon/RU/execute.js
+++ src/library/product/details/stores/o/ozon/RU/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'RU',
     store: 'ozon',
     domain: 'ozon.ru',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div[data-widget="webCharacteristics"] div h2[class="b0g6"]',
+    noResultsXPath: '//div[@class="error-main"]//h2[contains(text(),"Произошла ошибка")]',
     zipcode: '',
   },
 };
--- src/library/product/details/stores/o/ozon/transform.js
+++ src/library/product/details/stores/o/ozon/transform.js
@@ -9,9 +9,11 @@ const transform = (data) => {
       for (const row of group) {
         
         if (row.alternateImages) {
+          let info = [];          
           row.alternateImages.forEach(item => {
-            item.text = item.text.replace(/(\/wc\d+\/)+/g, '\/wc1200\/');
-          });
+            info.push(item.text.replace(/(\/wc\d+\/)+/g, '\/wc1200\/'));            
+          });          
+          row.alternateImages = [{'text':info.join(' | '),'xpath':row.alternateImages[0].xpath}];          
         }
 
         if (row.reviewCount) {
@@ -28,8 +30,91 @@ const transform = (data) => {
 
         if (row.description) {
           row.description.forEach(item => {
-            item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
+            let data = JSON.parse(item.text);
+            if(data['description']){
+              item.text = data['description'].replace(/(\s*\n\s*)+/g, ' || ').trim();
+            }            
           });
+        }
+
+        if (row.sku) {
+          row.sku.forEach(item => {
+            item.text = item.text.replace(/(\s*Код\s*товара\s*:\s*)+/g, '').trim();
+          });
+        }
+
+        if (row.availabilityText) {
+          row.availabilityText.forEach(item => {                            
+              if (item.text != 'In Stock') {
+                item.text = 'Out of Stock'
+                row.quantity.forEach(item => { 
+                  item.text = 0
+                });                
+              }
+          });
+        }
+
+        if (row.shippingInfo) {
+          let info = [];          
+          row.shippingInfo.forEach(item => {
+            info.push(item.text);            
+          });          
+          row.shippingInfo = [{'text':info.join(' | '),'xpath':row.shippingInfo[0].xpath}];          
+        }
+
+        if (row.specifications) {
+          let info = [];          
+          row.specifications.forEach(item => {
+            info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());            
+          });          
+          row.specifications = [{'text':info.join(' || '),'xpath':row.specifications[0].xpath}];          
+        }
+
+        if (row.productOtherInformation) {
+          let info = [];          
+          row.productOtherInformation.forEach(item => {
+            info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());            
+          });          
+          row.productOtherInformation = [{'text':info.join(' || '),'xpath':row.productOtherInformation[0].xpath}];          
+        }
+
+        if (row.variants) {
+            let variations = [];
+            let variant_info = []
+            row.variants.forEach(item => {
+                let data = JSON.parse(item.text);
+                if(data['aspects']){                    
+                    data['aspects'].forEach(variation => {                      
+                      variation['variants'].forEach(variants_data => {
+                        var link_data = variants_data['link']
+                        var matches = /.+\/(\d+)\//isg.exec(link_data);
+                        var matches1 = /.+-(\d+)\//isg.exec(link_data);
+                        if (matches){
+                          link_data = matches[1]
+                        }
+                        else if(matches1){
+                          link_data = matches1[1]
+                        }                        
+                        if (!variations.includes(link_data)) {
+                          variations.push(link_data);
+                        }
+                        if (variation['type'] == 'apparelPics' || variation['type'] == 'colors')  {
+                          variant_info.push(variants_data['data']['textRs'][0]['content'])
+                        }
+                      });
+                    });
+                }
+                
+            });
+            
+            if(variations.length){
+                //row.variantId = data['sku'];
+                row.variantCount = [{"text": variations.length, "xpath": row.variants[0]["xpath"]}];
+                row.variantInformation = [{"text": variant_info.join(' | '), "xpath": row.variants[0]["xpath"]}];
+                row.variants = [{"text": variations.join(' | '), "xpath": row.variants[0]["xpath"]}];
+            }else{
+                delete row.variants;
+            }          
         }
                
       }
