1e1d1f8b037d465d4d3e2e4507d42789e8ffa8d3
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-09-08 20:49:46 +0530

    #545. Videos logic updated.

--- src/library/product/details/stores/d/davidjones/NZ/format.js
+++ src/library/product/details/stores/d/davidjones/NZ/format.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /**
  *
  * @param {ImportIO.Group[]} data
@@ -23,8 +24,23 @@ const transform = (data) => {
       if (row.image) {
         row.image[0].text = row.image[0].text.replace(/productthumb/, 'magnify');
       }
-      if (row.warranty) {
-        row.warranty[0].text = row.warranty[0].text.replace(/:/, '');
+      if (!row.warranty) {
+        if (row.warranty1) {
+          const text = row.warranty1[0].text.replace(/\n/g, '').replace(/.*Warranty:(.*)/, '$1').replace(/^((?:\S+\s+){3}\S+).*/, '$1').replace(/-/g, '').replace(/:/g, '');
+          if (text.includes('guarantee')) {
+            row.warranty = [{ text }];
+          }
+        }
+      }
+      if (row.videos) {
+        row.videos.forEach(temp => {
+          if (temp.text.charAt(0) === '/') {
+            temp.text = temp.text.replace(/\/\//, 'https://');
+          }
+        });
+        row.videos.forEach(item => {
+          item.text = item.text.replace(/\/productimages/, 'https://www.davidjones.com/productimages');
+        });
       }
       row.variantCount = [{ text: 1 }];
     }
