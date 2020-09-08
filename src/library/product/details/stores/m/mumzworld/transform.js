4da44d4ea7b6227e7bb6ba76083e96bb52efc5d4
unknown <ganesh.working@gmail.com>
2020-09-08 14:17:40 +0530

    fix description issue

--- src/library/product/details/stores/m/mumzworld/transform.js
+++ src/library/product/details/stores/m/mumzworld/transform.js
@@ -15,10 +15,33 @@ const transform = (data) => {
           row.variantCount = [{'text': info.length}];
           row.variantInformation = [{'text':info.join(' | '),'xpath':row.variantInformation[0].xpath}];          
         }
-        if (row.description) {
-          row.description.forEach(item => {
-            item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
+        if (row.alternateImages) {
+          let info = [];          
+          row.alternateImages.forEach(item => {
+            info.push(item.text.trim());            
+          });          
+          row.alternateImages = [{'text':info.join(' | '),'xpath':row.alternateImages[0].xpath}];          
+        }
+        if (row.additionalDescBulletInfo) {
+          let info = [];          
+          row.additionalDescBulletInfo.forEach(item => {
+            info.push(item.text.trim());            
           });
+          row.descriptionBullets = [{'text': info.length}];
+          row.additionalDescBulletInfo = [{'text':info.join(' | '),'xpath':row.additionalDescBulletInfo[0].xpath}];          
+        }
+        if (row.specifications) {
+          row.specifications.forEach(item => {
+            item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
+            item.text = item.text.replace(/(\s*Overview:\s*\|\|\s*)+/g, '').trim();
+          });
+        }        
+        if (row.description) {
+          let info = [];          
+          row.description.forEach(item => {
+            info.push(item.text.replace(/(\s*\n\s*)+/g, ' || ').trim());            
+          });
+          row.description = [{'text':info.join(' || '),'xpath':row.description[0].xpath}];          
         }
         if (row.shippingInfo) {
           row.shippingInfo.forEach(item => {
