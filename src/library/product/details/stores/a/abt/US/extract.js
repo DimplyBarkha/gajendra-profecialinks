ad25930115405448db7f5936495e3ab3223be41d
Karolina <karolina.urbanska@tdcm.io>
2020-09-08 11:20:27 +0200

    Inserted xpaths to the schema

--- src/library/product/details/stores/a/abt/US/extract.js
+++ src/library/product/details/stores/a/abt/US/extract.js
@@ -9,4 +9,38 @@ module.exports = {
     domain: 'abt.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const description = document.querySelector('div#overview_content')
+        // @ts-ignore
+        ? document.querySelector('div#overview_content').innerText : '';
+      if (description) {
+        addElementToDocument('description', description.replace(/•/g, '||').replace(/\n|\s{2,}/g, ' '));
+      }
+
+      const variants = document.querySelector('div.display-group-color');
+      const variantColor = variants && document.querySelector('div.display-group-color strong')
+        // @ts-ignore
+        ? document.querySelector('div.display-group-color strong').innerText : '';
+      const specColorXpath = document.evaluate('//table[@id="specs_table"]//strong[text()="Color"]/../following-sibling::td[1]', document, null, XPathResult.STRING_TYPE, null);
+      const specColor = specColorXpath ? specColorXpath.stringValue : '';
+      const descColorXpath = document.evaluate('//li[strong[text()="Color:"]]/text()', document, null, XPathResult.STRING_TYPE, null);
+      const descColor = descColorXpath ? descColorXpath.stringValue : '';
+      if (variantColor) {
+        addElementToDocument('color', variantColor);
+      } else if (specColor) {
+        addElementToDocument('color', specColor);
+      } else {
+        addElementToDocument('color', descColor);
+      }
+    });
+    await context.extract(productDetails);
+  },
 };
