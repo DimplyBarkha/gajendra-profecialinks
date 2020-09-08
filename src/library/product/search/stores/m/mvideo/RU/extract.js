ba2d74aa239c14ba4d5c0323d2ba471b08173d0f
AjaySinh Zala <ajay@profecialinks.com>
2020-09-08 12:26:49 +0530

    JUST PUSH

--- src/library/product/search/stores/m/mvideo/RU/extract.js
+++ src/library/product/search/stores/m/mvideo/RU/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../transform');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'RU',
     store: 'mvideo',
-    transform: null,
+    transform,
     domain: 'mvideo.ru',
     zipcode: '',
   },
---
+++ src/library/product/search/stores/m/mvideo/transform.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+    for (const { group } of data) {
+      for (const row of group) {
+        if (row.id) {
+          row.id.forEach(item => {           
+            var splitData = item.text.split('-');
+            item.text =  parseInt(splitData[splitData.length-1]);
+          });
+        }
+        if (row.ratingCount) {
+          row.ratingCount.forEach(item => {           
+            item.text = parseInt(item.text);
+          });
+        }
+        if (row.thumbnail) {
+          row.thumbnail.forEach(item => {           
+            item.text = 'https:'+item.text;
+          });
+        }
+        if (row.prodcutUrl) {
+          row.prodcutUrl.forEach(item => {       
+            item.text = 'https://www.mvideo.ru'+item.text;
+          });
+        }          
+      }
+    }
+    return data;
+  };  
+  module.exports = { transform };
