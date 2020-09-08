5f9a21d2857afde1368e1595b323790f48b0fce2
nikita.kumari <nikita.kumari@synerzip.com>
2020-09-08 18:54:35 +0530

    Added logic and xpaths #544

--- src/library/navigation/goto/domains/ca/carrefour.es.js
+++ src/library/navigation/goto/domains/ca/carrefour.es.js
@@ -5,5 +5,39 @@ module.exports = {
     domain: 'carrefour.es',
     country: 'ES',
     store: 'carrefour',
+    timeout: 100000,
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setBlockAds(false);
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop !== 20000) {
+          await stall(500);
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 20000) {
+            await stall(5000);
+            break;
+          }
+        }
+        function stall (ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true });
+    await applyScroll(context);
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
--- src/library/product/details/stores/c/carrefourRegular/ES/extract.js
+++ src/library/product/details/stores/c/carrefourRegular/ES/extract.js
@@ -1,11 +1,35 @@
-
+const { transform } = require('./transform');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'ES',
     store: 'carrefourRegular',
-    transform: null,
+    transform,
     domain: 'carrefour.es',
     zipcode: '',
+  },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(async function () {
+      async function infiniteScroll () {
+        let prevScroll = document.documentElement.scrollTop;
+        while (true) {
+          window.scrollBy(0, document.documentElement.clientHeight);
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          const currentScroll = document.documentElement.scrollTop;
+          if (currentScroll === prevScroll) {
+            break;
+          }
+          prevScroll = currentScroll;
+        }
+      }
+      await infiniteScroll();
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
   },
 };
