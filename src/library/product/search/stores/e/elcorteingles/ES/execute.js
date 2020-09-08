a0c76a24ca07d43ecbfb6024edacd3bdf15cf634
Abhisek <abhisek.d@osmosys.asia>
2020-09-09 00:24:41 +0530

    added noresultxapth

--- src/library/product/search/stores/e/elcorteingles/ES/execute.js
+++ src/library/product/search/stores/e/elcorteingles/ES/execute.js
@@ -7,6 +7,6 @@ module.exports = {
     domain: 'elcorteingles.es',
     url: 'https://www.elcorteingles.es/supermercado/buscar/?term={searchTerms}&search=text',
     loadedSelector: 'div.product_tile-prices',
-    noResultsXPath: '//div[@class="composition _none_sc"]',
+    noResultsXPath: '//div[@class="composition _none_sc"] |  //div[contains(@class,"js-plp") and count(div[contains(@class,"js-grid-container")]) < 1]',
   },
 };
