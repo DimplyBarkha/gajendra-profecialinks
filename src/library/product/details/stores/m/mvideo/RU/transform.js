050d07585b9fec432984bcae65582263cf5dfff1
AjaySinh Zala <ajay@profecialinks.com>
2020-09-08 12:45:36 +0530

    pushing

--- src/library/product/details/stores/m/mvideo/RU/transform.js
+++ src/library/product/details/stores/m/mvideo/RU/transform.js
@@ -11,22 +11,58 @@ const transform = (data) => {
             item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
           });
         }
+
         if (row.shippingInfo) {
           row.shippingInfo.forEach(item => {
             item.text = item.text.replace(/(\s*\n\s*)+/g, ' ').trim();
           });
         }
+
+        if (row.image) {
+          row.image.forEach(item => {
+            item.text = 'https:'+item.text;
+          });
+        }
+
+        if (row.brandImage) {
+          row.brandImage.forEach(item => {
+            item.text = 'https:'+item.text;
+          });
+        }
+
+        if (row.alternateImages) {
+          row.alternateImages.forEach(item => {
+            item.text = 'https:'+item.text;
+          });
+        }
+
+        if (row.ratingCount) {
+          row.ratingCount.forEach(item => {
+            item.text = parseInt(item.text);
+          });
+        }
+
+        if (row.sku) {
+          row.sku.forEach(item => {
+            item.text = parseInt(item.text);
+          });
+        }
+
+        
+
         if (row.category) {
           row.category.forEach(item => {
             item.text = item.text.replace('Главная', '').trim();
             item.text = item.text.replace(/(\s*\n\s*)+/g, ' > ').trim();
           });
         }
+
         if (row.featureBullets) {
           row.featureBullets.forEach(item => {
             item.text = item.text.replace(/(\s*\n\s*)+/g, ' | ').trim();
           });
         }
+
         if (row.weightNet) {
             row.weightNet.forEach(item => {                
                 var matches = /Weight\s*:\s(.*?)\n/isg.exec(item.text);
@@ -38,6 +74,7 @@ const transform = (data) => {
                 }
             });
         }
+
         if (row.shippingWeight) {
             row.shippingWeight.forEach(item => {
                 var matches = /Shipping\s+Weight\s*:\s*(.*?)\n/isg.exec(item.text);                
@@ -49,6 +86,7 @@ const transform = (data) => {
                 }
             });
         }
+
         if (row.shippingDimensions) {
             row.shippingDimensions.forEach(item => {
                 var matches = /Product\s+Dimensions\s*:\s*(.*?)\n/isg.exec(item.text);
@@ -64,6 +102,7 @@ const transform = (data) => {
                 }
             });
         }
+
         if (row.warnings) {
             row.warnings.forEach(item => {
                 var matches = /Cautions\s*:\s*(.*?)\n/isg.exec(item.text);                
@@ -74,6 +113,7 @@ const transform = (data) => {
                   item.text = undefined
                 }
             });
+        
         }
       }
     }
