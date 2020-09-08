0e7f929be0c326b01718b736a8acd058fffa700b
Ankit Kumar <ankit.k@osmosys.asia>
2020-09-09 02:07:26 +0530

    added transform to extract.js

--- src/library/product/details/stores/c/coolblue/NL/extract.js
+++ src/library/product/details/stores/c/coolblue/NL/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'NL',
     store: 'coolblue',
-    transform: null,
+    transform: transform,
     domain: 'coolblue.nl',
     zipcode: '',
   },
