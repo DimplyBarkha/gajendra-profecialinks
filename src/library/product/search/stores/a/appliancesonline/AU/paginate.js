3e168a9cc22634f45659f44badc99bfb1bca3a1f
Bindu Datla <bindu.datla@import.io>
2020-09-07 21:34:10 -0400

    added implementation for paginate

--- src/library/product/search/stores/a/appliancesonline/AU/paginate.js
+++ src/library/product/search/stores/a/appliancesonline/AU/paginate.js
@@ -1,14 +1,75 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { keywords, page, offset } = inputs;
+  const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
+
+  if (nextLinkSelector) {
+    const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector), nextLinkSelector);
+    if (!hasNextLink) {
+      return false;
+    }
+  }
+
+  if(!openSearchDefinition){
+  const { pager } = dependencies;
+  const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
+  if (success) {
+    return true;
+  }
+}
+
+  let url = await context.evaluate(function () {
+    /** @type { HTMLLinkElement } */
+    const next = document.querySelector('head link[rel="next"]');
+    if (!next) {
+      return false;
+    }
+    return next.href;
+  });
+
+  if (!url && openSearchDefinition) {
+    url = openSearchDefinition.template
+      .replace('{searchTerms}', encodeURIComponent(keywords))
+      .replace('{page}', (page + (openSearchDefinition.pageOffset || 0)).toString())
+      .replace('{offset}', (offset + (openSearchDefinition.indexOffset || 0)).toString());
+  }
+
+  if (!url) {
+    return false;
+  }
+
+  console.log('Going to url', url);
+  await dependencies.goto({ url });
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
+
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'AU',
     store: 'appliancesonline',
-    domain: 'appliancesonline.com.au',
-    nextLinkSelector: null,
-    //nextLinkSelector: 'div.pagination > pagination-template > ul > li.ng-star-inserted:not([class*="active"]):last-child > a.ng-star-inserted',
+    domain: 'appliancesonline.com.au',    
+    nextLinkSelector: 'div.pagination > pagination-template > ul > li.ng-star-inserted:not([class*="active"]):last-child > a.ng-star-inserted',
     loadedSelector: 'div.grid-container-flex',
     openSearchDefinition: {
       template: 'https://www.appliancesonline.com.au/search/{searchTerms}?currentpage={page}&sortkey=highestrated',
     },
   },
+  implementation
 };
