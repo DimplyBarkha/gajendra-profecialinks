c97646f84bd0dc03c92bdb4da4153dd97c4037f1
Manas Ranjan <manas.n@osmosys.asia>
2020-09-08 18:59:03 +0530

    Added more files for costco.

---
+++ src/library/product/details/stores/c/costco/CA/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'CA',
+    store: 'costco',
+    domain: 'costco.ca',
+    zipcode: 'M5V 2A5',
+  },
+};
---
+++ src/library/product/details/stores/c/costco/CA/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'CA',
+    store: 'costco',
+    transform: null,
+    domain: 'costco.ca',
+    zipcode: 'M5V 2A5',
+  },
+};
