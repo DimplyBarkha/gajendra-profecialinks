e425802d08c8f550255f16086a745a7886c189fa
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-09-08 16:18:05 +0530

    #550. added xpath and extractor logic for variants

--- src/library/product/details/stores/m/microspot/CH/variantsExtract.js
+++ src/library/product/details/stores/m/microspot/CH/variantsExtract.js
@@ -1,4 +1,39 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { variants } = dependencies;
+  await context.evaluate(async function () {
+    const variant = JSON.parse(document.querySelector('script#INITIAL_STATE').innerText.trim()).products;
+    const productKey = Object.keys(variant)[0];
+    const variantUrls = JSON.parse(document.querySelector('script#INITIAL_STATE').innerText.trim()).products[productKey].productVariants[0].options;
+    function addEleToDoc (key, value) {
+      const prodEle = document.createElement('div');
+      prodEle.id = key;
+      prodEle.textContent = value;
+      prodEle.style.display = 'none';
+      document.body.appendChild(prodEle);
+    }
+    if (variantUrls && variantUrls.length) {
+      variantUrls.forEach(element => {
+        const item = element.product.code;
+        const link = window.location.href.replace(/(.*)--p(\d+)/gm, '$1--p');
+        if (item) {
+          addEleToDoc('unique_product_url', `${link}${item}`);
+          addEleToDoc('unique_product_id', item);
+        }
+      });
+    } else {
+      addEleToDoc('unique_product_url', window.location.href);
+      const Id = window.location.href.replace(/(.*)--p(\d+)/gm, '$2');
+      addEleToDoc('unique_product_id', Id);
+    }
+  });
 
+  return await context.extract(variants);
+}
 module.exports = {
   implements: 'product/details/variants/variantsExtract',
   parameterValues: {
@@ -8,4 +43,5 @@ module.exports = {
     domain: 'microspot.ch',
     zipcode: '',
   },
+  implementation,
 };
