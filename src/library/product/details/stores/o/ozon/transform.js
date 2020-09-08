0dde201fedbf140041a75bd6030491cca7c4e172
unknown <ganesh.working@gmail.com>
2020-09-08 12:43:23 +0530

    add core keys

--- src/library/product/details/stores/o/ozon/transform.js
+++ src/library/product/details/stores/o/ozon/transform.js
@@ -14,9 +14,21 @@ const transform = (data) => {
           });
         }
 
+        if (row.reviewCount) {
+          row.reviewCount.forEach(item => {
+            item.text = item.text.trim();
+          });
+        }
+
         if (row.category) {
           row.category.forEach(item => {
             item.text = item.text.replace(/(\s*\n\s*)+/g, ' > ').trim();
+          });
+        }
+
+        if (row.description) {
+          row.description.forEach(item => {
+            item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
           });
         }
                
