3950879eb19c2340666839fc511d98fc772fd9cb
Satyam <satyam.rathod@synerzip.com>
2020-09-09 15:53:24 +0530

    #575 deployed with sample inputs

--- src/library/product/search/stores/s/smithandcaugheys/NZ/execute.js
+++ src/library/product/search/stores/s/smithandcaugheys/NZ/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'smithandcaugheys.co.nz',
     url: 'https://www.smithandcaugheys.co.nz/search.aspx?q={searchTerms}',
     loadedSelector: 'article[data-productid]',
-    noResultsXPath: null,
+    noResultsXPath: '//div[contains(text(),"Sorry no results found")]',
     zipcode: '',
   },
 };
