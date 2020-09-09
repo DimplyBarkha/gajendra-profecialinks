6566b844299fd92d5dec847a28b3e99c2fdd1b8e
Rafal Perkowski <rafalpero1@gmail.com>
2020-09-09 17:24:31 +0200

    Initial commit krefel core

---
+++ src/library/product/details/stores/k/krefel/BE/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'krefel.be',
+    prefix: null,
+    url: null,
+    country: 'BE',
+    store: 'krefel',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/k/krefel/BE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'BE',
+    store: 'krefel',
+    transform: null,
+    domain: 'krefel.be',
+    zipcode: '',
+  },
+};
