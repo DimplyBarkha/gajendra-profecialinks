4012304d0dcc416bade6c2996d5c0f5aa1701351
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-09 18:58:28 +0530

    Created extractor for core

---
+++ src/library/navigation/goto/domains/do/douglas.at.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'douglas.at',
+    timeout: null,
+    country: 'AT',
+    store: 'douglas',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/d/douglas/AT/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'douglas.at',
+    prefix: null,
+    url: null,
+    country: 'AT',
+    store: 'douglas',
+    zipcode: '',
+  },
+};
