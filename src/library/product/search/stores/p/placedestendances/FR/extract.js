1f0ed2e567596505ac45fc2930168658bbe4536e
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-09 11:50:14 +0530

    Updated extract.js

--- src/library/product/search/stores/p/placedestendances/FR/extract.js
+++ src/library/product/search/stores/p/placedestendances/FR/extract.js
@@ -15,15 +15,10 @@ module.exports = {
   ) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
-    await context.evaluate(async function() {
-     
-    if(document.querySelector('div.lien_voir_tout_container a'))
-      {
+    await context.evaluate(async function () {
+      if (document.querySelector('div.lien_voir_tout_container a')) {
         document.querySelector('div.lien_voir_tout_container a').click();
-      
       }
-      await new Promise(r => setTimeout(r,8000)) ;
-
     });
     return await context.extract(productDetails, { transform });
   },
