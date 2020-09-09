611a00ed223737050f8d09d7d8741efacf903f2f
srishivams <srishivams97@gmail.com>
2020-09-09 18:27:28 +0530

    removed commented code

--- src/library/navigation/goto/domains/fu/fust.ch.js
+++ src/library/navigation/goto/domains/fu/fust.ch.js
@@ -8,15 +8,6 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
-    // let freshUrl;
-    // let extractorType;
-    // if (url.substring(23, 29) != 'search') {
-    //   freshUrl = url.substring(16, url.length);
-    //   extractorType = 'search';
-    // } else {
-    //   freshUrl = url;
-    //   extractorType = 'core';
-    // }
     await context.goto(url, {
       timeout: 250000,
       waitUntil: 'load',
