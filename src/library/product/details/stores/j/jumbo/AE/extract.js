137b5fc01a99be03ba75793f69d6d75898d9bffe
Prasanna Barate <prasanna.barate@synerzip.com>
2020-09-09 16:08:45 +0530

    (#524) fetched rating count and aggregate rating.

--- src/library/product/details/stores/j/jumbo/AE/extract.js
+++ src/library/product/details/stores/j/jumbo/AE/extract.js
@@ -9,4 +9,17 @@ module.exports = {
     domain: 'jumbo.ae',
     zipcode: '',
   },
+  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 5000))
+      await context.click('a#reviews-tab-header')
+      // await context.waitForSelector('div.hdca-fade-in-open')
+      await new Promise(resolve => setTimeout(resolve, 5000))
+      // await context.click('div.hdca-store-list-item__ctas>button.acl-button--primary')
+    } catch (e) {
+      console.log(e)
+    }
+    await new Promise(resolve => setTimeout(resolve, 10000))
+    return await context.extract(productDetails, { transform });
+  },
 };
