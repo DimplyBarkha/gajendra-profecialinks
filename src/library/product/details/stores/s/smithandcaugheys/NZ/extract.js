9385e13b428fe9bf7962de3e9cb19bcc5d94cfe6
Satyam <satyam.rathod@synerzip.com>
2020-09-09 12:54:13 +0530

    #558 added cleanup and deployed with sample inputs

--- src/library/product/details/stores/s/smithandcaugheys/NZ/extract.js
+++ src/library/product/details/stores/s/smithandcaugheys/NZ/extract.js
@@ -1,10 +1,10 @@
-
+const { cleanUp } = require('../../../../shared')
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'NZ',
     store: 'smithandcaugheys',
-    transform: null,
+    transform: cleanUp,
     domain: 'smithandcaugheys.co.nz',
     zipcode: '',
   },
