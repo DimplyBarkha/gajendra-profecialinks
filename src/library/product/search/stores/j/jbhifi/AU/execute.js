2916813557e215cb4a8aafbf0723fa59a6e8215f
prateek.s <psc3018@gmail.com>
2020-09-09 17:26:13 +0530

    checks fixed

--- src/library/product/search/stores/j/jbhifi/AU/execute.js
+++ src/library/product/search/stores/j/jbhifi/AU/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'jbhifi',
     domain: 'jbhifi.com.au',
     url: 'https://www.jbhifi.com.au/?q={searchTerms}&hPP=150',
-    loadedSelector: `div.product-tile__image--aspect-ratio>img`,
+    loadedSelector: 'div.product-tile__image--aspect-ratio>img',
     noResultsXPath: '//*[@class="no-search-result"]',
     zipcode: '',
   },
