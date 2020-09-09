4309a299e4283b5097be408e7aa4fed56ba2e832
Ravi Vachhani <ravi.vachhani@kcsitglobal.com>
2020-09-09 18:47:15 +0530

    Upload Search changes (1 Remaining)

---
+++ src/library/navigation/goto/domains/ba/basler-beauty.at.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'basler-beauty.at',
+    timeout: 50000,
+    country: 'AT',
+    store: 'basler-beauty',
+    zipcode: '',
+  },
+};
