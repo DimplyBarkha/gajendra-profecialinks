ad1e9d493a26b9c85aa7c6eb7df7feb4a9d9b68d
Prasanna Barate <prasanna.barate@synerzip.com>
2020-09-09 16:17:14 +0530

    (#533) Fetched aggregate ratings and rating count.

--- src/library/navigation/goto/domains/ju/jumbo.ae.js
+++ src/library/navigation/goto/domains/ju/jumbo.ae.js
@@ -3,9 +3,16 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'jumbo.ae',
-    timeout: 100000,
+    timeout: null,
     country: 'AE',
     store: 'jumbo',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setAntiFingerprint(false);
+    const timeout = parameters.timeout ? parameters.timeout : 30000;
+    await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true });
   },
 };
