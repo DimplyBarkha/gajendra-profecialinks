5ae6b7ab195bd125e4e81b8b7d9a2dbdca8b10c0
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-09 12:30:48 +0530

    added some fields in the schema

--- src/library/product/details/stores/b/bol/NL/extract.js
+++ src/library/product/details/stores/b/bol/NL/extract.js
@@ -9,6 +9,22 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+    let videoSelector = null;
+    await context.evaluate(async function () {
+      let videoEle = document.querySelector("a[data-test='product-video']")
+      if (videoEle) {
+        videoEle.click();
+        console.log('video clicked')
+        videoSelector = "video[data-test='wsp-video-element']";
+      }
+    });
+    // if (true) {
+    //   console.log('came in here')
+    //   console.log(videoSelector)
+    //   await context.waitForFunction(function (videoSelector) {
+    //     return Boolean(document.querySelector(videoSelector));
+    //   }, { timeout: 30000 }, videoSelector);
+    // }
     return await context.extract(productDetails, { transform: transformParam });
   },
 };
