9e42eb37d9e8c81c9440b4572bd6ba71aa9b8898
vikas kyatannawar <vikas.k@osmosys.asia>
2020-09-09 11:49:10 +0530

    Added variants extractor

---
+++ src/library/product/details/stores/s/saturn/DE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'DE',
+    store: 'saturn',
+    domain: 'saturn.de',
+    zipcode: '',
+  },
+};
