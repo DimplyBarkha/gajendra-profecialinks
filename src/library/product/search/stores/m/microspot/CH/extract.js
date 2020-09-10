4db470b4a159897032229bb2102ee4871aecac13
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-09-10 15:28:57 +0530

    #553. write extractor logic for aggregateRating

--- src/library/product/search/stores/m/microspot/CH/extract.js
+++ src/library/product/search/stores/m/microspot/CH/extract.js
@@ -1,5 +1,48 @@
-const { transform } = require('../../../../shared');
+const { transform } = require('./shared');
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  await context.evaluate(async function () {
+    // @ts-ignore
+    const productInfo = JSON.parse(document.querySelector('script#INITIAL_STATE').innerText.trim()).products;
+    function addEleToDoc (key, value, code) {
+      const productCode = new RegExp(code);
+      const productsDiv = document.querySelectorAll('div.wQ1zdx._14LFJJ._1ryioq');
+      const productDiv = Array.from(productsDiv, ele => ele.innerHTML);
+      const filteredDiv = productDiv.filter(element => element.match(productCode));
+      const prodEle = document.createElement('div');
+      prodEle.id = key;
+      prodEle.textContent = value;
+      if (filteredDiv && filteredDiv.length) {
+        const doc = new DOMParser().parseFromString(filteredDiv[0], 'text/xml');
+        if (!doc.getElementById('rating')) {
+          const id = doc.querySelector('._2FaHUU').id;
+          if (id) {
+            document.getElementById(id).appendChild(prodEle);
+          }
+        }
+      }
+    }
+    if (productInfo) {
+      const info = Object.keys(productInfo);
+      for (var i = 0; i < info.length; i++) {
+        var code = info[i];
+        var item = productInfo[code].code;
+        var aggregateRating = productInfo[code].averageRating;
+        if (item) {
+          addEleToDoc('rating', `${aggregateRating}`, `${item}`);
+        }
+      }
+    }
+  });
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,4 +52,5 @@ module.exports = {
     domain: 'microspot.ch',
     zipcode: '',
   },
+  implementation,
 };
