873307f63d2ecca5f3193a5eb76439fe4856ca45
Ankit Kumar <ankit.k@osmosys.asia>
2020-09-10 21:40:06 +0530

    lint fixes

--- src/library/product/details/stores/c/coolblue/NL/createUrl.js
+++ src/library/product/details/stores/c/coolblue/NL/createUrl.js
@@ -5,14 +5,8 @@ module.exports = {
     domain: 'coolblue.nl',
     prefix: null,
     url: 'https://www.coolblue.nl/product/{id}',
-<<<<<<< HEAD
-    zipcode: '',
-    country: 'NL',
-    store: 'coolblue',
-=======
     country: 'NL',
     store: 'coolblue',
     zipcode: '',
->>>>>>> DS_Coolblue_core_variants
   },
 };
