038778e72c5989d7b7251b1f4ab24c1d0f8f149d
vikas kyatannawar <vikas.k@osmosys.asia>
2020-09-10 21:57:20 +0530

    Added mergeType optional parameter

--- src/library/product/search/action.js
+++ src/library/product/search/action.js
@@ -22,6 +22,11 @@ module.exports = {
       description: 'default results value.',
       optional: true,
     },
+    {
+      name: 'mergeType',
+      description: 'For merge rows results calculation.',
+      optional: true,
+    },
   ],
   inputs: [
     {
@@ -51,7 +56,7 @@ module.exports = {
     extract: 'action:product/search/extract',
   },
   path: './search/stores/${store[0:1]}/${store}/${country}/search',
-  implementation: async ({ keywords, Keywords, Brands, results }, { country, store, domain, zipcode, defaultResults }, context, { execute, extract, paginate }) => {
+  implementation: async ({ keywords, Keywords, Brands, results }, { country, store, domain, zipcode, defaultResults, mergeType }, context, { execute, extract, paginate }) => {
     results = results || defaultResults || 150;
     // TODO: consider moving this to a reusable function
     const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
@@ -86,7 +91,7 @@ module.exports = {
         // no results
         break;
       }
-      collected += count;
+      collected = (mergeType && (mergeType === 'MERGE_ROWS') && count) || (collected + count);
       console.log('Got more results', collected);
       page++;
     }
