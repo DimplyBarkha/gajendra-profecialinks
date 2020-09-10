41b77cbfd5d5c54509a1a8216e409cf0a18ff5fa
sivasoarlogic <siva@soarlogic.com>
2020-09-10 14:58:10 +0530

    search

--- src/library/product/search/stores/t/trademe/format.js
+++ src/library/product/search/stores/t/trademe/format.js
@@ -7,26 +7,18 @@
 const transform = (data) => {
     for (const { group } of data) {
       for (const row of group) {
-        if (row.thumbnail) {
-          let text = '';
-          row.thumbnail.forEach(item => {
-            text += `${item.text
-              .match(/\(([^)]+)\)/)[1]
-              }  `;
-          });
-          row.thumbnail = [
-            {
-              text: text.slice(),
-            },
-          ];
-        }
         if (row.productUrl) {
           row.productUrl.forEach(item => {
             item.text = "https://www.trademe.co.nz" + item.text;
           });
         }
-        if (row.Id) {
-          row.Id.forEach(item => {
+        if (row.thumbnail) {
+          row.thumbnail.forEach(item => {
+            item.text = item.text.match(/\(([^)]+)\)/)[1].slice(1,-1);
+          });
+        }
+        if (row.variantId) {
+          row.variantId.forEach(item => {
               item.text = item.text.replace(/\?.*$/g, '').trim();
               item.text = item.text.replace(/\D/g, ' ').trim();
           });
