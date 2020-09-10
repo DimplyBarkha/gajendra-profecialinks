60ce481b79e1617aa253db30a19fd847384cedcd
KCSPL\jatin <jatin@kcspl.co.in>
2020-09-10 15:27:21 +0530

    DS-core-appliancesonline pagination change

--- src/library/product/search/stores/a/appliancesonline/AU/paginate.js
+++ src/library/product/search/stores/a/appliancesonline/AU/paginate.js
@@ -1,4 +1,4 @@
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -6,31 +6,21 @@ async function implementation (
 ) {
   const { keywords, page, offset } = inputs;
   const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
-  
-  if (nextLinkSelector) {
 
-    const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector), nextLinkSelector);
-    if (!hasNextLink) {
-      return false;
-    }
-  }
+   let url = await context.evaluate(function () {
+    
+    const next = document.querySelector('pagination-template > ul > li.active').nextElementSibling
 
-  if(!openSearchDefinition){
-  const { pager } = dependencies;
-  const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
-  if (success) {
-    return true;
-  }
-}
-
-  let url = await context.evaluate(function () {
-    /** @type { HTMLLinkElement } */
-    const next = document.querySelector('head link[rel="next"]');
     if (!next) {
       return false;
     }
-    return next.href;
+
   });
+
+  const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector).nextElementSibling, 'pagination-template > ul > li.active ');
+  if (!hasNextLink) {
+    return false;
+  }
 
   if (!url && openSearchDefinition) {
     url = openSearchDefinition.template
@@ -43,19 +33,16 @@ async function implementation (
     return false;
   }
 
-  console.log('Going to url', url);
   await dependencies.goto({ url });
   if (loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
     }, { timeout: 20000 }, loadedSelector, noResultsXPath);
   }
-  console.log('Checking no results', noResultsXPath);
+
   return await context.evaluate(function (xp) {
     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-    console.log(xp, r);
     const e = r.iterateNext();
-    console.log(e);
     return !e;
   }, noResultsXPath);
 }
@@ -66,13 +53,11 @@ module.exports = {
     country: 'AU',
     store: 'appliancesonline',
     domain: 'appliancesonline.com.au',
-    nextLinkSelector: 'div.pagination > pagination-template > ul > li',
-    mutationSelector: null,
-    spinnerSelector: null,
+    nextLinkSelector: 'pagination-template > ul > li:not(.active)',
     loadedSelector: 'div.grid-container-flex',
     openSearchDefinition: {
       template: 'https://www.appliancesonline.com.au/search/{searchTerms}?currentpage={page}&sortkey=highestrated',
     },
   },
-  implementation
+  implementation,
 };
