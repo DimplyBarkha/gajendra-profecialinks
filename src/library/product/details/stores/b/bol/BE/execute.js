5179a3fe3ac5b26af899ae6c8d260b31a9ec4d20
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-10 15:52:06 +0530

    added navigatin and handled locally

--- src/library/product/details/stores/b/bol/BE/execute.js
+++ src/library/product/details/stores/b/bol/BE/execute.js
@@ -1,4 +1,24 @@
 
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log(JSON.stringify(inputs))
+  await context.goto(inputs.url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+  await context.evaluate(async function () {
+    let concentPopUp = document.querySelector("button[data-test='consent-modal-confirm-btn']");
+    if (concentPopUp) {
+      document.querySelector("button[data-test='consent-modal-confirm-btn']").click()
+    }
+    let country = document.querySelector('a[data-test="country-BE"]');
+    if (country) {
+      country.click();
+    }
+  });
+}
+
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -9,4 +29,5 @@ module.exports = {
     noResultsXPath: null,
     zipcode: '',
   },
+  implementation
 };
