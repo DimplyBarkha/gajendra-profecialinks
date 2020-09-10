e313c0ab00a8f4e1830316a5c7ed8d8cf5e37eef
srishivams <srishivams97@gmail.com>
2020-09-10 19:42:15 +0530

    added noResultsXpath for search

--- src/library/product/search/stores/f/fust/CH/execute.js
+++ src/library/product/search/stores/f/fust/CH/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     domain: 'fust.ch',
     url: 'https://www.fust.ch/de/search.html?searchtext={searchTerms}',
     loadedSelector: 'div[id="productlisting"]',
-    noResultsXPath: null,
+    noResultsXPath: '//div[@class="row"]//p[text()="Es konnte für Ihre Suche leider keine passenden Ergebnisse gefunden werden."]',
     zipcode: '',
   },
 };
