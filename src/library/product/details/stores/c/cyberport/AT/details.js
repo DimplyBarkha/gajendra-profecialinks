8f2f01da17c62cef821850a77d7c363911368063
Tejas Belani <tejas.belani@kcsitglobal.com>
2020-09-10 13:41:18 +0530

    pushed changs to local branch

---
+++ src/library/product/details/stores/c/cyberport/AT/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'AT',
+    store: 'cyberport',
+    domain: 'cyberport.at',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/c/cyberport/AT/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'AT',
+    store: 'cyberport',
+    domain: 'cyberport.at',
+    loadedSelector: 'body',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
