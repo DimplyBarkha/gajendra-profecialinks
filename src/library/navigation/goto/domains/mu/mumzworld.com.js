873308821d95cedc9f3b639509ad01b7db4e0e34
unknown <ganesh.working@gmail.com>
2020-09-10 15:14:15 +0530

    set timeout null

--- src/library/navigation/goto/domains/mu/mumzworld.com.js
+++ src/library/navigation/goto/domains/mu/mumzworld.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'mumzworld.com',
-    timeout: 500000,
+    timeout: null,
     country: 'AE',
     store: 'mumzworld',
     zipcode: '',
