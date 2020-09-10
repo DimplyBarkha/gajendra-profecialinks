1cc459f7671fc470b515a74ba34b0e3f9bed30e8
irfan shaikh <irfan.shaikh@synerzip.com>
2020-09-10 19:07:52 +0530

    #546. Added variant extractor logic

---
+++ src/library/product/details/stores/i/interdiscount/CH/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'CH',
+    store: 'interdiscount',
+    domain: 'interdiscount.ch',
+    zipcode: '',
+  },
+};
