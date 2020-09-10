b70a25bcf59eb214f4c20643a66d5cc2a59ee873
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-09-10 14:25:21 -0400

    Reno Depot Core

--- src/library/product/details/stores/r/reno-depot/CA/extract.js
+++ src/library/product/details/stores/r/reno-depot/CA/extract.js
@@ -43,23 +43,33 @@ async function implementation (
       });
     }
 
-    addHiddenDiv('price', '$' + document.querySelector('.integer').innerText + '.' + document.querySelector('.decimal').innerText);
+    const alternateImages = [];
+    if (document.getElementById('product__image-gallery')) {
+      document.getElementById('product__image-gallery').querySelectorAll('img').forEach((img, ind) => {
+        if (ind > 0) {
+          alternateImages.push(img.getAttribute('src').replace('_S', '_L'));
+        }
+      })
+    }
+    if (alternateImages.length) {
+      addHiddenDiv('alternateImages', alternateImages.join(' | '));
+    }
+
+    addHiddenDiv('price', '$' + document.querySelector('.integer').innerText + (document.querySelector('.decimal') ?  '.' + document.querySelector('.decimal').innerText : ''));
     if (document.querySelector('.linethrough')) {
       addHiddenDiv('listPrice', document.querySelector('.linethrough').innerText.trim());
     } else {
-      addHiddenDiv('listPrice', '$' + document.querySelector('.integer').innerText + '.' + document.querySelector('.decimal').innerText);
+      addHiddenDiv('listPrice', '$' + document.querySelector('.integer').innerText + (document.querySelector('.decimal') ? '.' + document.querySelector('.decimal').innerText : ''));
     }
 
     let inStore = false;
     let delivery = false;
-    document.querySelectorAll('.availabilityMsg').forEach(msg => {
-      if (msg.innerText.includes('In-store pick-up')) {
-        inStore = true;
-      }
-      if (msg.innerText.includes('Home Delivery')) {
-        delivery = true;
-      }
-    });
+    if (document.querySelector('.availability.availMsgInStock.store')) {
+      inStore = true;
+    }
+    if (document.querySelector('.availability.availMsgInStock.parcel') || document.querySelector('.availability.availMsgTruck')) {
+      delivery = true;
+    }
 
     if (delivery) {
       addHiddenDiv('availabilityText', 'In Stock');
@@ -70,6 +80,67 @@ async function implementation (
     }
 
     addHiddenDiv('bulletCount', 0);
+
+    let specifications = [];
+    document.querySelectorAll('.title').forEach(el => {
+      const parent = el.parentElement;
+      if (parent && el.innerText.includes('Weight')) {
+        el.remove();
+        addHiddenDiv('weightNet', parent.innerText.trim());
+      }
+      if (parent && el.innerText.includes('Color')) {
+        el.remove();
+        addHiddenDiv('color', parent.innerText.trim());
+      }
+      if (parent && el.innerText.includes('Warranty')) {
+        el.remove();
+        addHiddenDiv('warranty', parent.innerText.trim());
+      }
+      if (parent && el.innerText.includes('Height')) {
+        el.remove();
+        specifications.push(parent.innerText.trim());
+      }
+      if (parent && el.innerText.includes('Width')) {
+        el.remove();
+        specifications.push(parent.innerText.trim());
+      }
+      if (parent && el.innerText.includes('Depth')) {
+        el.remove();
+        specifications.push(parent.innerText.trim());
+      }
+    });
+
+    addHiddenDiv('specifications', specifications.join(' | '));
+
+    if (document.querySelector('div[itemprop="ratingValue"]')) {
+      addHiddenDiv('aggregatedRatingText', document.querySelector('div[itemprop="ratingValue"]').innerText + ' out of 5');
+    }
+
+    let shippingInfo = '';
+    if (document.querySelector('.availabilityCommitmentMsg')) {
+      shippingInfo += document.querySelector('.availabilityCommitmentMsg').innerText + ' ';
+    }
+    if (document.querySelector('.show-EN.inventoryParcelMsgPromo')) {
+      shippingInfo += document.querySelector('.show-EN.inventoryParcelMsgPromo').innerText;
+    }
+    addHiddenDiv('shippingInfo', shippingInfo);
+
+    if (document.getElementById('wc-power-page')) {
+      console.log('hasManufacturerInfo1', document.getElementById('wc-power-page').innerText);
+      addHiddenDiv('manufacturerDescription', document.getElementById('wc-power-page').innerText);
+    }
+
+    addHiddenDiv('terms', 'No');
+    addHiddenDiv('privacyPolicy', 'Yes');
+    addHiddenDiv('customerServiceAvailability', 'Yes');
+    addHiddenDiv('rotateInfo', 'No');
+    addHiddenDiv('countryOfOrigin', 'Canada');
+
+    if (document.querySelector('.zoom_btn')){
+      addHiddenDiv('zoomInfo', 'Yes');
+    } else {
+      addHiddenDiv('zoomInfo', 'No');
+    }
 
   });
 
