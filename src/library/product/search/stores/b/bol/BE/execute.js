ded27e7194d72058499eb7a08494f501ba4f6555
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-10 15:59:36 +0530

    added search extractor for bol.com for be

---
+++ src/library/product/search/stores/b/bol/BE/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'BE',
+    store: 'bol',
+    domain: 'bol.com',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/b/bol/BE/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'BE',
+    store: 'bol',
+    domain: 'bol.com',
+    zipcode: '',
+  },
+};
