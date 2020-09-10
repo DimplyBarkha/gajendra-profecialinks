85bd05080044b515957e1611a7f0d569c08e4abd
Tomek887 <70725911+Tomek887@users.noreply.github.com>
2020-09-10 14:01:29 +0200

    created extractor core for elkjop.no

---
+++ src/library/product/details/stores/e/elkjop/NO/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'NO',
+    store: 'elkjop',
+    domain: 'elkjop.no',
+    loadedSelector: 'div#site-wrapper',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
