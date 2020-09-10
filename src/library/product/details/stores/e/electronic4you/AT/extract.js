c82d5e1e6ca1d6a226f74f8bb1825bf858125a65
Karolina <karolina.urbanska@tdcm.io>
2020-09-10 14:36:41 +0200

    Improved selectors

--- src/library/product/details/stores/e/electronic4you/AT/extract.js
+++ src/library/product/details/stores/e/electronic4you/AT/extract.js
@@ -34,6 +34,12 @@ module.exports = {
       if (techDetails) {
         addElementToDocument('desc_techDetails', techDetails.replace(/\n|•/g, '').replace(/\s{2,}/g, ' '));
       }
+      const manufacturerDesc = document.querySelector('div#flix-dyson-inpage')
+        // @ts-ignore
+        ? document.querySelector('div#flix-dyson-inpage').innerText : '';
+      if (manufacturerDesc) {
+        addElementToDocument('desc_manufacturer', manufacturerDesc.replace(/•/g, '||').replace(/\n|\s{2,}|-{1,}/g, ' '));
+      }
       const manufacturerDesc1 = document.querySelector('div.features-list-product-page')
         // @ts-ignore
         ? document.querySelector('div.features-list-product-page').innerText : '';
@@ -46,6 +52,26 @@ module.exports = {
       if (manufacturerDesc2) {
         addElementToDocument('desc_manufacturer2', manufacturerDesc2.replace(/•/g, '||').replace(/\n|\s{2,}/g, ' '));
       }
+      const desc3Xpath = document.evaluate("//h3[contains(text(),'Details')]/following-sibling::div[contains(@class,'std')]/p[1]", document, null, XPathResult.STRING_TYPE, null);
+      const desc3 = desc3Xpath ? desc3Xpath.stringValue : '';
+      if (desc3) {
+        addElementToDocument('desc_manufacturer3', desc3.replace(/•/g, '||').replace(/\s{2,}|\n/g, ' '));
+      }
+      const desc4Xpath = document.evaluate("//h3[contains(text(),'Details')]/following-sibling::div[contains(@class,'std')]/ul", document, null, XPathResult.STRING_TYPE, null);
+      const desc4 = desc4Xpath ? desc4Xpath.stringValue : '';
+      if (desc4) {
+        addElementToDocument('desc_manufacturer4', desc4.replace(/•/g, '||').replace(/\s{2,}|\n/g, ' '));
+      }
+      const desc5Xpath = document.evaluate("//h3[contains(text(),'Details')]/following-sibling::div[contains(@class,'std')]/div", document, null, XPathResult.ANY_TYPE, null);
+      // eslint-disable-next-line prefer-const
+      if (desc5Xpath) {
+        let parts = [];
+        for (let part = desc5Xpath.iterateNext(); part; part = desc5Xpath.iterateNext()) {
+          parts.push(part.innerText);
+        }
+        const desc5 = parts.join(', ');
+        addElementToDocument('desc_manufacturer5', desc5.replace(/•/g, '||').replace(/\s{2,}|\n/g, ' '));
+      }
       const warrantyXpath = document.evaluate("//h3[contains(text(), 'arantie')]/..", document, null, XPathResult.STRING_TYPE, null);
       const warranty = warrantyXpath ? warrantyXpath.stringValue : '';
       if (warranty) {
@@ -53,12 +79,14 @@ module.exports = {
       }
       const dimensions = document.evaluate("//th[contains(text(),'Breite') or contains(text(),'Höhe') or contains(text(),'Tiefe')]/following-sibling::td", document, null, XPathResult.ANY_TYPE, null);
       // eslint-disable-next-line prefer-const
-      let nodes = [];
-      for (let node = dimensions.iterateNext(); node; node = dimensions.iterateNext()) {
-        nodes.push(node.innerText);
+      if (dimensions) {
+        let nodes = [];
+        for (let node = dimensions.iterateNext(); node; node = dimensions.iterateNext()) {
+          nodes.push(node.innerText);
+        }
+        const specifications = nodes.join(' x ');
+        addElementToDocument('specifications', specifications);
       }
-      const specifications = nodes.join(' x ');
-      addElementToDocument('specifications', specifications);
       const pdfPresent = document.querySelector('a[title="Produktdatenblatt anzeigen"]')
         // @ts-ignore
         ? document.querySelector('a[title="Produktdatenblatt anzeigen"]').getAttribute('href') : '';
