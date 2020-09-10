61560a791747a413226e20b77173e1c6238a1c5e
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-09-10 12:43:49 -0400

    Reno Depot Updates

--- src/library/navigation/goto/domains/re/renodepot.com.js
+++ src/library/navigation/goto/domains/re/renodepot.com.js
@@ -5,4 +5,12 @@ module.exports = {
     domain: 'renodepot.com',
     store: 'reno-depot',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 50000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
--- src/library/product/details/stores/r/reno-depot/CA/extract.js
+++ src/library/product/details/stores/r/reno-depot/CA/extract.js
@@ -6,6 +6,73 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+
+  function stall(ms) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve()
+      }, ms);
+    })
+  }
+
+  await stall (3000);
+
+  await context.evaluate(function() {
+    document.cookie = "RenoWCCookie.province=QC";
+    document.cookie = "RenoWCCookie.region.selected=QUEBEC";
+    document.cookie = "RenoWCCookie.stores.selected=73020";
+    location.reload();
+  });
+
+  await stall (3000);
+
+  await context.evaluate(function() {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    addHiddenDiv('pageTimeStamp', new Date());
+    addHiddenDiv('url', window.location.href);
+
+    if (document.querySelector('.breadcrumb')) {
+      document.querySelector('.breadcrumb').querySelectorAll('a').forEach(category => {
+        addHiddenDiv('category', category.innerText);
+      });
+    }
+
+    addHiddenDiv('price', '$' + document.querySelector('.integer').innerText + '.' + document.querySelector('.decimal').innerText);
+    if (document.querySelector('.linethrough')) {
+      addHiddenDiv('listPrice', document.querySelector('.linethrough').innerText.trim());
+    } else {
+      addHiddenDiv('listPrice', '$' + document.querySelector('.integer').innerText + '.' + document.querySelector('.decimal').innerText);
+    }
+
+    let inStore = false;
+    let delivery = false;
+    document.querySelectorAll('.availabilityMsg').forEach(msg => {
+      if (msg.innerText.includes('In-store pick-up')) {
+        inStore = true;
+      }
+      if (msg.innerText.includes('Home Delivery')) {
+        delivery = true;
+      }
+    });
+
+    if (delivery) {
+      addHiddenDiv('availabilityText', 'In Stock');
+    } else if (inStore) {
+      addHiddenDiv('availabilityText', 'In Store Only');
+    } else {
+      addHiddenDiv('availabilityText', 'Out of Stock');
+    }
+
+    addHiddenDiv('bulletCount', 0);
+
+  });
+
   return await context.extract(productDetails, { transform });
 }
 
