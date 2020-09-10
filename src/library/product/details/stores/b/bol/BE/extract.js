ad9322a8f1bde173cc1c2cf4e0e3737a320ef313
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-10 14:42:32 +0530

    fixed extractr issues

--- src/library/product/details/stores/b/bol/BE/extract.js
+++ src/library/product/details/stores/b/bol/BE/extract.js
@@ -1,11 +1,36 @@
-
+const { transform } = require('./shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'BE',
     store: 'bol',
-    transform: null,
+    transform,
     domain: 'bol.com',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+
+    async function callWait() {
+      await context.waitForSelector("video[data-test='wsp-video-element']")
+    }
+
+    const waitForVideo = await context.evaluate(async function () {
+      let videoEle = document.querySelector("a[data-test='product-video']")
+      if (videoEle) {
+        videoEle.click();
+        return true;
+      }
+      return false;
+    });
+    
+    try {
+      if (waitForVideo) {
+        await callWait();
+      }
+    } catch (err) {
+      console.log(err.message)
+    }
+ 
+    return await context.extract(productDetails, { transform: transformParam });
   },
 };
---
+++ src/library/product/details/stores/b/bol/BE/shared.js
@@ -0,0 +1,94 @@
+/**
+*
+* @param {ImportIO.Group[]} data
+* @returns {ImportIO.Group[]}
+*/
+const transform = (data) => {
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.priceSufix) {
+        const sufixVal = row.priceSufix[0].text;
+        if (sufixVal === '-') {
+          row.price[0].text = row.price[0].text.replace(',-', '');
+        }
+        delete row.priceSufix
+      }
+
+      if (row.availabilityText) {
+        const prodInfo = JSON.parse(row.availabilityText[0].text);
+        if (prodInfo && prodInfo.offers && prodInfo.offers.availability) {
+          row.availabilityText[0].text = prodInfo.offers.availability
+        } else {
+          delete row.availabilityText
+        }
+      }
+
+      if (row.gtin) {
+        const prodInfo = JSON.parse(row.gtin[0].text);
+        if (prodInfo && prodInfo.gtin13) {
+          row.gtin[0].text = prodInfo.gtin13
+        } else {
+          delete row.gtin
+        }
+      }
+
+      if (row.sku) {
+        const prodInfo = JSON.parse(row.sku[0].text);
+        if (prodInfo && prodInfo.pdpTaxonomyObj && prodInfo.pdpTaxonomyObj.productInfo[0] && prodInfo.pdpTaxonomyObj.productInfo[0].productId) {
+          row.sku[0].text = prodInfo.pdpTaxonomyObj.productInfo[0].productId
+        } else {
+          delete row.sku
+        }
+      }
+
+      if (row.variantId) {
+        const prodInfo = JSON.parse(row.variantId[0].text);
+        if (prodInfo && prodInfo.productID) {
+          row.variantId[0].text = prodInfo.productID
+        } else {
+          delete row.variantId
+        }
+      }
+
+      if (row.aggregateRating) {
+        row.aggregateRating[0].text = row.aggregateRating[0].text.replace('.', ',')
+      }
+
+      if (row.shippingInfo) {
+        row.shippingInfo[0].text = row.shippingInfo[0].text.replace('Verkoop door:', '')
+      }
+
+      if (!row.shippingDimensions && row.shippingDimensionsSplit[0].text !== 'xx') {
+        row.shippingDimensions = [{ text: row.shippingDimensionsSplit[0].text }];
+        delete row.shippingDimensionsSplit;
+      }
+
+      if (row.specifications) {
+        row.specifications[0].text = row.specifications[0].text.replace('Productspecificaties', '').replace('Toon meer', '').replace('Toon minder', '')
+      }
+    }
+  }
+
+  // Clean up data
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
+    .trim();
+
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+
+  return data;
+};
+
+module.exports = { transform };
