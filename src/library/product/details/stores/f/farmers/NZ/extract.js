c50780ccc87bc65730e299fe0f2900ea2eb90453
vinod chavan <vinodchavan102@gmail.com>
2020-09-10 19:10:07 +0530

    #555. fixed farmers core extractors

--- src/library/product/details/stores/f/farmers/NZ/extract.js
+++ src/library/product/details/stores/f/farmers/NZ/extract.js
@@ -8,4 +8,31 @@ module.exports = {
     domain: 'farmers.co.nz',
     zipcode: '',
   },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async () => {
+      var src = '';
+      const videoEle = document.querySelector('a.phone-thumb-overlay-YouTube');
+      if (videoEle) {
+        videoEle.click();
+        src = document.querySelector('div.ish-product-photo iframe').getAttribute('src');
+      }
+      if (src) {
+        addHiddenDiv('video-url', src);
+      }
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
--- src/library/product/details/stores/f/farmers/NZ/format.js
+++ src/library/product/details/stores/f/farmers/NZ/format.js
@@ -6,6 +6,20 @@
 const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
+      if (row.specification) {
+        var specificationValues = {};
+        var spec = '';
+        row.specification.forEach((ele) => {
+          var data = ele.text.split('\n \n');
+          if (data.length !== 0) {
+            specificationValues[data[0].trim()] = data[1].trim();
+            spec += ' || ' + data[0].trim() + ': ' + data[1].trim();
+          }
+        });
+        row.specification = [{
+          text: spec.trim(),
+        }];
+      }
       if (row.availabilityText) {
         row.availabilityText[0].text = row.availabilityText[0].text === 'Out of Stock' ? row.availabilityText[0].text : 'In Stock';
       };
@@ -39,6 +53,9 @@ const transform = (data) => {
         if (row.category[0].text === 'Home') {
           row.category.shift();
         }
+        if (row.category[row.category.length - 1].text === row.name[0].text) {
+          row.category.pop();
+        }
       }
       if (row.sku) {
         row.sku[0].text = row.sku[0].text.substring(1);
@@ -48,6 +65,33 @@ const transform = (data) => {
       }
       if (row.firstVariant) {
         row.firstVariant[0].text = row.firstVariant[0].text.substring(1);
+      }
+      if (row.price) {
+        row.price[0].text = ((row.price[0].text.split(' ')).slice(-1))[0];
+      }
+      if (row.listPrice) {
+        row.listPrice[0].text = ((row.listPrice[0].text.split(' ')).slice(-1))[0];
+      }
+      if (row.description) {
+        var desc = '';
+        row.description.forEach(element => {
+          desc += '' + element.text;
+        });
+        row.additionalDescBulletInfo.forEach((ele) => {
+          desc += ' || ' + ele.text;
+        });
+        row.description = [{
+          text: desc.trim(),
+        }];
+      }
+      if (row.weightNet && specificationValues.hasOwnProperty('Weight')) {
+        row.weightNet[0].text = specificationValues['Weight'];
+      }
+      if (row.manufacturerDescription) {
+        row.manufacturerDescription = row.manufacturerDescription.map((ele) => {
+          ele.text = ele.text.replace(/\n/g, '');
+          return ele;
+        });
       }
     }
   }
