5e090629e675ff041b1e83aa553a5243f6aa73c8
prateek.s <psc3018@gmail.com>
2020-09-10 11:50:01 +0530

    Xpaths Updated

--- src/library/navigation/goto/domains/jb/jbhifi.com.au.js
+++ src/library/navigation/goto/domains/jb/jbhifi.com.au.js
@@ -9,10 +9,16 @@ module.exports = {
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.setBlockAds(false);
-    await context.setLoadAllResources(true);
-    await context.setLoadImages(true);
-    await context.setJavaScriptEnabled(true);
+    const currentUrl = url;
+    const result = await context.evaluate((currentUrl)=>{
+      return currentUrl.search('/products/');
+    },currentUrl)
+    if(result > 0){
+      await context.setBlockAds(false);
+      await context.setLoadAllResources(true);
+      await context.setLoadImages(true);
+      await context.setJavaScriptEnabled(true);
+    }
     await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
