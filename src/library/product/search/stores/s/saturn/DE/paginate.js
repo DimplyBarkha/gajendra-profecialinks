0a873d66d1448b405518c07b4d82c70363e67710
vikas kyatannawar <vikas.k@osmosys.asia>
2020-09-10 19:31:20 +0530

    #587 updated pagination

--- src/library/product/search/stores/s/saturn/DE/paginate.js
+++ src/library/product/search/stores/s/saturn/DE/paginate.js
@@ -1,19 +1,18 @@
 
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  return false;
-}
-
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'DE',
     store: 'saturn',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: 'div[data-test="mms-search-srp-productlist"]',
+    noResultsXPath: '//div[contains(@class,"ZeroResultsView")] | /html[not(//meta[@property="og:url"][contains(@content,"search.html")])] | /html[not(//div[@data-test="mms-search-srp-productlist"]//div[@data-test="mms-search-srp-productlist-item"])]',
+    openSearchDefinition: {
+      template: 'https://www.saturn.de/de/search.html?query={searchTerms}&page={page}',
+    },
     domain: 'saturn.de',
+    zipcode: '',
   },
-  implementation,
 };
