0fa3caafe3edec0a487dd94538ad7e2934a96dce
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-10 10:30:14 +0530

    added be extractor for bol.com

---
+++ src/library/product/details/stores/b/bol/BE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'BE',
+    store: 'bol',
+    transform: null,
+    domain: 'bol.com',
+    zipcode: '',
+  },
+};
