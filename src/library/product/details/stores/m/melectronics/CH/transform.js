65d05c8e9c87400d82f4df2a2ca798dc4a6f2ee5
Syed Abuthahir <sayed@profecialinks.com>
2020-09-10 22:40:21 +0530

    issue fixed

--- src/library/product/details/stores/m/melectronics/CH/transform.js
+++ src/library/product/details/stores/m/melectronics/CH/transform.js
@@ -1,18 +1,7 @@
 const transform = (data) => {
     for (const { group} of data) {
         for (const row of group) {
-            if (row.pageTimestamp) {
-                var d = new Date();
-                row.pageTimestamp = d.toISOString();
-            }
-
-            if (row.technicalInformationPdfPresent) {
-                row.technicalInformationPdfPresent = 'No';
-            }
-
-            if (row.Image360Present) {
-                row.Image360Present = 'No';
-            }
+           
             if (row.descriptionBullets) {
                 row.descriptionBullets= row.descriptionBullets.length
             }
@@ -22,8 +11,12 @@ const transform = (data) => {
                   item.text  = item.text.replace(/(\s*\n\s*)/g, ' | ').trim();
               });
           }
-  
-  
+       
+          if (row.category) {
+            row.category.forEach(item => {
+                item.text  = item.text.replace(/(\s*\n\s*)/g, ' > ').trim();
+            });
+        }
   
         }
       }
