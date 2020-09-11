7978be25add012a517f2dac584fa734b1ed879fe
Prasanna Barate <prasanna.barate@synerzip.com>
2020-09-11 17:06:36 +0530

    (#589) Added code to fetch product videos.

--- src/library/product/details/stores/m/monclick/IT/extract.js
+++ src/library/product/details/stores/m/monclick/IT/extract.js
@@ -8,4 +8,15 @@ module.exports = {
     domain: 'monclick.it',
     zipcode: '',
   },
+  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 5000))
+      await context.click('div[class*="image-content-gallery"] li[class*="gallery-video"]')
+      await context.waitForSelector('div[class*=product-gallery-overlay]')
+    } catch (e) {
+      console.log(e)
+    }
+    await new Promise(resolve => setTimeout(resolve, 10000))
+    return await context.extract(productDetails, { transform });
+  },
 };
