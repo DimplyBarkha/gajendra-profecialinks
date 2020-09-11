6c6922823216d24af24b530714dd5fac800917c2
Satyam <satyam.rathod@synerzip.com>
2020-09-11 13:35:16 +0530

    #470 added RPC as a input and added antifingerprint

--- src/library/product/details/stores/u/ulta/US/createUrl.js
+++ src/library/product/details/stores/u/ulta/US/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'ulta.com',
     prefix: null,
-    url: null,
+    url: 'https://www.ulta.com/ulta/a/_/Ntt-{id}/Nty-1?ciSelector=searchResults',
     country: 'US',
     store: 'ulta',
     zipcode: '',
