d1e2ce0fe46f4205eb994a327a6ec20212d062c5
Rafal Perkowski <rafalpero1@gmail.com>
2020-09-11 13:00:19 +0200

    More changes

--- src/library/product/details/stores/k/krefel/BE/extract.js
+++ src/library/product/details/stores/k/krefel/BE/extract.js
@@ -1,11 +1,28 @@
 
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'BE',
     store: 'krefel',
-    transform: null,
+    transform: cleanUp,
     domain: 'krefel.be',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      const scriptWithInfo = document.evaluate("//script[contains(text(), 'availability')]", document, null, XPathResult.STRING_TYPE, null);
+      const regexAvailability = /"availability":\s"([A-z]+)"/g;
+      const regexRating = /"ratingValue":\s"([\d,]+)"/g;
+      const foundAvailability = regexAvailability.exec(scriptWithInfo.stringValue);
+      const foundRatingValue = regexRating.exec(scriptWithInfo.stringValue);
+      if (foundAvailability) {
+        document.body.setAttribute('availability', foundAvailability[1]);
+      }
+      if (foundRatingValue) {
+        document.body.setAttribute('ratingValue', foundRatingValue[1].replace(',', '.'));
+      }
+    });
+    await context.extract(productDetails);
   },
 };
