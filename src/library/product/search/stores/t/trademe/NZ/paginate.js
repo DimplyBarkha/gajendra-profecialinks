0a499da894512ef9ebcc2a0ae937423699c1f929
sivasoarlogic <siva@soarlogic.com>
2020-09-11 21:15:21 +0530

    search

--- src/library/product/search/stores/t/trademe/NZ/paginate.js
+++ src/library/product/search/stores/t/trademe/NZ/paginate.js
@@ -7,7 +7,7 @@ module.exports = {
     nextLinkSelector: 'li#o-pagination__nav-item ng-star-inserted',
     mutationSelector: 'h3#tm-search-header-result-count__heading ng-star-inserted',
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector:'div#tm-marketplace-search-results__container',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'trademe.co.nz',
