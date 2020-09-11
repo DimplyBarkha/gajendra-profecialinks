99d5c184b97686a30aa5f2c41113be07615990ad
sivasoarlogic <siva@soarlogic.com>
2020-09-11 16:39:45 +0530

    core

--- src/library/product/details/stores/k/komputronik/format.js
+++ src/library/product/details/stores/k/komputronik/format.js
@@ -7,18 +7,7 @@
 const transform = (data) => {
     for (const { group } of data) {
       for (const row of group) {
-        if (row.descriptionBullets) {
-          let text = '';
-          row.descriptionBullets.forEach(item => {
-            text += `${item.text.replace(/\n/g, '||')}  `;
-          });
-          row.descriptionBullets = [
-            {
-              text: text.slice(0, -4),
-            },
-          ];
-        }
-
+  
         if (row.category) {
           row.category.forEach(item => {
               item.text = item.text.replace(/\s\n\s\n\s/g, ' > ').trim();
@@ -28,6 +17,12 @@ const transform = (data) => {
          if (row.variants) {
           row.variants.forEach(item => {
               item.text = item.text.replace(/\n/g, ' | ').trim();
+          });
+         }
+
+         if (row.gtin) {
+          row.gtin.forEach(item => {
+            item.text = item.text.match(/[^[\]]+(?=])/g);
           });
          }
 
