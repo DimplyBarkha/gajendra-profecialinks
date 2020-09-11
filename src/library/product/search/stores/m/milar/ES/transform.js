eeb1ff98c793cfc4fa5370fc7565cb7e4548ed77
Syed Abuthahir <sayed@profecialinks.com>
2020-09-11 12:14:58 +0530

    id changed

--- src/library/product/search/stores/m/milar/ES/transform.js
+++ src/library/product/search/stores/m/milar/ES/transform.js
@@ -2,8 +2,14 @@ const transform = (data) => {
     for (const { group} of data) {
         for (const row of group) {
             if (row.id) {
-                 var string = row.id[0].text.split(' ').splice(-2).toString();
-                row.id = string.replace(/,/g , ' ');
+                 var string = row.id[0].text.split('/').splice(-4).toString();
+                 var lastIndex = string.lastIndexOf(",");
+                 var text = string.substring(0, lastIndex);
+                row.id = text.replace(/,/g , '');
+            }
+
+            if(row.productUrl) {
+                row.productUrl = 'https://www.milar.es'.concat(row.productUrl[0].text)
             }
         }
       }
@@ -11,3 +17,4 @@ const transform = (data) => {
   };
   
   module.exports = { transform } 
+  
