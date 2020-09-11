8181ff91c707d31685cfbf5fc99b09fc2c317555
Tomek887 <70725911+Tomek887@users.noreply.github.com>
2020-09-11 14:50:29 +0200

    additional exclusion for no search results

--- src/library/product/search/stores/e/elkjop/NO/execute.js
+++ src/library/product/search/stores/e/elkjop/NO/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'NO',
     store: 'elkjop',
     domain: 'elkjop.no',
-    url: "https://www.elkjop.no/search?SearchTerm={searchTerms}&search=&searchResultTab=",
+    url: "https://www.elkjop.no/search?SearchTerm='{searchTerms}'&search=&searchResultTab=",
     loadedSelector: 'html body',
-    noResultsXPath: '//p[@id="no-search-result"]',
+    noResultsXPath: '//p[@id="no-search-result"]|//div[@id="site-wrapper"]',
     zipcode: '',
   },
 };
