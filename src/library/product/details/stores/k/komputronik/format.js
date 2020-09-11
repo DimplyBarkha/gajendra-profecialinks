b73d83130f7ffef5bac70f4cf07fa8a9d8345c05
sivasoarlogic <siva@soarlogic.com>
2020-09-11 14:31:29 +0530

    core

--- src/library/product/details/stores/k/komputronik/format.js
+++ src/library/product/details/stores/k/komputronik/format.js
@@ -18,92 +18,20 @@ const transform = (data) => {
             },
           ];
         }
+
         if (row.category) {
-            let text = '';
-            row.category.forEach(item => {
-              text += `${item.text.replace(/\s\n\s\n\s/g, '->')}  `;
-            });
-            row.category = [
-              {
-                text: text.slice(0, -4),
-              },
-            ];
-          }
-          if (row.variants) {
-            let text = '';
-            row.variants.forEach(item => {
-              text += `${item.text.replace(/\n/g, ' | ')}  `;
-            });
-            row.variants = [
-              {
-                text: text.slice(0, -4),
-              },
-            ];
-          }
-          if (row.lbb) {
-            let text = '';
-            row.lbb.forEach(item => {
-              text += `${item.text.replace(/\n/g, '')}  `;
-              
-            });
-            row.lbb = [
-              {
-                text: text.slice(0, -4),
-              },
-            ];
-          }
-          if (row.description) {
-            let text = '';
-            row.description.forEach(item => {
-              text += `${item.text
-                .replace(/\n\s*\n\s*\n\s*\n\s*/g, ' || ')
-                .replace(/\n\s*\n\s*/g, ' ')}  `;
-            });
-            row.description = [
-              {
-                text: text.trim(),
-              },
-            ];
-          }
-          if (row.specifications) {
-            let text = '';
-            row.specifications.forEach(item => {
-                text += `${item.text
-                    .replace(/\n\s*\n\s*\n\s*\n\s*/g, ' || ')
-                    .replace(/\n\s*\n\s*/g, ' ')}  `;
-            });
-            row.specifications = [
-              {
-                text: text.slice(0, -4),
-              },
-            ];
-          }
-          if (row.productOtherInformation) {
-            let text = '';
-            row.productOtherInformation.forEach(item => {
-                text += `${item.text
-                    .replace(/\n\s*\n\s*\n\s*\n\s*/g, ' || ')
-                    .replace(/\n\s*\n\s*/g, ' ')}  `;
-            });
-            row.productOtherInformation = [
-              {
-                text: text.slice(0, -4),
-              },
-            ];
-          }
-          if (row.manufacturerDescription) {
-            let text = '';
-            row.manufacturerDescription.forEach(item => {
-                text += `${item.text
-                    .replace(/\n\s*\n\s*\n\s*\n\s*/g, ' || ')
-                    .replace(/\n\s*\n\s*/g, ' ')}  `;
-            });
-            row.manufacturerDescription = [
-              {
-                text: text.slice(0, -4),
-              },
-            ];
-          }
+          row.category.forEach(item => {
+              item.text = item.text.replace(/\s\n\s\n\s/g, ' > ').trim();
+          });
+         }
+
+         if (row.variants) {
+          row.variants.forEach(item => {
+              item.text = item.text.replace(/\n/g, ' | ').trim();
+          });
+         }
+
+
       }
       
     }
