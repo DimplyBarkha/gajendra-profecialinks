a381f147a3666ddcd79c2d85886fa0eef8406104
Rafal Perkowski <rafalpero1@gmail.com>
2020-09-11 16:48:27 +0200

    Commit search krefel

--- src/library/product/search/stores/k/krefel/BE/extract.js
+++ src/library/product/search/stores/k/krefel/BE/extract.js
@@ -8,4 +8,23 @@ module.exports = {
     domain: 'krefel.be',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    const hoverDivs = await context.evaluate(async function (y) {
+      const allProducts = document.querySelectorAll('div[class^="col-md-4"]');
+      hoverDivs[y].setAttribute('abc', `${y}`);
+      return allProducts;
+    });
+    for (let y = 1; hoverDivs.length <= y; y++) {
+      hoverDivs;
+    }
+    await context.evaluate(async function () {
+      const allProducts = document.querySelectorAll('div[class^="col-md-4"]');
+      let x;
+      for (x = 0; allProducts.length - 1 >= x; x++) {
+        allProducts[x].setAttribute('count', `${x + 1}`);
+      }
+    });
+
+    return await context.extract(productDetails);
+  },
 };
