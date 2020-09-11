37dca5bca4473c2fe6256ba25254512cc3ca13d7
sushma-mangalapally <sushma.mangalapally@import.io>
2020-09-11 12:53:14 -0700

    Modify search url xpath

--- src/library/product/search/stores/e/expert/DE/extract.js
+++ src/library/product/search/stores/e/expert/DE/extract.js
@@ -1,5 +1,29 @@
 const { transform } = require('../transform');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
 
+  async function addHiddenInfo (elementID, content) {
+    await context.evaluate(function (elementID, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = elementID;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }, elementID, content);
+  }
+  const refererUrl = await context.evaluate(async function () {
+    console.log(window.location.href);
+    return window.location.href;
+  });
+  addHiddenInfo('ii_refererUrl', refererUrl);
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,6 +33,7 @@ module.exports = {
     domain: 'expert.de',
     zipcode: '',
   },
+  implementation,
 };
 
 // const { transform } = require('./transform');
