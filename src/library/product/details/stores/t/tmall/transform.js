91f52ff684586a7bd42e58917f08775889d93037
judhisahoo2009 <judhisahoo@hotmail.com>
2020-09-11 23:04:06 +0530

    updating

--- src/library/product/details/stores/t/tmall/transform.js
+++ src/library/product/details/stores/t/tmall/transform.js
@@ -9,37 +9,16 @@ const transform = (data) => {
         
         if(row.alternateImages){
           row.alternateImages.forEach(item => {
-              var tmpURL=item.text.replace("_50x50.jpg", "");;
-            item.text=tmpURL;
+            item.text=item.text.replace("_50x50.jpg", "");
           });
         }
 
-        if(row.ratingCount){
-          row.ratingCount.forEach(item => {
-            var ratingCountArr=item.text.split(" ");
-            item.text=parseInt(ratingCountArr[0]);
-          });
-        }
-
-        if(row.aggregateRating){
-          row.aggregateRating.forEach(item => {
-            item.text=parseFloat(item.text);
-          });
-        }
-
-        if(row.variantId){
-          row.variantId.forEach(item => {
-            var variantIdArr=item.text.split("/kf/");
-            var variantIdArr2=variantIdArr[1].split("/");
-            item.text=variantIdArr2[0];
-          });
-        }
-
+       
         if(row.sku){
           row.sku.forEach(item => {
             var skuArr=item.text.split("/item/");
             var skuArr1=skuArr[1].split(".");
-            item.text=parseInt(skuArr1[0]);
+            item.text=skuArr1[0];
           });
         }
         
