e36148f6ec508174483a48921d02ce477406d271
girish <girish@kcspl.co.in>
2020-09-11 22:55:12 +0530

    pushed changes to local dev branch

--- src/library/product/search/stores/s/shavershop/AU/paginate.js
+++ src/library/product/search/stores/s/shavershop/AU/paginate.js
@@ -4,9 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'AU',
     store: 'shavershop',
-    nextLinkSelector: '.page-next',    
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
     loadedSelector: '.search-result-content',
     noResultsXPath: 'div.sli_no_results_text',
+    openSearchDefinition: {
+      template: 'https://www.shavershop.com.au/search?q={searchTerms}&start={page}&sz=32',
+    },
     domain: 'shavershop.com.au',
     zipcode: '',
   },
