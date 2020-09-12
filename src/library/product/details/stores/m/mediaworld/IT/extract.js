45f9b23e7bbdab171826cc4da5b1995ecd95d726
Lokanath sahoo <loknath.s@osmosys.asia>
2020-09-11 15:16:44 -1200

    find error correction

--- src/library/product/details/stores/m/mediaworld/IT/extract.js
+++ src/library/product/details/stores/m/mediaworld/IT/extract.js
@@ -1,20 +1,21 @@
-const { cleanUp } = require('../../../../shared');
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'IT',
     store: 'mediaworld',
-    transform: cleanUp,
-    zipcode: '',
+    transform,
     domain: 'mediaworld.it',
   },
   implementation: async ({ input }, { transform }, context, { productDetails }) => {
     await context.evaluate(() => {
       const getAllData = JSON.parse(window.mwProductDetailData);
-      const videoUrl = getAllData.CatalogEntryView[0].Attributes;
+      const videoUrl = getAllData && getAllData.CatalogEntryView[0] && getAllData.CatalogEntryView[0].Attributes;
       const element = videoUrl.find(product => product.name === 'eVideoshopping');
-      const videoLink = element && element.Values[0] && element.Values[0].values;
-      document.body.setAttribute('video', videoLink);
+      if (element) {
+        const videoLink = element && element.Values[0] && element.Values[0].values;
+        document.body.setAttribute('video', videoLink);
+      }
     });
     await context.extract(productDetails, { transform });
   },
