f417c27520d239a14b05c80fdcc3f32c9b453458
nirmal-gspann <nirmal.genji@gspann.com>
2020-09-12 06:50:51 -0700

    Variants: xpath added

---
+++ src/library/product/details/stores/w/wildberries/RU/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'RU',
+    store: 'wildberries',
+    domain: 'wildberries.ru',
+    zipcode: '',
+  },
+};
