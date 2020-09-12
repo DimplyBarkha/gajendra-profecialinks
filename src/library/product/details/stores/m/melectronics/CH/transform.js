99d789cf92aebac648788a7bec7a4f448d79a112
Syed Abuthahir <sayed@profecialinks.com>
2020-09-12 13:13:21 +0530

    issue resolved

--- src/library/product/details/stores/m/melectronics/CH/transform.js
+++ src/library/product/details/stores/m/melectronics/CH/transform.js
@@ -1,26 +1,21 @@
 const transform = (data) => {
     for (const { group} of data) {
         for (const row of group) {
-           
             if (row.descriptionBullets) {
-                row.descriptionBullets= row.descriptionBullets.length
+                row.descriptionBullets= [{ "text" : row.descriptionBullets.length}]
             }
-  
             if (row.specifications) {
               row.specifications.forEach(item => {
                   item.text  = item.text.replace(/(\s*\n\s*)/g, ' | ').trim();
               });
-          }
-       
-          if (row.category) {
-            row.category.forEach(item => {
-                item.text  = item.text.replace(/(\s*\n\s*)/g, ' > ').trim();
-            });
+            }
+            if (row.category) {
+              row.category.forEach(item => {
+                  item.text  = item.text.replace(/(\s*\n\s*)/g, ' > ').trim();
+              });
+            }
         }
-  
-        }
-      }
+    }
       return data;
   };
-  
   module.exports = { transform } 
