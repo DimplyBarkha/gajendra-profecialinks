4a09b3a8b0926cfb044fa710f66cad70e5bdef1e
judhisahoo2009 <judhisahoo@hotmail.com>
2020-09-12 14:12:31 +0530

    updating

--- src/library/product/details/stores/m/mvideo/RU/variantTransform.js
+++ src/library/product/details/stores/m/mvideo/RU/variantTransform.js
@@ -4,22 +4,21 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.variantId) {
-          row.variantId.forEach(item => {                        
-            var splitData = item.text.split('-');
-            item.text = parseInt(splitData[splitData.length-1]);          
-          });
-        }
-        if (row.variantUrl) {
-          row.variantUrl.forEach(item => {                        
-            item.text = 'https://www.mvideo.ru/'+item.text;
-          });
-        }   
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.variantId) {
+        row.variantId.forEach(item => {
+          var splitData = item.text.split('-');
+          item.text = splitData[splitData.length-1];
+        });
+      }
+      if (row.variantUrl) {
+        row.variantUrl.forEach(item => {
+          item.text = 'https://www.mvideo.ru/'+item.text;
+        });
+      }
     }
-}
-    return data;
-  };
-  
-  module.exports = { transform };
+  } 
+  return data;
+};
+module.exports = { transform };
