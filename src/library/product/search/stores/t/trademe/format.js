35fd0cc25dd0414210abf35e4073bf250f1e8c71
judhisahoo2009 <judhisahoo@hotmail.com>
2020-09-12 14:57:58 +0530

    updating

--- src/library/product/search/stores/t/trademe/format.js
+++ src/library/product/search/stores/t/trademe/format.js
@@ -4,36 +4,32 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.productUrl) {
-          row.productUrl.forEach(item => {
-            item.text = "https://www.trademe.co.nz" + item.text;
-            item.text = item.text.toString();
-          });
-        }
-        if (row.thumbnail) {
-          row.thumbnail.forEach(item => {
-            item.text = item.text.match(/\(([^)]+)\)/)[1].slice(1,-1);
-            item.text = item.text.toString();
-          });
-        }
-        if (row.variantId) {
-          row.variantId.forEach(item => {
-              item.text = item.text.replace(/\?.*$/g, '').trim();
-              item.text = item.text.replace(/\D/g, ' ').trim();
-              item.text = item.text.toString();
-            });
-        }
-       if (row.id) {
-        row.id.forEach(item => {
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.productUrl) {
+        row.productUrl.forEach(item => {
+          item.text = "https://www.trademe.co.nz" + item.text;
+        });
+      }
+      if (row.thumbnail) {
+        row.thumbnail.forEach(item => {
+          item.text = item.text.match(/\(([^)]+)\)/)[1].slice(1,-1);
+        });
+      }
+      if (row.variantId) {
+        row.variantId.forEach(item => {
             item.text = item.text.replace(/\?.*$/g, '').trim();
             item.text = item.text.replace(/\D/g, ' ').trim();
-            item.text = item.text.toString();
-         });
-        }        
-      } 
-    }
-    return data;
-  };
- module.exports = { transform };
+          });
+      }
+      if (row.id) {
+      row.id.forEach(item => {
+          item.text = item.text.replace(/\?.*$/g, '').trim();
+          item.text = item.text.replace(/\D/g, ' ').trim();
+        });
+      }
+    } 
+  }
+  return data;
+};
+module.exports = { transform };
