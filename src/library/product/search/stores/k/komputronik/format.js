8673dbd1453e2de6a53be405a8eff1147d45a2f1
sivasoarlogic <siva@soarlogic.com>
2020-09-12 14:39:16 +0530

    search

--- src/library/product/search/stores/k/komputronik/format.js
+++ src/library/product/search/stores/k/komputronik/format.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
@@ -8,68 +7,52 @@ const transform = (data) => {
     for (const { group } of data) {
       for (const row of group) {
         if (row.ratingCount) {
-          row.ratingCount.forEach(item => {
+           row.ratingCount.forEach(item => {
               item.text = item.text.replace(/\|.*$/g, '').slice();
               item.text = item.text.replace(/\//g, '').slice(0, -2);
-              item.text = parseInt(item.text);
-          });
-      }
-
-      if (row.reviewCount) {
-        row.reviewCount.forEach(item => {
+           });
+        }
+       if (row.reviewCount) {
+         row.reviewCount.forEach(item => {
             item.text = item.text.replace(/.+(\|)/g, '').slice();
             item.text = item.text.replace(/\s/g, '').slice(0,-6)
-            item.text = parseInt(item.text);
-        });
-      }
-
-      if (row.shippingInfo) {
-        row.shippingInfo.forEach(item => {
+          });
+        }
+       if (row.shippingInfo) { 
+          row.shippingInfo.forEach(item => {
             item.text = item.text.replace(/\s\n/g, '').trim();
-        });
-      }   
-
-      if (row.name) {
-        row.name.forEach(item => {
+         });
+        }   
+       if (row.name) {
+          row.name.forEach(item => {
             item.text = item.text.replace(/\s\n/g, '').trim();
-        });
-      }   
-
-      if (row.price) {
-        row.price.forEach(item => {
+         });
+        }   
+       if (row.price) {
+          row.price.forEach(item => {
             item.text = item.text.replace(/\s\n/g, '').trim();
             item.text = item.text.replace(/\s*/g, '').trim();
-        });
-      }   
-         
-       if (row.variantId) {
-            row.variantId.forEach(item => {
-              item.text = item.text + "sj";
+         });
+        }     
+        if (row.variantId) {
+              row.variantId.forEach(item => {
+                item.text = item.text + "sj";
                 item.text = item.text.slice(33,-1);
                 item.text = item.text.match(/[^[\]]+(?=])/g);
-                item.text = item.text.toString();
-               
-                
-            });
+                item.text = item.text.toString();   
+              });
         } 
-
         if (row.id) {
             row.id.forEach(item => {
-              item.text = item.text + "sj";
+                 item.text = item.text + "sj";
                 item.text = item.text.slice(33,-1);
                 item.text = item.text.match(/[^[\]]+(?=])/g);
-                item.text = item.text.toString();
-               
-                
+                item.text = item.text.toString();   
             });
-        }         
-          
-          
-      }
-      
+        }             
+      } 
     }
     return data;
   };
-  
-  module.exports = { transform };
+module.exports = { transform };
   
