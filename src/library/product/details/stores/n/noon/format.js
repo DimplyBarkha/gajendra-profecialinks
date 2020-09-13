1ecbba8d59bc9a3786ae83507b3ca8ca11fcefe0
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-09-13 16:03:44 +0530

    Updated code

--- src/library/product/details/stores/n/noon/format.js
+++ src/library/product/details/stores/n/noon/format.js
@@ -12,6 +12,11 @@ const transform = (data) => {
           item.text = item.text.replace(/.+\/(.+?)_.+/g, '$1').trim();
         });
       }
+      if (row.variantId) {
+        row.variantId.forEach(item => {
+          item.text = item.text.replace(/.+\/(.+?)_.+/g, '$1').trim();
+        });
+      }      
       if (row.description) {
         row.description.forEach(item => {
           item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
