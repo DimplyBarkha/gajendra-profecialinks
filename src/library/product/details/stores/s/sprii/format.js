cd4b93650f23aab1f082820dad22c941bcce08f6
Nilesh <nilesh@profecialinks.com>
2020-09-13 19:54:58 +0530

    Changes to fix alternate images

--- src/library/product/details/stores/s/sprii/format.js
+++ src/library/product/details/stores/s/sprii/format.js
@@ -24,31 +24,31 @@ const transform = (data) => {
           row.availabilityText[0].text = 'In Stock';
         }
       }
-      if (row.alternateImages) {
-        try {
-          var regex = /(\/)(?!.*\/)(.*_)(.*)(\.)/g;
-          var regex1 = /(\/)(?!.*\/)(.*_)(.*)(\.)/g;
+      // if (row.alternateImages) {
+      //   try {
+      //     var regex = /(\/)(?!.*\/)(.*_)(.*)(\.)/g;
+      //     var regex1 = /(\/)(?!.*\/)(.*_)(.*)(\.)/g;
 
-          const altImgs = row.alternateImages;
-          if (altImgs && altImgs.length > 1) {
-            const prevImage = altImgs[0];
-            const nextImage = altImgs[altImgs.length - 1];
-            const match1 = regex.exec(prevImage.text);
-            const match2 = regex1.exec(nextImage.text);
-            const endNumber = Number(match1[3]);
-            const startNumber = Number(match2[3]);
-            const newAltImg = [];
-            for (let i = startNumber; i <= endNumber; i++) {
-              newAltImg.push({
-                text: prevImage.text.replace(`_${endNumber}.`, `_${i}.`),
-                xpath: prevImage.xpath
-              })
-            }
-            row.alternateImages = newAltImg;
-          }
-        }
-        catch { }
-      }
+      //     const altImgs = row.alternateImages;
+      //     if (altImgs && altImgs.length > 1) {
+      //       const prevImage = altImgs[0];
+      //       const nextImage = altImgs[altImgs.length - 1];
+      //       const match1 = regex.exec(prevImage.text);
+      //       const match2 = regex1.exec(nextImage.text);
+      //       const endNumber = Number(match1[3]);
+      //       const startNumber = Number(match2[3]);
+      //       const newAltImg = [];
+      //       for (let i = startNumber; i <= endNumber; i++) {
+      //         newAltImg.push({
+      //           text: prevImage.text.replace(`_${endNumber}.`, `_${i}.`),
+      //           xpath: prevImage.xpath
+      //         })
+      //       }
+      //       row.alternateImages = newAltImg;
+      //     }
+      //   }
+      //   catch { }
+      // }
     }
   }
   return data;
