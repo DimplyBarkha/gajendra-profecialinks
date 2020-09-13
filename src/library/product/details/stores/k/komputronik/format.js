ab85be2f19a74232e8d1e4529d4c16381a41b144
unknown <ganesh.working@gmail.com>
2020-09-13 17:20:38 +0530

    fix all keys

--- src/library/product/details/stores/k/komputronik/format.js
+++ src/library/product/details/stores/k/komputronik/format.js
@@ -6,21 +6,42 @@
 const transform = (data) => {
     for (const { group } of data) {
       for (const row of group) {
-        if (row.category) {
-          row.category.forEach(item => { 
-              item.text = item.text.replace(/\s\n\s\n\s/g, ' > ').trim();
+        
+        if (row.alternateImages) {
+          row.alternateImages.forEach(item => {
+            item.text = item.text.replace(/(product-picture\/4\/)+/g, 'product-picture/11/').trim();
           });
+        }
+        if (row.category) {
+          let info = [];          
+          row.category.forEach(item => {
+            info.push(item.text.replace(/(\s*\n\s*)+/g, '').trim());            
+          });
+          row.category = [{'text':info.join(' > '),'xpath':row.category[0].xpath}];
+        }
+        if (row.variantinformation) {
+          let info = [];          
+          row.variantinformation.forEach(item => {
+            info.push(item.text.replace(/(\s*\n\s*)+/g, '').trim());            
+          });
+          row.variantinformation = [{'text':info.join(' > '),'xpath':row.variantinformation[0].xpath}];
         }
         if (row.variants) {
           row.variants.forEach(item => {
               item.text = item.text.replace(/\n/g, ' | ').trim();
           });
         }
-        if (row.gtin) {
-          row.gtin.forEach(item => {
-            item.text = item.text.match(/[^[\]]+(?=])/g);
-            item.text = item.text.toString();
+        if (row.description) {
+          row.description.forEach(item => {
+              item.text = item.text.replace(/\n/g, ' | ').trim();
           });
+        }
+        if (row.specifications) {
+          let info = [];          
+          row.specifications.forEach(item => {
+            info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());            
+          });
+          row.specifications = [{'text':info.join(' || '),'xpath':row.specifications[0].xpath}];          
         }
       }  
     }
