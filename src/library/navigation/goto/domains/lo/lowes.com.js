326cee1fe1a702e982bba581033df709506dc3e4
irfan shaikh <irfan.shaikh@synerzip.com>
2020-09-13 13:47:31 +0530

    #521. Change variantCount field default value

--- src/library/navigation/goto/domains/lo/lowes.com.js
+++ src/library/navigation/goto/domains/lo/lowes.com.js
@@ -7,9 +7,9 @@ module.exports = {
     store: 'lowes',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    // await context.setAntiFingerprint(false);
-    // await context.setLoadAllResources(true);
-    // await context.setBlockAds(false);
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setBlockAds(false);
     await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
