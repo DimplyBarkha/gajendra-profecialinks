5b57c026ad729578fc87f70f3117ecee868c7065
unknown <ganesh.working@gmail.com>
2020-09-13 23:42:19 +0530

    use cleanup code on

--- src/library/product/details/stores/m/mediaexpert/transform.js
+++ src/library/product/details/stores/m/mediaexpert/transform.js
@@ -1,12 +1,29 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
+const transform = (data) => {  
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
     for (const { group } of data) {
-      for (const row of group) {
+      for (let row of group) {        
         if(row.image){
           row.image.forEach(item => {
             item.text='https://www.mediaexpert.pl'+item.text;
@@ -26,12 +43,12 @@ const transform = (data) => {
           });
         }
 
-        if (row.manufacture) {
-          row.manufacture.forEach(item => {
+        if (row.manufacture) {          
+          row.manufacture.forEach(item => {            
             var myRegexp = /producenta\/importera\s+\n(.+?)\s*\n/g;
             var match = myRegexp.exec(item.text);
             if(match.length){
-                item.text = match[1].trim();
+                item.text = match[1].trim();                
             }else{
                 item.text = "";
             }
@@ -60,10 +77,10 @@ const transform = (data) => {
                 item.text = "";
             }
           });
-        }
+        }          
       }
-    }
-    return data;
+    }    
+    return cleanUp(data);
   };
   
   module.exports = { transform };
