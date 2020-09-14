dcdebd32f6cd41d22a196f689da8abb12b57acae
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-14 18:50:00 +0530

    Added variant extractor for BE variant

---
+++ src/library/product/details/stores/b/bol/BE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'BE',
+    store: 'bol',
+    domain: 'bol.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/b/bol/BE/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'BE',
+    store: 'bol',
+    transform: null,
+    domain: 'bol.com',
+    zipcode: '',
+  },
+};
