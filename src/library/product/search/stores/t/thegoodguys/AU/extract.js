8dd880fa60f5d34fdf5b8a4f3481b2d4b46f2cdf
Vivek Gondil <gondilvivek@gmail.com>
2020-09-14 15:21:59 +0530

    Added custom implementaion for extract.js

--- src/library/product/search/stores/t/thegoodguys/AU/extract.js
+++ src/library/product/search/stores/t/thegoodguys/AU/extract.js
@@ -1,4 +1,32 @@
 const { transform } = require('../shared');
+async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
+) {
+    context.evaluate(() => {
+        document.querySelector('.breadcrumb>li').remove();
+        let reviewStars = document.querySelectorAll('.reviews-stars-link__stars'),
+            i;
+        for (i = 0; i < reviewStars.length; ++i) {
+            let htmlText = reviewStars[i].innerHTML;
+            let arr = htmlText.match(/star-o/g)
+            let arr2 = htmlText.match(/half-o/g)
+            let rating = 5
+            if (arr) {
+                rating = rating - arr.length
+            }
+            if (arr2) {
+                rating = rating - 0.5
+            }
+            reviewStars[i].innerHTML = '<div class="rating">' + rating + '</div>';
+        }
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+}
 module.exports = {
     implements: 'product/search/extract',
     parameterValues: {
@@ -8,4 +36,5 @@ module.exports = {
         domain: 'thegoodguys.com.au',
         zipcode: '',
     },
+    implementation,
 };
