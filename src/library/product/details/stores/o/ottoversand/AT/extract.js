33e094e6a0ac3250adc6ddff3a103fc4551c821a
rudra.bhatt <rudra.bhatt@kcsitglobal.com>
2020-09-14 15:37:35 +0530

    add code for get data from ottoversand site _core

--- src/library/product/details/stores/o/ottoversand/AT/extract.js
+++ src/library/product/details/stores/o/ottoversand/AT/extract.js
@@ -8,4 +8,33 @@ module.exports = {
     domain: 'ottoversand.at',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {      
+      
+     const productInfo = preFetchProductDetails();
+      addEleToDoc('skuId', productInfo.sku);
+
+      function preFetchProductDetails () {
+        let productInfo = findProductDetails('//script[@type="application/ld+json" and @id="schemaorg-product"]');        
+        productInfo = JSON.parse(productInfo.textContent);        
+        return productInfo;
+      }      
+            
+      function findProductDetails (xpath) {        
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;                
+        const productDetails = element;
+        return productDetails;
+      }
+
+      function addEleToDoc (key, value) {
+        const prodEle = document.createElement('div');
+        prodEle.id = key;
+        prodEle.textContent = value;
+        prodEle.style.display = 'none';
+        document.body.appendChild(prodEle);
+      }
+
+    });
+    return await context.extract(productDetails);
+  },  
 };
