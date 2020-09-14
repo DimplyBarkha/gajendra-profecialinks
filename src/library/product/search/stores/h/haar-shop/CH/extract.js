b9ff11ec60f9c36c047453cd3a642da77736b371
Nandakishor Valakunde <nvalakunde@amplifylife.com>
2020-09-14 18:50:49 +0530

    Added transform for rank
    Added transform for rank

--- src/library/product/search/stores/h/haar-shop/CH/extract.js
+++ src/library/product/search/stores/h/haar-shop/CH/extract.js
@@ -1,11 +1,24 @@
+const { transform } = require('../format');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'CH',
     store: 'haar-shop',
-    transform: null,
+    transform: transform,
     domain: 'haar-shop.ch',
     zipcode: '',
   },
+  implementation,
 };
---
+++ src/library/product/search/stores/h/haar-shop/format.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data, context) => {
+  let rankCounter = 0;
+  for (const { group } of data) {
+    for (const row of group) {
+      rankCounter += 1;
+      row.rank = [{ text: rankCounter }];
+    }
+  }
+  context.setState({ rankCounter });
+  return data;
+};
+module.exports = { transform };
