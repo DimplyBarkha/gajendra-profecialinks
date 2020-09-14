d72eda09d051141e7c3105433758b303eeb5473f
vikas kyatannawar <vikas.k@osmosys.asia>
2020-09-14 18:41:41 +0530

    Added code to load images

--- src/library/navigation/goto/domains/ta/tatacliq.com.js
+++ src/library/navigation/goto/domains/ta/tatacliq.com.js
@@ -1,21 +1,19 @@
 module.exports = {
-    implements: 'navigation/goto',
-    parameterValues: {
-        domain: 'tatacliq.com',
-        timeout: 90000,
-        country: 'IN',
-        store: 'tatacliq',
-        zipcode: '',
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'tatacliq.com',
+    timeout: 90000,
+    country: 'IN',
+    store: 'tatacliq',
+    zipcode: '',
 
-    },
-    implementation: async({ url, zipcode, storeId }, parameters, context, dependencies) => {
-        const timeout = parameters.timeout ? parameters.timeout : 10000;
-        await context.setLoadImages(true);
-        await context.setLoadAllResources(true);
-        await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true });
-        console.log(zipcode);
-        if (zipcode) {
-            await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-        }
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
     }
+  },
 };
--- src/library/product/search/stores/t/tatacliq/IN/extract.js
+++ src/library/product/search/stores/t/tatacliq/IN/extract.js
@@ -1,46 +1,59 @@
-const { transform } = require('../../../../shared')
+const { transform } = require('../../../../shared');
 module.exports = {
-    implements: 'product/search/extract',
-    parameterValues: {
-        country: 'IN',
-        store: 'tatacliq',
-        transform,
-        domain: 'tatacliq.com',
-        zipcode: '',
-    },
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'IN',
+    store: 'tatacliq',
+    transform,
+    domain: 'tatacliq.com',
+    zipcode: '',
+  },
 
-    implementation: async function implementation(
-        inputs,
-        parameters,
-        context,
-        dependencies,
-    ) {
-        // const result = await context.evaluate(() => {
-        //     return document.querySelector('#root>div>div:nth-child(4)>div')
-        // })
-        // if (result) {
-        //     await context.waitForFunction(() => {
-        //         return !document.querySelector('#root>div>div:nth-child(4)>div')
-        //     }, 50000)
-        // }
+  implementation: async function implementation (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    // const result = await context.evaluate(() => {
+    //     return document.querySelector('#root>div>div:nth-child(4)>div')
+    // })
+    // if (result) {
+    //     await context.waitForFunction(() => {
+    //         return !document.querySelector('#root>div>div:nth-child(4)>div')
+    //     }, 50000)
+    // }
 
-        await context.evaluate(() => {
-            document.querySelector('div#grid-wrapper_desktop>div>div>div>div>div:last-child').scrollIntoView({ behavior: "smooth" })
-        })
-        await context.waitForNavigation({ timeout: 50000, waitUntil: 'networkidle0' });
-
-        try {
-            await context.waitForSelector("div[id='grid-wrapper_desktop']>div>div>div>div>div:last-child>div:last-child", 80000)
-        } catch (e) {
-            console.log(e);
+    // await context.evaluate(() => {
+    //   document.querySelector('div#grid-wrapper_desktop>div>div>div>div>div:last-child').scrollIntoView({ behavior: 'smooth' });
+    // });
+    async function loadImages () {
+      const delay = t => new Promise(resolve => setTimeout(resolve, t));
+      async function waitForImages (node) {
+        while (node.querySelectorAll('img:not([alt=\'star icon\'])').length === 0) {
+          console.log('waiting for images to load');
+          await delay(1000);
         }
-
-        const { transform } = parameters;
-        const { productDetails } = dependencies;
-        const delay = t => new Promise(resolve => setTimeout(resolve, t));
-        await delay(7000);
-        return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
-
-
-    },
+        console.log('Images loaded.');
+      }
+      const nodes = Array.from(document.querySelectorAll("div[id='grid-wrapper_desktop']>div>div>div>div>div"));
+      console.log('Starting image loading');
+      let row = 1;
+      for (const node of nodes) {
+        console.log('Row Number: ', row++);
+        node.scrollIntoView({ behavior: 'smooth' });
+        await waitForImages(node);
+      }
+    }
+    // try {
+    //   await context.waitForSelector("div[id='grid-wrapper_desktop']>div>div>div>div>div:last-child>div:last-child", 80000);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+    await context.evaluate(loadImages);
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    // await delay(7000);
+    return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
+  },
 };
