7dddc3ff207ba686f843c8670b5900fedfec6af9
abhay.patil <abhay.patil@kcsitglobal.com>
2020-09-14 15:22:21 +0530

    remove wasted code

--- src/library/product/search/stores/k/kogan/AU/extract.js
+++ src/library/product/search/stores/k/kogan/AU/extract.js
@@ -1,28 +1,3 @@
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { productDetails } = dependencies;
-  await context.evaluate(async function () {
-    function addHiddenDiv (node, id, content) {
-      const newDiv = document.createElement('div');
-      newDiv.id = id;
-      newDiv.textContent = content;
-      newDiv.style.display = 'none';
-      node.appendChild(newDiv);
-    }
-
-    const element = document.evaluate('//script[contains(.,"k3.bootstrapData = JSON.parse")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-    
-    console.log("abhay" + element);
-    // @ts-ignore
-    // let pageStartIndex = paginationDiv ? paginationDiv.innerText.match(/\d+/) : 0;
-
-  });
-  return await context.extract(productDetails);
-}
 //const { transform } = require('../../../../shared');
 const { transform } = require('../shared');
 module.exports = {
