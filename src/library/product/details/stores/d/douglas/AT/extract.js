d39bf122d4fad42e0eff38678fe797b2c5d38c18
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-14 20:12:22 +0530

    Created extractor for douglas AT core

--- src/library/product/details/stores/d/douglas/AT/extract.js
+++ src/library/product/details/stores/d/douglas/AT/extract.js
@@ -8,4 +8,40 @@ module.exports = {
     domain: 'douglas.at',
     zipcode: '',
   },
+
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(async function () {
+      let brand = document.querySelector('a.product-detail-header__brand-line') ? document.querySelector('a.product-detail-header__brand-line').innerText : '';
+      let name = document.querySelector('span.product-detail-header__name') ? document.querySelector('span.product-detail-header__name').innerText : '';
+      let nameExtended = brand + ' ' + name;
+      document.body.setAttribute('nameExtended', nameExtended);
+
+      let getOtherInfo = document.querySelector('#react-tabs-2');
+      if (getOtherInfo) {
+        getOtherInfo.click();
+        let otherinfo = document.querySelector('div.product-detail-content__html').innerText;
+        document.body.setAttribute('otherinfo', otherinfo);
+      }
+
+      let getIngredients = document.querySelector('#react-tabs-3');
+      if (getIngredients) {
+        getIngredients.click();
+        let ingredients = document.querySelector('div.product-detail-content__html').innerText;
+        document.body.setAttribute('ingredients', ingredients);
+      }
+      let url = window.location.href;
+      console.log('url');
+      if (url.includes('variant')) {
+        let variantid = url.split('variant=')[1];
+        document.body.setAttribute('variantid', variantid);
+      }
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+  },
 };
