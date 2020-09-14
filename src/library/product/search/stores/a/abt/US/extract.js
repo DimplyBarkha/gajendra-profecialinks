5abce32da0a3b5334671ba6ccd58bb662bcdb93f
Karolina <karolina.urbanska@tdcm.io>
2020-09-14 23:50:19 +0200

    Improved selectors

--- src/library/product/search/stores/a/abt/US/extract.js
+++ src/library/product/search/stores/a/abt/US/extract.js
@@ -7,7 +7,43 @@ async function implementation (
   dependencies,
 ) {
   const { productDetails } = dependencies;
-  await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(500);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
+  await context.evaluate(async function () {
+    function addElementToDocument (doc, key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      doc.appendChild(catElement);
+    }
+    const lastProductPosition = localStorage.getItem('prodCount') ? Number(localStorage.getItem('prodCount')) : 1;
+    const arr = document.querySelectorAll('ul#category_results_list li[data-productid]');
+    for (let i = 0; i < arr.length; i++) {
+      addElementToDocument(arr[i], 'pd_rank', lastProductPosition + i);
+    }
+    localStorage.setItem('prodCount', `${lastProductPosition + arr.length}`);
+  });
   return await context.extract(productDetails);
 }
 
@@ -16,7 +52,7 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'abt',
-    transform: transform,
+    transform,
     domain: 'abt.com',
     zipcode: '',
   },
