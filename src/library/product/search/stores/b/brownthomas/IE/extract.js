22695eec6c1e25cdf982f814cdd43dc9f6ea9258
Mamatha Gundapaneni <mamatha.g@osmosys.asia>
2020-09-14 12:12:49 +0530

    Updated code

--- src/library/product/search/stores/b/brownthomas/IE/extract.js
+++ src/library/product/search/stores/b/brownthomas/IE/extract.js
@@ -9,6 +9,23 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+
+  const detailsPageSelector = 'h1[itemprop="name"]';
+
+  const isSelectorAvailable = async (detailsPageSelector) => {
+    console.log(`Is selector available: ${detailsPageSelector}`);
+    return await context.evaluate(function (detailsPageSelector) {
+      return !!document.querySelector(detailsPageSelector);
+    }, detailsPageSelector);
+  };
+
+  console.log('.....waiting......');
+
+  const xpathAvailable = await isSelectorAvailable(detailsPageSelector);
+  if (xpathAvailable) {
+    console.log('Redirected to details page');
+    throw new Error('ERROR: Loaded details page');
+  }
   return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
 }
 
--- src/library/product/search/stores/b/brownthomas/IE/paginate.js
+++ src/library/product/search/stores/b/brownthomas/IE/paginate.js
@@ -8,7 +8,6 @@ module.exports = {
     nextLinkSelector: 'div.js-product-load-more[data-grid-url*="https://www.brownthomas.com/search"]',
     mutationSelector: null,
     spinnerSelector: 'div[class="loader"][style="display: block;"]',
-    loadedSelector: null,
     noResultsXPath: '//p[contains(@class,"no-hits-content-results")]',
     openSearchDefinition: null,
     domain: 'brownthomas.com',
