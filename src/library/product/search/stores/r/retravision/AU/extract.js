84f7b1b3f41a9f3c3b528a027e0631b1a958d81b
rudra.bhatt <rudra.bhatt@kcsitglobal.com>
2020-09-14 17:04:50 +0530

    Rank and RankOrganic is not comes with data is solve

--- src/library/product/search/stores/r/retravision/AU/extract.js
+++ src/library/product/search/stores/r/retravision/AU/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'AU',
     store: 'retravision',
-    transform: null,
+    transform,
     domain: 'retravision.com.au',
     zipcode: '',
   },
