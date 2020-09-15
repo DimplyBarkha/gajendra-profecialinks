4238534ee211fa4833cacf8fdb29d4d758f0817b
Karolina <karolina.urbanska@tdcm.io>
2020-09-15 12:10:04 +0200

    Improved rank selector

--- src/library/product/search/stores/k/kastner-oehler/AT/extract.js
+++ src/library/product/search/stores/k/kastner-oehler/AT/extract.js
@@ -25,6 +25,7 @@ module.exports = {
       }
       const cookie = document.querySelector('span.tao_button_cookie_settings');
       if (cookie) cookie.click();
+      const lastProductPosition = localStorage.getItem('prodCount') ? Number(localStorage.getItem('prodCount')) : 1;
       const productsOnPage = document.querySelectorAll('section#product-list article.en_griditem');
       const numberOfProductsOnPage = productsOnPage ? productsOnPage.length : 0;
       for (let i = 0; i < numberOfProductsOnPage; i++) {
@@ -35,15 +36,16 @@ module.exports = {
           // @ts-ignore
           ? item.querySelector('div.en_mdl_product__image--alt').getAttribute('id').replace(/(^.+)_alt/g, '$1') : '';
         addElementToDocument(item, 'itemId', itemId);
-        addElementToDocument(item, 'count', `${i + 1}`);
-        const imgId = item.querySelector('div.en_mdl_product__image_img.cssimg')
+        const imgId = item && item.querySelector('div.en_mdl_product__image_img.cssimg')
           ? item.querySelector('div.en_mdl_product__image_img.cssimg').getAttribute('id') : '';
         const element = document.evaluate('//style[contains(text(),"cssimg")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         const regex = new RegExp(imgId + '\\s[^}]*url\\((.+)\\);\\}', 'g');
-        const imgArr = element.innerText.match(regex);
-        const img = imgArr[0].replace(regex, '$1').replace(/'\/\//g, 'https://').replace(/'/g, '');
+        const imgArr = element ? element.innerText.match(regex) : [];
+        const img = imgArr ? imgArr[0].replace(regex, '$1').replace(/'\/\//g, 'https://').replace(/'/g, '') : '';
         addElementToDocument(item, 'img', img);
-      }
+        addElementToDocument(item, 'count', lastProductPosition + i);
+      };
+      localStorage.setItem('prodCount', `${lastProductPosition + numberOfProductsOnPage}`);
     });
     return await context.extract(productDetails);
   },
