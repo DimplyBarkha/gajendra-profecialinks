1311f70893ea5e0942c41db8abe49268bd8fbfbd
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-09-15 12:32:26 +0530

    #545. Added cleanup transform and products per page field.

--- src/library/product/details/stores/d/davidjones/NZ/format.js
+++ src/library/product/details/stores/d/davidjones/NZ/format.js
@@ -5,6 +5,18 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+  // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
   for (const { group } of data) {
     for (const row of group) {
       if (row.description) {
@@ -42,9 +54,11 @@ const transform = (data) => {
           item.text = item.text.replace(/\/productimages/, 'https://www.davidjones.com/productimages');
         });
       }
-      row.variantCount = [{ text: 1 }];
     }
   }
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
   return data;
 };
 
