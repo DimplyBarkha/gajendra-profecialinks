3d5c3c84a761980d4f355e721e3d30271d9b097a
nikita.kumari <nikita.kumari@synerzip.com>
2020-09-15 18:49:30 +0530

    Loading Issues #606

--- src/library/navigation/goto/domains/jb/jbhifi.co.nz.js
+++ src/library/navigation/goto/domains/jb/jbhifi.co.nz.js
@@ -13,7 +13,7 @@ module.exports = {
     await context.setAntiFingerprint(false);
     await context.setLoadAllResources(true);
     await context.setBlockAds(false);
-    url = `${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+    url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
     await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
