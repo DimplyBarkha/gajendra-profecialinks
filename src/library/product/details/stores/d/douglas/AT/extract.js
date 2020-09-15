a9ea15728230e58bd3039855c5ac40a167c61f75
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-15 16:35:07 +0530

    Updated extract.js

--- src/library/product/details/stores/d/douglas/AT/extract.js
+++ src/library/product/details/stores/d/douglas/AT/extract.js
@@ -15,34 +15,32 @@ module.exports = {
     dependencies,
   ) => {
     await context.evaluate(async function () {
-
-      let brand = document.querySelector('a.product-detail-header__brand-line') ? document.querySelector('a.product-detail-header__brand-line').innerText : '';
-      let name = document.querySelector('span.product-detail-header__name') ? document.querySelector('span.product-detail-header__name').innerText : '';
-      let nameExtended = brand + ' ' + name;
+      const brand = document.querySelector('a.product-detail-header__brand-line') ? document.querySelector('a.product-detail-header__brand-line').innerText : '';
+      const name = document.querySelector('span.product-detail-header__name') ? document.querySelector('span.product-detail-header__name').innerText : '';
+      const nameExtended = brand + ' ' + name;
       document.body.setAttribute('nameExtended', nameExtended);
 
-      let getOtherInfo = document.querySelector('#react-tabs-2');
+      const getOtherInfo = document.querySelector('#react-tabs-2');
       if (getOtherInfo) {
         getOtherInfo.click();
-        let otherinfo = document.querySelector('div.product-detail-content__html').innerText;
+        const otherinfo = document.querySelector('div.product-detail-content__html').innerText;
         document.body.setAttribute('otherinfo', otherinfo);
       }
 
-      let getIngredients = document.querySelector('#react-tabs-3');
+      const getIngredients = document.querySelector('#react-tabs-3');
       if (getIngredients) {
         getIngredients.click();
-        let ingredients = document.querySelector('div.product-detail-content__html').innerText;
+        const ingredients = document.querySelector('div.product-detail-content__html').innerText;
         document.body.setAttribute('ingredients', ingredients);
       }
 
-      let expandDescription = document.querySelector('span.product-detail-content__toggle');
-      if (expandDescription && expandDescription.innerText.includes('Mehr'))
-        expandDescription.click();
+      const expandDescription = document.querySelector('span.product-detail-content__toggle');
+      if (expandDescription && expandDescription.innerText.includes('Mehr')) { expandDescription.click(); }
 
-      let url = window.location.href;
+      const url = window.location.href;
       console.log('url');
       if (url.includes('variant')) {
-        let variantid = url.split('variant=')[1];
+        const variantid = url.split('variant=')[1];
         document.body.setAttribute('variantid', variantid);
       }
     });
