0b69e5bb50ecbeda64d2f00e4f6b3eaa1d047f9e
Bindu Datla <bindu.datla@import.io>
2020-09-14 22:10:18 -0400

    updated scroll

--- src/library/product/search/stores/g/gigantti/FI/paginate.js
+++ src/library/product/search/stores/g/gigantti/FI/paginate.js
@@ -10,4 +10,54 @@ module.exports = {
     domain: 'gigantti.fi',
     zipcode: '',
   },
+  implementation: async function implementation (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { keywords, page, offset } = inputs;
+    const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
+
+    const scroll = async function (context) {
+      await context.evaluate( function () {
+        let scrollTop = 0;
+        let recordCount = 0;
+        while (scrollTop !== 20000) {
+          recordCount = keepScrolling(recordCount, 150);
+          if(recordCount > 0){
+          scrollTop += 10000;
+          window.parent.scroll(0, scrollTop);
+          }
+          else break;
+          if (scrollTop === 20000) {
+            break;
+          }  
+        }
+        function keepScrolling (recordsCollected, maxRecords) {
+          const recordSelector ='div.mini-product';
+          const recordsOnPage = 
+             document.querySelectorAll(recordSelector).length;
+          console.log(recordsOnPage);
+          //if(recordsOnPage == recordsCollected) return -1;
+          if(recordsOnPage < maxRecords) return recordsOnPage;
+          return -1;
+        }
+      });
+    };
+    await scroll(context);
+    if (loadedSelector) {
+      await context.waitForFunction(function (sel, xp) {
+        return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+      }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+    }
+    console.log('Checking no results', noResultsXPath);
+    return await context.evaluate(function (xp) {
+      const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+      console.log(xp, r);
+      const e = r.iterateNext();
+      console.log(e);
+      return !e;
+    }, noResultsXPath);
+  }
 };
