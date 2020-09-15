b1457160119a74606461b656009b5cbba75039d2
unknown <ganesh.working@gmail.com>
2020-09-15 21:51:53 +0530

    insert rank

--- src/library/product/search/stores/m/mumzworld/format.js
+++ src/library/product/search/stores/m/mumzworld/format.js
@@ -4,14 +4,13 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-  var p_count = 0
+  var p_count = 1
   for (const { group } of data) {
       for (const row of group) {          
-          if (row.rankOrganic) {
-              row.rankOrganic.forEach(item => {                  
-                  item.text = p_count;
-              });
-              p_count = p_count + 1;
+          if (row.rankOrganic && row.rank) {
+            row.rankOrganic = [{'text':p_count}];
+            row.rank = [{'text':p_count}];
+            p_count = p_count + 1;
           }
           if (row.id) {
             row.id.forEach(item => {
