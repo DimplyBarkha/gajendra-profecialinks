681cf7c5113eaa9f24b075adeec983126c6e2779
unknown <ganesh.working@gmail.com>
2020-09-16 03:33:13 +0530

    fix some keys

--- src/library/product/details/stores/n/noon/format.js
+++ src/library/product/details/stores/n/noon/format.js
@@ -33,45 +33,7 @@ const transform = (data) => {
         row.description.forEach(item => {
           item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
         });
-      }
-      if (row.category) {
-        let info = [];          
-        row.category.forEach(item => {
-          if (item.text != 'Home'){
-            info.push(item.text);            
-          }
-        });
-        if(info.length){          
-          row.category = [];
-          info.forEach(item => {
-              row.category.push({"text": item});
-          });
-        }
-      }
-      
-      
-      if (row.alternateImages) {
-        let info = [];          
-        row.alternateImages.forEach(item => {
-          info.push(item.text);            
-        });
-        if(info.length){          
-          row.alternateImages = [];
-          info.forEach(item => {
-              row.alternateImages.push({"text": item});
-          });
-        }
-      }
-      if (row.imageAlt) {
-        let info = [];          
-        row.imageAlt.forEach(item => {
-          info.push(item.text.trim());            
-        });
-        if (info.length){
-          row.imageAlt = info[0];
-        }
-        
-      }
+      }      
       if (row.ratingCount) {
         row.ratingCount.forEach(item => {
           var matches = /\s*(\d+)/isg.exec(item.text);
