5c593f87040fce5264eba9541e56825df013c75d
KCSPL\jatin <jatin@kcspl.co.in>
2020-09-15 17:58:33 +0530

    increase timeout

--- src/library/navigation/goto/domains/ba/basler-beauty.at.js
+++ src/library/navigation/goto/domains/ba/basler-beauty.at.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'basler-beauty.at',
-    timeout: 50000,
+    timeout: 100000,
     country: 'AT',
     store: 'basler-beauty',
     zipcode: '',
