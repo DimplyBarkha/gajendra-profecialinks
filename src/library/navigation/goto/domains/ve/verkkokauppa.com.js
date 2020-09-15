0e7b1770484e83495987a29e6b112c08433530e7
abhay.patil <abhay.patil@kcsitglobal.com>
2020-09-15 18:46:07 +0530

    add ds-core-verkkokauppa_fi branch

---
+++ src/library/navigation/goto/domains/ve/verkkokauppa.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'verkkokauppa.com',
+    timeout: 50000000,
+    country: 'FI',
+    store: 'verkkokauppa',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/v/verkkokauppa/FI/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'FI',
+    store: 'verkkokauppa',
+    domain: 'verkkokauppa.com',
+    loadedSelector: 'html',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
