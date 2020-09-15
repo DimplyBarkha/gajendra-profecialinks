cf17154f2cc17daac1838b918d5227c277e0712b
unknown <ganesh.working@gmail.com>
2020-09-15 21:18:55 +0530

    fix specification issue

--- src/library/product/details/stores/k/komputronik/format.js
+++ src/library/product/details/stores/k/komputronik/format.js
@@ -4,8 +4,26 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
     for (const { group } of data) {
-      for (const row of group) {
+      for (let row of group) {
         
         if (row.alternateImages) {
           row.alternateImages.forEach(item => {
@@ -28,24 +46,25 @@ const transform = (data) => {
         }
         if (row.variants) {
           row.variants.forEach(item => {
-              item.text = item.text.replace(/\n/g, ' | ').trim();
+              item.text = item.text.replace(/(\s*\n\s*)+/g, ' | ').trim();
           });
         }
         if (row.description) {
           row.description.forEach(item => {
-              item.text = item.text.replace(/\n/g, ' | ').trim();
+              item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
           });
         }
         if (row.specifications) {
           let info = [];          
           row.specifications.forEach(item => {
+            item.text = item.text.replace(/(.+)Gwarancja+/isg, 'Gwarancja')
             info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());            
           });
           row.specifications = [{'text':info.join(' || '),'xpath':row.specifications[0].xpath}];          
         }
       }  
     }
-    return data;
+    return cleanUp(data);
   };
 module.exports = { transform };
   
