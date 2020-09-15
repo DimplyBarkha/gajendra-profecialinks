5997a9ae3311a8833b9f47ffb7e08992a68a16b6
Karolina <karolina.urbanska@tdcm.io>
2020-09-15 10:36:11 +0200

    Improved selectors

--- src/library/product/search/stores/a/abt/US/execute.js
+++ src/library/product/search/stores/a/abt/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'abt',
     domain: 'abt.com',
-    url: 'https://www.abt.com/resources/pages/search.php?keywords=%22{searchTerms}%22',
+    url: 'https://www.abt.com/resources/pages/search.php?keywords=%22{searchTerms}%22&mpp=100',
     loadedSelector: 'ul#category_results_list',
     noResultsXPath: '//h2[@class="nopagetext"]',
     zipcode: '',
