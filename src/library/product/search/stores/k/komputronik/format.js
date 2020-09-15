8cbe27a16cb3ae2626fad4ea2c0cdf0b7adcf95d
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-09-16 03:35:07 +0530

    Updated code

--- src/library/product/search/stores/k/komputronik/format.js
+++ src/library/product/search/stores/k/komputronik/format.js
@@ -4,54 +4,57 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.ratingCount) {
-           row.ratingCount.forEach(item => {
-              item.text = item.text.replace(/\|.*$/g, '').slice();
-              item.text = item.text.replace(/\//g, '').slice(0, -2);
-           });
-        }
-       if (row.reviewCount) {
-         row.reviewCount.forEach(item => {
-            item.text = item.text.replace(/.+(\|)/g, '').slice();
-            item.text = item.text.replace(/\s/g, '').slice(0,-6)
-          });
-        }
-       if (row.shippingInfo) { 
-          row.shippingInfo.forEach(item => {
-            item.text = item.text.replace(/\s\n/g, '').trim();
-         });
-        }   
-       if (row.name) {
-          row.name.forEach(item => {
-            item.text = item.text.replace(/\s\n/g, '').trim();
-         });
-        }   
-       if (row.price) {
-          row.price.forEach(item => {
-            item.text = item.text.replace(/\s\n/g, '').trim();
-            item.text = item.text.replace(/\s*/g, '').trim();
-         });
-        }     
-        if (row.variantId) {
-              row.variantId.forEach(item => {
-                item.text = item.text + "sj";
-                item.text = item.text.slice(33,-1);
-                item.text = item.text.match(/[^[\]]+(?=])/g);
-                item.text = item.text.toString();   
-              });
-        } 
-        if (row.id) {
-            row.id.forEach(item => {
-                 item.text = item.text + "sj";
-                item.text = item.text.slice(33,-1);
-                item.text = item.text.match(/[^[\]]+(?=])/g);
-                item.text = item.text.toString();   
-            });
-        }             
-      } 
+  for (const { group } of data) {
+    let rank = 1;
+    for (const row of group) {
+      if (row.ratingCount) {
+        row.ratingCount.forEach(item => {
+          item.text = item.text.replace(/\|.*$/g, '').slice();
+          item.text = item.text.replace(/\//g, '').slice(0, -2);
+        });
+      }
+      if (row.reviewCount) {
+        row.reviewCount.forEach(item => {
+          item.text = item.text.replace(/.+(\|)/g, '').slice();
+          item.text = item.text.replace(/\s/g, '').slice(0, -6)
+        });
+      }
+      if (row.shippingInfo) {
+        row.shippingInfo.forEach(item => {
+          item.text = item.text.replace(/\s\n/g, '').trim();
+        });
+      }
+      if (row.name) {
+        row.name.forEach(item => {
+          item.text = item.text.replace(/\s\n/g, '').trim();
+        });
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace(/\s\n/g, '').trim();
+          item.text = item.text.replace(/\s*/g, '').trim();
+        });
+      }
+      if (row.variantId) {
+        row.variantId.forEach(item => {
+          item.text = item.text + "sj";
+          item.text = item.text.slice(33, -1);
+          item.text = item.text.match(/[^[\]]+(?=])/g);
+          item.text = item.text.toString();
+        });
+      }
+      if (row.id) {
+        row.id.forEach(item => {
+          item.text = item.text + "sj";
+          item.text = item.text.slice(33, -1);
+          item.text = item.text.match(/[^[\]]+(?=])/g);
+          item.text = item.text.toString();
+        });
+      }
+      row.rank = row.rankOrganic = [{"text":rank}];
+      rank++;        
     }
-    return data;
-  };
+  }
+  return data;
+};
 module.exports = { transform };
