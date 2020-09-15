066aed9b2453e0970e1925193f69201dd2fa18ec
judhisahoo2009 <judhisahoo@hotmail.com>
2020-09-15 20:19:29 +0530

    updating

---
+++ src/library/product/search/stores/m/mvideo/format.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.id) {
+        row.id.forEach(item => {           
+          var splitData = item.text.split('-');
+          item.text =  splitData[splitData.length-1];
+        });
+      }
+      if (row.thumbnail) {
+        row.thumbnail.forEach(item => {           
+          item.text = 'https:'+item.text;
+        });
+      }
+      if (row.prodcutUrl) {
+        row.prodcutUrl.forEach(item => {       
+          item.text = 'https://www.mvideo.ru'+item.text;
+        });
+      }          
+    }
+  }
+  return data;
+};  
+module.exports = { transform };
