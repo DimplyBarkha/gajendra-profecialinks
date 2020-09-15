2f6112d1d23f3933efa7bf411081f7a5738a9ecd
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-15 14:31:04 +0530

    pagination logic added (#601)

--- src/library/product/search/stores/l/liverpool/MX/paginate.js
+++ src/library/product/search/stores/l/liverpool/MX/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'MX',
     store: 'liverpool',
-    nextLinkSelector: 'ul.pagination li.page-item a i.icon-arrow_right',
+    // nextLinkSelector: 'ul.pagination li.page-item a i.icon-arrow_right',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
-    noResultsXPath: null,
-    openSearchDefinition: null,
+    loadedSelector: 'div[class="o-listing__products"]',
+    noResultsXPath: '//div[contains(@class , "o-content__noResults")]',
+    openSearchDefinition: {
+      template: 'https://www.liverpool.com.mx/tienda?s={searchTerms}?page-{page}',
+    },
     domain: 'liverpool.com.mx',
     zipcode: '',
   },
