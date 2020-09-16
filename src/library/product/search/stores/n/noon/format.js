22adf91792f653132fba31d0d9985f8c76c5eee2
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-09-16 23:45:47 +0530

    Updated code

--- src/library/product/search/stores/n/noon/format.js
+++ src/library/product/search/stores/n/noon/format.js
@@ -4,26 +4,25 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-  for (const { group } of data) {
-    var rank_count = 1
-    for (const row of group) {
-      row.rankOrganic = [{'text':rank_count,'xpath':''}];
-      row.rank = [{'text':rank_count,'xpath':''}];
-      rank_count = rank_count + 1;
-      if (row.productUrl) {
-        row.productUrl.forEach(item => {
-          item.text = "https://www.noon.com" + item.text;
-        });
-      }
-      if (row.id) {
-        row.id.forEach(item => {
-          var idArr=item.text.split('/p?');
-          var idArr1=idArr[0].split('/');
-          item.text=idArr1[idArr1.length-1];
-        });
+    for (const { group } of data) {
+      let rank = 1;
+      for (const row of group) {
+        if (row.productUrl) {
+          row.productUrl.forEach(item => {
+            item.text = "https://www.noon.com" + item.text;
+          });
+        }
+        if (row.id) {
+          row.id.forEach(item => {
+            item.text = item.text.replace(/.+o=(.+)$/, '$1');
+          });
+        }
+        row.rank = [{"text":rank}];
+        row.rankOrganic = [{"text":rank}];
+        rank++;
       }
     }
-  }
-  return data;
-};
-module.exports = { transform };
+    return data;
+  };
+  
+  module.exports = { transform };
