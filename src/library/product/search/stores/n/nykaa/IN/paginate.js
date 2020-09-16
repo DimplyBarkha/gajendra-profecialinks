8b227577574ac93746d4452f486f46c50bc1ae27
Manas Ranjan <manas.n@osmosys.asia>
2020-09-16 20:02:05 +0530

    Pushed offset value

--- src/library/product/search/stores/n/nykaa/IN/paginate.js
+++ src/library/product/search/stores/n/nykaa/IN/paginate.js
@@ -12,7 +12,7 @@ module.exports = {
     openSearchDefinition: {
       offset: 20,
       page: 1,
-      template: 'https://www.nykaa.com/nyk/aggregator-gludo/api/search.list?filter_format=v2&from=0&page_no={page}&search={searchTerms}#[!opt!]{"type":"json"}[/!opt!]',
+      template: 'https://www.nykaa.com/nyk/aggregator-gludo/api/search.list?filter_format=v2&from={offset}&page_no={page}&search={searchTerms}#[!opt!]{"type":"json"}[/!opt!]',
     },
     domain: 'nykaa.com',
     zipcode: '',
