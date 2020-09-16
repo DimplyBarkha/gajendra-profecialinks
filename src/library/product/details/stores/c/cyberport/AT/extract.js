ca6afe3f589ef1e38bba72a66237e3424e1767f2
kcs-abhaypatil <59007661+kcs-abhaypatil@users.noreply.github.com>
2020-09-16 18:27:56 +0530

    local dev changes

--- src/library/product/details/stores/c/cyberport/AT/extract.js
+++ src/library/product/details/stores/c/cyberport/AT/extract.js
@@ -8,4 +8,39 @@ module.exports = {
     domain: 'cyberport.at',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain ,transform: transformParam}, context, { productDetails }) => {     
+    
+    await context.evaluate(async function () {       
+     const productInfo = preFetchProductDetails();
+       addEleToDoc('divhiddenproductAvgRating', productInfo['productAvgRating']);
+       addEleToDoc('divhiddenproductRatingCount', productInfo['productRatingCount']);
+       addEleToDoc('divhiddenproductGrossPrice', productInfo['productGrossPrice'].analyticsFormat);
+      // addEleToDoc('gtinId', productInfo[0].gtin13);
+      // addEleToDoc('priceCurrency', productInfo[0].offers[0].priceCurrency);  
+     
+
+      function preFetchProductDetails () {
+        let productInfo = findProductDetails('//section//@data-productinfofordatalayer');                        
+        productInfo = JSON.parse(productInfo.textContent);   
+        return productInfo;
+      }      
+            
+      function findProductDetails (xpath) {        
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;                        
+        const productDetails = element;
+        return productDetails;
+      }
+
+      function addEleToDoc (key, value) {
+        const prodEle = document.createElement('div');
+        prodEle.id = key;
+        prodEle.textContent = value;
+        prodEle.style.display = 'none';
+        document.body.appendChild(prodEle);
+      }      
+
+    });
+   
+       return await context.extract(productDetails, { transform: transformParam });       
+  }, 
 };
