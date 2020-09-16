b92a784d5d5abd4d83fdfc7a80bf19f1c949f814
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-09-16 20:29:34 +0530

    Updated code

--- src/library/navigation/goto/domains/no/noon.com.js
+++ src/library/navigation/goto/domains/no/noon.com.js
@@ -4,11 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'noon.com',
     timeout: null,
-<<<<<<< HEAD
     country: 'SA',
-=======
-    country: 'AE',
->>>>>>> 3028437dbaddbdc9d3e6c47a8a5c6df99bc59e84
     store: 'noon',
     zipcode: '',
   },
--- src/library/navigation/goto/domains/no/setZipCode.noon.com.js
+++ src/library/navigation/goto/domains/no/setZipCode.noon.com.js
@@ -2,11 +2,7 @@
 module.exports = {
   implements: 'navigation/goto/setZipCode',
   parameterValues: {
-<<<<<<< HEAD
     country: 'SA',
-=======
-    country: 'AE',
->>>>>>> 3028437dbaddbdc9d3e6c47a8a5c6df99bc59e84
     domain: 'noon.com',
     store: 'noon',
     zipcode: '',
