d25b7e1a673292d24f7dbe189e36ca151e0cfec7
Karolina <karolina.urbanska@tdcm.io>
2020-09-16 17:34:57 +0200

    Created initial search extractor for power.no

---
+++ src/library/navigation/goto/domains/po/power.no.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'power.no',
+    timeout: 50000,
+    country: 'NO',
+    store: 'power',
+    zipcode: '',
+  },
+};
