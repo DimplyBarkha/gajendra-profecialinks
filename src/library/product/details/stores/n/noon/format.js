fb1b2ef43511b99304923f2b129ef5caa1a69c17
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-09-18 01:01:12 +0530

    Updated code

--- src/library/product/details/stores/n/noon/format.js
+++ src/library/product/details/stores/n/noon/format.js
@@ -15,13 +15,24 @@ const transform = (data) => {
         row.variantId.forEach(item => {
           item.text = item.text.replace(/.+\/(.+?)_.+/g, '$1').trim();
         });
+      }
+      if (row.alternateImages) {
+        var  img_arr = [];
+        row.alternateImages.forEach(item => {
+          img_arr.push({"text": item.text});
+        });
+        if (img_arr.length > 1){
+          img_arr.splice(0,1);
+          row.alternateImages = img_arr;
+        }else{
+          delete row.alternateImages;
+        }
       }      
       if (row.description) {
         row.description.forEach(item => {
           item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
         });
       }
-            
       if (row.ratingCount) {
         row.ratingCount.forEach(item => {
           var matches = /\s*(\d+)/isg.exec(item.text);
@@ -31,12 +42,30 @@ const transform = (data) => {
         });
       }
       if (row.specifications) {
-        let info = [];          
+        let info = [];
         row.specifications.forEach(item => {
-          info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());            
-        });          
-        row.specifications = [{'text':info.join(' || '),'xpath':row.specifications[0].xpath}];          
+          info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());
+        });
+        row.specifications = [{ 'text': info.join(' || '), 'xpath': row.specifications[0].xpath }];
       }
+      if(row.additionalDescBulletInfo){
+        var additionalDescBulletInfoStr='';
+        var oldXpath='';
+        row.additionalDescBulletInfo.forEach(item => {
+          oldXpath=item.xpath;
+          if(additionalDescBulletInfoStr==''){
+            additionalDescBulletInfoStr=item.text
+          }else{
+            additionalDescBulletInfoStr=additionalDescBulletInfoStr+' | '+item.text
+          }
+        });
+        row.additionalDescBulletInfo=[{text:additionalDescBulletInfoStr,xpath:oldXpath}];
+      }
+      if(row.mpc){
+        row.mpc.forEach(item => {
+          item.text=item.text.replace('Model Number: ','');
+        });
+      }      
     }
   }
   return data;
