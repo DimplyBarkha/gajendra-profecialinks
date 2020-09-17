a0881014f83dce7b16fd178d98ca0dfd22eebb3d
Dalton Kirkpatrick <dalton.kirkpatrick@import.io>
2020-09-17 15:41:49 -0700

    intermarche

--- src/library/product/details/stores/i/intermarche/FR/createUrl.js
+++ src/library/product/details/stores/i/intermarche/FR/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'intermarche.com',
     prefix: null,
-    url: 'https://www.intermarche.com/rechercheproduits/11833/recherche/meat/product/{id}',
+    url: 'https://www.intermarche.com/rechercheproduits/11833/recherche/product/{id}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":true,"cookies":[]}[/!opt!]',
     country: 'FR',
     store: 'intermarche',
     zipcode: '',
--- src/library/product/details/stores/i/intermarche/FR/extract.js
+++ src/library/product/details/stores/i/intermarche/FR/extract.js
@@ -9,7 +9,33 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ parentInput }, { country, domain, transform: transformParam }, context, { productDetails }) => {
-    await new Promise(resolve => setTimeout(resolve, 30000));
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    // const dataRaw = await context.evaluate(function () {
+    //   fetch('https://api.intermarche.com/produits/v2/pdvs/11833/produits/0000040198125', {
+    //     method: 'GET',
+    //     headers: {
+    //       'Accept':'application/json, text/plain, */*',
+    //       'X-Red-Device':'red_fo_desktop',
+    //       'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+    //       'X-Red-Version':'1',
+    //       'Sec-Fetch-Site':'same-site'
+    //     }
+    //   })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     if(data.url){
+    //       console.log("Failed !!!!!!!!!!!!!!!!!!!!")
+    //       // return context.reportBlocked(lastResponseData.code, 'CodeAction decided extractor got blocked');
+    //     }else{
+    //       debugger
+    //       console.log('Success:', data);
+    //     }
+    //   })
+    //   .catch((error) => {
+    //       console.error('Error:', error);
+    //   });
+    // });
 
     
     // await new Promise(resolve => setTimeout(resolve, 10000));
