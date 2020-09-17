12515c6ef6b5fac00e2a85c04e014cdbab7b25a5
Ravi Vachhani <ravi.vachhani@kcsitglobal.com>
2020-09-17 10:48:03 +0530

    Tab click change

--- src/library/product/details/stores/e/elgiganten/DK/extract.js
+++ src/library/product/details/stores/e/elgiganten/DK/extract.js
@@ -9,4 +9,21 @@ module.exports = {
     domain: 'elgiganten.dk',
     zipcode: '',
   },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    await context.evaluate(async function () {
+      const overlay = document.getElementById('tab-specs-trigger');
+      if (overlay !== undefined) {
+        overlay.click();
+      }
+    });
+
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.extract(productDetails, { transform });
+  },
 };
--- src/library/product/details/stores/e/elgiganten/shared.js
+++ src/library/product/details/stores/e/elgiganten/shared.js
@@ -4,45 +4,54 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.specifications) {
-          let text = '';
-          row.specifications.forEach(item => {
-            text += `${item.text.replace(/\n \n/g, ':')} || `;
-          });
-          row.specifications = [
-            {
-              text: text.slice(0, -4),
-            },
-          ];
-        }
-        if (row.description) {
-          let text = '';
-          row.description.forEach(item => {
-            text += `${item.text.replace(/\n \n/g, ':')} || `;
-          });
-          row.description = [
-            {
-              text: text.slice(0, -4),
-            },
-          ];
-        }
-        if (row.manufacturerDescription) {
-          let text = '';
-          row.manufacturerDescription.forEach(item => {
-            text += `${item.text.replace(/\n \n/g, ':')} || `;
-          });
-          row.manufacturerDescription = [
-            {
-              text: text.slice(0, -4),
-            },
-          ];
-        }
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.specifications) {
+        let text = '';
+        row.specifications.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        });
+        row.specifications = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+      }
+      if (row.description) {
+        let text = '';
+        row.description.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        });
+        row.description = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+      }
+      if (row.videos) {
+        let video = {};
+        row.videos.forEach(item => {
+          video = {
+            text: 'https://www.elgiganten.dk' + item.text,
+            xpath: item.xpath
+          }
+        });
+        row.videos = [video];
+      }
+      if (row.manufacturerDescription) {
+        let text = '';
+        row.manufacturerDescription.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        });
+        row.manufacturerDescription = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
       }
     }
-    return data;
-  };
-  
-  module.exports = { transform };
-  
+  }
+  return data;
+};
+
+module.exports = { transform };
