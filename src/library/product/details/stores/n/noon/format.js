5810a88d5bfcaf52fc5369e4a34d0b3209b6766f
judhisahoo2009 <judhisahoo@hotmail.com>
2020-09-17 18:33:26 +0530

    updating

--- src/library/product/details/stores/n/noon/format.js
+++ src/library/product/details/stores/n/noon/format.js
@@ -4,6 +4,24 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
   for (const { group } of data) {
     for (let row of group) {
       if (row.sku) {
@@ -28,13 +46,13 @@ const transform = (data) => {
           }
         });
       }
-      if (row.description) {
-        row.description.forEach(item => {
+      if (row.nameExtended) {
+        row.nameExtended.forEach(item => {
           item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
         });
       }
-      if (row.ratingCount) {
-        row.ratingCount.forEach(item => {
+      if (row.aggregateRating2) {
+        row.aggregateRating2.forEach(item => {
           var matches = /\s*(\d+)/isg.exec(item.text);
           if (matches) {
             item.text = matches[1]
@@ -47,6 +65,19 @@ const transform = (data) => {
           info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());
         });
         row.specifications = [{ 'text': info.join(' || '), 'xpath': row.specifications[0].xpath }];
+      }
+      if(row.additionalDescBulletInfo){
+        var additionalDescBulletInfoStr='';
+        var oldXpath='';
+        row.additionalDescBulletInfo.forEach(item => {
+          oldXpath=item.xpath;
+          if(additionalDescBulletInfoStr==''){
+            additionalDescBulletInfoStr=item.text
+          }else{
+            additionalDescBulletInfoStr=additionalDescBulletInfoStr+' | '+item.text
+          }
+        });
+        row.additionalDescBulletInfo=[{text:additionalDescBulletInfoStr,xpath:oldXpath}];
       }
     }
   }
