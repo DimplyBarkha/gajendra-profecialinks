75ade397fc54a734f56362de3fc162571225477f
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-09-17 20:27:57 +0530

    Updated code

--- src/library/product/search/stores/n/namshi/format.js
+++ src/library/product/search/stores/n/namshi/format.js
@@ -5,24 +5,24 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-        let rank = 1;
-        for (const row of group) {
-            if (row.productUrl) {
-                row.productUrl.forEach(item => {
-                    item.text = "https://en-ae.namshi.com" + item.text;
-                });
-            }
-            if (row.rankOrganic) {
-                row.rankOrganic.forEach(item => {
-                    item.text = parseInt(item.text) + 1;
-                });
-            }
-            row.rank = [{"text":rank}];
-            rank++;         
-        }
+  for (const { group } of data) {
+    let rank = 1;
+    for (const row of group) {
+      if (row.productUrl) {
+        row.productUrl.forEach(item => {
+          item.text = "https://en-ae.namshi.com" + item.text;
+        });
+      }
+      if (row.rankOrganic) {
+        row.rankOrganic.forEach(item => {
+          item.text = parseInt(item.text) + 1;
+        });
+      }
+      row.rank = [{ "text": rank }];
+      rank++;
     }
-    return data;
-  };
-  
-  module.exports = { transform };
+  }
+  return data;
+};
+
+module.exports = { transform };
