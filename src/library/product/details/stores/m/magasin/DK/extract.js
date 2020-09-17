68023e186adab1487fdccb2b70c1a95bf0233c9d
girish <girish@kcspl.co.in>
2020-09-17 18:35:41 +0530

    pushed changes to local dev branch

--- src/library/product/details/stores/m/magasin/DK/extract.js
+++ src/library/product/details/stores/m/magasin/DK/extract.js
@@ -1,10 +1,10 @@
-
+const {transform} = require('./transform')
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'DK',
     store: 'magasin',
-    transform: null,
+    transform: transform,
     domain: 'magasin.dk',
     zipcode: '',
   },
---
+++ src/library/product/details/stores/m/magasin/DK/transform.js
@@ -0,0 +1,39 @@
+  
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data, context) => {
+
+    for (const { group } of data) {
+      for (const row of group) {
+  
+        try { 
+         
+          if (row.gtin) {
+            row.gtin = [{ text: row.gtin[0].text.toString().replace(/\r\n|\r|\n/g, '').replace('EAN:   ','')
+         }];
+          }  
+          if (row.sku) {
+            row.sku = [{ text: row.sku[0].text.toString().replace(/\r\n|\r|\n/g, '').replace('EAN:   ','') }];
+          } 
+          if (row.variantId) {
+            row.variantId = [{ text: row.variantId[0].text.toString().replace(/\r\n|\r|\n/g, '').replace('EAN:  ','') }];
+          } 
+          if (row.mpc) {
+            row.mpc = [{ text: row.mpc[0].text.toString().replace(/\r\n|\r|\n/g, '').replace('EAN:   ','')}];
+          } 
+          if (row.upc) {
+            row.upc = [{ text: row.upc[0].text.toString().replace(/\r\n|\r|\n/g, '').replace('EAN:   ','') }];
+          } 
+          if (row.eangtin) {
+            row.eangtin = [{ text: row.eangtin[0].text.toString().replace(/\r\n|\r|\n/g, '').replace('EAN:   ','') }];
+          }          
+        } catch (exception) { console.log('Error in transform', exception); }
+       }
+    }
+    return data;
+  };
+  
+  module.exports = { transform };
