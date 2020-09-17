8a38249605ffd63500caa20c229a6420a265136f
Lokanath sahoo <loknath.s@osmosys.asia>
2020-09-17 10:06:11 -1200

    video links

--- src/library/navigation/goto/domains/me/mediaworld.it.js
+++ src/library/navigation/goto/domains/me/mediaworld.it.js
@@ -11,8 +11,9 @@ module.exports = {
   implementation: async ({ url }, parameterValues, context, dependencies) => {
     url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
     context.setBlockAds(false);
-    context.setLoadAllResources(false);
+    context.setLoadAllResources(true);
     context.setAntiFingerprint(false);
+    context.setLoadImages(true);
     await context.goto(url, {
       block_ads: false,
       load_all_resources: true,
--- src/library/product/details/stores/m/mediaworld/shared.js
+++ src/library/product/details/stores/m/mediaworld/shared.js
@@ -27,6 +27,19 @@ const cleanUp = (data, context) => {
         const text = row.manufacturerDescription.map(elm => elm.text.trim()).join(' ');
         row.manufacturerDescription = [{ text }];
       }
+      if (row.specifications) {
+        const text = row.specifications.map(elm => elm.text.trim()).join(' ');
+        row.specifications = [{ text }];
+      }
+      if (row.aggregateRating) {
+        const text = row.aggregateRating[0].text.replace(/(\d+)(.)(\d{0,1})(\d+)/g, '$1,$3');
+        row.aggregateRating = [{ text }];
+      }
+      if (row.manufacturerImages) {
+        row.manufacturerImages.forEach(image => {
+          image.text = image.text.replace(/(.+)(200w, )(.+)(\s)(400w)(.+)/g, 'https:$3');
+        })
+      }
     }
   }
   data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
