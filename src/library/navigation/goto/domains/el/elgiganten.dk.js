870104a07f012a5f74d16274f722f4775ef7e84b
Ravi Vachhani <ravi.vachhani@kcsitglobal.com>
2020-09-17 10:48:03 +0530

    Tab click change

--- src/library/navigation/goto/domains/el/elgiganten.dk.js
+++ src/library/navigation/goto/domains/el/elgiganten.dk.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'elgiganten.dk',
-    timeout: 20000,
+    timeout: 200000,
     country: 'DK',
     store: 'elgiganten',
     zipcode: '',
