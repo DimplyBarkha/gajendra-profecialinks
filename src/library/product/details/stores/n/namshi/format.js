037d951eaeb5a2a169a132892ca515bc87e2a8da
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-09-17 19:54:21 +0530

    Updated code

--- src/library/product/details/stores/n/namshi/format.js
+++ src/library/product/details/stores/n/namshi/format.js
@@ -5,88 +5,87 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-        for (const row of group) {
-            if (row.specifications) {
-                row.specifications.forEach(item => {
-                    item.text = item.text.replace(/\n\s*\n\s*\n\s*\n\s*/g, ' || ').trim();
-                    item.text = item.text.replace(/\n\s*\n\s*/g, ' : ').trim();
-                });
-            }
-            if (row.description) {
-                row.description.forEach(item => {
-                    item.text = item.text.replace(/\n/g, ' || ').trim();
-                });
-            }
-            if (row.category) {
-                var category_arr = [];
-                row.category.forEach(item => {
-                    var category = item.text.replace(/\s*\n\s*\n\s*/g, '').trim();
-                    category_arr = category.split(" >");
-                    category_arr.splice(0,1);
-                    category_arr.splice(category_arr.length-1,1);
-                });
-                if(category_arr.length){
-                    row.category = [];
-                    category_arr.forEach(item => {
-                        row.category.push({"text": item});
-                    });
-                }
-            }
-            if (row.variantInformation) {
-                let info = [];
-                row.variantInformation.forEach(item => {
-                    info.push(item.text);
-                });
-                if(info.length){
-                    row.variantInformation = [{"text": info.join(' | '), "xpath": row.variantInformation[0]["xpath"]}];
-                }else{
-                    delete row.variantInformation;
-                }
-            }
-            if (row.alternateImages) {
-                let info = [];
-                row.alternateImages.forEach(item => {
-                    item.text = item.text.replace(/cart\./g, 'zoom-desktop.').trim();
-                });
-            }            
-            if (row.variants) {
-                let variations = [];
-                let color = null;
-                row.variants.forEach(item => {
-                    let data = JSON.parse(item.text);
-                    if(data['variations']){
-                        data['variations'].forEach(variation => {
-                            variations.push(variation['sku']);
-                        });
-                    }
-                    color = data['attributes']['color'];
-                });
-                if(color){
-                    row.color = [{"text": color, "xpath": row.variants[0]["xpath"]}];
-                }
-                if(variations.length){
-                    row.variantId = data['sku'];
-                    row.variantCount = [{"text": variations.length, "xpath": row.variants[0]["xpath"]}];
-                    row.variants = [{"text": variations.join(' | '), "xpath": row.variants[0]["xpath"]}];
-                }else{
-                    delete row.variants;
-                }
-                if (row.ratingCount) {
-                    row.ratingCount.forEach(item => {
-                        item.text = item.text;
-                    });
-                }
-                if (row.aggregateRating) {
-                    row.aggregateRating.forEach(item => {
-                        item.text = item.text;
-                    });
-                }
-            }
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.specifications) {
+        row.specifications.forEach(item => {
+          item.text = item.text.replace(/\n\s*\n\s*\n\s*\n\s*/g, ' || ').trim();
+          item.text = item.text.replace(/\n\s*\n\s*/g, ' : ').trim();
+        });
+      }
+      if (row.description) {
+        row.description.forEach(item => {
+          item.text = item.text.replace(/\n/g, ' || ').trim();
+        });
+      }
+      if (row.category) {
+        var category_arr = [];
+        row.category.forEach(item => {
+          var category = item.text.replace(/\s*\n\s*\n\s*/g, '').trim();
+          category_arr = category.split(" >");
+          category_arr.splice(0, 1);
+          category_arr.splice(category_arr.length - 1, 1);
+        });
+        if (category_arr.length) {
+          row.category = [];
+          category_arr.forEach(item => {
+            row.category.push({ "text": item });
+          });
         }
+      }
+      if (row.variantInformation) {
+        let info = [];
+        row.variantInformation.forEach(item => {
+          info.push(item.text);
+        });
+        if (info.length) {
+          row.variantInformation = [{ "text": info.join(' | '), "xpath": row.variantInformation[0]["xpath"] }];
+        } else {
+          delete row.variantInformation;
+        }
+      }
+      if (row.alternateImages) {
+        let info = [];
+        row.alternateImages.forEach(item => {
+          item.text = item.text.replace(/cart\./g, 'zoom-desktop.').trim();
+        });
+      }
+      if (row.variants) {
+        let variations = [];
+        let color = null;
+        row.variants.forEach(item => {
+          let data = JSON.parse(item.text);
+          if (data['variations']) {
+            data['variations'].forEach(variation => {
+              variations.push(variation['sku']);
+            });
+          }
+          color = data['attributes']['color'];
+        });
+        if (color) {
+          row.color = [{ "text": color, "xpath": row.variants[0]["xpath"] }];
+        }
+        if (variations.length) {
+          row.variantId = data['sku'];
+          row.variantCount = [{ "text": variations.length, "xpath": row.variants[0]["xpath"] }];
+          row.variants = [{ "text": variations.join(' | '), "xpath": row.variants[0]["xpath"] }];
+        } else {
+          delete row.variants;
+        }
+        if (row.ratingCount) {
+          row.ratingCount.forEach(item => {
+            item.text = item.text;
+          });
+        }
+        if (row.aggregateRating) {
+          row.aggregateRating.forEach(item => {
+            item.text = item.text;
+          });
+        }
+      }
     }
-    return data;
+  }
+  return data;
 };
-  
+
 module.exports = { transform };
-  
--- src/library/product/details/stores/n/namshi/variantsFormat.js
+++ src/library/product/details/stores/n/namshi/variantsFormat.js
@@ -4,32 +4,32 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    let variations = [];
-    for (const { group } of data) {
-        for (const row of group) {    
-            if (row.variantId) {
-                row.variantId.forEach(item => {
-                    let v_data = JSON.parse(item.text);
-                    if(v_data['variations']){
-                        v_data['variations'].forEach(variation => {
-                            let tmp_variations = {};
-                            tmp_variations["variantId"] = [];
-                            tmp_variations["variantUrl"] = [];
-                            tmp_variations["variant"] = [];
-                            tmp_variations["variantId"].push({"text": variation['sku'], "xpath": item["xpath"]});
-                            tmp_variations["variantUrl"].push({"text": "https://en-ae.namshi.com/" + variation['link'], "xpath": item["xpath"]});
-                            tmp_variations["variant"].push({"text": variation['color'], "xpath": item["xpath"]});
-                            variations.push(tmp_variations);
-                        });
-                    }
-                });
-            }
-        }
+  let variations = [];
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.variantId) {
+        row.variantId.forEach(item => {
+          let v_data = JSON.parse(item.text);
+          if (v_data['variations']) {
+            v_data['variations'].forEach(variation => {
+              let tmp_variations = {};
+              tmp_variations["variantId"] = [];
+              tmp_variations["variantUrl"] = [];
+              tmp_variations["variant"] = [];
+              tmp_variations["variantId"].push({ "text": variation['sku'], "xpath": item["xpath"] });
+              tmp_variations["variantUrl"].push({ "text": "https://en-ae.namshi.com/" + variation['link'], "xpath": item["xpath"] });
+              tmp_variations["variant"].push({ "text": variation['color'], "xpath": item["xpath"] });
+              variations.push(tmp_variations);
+            });
+          }
+        });
+      }
     }
-    if(variations.length){
-        data[0]["group"] = variations;
-    }
-    return data;
+  }
+  if (variations.length) {
+    data[0]["group"] = variations;
+  }
+  return data;
 };
-  
+
 module.exports = { transform };
