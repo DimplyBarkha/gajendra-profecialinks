58420666dd1567085a3a840139da6646d7f93c04
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-17 17:21:09 +0530

    fixed bullet count issues

--- src/library/product/details/stores/b/bol/NL/extract.js
+++ src/library/product/details/stores/b/bol/NL/extract.js
@@ -24,11 +24,13 @@ module.exports = {
         const ulInDesc = "div[data-test='description']>ul";
         const descBullets = document.querySelector(ulInDesc);
         const body = document.querySelector('body');
+
         let liInDesc = document.querySelectorAll("div[data-test='description']>ul>li")
         let divEle = document.createElement('div')
         liInDesc.length > 0 ? body.appendChild(divEle) : false;
         divEle.id = 'bulletCount';
         divEle.title = liInDesc.length.toString();
+        
         if (descBullets) {
           body.appendChild(descBullets);
         }
