9e100d49e5a7caf92adcddc572beaab49cfc7656
Kishan Darji <kishan.darji@kcsitglobal.com>
2020-09-18 12:41:59 +0530

    Resolved Pagination and timeout issue
    Resolved Pagination and timeout issue

--- src/library/product/search/stores/c/catch/AU/paginate.js
+++ src/library/product/search/stores/c/catch/AU/paginate.js
@@ -7,20 +7,30 @@ async function implementation(
   const { keywords, page, offset } = inputs;
   const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
 
-   let url = await context.evaluate(function () {
-    
-    const next = document.querySelector('button.css-wzwbsc-Button-Button').nextElementSibling
+  if (nextLinkSelector) {
+    const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector), nextLinkSelector);
 
+    if (!hasNextLink) {
+      return false;
+    }
+  }
+
+  if (!openSearchDefinition) {
+    const { pager } = dependencies;
+    const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
+    if (success) {
+      return true;
+    }
+  }
+
+  let url = await context.evaluate(function () {
+    /* @type { HTMLLinkElement } */
+    const next = document.querySelector('head link[rel="next"]');
     if (!next) {
       return false;
     }
-
+    return next;
   });
-
-  const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector).nextElementSibling, 'button.css-wzwbsc-Button-Button');
-  if (!hasNextLink) {
-    return false;
-  }
 
   if (!url && openSearchDefinition) {
     url = openSearchDefinition.template
@@ -33,16 +43,19 @@ async function implementation(
     return false;
   }
 
+  console.log('Going to url', url);
   await dependencies.goto({ url });
   if (loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 20000 }, loadedSelector, noResultsXPath);
+    }, { timeout: 100000 }, loadedSelector, noResultsXPath);
   }
-
+  console.log('Checking no results', noResultsXPath);
   return await context.evaluate(function (xp) {
     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
     const e = r.iterateNext();
+    console.log(e);
     return !e;
   }, noResultsXPath);
 }
@@ -55,7 +68,7 @@ module.exports = {
     nextLinkSelector: 'button.css-wzwbsc-Button-Button',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'div.grid-row product-flex',
+    loadedSelector: null,
     noResultsXPath: null,    
     domain: 'catch.com.au',
     zipcode: '',
