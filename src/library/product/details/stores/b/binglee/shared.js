db350bdf707f0318e42a6eecb425d58775e2cc84
Kishan Darji <kishan.darji@kcsitglobal.com>
2020-09-18 10:11:16 +0530

    Feedback Point Resolved
    Feedback Point Resolved

--- src/library/product/details/stores/b/binglee/shared.js
+++ src/library/product/details/stores/b/binglee/shared.js
@@ -4,22 +4,55 @@
 * @param {ImportIO.Group[]} data
 * @returns {ImportIO.Group[]}
 */
-const transform = (data) => {
+
+const transform = (data,context) => {
+  //var $ = context.jQuery;
+  //var jsonLD = $('script[type="application/ld+json"]');
+  //console.log("data0=============="+JSON.stringify(data))
   for (const { group } of data) {
-    for (const row of group) {
-      /*if (row.specifications) {
+    for (const row of group) {  
+     // console.log("row=============="+JSON.stringify(row))   
+
+      if (row.ratingCount) {
         let text = '';
-        row.specifications.forEach(item => {
-          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        row.ratingCount.forEach(item => {
+         // console.log("ratingCount======"+JSON.stringify(JSON.parse(item.raw).aggregateRating.reviewCount));
+          text = JSON.parse(item.raw).aggregateRating.reviewCount;
         });
-        row.specifications = [
-          {
-            text: text.slice(0, -4),
-          },
-        ];
-      }*/
+        row.ratingCount = [{ text }];
+      }
+
+      if (row.aggregateRating) {
+        let text = '';
+        row.aggregateRating.forEach(item => {
+          //console.log("aggregateRating======"+JSON.stringify(JSON.parse(item.raw).aggregateRating.ratingValue));
+          text = JSON.parse(item.raw).aggregateRating.ratingValue;
+        });
+        row.aggregateRating = [{ text }];
+      }
+
     }
   }
+
+
+  // Clean up data
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+
   return data;
 };
 
