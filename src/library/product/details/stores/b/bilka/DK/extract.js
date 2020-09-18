122df5509034f3b5565128b5f9bb5b0607ae3797
abhay.patil <abhay.patil@kcsitglobal.com>
2020-09-18 10:57:35 +0530

    changes

--- src/library/product/details/stores/b/bilka/DK/extract.js
+++ src/library/product/details/stores/b/bilka/DK/extract.js
@@ -8,4 +8,37 @@ module.exports = {
     domain: 'bilka.dk',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain ,transform: transformParam}, context, { productDetails }) => {     
+    
+    await context.evaluate(async function () {       
+     const productInfo = preFetchProductDetails();
+     alert(productInfo['sku']);
+      addEleToDoc('skuId', productInfo['sku']);
+      // addEleToDoc('gtinId', productInfo[0].gtin13);
+      // addEleToDoc('priceCurrency', productInfo[0].offers[0].priceCurrency);  
+     
+      function preFetchProductDetails () {
+        let productInfo = findProductDetails('//script[@data-v-6280c757 and @type="application/ld+json"]');                        
+        productInfo = JSON.parse(productInfo.textContent);        
+        return productInfo;
+      }      
+
+      function findProductDetails (xpath) {        
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;                
+        const productDetails = element;
+        return productDetails;
+      }
+
+      function addEleToDoc (key, value) {
+        const prodEle = document.createElement('div');
+        prodEle.id = key;
+        prodEle.textContent = value;
+        prodEle.style.display = 'none';
+        document.body.appendChild(prodEle);
+      }         
+         
+    });      
+    
+    return await context.extract(productDetails, { transform: transformParam });
+  },
 };
