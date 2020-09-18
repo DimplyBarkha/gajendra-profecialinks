c13eaad69fbe4b704b045de3b83db7f4759e9ccb
nehashende <neha.shende@synerzip.com>
2020-09-18 16:19:38 +0530

    #628. Created extractor robot for Amazon IT core.

---
+++ src/library/product/details/stores/a/amazon/IT/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'IT',
+    store: 'amazon',
+    transform: null,
+    domain: 'amazon.it',
+    zipcode: '20019',
+  },
+};
