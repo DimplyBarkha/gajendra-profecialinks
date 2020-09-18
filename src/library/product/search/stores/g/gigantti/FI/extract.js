12d8c9d8eb587e52c6397ccbf8ca52dd609f1f53
KCSPL\jatin <jatin@kcspl.co.in>
2020-09-18 10:52:48 +0530

    addind break on getting max needed records

--- src/library/product/search/stores/g/gigantti/FI/extract.js
+++ src/library/product/search/stores/g/gigantti/FI/extract.js
@@ -1,6 +1,6 @@
 const { transform } = require('../../../../shared');
 
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -10,38 +10,40 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  const maxRecords = results ?  results : 150; // need to get from main action
-  
+  const maxRecords = results ? results : 150; // need to get from main action
+
 
   let scrollTop = 0;
-  let recordsOnPage = 0; 
+  let recordsOnPage = 0;
   //TODO: maxRecords should be updated based on max number of results on the page
-  while (scrollTop !== 200000) { // TODO: need to update it for a selector for end of page
-    if(recordsOnPage < maxRecords) {
-    scrollTop = await context.evaluate(function(scrollTop){
-      scrollTop += 10000;
-      window.parent.scroll(0, scrollTop);
-      return scrollTop;   
-    },scrollTop);
-    let btn = await context.evaluate(() => {return Boolean(!!document.querySelector('button.coi-banner__accept'))});
-    if(btn){
-      try{ 
-      await context.click('button.coi-banner__accept');
+  while (scrollTop !== 500000) { // TODO: need to update it for a selector for end of page
+    if (recordsOnPage < maxRecords) {
+      scrollTop = await context.evaluate(function (scrollTop) {
+        scrollTop += 10000;
+        window.parent.scroll(0, scrollTop);
+        return scrollTop;
+      }, scrollTop);
+      let btn = await context.evaluate(() => { return Boolean(!!document.querySelector('button.coi-banner__accept')) });
+      if (btn) {
+        try {
+          await context.click('button.coi-banner__accept');
+        }
+        catch (error) { }
       }
-      catch(error){}
-    }  
-    recordsOnPage = await context.evaluate(() => {return document.querySelectorAll('div.mini-product') ? document.querySelectorAll('div.mini-product').length : -1});
-    console.log(recordsOnPage);
-    //if(recordsOnPage == recordsCollected) return -1;
-     }
-     if (scrollTop === 200000) { // TODO: need to update it for a selector for end of page
+      recordsOnPage = await context.evaluate(() => { return document.querySelectorAll('div.mini-product') ? document.querySelectorAll('div.mini-product').length : -1 });
+      console.log('recordssss', recordsOnPage);
+      //if(recordsOnPage == recordsCollected) return -1;
+    } else {
       break;
-    } 
     }
-    
+    if (scrollTop === 500000) { // TODO: need to update it for a selector for end of page
+      break;
+    }
+  }
+
 
   return await context.extract(productDetails, { transform });
-  
+
 }
 
 module.exports = {
