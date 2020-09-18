8bd3d33a58330f45a1fafeb1cb536d32b8a4dbf6
abhay.patil <abhay.patil@kcsitglobal.com>
2020-09-18 12:09:36 +0530

    remove spaces

--- src/library/product/details/stores/k/kogan/AU/transform.js
+++ src/library/product/details/stores/k/kogan/AU/transform.js
@@ -8,18 +8,15 @@ const transform = (data, context) => {
     for (const { group } of data) {
       for (const row of group) {  
         try { 
-
         //   if (row.price || row.listPrice) {
         //     row.price = [{ text: row.price[0].text }, { text: row.onlinePriceCurrency[0].text }];
         //     row.listPrice = [{ text: row.listPrice[0].text }, { text: row.onlinePriceCurrency[0].text }];
         //   }    
 
-            if (row.price) {
-          row.price = [{ text: row.price[0].text.substring(1) }, { text: row.price[0].text.charAt(0) }];
-        //  row.listPrice = [{ text: row.listPrice[0].text.substring(1) }, { text: row.listPrice[0].text.charAt(0) }];
-  
-     }   
-  
+        if (row.price) {
+            row.price = [{ text: row.price[0].text.substring(1) }, { text: row.price[0].text.charAt(0) }];
+            //  row.listPrice = [{ text: row.listPrice[0].text.substring(1) }, { text: row.listPrice[0].text.charAt(0) }];  
+      }     
         } catch (exception) { console.log('Error in transform', exception); }
   
       }
