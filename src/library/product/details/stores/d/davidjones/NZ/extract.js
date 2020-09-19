58a0fa327584c726f265cefce93aefd6872b61e1
Prasanna Barate <prasanna.barate@synerzip.com>
2020-09-19 14:40:07 +0530

    (#545) Fixed additional_description_bullets_info and count.

--- src/library/product/details/stores/d/davidjones/NZ/extract.js
+++ src/library/product/details/stores/d/davidjones/NZ/extract.js
@@ -9,4 +9,39 @@ module.exports = {
     domain: 'davidjones.com',
     zipcode: '',
   },
+  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+    try {
+      await context.evaluate(() => {
+        function addHiddenDiv(id, content) {
+          const newDiv = document.createElement('div');
+          newDiv.id = id;
+          newDiv.textContent = content;
+          newDiv.style.display = 'none';
+          document.body.appendChild(newDiv);
+        }
+        if (document.querySelector('div[class*="long-description"]')) {
+          let desc = document.querySelector('div[class*="long-description"]').innerHTML;
+          desc = desc.replace(/>- /g, '>|| ').replace(/<li.*?>/gm, ' || ').replace(/<.*?>/gm, '').replace(/&nbsp;/g, '').trim();
+          addHiddenDiv('customDescription', desc);
+          const bulletExceptLast = desc.replace(/.*?(\|\|.*\|\|).*/g, '$1')
+          let bulletLast = ''
+          const lengthL = bulletExceptLast.match(/\|\|$/) ? bulletExceptLast.match(/\|\|$/).length : 0
+          if (lengthL) {
+            const remainingPart = desc.replace(/.*?(\|\|.*\|\|)(.*)/g, '$2').split('.') || []
+            bulletLast = remainingPart && remainingPart.length ? remainingPart[0] : ''
+          }
+          const descriptionBullet = bulletExceptLast + bulletLast
+          const descriptionBulletCount = descriptionBullet.match(/\|\|/g) ? descriptionBullet.match(/\|\|/g).length : 0
+          if (descriptionBulletCount) {
+            addHiddenDiv('bulletsFromDescription', descriptionBullet)
+            addHiddenDiv('bulletsFromDescriptionCount', descriptionBulletCount)
+          }
+        }
+      })
+    } catch (e) {
+      console.log(e)
+    }
+    await new Promise(resolve => setTimeout(resolve, 10000))
+    return await context.extract(productDetails, { transform });
+  },
 };
--- src/library/product/details/stores/d/davidjones/NZ/format.js
+++ src/library/product/details/stores/d/davidjones/NZ/format.js
@@ -19,17 +19,6 @@ const transform = (data) => {
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
   for (const { group } of data) {
     for (const row of group) {
-      if (row.description) {
-        if (row.description[0].text.match(/\n(\d+)\./g)) {
-          const bulletCount = row.description[0].text.match(/\n(\d+)\./g).length;
-          row.descriptionBullets = [{ text: bulletCount }];
-          var desc = row.description[0].text;
-          desc = desc.split('CAVEATS');
-          desc = desc[1].replace(/\n(\d+)\./g, '||');
-          row.additionalDescBulletInfo = [{ text: desc }];
-        }
-        row.description[0].text = row.description[0].text.replace(/\n(\d+)\./g, '||').replace(/\n \n/g, ' ').replace(/\n/g, '');
-      }
       if (row.availabilityText) {
         row.availabilityText = row.availabilityText[0].text.includes('Available Now') ? [{ text: 'In Stock' }] : [{ text: 'Out of Stock' }];
       }
