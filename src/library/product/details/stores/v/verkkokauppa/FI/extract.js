25534e21aca11b335249672abcaf28e9141752b7
abhay.patil <abhay.patil@kcsitglobal.com>
2020-09-19 21:03:45 +0530

    resolve bugs

--- src/library/product/details/stores/v/verkkokauppa/FI/extract.js
+++ src/library/product/details/stores/v/verkkokauppa/FI/extract.js
@@ -9,8 +9,7 @@ module.exports = {
     domain: 'verkkokauppa.com',
     zipcode: '',
   },
-  implementation: async ({ inputString }, { country, domain ,transform: transformParam}, context, { productDetails }) => {     
-    
+  implementation: async ({ inputString }, { country, domain ,transform: transformParam}, context, { productDetails }) => {        
     await context.evaluate(async function () {       
      const productInfo = preFetchProductDetails();
       addEleToDoc('skuId', productInfo[0].sku);
@@ -50,18 +49,25 @@ module.exports = {
         let tempweightNet1 = document.evaluate('//section[contains(@class,"description-container__full-text")]/div/ul/li[contains(text(),"Paino")]', document).iterateNext().textContent.trim();              
         let tempweightNet2 = tempweightNet1.split(':');
         if(tempweightNet2 !=null)
-        { 
-          let tempweightNet3 = tempweightNet2[1];  
-          let tempweightNet4=tempweightNet2[1];          
-          if(tempweightNet3.indexOf("kg") ==-1)
-          {
-            tempweightNet4 = tempweightNet2[1] + ' kg';
-          }             
-          if(tempweightNet1.indexOf("runko") > 0 )          
+        {           
+          if(tempweightNet2.length==1)
           {            
-            tempweightNet4 =  tempweightNet2[1].split('/')[1];            
-          }                         
-          addEleToDoc('weightNettempId', tempweightNet4);
+            var temp1234 =tempweightNet1.replace( /^\D+/g, ''); 
+            addEleToDoc('weightNettempId', tempweightNet1.replace( /^\D+/g, ''))     
+          }
+          else{          
+                let tempweightNet3 = tempweightNet2[1];  
+                let tempweightNet4=tempweightNet2[1];                             
+                if(tempweightNet3.indexOf("kg") ==-1)
+                {
+                  tempweightNet4 = tempweightNet2[1] + ' kg';
+                }                             
+                if(tempweightNet1.indexOf("runko") > 0 )          
+                {            
+                  tempweightNet4 =  tempweightNet2[1].split('/')[1];            
+                }                         
+                addEleToDoc('weightNettempId', tempweightNet4);
+           }
         }
         
       }
@@ -71,14 +77,7 @@ module.exports = {
       {
         let tempDescription1 = document.evaluate('//div[contains(@class, "product-description__description-container")]', document).iterateNext().textContent.trim();              
         addEleToDoc('descriptionTempId', tempDescription1);
-      }
-
-      // let tempWeightGross = document.evaluate('//section[contains(@class,"product-details")]/div/ul/li[contains(text(),"Paino")]',document).iterateNext(); 
-      // if(tempWeightGross !=null)
-      // {
-      //   let tempWeightGross1 = document.evaluate('//section[contains(@class,"description-container__full-text")]/div/ul/li[contains(text(),"Paino")]', document).iterateNext().textContent.trim();              
-      //   addEleToDoc('weightGrossTempId', tempWeightGross1);
-      // }
+      }      
 
       let tempManufactureImage = document.evaluate('//a[contains(@class,"product-shop-logo")]/figure/img/@src',document).iterateNext(); 
       if(tempManufactureImage !=null)
@@ -86,29 +85,14 @@ module.exports = {
         let tempManufactureImage1 = document.evaluate('//a[contains(@class,"product-shop-logo")]/figure/img/@src', document).iterateNext().textContent.trim();              
         addEleToDoc('tempManufactureImage1', tempManufactureImage1);
       }
-
-       //let tempVideos = document.evaluate('//iframe//@src',document).iterateNext();              
-      // if(tempVideos !=null)
-      // {
-      //   let tempVideos1 = document.evaluate('//ul[contains(@class,"product-description-links")]//iframe//@src', document).iterateNext().textContent.trim();              
-      //   alert(tempVideos1);
-      //   addEleToDoc('tempVideosId', tempVideos1);
-      // }
-
-      //let tempadditionalDescBulletInfo1 = document.querySelector('ul.product-description-links iframe');   
-      //alert(tempadditionalDescBulletInfo1);
-      // tempadditionalDescBulletInfo1.forEach(element => {
-      //   alert(element.innerHTML);
-      // });           
-
-      // let tempadditionalDescBulletInfo = document.evaluate('//section[contains(@class,"description-container__full-text")]/div/ul[1]//li',document).iterateNext(); 
-      // if(tempadditionalDescBulletInfo !=null)
-      // {
-      //   let tempadditionalDescBulletInfo1 = document.evaluate('//section[contains(@class,"description-container__full-text")]/div/ul[1]', document).iterateNext().childNodes.length;                      
-      //   alert(tempadditionalDescBulletInfo1);
-      //   //let temptest=tempadditionalDescBulletInfo1.split["."];        
-      //   addEleToDoc('tempadditionalDescBulletInfoId', tempadditionalDescBulletInfo1);
-      // }
+      
+      let tempVideos = document.evaluate('//li[contains(@class,"product-description-links__item")]//a//@src',document).iterateNext();                    
+      if(tempVideos !=null)
+      {
+        let tempVideos1 = document.evaluate('//ul[contains(@class,"product-description-links")]//a//@src', document).iterateNext().textContent.trim();                              
+        var tempVideos2=tempVideos1.split('/');        
+        addEleToDoc('tempVideosId', 'https://www.youtube.com/watch?v=' + tempVideos2[4]);
+      }    
 
       let tempadditionalDescBulletInfo = document.querySelectorAll('div.product-description__description-container div ul li').length;   
       if(tempadditionalDescBulletInfo > 0)
