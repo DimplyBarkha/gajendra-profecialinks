93fcdb0fe2f33b51ea90c2dccb88d928e2f51197
Gajender Ramola <ramola106@gmail.com>
2020-09-19 17:49:58 +0530

    fix price issue

--- src/library/product/search/stores/m/melectronics/format.js
+++ src/library/product/search/stores/m/melectronics/format.js
@@ -1,61 +1,59 @@
 const transform = (data) => {
     var rank_count = 1;
-    for (const { group} of data) {
-        
+    for (const { group } of data) {
         for (const row of group) {
-            
-            
             if (row.id) {
                 row.id.forEach(item => {
                     var myRegexp = /de\/p\/(\d+\/)/g;
                     var match = myRegexp.exec(item.text);
-                    if(match){
-                        if(match.length){                            
+                    if (match) {
+                        if (match.length) {
                             match[1] = match[1].replace(/\//g, '');
                             item.text = match[1].trim();
-                        }else{
+                        } else {
                             delete row.id;
-                        }                        
-                        row.rankOrganic = [{'text':rank_count}];
-                        row.rank = [{'text':rank_count}];
+                        }
+                        row.rankOrganic = [{ 'text': rank_count }];
+                        row.rank = [{ 'text': rank_count }];
                     }
-                    else{
+                    else {
                         delete row.id;
                     }
-                 });
-                 rank_count = rank_count + 1;
-            }  
-            if(row.productUrl) {
+                });
+                rank_count = rank_count + 1;
+            }
+            if (row.productUrl) {
                 row.productUrl.forEach(item => {
                     item.text = 'https://www.melectronics.ch'.concat(item.text);
                 });
             }
-            if(row.aggregateRating2) {
+            if (row.aggregateRating2) {
                 row.aggregateRating2.forEach(item => {
                     var myRegexp = /Keine/ig;
                     var match = myRegexp.exec(item.text);
-                    if(match){
+                    if (match) {
                         delete row.aggregateRating2;
                     }
-                    else{
+                    else {
                         item.text = item.text.replace(/\s*Sterne\s*/ig, '').trim();
                         item.text = item.text.replace(/\s*Stern\s*/ig, '').trim();
                     }
                 });
             }
-            if(row.name && row.brandText ) {
+            if (row.name && row.brandText) {
                 row.name.forEach(item => {
-                    item.text = row.brandText[0].text.concat(' ',item.text);
+                    item.text = row.brandText[0].text.concat(' ', item.text);
                 });
             }
-            if(row.price ) {
+            if (row.price) {
                 row.price.forEach(item => {
                     item.text = item.text.replace(/\.–/g, '').trim();
-                    item.text = item.text.replace(/–/g, '').trim();
+                    item.text = item.text.replace('.', ',').trim();
+                    item.text = item.text.replace('/\,$/g', '').trim();
                 });
             }
         }
     }
     return data;
-  };  
-  module.exports = { transform } 
+};
+module.exports = { transform } 
