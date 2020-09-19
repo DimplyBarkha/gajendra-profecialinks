91a99456d022986563e3cf415cba4e8ee911e5eb
Gajender Ramola <ramola106@gmail.com>
2020-09-19 20:21:55 +0530

    fix the price issue and other kyes

--- src/library/product/details/stores/m/melectronics/format.js
+++ src/library/product/details/stores/m/melectronics/format.js
@@ -1,74 +1,72 @@
 const transform = (data) => {
-    for (const { group} of data) {
-        for (const row of group) {
-            
-            if (row.availabilityText) {
-                row.availabilityText.forEach(item => {
-                    item.text  = 'In Stock';
-                });
-            }
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.alternateImages) {
+        row.alternateImages.forEach(item => {
+          item.text = item.text.replace(/(\/fm-thumbnail\/)/g, '/fm-xl/').trim();
+        });
+      }
+      if (row.image) {
+        row.image.forEach(item => {
+          item.text = item.text.replace(/(\/fm-sm\/)/g, '/fm-xl/').trim();
+        });
+      }
 
-            if (row.alternateImages) {              
-              row.alternateImages.forEach(item => {
-                item.text = item.text.replace(/(\/fm-thumbnail\/)/g, '/fm-xl/').trim();
-              });              
-            }
+      if (row.description) {
+        row.description.forEach(item => {
+          item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
+        });
+      }
 
-            if (row.image) {
-              row.image.forEach(item => {
-                  item.text  = item.text.replace(/(\/fm-sm\/)/g, '/fm-xl/').trim();
-              });
-            }
+      if (row.additionalDescBulletInfo) {
+        row.additionalDescBulletInfo.forEach(item => {
+          item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
+        });
+      }
 
-            if (row.description) {
-                row.description.forEach(item => {
-                  item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
-                });
-            }
+      if (row.descriptionBullets) {
+        row.descriptionBullets = [{ 'text': row.descriptionBullets.length, 'xpath': row.descriptionBullets[0].xpath }];
+      }
 
-            if (row.additionalDescBulletInfo) {
-              row.additionalDescBulletInfo.forEach(item => {
-                item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
-              });
-            }
+      if (row.specifications) {
+        let info = [];
+        row.specifications.forEach(item => {
+          info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());
+        });
+        row.specifications = [{ 'text': info.join(' || '), 'xpath': row.specifications[0].xpath }];
+      }
 
-            if (row.descriptionBullets) {
-              row.descriptionBullets = [{'text':row.descriptionBullets.length, 'xpath':row.descriptionBullets[0].xpath}];              
-            }
+      if (row.ratingCount) {
+        row.ratingCount.forEach(item => {
+          var matches = /(\d+)/isg.exec(item.text);
+          if (matches) {
+            item.text = matches[1]
+          }
+          else {
+            item.text = '';
+          }
+        });
+      }
 
-            if (row.specifications) {
-                let info = [];          
-                row.specifications.forEach(item => {
-                    info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());            
-                });          
-                row.specifications = [{'text':info.join(' || '),'xpath':row.specifications[0].xpath}];          
-            }
-
-            if (row.ratingCount) {
-              row.ratingCount.forEach(item => {
-                var matches = /(\d+)/isg.exec(item.text);                
-                if (matches){
-                  item.text = matches[1]
-                }
-                else{
-                  item.text  = '';
-                }
-              });
-            }
-
-            if (row.listPrice) {
-              row.listPrice.forEach(item => {
-                  item.text  = item.text.replace(/(\s*statt\s*)/g, '').trim();
-              });
-            }
-            if (row.price) {
-              row.price.forEach(item => {
-                  item.text  = item.text.replace(/\.–/g, '').trim();
-                  item.text  = item.text.replace(/–/g, '').trim();
-              });
-            }
-        }
+      if (row.listPrice) {
+        row.listPrice.forEach(item => {
+          item.text = item.text.replace(/(\s*statt\s*)/g, '').trim();
+          item.text = item.text.replace(/–/g, '').trim();
+          item.text = item.text.replace(/\’/g, '').trim();
+          item.text = item.text.replace(/\./g, ',').trim();
+          item.text = item.text.replace(/\,$/g, '').trim();
+        });
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace(/–/g, '').trim();
+          item.text = item.text.replace(/\’/g, '').trim();
+          item.text = item.text.replace(/\./g, ',').trim();
+          item.text = item.text.replace(/\,$/g, '').trim();
+        });
+      }
     }
-      return data;
-  };
-  module.exports = { transform } 
+  }
+  return data;
+};
+module.exports = { transform } 
