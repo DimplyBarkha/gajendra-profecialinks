df6c6fcb2dfa51ba62cbe7f4178fcc4ff55fd586
Abhisek <abhisek.d@osmosys.asia>
2020-09-20 17:31:36 +0530

    adding changes

--- src/library/product/search/stores/e/elcorteingles_deportes/ES/execute.js
+++ src/library/product/search/stores/e/elcorteingles_deportes/ES/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'ES',
     store: 'elcorteingles_deportes',
     domain: 'elcorteingles.es',
-    url: 'https://www.elcorteingles.es/search/?s={searchTerms}&stype=text_box',
+    url: 'https://www.elcorteingles.es/search/1/?s={searchTerms}',
     loadedSelector: 'img.js_preview_image.lazyloaded',
     noResultsXPath: '//div[@class="products_list-container _no_products vp"] | //html[not(//div[@id="products-list"]/ul/li)]',
     zipcode: '',
