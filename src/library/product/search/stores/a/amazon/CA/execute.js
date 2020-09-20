63444f4769177d2b6250e771020dfca3ff7ac94b
David Skoglund <david.skoglund@import.io>
2020-09-19 19:45:25 -0700

    #328. updated  loaded selector.

--- src/library/product/search/stores/a/amazon/CA/execute.js
+++ src/library/product/search/stores/a/amazon/CA/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'amazon',
     domain: 'amazon.ca',
     url: 'https://www.amazon.ca/s?k={searchTerms}&ref=nb_sb_noss_2',
-    loadedSelector: 'div[class*="result-list"]>div[data-asin]>div>span[cel_widget_id="MAIN-SEARCH_RESULTS"]',
+    loadedSelector: 'div[class*="result-list"]>div[data-asin]>div>span[cel_widget_id*="MAIN-SEARCH_RESULTS"]',
     noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
   },
 };
