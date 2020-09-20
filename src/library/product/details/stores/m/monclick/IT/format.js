1c0a2027be457fe3b01464603bd6c9d90ad3cc3e
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-09-20 11:28:52 +0530

    #589. QA comments fixed.

--- src/library/product/details/stores/m/monclick/IT/format.js
+++ src/library/product/details/stores/m/monclick/IT/format.js
@@ -20,16 +20,16 @@ const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
       if (row.additionalDescBulletInfo && row.additionalDescBulletInfo.length === 1) {
-        row.additionalDescBulletInfo[0].text = row.additionalDescBulletInfo[0].text.replace(/\n -/g, ' || ').replace(/^-/, '||').replace(/\\n/g, ' ')
-        const bulletCount = row.additionalDescBulletInfo[0].text.match(/\|\|/g).length
-        row.descriptionBullets = [{ text: bulletCount }]
+        row.additionalDescBulletInfo[0].text = row.additionalDescBulletInfo[0].text.replace(/\n -/g, ' || ').replace(/^-/, '||').replace(/\\n/g, ' ');
+        const bulletCount = row.additionalDescBulletInfo[0].text.match(/\|\|/g).length;
+        row.descriptionBullets = [{ text: bulletCount }];
       } else if (row.additionalDescBulletInfo && row.additionalDescBulletInfo.length > 1) {
-        row.descriptionBullets = [{ text: row.additionalDescBulletInfo.length }]
+        row.descriptionBullets = [{ text: row.additionalDescBulletInfo.length }];
         const bulletText = row.additionalDescBulletInfo.reduce((bulletText = '', item) => {
-          bulletText += ' || ' + item.text.replace(/^-/, '').replace(/\\n/g, ' ')
-          return bulletText
-        }, '')
-        row.additionalDescBulletInfo = [{ text: bulletText }]
+          bulletText += ' || ' + item.text.replace(/^-/, '').replace(/\\n/g, ' ');
+          return bulletText;
+        }, '');
+        row.additionalDescBulletInfo = [{ text: bulletText }];
       }
       if (row.description) {
         row.description1[0].text = row.description1[0].text.replace(/\n \n/g, ' ');
@@ -42,12 +42,30 @@ const transform = (data) => {
         row.description[0].text = row.description1[0].text + ' | ' + row.description[0].text + ' | ' + row.description2[0].text;
       }
       if (row.availabilityText) {
-        row.availabilityText = row.availabilityText[0].text.includes('Non disponibile') ? [{ text: 'Out of Stock' }] : [{ text: 'In Stock' }];
+        row.availabilityText = row.availabilityText[0].text === 'Disponibile' ? [{ text: 'In Stock' }] : [{ text: 'Out of Stock' }];
       }
       if (row.price) {
         let amt = '';
         amt = row.price[0].text.split('\n', 1)[0];
         row.price = [{ text: amt }];
+      }
+      if (!row.shippingDimensions) {
+        if (row.width && row.depth && row.height && row.weightNet) {
+          const w = row.width[0].text.split(' ');
+          const d = row.depth[0].text.split(' ');
+          const h = row.height[0].text.split(' ');
+          const dim = w[0] + ' x ' + d[0] + ' x ' + h[0] + w[1] + '; ' + row.weightNet[0].text;
+          row.shippingDimensions = [{ text: dim }];
+        }
+      }
+      if (!row.color) {
+        if (row.color1) {
+          row.color = row.color1;
+        } else if (row.color2) {
+          var text = row.color2[0].text.replace(/\n/g, '').replace(/.*- Colore: (.*)/, '$1').replace(/((?:\S+\s+){2}\S+)/, '$1');
+          text = text.split(' - ');
+          row.color = [{ text: text[0] }];
+        }
       }
     }
   }
