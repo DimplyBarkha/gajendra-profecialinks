467b61c9bc09547e064c7aef4788dbca6508d5c3
Nilesh <nilesh@profecialinks.com>
2020-09-21 20:09:41 +0530

    changes to fix price and rank bug

--- src/library/product/search/stores/s/sprii/format.js
+++ src/library/product/search/stores/s/sprii/format.js
@@ -4,16 +4,26 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-        for (const row of group) {
-            if (row.brandText && row.brandText.length) {
-                let str = row.brandText[0].text;
-                str = str.substr(str.lastIndexOf('/') + 1);
-                row.brandText[0].text = str.length ? str.split('-')[0] : 'Brand Not Found';
-            }
-        }
+  for (const { group } of data) {
+    var rankIndex = 1;
+    for (const row of group) {
+      if (row.brandText && row.brandText.length) {
+        let str = row.brandText[0].text;
+        str = str.substr(str.lastIndexOf('/') + 1);
+        row.brandText[0].text = str.length ? str.split('-')[0] : 'Brand Not Found';
+      }
+      if (row.price && row.price.length) {
+        let str = row.price[0].text;
+        str = str.replace('٫', ',').replace('٬', ',').replace('.', ',');
+        row.price[0].text = str;
+      }
+      if (row.rank) {
+        row.rank[0].text = rankIndex;
+      }
+      rankIndex++;
     }
-    return data;
+  }
+  return data;
 };
 
 module.exports = { transform };
