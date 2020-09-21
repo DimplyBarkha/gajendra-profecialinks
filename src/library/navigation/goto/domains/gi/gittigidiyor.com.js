393f413f13226fc7ce0d2052de92de47a2a46655
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-21 23:59:20 +0530

    Added search extractor

---
+++ src/library/navigation/goto/domains/gi/gittigidiyor.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'gittigidiyor.com',
+    timeout: null,
+    country: 'TR',
+    store: 'gittigidiyor',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/g/gittigidiyor/TR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'TR',
+    store: 'gittigidiyor',
+    transform: null,
+    domain: 'gittigidiyor.com',
+    zipcode: '',
+  },
+};
