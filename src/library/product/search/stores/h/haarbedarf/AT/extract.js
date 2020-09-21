21f6fb20985e2ee551963822bfd33242c4f49215
AdamTo <a.a.tomaszewski@gmail.com>
2020-09-21 12:57:26 +0200

    search complete

--- src/library/product/search/stores/h/haarbedarf/AT/extract.js
+++ src/library/product/search/stores/h/haarbedarf/AT/extract.js
@@ -1,11 +1,33 @@
+const { transform } = require('../../../../shared');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(() => {
+    const productsOnPage = document.querySelectorAll('div.loop-product-image-wrapper');
+    const numberOfProductsOnPage = productsOnPage ? productsOnPage.length : 0;
+    for (let i = 0; i < numberOfProductsOnPage; i++) {
+      document.querySelectorAll('div.loop-product-image-wrapper')[i].setAttribute('rankOrganic', `${i + 1}`);
+    }
+  });
+
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'AT',
     store: 'haarbedarf',
-    transform: null,
+    transform: transform,
     domain: 'haarbedarf.at',
     zipcode: '',
   },
+  implementation,
 };
