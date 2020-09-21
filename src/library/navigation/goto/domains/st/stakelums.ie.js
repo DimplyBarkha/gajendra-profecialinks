5c85a19e0c541f6759103301387e5615cbfffd9b
Vipin Kumar <vipinkumar.mangalam@hcl.com>
2020-09-21 18:59:23 +0530

    domain st

---
+++ src/library/navigation/goto/domains/st/stakelums.ie.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'stakelums.ie',
+    timeout: null,
+    country: 'IE',
+    store: 'stakelums',
+    zipcode: '',
+  },
+};
