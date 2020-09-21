f4e507d4eff1b3d47d0fb5787529f67a6092f831
Manoj Dey <manoj.d@OSMLIN16>
2020-09-21 20:25:41 +0530

    fixed lint errors

--- src/library/product/search/stores/h/hepsiburada/TR/extract.js
+++ src/library/product/search/stores/h/hepsiburada/TR/extract.js
@@ -8,12 +8,12 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
   await context.evaluate(async function () {
-    function timeout(ms) {
+    function timeout (ms) {
       return new Promise((resolve) => setTimeout(resolve, ms));
     }
-    document.querySelectorAll("span.ratings > span").forEach(item => {
+    document.querySelectorAll('span.ratings > span').forEach(item => {
       const element = item.getAttribute('style').match(/(\d+)/)[0];
-      item.setAttribute('aggregaterating', element/20);
+      item.setAttribute('aggregaterating', element / 20);
     });
     await timeout(5000);
   });
