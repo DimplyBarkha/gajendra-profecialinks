c39adedf1a9b34fe86a895333cf75de2521fc152
unknown <ganesh.working@gmail.com>
2020-09-21 23:30:17 +0530

    setup variant start

---
+++ src/library/product/details/stores/m/melectronics/CH/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'CH',
+    store: 'melectronics',
+    transform: null,
+    domain: 'melectronics.ch',
+    zipcode: '',
+  },
+};
