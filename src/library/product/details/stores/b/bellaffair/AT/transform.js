b9a5fc5fadf7b1379c372b7c581084fa7ffe7540
KCSPL\jatin <jatin@kcspl.co.in>
2020-09-21 13:32:00 +0530

    QA1 feedback points

--- src/library/product/details/stores/b/bellaffair/AT/transform.js
+++ src/library/product/details/stores/b/bellaffair/AT/transform.js
@@ -8,21 +8,59 @@ const transform = (data) => {
                 if (row.color) {
                     row.color = [{ text: row.color[0].text.substring(1) }];
                 }
-                if (row.firstVariant) {
+                /* if (row.firstVariant) {
                     row.firstVariant = [{ text: row.firstVariant[0].text.substring(1) }];
-                }
-                if (row.variantInformation) {
+                } */
+                /* if (row.variantInformation) {
                     row.variantInformation = [{ text: row.variantInformation[0].text.substring(1) }];
-                }
+                } */
                 if (row.packSize) {
                     row.packSize = [{ text: row.packSize[0].text.substring(1) }];
                 }
                 if (row.description) {
                     let text = '';
-                    text = row.description[0].text.replace(/\n/g, '||');;
+                    text = row.description[0].text.replace(/\n/g, ' || ');;
                     row.description = [{ text }];
                 }
+                if (row.variants) {
+                    let text = '';
+                    row.variants.forEach(item => {
+                        text += `${item.text} | `;
+                    });
+                    row.variants = [
+                        {
+                            text: text.slice(0, -3).trim(),
+                        },
+                    ];
+                }
 
+                if (row.additionalDescBulletInfo) {
+
+                    let text = '';
+                    row.additionalDescBulletInfo.forEach(item => {
+                        text += `${item.text} || `;
+                    });
+                    row.additionalDescBulletInfo = [
+                        {
+                            text: text.slice(0, -3).trim(),
+                        },
+                    ];
+
+                    /* let text = '';
+                    text = row.additionalDescBulletInfo[0].text.replace(/\n/g, ' || ');;
+                    row.additionalDescBulletInfo = [{ text }]; */
+                }
+                if (row.name && row.brandText) {
+                    let text = '';
+                    let brandName = row.brandText[0].text.replace(' / ', ' ').replace(/\s\s+/g, ' ').trim();
+                    text = row.name[0].text.replace(brandName, '').trim();
+                    row.name = [{ text }];
+                }
+                if (row.promotion) {
+                    let text = '';
+                    text = row.promotion[0].text.replace(/\n/g, ' ');;
+                    row.promotion = [{ text }];
+                }
 
             } catch (exception) { console.log('Error in transform', exception); }
         }
