7573e535f7d47cf72ad1954300b8d678ce2749af
Satyam <satyam.rathod@synerzip.com>
2020-09-21 11:09:27 +0530

    updated ES, FR and IT to only fetch result specific to countries

--- src/library/product/search/stores/e/ebay/FR/execute.js
+++ src/library/product/search/stores/e/ebay/FR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FR',
     store: 'ebay',
     domain: 'ebay.fr',
-    url: 'https://www.ebay.fr/sch/i.html?_nkw={searchTerms}&_sacat=0&_ipg=200',
+    url: 'https://www.ebay.fr/sch/i.html?_nkw={searchTerms}&_sacat=0&_ipg=200&LH_PrefLoc=1',
     loadedSelector: 'ul.srp-results > li[data-view]',
     noResultsXPath: '//h3[contains(@class,"srp-save-null-search__heading")]',
     zipcode: '',
