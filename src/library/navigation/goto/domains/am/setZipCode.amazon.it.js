c15e465b36f30f3eb8029517e083c5b30f0db71c
nehashende <neha.shende@synerzip.com>
2020-09-21 16:05:49 +0530

    #628. Added extractor logic for amazon IT core.

--- src/library/navigation/goto/domains/am/setZipCode.amazon.it.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.it.js
@@ -1,3 +1,29 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { zipcode } = inputs;
+  await context.waitForSelector('#nav-packard-glow-loc-icon');
+  await context.click('#nav-packard-glow-loc-icon');
+  await context.waitForSelector('input#GLUXZipUpdateInput');
+  try {
+    await context.click('a#GLUXChangePostalCodeLink');
+  } catch (error) {
+    console.log('Element not visible');
+  }
+  await context.setInputValue('input#GLUXZipUpdateInput', zipcode);
+  await context.waitForSelector('#GLUXZipUpdate input');
+  await context.click('#GLUXZipUpdate input');
+  try {
+    await context.waitForSelector('button[name="glowDoneButton"]');
+    await context.click('button[name="glowDoneButton"]');
+  } catch (error) {
+    console.log('Done button not found');
+  }
+  await context.waitForNavigation();
+}
 
 module.exports = {
   implements: 'navigation/goto/setZipCode',
@@ -7,4 +33,5 @@ module.exports = {
     store: 'amazon',
     zipcode: '20019',
   },
+  implementation,
 };
