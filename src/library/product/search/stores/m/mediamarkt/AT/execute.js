51efe1f59f39fee266dc04490784017d4d241300
sushma-mangalapally <sushma.mangalapally@import.io>
2020-09-21 09:49:04 -0700

    Add more xpaths for no results

--- src/library/product/search/stores/m/mediamarkt/AT/execute.js
+++ src/library/product/search/stores/m/mediamarkt/AT/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'mediamarkt.at',
     url: 'https://www.mediamarkt.at/de/search.html?query={searchTerms}&searchProfile=onlineshop&channel=mmatde',
     loadedSelector: 'div.product-wrapper',
-    noResultsXPath: '//div[contains(@id,"search_no_result")]|//div[@id="product-wrapper"]',
+    noResultsXPath: '//div[contains(@id,"search_no_result")]|//div[@id="product-wrapper"]|//ul[contains(@class,"dysonnavi")]',
     zipcode: '',
   },
 };
