d2c16a976b2b16eb0c1919194b4a80d4ee03acf7
judhisahoo2009 <judhisahoo@hotmail.com>
2020-09-22 13:31:17 +0530

    updating

--- src/library/product/details/stores/t/tmall/format.js
+++ src/library/product/details/stores/t/tmall/format.js
@@ -4,27 +4,8 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-  const cleanUp = (data, context) => {
-    const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      el.text = clean(el.text);
-    }))));
-    return data;
-  };
   for (const { group } of data) {
     for (const row of group) {
-      
       if(row.alternateImages){
         row.alternateImages.forEach(item => {
           item.text=item.text.replace("_50x50.jpg", "");
@@ -42,6 +23,9 @@ const transform = (data) => {
           var ratingCountArr=item.text.split(" ");
           item.text=ratingCountArr[0];
         });
+      }
+      if(row.nameExtended){
+        
       }
     }
   }
