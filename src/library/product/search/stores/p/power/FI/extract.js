bb3f83d046404f7dc08b8c7d01f602ed72285f6f
Karolina <karolina.urbanska@tdcm.io>
2020-09-22 09:32:06 +0200

    Improved selectors after qa1

--- src/library/product/search/stores/p/power/FI/extract.js
+++ src/library/product/search/stores/p/power/FI/extract.js
@@ -50,16 +50,13 @@ module.exports = {
         const item = document.querySelectorAll('div.product-container')
           ? document.querySelectorAll('div.product-container')[i] : [];
         // @ts-ignore
-        console.log(item);
         const starsWidth = item && item.querySelector('div.stars-on[dataaverage]')
           // @ts-ignore
           ? item.querySelector('div.stars-on[dataaverage]') : '';
-        console.log(starsWidth);
         const stars = starsWidth ? starsWidth.getAttribute('style') : '';
-        console.log(stars);
         if (stars) {
           const aggRating = parseFloat(stars.replace(/width:\s([\d.]+)%;/g, '$1')) / 20;
-          addElementToDocument(item, 'aggRating', aggRating.toFixed(1).replace(',', '.'));
+          addElementToDocument(item, 'aggRating', aggRating.toFixed(2).replace('.', ','));
         }
         addElementToDocument(item, 'pd_rank', lastProductPosition + i);
         const integerPrice = item && item.querySelector('pwr-price[type="integer"]')
