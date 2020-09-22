640ee65f27ab0ef827a4a10c3059c1f5214eca30
unknown <ganesh.working@gmail.com>
2020-09-22 13:07:06 +0530

    insert keys

--- src/library/product/details/stores/m/mumzworld/format.js
+++ src/library/product/details/stores/m/mumzworld/format.js
@@ -1,3 +1,4 @@
+
 /**
  *
  * @param {ImportIO.Group[]} data
@@ -54,9 +55,11 @@ const transform = (data) => {
         row.description = [{'text':info.join(' || '),'xpath':row.description[0].xpath}];          
       }
       if (row.shippingInfo) {
+        let info = [];          
         row.shippingInfo.forEach(item => {
-          item.text = item.text.replace(/(\s*\n\s*)+/g, ' ').trim();
+          info.push(item.text.replace(/(\s*\n\s*)+/g, ' | ').trim());            
         });
+        row.shippingInfo = [{'text':info.join(' | '),'xpath':row.shippingInfo[0].xpath}];
       }      
       if (row.featureBullets) {
         row.featureBullets.forEach(item => {
