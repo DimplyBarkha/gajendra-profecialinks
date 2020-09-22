0c8180471c24ee6b887cd8ec20ce7a8b9029705b
rudra.bhatt <rudra.bhatt@kcsitglobal.com>
2020-09-22 10:38:49 +0530

    create extractor for variant

---
+++ src/library/product/details/stores/o/ottoversand/AT/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'AT',
+    store: 'ottoversand',
+    domain: 'ottoversand.at',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/o/ottoversand/AT/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'AT',
+    store: 'ottoversand',
+    transform: null,
+    domain: 'ottoversand.at',
+    zipcode: '',
+  },
+};
