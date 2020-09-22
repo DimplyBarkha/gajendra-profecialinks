5bdc5b582147786b44e7fb724089c100927487ef
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-09-22 13:47:04 +0530

    #589. variantId minor fix.

--- src/library/product/details/stores/m/monclick/IT/extract.js
+++ src/library/product/details/stores/m/monclick/IT/extract.js
@@ -10,13 +10,24 @@ module.exports = {
   },
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     try {
-      await new Promise(resolve => setTimeout(resolve, 5000))
-      await context.click('div[class*="image-content-gallery"] li[class*="gallery-video"]')
-      await context.waitForSelector('div[class*=product-gallery-overlay]')
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      await context.click('div[class*="image-content-gallery"] li[class*="gallery-video"]');
+      await context.waitForSelector('div[class*=product-gallery-overlay]');
+      await context.evaluate(async () => {
+        const element = document.querySelector('footer#footer');
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+          console.log('*** Scrolling it ****');
+          await new Promise((resolve) => setTimeout(resolve, 10000));
+        }
+      });
+      // await context.scrollIntoView();
+      // var elmnt = document.getElementById('techspecs');
+      // elmnt.scrollIntoView();
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-    await new Promise(resolve => setTimeout(resolve, 10000))
+    await new Promise(resolve => setTimeout(resolve, 10000));
     return await context.extract(productDetails, { transform });
   },
 };
