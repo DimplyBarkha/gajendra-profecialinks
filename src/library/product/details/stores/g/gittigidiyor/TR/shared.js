d408e6f14c11115be2532581dead7c4a04d851fb
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-22 18:53:52 +0530

    Fixed issues with description

--- src/library/product/details/stores/g/gittigidiyor/TR/shared.js
+++ src/library/product/details/stores/g/gittigidiyor/TR/shared.js
@@ -24,14 +24,34 @@ const transform = (data) => {
 				let specTitles = row.specTitles;
 				let specStr = '';
 				for (let i = 0; i < specTitles.length; i++) {
-					specStr += `${specTitles[i].text} : ${specValues[i].text} | `
+					specStr += `${specTitles[i].text} : ${specValues[i].text} || `
 				}
-				specStr = specStr.substring(0, specStr.lastIndexOf(' |'));
+				specStr = specStr.substring(0, specStr.lastIndexOf(' ||'));
 				row.specifications = [
 					{
 						text: specStr
 					}
 				]
+			}
+
+			if (row.description && row.featureHeading) {
+				let finalDescription = '';
+				finalDescription = row.descriptionTitle ? row.descriptionTitle[0].text : '';
+				finalDescription += row.descriptionIconInfo ? ` ${row.descriptionIconInfo[0].text}` : '';
+				let featureStr = '';
+				if (row.featureTitles) {
+					let featureTitles = row.featureTitles;
+					let featureValues = row.featureValues;
+					for (let i = 0; i < featureTitles.length; i++) {
+						featureStr += `${featureTitles[i].text} : ${featureValues[i].text} || `
+					}
+					featureStr = featureStr.substring(0, featureStr.lastIndexOf(' ||'));
+				}
+
+				finalDescription += featureStr.length > 0 ? ` || ${featureStr}` : '';
+				finalDescription += row.descriptionContent ? `${row.descriptionContent[0].text}` : '';
+				row.description = [{ text: finalDescription }]
+				// row.description = [{ text: `${row.description[0].text} ${row.featureHeading[0].text} || ${featureStr}` }];
 			}
 		}
 	}
