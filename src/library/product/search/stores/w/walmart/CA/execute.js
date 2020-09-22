534f8837dee867ad4cb232f21610f7075b42db00
nikita.kumari <nikita.kumari@synerzip.com>
2020-09-22 15:12:33 +0530

    Loading handling in pagination

--- src/library/product/search/stores/w/walmart/CA/execute.js
+++ src/library/product/search/stores/w/walmart/CA/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'walmart',
     domain: 'walmart.ca',
     url: 'https://www.walmart.ca/search?q={searchTerms}',
-    loadedSelector: "[data-automation*='product-results'] [data-automation*='product'] img",
+    loadedSelector: "[data-automation*='product-results'] [data-automation*='product']",
     noResultsXPath: "//div[contains(@data-automation,'search-result-header') and contains(.,'We couldn')] | //h1[contains(@data-automation,'null-results-message')]",
     zipcode: '',
   },
--- src/library/product/search/stores/w/walmart/CA/paginate.js
+++ src/library/product/search/stores/w/walmart/CA/paginate.js
@@ -1,3 +1,64 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { keywords, page, offset } = inputs;
+  const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
+
+  if (nextLinkSelector) {
+    const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector), nextLinkSelector);
+    if (!hasNextLink) {
+      return false;
+    }
+  }
+
+  const { pager } = dependencies;
+  const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
+  if (success) {
+    return true;
+  }
+
+  let url = await context.evaluate(function () {
+    /** @type { HTMLLinkElement } */
+    const next = document.querySelector('head link[rel="next"]');
+    if (!next) {
+      return false;
+    }
+    return next.href;
+  });
+
+  if (!url && openSearchDefinition) {
+    url = openSearchDefinition.template
+      .replace('{searchTerms}', encodeURIComponent(keywords))
+      .replace('{page}', (page + (openSearchDefinition.pageOffset || 0)).toString())
+      .replace('{offset}', (offset + (openSearchDefinition.indexOffset || 0)).toString());
+  }
+
+  if (!url) {
+    return false;
+  }
+
+  console.log('Going to url', url);
+  await context.setLoadAllResources(true);
+  await context.setAntiFingerprint(false);
+  await context.setBlockAds(false);
+  await dependencies.goto({ url }, { timeout: 30000, waitUntil: 'load', checkBlocked: true, first_request_timeout: 60, load_timeout: 60 });
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
@@ -6,7 +67,7 @@ module.exports = {
     // nextLinkSelector: "a[data-automation*='pagination-next-button']",
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: "[data-automation*='product-results'] [data-automation*='product'] img",
+    loadedSelector: "[data-automation*='product-results'] [data-automation*='product']",
     noResultsXPath: "//div[contains(@data-automation,'search-result-header') and contains(.,'We couldn')] | //h1[contains(@data-automation,'null-results-message')]",
     openSearchDefinition: {
       page: 0,
@@ -15,4 +76,5 @@ module.exports = {
     domain: 'walmart.ca',
     zipcode: '',
   },
+  implementation,
 };
