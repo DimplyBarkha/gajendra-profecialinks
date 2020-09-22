b200704fcffebe2a562c4601108045186aab1276
Ravi Vachhani <ravi.vachhani@kcsitglobal.com>
2020-09-22 17:01:12 +0530

    Resolve client feedback excluding video change

--- src/library/product/details/stores/e/elgiganten/shared.js
+++ src/library/product/details/stores/e/elgiganten/shared.js
@@ -6,12 +6,44 @@
 const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
+      if (row.image) {
+        let text = "";
+        row.image.forEach((item, index) => {
+          text = item.text.replace("?$prod_all4one$", "");
+          row.image[index].text = text;
+        });
+      }
+      if (row.alternateImages) {
+        let text = "";
+        row.alternateImages.forEach((item, index) => {
+          text = item.text.replace("?$prod_all4one$", "");
+          row.alternateImages[index].text = text;
+        });
+      }
+      if (row.price) {
+        let text = "";
+        row.price.forEach(item => {
+          text = item.text.split(":");
+        });
+        row.price = [{ text: text[0] }, { text: text[1] }];
+      }
       if (row.specifications) {
         let text = '';
         row.specifications.forEach(item => {
           text += `${item.text.replace(/\n \n/g, ':')} || `;
         });
         row.specifications = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+      }
+      if (row.productOtherInformation) {
+        let text = '';
+        row.productOtherInformation.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        });
+        row.productOtherInformation = [
           {
             text: text.slice(0, -4),
           },
@@ -38,14 +70,25 @@ const transform = (data) => {
         });
         row.videos = [video];
       }
-      if (row.manufacturerDescription) {
+      if (row.manufacturerImages) {
         let text = '';
-        row.manufacturerDescription.forEach(item => {
+        row.manufacturerImages.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} | `;
+        });
+        row.manufacturerImages = [
+          {
+            text: text.slice(0, -3),
+          },
+        ];
+      }
+      if (row.additionalDescBulletInfo) {
+        let text = '';
+        row.additionalDescBulletInfo.forEach(item => {
           text += `${item.text.replace(/\n \n/g, ':')} || `;
         });
-        row.manufacturerDescription = [
+        row.additionalDescBulletInfo = [
           {
-            text: text.slice(0, -4),
+            text: text.slice(0, -3),
           },
         ];
       }
