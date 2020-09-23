38e91d105eae748bb66c4eb72e333d94fda59c98
NGenji <ngenji@wsgc.com>
2020-09-23 04:03:14 -0700

    QA1 fixes

--- src/library/product/search/stores/a/andorrafreemarket/format.js
+++ src/library/product/search/stores/a/andorrafreemarket/format.js
@@ -1,37 +1,56 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const clean = (text) =>
+    text
+      .toString()
+      .replace(/\r\n|\r|\n/g, " ")
+      .replace(/&amp;nbsp;/g, " ")
+      .replace(/&amp;#160/g, " ")
+      .replace(/\u00A0/g, " ")
+      .replace(/\s{2,}/g, " ")
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, " ")
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, "")
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, " ");
+
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
 
   for (const { group } of data) {
     for (const row of group) {
       if (row.name) {
-        row.rank = row.name.map((item, index) => { return { text: index }; });
+        row.rank = row.name.map((item, index) => {
+          return { text: index };
+        });
       }
       if (row.price && row.price[0]) {
-        row.price[0].text = row.price[0].text.replace(/\./g, ',');
+        row.price[0].text = row.price[0].text.replace(/\./g, ",");
       }
       if (row.thumbnail && row.thumbnail[0]) {
-        row.thumbnail[0].text = row.thumbnail[0].text.replace('\\', '');
+        row.thumbnail[0].text = row.thumbnail[0].text.replace("\\", "");
       }
-      Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-      }));
+
+      if (!row.sponsored) {
+        orgRankCounter = orgRankCounter + 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      rankCounter = rankCounter + 1;
+      row.rank = [{ text: rankCounter }];
+      context.setState({ orgRankCounter });
+      context.setState({ rankCounter });
+
+      Object.keys(row).forEach((header) =>
+        row[header].forEach((el) => {
+          el.text = clean(el.text);
+        })
+      );
     }
   }
   return data;
