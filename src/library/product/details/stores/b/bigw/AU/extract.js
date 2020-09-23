562edb7ef3b7d349e787ef9168cc6579aee6d268
Jatin Dhori - KCS <34161864+kcs-jatindhori@users.noreply.github.com>
2020-09-23 20:18:06 +0530

    add changes for fields as per QA sheet

--- src/library/product/details/stores/b/bigw/AU/extract.js
+++ src/library/product/details/stores/b/bigw/AU/extract.js
@@ -1,9 +1,11 @@
+const { transform } = require('./shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AU',
     store: 'bigw',
+    transform,
     domain: 'bigw.com.au',
     zipcode: '',
   },
