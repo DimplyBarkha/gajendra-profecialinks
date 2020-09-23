68bc7162f1d061944f989f94c11cc3d1413d5ac3
Manoj Dey <manoj.d@OSMLIN16>
2020-09-23 22:52:01 +0530

    updated code

--- src/library/product/search/stores/t/trendyol/TR/extract.js
+++ src/library/product/search/stores/t/trendyol/TR/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require('./transform')
+const { transform } = require('./transform');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -13,37 +13,37 @@ module.exports = {
     parameters,
     context,
     dependencies,
-    ) => {
-      const checkExistance = async (selector) => {
-        return await context.evaluate(async (currentSelector) => {
-          return await Boolean(document.querySelector(currentSelector));
-        }, selector);
-      };
-      const { transform } = parameters;
-      const { productDetails } = dependencies;
-      const delay = t => new Promise(resolve => setTimeout(resolve, t));
+  ) => {
+    const checkExistance = async (selector) => {
+      return await context.evaluate(async (currentSelector) => {
+        return await Boolean(document.querySelector(currentSelector));
+      }, selector);
+    };
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    const delay = t => new Promise(resolve => setTimeout(resolve, t));
     await context.evaluate(async function () {
       // @ts-ignore
       const getRating = [...document.querySelectorAll('div.ratings')];
       getRating.forEach(ele => {
-      var stars = [...ele.querySelectorAll('div.full')];
-      var sum = 0;
-      stars.forEach(element => {
-        var addition = element.getAttribute("style").match(/(\d+)/)[1]
-        sum = sum+Number(addition);
-    })
-    var convertedfull = (((sum/500)*100)/20).toFixed(1).replace(/(\d+)(\.)(\d+)/,'$1,$3');
-    ele.setAttribute('sum',convertedfull);
-      })
+        var stars = [...ele.querySelectorAll('div.full')];
+        var sum = 0;
+        stars.forEach(element => {
+          var addition = element.getAttribute('style').match(/(\d+)/)[1];
+          sum = sum + Number(addition);
+        });
+        var convertedfull = (((sum / 500) * 100) / 20).toFixed(1).replace(/(\d+)(\.)(\d+)/, '$1,$3');
+        ele.setAttribute('sum', convertedfull);
+      });
     });
-    if(await checkExistance('.prdct-cntnr-wrppr > div:nth-child(13)')){
-    await context.evaluate(()=>{
-      document.querySelector('.prdct-cntnr-wrppr > div:nth-child(13)').scrollIntoView({behavior: 'smooth'});
-    })
-  }
+    if (await checkExistance('.prdct-cntnr-wrppr > div:nth-child(13)')) {
+      await context.evaluate(() => {
+        document.querySelector('.prdct-cntnr-wrppr > div:nth-child(13)').scrollIntoView({ behavior: 'smooth' });
+      });
+    }
     await delay(5000);
     await context.waitForSelector('div.srch-prdcts-cntnr img');
     await delay(5000);
     return await context.extract(productDetails, { transform });
-    },
+  },
 };
--- src/library/product/search/stores/t/trendyol/TR/paginate.js
+++ src/library/product/search/stores/t/trendyol/TR/paginate.js
@@ -10,8 +10,8 @@ module.exports = {
     loadedSelector: 'div.srch-prdcts-cntnr img',
     noResultsXPath: '//*[@class="srch-no-rslt"]',
     openSearchDefinition: {
-      template:'https://www.trendyol.com/tum--urunler?q={searchTerms}&pi={page}',
-      offset:'24',
+      template: 'https://www.trendyol.com/tum--urunler?q={searchTerms}&pi={page}',
+      offset: '24',
     },
     zipcode: '',
     domain: 'trendyol.com',
