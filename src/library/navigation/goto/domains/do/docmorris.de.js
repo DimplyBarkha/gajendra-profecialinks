a2e929a6cff943a88cf9f540a43d35d01404e661
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-23 14:22:30 +0530

    Created extractor for docmorris DE

---
+++ src/library/navigation/goto/domains/do/docmorris.de.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'docmorris.de',
+    timeout: null,
+    country: 'DE',
+    store: 'docmorris',
+    zipcode: '',
+  },
+};
