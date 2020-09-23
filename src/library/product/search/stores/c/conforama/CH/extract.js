c7e053484daeddcea92f21836303452359c6bc59
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-23 21:37:15 +0530

    Error rate issue fixed

--- src/library/product/search/stores/c/conforama/CH/extract.js
+++ src/library/product/search/stores/c/conforama/CH/extract.js
@@ -58,7 +58,7 @@ async function implementation (
       newDiv.textContent = content;
       newDiv.style.display = 'none';
       const originalDiv = document.querySelectorAll('section#contentSegment article form#addToCartForm')[index];
-      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      originalDiv ? originalDiv.parentNode.insertBefore(newDiv, originalDiv) : '';
     }
     let ratingCountSelector = document.querySelector('span[class="avisBaazar"]');
     // @ts-ignore
@@ -68,45 +68,47 @@ async function implementation (
     // @ts-ignore
     let tc_vars = window.tc_vars;
     console.log('tc_vars: ', tc_vars);
-    let listProduct = tc_vars ? tc_vars.list_products : '';
-    console.log('listProduct: ', listProduct);
-    var objs = listProduct.map(function (x) {
-      return {
-        itemId: x['product_sku'],
-        src: x['product_url_picture']
-      };
-    });
-
-    let images = document.querySelectorAll('div[class="image-product"] a');
-    let itemArr = [];
-    for (let index = 0; index < images.length; index++) {
-      let itemObj = new Object();
-      let itemId = images[index].getAttribute('tcproductclick');
-      // @ts-ignore
-      itemObj.itemId = itemId;
-      // @ts-ignore
-      const element = images[index].querySelector('img');
-      let src = element ? element.getAttribute('src') : '';
-      // @ts-ignore
-      itemObj.src = src;
-      // @ts-ignore
-      // itemArr.push(itemObj);
-      objs.find(param => {
-
-        if (param) {
-          // @ts-ignore
-          if (param.itemId === itemId) {
-            console.log("Matched", param.itemId)
-            if(src.includes('https')){
-             src = src;
-            }else{
-             src = 'https:'+param.src;
-            }
-            addHiddenDiv('pd_productImg', src , index);
-          }
-        }
-
+    let listProduct = tc_vars ? tc_vars.list_products : [];
+    if(listProduct.length > 0){
+      console.log('listProduct: ', listProduct);
+      var objs = listProduct.map(function (x) {
+        return {
+          itemId: x['product_sku'],
+          src: x['product_url_picture']
+        };
       });
+  
+      let images = document.querySelectorAll('div[class="image-product"] a');
+      let itemArr = [];
+      for (let index = 0; index < images.length; index++) {
+        let itemObj = new Object();
+        let itemId = images[index].getAttribute('tcproductclick');
+        // @ts-ignore
+        itemObj.itemId = itemId;
+        // @ts-ignore
+        const element = images[index].querySelector('img');
+        let src = element ? element.getAttribute('src') : '';
+        // @ts-ignore
+        itemObj.src = src;
+        // @ts-ignore
+        // itemArr.push(itemObj);
+        objs.find(param => {
+  
+          if (param) {
+            // @ts-ignore
+            if (param.itemId === itemId) {
+              console.log("Matched", param.itemId)
+              if(src.includes('https')){
+               src = src;
+              }else{
+               src = 'https:'+param.src;
+              }
+              addHiddenDiv('pd_productImg', src , index);
+            }
+          }
+  
+        });
+      }
     }
     //-----------------------------------------------------------
   });
