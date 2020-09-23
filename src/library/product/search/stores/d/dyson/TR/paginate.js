69b33a87e146254e112dd3fd92f3b3fbc6b88dd6
Jjon Campbell <jon.campbell@BOTNUSJONATHAN.local>
2020-09-23 16:31:09 -0400

    Search/Core Updates

--- src/library/product/search/stores/d/dyson/TR/paginate.js
+++ src/library/product/search/stores/d/dyson/TR/paginate.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'dyson',
     nextLinkSelector: 'a.search-pagination__text.js-search-pagination-link',
     spinnerSelector: '.active .js-search-loader',
-    noResultsXPath: '//h2[contains(.,"returned no  results")]',
+    noResultsXPath: '//h2[contains(.,"aramanız hiçbir sonuç vermedi")]',
     domain: 'dyson.com.tr',
     zipcode: '',
   },
