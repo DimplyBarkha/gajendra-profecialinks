5b7117092901d1a3cfa088796e6e812c5054800e
nilofarsh <nilofar.shaikh@synerzip.com>
2020-09-23 18:04:32 +0530

    Added xpaths for core & Search & deployed core

---
+++ src/library/product/search/stores/d/dyson/IN/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'IN',
+    store: 'dyson',
+    transform: null,
+    domain: 'dyson.in',
+    zipcode: '',
+  },
+};
