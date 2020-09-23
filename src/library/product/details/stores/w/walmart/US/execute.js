b40f3cc6424e01add892b9aecb1d6c85d6a692fd
Guillaume Vial <guillaumevial@Guiguis-mac.local>
2020-09-23 16:28:55 +0100

    Changed execute function

--- src/library/product/details/stores/w/walmart/US/execute.js
+++ src/library/product/details/stores/w/walmart/US/execute.js
@@ -1,33 +1,3 @@
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  let { url, id, zipcode, storeId } = inputs;
-  if (!url) {
-    if (!id) {
-      throw new Error('no id provided');
-    }
-    url = await dependencies.createUrl({ id });
-  }
-  await dependencies.goto({ url, zipcode, storeId });
-
-  if (parameters.loadedSelector) {
-    await context.waitForFunction(function (sel, xp) {
-      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
-  }
-  console.log('Checking no results', parameters.noResultsXPath);
-  return await context.evaluate(function (xp) {
-    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-    console.log(xp, r);
-    const e = r.iterateNext();
-    console.log(e);
-    return !e;
-  }, parameters.noResultsXPath);
-}
-
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -37,5 +7,4 @@ module.exports = {
     loadedSelector: '#hf-home-link',
     noResultsXPath: '//div[@class="error-page__content"] | //div[@class="error-page-content"]',
   },
-  implementation,
 };
