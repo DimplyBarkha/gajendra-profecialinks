4070daedc2da4f49f87f87a1eeb5a2a9205b3a9d
KCSPL\jatin <jatin@kcspl.co.in>
2020-09-23 13:35:31 +0530

    Getting more videos changes

--- src/library/product/details/stores/e/elgiganten/DK/extract.js
+++ src/library/product/details/stores/e/elgiganten/DK/extract.js
@@ -15,14 +15,34 @@ module.exports = {
     dependencies,
   ) => {
     await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+
+    //let btn = await context.evaluate(() => { return Boolean(!!document.querySelector('button.coi-banner__accept')) });
+    /* if (btn) {
+      try {
+        await context.click('button.coi-banner__accept');
+      }
+      catch (error) { }
+    } */
+
     await context.evaluate(async function () {
-      const overlay = document.getElementById('tab-specs-trigger');
+
+/*       const overlay = document.getElementById('tab-specs-trigger');
       if (overlay !== undefined) {
         overlay.click();
       }
       const overlay1 = document.getElementById('tab-more-info-trigger');
       if (overlay1 !== undefined) {
         overlay1.click();
+      } */
+
+      let btn = Boolean(!!document.querySelector('button.coi-banner__accept'))
+      console.log('BBBBBBB',btn);
+      if (btn) {
+        try {
+          await context.click('button.coi-banner__accept');
+        }
+        catch (error) { }
       }
 
       const videoData = document.querySelectorAll('iframe.videoly-box')[0].contentWindow.document.getElementsByTagName('ul')[0]
