874d164fca621f46ad58f28349ab95872d635881
Tomek887 <70725911+Tomek887@users.noreply.github.com>
2020-09-23 15:37:53 +0200

    QA1 fix

--- src/library/product/details/stores/e/elkjop/FI/extract.js
+++ src/library/product/details/stores/e/elkjop/FI/extract.js
@@ -28,16 +28,31 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
+      const nodeListH = document.querySelectorAll('.article-text.article-free-html h2');
+      const nodeListP = document.querySelectorAll('.article-text.article-free-html p');
+      var allEnhancedContent = '';
+      if (nodeListH && nodeListP && nodeListH.length > 0 && nodeListP.length > 0) {
+        for (var k = 0; k < nodeListH.length; k++) {
+          // @ts-ignore
+          allEnhancedContent = allEnhancedContent + ' ' + nodeListH[k].innerText;
+          // @ts-ignore
+          allEnhancedContent = allEnhancedContent + ' ' + nodeListP[k].innerText;
+        }
+        addElementToDocument('enhCont', allEnhancedContent);
+      }
       const rating = document.evaluate("//div[@itemprop='aggregateRating']/meta[@itemprop='ratingValue']/@content", document, null, XPathResult.STRING_TYPE, null);
       if (rating && rating.stringValue) {
-        const formattedRating = rating.stringValue.replace(',', '.');
-        addElementToDocument('rating', formattedRating);
+        // const formattedRating = rating.stringValue.replace(',', '.');
+        addElementToDocument('rating', rating.stringValue);
       }
-      const warranty = document.evaluate("//p[text()[contains(.,'takuu')]]", document, null, XPathResult.STRING_TYPE, null);
-      if (warranty && warranty.stringValue) {
-        var rEE = /(.*)(\.)(.*)takuu(.*)/;
-        var cleanWarranty = warranty.stringValue.replace(rEE, '$3taaku.');
-        addElementToDocument('warranty', cleanWarranty);
+      const warranty = document.evaluate("//*[contains(text(), 'takuu')]", document, null, XPathResult.STRING_TYPE, null);
+      if (warranty) {
+        if (warranty.stringValue.length === 0) {
+          var cleanWarranty = document.evaluate("//div[@class='tab-slot']/p/text()[position() = last()]", document, null, XPathResult.STRING_TYPE, null).stringValue;
+          addElementToDocument('warranty', cleanWarranty);
+        } else {
+          addElementToDocument('warranty', warranty.stringValue);
+        }
       }
       const weight = document.evaluate("//td[contains(text(), 'Paino')]", document, null, XPathResult.STRING_TYPE, null);
       if (weight && weight.stringValue) {
