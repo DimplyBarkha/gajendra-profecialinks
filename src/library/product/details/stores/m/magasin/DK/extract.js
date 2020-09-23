e502f61875d08599d0c06256d0a55a1e74cd1012
abhay.patil <abhay.patil@kcsitglobal.com>
2020-09-23 11:43:09 +0530

    resolve sku issues from qa2

--- src/library/product/details/stores/m/magasin/DK/extract.js
+++ src/library/product/details/stores/m/magasin/DK/extract.js
@@ -8,4 +8,37 @@ module.exports = {
     domain: 'magasin.dk',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain ,transform: transformParam}, context, { productDetails }) => {        
+
+    await context.waitForSelector('product-details', { timeout: 5000 });
+
+    await context.evaluate(async function () {       
+     const productInfo = preFetchProductDetails();     
+    addEleToDoc('tempSkuId', productInfo['sku']);     
+     
+      function preFetchProductDetails () {        
+        let productInfo = findProductDetails('//script[@type="application/ld+json" and contains(text(),"sku")]');                
+        productInfo = JSON.parse(productInfo.textContent);
+        return productInfo;
+      }      
+
+      function findProductDetails (xpath) {        
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;                
+        const productDetails = element;
+        return productDetails;
+      }
+
+      function addEleToDoc (key, value) {
+        const prodEle = document.createElement('div');
+        prodEle.id = key;
+        prodEle.textContent = value;
+        prodEle.style.display = 'none';
+        document.body.appendChild(prodEle);
+      }    
+    });      
+
+    
+    
+    return await context.extract(productDetails, { transform: transformParam });
+  },
 };
