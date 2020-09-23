4f274ea6c2f4346531b0dd3e1b2cb8c5d16b7828
Sindhusha balla <sindhusha.b@osmosys.asia>
2020-09-23 18:16:37 +0530

    Reverted lint changes in action.js

--- src/library/product/search/paginate/action.js
+++ src/library/product/search/paginate/action.js
@@ -1,21 +1,21 @@
 /**
  *
  * @param {{
-  *  keywords: string,
-  *  page: number,
-  *  offset: number,
-  * }} inputs
-  * @param {{
-  *  nextLinkSelector: string,
-  *  mutationSelector: string,
-  *  loadedSelector: string,
-  *  noResultsXPath: string,
-  *  spinnerSelector: string,
-  *  openSearchDefinition: { template: string, indexOffset?: number, pageOffset?: number }
-  * }} parameters
-  * @param { ImportIO.IContext } context
-  * @param { Record<string, any> } dependencies
-  */
+ *  keywords: string,
+ *  page: number,
+ *  offset: number,
+ * }} inputs
+ * @param {{
+ *  nextLinkSelector: string,
+ *  mutationSelector: string,
+ *  loadedSelector: string,
+ *  noResultsXPath: string,
+ *  spinnerSelector: string,
+ *  openSearchDefinition: { template: string, indexOffset?: number, pageOffset?: number }
+ * }} parameters
+ * @param { ImportIO.IContext } context
+ * @param { Record<string, any> } dependencies
+ */
 async function implementation (
   inputs,
   parameters,
