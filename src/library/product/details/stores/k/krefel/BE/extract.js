5c1bdfce9219815875ca5570aabc0940c47617ff
Karolina <karolina.urbanska@tdcm.io>
2020-09-24 16:25:37 +0200

    Fixes after qa

--- src/library/product/details/stores/k/krefel/BE/extract.js
+++ src/library/product/details/stores/k/krefel/BE/extract.js
@@ -11,18 +11,51 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const accCookie = document.querySelector('section.cookie-alert div.button-section a');
+      if (accCookie) accCookie.click();
+      await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+
       const scriptWithInfo = document.evaluate("//script[contains(text(), 'availability')]", document, null, XPathResult.STRING_TYPE, null);
       const regexAvailability = /"availability":\s"([A-z]+)"/g;
       const regexRating = /"ratingValue":\s"([\d,]+)"/g;
       const foundAvailability = regexAvailability.exec(scriptWithInfo.stringValue);
       const foundRatingValue = regexRating.exec(scriptWithInfo.stringValue);
-      if (foundAvailability) {
-        document.body.setAttribute('availability', foundAvailability[1]);
-      }
+      if (foundAvailability && foundAvailability[1] === 'InStock') {
+        addElementToDocument('availability', 'In Stock');
+      } else addElementToDocument('availability', 'Out of Stock');
       if (foundRatingValue) {
-        document.body.setAttribute('ratingValue', foundRatingValue[1].replace(',', '.'));
+        addElementToDocument('ratingValue', foundRatingValue[1]);
       }
+      const pdfExists = document.evaluate('//div[@class="description-wrap"]//a[contains(text(), "PDF")]/@href', document, null, XPathResult.STRING_TYPE, null);
+      if (pdfExists && pdfExists.stringValue) addElementToDocument('pdfExists', 'Yes');
+      const specifications = document.querySelectorAll('div.block-header, div.specs-overview li');
+      const specArr = [];
+      if (specifications.length) {
+        specifications.forEach(e => {
+          specArr.push(e.innerText.replace(/\n/g, ' '));
+        });
+      }
+      addElementToDocument('specifications', specArr.join(' || '));
+      const description = document.querySelectorAll('section.description div p');
+      const descArr = [];
+      if (description.length) {
+        description.forEach(e => {
+          descArr.push(e.innerText);
+        });
+      }
+      addElementToDocument('description', descArr.join(' || '));
+      const warrantyXpath = document.evaluate('//div[@class="description-wrap"]//*[contains(text(),"garantie")]', document, null, XPathResult.STRING_TYPE, null);
+      const warranty = warrantyXpath ? warrantyXpath.stringValue : '';
+      if (warranty) addElementToDocument('warranty', warranty);
     });
-    await context.extract(productDetails);
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+    return await context.extract(productDetails);
   },
 };
