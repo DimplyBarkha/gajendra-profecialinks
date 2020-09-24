03a91b17f3f67c7cd5f3214bc803f7cb19a4cb3c
Karolina <karolina.urbanska@tdcm.io>
2020-09-24 16:29:50 +0200

    Inserted shared functions

--- src/library/product/search/stores/k/krefel/BE/extract.js
+++ src/library/product/search/stores/k/krefel/BE/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'BE',
     store: 'krefel',
-    transform: null,
+    transform: transform,
     domain: 'krefel.be',
     zipcode: '',
   },
