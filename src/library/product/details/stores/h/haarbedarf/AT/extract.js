7eb953a0be418121f1cc0ad19d199ec973dd69fe
Rafał Perkowski <32035394+RafalPer@users.noreply.github.com>
2020-09-24 20:15:06 +0200

    Tdcm haarbedarf at core rp fix (#652)
    * Fixes
    
    * more fixes
    
    * Comments

--- src/library/product/details/stores/h/haarbedarf/AT/extract.js
+++ src/library/product/details/stores/h/haarbedarf/AT/extract.js
@@ -12,6 +12,30 @@ module.exports = {
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     // await new Promise((resolve, reject) => setTimeout(resolve, 10000));
     await context.evaluate(async function () {
+      // Removing spaces between colors e.g. 'schwarz / nickel' to 'schwarz/nickel'
+      const colorInH1 = document.querySelector('h1.product_title.entry-title');
+      const newColorSlash = colorInH1.textContent.replace(/\s\/\s/, '/');
+      colorInH1.innerHTML = newColorSlash;
+      // Creating div with enchancedDescription with || separator
+      const enchancedDescription = document.querySelectorAll('div#tab-description h3, div#tab-description p');
+      const enchancedList = [];
+      const enchancedDiv = document.createElement('div');
+      enchancedDiv.setAttribute('class', 'enchanceddescription');
+      enchancedDescription.forEach(element => {
+        enchancedList.push(element.textContent);
+      });
+      enchancedDiv.textContent = enchancedList.join('||');
+      document.body.appendChild(enchancedDiv);
+      // Creating div with additionalDescBulletInfo with || separator
+      const bulletInfoSelector = document.querySelectorAll('div.product-vorteil ul li');
+      const bulletList = [];
+      const bulletDiv = document.createElement('div');
+      bulletDiv.setAttribute('class', 'descbulletinfo');
+      bulletInfoSelector.forEach(element => {
+        bulletList.push(element.textContent);
+      });
+      bulletDiv.textContent = bulletList.join('||');
+      document.body.appendChild(bulletDiv);
     });
     await context.extract(productDetails);
   },
