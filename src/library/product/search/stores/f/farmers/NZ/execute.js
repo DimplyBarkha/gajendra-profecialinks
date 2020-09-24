5e994de681e9ce5d389f7a171fa79ad2bb979dbf
vinod chavan <vinodchavan102@gmail.com>
2020-09-24 18:32:26 +0530

    #556 - added loadselector and nextselector xpath

--- src/library/product/search/stores/f/farmers/NZ/execute.js
+++ src/library/product/search/stores/f/farmers/NZ/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'farmers',
     domain: 'farmers.co.nz',
     url: 'https://www.farmers.co.nz/search?SearchTerm={searchTerms}',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'ul.ish-productList li:first-child img',
+    noResultsXPath: "//div[contains(@class, 'search-result-main-block-contents')]//h1[contains(text(),'No Results Found:')]",
     zipcode: '',
   },
 };
