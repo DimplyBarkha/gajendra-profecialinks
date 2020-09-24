9d217a77189e8febb0f38cbb0564fcee47872d3e
NGenji <ngenji@wsgc.com>
2020-09-24 09:02:18 -0700

    QA2 fix

--- src/library/product/search/stores/w/wildberries/format.js
+++ src/library/product/search/stores/w/wildberries/format.js
@@ -7,41 +7,42 @@ const transform = (data, context) => {
   const state = context.getState();
   let orgRankCounter = state.orgRankCounter || 0;
   let rankCounter = state.rankCounter || 0;
-    for (const { group } of data) {
-      for (const row of group) {
-        try {
-            if (row.price && row.price[0]) {
-              row.price[0].text = row.price[0].text !="" ? `${row.price[0].text.replace(/ /, '')}` :  "";
-            }
-
-            if (row.thumbnail) {
-                let loadedImageUrl = "";
-                row.thumbnail.map(item=>{
-                    loadedImageUrl += `https:${item.text}`;
-                })
-                row.thumbnail = [
-                  {
-                    text: loadedImageUrl
-                  },
-                ];
-            }
-
-            if (!row.sponsored) {
-              orgRankCounter = orgRankCounter + 1;
-              row.rankOrganic = [{ text: orgRankCounter }];
-            }
-            rankCounter = rankCounter + 1;
-            row.rank = [{ text: rankCounter }];
-            context.setState({ orgRankCounter });
-            context.setState({ rankCounter });
-        }
-        catch(exception){
-            console.log(exception);
+  for (const { group } of data) {
+    for (const row of group) {
+      try {
+        if (row.price && row.price[0]) {
+          row.price[0].text =
+            row.price[0].text != ""
+              ? `${row.price[0].text.split("₽")[0]}₽`
+              : "";
         }
 
+        if (row.thumbnail) {
+          let loadedImageUrl = "";
+          row.thumbnail.map((item) => {
+            loadedImageUrl += `https:${item.text}`;
+          });
+          row.thumbnail = [
+            {
+              text: loadedImageUrl,
+            },
+          ];
+        }
+
+        if (!row.sponsored) {
+          orgRankCounter = orgRankCounter + 1;
+          row.rankOrganic = [{ text: orgRankCounter }];
+        }
+        rankCounter = rankCounter + 1;
+        row.rank = [{ text: rankCounter }];
+        context.setState({ orgRankCounter });
+        context.setState({ rankCounter });
+      } catch (exception) {
+        console.log(exception);
       }
     }
-    return data;
-  };
-  
-  module.exports = { transform };
+  }
+  return data;
+};
+
+module.exports = { transform };
