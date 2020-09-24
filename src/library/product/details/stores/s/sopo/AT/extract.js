2af1ccdecaabb7709bdb4d8014265126583299ed
Rafal Perkowski <rafalpero1@gmail.com>
2020-09-24 15:02:28 +0200

    Fixes

--- src/library/product/details/stores/s/sopo/AT/extract.js
+++ src/library/product/details/stores/s/sopo/AT/extract.js
@@ -18,27 +18,18 @@ module.exports = {
         const abc = document.evaluate(`//li[contains(text(), 'Verpackungs')][${x}]`, document, null, XPathResult.STRING_TYPE, null).stringValue;
         text.push(abc);
       }
-      /* for future if shipping details will fail QA again
-      const shippingDetails = document.querySelector("a[title='Versandkosten']");
-      if (shippingDetails) {
-        // @ts-ignore
-        shippingDetails.click();
-        await new Promise(resolve => setTimeout(resolve, 500));
-        const shippingSelector = document.querySelectorAll("div[class='panel--body is--wide'] > table > tbody > tr");
-        console.log(shippingSelector.length);
-        for (let y = 0; shippingSelector.length - 1 >= y; y++) {
-          const shippingDiv = document.createElement('div');
-          shippingDiv.setAttribute('class', `shippingdetails${y}`);
-          // @ts-ignore
-          shippingDiv.innerHTML = shippingSelector[y].innerText;
-          document.body.appendChild(shippingDiv);
-        }
-      } */
       const properText = text.toString();
       const newDiv = document.createElement('div');
       newDiv.setAttribute('class', 'dimension');
       newDiv.innerHTML = properText;
       document.body.appendChild(newDiv);
+
+      const descBullets = document.querySelectorAll('div[class="box"]>ul>li');
+      for (let y = 0; y < descBullets.length; y++) {
+        let content = descBullets[y].textContent.replace(/(.+)/, '$1||');
+        descBullets[y].innerHTML = content;
+
+      }
     });
     await context.extract(productDetails);
   },
