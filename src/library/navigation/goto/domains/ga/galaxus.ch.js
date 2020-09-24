84416e4155810c266e693b48697792f3176731a7
nehashende <neha.shende@synerzip.com>
2020-09-24 16:18:11 +0530

    #523. Updated rating xpath.

--- src/library/navigation/goto/domains/ga/galaxus.ch.js
+++ src/library/navigation/goto/domains/ga/galaxus.ch.js
@@ -9,7 +9,6 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    // await context.setBlockAds(false);
     const timeout = parameters.timeout ? parameters.timeout : 30000;
     await context.goto(url, { first_request_timeout: 60000, forced200: true, timeout, waitUntil: 'load', checkBlocked: false });
   },
