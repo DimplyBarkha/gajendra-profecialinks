80caa202c62d31b12f25f0a9bb68374756296d38
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-24 15:11:32 +0530

    Added variant extractor

---
+++ src/library/product/details/stores/b/bloomingdales/AE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'AE',
+    store: 'bloomingdales',
+    domain: 'bloomingdales.ae',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/b/bloomingdales/AE/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'AE',
+    store: 'bloomingdales',
+    transform: null,
+    domain: 'bloomingdales.ae',
+    zipcode: '',
+  },
+};
