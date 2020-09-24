4f407cd6ada535a05e88d0849b6108333aa4f156
AdamTo <a.a.tomaszewski@gmail.com>
2020-09-24 16:49:08 +0200

    extract JS raw

--- src/library/product/details/stores/h/haarbedarf/AT/extract.js
+++ src/library/product/details/stores/h/haarbedarf/AT/extract.js
@@ -1,10 +1,18 @@
+const { cleanUp } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AT',
     store: 'haarbedarf',
-    transform: null,
+    transform: cleanUp,
     domain: 'haarbedarf.at',
+  },
+
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    // await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    await context.evaluate(async function () {
+    });
+    await context.extract(productDetails);
   },
 };
