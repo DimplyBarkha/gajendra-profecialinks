9b8100f2511309a84a662c7723e025737cde771f
AdamTo <a.a.tomaszewski@gmail.com>
2020-09-24 10:09:41 +0200

    wip

---
+++ src/library/product/details/stores/r/rakuten/DE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'rakuten',
+    domain: 'rakuten.de',
+    loadedSelector: 'div.vw-productMain',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/r/rakuten/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'rakuten',
+    transform: null,
+    domain: 'rakuten.de',
+    zipcode: '',
+  },
+};
