4180b877461d4febc43c8701aaad00b7d22e969b
Adheer <amit.ranjan@pune.corp.reflexisinc.com>
2020-09-25 15:46:08 +0530

    core fixes

--- src/library/product/details/stores/n/neonet/PL/extract.js
+++ src/library/product/details/stores/n/neonet/PL/extract.js
@@ -7,37 +7,115 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const cssBtn = 'button.productDetailGallery-finiteNavigator-3iH:last-child';
 
   const isSelectorAvailable = async (cssSelector) => {
-    console.log(`Is selector available: ${cssSelector}`);
     return await context.evaluate(function (selector) {
       return !!document.querySelector(selector);
     }, cssSelector);
   };
 
-  console.log('.....waiting......');
-
-  for (let cnt = 0; cnt < 15; cnt++) {
+  for (let cnt = 0; cnt < 30; cnt++) {
     try {
-      await context.waitForSelector(cssBtn, { timeout: 10000 });
-
+      await context.waitForSelector(cssBtn, { timeout: 2000 });
       const productAvailable = await isSelectorAvailable(cssBtn);
-      console.log(`productAvailable: ${productAvailable}`);
       if (productAvailable) {
         console.log('clicking product link');
         await context.click(cssBtn);
-        await context.waitForNavigation({ timeout: 10000, waitUntil: 'load' });
-        console.log('navigation complete!!');
+        await context.waitForNavigation({ timeout: 5000, waitUntil: 'load' });
+      } else {
+        console.log('else block ## clicking product link');
+        break;
       }
-    } catch (err) {
+    } catch (err) { }
+  }
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  // Function to check whether video content exists
+  async function checkVideo () {
+    return await context.evaluate(async function () {
+      const videoSelector = document.evaluate('//iframe[@title="movie"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      const videoSrc = videoSelector ? videoSelector.src : '';
+      if (videoSrc) {
+        return videoSrc;
+      } else {
+        return false;
+      }
+    });
+  }
 
+  const videoLink = await checkVideo();
+  // console.log('videoLink ....', videoLink);
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  console.log('Correct till here...............videoLink --', videoLink);
+  // Function to check whether manufacturer content exists
+  async function checkmanufacturerContent () {
+    return await context.evaluate(async function () {
+      const manufacturerIFrameSelector = document.evaluate('//div[@id="rich-content-ext"]/iframe[@class="iFrame-iframe-2gX"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      const manufacturerIFrameSrc = manufacturerIFrameSelector ? manufacturerIFrameSelector.src : '';
+      if (manufacturerIFrameSrc) {
+        return manufacturerIFrameSrc;
+      } else {
+        return false;
+      }
+    });
+  }
+  // Function to fetch manufacturer content and mnufacturer images after visiting iframe URL
+  async function fetchManufacturerContentIframe (url) {
+    try {
+      await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+    } catch (err) {
+    }
+    return await context.evaluate(async function () {
+      const manufacturerDescription = document.body.innerText;
+      const manufacturerImagesList = document.querySelectorAll('img');
+      const manufacturerImageArray = [];
+
+      for (let i = 1; i < manufacturerImagesList.length; i++) {
+        const imgUrl = manufacturerImagesList[i].getAttribute('src');
+        imgUrl && manufacturerImageArray.push(imgUrl);
+      }
+      return { manufacturerImageArray, manufacturerDescription };
+    });
+  }
+  // Function to add manufacturer content and description to DOM
+  async function addContentToDOM (manContentObj, manufacturerContentLink, videoLink) {
+    await context.evaluate(async function ([manContentObj, manufacturerContentLink, videoLink]) {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+      if (manufacturerContentLink) {
+        const imageUrlSfx = manufacturerContentLink.replace('index.html', '');
+        // Adding manufacturer images to DOM
+        for (let i = 0; i < manContentObj.manufacturerImageArray.length; i++) {
+          addHiddenDiv('added-manufacturer-images-' + i, imageUrlSfx + manContentObj.manufacturerImageArray[i]);
+        }
+        addHiddenDiv('added-manufacturer-description', manContentObj.manufacturerDescription);
+      }
+      if (videoLink) {
+        addHiddenDiv('added-video', videoLink);
+      }
+    }, [manContentObj, manufacturerContentLink, videoLink]);
+  }
+
+  const manufacturerContentLink = await checkmanufacturerContent();
+  let manContentObj;
+  if (manufacturerContentLink) {
+    manContentObj = await fetchManufacturerContentIframe(manufacturerContentLink);
+    try {
+      await context.goto(inputs.url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+    } catch (err) {
     }
   }
-  await context.evaluate(async function () {
 
-  });
+  await addContentToDOM(manContentObj, manufacturerContentLink, videoLink);
+
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
--- src/library/product/details/stores/n/neonet/format.js
+++ src/library/product/details/stores/n/neonet/format.js
@@ -4,6 +4,22 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const cleanUp = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  let manufacturerImages = [];
+  const manufacturerDescription = [];
+  const video = [];
   for (const { group } of data) {
     for (const row of group) {
       if (row.description) {
@@ -30,7 +46,7 @@ const transform = (data) => {
         row.specifications.forEach(item => {
           specs[0] = item;
           item.text = item.text.replace(/(\s?\n)+/g, ' ');
-          if (cnt > 0) newTxt = newTxt + '|' + item.text;
+          if (cnt > 0) newTxt = newTxt + ' | ' + item.text;
           else newTxt = newTxt + item.text;
           cnt++;
         });
@@ -84,12 +100,36 @@ const transform = (data) => {
       }
       if (row.technicalInformationPdfPresent) {
         row.technicalInformationPdfPresent.forEach(item => {
-          if (item.text && item.text.length > 0) {
+          if (item.text && item.text.length > 0 && item.text !== 'No') {
             item.text = 'Yes';
           } else {
             item.text = 'No';
           }
         });
+      }
+
+      if (row.manufacturerImages1) {
+        manufacturerImages = [...manufacturerImages, ...row.manufacturerImages1];
+        delete row.manufacturerImages1;
+        row.manufacturerImages = manufacturerImages;
+      }
+
+      if (row.manufacturerDescription1) {
+        row.manufacturerDescription1.forEach(item => {
+          item.text = cleanUp(item.text);
+          manufacturerDescription[0] = item;
+        });
+        delete row.manufacturerDescription1;
+        row.manufacturerDescription = manufacturerDescription;
+      }
+
+      if (row.video1) {
+        row.video1.forEach(item => {
+          item.text = cleanUp(item.text);
+          video[0] = item;
+        });
+        delete row.video1;
+        row.videos = video;
       }
     }
   }
