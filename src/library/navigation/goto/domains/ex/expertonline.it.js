c7af45386c33bd7ca8e46ea7356397969af373d3
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-25 10:41:43 +0530

    enhancedContent loading issue (#616)

--- src/library/navigation/goto/domains/ex/expertonline.it.js
+++ src/library/navigation/goto/domains/ex/expertonline.it.js
@@ -8,4 +8,14 @@ module.exports = {
     store: 'expert',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
