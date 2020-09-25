b62c8196272aced110c674c85554083a4473206d
Sourabh Karmarkar <saurabhkarmarkar@ymail.com>
2020-09-25 12:58:29 +0530

    #449 Added captcha solver code for Fnac_FR

--- src/library/navigation/goto/domains/fn/fnac.com.js
+++ src/library/navigation/goto/domains/fn/fnac.com.js
@@ -8,14 +8,45 @@ module.exports = {
     store: 'fnac',
     zipcode: '',
   },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+  implementation: async (
+    { url, zipcode, storeId },
+    parameters,
+    context,
+    dependencies
+  ) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.setAntiFingerprint(false);
     await context.setLoadAllResources(true);
-    await context.setBlockAds(false);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-    }
+    await context.goto(url, {
+      antiCaptchaOptions: {
+        provider: "2-captcha",
+        type: "GEETEST",
+      },
+      timeout: timeout,
+      waitUntil: "load",
+      checkBlocked: false,
+      js_enabled: true,
+      load_timeout: 30,
+      css_enabled: true,
+    });
+
+    await new Promise((resolve, reject) => setTimeout(resolve, 10000))
+
+    await context.evaluate(
+      function () {
+        const code = geetest.toString().replace(/appendTo\("#([^"]+)"\)/,'appendTo(document.getElementById("$1"))');
+        return eval(`(${code})('/captcha/geetest');`);
+      }, undefined, 'iframe');
+
+    await new Promise((r) => setTimeout(r, 500));
+
+    await context.evaluate(
+      function () {
+        document.querySelector(".captcha-handler").click();
+      },
+      undefined,
+      "iframe"
+    );
+
+    await context.waitForNavigtion();
   },
 };
