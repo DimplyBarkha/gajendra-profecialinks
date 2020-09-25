1861585a20913a585a675a21c45510cb958fa92d
vikas kyatannawar <vikas.k@osmosys.asia>
2020-09-25 17:50:40 +0530

    fixed QA issues

--- src/library/navigation/goto/domains/te/teknosa.com.js
+++ src/library/navigation/goto/domains/te/teknosa.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'teknosa.com',
-    timeout: null,
+    timeout: 60000,
     country: 'TR',
     store: 'teknosa',
     zipcode: '',
@@ -11,7 +11,7 @@ module.exports = {
   implementation: async ({ url }, { timeout }, context, dependencies) => {
     url = `${url}#[!opt!]{"block_ads":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
     timeout = timeout || 10000;
-    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url, { timeout, waitUntil: 'networkidle0', checkBlocked: true });
     await context.evaluate(() => {
       document.body.setAttribute('current-page', window.location.href);
     });
