609cc52555a3a21fa076812ef57140cb340e2681
Karolina <karolina.urbanska@tdcm.io>
2020-09-25 15:03:00 +0200

    Fixed variantInfo selector and linter issues

--- src/library/product/details/stores/k/krefel/BE/extract.js
+++ src/library/product/details/stores/k/krefel/BE/extract.js
@@ -1,23 +1,23 @@
-const { cleanUp } = require("../../../../shared");
+const { cleanUp } = require('../../../../shared');
 module.exports = {
-  implements: "product/details/extract",
+  implements: 'product/details/extract',
   parameterValues: {
-    country: "BE",
-    store: "krefel",
+    country: 'BE',
+    store: 'krefel',
     transform: cleanUp,
-    domain: "krefel.be",
-    zipcode: "",
+    domain: 'krefel.be',
+    zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
       function addElementToDocument(key, value) {
-        const catElement = document.createElement("div");
+        const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
-        catElement.style.display = "none";
+        catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
-      const accCookie = document.querySelector("section.cookie-alert div.button-section a");
+      const accCookie = document.querySelector('section.cookie-alert div.button-section a');
       if (accCookie) accCookie.click();
       await new Promise((resolve, reject) => setTimeout(resolve, 6000));
 
@@ -26,11 +26,11 @@ module.exports = {
       const regexRating = /"ratingValue":\s"([\d,]+)"/g;
       const foundAvailability = regexAvailability.exec(scriptWithInfo.stringValue);
       const foundRatingValue = regexRating.exec(scriptWithInfo.stringValue);
-      if (foundAvailability && foundAvailability[1] === "InStock") {
-        addElementToDocument("availability", "In Stock");
-      } else addElementToDocument("availability", "Out of Stock");
+      if (foundAvailability && foundAvailability[1] === 'InStock') {
+        addElementToDocument('availability', 'In Stock');
+      } else addElementToDocument('availability', 'Out of Stock');
       if (foundRatingValue) {
-        addElementToDocument("ratingValue", foundRatingValue[1]);
+        addElementToDocument('ratingValue', foundRatingValue[1]);
       }
 
       const price = document.querySelector('div.product-info span.current-price.orange.nl')
@@ -38,6 +38,13 @@ module.exports = {
       if (price) {
         addElementToDocument('price', `€ ${price.replace(/\.|\s/g, '')}`);
       }
+
+      const variantInfo = document.querySelector('select.product-variant-select option[selected]')
+        ? document.querySelector('select.product-variant-select option[selected]').innerText : '';
+      if (variantInfo) {
+        addElementToDocument('variantInfo', variantInfo);
+      }
+
       const pdfExists = document.evaluate('//div[@class="description-wrap"]//a/@href[contains(.,"pdf") or contains(.,"PDF")]', document, null, XPathResult.STRING_TYPE, null);
       if (pdfExists && pdfExists.stringValue) addElementToDocument('pdfExists', 'Yes');
       const specifications = document.querySelectorAll('div.block-header, div.specs-overview li');
@@ -45,7 +52,7 @@ module.exports = {
       const specArr = [];
       if (specifications.length) {
         specifications.forEach((e) => {
-          specArr.push(e.innerText.replace(/\n/g, " "));
+          specArr.push(e.innerText.replace(/\n/g, ' '));
         });
       }
 
@@ -55,22 +62,22 @@ module.exports = {
       addElementToDocument('description', description);
 
       const warrantyXpath = document.evaluate('//div[@class="description-wrap"]//*[contains(text(),"garantie")]', document, null, XPathResult.STRING_TYPE, null);
-      const warranty = warrantyXpath ? warrantyXpath.stringValue : "";
-      if (warranty) addElementToDocument("warranty", warranty);
+      const warranty = warrantyXpath ? warrantyXpath.stringValue : '';
+      if (warranty) addElementToDocument('warranty', warranty);
 
-      const promotionText = document.querySelector("div.product-info .promotion-wrap > span") ? document.querySelector("div.product-info .promotion-wrap > span").innerText : "";
-      if (promotionText) addElementToDocument("promotionText", promotionText);
+      const promotionText = document.querySelector('div.product-info .promotion-wrap > span') ? document.querySelector('div.product-info .promotion-wrap > span').innerText : '';
+      if (promotionText) addElementToDocument('promotionText', promotionText);
 
-      let videosText = document.querySelector("div.autheos-videothumbnail img") ? document.querySelector("div.autheos-videothumbnail img").getAttribute("src") : "";
+      let videosText = document.querySelector('div.autheos-videothumbnail img') ? document.querySelector('div.autheos-videothumbnail img').getAttribute('src') : '';
       if (videosText) {
-        const index = videosText.indexOf("thumb");
-        if (videosText.includes("generic")) {
+        const index = videosText.indexOf('thumb');
+        if (videosText.includes('generic')) {
           videosText = videosText.slice(0, index - 1);
         } else {
           videosText = videosText.slice(0, index);
-          videosText += "high.mp4";
+          videosText += 'high.mp4';
         }
-        addElementToDocument("videosText", videosText);
+        addElementToDocument('videosText', videosText);
       }
     });
     await new Promise((resolve, reject) => setTimeout(resolve, 6000));
