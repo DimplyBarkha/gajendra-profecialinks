92fcbfaae975f153ea2fe236bd97be1e3026f929
Prasanna Barate <prasanna.barate@synerzip.com>
2020-09-25 14:50:30 +0530

    (#389) Addded code to set zip code and selection of first store.

--- src/library/navigation/goto/domains/ho/setZipCode.homedepot.ca.js
+++ src/library/navigation/goto/domains/ho/setZipCode.homedepot.ca.js
@@ -1,3 +1,29 @@
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { zipcode } = inputs;
+  try {
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    try {
+      await context.click('localization-confirmation button[tabindex="0"]')
+    } catch (e) {
+      console.log(e)
+    }
+    await context.click('acl-icon[symbol="pin"]')
+    await context.waitForSelector('input[placeholder*="Postal Code"]')
+    await context.setInputValue('input[placeholder*="Postal Code"]', zipcode);
+    await context.click('button[aria-describedby="store-selector-search"]')
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    await context.click('div[class*="__column-cta"] button[acl-button]')
+    await new Promise(resolve => setTimeout(resolve, 5000))
+  } catch (e) {
+    console.log('Error: ', e)
+  }
+  await context.waitForNavigation();
+}
 
 module.exports = {
   implements: 'navigation/goto/setZipCode',
@@ -7,4 +33,5 @@ module.exports = {
     store: 'homedepot',
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/details/stores/h/homedepot/CA/extract.js
+++ src/library/product/details/stores/h/homedepot/CA/extract.js
@@ -10,15 +10,6 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
-    try {
-      await new Promise(resolve => setTimeout(resolve, 5000))
-      await context.click('a[class*="acl-link--weak order-1"]')
-      await context.waitForSelector('div.hdca-fade-in-open')
-      await new Promise(resolve => setTimeout(resolve, 5000))
-      await context.click('div.hdca-store-list-item__ctas>button.acl-button--primary')
-    } catch (e) {
-      console.log(e)
-    }
     await new Promise(resolve => setTimeout(resolve, 10000))
     return await context.extract(productDetails, { transform });
   },
