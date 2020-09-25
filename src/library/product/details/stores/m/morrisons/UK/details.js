95380d4a2ffddf24c2e18443b213664ff2b2e118
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-25 20:08:59 +0530

    Added extractor for morrisions.com

---
+++ src/library/product/details/stores/m/morrisons/UK/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'UK',
+    store: 'morrisons',
+    domain: 'groceries.morrisons.com',
+    zipcode: '',
+  },
+};
