1cf6410954be89fc3e0a5fe37be50db8ba019c05
Nilesh <nilesh.b@osmosys.asia>
2020-09-28 18:10:12 +0530

    Fixed no result xpath issue.

--- src/library/product/search/stores/n/nykaa/IN/execute.js
+++ src/library/product/search/stores/n/nykaa/IN/execute.js
@@ -5,6 +5,7 @@ module.exports = {
     country: 'IN',
     store: 'nykaa',
     domain: 'nykaa.com',
+    noResultsXPath: '//td[contains(@class,"products depth_1")]/table/tbody[count(*)=0]',
     url: 'https://www.nykaa.com/nyk/aggregator-gludo/api/search.list?filter_format=v2&from=0&page_no=1&search={searchTerms}&source=react#[!opt!]{"type":"json"}[/!opt!]',
   },
 };
--- src/library/product/search/stores/n/nykaa/IN/paginate.js
+++ src/library/product/search/stores/n/nykaa/IN/paginate.js
@@ -5,6 +5,7 @@ module.exports = {
     country: 'IN',
     store: 'nykaa',
     loadedSelector: 'td.dynamic_tags',
+    noResultsXPath: '//td[contains(@class,"products depth_1")]/table/tbody[count(*)=0]',
     openSearchDefinition: {
       indexOffset: 20,
       template: 'https://www.nykaa.com/nyk/aggregator-gludo/api/search.list?filter_format=v2&from={offset}&search={searchTerms}&source=react#[!opt!]{"type":"json"}[/!opt!]',
