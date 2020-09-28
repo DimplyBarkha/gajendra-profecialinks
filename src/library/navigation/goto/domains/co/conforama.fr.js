bebe71c9f25bdc0d909c951e9be7f93188013f45
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-28 13:28:22 +0530

    evaluateInFrame changes

--- src/library/navigation/goto/domains/co/conforama.fr.js
+++ src/library/navigation/goto/domains/co/conforama.fr.js
@@ -11,20 +11,30 @@ module.exports = {
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
     console.log("BEFORE GOTO");
-   
       await context.setAntiFingerprint(false);
       await context.setLoadAllResources(true);
       await context.setBlockAds(false);
-      // await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
-      await context.goto(url,  {
-        timeout: 50000, waitUntil: 'load', checkBlocked: true,
-        antiCaptchaOptions: {
-        type: 'RECAPTCHA',
-        },
-        // proxy: {
-        //   use_relay_proxy: false
-        // },
+      await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: false });
+      const isCaptcha = async () => {
+        return await context.evaluate(async function () {
+        let captchaEl = document.querySelector('div.g-recaptcha');
+        if(captchaEl){
+         return captchaEl;
+        }
         });
+      };
+      console.log('isCaptcha: ', await isCaptcha);
+      await context.evaluateInFrame(isCaptcha, async function () {
+        console.log('evaluateInFrame:');
+        let captchaCheckBox = document.querySelector('div.g-recaptcha');
+        if(captchaCheckBox){
+          console.log('captchaCheckBox: ', captchaCheckBox);
+         // @ts-ignore
+         captchaCheckBox.click();
+        }
+      // @ts-ignore
+      });
+      await context.waitForNavigtion();
         try {
           let acceptCookie = await context.waitForSelector('button#footer_tc_privacy_button');
           if(acceptCookie){
@@ -32,7 +42,7 @@ module.exports = {
           }
           await new Promise((resolve, reject) => setTimeout(resolve, 6000));
         } catch (error) {
-          
+          console.log('error: ', error);
         }
   }
 };
