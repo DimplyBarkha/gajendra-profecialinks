aadc663f04e2606bd8d6f806a1c93241129f70e7
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-28 14:28:17 +0530

    cleanUp added in IT(#616)

--- src/library/product/details/stores/e/expert/IT/shared.js
+++ src/library/product/details/stores/e/expert/IT/shared.js
@@ -18,10 +18,6 @@ const transform = (data) => {
       // eslint-disable-next-line no-control-regex
       .replace(/[\x00-\x1F]/g, '')
       .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      el.text = clean(el.text);
-    }))));
-  
     for (const { group } of data) {
       for (const row of group) {
         if (row.additionalDescBulletInfo) {
@@ -47,6 +43,9 @@ const transform = (data) => {
     
       }
     }
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
     return data;
   };
   
