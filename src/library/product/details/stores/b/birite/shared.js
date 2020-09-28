abefdaa4629f7ef13734277eed6fc8f1864037e8
KCSPL\jatin <jatin@kcspl.co.in>
2020-09-28 17:27:54 +0530

    QA1 feedback points

--- src/library/product/details/stores/b/birite/shared.js
+++ src/library/product/details/stores/b/birite/shared.js
@@ -28,8 +28,8 @@ const transform = (data) => {
             text: text.slice(0, -4),
           },
         ];
-      } 
-       if (row.productOtherInformation) {
+      }
+      if (row.productOtherInformation) {
         let text = '';
         row.productOtherInformation.forEach(item => {
           text += `${item.text.replace(/\n \n/g, ':')} || `;
@@ -39,9 +39,37 @@ const transform = (data) => {
             text: text.slice(0, -4),
           },
         ];
-      } 
+      }
+      if (row.alternateImages) {
+        let text = '';
+        let newAltImage = []
+        row.alternateImages.forEach(item => {
+          text = item.text.replace('-147x116', '');
+          newAltImage.push({ text });
+        });
+        row.alternateImages = newAltImage;
+      }
+      
     }
   }
+  // Clean up data
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+
   return data;
 };
 
