6b76cc24deb570bc0a36988bbf33134772834450
Ankit Kumar <ankit.k@osmosys.asia>
2020-09-29 15:16:31 +0530

    added script to handle captcha issue (wip)

--- src/library/navigation/goto/domains/co/courir.com.js
+++ src/library/navigation/goto/domains/co/courir.com.js
@@ -8,4 +8,43 @@ module.exports = {
     store: 'courir',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+
+    const responseStatus = await context.goto(url, {
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: true,
+      embed_iframes: true,
+    });
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
+    const captchaFrame = 'iframe[_src*="https://geo.captcha"]';
+    // const captchaSelector = '.g-recaptcha';
+    const checkExistance = async (selector) => {
+      return await context.evaluate(async (captchaSelector) => {
+        console.log('captcha present');
+        return Boolean(document.querySelector(captchaSelector));
+      }, selector);
+    };
+    // await checkExistance(captchaSelector);
+    const isCaptchaFramePresent = await checkExistance(captchaFrame);
+
+    if (isCaptchaFramePresent) {
+      console.log('isCaptcha', true);
+      // context.evaluateInFrame(captchaFrame, () => grecaptcha.execute());
+      await context.solveCaptcha({
+        type: 'GEETEST',
+        inputElement: 'div.captcha__human__captcha-container',
+        autoSubmit: true,
+      });
+      console.log('solved captcha, waiting for page change');
+      await context.waitForNavigation({ timeout });
+    }
+  },
 };
