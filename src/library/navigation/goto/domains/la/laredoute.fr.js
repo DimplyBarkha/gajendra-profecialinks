fa56dc75d25466550b855762deb14be7ff07a1d4
nehashende <neha.shende@synerzip.com>
2020-09-29 12:01:20 +0530

    #475. Added evaluateInFrame in captcha code.

--- src/library/navigation/goto/domains/la/laredoute.fr.js
+++ src/library/navigation/goto/domains/la/laredoute.fr.js
@@ -10,15 +10,21 @@ module.exports = {
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 30000;
-    await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true });
-    await context.evaluateInFrame(function () {
-      const captchaCheckBox = document.querySelector('div.g-recaptcha');
-      if (captchaCheckBox) {
-        console.log('captchaCheckBox: ', captchaCheckBox);
-        // @ts-ignore
-        captchaCheckBox.click();
-      }
-    // @ts-ignore
+    await context.goto(url, {
+      first_request_timeout: 60000,
+      timeout,
+      waitUntil: 'load',
+      checkBlocked: true,
+      options: {
+        antiCaptchaOptions: {
+          type: 'RECAPTCHA',
+        },
+        proxy: {
+          use_relay_proxy: false,
+        },
+      },
     });
+
+    context.evaluateInFrame('iframe', () => grecaptcha.execute());
   },
 };
