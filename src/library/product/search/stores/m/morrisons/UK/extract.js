e95025b54593c7edd9a51356743aebd594ce73d6
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-30 01:16:52 +0530

    Added schema for the extractor

--- src/library/product/search/stores/m/morrisons/UK/extract.js
+++ src/library/product/search/stores/m/morrisons/UK/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('./shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'UK',
     store: 'morrisons',
-    transform: null,
+    transform,
     domain: 'groceries.morrisons.com',
     zipcode: '',
   },
