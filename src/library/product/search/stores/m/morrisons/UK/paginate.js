a604d2969d37752e94e6487ceb295642542fa301
Radha Krishna Telu <radhakrishna.t@osmosys.asia>
2020-09-30 01:08:53 +0530

    added search extractor

---
+++ src/library/product/search/stores/m/morrisons/UK/paginate.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'UK',
+    store: 'morrisons',
+    nextLinkSelector: null,
+    noResultsXPath: null,
+    domain: 'groceries.morrisons.com',
+    zipcode: '',
+  },
+};
