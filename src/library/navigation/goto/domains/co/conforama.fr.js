747bc927bfc316eb3523aa7cda62f98ebe45a73e
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-30 14:05:00 +0530

    Captcha code final (#438)

--- src/library/navigation/goto/domains/co/conforama.fr.js
+++ src/library/navigation/goto/domains/co/conforama.fr.js
@@ -9,40 +9,81 @@ module.exports = {
     first_request_timeout: 90000,
     zipcode: '',
   },
-  implementation: async ({ url }, parameters, context, dependencies) => {
-    console.log("BEFORE GOTO");
-      await context.setAntiFingerprint(false);
-      await context.setLoadAllResources(true);
-      await context.setBlockAds(false);
-      await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: false });
-      const isCaptcha = async () => {
-        return await context.evaluate(async function () {
-        let captchaEl = document.querySelector('div.g-recaptcha');
-        if(captchaEl){
-         return captchaEl;
-        }
-        });
-      };
-      console.log('isCaptcha: ', await isCaptcha);
-      await context.evaluateInFrame(isCaptcha, async function () {
-        console.log('evaluateInFrame:');
-        let captchaCheckBox = document.querySelector('div.g-recaptcha');
-        if(captchaCheckBox){
-          console.log('captchaCheckBox: ', captchaCheckBox);
-         // @ts-ignore
-         captchaCheckBox.click();
-        }
+  implementation: async (
+    { url },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+
+    const responseStatus = await context.goto(url, {
+      firstRequestTimeout: 60000,
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+      antiCaptchaOptions: {
+        type: 'RECAPTCHA',
+      },
+    });
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
+    let captchaFrame = "iframe[_src*='captcha']:not([title]), iframe[src*='captcha']:not([title]), div.g-recaptcha";
+    try {
+      await context.waitForSelector(captchaFrame);
+    } catch (error) {
+      console.log('error: without undescore ', error);
+    }
+
+    console.log('captchaFrame', captchaFrame);
+    const checkExistance = async (selector) => {
+      return await context.evaluate(async (captchaSelector) => {
+        return Boolean(document.querySelector(captchaSelector));
+      }, selector);
+    };
+    const isCaptchaFramePresent = await checkExistance(captchaFrame);
+    // const isCaptchaFramePresent = true;
+    if (isCaptchaFramePresent) {
+      console.log('isCaptcha', true);
+      await context.waitForNavigation({ timeout });
       // @ts-ignore
-      });
-      await context.waitForNavigtion();
-        try {
-          let acceptCookie = await context.waitForSelector('button#footer_tc_privacy_button');
-          if(acceptCookie){
-            await context.click('button#footer_tc_privacy_button');
-          }
-          await new Promise((resolve, reject) => setTimeout(resolve, 6000));
-        } catch (error) {
-          console.log('error: ', error);
+      // eslint-disable-next-line no-undef
+      await context.evaluateInFrame('iframe', () => grecaptcha.execute());
+      console.log('solved captcha, waiting for page change');
+      await context.waitForNavigation({ timeout });
+      await context.waitForXPath('//div[@id="product-detail-page"]', { timeout });
+      try {
+        let cookieButton = document.querySelector('button#footer_tc_privacy_button');
+        if (cookieButton) {
+          // @ts-ignore
+          cookieButton.click();
         }
-  }
+      } catch (error) {
+        console.log('error: ', error);
+
+      }
+    }
+    //   if (responseStatus.url) {
+    //     console.log('responseStatus.url: ', responseStatus.url);
+    //   return context.reportBlocked(responseStatus.url, 'Blocked: ' + responseStatus.url);
+    // }
+    try {
+      let cookieButton = document.querySelector('button#footer_tc_privacy_button');
+      if (cookieButton) {
+        // @ts-ignore
+        cookieButton.click();
+      }
+    } catch (error) {
+      console.log('error: ', error);
+
+    }
+  },
 };
