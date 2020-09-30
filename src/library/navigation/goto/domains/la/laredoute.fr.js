ac059885a765589bb6e1cbd9d3dd9b88cf621715
nehashende <neha.shende@synerzip.com>
2020-09-30 14:08:14 +0530

    #475. Added working captcha code.

--- src/library/navigation/goto/domains/la/laredoute.fr.js
+++ src/library/navigation/goto/domains/la/laredoute.fr.js
@@ -8,23 +8,59 @@ module.exports = {
     store: 'laredoute',
     zipcode: '',
   },
-  implementation: async ({ url }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 30000;
-    await context.goto(url, {
-      first_request_timeout: 60000,
-      timeout,
+  implementation: async (
+    { url },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+
+    const responseStatus = await context.goto(url, {
+      firstRequestTimeout: 60000,
+      timeout: timeout,
       waitUntil: 'load',
-      checkBlocked: true,
-      options: {
-        antiCaptchaOptions: {
-          type: 'RECAPTCHA',
-        },
-        proxy: {
-          use_relay_proxy: false,
-        },
+      checkBlocked: false,
+      antiCaptchaOptions: {
+        type: 'RECAPTCHA',
       },
     });
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
 
-    context.evaluateInFrame('iframe', () => grecaptcha.execute());
+    const captchaFrame = "iframe[_src*='captcha']:not([title]), iframe[src*='captcha']:not([title])";
+    try {
+      await context.waitForSelector(captchaFrame);
+    } catch (e) {
+      console.log('Captcha frame not found', e);
+    }
+    const checkExistance = async (selector) => {
+      return await context.evaluate(async (captchaSelector) => {
+        return Boolean(document.querySelector(captchaSelector));
+      }, selector);
+    };
+    const isCaptchaFramePresent = await checkExistance(captchaFrame);
+
+    if (isCaptchaFramePresent) {
+      console.log('isCaptcha', true);
+      await context.waitForNavigation({ timeout });
+      // @ts-ignore
+      // eslint-disable-next-line no-undef
+      await context.evaluateInFrame('iframe', () => grecaptcha.execute());
+      console.log('solved captcha, waiting for page change');
+      await context.waitForNavigation({ timeout });
+      try {
+        await context.waitForSlector('#corePage');
+      } catch (e) {
+        console.log('Details page selector not found');
+      }
+    }
   },
 };
