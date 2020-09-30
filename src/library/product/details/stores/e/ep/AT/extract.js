d6cc002344909e4ee698be2a51d11e12a25ca127
GirishSolanki <girish.solanki@kcsitglobal.com>
2020-09-30 22:44:53 +0530

    pushed changes to dev branch

--- src/library/product/details/stores/e/ep/AT/extract.js
+++ src/library/product/details/stores/e/ep/AT/extract.js
@@ -1,10 +1,11 @@
+const{transform} = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AT',
     store: 'ep',
-    transform: null,
+    transform: transform,
     domain: 'ep.at',
     zipcode: '',
   },
