1f5f668317232f75f3a28ef7d55cd4d69f6ef125
richa.t@osmosys.asia <richa.t@osmosys.asia>
2020-09-30 17:32:20 +0530

    Updated code for category

--- src/library/product/details/stores/b/babyjogger/US/extract.js
+++ src/library/product/details/stores/b/babyjogger/US/extract.js
@@ -16,22 +16,26 @@ module.exports = {
   ) => {
     await context.evaluate(async function () {
       let breadcrumbs = document.querySelectorAll('li.breadcrumb-item');
-      for (i = 0; i < breadcrumbs.length - 1; i++) {
-        if (breadcrumbs.length > 1) {
+      if (breadcrumbs.length > 1) {
+        for (i = 0; i < breadcrumbs.length - 1; i++) {
           var div = document.createElement('li');
           div.className = 'categories';
           var getInput = document.createElement('input');
           div.appendChild(getInput);
           document.head.appendChild(div);
           getInput.setAttribute("value", breadcrumbs[i + 1].querySelector('a').innerText);
+
         }
-        else {
-          var div = document.createElement('li');
-          div.className = 'categories';
-          var getInput = document.createElement('input');
-          div.appendChild(getInput);
-          document.head.appendChild(div);
-          getInput.setAttribute("value", breadcrumbs[i].querySelector('a').innerText);
+      }
+      else {
+        breadcrumbs = document.querySelector('li.breadcrumb-item a');
+        if(breadcrumbs){
+        var div = document.createElement('li');
+        div.className = 'categories';
+        var getInput = document.createElement('input');
+        div.appendChild(getInput);
+        document.head.appendChild(div);
+        getInput.setAttribute("value", breadcrumbs.innerText);
         }
       }
       let getAllImages = document.querySelectorAll('button.carousel-img-wrap img');
