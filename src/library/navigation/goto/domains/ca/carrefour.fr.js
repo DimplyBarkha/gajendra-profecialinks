3bcfdc030b72b863a7c016907ec92c842eac4be8
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-09-30 13:46:45 +0530

    Final Captcha code  (#456)

--- src/library/navigation/goto/domains/ca/carrefour.fr.js
+++ src/library/navigation/goto/domains/ca/carrefour.fr.js
@@ -8,164 +8,81 @@ module.exports = {
     store: 'carrefour',
     zipcode: '',
   },
-  implementation: async ({ url }, parameterValues, context, dependencies) => {
-    const memory = {};
-    const backconnect = !!memory.backconnect;
-    console.log('backconnect', backconnect);
-    const benchmark = !!memory.benchmark;
-    console.log('benchmark', benchmark);
-    const start = Date.now();
-    const MAX_CAPTCHAS = 3;
-    // await context.setUseRelayProxy(false);
+  implementation: async (
+    { url },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
 
-    // let pageId;
-    let captchas = 0;
-    let hasCaptcha = false;
-    let lastResponseData;
-    // eslint-disable-next-line
-    // const js_enabled = true; // Math.random() > 0.7;
-    // console.log('js_enabled', js_enabled); ;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
 
-    const isCaptcha = async () => {
-      return await context.evaluate(async function () {
-        const captchaEl = document.evaluate("//iframe')]", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        if (captchaEl.snapshotLength) {
-          return 'true';
-        } else {
-          return 'false';
-        }
-      });
-    };
-
-    const solveCaptcha = async () => {
-      console.log('isCaptcha', true);
-
-      await context.solveCaptcha({
+    const responseStatus = await context.goto(url, {
+      firstRequestTimeout: 60000,
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+      antiCaptchaOptions: {
         type: 'RECAPTCHA',
-      //  inputElement: 'form input[type=text][name]',
-      //  imageElement: 'form img',
-      //  autoSubmit: true,
-      });
-      console.log('solved captcha, waiting for page change');
-      context.waitForNavigation();
-      console.log('Captcha vanished');
-    };
-
-    const solveCaptchaIfNecessary = async () => {
-      console.log('Checking for CAPTCHA');
-      while (await isCaptcha() === 'true' && captchas < MAX_CAPTCHAS) {
-        captchas++;
-        if (backconnect) {
-          throw Error('CAPTCHA received');
-        }
-        console.log('Solving a captcha', await isCaptcha(), captchas);
-        await solveCaptcha();
-      }
-      if (await isCaptcha() === 'true') {
-        if (!benchmark) {
-          // we failed to solve the CAPTCHA
-          console.log('We failed to solve the CAPTCHA');
-          return context.reportBlocked(lastResponseData.code, 'Blocked: Could not solve CAPTCHA, attempts=' + captchas);
-        }
-        return false;
-      }
-      return true;
-    };
-    const run = async () => {
-      await context.setUseRelayProxy(false);
-      lastResponseData = await context.goto(url, {
-        antiCaptchaOptions: {
-          type: 'RECAPTCHA',
-        },
-        timeout: 10000,
-        waitUntil: 'load',
-        checkBlocked: true,
-        js_enabled: true,
-        //  css_enabled: false,
-        random_move_mouse: true,
-      });
-
-      if (lastResponseData.status === 404 || lastResponseData.status === 410) {
-        return;
-      }
-
-      if (lastResponseData.status === 503) {
-        console.log('Clicking 503 image');
-        await context.click('a img[src*="503.png"], a[href*="ref=cs_503_link"]');
-
-        console.log('Waiting for page to reload on homepage');
-        context.waitForNavigation();
-        if (!await solveCaptchaIfNecessary()) {
-          hasCaptcha = true;
-          return;
-        }
-
-        console.log('Going back to desired page');
-        await context.setUseRelayProxy(false);
-        lastResponseData = await context.goto(url, {
-          antiCaptchaOptions: {
-            type: 'RECAPTCHA',
-          },
-          timeout: 10000,
-          waitUntil: 'load',
-          checkBlocked: true,
-          js_enabled: true,
-          //      css_enabled: false,
-          random_move_mouse: true,
-        });
-        console.log('lastResponseData', lastResponseData);
-        // @ts-ignore
-        await context.evaluateInFrame('iframe', ()=> grecaptcha.execute())
-      }
-
-      if (lastResponseData.status === 404 || lastResponseData.status === 410) {
-        return;
-      }
-
-      if (lastResponseData.status !== 200) {
-        console.log('Blocked: ' + lastResponseData.status);
-        if (benchmark) {
-          return;
-        }
-        if (backconnect) {
-          throw Error('Bad response code: ' + lastResponseData.code);
-        }
-        return context.reportBlocked(lastResponseData.status, 'Blocked: ' + lastResponseData.status);
-      }
-
-      if (!await solveCaptchaIfNecessary) {
-        hasCaptcha = true;
-        return;
-      }
-
-      if (lastResponseData.status === 404 || lastResponseData.status === 410) {
-
-      }
-    };
-
+      },
+    });
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
+    let captchaFrame = "iframe[_src*='captcha']:not([title]), iframe[src*='captcha']:not([title]), div.g-recaptcha";
     try {
-      await run();
-    } finally {
-    // needs to be non-fat arrow
-      await context.evaluate((captchaCount, duration, js, hasCaptcha) => {
-        const captchasElt = document.createElement('meta');
-        captchasElt.name = 'captchas';
-        captchasElt.content = captchaCount;
-        document.head.appendChild(captchasElt);
-        const hasCaptchaElt = document.createElement('meta');
-        hasCaptchaElt.name = 'hasCaptcha';
-        hasCaptchaElt.content = hasCaptcha;
-        document.head.appendChild(hasCaptchaElt);
-        const timeElt = document.createElement('meta');
-        timeElt.name = 'durationmillis';
-        timeElt.content = duration;
-        document.head.appendChild(timeElt);
-        const javascriptElt = document.createElement('meta');
-        javascriptElt.name = 'javascript';
-        javascriptElt.content = js;
-        document.head.appendChild(javascriptElt);
-        // js_enabled
-      }, [captchas, Date.now() - start, hasCaptcha]);
+      await context.waitForSelector(captchaFrame);
+    } catch (error) {
+      console.log('error: without undescore ', error);
+    }
+
+    console.log('captchaFrame', captchaFrame);
+    const checkExistance = async (selector) => {
+      return await context.evaluate(async (captchaSelector) => {
+        return Boolean(document.querySelector(captchaSelector));
+      }, selector);
+    };
+    const isCaptchaFramePresent = await checkExistance(captchaFrame);
+    // const isCaptchaFramePresent = true;
+    if (isCaptchaFramePresent) {
+      console.log('isCaptcha', true);
+      await context.waitForNavigation({ timeout });
+      // @ts-ignore
+      // eslint-disable-next-line no-undef
+      await context.evaluateInFrame('iframe', () => grecaptcha.execute());
+      console.log('solved captcha, waiting for page change');
+      await context.waitForNavigation({ timeout });
+      await context.waitForXPath('//div[@id="product-detail-page"]', { timeout });
+      try {
+        let cookieButton = document.querySelector('button#footer_tc_privacy_button');
+        if (cookieButton) {
+          // @ts-ignore
+          cookieButton.click();
+        }
+      } catch (error) {
+        console.log('error: ', error);
+
+      }
+    }
+    //   if (responseStatus.url) {
+    //     console.log('responseStatus.url: ', responseStatus.url);
+    //   return context.reportBlocked(responseStatus.url, 'Blocked: ' + responseStatus.url);
+    // }
+    try {
+      let cookieButton = document.querySelector('button#footer_tc_privacy_button');
+      if (cookieButton) {
+        // @ts-ignore
+        cookieButton.click();
+      }
+    } catch (error) {
+      console.log('error: ', error);
+
     }
   },
 };
