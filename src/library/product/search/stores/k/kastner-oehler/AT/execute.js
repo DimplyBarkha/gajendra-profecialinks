bbac8cdf69c4712c6e496d16639bd3af6bf0589f
Rafal Perkowski <rafalpero1@gmail.com>
2020-09-30 13:16:41 +0200

    updated url in execute.js and tests

--- src/library/product/search/stores/k/kastner-oehler/AT/execute.js
+++ src/library/product/search/stores/k/kastner-oehler/AT/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'AT',
     store: 'kastner-oehler',
     domain: 'kastner-oehler.at',
-    url: 'https://www.kastner-oehler.at/shop-suche/suche-%22{searchTerms}%22/',
+    url: 'www.kastner-oehler.at/index.php?searchparam={searchTerms}&lang=0&cl=search',
     loadedSelector: 'div.site_inner',
     noResultsXPath: '//div[@class="en_grid"]//h3[contains(text(),"Keine Treffer!")]',
     zipcode: '',
