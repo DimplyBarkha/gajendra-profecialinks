e31ed48588667cd0567489d7a6ad0a31ab3cabf4
PradyumnaD29 <pradyumna.deshpande@synerzip.com>
2020-10-01 14:41:30 +0530

    #423 added captcha solver

--- src/library/navigation/goto/domains/da/darty.com.js
+++ src/library/navigation/goto/domains/da/darty.com.js
@@ -8,4 +8,66 @@ module.exports = {
     store: 'darty',
     zipcode: '',
   },
+  implementation: async (
+    { url, zipcode, storeId },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 60000;
+    await context.setJavaScriptEnabled(true);
+    await context.setCssEnabled(true);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setBlockAds(false);
+
+    const responseStatus = await context.goto(url, {
+      antiCaptchaOptions: {
+        provider: '2-captcha',
+        type: 'GEETEST',
+      },
+      firstRequestTimeout: 60000,
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+    });
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
+
+    await context.waitForNavigation({ timeout: 30000 });
+
+    try {
+      await context.evaluateInFrame('iframe',
+        function () {
+          const code = geetest
+            .toString()
+            .replace(
+              /appendTo\("#([^"]+)"\)/,
+              'appendTo(document.getElementById("$1"))',
+            );
+          return eval(`(${code})('/captcha/geetest');`);
+        },
+      );
+
+      await new Promise(resolve => setTimeout(resolve, 500));
+      await context.evaluateInFrame('iframe',
+        function () {
+          document.querySelector('.captcha-handler').click();
+        },
+      );
+      await new Promise(resolve => setTimeout(resolve, 60000));
+      try {
+        await context.waitForSelector('#produit > div.product_head');
+      } catch (e) {
+        console.log('No details page');
+      }
+      try {
+        await context.waitForSelector('div.product_detail');
+      } catch (e) {
+        console.log('No details page');
+      }
+    } catch (e) {
+      console.log('No captcha encountered');
+    }
+  },
 };
