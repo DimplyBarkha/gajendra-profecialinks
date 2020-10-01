2032f807e915d9955043b0bcb11643f9c3f1ae29
abhay.patil <abhay.patil@kcsitglobal.com>
2020-10-01 10:49:05 +0530

    resolve issues

--- src/library/product/details/stores/k/kogan/AU/transform.js
+++ src/library/product/details/stores/k/kogan/AU/transform.js
@@ -5,6 +5,23 @@
  */
 const transform = (data, context) => {
 
+      // Default transform function
+      const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+
     for (const { group } of data) {
       for (const row of group) {  
         try { 
@@ -32,21 +49,58 @@ const transform = (data, context) => {
 
         if (row.sku) {          
           let tempSku=row.sku[0].text.split(':')
-          row.sku = [{ text: tempSku[1].replace('\\u0022','').replace('\\u00','').replace('\\u0022','').replace('\\u00','').replace(',','').trim() }];
+          tempSku[1] = tempSku[1].substring(16);          
+          row.sku = [{ text: tempSku[1].replace(/\\u002D/g,'-').replace(/\\u0022/g,'').replace(',','').trim() }];
         }
 
          if (row.variantId) {          
           let tempSku=row.variantId[0].text.split(':')
-          row.variantId = [{ text: tempSku[1].replace('\\u0022','').replace('\\u00','').replace('\\u0022','').replace('\\u00','').replace(',','').trim() }];
+          tempSku[1] = tempSku[1].substring(16);
+          row.variantId = [{ text: tempSku[1].replace(/\\u002D/g,'-').replace(/\\u0022/g,'').replace(',','').trim() }];
         }
 
-        if (row.description) {
-          let text = '';
-          row.description.forEach(item => {
-            text = row.description.map(elm => elm.text).join(' ').replace(/\n/g, '||');
-          });
-          row.description = [{ text }];
+
+        if (row.brandText1) {                    
+          row.brandText = [{ text: row.brandText1[0].text.trim() }];
         }
+
+        if (row.warranty1) {                    
+          row.warranty = [{ text: row.warranty1[0].text.trim() + ' Year' }];
+        }
+
+
+        if (row.brandText) {          
+          let tempbrandText=row.brandText[0].text.split(':')          
+          row.brandText = [{ text: tempbrandText[1].replace(/\\u002D/g,'-').replace(/\\u0022/g,'').replace(',','').trim() }];
+        }
+
+        
+        let newText1 = '';
+        if (row.additionalDescBulletInfo) {
+              let newText = '';
+              let itemp=1;				
+              row.additionalDescBulletInfo.forEach(item => {	
+              if(itemp===row.additionalDescBulletInfo.length)
+              {
+                item.text = item.text;
+              }								
+              else{
+                item.text = item.text + ' || ';
+              }	
+              newText += `${item.text.replace(/\n|&dash;|\r/g, ' || ')}`;
+              itemp = itemp+ 1;
+              });				  
+            newText1 = newText.trim();        
+            row.additionalDescBulletInfo = [{ text: newText.trim() }];
+          }
+
+          if (row.description) {
+            let newText = '';
+            row.description.forEach(item => {
+              newText += row.description.map(elm => elm.text).join(' ').replace(/\n/g, '||');
+            });
+            row.description = [{ text: newText.trim() + newText1.trim() }];
+          }
 
         } catch (exception) { console.log('Error in transform', exception); }       
                  
