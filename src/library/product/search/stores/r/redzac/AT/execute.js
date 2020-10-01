872ecc26fbe4c04197dc7793269a762bce38d41d
Rafal Perkowski <rafalpero1@gmail.com>
2020-10-01 12:05:53 +0200

    search redzac commit

--- src/library/product/search/stores/r/redzac/AT/execute.js
+++ src/library/product/search/stores/r/redzac/AT/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'AT',
     store: 'redzac',
     domain: 'redzac.at',
-    url: 'https://www.redzac.at/suche/Produkt-Uebersicht?search_term__value__i=&search_term={searchTerms}',
+    url: 'https://www.redzac.at/suche/Produkt-Uebersicht?search_term__value__i=&search_term="{searchTerms}"',
     loadedSelector: 'div.product_search_result_wrapper',
     noResultsXPath: 'div.product_search_noresult_area',
     zipcode: '',
--- src/library/product/search/stores/r/redzac/AT/extract.js
+++ src/library/product/search/stores/r/redzac/AT/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'AT',
     store: 'redzac',
-    transform: null,
+    transform: transform,
     domain: 'redzac.at',
     zipcode: '',
   },
@@ -19,15 +20,36 @@ module.exports = {
           });
         }
         const scrollSelector = document.querySelector('div.infinite_scroll__activity_container');
-        do {
-          scrollSelector.scrollIntoView();
-          await stall(1000);
+        if (scrollSelector) {
+          do {
+            scrollSelector.scrollIntoView();
+            await stall(1000);
+          }
+          // @ts-ignore
+          while (scrollSelector.style.cssText === '');
         }
-        // @ts-ignore
-        while (scrollSelector.style.cssText === '');
       });
     };
     await applyScroll(context);
+    await context.evaluate(async function () {
+      const allProducts = document.querySelectorAll('li.product_cell');
+      let x;
+      for (x = 0; allProducts.length - 1 >= x; x++) {
+        allProducts[x].setAttribute('count', `${x + 1}`);
+      }
+      const allProductsReviews = document.querySelectorAll('div.bv-off-screen');
+      let y;
+      for (y = 0; allProductsReviews.length > y; y++) {
+        const newAggregateRating = allProductsReviews[y].textContent.replace(/\./, ',');
+        allProductsReviews[y].textContent = newAggregateRating;
+      }
+      const allProductsPrice = document.querySelectorAll('div.single_price');
+      let z;
+      for (z = 0; allProductsPrice.length > z; z++) {
+        const newProductsPrice = allProductsPrice[z].textContent.replace(/[,-]\D/, '');
+        allProductsPrice[z].textContent = newProductsPrice;
+      }
+    });
     return await context.extract(productDetails);
   },
 };
