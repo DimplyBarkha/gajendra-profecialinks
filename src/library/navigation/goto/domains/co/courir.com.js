61ebcf44a36355d02b723a741eb3b4dafa559d66
Ankit Kumar <ankit.k@osmosys.asia>
2020-10-01 16:28:57 +0530

    experiments

--- src/library/navigation/goto/domains/co/courir.com.js
+++ src/library/navigation/goto/domains/co/courir.com.js
@@ -3,63 +3,58 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'courir.com',
-    timeout: null,
+    timeout: 200000,
     country: 'FR',
     store: 'courir',
     zipcode: '',
   },
-  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.setBlockAds(false);
+  implementation: async (
+    { url, zipcode, storeId },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 60000;
+    await context.setJavaScriptEnabled(true);
+    await context.setCssEnabled(true);
     await context.setLoadAllResources(true);
     await context.setLoadImages(true);
-    await context.setJavaScriptEnabled(true);
-    await context.setAntiFingerprint(false);
+    await context.setBlockAds(false);
 
     const responseStatus = await context.goto(url, {
-      timeout: 50000,
-      waitUntil: 'load',
-      checkBlocked: true,
       antiCaptchaOptions: {
         provider: '2-captcha',
         type: 'GEETEST',
       },
+      firstRequestTimeout: 60000,
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
     });
     console.log('Status :', responseStatus.status);
     console.log('URL :', responseStatus.url);
-    const captchaFrame = 'iframe[src*="https://geo.captcha"]';
-    // const captchaSelector = '.g-recaptcha';
-    const checkExistance = async (selector) => {
-      return await context.evaluate(async (captchaFrame) => {
-        console.log('captcha present');
-        return Boolean(document.querySelector(captchaFrame));
-      }, selector);
-    };
 
-    const isCaptchaFramePresent = await checkExistance(captchaFrame);
+    await context.waitForNavigation({ timeout: 30000 });
 
-    if (isCaptchaFramePresent) {
-      console.log('isCaptcha', true);
-      // context.evaluateInFrame(captchaFrame, () => grecaptcha.execute());
-
-      await context.evaluateInFrame('iframe', () => {
-        const code = geetest.toString().replace(/appendTo\("#([^"]+)"\)/g, 'appendTo(document.getElementById("$1"))');
-        console.log(code);
+    await context.evaluateInFrame('iframe[src*="https://geo.captcha"]',
+      function () {
+        const code = geetest
+          .toString()
+          .replace(
+            /appendTo\("#([^"]+)"\)/,
+            'appendTo(document.getElementById("$1"))',
+          );
         return eval(`(${code})('/captcha/geetest');`);
-      });
+      },
+    );
 
-      await context.evaluateInFrame('iframe', () => {
-        document.querySelector('.geetest_radar_tip_content').click();
-      });
-
-      // await context.solveCaptcha({
-      //   type: 'GEETEST',
-      //   inputElement: 'div.captcha__human__captcha-container',
-      //   autoSubmit: true,
-      // });
-      console.log('solved captcha, waiting for page change');
-      await new Promise(r => setTimeout(r, 50000));
-      await context.waitForNavigation({ timeout });
-    }
+    await new Promise(resolve => setTimeout(resolve, 500));
+    await context.evaluateInFrame('iframe[src*="https://geo.captcha"]',
+      function () {
+        document.querySelector('div.captcha__human__captcha-container').click();
+      },git 
+    );
+    await new Promise(resolve => setTimeout(resolve, 60000));
+    await context.waitForSelector('#pdpMain');
   },
 };
