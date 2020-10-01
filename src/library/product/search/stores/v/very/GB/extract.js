aba4bfa2b5d84499bcff712476509024ba5b5f8d
Rafal Perkowski <rafalpero1@gmail.com>
2020-10-01 17:00:08 +0200

    Updates

--- src/library/product/search/stores/v/very/GB/extract.js
+++ src/library/product/search/stores/v/very/GB/extract.js
@@ -8,4 +8,37 @@ module.exports = {
     domain: 'very.co.uk',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      const allProducts = document.querySelectorAll('li.product');
+      let x;
+      for (x = 0; allProducts.length - 1 >= x; x++) {
+        allProducts[x].setAttribute('count', `${x + 1}`);
+      }
+      const productRatings = document.querySelectorAll('a.productRating');
+      if (productRatings) {
+        for (let x = 0; productRatings.length > x; x++) {
+          // @ts-ignore
+          const newRating = productRatings[x].title.replace(/\./, ',');
+          // @ts-ignore
+          productRatings[x].title = newRating;
+        }
+      }
+      for (let i = 0; i < allProducts.length; i++) {
+        if(!document.querySelectorAll('li.product')[i].querySelector('div.reviewsScore>div>ul')) {
+          continue;
+        }
+        ratings = document.querySelectorAll('li.product')[i].querySelector('div.reviewsScore>div>ul');
+        value = 0;
+        for (let j = 0; j < 5; j++) {
+          stars = parseInt(ratings.querySelectorAll('li')[j].querySelector('span.starCount').innerText)
+          console.log('stars=', stars)
+          value += stars;
+          console.log('value=', value)
+        }
+        document.querySelectorAll('li.product')[i].setAttribute('ratingCount', `${value}`);
+      }
+    });
+    return await context.extract(productDetails);
+  },
 };
