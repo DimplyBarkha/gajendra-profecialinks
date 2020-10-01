ef7f872b33a08de6b45b569ab76dc5bf28c52969
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-10-01 12:49:15 +0530

    Captcha handling (#420)

--- src/library/navigation/goto/domains/da/darty.com.js
+++ src/library/navigation/goto/domains/da/darty.com.js
@@ -11,25 +11,33 @@ module.exports = {
     { url, zipcode, storeId },
     parameters,
     context,
-    dependencies
+    dependencies,
   ) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    const timeout = parameters.timeout ? parameters.timeout : 60000;
+    await context.setJavaScriptEnabled(true);
+    await context.setCssEnabled(true);
     await context.setLoadAllResources(true);
-    await context.goto(url, {
+    await context.setLoadImages(true);
+    await context.setBlockAds(false);
+
+    const responseStatus = await context.goto(url, {
       antiCaptchaOptions: {
         provider: '2-captcha',
         type: 'GEETEST',
       },
+      firstRequestTimeout: 60000,
       timeout: timeout,
       waitUntil: 'load',
       checkBlocked: false,
-      js_enabled: true,
-      load_timeout: 30,
-      css_enabled: true,
     });
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
 
-    await context.evaluateInFrame('iframe',
+    await context.waitForNavigation({ timeout: 30000 });``
+    try {
+      await context.evaluateInFrame('iframe',
       function () {
+        // @ts-ignore
         const code = geetest
           .toString()
           .replace(
@@ -37,17 +45,21 @@ module.exports = {
             'appendTo(document.getElementById("$1"))',
           );
         return eval(`(${code})('/captcha/geetest');`);
-      }
+      },
     );
 
-    await new Promise((r) => setTimeout(r, 500));
-
+    await new Promise(resolve => setTimeout(resolve, 500));
     await context.evaluateInFrame('iframe',
       function () {
+        // @ts-ignore
         document.querySelector('.captcha-handler').click();
-      }
+      },
     );
-
-    await context.waitForNavigation();
+    await new Promise(resolve => setTimeout(resolve, 60000));
+    await context.waitForSelector('#produit > div.product_head');
+    } catch (error) {
+      console.log('error: NO CPATCHA ENCOUNTER', error);
+      
+    }
   },
 };
