2974ba41dda8c5bdc3e14118cb66ee441264f8e6
Praveen <pravinr@synerzipune.local>
2020-10-02 12:37:40 +0530

    fixed no result xpath

--- src/library/product/search/stores/n/nettoshop/CH/execute.js
+++ src/library/product/search/stores/n/nettoshop/CH/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'nettoshop.ch',
     url: 'https://www.nettoshop.ch/search?text={searchTerms}',
     loadedSelector: 'div .c-product-grid__item-wrapper',
-    noResultsXPath: null,
+    noResultsXPath: '//div[@class="c-search-result ember-view"]/h1[contains(text(),"0 Ergebnisse")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/n/nettoshop/CH/paginate.js
+++ src/library/product/search/stores/n/nettoshop/CH/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null, // 'div.c-product-grid__header-left'
     spinnerSelector: null,
     loadedSelector: 'div.c-product-grid__header-left',
-    noResultsXPath: '//div[@class="c-search-result ember-view"]',
+    noResultsXPath: '//div[@class="c-search-result ember-view"]/h1[contains(text(),"0 Ergebnisse")]',
     openSearchDefinition: null,
     domain: 'nettoshop.ch',
     zipcode: '',
