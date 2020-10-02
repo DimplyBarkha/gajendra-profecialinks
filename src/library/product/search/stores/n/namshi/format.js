40db8c9bd5e6eaf3a698b8056988abf123cd6d45
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-10-02 23:34:24 +0530

    Updated cleanup code

--- src/library/product/search/stores/n/namshi/format.js
+++ src/library/product/search/stores/n/namshi/format.js
@@ -5,6 +5,24 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
   for (const { group } of data) {
     let rank = 1;
     for (const row of group) {
@@ -22,11 +40,12 @@ const transform = (data) => {
         row.nameExtended.forEach(item => {
           item.text = item.text + " - " + row.name[0]["text"];
         });
-      }      
+      }
       row.rank = [{ "text": rank }];
       rank++;
     }
   }
+  cleanUp(data);
   return data;
 };
 
