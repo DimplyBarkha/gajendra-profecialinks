9ae695a4145858547692662fe26fcf8d3c3d1c8d
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-10-03 01:15:10 +0530

    Updated code

--- src/library/product/details/stores/k/komputronik/PL/execute.js
+++ src/library/product/details/stores/k/komputronik/PL/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'komputronik',
     domain: 'komputronik.pl',
     loadedSelector: null,
-    noResultsXPath: null,
+    noResultsXPath: '//h2[contains(text(),"404")]',
     zipcode: "''",
   },
 };
