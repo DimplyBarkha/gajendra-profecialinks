37963d786f083265d968ad81c3a599f682037f31
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-10-03 00:30:08 +0530

    Apply cleanup code

--- src/library/product/search/stores/t/trademe/format.js
+++ src/library/product/search/stores/t/trademe/format.js
@@ -4,59 +4,70 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-      let rank = 1;
-      for (const row of group) {
-        if (row.productUrl) {
-          row.productUrl.forEach(item => {
-            item.text = "https://www.trademe.co.nz" + item.text;
-            item.text = item.text.toString();
-          });
-        }
-        if (row.thumbnail) {
-          row.thumbnail.forEach(item => {
-            item.text = item.text.match(/\(([^)]+)\)/)[1].slice(1,-1);
-            item.text = item.text.toString();
-          });
-        }
-        if (row.brandText) {
-          row.brandText.forEach(item => {
-            let itemmatch = item.text.match(/Dyson/);
-            if (itemmatch) {
-              item.text = 'Dyson';
-            } else {
-              delete row.brandText;
-            }
-          });
-        }
-        if (row.variantId) {
-          row.variantId.forEach(item => {
-              item.text = item.text.replace(/\?.*$/g, '').trim();
-              item.text = item.text.replace(/\D/g, ' ').trim();
-              item.text = item.text.replace(/\s/g, '').trim();
-              item.text = item.text.toString();
-            });
-        }
-        if (row.price) {
-          row.price.forEach(item => {
-              item.text = item.text;
-          });
-        }
-        else {
-          row.price = row.listprice ;
-        }
-       if (row.id) {
-        row.id.forEach(item => {
-            item.text = item.text.replace(/\?.*$/g, '').trim();
-            item.text = item.text.replace(/\D/g, '').trim();
-            item.text = item.text.replace(/\s/g, '').trim();
-            item.text = item.text.toString();
-          });
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
+  for (const { group } of data) {
+    let rank = 1;
+    for (const row of group) {
+      if (row.productUrl) {
+        row.productUrl.forEach(item => {
+          item.text = "https://www.trademe.co.nz" + item.text;
+          item.text = item.text.toString();
+        });
       }
-      row.rank = row.rankOrganic = [{"text":rank}];
-      rank++;      
-    } 
+      if (row.thumbnail) {
+        row.thumbnail.forEach(item => {
+          item.text = item.text.match(/\(([^)]+)\)/)[1].slice(1, -1);
+          item.text = item.text.toString();
+        });
+      }
+      if (row.brandText) {
+        row.brandText.forEach(item => {
+          let itemmatch = item.text.match(/Dyson/);
+          if (itemmatch) {
+            item.text = 'Dyson';
+          } else {
+            delete row.brandText;
+          }
+        });
+      }
+      if (row.variantId) {
+        row.variantId.forEach(item => {
+          item.text = item.text.replace(/\?.*$/g, '').trim();
+          item.text = item.text.replace(/\D/g, ' ').trim();
+          item.text = item.text.replace(/\s/g, '').trim();
+          item.text = item.text.toString();
+        });
+      }
+      if (row.id) {
+        row.id.forEach(item => {
+          item.text = item.text.replace(/\?.*$/g, '').trim();
+          item.text = item.text.replace(/\D/g, '').trim();
+          item.text = item.text.replace(/\s/g, '').trim();
+          item.text = item.text.toString();
+        });
+      }
+      row.rank = row.rankOrganic = [{ "text": rank }];
+      rank++;
+    }
   }
+  cleanUp(data);
   return data;
 };
 module.exports = { transform };
