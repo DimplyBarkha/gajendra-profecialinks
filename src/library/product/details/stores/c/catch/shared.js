12f53c606d3dd67db271d4ee29e932689eb0dccf
KCSPL\jatin <jatin@kcspl.co.in>
2020-10-02 13:46:25 +0530

    QA2 feedback points

--- src/library/product/details/stores/c/catch/shared.js
+++ src/library/product/details/stores/c/catch/shared.js
@@ -5,22 +5,6 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-  // Default transform function
-  const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-    el.text = clean(el.text);
-  }))));
 
   for (const { group } of data) {
     for (const row of group) {
@@ -77,8 +61,39 @@ const transform = (data) => {
           row.variantId[0].text = "";
         }
       }
+
+      if (row.shippingInfo) {
+        let data = row.shippingInfo[0].text.split("\n");
+        if (data[1]) {
+          row.shippingInfo[0].text = data[1];
+        }
+      }
+      if (row.videos) {
+        let text = '';
+        row.videos.forEach(item => {
+          text = 'https://www.youtube.com/watch?v=' + item.text;
+        });
+        row.videos = [{ text }];
+      }
     }
   }
+
+  // Default transform function
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
   return data;
 };
 
