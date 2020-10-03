8fdbe5a9d298a3119d4cef259e3ca479135d279f
abhay.patil <abhay.patil@kcsitglobal.com>
2020-10-03 09:11:00 +0530

    add clean up files

--- src/library/product/details/stores/e/ek-onlineshop/shared.js
+++ src/library/product/details/stores/e/ek-onlineshop/shared.js
@@ -5,6 +5,20 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+	
+	const clean = text => text.toString()
+		.replace(/\r\n|\r|\n/g, ' ')
+		.replace(/&amp;nbsp;/g, ' ')
+		.replace(/&amp;#160/g, ' ')
+		.replace(/\u00A0/g, ' ')
+		.replace(/\s{2,}/g, ' ')
+		.replace(/"\s{1,}/g, '"')
+		.replace(/\s{1,}"/g, '"')
+		.replace(/^ +| +$|( )+/g, ' ')
+		// eslint-disable-next-line no-control-regex
+		.replace(/[\x00-\x1F]/g, '')
+		.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
 	for (const { group } of data) {
 		for (const row of group) {
 			if (row.Image360Present) {				
@@ -134,18 +148,7 @@ const transform = (data) => {
 			  }
 		}
 	}
-	const clean = text => text.toString()
-		.replace(/\r\n|\r|\n/g, ' ')
-		.replace(/&amp;nbsp;/g, ' ')
-		.replace(/&amp;#160/g, ' ')
-		.replace(/\u00A0/g, ' ')
-		.replace(/\s{2,}/g, ' ')
-		.replace(/"\s{1,}/g, '"')
-		.replace(/\s{1,}"/g, '"')
-		.replace(/^ +| +$|( )+/g, ' ')
-		// eslint-disable-next-line no-control-regex
-		.replace(/[\x00-\x1F]/g, '')
-		.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+	
 
 	data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
 		el.text = clean(el.text);
