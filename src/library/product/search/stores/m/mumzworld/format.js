19c753ae299daffdb7dc2e0d95c67fd51dda49e0
unknown <ganesh.working@gmail.com>
2020-10-04 17:14:09 +0530

    add cleanup code

--- src/library/product/search/stores/m/mumzworld/format.js
+++ src/library/product/search/stores/m/mumzworld/format.js
@@ -4,9 +4,27 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+    const cleanUp = (data, context) => {
+      const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }))));
+      return data;
+    };
     var p_count = 1
     for (const { group } of data) {
-        for (const row of group) {          
+        for (let row of group) {          
             if (row.rankOrganic && row.rank) {
               row.rankOrganic = [{'text':p_count}];
               row.rank = [{'text':p_count}];
@@ -14,7 +32,7 @@ const transform = (data) => {
             }          
         }
     }
-    return data;
+    return cleanUp(data);
   };
   
   module.exports = { transform };
