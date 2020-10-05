550bd7adf3d07d5d74f34c64fbbf81829b0ccc9e
judhisahoo2009 <judhisahoo@hotmail.com>
2020-10-05 11:00:29 +0530

    updating

--- src/library/product/details/stores/n/noon/SA/createUrl.js
+++ src/library/product/details/stores/n/noon/SA/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'noon.com',
     prefix: null,
-    url: null,
+    url: 'https://www.noon.com/saudi-en/{id}/p',
     country: 'SA',
     store: 'noon',
     zipcode: '',
