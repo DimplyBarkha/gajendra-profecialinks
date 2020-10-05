cc657ca771be842cdece5e556efd4d00a39a57b7
Ankit Kumar <ankit.k@osmosys.asia>
2020-10-05 13:48:14 +0530

    fixed issue with price

--- src/library/product/search/stores/p/pixmania/ES/execute.js
+++ src/library/product/search/stores/p/pixmania/ES/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'pixmania',
     domain: 'pixmania.es',
     url: 'https://www.pixmania.es/s?q={searchTerms}',
-    loadedXPath: '//div[@product-card="product"]',
+    loadedXPath: '//div[@itemprop="image"]',
     noResultsXPath: '//nav[contains(@class,"catalog-products-nav")]//span[contains(text(),"0 artículos")]',
   },
 };
