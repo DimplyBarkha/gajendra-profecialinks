a0687bd27a5f519d8eaf513ffa6570610eb5cdca
sirisha <sirisha.thotla-odc@partner.import.io>
2020-10-05 22:27:56 +0530

    prefix added for url

--- src/library/product/details/stores/l/littlewoods/UK/createUrl.js
+++ src/library/product/details/stores/l/littlewoods/UK/createUrl.js
@@ -4,41 +4,10 @@ module.exports = {
   parameterValues: {
     domain: 'littlewoods.com',
     prefix: null,
-    suffix: null,
+    suffix: '.prd',
     url: 'https://www.littlewoods.com/{id}.prd',
-ull,
-try: 'UK',
+    country: 'UK',
     store: 'littlewoods',
     zipcode: '',
   },
 };
-/**
- *
- * @param { { id: any } } inputs
- * @param { { domain: string, prefix?: string, suffix?: string, url?: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { } } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { id } = inputs;
-  const { domain, prefix, suffix } = parameters;
-
-  if (parameters.url) {
-    const url = parameters.url.replace('{id}', encodeURIComponent(id));
-    return url;
-  }
-  let gotoUrl = `https://${domain}`;
-  if (prefix) {
-    gotoUrl += `/${prefix}`;
-  }
-  gotoUrl += `/${id}`;
-  if (suffix) {
-    gotoUrl += `/${suffix}`;
-  }
-  return gotoUrl;
-}
