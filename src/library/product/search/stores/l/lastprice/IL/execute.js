44a40e12bb769df88f6e6d9b95530611d11986bd
Heramba Rao Boddu <herambarao.boddu@hcl.com>
2020-10-05 17:45:07 +0530

    search file updated

--- src/library/product/search/stores/l/lastprice/IL/execute.js
+++ src/library/product/search/stores/l/lastprice/IL/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'lastprice',
     domain: 'lastprice.co.il',
     url: 'https://www.lastprice.co.il/category.asp?q={searchTerms}',
-    loadedSelector: 'div.price > div > div.sm > font > font',
-    noResultsXPath: '//h4//span[contains(text(),"לא נמצאו מוצרים")]',
+    loadedSelector: null,
+    noResultsXPath: null,
     zipcode: '',
   },
 };
