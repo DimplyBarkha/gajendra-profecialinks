a407d315f3c99fb362176b9b06ad12b509a5b1e1
sirisha <sirisha.thotla-odc@partner.import.io>
2020-10-05 19:58:43 +0530

    id code added in createurl.js

--- src/library/product/details/stores/l/littlewoods/UK/createUrl.js
+++ src/library/product/details/stores/l/littlewoods/UK/createUrl.js
@@ -11,3 +11,33 @@ module.exports = {
     zipcode: '',
   },
 };
+/**
+ *
+ * @param { { id: any } } inputs
+ * @param { { domain: string, prefix?: string, suffix?: string, url?: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { } } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { id } = inputs;
+  const { domain, prefix, suffix } = parameters;
+
+  if (parameters.url) {
+    const url = parameters.url.replace('{id}', encodeURIComponent(id));
+    return url;
+  }
+  let gotoUrl = `https://${domain}`;
+  if (prefix) {
+    gotoUrl += `/${prefix}`;
+  }
+  gotoUrl += `/${id}`;
+  if (suffix) {
+    gotoUrl += `/${suffix}`;
+  }
+  return gotoUrl;
+}
