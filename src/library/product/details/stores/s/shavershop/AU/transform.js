a87d7ec325fb80f996f69d0357c4e67f8463df4f
GirishSolanki <girish.solanki@kcsitglobal.com>
2020-10-05 11:41:40 +0530

    pushed changes to dev branch

--- src/library/product/details/stores/s/shavershop/AU/transform.js
+++ src/library/product/details/stores/s/shavershop/AU/transform.js
@@ -12,25 +12,17 @@ const transform = (data, context) => {
          
           if (row.Image360Present) {
             row.Image360Present = [{ text: row.Image360Present[0].text == 'true' ? 'YES' : 'NO' }];
-          }      
+          }   
           
-          // if (row.variantId) {
-          //   var variantId = row.variantId[0].text.toString();
-          //   var length = variantId.split("-").length;
-          //   variantId = variantId.split("-")[length-1];
-          //   console.log('variant:'+variantId);
-          //   row.variantId = [
-          //     {
-          //       text: variantId.replace('.html',''),
-          //     },
-          //   ];
-          // } 
-
+          if (row.manufacturerImages) {
+            row.manufacturerImages = [{ text: "https://www.shavershop.com.au"+row.manufacturerImages[0].text  }];
+          }  
           if (row.manufacturerDescription) {
             let text = '';
-            row.manufacturerDescription.forEach(item => {
-              text += item.text.replace(/\s{2,}/g, ' ').replace(/\n/g, ' ').trim();
+            row.manufacturerDescription.forEach(item => {              
+                text += item.text.replace(/\s{2,}/g, ' ').replace(/\n/g, ' ').trim();             
             });
+            
             row.manufacturerDescription = [
               {
                 text: text,
