6b4dc61c68a636d8f4ecf1ca50f62be824dcf5c4
sirisha <sirisha.thotla-odc@partner.import.io>
2020-10-06 00:15:31 +0530

    search extarct.yaml file changed

--- src/library/product/search/stores/l/littlewoods/UK/paginate.js
+++ src/library/product/search/stores/l/littlewoods/UK/paginate.js
@@ -4,10 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'UK',
     store: 'littlewoods',
-    nextLinkSelector: null,
+    nextLinkSelector: '#pagnNextString, #pagnNextLink, ul li a.paginationNext:not(.a-disabled)',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'div.productImages a.productMainImage',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'littlewoods.com',
