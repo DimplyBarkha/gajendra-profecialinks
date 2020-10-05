f5bd8ffd69e9af989f855881ac9a5197e7baf48d
Ankit Kumar <ankit.k@osmosys.asia>
2020-10-06 02:14:46 +0530

    added fields and wait for page to load properly

--- src/library/product/details/stores/p/pixmania/ES/extract.js
+++ src/library/product/details/stores/p/pixmania/ES/extract.js
@@ -1,10 +1,17 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'ES',
     store: 'pixmania',
-    transform: null,
+    transform: transform,
     domain: 'pixmania.es',
+  },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      await new Promise(r => setTimeout(r, 10000));
+    });
+    return await context.extract(productDetails);
   },
 };
