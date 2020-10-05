f854f47e8cfb01f9baf2be29ecb9c7c835688b2a
Tomasz <tomekwitoslaw@gmail.com>
2020-10-05 18:24:13 +0200

    breuninger_DE core QA2 state

--- src/library/product/details/stores/b/breuninger/DE/extract.js
+++ src/library/product/details/stores/b/breuninger/DE/extract.js
@@ -31,12 +31,21 @@ module.exports = {
     await new Promise((resolve, reject) => setTimeout(resolve, 2000));
     // manually extracting description
     const description = await context.evaluate(async function () {
-      const descParts = document.querySelectorAll('div>div[class="bewerten-textformat"]>p');
+      const descParts = document.querySelectorAll('div>div[class="bewerten-textformat"]>*');
       var description = '';
       for (let i = 0; i < descParts.length; i++) {
         description += descParts[i].innerText;
       }
       return description;
+    });
+    // manually extracting additional description bullet info
+    const additionalDescBulletInfo = await context.evaluate(async function () {
+      const bulletInfoParts = document.querySelectorAll('div>div[class="bewerten-textformat"]>ul>li');
+      var additionalDescBulletInfo = '';
+      for (let i = 0; i < bulletInfoParts.length; i++) {
+        additionalDescBulletInfo += `|| ${bulletInfoParts[i].innerText} `;
+      }
+      return additionalDescBulletInfo;
     });
     // manually extracting directions
     const directions = await context.evaluate(async function () {
@@ -50,8 +59,13 @@ module.exports = {
     // manually extracting shippingInfo
     const shippingInfo = await context.evaluate(async function () {
       document.querySelector('div[class*="desktop"]>h2+h2').click();
-      await new Promise((resolve, reject) => setTimeout(resolve, 100));
-      return document.querySelector('div[class*="tab-content"]>section').innerText;
+      await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+      var shippingInfo = "";
+      while (!(shippingInfo.includes('Versandoptionen'))){
+        await new Promise((resolve, reject) => setTimeout(resolve, 100));
+        shippingInfo = document.querySelector('div[class*="tab-content"]>section').innerText;
+      }
+      return shippingInfo;
     });
     // manually extracting json stored data
     const jsonData = await context.evaluate(async function () {
@@ -59,6 +73,9 @@ module.exports = {
     });
     // manually extracting iframe url and navigating to it
     const gotoIframe = await context.evaluate(async function () {
+      if (!(document.querySelector('iframe[id=loadbeeIframeId]'))) {
+        await new Promise((resolve, reject) => setTimeout(resolve, 100));
+      }
       return document.querySelector('iframe[id=loadbeeIframeId]').src;
     });
     await context.goto(gotoIframe);
@@ -83,23 +100,26 @@ module.exports = {
       return [description, images];
     });
     // manually extracting videos
-    const videos = await context.evaluate(async function () {
+    var videos = await context.evaluate(async function () {
       var videos = [];
       const rawVideos = document.querySelectorAll('div[data-video]');
       for (let i = 0; i < rawVideos.length; i++) {
         videos.push(rawVideos[i].getAttribute('data-video'));
       }
-      videos = [...new Set(videos)];
       return videos;
     });
-    // manually extracting specifications
-    const specifications = await context.evaluate(async function () {
+    // manually extracting iframe data (specifications, weightNet)
+    const iframeData = await context.evaluate(async function () {
       var specifications = '';
+      var weightNet = '';
       const rawSpecifications = document.querySelectorAll('div[class*="info"]>div[class*="info"]');
       for (let i = 0; i < rawSpecifications.length; i++) {
+        if (rawSpecifications[i].querySelector('h5').innerText == 'Gewicht') {
+          weightNet = rawSpecifications[i].querySelector('p').innerText;
+        }
         specifications += rawSpecifications[i].innerText;
       }
-      return specifications;
+      return {specifications: specifications, weightNet: weightNet};
     });
     // inserting manually extracted data to output data ref
     var dataRef = await context.data();
@@ -112,19 +132,27 @@ module.exports = {
     }
     dataRef[0].data[0].group[0].directions[0].text += '\n';
     dataRef[0].data[0].group[0].directions[0].text += directions;
-    dataRef[0].data[0].group[0].specifications[0].text = specifications;
+    dataRef[0].data[0].group[0].specifications[0].text = iframeData.specifications;
+    dataRef[0].data[0].group[0].weightNet = [{ text: iframeData.weightNet }];
     dataRef[0].data[0].group[0].manufacturerDescription[0].text = manufacturerDescImg[0];
     dataRef[0].data[0].group[0].manufacturerImages = [];
     for (let i = 0; i < manufacturerDescImg[1].length; i++) {
       dataRef[0].data[0].group[0].manufacturerImages.push({ text: manufacturerDescImg[1][i] });
     }
-    if (!('videos' in dataRef[0].data[0].group[0])) {
-      dataRef[0].data[0].group[0].videos = [];
+    if ('videos' in dataRef[0].data[0].group[0]) {
+      for (let i = 0; i < dataRef[0].data[0].group[0].videos.length; i++) {
+        videos.push(dataRef[0].data[0].group[0].videos[i].text)
+      }
     }
+    console.log(videos);
+    videos = [...new Set(videos)];
+    dataRef[0].data[0].group[0].videos = [];
+    console.log(videos);
     for (let i = 0; i < videos.length; i++) {
       dataRef[0].data[0].group[0].videos.push({ text: videos[i] });
     }
     dataRef[0].data[0].group[0].description[0].text = description;
+    dataRef[0].data[0].group[0].additionalDescBulletInfo = [{ text: additionalDescBulletInfo }];
     dataRef[0].data[0].group[0].shippingInfo[0].text = shippingInfo;
     dataRef[0].data[0].group[0].sku[0].text = jsonData.artikelKey;
     if (dataRef[0].data[0].group[0].alternateImages.length + 1 === jsonData.artikel[jsonData.artikelKey].ansichten.length) {
