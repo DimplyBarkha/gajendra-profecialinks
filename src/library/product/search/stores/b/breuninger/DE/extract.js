db79c02e2c507276c286a4ef01a16f0b3ebe691c
Tomasz <tomekwitoslaw@gmail.com>
2020-10-05 19:23:04 +0200

    breuninger_DE QA2 state

--- src/library/product/search/stores/b/breuninger/DE/extract.js
+++ src/library/product/search/stores/b/breuninger/DE/extract.js
@@ -8,4 +8,14 @@ module.exports = {
     domain: 'breuninger.de',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    // extracting data automatically
+    await context.extract(productDetails);
+    // inserting data manually
+    var dataRef = await context.data();
+    for (let i = 0; i < dataRef[0].data[0].group.length; i++) {
+      dataRef[0].data[0].group[i].rank[0].text = (parseInt(dataRef[0].data[0].group[i].rank[0].text) + 1).toString();
+      dataRef[0].data[0].group[i].rankOrganic[0].text = (parseInt(dataRef[0].data[0].group[i].rankOrganic[0].text) + 1).toString();
+    }
+  },
 };
