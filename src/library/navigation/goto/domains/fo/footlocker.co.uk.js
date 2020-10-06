a9192beb17398bbf2df39d88859b6041e2501928
Vivek Gondil <gondilvivek@gmail.com>
2020-10-06 15:21:34 +0530

    Resolved (second)  PR comment

--- src/library/navigation/goto/domains/fo/footlocker.co.uk.js
+++ src/library/navigation/goto/domains/fo/footlocker.co.uk.js
@@ -7,6 +7,7 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    await context.goto((url + '#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]'), { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
   },
 };
