553f215063c9855895ee6b55062f473c24b92ced
Prasanna Barate <prasanna.barate@synerzip.com>
2020-10-06 14:33:25 +0530

    Updated ebay UK search url and expaths.

--- src/library/product/search/stores/e/ebay/UK/execute.js
+++ src/library/product/search/stores/e/ebay/UK/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'UK',
     store: 'ebay',
     domain: 'ebay.co.uk',
-    url: 'https://www.ebay.co.uk/sch/i.html?_nkw={searchTerms}&_sacat=0&_ipg=200',
-    loadedSelector: 'ul.srp-results > li[data-view]',
-    noResultsXPath: '//h3[contains(@class,"srp-save-null-search__heading")]',
+    url: 'http://www.ebaystores.co.uk/Dyson-Outlet/_i.html?_nkw={searchTerms}&',
+    loadedSelector: 'div[class=tpgv]',
+    noResultsXPath: '//div[contains(@class, "tprs") and not(descendant::div[contains(@class, "tpgv")])]',
     zipcode: '',
   },
 };
