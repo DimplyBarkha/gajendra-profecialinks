5a7eb50b03c935d011ae9388d4fdd9b1842ee684
Vipin Kumar <vipinkumar.mangalam@hcl.com>
2020-10-06 10:54:35 +0530

    QA2 Fixed

--- src/library/product/details/stores/s/stakelums/IE/extract.js
+++ src/library/product/details/stores/s/stakelums/IE/extract.js
@@ -15,7 +15,7 @@ module.exports = {
     dependencies,
   ) {
     const { productDetails } = dependencies;
-  
+      
     await context.evaluate(async function () {
 
       const getAllXpath = (xpath, prop) => {
@@ -92,151 +92,118 @@ module.exports = {
         addHiddenDiv('aggregate_rating',ratingCount);
       }
 
+      addHiddenDiv('added_variantCount', 0);
+
       const description_xpath="//div[@class='product_review']/following-sibling::p";
       let description = getXpath(description_xpath, 'innerText');
       const additional_description_xpath = "//div[@id='specificationTab']/p";
       let additional_description = getAllXpath(additional_description_xpath, 'innerText').join('|');
-      
-      const weight_xpath = '//div[@id="specificationTab"]//table/tbody/tr[2]/td[2]/p[text()]';
-      let weight_xpath_str = getXpath(weight_xpath, 'innerText');
 
-      let weight_xpath_other = "//div[@id='specificationTab']//p[@class='product-specificationlabel-top'][5]";
-      let weight_xpath_other_str = getXpath(weight_xpath_other, 'innerText');
-
-      addHiddenDiv('added_variantCount', 0);
-      console.log(weight_xpath_other_str);
-
-
-      if(weight_xpath_other_str!=null && weight_xpath_other_str!="" )
-      {
-
-        let weightData= weight_xpath_other_str.toString().trim().split(" ");
-        addHiddenDiv('product_weight',weightData[2]+" Kg");
-        
-        const manufacturerDescription_xpath = "//div[contains(@class,'flix-d-p')]";
-        const manufacturerDescription_str = getAllXpath(manufacturerDescription_xpath, 'innerText');
-        addHiddenDiv('added_manufacturerDescription', manufacturerDescription_str);
-
-        let specifications_xpath = "//div[@id='specificationTab']/p";
-        let specifications_str = getAllXpath(specifications_xpath, 'innerText').join('||');
-        addHiddenDiv('added_specifications', specifications_str);
-
-        addHiddenDiv('product_other_information',additional_description);
-
-        let final_additional_description=description+"|"+additional_description;
-        const additional_description_list = final_additional_description.split('|');
-        additional_description_list.forEach(function(description) {
-          addHiddenDiv('additional_description',description);
-        });
-      }
-      else if(weight_xpath_str!="" && weight_xpath_str!=null) {
-
-         //www.stakelums.ie/product/v7-total-clean/
- 
-        const dimension_xpath = '//div[@id="specificationTab"]//table/tbody/tr[5]/td[2]/p[text()]';
-        let dimension_xpath_str = getXpath(dimension_xpath, 'innerText');
-
-        addHiddenDiv('product_weight',weight_xpath_str+" Kg");
-        let dimensionList=dimension_xpath_str.split("/");
-        addHiddenDiv('product_dimension',dimensionList[0]+"x"+dimensionList[1]+"x"+dimensionList[2]);
-
-        const shippingweight_xpath = '//div[@id="specificationTab"]//table/tbody/tr[3]/td[2]/p[text()]';
-        let shippingweight_str = getXpath(shippingweight_xpath, 'innerText');
-        addHiddenDiv('shipping_weight',shippingweight_str);
-    
-        let specifications_xpath = "//div[@id='specificationTab']//table/tbody/tr";
-        let specifications_str = getAllXpath(specifications_xpath, 'innerText').join('||');
-        addHiddenDiv('added_specifications', specifications_str);
-
-        const manufacturerDescription_xpath = "//div[contains(@class,'flix-d-p')]";
-        const manufacturerDescription_str = getAllXpath(manufacturerDescription_xpath, 'innerText');
-        addHiddenDiv('added_manufacturerDescription', manufacturerDescription_str);
-
-        const additional_feature_xpath ="//div[@id='specificationTab']/ul/li";
-        let additional_feature_str = getAllXpath(additional_feature_xpath, 'innerText').join('|');
-        
-        additional_description=additional_description+"|"+additional_feature_str;
-        addHiddenDiv('product_other_information',additional_description);
-        
-        let final_additional_description=description+"|"+additional_description;
-        const additional_description_list = final_additional_description.split('|');
-        additional_description_list.forEach(function(description) {
-          addHiddenDiv('additional_description',description);
-        });
-      }
-      else {
-
-        const shippingSpecification = document.querySelectorAll('.inpage_selector_specification .flix-std-table .flix-std-specs-table .flix-title');
-        let dimension="";
-        let weight="";
-        shippingSpecification.forEach((item)=>{
+      //check Weight from Specification 
+      const weightSpecification = document.querySelectorAll('.inpage_selector_specification .flix-std-table .flix-std-specs-table .flix-title');
+      let weight="";
+        weightSpecification.forEach((item)=>{
           if(item.textContent.includes("kg")){
-            weight=item.textContent;
-            addHiddenDiv('product_weight',weight);
-          }else if(item.textContent.includes("mm")){
-            dimension+=item.textContent +"X";
+              weight=item.textContent;
+              addHiddenDiv('product_weight',weight);
           }
         });
-        dimension=dimension.slice(0,-1);
-      //  addHiddenDiv('product_dimension',dimension);
         if(weight==""){
             const shippingWeight = document.querySelectorAll('.inpage_selector_specification .flix-std-specs-table .flix-title');
             shippingWeight.forEach((item)=>{
-            if(item.textContent.includes("KG") || item.textContent.includes("kg")){
-            weight=item.textContent;
+             if(item.textContent.includes("KG") || item.textContent.includes("kg")){
+              weight=item.textContent;
             }
             });
-            addHiddenDiv('product_weight',weight);
+            if(weight!=""){
+              addHiddenDiv('product_weight',weight);
+            }
         }
-      const specifications_xpath = "//table[@class='flix-std-specs-table']/tbody/tr";
-      let specifications_str = getAllXpath(specifications_xpath, 'innerText').join('||');
+      if(weight==""){
+          //weightXpath
+          let weight_xpath_option1=getXpath("//div[@id='specificationTab']//table/tbody/tr[2]/td[2]/p[text()]",'innerText');
+          let weight_xpath_option2=getXpath("//div[@id='specificationTab']//p[@class='product-specificationlabel-top'][5]",'innerText');
+          let weight_xpath_option3=getXpath("//div[@class='tabs-content']//p[contains(text(),'Weight')]",'innerText');
+          if(weight_xpath_option1!=null)
+          {
+            addHiddenDiv('product_weight',weight_xpath_option1+" Kg");
+          }
+          else if(weight_xpath_option2!=null){
+            let weightData= weight_xpath_option2.toString().trim().split(":");
+            addHiddenDiv('product_weight',weightData[1]);
+          }
+          else if(weight_xpath_option3!=null){
+            let weightData= weight_xpath_option3.toString().trim().split(":");
+            addHiddenDiv('product_weight',weightData[1]);
+          }
+    }
 
-      const specifications_part = getXpath("//div[@id='flix-inpage']//div[@id='inpage_container']//iframe",'innerText');
-      if(specifications_part){
-        
-        specifications_str = specifications_str.concat(specifications_part);
-      }
-      addHiddenDiv('added_specifications', specifications_str);
-      const manufacturerDescription_xpath = "//div[@class='flix-Text-block']";
-      const manufacturerDescription_str = getAllXpath(manufacturerDescription_xpath, 'innerText');
+    //Manufacture Description
+   let manufacturerDescription_xpath_option = getAllXpath("//div[contains(@id,'inpage_container')]//div[(contains(@class,'flix-d-p') or contains(@class,'flix-std-desc')) and not(contains(@class,'flix-title')or contains(@class,'flix-caveat-desc'))]", 'innerText');
+   if(manufacturerDescription_xpath_option.length>0){
+    addHiddenDiv('added_manufacturerDescription', manufacturerDescription_xpath_option);
+   }
 
-      const manufacturerDescription_tool_xpath = "//div[contains(@class,'flix-std-content')]//div[contains(@class,'flix-std-desc')]";
-      const manufacturerDescription_tool_str = getAllXpath(manufacturerDescription_tool_xpath, 'innerText');
-      addHiddenDiv('added_manufacturerDescription', manufacturerDescription_str+" "+manufacturerDescription_tool_str);
-  
-      addHiddenDiv('product_other_information',additional_description);
+   //shipping_Weight
+   let shippingweight_xpath_option = getXpath('//div[@id="specificationTab"]//table/tbody/tr[3]/td[2]/p[text()]', 'innerText');
+   if(shippingweight_xpath_option!=null){
+    addHiddenDiv('shipping_weight',shippingweight_xpath_option);
+    addHiddenDiv('addgross_weight',shippingweight_xpath_option);
+   }
+   //Dimension
+   let dimension_xpath_option = getXpath('//div[@id="specificationTab"]//table/tbody/tr[5]/td[2]/p[text()]', 'innerText');
+   if(dimension_xpath_option!=null){
+    let dimensionList=dimension_xpath_option.split("/");
+    addHiddenDiv('product_dimension',dimensionList[0]+"x"+dimensionList[1]+"x"+dimensionList[2]);
+   }
 
-       /*const production_description_list = additional_description.split('||');
+   //Specification Xpath
+   let specifications_xpath_option1 = getAllXpath("//table[@class='flix-std-specs-table']/tbody/tr/td", 'innerText');
+   let specifications_xpath_option2 = getAllXpath("//div[@id='specificationTab']//table/tbody/tr", 'innerText');
+   let specifications_xpath_option3 = getAllXpath("//div[@id='specificationTab']//p[@class='product-specificationlabel-top']", 'innerText');
+   if(specifications_xpath_option1.length>0){
+    addHiddenDiv('added_specifications', specifications_xpath_option1.join('||'));
+   }
+   else if(specifications_xpath_option2.length>0)
+   {
+    addHiddenDiv('added_specifications', specifications_xpath_option2.join('||'));
+   }
+   else if(specifications_xpath_option3.length>0){
+    addHiddenDiv('added_specifications', specifications_xpath_option3.join('||'));
+   }
 
-        production_description_list.forEach(function(data) {
-          addHiddenDiv('product_other_information',data);
-        });*/
-        
-        let final_additional_description=description+"|"+additional_description;
-        const additional_description_list = final_additional_description.split('|');
-        additional_description_list.forEach(function(description) {
-          addHiddenDiv('additional_description',description);
-        });
-
-      }
-
-     
-      const video_url_path = getXpath("//div[@class='fullJwPlayerWarp']//input[@class='flix-jw']/@value", 'nodeValue');
-      if(video_url_path && typeof video_url_path == 'string'){
-        var video_url_obj = JSON.parse(video_url_path);
-        addHiddenDiv('added_video_url', "http:"+video_url_obj.playlist[0].file);
-      }
+   //product Information and additional Description
+   let additional_xpath_option1 =getAllXpath("//div[@id='specificationTab']/ul/li",'innerText');
+   let final_additional_description;
+   if(additional_xpath_option1.length>0){
+    //addHiddenDiv('product_other_information',additional_description+"|"+additional_xpath_option1.join('|'));
+    final_additional_description=description+"|"+additional_description+"|"+additional_xpath_option1.join('|');
+   }
+   else {
+    //addHiddenDiv('product_other_information',additional_description);
+    final_additional_description=description+"|"+additional_description;
+   }
+   const additional_description_list = final_additional_description.split('|');
+   additional_description_list.forEach(function(description) {
+     addHiddenDiv('additional_description',description);
+   });
+   //Video
+    const video_url_path = getXpath("//div[@class='fullJwPlayerWarp']//input[@class='flix-jw']/@value", 'nodeValue');
+    if(video_url_path && typeof video_url_path == 'string'){
+      var video_url_obj = JSON.parse(video_url_path);
+      addHiddenDiv('added_video_url', "http:"+video_url_obj.playlist[0].file);
+    }
       
-      let scrollTop = 500;
-      while (true) {
-        window.scroll(0, scrollTop);
-        await stall(1000);
-        scrollTop += 500;
-        if (scrollTop === 10000) {
-          break;
-        }
+    let scrollTop = 500;
+    while (true) {
+      window.scroll(0, scrollTop);
+      await stall(1000);
+      scrollTop += 500;
+      if (scrollTop === 10000) {
+        break;
       }
-    });
+    }
+  });
   
     return await context.extract(productDetails, { transform: parameters.transform });
   }
