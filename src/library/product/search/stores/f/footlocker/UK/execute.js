b710a7aa140659840a34f269f993ffb7fcaa2c5d
Vivek Gondil <gondilvivek@gmail.com>
2020-10-06 13:24:55 +0530

    lint fixed

--- src/library/product/search/stores/f/footlocker/UK/execute.js
+++ src/library/product/search/stores/f/footlocker/UK/execute.js
@@ -1,12 +1,12 @@
 module.exports = {
-    implements: 'product/search/execute',
-    parameterValues: {
-        country: 'UK',
-        store: 'footlocker',
-        domain: 'footlocker.co.uk',
-        url: 'https://www.footlocker.co.uk/en/search?q={searchTerms}',
-        loadedSelector: 'img[alt*="Logo"]',
-        noResultsXPath: '//h2[contains(text(),"no results")]',
-        zipcode: '',
-    },
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'footlocker',
+    domain: 'footlocker.co.uk',
+    url: 'https://www.footlocker.co.uk/en/search?q={searchTerms}',
+    loadedSelector: 'img[alt*="Logo"]',
+    noResultsXPath: '//h2[contains(text(),"no results")]',
+    zipcode: '',
+  },
 };
