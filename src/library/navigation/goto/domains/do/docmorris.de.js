3761a76b5fa8d6e31d3e92a4fe6faae10b6dfa89
Vivek Gondil <gondilvivek@gmail.com>
2020-10-06 10:34:06 +0530

    lint fixed code

--- src/library/navigation/goto/domains/do/docmorris.de.js
+++ src/library/navigation/goto/domains/do/docmorris.de.js
@@ -1,10 +1,10 @@
 module.exports = {
-    implements: 'navigation/goto',
-    parameterValues: {
-        domain: 'docmorris.de',
-        timeout: 50000,
-        country: 'DE',
-        store: 'docmorris',
-        zipcode: '',
-    },
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'docmorris.de',
+    timeout: 50000,
+    country: 'DE',
+    store: 'docmorris',
+    zipcode: '',
+  },
 };
--- src/library/product/search/stores/d/docmorris/DE/paginate.js
+++ src/library/product/search/stores/d/docmorris/DE/paginate.js
@@ -1,15 +1,15 @@
 module.exports = {
-    implements: 'product/search/paginate',
-    parameterValues: {
-        country: 'DE',
-        store: 'docmorris',
-        nextLinkSelector: 'a[rel="next"]',
-        mutationSelector: null,
-        spinnerSelector: null,
-        loadedSelector: '#obligatories',
-        noResultsXPath: '//*[contains(text(),"Ihrem Suchbegriff kein")]',
-        openSearchDefinition: null,
-        domain: 'docmorris.de',
-        zipcode: '',
-    },
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'DE',
+    store: 'docmorris',
+    nextLinkSelector: 'a[rel="next"]',
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: '#obligatories',
+    noResultsXPath: '//*[contains(text(),"Ihrem Suchbegriff kein")]',
+    openSearchDefinition: null,
+    domain: 'docmorris.de',
+    zipcode: '',
+  },
 };
