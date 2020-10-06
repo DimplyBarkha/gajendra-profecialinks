e2d1d282fae8c0b4fafc071fbb587afef1688530
Vivek Gondil <gondilvivek@gmail.com>
2020-10-06 10:28:18 +0530

    lint fixed

--- src/library/product/search/stores/f/footlocker/UK/extract.js
+++ src/library/product/search/stores/f/footlocker/UK/extract.js
@@ -1,30 +1,30 @@
 const { transform } = require('../../../../shared');
-async function implementation(
-    inputs,
-    parameters,
-    context,
-    dependencies,
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
 ) {
-    await context.evaluate(() => {
-        var currentUrl = window.location.href;
-        var newElement = document.createElement("DIV");
-        newElement.setAttribute('id', 'search-url');
-        newElement.innerHTML = currentUrl;
-        document.body.appendChild(newElement);
-    });
+  await context.evaluate(() => {
+    var currentUrl = window.location.href;
+    var newElement = document.createElement('DIV');
+    newElement.setAttribute('id', 'search-url');
+    newElement.innerHTML = currentUrl;
+    document.body.appendChild(newElement);
+  });
 
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    return await context.extract(productDetails, { transform });
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  return await context.extract(productDetails, { transform });
 }
 module.exports = {
-    implements: 'product/search/extract',
-    parameterValues: {
-        country: 'UK',
-        store: 'footlocker',
-        transform,
-        domain: 'footlocker.co.uk',
-        zipcode: '',
-    },
-    implementation,
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'footlocker',
+    transform,
+    domain: 'footlocker.co.uk',
+    zipcode: '',
+  },
+  implementation,
 };
