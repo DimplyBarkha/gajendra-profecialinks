67e29671f92b08472cff1bb1458ff552d8ebfb40
Nilesh <nilesh.b@osmosys.asia>
2020-10-06 19:22:52 +0530

    Added separate shared.js.

--- src/library/product/search/stores/n/nykaa/IN/extract.js
+++ src/library/product/search/stores/n/nykaa/IN/extract.js
@@ -1,22 +1,11 @@
-const { transform } = require('../../../../shared');
+const { transform } = require('../shared');
 
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-  return await context.extract(productDetails, { transform });
-}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'IN',
     store: 'nykaa',
-    transform: transform,
+    transform,
     domain: 'nykaa.com',
   },
-  implementation,
 };
