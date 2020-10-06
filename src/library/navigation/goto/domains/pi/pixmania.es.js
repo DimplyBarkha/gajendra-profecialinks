8c1d27407dac2a1ec77b444f632eb5db3125f533
Ankit Kumar <ankit.k@osmosys.asia>
2020-10-06 18:52:11 +0530

    wrote script to handle missing description part of the page

--- src/library/navigation/goto/domains/pi/pixmania.es.js
+++ src/library/navigation/goto/domains/pi/pixmania.es.js
@@ -3,8 +3,41 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'pixmania.es',
-    timeout: null,
+    timeout: 60000,
     country: 'ES',
     store: 'pixmania',
   },
+  // implementation: async (
+  //   { url, zipcode, storeId },
+  //   parameters,
+  //   context,
+  //   dependencies,
+  // ) => {
+  //   url = `${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":false}[/!opt!]`;
+  //   const timeout = parameters.timeout ? parameters.timeout : 60000;
+  //   // await context.setJavaScriptEnabled(true);
+  //   // await context.setCssEnabled(true);
+  //   // await context.setLoadAllResources(true);
+  //   // await context.setLoadImages(true);
+  //   // await context.setBlockAds(false);
+  //   // await context.setAntiFingerprint(false);
+
+  //   await context.goto(url, {
+  //     // firstRequestTimeout: 60000,
+  //     timeout: timeout,
+  //     waitUntil: 'load',
+  //     // checkBlocked: false,
+  //     // cssEnabled: true,
+  //     // discardCSPHeader: true,
+  //     // embedIframes: true,
+  //     // imagesEnabled: true,
+  //     // loadAllResources: true,
+  //     // force200: true,
+  //     // blockAds: false,
+  //     // jsEnabled: true,
+  //     // gotoTimeout: 1000,
+  //     // randomMoveMouse: false,
+  //   });
+  //   await context.waitForNavigation({ timeout: 30000 });
+  // },
 };
--- src/library/product/details/stores/p/pixmania/ES/extract.js
+++ src/library/product/details/stores/p/pixmania/ES/extract.js
@@ -10,8 +10,32 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      await new Promise(r => setTimeout(r, 10000));
+      // console.log(`url: ${inputString.URL}`);
+      console.log('Waiting for the page to load.....');
+      await new Promise(resolve => setTimeout(resolve, 10000));
+      console.log('Wait over.....');
+      // api to fetch the description of the details page
+      const url = 'https://www.pixmania.es/p/kodak-pixpro-fz41-camara-compacta-negro-7868164?offerId=24511648';
+      fetch('https://live.icecat.biz/api/html?lang=es&content=title%2Cgallery%2Cfeaturelogos%2Cessentialinfo%2Cbulletpoints%2Cmarketingtext%2Cmanuals%2Creasonstobuy%2Ctours3d%2Cvideos%2Cfeaturegroups%2Creviews%2Cproductstory&version=2.0.0&UserName=v.poezevara&GTIN=0819900012903&selector=%23IcecatLive', {
+        headers: {
+          accept: '*/*',
+          'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+        },
+        referrer: `${url}`,
+        referrerPolicy: 'no-referrer-when-downgrade',
+        body: null,
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
+        // @ts-ignore
+      }).then(txt => txt.text()).then(data => responseHtml = data).then(() => console.log(responseHtml));
     });
+    console.log('Waiting before exiting navigation.....');
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    console.log('Wait over.....');
     return await context.extract(productDetails);
   },
 };
