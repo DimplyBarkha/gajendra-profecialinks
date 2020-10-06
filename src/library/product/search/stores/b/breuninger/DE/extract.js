9a8f307e013a99d15985425bd79643eee68a5db0
Tomasz <tomekwitoslaw@gmail.com>
2020-10-06 13:11:06 +0200

    breuninger_DE search proper rank

--- src/library/product/search/stores/b/breuninger/DE/extract.js
+++ src/library/product/search/stores/b/breuninger/DE/extract.js
@@ -1,21 +1,12 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'DE',
     store: 'breuninger',
-    transform: null,
+    transform,
     domain: 'breuninger.de',
     zipcode: '',
-  },
-  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
-    // extracting data automatically
-    await context.extract(productDetails);
-    // inserting data manually
-    var dataRef = await context.data();
-    for (let i = 0; i < dataRef[0].data[0].group.length; i++) {
-      dataRef[0].data[0].group[i].rank[0].text = (parseInt(dataRef[0].data[0].group[i].rank[0].text) + 1).toString();
-      dataRef[0].data[0].group[i].rankOrganic[0].text = (parseInt(dataRef[0].data[0].group[i].rankOrganic[0].text) + 1).toString();
-    }
   },
 };
