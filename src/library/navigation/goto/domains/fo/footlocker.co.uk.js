7034a10e8da595397924d79b3fe5b5b70d32cbcf
Vivek Gondil <gondilvivek@gmail.com>
2020-10-06 13:24:10 +0530

    Added Changes from PR

--- src/library/navigation/goto/domains/fo/footlocker.co.uk.js
+++ src/library/navigation/goto/domains/fo/footlocker.co.uk.js
@@ -1,11 +1,12 @@
-
 module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'footlocker.co.uk',
-    timeout: null,
     country: 'UK',
     store: 'footlocker',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    await context.goto((url + '#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]'), { timeout: 10000, waitUntil: 'load', checkBlocked: true });
   },
 };
