b39764be0a80c71fe0fcf7744d9d8f93254d2428
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-10-06 18:01:55 +0530

    initial setup (#670)

---
+++ src/library/product/search/stores/w/walmart/MX/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'MX',
+    store: 'walmart',
+    domain: 'walmart.com.mx',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/w/walmart/MX/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'MX',
+    store: 'walmart',
+    transform: null,
+    domain: 'walmart.com.mx',
+    zipcode: '',
+  },
+};
