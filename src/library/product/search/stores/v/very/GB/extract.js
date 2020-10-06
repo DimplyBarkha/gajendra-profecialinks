8248392f0dadfe324ff8fefc92140d6f8c974a93
Lech Kulesza <lmkulesz@icloud.com>
2020-10-06 18:05:18 +0200

    rating number fix

--- src/library/product/search/stores/v/very/GB/extract.js
+++ src/library/product/search/stores/v/very/GB/extract.js
@@ -1,43 +1,53 @@
-
 module.exports = {
-  implements: 'product/search/extract',
+  implements: "product/search/extract",
   parameterValues: {
-    country: 'GB',
-    store: 'very',
+    country: "GB",
+    store: "very",
     transform: null,
-    domain: 'very.co.uk',
-    zipcode: '',
+    domain: "very.co.uk",
+    zipcode: "",
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      const allProducts = document.querySelectorAll('li.product');
+      const allProducts = document.querySelectorAll("li.product");
       let x;
       for (x = 0; allProducts.length - 1 >= x; x++) {
-        allProducts[x].setAttribute('count', `${x + 1}`);
+        allProducts[x].setAttribute("count", `${x + 1}`);
       }
-      const productRatings = document.querySelectorAll('a.productRating');
+      const productRatings = document.querySelectorAll("a.productRating");
       if (productRatings) {
         for (let x = 0; productRatings.length > x; x++) {
           // @ts-ignore
-          const newRating = productRatings[x].title.replace(/\./, ',');
+          const newRating = productRatings[x].title.replace(/\./, ",");
           // @ts-ignore
           productRatings[x].title = newRating;
         }
       }
       for (let i = 0; i < allProducts.length; i++) {
-        if(!document.querySelectorAll('li.product')[i].querySelector('div.reviewsScore>div>ul')) {
+        if (!document.querySelectorAll("li.product")[i].querySelector("div.reviewsScore>div>ul")) {
           continue;
         }
-        ratings = document.querySelectorAll('li.product')[i].querySelector('div.reviewsScore>div>ul');
+        ratings = document.querySelectorAll("li.product")[i].querySelector("div.reviewsScore>div>ul");
         value = 0;
         for (let j = 0; j < 5; j++) {
-          stars = parseInt(ratings.querySelectorAll('li')[j].querySelector('span.starCount').innerText)
-          console.log('stars=', stars)
+          stars = parseInt(ratings.querySelectorAll("li")[j].querySelector("span.starCount").innerText);
+          console.log("stars=", stars);
           value += stars;
-          console.log('value=', value)
+          console.log("value=", value);
         }
-        document.querySelectorAll('li.product')[i].setAttribute('ratingCount', `${value}`);
+        document.querySelectorAll("li.product")[i].setAttribute("ratingCount", `${value}`);
       }
+      allProducts.forEach((product) => {
+        const div = document.createElement("div");
+        div.classList.add("ratingNumber");
+        const element = product.querySelector(".bvReviewStars a.productRating");
+        const ratingNumber = element
+          .getAttribute("title")
+          .match(/\d?.?\d/)[0]
+          .replace(".", ",");
+        div.innerHTML = ratingNumber;
+        product.appendChild(div);
+      });
     });
     return await context.extract(productDetails);
   },
