0ce3724c40b2ac4c34583cc577444a37c4605bf3
nilofarsh <nilofar.shaikh@synerzip.com>
2020-10-07 18:43:59 +0530

    #575 noResultsXPath added

--- src/library/product/search/stores/s/smithandcaugheys/NZ/paginate.js
+++ src/library/product/search/stores/s/smithandcaugheys/NZ/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: null,
-    noResultsXPath: null,
+    noResultsXPath: '//div[contains(text(),"Sorry no results found")]',
     openSearchDefinition: null,
     domain: 'smithandcaugheys.co.nz',
     zipcode: '',
