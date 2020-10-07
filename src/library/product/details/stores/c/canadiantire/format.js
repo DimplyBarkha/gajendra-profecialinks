370c7e3b239d11231908c86fe15493df43bbd283
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-10-07 18:05:01 +0530

    #385. added cleanUp code

--- src/library/product/details/stores/c/canadiantire/format.js
+++ src/library/product/details/stores/c/canadiantire/format.js
@@ -24,7 +24,7 @@ const transform = (data) => {
       };
       if (row.videos) {
         row.videos.forEach(video => {
-          video.text = video.text.replace('thumbnails','videos')
+          video.text = video.text.replace('thumbnails', 'videos');
         });
       };
       if (row.listPrice && !row.price) {
@@ -78,6 +78,21 @@ const transform = (data) => {
       group.push({ ...row1 });
     });
   }
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
   return data;
 };
 
