c5f5d8c0af5397f1179264e86154e35cc937164c
Ankit Kumar <ankit.k@osmosys.asia>
2020-10-07 13:53:55 +0530

    added script to load the enhanced content

--- src/library/navigation/goto/domains/pi/pixmania.es.js
+++ src/library/navigation/goto/domains/pi/pixmania.es.js
@@ -3,41 +3,57 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'pixmania.es',
-    timeout: 60000,
+    timeout: 100000,
     country: 'ES',
     store: 'pixmania',
   },
-  // implementation: async (
-  //   { url, zipcode, storeId },
-  //   parameters,
-  //   context,
-  //   dependencies,
-  // ) => {
-  //   url = `${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":false}[/!opt!]`;
-  //   const timeout = parameters.timeout ? parameters.timeout : 60000;
-  //   // await context.setJavaScriptEnabled(true);
-  //   // await context.setCssEnabled(true);
-  //   // await context.setLoadAllResources(true);
-  //   // await context.setLoadImages(true);
-  //   // await context.setBlockAds(false);
-  //   // await context.setAntiFingerprint(false);
-
-  //   await context.goto(url, {
-  //     // firstRequestTimeout: 60000,
-  //     timeout: timeout,
-  //     waitUntil: 'load',
-  //     // checkBlocked: false,
-  //     // cssEnabled: true,
-  //     // discardCSPHeader: true,
-  //     // embedIframes: true,
-  //     // imagesEnabled: true,
-  //     // loadAllResources: true,
-  //     // force200: true,
-  //     // blockAds: false,
-  //     // jsEnabled: true,
-  //     // gotoTimeout: 1000,
-  //     // randomMoveMouse: false,
-  //   });
-  //   await context.waitForNavigation({ timeout: 30000 });
-  // },
+  implementation: async (
+    { url, zipcode, storeId },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 100000;
+    await context.setJavaScriptEnabled(true);
+    await context.setCssEnabled(true);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setBlockAds(false);
+    // await context.setAntiFingerprint(false);
+    await context.goto(url, {
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+      cssEnabled: true,
+      discardCSPHeader: true,
+      embedIframes: true,
+      imagesEnabled: true,
+      loadAllResources: true,
+      force200: true,
+      blockAds: false,
+      jsEnabled: true,
+    });
+    return await context.evaluate(async function () {
+      async function addEnhancedContent () {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'IcecatLive -icecat-tabs_body for-title,gallery,featurelogos,essentialinfo,bulletpoints,marketingtext,manuals,reasonstobuy,tours3d,videos,featuregroups,reviews,productstory');
+        const script = document.createElement('script');
+        script.setAttribute('src', 'https://icecat.biz/stats/scripts/trackLive.js');
+        div.append(script);
+        const gtin = document.querySelector('[data-flix-ean]').getAttribute('data-flix-ean');
+        const response = await fetch(`https://live.icecat.biz/api/html?lang=es&content=title,gallery,featurelogos,essentialinfo,bulletpoints,marketingtext,manuals,reasonstobuy,tours3d,videos,featuregroups,reviews,productstory&version=2.0.0&UserName=v.poezevara&GTIN=${gtin}`);
+        const html = await response.text();
+        div.innerHTML += html;
+        document.querySelector('#IcecatLive').append(div);
+      }
+      await addEnhancedContent();
+      const isLoaded = Boolean(document.querySelector('#IcecatLive'));
+      console.log(`isLoaded: ${isLoaded}`);
+      if (!isLoaded) {
+        const script = document.createElement('script');
+        script.setAttribute('src', 'https://icecat.biz/stats/scripts/trackLive.js');
+        div.append(script);
+      }
+    });
+  },
 };
