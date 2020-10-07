6ea0291ebe88e670983fb63904cdf7c00de5fb03
adrian_swiderek <adrian.swiderek@tdcm.io>
2020-10-07 21:52:50 +0200

    improved selectors and extract.js after QA 1

--- src/library/product/details/stores/f/flipkart/IN/extract.js
+++ src/library/product/details/stores/f/flipkart/IN/extract.js
@@ -1,4 +1,6 @@
 
+const { cleanUp } = require('../../../../shared.js');
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -7,5 +9,34 @@ module.exports = {
     transform: null,
     domain: 'flipkart.com',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    // await context.hover('div.sWIg8E');
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const ratingCountExists = document.evaluate("//span[@class='_38sUEc']//*[contains(text(), 'Reviews') or contains(text(), 'reviews')]", document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue;
+      if (ratingCountExists) {
+        const ratingCountString = document.evaluate("//span[@class='_38sUEc']//*[contains(text(), 'Reviews') or contains(text(), 'reviews')]", document, null, XPathResult.STRING_TYPE, null).stringValue;
+        const ratingCountRegexp = /(\d+)\s+(Reviews|reviews)/g;
+        const ratingCountMatch = ratingCountRegexp.exec(ratingCountString)[1];
+        addElementToDocument('ratingCount', ratingCountMatch);
+      }
+
+      const jsonData = JSON.parse(document.getElementById('jsonLD').innerHTML);
+      if ('brand' in jsonData[0]){
+        const brandText = jsonData[0].brand.name;
+        addElementToDocument('brandText', brandText);
+      }
+
+      const defaultVariantCount = 0;
+      addElementToDocument('variantCount', defaultVariantCount);
+    });
+    await context.extract(productDetails);
   },
 };
