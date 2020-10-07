da38614d9688ec5ae70e85b3c952c939c9d4ffda
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-10-07 18:15:56 +0530

    #449. implemented id in createUrl

--- src/library/product/details/stores/f/fnac/FR/createUrl.js
+++ src/library/product/details/stores/f/fnac/FR/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'fnac.com',
     prefix: null,
-    url: null,
+    url: 'https://www.fnac.com/a{id}',
     country: 'FR',
     store: 'fnac',
     zipcode: '',
