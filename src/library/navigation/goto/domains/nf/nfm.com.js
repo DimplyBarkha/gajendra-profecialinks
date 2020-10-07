e0c79d68d3cd57228ce2eb5ba1894dcc8cad6852
Bindu Datla <bindu.datla@import.io>
2020-10-06 23:18:50 -0400

    increased timeout

--- src/library/navigation/goto/domains/nf/nfm.com.js
+++ src/library/navigation/goto/domains/nf/nfm.com.js
@@ -2,9 +2,18 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'nfm.com',
-    timeout: null,
+    timeout: 60000,
     country: 'US',
     store: 'nfm',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setFirstRequestTimeout(30000);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
