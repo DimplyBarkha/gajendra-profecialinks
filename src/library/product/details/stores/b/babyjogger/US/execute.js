1405c8e9a75fed810e1db78a265c2b3125319281
Manoj Dey <manoj.d@OSMLIN16>
2020-10-07 20:26:49 +0530

    updated code

--- src/library/product/details/stores/b/babyjogger/US/execute.js
+++ src/library/product/details/stores/b/babyjogger/US/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'babyjogger',
     domain: 'babyjogger.com',
     loadedSelector: 'div[data-component="ZoomView"]',
-    noResultsXPath: "//div[contains(@class,'error-code')] | //div[@class='storepage'] | //*[@id='product-search-results'] | //*[@id='bbjmainhomepage'] | //h2[contains(@class,'error-page-title')]",
+    noResultsXPath: "//h2[contains(.,'Internal Server Error')] | //div[contains(@class,'error-code')] | //div[@class='storepage'] | //*[@id='product-search-results'] | //*[@id='bbjmainhomepage'] | //h2[contains(@class,'error-page-title')]",
     zipcode: '',
   },
 };
--- src/library/product/details/stores/b/babyjogger/US/extract.js
+++ src/library/product/details/stores/b/babyjogger/US/extract.js
@@ -17,7 +17,7 @@ module.exports = {
     await context.evaluate(async function () {
       let breadcrumbs = document.querySelectorAll('li.breadcrumb-item');
       if (breadcrumbs.length > 1) {
-        for (i = 0; i < breadcrumbs.length - 1; i++) {
+        for (let i = 0; i < breadcrumbs.length - 1; i++) {
           var div = document.createElement('li');
           div.className = 'categories';
           var getInput = document.createElement('input');
@@ -28,11 +28,11 @@ module.exports = {
         }
       }
       else {
-        breadcrumbs = document.querySelector('li.breadcrumb-item a');
+        let breadcrumbs = document.querySelector('li.breadcrumb-item a');
         if(breadcrumbs){
-        var div = document.createElement('li');
+        let div = document.createElement('li');
         div.className = 'categories';
-        var getInput = document.createElement('input');
+        let getInput = document.createElement('input');
         div.appendChild(getInput);
         document.head.appendChild(div);
         getInput.setAttribute("value", breadcrumbs.innerText);
@@ -40,20 +40,19 @@ module.exports = {
       }
       let getAllImages = document.querySelectorAll('button.carousel-img-wrap img');
       let allImages = [];
-      for (i = 0; i < getAllImages.length; i++) {
+      for (let i = 0; i < getAllImages.length; i++) {
         allImages.push(getAllImages[i].getAttribute('src'));
       }
       let uniqueChars = [...new Set(allImages)];
-      for (i = 0; i < uniqueChars.length; i++) {
-        var div = document.createElement('div');
+      for (let i = 0; i < uniqueChars.length; i++) {
+        let div = document.createElement('div');
         div.className = 'secondaryimages';
-        var getInput = document.createElement('input');
+        let getInput = document.createElement('input');
         div.appendChild(getInput);
         document.body.appendChild(div);
         getInput.setAttribute("value", uniqueChars[i]);
       }
     });
-
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.extract(productDetails, { transform });
