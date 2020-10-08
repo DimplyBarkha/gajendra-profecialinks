959ef90a4403d7cb8a49a318247226dae1f7f17b
Ankit Kumar <ankit.k@osmosys.asia>
2020-10-08 16:38:41 +0530

    fixed list price issue

--- src/library/product/details/stores/c/coolblue/NL/extract.js
+++ src/library/product/details/stores/c/coolblue/NL/extract.js
@@ -10,6 +10,13 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
+      // append listprice in the DOM
+      let getPrice;
+      const getPriceSelector = document.querySelector('div.js-desktop-order-block span.sales-price__former-price');
+      if (getPriceSelector) {
+        getPrice = document.querySelector('div.js-desktop-order-block span.sales-price__former-price').textContent.trim().replace(/(\d+)(,?\.?)?(\d+)?(,-?)?/g, 'EUR $1,$3');
+        document.body.setAttribute('newprice', getPrice);
+      }
       // click on the image to get the alternate image info (no need to revert this as all the data is available in this situation too)
       const mainImageselector = document.querySelector('ul.product-media-gallery__wrapper>li');
       if (mainImageselector) {
