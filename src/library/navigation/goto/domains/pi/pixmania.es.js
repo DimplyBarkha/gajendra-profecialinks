466205dca4d6769207c921620f46c2bb6b288aad
Ankit Kumar <ankit.k@osmosys.asia>
2020-10-09 00:21:01 +0530

    updated the script to fix the texcontent of null issue

--- src/library/navigation/goto/domains/pi/pixmania.es.js
+++ src/library/navigation/goto/domains/pi/pixmania.es.js
@@ -40,20 +40,20 @@ module.exports = {
         const script = document.createElement('script');
         script.setAttribute('src', 'https://icecat.biz/stats/scripts/trackLive.js');
         div.append(script);
-        await new Promise(resolve => setTimeout(resolve, 2000));
+        console.log('waiting for selector......');
+        await new Promise(resolve => setTimeout(resolve, 6000));
+        console.log('wait over......');
         const gtin = document.querySelector('[data-flix-ean]').getAttribute('data-flix-ean');
         const response = await fetch(`https://live.icecat.biz/api/html?lang=es&content=title,gallery,featurelogos,essentialinfo,bulletpoints,marketingtext,manuals,reasonstobuy,tours3d,videos,featuregroups,reviews,productstory&version=2.0.0&UserName=v.poezevara&GTIN=${gtin}`);
         const html = await response.text();
         div.innerHTML += html;
         document.querySelector('#IcecatLive').append(div);
       }
-      await addEnhancedContent();
-      const isLoaded = Boolean(document.querySelector('#IcecatLive'));
-      console.log(`isLoaded: ${isLoaded}`);
-      if (!isLoaded) {
-        const script = document.createElement('script');
-        script.setAttribute('src', 'https://icecat.biz/stats/scripts/trackLive.js');
-        div.append(script);
+      // check for the product page , if so the wait for the enhancedContent
+      await new Promise(resolve => setTimeout(resolve, 6000));
+      const isProductPage = document.querySelector('h1[itemprop=name]');
+      if (isProductPage) {
+        await addEnhancedContent();
       }
     });
   },
