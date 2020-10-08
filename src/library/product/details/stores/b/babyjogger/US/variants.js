d2503dd898c849d96851274e26c5c0c733d7c84c
Manoj Dey <manoj.d@OSMLIN16>
2020-10-08 18:31:10 +0530

    added variant extractor

---
+++ src/library/product/details/stores/b/babyjogger/US/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'US',
+    store: 'babyjogger',
+    domain: 'babyjogger.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/b/babyjogger/US/variantsExtract.js
@@ -0,0 +1,44 @@
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { createUrl, variants } = dependencies;
+  await context.evaluate(function () {
+    var variantArr = [];
+    function timeout(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    var getVariant = async function () {
+      var image_rows = [...document.querySelectorAll('div.color-attribute')];
+      for (let index = 0; index < image_rows.length; index++) {
+        image_rows[index].click()
+        await timeout(1000)
+        var variantUrl = window.location.href;
+        variantArr.push(variantUrl);
+        let div = document.createElement('div');
+        div.className = 'variant';
+        let getInput = document.createElement('input');
+        div.appendChild(getInput);
+        document.body.appendChild(div);
+        getInput.setAttribute("value", variantUrl);
+        await timeout(5000)
+      }
+    }
+    getVariant()
+    
+  }, createUrl);
+  return await context.extract(variants);
+}
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'US',
+    store: 'babyjogger',
+    transform: null,
+    domain: 'babyjogger.com',
+    zipcode: '',
+  },
+  implementation,
+};
