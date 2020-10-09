3ad96cdb731181486787ecb99809414229794e40
Ankit Kumar <ankit.k@osmosys.asia>
2020-10-09 10:22:29 +0530

    moved the page loading script from navigation to extract.js

--- src/library/navigation/goto/domains/pi/pixmania.es.js
+++ src/library/navigation/goto/domains/pi/pixmania.es.js
@@ -19,7 +19,6 @@ module.exports = {
     await context.setLoadAllResources(true);
     await context.setLoadImages(true);
     await context.setBlockAds(false);
-    // await context.setAntiFingerprint(false);
     await context.goto(url, {
       timeout: timeout,
       waitUntil: 'load',
@@ -32,29 +31,6 @@ module.exports = {
       force200: true,
       blockAds: false,
       jsEnabled: true,
-    });
-    return await context.evaluate(async function () {
-      async function addEnhancedContent () {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'IcecatLive -icecat-tabs_body for-title,gallery,featurelogos,essentialinfo,bulletpoints,marketingtext,manuals,reasonstobuy,tours3d,videos,featuregroups,reviews,productstory');
-        const script = document.createElement('script');
-        script.setAttribute('src', 'https://icecat.biz/stats/scripts/trackLive.js');
-        div.append(script);
-        console.log('waiting for selector......');
-        await new Promise(resolve => setTimeout(resolve, 6000));
-        console.log('wait over......');
-        const gtin = document.querySelector('[data-flix-ean]').getAttribute('data-flix-ean');
-        const response = await fetch(`https://live.icecat.biz/api/html?lang=es&content=title,gallery,featurelogos,essentialinfo,bulletpoints,marketingtext,manuals,reasonstobuy,tours3d,videos,featuregroups,reviews,productstory&version=2.0.0&UserName=v.poezevara&GTIN=${gtin}`);
-        const html = await response.text();
-        div.innerHTML += html;
-        document.querySelector('#IcecatLive').append(div);
-      }
-      // check for the product page , if so the wait for the enhancedContent
-      await new Promise(resolve => setTimeout(resolve, 6000));
-      const isProductPage = document.querySelector('h1[itemprop=name]');
-      if (isProductPage) {
-        await addEnhancedContent();
-      }
     });
   },
 };
--- src/library/product/details/stores/p/pixmania/ES/extract.js
+++ src/library/product/details/stores/p/pixmania/ES/extract.js
@@ -1,5 +1,31 @@
 const { transform } = require('../../../../shared');
-
+async function implementation (
+  // @ts-ignore
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.waitForSelector('[data-flix-ean]');
+  await context.evaluate(async function () {
+    async function addEnhancedContent () {
+      const div = document.createElement('div');
+      div.setAttribute('class', 'IcecatLive -icecat-tabs_body for-title,gallery,featurelogos,essentialinfo,bulletpoints,marketingtext,manuals,reasonstobuy,tours3d,videos,featuregroups,reviews,productstory');
+      const script = document.createElement('script');
+      script.setAttribute('src', 'https://icecat.biz/stats/scripts/trackLive.js');
+      div.append(script);
+      const gtin = document.querySelector('[data-flix-ean]').getAttribute('data-flix-ean');
+      const response = await fetch(`https://live.icecat.biz/api/html?lang=es&content=title,gallery,featurelogos,essentialinfo,bulletpoints,marketingtext,manuals,reasonstobuy,tours3d,videos,featuregroups,reviews,productstory&version=2.0.0&UserName=v.poezevara&GTIN=${gtin}`);
+      const html = await response.text();
+      div.innerHTML += html;
+      document.querySelector('#IcecatLive').append(div);
+    }
+    await addEnhancedContent();
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,4 +34,5 @@ module.exports = {
     transform: transform,
     domain: 'pixmania.es',
   },
+  implementation,
 };
