ea948c2660cbfd21d924bd98d5cd9c6b5b39b799
irfan shaikh <irfan.shaikh@synerzip.com>
2020-10-09 21:10:06 +0530

    #676. Created core extractor for foodlion_28147.

---
+++ src/library/product/details/stores/f/foodlion_28147/US/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'US',
+    store: 'foodlion_28147',
+    domain: 'foodlion.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '28147',
+  },
+};
