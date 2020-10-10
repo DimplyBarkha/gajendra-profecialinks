2834c50fff816c135f46b7f6f9ce843a8d654c95
David Skoglund <david.skoglund@import.io>
2020-10-10 16:58:08 -0700

    latest push

--- src/library/product/search/stores/a/amazonMsCategory/US/execute.js
+++ src/library/product/search/stores/a/amazonMsCategory/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'amazonMsCategory',
     domain: 'amazon.com',
-    url: 'https://www.amazon.com/gp/bestsellers/{searchTerms}',
+    url: 'https://www.amazon.com/gp/bestsellers/*/{searchTerms}',
     noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "zeitgeist"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //img[contains(@alt,"Dogs of Amazon")] | /html[not(//ol[@id="zg-ordered-list"]/li)] | //*[contains(text(),"Looking for something?")]',
   },
 };
