e9fcf2a3a75c35624d20a29669b78a57437cc363
Vivek Gondil <gondilvivek@gmail.com>
2020-10-12 00:31:11 +0530

    Removing the shared function

--- src/library/product/search/stores/e/elcorteingles_electronica/PT/extract.js
+++ src/library/product/search/stores/e/elcorteingles_electronica/PT/extract.js
@@ -15,7 +15,16 @@ module.exports = {
     context,
     dependencies,
   ) {
-    await context.evaluate(createSearchUrl);
+    await context.evaluate(() => {
+      var newElement = document.createElement('DIV');
+      newElement.setAttribute('class', 'page-link');
+      newElement.innerHTML = window.location.href;
+      document.body.appendChild(newElement);
+      const productTiles = document.querySelectorAll('.products_list-item');
+      productTiles.forEach((tile) => {
+        tile.scrollIntoView();
+      });
+    });
     const { transform } = parameters;
     const { productDetails } = dependencies;
     return await context.extract(productDetails, { transform });
