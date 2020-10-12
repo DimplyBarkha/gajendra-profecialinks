a5b282b7744f51dd9a7cffcad4a0d13ca236e0ab
vikas kyatannawar <vikas.k@osmosys.asia>
2020-10-12 15:24:49 +0530

    updated image to exclude default images, updated manufacturer name to be consitent, full and correct.

--- src/library/product/details/stores/a/albertheijn/NL/extract.js
+++ src/library/product/details/stores/a/albertheijn/NL/extract.js
@@ -1,5 +1,26 @@
 const { transform } = require('./transform');
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  function addManufacturer () {
+    try {
+      const manufacturer = __INITIAL_STATE__.product.card.meta.owner.contacts.pop().name;
+      document.body.setAttribute('manufacturer', manufacturer);
+    } catch (err) {
+      console.log('Could not add manufacturer');
+    }
+  }
+
+  await context.evaluate(addManufacturer);
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -9,4 +30,5 @@ module.exports = {
     domain: 'ah.nl',
     zipcode: '',
   },
+  implementation,
 };
