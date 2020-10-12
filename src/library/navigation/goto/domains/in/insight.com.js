ef737d975f90ca353b7959415dbba39bdf233420
sivasoarlogic <siva@soarlogic.com>
2020-10-12 13:49:15 +0530

    core

---
+++ src/library/navigation/goto/domains/in/insight.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'insight.com',
+    timeout: null,
+    country: 'US',
+    store: 'insight',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/details/stores/i/insight/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'insight',
+    transform: null,
+    domain: 'insight.com',
+    zipcode: "''",
+  },
+};
