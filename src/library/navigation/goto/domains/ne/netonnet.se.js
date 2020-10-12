75cbefd1e596e7416d73ea648df577e76b6df881
sushma-mangalapally <sushma.mangalapally@import.io>
2020-10-12 15:39:27 -0700

    Add a fix for loading content

--- src/library/navigation/goto/domains/ne/netonnet.se.js
+++ src/library/navigation/goto/domains/ne/netonnet.se.js
@@ -3,9 +3,14 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'netonnet.se',
-    timeout: 20000,
+    timeout: 35000,
     country: 'SE',
     store: 'netonnet',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+
+    url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+    await context.goto(url);
   },
 };
