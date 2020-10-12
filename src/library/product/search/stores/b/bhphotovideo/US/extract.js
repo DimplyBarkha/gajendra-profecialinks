7f7da4a7e0343d6a77837bdc014ad0efe253e6b9
Tomasz <tomekwitoslaw@gmail.com>
2020-10-12 17:04:28 +0200

    bhphotovideo_US search initial commit

---
+++ src/library/product/search/stores/b/bhphotovideo/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'bhphotovideo',
+    transform: null,
+    domain: 'bhphotovideo.com',
+    zipcode: '',
+  },
+};
