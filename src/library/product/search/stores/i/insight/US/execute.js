3ba055cc0016f89d005e2704c7af69a445a9f7f4
sivasoarlogic <siva@soarlogic.com>
2020-10-12 20:17:40 +0530

    search

--- src/library/product/search/stores/i/insight/US/execute.js
+++ src/library/product/search/stores/i/insight/US/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'US',
     store: 'insight',
     domain: 'insight.com',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.insight.com/en_US/search.html?qtype=all&q={searchTerms}',
+    loadedSelector: 'div#columns medium-9 result-col js-search-load-result',
+    noResultsXPath: '//div[contains(text(),"Sorry, no items were found. Please click on one of the other categories above, or try a different search.")]',
     zipcode: "''",
   },
 };
