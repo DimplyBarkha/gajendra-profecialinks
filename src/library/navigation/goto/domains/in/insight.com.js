e6670d98317a7effef3afe87cac97ba0ebcb3aec
sivasoarlogic <siva@soarlogic.com>
2020-10-12 17:07:50 +0530

    search

---
+++ src/library/navigation/goto/domains/in/insight.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'insight.com',
+    timeout: 100000,
+    country: 'US',
+    store: 'insight',
+    zipcode: "''",
+  },
+};
