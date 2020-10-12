73733f0d9ead9f9355abc4e7fbd1aab93950d370
Manas Ranjan <manasnilarout@gmail.com>
2020-10-12 18:08:40 +0530

    Updated navigation

--- src/library/navigation/goto/domains/co/courir.com.js
+++ src/library/navigation/goto/domains/co/courir.com.js
@@ -21,7 +21,6 @@ module.exports = {
     // await context.setLoadImages(true);
     // await context.setBlockAds(false);
 
-<<<<<<< HEAD
     try {
       const responseStatus = await context.goto(url, {
         antiCaptchaOptions: {
@@ -42,23 +41,6 @@ module.exports = {
       throw err;
     }
 
-=======
-    const responseStatus = await context.goto(url, {
-      antiCaptchaOptions: {
-        provider: '2-captcha',
-        type: 'GEETEST',
-      },
-      firstRequestTimeout: 60000,
-      timeout: timeout,
-      waitUntil: 'load',
-      checkBlocked: false,
-    });
-    console.log('Status :', responseStatus.status);
-    console.log('URL :', responseStatus.url);
-    await context.evaluate(async function () {      
-      window.location.reload();
-    });
->>>>>>> be4575d82ceb6b4c6a6a282de20654adf63f35ba
     await context.waitForNavigation({ timeout: 30000 });
 
     const checkExistance = async (selector) => {
@@ -74,19 +56,10 @@ module.exports = {
 
     if (isCaptchaFramePresent) {
       try {
-<<<<<<< HEAD
         const isHardBlocked = await context.evaluateInFrame(captchaSelector, function () {
           return document.body.innerText.search('You have been blocked') > -1;
         });
 
-=======
-        const isHardBlocked = await context.evaluateInFrame(
-          captchaSelector,
-          function () {
-            return document.body.innerText.search('You have been blocked') > -1;
-          }
-        );
->>>>>>> be4575d82ceb6b4c6a6a282de20654adf63f35ba
         if (isHardBlocked) {
           console.log('IP is hard blocked');
           throw new Error('Blocked');
@@ -98,14 +71,7 @@ module.exports = {
             // @ts-ignore
             const code = geetest
               .toString()
-<<<<<<< HEAD
               .replace(/appendTo\("#([^"]+)"\)/, 'appendTo(document.getElementById("$1"))');
-=======
-              .replace(
-                /appendTo\("#([^"]+)"\)/,
-                'appendTo(document.getElementById("$1"))',
-              );
->>>>>>> be4575d82ceb6b4c6a6a282de20654adf63f35ba
             return eval(`(${code})('/captcha/geetest');`);
           },
         );
