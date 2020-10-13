f0e8a8f0c1c1d209a5d88f6ec2f4bd5600464f7c
sivasoarlogic <siva@soarlogic.com>
2020-10-13 13:16:39 +0530

    search update

--- src/library/product/search/stores/i/insight/US/paginate.js
+++ src/library/product/search/stores/i/insight/US/paginate.js
@@ -4,10 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'insight',
-    nextLinkSelector: 'div.pagination-container > ul.pagination > li:last-child > a',
+    nextLinkSelector: 'li#nav-page__next-page nav-page__desktop',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'div#columns medium-9 result-col js-search-load-result',
+    loadedSelector: 'div#js-search-product-items',
     noResultsXPath: '//div[contains(text(),"Sorry, no items were found. Please click on one of the other categories above, or try a different search.")]',
     openSearchDefinition: null,
     domain: 'insight.com',
