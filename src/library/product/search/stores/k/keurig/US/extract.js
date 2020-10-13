6a8a4ceebb72b150d8dc4e03ce034b7a079e6e63
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-10-13 13:29:36 +0530

    #691. initial commit for search extractor

---
+++ src/library/product/search/stores/k/keurig/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'keurig',
+    transform: null,
+    domain: 'keurig.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/k/keurig/US/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'US',
+    store: 'keurig',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'keurig.com',
+    zipcode: '',
+  },
+};
