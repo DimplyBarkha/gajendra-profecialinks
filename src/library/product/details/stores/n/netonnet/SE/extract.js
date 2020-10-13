c7b06fb1965cbf22afbd701dbfc47dabeb39aae4
sushma-mangalapally <sushma.mangalapally@import.io>
2020-10-13 11:08:53 -0700

    Add iframe work

--- src/library/product/details/stores/n/netonnet/SE/extract.js
+++ src/library/product/details/stores/n/netonnet/SE/extract.js
@@ -21,6 +21,33 @@ module.exports = {
       }
     });
 
+    try {
+      await context.waitForSelector('iframe[id^="quchbox-videolist"]', { timeout: 45000 });
+    } catch (error) {
+      console.log('No videos loading');
+    }
+
+    await context.evaluate(async function () {
+      // document.body.setAttribute("ii_url", window.location.href);
+
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+
+      if (document.querySelector('iframe[id^="quchbox-videolist"]')) {
+        const videosIds = [...document.querySelector('iframe[id^="quchbox-videolist"]').contentDocument.querySelectorAll('li')].map((video) => video.querySelector('div').getAttribute('data-videoid'));
+        for (let i = 0; i < videosIds.length; i++) {
+          videosIds[i] = `www.youtube.com/embed/${videosIds[i]}?rel=0`;
+        }
+
+        addHiddenDiv('ii_videos', videosIds.join(' || '));
+      }
+    });
+
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.extract(productDetails, { transform });
