4952393b8c55a025d2e5bb1f12c88c56494c3374
vikas kyatannawar <vikas.k@osmosys.asia>
2020-10-13 16:26:14 +0530

    #570 removed asin check

--- src/library/navigation/goto/domains/am/amazon.js
+++ src/library/navigation/goto/domains/am/amazon.js
@@ -1,12 +1,7 @@
 async function implementation ({ url, zipcode, inputs }, { addressRegExp, zipRegExp, countryCode, domain, extractor }, context, { customGoto }) {
   const input = { _url: url, domain, context, asin: (inputs.id || inputs.keywords), extractor };
   // Do we have a way to get extractor id during the running?
-  if (input.asin) {
-    console.log('calling custom goto function');
-    await customGoto(input);
-  } else {
-    throw new Error('ASIN not provided.');
-  }
+  await customGoto(input);
 };
 
 module.exports = {
