0f3c092c47566454b152bf6a9b94074e66d36ea7
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-10-13 14:48:08 +0530

    Added loaded selector and No products found slector

--- src/library/product/search/stores/g/gittigidiyor/TR/execute.js
+++ src/library/product/search/stores/g/gittigidiyor/TR/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'gittigidiyor',
     domain: 'gittigidiyor.com',
     url: `https://www.gittigidiyor.com/arama/?k={searchTerms}`,
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'ul.catalog-view.products-container>li',
+    noResultsXPath: '//div[contains(@class, "no-result-icon")]',
     zipcode: '',
   },
 };
