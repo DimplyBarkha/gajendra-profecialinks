a682bfdcbb83a02b9dd47d4d53058b8361d3f656
Ankit <55212922+osmankit@users.noreply.github.com>
2020-10-13 15:16:00 +0530

    Update amazon.de.js

--- src/library/navigation/goto/domains/am/amazon.de.js
+++ src/library/navigation/goto/domains/am/amazon.de.js
@@ -1,8 +1,8 @@
-module.exports = {
-  extends: 'navigation/goto/domains/am/amazon',
-  parameterValues: {
-    domain: 'amazon.de',
-    country: 'DE',
-    store: 'amazon',
-  },
+module.exports = {	module.exports = {
+  extends: 'navigation/goto/domains/am/amazon',	  extends: 'navigation/goto/domains/am/amazon',
+  parameterValues: {	  parameterValues: {
+    countryCode: 'DE',	    domain: 'amazon.de',
+    // addressRegExp: /adresse/i,	    country: 'DE',
+    // zipRegExp: /\b\d{5}\b/,	    store: 'amazon',
+  },	  },
 };
