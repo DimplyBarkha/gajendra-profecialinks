dc1972b659d9e73efaf7cd257f588b5878191f29
Heramba Rao Boddu <herambarao.boddu@hcl.com>
2020-10-13 19:08:07 +0530

    QA2 fixes

--- src/library/product/details/stores/l/lastprice/IL/extract.js
+++ src/library/product/details/stores/l/lastprice/IL/extract.js
@@ -47,11 +47,29 @@ module.exports = {
       const image = getXpath("//meta[@property='og:image']/@content", 'nodeValue');
       console.log('image fetched ' + image);
       addElementToDocument('added_image', image);
+      let tempImage;
+      if (image.includes('/')) {
+        const imageTemp = image.split('/');
+        imageTemp.forEach(element => {
+          if (element.includes('.jpg')) {
+            element = element.split('.jpg')[0];
+          }
+          if (element.includes('.JPG')) {
+            element = element.split('.JPG')[0];
+          }
+          tempImage = element;
+        });
+      }
 
       const netWeight = getAllXpath("//div[@class='col-lg-8 col-md-12 col-sm-12 pl-20 pr-20']//p/text()", 'nodeValue');
       netWeight.forEach(element => {
-        if (element.includes('משקל') && element.includes(':')) {
-          element = element.split(':')[1];
+        if (element.includes('משקל')) {
+          if (element.includes(':')) {
+            element = element.split(':')[1];
+          }
+          if (element.includes('-')) {
+            element = element.split('-')[1];
+          }
           addElementToDocument('added_netWeight', element);
         }
       });
@@ -66,7 +84,7 @@ module.exports = {
         }
       }
 
-      const description = getAllXpath("//div[@class='shortdec']/p[not(contains(@class,'pt-10 pointer'))]/text()", 'nodeValue').join(' || ');
+      const description = getAllXpath("//div[@class='shortdec']/p[not(contains(@class,'pt-10 pointer'))]/text()", 'nodeValue').join('|');
       console.log('description fetched ' + description);
       addElementToDocument('added_description', description);
 
@@ -84,18 +102,21 @@ module.exports = {
       console.log('manufacturerDescription fetched ' + manufacturerDescription);
       addElementToDocument('added_manufacturerDescription', manufacturerDescription);
 
-      const manufacturerImages = getAllXpath("//div[@id='descr']//div[@class='col-lg-8 col-md-12 col-sm-12 pl-20 pr-20']/div/p//img/@src", 'nodeValue').join('|');
+      const manufacturerImages = getAllXpath("//div[@id='descr']//div//p//img/@src | //tr//td//img/@src", 'nodeValue').join('|');
       console.log('manufacturerImages fetched ' + manufacturerImages);
       addElementToDocument('added_manufacturerImages', manufacturerImages);
-
-      let name = getXpath("//script[@type='application/ld+json']/text()", 'nodeValue');
-      name = JSON.parse(name);
-      console.log('name fetched ' + name);
-      addElementToDocument('added_name', name.name);
-
-      const alternateImages = getAllXpath("//div[@class='ms-thumb-list ms-dir-h']//div[@class='ms-thumb-frame' and position()>2]//img/@src", 'nodeValue').join('|');
-      console.log('alternateImages fetched ' + alternateImages);
-      addElementToDocument('added_alternateImages', alternateImages);
+      const alternateImages = getAllXpath("//div[@class='ms-thumb-list ms-dir-h']//div[@class='ms-thumb-frame' and position()>1]//img/@src", 'nodeValue').join('|');
+      const finalArray = [];
+      if (alternateImages != null && alternateImages.length > 0 && alternateImages.includes('|')) {
+        const alternateArray = alternateImages.split('|');
+        alternateArray.forEach(element => {
+          if (!element.includes(tempImage)) {
+            finalArray.push(element + '|');
+          }
+        });
+      }
+      console.log('alternateImages fetched ' + finalArray);
+      addElementToDocument('added_alternateImages', finalArray);
 
       const warranty = getXpath("//div[@id='WarrantyText']", 'innerText');
       console.log('warranty fetched ' + warranty);
@@ -105,19 +126,11 @@ module.exports = {
       console.log('productOtherInformation fetched ' + productOtherInformation);
       addElementToDocument('added_productOtherInformation', productOtherInformation);
 
-      const specifications = getAllXpath("//div[@id='descr']//div[@class='col-lg-8 col-md-12 col-sm-12 pl-20 pr-20']/div/p[not(img)]", 'innerText');
-      specifications.forEach(element => {
-        if (element.includes('מפרט טכני')) {
-          console.log('specifications fetched ' + element);
-          addElementToDocument('added_specifications', element);
-        }
-      });
+      const specifications = getAllXpath("//div[@id='descr']//div[@class='col-lg-8 col-md-12 col-sm-12 pl-20 pr-20']/div/p/strong[contains(text(),'מפרט')]/parent::p/text()", 'nodeValue').join('||');
+      console.log('specifications fetched ' + specifications);
+      addElementToDocument('added_specifications', specifications);
 
-      const category = getAllXpath("//ol[@class='breadcrumb container']/li[position()>1]/a/text()", 'nodeValue').join(' > ');
-      console.log('category fetched ' + category);
-      addElementToDocument('added_category', category);
-
-      // document.querySelector('.ms-slide-vpbtn').click();
+      addElementToDocument('added_variantCount', 0);
     });
     await context.extract(productDetails);
   },
