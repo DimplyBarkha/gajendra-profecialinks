3159df4aae1e0414333c4c9f725d0dfa2de8c5d8
abhay.patil <abhay.patil@kcsitglobal.com>
2020-10-13 14:30:25 +0530

    resolve serch issues

--- src/library/product/search/stores/r/retravision/AU/extract.js
+++ src/library/product/search/stores/r/retravision/AU/extract.js
@@ -1,5 +1,40 @@
 const { transform } = require('../../../../shared');
 
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async function () {
+    let tempUrl ='';
+    let tempCheck = document.evaluate('//li[contains(@class,"ais-pagination--item ais-pagination--item__page ais-pagination--item__active")]//a//@href', document).iterateNext();      
+    if(tempCheck !=null)
+    {
+      tempUrl = document.evaluate('//li[contains(@class,"ais-pagination--item ais-pagination--item__page ais-pagination--item__active")]//a//@href', document).iterateNext().textContent.trim();                        
+    }
+    function addHiddenDiv (node, id, content) {
+      const newDiv = document.createElement('tempWebUrl-id');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      node.appendChild(newDiv);
+    }
+
+    let i = 0;
+    document.querySelectorAll('div.ais-hit').forEach(node => {    
+      addHiddenDiv(node,'tempWebUrl',tempUrl);
+      i++;
+    });
+  });
+  return await context.extract(productDetails, { transform });
+}
+
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,4 +44,5 @@ module.exports = {
     domain: 'retravision.com.au',
     zipcode: '',
   },
+  implementation
 };
