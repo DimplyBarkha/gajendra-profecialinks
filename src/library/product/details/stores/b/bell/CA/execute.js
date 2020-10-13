38d8493533d0eae16e6ac9f60f70e75f8c47652d
ganesh <ganesh@profecialinks.com>
2020-10-13 18:12:33 +0530

    add click code

--- src/library/product/details/stores/b/bell/CA/execute.js
+++ src/library/product/details/stores/b/bell/CA/execute.js
@@ -1,4 +1,33 @@
 
+/**
+ *
+ * @param { { url?: string,  id?: string, zipcode?: any, storeId?:any} } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action, createUrl} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters);
+  let { url, id, zipcode, storeId } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
+  try {
+    await context.click('div#expandable-section-3');
+  }catch (e) {
+    console.log(e);
+  }
+  
+}
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -9,4 +38,5 @@ module.exports = {
     noResultsXPath: '//main[@class="rsx-page-content error-page"]',
     zipcode: '',
   },
+  implementation,
 };
