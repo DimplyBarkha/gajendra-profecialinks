2dbd0fa3b913cf4ed12e1f577d2d4bcaba27d061
Bindu Datla <bindu.datla@import.io>
2020-10-12 21:18:39 -0400

    update timeout

--- src/library/navigation/goto/domains/ek/ek-onlineshop.at.js
+++ src/library/navigation/goto/domains/ek/ek-onlineshop.at.js
@@ -3,15 +3,18 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'ek-onlineshop.at',
-    timeout: 5000000,
+    timeout: 120000,
     country: 'AT',
     store: 'ek-onlineshop',
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    
     await context.setAntiFingerprint(false);
     await context.setLoadAllResources(true);
     await context.setBlockAds(false);
-    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    await context.setFirstRequestTimeout(60000);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
   },
 };
