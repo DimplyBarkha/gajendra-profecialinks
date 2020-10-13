cb43952fd73b747dc7027635c76f36a23408a3c3
ankit.k@osmosys.asia <richa.t@osmosys.asia>
2020-10-13 16:28:04 +0530

    Created extractor for Amazon FR Promos Issues #352

---
+++ src/library/product/details/stores/a/amazonMsPromos/FR/execute.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'FR',
+    store: 'amazonMsPromos',
+    domain: 'amazon.fr',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "GoldBox"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")]',
+    zipcode: '',
+  },
+};
