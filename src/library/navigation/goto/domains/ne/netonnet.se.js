aa8534d8704dad6fb7aa62900d1142d5ba491136
sushma-mangalapally <sushma.mangalapally@import.io>
2020-10-13 11:29:59 -0700

    Add timeout increase

--- src/library/navigation/goto/domains/ne/netonnet.se.js
+++ src/library/navigation/goto/domains/ne/netonnet.se.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'netonnet.se',
-    timeout: 35000,
+    timeout: 45000,
     country: 'SE',
     store: 'netonnet',
     zipcode: '',
