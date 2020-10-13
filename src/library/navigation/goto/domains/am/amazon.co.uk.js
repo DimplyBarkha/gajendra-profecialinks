2fd85396eab6616ba8843bf3319e1a1db859c338
Ankit <55212922+osmankit@users.noreply.github.com>
2020-10-13 15:15:13 +0530

    Update amazon.co.uk.js

--- src/library/navigation/goto/domains/am/amazon.co.uk.js
+++ src/library/navigation/goto/domains/am/amazon.co.uk.js
@@ -1,9 +1,8 @@
-
-module.exports = {
-  implements: 'navigation/goto',
-  parameterValues: {
-    country: 'UK',
-    domain: 'amazon.co.uk',
-    store: 'amazon',
-  },
-};
+module.exports = {	module.exports = {
+  extends: 'navigation/goto/domains/am/amazon',	  implements: 'navigation/goto',
+  parameterValues: {	  parameterValues: {
+    countryCode: 'UK',	    country: 'UK',
+    // addressRegExp: /address/i,	    domain: 'amazon.co.uk',
+    // zipRegExp: /\b\d{5}\b/,	    store: 'amazon',
+  },	  },
+}; 
