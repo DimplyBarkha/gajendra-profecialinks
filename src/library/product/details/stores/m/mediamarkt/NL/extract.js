b4384ae2553995c38d2a9110814fa5748560abd7
Taras Chuiko <taraschuiko@gmail.com>
2020-10-13 17:51:28 +0300

    #658. Added specifications getting logic

--- src/library/product/details/stores/m/mediamarkt/NL/extract.js
+++ src/library/product/details/stores/m/mediamarkt/NL/extract.js
@@ -8,4 +8,24 @@ module.exports = {
     domain: 'mediamarkt.nl',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+
+      // Getting specifications
+      document.querySelector('.js-toggle-collapsed').click();
+      let specifications = '';
+      document.querySelectorAll('dl.specification').forEach((specificationGroup) => {
+        console.log(specifications += `${specificationGroup.innerText}\n`);
+      });
+      addElementToDocument('mm_specifications', specifications);
+    });
+    await context.extract(productDetails);
+  },
 };
