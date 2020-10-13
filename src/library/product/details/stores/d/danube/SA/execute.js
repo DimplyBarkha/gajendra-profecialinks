878e7d183c7e75c8154516251800a928f1e12f97
Saurabh Ghewari <saurabhghewari@synerzip.com>
2020-10-13 12:40:11 +0530

    #690 Added xpaths and manualSelector. Updated inputs yaml.

---
+++ src/library/product/details/stores/d/danube/SA/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'SA',
+    store: 'danube',
+    domain: 'danube.sa',
+    loadedSelector: 'div.product-show__wrapper',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/d/danube/SA/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'SA',
+    store: 'danube',
+    transform: null,
+    domain: 'danube.sa',
+    zipcode: '',
+  },
+};
