f5834d1309b727f94d48a3a2375ef2fdc6b5e2ca
ankit.k@osmosys.asia <richa.t@osmosys.asia>
2020-10-13 12:26:00 +0530

    Created extractor for amazonMsSearch_IT Issue #325

---
+++ src/library/product/search/stores/a/amazonMsSearch/IT/paginate.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'IT',
+    store: 'amazon',
+    openSearchDefinition: {
+      template: 'https://www.amazon.it/s?k={searchTerms}&__mk_it_IT=ÅMÅŽÕÑ&ref=nb_sb_noss_2&page={page}',
+    },
+    loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "Nessun risultato")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Cerchi qualcosa in particolare")]',
+    domain: 'amazon.it',
+    zipcode: '',
+  },
+};
