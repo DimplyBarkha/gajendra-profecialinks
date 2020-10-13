eac738da326b68d4a2e54bd1a134434272b84d18
Tasos Bitsios <tasos.bitsios@import.io>
2020-10-13 14:10:10 +0200

    courir navigation tentative fixes

--- src/library/navigation/goto/domains/co/courir.com.js
+++ src/library/navigation/goto/domains/co/courir.com.js
@@ -21,27 +21,31 @@ module.exports = {
     // await context.setLoadImages(true);
     // await context.setBlockAds(false);
 
-    try {
-      const responseStatus = await context.goto(url, {
-        antiCaptchaOptions: {
-          provider: '2-captcha',
-          type: 'GEETEST',
-        },
-        firstRequestTimeout: 60000,
-        timeout: timeout,
-        waitUntil: 'load',
-        checkBlocked: true,
-      });
+    async function gotoPage() {
+      try {
+        const responseStatus = await context.goto(url, {
+          antiCaptchaOptions: {
+            provider: '2-captcha',
+            type: 'GEETEST',
+          },
+          firstRequestTimeout: 60000,
+          timeout: timeout,
+          waitUntil: 'load',
+          checkBlocked: true,
+        });
 
-      console.log('NAVIGATED SOMEWHERE.');
-      console.log('Status :', responseStatus.status);
-      console.log('URL :', responseStatus.url);
-    } catch (err) {
-      console.log('Error navigating to page.');
-      throw err;
+        console.log('NAVIGATED SOMEWHERE.');
+        console.log('Status :', responseStatus.status);
+        console.log('URL :', responseStatus.url);
+
+        await context.waitForNavigation({ timeout: 30000 });
+      } catch (err) {
+        console.log('Error navigating to page.');
+        throw err;
+      }
     }
 
-    await context.waitForNavigation({ timeout: 30000 });
+    await gotoPage();
 
     const checkExistance = async (selector) => {
       return await context.evaluate(async (sel) => {
@@ -76,20 +80,31 @@ module.exports = {
           },
         );
 
-        await new Promise(resolve => setTimeout(resolve, 500));
+        // wait for longer than 500ms here
+        await new Promise(resolve => setTimeout(resolve, 10000));
+
         await context.evaluateInFrame('iframe', function () {
           // @ts-ignore
           document.querySelector('.captcha-handler').click();
         });
 
         console.log('Captcha Resolved.');
-
-        await new Promise(resolve => setTimeout(resolve, 10000));
-        await context.waitForSelector('#produit > div.product_head');
       } catch (error) {
-        console.log('error: NO CAPTCHA ENCOUNTER', error);
-        if (error.message === 'Blocked')
-          throw error; // TODO what is the correct way to indicate retry from different virtual browser?
+          console.log('error: CAPTCHA error', error);
+          throw error;
+      }
+      // we may sometimes be redirected to main page instead of detail page, re-check
+      try {
+        // this element exists on product pages
+        await context.waitForSelector('#product-content', { timeout: 20000 });
+      } catch(e) {
+        // if timeout, attempt to go to initial page again
+        await gotoPage();
+
+        const isCaptchaFramePresent = await checkExistance(captchaSelector);
+        if (isCaptchaFramePresent) {
+          throw new Error('Got captcha a second time');
+        }
       }
     } else {
       console.log('NO CAPTCHA ENCOUNTER');
