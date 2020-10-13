ff822fd13efc5301de97c93702c6e9e05452132b
ankit.k@osmosys.asia <richa.t@osmosys.asia>
2020-10-13 12:15:11 +0530

    Created extractor for amazonMsSearch_ES Issue #329

---
+++ src/library/product/search/stores/a/amazonMsSearch/ES/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'amazon',
+    domain: 'amazon.es',
+    url: 'https://www.amazon.es/s?k={searchTerms}&__mk_es_ES=ÅMÅŽÕÑ&ref=nb_sb_noss_2',
+    loadedSelector: 'div[class*="result-list"]>div[data-asin]>div>span[cel_widget_id*="MAIN-SEARCH_RESULTS"]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No hay resultados")] | //img[contains(@alt,"Dogs of Amazon")] | //*[contains(text(),"Buscas algo")]',
+    zipcode: '',
+  },
+};
