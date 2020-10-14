2327f93b2bdfd78fd476febe9aa7d05884b61b22
Nilesh <nilesh.b@osmosys.asia>
2020-10-14 14:42:19 +0530

    Fixed all the issue.

--- src/library/product/details/stores/k/k-ruoka/FI/createUrl.js
+++ src/library/product/details/stores/k/k-ruoka/FI/createUrl.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'k-ruoka.fi',
-    url: 'https://www.k-ruoka.fi/kauppa/tuote/${id}',
+    url: 'https://www.k-ruoka.fi/kauppa/tuote/{id}',
     country: 'FI',
     store: 'k-ruoka',
     zipcode: '',
--- src/library/product/details/stores/k/k-ruoka/FI/extract.js
+++ src/library/product/details/stores/k/k-ruoka/FI/extract.js
@@ -8,27 +8,20 @@ module.exports = {
     transform,
     domain: 'k-ruoka.fi',
     zipcode: '',
-  }, implementation: async (
+  },
+  implementation: async (
     inputs,
     parameters,
     context,
     dependencies,
   ) => {
-    // await context.evaluate(async function () {
-    //   var acceptCookies = document.querySelector('button.accept-button');
-    //   if (acceptCookies) {
-    //     acceptCookies.click()
-    //   }
-
-    // });
     await context.evaluate(async function () {
-      // function timeout(ms) {
-      //   return new Promise((resolve) => setTimeout(resolve, ms));
-      // }
-      // await timeout(5000);
-      const NutritionInfo = document.querySelector('div.shopping-list-product-details-container section.collapsible-container.product-nutritional-detail a');
-      if (NutritionInfo) {
+      var NutritionInfo = document.querySelector('div.shopping-list-product-details-container section.collapsible-container.product-nutritional-detail a');
+      var checkIfNutritionInfoAlreadyOpen = document.querySelector('div.shopping-list-product-details-container section.collapsible-container.product-nutritional-detail.open');
+      if (NutritionInfo && !checkIfNutritionInfoAlreadyOpen) {
         NutritionInfo.click();
+      } else {
+        console.log('already open');
       }
     });
 
