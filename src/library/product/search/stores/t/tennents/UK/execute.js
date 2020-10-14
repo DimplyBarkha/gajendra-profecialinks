072e3760f697d6b8ae44477169af2ca53d08ddda
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-10-14 12:58:00 +0200

    created a template for the search extractor

---
+++ src/library/product/search/stores/t/tennents/UK/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'tennents',
+    domain: 'tennents.co.uk',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/t/tennents/UK/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'tennents',
+    transform: null,
+    domain: 'tennents.co.uk',
+    zipcode: '',
+  },
+};
