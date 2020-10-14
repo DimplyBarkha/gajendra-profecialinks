4a6843b8e8be3c51eab80ee91acfa15e2ca70aaa
Gajender Ramola <ramola106@gmail.com>
2020-10-14 18:30:29 +0530

    remove comments

--- src/library/product/search/stores/h/hulpmiddelwereld/format.js
+++ src/library/product/search/stores/h/hulpmiddelwereld/format.js
@@ -8,23 +8,13 @@ const transform = (data) => {
       var rank_temp = 1
       for (const row of group) {
         row.rank = row.rankOrganic = [{ 'text': rank_temp }];
-        rank_temp = rank_temp + 1;
-        // if (row.productUrl) {
-        //   row.productUrl.forEach(item => {
-        //     item.text = "https://www.noon.com" + item.text;
-        //   });
-        // }
+        rank_temp = rank_temp + 1;        
         if (row.id) {
           row.id.forEach(item => {            
             var arr_temp = item.text.split('/');
             var temp_id = arr_temp[arr_temp.length - 1]
             arr_temp = temp_id.split('?');
-            item.text = arr_temp[0];
-            // var myRegexp = /.+\/(.+?)\?/g;
-            // var match = myRegexp.exec(item.text);
-            // if (match.length) {
-            //     item.text = match[1].trim();
-            // }
+            item.text = arr_temp[0];           
           });
         }
       }
