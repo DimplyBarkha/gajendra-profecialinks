ecb5dc99e14e1c874002b4f89eb87bd52ca6389c
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-10-14 13:48:43 +0200

    created a template for the search extractor

---
+++ src/library/product/search/stores/t/thewhiskyexchange/UK/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'thewhiskyexchange',
+    transform: null,
+    domain: 'thewhiskyexchange.com',
+    zipcode: '',
+  },
+};
