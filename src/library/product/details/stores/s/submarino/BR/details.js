fb49fd89017af3b6d0c0d276821d7cf72bb869e4
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-10-14 12:24:49 +0200

    created a template for the core extractor

---
+++ src/library/product/details/stores/s/submarino/BR/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'BR',
+    store: 'submarino',
+    domain: 'submarino.com.br',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/s/submarino/BR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'BR',
+    store: 'submarino',
+    transform: null,
+    domain: 'submarino.com.br',
+    zipcode: '',
+  },
+};
