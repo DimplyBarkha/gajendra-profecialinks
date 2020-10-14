8973b0377086fbfe10e0c0b9a1ae3dda88b0bb44
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-10-14 21:49:51 +0530

    #710. initial commit for core extractor

---
+++ src/library/navigation/goto/domains/ke/setZipCode.keurig.com.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'US',
+    domain: 'keurig.com',
+    store: 'keurig',
+    zipcode: '',
+  },
+};
