3754a711ab6dba765873d308ec32ff9947c66b18
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-10-14 12:34:43 +0200

    created a template for the core extractor

---
+++ src/library/navigation/goto/domains/su/supply.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'supply.com',
+    timeout: null,
+    country: 'US',
+    store: 'supply',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/s/supply/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'supply',
+    transform: null,
+    domain: 'supply.com',
+    zipcode: '',
+  },
+};
