3204a4f084d0074007e3f239d0f1737aa3f2516e
anu tiwari <anu@anus-MacBook-Pro.local>
2020-10-15 18:48:25 +0545

    Updated extractor.yaml

--- src/library/product/details/stores/a/amazonApparel/DE/createUrl.js
+++ src/library/product/details/stores/a/amazonApparel/DE/createUrl.js
@@ -1,10 +1,9 @@
-
 module.exports = {
-  implements: 'product/details/createUrl',
-  parameterValues: {
-    domain: 'amazon.de',
-    url: 'https://www.amazon.de/dp/{id}',
-    country: 'DE',
-    store: 'amazon',
-  },
+    implements: 'product/details/createUrl',
+    parameterValues: {
+        domain: 'amazon.de',
+        url: 'https://www.amazon.de/dp/{id}',
+        country: 'DE',
+        store: 'amazonApparel',
+    },
 };
