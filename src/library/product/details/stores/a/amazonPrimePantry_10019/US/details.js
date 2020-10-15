685caf97cf8b5bef34a9afc252af590682a91102
anu tiwari <anu@anus-MacBook-Pro.local>
2020-10-15 13:22:20 +0545

    Created extractor for AmazonPantry 10019 Core

---
+++ src/library/product/details/stores/a/amazonPrimePantry_10019/US/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'US',
+    store: 'amazonPrimePantry_10019',
+    domain: 'amazon.com',
+    zipcode: '10019',
+  },
+};
