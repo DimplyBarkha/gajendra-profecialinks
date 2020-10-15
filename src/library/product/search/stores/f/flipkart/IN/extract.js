92cfc7e643957b656d9a8b00021332626086399b
Karolina <karolina.urbanska@tdcm.io>
2020-10-15 12:49:14 +0200

    Fixed thumbnails function

--- src/library/product/search/stores/f/flipkart/IN/extract.js
+++ src/library/product/search/stores/f/flipkart/IN/extract.js
@@ -10,6 +10,13 @@ async function implementation (
   const { productDetails } = dependencies;
   await new Promise((resolve, reject) => setTimeout(resolve, 5000));
   await context.evaluate(async function () {
+    function stall (ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    };
     const GetTagByIdUsingRegex = (tag, id, html) => {
       return new RegExp('<' + tag + "[^>]*id[\\s]?=[\\s]?['\"]" + id + "['\"][\\s\\S]*?</" + tag + '>').exec(html);
     };
@@ -20,6 +27,7 @@ async function implementation (
       catElement.style.display = 'none';
       doc.appendChild(catElement);
     }
+    await stall(10000);
     await fetch(window.location.href, { method: 'GET' }).then(r => r.text()).then(htm => {
       const result = GetTagByIdUsingRegex('script', 'is_script', htm);
       const outerHTML = result && result[0] ? result[0] : '';
@@ -34,12 +42,15 @@ async function implementation (
         const urlData = document.querySelectorAll('a._31qSD5')[i] ? document.querySelectorAll('a._31qSD5')[i].href : document.querySelectorAll('a.Zhf2z-')[i].href;
         document.querySelectorAll('div._3O0U0u>div')[i].setAttribute('producturl', `${urlData}`);
         for (let j = 1; j < widgets.length; j++) {
-          if (widgets[j].widget.data.products) {
-            const productId = widgets[j].widget.data.products[0].productInfo.value.id;
-            const imageUrl = widgets[j].widget.data.products[0].productInfo.value.media.images[0].url;
-            const image = imageUrl.replace(/\{@width\}|\{@height\}/g, '312').replace(/\{@quality\}/g, '70');
-            if (id === productId) {
-              addElementToDocument(productSelectors[i], 'image', image);
+          stall(15000);
+          if (widgets[j].widget.data.products && widgets[j].widget.data.products.length) {
+            for (let k = 0; k < widgets[j].widget.data.products.length; k++) {
+              const productId = widgets[j].widget.data.products[k].productInfo.value.id;
+              const imageUrl = widgets[j].widget.data.products[k].productInfo.value.media.images[0].url;
+              const image = imageUrl.replace(/\{@width\}|\{@height\}/g, '312').replace(/\{@quality\}/g, '70');
+              if (id === productId) {
+                addElementToDocument(productSelectors[i], 'image', image);
+              }
             }
           }
         }
