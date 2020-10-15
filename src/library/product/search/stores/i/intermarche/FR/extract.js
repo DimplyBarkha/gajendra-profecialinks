2dd9b7a629a2969baf48e0456a8ba5e425021a95
Bindu Datla <bindu.datla@import.io>
2020-10-14 22:58:15 -0400

    update pagination

--- src/library/product/search/stores/i/intermarche/FR/extract.js
+++ src/library/product/search/stores/i/intermarche/FR/extract.js
@@ -9,4 +9,30 @@ module.exports = {
     domain: 'intermarche.com',
     zipcode: '',
   },
+  implementation: async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let morePages = true;
+        let pageCount = 0;
+        let scrollTop = 0
+        while (morePages) {
+          await new Promise((resolve) => setTimeout(resolve, 10000));
+          const ele = document.querySelector('div[class*="styled__LoaderWrapper"]');
+          scrollTop = scrollTop + 5000;
+          window.scroll(0, scrollTop);
+          morePages = !!ele;
+          if (pageCount++ == 7) break;
+        }
+      });
+    }
+    await applyScroll(context);
+    return await context.extract(productDetails, { transform });
+  }
 };
