e66b7f7993d386c934b980a17402497a57493b10
Gajender Ramola <ramola106@gmail.com>
2020-10-15 22:00:36 +0530

    apply pagination

--- src/library/product/search/stores/h/hulpmiddelwereld/NL/execute.js
+++ src/library/product/search/stores/h/hulpmiddelwereld/NL/execute.js
@@ -26,6 +26,28 @@ async function implementation (
     console.log(e);
   }
   await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        resolve();
+        }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
   return await context.evaluate(function (xp) {
     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
     console.log(xp, r);
