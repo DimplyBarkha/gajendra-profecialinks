d7c66ba51913992a0661a05d3ae24eb6d3e34b26
anu tiwari <anu@anus-MacBook-Pro.local>
2020-10-15 19:55:16 +0545

    Added implementatiom

---
+++ src/library/product/details/stores/a/amazonPrimePantry_45202/shared.js
@@ -0,0 +1,19 @@
+module.exports.implementation = async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
+) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.waitForSelector('div[data-feature-name="wayfinding-breadcrumbs"]');
+    await context.evaluate(async function() {
+        await new Promise(resolve => setTimeout(resolve, 2814));
+        const element = document.getElementById('aplus');
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+            await new Promise(resolve => setTimeout(resolve, 2197));
+        }
+    });
+    return await context.extract(productDetails, { transform });
+}
