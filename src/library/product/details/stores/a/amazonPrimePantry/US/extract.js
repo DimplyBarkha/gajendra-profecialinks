332b7027fb391e14b76642c32b90d972e9835ce7
anu tiwari <anu@anus-MacBook-Pro.local>
2020-10-15 11:04:40 +0545

    Updated extractor for AmazonPrimePantry US

--- src/library/product/details/stores/a/amazonPrimePantry/US/extract.js
+++ src/library/product/details/stores/a/amazonPrimePantry/US/extract.js
@@ -1,28 +1,14 @@
-const { transform } = require('../../../../sharedAmazon/transform');
+const { transform } = require('../../../../sharedAmazon/transformNew');
+const { implementation } = require('../shared');
 
 module.exports = {
-  implements: 'product/details/extract',
-  parameterValues: {
-    country: 'US',
-    store: 'amazonPrimePantry',
-    transform,
-    domain: 'amazon.com',
-    zipcode: '10001',
-  },
-  implementation: async (inputs,
-    parameters,
-    context,
-    dependencies) => {
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    await context.evaluate(async function () {
-      await new Promise(resolve => setTimeout(resolve, 2814));
-      const element = document.getElementById('aplus');
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-        await new Promise(resolve => setTimeout(resolve, 2197));
-      }
-    });
-    return await context.extract(productDetails, { transform });
-  },
+    implements: 'product/details/extract',
+    parameterValues: {
+        country: 'US',
+        store: 'amazonPrimePantry',
+        transform,
+        domain: 'amazon.com',
+        zipcode: '10001',
+    },
+    implementation,
 };
