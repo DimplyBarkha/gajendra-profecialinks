119b2cc8f19c2d34fd1fc9574dc3e6cb3ed199a0
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-10-15 13:36:03 +0530

    #691. change price and added scroll code to load

--- src/library/product/search/stores/k/keurig/US/extract.js
+++ src/library/product/search/stores/k/keurig/US/extract.js
@@ -9,4 +9,23 @@ module.exports = {
     domain: 'keurig.com',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async function () {
+      let scrollSelector = document.querySelector('div.footer-wrapper');
+      // @ts-ignore
+      let scrollLimit = scrollSelector ? scrollSelector.offsetTop : '';
+      let yPos = 0;
+      while (scrollLimit && yPos < scrollLimit) {
+        yPos = yPos + 350;
+        window.scrollTo(0, yPos);
+        scrollSelector = document.querySelector('div.footer-wrapper');
+        // @ts-ignore
+        scrollLimit = scrollSelector ? scrollSelector.offsetTop : '';
+        await new Promise(resolve => setTimeout(resolve, 3500));
+      }
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
