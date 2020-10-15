8a1aeb142aff8f3e38ef50abcb0dc9ab0c33f0c4
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-10-15 17:54:06 +0530

    #710. initial commit for variant extractor

---
+++ src/library/product/details/stores/k/keurig/US/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'US',
+    store: 'keurig',
+    transform: null,
+    domain: 'keurig.com',
+    zipcode: '',
+  },
+};
