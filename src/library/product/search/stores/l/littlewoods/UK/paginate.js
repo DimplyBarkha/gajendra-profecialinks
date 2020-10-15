84bf08939cfb83bcf8006407a27526bb82e373f2
sirisha <sirisha.thotla-odc@partner.import.io>
2020-10-16 00:02:29 +0530

    Search_url issue fixed

--- src/library/product/search/stores/l/littlewoods/UK/paginate.js
+++ src/library/product/search/stores/l/littlewoods/UK/paginate.js
@@ -4,10 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'UK',
     store: 'littlewoods',
-    nextLinkSelector: null,
+    nextLinkSelector: '#main > div.viewOptions.viewOptionsFooter > div.pagination > ul > li:nth-child(4) > a',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: '#products>ul',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'littlewoods.com',
