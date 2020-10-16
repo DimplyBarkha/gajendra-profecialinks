4569d760ac7612c1b6c7757b6f7eb99591fa2124
Karina Miernik <karina.miernik@gmail.com>
2020-10-16 12:36:30 +0200

    modified paginate.js

--- src/library/product/search/stores/t/thefragranceshop/UK/paginate.js
+++ src/library/product/search/stores/t/thefragranceshop/UK/paginate.js
@@ -1,14 +1,13 @@
-
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'UK',
     store: 'thefragranceshop',
-    nextLinkSelector: null,
+    nextLinkSelector: 'li:last-child a.btn.btn-default.btn-smangle-left',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div.col-sm-12.col-xs-12.no-padding.gridView',
+    noResultsXPath: '//div[@class="fnl-landing-noresults"]',
     openSearchDefinition: null,
     domain: 'thefragranceshop.co.uk',
     zipcode: '',
