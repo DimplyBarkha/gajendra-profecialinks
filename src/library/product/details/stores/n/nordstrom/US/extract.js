6dd012d9eb42649820d0f21deaa7f84125546dde
Nilesh <nilesh.b@osmosys.asia>
2020-10-17 00:16:37 +0530

    updated code.

--- src/library/product/details/stores/n/nordstrom/US/extract.js
+++ src/library/product/details/stores/n/nordstrom/US/extract.js
@@ -15,10 +15,13 @@ module.exports = {
   ) => {
     await context.evaluate(async function () {
      
+     //selecting first variant to get price in case of range price
       document.querySelector('ul#size-filter-product-page-option-list li') && document.querySelector('ul#size-filter-product-page-option-list li').click();
       document.querySelector('ul#product-page-swatches li button') && document.querySelector('ul#product-page-swatches li button').click();
       document.querySelector('div#size-filter-product-page-anchor') && document.querySelector('div#size-filter-product-page-anchor').click();
 
+
+      //video Url
       const videoUrls = [];
       const dataArr = window.__INITIAL_CONFIG__.viewData;
       const videoID = dataArr.salesVideoShot ? dataArr.salesVideoShot.id : dataArr.vendorVideoShot ? dataArr.vendorVideoShot.id : '';
@@ -36,72 +39,25 @@ module.exports = {
         });
       }
 
-
-
-      var variantArr = [];
-      var colorArr = [];
-      var sizeArr = [];
-      var variantCount = 0;
-      var variants = window.__INITIAL_CONFIG__.stylesById.data;
-      if (Object.keys(variants).length) {
-        var checksize = variants && variants[Object.keys(variants)[0]] && variants[Object.keys(variants)[0]].filters && variants[Object.keys(variants)[0]].filters.size && variants[Object.keys(variants)[0]].filters.size.byId;
-        var checkcolour = variants && variants[Object.keys(variants)[0]].filters && variants[Object.keys(variants)[0]].filters.color && variants[Object.keys(variants)[0]].filters.color.allIds;
-        var getColour = variants && variants[Object.keys(variants)[0]].filters && variants[Object.keys(variants)[0]].filters.color && variants[Object.keys(variants)[0]].filters.color.byId;
-        if (Object.keys(checksize).length && checkcolour.length > 1) {
-          for (const property in checksize) {
-            const variantId = `${property} - ${checksize[property].isAvailableWith.substring(0, checksize[property].isAvailableWith.length - 1).replace(/_c:/g, '')}`
-            var variantCount = variantCount +  Number(`${checksize[property].relatedSkuIds.length}`);
-            sizeArr.push(property);
-            variantArr.push(variantId);
-            console.log(variantId);
+      //variants
+      var filterOption = window.__INITIAL_CONFIG__.stylesById.data[Object.keys(window.__INITIAL_CONFIG__.stylesById.data)[0]].filterOptions;
+      var filters = window.__INITIAL_CONFIG__.stylesById.data[Object.keys(window.__INITIAL_CONFIG__.stylesById.data)[0]].filters;
+      var styleMediaa = window.__INITIAL_CONFIG__.stylesById.data[Object.keys(window.__INITIAL_CONFIG__.stylesById.data)[0]].styleMedia.byId;
+      styleMediaa[Object.keys(styleMediaa)[1]]
+      var allIds= [] ;
+      for (const property in filters) {
+          for (let index = 0; index < filterOption.length; index++) {
+              if (filterOption[index] === property) {
+                  allIds.push(filters[property].allIds)
+              }
           }
-         
-       
+      }
+      // console.log(allColorIds);
+      
+      for (let index = 0; index < allIds[0].length; index++) {
+          console.log(styleMediaa[Object.keys(styleMediaa)[index]]);
+      }
 
-        }
-        if (checkcolour.length && !Object.keys(checksize).length) {
-          variantArr = checkcolour;
-          variantCount = variantArr.length
-        }
-        if (checkcolour.length === 1) {
-          for (const property in checksize) {
-            const variantId = `${property}`
-            variantArr.push(variantId);
-            variantCount = variantArr.length
-            console.log(`${property}`);
-          }
-        }
-      }
-      variantArr.map(ele => {
-        const element = document.createElement('a');
-        element.setAttribute('class', 'appendedvariantid');
-        element.href = ele;
-        document.body.appendChild(element);
-      });
-      document.querySelector('div#size-filter-product-page-anchor') && document.querySelector('div#size-filter-product-page-anchor').click();
-      for (const property in getColour) {
-        colorArr.push(`${getColour[property].value}`);
-      }
-      colorArr.map(ele => {
-        const element = document.createElement('a');
-        element.setAttribute('class', 'appendedcolor');
-        element.href = ele;
-        document.body.appendChild(element);
-      });
-
-      if(variantCount > 1) {
-        document.body.setAttribute('variant_count',variantCount.toString())
-      }
-      const finalArray = [];
-      const rpc = dataArr && dataArr.skus && dataArr.skus.allIds && dataArr.skus.allIds[0] ? dataArr.skus.allIds[0] : '';
-      const size = dataArr && dataArr.filters && dataArr.filters.size && dataArr.filters.size.allIds;
-      const object = { rpc, size };
-      finalArray.push(object);
-      if (finalArray) {
-        for (const key in finalArray[0]) {
-          document.body.setAttribute(key, finalArray[0][key]);
-        }
-      }
     });
     const { transform } = parameters;
     const { productDetails } = dependencies;
