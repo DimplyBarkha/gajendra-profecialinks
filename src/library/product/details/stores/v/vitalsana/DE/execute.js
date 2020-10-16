0d0137a76ee96f791a6f271bf10998955f25bf41
Karthickravi <karthik.ravi-odc@partner.import.io>
2020-10-16 18:31:43 +0530

    xpath for core

---
+++ src/library/product/details/stores/v/vitalsana/DE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'vitalsana',
+    domain: 'vitalsana.com',
+    loadedSelector: '#maincontent',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/v/vitalsana/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'vitalsana',
+    transform: null,
+    domain: 'vitalsana.com',
+    zipcode: '',
+  },
+};
