73c346093c528374b14f5702deaf124fe71c3a97
abhay.patil <abhay.patil@kcsitglobal.com>
2020-10-16 22:42:00 +0530

    resolve paginate issues

--- src/library/navigation/goto/domains/re/retravision.com.au.js
+++ src/library/navigation/goto/domains/re/retravision.com.au.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'retravision.com.au',
-    timeout: 50000,
+    timeout: 50000000,
     country: 'AU',
     store: 'retravision',
     zipcode: '',
--- src/library/product/search/stores/r/retravision/AU/paginate.js
+++ src/library/product/search/stores/r/retravision/AU/paginate.js
@@ -1,15 +1,77 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+  ) {
+  const { keywords, page, offset } = inputs;
+  const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
+  if (nextLinkSelector) {
+  
+   const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector), nextLinkSelector);
+  if (!hasNextLink) {
+  return false;
+  }
+  }
+  
+   if(!openSearchDefinition){
+  const { pager } = dependencies;
+  const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
+  if (success) {
+  return true;
+  }
+  }
+  
+   let url = await context.evaluate(function () {
+  /** @type { HTMLLinkElement } */
+  const next = document.querySelector('div#instant-search-pagination-container > div > ul.ais-pagination > li.ais-pagination--item__next > a');
+  if (!next) {
+  return false;
+  }
+  return next.href;
+  });
+  
+   if (!url && openSearchDefinition) {
+     console.log("test data page:",page)
+  url = openSearchDefinition.template
+  .replace('{searchTerms}', encodeURIComponent(keywords))
+  .replace('{page}', (page + (openSearchDefinition.pageOffset || 0)).toString())
+  .replace('{offset}', (offset + (openSearchDefinition.indexOffset || 0)).toString());
+  }
+  
+   if (!url) {
+  return false;
+  }
+  
+  console.log('Going to url', url);
+  await dependencies.goto({ url });
+  if (loadedSelector) {
+  await context.waitForFunction(function (sel, xp) {
+  return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+  }, { timeout: 200000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate(function (xp) {
+  const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+  //console.log(xp, r);
+  const e = r.iterateNext();
+  //console.log(e);
+  return !e;
+  }, noResultsXPath);
+  }
 
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'AU',
     store: 'retravision',
-    // nextLinkSelector: 'li.ais-pagination--item__next > a',   
+    nextLinkSelector: 'div#instant-search-pagination-container > div > ul.ais-pagination > li.ais-pagination--item__next > a',   
     loadedSelector: 'div#instant-search-results-container > div.ais-hits',
     noResultsXPath: '//div[@class="no-results"]',
     domain: 'retravision.com.au',
     openSearchDefinition: {
-      template: 'https://www.retravision.com.au/#q={searchTerms}&idx=retravision_default_products&p={page}&is_v=1',
+      template: 'https://www.retravision.com.au/#q={searchTerms}&idx=retravision_default_products&p={page}',
     },
   },
+  implementation
 };
