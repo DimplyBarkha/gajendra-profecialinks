75ee25f659ada0d776ea19e90178c47d337abf19
BorysMaciuch <borysmaciuch@gmail.com>
2020-10-16 09:45:40 +0200

    Init core extractor

---
+++ src/library/product/details/stores/t/teknosa/TR/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'teknosa.com',
+    prefix: null,
+    url: null,
+    country: 'TR',
+    store: 'teknosa',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/t/teknosa/TR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'TR',
+    store: 'teknosa',
+    transform: null,
+    domain: 'teknosa.com',
+    zipcode: '',
+  },
+};
