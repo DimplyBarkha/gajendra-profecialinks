e737de3271c738df8094f372ddd4eb8fde903b75
astropaula <wojciechowska.fran@gmail.com>
2020-10-16 17:14:44 +0200

    Initial commit

--- src/library/product/search/stores/s/supply/US/execute.js
+++ src/library/product/search/stores/s/supply/US/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'US',
     store: 'supply',
     domain: 'supply.com',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.supply.com/search?terms={searchTerms}',
+    loadedSelector: 'div[class="w-100 mb3 flex flex-column flex-row-l flex-wrap-l justify-between"]',
+    noResultsXPath: '//div[@class="pr3 stymie large _1Wa5W"]',
     zipcode: '',
   },
 };
