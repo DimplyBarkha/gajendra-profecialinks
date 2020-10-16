983434b08e7b9446843315e73fadceef8dd0fbb5
Karina Miernik <karina.miernik@gmail.com>
2020-10-16 12:36:14 +0200

    modified extract.js

--- src/library/product/search/stores/t/thefragranceshop/UK/extract.js
+++ src/library/product/search/stores/t/thefragranceshop/UK/extract.js
@@ -1,3 +1,37 @@
+async function implementation(inputs, parameters, context, dependencies) {
+  const {
+    transform,
+  } = parameters;
+  const {
+    productDetails,
+  } = dependencies;
+
+  await new Promise((resolve, reject) => setTimeout(resolve, 1500));
+
+  await context.evaluate(() => {
+    function addProp(selector, iterator, propName, value) {
+      document
+        .querySelectorAll(selector)[iterator].setAttribute(propName, value);
+    }
+    const allProducts = document.querySelectorAll(
+      'div.item-sku',
+    );
+    for (let i = 0; i < allProducts.length; i++) {
+      addProp(
+        'div.item-sku',
+        i,
+        'rankorganic',
+        `${i + 1}`,
+      );
+    }
+  });
+
+  await new Promise((resolve, reject) => setTimeout(resolve, 1500));
+
+  return await context.extract(productDetails, {
+    transform,
+  });
+}
 
 module.exports = {
   implements: 'product/search/extract',
@@ -8,4 +42,5 @@ module.exports = {
     domain: 'thefragranceshop.co.uk',
     zipcode: '',
   },
+  implementation,
 };
