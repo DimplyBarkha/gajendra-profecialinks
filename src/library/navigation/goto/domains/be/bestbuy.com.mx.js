a234a05466683c8b87bab4330d23e997d370847a
ganesh <ganesh@profecialinks.com>
2020-10-16 10:07:22 +0530

    initial commit

---
+++ src/library/navigation/goto/domains/be/bestbuy.com.mx.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'bestbuy.com.mx',
+    timeout: null,
+    country: 'MX',
+    store: 'bestbuy',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/b/bestbuy/MX/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'MX',
+    store: 'bestbuy',
+    transform: null,
+    domain: 'bestbuy.com.mx',
+    zipcode: '',
+  },
+};
