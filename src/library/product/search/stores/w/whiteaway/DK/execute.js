39565aaf5b237107e3cae9b8b35d8edb67ffe6f2
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-10-17 00:10:52 +0530

    whiteaway search - Damayanthi

---
+++ src/library/product/search/stores/w/whiteaway/DK/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'DK',
+    store: 'whiteaway',
+    domain: 'whiteaway.com',
+    url: 'https://www.whiteaway.com/search_result/?keywords=bosch%20idos#/',
+    loadedSelector: 'body',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/w/whiteaway/DK/extract.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'DK',
+    store: 'whiteaway',
+    transform: null,
+    domain: 'whiteaway.com',
+    zipcode: '',
+  },
+};
+
