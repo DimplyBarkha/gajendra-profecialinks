054ba4dabfc43ee1812a33cf5b4963f5d5ad44c6
anu tiwari <anu@anus-MacBook-Pro.local>
2020-10-16 17:44:58 +0545

    Extractor for Amazon Apparel US

--- src/library/product/details/stores/a/amazonApparel/US/extract.js
+++ src/library/product/details/stores/a/amazonApparel/US/extract.js
@@ -1,10 +1,10 @@
-const { transform } = require('../../amazonPharmapacks/US/shared');
+const { transform } = require('../../../../sharedAmazon/transformNew');
 module.exports = {
-  implements: 'product/details/extract',
-  parameterValues: {
-    country: 'US',
-    store: 'amazonApparel',
-    transform,
-    domain: 'amazon.com',
-  },
+    implements: 'product/details/extract',
+    parameterValues: {
+        country: 'US',
+        store: 'amazonApparel',
+        transform,
+        domain: 'amazon.com',
+    },
 };
