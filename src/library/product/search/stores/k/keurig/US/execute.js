0652f9695fea29820f4acd82081fe7ae71950404
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-10-16 20:19:17 +0530

    #691. added more noResultXpath

--- src/library/product/search/stores/k/keurig/US/execute.js
+++ src/library/product/search/stores/k/keurig/US/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'keurig.com',
     url: 'https://www.keurig.com/search?text={searchTerms}',
     loadedSelector: 'div.top_content img,img#base_image',
-    noResultsXPath: '//div[@class="search-result-heading-empty"] | //h1[contains(.,"Sorry...")] | //div[contains(@class,"grid-area") and contains(.,"No products found")]',
+    noResultsXPath: '//div[@class="search-result-heading-empty"] | //h1[contains(.,"Sorry...")] | //div[contains(@class,"grid-area") and contains(.,"No products found")] | //div[contains(@class,"availablity-form")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/k/keurig/US/paginate.js
+++ src/library/product/search/stores/k/keurig/US/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div.top_content img,img#base_image',
-    noResultsXPath: '//div[@class="search-result-heading-empty"] | //h1[contains(.,"Sorry...")] | //div[contains(@class,"grid-area") and contains(.,"No products found")]',
+    noResultsXPath: '//div[@class="search-result-heading-empty"] | //h1[contains(.,"Sorry...")] | //div[contains(@class,"grid-area") and contains(.,"No products found")] | //div[contains(@class,"availablity-form")]',
     openSearchDefinition: null,
     domain: 'keurig.com',
     zipcode: '',
