7a8d12a4240ab0ba955e6b84f0c62545767776b0
judhisahoo2009 <judhisahoo@hotmail.com>
2020-10-17 22:44:15 +0530

    updating

--- src/library/product/search/stores/w/whiskyzone/DE/execute.js
+++ src/library/product/search/stores/w/whiskyzone/DE/execute.js
@@ -59,7 +59,7 @@ module.exports = {
     store: 'whiskyzone',
     domain: 'whiskyzone.de',
     url: 'https://www.whiskyzone.de/search?sSearch={searchTerms}',
-    loadedSelector: 'div.listing--container',
+    loadedSelector: 'div.listing--wrapper',
     noResultsXPath: '//div[@class="alert--content" and normalize-space()="Leider wurden zu Ihrer Suchanfrage keine Artikel gefunden"]/text()',
     zipcode: '',
   },
--- src/library/product/search/stores/w/whiskyzone/DE/paginate.js
+++ src/library/product/search/stores/w/whiskyzone/DE/paginate.js
@@ -7,7 +7,7 @@ module.exports = {
     nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'div.listing--container',
+    loadedSelector: 'div.listing--wrapper',
     noResultsXPath: '//div[@class="alert--content" and normalize-space()="Leider wurden zu Ihrer Suchanfrage keine Artikel gefunden"]/text()',
     openSearchDefinition: null,
     domain: 'whiskyzone.de',
