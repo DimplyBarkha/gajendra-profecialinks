b22f299b3309cd3a2ade9ad3045fca97fbf2ef0b
Taras Chuiko <taraschuiko@gmail.com>
2020-10-17 11:26:07 +0300

    #787. Fixed TypeError: Cannot read property 'replace' of null

--- src/library/product/search/stores/k/kalunga/BR/extract.js
+++ src/library/product/search/stores/k/kalunga/BR/extract.js
@@ -1,11 +1,26 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'BR',
     store: 'kalunga',
-    transform: null,
+    transform: transform,
     domain: 'kalunga.com.br',
     zipcode: '',
+  },
+  async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+
+    await context.evaluate(() => {
+    });
+
+    return await context.extract(productDetails, { transform });
   },
 };
