871666f3bf2769240baf3186241511df4df73d1e
unknown <leena.gavali-odc@partner.import.io>
2020-10-17 22:25:02 +0530

    tigerdirect core

--- src/library/product/search/stores/b/bestwaywholesale/UK/search.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  implements: 'product/search',
-  parameterValues: {
-    country: 'UK',
-    store: 'bestwaywholesale',
-    domain: 'bestwaywholesale.co.uk',
-  },
-};
--- src/library/product/search/stores/l/lowes/US/execute.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  implements: 'product/search/execute',
-  parameterValues: {
-    country: 'US',
-    store: 'lowes',
-    domain: 'lowes.com',
-    url: 'https://www.lowes.com/search?searchTerm={searchTerms}',
-  },
-};
