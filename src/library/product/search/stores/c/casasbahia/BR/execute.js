e4af4bf1894a2a4dd149fe42c1a04600843dc171
Gajender Ramola <ramola106@gmail.com>
2020-10-18 12:31:50 +0530

    fix price issue

---
+++ src/library/product/search/stores/c/casasbahia/BR/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'BR',
+    store: 'casasbahia',
+    domain: 'casasbahia.com.br',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/c/casasbahia/BR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'BR',
+    store: 'casasbahia',
+    transform: null,
+    domain: 'casasbahia.com.br',
+    zipcode: '',
+  },
+};
