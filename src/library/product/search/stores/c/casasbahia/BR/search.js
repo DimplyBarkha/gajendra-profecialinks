d169543bb5144303357ede59d809bd174ca08c80
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-10-18 15:35:14 +0530

    Search implemented

---
+++ src/library/product/search/stores/c/casasbahia/BR/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'BR',
+    store: 'casasbahia',
+    domain: 'casasbahia.com.br',
+    zipcode: "''",
+  },
+};
