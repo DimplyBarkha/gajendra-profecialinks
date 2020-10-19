12868862a82f5240eb97d79ecd5b6af28a6480ed
srishivams <srishivams97@gmail.com>
2020-10-19 21:52:14 +0530

    extracted more aplus images

--- src/library/product/details/stores/f/farmers/NZ/extract.js
+++ src/library/product/details/stores/f/farmers/NZ/extract.js
@@ -23,10 +23,56 @@ module.exports = {
     if (Object.keys(imageEle).length !== 0) {
       await context.waitForSelector('div#inpage_container img');
     }
-    await context.evaluate(async () => {
+
+    const productUrl = await context.evaluate(async () => {
+      const url = window.location.href;
+      return url;
+    });
+
+    const iframeSelector = 'div#inpage_container iframe';
+    let newImages = null;
+    const checkExistance = async (selector) => {
+      return await context.evaluate(async (currentSelector) => {
+        return await Boolean(document.querySelector(currentSelector));
+      }, selector);
+    };
+    if (await checkExistance(iframeSelector)) {
+      const iframeUrl = await context.evaluate((iframeSelector) => {
+        return document.querySelector(iframeSelector).getAttribute('src');
+      }, iframeSelector);
+      await context.goto(iframeUrl, { timeout: 50000, waitUntil: 'networkidle0', checkBlocked: true });
+      await context.waitForXPath('//img');
+      const lateVideoElement = 'div[class*="video-container"] video';
+      if (await checkExistance(lateVideoElement)) {
+        await context.evaluate((lateVideoElement) => {
+          const ele = document.querySelector(lateVideoElement);
+          ele.scrollIntoView({ behavior: 'smooth' });
+        }, lateVideoElement);
+      }
+      newImages = await context.evaluate(() => {
+        const src = ele('img');
+        function ele (tag) {
+          return document.querySelectorAll(tag);
+        }
+        const value = [];
+        retrieve(src);
+        function retrieve (src) {
+          for (let i = 0; i < src.length; i++) {
+            value.push(src[i].src);
+          }
+        }
+        return value;
+      });
+      await context.goto(productUrl, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    }
+    console.log(newImages + ' are new images and product url is ' + productUrl);
+
+    await context.evaluate(async (newImages) => {
       var src = '';
+      // let iframeLink='';
       const imageEle = document.querySelectorAll('div#inpage_container img');
       const videoEle = document.querySelector('a.phone-thumb-overlay-YouTube');
+      // const iframeEle=document.querySelector('div#inpage_container iframe');
       if (videoEle) {
         videoEle.click();
         src = document.querySelector('div.ish-product-photo iframe').getAttribute('src');
@@ -34,6 +80,38 @@ module.exports = {
           addHiddenDiv('video-url', src);
         }
       }
+
+      if (newImages) {
+        let str = '';
+        for (let i = 0; i < newImages.length; i++) {
+          if (i !== newImages.length - 2) {
+            if (i !== newImages.length - 1) { str += newImages[i] + ' | '; }
+          }
+          if (i === newImages.length - 3) {
+            str += newImages[i];
+          }
+        // addNewHiddenDiv(newImages[i]);
+        // console.log(newImages[i]);
+        }
+        addNewHiddenDiv(str);
+      }
+      // if(iframeEle){
+      //   // iframeLink=iframeEle.getAttribute('src');
+      //   let iWindow = iframeEle.contentWindow;
+      //   iWindow.addEventListener("load", function() {
+      //     // get the document from the window
+      //     let doc = iframeEle.contentDocument || iframeEle.contentWindow.document;
+      //     // find the target in the iframe content
+      //     let target = doc.getElementById("img");
+      //     target.innerHTML = "Found It!";
+      // });
+      //   // context.goto(src);
+      // }
+
+      // if(Object.keys(newImages).length !== 0){
+      //   for(let i=0;i<newImages.length;i++)
+      //     addHiddenDiv('manufacture-images', newImages[i]);
+      // }
       if (Object.keys(imageEle).length !== 0) {
         var urls = [];
         imageEle.forEach((ele) => {
@@ -51,14 +129,21 @@ module.exports = {
       if (manufacturer) {
         addHiddenDiv('manufacture-name', manufacturer);
       }
-      function addHiddenDiv(id, content) {
+      function addHiddenDiv (id, content) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
         newDiv.textContent = content;
         newDiv.style.display = 'none';
         document.body.appendChild(newDiv);
       }
-    });
+      function addNewHiddenDiv (imageUrl) {
+        const newDiv2 = document.createElement('div');
+        newDiv2.setAttribute('class', 'extra-manufacturer-images');
+        newDiv2.textContent = imageUrl;
+        newDiv2.style.display = 'none';
+        document.body.appendChild(newDiv2);
+      }
+    }, newImages);
     return await context.extract(productDetails, { transform });
   },
 };
