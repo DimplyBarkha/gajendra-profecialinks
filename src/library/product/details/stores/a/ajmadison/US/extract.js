cf690873446b4c7f699f5c2cc9d7071824c0e5e7
venkatsn <venkat.sn-odc@partner.import.io>
2020-10-19 22:55:17 +0530

    core for aj madison pushed by Deepak

--- src/library/product/details/stores/a/ajmadison/US/extract.js
+++ src/library/product/details/stores/a/ajmadison/US/extract.js
@@ -8,4 +8,39 @@ module.exports = {
     domain: 'ajmadison.com',
     zipcode: '',
   },
+  implementation: async ({ url }, { country, domain }, context, dependencies) => {
+    await context.evaluate(() => {
+
+      function addElementToDocument(key, value) {
+
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      // @ts-ignore
+      const variantInformation = window.dataLayer[0].ecommerce.detail.products[0].variant;
+      addElementToDocument('variant', variantInformation)
+
+      // // Method to Retrieve Xpath content of a Multiple Nodes
+      // const getAllXpath = (xpath, prop) => {
+      //   const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      //   const result = [];
+      //   for (let index = 0; index < nodeSet.snapshotLength; index++) {
+      //     const element = nodeSet.snapshotItem(index);
+      //     if (element) result.push(prop ? element[prop] : element.nodeValue);
+      //   }
+      //   return result;
+      // };
+      // // Double Pipe Concatenation
+      // const pipeSeparatorDouble = (id, data) => {
+      //   var doubleSeparatorText = data.join(' | ');
+      //   addElementToDocument(id, doubleSeparatorText)
+      // };
+      // const addDescription = getAllXpath("//div[@id='quick_specs']//div[@class='bold black-0'][.//text()[contains(.,'Dimensions')]]//following::div[1]", 'nodeValue');
+      // pipeSeparatorDouble('addDescription', addDescription);
+    });
+    await context.extract(dependencies.productDetails);
+  },
 };
