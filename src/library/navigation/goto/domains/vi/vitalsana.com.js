59cf7c38333df86ff8d89b0d94fb682a1483e35d
Karthickravi <karthik.ravi-odc@partner.import.io>
2020-10-19 12:21:38 +0530

    karthickravi

--- src/library/navigation/goto/domains/vi/vitalsana.com.js
+++ src/library/navigation/goto/domains/vi/vitalsana.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'vitalsana.com',
-    timeout: 40000,
+    timeout: 80000,
     country: 'DE',
     store: 'vitalsana',
     zipcode: '',
