bd140a28aa1feea33befc25642c5e2ab6a5245c4
Prasanna Barate <prasanna.barate@synerzip.com>
2020-10-19 19:27:59 +0530

    (#678) Fixed QA1 comments

--- src/library/product/details/stores/e/ebag/BG/extract.js
+++ src/library/product/details/stores/e/ebag/BG/extract.js
@@ -44,41 +44,65 @@ module.exports = {
           cal = cal + ' ' + calUom;
           addHiddenDiv('caloriesPerServ', cal);
 
-          const totalFatPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(4) > td:nth-child(2)');
-          const fat = totalFatPerServ && totalFatPerServ.innerHTML ? totalFatPerServ.innerHTML : '';
-          addHiddenDiv('totalFatPerServ', fat);
-          const fatUom = 'r';
-          addHiddenDiv('fat_uom', fatUom);
-
-          const saturatedFatPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(5) > td:nth-child(2)');
-          const sfat = saturatedFatPerServ && saturatedFatPerServ.innerHTML ? saturatedFatPerServ.innerHTML : '';
-          addHiddenDiv('saturatedFatPerServ', sfat);
-          const sfatUom = 'r';
-          addHiddenDiv('sfat_uom', sfatUom);
-
           const totalCarbPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(2) > td:nth-child(2)');
           const carb = totalCarbPerServ && totalCarbPerServ.innerHTML ? totalCarbPerServ.innerHTML : '';
           addHiddenDiv('totalCarbPerServ', carb);
-          const carbUom = 'r';
-          addHiddenDiv('carb_uom', carbUom);
+          if (carb) {
+            const carbUom = 'r';
+            addHiddenDiv('carb_uom', carbUom);
+          }
 
           const totalSugarsPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(3) > td:nth-child(2)');
           const sugar = totalSugarsPerServ && totalSugarsPerServ.innerHTML ? totalSugarsPerServ.innerHTML : '';
           addHiddenDiv('totalSugarsPerServ', sugar);
-          const sugarUom = 'r';
-          addHiddenDiv('sugar_uom', sugarUom);
+          if (sugar) {
+            const sugarUom = 'r';
+            addHiddenDiv('sugar_uom', sugarUom);
+          }
 
-          const proteinPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(6) > td:nth-child(2)');
-          const protein = proteinPerServ && proteinPerServ.innerHTML ? proteinPerServ.innerHTML : '';
-          addHiddenDiv('proteinPerServ', protein);
-          const proteinUom = 'r';
-          addHiddenDiv('protein_uom', proteinUom);
+          const totalFatPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(4) > td:nth-child(2)');
+          const fat = totalFatPerServ && totalFatPerServ.innerHTML ? totalFatPerServ.innerHTML : '';
+          addHiddenDiv('totalFatPerServ', fat);
+          if (fat) {
+            const fatUom = 'r';
+            addHiddenDiv('fat_uom', fatUom);
+          }
+
+          if (document.querySelector('div[class*=energy-values]  tr:nth-child(5) > td:nth-child(1)').innerHTML.includes('които наситени')) {
+            const saturatedFatPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(5) > td:nth-child(2)');
+            const sfat = saturatedFatPerServ && saturatedFatPerServ.innerHTML ? saturatedFatPerServ.innerHTML : '';
+            addHiddenDiv('saturatedFatPerServ', sfat);
+            if (sfat) {
+              const sfatUom = 'r';
+              addHiddenDiv('sfat_uom', sfatUom);
+            }
+          } else {
+            const proteinPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(5) > td:nth-child(2)');
+            const protein = proteinPerServ && proteinPerServ.innerHTML ? proteinPerServ.innerHTML : '';
+            addHiddenDiv('proteinPerServ', protein);
+            if (protein) {
+              const proteinUom = 'r';
+              addHiddenDiv('protein_uom', proteinUom);
+            }
+          }
+
+          if (document.querySelector('div[class*=energy-values]  tr:nth-child(6) > td:nth-child(1)') && document.querySelector('div[class*=energy-values]  tr:nth-child(6) > td:nth-child(1)').innerHTML.includes('Протеини')) {
+            const proteinPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(6) > td:nth-child(2)');
+            const protein = proteinPerServ && proteinPerServ.innerHTML ? proteinPerServ.innerHTML : '';
+            addHiddenDiv('proteinPerServ', protein);
+            if (protein) {
+              const proteinUom = 'r';
+              addHiddenDiv('protein_uom', proteinUom);
+            }
+          }
 
           const saltPerServ = document.querySelector('div[class*=energy-values]  tr:nth-child(7) > td:nth-child(2)');
           const salt = saltPerServ && saltPerServ.innerHTML ? saltPerServ.innerHTML : '';
           addHiddenDiv('saltPerServ', salt);
-          const saltUom = 'r';
-          addHiddenDiv('salt_uom', saltUom);
+          if (salt) {
+            const saltUom = 'r';
+            addHiddenDiv('salt_uom', saltUom);
+          }
         });
         // Navigate to description tab
         await context.click('li#react-tabs-0');
--- src/library/product/details/stores/e/ebag/BG/format.js
+++ src/library/product/details/stores/e/ebag/BG/format.js
@@ -21,8 +21,8 @@ const transform = (data) => {
     for (const row of group) {
       if (row.availabilityText) {
         row.availabilityText.forEach(item => {
-          item.text = item.text.includes('Не е наличен') ? 'Out Of Stock' : 'In Stock'
-        })
+          item.text = item.text.includes('Не е наличен') ? 'Out Of Stock' : 'In Stock';
+        });
       }
       if (row.alternateImages) {
         row.alternateImages.forEach(item => {
@@ -31,13 +31,79 @@ const transform = (data) => {
       }
       if (row.ingredientsList) {
         row.ingredientsList.forEach(item => {
-          item.text = item.text.replace(/^Съставки: (.+)/g, '$1').replace(/\n/g, '')
-        })
+          item.text = item.text.replace(/^Съставки: (.+)/g, '$1').replace(/\n/g, '');
+        });
       }
       if (row.directions) {
         row.directions.forEach(item => {
           item.text = item.text.replace(/Начин на приготвяне:/, '').trim();
         });
+      }
+      if (!row.allergyAdvice) {
+        if (row.allergyAdvice1) {
+          row.allergyAdvice1[0].text = row.allergyAdvice1[0].text.replace(/Информация за алергени:/, '').trim();
+          row.allergyAdvice = row.allergyAdvice1;
+        }
+      }
+      if (row.additives) {
+        row.additives.forEach(item => {
+          item.text = item.text.replace(/Добавки:/g, '').trim();
+        });
+      }
+      if (!row.caloriesPerServing) {
+        if (row.nutritionValues1) {
+          let calorie = row.nutritionValues1[0].text.replace(/\n/g, '').replace(/.*Енергийност: (.*)/, '$1').replace(/^((?:\S+\s+){2}\S+).*/, '$1');
+          calorie = calorie.split('Мазнини:')[0];
+          row.caloriesPerServing = [{ text: calorie }];
+
+          let fat = row.nutritionValues1[0].text.replace(/\n/g, '').replace(/.*Мазнини: (.*)/, '$1').replace(/^((?:\S+\s+){1}\S+).*/, '$1');
+          fat = fat.split('От')[0];
+          row.totalFatPerServing = [{ text: fat }];
+          const fatUom = fat.split(' ')[1];
+          row.totalFatPerServingUom = [{ text: fatUom }];
+
+          let carb = row.nutritionValues1[0].text.replace(/\n/g, '').replace(/.*Въглехидрати: (.*)/, '$1').replace(/^((?:\S+\s+){1}\S+).*/, '$1');
+          carb = carb.split('От')[0];
+          row.totalCarbPerServing = [{ text: carb }];
+          const carbUom = carb.split(' ')[1];
+          row.totalCarbPerServingUom = [{ text: carbUom }];
+
+          let protein = row.nutritionValues1[0].text.replace(/\n/g, '').replace(/.*Протеини: (.*)/, '$1').replace(/^((?:\S+\s+){1}\S+).*/, '$1');
+          protein = protein.split('Сол:')[0];
+          row.proteinPerServing = [{ text: protein }];
+          const proteinUom = carb.split(' ')[1];
+          row.proteinPerServingUom = [{ text: proteinUom }];
+
+          let salt = row.nutritionValues1[0].text.replace(/\n/g, '').replace(/.*Сол: (.*)/, '$1').replace(/^((?:\S+\s+){1}\S+).*/, '$1');
+          salt = salt.split('Съдържанието')[0];
+          row.saltPerServing = [{ text: salt }];
+          const saltUom = carb.split(' ')[1];
+          row.saltPerServingUom = [{ text: saltUom }];
+        } else if (row.nutritionValues2) {
+          let calorie = row.nutritionValues2[0].text.replace(/\n/g, '').replace(/.*енергия - (.*)/, '$1').replace(/^((?:\S+\s+){4}\S+).*/, '$1');
+          calorie = calorie.split(';')[0];
+          row.caloriesPerServing = [{ text: calorie }];
+
+          let carb = row.nutritionValues2[0].text.replace(/\n/g, '').replace(/.*въглехидрати – (.*)/, '$1').replace(/^((?:\S+\s+){1}\S+).*/, '$1');
+          carb = carb.split(', от')[0];
+          row.totalCarbPerServing = [{ text: carb }];
+          row.totalCarbPerServingUom = [{ text: 'g' }];
+
+          let sugar = row.nutritionValues2[0].text.replace(/\n/g, '').replace(/.*които захари - (.*)/, '$1').replace(/^((?:\S+\s+){1}\S+).*/, '$1');
+          sugar = sugar.split('; фибри')[0];
+          row.totalSugarsPerServing = [{ text: sugar }];
+          row.totalCarbPerServingUom = [{ text: 'g' }];
+
+          let protein = row.nutritionValues2[0].text.replace(/\n/g, '').replace(/.* фибри – (.*)/, '$1').replace(/^((?:\S+\s+){1}\S+).*/, '$1');
+          protein = protein.split('; белтъчини')[0];
+          row.proteinPerServing = [{ text: protein }];
+          row.proteinPerServingUom = [{ text: 'g' }];
+
+          let salt = row.nutritionValues2[0].text.replace(/\n/g, '').replace(/.* фибри – (.*)/, '$1').replace(/^((?:\S+\s+){1}\S+).*/, '$1');
+          salt = salt.split('; белтъчини')[0];
+          row.saltPerServing = [{ text: salt }];
+          row.saltPerServingUom = [{ text: 'g' }];
+        }
       }
     }
   }
