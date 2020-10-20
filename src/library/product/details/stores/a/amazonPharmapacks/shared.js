cf8383d5ceaa0a56a05339c31312b0097dca5eb8
anu tiwari <anu@anus-MacBook-Pro.local>
2020-10-20 10:20:43 +0545

    Updated extractor inputs

--- src/library/product/details/stores/a/amazonPharmapacks/shared.js
+++ src/library/product/details/stores/a/amazonPharmapacks/shared.js
@@ -7,6 +7,35 @@ module.exports.implementation = async function implementation(
     const { transform } = parameters;
     const { productDetails } = dependencies;
 
+    await context.evaluate(async() => {
+        function removeDuplicates(array) {
+            array.splice(0, array.length, ...(new Set(array)))
+        };
+        let getRank = document.querySelectorAll('#detailBulletsWrapper_feature_div ul span');
+        let rankDetails;
+        let product_rank = [];
+        let product_rank_category = [];
+        getRank.forEach((element) => {
+            if (element.innerText && element.innerText.includes('Best Sellers Rank') && element.innerText.includes('#')) {
+
+                rankDetails = element.innerText.split('Best Sellers Rank: ')[1].split('\n');
+                if (rankDetails.length) {
+                    for (let i = 0; i < rankDetails.length; i++) {
+                        product_rank.push(rankDetails[i].split(' ')[0].trim().replace(/#/, '').replace(/,/g, "").trim());
+                        product_rank_category.push(rankDetails[i].replace(/^[#,0-9 in]+/, '').trim());
+                    }
+                }
+                console.log(rankDetails)
+            }
+        });
+        removeDuplicates(product_rank);
+        removeDuplicates(product_rank_category);
+        let rank = product_rank.join(' | ');
+        let category = product_rank_category.join(' | ');
+        document.head.setAttribute('rank', rank);
+        document.head.setAttribute('category', category);
+    });
+
     const productPrimeCheck = async() => {
         console.log('EXECUTING PRIME RELATED CODE.');
         let primeValue = 'No';
