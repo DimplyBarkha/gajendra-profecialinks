ddf352d7a8864fcc433d56824ccea1fa24961e94
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-10-20 13:58:53 +0530

    cabelas core updating extract.js - akshay channa

--- src/library/product/details/stores/c/cabelas/US/extract.js
+++ src/library/product/details/stores/c/cabelas/US/extract.js
@@ -1,10 +1,10 @@
-
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'cabelas',
-    transform: null,
+    transform: cleanUp,
     domain: 'cabelas.com',
     zipcode: '',
   },
