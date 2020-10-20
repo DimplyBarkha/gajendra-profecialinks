7f130ebe3921d8dd21e0150994a025d9ba9a9333
sirisha <sirisha.thotla-odc@partner.import.io>
2020-10-20 23:47:12 +0530

    added noresults xpath

--- src/library/product/search/stores/l/littlewoods/UK/execute.js
+++ src/library/product/search/stores/l/littlewoods/UK/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'UK',
     store: 'littlewoods',
     domain: 'littlewoods.com',
-    url: 'https://www.littlewoods.com/e/q/{dyson-pure-cool-me}.end',
+    url: 'https://www.littlewoods.com/e/q/{searchTerms}.end',
     loadedSelector: '#products>ul',
     noResultsXPath: '//*[@id="main"]/div/p[1]/strong/text()',
     zipcode: '',
