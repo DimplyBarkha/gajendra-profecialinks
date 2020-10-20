902d282d819190919e25a3ff9286f954ab8ed398
irfan shaikh <irfan.shaikh@synerzip.com>
2020-10-20 20:53:33 +0530

    #789. Created details extractor for kauppahlli24_fi

---
+++ src/library/navigation/goto/domains/ka/kauppahalli24.fi.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'kauppahalli24.fi',
+    timeout: '50000',
+    country: 'FI',
+    store: 'kauppahalli24',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/k/kauppahalli24/FI/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'FI',
+    store: 'kauppahalli24',
+    domain: 'kauppahalli24.fi',
+    loadedSelector: null,
+    noResultsXPath: '//div[contains(@class,"not-found-container")]',
+    zipcode: '',
+  },
+};
