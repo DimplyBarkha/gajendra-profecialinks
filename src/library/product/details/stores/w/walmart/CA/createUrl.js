5d28245b7fff2b766106e8cbf2005c15a763faa0
nikita.kumari <nikita.kumari@synerzip.com>
2020-10-20 16:55:14 +0530

    Id as input implementation

--- src/library/product/details/stores/w/walmart/CA/createUrl.js
+++ src/library/product/details/stores/w/walmart/CA/createUrl.js
@@ -3,8 +3,8 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'walmart.ca',
-    prefix: 'en/ip',
-    url: 'https://www.walmart.ca/en',
+    prefix: '',
+    url: 'https://www.walmart.ca/en/ip/{id}',
     country: 'CA',
     store: 'walmart',
     zipcode: '',
