8ad986a790ede8c9c172dc7d8eaed5fbbec9cc84
madhanimportio <madhan.selvaraj-odc@partner.import.io>
2020-10-20 19:07:35 +0530

    zattini_BR_search

--- src/library/product/search/stores/z/zattini/BR/execute.js
+++ src/library/product/search/stores/z/zattini/BR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'BR',
     store: 'zattini',
     domain: 'zattini.com.br',
-    url: 'https://www.zattini.com.br/busca?nsCat=Natural&q=body+spray&searchTermCapitalized=Body+Spray',
+    url: 'https://www.zattini.com.br/busca?nsCat=Natural&q=body+spray&searchTermCapitalized={searchTerms}',
     loadedSelector: '#item-list',
     noResultsXPath: null,
     zipcode: '',
