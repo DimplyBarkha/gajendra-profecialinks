d9172c41353266bd3178217f8fede0c060046b85
unknown <shalini.kumari-odc@partner.import.io>
2020-10-21 21:52:47 +0530

    Tokmanni_core

---
+++ src/library/product/details/stores/t/tokmanni/FI/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'FI',
+    store: 'tokmanni',
+    domain: 'tokmanni.fi',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
