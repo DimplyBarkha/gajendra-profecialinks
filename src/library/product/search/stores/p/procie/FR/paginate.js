dccccd6f7325c555c64188e86c57d84c8ad1b337
srishivams <srishivams97@gmail.com>
2020-10-21 20:07:00 +0530

    increased timeout

--- src/library/product/search/stores/p/procie/FR/paginate.js
+++ src/library/product/search/stores/p/procie/FR/paginate.js
@@ -4,14 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'FR',
     store: 'procie',
-    // nextLinkSelector: 'li[class="pagination-next ng-scope"] a',
+    nextLinkSelector: 'li[class="pagination-next ng-scope"] a',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div.frameProductList',
     noResultsXPath: '//p[contains(.,"Aucun résultat ne correspond à votre recherche")]',
-    // openSearchDefinition: {
-    //   template: 'https://www.procie.com/resultats-de-recherche.html?Search={searchTerms}',
-    // },
+    openSearchDefinition: {
+      template: 'https://www.procie.com/resultats-de-recherche.html?Search={searchTerms}',
+    },
     domain: 'procie.com',
     zipcode: '',
   },
