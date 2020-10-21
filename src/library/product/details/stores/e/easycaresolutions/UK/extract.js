d0161dd877536497d78c6db3331e3fe239cd08f4
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-10-21 17:44:58 +0530

    702. Internal QA comments fixed.

--- src/library/product/details/stores/e/easycaresolutions/UK/extract.js
+++ src/library/product/details/stores/e/easycaresolutions/UK/extract.js
@@ -12,6 +12,20 @@ module.exports = {
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     try {
       await context.evaluate(async () => {
+        function addHiddenDiv (id, content) {
+          const newDiv = document.createElement('div');
+          newDiv.id = id;
+          newDiv.textContent = content;
+          newDiv.style.display = 'none';
+          document.body.appendChild(newDiv);
+        }
+        const liSelector = Array.from(document.querySelectorAll('div[itemprop*=description] li'));
+        if (liSelector.length > 0) {
+          addHiddenDiv('descriptionBulletCount', liSelector.length);
+        }
+      });
+
+      await context.evaluate(async () => {
         await new Promise(resolve => setTimeout(resolve, 5000));
         await context.waitForSelector('div[class*=page-wrapper]');
       });
