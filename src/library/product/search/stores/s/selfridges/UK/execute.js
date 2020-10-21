28a3c106437c367ece8126c56d14b45f1acc774a
venkatsn <venkat.sn-odc@partner.import.io>
2020-10-21 10:37:07 +0530

    noresultxpath added for selfridges by Deepak

--- src/library/product/search/stores/s/selfridges/UK/execute.js
+++ src/library/product/search/stores/s/selfridges/UK/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'selfridges.com',
     url: 'https://www.selfridges.com/GB/en/cat/?freeText={searchTerms}&srch=Y',
     loadedSelector: '.listing-items.c-listing-items.initialized',
-    noResultsXPath: null,
+    noResultsXPath: '.page-no-results.layout-default.template-error-template.grid-bootstrap.spinner-off.initial-spinner-off',
     zipcode: '',
   },
 };
