9d9ef7ec117b2fc8f8a92ca4c438e9a879ffe5d2
byQ96 <michal126@hotmail.com>
2020-10-21 08:17:21 +0200

    rankOrganic fixed imageUrl fixed with adding scrolling

--- src/library/product/search/stores/t/thewhiskyexchange/UK/extract.js
+++ src/library/product/search/stores/t/thewhiskyexchange/UK/extract.js
@@ -1,6 +1,9 @@
+const { transform } = require('../../../../shared');
+
 async function implementation(inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+
 
   await context.evaluate(() => {
     function addProp(selector, iterator, propName, value) {
@@ -9,31 +12,50 @@ async function implementation(inputs, parameters, context, dependencies) {
         [iterator].setAttribute(propName, value);
     }
     const allProducts = document.querySelectorAll(
-      "div.item"
+      'div.item'
     );
     for (let i = 0; i < allProducts.length; i++) {
       addProp(
-        "div.item",
+        'div.item',
         i,
-        "rankorganic",
+        'rankorganic',
         `${i + 1}`
       );
     }
   });
 
-  await new Promise((resolve, reject) => setTimeout(resolve, 1500));
+  await context.evaluate(async () => {
+    function stall(ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+
+    let scrollTop = 0;
+    while (scrollTop !== 1000) {
+      await stall(1000);
+      scrollTop += 2000;
+      window.scroll(0, scrollTop);
+      if (scrollTop === 8000) {
+        await stall(1000);
+        break;
+      }
+    }
+  });
 
   return await context.extract(productDetails, { transform });
 }
 
 module.exports = {
-  implements: "product/search/extract",
+  implements: 'product/search/extract',
   parameterValues: {
-    country: "UK",
-    store: "thewhiskyexchange",
+    country: 'UK',
+    store: 'thewhiskyexchange',
     transform: null,
-    domain: "thewhiskyexchange.com",
-    zipcode: "",
+    domain: 'thewhiskyexchange.com',
+    zipcode: '',
   },
   implementation
 };
