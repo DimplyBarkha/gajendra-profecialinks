cfff1a171b691e2be9fbf9c3d4194af38d46945b
shrutikab <shrutika.bongarde@synerzip.com>
2020-10-21 15:34:48 +0530

    input file added(#792)

---
+++ src/library/navigation/goto/domains/lo/lojasrenner.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'lojasrenner.com',
+    timeout: 90000,
+    country: 'BR',
+    store: 'lojasrenner',
+    zipcode: '',
+  },
+};
