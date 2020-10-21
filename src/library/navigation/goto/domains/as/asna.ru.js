5ca97828105fda2f694c83d48d8f4ac0a36553e4
ganesh <ganesh@profecialinks.com>
2020-10-21 15:25:54 +0530

    timeout null

--- src/library/navigation/goto/domains/as/asna.ru.js
+++ src/library/navigation/goto/domains/as/asna.ru.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'asna.ru',
-    timeout: 500000,
+    timeout: null,
     country: 'RU',
     store: 'asna',
     zipcode: '',
