631c954f816c3cdf85cba4325d6fedd2f9f47f5f
Venkatsn <70714102+Venkatsn@users.noreply.github.com>
2020-10-21 20:42:36 +0530

    searchterms changed

--- src/library/product/search/stores/a/ao/UK/execute.js
+++ src/library/product/search/stores/a/ao/UK/execute.js
@@ -4,7 +4,7 @@ module.exports = {
     country: 'UK',
     store: 'ao',
     domain: 'ao.com',
-    url: 'https://ao.com/l/dyson/6/99/',
+    url: 'https://ao.com/l/{searchTerms}/6/99/',
     loadedSelector: 'body',
     noResultsXPath: '//h1[@class="break-words mb-4 mt-6 text-center text-display font-regular"]/text()',
     zipcode: '',
