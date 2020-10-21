ff1d6d7cdefd766b741e69a03528efc9870ef23c
IpsitaM <iamipsita07@gmail.com>
2020-10-21 17:47:35 +0530

    Removed comments from goto

--- src/library/navigation/goto/domains/me/metro.de.js
+++ src/library/navigation/goto/domains/me/metro.de.js
@@ -14,39 +14,5 @@ module.exports = {
         await context.setLoadImages(true);
         await context.setLoadAllResources(true);
         await context.goto(url);
-        // await context.waitForNavigation({ timeout: 50000, waitUntil: 'load' });
-        // const clickMethod = await context.evaluate(async function() {
-        //     const cookieSelector = document.querySelector("cms-cookie-disclaimer") && document.querySelector("cms-cookie-disclaimer").shadowRoot.querySelector("button.field-accept-button-name");
-        //     // const cookieSelector = document.querySelector("cms-cookie-disclaimer")?.shadowRoot.querySelector("button.field-accept-button-name");
-        //     if (cookieSelector) {
-        //         console.log('Cookie selector found');
-        //         cookieSelector.click();
-        //         console.log('Cookie selector clicked');
-        //         return true;
-        //     } else {
-        //         return false;
-        //     }
-        // })
-        // const check = clickMethod();
-        // if (check) {
-        //     await context.waitForNavigation({ timeout: 30000 });
-        // }
-        //     const clickMethod = async() => {
-        //         return await context.evaluate(async function() {
-        //             const cookieSelector = document.querySelector("cms-cookie-disclaimer") && document.querySelector("cms-cookie-disclaimer").shadowRoot.querySelector("button.field-accept-button-name");
-        //             if (cookieSelector) {
-        //                 console.log('Cookie selector found');
-        //                 cookieSelector.click();
-        //                 console.log('Cookie selector clicked');
-        //                 return true;
-        //             } else {
-        //                 return false;
-        //             }
-        //         })
-        //     }
-        //     const check = clickMethod();
-        //     if (check) {
-        //         await context.waitForNavigation({ timeout: 30000 });
-        //     }
     }
 };
