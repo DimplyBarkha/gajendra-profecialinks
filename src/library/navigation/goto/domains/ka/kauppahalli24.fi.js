21c798f61027f30d84574811b7836efeb42096c7
irfan shaikh <irfan.shaikh@synerzip.com>
2020-10-21 13:17:19 +0530

    #789. Updated url creation logic and some fields xpaths.

--- src/library/navigation/goto/domains/ka/kauppahalli24.fi.js
+++ src/library/navigation/goto/domains/ka/kauppahalli24.fi.js
@@ -8,4 +8,16 @@ module.exports = {
     store: 'kauppahalli24',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setBlockAds(false);
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
--- src/library/product/details/stores/k/kauppahalli24/FI/createUrl.js
+++ src/library/product/details/stores/k/kauppahalli24/FI/createUrl.js
@@ -1,38 +1,12 @@
 
-/**
- *
- * @param { { id: any } } inputs
- * @param { { domain: string, prefix?: string, suffix?: string, url?: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { } } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { id } = inputs;
-  const response = await context.evaluate(async (id) => {
-    return await fetch(`https://kauppahalli24_fi_api.frosmo.com/?queries=%5B%22${id}%22%5D&method=getProductsBySku`).then(response => response.json())
-      .then(data => data)
-      .catch(error => console.error('Error:', error));
-  }, id);
-  if (response && response.data[0] && response.data[0].products[0] && response.data[0].products[0].id) {
-    return `https://www.kauppahalli24.fi/tuotteet/?id=${id}#/pid/${response.data[0].products[0].id}`;
-  } else {
-    return `https://www.kauppahalli24.fi/tuotteet/?id=${id}&s=#pid/${id}`;
-  }
-}
 module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'kauppahalli24.fi',
     prefix: null,
-    url: null,
+    url: 'https://www.kauppahalli24.fi/tuotteet/#pid/{id}',
     country: 'FI',
     store: 'kauppahalli24',
     zipcode: '',
   },
-  implementation,
 };
