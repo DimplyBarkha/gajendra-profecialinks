40d1759f16e340913e603230b66cc8e87783de92
byQ96 <michal126@hotmail.com>
2020-10-22 11:53:32 +0200

    updated files for deploy

--- src/library/product/details/stores/t/thewhiskyexchange/UK/extract.js
+++ src/library/product/details/stores/t/thewhiskyexchange/UK/extract.js
@@ -1,11 +1,24 @@
+const { cleanUp } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'UK',
     store: 'thewhiskyexchange',
-    transform: null,
+    transform: cleanUp,
     domain: 'thewhiskyexchange.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDom (element, id) {
+        const div = document.createElement('div');
+        div.id = id;
+        div.innerHTML = element;
+        document.body.appendChild(div);
+      }
+      
+    });
+    await context.extract(productDetails);
+  }
 };
