962390a9aa2489ffa061f3b01a6c6df6e71d439e
sivasoarlogic <siva@soarlogic.com>
2020-10-22 19:11:19 +0530

    search

--- src/library/navigation/goto/domains/sc/screwfix.com.js
+++ src/library/navigation/goto/domains/sc/screwfix.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'screwfix.com',
-    timeout: null,
+    timeout: 100000,
     country: 'UK',
     store: 'screwfix',
     zipcode: "''",
