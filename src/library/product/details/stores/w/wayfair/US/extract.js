17c6f08c246602b3e2121499953952042ad0be26
Nilesh <nilesh.b@osmosys.asia>
2020-10-22 19:51:39 +0530

    Added missing columns.

--- src/library/product/details/stores/w/wayfair/US/extract.js
+++ src/library/product/details/stores/w/wayfair/US/extract.js
@@ -25,6 +25,30 @@ async function implementation (
         }, ms);
       });
     }
+    function getElementsByXPath (xpath, type, parent) {
+      const results = [];
+      const query = document.evaluate(xpath, parent || document,
+        null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+        if (type === 'Specifications') {
+          if (i % 2 === 0) {
+            results.push(query.snapshotItem(i).innerText + ' : ' + query.snapshotItem(i + 1).innerText);
+          }
+        }
+        if (type === 'Description') {
+          results.push(query.snapshotItem(i).innerText);
+        }
+      }
+      return results;
+    }
+    const specification = getElementsByXPath("//h4[contains(text(),'Specifications')]/following-sibling::div/dl/dt|//h4[contains(text(),'Specifications')]/following-sibling::div/dl/dd", 'Specifications').join(' || ');
+    const description = getElementsByXPath('//div[@class="ProductOverviewInformation-description"]', 'Description');
+    const feature = getElementsByXPath('//h4[contains(text(),"Features")]/following-sibling::ul/li', 'Description') ? 'Features : ' + getElementsByXPath('//h4[contains(text(),"Features")]/following-sibling::ul/li', 'Description').join(' || ') : '';
+    const ProductDetails = getElementsByXPath('//h4[contains(text(),"Product Details")]/following-sibling::ul/li', 'Description') ? 'Product Details : ' + getElementsByXPath('//h4[contains(text(),"Product Details")]/following-sibling::ul/li', 'Description').join(' || ') : '';
+    const included = getElementsByXPath('//h3[contains(text(),"Included?")]/following-sibling::ul/li', 'Description') ? 'What\'s Included? : ' + getElementsByXPath('//h3[contains(text(),"Included?")]/following-sibling::ul/li', 'Description').join(' || ') : '';
+    const descriptionArr = [description, included, feature, ProductDetails].filter(e => e).join(' || ');
+    document.body.setAttribute('description', descriptionArr);
+    document.body.setAttribute('specification', specification);
   });
   return await context.extract(productDetails, { transform });
 }
@@ -39,4 +63,3 @@ module.exports = {
   },
   implementation,
 };
-
