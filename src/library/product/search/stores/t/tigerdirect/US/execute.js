f0585a6b71f4cc4538bc5b3aa86b6787651a1375
unknown <leena.gavali-odc@partner.import.io>
2020-10-22 22:29:01 +0530

    final

--- src/library/product/search/stores/t/tigerdirect/US/execute.js
+++ src/library/product/search/stores/t/tigerdirect/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'tigerdirect',
     domain: 'tigerdirect.com',
-    url: 'https://www.tigerdirect.com/applications/SearchTools/search.asp?keywords=apc+backup',
+    url: 'https://www.tigerdirect.com/applications/SearchTools/search.asp?keywords={searchTerms}',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
