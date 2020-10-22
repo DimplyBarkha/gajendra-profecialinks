16559eeafc77280ef9301fa15a2a1386516fe1eb
rajeshprofecialinks <rajesh.g@profecialinks.com>
2020-10-22 18:11:40 +0530

    worlddutyfree core initial

---
+++ src/library/navigation/goto/domains/wo/worlddutyfree.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'worlddutyfree.com',
+    timeout: null,
+    country: 'UK',
+    store: 'worlddutyfree',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/w/worlddutyfree/UK/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'worlddutyfree',
+    domain: 'worlddutyfree.com',
+    loadedSelector: null,
+    noResultsXPath: '//div[@class="cms-404-page-text column"]/h2',
+    zipcode: '',
+  },
+};
