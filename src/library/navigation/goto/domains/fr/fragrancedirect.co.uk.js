8556329c86c99e4a22d1ce4e3f8b1a814da8967f
Praveen Ranjan <pravinr@synerzipune.local>
2020-10-22 22:39:08 +0530

    search

---
+++ src/library/navigation/goto/domains/fr/fragrancedirect.co.uk.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'fragrancedirect.co.uk',
+    timeout: null,
+    country: 'UK',
+    store: 'fragrancedirect',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/f/fragrancedirect/UK/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'UK',
+    store: 'fragrancedirect',
+    domain: 'fragrancedirect.co.uk',
+    zipcode: '',
+  },
+};
