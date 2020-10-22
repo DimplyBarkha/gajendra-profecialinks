2b536b89418a257413cf95122beb8bdb441cfd05
Mamatha-Gundapaneni <mamatha.g@osmosys.asia>
2020-10-22 11:28:29 +0530

    Updated code

--- src/library/product/details/stores/j/jcpenney/US/extract.js
+++ src/library/product/details/stores/j/jcpenney/US/extract.js
@@ -19,9 +19,11 @@ async function implementation(
     }
 
     function appendImages(selector, images) {
-      [...document.querySelectorAll(selector)].map((e, index) => { e.setAttribute("mainImage", `${images[index][0]}`) })
-
+      [...document.querySelectorAll(selector)].map((e, index) => { e.setAttribute("mainImage", `${images[index][0]}`)});
     }
+    // function secondaryImages(selector, images) {
+    //   [...document.querySelectorAll(selector)].map((e, index) => { e.setAttribute("secondaryImages", `${alternateImages}`)});
+    // }
 
     // function addHiddenDiv (id, content) {
     //   const newDiv = document.createElement('div');
@@ -38,15 +40,29 @@ async function implementation(
     //     console.log(`Couldn't load selector => ${sel}`);
     //   }
     // };
-    let finalImages = [];
+    const finalImages = [];
     const isVariants = document.querySelector('ul[data-automation-id="product-dimensions-color"]');
-    const variants = "#product-options-false > div._2E0-s > div > div > ul li"
+    const variants = "#option-wrapper-false #product-options-false > div:nth-last-child(1) ul li"
+
 
     if (isVariants) {
-      document.querySelectorAll('ul[data-automation-id="product-dimensions-color"] li div button').forEach(ele => {
+      document.querySelectorAll('#option-wrapper-false #product-options-false > div:nth-last-child(1) ul li button').forEach(ele => {
         ele.click();
         console.log('Clicked');
-        finalImages.push([...document.querySelectorAll('#contentContainer > section > section:nth-child(3) > div.sm12.md6.lg6.xl7._3AtEJ > div > div.carousel-wrapper > div > div._3h_IA.lg2.xl2.md2.sm2._1sbcC.noPad > div > div.slick-list a')].map(e => { return e.querySelector('img').getAttribute('src') }))
+        var jsonString = document.querySelector("script[type='application/ld+json']").innerText;
+        let jsonParsed = {};
+        jsonParsed = JSON.parse(jsonString);
+
+        [...document.querySelectorAll('#option-wrapper-false #product-options-false > div:nth-last-child(1) ul li')].map((e) => {
+          e.setAttribute("sku", jsonParsed.offers.map(( ele )=>{ return ele.sku }))});
+
+        [...document.querySelectorAll('#option-wrapper-false #product-options-false > div:nth-last-child(1) ul li')].map((e) => {
+          e.setAttribute("nameExtended",`${document.querySelector('h1[data-automation-id="product-title"]').textContent} ` + `- ${e.textContent}`)});
+
+        [...document.querySelectorAll('#option-wrapper-false #product-options-false > div:nth-last-child(1) ul li')].map((e) => {
+          e.setAttribute("secondaryImages", [...document.querySelectorAll('#contentContainer > section > section:nth-child(3) > div.sm12.md6.lg6.xl7._3AtEJ > div > div.carousel-wrapper > div > div._3h_IA.lg2.xl2.md2.sm2._1sbcC.noPad > div > div.slick-list a:not(._2JSHu)')].map(e => { return `https:${e.querySelector('img').getAttribute('src')}`}))});
+
+        finalImages.push([...document.querySelectorAll('#contentContainer > section > section:nth-child(3) > div.sm12.md6.lg6.xl7._3AtEJ > div > div.carousel-wrapper > div > div._3h_IA.lg2.xl2.md2.sm2._1sbcC.noPad > div > div.slick-list a:not(._2JSHu)')].map(e => { return e.querySelector('img').getAttribute('src')}));
         // optionalWait('h1[data-automation-id="product-title"]');
         // const mainImage = document.querySelector('div._3IMdO div.image-desktop img').getAttribute('src');
         // const newDiv = document.createElement('div');
@@ -54,12 +70,15 @@ async function implementation(
         // newDiv.textContent = mainImage;
         // // newDiv.style.display = 'none';
         // document.querySelector('main#mainContainerBlock').appendChild(newDiv);
+        // alternateImages.push([...document.querySelectorAll('#contentContainer > section > section:nth-child(3) > div.sm12.md6.lg6.xl7._3AtEJ > div > div.carousel-wrapper > div > div._3h_IA.lg2.xl2.md2.sm2._1sbcC.noPad > div > div.slick-list a:not(._2JSHu)')].map(e => { return e.querySelector('img').getAttribute('src')}));
+        // secondaryImages(variants, alternateImages);
       });
     }
-    appendImages(variants, finalImages)
-
-
-
+    appendImages(variants, finalImages);
+    // let images = '';
+    // Array.from(alternateImages).forEach(el => {
+    //   images += `https:${el}` ? `| https:${el}` : `https:${el}`;
+    // });
 
     let scrollTop = 500;
     while (true) {
