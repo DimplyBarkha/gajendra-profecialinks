73775d245161c32b2a41846cb7a0627ae8bf18fa
Stolyar100 <olejer555@gmail.com>
2020-10-23 22:45:07 +0300

    created new elements for previous commits fields

--- src/library/product/search/stores/k/kalunga/BR/extract.js
+++ src/library/product/search/stores/k/kalunga/BR/extract.js
@@ -9,16 +9,45 @@ module.exports = {
     domain: 'kalunga.com.br',
     zipcode: '',
   },
-  async function (
+  implementation: async (
     inputs,
     parameters,
     context,
     dependencies,
-  ) {
+  ) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
 
     await context.evaluate(() => {
+      function addHiddenDiv (className, content, index) {
+        console.log(`className: ${className}, content: ${content}, index: ${index}`);
+        const newDiv = document.createElement('div');
+        newDiv.classList.add(className);
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll('div.blocoproduto')[index];
+        // originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+        if (!originalDiv.querySelector(`.${className}`)) {
+          originalDiv.appendChild(newDiv);
+        }
+      }
+
+      const products = document.querySelectorAll('div.blocoproduto');
+
+      products.forEach((product, index) => {
+        // Gets aggregate rating
+        const aggregateRating = product.querySelector('span.reviews__star_text').innerHTML.match(/\d/g);
+        addHiddenDiv('kalunga_aggregateRating', aggregateRating, index);
+
+        //Gets product url
+        const productUrl = product.querySelector('a.blocoproduto__link').href
+        addHiddenDiv('kalunga_productUrl', productUrl, index);
+
+        // Gets product sku
+        const sku = productUrl.split('/').pop();
+        addHiddenDiv('kalunga_sku', sku, index);
+
+      });
     });
 
     return await context.extract(productDetails, { transform });
