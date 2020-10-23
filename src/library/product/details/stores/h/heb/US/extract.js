1a546fb10fb752cb37696f56ffc5a57e91aa08ac
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-10-23 20:05:33 +0530

    heb core - akshay channa

---
+++ src/library/product/details/stores/h/heb/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'heb',
+    transform: null,
+    domain: 'heb.com',
+    zipcode: '',
+  },
+};
