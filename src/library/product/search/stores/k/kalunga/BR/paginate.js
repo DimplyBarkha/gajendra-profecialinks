bdfaebcccc007ade900fc6f0ed7c125f58b5462b
Stolyar100 <olejer555@gmail.com>
2020-10-23 23:02:37 +0300

    updated paginate.js with nextLinkXpath

--- src/library/product/search/stores/k/kalunga/BR/paginate.js
+++ src/library/product/search/stores/k/kalunga/BR/paginate.js
@@ -5,6 +5,7 @@ module.exports = {
     country: 'BR',
     store: 'kalunga',
     nextLinkSelector: null,
+    nextLinkXpath: "//li[contains(concat(' ',normalize-space(@class),' '),'page-item')][.//a[contains(concat(' ',normalize-space(@class),' '),'active')] or (.//a[contains(., '...')] and boolean(not(./../..//a[contains(concat(' ',normalize-space(@class),' '),'active')])))]/following-sibling::li[1]/a",
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div.blocoproduto',
