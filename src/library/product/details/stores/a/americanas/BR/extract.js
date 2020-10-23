b49e7ca70332ad8e5c80a82cb9b632858acae6b9
Lech Kulesza <lmkulesz@icloud.com>
2020-10-23 14:38:38 +0200

    #758, finished core extractor

--- src/library/product/details/stores/a/americanas/BR/extract.js
+++ src/library/product/details/stores/a/americanas/BR/extract.js
@@ -1,9 +1,11 @@
+const { cleanUp } = require("../../../../shared");
+
 module.exports = {
   implements: "product/details/extract",
   parameterValues: {
     country: "BR",
     store: "americanas",
-    transform: null,
+    transform: cleanUp,
     domain: "americanas.com.br",
     zipcode: "",
   },
@@ -15,6 +17,20 @@ module.exports = {
         div.innerHTML = element;
         document.body.appendChild(div);
       }
+
+      function getHTML(url, callback) {
+        if (!window.XMLHttpRequest) return;
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+          if (callback && typeof callback === "function") {
+            callback(this.responseXML);
+          }
+        };
+        xhr.open("GET", url);
+        xhr.responseType = "document";
+        xhr.send();
+      }
+
       const productUrl = window.location.href;
       addElementToDom(productUrl, "productUrl");
 
@@ -33,23 +49,84 @@ module.exports = {
       let nameExtended = document.querySelector("h1#product-name-default") ? document.querySelector("h1#product-name-default").innerText : "";
       const specificationsList = document.querySelector("div#info-section div:nth-of-type(2) table")
         ? document.querySelectorAll("div#info-section div:nth-of-type(2) table tr")
+        : document.evaluate('//div[contains(@class,"src__Container")]/div[contains(@class,"src__DeskWrapper")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+            .singleNodeValue
+        ? document
+            .evaluate('//div[contains(@class,"src__Container")]/div[contains(@class,"src__DeskWrapper")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+            .singleNodeValue.querySelectorAll("table tr")
         : [];
       let brandText = "";
       let manufacturer = "";
+      let weightNet = "";
+      let gtinArr = [];
+      let sku = "";
+      let shippingDimensions = "";
+      let color = "";
+      let warranty = "";
       if (specificationsList) {
         specificationsList.forEach((item) => {
-          let firstTd = item.querySelector("td:nth-of-type(1)");
-          let secondTd = item.querySelector("td:nth-of-type(2)");
-          if (firstTd.innerText.toLowerCase() === "marca") brandText = secondTd;
-          if (firstTd.innerText.toLowerCase() === "fabricante") manufacturer = secondTd;
+          let firstTd = item.querySelector("td:nth-of-type(1)").innerText;
+          let secondTd = item.querySelector("td:nth-of-type(2)").innerText;
+          if (firstTd.toLowerCase() === "marca") brandText = secondTd;
+          if (firstTd.toLowerCase() === "fabricante") manufacturer = secondTd;
+          if (firstTd.toLowerCase().includes("peso liq")) weightNet = secondTd.toLowerCase().includes("kg") ? secondTd : secondTd + "kg";
+          if (firstTd.toLowerCase() === "código de barras") gtinArr = secondTd.split(",");
+          if (firstTd.toLowerCase() === "código") sku = secondTd;
+          if (firstTd.toLowerCase().includes("dimensões do produto")) shippingDimensions = secondTd;
+          if (firstTd.toLowerCase().includes("cor")) color = secondTd;
+          if (firstTd.toLowerCase().includes("garantia")) warranty = secondTd;
         });
       }
+      const description = document.querySelector("#info-section div:nth-of-type(1)") ? document.querySelector("#info-section div:nth-of-type(1)").innerText : "";
+      if (description) {
+        if (brandText === "") {
+          let regExpGroup = description.match(/Marca.*?:(.*?)-/);
+          if (!regExpGroup) regExpGroup = description.match(/Marca.*?:(.*?)Aviso/);
+          brandText = regExpGroup ? regExpGroup[1] : brandText;
+        }
+        if (manufacturer === "") {
+          let regExpGroup = description.match(/Fabricante.*?:(.*?)-/);
+          if (!regExpGroup) regExpGroup = description.match(/Fabricante.*?:(.*?)Aviso/);
+          manufacturer = regExpGroup ? regExpGroup[1] : manufacturer;
+        }
+        if (weightNet === "") {
+          let regExpGroup = description.match(/Peso Líquido.*?:(.*?)-/);
+          if (!regExpGroup) regExpGroup = description.match(/Peso Líquido.*?:(.*?)Aviso/);
+          weightNet = regExpGroup ? regExpGroup[1] : weightNet;
+        }
+        if (shippingDimensions === "") {
+          let regExpGroup = description.match(/Dimensões do produto.*?:(.*?)-/);
+          if (!regExpGroup) regExpGroup = description.match(/Dimensões do produto.*?:(.*?)Aviso/);
+          shippingDimensions = regExpGroup ? regExpGroup[1] : shippingDimensions;
+        }
+        if (color === "") {
+          let regExpGroup = description.match(/Cor.*?:(.*?)-/);
+          if (!regExpGroup) regExpGroup = description.match(/Cor.*?:(.*?)Aviso/);
+          color = regExpGroup ? regExpGroup[1] : color;
+        }
+        if (warranty === "") {
+          let regExpGroup = description.match(/Garantia.*?:(.*?)-/);
+          if (!regExpGroup) regExpGroup = description.match(/Garantia.*?:(.*)/);
+          warranty = regExpGroup ? regExpGroup[1] : warranty;
+        }
+      }
       if (nameExtended && brandText) {
-        nameExtended = nameExtended.toLowercase().includes(brandText) ? nameExtended : `${brandText} - ${nameExtended}`;
+        nameExtended = nameExtended.toLowerCase().includes(brandText.toLowerCase()) ? nameExtended : `${brandText} - ${nameExtended}`;
       }
       addElementToDom(nameExtended, "nameExtended");
       addElementToDom(brandText, "brandText");
       addElementToDom(manufacturer, "manufacturer");
+      addElementToDom(weightNet, "weightNet");
+      addElementToDom(gtinArr[0], "gtin");
+      addElementToDom(sku, "sku");
+      addElementToDom(gtinArr.length, "variantCount");
+      addElementToDom(shippingDimensions, "shippingDimensions");
+      addElementToDom(gtinArr.join(" | "), "variants");
+
+      const specifications = document.querySelector("div#info-section div:nth-of-type(2) table")
+        ? document.querySelector("div#info-section div:nth-of-type(2) table").innerText
+        : "";
+      addElementToDom(specifications, "specifications");
 
       const priceTagNumber = document.evaluate(
         'count(//div[contains(@class,"buybox__BigSection")]//div[contains(@class,"main-offer__Container")]/div[1]/div[contains(@class,"price__Paragraph")])',
@@ -62,7 +139,7 @@ module.exports = {
         priceTagNumber.numberValue > 0
           ? document
               .evaluate(
-                '//div[contains(@class,"buybox__BigSection")]//div[contains(@class,"main-offer__Container")]/div[1]/div[contains(@class,"price__Paragraph")][1]',
+                '//div[contains(@class,"buybox__BigSection")]//div[contains(@class,"main-offer__Container")]//div[contains(@class,"price__Paragraph")][1]',
                 document,
                 null,
                 XPathResult.FIRST_ORDERED_NODE_TYPE,
@@ -73,7 +150,7 @@ module.exports = {
       const price =
         priceTagNumber.numberValue > 1
           ? document.evaluate(
-              '//div[contains(@class,"buybox__BigSection")]//div[contains(@class,"main-offer__Container")]/div[1]/div[contains(@class,"price__Paragraph")][2]',
+              '//div[contains(@class,"buybox__BigSection")]//div[contains(@class,"main-offer__Container")]//div[contains(@class,"price__Paragraph")][2]',
               document,
               null,
               XPathResult.FIRST_ORDERED_NODE_TYPE,
@@ -82,9 +159,50 @@ module.exports = {
           : listedPrice;
       const availabilityText =
         priceTagNumber.numberValue === 0 ? (document.querySelector("#title-stock") ? document.querySelector("#title-stock").innerText : "Out of Stock") : "In Stock";
+      const coupon =
+        priceTagNumber.numberValue > 1
+          ? document.evaluate(
+              '//div[contains(@class,"buybox__BigSection")]//div[contains(@class,"main-offer__Container")]//div[contains(@class,"price__Paragraph")][1]/div[contains(@class,"price__Badge")]',
+              document,
+              null,
+              XPathResult.FIRST_ORDERED_NODE_TYPE,
+              null
+            ).singleNodeValue.innerText
+          : "";
       addElementToDom(listedPrice, "listedPrice");
       addElementToDom(price, "priceOnline");
       addElementToDom(availabilityText, "availabilityText");
+      addElementToDom(coupon, "coupon");
+
+      const ratingCount = document.querySelector(".card-reviews header.summary-bar .summary-rating span:nth-of-type(2)")
+        ? document.querySelector(".card-reviews header.summary-bar .summary-rating span:nth-of-type(2)").innerText.replace(/\n/g, "").replace(/\(/g, "").replace(/\)/g, "")
+        : "";
+      addElementToDom(ratingCount, "ratingCount");
+
+      const aggregateRating = document.querySelector(".card-reviews header.summary-bar .summary-rating span:nth-of-type(1)")
+        ? document.querySelector(".card-reviews header.summary-bar .summary-rating span:nth-of-type(1)").innerText
+        : "";
+      addElementToDom(aggregateRating, "aggregateRating");
+
+      const manufacturerDescriptionFrameLink = document.querySelector("#info-section iframe") ? document.querySelector("#info-section iframe").getAttribute("src") : "";
+      let manufacturerImagesArr = [];
+      if (manufacturerDescriptionFrameLink) {
+        getHTML(manufacturerDescriptionFrameLink, function (response) {
+          addElementToDom("", "manufacturerSite");
+          let manufacturerSiteDiv = document.querySelector("#manufacturerSite");
+          manufacturerSiteDiv.innerHTML = response.querySelector("body").innerHTML;
+          manufacturerImagesArr = Array.from(response.querySelectorAll("img")).map((item) => {
+            return `${manufacturerDescriptionFrameLink}${item.getAttribute("src")}`.replace("index.html", "");
+          });
+        });
+      }
+      const manufacturerDescription = document.querySelector("#manufacturerSite") ? document.querySelector("#manufacturerSite").innerText : "";
+      addElementToDom(manufacturerDescription, "manufacturerDescription");
+      setTimeout(() => {
+        addElementToDom(manufacturerImagesArr, "manufacturerImages");
+      }, 500);
+      const productName = document.querySelector("h1#product-name-default") ? document.querySelector("h1#product-name-default").innerText : "";
+      addElementToDom(productName, "productName");
     });
     await context.extract(productDetails);
   },
