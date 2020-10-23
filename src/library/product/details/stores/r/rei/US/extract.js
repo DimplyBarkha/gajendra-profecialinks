133f8d05d01c90cfa3c17221f118c71d22479b93
Gajender Ramola <ramola106@gmail.com>
2020-10-23 18:05:41 +0530

    initial commit

---
+++ src/library/product/details/stores/r/rei/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'rei',
+    transform: null,
+    domain: 'rei.com',
+    zipcode: '',
+  },
+};
