69e28813177d36a1ccb64c20d574a31d8880052f
Vivek Gondil <gondilvivek@gmail.com>
2020-10-23 14:35:36 +0530

    adding implementation in goto

--- src/library/navigation/goto/domains/ca/carrefour.com.br.js
+++ src/library/navigation/goto/domains/ca/carrefour.com.br.js
@@ -2,9 +2,23 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'carrefour.com.br',
-    timeout: 50000,
+    timeout: 100000,
     country: 'BR',
     store: 'Carrefour',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+    await context.goto(url, {
+      images_enabled: true,
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: true,
+    });
   },
 };
