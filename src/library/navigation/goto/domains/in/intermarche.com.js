df8d8de14bef6edd0b649b7af590a86c4a321929
Bindu Datla <bindu.datla@import.io>
2020-10-25 12:59:45 -0400

    Handling wait for loading for data

--- src/library/navigation/goto/domains/in/intermarche.com.js
+++ src/library/navigation/goto/domains/in/intermarche.com.js
@@ -8,5 +8,18 @@ module.exports = {
     store: 'intermarche',
     zipcode: '31130',
   },
+
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.captureRequests();
+    await context.setBlockAds(false);
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
 
