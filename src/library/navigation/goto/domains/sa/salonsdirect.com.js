4c17679dd163d06d7c4aaa24a127a81c3de064e2
sunil-synerzip <sunil.kshirasagar@synerzip.com>
2020-10-25 18:55:20 +0530

    salonsdirect code changes for search extractor

---
+++ src/library/navigation/goto/domains/sa/salonsdirect.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'salonsdirect.com',
+    timeout: 200000,
+    country: 'UK',
+    store: 'salonsdirect',
+    zipcode: '',
+  },
+};
