8eb89397a94a94f46f66d79b3bb6ba19127d49ef
sangeeta <sangeta.kumari@synerzip.com>
2020-10-26 18:14:02 +0530

    #886 foodsaver details

---
+++ src/library/navigation/goto/domains/fo/foodsaver.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'foodsaver.com',
+    timeout: 50000,
+    country: 'US',
+    store: 'foodsaver',
+    zipcode: '',
+  },
+};
---
+++ src/library/navigation/goto/domains/fo/setZipCode.foodsaver.com.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'US',
+    domain: 'foodsaver.com',
+    store: 'foodsaver',
+    zipcode: '',
+  },
+};
