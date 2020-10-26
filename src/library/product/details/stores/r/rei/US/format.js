66d53c1f03a406d5b0addac96bb9c5e06ee371f6
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-10-26 17:04:32 +0530

    Updated code

--- src/library/product/details/stores/r/rei/US/format.js
+++ src/library/product/details/stores/r/rei/US/format.js
@@ -23,32 +23,40 @@ const transform = (data) => {
     }))));
     return data;
   };
-  for (const { group } of data) {    
+  for (const { group } of data) {
     for (let row of group) {
       if (row.image) {
-          row.image.forEach(item => {
-              item.text = 'https://www.rei.com' + item.text              
-          });
+        row.image.forEach(item => {
+          item.text = 'https://www.rei.com' + item.text
+        });
       }
       if (row.alternateImages) {
         row.alternateImages.forEach(item => {
-            item.text = 'https://www.rei.com' + item.text              
+          item.text = 'https://www.rei.com' + item.text
         });
       }
       if (row.videos) {
-
         row.videos.forEach(item => {
           var temp_json = JSON.parse(item.text);
           var json_obj = temp_json['videos'];
           var arr_video = [];
           json_obj.forEach(inner_obj => {
-            arr_video.push(inner_obj['articleUrl']);                
+            arr_video.push(inner_obj['articleUrl']);
           });
-          if(arr_video.length){
-            row.videos = [{'text':arr_video}];
+          if (arr_video.length) {
+            row.videos = [{ 'text': arr_video }];
           }
         });
       }
+      if (row.specifications) {
+        var specificationsArr = [];
+        row.specifications.forEach(item => {
+          specificationsArr.push(item.text.replace(/\n\s\n\s\n/,' : '));
+        });
+        if(specificationsArr.length){
+          row.specifications = [{"text": specificationsArr.join(" || ")}];
+        }
+      }      
     }
   }
   return cleanUp(data);
