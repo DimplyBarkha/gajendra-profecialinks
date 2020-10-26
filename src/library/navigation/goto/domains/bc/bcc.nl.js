724bf8cefc738924e9ecc48c1e585a2a23b6eb42
Andrii Kuzmych <andriy.kuzjmych@gmail.com>
2020-10-26 15:51:36 +0200

    #820 Create extractor

---
+++ src/library/navigation/goto/domains/bc/bcc.nl.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'bcc.nl',
+    timeout: null,
+    country: 'NL',
+    store: 'bcc',
+    zipcode: '',
+  },
+};
