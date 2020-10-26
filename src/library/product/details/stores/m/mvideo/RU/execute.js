509dfa4a515338bcea2ee4d9ad694b3d3295d0a9
unknown <shalini.kumari-odc@partner.import.io>
2020-10-26 23:42:16 +0530

    mvideo_core

---
+++ src/library/product/details/stores/m/mvideo/RU/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'RU',
+    store: 'mvideo',
+    domain: 'mvideo.ru',
+    loadedSelector: 'body',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
