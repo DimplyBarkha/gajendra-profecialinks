806cc5ebd122d6a1a973f14dfba7860d62c653dd
unknown <shalini.kumari-odc@partner.import.io>
2020-10-26 15:16:31 +0530

    mvideo_search

--- src/library/product/search/stores/m/mvideo/RU/paginate.js
+++ src/library/product/search/stores/m/mvideo/RU/paginate.js
@@ -4,10 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'RU',
     store: 'mvideo',
-    nextLinkSelector: null,
+    nextLinkSelector: '#pagnNextString, #pagnNextLink div div.pagination-container div div a.c-pagination__next.font-icon.icon-up',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'body',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'mvideo.ru',
