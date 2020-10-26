d46aa488c746d51911f610bd4c19741cb54801ac
sangeeta <sangeta.kumari@synerzip.com>
2020-10-26 18:30:42 +0530

    868 eroski search

---
+++ src/library/product/search/stores/e/eroski/ES/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'ES',
+    store: 'eroski',
+    domain: 'supermercado.eroski.es',
+    zipcode: '',
+  },
+};
