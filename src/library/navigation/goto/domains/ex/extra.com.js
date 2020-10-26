8d7915502d52eda645709b2f615816f87adbf8c0
atish.shelke <atish.shelke@sz.import.io>
2020-10-26 19:11:52 +0530

    Extra SA search rank fix

--- src/library/navigation/goto/domains/ex/extra.com.js
+++ src/library/navigation/goto/domains/ex/extra.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'extra.com',
-    timeout: null,
+    timeout: 1000000,
     country: 'SA',
     store: 'extra',
     zipcode: "''",
