0368c6dacb1f1a465fb3895e2e1c12330907fbb3
astropaula <wojciechowska.fran@gmail.com>
2020-10-26 12:55:34 +0100

    #768 Updated xpaths, modified extract.js file

--- src/library/product/search/stores/b/belk/US/extract.js
+++ src/library/product/search/stores/b/belk/US/extract.js
@@ -1,5 +1,31 @@
 const { transform } = require('../../../../shared');
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  var data = await context.extract(productDetails, { transform });
+
+  for(let i=0;i<data[0].group.length;i++){
+    if("thumbnail" in data[0].group[i]){
+      for(let j=0;j<2;j++){
+        if(data[0].group[i].thumbnail[j].text=='https://belk.scene7.com/is/image/Belk/defaultimage'){
+        data[0].group[i].thumbnail.splice(j,1);
+        }
+      }
+      if(data[0].group[i].thumbnail.length == 2){
+        data[0].group[i].thumbnail.splice(1,1);
+      }   
+    }
+  }
+
+  return data;
+}
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,4 +35,5 @@ module.exports = {
     domain: 'belk.com',
     zipcode: '',
   },
+  implementation,
 };
