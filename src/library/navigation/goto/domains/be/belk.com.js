33c2206859b5202a14579c457059e5642fb3cbce
astropaula <wojciechowska.fran@gmail.com>
2020-10-26 18:03:20 +0100

    #768 Changes in extract.js, updated xpaths and timeout

--- src/library/navigation/goto/domains/be/belk.com.js
+++ src/library/navigation/goto/domains/be/belk.com.js
@@ -3,9 +3,25 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'belk.com',
-    timeout: 50000,
+    timeout: 100000,
     country: 'US',
     store: 'belk',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    await context.setBlockAds(false);
+    await context.setFirstRequestTimeout(100000);
+    await context.goto(url,
+      {
+        block_ads: false,
+        timeout: 100000,
+        waitUntil: 'load',
+        load_all_resources: true,
+        images_enabled: true,
+      });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
--- src/library/product/search/stores/b/belk/US/extract.js
+++ src/library/product/search/stores/b/belk/US/extract.js
@@ -11,18 +11,25 @@ async function implementation (
   var data = await context.extract(productDetails, { transform });
 
   for(let i=0;i<data[0].group.length;i++){
+
     if("thumbnail" in data[0].group[i]){
-      for(let j=0;j<2;j++){
+ 
+      for(let j=0;j<data[0].group[i].thumbnail.length;j++){
+ 
         if(data[0].group[i].thumbnail[j].text=='https://belk.scene7.com/is/image/Belk/defaultimage'){
-        data[0].group[i].thumbnail.splice(j,1);
+          data[0].group[i].thumbnail.splice(j,1);
         }
       }
       if(data[0].group[i].thumbnail.length == 2){
         data[0].group[i].thumbnail.splice(1,1);
-      }   
+      }  
+    }
+
+    if("price" in data[0].group[i] && data[0].group[i].price.length == 2){
+      data[0].group[i].price[0].text += ' - ' + data[0].group[i].price[1].text
+      data[0].group[i].price.splice(1,1);
     }
   }
-
   return data;
 }
 
@@ -31,7 +38,7 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'belk',
-    transform,
+    transform: null,
     domain: 'belk.com',
     zipcode: '',
   },
