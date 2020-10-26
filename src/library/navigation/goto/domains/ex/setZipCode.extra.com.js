3593cd677115adeb65f4fd0e1f44590143539be8
atish.shelke <atish.shelke@sz.import.io>
2020-10-26 14:21:17 +0530

    Extra SA Core

---
+++ src/library/navigation/goto/domains/ex/setZipCode.extra.com.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'SA',
+    domain: 'extra.com',
+    store: 'extra',
+    zipcode: "''",
+  },
+};
