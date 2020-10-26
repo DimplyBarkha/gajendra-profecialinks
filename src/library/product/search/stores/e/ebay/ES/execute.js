accd88df9f700a289311fc3cf815bec64a7cf78a
PradyumnaD29 <pradyumna.deshpande@synerzip.com>
2020-10-26 19:51:23 +0530

    updated noResultsXpath

--- src/library/product/search/stores/e/ebay/ES/execute.js
+++ src/library/product/search/stores/e/ebay/ES/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'ebay.es',
     url: 'http://www.ebaystores.es/Dyson-Oficial?_nkw={searchTerms}&',
     loadedSelector: 'div[class*="rs-pview"]',
-    noResultsXPath: '//span[contains(@class, "countClass") and contains(text(), "0")]',
+    noResultsXPath: '//span[contains(@class, "countClass") and contains(text(), " 0 ")]',
     zipcode: '',
   },
 };
