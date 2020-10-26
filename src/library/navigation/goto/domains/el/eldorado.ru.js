66078b10d1cbe89ab489d16152b6737724131867
Manikandan Ravi <mailto:r.rahul@olivedatacentre.com>
2020-10-26 15:42:45 +0530

    prasath-eldorado core

---
+++ src/library/navigation/goto/domains/el/eldorado.ru.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'eldorado.ru',
+    timeout: 100000,
+    country: 'RU',
+    store: 'eldorado',
+    zipcode: '',
+  },
+};
