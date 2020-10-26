cef08aad06fda5fba4cfb468d1fab49267d9931c
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-10-26 20:07:07 +0530

    pacson core - akshay channa

---
+++ src/library/product/details/stores/p/pacson/SE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'SE',
+    store: 'pacson',
+    domain: 'pacson.se',
+    loadedSelector: 'body',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/p/pacson/SE/extract.js
@@ -0,0 +1,11 @@
+const { cleanUp } = require('../../../../shared');
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'SE',
+    store: 'pacson',
+    transform: cleanUp,
+    domain: 'pacson.se',
+    zipcode: '',
+  },
+};
