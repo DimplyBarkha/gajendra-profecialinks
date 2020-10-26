9de8ec4012572a51b102b31d49cd6f6ebf7ad693
Karina Miernik <karina.miernik@gmail.com>
2020-10-26 15:38:44 +0100

    #737 added more & improved xpaths

--- src/library/product/details/stores/s/supply/US/extract.js
+++ src/library/product/details/stores/s/supply/US/extract.js
@@ -11,19 +11,45 @@ module.exports = {
     zipcode: '',
   },
   // @ts-ignore
-  // implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
-  //   await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-  //   await context.click('div[data-title="Product Details"]');
-  //   await context.evaluate(async function () {
-  //     function addElementToDocument (key, value) {
-  //       const catElement = document.createElement('div');
-  //       catElement.id = key;
-  //       catElement.textContent = value;
-  //       catElement.style.display = 'none';
-  //       document.body.appendChild(catElement);
-  //     }
-  //   });
-  //   await context.extract(productDetails, { transform });
-  // },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    // await context.click('span[class*= "trigger"]');
+    await context.evaluate(async function () {
+      const moreImgs = document.querySelector('span[class*= "trigger"]');
+      // @ts-ignore
+      if (moreImgs) moreImgs.click();
+    });
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    await context.evaluate(async function () {
+      function addElementToDocument (key, href, value) {
+        const catElement = document.createElement('div');
+        catElement.classList.add(key);
+        catElement.setAttribute('href', href);
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const imgList = document.querySelectorAll('div[data-testId="modal-container"] img')
+        ? document.querySelectorAll('div[data-testId="modal-container"] img') : [];
+      imgList.forEach(e => addElementToDocument('alternate-images', e.src));
 
+      const pdfPresent = document.querySelectorAll('a[href*=".pdf"]')
+        ? document.querySelector('a[href*=".pdf"]') : [];
+      if (pdfPresent) {
+        addElementToDocument('pdfPresent', '#', 'Yes');
+      } else addElementToDocument('pdfPresent', '#', 'No');
+
+      const ratingCount = document.querySelector('button[class*="numReviews"]')
+        // @ts-ignore
+        ? document.querySelector('button[class*="numReviews"]').innerText : '';
+      addElementToDocument('ratingCount', '#', ratingCount.replace(/[()']+/g, ''));
+    });
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    await context.evaluate(async function () {
+      const closeMoreImgs = document.querySelector('a[class*= "trigger"]');
+      // @ts-ignore
+      if (closeMoreImgs) closeMoreImgs.click();
+    });
+    // await context.click('a[class*= "trigger"]');
+    await context.extract(productDetails, { transform });
+  },
 };
