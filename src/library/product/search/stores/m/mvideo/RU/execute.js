972e7420d78bee35907bbd7bc478abcad99e6c74
unknown <shalini.kumari-odc@partner.import.io>
2020-10-26 14:55:22 +0530

    mvideo_search

--- src/library/product/search/stores/m/mvideo/RU/execute.js
+++ src/library/product/search/stores/m/mvideo/RU/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'RU',
     store: 'mvideo',
     domain: 'mvideo.ru',
-    url: 'https://www.mvideo.ru/product-list-page-cls?limit=24&category_id=cat2_cis_0000000064&region_id=1&q=Braun%20%D0%B1%D0%BB%D0%B5%D0%BD%D0%B4%D0%B5%D1%80&offset=24',
+    url: 'https://www.mvideo.ru/product-list-page-cls?q=Braun+%D0%B1%D0%BB%D0%B5%D0%BD%D0%B4%D0%B5%D1%80&region_id=1&limit=24&category_id=cat2_cis_0000000064',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
