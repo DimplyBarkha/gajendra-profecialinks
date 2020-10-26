50f84d9f04e8f180c2a11d5d55b3bb65e38fd6c4
AdamTo <adrec@DESKTOP-3S96701.localdomain>
2020-10-26 09:55:46 +0100

    wip

--- src/library/product/search/stores/s/submarino/BR/extract.js
+++ src/library/product/search/stores/s/submarino/BR/extract.js
@@ -9,16 +9,21 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
 
   await context.evaluate(() => {
     function addProp(selector, iterator, propName, value) {
       document.querySelectorAll(selector)[iterator].setAttribute(propName, value);
     };
     const allProducts = document.querySelectorAll('div.product-grid-item.ColUI-gjy0oc-0.ifczFg.ViewUI-sc-1ijittn-6.iXIDWU');
+    let sum = 0
     for (let i=0; i < allProducts.length; i++){
+      if (document.querySelectorAll('div.RippleContainer-sc-1rpenp9-0.dMCfqq')[i]){
+        sum += 1;
+      }
       addProp('div.RippleContainer-sc-1rpenp9-0.dMCfqq', i,'rankorganic', `${i + 1}`)
     }
+    console.log(sum)
   });
 
   await new Promise((resolve, reject) => setTimeout(resolve, 1500));
