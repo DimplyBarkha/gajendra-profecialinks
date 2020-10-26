e607da53cd86b87bde6045c0e40598dec8e9d81b
sangeeta <sangeta.kumari@synerzip.com>
2020-10-26 18:29:23 +0530

    #867 eroski core

---
+++ src/library/product/details/stores/e/eroski/ES/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'eroski',
+    domain: 'supermercado.eroski.es',
+    //loadedSelector: 'div[class*=product-item]',
+    //noResultsXPath: null,
+    zipcode: '',
+  },
+};
