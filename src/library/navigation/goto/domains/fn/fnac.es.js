6a6939c1c9d8e5d72a410f49db04f10f210088a1
prateek.s <psc3018@gmail.com>
2020-10-27 17:27:00 +0530

    updated xpath and added some functions for video extraction

--- src/library/navigation/goto/domains/fn/fnac.es.js
+++ src/library/navigation/goto/domains/fn/fnac.es.js
@@ -7,5 +7,24 @@ module.exports = {
     country: 'ES',
     store: 'fnac',
     zipcode: '',
+  },implementation: async (
+    { url, zipcode, storeId },
+    parameters, context, dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    const maxRetries = 3;
+    let numberOfCaptchas = 0;
+
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+
+    await context.goto(url, {
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: true,
+    });
   },
 };
