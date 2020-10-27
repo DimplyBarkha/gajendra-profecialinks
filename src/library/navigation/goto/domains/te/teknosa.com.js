c7925e90c7c038040068d0c04e4fb368859e74c5
Borys Maciuch <51968632+BorysMaciuch@users.noreply.github.com>
2020-10-27 11:01:42 +0100

    Tdcm teknosa tr search (#885)
    * Added issue template for new sources
    
    All possible documentation is gathered here
    
    * First edit
    
    * Finalized
    
    * added md extension
    
    * Update extractor-build.md
    
    * small change
    
    * Remove option for blank issues
    
    * Update extractor-build.md
    
    * Update digital-shelf-source-build.md
    
    * Update extractor-build.md
    
    * Update extractor-build.md
    
    * Update extractor-build.md
    
    * Update marketshare-extensions-source-build.md
    
    * Update digital-shelf-source-build.md
    
    * Init search extractor
    
    * Update schema, xpaths and url
    
    * Add ranking function
    
    * Add pagination
    
    * Fixed price and name, changed timeout
    
    * Reverse nav timeout change
    
    * Reverse changes in master files and add inputs
    
    * Delete schema empty line
    
    * Fix inputs keys
    
    * Change price xpath
    
    * Add tests
    
    * Run linter
    
    * Change productUrl xpath
    
    * Change rank, pagination and productUrl
    
    * Add url function
    
    * Add tests
    
    * Add recordXpath
    
    * Add tests
    
    * Rerun tests
    
    * Change manufacturer xpath
    
    * Change xpath for manufacturer
    
    Co-authored-by: Guillaume-Vial <61331399+Guillaume-Vial@users.noreply.github.com>

---
+++ src/library/navigation/goto/domains/te/teknosa.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'teknosa.com',
+    timeout: 30000,
+    country: 'TR',
+    store: 'teknosa',
+    zipcode: '',
+  },
+};
