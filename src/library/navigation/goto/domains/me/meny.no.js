6f1360c4ff162a22528b86672128714b749dc8bb
Manas Ranjan <manasranjan@pop-os.localdomain>
2020-10-27 13:34:04 +0530

    Fixed search extractor

--- src/library/navigation/goto/domains/me/meny.no.js
+++ src/library/navigation/goto/domains/me/meny.no.js
@@ -3,10 +3,18 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'meny.no',
-    timeout: 90000,
+    timeout: 30000,
     country: 'NO',
     store: 'meny',
     zipcode: '',
   },
-
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.captureRequests();
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
--- src/library/product/search/stores/m/meny/NO/extract.js
+++ src/library/product/search/stores/m/meny/NO/extract.js
@@ -8,26 +8,55 @@ module.exports = {
     domain: 'meny.no',
     zipcode: '',
   },
-  // implementation: async function (
-  //   inputs,
-  //   parameters,
-  //   context,
-  //   dependencies,
-  // ) {
-  //   const { productDetails } = dependencies;
-  //   const { transform } = parameters;
-  //   await context.evaluate(async function(context) {
-  //     const seeAllSelector = document.querySelector("div[id='ws-search-block-products']>button[class*='ws-link']");
-  //     if (seeAllSelector) {
-  //       console.log('See all selector found');
-  //       seeAllSelector.click()
-  //       console.log('See all selector clicked');
-  //       await new Promise(resolve => { setTimeout(resolve, 20000) })
-  //     }
-      
-  //   },context);
-    
-  //   return await context.extract(productDetails, { transform })
-  // }
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { productDetails } = dependencies;
+    const { transform } = parameters;
 
+    const optionalWait = async (sel) => {
+      try {
+        await context.waitForSelector(sel, { timeout: 30000 });
+        console.log(`Selector => "${sel}" loaded`);
+        return true;
+      } catch (err) {
+        console.log(`Selector => ${sel} did not load.`);
+        return false;
+      }
+    };
+
+    const seeAll = await optionalWait("div[id='ws-search-block-products']>button[class*='ws-link']");
+
+    if (seeAll) {
+      await context.click("div[id='ws-search-block-products']>button[class*='ws-link']");
+      await context.waitForNavigation({ timeout: 30000, waitUntil: 'networkidle0' });
+    }
+
+    const response = await context.searchAllRequests('/episearch/.*');
+
+    for (const res of response) {
+      if (!res.responseBody || !res.responseBody.body) continue;
+      const searchResponse = JSON.parse(res.responseBody.body);
+
+      if (!searchResponse.hits || !searchResponse.hits.hits) continue;
+
+      for (const hit of searchResponse.hits.hits) {
+        await context.evaluate(function (param) {
+          const imgId = param.contentId;
+          const url = param.contentData && param.contentData._source && param.contentData._source.slugifiedUrl;
+
+          if (!imgId || !url) return;
+
+          const img = document.querySelector(`img[src*="${param.contentId}"]`);
+          console.log(`Setting URL for ${param.contentId}`);
+          img.setAttribute('url', `https://meny.no/varer${url}`);
+        }, hit);
+      }
+    }
+
+    return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
+  },
 };
