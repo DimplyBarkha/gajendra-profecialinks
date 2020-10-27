d3d83f5a52fe74723a4f23c7c9dc41d77052ce2b
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-10-27 10:29:02 +0100

    created a template for the core extractor

---
+++ src/library/product/details/stores/m/mifarma/ES/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'mifarma',
+    domain: 'mifarma.es',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/m/mifarma/ES/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'ES',
+    store: 'mifarma',
+    transform: null,
+    domain: 'mifarma.es',
+    zipcode: '',
+  },
+};
