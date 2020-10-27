bdd62b4575414d71f570bd10a0a73ac8de891883
sangeeta <sangeta.kumari@synerzip.com>
2020-10-27 14:24:52 +0530

    #893 riachuelo core

--- src/library/product/details/stores/r/riachuelo/BR/extract.js
+++ src/library/product/details/stores/r/riachuelo/BR/extract.js
@@ -1,11 +1,39 @@
-
+const { transform } = require('../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  //await new Promise((resolve, reject) => setTimeout(resolve, 20000));
+  var variantLength = await context.evaluate(async () => {
+    return (document.querySelectorAll('div[class="swatch-attribute-options clearfix"] a')) ? document.querySelectorAll('div[class="swatch-attribute-options clearfix"] a').length : 0;
+  });
+  console.log("variantLength:: ", variantLength);
+  if (variantLength > 1) {
+    // await preparePageForCommonElement(0, variantLength);
+    for (let j = 0; j < variantLength; j++) {
+      await context.evaluate(async (j) => {
+        return document.querySelectorAll('a[class="swatch-option   text"]')[j].click();
+      }, j);
+      // await context.click(`ul.topic li label`);
+      console.log('Inside variants', j);
+      // await preparePage(j, variantLength);
+      if (j !== variantLength - 1) { await context.extract(productDetails, { transform }, { type: 'APPEND' }); }
+    }
+  }
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'BR',
     store: 'riachuelo',
-    transform: null,
+    transform: transform,
     domain: 'riachuelo.com.br',
     zipcode: '',
   },
+  implementation,
 };
---
+++ src/library/product/details/stores/r/riachuelo/shared.js
@@ -0,0 +1,61 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
+  for (const { group } of data) {
+    for (const row of group) {
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+     
+      if (row.productUrl) {
+        let url = '';
+        row.productUrl.forEach(item => {
+          //console.log('url----', item.text);
+          url = item.text.value,
+          url = item.text.split(',')[2];
+          //console.log("partial url1: ", url);
+          url = url.split(':')[1];
+          url = url.replace('}}', '');
+          url = url.replace('"', '');
+          //console.log("partial url: ", url);
+          url ='https://www.riachuelo.com.br'+url;
+          url = url.replace('\"', '');
+          console.log('url', url);
+          item.text = url;
+
+        });
+      }
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
+    }
+  }
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  console.log(productCodes);
+  return data;
+};
+module.exports = { transform };
