4b39d4b6c4991b011694ec053178bba4f8e0779a
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-10-27 21:45:52 +0530

    lowes - Damayanthi core

--- src/library/product/search/stores/l/lowes/US/paginate.js
+++ src/library/product/search/stores/l/lowes/US/paginate.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'lowes',
-    nextLinkSelector: '#main > div > div.splc > div.ru7jno-0.jHJUqJ > div > div.pl > div > div > div.k0dn21-0.jSQPlH > ul > li:nth-child(7) > a.Link__LinkStyled-RC__sc-b3hjw8-0',
+    nextLinkSelector: 'a[aria-label="arrow right"]href',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'body',
