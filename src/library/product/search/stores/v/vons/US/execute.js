6ad24023bc52ed490cbd81f5c1b6f9a57c9f5694
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-10-27 14:17:14 +0530

    vons

--- src/library/product/search/stores/v/vons/US/execute.js
+++ src/library/product/search/stores/v/vons/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'vons',
     domain: 'vons.com',
-    url: 'https://www.vons.com/shop/search-results.html?q=%22beer%2012%20pack%22',
+    url: 'https://www.vons.com/shop/search-results.html?q={searchTerms}',
     loadedSelector: '#search-grid_0 > div.row.gutters-items-v2.grid-wrapper.product-grid-v2 > product-item-v2',
     noResultsXPath: null,
     zipcode: '',
