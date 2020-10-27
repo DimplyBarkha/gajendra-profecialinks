61eba1aa0e575e07621f6b92acad8d0d14c72eb9
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-10-27 08:43:41 +0100

    created a template for the core extractor

---
+++ src/library/product/details/stores/m/Morrisons/UK/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'groceries.morrisons.com',
+    prefix: null,
+    url: null,
+    country: 'UK',
+    store: 'morrisons',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/m/Morrisons/UK/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'morrisons',
+    domain: 'groceries.morrisons.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
