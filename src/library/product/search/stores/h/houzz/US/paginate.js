59725118de10294e7f64eef3dc194aecf1924784
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-10-28 20:10:56 +0530

    Changes made according to QA

--- src/library/product/search/stores/h/houzz/US/paginate.js
+++ src/library/product/search/stores/h/houzz/US/paginate.js
@@ -6,14 +6,18 @@ module.exports = {
     nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector:
-      "#hz-br__result-set-root > div.hz-card.clearfix.hz-br__result-set > div:nth-child(3) > div > div",
+    loadedSelector: null,
+    // "#hz-br__result-set-root > div.hz-card.clearfix.hz-br__result-set > div:nth-child(3) > div > div",
     noResultsXPath: null,
-    openSearchDefinition: {
-      template:
-        "https://www.houzz.com/products/query/{searchTerms}/nqrwns/p/{page}?oq={searchTerms}",
-    },
+    openSearchDefinition: null,
+    // {
+    //   template:
+    //     // "https://www.houzz.com/products/query/{rug}/nqrwns/p/{108}?oq={rug}",
+    //     null,
+    // },
     domain: "houzz.com",
     zipcode: "",
   },
 };
+
+// "https://www.houzz.com/products/query/{searchTerms}/nqrwns/p/{page}?oq={searchTerms}",
