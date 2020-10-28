6fef1afb1e8afbdb2d6f4d68597400b0fccde363
jignesh013 <jignesh@profecialinks.com>
2020-10-28 20:06:15 +0530

    Added cleanup function

--- src/library/product/search/stores/s/sprii/format.js
+++ src/library/product/search/stores/s/sprii/format.js
@@ -4,6 +4,19 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
   for (const { group } of data) {
     var rankIndex = 1;
     for (const row of group) {
@@ -21,6 +34,9 @@ const transform = (data) => {
         row.rankOrganic[0].text = rankIndex;
       }
       rankIndex++;
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
     }
   }
   return data;
