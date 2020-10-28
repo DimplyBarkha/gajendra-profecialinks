4385a72ed9cd0fde1aab2a7fc4123c37e05b4ac8
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-10-28 12:33:56 +0530

    load selector xpath added (#713)

--- src/library/navigation/goto/domains/eg/egypt.souq.com.js
+++ src/library/navigation/goto/domains/eg/egypt.souq.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'egypt.souq.com',
-    timeout: null,
+    timeout: 90000,
     country: 'EG',
     store: 'souq',
     zipcode: '',
--- src/library/product/details/stores/s/souq/EG/execute.js
+++ src/library/product/details/stores/s/souq/EG/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'EG',
     store: 'souq',
     domain: 'egypt.souq.com',
-    loadedSelector: 'div[class="tpl-results"]',
-    noResultsXPath: '//div[contains(@class , "zero-results")] | //div[contains(@class,"warning callout zero-results")]',
+    loadedSelector: 'div[class="tpl-results"] , div[class="product_content"]',
+    noResultsXPath: '//div[contains(@class , "zero-results")] | //div[contains(@class,"warning callout zero-results")] | //ul/li[contains(text(),"item can\'t be found")]',
     zipcode: '',
   },
 };
