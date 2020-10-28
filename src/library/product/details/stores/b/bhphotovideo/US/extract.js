d563549459c422f392f2b5f0c3a31252160780f0
Julia Lojek <julialojek@gmail.com>
2020-10-28 09:08:37 +0100

    #769 core extractor fields finished

--- src/library/product/details/stores/b/bhphotovideo/US/extract.js
+++ src/library/product/details/stores/b/bhphotovideo/US/extract.js
@@ -13,6 +13,43 @@ module.exports = {
     context,
     { productDetails },
   ) => {
-    await context.extract(productDetails);
+    await context.evaluate(async () => {
+      let scrollTop = 0;
+      while (scrollTop !== 15000) {
+        await stall(1000);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 15000) {
+          await stall(1000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+
+    await context.evaluate(async () => {
+      const image360 = document.querySelector('button[data-selenium="inlineMediaOpenMedia inlineMedia3DGallery"]');
+      const image360Present = image360 ? 'Yes' : 'No';
+      document.querySelector('body').setAttribute('image360Present', image360Present);
+    });
+
+    var dataRef = await context.extract(productDetails);
+    const descriptions = dataRef[0].group[0].manufacturerDescription;
+    if (descriptions) {
+      descriptions.forEach((desc) => {
+        descriptions[0].text += ' ' + desc.text;
+      });
+      descriptions.splice(1);
+    }
+    const customerServiceAvailability = dataRef[0].group[0].customerServiceAvailability;
+    if (customerServiceAvailability && customerServiceAvailability[0].text !== 'No') {
+      customerServiceAvailability[0].text = 'Yes';
+    }
   },
 };
