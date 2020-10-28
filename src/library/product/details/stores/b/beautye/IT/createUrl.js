fcdc6072d7c01cb1da5268482b529cf9941e55f4
sangeeta <sangeta.kumari@synerzip.com>
2020-10-28 12:59:26 +0530

    #935 intial submit

---
+++ src/library/product/details/stores/b/beautye/IT/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'beautye.it',
+    prefix: null,
+    url: null,
+    country: 'IT',
+    store: 'beautye',
+    zipcode: '',
+  },
+};
