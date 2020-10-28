2ab6b5337b452bfa9ee56c263a7bd82119d06ab1
jignesh013 <jignesh@profecialinks.com>
2020-10-28 20:12:00 +0530

    Added cleanup function

--- src/library/product/details/stores/s/sprii/format.js
+++ src/library/product/details/stores/s/sprii/format.js
@@ -4,6 +4,19 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
   for (const { group } of data) {
     for (const row of group) {
       if (row.color) {
@@ -193,6 +206,9 @@ const transform = (data) => {
       }
     }
   }
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = el.text ? clean(el.text) : el.text;
+  }))));
   return data;
 };
 
