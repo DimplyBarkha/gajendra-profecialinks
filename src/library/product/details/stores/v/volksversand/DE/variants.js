16fb0247e64a49776747161e8e73b62ed55945e8
Gajender Ramola <ramola106@gmail.com>
2020-10-28 11:19:30 +0530

    variant added

---
+++ src/library/product/details/stores/v/volksversand/DE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'DE',
+    store: 'volksversand',
+    domain: 'volksversand.de',
+    zipcode: '',
+  },
+};
