d03e36e9de73bd397989717104b25c095b79d09b
AdamTo <adrec@DESKTOP-3S96701.localdomain>
2020-10-28 11:38:05 +0100

    wip #736

--- src/library/product/search/stores/s/submarino/BR/extract.js
+++ src/library/product/search/stores/s/submarino/BR/extract.js
@@ -16,14 +16,9 @@ async function implementation (
       document.querySelectorAll(selector)[iterator].setAttribute(propName, value);
     };
     const allProducts = document.querySelectorAll('div.product-grid-item.ColUI-gjy0oc-0.ifczFg.ViewUI-sc-1ijittn-6.iXIDWU');
-    let sum = 0
-    for (let i=0; i < allProducts.length; i++){
-      if (document.querySelectorAll('div.RippleContainer-sc-1rpenp9-0.dMCfqq')[i]){
-        sum += 1;
-      }
-      addProp('div.RippleContainer-sc-1rpenp9-0.dMCfqq', i,'rankorganic', `${i + 1}`)
+    for (let i = 0; i < allProducts.length; i++) {
+      addProp('div.RippleContainer-sc-1rpenp9-0.dMCfqq', i,'rankorganic', `${i + 1}`);
     }
-    console.log(sum)
   });
 
   await new Promise((resolve, reject) => setTimeout(resolve, 1500));
