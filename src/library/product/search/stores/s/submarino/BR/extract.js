019ddaf0a4bbff4c15d152c2c04720e23caf2141
AdamTo <adrec@DESKTOP-3S96701.localdomain>
2020-10-28 21:45:54 +0100

    working final search #736

--- src/library/product/search/stores/s/submarino/BR/extract.js
+++ src/library/product/search/stores/s/submarino/BR/extract.js
@@ -16,17 +16,38 @@ async function implementation (
       document.querySelectorAll(selector)[iterator].setAttribute(propName, value);
     };
     const allProducts = document.querySelectorAll('div.product-grid-item.ColUI-gjy0oc-0.ifczFg.ViewUI-sc-1ijittn-6.iXIDWU');
+    const allSponsored = document.querySelectorAll('a.card-product-url');
+    let multiplier = 0;
+    if (allSponsored[0]) {
+      multiplier = allSponsored.length;
+    };
+
     for (let i = 0; i < allProducts.length; i++) {
-      addProp('div.RippleContainer-sc-1rpenp9-0.dMCfqq', i,'rankorganic', `${i + 1}`);
+      addProp('div.RippleContainer-sc-1rpenp9-0.dMCfqq', i, 'rankorganic', `${i + 1}`);
+      addProp('div.RippleContainer-sc-1rpenp9-0.dMCfqq', i, 'rank', `${i + 1 + multiplier}`);
+
+      // if I try add id byy extract.yaml and XPATH sellector there always missing random number of ids.
+      let id = document.querySelectorAll('div[class="row product-grid no-gutters main-grid"] > div > div > a')[i].href;
+      id = id.match(/\/(\d+)\?/)[1];
+      addProp('div.RippleContainer-sc-1rpenp9-0.dMCfqq', i, 'currentid', `${id}`);
+      addProp('div.RippleContainer-sc-1rpenp9-0.dMCfqq', i, 'urldata', `https://www.submarino.com.br/produto/${id}`);
+    };
+
+    if (allSponsored[0]) {
+      console.log('sponsored exists')
+      for (let i = 0; i < allSponsored.length; i++) {
+        const sponsoredId = allSponsored[i].href.match(/\/(\d+)\?/)[1];
+        addProp('a.card-product-url', i, 'currentid', `${sponsoredId}`);
+        addProp('a.card-product-url', i, 'urldata', `https://www.submarino.com.br/produto/${sponsoredId}`);
+        addProp('a.card-product-url', i, 'rank', `${i + 1}`);
+      }
     }
   });
 
-  await new Promise((resolve, reject) => setTimeout(resolve, 1500));
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
 
   return await context.extract(productDetails, { transform });
 }
-
-
 
 module.exports = {
   implements: 'product/search/extract',
