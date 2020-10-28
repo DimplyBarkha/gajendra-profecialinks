2f7a8a66be690edc4dd20b8c5c0875f3c64d4a28
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-10-28 14:37:43 +0530

    QA Fixes

--- src/library/product/search/stores/c/calphalon/format.js
+++ src/library/product/search/stores/c/calphalon/format.js
@@ -4,27 +4,31 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    var p_count = 1
-    for (const { group } of data) {
-        for (const row of group) {          
-            if (row.rankOrganic && row.rank) {
-              row.rankOrganic = [{'text':p_count}];
-              row.rank = [{'text':p_count}];
-              p_count = p_count + 1;
-            }
-            if (row.aggregateRating) {
-                row.aggregateRating.forEach(item => {
-                    var myRegexp = /bv-width-from-rating-stats-(\d+)/g;
-                    var match = myRegexp.exec(item.text);
-                    if (match.length) {
-                        item.text = match[1];
-                        item.text = (item.text*5)/100
-                    }
-                });                
-            }          
-        }
+  var rank = 1
+  for (const { group } of data) {
+    for (const row of group) {
+      row.rankOrganic = row.rank = [{ 'text': rank }];
+      if (row.aggregateRating) {
+        row.aggregateRating.forEach(item => {
+          var myRegexp = /bv-width-from-rating-stats-(\d+)/g;
+          var match = myRegexp.exec(item.text);
+          if (match.length) {
+            item.text = match[1];
+            item.text = (item.text * 5) / 100;
+          }
+        });
+      }
+      if (row.thumbnail) {
+        row.thumbnail.forEach(item => {
+          if (item.text.indexOf('https') < 0) {
+            item.text = "https:" + item.text;
+          }
+        });
+      }
+      rank = rank + 1;
     }
-    return data;
-  };
-  
-  module.exports = { transform };
+  }
+  return data;
+};
+
+module.exports = { transform };
