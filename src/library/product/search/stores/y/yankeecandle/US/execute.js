d6860dcb469dd6d20d7a9c200e47f93798ea3f25
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-10-28 16:00:05 +0530

    Yankeecandle - Damayanthi

--- src/library/product/search/stores/y/yankeecandle/US/execute.js
+++ src/library/product/search/stores/y/yankeecandle/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'yankeecandle',
     domain: 'yankeecandle.com',
-    url: 'https://www.yankeecandle.com/search?Ntt={searchTerms}&Ns=sku.stockLevel|1||DefaultSort|1||sku.bestSeller|1||sku.new|1||sku.ranking|0||product.heilerParentProdNbr|0&&No=0&Nrpp=27',
+    url: 'https://www.yankeecandle.com/search?Ntt={searchTerms}&Ns=sku.stockLevel|1||DefaultSort|1||sku.bestSeller|1||sku.new|1||sku.ranking|0||product.heilerParentProdNbr|0&&No=0&Nrpp=850',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
