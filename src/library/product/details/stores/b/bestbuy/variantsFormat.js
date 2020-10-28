37af3dd62c50470b5c074da243e1a922e003f9f8
ganesh <ganesh@profecialinks.com>
2020-10-28 12:15:51 +0530

    fix variant js code

--- src/library/product/details/stores/b/bestbuy/variantsFormat.js
+++ src/library/product/details/stores/b/bestbuy/variantsFormat.js
@@ -3,55 +3,16 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
-    let final_variations = [];
-    let variations = [];
+const transform = (data) => {    
     for (const { group } of data) {
-        for (const row of group) {    
-            let v_url = [];
-            let v_url_xpath = [];
-            let v_id = [];
-            let v_data = [];
-            let v_xpath = [];
+        for (const row of group) {                
             if (row.variantUrl) {
                 row.variantUrl.forEach(item => {
-                    v_url.push("https://www.bestbuy.com.mx/p/"+item.text)
-                    v_url_xpath.push(item.xpath)
+                    item.text = "https://www.bestbuy.com.mx/p/"+item.text;
                 });
             }
-            if (row.variantId) {
-                row.variantId.forEach(item => {
-                    v_id.push(item.text)
-                    v_xpath.push(item.xpath)
-                });
-            }
-            if (row.variant) {
-                row.variant.forEach(item => {
-                    v_data.push(item.text)
-                });
-            }
-            v_id.forEach((v_item,index) => {
-            
-                let tmp_variations = {};
-                tmp_variations["variantId"] = [];
-                tmp_variations["variantUrl"] = [];
-                tmp_variations["variant"] = [];
-    
-                            
-                tmp_variations["variantUrl"].push({"text": v_url[index], "xpath": v_url_xpath[index]});
-                tmp_variations["variantId"].push({"text": v_id[index], "xpath": v_xpath[index]});
-                tmp_variations["variant"].push({"text": v_data[index], "xpath": v_xpath[index]});
-                final_variations.push(tmp_variations);
-            });            
-                          
-                         
-                
-            
         }
-    }
-    if(final_variations.length){
-        data[0]["group"] = final_variations;
-    }
+    }    
     return data;
   };
   
