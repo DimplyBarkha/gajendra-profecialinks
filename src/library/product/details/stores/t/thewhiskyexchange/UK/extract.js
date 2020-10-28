8903b68c1b4c2bcb6c33d47dca3798f834d82f4b
byQ96 <michal126@hotmail.com>
2020-10-28 09:45:39 +0100

    removed unused code

--- src/library/product/details/stores/t/thewhiskyexchange/UK/extract.js
+++ src/library/product/details/stores/t/thewhiskyexchange/UK/extract.js
@@ -1,24 +1,10 @@
-const { cleanUp } = require('../../../../shared');
-
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'UK',
     store: 'thewhiskyexchange',
-    transform: cleanUp,
+    transform: null,
     domain: 'thewhiskyexchange.com',
     zipcode: '',
   },
-  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
-    await context.evaluate(async function () {
-      function addElementToDom (element, id) {
-        const div = document.createElement('div');
-        div.id = id;
-        div.innerHTML = element;
-        document.body.appendChild(div);
-      }
-      
-    });
-    await context.extract(productDetails);
-  }
 };
