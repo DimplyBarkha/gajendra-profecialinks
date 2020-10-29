794f41e0c5218ce7a30bbf031267eaf1c7a6ebb4
Wayne E <bartekstafiej2@gmail.com>
2020-10-29 18:43:35 +0100

    Create new source

---
+++ src/library/product/search/stores/a/alcampo/ES/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'ES',
+    store: 'alcampo',
+    transform: null,
+    domain: 'alcampo.es',
+    zipcode: "''",
+  },
+};
