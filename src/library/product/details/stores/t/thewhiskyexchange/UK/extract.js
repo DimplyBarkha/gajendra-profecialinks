d15a5fb371d127f1c0cd14e5ed7fb6cea95fbd33
byQ96 <michal126@hotmail.com>
2020-10-29 15:17:55 +0100

    some minor changes, changed inputs

--- src/library/product/details/stores/t/thewhiskyexchange/UK/extract.js
+++ src/library/product/details/stores/t/thewhiskyexchange/UK/extract.js
@@ -1,10 +1,38 @@
+const { transform } = require('../../../../shared');
+
+async function implementation(inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async () => {
+    var prodImg = document.querySelector(
+      'div[class="product-main__image-container"] img[class="product-slider__image lazyloaded"]'
+    ).getAttribute('src');
+    
+    if (prodImg !== null){
+      var prodContainer = document.querySelector('div[class="product-main__image-container"]')
+      prodContainer.setAttribute('img_url', prodImg);
+    }
+    else{
+      prodImg = document.querySelector(
+        'div[class="product-main__image-container"] img'
+      ).getAttribute('src');
+      var prodContainer = document.querySelector('div[class="product-main__image-container"]')
+      prodContainer.setAttribute('img_url', prodImg);
+    }
+  });
+
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'UK',
     store: 'thewhiskyexchange',
-    transform: null,
+    transform: transform,
     domain: 'thewhiskyexchange.com',
     zipcode: '',
   },
+  implementation,
 };
