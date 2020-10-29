33494963257d16d1df847a342d4f1d923a5931d1
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-10-29 23:56:15 +0530

    searchTerms - akshay channa

--- src/library/product/search/stores/c/cabelas/US/execute.js
+++ src/library/product/search/stores/c/cabelas/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'cabelas',
     domain: 'cabelas.com',
-    url: 'https://www.cabelas.com/shop/en/SearchDisplay?categoryId=&storeId=10651&catalogId=10551&langId=-1&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=16&searchTerm=6+person+tent',
+    url: 'https://www.cabelas.com/shop/en/SearchDisplay?categoryId=&storeId=10651&catalogId=10551&langId=-1&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=16&searchTerm={searchTerms}#facet:&productBeginIndex:0&facetLimit:&orderBy:&pageView:grid&minPrice:&maxPrice:&pageSize:&',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
