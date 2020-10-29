28c9683a8b499f7fa69fae28f807d1abacb264c3
sangeeta <sangeta.kumari@synerzip.com>
2020-10-29 14:25:55 +0530

    #893 riachuelo

--- src/library/product/details/stores/r/riachuelo/shared.js
+++ src/library/product/details/stores/r/riachuelo/shared.js
@@ -1,9 +1,4 @@
-/**
- *
- * @param {ImportIO.Group[]} data
- * @returns {ImportIO.Group[]}
- */
-const transform = (data, context) => {
+const transform = (data) => {
   const clean = text => text.toString()
     .replace(/\r\n|\r|\n/g, ' ')
     .replace(/&amp;nbsp;/g, ' ')
@@ -16,46 +11,69 @@ const transform = (data, context) => {
     // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-  const state = context.getState();
-  let orgRankCounter = state.orgRankCounter || 0;
-  let rankCounter = state.rankCounter || 0;
-  const productCodes = state.productCodes || [];
+
   for (const { group } of data) {
     for (const row of group) {
-      rankCounter += 1;
-      if (!row.sponsored) {
-        orgRankCounter += 1;
-        row.rankOrganic = [{ text: orgRankCounter }];
-      }
-      row.rank = [{ text: rankCounter }];
-     
-      if (row.productUrl) {
-        let url = '';
-        row.productUrl.forEach(item => {
-          //console.log('url----', item.text);
-          url = item.text.value,
-          url = item.text.split(',')[2];
-          //console.log("partial url1: ", url);
-          url = url.split(':')[1];
-          url = url.replace('}}', '');
-          url = url.replace('"', '');
-          //console.log("partial url: ", url);
-          url ='https://www.riachuelo.com.br'+url;
-          url = url.replace('\"', '');
-          console.log('url', url);
-          item.text = url;
-
+      if (row.shippingDimensions) {
+        row.shippingDimensions.forEach(item => {
+          const locText = item.text;
+          if (locText.indexOf('###') > 0) {
+            item.text = locText.substring(0, locText.indexOf('###'));
+          } else if (locText.indexOf('###') === 0) {
+            item.text = locText.replace('###', '');
+          }
+          console.log(item.text);
         });
       }
-      Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-      }));
+
+      if (row.description) {
+        row.description.forEach(item => {
+          const locText = item.text;
+          const length = locText.length;
+          const startIdx = locText.indexOf('\n \n \n');
+          let lastIdx = locText.lastIndexOf('\n \n \n');
+          if (startIdx === lastIdx) lastIdx = length - 1;
+          let firstStr = locText.substring(0, startIdx);
+          firstStr = firstStr.replace(/\n \n/g, ' ');
+          let secStr = locText.substring(startIdx, lastIdx);
+          secStr = secStr.replace(/\n \n \n/g, ' || ');
+          secStr = secStr.replace(/\n \n/g, ' || ');
+          let lastString = locText.substring(lastIdx, length);
+          lastString = lastString.replace(/\n \n \n/g, ' || ');
+          lastString = lastString.replace(/\n \n/g, ' ');
+
+          item.text = firstStr + secStr + lastString;
+          // console.log(item.text);
+        });
+      }
+
+      if (row.additionalDescBulletInfo) {
+        row.additionalDescBulletInfo.forEach(item => {
+          item.text = item.text.replace(/\n \n/g, ' | ');
+          // console.log(item.text);
+        });
+      }
+
+      if (row.specifications) {
+        const nDesc = [];
+        let newDesc = '';
+        let idx = 0;
+        row.specifications.forEach(item => {
+          nDesc[0] = item;
+          if (idx > 0) {
+            newDesc = newDesc + '||';
+          }
+          newDesc = newDesc + item.text;
+          idx++;
+        });
+        nDesc.forEach(item => {
+          item.text = newDesc;
+        });
+        row.specifications = nDesc;
+      }
+
     }
-  }
-  context.setState({ rankCounter });
-  context.setState({ orgRankCounter });
-  context.setState({ productCodes });
-  console.log(productCodes);
   return data;
 };
+
 module.exports = { transform };
--- src/library/product/search/stores/r/riachuelo/shared.js
+++ src/library/product/search/stores/r/riachuelo/shared.js
@@ -20,16 +20,42 @@ const transform = (data, context) => {
   let orgRankCounter = state.orgRankCounter || 0;
   let rankCounter = state.rankCounter || 0;
   const productCodes = state.productCodes || [];
-  for (const { group } of data) {    
+  for (const { group } of data) {
+    for (const row of group) {
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+     
+      if (row.productUrl) {
+        let url = '';
+        row.productUrl.forEach(item => {
+          //console.log('url----', item.text);
+          url = item.text.value,
+          url = item.text.split(',')[2];
+          //console.log("partial url1: ", url);
+          url = url.split(':')[1];
+          url = url.replace('}}', '');
+          url = url.replace('"', '');
+          //console.log("partial url: ", url);
+          url ='https://www.riachuelo.com.br'+url;
+          url = url.replace('\"', '');
+          console.log('url', url);
+          item.text = url;
+
+        });
+      }
       Object.keys(row).forEach(header => row[header].forEach(el => {
         el.text = clean(el.text);
       }));
     }
   }
-  // context.setState({ rankCounter });
-  // context.setState({ orgRankCounter });
-  // context.setState({ productCodes });
-  // console.log(productCodes);
-  // return data;
-// };
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  console.log(productCodes);
+  return data;
+};
 module.exports = { transform };
