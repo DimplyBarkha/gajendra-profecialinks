d01fbaf590ce3af2aa84ed0cf44ddb7fa428ff39
sangeeta <sangeta.kumari@synerzip.com>
2020-10-29 13:15:17 +0530

    rebase

---
+++ src/library/navigation/goto/domains/el/elgiganten.se.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'elgiganten.se',
+    timeout: 20000,
+    store: 'elgiganten',
+    country: 'SE',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/e/elgiganten/SE/createUrl.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'elgiganten.se',
+    prefix: null,
+    url: 'https://www.elgiganten.se/search?SearchTerm={id}',
+    store: 'elgiganten',
+    country: 'SE',
+    zipcode: '',
+  },
+};
+
