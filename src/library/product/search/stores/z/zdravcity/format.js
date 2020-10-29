a30d698b184b7f14342410b939a86ea0c3700947
Gajender Ramola <ramola106@gmail.com>
2020-10-29 12:17:30 +0530

    price issue fixed

--- src/library/product/search/stores/z/zdravcity/format.js
+++ src/library/product/search/stores/z/zdravcity/format.js
@@ -6,12 +6,19 @@
 const transform = (data) => {
     var p_count = 1
     for (const { group } of data) {
-        for (const row of group) {          
-            if (row.rankOrganic && row.rank) {
-              row.rankOrganic = [{'text':p_count}];
-              row.rank = [{'text':p_count}];
-              p_count = p_count + 1;
-            }          
+        for (const row of group) {
+          if (row.thumbnail) {
+            row.thumbnail.forEach(item => {
+              item.text = 'https://www.wine.com'.concat(item.text);
+            });
+          }
+          if (row.price) {
+            row.price.forEach(item => {
+              item.text = item.text.replace('.',',');
+            });
+          }
+          row.rank = row.rankOrganic = [{"text": p_count}];
+          p_count = p_count + 1;
         }
     }
     return data;
