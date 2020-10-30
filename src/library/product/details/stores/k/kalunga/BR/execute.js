7bb717d393b8dfdf840505bc880160117421ce1b
Ihor <ihor.vasylechko-makeitease@partner.import.io>
2020-10-30 14:13:39 +0200

    kalunga core

---
+++ src/library/product/details/stores/k/kalunga/BR/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'BR',
+    store: 'kalunga',
+    domain: 'kalunga.com.br',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/k/kalunga/BR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'BR',
+    store: 'kalunga',
+    transform: null,
+    domain: 'kalunga.com.br',
+    zipcode: '',
+  },
+};
