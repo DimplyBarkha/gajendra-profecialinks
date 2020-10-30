55ce0219d636ddb08022d5a533d04b93570c2d8f
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-10-30 15:55:07 +0530

    devwhiskydetails

--- src/library/product/search/stores/w/whisky/DE/execute.js
+++ src/library/product/search/stores/w/whisky/DE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DE',
     store: 'whisky',
     domain: 'whisky.de',
-    url: 'https://at.whisky.de/shop/index.php?cl=search&searchparam={searchTerms}',
+    url: 'https://at.whisky.de/shop/index.php?cl=search&searchparam=%22Rum%22&searchorigin=1&_artperpage=10&pgNr=0',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
