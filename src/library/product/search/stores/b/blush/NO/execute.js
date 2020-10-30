909977e25f1192ec11cf2f1c9a682571e08ada45
Karolina <karolina.urbanska@tdcm.io>
2020-10-30 17:02:52 +0100

    Deployed search extractor blush_no (#1022)

--- src/library/product/search/stores/b/blush/NO/execute.js
+++ src/library/product/search/stores/b/blush/NO/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'NO',
     store: 'blush',
     domain: 'blush.no',
-    url: 'https://www.blush.no/search?q={searchTerms}',
+    url: 'https://www.blush.no/search?q={searchTerms}&hits=150',
     loadedSelector: 'form.product-list',
     noResultsXPath: '//div[@class="responsive-content-wrapper"]//div[@class="alert-content"]',
     zipcode: '',
--- src/library/product/search/stores/b/blush/NO/extract.js
+++ src/library/product/search/stores/b/blush/NO/extract.js
@@ -1,11 +1,28 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'NO',
     store: 'blush',
-    transform: null,
+    transform,
     domain: 'blush.no',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    await context.evaluate(async function () {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+      const lastProductPosition = localStorage.getItem('prodCount') ? Number(localStorage.getItem('prodCount')) : 1;
+      const products = document.querySelectorAll('div.product-list-item');
+      for (let i = 0; i < products.length; i++) {
+        const price = products[i].querySelector('span.product-price-now') ? products[i].querySelector('span.product-price-now').innerText.replace(/\s/g, '') : '';
+        const currency = products[i].querySelector('span.product-price-now-label') ? products[i].querySelector('span.product-price-now-label').innerText : '';
+        products[i].setAttribute('priceId', `${currency} ${price}`);
+        products[i].setAttribute('rank', `${lastProductPosition + i}`);
+      }
+      localStorage.setItem('prodCount', `${lastProductPosition + products.length}`);
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
