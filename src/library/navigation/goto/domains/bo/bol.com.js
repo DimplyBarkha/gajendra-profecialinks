1cfe61391d2cdec5ca72ac159d154389b8c9b47e
Wayne E <bartekstafiej2@gmail.com>
2020-10-30 20:27:06 +0100

    creates new source

---
+++ src/library/navigation/goto/domains/bo/bol.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'bol.com',
+    timeout: null,
+    country: 'NL',
+    store: 'bol',
+    zipcode: '',
+  },
+};
