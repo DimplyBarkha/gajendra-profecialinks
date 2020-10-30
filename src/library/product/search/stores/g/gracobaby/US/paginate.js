7eef908dabd74629419b26da3057c06c3a01c7f9
sai pavan <saipavan.guntraddi@gmail.com>
2020-10-30 22:48:18 +0530

    paginate.js

--- src/library/product/search/stores/g/gracobaby/US/paginate.js
+++ src/library/product/search/stores/g/gracobaby/US/paginate.js
@@ -1,13 +1,12 @@
-
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'US',
     store: 'gracobaby',
-    nextLinkSelector: null,
-    mutationSelector: null,
+    nextLinkSelector: 'button[class="btn btn-tertiary col-12 col-sm-4"]',
+    mutationSelector: 'div[class="row grid-header m-0 mt-3"]>div>p>span>span',
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'div[class="tab-content col-12 p-0"]',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'gracobaby.com',
