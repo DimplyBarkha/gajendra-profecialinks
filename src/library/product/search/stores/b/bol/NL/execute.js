4efc16aee0510720e46031dcfeeec32e695f330b
Wayne E <bartekstafiej2@gmail.com>
2020-10-30 20:30:24 +0100

    created new source

---
+++ src/library/product/search/stores/b/bol/NL/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'NL',
+    store: 'bol',
+    domain: 'bol.com',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
