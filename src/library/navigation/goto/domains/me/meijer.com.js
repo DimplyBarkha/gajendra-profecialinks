ec8284c36f37dda418ff50d7e10f378009277ace
venkatsn <venkat.sn-odc@partner.import.io>
2020-10-30 19:24:23 +0530

    meijer by deepak

--- src/library/navigation/goto/domains/me/meijer.com.js
+++ src/library/navigation/goto/domains/me/meijer.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'meijer.com',
-    timeout: 50000,
+    timeout: 100000,
     country: 'US',
     store: 'meijer',
     zipcode: '',
--- src/library/product/search/stores/m/meijer/US/paginate.js
+++ src/library/product/search/stores/m/meijer/US/paginate.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'meijer',
-    nextLinkSelector: null,
+    nextLinkSelector: 'body > main > div.main-content > div:nth-child(7) > div.product-grid.js-product-grid > div.plp-pagination.js-plp-pagination > div > a.arrows.js-right-last-arrow',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: null,
