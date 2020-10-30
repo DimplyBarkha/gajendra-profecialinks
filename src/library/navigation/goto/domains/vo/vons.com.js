3aade9c229ea79916010782d1eb4d6696caa0ee0
Karthickravi <karthik.ravi-odc@partner.import.io>
2020-10-30 21:47:58 +0530

    karthickravi

---
+++ src/library/navigation/goto/domains/vo/vons.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'vons.com',
+    timeout: 50000,
+    country: 'US',
+    store: 'vons',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/p/promofarma/ES/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'promofarma.com',
+    prefix: null,
+    url: "https://www.promofarma.com/pedisilk-lima-electrica-antidurezas",
+    country: 'ES',
+    store: 'promofarma',
+    zipcode: '',
+  },
+};
