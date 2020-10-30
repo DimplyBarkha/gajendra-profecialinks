edc9e09cb10d9cc658494585381b58282d24f7bf
Vaijat Kokate <vaijatvk@gmail.com>
2020-10-30 16:48:54 +0530

    Issue fixes

--- src/library/product/details/stores/e/extra/BR/extract.js
+++ src/library/product/details/stores/e/extra/BR/extract.js
@@ -16,11 +16,23 @@ async function implementation(
       newDiv.style.display = 'none';
       document.body.appendChild(newDiv);
     }
+
     const url = window.location.href;
       addHiddenDiv(`document_url`, url);
     //@ts-ignore
     var productId = window.siteMetadata.page.product.idProduct;
     addHiddenDiv(`document_product_id`, productId);
+
+    
+    function addHiddenDivWithClass (class1, content1) {
+      const newDiv1 = document.createElement('div');
+      newDiv1.className = class1;
+      newDiv1.textContent = content1;
+      newDiv1.style.display = 'none';
+      document.body.appendChild(newDiv1);
+    }
+    var imgs = document.querySelectorAll("#flix-lg-inpage img")
+    imgs.forEach(x=> addHiddenDivWithClass('aplusImages',x.getAttribute("srcset")));
   });
   
   return await context.extract(productDetails, { transform });
