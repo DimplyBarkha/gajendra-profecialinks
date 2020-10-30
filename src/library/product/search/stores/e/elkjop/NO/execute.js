a63d2558c95aa538fed02162e23ce6bff1eab1f4
irfan shaikh <irfan.shaikh@synerzip.com>
2020-10-30 10:42:17 +0530

    #987. Created search extractor for elkjop NO.

--- src/library/product/search/stores/e/elkjop/NO/execute.js
+++ src/library/product/search/stores/e/elkjop/NO/execute.js
@@ -1,13 +1,47 @@
-
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  const productsPresent = await context.evaluate(() => Boolean(document.querySelector('div[id*="searchProductsInfo"]  div[class*="col-mini-product"] > div , div[class="product-detail-page"]')));
+  if (productsPresent) {
+    return;
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+}
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
     country: 'NO',
     store: 'elkjop',
     domain: 'elkjop.no',
-    url: "https://www.elkjop.no/search?SearchTerm='{searchTerms}'&search=&searchResultTab=",
-    loadedSelector: '.sidebar',
-    noResultsXPath: '//*[contains(@class, "no-search-result")]',
+    url: 'https://www.elkjop.no/search?SearchTerm={searchTerms}',
+    loadedSelector: 'div[id*="searchProductsInfo"]  div[class*="col-mini-product"] > div   , div[class="product-detail-page"]',
+    noResultsXPath: '//*[contains(@class, "no-search-result")] | //span[contains(@class,"box-title") and  contains(., "Beklager, vi fant ingen produkttreff for søkeordet")] | //section[contains(@class,"article-page main-content")]',
     zipcode: '',
+    implementation,
   },
 };
--- src/library/product/search/stores/e/elkjop/NO/extract.js
+++ src/library/product/search/stores/e/elkjop/NO/extract.js
@@ -15,43 +15,43 @@ module.exports = {
   ) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
+    try {
+      await context.waitForSelector('div[id*="coiPage-1"] button[class*="coi-banner__accept"]');
+    } catch (error) {
+      console.log('cookie button not present');
+    }
+    try {
+      await context.click('div[id*="coiPage-1"] button[class*="coi-banner__accept"]');
+    } catch (error) {
+      console.log('cookie button click failed!');
+    }
     await context.evaluate(async function () {
-      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
-      const accCookie = document.querySelector('button.coi-banner__accept');
-      if (accCookie) {
-        // @ts-ignore
-        accCookie.click();
-      }
-      const resCount = document.querySelector('.count');
-      // @ts-ignore
-      if (resCount && resCount.innerText) {
-        const resVisible = 12;
-        // @ts-ignore
-        var iter = Math.ceil(resCount.innerText / resVisible);
-        var i;
-        for (i = 0; i < iter; i++) {
-          if (document.querySelector('#searchProductsInfo')) {
-            window.scrollTo(0, document.querySelector('#searchProductsInfo').scrollHeight);
-            await new Promise((resolve, reject) => setTimeout(resolve, 2000));
-          }
+      let scrollSelector = document.querySelector('footer[class*="master-foot"]');
+      let scrollLimit = scrollSelector ? scrollSelector.offsetTop : '';
+      let yPos = 0;
+      while (scrollLimit && yPos < scrollLimit) {
+        yPos = yPos + 350;
+        window.scrollTo(0, yPos);
+        scrollSelector = document.querySelector('footer[class*="master-foot"]');
+        scrollLimit = scrollSelector ? scrollSelector.offsetTop : '';
+        await new Promise(resolve => setTimeout(resolve, 3500));
+        if (document.querySelectorAll('div[id*="searchProductsInfo"] div[class*="col-mini-product"] > div').length > 150) {
+          break;
         }
       }
     });
-    await context.evaluate(async function () {
-      function addElementToDocument (doc, key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        doc.appendChild(catElement);
+    async function addUrl () {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
       }
-      const lastProductPosition = localStorage.getItem('prodCount') ? Number(localStorage.getItem('prodCount')) : 1;
-      const arr = document.querySelectorAll('div.mini-product');
-      for (let i = 0; i < arr.length; i++) {
-        addElementToDocument(arr[i], 'pd_rank', lastProductPosition + i);
-      }
-      localStorage.setItem('prodCount', `${lastProductPosition + arr.length}`);
-    });
+      const url = window.location.href;
+      addHiddenDiv('added-searchurl', url);
+    }
+    await context.evaluate(addUrl);
     return await context.extract(productDetails, { transform });
   },
 };
