2a1cecc880eab8f67b7c973a619122a88c9cbbc1
Julia Lojek <julialojek@gmail.com>
2020-10-30 15:50:05 +0100

    added more fields to core extractor

--- src/library/product/details/stores/a/alcampo/ES/extract.js
+++ src/library/product/details/stores/a/alcampo/ES/extract.js
@@ -14,6 +14,23 @@ module.exports = {
     context,
     { productDetails },
   ) => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await context.evaluate(async () => {
+      const body = document.querySelector('body');
+      const shippingOptions = document.querySelectorAll('div[class*="imageServiceProduct"] + div');
+      let shippingInfo = '';
+      shippingOptions.forEach(option => {
+        shippingInfo += option.innerHTML;
+      });
+      body.setAttribute('shippinginfo', shippingInfo);
+
+      const isNutritionalInfoPresent = document.querySelector('div.pictogramasFood');
+      if (isNutritionalInfoPresent) {
+        body.setAttribute('servingsize', 'por 100ml/g');
+        body.setAttribute('servingunit', 'ml/g');
+      }
+    });
+
     await context.extract(productDetails);
   },
 };
