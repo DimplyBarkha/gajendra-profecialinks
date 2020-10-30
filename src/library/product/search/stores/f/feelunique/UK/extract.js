d65a597006561d30569ebe8d5b0627d76436e18a
madhanimportio <madhan.selvaraj-odc@partner.import.io>
2020-10-30 17:07:32 +0530

    changes in details extractor yaml file

--- src/library/product/search/stores/f/feelunique/UK/extract.js
+++ src/library/product/search/stores/f/feelunique/UK/extract.js
@@ -1,4 +1,23 @@
 const {cleanUp} = require('../../../../shared');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async () => {
+    while(!!document.querySelector('a.loadMoreButton')){
+      // @ts-ignore
+      document.querySelector('a.loadMoreButton').click()
+      await new Promise(r => setTimeout(r, 1000));
+    }
+  })
+  await context.extract(productDetails);
+}
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
