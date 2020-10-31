db55012d08d741c96fd890a7ad9c5f0a4fbb9a2f
SS <toaranjan@gmail.com>
2020-10-31 18:21:28 +0530

    #875. ralphs search

--- src/library/product/search/stores/r/ralphs_92201/US/paginate.js
+++ src/library/product/search/stores/r/ralphs_92201/US/paginate.js
@@ -5,8 +5,10 @@ module.exports = {
     country: 'US',
     store: 'ralphs_92201',
     loadedSelector: '.PaginateItems',
-    nextLinkSelector: 'button.kds-Button.kds-Button--primaryInverse.kds-Button--compact.kds-Button--hasIconOnly.kds-Pagination-next',
-    openSearchDefinition: null,
+    nextLinkSelector: 'button.kds-Button.kds-Button--primaryInverse.kds-Button--compact.kds-Button--hasIconOnly.kds-Pagination-next:not(:disabled)',
+    // openSearchDefinition: {
+    //   template: 'https://www.ralphs.com/search?query={searchTerms}&searchType=default_search&fulfillment=all&page={page}',
+    // },
     domain: 'ralphs.com',
     zipcode: '',
   },
