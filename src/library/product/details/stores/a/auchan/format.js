8933fb2b7499bb1fcb59dd94d67a3a4edda4d946
Nilesh <nilesh@madasolns.com>
2020-10-31 22:06:25 +0530

    added xpath to retrieve data for food product

--- src/library/product/details/stores/a/auchan/format.js
+++ src/library/product/details/stores/a/auchan/format.js
@@ -4,6 +4,24 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
   for (const { group } of data) {
     for (const row of group) {
       if (row.specifications) {
@@ -70,8 +88,33 @@ const transform = (data) => {
           row.technicalInformationPdfPresent = [{ text: 'No', xpath: '' }];
         }
       }
+      if (row.customVariantId && row.customVariantId.length) {
+        row.varientId = [{
+          text: row.customVariantId[0].text,
+          xpath: row.customVariantId[0].xpath
+        }]
+      }
+      if (row.servingSizeUom && row.servingSizeUom.length) {
+        row.servingSizeUom = [{
+          text: row.servingSizeUom[0].text.replace(/\d+/, ''),
+          xpath: ''
+        }]
+      }
+
+      if (row.imageZoomFeaturePresent && row.imageZoomFeaturePresent.length) {
+        row.imageZoomFeaturePresent = [{
+          text: 'Yes',
+          xpath: ''
+        }]
+      } else {
+        row.imageZoomFeaturePresent = [{
+          text: 'No',
+          xpath: ''
+        }]
+      }
     }
   }
+  data = cleanUp(data, undefined);
   return data;
 };
 
