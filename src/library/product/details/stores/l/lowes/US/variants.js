3432d49b83306e02db70d4c3ff1a196d7591bfcc
Prashant Wakchaure <prashant.wakchaure@synerzip.com>
2020-11-01 21:38:03 +0530

    Lowes US retailer changes

---
+++ src/library/product/details/stores/l/lowes/US/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'US',
+    store: 'lowes',
+    domain: 'lowes.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/l/lowes/US/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'US',
+    store: 'lowes',
+    transform: null,
+    domain: 'lowes.com',
+    zipcode: '',
+  },
+};
