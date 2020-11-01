04b437e7a3c1b74ae51e395ae4758cdbb323c4ab
Bindu Datla <bindu.datla@import.io>
2020-11-01 11:06:27 -0500

    added schema element availbility element

--- src/library/product/search/stores/k/kroger/US/execute.js
+++ src/library/product/search/stores/k/kroger/US/execute.js
@@ -12,7 +12,7 @@ async function implementation (
     console.log(`loaded selector in parameters is - ${parameters.loadedSelector}`);
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 20000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    }, { timeout: 40000 }, parameters.loadedSelector, parameters.noResultsXPath);
   }
   console.log('Checking no results', parameters.noResultsXPath);
   return await context.evaluate(function (xp) {
