8ee41be7ef78f0f49b2f81d8fc00db0d68931866
SS <toaranjan@gmail.com>
2020-11-02 01:11:58 +0530

    #925. att changes

--- src/library/product/details/stores/a/att/US/extract.js
+++ src/library/product/details/stores/a/att/US/extract.js
@@ -1,9 +1,10 @@
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: "product/details/extract",
   parameterValues: {
     country: "US",
     store: "att",
-    transform: null,
+    transform: transform,
     domain: "att.com",
     zipcode: "''",
   },
@@ -12,39 +13,40 @@ module.exports = {
     context,
     dependencies,) => {
     await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-    // await context.evaluate(async function () {
-    //   const newDiv = document.createElement('div');
-    //   newDiv.id = 'test';
-    //   newDiv.textContent = 'test';
-    //   newDiv.style.display = 'none';
-    //   document.body.appendChild(newDiv);
+    await context.evaluate(async function () {
+      function addHiddenDiv(id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+        return newDiv;
+      }
+      try {
+        const videoSelector = '//script[@type="application/ld+json"][contains(text(),"VideoObject")]';
+        const videoApis = document.evaluate(
+          videoSelector,
+          document,
+          null,
+          XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+          null
+        );
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+        for (let i = 0; i < videoApis.snapshotLength; i++) {
+          let text = videoApis.snapshotItem(i).textContent;
+          let descTillEnd = text.substring(text.indexOf("description") - 1);
+          let desc = descTillEnd.substring(0, descTillEnd.indexOf('",') + 2);
+          text = text.replace(desc, '');
+          addHiddenDiv('test-video', text);
+          const videoObj = JSON.parse(text);
 
-    //   const videoSelector = '//script[@type="application/ld+json"][contains(text(),"VideoObject")]';
-    //   const videoApis = document.evaluate(
-    //     videoSelector,
-    //     document,
-    //     null,
-    //     XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-    //     null
-    //   );
-    //   await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-    //   for (let i=0 ; i < videoApis.snapshotLength; i++ ) {
-    //     let text = videoApis.snapshotItem(i).textContent;
-    //     text = text.match(/[,]{1}[\s]*?\"contentUrl\"[ ]*?[:][ ]*?\".*\"[^,].*/)[0];
-    //     //const videoObj = JSON.parse(text);
-    //     //if (videoObj && videoObj.contentUrl) {
-    //     if (text) {  
-    //       // const url = videoObj.contentUrl;
-    //       const arr = text.split(':');
-    //       const newDiv = document.createElement('div');
-    //       newDiv.id = `${i}-myvideo`;
-    //       newDiv.textContent = arr[1];
-    //       newDiv.style.display = 'none';
-    //       document.body.appendChild(newDiv);
-    //     }
-    //  }
-    // });
-    
+          if (videoObj && videoObj.contentUrl) {
+            addHiddenDiv(`${i}-my-video`, videoObj.contentUrl);
+          }
+        }
+      } catch (err) { }
+    });
+
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.extract(productDetails, { transform });
