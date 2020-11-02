4f46078e830669fecaeb28e703405e6593ed7a85
astropaula <wojciechowska.fran@gmail.com>
2020-11-02 19:32:35 +0100

    Updated extract.js file

--- src/library/product/search/stores/b/bhphotovideo/US/extract.js
+++ src/library/product/search/stores/b/bhphotovideo/US/extract.js
@@ -8,6 +8,37 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+
+
+
+  // const prod = await context.evaluate(async () => {
+    
+  //   return JSON.parse((document.querySelector('div[class="aperture-body"]>script').innerText).replace(/^window.dlo = +/, ""));
+  // });
+  // console.log("coto?", typeof(prod));
+  // console.log("coto??????????", prod);
+  await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+  await context.evaluate(async function () {
+    const GetTagByIdUsingRegex = (tag, html) => {
+      // return new RegExp('window.__PRELOADED_DATA .*[\\s\\S]?').exec(html);
+      return new RegExp('<' + tag + '>window.dlo.*[\\s\\S]?(\n\t{3}var.*\n\t{3}BH.*\n\t{3}B.*\n\t{2})<\/' + tag + '>').exec(html)
+    };
+
+    await fetch(window.location.href, {
+      method: 'GET',
+    }).then(r => r.text()).then(htm => {
+      const result = GetTagByIdUsingRegex('script', htm);
+      const outerHTML = result && result[0] ? result[0] : '';
+      const dataStr = outerHTML && outerHTML.split('window.__PRELOADED_DATA = ') && outerHTML.split('window.__PRELOADED_DATA = ')[1] && outerHTML.split('window.__PRELOADED_DATA = ')[1].split('window.__SERVER_RENDER_TIME')[0] ? outerHTML.split('window.__PRELOADED_DATA = ')[1].split('window.__SERVER_RENDER_TIME')[0] : '';
+
+      const manufactStr = dataStr && dataStr.split('fct_brand_name') ? '{' + dataStr.split('fct_brand_name')[1].split('[')[1].split(']')[0] + '}' : '';
+
+
+      const obj = manufactStr.match(/(?<="name":").*?(?=",)/gs);
+    });
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+  });
+
   const windowScroll = await context.evaluate(async () => {
     for(let i=0;i<=document.body.scrollHeight;i=i+500){
       window.scroll(0,i);
