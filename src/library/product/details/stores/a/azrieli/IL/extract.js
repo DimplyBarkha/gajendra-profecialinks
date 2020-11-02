9b14dbe0cc0694013f2c46e27ba45cdba417a379
VikramGoud1576 <vikramkumargo.alval@hcl.com>
2020-11-02 12:26:14 +0530

    DS-azrieli transformparam is updated

--- src/library/product/details/stores/a/azrieli/IL/extract.js
+++ src/library/product/details/stores/a/azrieli/IL/extract.js
@@ -41,52 +41,50 @@ module.exports = {
       addElementToDocument('added_alternateImages', alternateImages);
 
       const variantId = getXpath("//meta[@property='og:url']", 'content');
-      const variantIdValue = variantId.split("/")[4]
+      const variantIdValue = variantId.split('/')[4];
       addElementToDocument('added_variantId', variantIdValue);
 
       const sku = getXpath("//meta[@property='og:url']", 'content');
-      const skuValue = sku.split("/")[4]
+      const skuValue = sku.split('/')[4];
       addElementToDocument('added_sku', skuValue);
-      
+
       let listPrice = getXpath("//div[@id='DealValueWrap']", 'innerText');
       console.log('listPrice ', listPrice);
       let listPriceValue;
-      if(listPrice != null){
-        listPrice =  listPrice.replace(/\r\n|\r|\n/g, ' ');
-        listPriceValue = listPrice.split(" ")[1]+listPrice.split(" ")[0]
+      if (listPrice != null) {
+        listPrice = listPrice.replace(/\r\n|\r|\n/g, ' ');
+        listPriceValue = listPrice.split('')[1] + listPrice.split('')[0];
       }
-      
+
       addElementToDocument('added_listPrice', listPriceValue);
 
       const price = getXpath("//div[@class='deal-price']//div[@style='float:right;']", 'innerText').replace(/\r\n|\r|\n/g, ' ');
-      const priceValue = price.split(" ")[1]+price.split(" ")[0]
+      const priceValue = price.split('')[1] + price.split('')[0];
       addElementToDocument('added_price', priceValue);
 
-     const specifications = getAllXpath("//div[@class='finePrint']/descendant::strong[text()='מפרט טכני']/following::div/ul//li//text() | //div[@class='finePrint']/descendant::strong[text()='מפרט טכני']/following::ul[1]//li//text() | //div[@class='finePrint']/descendant::strong[text()='מפרט טכני:']/following::text()[position()<9] | //div[@class='finePrint']/descendant::strong[text()='מפרט טכני:']/following::div[position()<9]", 'nodeValue').join('|');
+      const specifications = getAllXpath("//div[@class='finePrint']/descendant::strong[text()='מפרט טכני']/following::div/ul//li//text() | //div[@class='finePrint']/descendant::strong[text()='מפרט טכני']/following::ul[1]//li//text() | //div[@class='finePrint']/descendant::strong[text()='מפרט טכני:']/following::text()[position()<9] | //div[@class='finePrint']/descendant::strong[text()='מפרט טכני:']/following::div[position()<9]", 'nodeValue').join('|');
       addElementToDocument('added_specifications', specifications);
 
       let netWeight = getXpath("//div[@class='finePrint']//p/text()[preceding-sibling::br and contains(.,'משקל') and not(contains(.,'קל-משקל'))] | //div[@class='finePrint']//p//following-sibling::ul[@dir='rtl']//li/text()[contains(.,'משקל')] | //div[@class='finePrint']//div//ul//li/text()[contains(.,'משקל')] | //div[@class='finePrint']//div/text()[contains(.,'משקל כולל:')]", 'nodeValue');
-        if (netWeight != null && netWeight.includes(':')) {
-          netWeight = netWeight.split(':')[1];
-          }
-         else if (netWeight != null && netWeight.includes('-')) {
-            netWeight = netWeight.split('-')[1];
-          }
-          else if (netWeight != null && netWeight.includes('משקל')){
-            netWeight = netWeight.replace('משקל','');
-          }
-        addElementToDocument('added_netWeight', netWeight);
-  
-        let technicalDoc = getXpath("//div[@class='PsoletMessage']//a[contains(@href,'https')]/@href", 'nodeValue');
-        console.log('technicalDoc   ' + technicalDoc);
-         if (technicalDoc!=null && technicalDoc.includes('https')) {
-           technicalDoc = 'Yes';
-         } else {
-           technicalDoc = 'No';
-         }
-         addElementToDocument('added_technicalDoc', technicalDoc);
+      if (netWeight != null && netWeight.includes(':')) {
+        netWeight = netWeight.split(':')[1];
+      } else if (netWeight != null && netWeight.includes('-')) {
+        netWeight = netWeight.split('-')[1];
+      } else if (netWeight != null && netWeight.includes('משקל')) {
+        netWeight = netWeight.replace('משקל', '');
+      }
+      addElementToDocument('added_netWeight', netWeight);
 
-         addElementToDocument('added_variantCount', 0);
+      let technicalDoc = getXpath("//div[@class='PsoletMessage']//a[contains(@href,'https')]/@href", 'nodeValue');
+      console.log('technicalDoc   ' + technicalDoc);
+      if (technicalDoc != null && technicalDoc.includes('https')) {
+        technicalDoc = 'Yes';
+      } else {
+        technicalDoc = 'No';
+      }
+      addElementToDocument('added_technicalDoc', technicalDoc);
+
+      addElementToDocument('added_variantCount', 0);
     });
     await context.extract(productDetails, { transform: transformParam });
   },
