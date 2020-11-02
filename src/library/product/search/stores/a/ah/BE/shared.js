65a031f4e186aabfc31d668e3bd0e0acf5aeb8e7
Ihor <ihor.vasylechko-makeitease@partner.import.io>
2020-11-02 17:22:35 +0200

    pagination

--- src/library/product/search/stores/a/ah/BE/shared.js
+++ src/library/product/search/stores/a/ah/BE/shared.js
@@ -9,7 +9,7 @@ const transform = (data) => {
     const group = [];
     el.group.forEach(element => {
     // eslint-disable-next-line no-unused-expressions
-      element.url[0].text = 'https://www.ah.nl' + element.url[0].text;
+      element.url[0].text = 'https://www.ah.be' + element.url[0].text;
       element.price = [{ text: element.price.map(p => p.text).join('') + ' €' }];
       element.discount = [{ text: element.discount.map(d => d.text).join('') + ' €' }];
       group.push(element);
