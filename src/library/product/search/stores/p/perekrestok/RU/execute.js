edfa5ec0be1b799d906f3fdee419df80883455c2
Rahulolive <r.rahul@olivedatacentre.com>
2020-11-02 15:01:03 +0530

    rahul-branch

---
+++ src/library/product/search/stores/p/perekrestok/RU/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'RU',
+    store: 'perekrestok',
+    domain: 'vprok.ru',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/p/perekrestok/RU/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'RU',
+    store: 'perekrestok',
+    domain: 'vprok.ru',
+    zipcode: '',
+  },
+};
