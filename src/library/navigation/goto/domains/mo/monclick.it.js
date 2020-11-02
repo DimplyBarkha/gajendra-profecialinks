53ac5ed254061d51fb9c6cef4736bc9d9a71579e
prateek.s <psc3018@gmail.com>
2020-11-02 22:57:23 +0530

    Fixed the blocking issue

--- src/library/navigation/goto/domains/mo/monclick.it.js
+++ src/library/navigation/goto/domains/mo/monclick.it.js
@@ -8,4 +8,13 @@ module.exports = {
     store: 'monclick',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
+    try{
+      await context.waitForSelector('section.mk-product-page')
+    }catch(err){
+      console.log('product page did not load')
+    }
+  },
 };
