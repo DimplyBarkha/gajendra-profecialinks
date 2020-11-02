065289903e385d787e6ba3938a4f72cf566257b4
Mukund Murari <mukund.murari@hcl.com>
2020-11-02 15:29:17 +0530

    olsales changes for navigation

---
+++ src/library/navigation/goto/domains/ol/olsale.co.il.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'olsale.co.il',
+    timeout: null,
+    country: 'IL',
+    store: 'olsale',
+    zipcode: '',
+  },
+};
---
+++ src/library/navigation/goto/domains/ol/setZipCode.olsale.co.il.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'IL',
+    domain: 'olsale.co.il',
+    store: 'olsale',
+    zipcode: '',
+  },
+};
