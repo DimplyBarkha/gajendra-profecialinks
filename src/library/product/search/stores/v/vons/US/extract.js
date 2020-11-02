d1a25e9be41202eaae302b105bd265629fef339b
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-02 20:27:01 +0530

    devvonsdetails

--- src/library/product/search/stores/v/vons/US/extract.js
+++ src/library/product/search/stores/v/vons/US/extract.js
@@ -1,10 +1,4 @@
-await fetch("https://www.vons.com/abs/pub/xapi/search/products?request-id=3075590757930&url=https://www.vons.com&pageurl=https://www.vons.com&pagename=search&rows=30&start=30&search-type=keyword&storeid=2053&featured=true&search-uid=&q=%2212%20pack%20beer%22&sort=&userid=&featuredsessionid=80db3232-de68-4c6a-bdb2-e1e1d1d5c468&screenwidth=1325", {
-    "credentials": "include",
-    "headers": {
-        "Ocp-Apim-Subscription-Key": "e914eec9448c4d5eb672debf5011cf8f",
-    },
-    "method": "GET"
-}).then(r => r.json());
+const { transform } = require('../../../../shared');
 module.exports = {
 implements: 'product/search/extract',
 parameterValues: {
@@ -13,5 +7,15 @@ store: 'vons',
 transform: cleanUp,
 domain: 'vons.com',
 zipcode: '',
+},
+implementation: async () => {
+    await fetch("https://www.vons.com/abs/pub/xapi/search/products?request-id=3075590757930&url=https://www.vons.com&pageurl=https://www.vons.com&pagename=search&rows=30&start=30&search-type=keyword&storeid=2053&featured=true&search-uid=&q=%2212%20pack%20beer%22&sort=&userid=&featuredsessionid=80db3232-de68-4c6a-bdb2-e1e1d1d5c468&screenwidth=1325",
+    {
+    "credentials": "include",
+    "headers": {
+        "Ocp-Apim-Subscription-Key": "e914eec9448c4d5eb672debf5011cf8f",
+    },
+    "method": "GET"
+}).then(r => r.json());
 },
 };
