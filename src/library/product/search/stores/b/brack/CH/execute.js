fc87674f6fb112492e4c2b09a4913a20cf0a2a19
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-02 10:45:12 +0100

    created a template for the search extractor

---
+++ src/library/product/search/stores/b/brack/CH/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'CH',
+    store: 'brack',
+    domain: 'brack.ch',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/b/brack/CH/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'CH',
+    store: 'brack',
+    transform: null,
+    domain: 'brack.ch',
+    zipcode: '',
+  },
+};
