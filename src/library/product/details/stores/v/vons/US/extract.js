524c32ebb5ecc99129a41617b24e3af9a78f91dd
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-02 13:03:06 +0530

    devvonsdetails

--- src/library/product/details/stores/v/vons/US/extract.js
+++ src/library/product/details/stores/v/vons/US/extract.js
@@ -8,4 +8,18 @@ module.exports = {
     domain: 'vons.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+    function addElementToDocument (key, value) {
+    const catElement = document.createElement('div');
+    catElement.id = key;
+    catElement.textContent = value;
+    catElement.style.display = 'none';
+    document.body.appendChild(catElement);
+    }
+    var url=window.location.href
+    addElementToDocument('url', url);
+    });
+    await context.extract(productDetails);
+    },
 };
