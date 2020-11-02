f7ebb4820c5f9a4db15f83a8bc0d20a50a3c9104
unknown <suryakanta.giet119@gmail.com>
2020-11-03 00:38:19 +0530

    appie.nl core

---
+++ src/library/navigation/goto/domains/ap/appie.nl.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'appie.nl',
+    timeout: 100000,
+    country: 'NL',
+    store: 'appie',
+    zipcode: '',
+  },
+};
