791764b2253c697dfa403f10cfb588711f5f94e1
Bindu Datla <bindu.datla@import.io>
2020-11-02 08:50:50 -0500

    fixed the updates

--- src/library/product/details/stores/i/intermarche/FR/execute.js
+++ src/library/product/details/stores/i/intermarche/FR/execute.js
@@ -17,7 +17,7 @@ async function implementation(
     if (parameters.loadedSelector) {
       await context.waitForFunction(function (sel, xp) {
         return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-      }, { timeout: 20000 }, parameters.loadedSelector, parameters.noResultsXPath);
+      }, { timeout: 60000 }, parameters.loadedSelector, parameters.noResultsXPath);
     }
   }
   catch (exception) {
