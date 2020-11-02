80e8580889a66f5b286bd3468efe1bd11dedba53
unknown <leena.gavali-odc@partner.import.io>
2020-11-02 18:25:51 +0530

    menards_QA_Search

--- src/library/navigation/goto/domains/me/menards.com.js
+++ src/library/navigation/goto/domains/me/menards.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'menards.com',
-    timeout: 40000,
+    timeout: 80000,
     country: 'US',
     store: 'menards',
     zipcode: '',
--- src/library/product/search/paginate/pager/action.js
+++ src/library/product/search/paginate/pager/action.js
@@ -30,7 +30,7 @@ async function implementation (
     await context.waitForFunction((selector) => {
       console.log(selector, document.querySelector(selector));
       return !document.querySelector(selector);
-    }, { timeout: 40000 }, spinnerSelector);
+    }, { timeout: 80000 }, spinnerSelector);
     console.log('Spinner went away', spinnerSelector);
     return true;
   }
@@ -40,16 +40,16 @@ async function implementation (
     await Promise.all([
       context.click(nextLinkSelector),
       // possible race condition if the data returned too fast, but unlikely
-      context.waitForMutuation(mutationSelector, { timeout: 40000 }),
+      context.waitForMutuation(mutationSelector, { timeout: 80000 }),
     ]);
     return true;
   }
 
   if (nextLinkSelector) {
     console.log('Clicking', nextLinkSelector);
-    await context.clickAndWaitForNavigation(nextLinkSelector, {}, { timeout: 40000 });
+    await context.clickAndWaitForNavigation(nextLinkSelector, {}, { timeout: 80000 });
     if (loadedSelector) {
-      await context.waitForSelector(loadedSelector, { timeout: 40000 });
+      await context.waitForSelector(loadedSelector, { timeout: 80000 });
     }
     return true;
   }
