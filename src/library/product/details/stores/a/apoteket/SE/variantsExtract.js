e25d3ba77e9540e883f13fb461af36bd37db465f
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-11-02 14:39:03 +0530

    variant extract code changed (#684)

--- src/library/product/details/stores/a/apoteket/SE/variantsExtract.js
+++ src/library/product/details/stores/a/apoteket/SE/variantsExtract.js
@@ -23,28 +23,31 @@ async function implementation (
   } catch (error) {
     console.log('error: ', error);
   }
-  async function firstItemLink () {
-    return await context.evaluate(function () {
-      let firstItem = document.querySelector('div.grid-product__content.grid-item__content a')
-      // @ts-ignore
-      firstItem = firstItem ? firstItem.href : '';
-      let finalLink
-      // @ts-ignore
-      if(firstItem.includes('http') & firstItem !== ''){
-        finalLink = firstItem
-      // @ts-ignore
-      }else if(firstItem !== ''){
-        finalLink = 'https://www.apoteket.se'+firstItem;
-      }
-      return finalLink;
-    });
+  let list = await context.evaluate(() => !document.querySelector('div[class="product-grid__items"]'))
+  if (!list) {
+    async function firstItemLink() {
+      return await context.evaluate(function () {
+        let firstItem = document.querySelector('div.grid-product__content.grid-item__content a')
+        // @ts-ignore
+        firstItem = firstItem ? firstItem.href : '';
+        let finalLink
+        // @ts-ignore
+        if (firstItem.includes('http') & firstItem !== '') {
+          finalLink = firstItem
+          // @ts-ignore
+        } else if (firstItem !== '') {
+          finalLink = 'https://www.apoteket.se' + firstItem;
+        }
+        return finalLink;
+      });
+    }
+    const url = await firstItemLink();
+    if (url !== null) {
+      await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+    }
+    await context.waitForNavigation();
   }
-  const url = await firstItemLink();
-  if (url !== null) {
-    await context.goto(url, { timeout: 90000, waitUntil: 'load', checkBlocked: true });
-  }
-  // await context.waitForNavigation();
-  await new Promise((resolve, reject) => setTimeout(resolve, 8000));
+  await new Promise((resolve, reject) => setTimeout(resolve, 8000))
   //-------------------------
   await context.evaluate(async (parentInput) => {
     function addElementToDocument (key, value) {
