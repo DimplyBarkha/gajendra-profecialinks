d04ba9fd5f69387a458772fcf5bcbb3389f9b9d7
Mamatha-Gundapaneni <mamatha.g@osmosys.asia>
2020-11-02 18:06:52 +0530

    Updated code

--- src/library/product/details/stores/j/jcpenney/US/extract.js
+++ src/library/product/details/stores/j/jcpenney/US/extract.js
@@ -29,7 +29,6 @@ await context.evaluate(async function () {
       console.log(`Couldn't load selector => ${sel}`);
     }
   };
-
   const finalImages = [];
   const isVariants = document.querySelector('#option-wrapper-false #product-options-false');
   const variants = '#option-wrapper-false #product-options-false > div:nth-last-child(1) ul li';
@@ -53,7 +52,7 @@ await context.evaluate(async function () {
         for (i = 0; i < __PRELOADED_STATE__.productDetails.lots[0].items.length; i++) {
           skus.push(__PRELOADED_STATE__.productDetails.lots[0].items[i].id);
         }
-        for (i = 0; i < __PRELOADED_STATE__.productDetails.lots[0].items.length; i++) {
+        for (i = 0; i < __PRELOADED_STATE__.productDetails.lots.length; i++) {
           mpc.push(__PRELOADED_STATE__.productDetails.lots[i].id);
         }
       }
@@ -65,6 +64,7 @@ await context.evaluate(async function () {
           array[i].setAttribute('skuId', skus[i]);
         }
       }
+      //  let variants = '';
       const arr = skus.filter (function (value, index, array) {
         return skus.indexOf(value) === index;
     });
@@ -78,7 +78,6 @@ await context.evaluate(async function () {
           array[i].setAttribute('mpc', mpc[i]);
         }
       }
-
       document.querySelector('h1[data-automation-id="product-title"]').setAttribute('firstvariant', skus[0]);
 
       [...document.querySelectorAll('#option-wrapper-false #product-options-false > div:nth-last-child(1) ul li')].map((e) => {
@@ -89,7 +88,14 @@ await context.evaluate(async function () {
         e.setAttribute('secondaryImages', [...document.querySelectorAll('#contentContainer > section > section:nth-child(3) > div.sm12.md6.lg6.xl7._3AtEJ > div > div.carousel-wrapper > div > div._3h_IA.lg2.xl2.md2.sm2._1sbcC.noPad > div > div.slick-list a:not(._2JSHu)')].map(e => { return `https:${e.querySelector('img').getAttribute('src')}`; }));
       });
 
-      finalImages.push([...document.querySelectorAll('#contentContainer > section > section:nth-child(3) > div.sm12.md6.lg6.xl7._3AtEJ > div > div.carousel-wrapper > div > div._3h_IA.lg2.xl2.md2.sm2._1sbcC.noPad > div > div.slick-list a')].map(e => { return e.querySelector('img').getAttribute('src'); }))});
+      finalImages.push([...document.querySelectorAll('#contentContainer > section > section:nth-child(3) > div.sm12.md6.lg6.xl7._3AtEJ > div > div.carousel-wrapper > div > div._3h_IA.lg2.xl2.md2.sm2._1sbcC.noPad > div > div.slick-list a')].map(e => { return e.querySelector('img').getAttribute('src'); }));
+    });
+
+    let color = [...document.querySelectorAll('ul[data-automation-id="product-dimensions-color"] li')].map(e => { return e.querySelector('button').getAttribute('data-for')});
+
+    for (i = 0; i < color.length; i++) {
+      document.querySelectorAll('ul[data-automation-id="product-dimensions-color"] li')[i].setAttribute('color', color[i]);
+      }
 
   } else {
     optionalWait('h1[data-automation-id="product-title"]');
@@ -108,7 +114,6 @@ await context.evaluate(async function () {
     var array = [...document.querySelectorAll('#option-wrapper-false #product-options-false > div:nth-last-child(1) ul li')];
     var jsonString = JSON.parse(document.querySelector("script[type='application/ld+json']").innerText);
     var skus = jsonString.offers.map(e => e.sku);
-    
     for (i = 0; i < array.length; i++) {
       array[i].setAttribute('skuId', skus[i]);
     }
@@ -127,7 +132,6 @@ await context.evaluate(async function () {
 
     finalImages.push([...document.querySelectorAll('#contentContainer > section > section:nth-child(3) > div.sm12.md6.lg6.xl7._3AtEJ > div > div.carousel-wrapper > div > div._3h_IA.lg2.xl2.md2.sm2._1sbcC.noPad > div > div.slick-list a')].map(e => { return e.querySelector('img').getAttribute('src'); }));
   }
-
   appendImages(variants, finalImages);
   const brand = document.evaluate('//script[@type="application/ld+json"][contains(.,"brand")]', document).iterateNext().textContent.split('"brand":{"@type":"Thing","name":"')[1].split('"},')[0];
   document.querySelector('h1[data-automation-id="product-title"]').setAttribute('brand', brand);
@@ -142,14 +146,22 @@ await context.evaluate(async function () {
     availability = 'In Stock';
     document.querySelector('h1[data-automation-id="product-title"]').setAttribute('availability', availability);
   }
-  let prop = document.evaluate('//h3[@data-automation-id="general-info-title"][contains(.,"Proposition 65 WARNING")]', document).iterateNext();
-  if(prop) {
+
+
+  let prop = document.evaluate('//h3[@data-automation-id="general-info-title"][contains(.,"Proposition 65 WARNING")]',document).iterateNext();
+
+  if(prop){
     document.querySelector('p._1d56W button').click();
-   let prop65Info =  document.querySelector('[data-automation-id="modal-content"] div div p:nth-last-child(1)').textContent.replace(/www/g ,"https://www");
+   let prop65Info =  document.querySelector('[data-automation-id="modal-content"] div div p:nth-last-child(1)').textContent.replace(/www/g,"https://www")
    document.querySelector('h1[data-automation-id="product-title"]').setAttribute('prop65', prop65Info);
    document.querySelector('[data-automation-id="modal-content"] button[data-automation-id="at-button-default-id"]').click();
   }
 
+  let color = [...document.querySelectorAll('ul[data-automation-id="product-dimensions-color"] li')].map(e => { return e.querySelector('button').getAttribute('data-for')});
+
+    for (i = 0; i < color.length; i++) {
+      document.querySelectorAll('ul[data-automation-id="product-dimensions-color"] li')[i].setAttribute('color', color[i]);
+      }
   let scrollTop = 500;
   while (true) {
     window.scroll(0, scrollTop);
@@ -159,7 +171,6 @@ await context.evaluate(async function () {
       break;
     }
   }
-
 });
 return await context.extract(productDetails, { transform });
 }
