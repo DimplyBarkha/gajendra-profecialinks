893e73cbf1915ac207c922705a6c450f29688f9f
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-02 10:52:01 +0100

    created a template for the search extractor

---
+++ src/library/product/search/stores/b/brake/UK/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'brake',
+    transform: null,
+    domain: 'brake.co.uk',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/b/brake/UK/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'UK',
+    store: 'brake',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'brake.co.uk',
+    zipcode: '',
+  },
+};
