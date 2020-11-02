c30ada38b490b88719812a990f40d1afd6ed0a22
prateek.s <psc3018@gmail.com>
2020-11-02 22:58:04 +0530

    fixed blocking issue

--- src/library/navigation/goto/domains/mo/monclick.it.js
+++ src/library/navigation/goto/domains/mo/monclick.it.js
@@ -8,4 +8,13 @@ module.exports = {
     store: 'monclick',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
+    try{
+      await context.waitForSelector('div[id*="searchresults"]')
+    }catch(err){
+      console.log('search page did not load')
+    }
+  },
 };
