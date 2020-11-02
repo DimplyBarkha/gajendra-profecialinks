470f389c4daf84847c15482624715845d31e84ff
Karthickravi <karthik.ravi-odc@partner.import.io>
2020-11-02 22:24:02 +0530

    karthickravi

--- src/library/product/search/stores/u/unieuro/IT/extract.js
+++ src/library/product/search/stores/u/unieuro/IT/extract.js
@@ -8,4 +8,39 @@ module.exports = {
     domain: 'unieuro.it',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(() => {
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.className = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      // Method to Retrieve Xpath content of a Multiple Nodes
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+      const sliceURL = (data) => {
+        var cnt = 0;
+        for (let index = 0; index < data.length; index++) {
+          if (data[0] != 0) {
+            cnt++;
+            addElementToDocument('altImages', cnt);
+          }
+        }
+      };
+      var backgroundURL = getAllXpath('//div[@class="item-container"]//div/div[@class="title"]/a/text()', 'nodeValue');
+      sliceURL(backgroundURL);
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
