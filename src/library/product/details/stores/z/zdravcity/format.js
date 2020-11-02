87ae864dc1a80f4f4dbd957bc41588ef76a3f222
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-11-02 22:06:04 +0530

    Updated code

--- src/library/product/details/stores/z/zdravcity/format.js
+++ src/library/product/details/stores/z/zdravcity/format.js
@@ -1,12 +1,11 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    const cleanUp = (data, context) => {
-      const clean = text => text.toString()
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
       .replace(/\r\n|\r|\n/g, ' ')
       .replace(/&amp;nbsp;/g, ' ')
       .replace(/&amp;#160/g, ' ')
@@ -18,32 +17,58 @@ const transform = (data) => {
       // eslint-disable-next-line no-control-regex
       .replace(/[\x00-\x1F]/g, '')
       .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-      }))));
-      return data;
-    };
-    for (const { group } of data) {
-      for (let row of group) {       
-        if (row.category) {
-            row.category.splice(0,1)            
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
+  for (const {
+      group
+    } of data) {
+    for (let row of group) {
+      if (row.category) {
+        if (row.category.length) {
+          row.category.splice(0, 1)
         }
-        if (row.image) {
-            let info = [];          
-            row.image.forEach(item => {
-              item.text = 'https://zdravcity.ru/'+item.text
-            });            
+        if (row.category.length) {
+          row.category.splice(row.category.length - 1, 1);
         }
-        if (row.alternateImages) {
-            let info = [];          
-            row.alternateImages.forEach(item => {
-              item.text = 'https://zdravcity.ru/'+item.text
-            });            
-        }                     
+      }
+      if (row.image) {
+        row.image.forEach(item => {
+          item.text = 'https://zdravcity.ru/' + item.text
+        });
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace('.', ',');
+        });
+      }
+      if (row.listPrice) {
+        row.listPrice.forEach(item => {
+          item.text = item.text.replace('.', ',');
+        });
+      }
+      if (row.alternateImages) {
+        row.alternateImages.forEach(item => {
+          item.text = 'https://zdravcity.ru/' + item.text
+        });
+      }
+      if (row.manufacturer) {
+        row.manufacturer.forEach(item => {
+          item.text = item.text.replace(/Производитель\s*:/, '').trim();
+        });
+      }
+      if (row.countryOfOrigin) {
+        row.countryOfOrigin.forEach(item => {
+          item.text = item.text.replace(/Страна\s+производства\s*:/, '').trim();
+        });
       }
     }
-    return cleanUp(data);
-  };
-  
-  module.exports = { transform };
-  
+  }
+  return cleanUp(data);
+};
+
+module.exports = {
+  transform
+};
