8f3499230e1b7bb8a7ff8fbe18d19ea4cb948cbe
sivasoarlogic <siva@soarlogic.com>
2020-11-03 13:13:57 +0530

    core

--- src/library/navigation/goto/domains/bi/bigw.com.au.js
+++ src/library/navigation/goto/domains/bi/bigw.com.au.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'bigw.com.au',
-    timeout: null,
+    timeout: 100000,
     country: 'AU',
     store: 'bigw',
     zipcode: "''",
