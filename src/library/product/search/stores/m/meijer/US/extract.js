6a63a0043a9cb86ac07039e10091dba6e034266c
venkatsn <venkat.sn-odc@partner.import.io>
2020-11-03 17:20:52 +0530

    file updated for qa2 by deepak for meijer_49682

--- src/library/product/search/stores/m/meijer/US/extract.js
+++ src/library/product/search/stores/m/meijer/US/extract.js
@@ -8,4 +8,27 @@ module.exports = {
     domain: 'meijer.com',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async function () {
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const sliceURL = (data) => {
+        var cnt = 0;
+        for (let index = 0; index < data; index++) {
+          cnt++;
+          addElementToDocument('rankOrganic', cnt);
+        }
+      };
+      var backgroundURL = document.querySelectorAll('.product-tile-container').length;
+      sliceURL(backgroundURL);
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
