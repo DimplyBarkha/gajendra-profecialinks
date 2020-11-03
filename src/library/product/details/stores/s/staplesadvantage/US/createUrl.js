87c9dc19e4b98e8823ef779865f25fe289916b74
Wayne E <bartekstafiej2@gmail.com>
2020-11-03 14:06:56 +0100

    #730 fixed extractor

---
+++ src/library/product/details/stores/s/staplesadvantage/US/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'staplesadvantage.com',
+    prefix: null,
+    url: null,
+    country: 'US',
+    store: 'staplesadvantage',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/details/stores/s/staplesadvantage/US/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'US',
+    store: 'staplesadvantage',
+    domain: 'staplesadvantage.com',
+    zipcode: '',
+  },
+};
