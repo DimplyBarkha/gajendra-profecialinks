d0ddb6d32bca7225c4172c4367ab3c4612041b11
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-03 13:00:40 +0530

    Basic Building if AU #1026

---
+++ src/library/product/details/stores/c/colesonline/AU/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'AU',
+    store: 'colesonline',
+    domain: 'shop.coles.com.au',
+    zipcode: '',
+  },
+};
