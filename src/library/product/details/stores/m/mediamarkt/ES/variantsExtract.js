695f9f7c21a28875fd7687f4507fdd661f19de29
aakash-osmosys <akash.s@osmosys.asia>
2020-11-03 12:25:47 +0530

    Mediamarkt ES variants extractor

---
+++ src/library/product/details/stores/m/mediamarkt/ES/variantsExtract.js
@@ -0,0 +1,12 @@
+const { implementation } = require('../TR/variantsExtract');
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'ES',
+    store: 'mediamarkt',
+    transform: null,
+    domain: 'mediamarkt.es',
+    zipcode: '',
+  },
+  implementation,
+};
