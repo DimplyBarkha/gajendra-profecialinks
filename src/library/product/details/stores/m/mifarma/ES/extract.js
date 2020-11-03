4b2fd3ba2287aec17d56792eb8dc623284a58b84
BorysMaciuch <borysmaciuch@gmail.com>
2020-11-03 11:16:37 +0100

    #1130 Change url, add concatFunction, run linter

--- src/library/product/details/stores/m/mifarma/ES/extract.js
+++ src/library/product/details/stores/m/mifarma/ES/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'ES',
     store: 'mifarma',
-    transform: null,
+    transform: transform,
     domain: 'mifarma.es',
     zipcode: '',
   },
