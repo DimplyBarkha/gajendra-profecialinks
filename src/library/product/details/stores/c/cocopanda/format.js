f48e080f93003c836492425be5d7c2c498c44355
judhisahoo2009 <judhisahoo@hotmail.com>
2020-11-03 12:59:53 +0530

    #1180 deployemtn done and prepraing for workbenceh process

--- src/library/product/details/stores/c/cocopanda/format.js
+++ src/library/product/details/stores/c/cocopanda/format.js
@@ -24,15 +24,31 @@ const transform = (data) => {
     };
     for (const { group } of data) {
       for (let row of group) {
-        if(row.productUrl){
-          row.productUrl.forEach(item => {
-            item.text='https://www.cocopanda.dk'+item.text;
-          });
-        }
         if(row.price){
           row.price.forEach(item => {
             item.text=item.text.slice(0, -2);
           });
+        }
+        if(row.listPrice){
+          row.listPrice.forEach(item => {
+            item.text=item.text.slice(0, -2);
+          });
+        }
+        if(row.availabilityText){
+          row.availabilityText.forEach(item=>{
+            if(item.text=='display: none;'){
+              item.text='In Stock';
+            }else{
+              item.text='Out of Stock';
+            }
+          })
+        }
+        if(row.variantId){
+          console.log('variantId');
+        }else{
+          row.sku.forEach(item=>{
+            row.variantId=[{"text":item.text}];
+          })
         }
         if(row.aggregateRating){
           row.aggregateRating.forEach(item => {
@@ -41,9 +57,24 @@ const transform = (data) => {
           });
         }
         if(row.ratingCount){
-            row.ratingCount.forEach(item => {
-                item.text=item.text.replace('(','').replace(')','');
-            });
+          row.ratingCount.forEach(item => {
+            item.text=item.text.replace('(','').replace(')','');
+          });
+        }
+        if(row.secondaryImageTotal){
+          var tot=0;
+          row.secondaryImageTotal.forEach(item=>{
+            tot++;
+          })
+          row.secondaryImageTotal=[{"text":tot}];
+        }
+        if(row.variants){
+          var info=[];
+          row.variants.forEach(item=>{
+            info.push(item.text);
+          })
+          row.variantCount=[{"text":info.length}];
+          row.variants=[{"text":info.join(" || ")}];
         }
       }
     }
