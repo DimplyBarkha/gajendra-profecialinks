579f5fd28a8dff3e4538d203f5ffd06678530fab
klaudiasko <61785823+klaudiasko@users.noreply.github.com>
2020-11-03 09:36:24 +0100

    staplescontract (#1187)
    * created a template for the search extractor
    
    * changed extract.yaml
    
    * search extractor for StaplesAdvantage
    
    * more fields added to staplesadvantage search extractor
    
    * #729 added rankOrganic code and xpath
    
    * #729 login page and popup
    
    * #729 changed url for search results, login implementation draft in execute.js
    
    * #729 conditional login implementation
    
    * #729 conditional login implementation fixed, cookies popup closing
    
    * #729 cookies popup - problem with pagination fixed
    
    * #729 search results site added as the first in the login implementation sequence
    
    * #729 comments added to execute.js, thumbnail xpath
    
    * #729 extractor finishe, deployed, tests added
    
    * #729 product image and aggregateRating fixed
    
    * #729 aggregateRating2 field fixed
    
    * #729 storeId page added to execute.js
    
    * created a template for the search extractor
    
    * #732 fileds alnd logic added
    
    * #732 tests added
    
    * #732 extractor finished, tests added
    
    * #732 aggregateRating2: , changed to .
    
    * #732 tests added
    
    Co-authored-by: Julia Lojek <julialojek@gmail.com>

---
+++ src/library/navigation/goto/domains/st/staplesadvantage.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'staplesadvantage.com',
+    timeout: null,
+    country: 'US',
+    store: 'staplesadvantage',
+    zipcode: '',
+  },
+};
