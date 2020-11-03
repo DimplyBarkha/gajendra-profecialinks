eeca578e570315642b7ce6f8789acfc1607187a2
Richa Tiwari <richa.t@osmosys.asia>
2020-11-03 10:40:53 +0545

    Updated extract.yaml and goto

--- src/library/product/details/stores/a/amazon/ES/variantsExtract.js
+++ src/library/product/details/stores/a/amazon/ES/variantsExtract.js
@@ -1,58 +1,42 @@
-// const { implementation } = require('../../../../sharedAmazon/variantExtract');
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
+module.exports.implementation = async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
 ) {
-  const { variants, Helpers: { Helpers }, AmazonHelp: { AmazonHelp } } = dependencies;
+    const { Helpers: { Helpers }, AmazonHelp: { AmazonHelp }, variants } = dependencies;
 
-  const helpers = new Helpers(context);
-  const amazonHelp = new AmazonHelp(context, helpers);
+    const helpers = new Helpers(context);
+    const amazonHelp = new AmazonHelp(context, helpers);
 
-  const thisUPC = await context.evaluate(() => {
-    const url = window.location.href;
-    let splits = url && url.split('dp/product/')[1] ? url.split('dp/product/')[1].split('/?') : [];
-    if (splits.length < 1) {
-      splits = url && url.split('dp/')[1] ? url.split('dp/')[1].split('/') : [];
-    }
-    return splits[0];
-  });
-
-  const allVariants = await amazonHelp.getVariants();
-  if (thisUPC && !allVariants.includes(thisUPC.slice(0, 10))) {
-    allVariants.push(thisUPC.slice(0, 10));
-  }
-
-  await context.evaluate((allVariants) => {
-    function addHiddenDiv (id, content) {
-      const newDiv = document.createElement('div');
-      newDiv.id = id;
-      newDiv.textContent = content;
-      newDiv.style.display = 'none';
-      document.body.appendChild(newDiv);
-      return newDiv;
-    }
-    allVariants.forEach(variant => {
-      addHiddenDiv('ii_variant', variant);
+    const thisUPC = await context.evaluate(() => {
+        const url = window.location.href;
+        console.log('URL', url);
+        let splits = url && url.split('dp/product/')[1] ? url.split('dp/product/')[1].split('/?') : [];
+        if (splits.length < 1) {
+            splits = url && url.split('dp/')[1] ? url.split('dp/')[1].split('/') : [];
+        }
+        return splits[0];
     });
-  }, allVariants);
 
-  return await context.extract(variants);
-}
-module.exports = {
-  implements: 'product/details/variants/variantsExtract',
-  parameterValues: {
-    country: 'ES',
-    store: 'amazon',
-    transform: null,
-    domain: 'amazon.es',
-    zipcode: '',
-  },
-  dependencies: {
-    variants: 'extraction:product/details/stores/a/amazon/ES/variantsExtract',
-    Helpers: 'module:helpers/helpers',
-    AmazonHelp: 'module:helpers/amazonHelp',
-  },
-  implementation,
+    const allVariants = await amazonHelp.getVariants();
+    if (thisUPC && !allVariants.includes(thisUPC.slice(0, 10))) {
+        allVariants.push(thisUPC.slice(0, 10));
+    }
+
+    await context.evaluate((allVariants) => {
+        function addHiddenDiv(id, content) {
+            const newDiv = document.createElement('div');
+            newDiv.id = id;
+            newDiv.textContent = content;
+            newDiv.style.display = 'none';
+            document.body.appendChild(newDiv);
+            return newDiv;
+        }
+        allVariants.forEach(variant => {
+            addHiddenDiv('ii_variant', variant);
+        });
+    }, allVariants);
+
+    return await context.extract(variants);
 };
