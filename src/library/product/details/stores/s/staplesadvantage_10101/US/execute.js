0b0d6c85031ec77d0722d74df7b0eace50e41858
Wayne E <bartekstafiej2@gmail.com>
2020-11-03 11:19:09 +0100

    Created fixed extractor

---
+++ src/library/product/details/stores/s/staplesadvantage_10101/US/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'US',
+    store: 'staplesadvantage_10101',
+    domain: 'staplesadvantage.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '10101',
+  },
+};
---
+++ src/library/product/details/stores/s/staplesadvantage_10101/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'staplesadvantage_10101',
+    transform: null,
+    domain: 'staplesadvantage.com',
+    zipcode: '10101',
+  },
+};
