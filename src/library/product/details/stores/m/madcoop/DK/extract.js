e62c2385a9df0ea06650f92920d6984f06a0a90d
sujoypal144 <sujoypal144@gmail.com>
2020-11-03 10:29:56 +0530

    #1104 modified the extractor

--- src/library/product/details/stores/m/madcoop/DK/extract.js
+++ src/library/product/details/stores/m/madcoop/DK/extract.js
@@ -8,24 +8,4 @@ module.exports = {
     domain: 'madcoop.dk',
     zipcode: '',
   },
-  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
-    try {
-      const a = await context.waitForSelector('.tabs-component-tab>a[aria-controls="#ingredienser"]');
-      const b = await context.waitForSelector('.tabs-component-tab>a[aria-controls="#opbevaring"]');
-      const c = await context.waitForSelector('.tabs-component-tab>a[aria-controls="#tilberedning"]');
-      if (a) {
-        await context.click('.tabs-component-tab>a[aria-controls="#ingredienser"]');
-      } else if (b) {
-        await context.click('.tabs-component-tab>a[aria-controls="#opbevaring"]');
-      } else if (c) {
-        await context.click('.tabs-component-tab>a[aria-controls="#tilberedning"]');
-      } else {
-        console.log('No such section present in the website.');
-      }
-    } catch (e) {
-      console.log(e.message);
-    } finally {
-      await context.extract(productDetails);
-    }
-  },
 };
