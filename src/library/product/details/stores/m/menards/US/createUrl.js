33acde1b2f17c405a095f0c783928a2999c0f990
unknown <leena.gavali-odc@partner.import.io>
2020-11-03 16:10:10 +0530

    menards_core_IM

--- src/library/product/details/stores/m/menards/US/createUrl.js
+++ src/library/product/details/stores/m/menards/US/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'menards.com',
     prefix: null,
-    url: 'https://www.menards.com/main/bath/toilets/two-piece-toilets/american-standard-astute-trade-vormax-reg-tall-height-elongated-complete-toilet-with-slow-close-seat/727aa-121-020/p-1444423992105-c-5974.htm?tid=5976492407131538170&ipos=1',
+    url: 'https://www.menards.com/main/paint/interior-paint-stain/wall-trim-paint/smartcoat-interior-advanced-paint-primer-blue-color-family-1-qt/357555t-sc454/p-4364363663558667-c-8025.htm?tid=-1&ipos=6',
     country: 'US',
     store: 'menards',
     zipcode: '',
