d306d031ea90f428f6eab97659ac9439300f40da
prasanti mylavarapu <prasanti.mylavarapu-od@partner.import.io>
2020-11-03 14:49:21 +0530

    move to qa1

--- src/library/product/search/stores/i/iga_fr/CA/execute.js
+++ src/library/product/search/stores/i/iga_fr/CA/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'CA',
     store: 'iga_fr',
     domain: 'iga.net/fr',
-    url: 'https://www.iga.net/fr/search',
+    url: 'https://www.iga.net/fr/search?t={14A91276-40BA-49D5-940D-3692DE3DC381}&k=olive%20oil',
     loadedSelector: 'body',
     noResultsXPath: 'body',
     zipcode: '',
