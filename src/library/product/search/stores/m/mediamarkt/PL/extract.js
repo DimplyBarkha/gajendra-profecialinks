f5d2a793c8df64254fc0a5997676dba15f550997
IpsitaM <iamipsita07@gmail.com>
2020-11-03 10:11:38 +0530

    Added PL search extractor

---
+++ src/library/product/search/stores/m/mediamarkt/PL/extract.js
@@ -0,0 +1,66 @@
+const { transform } = require('./transform');
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'PL',
+    store: 'mediamarkt',
+    transform,
+    domain: 'mediamarkt.pl',
+    zipcode: '',
+  },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { productDetails } = dependencies;
+    const { transform } = parameters;
+    await context.evaluate(() => {
+      const searchUrl = window.location.href;
+      const appendElements = document.querySelectorAll('div[class="m-offerBox_header"]>h2[class*="headline"]>a,p[class="m-productsBox_name"]>a');
+      if (appendElements.length) {
+        appendElements.forEach((element) => {
+          element.setAttribute('searchurl', searchUrl);
+        });
+      }
+    });
+    await context.evaluate(() => {
+      const ratingBox = document.querySelectorAll('div[class="m-offerBox_header"]>div[class="m-offerBox_rating"],div[class="js-productsBox_headerEqual"]>div[class="m-productsBox_rating"]');
+      const ratingArr = [];
+      ratingBox.forEach((elem) => {
+        const ratingValue = elem.querySelectorAll('i[class*="active"]');
+        ratingArr.push(ratingValue.length);
+      });
+      const rows = document.querySelectorAll('div[class="m-offerBox_header"]>h2[class*="headline"]>a,p[class="m-productsBox_name"]>a');
+      for (let i = 0; i < rows.length; i++) {
+        rows[i].setAttribute('ratingvalue', ratingArr[i]);
+      }
+    });
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop !== 20000) {
+          await stall(2000);
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 20000) {
+            await stall(4000);
+            break;
+          }
+        }
+
+        function stall (ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await applyScroll(context);
+    return await context.extract(productDetails, { transform });
+  },
+
+};
