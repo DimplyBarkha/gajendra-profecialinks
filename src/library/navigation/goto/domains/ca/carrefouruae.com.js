9682d3dda0c3e2f1b8fa62bf8408544fa3023e73
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-11-04 14:41:25 +0530

    #1201. Created extractor robot.

---
+++ src/library/navigation/goto/domains/ca/carrefouruae.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'carrefouruae.com',
+    timeout: 500000,
+    country: 'AE',
+    store: 'carrefour',
+    zipcode: "''",
+  },
+};
