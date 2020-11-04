bac70be754ff670914af32e66f34b3f979559508
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-04 22:03:20 +0100

    created a template for the search extractor

---
+++ src/library/product/search/stores/w/windeln/DE/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'windeln',
+    domain: 'windeln.de',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/w/windeln/DE/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'DE',
+    store: 'windeln',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'windeln.de',
+    zipcode: '',
+  },
+};
