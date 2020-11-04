9a92473f45c4149574e51d63e2d38bbda765a2be
sangeeta <sangeta.kumari@synerzip.com>
2020-11-04 23:47:44 +0530

    935 cotr beauty it changes

--- src/library/product/details/stores/b/beautye/IT/createUrl.js
+++ src/library/product/details/stores/b/beautye/IT/createUrl.js
@@ -3,8 +3,8 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'beautye.it',
-    prefix: null,
-    url: null,
+    // prefix: null,
+    // url: null,
     country: 'IT',
     store: 'beautye',
     zipcode: '',
