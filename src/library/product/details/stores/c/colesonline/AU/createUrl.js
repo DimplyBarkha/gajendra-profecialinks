a41d53b113872abf8df634431dff994402ac593a
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-04 16:01:19 +0530

    updated goto logic #1026

--- src/library/product/details/stores/c/colesonline/AU/createUrl.js
+++ src/library/product/details/stores/c/colesonline/AU/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'shop.coles.com.au',
     prefix: null,
-    url: null,
+    url: 'https://shop.coles.com.au/a/national/everything/search/{id}?pageNumber=1',
     country: 'AU',
     store: 'colesonline',
     zipcode: '',
