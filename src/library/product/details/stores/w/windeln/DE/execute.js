ba75662bbfd49c577e58b29388177be1d07cfd0e
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-04 22:01:37 +0100

    created a template for the core extractor

---
+++ src/library/product/details/stores/w/windeln/DE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'windeln',
+    domain: 'windeln.de',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
