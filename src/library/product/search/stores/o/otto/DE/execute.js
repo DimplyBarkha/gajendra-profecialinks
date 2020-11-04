5c75c3bdcea94cb38957ca53640ab178233013fd
unknown <jagruti.chaudhary@od.import.io>
2020-11-04 09:42:12 +0530

    otto search deploy

---
+++ src/library/product/search/stores/o/otto/DE/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'otto',
+    domain: 'otto.de',
+    url: "https://www.otto.de/suche/adidas%20golf/",
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/o/otto/DE/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'DE',
+    store: 'otto',
+    nextLinkSelector: '#san_pagingBottomNext > button',
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: 'boby',
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'otto.de',
+    zipcode: '',
+  },
+};
