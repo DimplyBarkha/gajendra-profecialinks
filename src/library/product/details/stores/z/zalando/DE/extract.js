9a775f43b72f69a6d825b89156cb1d70655669a2
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-04 22:21:41 +0100

    created a template for the core extractor

---
+++ src/library/product/details/stores/z/zalando/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'zalando',
+    transform: null,
+    domain: 'zalando.de',
+    zipcode: '',
+  },
+};
