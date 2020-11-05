dcc0e7207951fffac74ffe924e505fd62d02c41c
SS <toaranjan@gmail.com>
2020-11-05 09:10:22 +0530

    #875. ralphs site updated

--- src/library/product/search/stores/r/ralphs_92201/US/paginate.js
+++ src/library/product/search/stores/r/ralphs_92201/US/paginate.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'ralphs_92201',
     loadedSelector: '.PaginateItems',
-    nextLinkSelector: 'button.kds-Button.kds-Button--primaryInverse.kds-Button--compact.kds-Button--hasIconOnly.kds-Pagination-next:not(:disabled)',
+    nextLinkSelector: 'button.kds-Button.kds-Button--tertiary.kds-Button--compact.kds-Button--hasIconOnly.kds-Pagination-next:not(:disabled)',
     // openSearchDefinition: {
     //   template: 'https://www.ralphs.com/search?query={searchTerms}&searchType=default_search&fulfillment=all&page={page}',
     // },
