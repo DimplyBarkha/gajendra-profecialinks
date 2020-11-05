95da4091c2aecd97d364c5515ffd65fcb98a6e4f
osmosysprakhar <prakhar.s@osmosys.asia>
2020-11-05 05:35:34 +0530

    Updated CreateURL.JS for amazon promos FR

--- src/library/product/details/stores/a/amazonMsPromos/FR/createUrl.js
+++ src/library/product/details/stores/a/amazonMsPromos/FR/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'amazon.fr',
     prefix: null,
-    url: 'https://www.amazon.fr/gp/goldbox',
+    url: 'https://www.amazon.fr/',
     country: 'FR',
     store: 'amazonMsPromos',
     zipcode: '',
