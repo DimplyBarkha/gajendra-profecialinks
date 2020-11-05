dd499910e82e21aee82976837d9ca600c7618ef7
nilofarsh <nilofar.shaikh@synerzip.com>
2020-11-05 15:41:43 +0530

    #688 QA1 bug fixed

--- src/library/navigation/goto/domains/pa/paris.cl.js
+++ src/library/navigation/goto/domains/pa/paris.cl.js
@@ -3,9 +3,35 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'paris.cl',
-    timeout: 100000,
+    timeout: 50000,
     country: 'CL',
     store: 'paris',
     zipcode: '',
+  },
+  implementation: async (
+    { url, zipcode, storeId },
+    parameters,
+    context,
+    dependencies
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setLoadImages(false);
+    await context.setLoadAllResources(false);
+    /**
+     * Adding goto in try catch as the webpage keeps on loading indefinitely
+     * even if we give huge time out and eventually throws timeout error
+     */
+    try {
+      await context.goto(url, {
+        timeout: timeout,
+        waitUntil: 'load',
+        checkBlocked: true,
+      });
+    } catch (e) {
+      console.log('page load time out ----> ', e);
+    }
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
