f4579c24bb6baf7dd81a8faea79b41b1acf44fbe
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-11-05 15:02:08 +0530

    detail page changed according to QA

--- src/library/product/details/stores/h/houzz/US/extract.js
+++ src/library/product/details/stores/h/houzz/US/extract.js
@@ -1,3 +1,65 @@
+async function implementation(
+  // @ts-ignore
+  inputs,
+  parameters,
+  context,
+  dependencies
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(() => {
+    document.querySelector("li[class='hzui-tabs__label   ']").click();
+  });
+  await context.evaluate(async function () {
+    function findLabel(productObj, label) {
+      const value = productObj[label];
+      if (Array.isArray(value)) {
+        return {
+          label: value.reduce((prevVal, currentVal) => {
+            return prevVal ? prevVal + "," + currentVal : currentVal;
+          }, ""),
+        };
+      } else if (value) {
+        return { label: value };
+      }
+      return null;
+    }
+    function addHiddenDiv(id, content) {
+      const newDiv = document.createElement("div");
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = "none";
+      document.body.appendChild(newDiv);
+    }
+    function findAndInsertLabel(obj, labelName, outputName) {
+      const result = findLabel(obj, labelName);
+      if (result != null) {
+        addHiddenDiv("ii_" + outputName, result.label);
+      }
+    }
+    // @ts-ignore
+    const jsonString = document.querySelector(
+      "script[type='application/ld+json']"
+    ).innerText;
+    let jsonParsed = {};
+    if (jsonString && jsonString.trim()) {
+      jsonParsed = JSON.parse(jsonString);
+      findAndInsertLabel(jsonParsed[0], "image", "image");
+      findAndInsertLabel(jsonParsed[0], "category", "category");
+      findAndInsertLabel(jsonParsed[0], "name", "nameExtended");
+      findAndInsertLabel(jsonParsed[0], "description", "description");
+      findAndInsertLabel(jsonParsed[0], "sku", "sku");
+      findAndInsertLabel(jsonParsed[0], "mpn", "mpc");
+      findAndInsertLabel(jsonParsed[0], "material", "material");
+      findAndInsertLabel(jsonParsed[0], "color", "color");
+      findAndInsertLabel(jsonParsed[0].weight, "description", "weightNet");
+      findAndInsertLabel(jsonParsed[0].manufacturer, "name", "manufacturer");
+      findAndInsertLabel(jsonParsed[0], "productID", "variantId");
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
   implements: "product/details/extract",
   parameterValues: {
@@ -7,15 +69,5 @@ module.exports = {
     domain: "houzz.com",
     zipcode: "",
   },
-  implementation: async (
-    { url },
-    { country, domain },
-    context,
-    dependencies
-  ) => {
-    await context.evaluate(() => {
-      document.querySelector("li[class='hzui-tabs__label   ']").click();
-    });
-    await context.extract(dependencies.productDetails);
-  },
+  implementation,
 };
