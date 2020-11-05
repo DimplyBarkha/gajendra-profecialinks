4196ced3f9b54172b4d54f7d13b1ba2942712e55
unknown <leena.gavali-odc@partner.import.io>
2020-11-05 12:26:39 +0530

    menards_coreIM_QA2

--- src/library/product/details/stores/m/menards/US/createUrl.js
+++ src/library/product/details/stores/m/menards/US/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'menards.com',
     prefix: null,
-    url: 'https://www.menards.com/main/paint/interior-paint-stain/wall-trim-paint/smartcoat-interior-advanced-paint-primer-blue-color-family-1-qt/357555t-sc454/p-4364363663558667-c-8025.htm?tid=-1&ipos=6',
+    url: null,
     country: 'US',
     store: 'menards',
     zipcode: '',
