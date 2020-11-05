6494e295b4997a83214b2fbc125390d1840e1f9c
shrutikab <shrutika.bongarde@synerzip.com>
2020-11-05 15:24:31 +0530

    variant added(#1219)

---
+++ src/library/product/details/stores/l/lowes_lg/US/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'US',
+    store: 'lowes_lg',
+    transform: null,
+    domain: 'lowes.com',
+    zipcode: '',
+  },
+};
