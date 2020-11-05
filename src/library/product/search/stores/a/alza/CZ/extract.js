007dc9ffcb87bd94d519935d8580a3eeb33faf0f
Shubham Sourav <shubhsrv7@gmail.com>
2020-11-05 21:43:19 +0530

    Issue #1105

--- src/library/product/search/stores/a/alza/CZ/extract.js
+++ src/library/product/search/stores/a/alza/CZ/extract.js
@@ -8,23 +8,4 @@ module.exports = {
     domain: 'alza.cz',
     zipcode: '',
   },
-
-  implementation: async function  (
-    inputs,
-    parameters,
-    context,
-    dependencies,
-  ) {
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    await context.evaluate(async function()
-    {
-      var element = window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
-      return element;
-    })
-    await context.waitForXPath('//div[@class="browsingitemcontainer"]/div[last()-1]//img/@srcset')
-    
-    return await context.extract(productDetails, { transform });
-  }
-  
 };
