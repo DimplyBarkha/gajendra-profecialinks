fa49af3d85759f8fa29651ccda53d59bbf33e67a
vikas kyatannawar <vikas.k@osmosys.asia>
2020-11-05 12:16:16 +0530

    added amazon primenow core extractors

--- src/library/product/details/stores/a/amazonPrimeNow/US/extract.js
+++ src/library/product/details/stores/a/amazonPrimeNow/US/extract.js
@@ -1,10 +1,19 @@
+const { implementation } = require('../../../a/amazon/US/extract');
+const { transform } = require('../../../../sharedAmazon/transformNew');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'amazonPrimeNow',
-    transform: null,
+    transform,
     domain: 'primenow.amazon.com',
   },
+  dependencies: {
+    productDetails: 'extraction:product/details/stores/${store[0:1]}/${store}/${country}/extract',
+    Helpers: 'module:helpers/helpers',
+    AmazonHelp: 'module:helpers/amazonHelp',
+    goto: 'action:navigation/goto',
+  },
+  implementation,
 };
