ee1c0a85c7bab70ea6dde7c63465117a52bce6a0
Heramba Rao Boddu <herambarao.boddu@hcl.com>
2020-11-05 10:55:14 +0530

    UAT fixes

--- src/library/product/search/stores/d/dafiti/BR/extract.js
+++ src/library/product/search/stores/d/dafiti/BR/extract.js
@@ -8,4 +8,19 @@ module.exports = {
     domain: 'dafiti.com.br',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, store, transform: transformParam }, context, { productDetails }) => {
+    async function addUrl () {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+      const url = window.location.href;
+      addHiddenDiv('added-searchurl', url);
+    }
+    await context.evaluate(addUrl);
+    return await context.extract(productDetails, { transform: transformParam });
+  },
 };
