f6d805a448fff1c28434ad6825a98aea21a91ec1
Karina Miernik <karina.miernik@gmail.com>
2020-11-05 08:57:29 +0100

    #1152 modified extract.js

--- src/library/product/details/stores/b/boxed/US/extract.js
+++ src/library/product/details/stores/b/boxed/US/extract.js
@@ -1,11 +1,33 @@
 
+const { cleanUp } = require('../../../../shared');
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'boxed',
-    transform: null,
+    transform: cleanUp,
     domain: 'boxed.com',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      // const zoom = document.querySelector('.magnifyframe');
+
+      // if (zoom) {
+      //   zoom.setAttribute('zoom', 'yes');
+      // }
+
+      // function addElementToDocument (id, key, value) {
+      //   const catElement = document.createElement('div');
+      //   catElement.id = id;
+      //   catElement.setAttribute('value', key);
+      //   catElement.innerText = value;
+      //   catElement.style.display = 'none';
+      //   document.body.appendChild(catElement);
+      // };
+    });
+
+    await context.extract(productDetails);
   },
 };
