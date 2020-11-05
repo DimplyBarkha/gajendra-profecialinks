69691c7e726898cd38e35799ff15e93083a3f81f
prasath <r.rahul@olivedatacentre.com>
2020-11-05 13:51:45 +0530

    prasath-notino

---
+++ src/library/navigation/goto/domains/no/notino.de.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'notino.de',
+    timeout: null,
+    country: 'DE',
+    store: 'notino',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/n/notino/DE/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'DE',
+    store: 'notino',
+    domain: 'notino.de',
+    zipcode: '',
+  },
+};
