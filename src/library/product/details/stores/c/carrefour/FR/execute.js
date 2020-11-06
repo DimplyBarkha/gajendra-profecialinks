bc0eb7e2c1cacae2270d43daa085369339886e03
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-11-06 12:21:10 +0530

    initial setup carrefour_fr (#1258)

---
+++ src/library/product/details/stores/c/carrefour/FR/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'FR',
+    store: 'carrefour',
+    domain: 'carrefour.fr',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
