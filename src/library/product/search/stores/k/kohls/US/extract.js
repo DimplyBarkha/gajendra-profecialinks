115defa3638e262b8add2f7187df249ecf758c42
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-06 18:05:53 +0100

    created a template for the search extractor

---
+++ src/library/product/search/stores/k/kohls/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'kohls',
+    transform: null,
+    domain: 'kohls.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/k/kohls/US/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'US',
+    store: 'kohls',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'kohls.com',
+    zipcode: '',
+  },
+};
