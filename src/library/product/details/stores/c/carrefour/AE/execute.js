2a8621a3248c7bf36ebdea737f120badfdd3622b
Gajender Ramola <ramola106@gmail.com>
2020-11-06 13:45:50 +0530

    issue fix of manufactrure

--- src/library/product/details/stores/c/carrefour/AE/execute.js
+++ src/library/product/details/stores/c/carrefour/AE/execute.js
@@ -1,4 +1,55 @@
+/**
+ *
+ * @param { { url?: string,  id?: string, zipcode?: any, storeId?:any} } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action, createUrl: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  let { url, id, zipcode, storeId } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  await dependencies.goto({ url, zipcode, storeId });
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 1200) {
+        await stall(500);
+        scrollTop += 200;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 1200) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
 
+  // TODO: Check for not found?
+}
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -6,7 +57,8 @@ module.exports = {
     store: 'carrefour',
     domain: 'carrefouruae.com',
     loadedSelector: 'h1.css-17yb6kp',
-    noResultsXPath: "//h2[contains(text(),'Sorry! The page you're looking for does not exist')]",
+    noResultsXPath: '//h2[contains(text(),"Sorry! The page you\'re looking for does not exist")]',
     zipcode: '',
   },
+  implementation,
 };
