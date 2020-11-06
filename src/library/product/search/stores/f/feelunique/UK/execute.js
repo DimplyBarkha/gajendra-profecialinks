389db88b5bd1a6ccdd3512fc2fd96cfedef6d948
madhanimportio <madhan.selvaraj-odc@partner.import.io>
2020-11-06 15:21:17 +0530

    changes in search extractor yaml file

--- src/library/product/search/stores/f/feelunique/UK/execute.js
+++ src/library/product/search/stores/f/feelunique/UK/execute.js
@@ -6,6 +6,7 @@ module.exports = {
     store: 'feelunique',
     domain: 'feelunique.com',
     url: "https://www.feelunique.com/search?q={searchTerms}",
+    // url: 'https://www.feelunique.com/search?q=nyx%20eyeshadow',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
