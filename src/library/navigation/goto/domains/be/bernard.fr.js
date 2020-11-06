b5c0fb1c90fd8318ff342e4e008824c1c4142434
unknown <shalini.kumari-odc@partner.import.io>
2020-11-06 12:59:44 +0530

    bernard

--- src/library/navigation/goto/domains/be/bernard.fr.js
+++ src/library/navigation/goto/domains/be/bernard.fr.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'bernard.fr',
-    timeout: null,
+    timeout: 50000,
     country: 'FR',
     store: 'bernard',
     zipcode: '',
