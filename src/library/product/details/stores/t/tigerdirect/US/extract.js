cb3532c46c17ccb15c1791f02c5a9bcf699cf9c6
unknown <leena.gavali-odc@partner.import.io>
2020-11-06 21:47:19 +0530

    Tiger_direct_final

--- src/library/product/details/stores/t/tigerdirect/US/extract.js
+++ src/library/product/details/stores/t/tigerdirect/US/extract.js
@@ -52,37 +52,37 @@ async function implementation(
       return result;
     };
     // for rank
-    const sliceURL = (data1,data2,data3,data4,data5) => {
-      var cnt = data1+" || "+data2+" || "+data3+" || "+data4+" || "+data5;
-          addElementToDocument('altImages', cnt);
-          //addElementToDocument('altImages', cnt);
-      }
-    var URL1 = getXpath('//div[@class="pdp-variants"]//ul/li[1]/a//@href', 'nodeValue');
-    var URL2 = getXpath('//div[@class="pdp-variants"]//ul/li[2]/a//@href', 'nodeValue');
-    var URL3 = getXpath('//div[@class="pdp-variants"]//ul/li[3]/a//@href', 'nodeValue');
-    var URL4 = getXpath('//div[@class="pdp-variants"]//ul/li[4]/a//@href', 'nodeValue');
-    var URL5 = getXpath('//div[@class="pdp-variants"]//ul/li[5]/a//@href', 'nodeValue');
-    if (URL1 != 0) {
-      URL1 = URL1.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    }
-    if (URL2 != 0) {
-      URL2 = URL2.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    }
-    if (URL3 != 0) {
-      URL3 = URL3.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    }
-    if (URL4 != 0) {
-      URL4 = URL4.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    }
-    if (URL5 != 0) {
-      URL5 = URL5.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    }
+    // const sliceURL = (data1,data2,data3,data4,data5) => {
+    //   var cnt = data1+" || "+data2+" || "+data3+" || "+data4+" || "+data5;
+    //       addElementToDocument('altImages', cnt);
+    //       //addElementToDocument('altImages', cnt);
+    //   }
+    // var URL1 = getXpath('//div[@class="pdp-variants"]//ul/li[1]/a//@href', 'nodeValue');
+    // var URL2 = getXpath('//div[@class="pdp-variants"]//ul/li[2]/a//@href', 'nodeValue');
+    // var URL3 = getXpath('//div[@class="pdp-variants"]//ul/li[3]/a//@href', 'nodeValue');
+    // var URL4 = getXpath('//div[@class="pdp-variants"]//ul/li[4]/a//@href', 'nodeValue');
+    // var URL5 = getXpath('//div[@class="pdp-variants"]//ul/li[5]/a//@href', 'nodeValue');
+    // if (URL1 != 0) {
+    //   URL1 = URL1.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
+    // }
+    // if (URL2 != 0) {
+    //   URL2 = URL2.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
+    // }
+    // if (URL3 != 0) {
+    //   URL3 = URL3.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
+    // }
+    // if (URL4 != 0) {
+    //   URL4 = URL4.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
+    // }
+    // if (URL5 != 0) {
+    //   URL5 = URL5.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
+    // }
     // URL1 = URL1.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
     // URL2 = URL2.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
     // URL3 = URL3.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
     // URL4 = URL4.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
     // URL5 = URL5.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    sliceURL(URL1,URL2,URL3,URL4,URL5);
+    // sliceURL(URL1,URL2,URL3,URL4,URL5);
     // var upc = ""
     // addElementToDocument('upc', upc);
   });
