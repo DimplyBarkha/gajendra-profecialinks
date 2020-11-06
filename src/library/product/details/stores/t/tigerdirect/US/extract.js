f7ea5e8717d4ae1bcfd36111d482de6985bf6270
unknown <leena.gavali-odc@partner.import.io>
2020-11-07 00:02:18 +0530

    Tiger_direct_IM_CORE_UAT

--- src/library/product/details/stores/t/tigerdirect/US/extract.js
+++ src/library/product/details/stores/t/tigerdirect/US/extract.js
@@ -24,16 +24,6 @@ async function implementation(
       var elems = document.querySelectorAll(xpathforpagination);
       elems[0].classList.add('pagination');
     }
-    // Method to Retrieve Xpath content of a Single Node
-    var getXpath = (xpath, prop) => {
-      var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-      let result;
-      if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-      else result = elem ? elem.singleNodeValue : '';
-      return result && result.trim ? result.trim() : result;
-    };
-    
-    // for rank
     function addElementToDocument(key, value) {
       const catElement = document.createElement('div');
       catElement.id = key;
@@ -51,41 +41,25 @@ async function implementation(
       }
       return result;
     };
-    // for rank
-    // const sliceURL = (data1,data2,data3,data4,data5) => {
-    //   var cnt = data1+" || "+data2+" || "+data3+" || "+data4+" || "+data5;
-    //       addElementToDocument('altImages', cnt);
-    //       //addElementToDocument('altImages', cnt);
-    //   }
-    // var URL1 = getXpath('//div[@class="pdp-variants"]//ul/li[1]/a//@href', 'nodeValue');
-    // var URL2 = getXpath('//div[@class="pdp-variants"]//ul/li[2]/a//@href', 'nodeValue');
-    // var URL3 = getXpath('//div[@class="pdp-variants"]//ul/li[3]/a//@href', 'nodeValue');
-    // var URL4 = getXpath('//div[@class="pdp-variants"]//ul/li[4]/a//@href', 'nodeValue');
-    // var URL5 = getXpath('//div[@class="pdp-variants"]//ul/li[5]/a//@href', 'nodeValue');
-    // if (URL1 != 0) {
-    //   URL1 = URL1.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // }
-    // if (URL2 != 0) {
-    //   URL2 = URL2.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // }
-    // if (URL3 != 0) {
-    //   URL3 = URL3.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // }
-    // if (URL4 != 0) {
-    //   URL4 = URL4.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // }
-    // if (URL5 != 0) {
-    //   URL5 = URL5.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // }
-    // URL1 = URL1.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // URL2 = URL2.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // URL3 = URL3.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // URL4 = URL4.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // URL5 = URL5.replace("/applications/searchtools/item-details.asp?EdpNo=", "");
-    // sliceURL(URL1,URL2,URL3,URL4,URL5);
-    // var upc = ""
-    // addElementToDocument('upc', upc);
+    // Double Pipe Concatenation
+    const pipeSeparatorDouble = (id, data) => {
+      // var doubleSeparatorText = data.join(' || ');
+      addElementToDocument('id', data);
+    };
+    const pipeSeparatorDouble1 = (id, data) => {
+      // var doubleSeparatorText = data.join(' || ');
+      addElementToDocument('zoom', data);
+    };
+    // XPATH Data Extraction For Additional Description Bullet
+    const addDescBulletInfo1 = getAllXpath("//div[@class='leftCol shortDesc']/ul/li/text()", 'nodeValue');
+    const addDescBulletInfo2 = getAllXpath("//div[@id='ProductManuals']/ul[@class='pdManuals']/li/a/text()", 'nodeValue');
+    var xyz = addDescBulletInfo1.concat(addDescBulletInfo2)
+    var lee = xyz.join(" || ");
+    var abc = addDescBulletInfo1.join(" || ");
+    // addElementToDocument(id, lee);
+    pipeSeparatorDouble('addDescBulletInfo', lee);
+    pipeSeparatorDouble1('addDescBulletInfo', abc);
   });
-  //rank end
+
   return await context.extract(productDetails, { transform });
 }
