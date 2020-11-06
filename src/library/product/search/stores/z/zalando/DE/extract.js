940be9aab8d3d3bc9dbcd0139a2f31baeb8bbb16
Karina Miernik <karina.miernik@gmail.com>
2020-11-06 15:29:18 +0100

    #1279 added logic for extractor

--- src/library/product/search/stores/z/zalando/DE/extract.js
+++ src/library/product/search/stores/z/zalando/DE/extract.js
@@ -5,24 +5,28 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await new Promise(resolve => setTimeout(resolve, 2000));
-
   await context.evaluate(() => {
-    function addProp (selector, iterator, propName, value) {
-      document.querySelectorAll(selector)[iterator].setAttribute(propName, value);
-    }
-    // const allProducts = document.querySelectorAll('div[class*="DvypSJ"]');
-    // for (let i = 0; i < allProducts.length; i++) {
-    //   addProp('div[class*="DvypSJ"]', i, 'rank', `${i + 1}`);
-    // }
+    const allProducts = document.querySelectorAll('div[class*="DvypSJ"]');
+    allProducts.forEach((product, index) => {
+      product.setAttribute('rank', `${index + 1}`);
+    });
     const sponsoredProducts = document.querySelectorAll('div[class*="weHhRC"]');
     // @ts-ignore
-    if (sponsoredProducts) sponsoredProducts.forEach(e => e.parentNode.classList.add('sponsored'));
-    const notSponsoredProducts = document.querySelectorAll('div[class="qMZa55 SQGpu8 iOzucJ JT3_zV DvypSJ"]');
-    for (let i = 0; i < notSponsoredProducts.length; i++) {
-      addProp('div[class="qMZa55 SQGpu8 iOzucJ JT3_zV DvypSJ"]', i, 'rankorganic', `${i + 1}`);
+    if (sponsoredProducts) {
+      sponsoredProducts.forEach(e => {
+        // @ts-ignore
+        e.parentNode.classList.add('sponsored');
+        // @ts-ignore
+        e.parentNode.setAttribute('sponsored', 'yes');
+      });
     }
-    const productUrl = document.querySelectorAll('article a');
+
+    const notSponsoredProducts = document.querySelectorAll('div[class="qMZa55 SQGpu8 iOzucJ JT3_zV DvypSJ"]');
+    notSponsoredProducts.forEach((product, index) => {
+      product.setAttribute('rankorganic', `${index + 1}`);
+    });
+
+    const productUrl = document.querySelectorAll('article > a');
     const prefix = 'https://zalando.de';
 
     productUrl.forEach((element) => {
