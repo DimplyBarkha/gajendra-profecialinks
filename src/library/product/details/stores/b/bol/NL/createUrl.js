f2f5fe0af73b0e63f3fdfa55d667d1a157dc265e
byQ96 <michal126@hotmail.com>
2020-11-06 10:13:55 +0100

    created template for bol core extractor

---
+++ src/library/product/details/stores/b/bol/NL/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'bol.com',
+    prefix: null,
+    url: 'https://www.bol.com/nl/p/something/{id}/',
+    country: 'NL',
+    store: 'bol',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/b/bol/NL/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'NL',
+    store: 'bol',
+    domain: 'bol.com',
+    zipcode: '',
+  },
+};
