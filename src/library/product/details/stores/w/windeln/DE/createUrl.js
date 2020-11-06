33ced861554a2db899c820ea221dcf54bba1284d
Wayne E <bartekstafiej2@gmail.com>
2020-11-06 11:17:01 +0100

    #1274 added url

--- src/library/product/details/stores/w/windeln/DE/createUrl.js
+++ src/library/product/details/stores/w/windeln/DE/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'windeln.de',
     prefix: null,
-    url: null,
+    url: 'https://www.windeln.de/{id}.html',
     country: 'DE',
     store: 'windeln',
     zipcode: '',
