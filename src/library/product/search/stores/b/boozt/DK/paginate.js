098b9ec901622fcf8edf0871383105aba0427dc8
astropaula <wojciechowska.fran@gmail.com>
2020-11-06 14:35:03 +0100

    #1151 Fixed nextLinkSelector

--- src/library/product/search/stores/b/boozt/DK/paginate.js
+++ src/library/product/search/stores/b/boozt/DK/paginate.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'DK',
     store: 'boozt',
-    nextLinkSelector: 'a[class="ignore-hidden-toggle pagination-next "] span[class="icon-arrow-right"], button[class="btn btn--secondary btn--big pagination__btn-next"]:not([disabled])',
+    nextLinkSelector: 'div [class="pagination bottom-paging"] a[class="ignore-hidden-toggle pagination-next "] span[class="icon-arrow-right"], button[class="btn btn--secondary btn--big pagination__btn-next"]:not([disabled])',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div[class="product-list clearfix"]',
