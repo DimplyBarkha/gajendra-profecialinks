5dc87447d4be12f9a16312d78501dc4ee7dcbd41
Wayne E <bartekstafiej2@gmail.com>
2020-11-06 11:17:41 +0100

    #1274 Added some xpaths, added clicking in variants and category xpath

--- src/library/product/details/stores/w/windeln/DE/extract.js
+++ src/library/product/details/stores/w/windeln/DE/extract.js
@@ -1,4 +1,38 @@
+async function implementation (inputs, parameters, context, dependencies) {
+  const { productDetails } = dependencies;
 
+  // const variantsNumber = await context.evaluate(() => {
+  //   const variantLength = document.querySelectorAll('.variant-2-panel.active.scroll-pane>div').length;
+  //   return variantLength;
+  // });
+
+  for (let i = 0; i < 2; i++) { // variantsNumber.length;
+    await context.evaluate(() => {
+      const categorySelector = document.querySelector('.row.breadcrumbs-section').textContent;
+      const category = categorySelector.replace('Zurück zu:', '>');
+
+      document.querySelector('.row.breadcrumbs-section').setAttribute('category', category);
+    });
+
+    await context.evaluate(() => {
+      if (document.querySelector('.play-button') !== null) {
+        document.querySelector('.play-button').click();
+      }
+    });
+
+    const nextVariant = await context.evaluate(() => {
+      return document.querySelectorAll('.variant-2-panel.active.scroll-pane>div')[i];
+    });
+
+    if (nextVariant === null) {
+      return await context.extract(productDetails);
+    } else {
+      await context.click(document.querySelectorAll('.variant-2-panel.active.scroll-pane>div')[i]);
+    }
+
+    await context.extract(productDetails);
+  }
+};
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,4 +42,5 @@ module.exports = {
     domain: 'windeln.de',
     zipcode: '',
   },
+  implementation,
 };
