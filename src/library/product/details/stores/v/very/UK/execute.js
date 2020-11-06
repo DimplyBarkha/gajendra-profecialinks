4b680aaa63f0763a2d79e420aefd2801974cfcc3
mania307 <maniakalnaa@gmail.com>
2020-11-06 13:45:37 +0100

    updated execute.js, xpaths, added exampleInput

--- src/library/product/details/stores/v/very/UK/execute.js
+++ src/library/product/details/stores/v/very/UK/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'UK',
     store: 'very',
     domain: 'very.co.uk',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div#recs_1',
+    noResultsXPath: '//span[@id="noSearchResultsString"]',
     zipcode: '',
   },
 };
