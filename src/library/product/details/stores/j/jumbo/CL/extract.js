be38420dc2936111269d8b61c73fa17cb87b7061
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-06 09:34:44 +0100

    created a template for the core extractor

---
+++ src/library/product/details/stores/j/jumbo/CL/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'CL',
+    store: 'jumbo',
+    transform: null,
+    domain: 'jumbo.cl',
+    zipcode: '',
+  },
+};
