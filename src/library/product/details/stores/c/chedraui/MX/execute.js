a353fa0c7c2c3182101cdb85441ab4520707650a
rajeev <rajeev.ranjan@synerzip.com>
2020-11-06 15:30:50 +0530

    #1285 #1286 initial core submit

---
+++ src/library/product/details/stores/c/chedraui/MX/execute.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'MX',
+    store: 'chedraui',
+    domain: 'chedraui.com.mx',
+    zipcode: "''",
+  },
+};
