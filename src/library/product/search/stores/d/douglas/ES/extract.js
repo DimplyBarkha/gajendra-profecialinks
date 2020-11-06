f11417295f305970389e302066d2d5936279b5b8
sangeeta <sangeta.kumari@synerzip.com>
2020-11-06 16:05:50 +0530

    search-douglas_es #1007

--- src/library/product/search/stores/d/douglas/ES/extract.js
+++ src/library/product/search/stores/d/douglas/ES/extract.js
@@ -1,4 +1,42 @@
 const { transform } = require('./transform');
+// async function implementation (
+//   inputs,
+//   parameters,
+//   context,
+//   dependencies,
+// ) {
+//   const { transform } = parameters;
+//   const { productDetails } = dependencies;
+
+//   await new Promise((resolve, reject) => setTimeout(resolve, 20000));
+
+//   await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+//   const applyScroll = async function (context) {
+//     console.log('calling applyScroll-----------');
+//     await context.evaluate(async function () {
+//       let scrollTop = 0;
+//       while (scrollTop !== 20000) {
+//         await stall(500);
+//         scrollTop += 1000;
+//         console.log('calling applyScroll evaluate-----------', window);
+//         window.scroll(0, scrollTop);
+//         if (scrollTop === 20000) {
+//           await stall(5000);
+//           break;
+//         }
+//       }
+//       function stall (ms) {
+//         return new Promise((resolve, reject) => {
+//           setTimeout(() => {
+//             resolve();
+//           }, ms);
+//         });
+//       }
+//     });
+//   };
+//   await applyScroll(context);
+//   return await context.extract(productDetails, { transform });
+// }
 
 module.exports = {
   implements: 'product/search/extract',
--- src/library/product/search/stores/d/douglas/ES/paginate.js
+++ src/library/product/search/stores/d/douglas/ES/paginate.js
@@ -4,10 +4,17 @@ module.exports = {
   parameterValues: {
     country: 'ES',
     store: 'douglas',
-    openSearchDefinition: {
-      offset: 24,
-      template: 'https://api.empathybroker.com/search/v1/query/douglas/search?banner.start=0&direct.start=0&lang=es&origin=default&promoted.start=0&q={searchTerms}&rows=24&scope=desktop&start={offset}&tariff=V005#[!opt!]{"type":"json"}[/!opt!]',
-    },
+    // openSearchDefinition: {
+    //   offset: 24,
+    //   template: 'https://api.empathybroker.com/search/v1/query/douglas/search?banner.start=0&direct.start=0&lang=es&origin=default&promoted.start=0&q={searchTerms}&rows=24&scope=desktop&start={offset}&tariff=V005#[!opt!]{"type":"json"}[/!opt!]',
+    // },
+    //spinnerSelector: '#eb-loading-spinning-balls',
+    // spinnerSelector: 'div.rd__offcanvas__content__spinner div div span',
+    // openSearchDefinition: {
+    //   template: 'https://douglas.es/?q={searchTerms}&page=6',
+    // },
+    loadedSelector: 'data-eb-results[id="eb-results"] data-eb-result',
+    noResultsXPath: '//p[@data-translate="RESULTS_NORESULTS"]',
     domain: 'douglas.es',
     zipcode: '',
   },
