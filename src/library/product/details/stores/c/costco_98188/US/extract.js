b4b2df12de11162fea0ae49d837bf9f441d68650
vinod chavan <vinodchavan102@gmail.com>
2020-11-06 20:03:26 +0530

    #1169. costco_98188 core extractors

--- src/library/product/details/stores/c/costco_98188/US/extract.js
+++ src/library/product/details/stores/c/costco_98188/US/extract.js
@@ -1,9 +1,56 @@
-
+const { transform } = require('./shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'costco_98188',
     domain: 'costco.com',
+    transform,
+  },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    // await context.waitForSelector('div.product-info-description');
+    const videoEle = await context.evaluate(async () => {
+      var videoEle = false;
+      const iframeNode = document.querySelectorAll('.wc-ribbon-button');
+      // const testNode = document.querySelectorAll('iframe.data-iframe-viewed');
+      if (iframeNode.length) {
+        iframeNode.forEach((ele) => {
+          var videoAttr = ele.getAttribute('data-wc-open-content-type')
+          if (videoAttr && videoAttr === 'video-gallery') {
+            videoEle = true;
+          }
+        });
+      }
+      return videoEle;
+    });
+    if (videoEle) {
+      await context.waitForSelector('div#vjs_video_1 video');
+    }
+    await context.evaluate(async () => {
+      const descNode = document.querySelector('div.product-info-description');
+      const videoNode = document.querySelectorAll('div#vjs_video_1 video');
+      if (videoNode.length) {
+        if (videoNode[0].getAttribute('src')) {
+          addHiddenDiv('product-video', videoNode[0].getAttribute('src'));
+        }
+      }
+      if (descNode && descNode.innerText) {
+        addHiddenDiv('product-desc', descNode.innerText)
+      }
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
---
+++ src/library/product/details/stores/c/costco_98188/US/shared.js
@@ -0,0 +1,81 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  for (const { group } of data) {
+    for (const row of group) {
+      var bulletsText = '';
+      if (row.additionalDescBulletInfo && row.additionalDescBulletInfo.length) {
+        var originalArray = row.additionalDescBulletInfo;
+        var newArray = [];
+        var lookupObject = {};
+        for (var i in originalArray) {
+          lookupObject[originalArray[i]['text']] = originalArray[i];
+        }
+        for (i in lookupObject) {
+          newArray.push(lookupObject[i]);
+        }
+        newArray.forEach((ele) => {
+          if (ele.text) {
+            bulletsText += '||' + ele.text.trim();
+          }
+        })
+        row.additionalDescBulletInfo = [
+          {
+            text: bulletsText
+          }
+        ];
+      }
+      if (row.descriptionBullets && newArray.length) {
+        row.descriptionBullets[0].text = newArray.length;
+      }
+      if (row.description) {
+          var desc = '';
+          row.description.forEach((ele) => {
+            desc += ' ' + ele.text;
+          })
+          row.description = [
+            {
+              text: bulletsText + ' ' + desc
+            }
+          ] 
+      } else if (bulletsText) {
+        row.description = [
+          {
+            text: bulletsText
+          }
+        ] 
+      }
+      if (row.specifications) {
+        var specText = '';
+        for (var j = 0; j < row.specifications.length; j = j + 2) {
+          specText += row.specifications[j].text + ': ' + row.specifications[j + 1].text + " || ";
+        }
+        row.specifications = [{
+          text: (specText.slice(0, -3)).trim()
+        }]
+      }
+    }
+  }
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+
+  return data;
+};
+
+module.exports = { transform };
