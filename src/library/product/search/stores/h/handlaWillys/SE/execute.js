7c31c6f97946056d02c1b1354c3f7a1d1684712a
SS <toaranjan@gmail.com>
2020-11-06 11:51:30 +0530

    #890. search fixes

--- src/library/product/search/stores/h/handlaWillys/SE/execute.js
+++ src/library/product/search/stores/h/handlaWillys/SE/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'SE',
     store: 'handlaWillys',
     domain: 'willys.se',
-    url: 'https://www.willys.se/sok?q={searchTerms}&size=150',
-    loadedSelector: 'div.ax-search-result',
+    url: 'https://www.willys.se/sok?q={searchTerms}', //&size=150
+    loadedSelector: 'div.ax-search-result', // 'ax-product-grid[type-of-results="results"] > div > div > ax-product-puff.ax-product-grid-tile', // 'div.ax-search-result',
     noResultsXPath: '//div[contains(@class,"no-search-result")]',
     zipcode: '',
   },
--- src/library/product/search/stores/h/handlaWillys/SE/extract.js
+++ src/library/product/search/stores/h/handlaWillys/SE/extract.js
@@ -9,10 +9,12 @@ async function implementation(
   const { transform } = parameters;
   const { productDetails } = dependencies;
   await new Promise((resolve, reject) => setTimeout(resolve, 15000));
+
   await context.evaluate(async function () {
     let productInfo = [];
     const refURL = window.location.href;
-    const fetchURL = refURL.replace('sok', 'search');
+    const refURL1 = refURL.replace('sok', 'search');
+    const fetchURL = `${refURL1}&size=150`;
     console.log('fetchURL: ', fetchURL);
 
     const response = await fetch(fetchURL, {
@@ -43,38 +45,81 @@ async function implementation(
       const data = await response.json();
       productInfo = data.results;
       const prodCount = productInfo.length;
+      let j = 0
+      const results = document.querySelectorAll('ax-product-grid[type-of-results="results"] > div > div > ax-product-puff.ax-product-grid-tile');
+      const resultCount = results.length;
+      const oneElement = document.querySelector('ax-product-grid[type-of-results="results"] > div > div > ax-product-puff.ax-product-grid-tile')
+      const parentNode = oneElement.parentNode;
+      const diff = prodCount - resultCount;
+      while (j < diff) {
+        try {
+          var cln = oneElement.cloneNode(true);
+          parentNode.appendChild(cln);
+          j++;
+        } catch (err) {}
+      }
       const productCards = document.getElementsByClassName('ax-product-grid-tile');
       let i = 0;
       while (i < prodCount) {
-        if (productCards.item(i).querySelectorAll('.extra-info').length > 0) {
-          document.getElementById(i.toString()).remove();
-        }
+        // if (productCards.item(i).querySelectorAll('.extra-info').length > 0) {
+        //   document.getElementById(i.toString()).remove();
+        // }
         addHiddenDiv(i, productCards, productInfo);
         i++;
       }
+      return productCards.length;
     }
 
     function addHiddenDiv(i, productCards, productInfo) {
-      const newDiv = document.createElement('div');
-      newDiv.id = i;
-      newDiv.className = 'extra-info-url';
-      newDiv.style.display = 'none';
+      const newDivUrl = document.createElement('div');
+      newDivUrl.id = i;
+      newDivUrl.className = 'extra-info-url';
+      newDivUrl.style.display = 'none';
      
       const newDivId = document.createElement('div');
       newDivId.id = i;
       newDivId.className = 'extra-info-id';
       newDivId.style.display = 'none';
 
+      const newDivPrice = document.createElement('div');
+      newDivPrice.id = i;
+      newDivPrice.className = 'extra-info-price';
+      newDivPrice.style.display = 'none';
+
+      const newDivName = document.createElement('div');
+      newDivName.id = i;
+      newDivName.className = 'extra-info-name';
+      newDivName.style.display = 'none';
+
+      const newDivThumbnail = document.createElement('div');
+      newDivThumbnail.id = i;
+      newDivThumbnail.className = 'extra-info-thumbnail';
+      newDivThumbnail.style.display = 'none';
+
+      const newDivManufacturer = document.createElement('div');
+      newDivManufacturer.id = i;
+      newDivManufacturer.className = 'extra-info-manufacturer';
+      newDivManufacturer.style.display = 'none';
+      
+      
       if (productInfo && productInfo[i]) {
         let prodUrl = "https://www.willys.se/produkt/${name}";
         let name = productInfo[i].name + '-' + productInfo[i].code;
         name = name.replace(/\s+/g, '-');
-        newDiv.textContent = `https://www.willys.se/produkt/${name}`;
+        newDivUrl.textContent = `https://www.willys.se/produkt/${name}`;
         newDivId.textContent = productInfo[i].code;
+        newDivPrice.textContent = productInfo[i].price;
+        newDivName.textContent = productInfo[i].name;
+        newDivThumbnail.textContent = productInfo[i].image.url;
+        newDivManufacturer.textContent = productInfo[i].manufacturer;
       }
       if (productCards && productCards.item(i)) {
-        productCards.item(i).appendChild(newDiv);
+        productCards.item(i).appendChild(newDivUrl);
         productCards.item(i).appendChild(newDivId);
+        productCards.item(i).appendChild(newDivPrice);
+        productCards.item(i).appendChild(newDivName);
+        productCards.item(i).appendChild(newDivThumbnail);
+        productCards.item(i).appendChild(newDivManufacturer);
       }
     }
 
@@ -84,10 +129,11 @@ async function implementation(
     hiddenSearchDiv.style.display = 'none';
     hiddenSearchDiv.textContent = searchUrl;
     document.body.appendChild(hiddenSearchDiv);
-
   });
 
+
   await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+  
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
