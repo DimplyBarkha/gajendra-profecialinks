01f93930c09ef758b9cba68d0eccdf88cb0d86c3
Karina Miernik <karina.miernik@gmail.com>
2020-11-07 19:02:54 +0100

    #1154 added sponsored and logic for pagination

--- src/library/product/search/stores/b/boxed/US/extract.js
+++ src/library/product/search/stores/b/boxed/US/extract.js
@@ -4,52 +4,68 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await new Promise(resolve => setTimeout(resolve, 5000));
-  const isPopupPresent = await context.evaluate(async () => {
-    return document.querySelector('p[id*="dialog"] + button');
+  await context.evaluate(async function () {
+    const isPopupPresent = document.querySelector('p[id*="dialog"] + button');
+    // @ts-ignore
+    if (isPopupPresent) isPopupPresent.click();
   });
-  if (isPopupPresent) {
-    await context.click('p[id*="dialog"] + button');
-  }
-  await context.evaluate(async () => {
-    // scroll
+
+  await context.evaluate(async function () {
+    while (document.querySelector('button[class*="b05-less _"]') !== null) {
+      // @ts-ignore
+      document.querySelector('button[class*="b05-less _"]').click();
+      await stall(2000);
+    };
+
     function stall (ms) {
-      return new Promise((resolve, reject) => {
+      return new Promise((resolve) => {
         setTimeout(() => {
           resolve();
         }, ms);
       });
     }
-    while (document.querySelector('button[class*="b05-less _"]') !== null) {
-      // @ts-ignore
-      document.querySelector('button[class*="b05-less _"]').click();
-      await new Promise((resolve, reject) => setTimeout(resolve, 1500));
+  });
+  await context.evaluate(async () => {
+    // @ts-ignore
+    if (window !== undefined) {
+      return window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
     }
-    // add prefix to product url routes
-    var productUrl = document.querySelectorAll('div[class*="ab4-less"] div[data-bx="ple-wrap"] a');
-    var prefix = 'https://www.boxed.com';
+  });
+  await context.evaluate(async () => {
+    // @ts-ignore
+    if (window !== undefined) {
+      return window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  });
+  await new Promise((resolve, reject) => setTimeout(resolve, 1500));
+
+  await context.evaluate(async () => {
+    const productUrl = document.querySelectorAll('div[class*="ab4-less"] div[data-bx="ple-wrap"] a');
+    const prefix = 'https://www.boxed.com';
 
     productUrl.forEach((element) => {
       element.setAttribute('href', prefix.concat(element.getAttribute('href')));
     });
     const prodContainer = document.querySelectorAll('div[class*="ab4-less"] li[data-bx="ple"]');
     prodContainer.forEach((element, index) => {
-      element.setAttribute('rank', (index + 1).toString());
+      element.setAttribute('rank', `${index + 1}`);
     });
-
-    const rawNumber = document.querySelector('span[class*="db585-less"] span span').textContent;
-    const match = parseInt(rawNumber);
-
-    let scrollTop = 0;
-    const scrollLimit = match * 40;
-    while (scrollTop <= scrollLimit) {
-      await stall(1000);
-      scrollTop += 1000;
-      window.scroll(0, scrollTop);
+    const sponsoredProducts = document.querySelectorAll('div[class*="cc3-less"]');
+    // @ts-ignore
+    if (sponsoredProducts) {
+      sponsoredProducts.forEach(e => {
+        // @ts-ignore
+        e.parentNode.classList.add('sponsored');
+        // @ts-ignore
+        e.parentNode.setAttribute('sponsored', 'yes');
+      });
     }
-  });
 
-  await new Promise((resolve, reject) => setTimeout(resolve, 1500));
+    const notSponsoredProducts = document.querySelectorAll('div[class="c5d964238211fd3d6146832cb02cca27-less"]');
+    notSponsoredProducts.forEach((product, index) => {
+      product.setAttribute('rankorganic', `${index + 1}`);
+    });
+  });
 
   return await context.extract(productDetails, { transform });
 }
