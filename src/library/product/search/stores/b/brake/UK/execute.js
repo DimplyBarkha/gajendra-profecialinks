0889b8258d8b696fe192cb857024f852d99ded53
sasz94 <sacha.bartlomiej@gmail.com>
2020-11-07 18:52:05 +0100

    Deployed search extractor and run tests (#1160)

--- src/library/product/search/stores/b/brake/UK/execute.js
+++ src/library/product/search/stores/b/brake/UK/execute.js
@@ -1,13 +1,42 @@
-
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
     country: 'UK',
     store: 'brake',
     domain: 'brake.co.uk',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.brake.co.uk',
+    loadedSelector: 'div.product__grid',
+    noResultsXPath: '//div[@class="search-empty"]',
     zipcode: '',
+  },
+  implementation: async function (inputs, parameters, context, dependencies) {
+    // Retailer website fails to correctly paginate throughout products with inputs containing '&' sign so we change every occurance of '&' to 'and'
+    await context.goto(parameters.url, {
+      timeout: 30000,
+      waitUntil: 'load',
+      checkBlocked: true,
+    });
+    await context.evaluate(function (inputs) {
+      Object.keys(inputs).forEach(function (key) {
+        inputs[key] = inputs[key].replace(/&/g, 'and');
+      });
+      document.querySelector('input.js-site-search-input').value = inputs.keywords;
+      document.querySelector('button.js_search_button').disabled = false;
+    }, inputs);
+    await context.clickAndWaitForNavigation(
+      'button.js_search_button',
+      {},
+      { timeout: 30000 },
+    );
+    if (parameters.loadedSelector) {
+      await context.waitForFunction(function (sel, xp) {
+        return document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
+      }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    }
+    return await context.evaluate(async function (xp) {
+      const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+      const e = r.iterateNext();
+      return !e;
+    }, parameters.noResultsXPath);
   },
 };
--- src/library/product/search/stores/b/brake/UK/extract.js
+++ src/library/product/search/stores/b/brake/UK/extract.js
@@ -1,11 +1,34 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'UK',
     store: 'brake',
-    transform: null,
+    transform,
     domain: 'brake.co.uk',
     zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    // selecting link node and modifying href attribute to paginate, instead of loading products using "Load more" button
+    const prepareForPagination = async function (context) {
+      await context.evaluate(async function () {
+        if (document.querySelector('head link[rel="next"]')) {
+          const linkNode = document.querySelector('head link[rel="next"]');
+          let paginationHref = linkNode.getAttribute('href');
+          paginationHref = paginationHref.replace(/(\/results?)/, '');
+          const regEx = /q=([^=]*):/;
+          const match = regEx.exec(paginationHref);
+          const escapedSearchTerm = escape(match[1]);
+          const reg = new RegExp(match[1]);
+          paginationHref = paginationHref.replace(reg, escapedSearchTerm);
+          linkNode.setAttribute('href', paginationHref);
+        }
+      });
+    };
+    await prepareForPagination(context);
+    return await context.extract(productDetails, { transform });
   },
 };
