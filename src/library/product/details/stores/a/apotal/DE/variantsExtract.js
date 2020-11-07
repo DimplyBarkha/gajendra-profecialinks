f9b85312c6b790d242d1a2aef2b8d7d905c5536c
Sagar B <sagar.bartakke@sz.import.io>
2020-11-08 01:46:37 +0530

    variant changes

--- src/library/product/details/stores/a/apotal/DE/variantsExtract.js
+++ src/library/product/details/stores/a/apotal/DE/variantsExtract.js
@@ -1,6 +1,71 @@
-
 const { transform } = require('../formatVariant.js');
-
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { variants } = dependencies;
+  var variantCount = await context.evaluate(async () => {
+    return (document.querySelectorAll('div.h3 a').length) ? document.querySelectorAll('div.h3 a').length : 1;
+  });
+  for (var i = 0; i < variantCount; i++) {
+    await preparePage(i + 1, variantCount, true);
+  // if (i !== variantCount - 1) { return await context.extract(variants, { type: 'APPEND' }); }
+  }
+  async function preparePage (index, variantCount) {
+    await context.evaluate(async (index, variantCount) => {
+      function addHiddenDiv (id, content, parentId = null) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        if (parentId && parentId.length) {
+          document.body.querySelector(`#${parentId}`).appendChild(newDiv);
+        } else {
+          newDiv.class = 'parent';
+          document.body.appendChild(newDiv);
+        }
+      }
+      async function infiniteScroll () {
+        let prevScroll = document.documentElement.scrollTop;
+        while (true) {
+          window.scrollBy(0, document.documentElement.clientHeight);
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          const currentScroll = document.documentElement.scrollTop;
+          if (currentScroll === prevScroll) {
+            break;
+          }
+          prevScroll = currentScroll;
+        }
+      }
+      await infiniteScroll();
+      function getSingleText (xpath, document) {
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        if (element && element.singleNodeValue) {
+          const nodeElement = element.singleNodeValue;
+          return nodeElement && nodeElement.textContent.trim().length > 1 ? nodeElement.textContent : '';
+        } else {
+          return '';
+        }
+      }
+      // Adding parent div
+      addHiddenDiv(`parent-${index}`, '');
+      if (variantCount > 1) {
+        const idXpath = '(//div[contains(@class,"similarProducts")]//div[contains(@class,"boxProductSimilar")]//a[@itemprop="url"]/@href)[' + index + ']';
+        addHiddenDiv('variant_id_text', getSingleText(idXpath, document), `parent-${index}`);
+        const variantUrlXpath = '(//div[contains(@class,"similarProducts")]//div[contains(@class,"boxProductSimilar")]//a[@itemprop="url"]/@href)[' + index + ']';
+        addHiddenDiv('variant_url_text', getSingleText(variantUrlXpath, document), `parent-${index}`);
+      } else {
+        const idXpath = '//link[@rel="canonical"]/@href';
+        addHiddenDiv('variant_id_text', getSingleText(idXpath, document), `parent-${index}`);
+        const variantUrlXpath = '//link[@rel="canonical"]/@href';
+        addHiddenDiv('variant_url_text', getSingleText(variantUrlXpath, document), `parent-${index}`);
+      }
+    }, index, variantCount);
+  }
+  return await context.extract(variants, { transform });
+}
 module.exports = {
   implements: 'product/details/variants/variantsExtract',
   parameterValues: {
@@ -10,4 +75,5 @@ module.exports = {
     domain: 'apotal.de',
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/details/stores/a/apotal/formatVariant.js
+++ src/library/product/details/stores/a/apotal/formatVariant.js
@@ -5,57 +5,14 @@
  */
 const transform = (data) => {
   for (const { group } of data) {
-    // var rank = 1;
     for (const row of group) {
       if (row.variantId) {
-        const variantIds = row.variantId.map((item) => {
-          return item.text.trim().substring(item.text.lastIndexOf('-') + 1);
-        });
-        if (variantIds.length > 1) {
-          variantIds.shift();
-        }
-        row.variantId = variantIds.map((item) => {
-          return { text: item, xpath: row.variantId[0].xpath };
-        });
-      }
-      if (row.variantUrl) {
-        const variantUrls = row.variantUrl.map((item) => {
-          return item.text.trim();
-        });
-        if (variantUrls.length > 1) {
-          variantUrls.shift();
-        }
-        row.variantUrl = variantUrls.map((item) => {
-          return { text: item, xpath: row.variantUrl[0].xpath };
-        });
+        const variantIds = row.variantId[0].text.trim().split('-');
+        console.log('variantIds ', variantIds);
+        row.variantId = [{ text: variantIds[variantIds.length - 1].toString(), xpath: row.variantId[0].xpath }];
       }
     }
   }
-
-  const clean = text =>
-    text
-      .toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-
-  data.forEach(obj =>
-    obj.group.forEach(row =>
-      Object.keys(row).forEach(header =>
-        row[header].forEach(el => {
-          el.text = clean(el.text);
-        }),
-      ),
-    ),
-  );
   return data;
 };
 
