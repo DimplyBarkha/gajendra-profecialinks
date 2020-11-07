067c3721d975d10581c07f9afd57fd472bd455d8
Wayne E <bartekstafiej2@gmail.com>
2020-11-07 15:42:41 +0100

    #1274 Added manually added catrgory and videos sources

--- src/library/product/details/stores/w/windeln/DE/extract.js
+++ src/library/product/details/stores/w/windeln/DE/extract.js
@@ -1,38 +1,32 @@
 async function implementation (inputs, parameters, context, dependencies) {
   const { productDetails } = dependencies;
 
-  // const variantsNumber = await context.evaluate(() => {
-  //   const variantLength = document.querySelectorAll('.variant-2-panel.active.scroll-pane>div').length;
-  //   return variantLength;
-  // });
+  await context.evaluate(() => {
+    const categorySelector = document.querySelector('.row.breadcrumbs-section').textContent;
+    const category = categorySelector.replace(/\b(\w*Zurück zu\w*)\b/, '');
 
-  for (let i = 0; i < 2; i++) { // variantsNumber.length;
-    await context.evaluate(() => {
-      const categorySelector = document.querySelector('.row.breadcrumbs-section').textContent;
-      const category = categorySelector.replace('Zurück zu:', '>');
+    document.querySelector('.row.breadcrumbs-section').setAttribute('category', category);
+  });
 
-      document.querySelector('.row.breadcrumbs-section').setAttribute('category', category);
-    });
+  await context.evaluate(() => {
+    const images = document.querySelectorAll('.slick-track>div>img');
+    let imageUrl;
+    let j = 0;
+    for (let i = 0; i < images.length; i++) {
+      imageUrl = images[i].src;
 
-    await context.evaluate(() => {
-      if (document.querySelector('.play-button') !== null) {
-        document.querySelector('.play-button').click();
-      }
-    });
+      if (imageUrl.includes('youtube.com')) {
+        const urlProp = imageUrl.replace('.jpg', '');
 
-    const nextVariant = await context.evaluate(() => {
-      return document.querySelectorAll('.variant-2-panel.active.scroll-pane>div')[i];
-    });
+        document.querySelectorAll('.media-wrapper.video-wrapper')[j].setAttribute('videoUrl', urlProp);
+        j++;
+      };
+    };
+  });
 
-    if (nextVariant === null) {
-      return await context.extract(productDetails);
-    } else {
-      await context.click(document.querySelectorAll('.variant-2-panel.active.scroll-pane>div')[i]);
-    }
-
-    await context.extract(productDetails);
-  }
+  await context.extract(productDetails);
 };
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
