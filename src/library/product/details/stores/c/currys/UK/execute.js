1e03be5e5edba9f9436acb9b3bd62e2bfe96e361
Gajender Ramola <ramola106@gmail.com>
2020-11-07 19:50:52 +0530

    #1331 initial commit

---
+++ src/library/product/details/stores/c/currys/UK/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'currys',
+    domain: 'currys.co.uk',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/c/currys/UK/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'currys',
+    transform: null,
+    domain: 'currys.co.uk',
+    zipcode: '',
+  },
+};
