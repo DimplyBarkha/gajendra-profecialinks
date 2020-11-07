088981dda78a3ad420babc9e3305af922284c975
Karina Miernik <karina.miernik@gmail.com>
2020-11-07 20:27:22 +0100

    #1152 added xpaths

--- src/library/product/details/stores/b/boxed/US/extract.js
+++ src/library/product/details/stores/b/boxed/US/extract.js
@@ -12,20 +12,29 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
     await context.evaluate(async function () {
+      const productDetails = document.querySelector('a[data-tab-key="productDetails"]');
+      // @ts-ignore
+      if (productDetails) productDetails.click();
+    });
+    await context.evaluate(async function () {
       // const zoom = document.querySelector('.magnifyframe');
 
       // if (zoom) {
       //   zoom.setAttribute('zoom', 'yes');
       // }
 
-      // function addElementToDocument (id, key, value) {
+      // function addElementToDocument (id, value) {
       //   const catElement = document.createElement('div');
       //   catElement.id = id;
-      //   catElement.setAttribute('value', key);
       //   catElement.innerText = value;
       //   catElement.style.display = 'none';
       //   document.body.appendChild(catElement);
       // };
+      // const nameExtendedNodes = document.querySelectorAll('div#description p')
+      //   ? document.querySelectorAll('div#description p') : [];
+      // // @ts-ignore
+      // const nameExtendedText = [...nameExtendedNodes].map(e => e.innerText).join('  ');
+      // addElementToDocument('nameExtended', nameExtendedText);
     });
 
     await context.extract(productDetails);
