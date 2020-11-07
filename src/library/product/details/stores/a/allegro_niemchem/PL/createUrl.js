33068d501a96907ac68152665c6fc44d4e3bf3f1
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-11-08 00:01:21 +0530

    created extractor and deployed

---
+++ src/library/product/details/stores/a/allegro_niemchem/PL/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'allegro.pl',
+    prefix: null,
+    url: 'https://allegro.pl/listing?string={id}',
+    country: 'PL',
+    store: 'allegro_niemchem',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/allegro_niemchem/PL/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'PL',
+    store: 'allegro_niemchem',
+    domain: 'allegro.pl',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
