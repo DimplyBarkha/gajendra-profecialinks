31b6304a5125d4cad51e6fb7a8d571d7103f232e
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-11-07 23:54:35 +0530

    added extractor for allegro lovela

---
+++ src/library/product/details/stores/a/allegro_lovela/PL/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'PL',
+    store: 'allegro_lovela',
+    domain: 'allegro.pl',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
