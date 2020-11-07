85161c85c50509a41b3e469b89bd089edad1ea73
unknown <shalini.kumari-odc@partner.import.io>
2020-11-07 23:21:16 +0530

    mvideo

--- src/library/product/search/stores/m/mvideo/RU/execute.js
+++ src/library/product/search/stores/m/mvideo/RU/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'RU',
     store: 'mvideo',
     domain: 'mvideo.ru',
-    url: 'https://www.mvideo.ru/product-list-page-cls?q={searchTerms}',
+    url: 'https://www.mvideo.ru/product-list-pa%D0%BD%D0%B0&region_id=1&limit=24&category_id=cat2_cis_0000000049',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
