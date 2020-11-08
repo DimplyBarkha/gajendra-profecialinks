f38bcb539ecf9629c9bc2ae7157de4851f67f4fe
ravitejaw324 <ravi.w@partner.import.io>
2020-11-08 09:58:04 +0530

    added a column videos

--- src/library/product/details/stores/m/melectronics/CH/extract.js
+++ src/library/product/details/stores/m/melectronics/CH/extract.js
@@ -7,5 +7,18 @@ module.exports = {
     transform,
     domain: 'melectronics.ch',
     zipcode: '',
-  },
+  }, implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    try {
+      await context.waitForSelector(".detail-showcase--additional-img-box img[alt='Video']");
+    } catch(er) {
+      console.log("No video selector");
+    }
+    await context.evaluate(async function() {
+      if(document.querySelector(".detail-showcase--additional-img-box img[alt='Video']")) {
+        document.querySelector(".detail-showcase--additional-img-box img[alt='Video']").click();
+        await new Promise((resolve, reject) => setTimeout(resolve, 4000));
+      }
+    })
+  await context.extract(productDetails);
+  }
 };
