9e943899d460e5dac2bc97be84be71515a40f20b
irfan shaikh <irfan.shaikh@synerzip.com>
2020-11-08 23:04:09 +0530

    #803. Updated api call to fecth more products and also update search.js file to get keyword and inputs.

--- src/library/product/search/stores/k/kauppahalli24/FI/extract.js
+++ src/library/product/search/stores/k/kauppahalli24/FI/extract.js
@@ -8,19 +8,19 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+  const { keywords, results } = inputs;
 
-  const response = await context.evaluate(async function () {
-    const searchTerm = document.querySelector('div[id*="pd_search_term"]');
-    if (searchTerm && searchTerm.innerText) {
-      return await fetch(`https://kauppahalli24_fi_api.frosmo.com/?queries=%5B%22${searchTerm.innerText}%22%5D&size=10&from=0&method=productSearch_100`)
+  const response = await context.evaluate(async function (keywords) {
+    if (keywords) {
+      return await fetch(`https://kauppahalli24_fi_api.frosmo.com/?queries=%5B%22${keywords}%22%5D&size=1500&from=0&method=productSearch_100`)
         .then(response => response.json())
         .catch(error => console.error('Error:', error));
     }
-  });
+  }, keywords);
   if (response && response.data[0] && response.data[0].products && response.data[0].products[0]) {
     console.log('Products Found!!!!');
     const products = response.data[0].products;
-    await context.evaluate(async function (products) {
+    await context.evaluate(async function ({ products, results, keywords }) {
       function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
@@ -37,14 +37,16 @@ async function implementation (
       }
       const l = products.length;
       for (let index = 0; index < l; index++) {
+        if (results && index === Number(results)) {
+          break;
+        }
         const rowId = `pd_div_${index}`;
         const element = products[index];
         if (!document.querySelector(rowId)) {
           addElementToDocument(`pd_div_${index}`, index);
         }
-        const searchTerm = document.querySelector('div[id*="pd_search_term"]');
-        if (searchTerm && searchTerm.innerText) {
-          const searchUrl = `https://kauppahalli24_fi_api.frosmo.com/?queries=%5B%22${searchTerm.innerText}%22%5D&size=10&from=0&method=productSearch_100`;
+        if (keywords) {
+          const searchUrl = `https://kauppahalli24_fi_api.frosmo.com/?queries=%5B%22${keywords}%22%5D&size=10&from=0&method=productSearch_100`;
           !document.querySelector('div[id*="search-url"]') && addElementToDocument('search-url', searchUrl);
         }
         addDataToDocument('id', element.sku, rowId);
@@ -53,7 +55,7 @@ async function implementation (
         addDataToDocument('pd_name', element.name, rowId);
         addDataToDocument('pd_thumbnail', element.thumbnail, rowId);
       }
-    }, products);
+    }, { products, results, keywords });
   }
   return await context.extract(productDetails, { transform });
 }
