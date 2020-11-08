5c461d16056630b4800bd2e6c4c4d2cba4c66788
Gajender Ramola <ramola106@gmail.com>
2020-11-08 22:40:16 +0530

    format keys

---
+++ src/library/product/details/stores/c/currys/UK/format.js
@@ -0,0 +1,85 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.alternateImages) {
+        row.alternateImages.splice(0, 1);
+        if (row.alternateImages.length === 0) {
+          delete row.alternateImages;
+        }
+      }
+      if (row.descriptionBullets) {
+        var bulletArr = [];
+        row.descriptionBullets.forEach(item => {
+          bulletArr.push(item.text.replace(/^\s*-\s*/, ''));
+        });
+        row.descriptionBullets = [{ text: '|| ' + bulletArr.join(' || ') }];
+      }
+      if (row.specifications) {
+        var arrSpecs = [];
+        row.specifications.forEach(item => {
+          item.text = item.text.replace(/\n\s+\n/, ':');
+          arrSpecs.push(item.text);
+        });
+        row.specifications = [{ text: arrSpecs.join(' || ') }];
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace(',', '');
+        });
+      }
+      if (row.listPrice) {
+        row.listPrice.forEach(item => {
+          item.text = item.text.replace(',', '');
+        });
+      }
+      if (row.variantCount) {
+        row.variantCount = [{ text: row.variantCount.length }];
+      }
+      if (row.variants) {
+        var arrTemp = [];
+        row.price.forEach(item => {
+          arrTemp.push(item.text);
+        });
+        row.variants = [{ text: arrTemp.join('|') }];
+      }
+      if (row.additionalDescBulletInfo) {
+        var arrBullets = [];
+        row.price.forEach(item => {
+          arrBullets.push(item.text);
+        });
+        row.additionalDescBulletInfo = [{ text: '||' + arrBullets.join('||') }];
+      }
+      if (row.aggregateRating) {
+        row.aggregateRating.forEach(item => {
+          item.text = (item.text * 5) / 10;
+        });
+      }
+    }
+  }
+  return cleanUp(data);
+};
+
+module.exports = { transform };
