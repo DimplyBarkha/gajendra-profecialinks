8851c6ffc249d71988de0b08bcb30e1abd6de38f
irfan shaikh <irfan.shaikh@synerzip.com>
2020-11-08 21:45:22 +0530

    #988. Created core extractor for elkjop NO.

--- src/library/navigation/goto/domains/el/elkjop.no.js
+++ src/library/navigation/goto/domains/el/elkjop.no.js
@@ -8,4 +8,22 @@ module.exports = {
     store: 'elkjop',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+    try {
+      await context.waitForSelector('div[id*="coiPage-1"] button[class*="coi-banner__accept"]');
+    } catch (error) {
+      console.log('cookie button not present');
+    }
+    try {
+      await context.click('div[id*="coiPage-1"] button[class*="coi-banner__accept"]');
+    } catch (error) {
+      console.log('cookie button click failed!');
+    }
+  },
 };
