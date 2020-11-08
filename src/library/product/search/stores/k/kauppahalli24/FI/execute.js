cc41fee971f0657279472e19ab275a36b1806d3e
irfan shaikh <irfan.shaikh@synerzip.com>
2020-11-08 23:04:09 +0530

    #803. Updated api call to fecth more products and also update search.js file to get keyword and inputs.

--- src/library/product/search/stores/k/kauppahalli24/FI/execute.js
+++ src/library/product/search/stores/k/kauppahalli24/FI/execute.js
@@ -1,42 +1,3 @@
-
-/**
- *
- * @param { { keywords: string, zipcode: string } } inputs
- * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { goto: ImportIO.Action} } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  console.log('params', parameters);
-  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
-  const { keywords } = inputs;
-  await dependencies.goto({ url, zipcode: inputs.zipcode });
-  await context.evaluate(async (keywords) => {
-    const catElement = document.createElement('div');
-    catElement.id = 'pd_search_term';
-    catElement.textContent = keywords;
-    catElement.style.display = 'none';
-    document.body.appendChild(catElement);
-  }, keywords);
-  if (parameters.loadedSelector) {
-    await context.waitForFunction(function (sel, xp) {
-      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
-  }
-  console.log('Checking no results', parameters.noResultsXPath);
-  return await context.evaluate(function (xp) {
-    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-    console.log(xp, r);
-    const e = r.iterateNext();
-    console.log(e);
-    return !e;
-  }, parameters.noResultsXPath);
-}
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -48,5 +9,4 @@ module.exports = {
     noResultsXPath: null,
     zipcode: '',
   },
-  implementation,
 };
--- src/library/product/search/stores/k/kauppahalli24/FI/search.js
+++ src/library/product/search/stores/k/kauppahalli24/FI/search.js
@@ -7,4 +7,25 @@ module.exports = {
     domain: 'kauppahalli24.fi',
     zipcode: '',
   },
+  implementation: async ({ keywords, Keywords, Brands, results = 150 }, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
+    // TODO: consider moving this to a reusable function
+    const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
+
+    keywords = (Keywords) || (Brands) || (keywords);
+    console.log('zip:' + zipcode);
+    // do the search
+    const resultsReturned = await execute({ keywords, zipcode });
+
+    if (!resultsReturned) {
+      console.log('No results were returned');
+      return;
+    }
+
+    // try gettings some search results
+    const pageOne = await extract({ keywords, results });
+
+    const collected = length(pageOne);
+
+    console.log('Got initial number of results', collected);
+  },
 };
