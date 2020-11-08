529cc6bafb256497d1080519cf28ed08b8035f7a
irfan shaikh <irfan.shaikh@synerzip.com>
2020-11-08 21:46:29 +0530

    #988. Added variant extractor logic for elkjop NO.

---
+++ src/library/product/details/stores/e/elkjop/NO/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'NO',
+    store: 'elkjop',
+    domain: 'elkjop.no',
+    zipcode: '',
+  },
+};
