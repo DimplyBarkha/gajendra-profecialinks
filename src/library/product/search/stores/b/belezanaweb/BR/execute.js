01da7426b44fcb2ecde98284ab084cdc3d215fee
ganesh <ganesh@profecialinks.com>
2020-11-09 22:11:56 +0530

    fix image issue

--- src/library/product/search/stores/b/belezanaweb/BR/execute.js
+++ src/library/product/search/stores/b/belezanaweb/BR/execute.js
@@ -1,4 +1,57 @@
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
 
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+}
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -10,4 +63,5 @@ module.exports = {
     noResultsXPath: "//h1[@class='content-page-title content-page-title-list' and contains(.,'não possui resultados')]",
     zipcode: '',
   },
+  implementation,
 };
