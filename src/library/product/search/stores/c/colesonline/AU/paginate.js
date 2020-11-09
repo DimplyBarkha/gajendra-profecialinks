a06c869aa198f8cb5bc213a17e8ab0df35a336f6
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-09 17:55:35 +0530

    Search location set AU #1026

--- src/library/product/search/stores/c/colesonline/AU/paginate.js
+++ src/library/product/search/stores/c/colesonline/AU/paginate.js
@@ -1,4 +1,67 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { keywords, page, offset } = inputs;
+  const { nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, spinnerSelector, openSearchDefinition } = parameters;
 
+  if (nextLinkSelector) {
+    const hasNextLink = await context.evaluate((selector) => !!document.querySelector(selector), nextLinkSelector);
+    if (!hasNextLink) {
+      return false;
+    }
+  }
+
+  const { pager } = dependencies;
+  const success = await pager({ keywords, nextLinkSelector, loadedSelector, mutationSelector, spinnerSelector });
+  if (success) {
+    return true;
+  }
+
+  let url = await context.evaluate(function () {
+    /** @type { HTMLLinkElement } */
+    const next = document.querySelector('head link[rel="next"]');
+    if (!next) {
+      return false;
+    }
+    return next.href;
+  });
+
+  if (!url && openSearchDefinition) {
+    url = openSearchDefinition.template
+      .replace('{searchTerms}', encodeURIComponent(keywords))
+      .replace('{page}', (page + (openSearchDefinition.pageOffset || 0)).toString())
+      .replace('{offset}', (offset + (openSearchDefinition.indexOffset || 0)).toString());
+  }
+
+  if (!url) {
+    return false;
+  }
+
+  console.log('Going to url', url);
+  // await dependencies.goto({ url });
+  await context.goto(url, {
+    firstRequestTimeout: 60000,
+    timeout: 60000,
+    waitUntil: 'load',
+    checkBlocked: false,
+  });
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
@@ -10,9 +73,10 @@ module.exports = {
     loadedSelector: null,
     noResultsXPath: null,
     openSearchDefinition: {
-      template: 'https://shop.coles.com.au/a/national/everything/search/{searchTerms}?pageNumber={page}',
+      template: 'https://shop.coles.com.au/a/alexandria/everything/search/{searchTerms}?pageNumber={page}',
     },
     domain: 'shop.coles.com.au',
     zipcode: '',
   },
+  implementation,
 };
