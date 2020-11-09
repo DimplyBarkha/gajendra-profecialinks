a9cff740b6bdc5d8575693216472e24e3ef40f03
unknown <ramraut25@gmail.com>
2020-11-09 09:52:01 +0530

    Updated extract.yaml with newer xpaths

--- src/library/product/search/stores/a/acehardware/UAE/execute.js
+++ src/library/product/search/stores/a/acehardware/UAE/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'acehardware',
     domain: 'aceuae.com',
     url: 'https://www.aceuae.com/en-AE/search/?q={searchTerms}',
-    loadedXPath: '//ul[contains(@class,"js-products-grid")]/li//picture/img',
-    noResultsXPath: '//span[@class="b-noresult-banner__title"]',
+    loadedXPath: '//div[contains(@class,"row-cols")]/div[@class="col"]',
+    noResultsXPath: '//div[@class="search-no-result"]',
     zipcode: '',
   },
 };
