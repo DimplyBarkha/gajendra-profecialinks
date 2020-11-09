e6eb057e8efca616ae51f402701aa03562c194c9
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-09 13:46:41 +0530

    devwhiskydetails

--- src/library/navigation/goto/domains/wh/whisky.de.js
+++ src/library/navigation/goto/domains/wh/whisky.de.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'whisky.de',
-    timeout: 80000,
+    timeout: 10000,
     country: 'DE',
     store: 'whisky',
     zipcode: '',
