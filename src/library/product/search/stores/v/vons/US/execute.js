8df704b5a027aebee7d0a19d9eeb97529dc58bce
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-09 14:53:10 +0530

    devvonssearch

--- src/library/product/search/stores/v/vons/US/execute.js
+++ src/library/product/search/stores/v/vons/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'vons',
     domain: 'vons.com',
-    url: 'https://www.vons.com/shop/search-results.html?q={searchTerms}',
+    url: 'https://www.vons.com/shop/search-results.html?q=%22IPA%20beer%22',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
