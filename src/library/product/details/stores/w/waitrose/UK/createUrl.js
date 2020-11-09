bbfc4983c2e1210000f820abdce8cf087cbcfc73
astropaula <wojciechowska.fran@gmail.com>
2020-11-09 19:37:06 +0100

    Setup extractor, updated xpaths in extract.yaml

--- src/library/product/details/stores/w/waitrose/UK/createUrl.js
+++ src/library/product/details/stores/w/waitrose/UK/createUrl.js
@@ -3,9 +3,10 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'waitrose.com',
-    prefix: 'search',
+    prefix: null,
     country: 'UK',
     store: 'waitrose',
+    url: 'https://www.waitrose.com/ecom/shop/search?&searchTerm={id}',
     zipcode: '',
   },
 };
--- src/library/product/details/stores/w/waitrose/UK/extract.js
+++ src/library/product/details/stores/w/waitrose/UK/extract.js
@@ -1,10 +1,40 @@
+const { cleanUp } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'UK',
     store: 'waitrose',
-    transform: null,
+    transform: cleanUp,
     domain: 'waitrose.com',
+    zipcode: '',
   },
+  implementation: async (
+    { inputString },
+    { country, domain },
+    context,
+    { productDetails }) => {
+    var detailsPage = await context.evaluate(async () => {
+      if (document.querySelector('a[data-origincomponent="ProductPod"]') != null) {
+        var productLink = document.querySelector('a[data-origincomponent="ProductPod"]').getAttribute('href');
+      }
+      return productLink;
+    });
+    if (detailsPage) {
+      await context.goto('https://www.waitrose.com/' + detailsPage);
+    }
+
+    //remove cookies popup
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    await context.evaluate(async function () {
+      const cookies = document.querySelector('div.acceptCookieCTA___NwqHh button');
+      if (cookies) cookies.click();
+    });
+
+    // await context.waitForNavigation();
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    await context.extract(productDetails);
+  },
+
 };
