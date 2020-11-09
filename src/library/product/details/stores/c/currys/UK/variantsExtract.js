9e2e358208950319b1b645042fcd366e797d68b1
Gajender Ramola <ramola106@gmail.com>
2020-11-09 12:50:15 +0530

    #1337 addign variant functionality

---
+++ src/library/product/details/stores/c/currys/UK/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'UK',
+    store: 'currys',
+    transform: null,
+    domain: 'currys.co.uk',
+    zipcode: '',
+  },
+};
