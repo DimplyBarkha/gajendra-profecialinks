078a8ba995f0c1e189e7efe3de3b1e16dc5aed2e
Nishant Sinha <nishantsinha1197@gmail.com>
2020-11-09 10:00:07 +0530

    Committing

--- src/library/product/search/stores/a/alloffice/SE/execute.js
+++ src/library/product/search/stores/a/alloffice/SE/execute.js
@@ -8,7 +8,7 @@ async function implementation (
   const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
   const responseStatus = await context.goto(url, {
     firstRequestTimeout: 60000,
-    timeout: 60000,
+    timeout: 50000,
     waitUntil: 'load',
     checkBlocked: false,
   });
@@ -16,24 +16,42 @@ async function implementation (
   console.log('Status :', responseStatus.status);
   console.log('URL :', responseStatus.url);
 
+  // Check if accept cookies dialog pops up
+  /*const doesAcceptCookiesBtnExists = await context.evaluate(function () {if(document.evaluate(`//button[contains(.,'Jag godkänner')]`, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext()) {
+    document.evaluate(`//button[contains(.,'Jag godkänner')]`, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext().click()
+    }
+  });
+
+  if (doesAcceptCookiesBtnExists) {
+    console.log('Clicking on accept cookies btn');
+    await context.click(`//button[contains(.,'Jag godkänner')]`);
+    await context.waitForNavigation();
+  }*/
+  await context.evaluate(function () {if(document.evaluate(`//button[contains(.,'Jag godkänner')]`, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext()) {
+    document.evaluate(`//button[contains(.,'Jag godkänner')]`, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext().click()
+    }
+  });
+ 
   
+
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
     }, { timeout: 50000 }, parameters.loadedSelector, parameters.noResultsXPath);
   }
-
   // Apply scroll
   const applyScroll = async function (context) {
     await context.evaluate(async function () {
       let scrollTop = 0;
       while (scrollTop !== 20000) {
-        const products = document.evaluate('//div[@id="content-container"]/main/div[2]/div[1]/div[2]/div/div/div//a/div//img', document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const products = document.evaluate('//div[@id="content-container"]//img/@src', document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
         const productsCount = products.snapshotLength;
-        scrollTop += 1000;
+        console.log('Length: ' + productsCount);
+        await stall(5000);
+        scrollTop += 500;
         window.scroll(0, scrollTop);
         if (scrollTop === 20000 || productsCount > 160) {
-          await stall(10000);
+          await stall(5000);
           break;
         }
       }
@@ -46,9 +64,7 @@ async function implementation (
       }
     });
   };
-
   await applyScroll(context);
-
   console.log('Checking no results', parameters.noResultsXPath);
   return await context.evaluate(function (xp) {
     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
@@ -65,8 +81,8 @@ module.exports = {
     store: 'alloffice',
     domain: 'alloffice.se',
     url: 'https://www.alloffice.se/sok?q={searchTerms}&count=150',
-    loadedSelector: 'div>img',
-    noResultsXPath: '//div[@id="content-container"]',
+    loadedSelector: 'a[data-test-id="product-link"]',
+    noResultsXPath: '//div[contains(.,"sökning gav inga träffar")]',
     zipcode: '',
   },
   implementation,
