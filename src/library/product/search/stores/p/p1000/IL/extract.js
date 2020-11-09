89dfbf0ae948f20999e1691865427c987f39575e
Sheetal Bharara <SheetalB@hcl.com>
2020-11-09 15:41:10 +0530

    QA1 fixes

--- src/library/product/search/stores/p/p1000/IL/extract.js
+++ src/library/product/search/stores/p/p1000/IL/extract.js
@@ -1,11 +1,11 @@
 
-const { cleanUp } = require('../../../../shared');
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'IL',
     store: 'p1000',
-    transform: cleanUp,
+    transform: transform,
     domain: 'p1000.co.il',
     zipcode: '',
   },
