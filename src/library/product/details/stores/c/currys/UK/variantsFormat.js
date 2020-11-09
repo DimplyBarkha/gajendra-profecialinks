f896617c8dee83a0ee2890399b63789c4abc399a
Gajender Ramola <ramola106@gmail.com>
2020-11-09 15:04:58 +0530

    variant fixes

--- src/library/product/details/stores/c/currys/UK/variantsFormat.js
+++ src/library/product/details/stores/c/currys/UK/variantsFormat.js
@@ -4,24 +4,33 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const variations = [];
   for (const { group } of data) {
     for (const row of group) {
       if (row.variants) {
         var scriptJSON = JSON.parse(row.variants[0].text);
         if (scriptJSON.productVariants) {
           var objectsInVariants = Object.keys(scriptJSON.productVariants).length;
-          var varientIds = [];
           for (var i = 0; i < objectsInVariants; i++) {
             var keyName = Object.keys(scriptJSON.productVariants)[i];
             var variants = scriptJSON.productVariants[keyName].variants;
             variants.forEach(function (item, index) {
-              varientIds.push(item.fupid);
+              const tmpVariations = {};
+              tmpVariations['variantId'] = [];
+              tmpVariations['variantUrl'] = [];
+              tmpVariations['variant'] = [];
+              tmpVariations['variantId'].push({ text: item.fupid });
+              tmpVariations['variantUrl'].push({ text: item.product_url });
+              tmpVariations['variant'].push({ text: item.variant });
+              variations.push(tmpVariations);
             });
           }
         }
-        row.variants = [{ text: varientIds.join(' | ') }];
       }
     }
+  }
+  if (variations.length) {
+    data[0].group = variations;
   }
   return data;
 };
