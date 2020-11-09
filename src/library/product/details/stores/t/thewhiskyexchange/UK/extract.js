00da3ff48dff4b81774777f43345044ef1b31570
byQ96 <michal126@hotmail.com>
2020-11-09 12:11:57 +0100

    fixed all QA issues

--- src/library/product/details/stores/t/thewhiskyexchange/UK/extract.js
+++ src/library/product/details/stores/t/thewhiskyexchange/UK/extract.js
@@ -6,15 +6,13 @@ async function implementation (inputs, parameters, context, dependencies) {
 
   await context.evaluate(async () => {
     var prodImg = document.querySelector(
-      'div[class="product-main__image-container"] img[class="product-slider__image lazyloaded"]',
-    );
+      'div[class="product-main__image-container"] img[class="product-slider__image lazyloaded"]');
     var prodImgAttr;
     var prodContainer;
     if (prodImg !== null) {
       prodImgAttr = prodImg.getAttribute('src');
       prodContainer = document.querySelector(
-        'div[class="product-main__image-container"]',
-      );
+        'div[class="product-main__image-container"]');
       prodContainer.setAttribute('img_url', prodImgAttr);
     } else {
       prodImgAttr = document
@@ -25,6 +23,18 @@ async function implementation (inputs, parameters, context, dependencies) {
       );
       prodContainer.setAttribute('img_url', prodImgAttr);
     }
+
+    // more images
+    var moreImgs = document.querySelectorAll(
+      'div[class="product-slider__navigation"] img');
+    const prefix = 'https://thewhiskyexchange.com';
+    moreImgs.forEach((element, index) => {
+      if (index > 0) {
+        element.setAttribute(
+          'otherimgs',
+          prefix + element.getAttribute('data-original'));
+      }
+    });
 
     // join facts into description
     var facts = document.querySelectorAll('li[class="product-facts__item"]');
@@ -43,34 +53,23 @@ async function implementation (inputs, parameters, context, dependencies) {
     elem.style.display = 'none';
     document.body.appendChild(elem);
 
-    // join shipping info
-    var deliveries = document.querySelectorAll(
-      'li[class="product-shipping__item"]',
-    );
-    var deliveryInfo = '';
-    deliveries.forEach((element) => {
-      deliveryInfo += element.children[0].textContent.concat(
-        ': ',
-        element.children[1].textContent,
-        '\n',
-      );
-    });
-
-    var elem2 = document.createElement('div');
-    elem2.classList.add('deliveryInfo');
-    elem2.innerText = deliveryInfo;
-    elem2.style.display = 'none';
-    document.body.appendChild(elem2);
-
     // filter breadcrumbs
     var breadcrumps = document.querySelectorAll(
-      'div[class="breadcrumb"] li[class="breadcrumb__item"] a',
-    );
+      'div[class="breadcrumb"] li[class="breadcrumb__item"] a');
     breadcrumps.forEach((element, index) => {
       if (index > 0) {
         element.setAttribute('category', element.textContent);
       }
     });
+
+    // convert priceperunit unit
+    const unitElem = document.querySelector("p[class='product-action__unit-price']");
+    if (unitElem) {
+      const text = unitElem.textContent;
+      const regexpUnit = /per ([a-z0-9]+)/;
+      const result = text.match(regexpUnit);
+      unitElem.setAttribute('unit', result[1].toString());
+    }
   });
 
   return await context.extract(productDetails, { transform });
