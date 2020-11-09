bc6f6ad0d5a8c3f9677319af4512fcbc21028dfd
unknown <suryakanta.s@osmosys.asia>
2020-11-09 21:27:47 +0530

    lint fix

--- src/library/product/details/stores/a/apotekhjartat/SE/createUrl.js
+++ src/library/product/details/stores/a/apotekhjartat/SE/createUrl.js
@@ -1,11 +1,11 @@
 module.exports = {
-    implements: 'product/details/createUrl',
-    parameterValues: {
-        domain: 'apotekhjartat.se',
-        prefix: null,
-        url: 'https://www.apotekhjartat.se/soksida/?query={id}',
-        country: 'SE',
-        store: 'apotekhjartat',
-        zipcode: '',
-    },
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'apotekhjartat.se',
+    prefix: null,
+    url: 'https://www.apotekhjartat.se/soksida/?query={id}',
+    country: 'SE',
+    store: 'apotekhjartat',
+    zipcode: '',
+  },
 };
--- src/library/product/details/stores/a/apotekhjartat/SE/extract.js
+++ src/library/product/details/stores/a/apotekhjartat/SE/extract.js
@@ -1,35 +1,35 @@
 const { cleanUp } = require('../../../../shared');
 module.exports = {
-    implements: 'product/details/extract',
-    parameterValues: {
-        country: 'SE',
-        store: 'apotekhjartat',
-        transform: cleanUp,
-        domain: 'apotekhjartat.se',
-        zipcode: '',
-    },
-    implementation: async(
-        inputs,
-        parameters,
-        context,
-        dependencies,
-    ) => {
-        await context.evaluate(() => {
-            let a = document.evaluate('//ul[@class="slides"]/li[not(contains(@class,"clone")) and (position()>2)]/img', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null)
-            let b = a.iterateNext();
-            let c = [];
-            while (b !== null) {
-                c.push(b.getAttribute("src").split('?')[0]);
-                b = a.iterateNext();
-            }
-            const div = document.createElement('div');
-            div.setAttribute("id", "alt-img");
-            div.innerText = c.join(' | ');
-            document.body.append(div);
-        });
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'SE',
+    store: 'apotekhjartat',
+    transform: cleanUp,
+    domain: 'apotekhjartat.se',
+    zipcode: '',
+  },
+  implementation: async (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(() => {
+      const a = document.evaluate('//ul[@class="slides"]/li[not(contains(@class,"clone")) and (position()>2)]/img', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+      let b = a.iterateNext();
+      const c = [];
+      while (b !== null) {
+        c.push(b.getAttribute('src').split('?')[0]);
+        b = a.iterateNext();
+      }
+      const div = document.createElement('div');
+      div.setAttribute('id', 'alt-img');
+      div.innerText = c.join(' | ');
+      document.body.append(div);
+    });
 
-        const { transform } = parameters;
-        const { productDetails } = dependencies;
-        return await context.extract(productDetails, { transform });
-    },
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+  },
 };
