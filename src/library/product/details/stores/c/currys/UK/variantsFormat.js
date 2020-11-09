4d8df12ec05763ed79841a3b8be66499c201cc23
Gajender Ramola <ramola106@gmail.com>
2020-11-09 17:07:17 +0530

    #1337 variant fix

--- src/library/product/details/stores/c/currys/UK/variantsFormat.js
+++ src/library/product/details/stores/c/currys/UK/variantsFormat.js
@@ -7,8 +7,8 @@ const transform = (data) => {
   const variations = [];
   for (const { group } of data) {
     for (const row of group) {
-      if (row.variants) {
-        var scriptJSON = JSON.parse(row.variants[0].text);
+      if (row.variantId) {
+        var scriptJSON = JSON.parse(row.variantId[0].text);
         if (scriptJSON.productVariants) {
           var objectsInVariants = Object.keys(scriptJSON.productVariants).length;
           for (var i = 0; i < objectsInVariants; i++) {
@@ -16,12 +16,12 @@ const transform = (data) => {
             var variants = scriptJSON.productVariants[keyName].variants;
             variants.forEach(function (item, index) {
               const tmpVariations = {};
-              tmpVariations['variantId'] = [];
-              tmpVariations['variantUrl'] = [];
-              tmpVariations['variant'] = [];
-              tmpVariations['variantId'].push({ text: item.fupid });
-              tmpVariations['variantUrl'].push({ text: item.product_url });
-              tmpVariations['variant'].push({ text: item.variant });
+              tmpVariations.variantId = [];
+              tmpVariations.variantUrl = [];
+              tmpVariations.variant = [];
+              tmpVariations.variantId.push({ text: item.fupid });
+              tmpVariations.variantUrl.push({ text: item.product_url });
+              tmpVariations.variant.push({ text: item.variant });
               variations.push(tmpVariations);
             });
           }
@@ -29,9 +29,7 @@ const transform = (data) => {
       }
     }
   }
-  if (variations.length) {
-    data[0].group = variations;
-  }
+  data[0].group = variations;
   return data;
 };
 
