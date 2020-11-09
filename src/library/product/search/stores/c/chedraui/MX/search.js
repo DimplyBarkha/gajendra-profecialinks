8c43c6d4924076a4d46c2a6acacb887faf79b757
rajeev <rajeev.ranjan@synerzip.com>
2020-11-09 19:11:18 +0530

    #1285 # 1286 search and core checking

---
+++ src/library/product/search/stores/c/chedraui/MX/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'MX',
+    store: 'chedraui',
+    domain: 'chedraui.com.mx',
+    zipcode: "''",
+  },
+};
