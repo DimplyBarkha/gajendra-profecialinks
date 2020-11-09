3aebf843a9212299863960557d8557bf8c2d3169
IpsitaM <iamipsita07@gmail.com>
2020-11-09 20:30:29 +0530

    Added some changes to core

--- src/library/navigation/goto/domains/al/albertsons.com.js
+++ src/library/navigation/goto/domains/al/albertsons.com.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
     implements: 'navigation/goto',
     parameterValues: {
         domain: 'albertsons.com',
@@ -11,30 +10,10 @@ module.exports = {
     implementation: async({ url, zipcode, storeId }, parameters, context, dependencies) => {
         const timeout = parameters.timeout ? parameters.timeout : 10000;
         await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
-        await new Promise(resolve => { setTimeout(resolve, 50000) })
+        await new Promise(resolve => { setTimeout(resolve, 50000); });
         console.log(zipcode);
         if (zipcode) {
             await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
         }
     },
 };
-=======
-  implements: 'navigation/goto',
-  parameterValues: {
-    domain: 'albertsons.com',
-    timeout: 50000,
-    country: 'US',
-    store: 'albertsons',
-    zipcode: '83642',
-  },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
-    await new Promise(resolve => { setTimeout(resolve, 50000); });
-    console.log(zipcode);
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-    }
-  },
-};
->>>>>>> d99fe3f32a749d105192af745c8b54cb160f9817
--- src/library/navigation/goto/domains/al/setZipCode.albertsons.com.js
+++ src/library/navigation/goto/domains/al/setZipCode.albertsons.com.js
@@ -1,36 +1,34 @@
-<<<<<<< HEAD
 async function implementation(
     inputs,
     parameters,
     context,
     dependencies,
 ) {
-    const { url, zipcode } = inputs;
-    const { country, domain, store } = parameters;
-    await context.evaluate(async(context) => {
+    const { zipcode } = inputs;
+    await context.evaluate(async() => {
         const changeButton = document.querySelector('button[id="openFulfillmentModalButton"][class*="button"]');
         if (changeButton) {
             console.log('entered if block');
             changeButton.click();
             console.log('button clicked sucessfully');
-            await new Promise(resolve => { setTimeout(resolve, 30000) })
+            await new Promise(resolve => { setTimeout(resolve, 30000); });
         }
         console.log('sucessfully clicked the change button');
-    })
+    });
     await context.setInputValue('input[aria-labelledby="zipcode"]', `${zipcode}`);
     await context.evaluate(async() => {
         const searchButton = document.querySelector('span[aria-label*="Zipcode"]');
         if (searchButton) {
-            searchButton.click()
-            console.log('able to click the button')
-            await new Promise(resolve => { setTimeout(resolve, 30000) })
+            searchButton.click();
+            console.log('able to click the button');
+            await new Promise(resolve => { setTimeout(resolve, 30000); });
         }
         const selectButton = document.querySelector('div[class="card-wrapper"]>div[class="card-store"]:nth-child(1) a[role="button"]');
         if (selectButton) {
             selectButton.click();
         }
-        await new Promise(resolve => { setTimeout(resolve, 30000) });
-    })
+        await new Promise(resolve => { setTimeout(resolve, 30000); });
+    });
 }
 module.exports = {
     implements: 'navigation/goto/setZipCode',
@@ -42,47 +40,3 @@ module.exports = {
     },
     implementation,
 };
-=======
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { zipcode } = inputs;
-  await context.evaluate(async () => {
-    const changeButton = document.querySelector('button[id="openFulfillmentModalButton"][class*="button"]');
-    if (changeButton) {
-      console.log('entered if block');
-      changeButton.click();
-      console.log('button clicked sucessfully');
-      await new Promise(resolve => { setTimeout(resolve, 30000); });
-    }
-    console.log('sucessfully clicked the change button');
-  });
-  await context.setInputValue('input[aria-labelledby="zipcode"]', `${zipcode}`);
-  await context.evaluate(async () => {
-    const searchButton = document.querySelector('span[aria-label*="Zipcode"]');
-    if (searchButton) {
-      searchButton.click();
-      console.log('able to click the button');
-      await new Promise(resolve => { setTimeout(resolve, 30000); });
-    }
-    const selectButton = document.querySelector('div[class="card-wrapper"]>div[class="card-store"]:nth-child(1) a[role="button"]');
-    if (selectButton) {
-      selectButton.click();
-    }
-    await new Promise(resolve => { setTimeout(resolve, 30000); });
-  });
-}
-module.exports = {
-  implements: 'navigation/goto/setZipCode',
-  parameterValues: {
-    country: 'US',
-    domain: 'albertsons.com',
-    store: 'albertsons',
-    zipcode: '83642',
-  },
-  implementation,
-};
->>>>>>> d99fe3f32a749d105192af745c8b54cb160f9817
