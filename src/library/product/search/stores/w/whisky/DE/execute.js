79891c5b7e8d56746d11312c943a91eb91c44055
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-10 01:18:21 +0530

    devwhiskysearch

--- src/library/product/search/stores/w/whisky/DE/execute.js
+++ src/library/product/search/stores/w/whisky/DE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DE',
     store: 'whisky',
     domain: 'whisky.de',
-    url: 'https://www.whisky.de/shop/index.php?cl=search&searchparam=Scotch',
+    url: 'https://www.whisky.de/shop/index.php?cl=search&searchparam={searchTerms}',
     loadedSelector:'body',
     noResultsXPath: null,
     zipcode: '',
