9541033d7a39a0ca10ace8fdc189f02354074dfc
madhanimportio <madhan.selvaraj-odc@partner.import.io>
2020-11-10 08:53:31 +0530

    changes in details extractor yaml file

--- src/library/product/search/stores/r/rs-online/UK/extract.js
+++ src/library/product/search/stores/r/rs-online/UK/extract.js
@@ -7,6 +7,60 @@ module.exports = {
     transform: cleanUp,
     domain: 'uk.rs-online.com',
     zipcode: '',
-  },
+  }, 
+ implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addclass(xpathforpagination) {
+      var elems = document.querySelectorAll(xpathforpagination);
+      elems[0].classList.add('pagination');
+    }
 
+    //for rank
+    function addHiddenDiv(id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll('tr[class="resultRow"]')[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    let rankOrganic;
+    let url = window.location.href;
+    let checkPageNumber = url.split('&')[3];
+    try {
+      if (checkPageNumber.startsWith('pn=')) {
+        rankOrganic = checkPageNumber.replace('pn=', '');
+      }
+    }
+    catch (err) {
+    }
+
+
+    var dup = Number(rankOrganic);
+    dup = dup - 1; 
+
+    if (!rankOrganic) {
+      rankOrganic = 1;
+    } else {
+      rankOrganic = (dup * 20) + 1;
+    }
+    // const urlProduct = document.querySelectorAll('tr[class="resultRow"]');
+    for (let i = 1; i < 21; i++) {
+      addHiddenDiv('rankOrganic', rankOrganic++, i);
+    }
+
+  });
+  //rank end
+
+
+  return await context.extract(productDetails, { transform });
+}
