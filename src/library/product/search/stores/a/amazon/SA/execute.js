5c42fc6f194f485623df51df211f3f14e5317e1b
dubok1195 <dubovyk1195@gmail.com>
2020-11-10 19:11:46 +0200

    #1196 add changes

--- src/library/product/search/stores/a/amazon/SA/execute.js
+++ src/library/product/search/stores/a/amazon/SA/execute.js
@@ -4,9 +4,9 @@ module.exports = {
     country: 'US',
     store: 'amazon',
     domain: 'amazon.sa',
-    url: 'https://www.amazon.sa/s?k={searchTerms}&ref=nb_sb_noss',
-    loadedSelector: 'section.ebx-empathy-x__body',
-    noResultsXPath: '//span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")]',
+    url: 'https://www.amazon.sa/s?k={searchTerms}',
+    loadedSelector: null,
+    noResultsXPath: null,
     zipcode: '',
   },
 };
