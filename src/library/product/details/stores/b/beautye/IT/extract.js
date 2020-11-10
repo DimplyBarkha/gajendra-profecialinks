1103ea1028fb5000c331273d77f9da0dc3c05bc0
PR <pravinr@synerzipune.local>
2020-11-10 23:28:56 +0530

    beauty IT bug fixes
    rebase

--- src/library/product/details/stores/b/beautye/IT/extract.js
+++ src/library/product/details/stores/b/beautye/IT/extract.js
@@ -9,40 +9,6 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
   await new Promise((resolve, reject) => setTimeout(resolve, 20000));
-  // await context.evaluate(async () => {
-  //   function addHiddenDiv (id, content) {
-  //     const newDiv = document.createElement('div');
-  //     newDiv.id = id;
-  //     newDiv.textContent = content;
-  //     newDiv.style.display = 'none';
-  //     document.body.appendChild(newDiv);
-  //   }
-
-  //   const parent = document.evaluate('//div[contains(@class, "product data") and contains(@role,"tablist")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  //   if (parent) {
-  //     const children = parent.children;
-  //     const headersArr = []; 
-  //     const textArr = []; 
-  //     for (let i = 0; i < children.length; i++) {
-  //       // const text = children[i].textContent;
-  //       if (i %2 === 0) {
-  //         const header = children[i].children[0].children[0].textContent;
-  //         if (header.indexOf('Consigli') > -1) {
-  //           const adviceTxt = children[i+1].textContent.trim();
-  //           addHiddenDiv('my-info-direction', adviceTxt);
-  //         } else if (header.indexOf('Ingredienti') > -1) {
-  //           const IngredientTxt = children[i+1].textContent.trim();
-  //           addHiddenDiv('my-info-ingredient', IngredientTxt);
-  //         } else if (header.indexOf('Descrizione') > -1) {
-  //           const descTxt = children[i+1].textContent.trim();
-  //           addHiddenDiv('my-info-desc', descTxt);
-  //         }
-  //       }
-  //     }
-  //     return [`Children count is ${children.length}`, headersArr, textArr];
-  //   }
-  // });
-  await new Promise((resolve, reject) => setTimeout(resolve, 1000));
   var variantLength = await context.evaluate(async () => {
     return (document.querySelectorAll('div.swatch-attribute-options div.swatch-option')) ? document.querySelectorAll('div.swatch-attribute-options div.swatch-option').length : 0;
   });
@@ -50,12 +16,13 @@ async function implementation (
   if (variantLength > 1) {
     for (let j = 0; j < variantLength; j++) {
       try {
-        try {
-          await context.evaluate( (j) => {
-            return document.querySelectorAll('div.swatch-option')[j].click();
-          }, j);
-          console.log('Inside variants', j);
-        } catch (err) {}
+        await context.evaluate( (j) => {
+          return document.querySelectorAll('div.swatch-option')[j].click();
+        }, j);
+        // await context.click('div.swatch-option div.custom-option-bg');
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+        console.log('Inside variants', j);
+        // await preparePage(j, variantLength);
         if (j !== variantLength - 1) { await context.extract(productDetails, { transform }, { type: 'APPEND' }); }
       } catch (err) {}
     }
