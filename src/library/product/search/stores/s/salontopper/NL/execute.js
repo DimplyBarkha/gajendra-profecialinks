29fa9c234380e9a4e9e9805e03e6d6eedd5afefa
Karina Miernik <karina.miernik@gmail.com>
2020-11-10 15:33:19 +0100

    added xpaths and selectors

--- src/library/product/search/stores/s/salontopper/NL/execute.js
+++ src/library/product/search/stores/s/salontopper/NL/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'NL',
     store: 'salontopper',
     domain: 'salontopper.nl',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.salontopper.nl/zoeken?q={searchTerms}',
+    loadedSelector: 'div[class*="product-gallery"]',
+    noResultsXPath: '//p[contains(text(), "zijn er geen")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/s/salontopper/NL/extract.js
+++ src/library/product/search/stores/s/salontopper/NL/extract.js
@@ -1,11 +1,41 @@
+const { transform } = require('../../../../shared');
+
+async function implementation (inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(() => {
+    // function addElementToDocument (id, value) {
+    //   const catElement = document.createElement('div');
+    //   catElement.id = id;
+    //   catElement.innerText = value;
+    //   catElement.style.display = 'none';
+    //   document.body.appendChild(catElement);
+    // };
+
+    // const manufacturer = document.querySelectorAll('div.title')
+    //   ? document.querySelectorAll('div.title') : [];
+
+    function addProp (selector, iterator, propName, value) {
+      document.querySelectorAll(selector)[iterator].setAttribute(propName, value);
+    }
+    const allProducts = document.querySelectorAll('div[class*="product-gallery"] div.cell');
+    for (let i = 0; i < allProducts.length; i++) {
+      addProp('div[class*="product-gallery"] div.cell', i, 'rankorganic', `${i + 1}`);
+    }
+  });
+
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'NL',
     store: 'salontopper',
-    transform: null,
+    transform: transform,
     domain: 'salontopper.nl',
     zipcode: '',
   },
+  implementation,
 };
