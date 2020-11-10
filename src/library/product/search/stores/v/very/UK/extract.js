d94b93b3e1005e0d5bfc0a9792856b6d8448094e
Michał Wieczorek <michal126@hotmail.com>
2020-11-10 15:09:07 +0100

    finished dev1 stage

--- src/library/product/search/stores/v/very/UK/extract.js
+++ src/library/product/search/stores/v/very/UK/extract.js
@@ -1,23 +1,37 @@
 const { transform } = require('../../../../shared');
 
-async function implementation(inputs, parameters, context, dependencies) {
+async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
   await context.evaluate(async () => {
-    //code
+    // add price
     var prices = document.querySelectorAll('a[class="productPrice"]');
 
     prices.forEach((element) => {
       let price = '';
-      if (element.querySelector('dd[class="productPrice"')){
+      if (element.querySelector('dd[class="productPrice"')) {
         price = element.querySelector('dd[class="productPrice"').textContent;
-      }
-      else if (element.querySelector('dd[class="productNowPrice"')){
+      } else if (element.querySelector('dd[class="productNowPrice"')) {
         price = element.querySelector('dd[class="productNowPrice"').textContent;
       }
 
-      element.setAttribute('price', price.replace(/(\r\n|\n|\r)/gm,""));
+      if (price !== '') {
+        price = price.replace(/(\r\n|\n|\r)/gm, '');
+        const results = price.match(/.\d+\.?(\d+)?/gm);
+
+        element.setAttribute('price', results[0].toString());
+      }
+    });
+  });
+
+  await context.evaluate(async () => {
+    // add rank
+    var prices = document.querySelectorAll(
+      'ul[class="productList"] div[class="productInfo"]');
+
+    prices.forEach((element, index) => {
+      element.setAttribute('rank', (index + 1).toString());
     });
   });
 
