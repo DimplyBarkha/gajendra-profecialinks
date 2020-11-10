b888f85522438c544ad55a7c7024202d6a844e79
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-10 14:52:44 +0530

    aptekaolmed - Damayanthi

--- src/library/product/search/stores/a/aptekaolmed/PL/extract.js
+++ src/library/product/search/stores/a/aptekaolmed/PL/extract.js
@@ -36,9 +36,13 @@ async function implementation(
     let rankOrganic;
     let url = window.location.href;
     let checkPageNumber = url.split('&')[1];
+    // if (checkPageNumber != null) {
+    //   checkPageNumber = checkPageNumber.split("-")[0];
+    // }
+    
     try {
-      if (checkPageNumber.startsWith('page=')) {
-        rankOrganic = checkPageNumber.replace('page=', '');
+      if (checkPageNumber.startsWith('counter=')) {
+        rankOrganic = checkPageNumber.replace('counter=', '');
       }
     }
     catch (err) {
@@ -60,54 +64,54 @@ async function implementation(
 
 
     // Method to Retrieve Xpath content of a Single Node
-    var getXpath = (xpath, prop) => {
-      var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-      let result;
-      if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-      else result = elem ? elem.singleNodeValue : '';
-      return result && result.trim ? result.trim() : result;
-    };
+    // var getXpath = (xpath, prop) => {
+    //   var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+    //   let result;
+    //   if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+    //   else result = elem ? elem.singleNodeValue : '';
+    //   return result && result.trim ? result.trim() : result;
+    // };
 
-    var pagination = getXpath("//ul[@class='pagination']/li[last()]/a/@class", 'nodeValue');
-    if (pagination === 'pagination__link') {
-      addclass('ul.pagination li:last-child a');
-    };
+    // var pagination = getXpath("li[class="item"]', 'nodeValue');
+    // if (pagination === 'pagination__link') {
+    //   addclass('ul.pagination li:last-child a');
+    // };
 
     // for rank
-    function addElementToDocument(key, value) {
-      const catElement = document.createElement('div');
-      catElement.id = key;
-      catElement.textContent = value;
-      catElement.style.display = 'none';
-      document.body.appendChild(catElement);
-    }
+    // function addElementToDocument(key, value) {
+    //   const catElement = document.createElement('div');
+    //   catElement.id = key;
+    //   catElement.textContent = value;
+    //   catElement.style.display = 'none';
+    //   document.body.appendChild(catElement);
+    // }
 
     // Method to Retrieve Xpath content of a Multiple Nodes
-    const getAllXpath = (xpath, prop) => {
-      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-      const result = [];
-      for (let index = 0; index < nodeSet.snapshotLength; index++) {
-        const element = nodeSet.snapshotItem(index);
-        if (element) result.push(prop ? element[prop] : element.nodeValue);
-      }
-      return result;
-    };
+    // const getAllXpath = (xpath, prop) => {
+    //   const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    //   const result = [];
+    //   for (let index = 0; index < nodeSet.snapshotLength; index++) {
+    //     const element = nodeSet.snapshotItem(index);
+    //     if (element) result.push(prop ? element[prop] : element.nodeValue);
+    //   }
+    //   return result;
+    // };
 
     // for rank
-    const sliceURL = (data) => {
-      var cnt = 0;
-      for (let index = 0; index < data.length; index++) {
-        if (data[0] != 0) {
-          cnt++;
-          addElementToDocument('altImages', cnt);
-        }
-      }
-    };
-    var backgroundURL = getAllXpath('//a[@class="product-name"]/text()', 'nodeValue');
-    sliceURL(backgroundURL);
+    // const sliceURL = (data) => {
+    //   var cnt = 0;
+    //   for (let index = 0; index < data.length; index++) {
+    //     if (data[0] != 0) {
+    //       cnt++;
+    //       addElementToDocument('altImages', cnt);
+    //     }
+    //   }
+    // };
+    // var backgroundURL = getAllXpath('//span[@class="product__name__productitemno"]/text()', 'nodeValue');
+    // sliceURL(backgroundURL);
   });
   //rank end
 
 
   return await context.extract(productDetails, { transform });
-}
+}  
