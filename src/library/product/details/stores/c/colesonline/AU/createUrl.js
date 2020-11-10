892b1764e538d970348763725cd5467d154feca9
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-10 13:45:13 +0530

    Core location added

--- src/library/product/details/stores/c/colesonline/AU/createUrl.js
+++ src/library/product/details/stores/c/colesonline/AU/createUrl.js
@@ -4,7 +4,8 @@ module.exports = {
   parameterValues: {
     domain: 'shop.coles.com.au',
     prefix: null,
-    url: 'https://shop.coles.com.au/a/national/everything/search/{id}?pageNumber=1',
+    // url: 'https://shop.coles.com.au/a/national/everything/search/{id}?pageNumber=1',
+    url: 'https://shop.coles.com.au/a/alexandria/everything/search/{id}?pageNumber=1',
     country: 'AU',
     store: 'colesonline',
     zipcode: '',
--- src/library/product/details/stores/c/colesonline/AU/execute.js
+++ src/library/product/details/stores/c/colesonline/AU/execute.js
@@ -1,4 +1,36 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  let { url, id, zipcode, storeId } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  await dependencies.goto({ url, zipcode, storeId });
+  const link = await context.evaluate(() => {
+    const link = document.querySelector("section[id*='product-list'] a[class*='product-image-link']") ? document.querySelector("section[id*='product-list'] a[class*='product-image-link']").getAttribute('href') : '';
+    return link;
+  });
+  console.log('Link:::', link);
+  await context.goto('https://shop.coles.com.au' + link, {
+    firstRequestTimeout: 60000,
+    timeout: 60000,
+    waitUntil: 'load',
+    checkBlocked: false,
+  });
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
 
+  // TODO: Check for not found?
+}
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -9,4 +41,5 @@ module.exports = {
     noResultsXPath: "//h1[contains(@class,'error-heading')] | //nav[not(contains(@class,'ng-hide'))]//ol[contains(@id,'tablist')]//li",
     zipcode: '',
   },
+  implementation,
 };
