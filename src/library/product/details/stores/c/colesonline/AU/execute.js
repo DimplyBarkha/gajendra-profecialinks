590d7edcba2143d6deef2e23c73d0c0bd40fcd94
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-10 14:34:30 +0530

    Colesonline Core Goto updated

--- src/library/product/details/stores/c/colesonline/AU/execute.js
+++ src/library/product/details/stores/c/colesonline/AU/execute.js
@@ -4,14 +4,49 @@ async function implementation (
   context,
   dependencies,
 ) {
-  let { url, id, zipcode, storeId } = inputs;
+  let { url, id } = inputs;
   if (!url) {
     if (!id) {
       throw new Error('no id provided');
     }
     url = await dependencies.createUrl({ id });
   }
-  await dependencies.goto({ url, zipcode, storeId });
+  // -------------Search--------------------------
+  const timeout = parameters.timeout ? parameters.timeout : 10000;
+  await context.setBlockAds(false);
+  await context.setLoadAllResources(true);
+  await context.setLoadImages(true);
+  await context.setJavaScriptEnabled(true);
+  await context.setAntiFingerprint(false);
+  await context.goto('https://shop.coles.com.au/', {
+    firstRequestTimeout: 60000,
+    timeout: timeout,
+    waitUntil: 'load',
+    checkBlocked: false,
+  });
+  // -------------To set location-----------------
+  await context.waitForSelector("button[id*='changeLocationBar']");
+  await context.click("button[id*='changeLocationBar']");
+  await context.waitForSelector("input[id*='localisation-search']");
+  await context.setInputValue("input[id*='localisation-search']", 'Sydney 2000');
+  await context.waitForSelector("div[id*='search-autocomplete'] li[role*='option']:nth-child(1)");
+  await context.clickAndWaitForNavigation("div[id*='search-autocomplete'] li[role*='option']:nth-child(1)", {}, { timeout: 20000 });
+  await context.waitForSelector("input[id*='localisation-search']", { timeout: 20000 });
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  // ------------- GoTo requried URL --------------
+  await context.setBlockAds(false);
+  await context.setLoadAllResources(true);
+  await context.setLoadImages(true);
+  await context.setJavaScriptEnabled(true);
+  await context.setAntiFingerprint(false);
+  url = `${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_all_resources":true}[/!opt!]`;
+  await context.goto(url, {
+    firstRequestTimeout: 60000,
+    timeout: timeout,
+    waitUntil: 'load',
+    checkBlocked: false,
+  });
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const link = await context.evaluate(() => {
     const link = document.querySelector("section[id*='product-list'] a[class*='product-image-link']") ? document.querySelector("section[id*='product-list'] a[class*='product-image-link']").getAttribute('href') : '';
     return link;
