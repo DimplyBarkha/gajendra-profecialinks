e089427fa08481f63049a540a61fe877f907f28e
Karthickravi <karthik.ravi-odc@partner.import.io>
2020-11-10 14:50:42 +0530

    karthickravi

--- src/library/product/search/stores/u/unieuro/IT/extract.js
+++ src/library/product/search/stores/u/unieuro/IT/extract.js
@@ -24,13 +24,7 @@ module.exports = {
         const originalDiv = document.querySelectorAll('section[class="collapsed"]')[index];
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
-      function addElementToDocument(key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
-        }
+      
       let rankOrganic;
       let url = window.location.href;
       let checkPageNumber = url.split('&')[1];
@@ -50,19 +44,7 @@ module.exports = {
       for (let i = 0; i < urlProduct.length; i++) {
         addHiddenDiv('rankOrganic', rankOrganic++, i);
       } 
-      var getXpath = (xpath, prop) => {
-        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-        let result;
-        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-        else result = elem ? elem.singleNodeValue : '';
-        return result && result.trim ? result.trim() : result;
-        };
-        var backgroundURL = getXpath('//iframe[@id="esw_storage_iframe"]/@src' , 'nodeValue'); 
-        var xyz = backgroundURL.split("=");
-        xyz.shift();
-        var zz = xyz.join();
-        zz = zz.replaceAll(",","="); 
-        addElementToDocument('url', zz);
+      
     });
     return await context.extract(productDetails, { transform });
   },
