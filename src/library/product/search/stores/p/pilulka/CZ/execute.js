cbb8a81461db63b45bd8e94608ded5e92010a2ec
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-10 10:28:51 +0100

    created a template for the search extractor

---
+++ src/library/product/search/stores/p/pilulka/CZ/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'CZ',
+    store: 'pilulka',
+    domain: 'pilulka.cz',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/p/pilulka/CZ/paginate.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'CZ',
+    store: 'pilulka',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'pilulka.cz',
+    zipcode: '',
+  },
+};
