61889f8db8a64bc7ecef71147d04432816e6dd49
sujoypal144 <sujoypal144@gmail.com>
2020-11-10 06:27:10 +0530

    Fixed the video attribute

--- src/library/product/details/stores/m/melectronics/CH/extract.js
+++ src/library/product/details/stores/m/melectronics/CH/extract.js
@@ -1,4 +1,4 @@
-const {transform} = require('../format.js')
+const { transform } = require('../format.js')
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -7,5 +7,34 @@ module.exports = {
     transform,
     domain: 'melectronics.ch',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      if (document.querySelector('img[alt="Video"]')) {
+        document.querySelector('img[alt="Video"]').click();
+        function stall(ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+        await stall(3000);
+        console.log(document.querySelector('#widget2').getAttribute('src'));
+        let videoUrl=document.querySelector('#widget2').getAttribute('src');
+
+        function addElementToDocument(key, value) {
+          const catElement = document.createElement('div');
+          catElement.id = key;
+          catElement.textContent = value;
+          catElement.style.display = 'none';
+          document.body.appendChild(catElement);
+        }
+        addElementToDocument('videoLink', videoUrl);
+        return document.querySelector('#widget2').getAttribute('src');
+      }
+    });
+    
+    await context.extract(productDetails);
   },
 };
