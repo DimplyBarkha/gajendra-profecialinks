c9f9ad53ffd866ca7e1e3f9a33d38bfa2fa2433a
Wayne E <bartekstafiej2@gmail.com>
2020-11-10 13:45:01 +0100

    #1141 Fixed url

--- src/library/product/search/stores/a/alcampo/ES/execute.js
+++ src/library/product/search/stores/a/alcampo/ES/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'ES',
     store: 'alcampo',
     domain: 'alcampo.es',
-    url: 'https://www.alcampo.es/compra-online/search?q={searchTerms}',
+    url: 'https://www.alcampo.es/compra-online/search/?department=&text={searchTerms}',
     loadedSelector: 'div.caja.caja-reposo.productGridItem ',
     noResultsXPath: '//div[@class="page-not-found-content"]',
     zipcode: '',
