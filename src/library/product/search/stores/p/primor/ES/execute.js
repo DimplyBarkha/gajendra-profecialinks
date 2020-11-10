bddc211af66c465772f47effdf1c09fa5533b224
unknown <leena.gavali-odc@partner.import.io>
2020-11-10 20:54:20 +0530

    primor_core_update_pagination

--- src/library/product/search/stores/p/primor/ES/execute.js
+++ src/library/product/search/stores/p/primor/ES/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'primor',
     domain: 'primor.eu',
     url: 'https://www.primor.eu/buscar?controller=search&orderby=position&orderway=desc&search_query={searchTerms}&submit_search=',
-    loadedSelector: null,
+    loadedSelector: '#center_column > ul > li',
     noResultsXPath: null,
     zipcode: '',
   },
--- src/library/product/search/stores/p/primor/ES/paginate.js
+++ src/library/product/search/stores/p/primor/ES/paginate.js
@@ -7,10 +7,13 @@ module.exports = {
     nextLinkSelector: '[id="pagination_next_bottom"] a',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: '#center_column > ul > li',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'primor.eu',
     zipcode: '',
+  },
+  openSearchDefinition : {
+    template: 'https://www.primor.eu/buscar?controller=search&orderby=position&orderway=desc&search_query={searchTerms}&submit_search=#/page-{page}',
   },
 };
