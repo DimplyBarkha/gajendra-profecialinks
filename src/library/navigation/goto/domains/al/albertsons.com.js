10d83176e02a15b56f8183c220c60bbd73912660
Sephali-23 <sephalisingh023@gmail.com>
2020-11-10 18:38:55 +0530

    fixing issues

--- src/library/navigation/goto/domains/al/albertsons.com.js
+++ src/library/navigation/goto/domains/al/albertsons.com.js
@@ -1,19 +1,19 @@
 module.exports = {
-    implements: 'navigation/goto',
-    parameterValues: {
-        domain: 'albertsons.com',
-        timeout: 50000,
-        country: 'US',
-        store: 'albertsons',
-        zipcode: '83642',
-    },
-    implementation: async({ url, zipcode, storeId }, parameters, context, dependencies) => {
-        const timeout = parameters.timeout ? parameters.timeout : 10000;
-        await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
-        await new Promise(resolve => { setTimeout(resolve, 50000); });
-        console.log(zipcode);
-        if (zipcode) {
-            await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-        }
-    },
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'albertsons.com',
+    timeout: 50000,
+    country: 'US',
+    store: 'albertsons',
+    zipcode: '83642',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    await new Promise(resolve => { setTimeout(resolve, 50000); });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
--- src/library/product/details/stores/a/albertsons/US/extract.js
+++ src/library/product/details/stores/a/albertsons/US/extract.js
@@ -1,35 +1,35 @@
 const { cleanUp } = require('../transform');
 
 module.exports = {
-    implements: 'product/details/extract',
-    parameterValues: {
-        country: 'US',
-        store: 'albertsons',
-        transform: cleanUp,
-        domain: 'albertsons.com',
-        zipcode: '83642',
-    },
-    implementation: async function(
-        inputs,
-        parameters,
-        context,
-        dependencies,
-    ) {
-        const { productDetails } = dependencies;
-        const { transform } = parameters;
-        await context.evaluate(() => {
-            const scriptElement1 = document.querySelectorAll('script[type="application/ld+json"]');
-            // @ts-ignore
-            const data = scriptElement1[1] && scriptElement1[1].innerText;
-            const obj = data && JSON.parse(data);
-            const avail = obj && obj.offers && obj.offers.availability;
-            var appendElement = document.querySelector('picture[class*="img-responsive"]>img');
-            if (avail) {
-                appendElement.setAttribute('availability', 'In Stock');
-            } else {
-                appendElement.setAttribute('availability', 'Out Of Stock');
-            }
-        });
-        return await context.extract(productDetails, { transform });
-    },
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'albertsons',
+    transform: cleanUp,
+    domain: 'albertsons.com',
+    zipcode: '83642',
+  },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { productDetails } = dependencies;
+    const { transform } = parameters;
+    await context.evaluate(() => {
+      const scriptElement1 = document.querySelectorAll('script[type="application/ld+json"]');
+      // @ts-ignore
+      const data = scriptElement1[1] && scriptElement1[1].innerText;
+      const obj = data && JSON.parse(data);
+      const avail = obj && obj.offers && obj.offers.availability;
+      var appendElement = document.querySelector('picture[class*="img-responsive"]>img');
+      if (avail) {
+        appendElement.setAttribute('availability', 'In Stock');
+      } else {
+        appendElement.setAttribute('availability', 'Out Of Stock');
+      }
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
