a439f0a291e845ab7179a5c310832513bca7d032
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-10 23:17:13 +0530

    #1390 working

--- src/library/navigation/goto/domains/fa/farmatodo.com.co.js
+++ src/library/navigation/goto/domains/fa/farmatodo.com.co.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'farmatodo.com.co',
-    timeout: null,
+    timeout: 30000,
     country: 'CO',
     store: 'farmatodo',
     zipcode: '',
