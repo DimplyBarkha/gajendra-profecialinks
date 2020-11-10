1e018f1c2900ebc11e5e3b2ca0e26489382cba61
sivasoarlogic <siva@soarlogic.com>
2020-11-10 21:44:36 +0530

    searchupdated

--- src/library/navigation/goto/domains/ic/iciparisxl.be.js
+++ src/library/navigation/goto/domains/ic/iciparisxl.be.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'iciparisxl.be',
-    timeout: null,
+    timeout: 100000,
     country: 'BE',
     store: 'iciparisxl',
     zipcode: "''",
--- src/library/product/search/stores/i/iciparisxl/BE/extract.js
+++ src/library/product/search/stores/i/iciparisxl/BE/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('./format');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'BE',
     store: 'iciparisxl',
-    transform: null,
+    transform,
     domain: 'iciparisxl.be',
     zipcode: "''",
   },
