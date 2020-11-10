71826aff13e1372f0fef3b441a83cb7140bee337
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-10 14:28:45 +0530

    Colesonline_3216 core

---
+++ src/library/product/details/stores/c/colesonline_3216/AU/execute.js
@@ -0,0 +1,80 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  let { url, id } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  // await dependencies.goto({ url, zipcode, storeId });
+  const timeout = parameters.timeout ? parameters.timeout : 10000;
+  await context.setBlockAds(false);
+  await context.setLoadAllResources(true);
+  await context.setLoadImages(true);
+  await context.setJavaScriptEnabled(true);
+  await context.setAntiFingerprint(false);
+  await context.goto('https://shop.coles.com.au/', {
+    firstRequestTimeout: 60000,
+    timeout: timeout,
+    waitUntil: 'load',
+    checkBlocked: false,
+  });
+  // -------------To set location-----------------
+  await context.waitForSelector("button[id*='changeLocationBar']");
+  await context.click("button[id*='changeLocationBar']");
+  await context.waitForSelector("input[id*='localisation-search']");
+  await context.setInputValue("input[id*='localisation-search']", 'WAURN PONDS, 3216');
+  await context.waitForSelector("div[id*='search-autocomplete'] li[role*='option']:nth-child(1)");
+  await context.clickAndWaitForNavigation("div[id*='search-autocomplete'] li[role*='option']:nth-child(1)", {}, { timeout: 20000 });
+  await context.waitForSelector("input[id*='localisation-search']", { timeout: 20000 });
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  // ------------- GoTo requried URL --------------
+  await context.setBlockAds(false);
+  await context.setLoadAllResources(true);
+  await context.setLoadImages(true);
+  await context.setJavaScriptEnabled(true);
+  await context.setAntiFingerprint(false);
+  url = `${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_all_resources":true}[/!opt!]`;
+  await context.goto(url, {
+    firstRequestTimeout: 60000,
+    timeout: timeout,
+    waitUntil: 'load',
+    checkBlocked: false,
+  });
+  await new Promise((resolve) => setTimeout(resolve, 6000));
+  const link = await context.evaluate(() => {
+    const link = document.querySelector("section[id*='product-list'] a[class*='product-image-link']") ? document.querySelector("section[id*='product-list'] a[class*='product-image-link']").getAttribute('href') : '';
+    return link;
+  });
+  console.log('Link:::', link);
+  await context.goto('https://shop.coles.com.au' + link, {
+    firstRequestTimeout: 60000,
+    timeout: 60000,
+    waitUntil: 'load',
+    checkBlocked: false,
+  });
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+
+  // TODO: Check for not found?
+}
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'AU',
+    store: 'colesonline_3216',
+    domain: 'shop.coles.com.au',
+    loadedSelector: 'div[class*="product-hero-image-container"] img',
+    noResultsXPath: "//h1[contains(@class,'error-heading')] | //nav[not(contains(@class,'ng-hide'))]//ol[contains(@id,'tablist')]//li",
+    zipcode: '',
+  },
+  implementation,
+};
---
+++ src/library/product/details/stores/c/colesonline_3216/AU/extract.js
@@ -0,0 +1,114 @@
+
+const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async () => {
+    function findXpathArr (xpath) {
+      const element = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+      let node = element.iterateNext();
+      const value = [];
+      while (node) {
+        value.push(node.textContent);
+        node = element.iterateNext();
+      }
+      return value;
+    }
+    function findXpath (xpath) {
+      const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      const productDetails = element && element.textContent ? element.textContent : '';
+      return productDetails;
+    }
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    function energyCalculation (value, name, name2) {
+      const arr = value.match(/([\d]+(?:.[\d]+)?)(?:\s*)([a-zA-Z]*)\s{0,1}/);
+      arr && arr[1] && addHiddenDiv('ii_' + name, arr[1]);
+      arr && arr[2] && name2 && addHiddenDiv('ii_' + name2, arr[2]);
+    }
+    const totalFatPerServing = findXpath("//tr[contains(. , 'Total Fat')]//td");
+    energyCalculation(totalFatPerServing, 'totalFatPerServing', 'totalFatPerServingUom');
+    const saturatedFatPerServing = findXpath("//tr[contains(. , 'Saturated Fat')]//td");
+    energyCalculation(saturatedFatPerServing, 'saturatedFatPerServing', 'saturatedFatPerServingUom');
+    const sodiumPerServing = findXpath("//tr[contains(. , 'Sodium')]//td");
+    energyCalculation(sodiumPerServing, 'sodiumPerServing', 'sodiumPerServingUom');
+    const totalCarbPerServing = findXpath("//tr[contains(. , 'Carbohydrate')]//td");
+    energyCalculation(totalCarbPerServing, 'totalCarbPerServing', 'totalCarbPerServingUom');
+    const totalSugarsPerServing = findXpath("//tr[contains(. , 'Sugar')]//td");
+    energyCalculation(totalSugarsPerServing, 'totalSugarsPerServing', 'totalSugarsPerServingUom');
+    const proteinPerServing = findXpath("//tr[contains(. , 'Protein')]//td");
+    energyCalculation(proteinPerServing, 'proteinPerServing', 'proteinPerServingUom');
+    const dietaryFibrePerServing = findXpath("//tr[contains(. , 'Dietary Fibre Total')]//td");
+    energyCalculation(dietaryFibrePerServing, 'dietaryFibrePerServing', 'dietaryFibrePerServingUom');
+    const transFatPerServing = findXpath("//tr[contains(. , 'Trans')]//td");
+    energyCalculation(transFatPerServing, 'transFatPerServing', 'transFatPerServingUom');
+    const vitaminAPerServing = findXpath("//tr[contains(. , 'Vitamin A')]//td");
+    energyCalculation(vitaminAPerServing, 'vitaminAPerServing', 'vitaminAPerServingUom');
+    const vitaminCPerServing = findXpath("//tr[contains(. , 'Vitamin C')]//td");
+    energyCalculation(vitaminCPerServing, 'vitaminCPerServing', 'vitaminCPerServingUom');
+    const calciumPerServing = findXpath("//tr[contains(. , 'Calcium')]//td");
+    energyCalculation(calciumPerServing, 'calciumPerServing', 'calciumPerServingUom');
+    const ironPerServing = findXpath("//tr[contains(. , 'Iron')]//td");
+    energyCalculation(ironPerServing, 'ironPerServing', 'ironPerServingUom');
+    const magnesiumPerServing = findXpath("//tr[contains(. , 'Magnesium')]//td");
+    energyCalculation(magnesiumPerServing, 'magnesiumPerServing', 'magnesiumPerServingUom');
+    const script = findXpath("//script[contains(@type,'application/ld+json')]") && JSON.parse(findXpath("//script[contains(@type,'application/ld+json')]")) ? JSON.parse(findXpath("//script[contains(@type,'application/ld+json')]")) : '';
+    if (script) {
+      const avail = script.offers && script.offers.availability ? script.offers.availability : '';
+      avail.includes('InStock') && addHiddenDiv('ii_avail', 'In Stock');
+      !avail.includes('InStock') && addHiddenDiv('ii_avail', 'Out of Stock');
+      const sku = script.sku ? script.sku : '';
+      addHiddenDiv('ii_sku', sku);
+      const variantId = findXpath("//li[contains(.,'Code:')]//span[contains(@class,'accessibility')]");
+      addHiddenDiv('ii_variantId', variantId.replace(/ /gm, ''));
+      const additionalProperty = script.additionalProperty ? script.additionalProperty : '';
+      additionalProperty.forEach(element => {
+        element.name === 'Warning' && addHiddenDiv('ii_warning', element.text);
+      });
+    }
+    let servingSize = findXpath("//*[@class='nutritional-table-intro']");
+    servingSize = servingSize.split('=')[1] ? servingSize.split('=')[1] : servingSize;
+    energyCalculation(servingSize, 'servingSize', 'servingSizeUom');
+    let name = findXpathArr("//h1[@class='product-title']//span[@class='product-brand'] | //h1[@class='product-title']//span[@class='product-name']");
+    name = name.join(' ');
+    addHiddenDiv('ii_name', name);
+    let specifications = findXpath("//div[contains(@class,'product-specific')]");
+    specifications = specifications.replace(/\n|\t|\s{2,}/gm, ' ').trim().indexOf('Details') === 0 ? specifications.replace(/Details/, '') : specifications;
+    addHiddenDiv('ii_specifications', specifications);
+    const direction = findXpathArr("//h3[contains(@class,'product-specific-heading') and contains(.,'Usage Instructions:')]//following-sibling::span[contains(@data-ng-bind-html,'product')] | //div[contains(@data-ng-repeat,'product.getPreparation')]/*[not(self::h3)]");
+    addHiddenDiv('ii_direction', direction.join(' '));
+    let pricePerUnit = findXpath("//div[contains(@class,'product-information')]//span[contains(@class,'package-price')]");
+    const val = pricePerUnit.split('per');
+    let pricePerUnitUom = '';
+    if (val[1] && val[1].match(/([\d]+)/)) {
+      pricePerUnit = val[0].trim() + ' per ' + val[1].match(/([\d]{1,})/)[0].trim();
+      pricePerUnitUom = val[1].replace(/.*(?:[\d]+(?:.[\d]+)?)\s{0,}(.*)/, '$1');
+    }
+    addHiddenDiv('ii_pricePerUnit', pricePerUnit);
+    addHiddenDiv('ii_pricePerUnitUom', pricePerUnitUom);
+  });
+  return await context.extract(productDetails, { transform });
+}
+
+module.exports = {
+  extends: 'product/details/stores/c/colesonline/extract',
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'AU',
+    store: 'colesonline_3216',
+    transform,
+    domain: 'shop.coles.com.au',
+    zipcode: '',
+  },
+  implementation,
+};
