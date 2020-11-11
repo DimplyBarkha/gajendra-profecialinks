e563803e9a911449665efa86e331a6d8d9342fa7
ganesh <ganesh@profecialinks.com>
2020-11-12 00:42:22 +0530

    add scroll

--- src/library/product/search/stores/e/edigital/HU/extract.js
+++ src/library/product/search/stores/e/edigital/HU/extract.js
@@ -1,57 +1,5 @@
 const {transform} = require('./format')
-/**
- *
- * @param { { keywords: string, zipcode: string } } inputs
- * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { goto: ImportIO.Action} } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  console.log('params', parameters);
-  //const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
-  //await dependencies.goto({ url, zipcode: inputs.zipcode });
-  const { productDetails } = dependencies;
-  const applyScroll = async function (context) {
-    await context.evaluate(async function () {
-      let scrollTop = 0;
-      while (scrollTop !== 20000) {
-        await stall(500);
-        scrollTop += 1000;
-        window.scroll(0, scrollTop);
-        if (scrollTop === 20000) {
-          await stall(5000);
-          break;
-        }
-      }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-    });
-  };
-  try {
-    await context.click('div[id="wps_popup"] div[data-wps-popup-close]');
-  }catch (e) {
-    console.log(e);
-  }
-  await applyScroll(context);
-  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-  if (parameters.loadedSelector) {
-    await context.waitForFunction(function (sel, xp) {
-      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
-  }
-  console.log('Checking no results', parameters.noResultsXPath);
-  return await context.extract(productDetails, { transform });
-}
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -60,6 +8,5 @@ module.exports = {
     transform,
     domain: 'edigital.hu',
     zipcode: '',
-  },
-  implementation,
+  },  
 };
