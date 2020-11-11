9362224be622d14c7f955afd7651cb3ed783a00b
dhanush <dhanush.isukapalli@od.import.io>
2020-11-11 23:30:06 +0530

    kavya ottooffice

---
+++ src/library/product/details/stores/o/ottooffice/DE/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'otto-office.com',
+    prefix: null,
+    url: null,
+    country: 'DE',
+    store: 'ottooffice',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/o/ottooffice/DE/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'DE',
+    store: 'ottooffice',
+    domain: 'otto-office.com',
+    zipcode: '',
+  },
+};
