bb61f9c8c41c6ac00ce53676c4e384518489bc72
IpsitaM <iamipsita07@gmail.com>
2020-11-11 10:43:57 +0530

    Updated few xpaths

--- src/library/product/details/stores/t/tesco/shared.js
+++ src/library/product/details/stores/t/tesco/shared.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
@@ -7,52 +6,48 @@
 const transform = (data) => {
     // Default transform function
     const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+        .replace(/\r\n|\r|\n/g, ' ')
+        .replace(/&amp;nbsp;/g, ' ')
+        .replace(/&amp;#160/g, ' ')
+        .replace(/\u00A0/g, ' ')
+        .replace(/\s{2,}/g, ' ')
+        .replace(/"\s{1,}/g, '"')
+        .replace(/\s{1,}"/g, '"')
+        .replace(/^ +| +$|( )+/g, ' ')
+        // eslint-disable-next-line no-control-regex
+        .replace(/[\x00-\x1F]/g, '')
+        .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
     data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      el.text = clean(el.text);
+        el.text = clean(el.text);
     }))));
-  
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.additives) {
-            let text = '';
-            row.additives.forEach(item => {
-              text += item.text.replace(/\\n/g, '   ');
-            });
-            row.additives = [
-              {
-                text: text,
-              },
-            ];
+
+    for (const { group }
+        of data) {
+        for (const row of group) {
+            if (row.additives) {
+                let text = '';
+                row.additives.forEach(item => {
+                    text += item.text.replace(/\\n/g, '   ');
+                });
+                row.additives = [{
+                    text: text,
+                }, ];
+            }
+            if (row.caloriesPerServing) {
+                let text = '';
+                row.caloriesPerServing.forEach(item => {
+                    text += item.text.replace(new RegExp('(.+)(\\/|\\/-)$', 'g'), '$1');
+                    if (text === '/') {
+                        text = '';
+                    }
+                });
+                row.caloriesPerServing = [{
+                    text: text.replace(new RegExp('(.+\\/)\\/(.+)', 'g'), '$1$2'),
+                }, ];
+            }
         }
-        if (row.caloriesPerServing) {
-            let text = '';
-            row.caloriesPerServing.forEach(item => {
-              text += item.text.replace(new RegExp('(.+)(\\/|\\/-)$', 'g'), '$1');
-              if (text === '/') {
-                text = '';
-              }
-            });
-            row.caloriesPerServing = [
-              {
-                text: text.replace(new RegExp('(.+\\/)\\/(.+)', 'g'), '$1$2'),
-              },
-            ];
-        }
-      }
     }
     return data;
-  };
-  
-  module.exports = { transform };
-  
+};
+
+module.exports = { transform };
