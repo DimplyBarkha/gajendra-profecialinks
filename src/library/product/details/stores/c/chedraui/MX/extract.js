3bc5caa79e82ba2693e991d0917d4b3630fa361e
rajeev <rajeev.ranjan@synerzip.com>
2020-11-11 14:40:36 +0530

    1285 variants added and description

--- src/library/product/details/stores/c/chedraui/MX/extract.js
+++ src/library/product/details/stores/c/chedraui/MX/extract.js
@@ -1,5 +1,31 @@
 const { transform } = require('../shared');
-
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  //await new Promise((resolve, reject) => setTimeout(resolve, 20000));
+  var variantLength = await context.evaluate(async () => {
+    return (document.querySelectorAll('div.variant-selector option')) ? document.querySelectorAll('div.variant-selector option').length : 0;
+  });
+  console.log("variantLength:: ", variantLength);
+  if (variantLength > 1) {
+    // await preparePageForCommonElement(0, variantLength);
+    for (let j = 0; j < variantLength; j++) {
+      await context.evaluate(async (j) => {
+        return document.querySelectorAll('option[value]')[j].click();
+      }, j);
+      // await context.click(`ul.topic li label`);
+      console.log('Inside variants', j);
+      // await preparePage(j, variantLength);
+      if (j !== variantLength - 1) { await context.extract(productDetails, { transform }, { type: 'APPEND' }); }
+    }
+  }
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -9,4 +35,5 @@ module.exports = {
     domain: 'chedraui.com.mx',
     zipcode: "''",
   },
+  implementation,
 };
