223ea39b2fca1871e8ffbbf58af5edf0a1d511dc
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-11-11 11:52:08 +0100

    - Fixed an issue with extracting 'rankOrganic'

--- src/library/product/search/stores/r/romeos/AU/extract.js
+++ src/library/product/search/stores/r/romeos/AU/extract.js
@@ -10,16 +10,15 @@ module.exports = {
     zipcode: '',
   },
   implementation: async (inputs, parameters, context, { productDetails }) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     await context.evaluate(async () => {
       const productsPerPage = 48;
       const pageElem = document.querySelector('em[aria-current="page"]');
       const pageNum = pageElem && pageElem.textContent ? parseInt(pageElem.textContent, 10) : 1;
       const products = document.querySelectorAll('div[class="TalkerGrid__Item"]');
       for (let i = 0; i < products.length; i++) {
-        console.log(pageNum);
-        const rank = (pageNum - 1) * productsPerPage + i;
-        console.log(rank);
-        products[i].setAttribute('rank', rank.toString());
+        const rankOrganic = (pageNum - 1) * productsPerPage + (i + 1);
+        products[i].setAttribute('rankOrganic', rankOrganic.toString());
       }
     });
     return await context.extract(productDetails);
