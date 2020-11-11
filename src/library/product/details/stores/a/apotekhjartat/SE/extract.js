dd766d10958380c5c397c7eb42c50fe53dc72905
Vivek Gondil <gondilvivek@gmail.com>
2020-11-11 10:17:53 +0530

    Adding implementation for direction and warnings.

--- src/library/product/details/stores/a/apotekhjartat/SE/extract.js
+++ src/library/product/details/stores/a/apotekhjartat/SE/extract.js
@@ -14,22 +14,56 @@ module.exports = {
     context,
     dependencies,
   ) => {
-    await context.evaluate(() => {
-      const a = document.evaluate('//ul[@class="slides"]/li[not(contains(@class,"clone")) and (position()>2)]/img', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-      let b = a.iterateNext();
-      const c = [];
-      while (b !== null) {
-        c.push(b.getAttribute('src').split('?')[0]);
-        b = a.iterateNext();
-      }
-      const div = document.createElement('div');
-      div.setAttribute('id', 'alt-img');
-      div.innerText = c.join(' | ');
-      document.body.append(div);
-    });
+    if (await context.evaluate(() => {
+      return (!window.location.href.toString().includes('soksida'));
+    })) {
+      await context.evaluate(() => {
+        let a = document.evaluate('//ul[@class="slides"]/li[not(contains(@class,"clone")) and (position()>2)]/img', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+        let b = a.iterateNext();
+        let c = [];
+        while (b !== null) {
+          c.push(b.getAttribute('src').split('?')[0]);
+          b = a.iterateNext();
+        }
+        const div = document.createElement('div');
+        div.setAttribute('id', 'alt-img');
+        div.innerText = c.join(' | ');
+        document.body.append(div);
 
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    return await context.extract(productDetails, { transform });
+        const pmDoc = document.querySelector('#pm-document');
+        if (pmDoc) {
+          const hs = document.querySelectorAll('#pm-document>h2');
+          hs.forEach((h) => { h.setAttribute('id', 'br'); });
+          const children = pmDoc.children;
+          let secDiv = document.createElement('div');
+          secDiv.setAttribute('id', 'section');
+          for (let i = 0; i < children.length; i++) {
+            if (children[i].id === 'br') {
+              document.body.append(secDiv);
+              secDiv = document.createElement('div');
+              secDiv.setAttribute('id', 'section');
+            }
+            secDiv.innerHTML += children[i].innerHTML;
+          }
+          document.body.append(secDiv);
+        }
+
+        a = document.evaluate('//div[@id="section" and contains(.,"ur du använder")]//text()', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+        b = a.iterateNext();
+        c = [];
+        while (b !== null) {
+          c.push(b.textContent);
+          b = a.iterateNext();
+        }
+        const div2 = document.createElement('div');
+        div2.setAttribute('id', 'directions');
+        div2.innerText = c.join(' ');
+        document.body.append(div2);
+      });
+      const { transform } = parameters;
+      const { productDetails } = dependencies;
+      return await context.extract(productDetails, { transform });
+    }
   },
+
 };
