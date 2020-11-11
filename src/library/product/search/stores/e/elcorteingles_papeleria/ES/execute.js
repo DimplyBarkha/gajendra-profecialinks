841345e2c081221ab4e9d83992ab4238638c6134
Abhisek <abhisek.d@osmosys.asia>
2020-11-11 21:40:29 +0530

    Added Proxy

--- src/library/product/search/stores/e/elcorteingles_papeleria/ES/execute.js
+++ src/library/product/search/stores/e/elcorteingles_papeleria/ES/execute.js
@@ -4,7 +4,7 @@ module.exports = {
     country: 'ES',
     store: 'elcorteingles_papeleria',
     domain: 'elcorteingles.es',
-    url: 'https://www.elcorteingles.es/search/?v=Papeler%C3%ADa&s={searchTerms}&stype=text_box',
+    url: 'https://www.elcorteingles.es/papeleria/search/?s={searchTerms}',
     loadedSelector: 'li.products_list-item',
     noResultsXPath: '//div[@class="products_list-container _no_products vp"] | //html[not(//div[@id="products-list"]/ul/li)]',
     zipcode: '',
