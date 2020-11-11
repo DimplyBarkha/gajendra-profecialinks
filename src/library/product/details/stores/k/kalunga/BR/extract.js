66884b87ee7992209c5ac54c07ac0a35823075e5
Stolyar100 <olejer555@gmail.com>
2020-11-11 11:07:45 +0200

    kalunga core added new fields

--- src/library/product/details/stores/k/kalunga/BR/extract.js
+++ src/library/product/details/stores/k/kalunga/BR/extract.js
@@ -8,4 +8,31 @@ module.exports = {
     domain: 'kalunga.com.br',
     zipcode: '',
   },
+  implementation: async (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+
+    await context.evaluate(() => {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+
+      //Gets brand link
+      const outOfStock = document.querySelector('#imgProdutoIndisponivel');
+      if (!outOfStock) {
+        addElementToDocument('availabilityText', 'In Stock');
+      }
+    });
+
+    return await context.extract(productDetails, { transform });
+  }
 };
