1e4e46444084caaceb50a107b27999a4d2e907f5
Pashya <pashya@profecialinks.com>
2020-11-11 15:41:27 +0530

    Decathlon

--- src/library/product/details/stores/d/decathlon/FR/extract.js
+++ src/library/product/details/stores/d/decathlon/FR/extract.js
@@ -1,5 +1,26 @@
 const { transform } = require('../format');
-
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  var variantLength = await context.evaluate(async () => {
+    return (document.querySelectorAll('.sizes__wrapper.sizes__wrapper--visible .sizes__list .sizes__size')) ? document.querySelectorAll('.sizes__wrapper.sizes__wrapper--visible .sizes__list .sizes__size').length : 0;
+  });
+  if (variantLength > 1) {
+    for (let j = 0; j < variantLength; j++) {
+      await context.evaluate(async (j) => {
+        return document.querySelectorAll('.sizes__wrapper.sizes__wrapper--visible .sizes__list .sizes__size')[j].click();
+      }, j);
+      console.log('Inside variants', j);
+      if (j !== variantLength - 1) { await context.extract(productDetails, { transform }, { type: 'APPEND' }); }
+    }
+  }
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -9,4 +30,5 @@ module.exports = {
     domain: 'decathlon.fr',
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/details/stores/d/decathlon/format.js
+++ src/library/product/details/stores/d/decathlon/format.js
@@ -29,13 +29,49 @@ const transform = (data) => {
           });
           row.availabilityText = [{ text: availabilityTextArr.join(), xpath: row.availabilityText[0].xpath }];
         }
+        if (row.variants) {
+          const variantArray = row.variants.map((item) => {
+            return item.text;
+          });
+          row.variants = [{ text: variantArray.join('|'), xpath: row.variants[0].xpath }];
+        }
         if (row.alternateImages) {
           row.alternateImages.shift();
-          const countStr = row.alternateImages.length.toString;
-          row.secondaryImageTotal = [{ text: countStr, xpath: row.ingredientsList[0].xpath }];
         }
-        if (row.nameExtended && row.brandText) {
+        if (row.manufacturerImages) {
+          const manufacturerImagesArray = row.manufacturerImages.map((item) => {
+            return item.text;
+          });
+          row.manufacturerImages = [{ text: manufacturerImagesArray.join(' | '), xpath: row.manufacturerImages[0].xpath }];
+        }
+        if (row.nameExtended && row.brandText && row.quantity) {
+          row.nameExtended = [{ text: row.brandText[0].text + ' ' + row.nameExtended[0].text + ' ' + row.quantity[0].text, xpath: row.nameExtended[0].xpath }];
+        } else if (row.nameExtended && row.brandText) {
           row.nameExtended = [{ text: row.brandText[0].text + ' ' + row.nameExtended[0].text, xpath: row.nameExtended[0].xpath }];
+        }
+        if (row.variantInformation) {
+          const variantInformationArray = row.variantInformation.map((item) => {
+            return item.text;
+          });
+          row.variantInformation = [{ text: variantInformationArray.join('/'), xpath: row.variantInformation[0].xpath }];
+        }
+        if (row.description) {
+          const descriptionArray = row.description.map((item) => {
+            return typeof (item.text) === 'string' ? item.text.replace(/\n \n/g, ' : ').replace(/\n/g, '') : '||';
+          });
+          row.description = [{ text: descriptionArray.join(' || '), xpath: row.description[0].xpath }];
+        }
+        if (row.additionalDescription) {
+          const additionalDescriptionArray = row.additionalDescription.map((item) => {
+            return item.text;
+          });
+          row.additionalDescription = [{ text: additionalDescriptionArray.join('|'), xpath: row.additionalDescription[0].xpath }];
+        }
+        if (row.manufacturerImages) {
+          const manufacturerImagesArray = row.manufacturerImages.map((item) => {
+            return item.text;
+          });
+          row.manufacturerImages = [{ text: manufacturerImagesArray.join('|'), xpath: row.manufacturerImages[0].xpath }];
         }
         row = cleanUp(row);
       } catch (exception) {
