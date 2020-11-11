23aea6f2429d0a007892348a009d1268be515637
prasanti mylavarapu <prasanti.mylavarapu-od@partner.import.io>
2020-11-11 21:32:01 +0530

    move to qa1

--- src/library/product/search/stores/l/lomax/DK/execute.js
+++ src/library/product/search/stores/l/lomax/DK/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DK',
     store: 'lomax',
     domain: 'lomax.dk',
-    url: 'https://www.lomax.dk/soeg/?q=dug',
+    url: 'https://www.lomax.dk/soeg/?q={searchTerms}',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
