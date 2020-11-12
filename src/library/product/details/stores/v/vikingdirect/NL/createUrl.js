3715278d24886a56c8921f5c7dff1b6db2c13ce0
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-12 10:10:59 +0100

    created a template for the core extractor

---
+++ src/library/product/details/stores/v/vikingdirect/NL/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'vikingdirect.nl',
+    prefix: null,
+    url: null,
+    country: 'NL',
+    store: 'vikingdirect',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/v/vikingdirect/NL/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'NL',
+    store: 'vikingdirect',
+    domain: 'vikingdirect.nl',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
