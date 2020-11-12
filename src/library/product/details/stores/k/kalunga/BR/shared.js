0c3098b56ca9ec70b7ef18007e96eb6bdaec5ae4
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-11-12 11:28:59 +0200

    core

---
+++ src/library/product/details/stores/k/kalunga/BR/shared.js
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  const onlyNumbers = /[^\d,]+/g;
+  const currency = /[\d\., ]/g;
+  data.forEach(el => {
+    el.group.forEach(gr => {
+      console.log(gr, 'ssssssssssssssssssssssss');
+      gr.rpc[0].text = gr.rpc[0].text.replace(onlyNumbers, '');
+      gr.price[0].text = gr.price[0].text.replace(onlyNumbers, '');
+      gr.uniqCode[0].text = gr.uniqCode[0].text.replace(onlyNumbers, '');
+      if (gr.listPrice) gr.listPrice[0].text = gr.listPrice[0].text.replace(onlyNumbers, '');
+      gr.currency[0].text = gr.currency[0].text.replace(currency, '').charAt(1);
+    });
+  });
+  return data;
+};
+
+module.exports = { transform };
