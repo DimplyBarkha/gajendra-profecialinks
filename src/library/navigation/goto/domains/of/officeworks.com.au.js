20b17bad7f94452f77dbce55db769640e8c20d8d
Karolina <karolina.urbanska@tdcm.io>
2020-11-12 13:38:54 +0100

    Deployed and tested search extractor (#1439)

--- src/library/navigation/goto/domains/of/officeworks.com.au.js
+++ src/library/navigation/goto/domains/of/officeworks.com.au.js
@@ -8,4 +8,22 @@ module.exports = {
     store: 'officeworks',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    await context.setBlockAds(false);
+    await context.setAntiFingerprint(false);
+    await context.goto(url,
+      {
+        first_request_timeout: 50000,
+        block_ads: false,
+        timeout: 50000,
+        waitUntil: 'load',
+        load_all_resources: true,
+        images_enabled: true,
+        checkBlocked: true,
+      });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
--- src/library/product/search/stores/o/officeworks/AU/extract.js
+++ src/library/product/search/stores/o/officeworks/AU/extract.js
@@ -11,6 +11,36 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
     await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop !== 5000) {
+          await stall(1000);
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 5000) {
+            await stall(1000);
+            break;
+          }
+        }
+        function stall (ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await applyScroll(context);
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    await context.evaluate(async () => {
+      const allProducts = document.querySelectorAll('div[data-ref*="product-tile"]');
+      for (let i = 0; i < allProducts.length; i++) {
+        allProducts[i].focus();
+        await new Promise((resolve, reject) => setTimeout(resolve, 100));
+      }
+    });
     return await context.extract(productDetails, { transform });
   },
 };
