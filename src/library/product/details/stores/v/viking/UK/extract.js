ef8c64d40eb787bb31f0c5087ac25c7033e65768
Karolina <karolina.urbanska@tdcm.io>
2020-11-12 19:48:37 +0100

    Fixes after qa (#1267)

--- src/library/product/details/stores/v/viking/UK/extract.js
+++ src/library/product/details/stores/v/viking/UK/extract.js
@@ -12,6 +12,14 @@ module.exports = {
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     await new Promise(resolve => setTimeout(resolve, 5000));
     await context.evaluate(async () => {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    });
+    await context.evaluate(async () => {
+      const cookies = document.querySelector('button[id="footerCookiePolicyClose"]');
+      if (cookies) cookies.click();
+    });
+
+    await context.evaluate(async () => {
       function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
@@ -23,21 +31,39 @@ module.exports = {
       addElementToDocument('availability', availability);
       const pdfPresent = document.querySelector('div#productPage a.product-attachments-table__link') ? 'Yes' : 'No';
       addElementToDocument('pdfPresent', pdfPresent);
+      const tAndCs = document.querySelector('a[href*=terms-conditions]') ? 'Yes' : 'No';
+      addElementToDocument('tAndCs', tAndCs);
+      const privacyPolicy = document.querySelector('a[href*=privacy_notice]') ? 'Yes' : 'No';
+      addElementToDocument('privacyPolicy', privacyPolicy);
+      const customerService = document.querySelector('div.footer-links a[href="/en/customer-service"]') ? 'Yes' : 'No';
+      addElementToDocument('customerService', customerService);
+      const zoomIn = document.querySelector('div#s7viewer_container_inner div[data-component="ZoomInButton"]') ? 'Yes' : 'No';
+      addElementToDocument('zoomIn', zoomIn);
 
       const alternateImages = document.querySelectorAll('div#s7viewer_swatches_listbox div.s7thumb');
       for (let i = 1; i < alternateImages.length; i++) {
-        const img = alternateImages[i].getAttribute('style').replace(/.*url\("(https.*\?).*/g, '$1');
-        addElementToDocument('alternateImg', img);
+        if (alternateImages[i] && alternateImages[i].querySelector('div[type="image"]')) {
+          const img = alternateImages[i] ? alternateImages[i].getAttribute('style').replace(/.*url\("(https.*\?).*/g, '$1') : '';
+          addElementToDocument('alternateImg', img);
+        }
       }
+
+      const productId = document.querySelector('span[itemprop="sku"]') ? document.querySelector('span[itemprop="sku"]').innerText : '';
+      const videos = document.querySelectorAll('div#s7viewer_swatches_listbox div.s7thumb div[type="video"]');
+      for (let j = 1; j < videos.length + 1; j++) {
+        const video = `https://odeu.scene7.com/is/content/odeu13/%21${productId}_v${j}`;
+        addElementToDocument('video', video);
+      }
+
       const specifications = document.querySelectorAll('div#contentproductSpecifications tr');
-      let specificationsArr = [];
+      const specificationsArr = [];
       for (let j = 0; j < specifications.length; j++) {
-        specificationsArr.push(specifications[j].innerText.replace(/\s+/g, ' '));
+        const spec = specifications[j] && specifications[j].innerText ? specifications[j].innerText.replace(/\s+/g, ' ') : '';
+        specificationsArr.push(spec);
       }
       addElementToDocument('specifications', specificationsArr.join(' || '));
     });
     await new Promise(resolve => setTimeout(resolve, 5000));
-    console.log('waiting');
     return await context.extract(productDetails, { transform });
   },
 };
