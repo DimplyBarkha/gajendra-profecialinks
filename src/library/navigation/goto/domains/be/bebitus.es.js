59eae434869d73f81817929c8c3dd48fd4e060b1
PrinceYadav03 <73944479+PrinceYadav03@users.noreply.github.com>
2020-11-12 12:33:44 +0530

    Updated bebitus.es.js

--- src/library/navigation/goto/domains/be/bebitus.es.js
+++ src/library/navigation/goto/domains/be/bebitus.es.js
@@ -8,16 +8,4 @@ module.exports = {
     store: 'bebitus',
     zipcode: '',
   },
-  implementation: async ({ url }, { country, domain }, context, { productDetails }) => {
-    await context.setLoadImages(true);
-    await context.setBlockAds(false);
-    await context.setLoadAllResources(true);
-    await context.goto(url, {
-      block_ads: false,
-      load_all_resources: true,
-      images_enabled: true,
-      timeout: 100000,
-      waitUntil: 'networkidle0',
-    });
-  },
 };
