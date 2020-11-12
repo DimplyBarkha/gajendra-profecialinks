ae685ca1215d4b3c2b630cabb60081970d66596c
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-11-12 08:42:25 +0530

    colesonline_macquariePark core xpath added (#883)

---
+++ src/library/product/details/stores/c/colesonline_macquariePark/AU/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'shop.coles.com.au',
+    prefix: null,
+    url: 'https://shop.coles.com.au/a/waurn-ponds/everything/search/{id}?pageNumber=1',
+    country: 'AU',
+    store: 'colesonline_macquariePark',
+    zipcode: '',
+  },
+};
