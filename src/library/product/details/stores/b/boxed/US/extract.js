72eb1e575b525a8720aa8fa186ae35a94396fbbc
Karina Miernik <karina.miernik@gmail.com>
2020-11-12 15:41:53 +0100

    #1152 fixes after QA

--- src/library/product/details/stores/b/boxed/US/extract.js
+++ src/library/product/details/stores/b/boxed/US/extract.js
@@ -21,7 +21,6 @@ module.exports = {
     });
 
     await new Promise(resolve => setTimeout(resolve, 2000));
-
     await context.evaluate(async function () {
       function addElementToDocument (id, value, key) {
         const catElement = document.createElement('div');
@@ -31,11 +30,10 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       };
-
-      const dataFromJSON = document.querySelector('script[type*="json"]')
-        ? document.querySelector('script[type*="json"]').textContent : '';
-      const quantity = JSON.parse(dataFromJSON).disambiguatingDescription.split('-').filter(e => /\d/.test(e)).join(' ');
-      addElementToDocument('quantity', quantity, '#');
+      const quantitySecond = document.evaluate('//span[contains(@class, "a8ac-less")]/following-sibling::text()[1]', document, null, XPathResult.STRING_TYPE).stringValue;
+      const quantityFirst = document.evaluate('//span[contains(@class, "a8ac-less")]/preceding-sibling::text()[1]', document, null, XPathResult.STRING_TYPE).stringValue;
+      if (quantitySecond) addElementToDocument('quantity', quantitySecond, '#');
+      if (!quantitySecond) addElementToDocument('quantity', quantityFirst, '#');
 
       const brandLink = document.querySelector('section[data-bx="pdp-name"] a')
         ? document.querySelector('section[data-bx="pdp-name"] a') : null;
@@ -72,7 +70,11 @@ module.exports = {
       } else {
         addElementToDocument('isAvailable', 'In Stock', 'Yes');
       }
-
+      const sku = document.querySelector('meta#meta-og-url')
+        ? document.querySelector('meta#meta-og-url') : '';
+      const regex = /\/(\d+)\//;
+      // @ts-ignore
+      sku.setAttribute('sku', sku.getAttribute('content').match(regex)[1]);
       const allImages = document.querySelectorAll("div>a[aria-label*='Slide']");
       if (allImages.length) {
         for (let i = 0; i < allImages.length; i++) {
@@ -80,12 +82,27 @@ module.exports = {
           allImages[i].click();
           await new Promise(resolve => setTimeout(resolve, 1000));
           if (i !== 0) {
-            const regex = /https.*/;
+            const regex = /vpc.*/;
             const currentImage = document.querySelector(`div[data-index='${i}'] img[alt*='Product image']`);
             addElementToDocument(`addedImage${i}`, currentImage.getAttribute('src').match(regex)[0]);
           }
         }
       }
+      const terms = document.querySelector('a[href*="terms-conditions"]')
+        ? document.querySelector('a[href*="terms-conditions"]') : null;
+      if (terms.textContent.includes('Terms')) {
+        terms.setAttribute('terms', 'Yes');
+      } else terms.setAttribute('terms', 'No');
+      const policy = document.querySelector('a[href*="privacy-policy"]')
+        ? document.querySelector('a[href*="privacy-policy"]') : null;
+      if (policy.textContent.includes('Policy')) {
+        policy.setAttribute('policy', 'Yes');
+      } else policy.setAttribute('policy', 'No');
+
+      const couponText = document.querySelector('div[id*="couponId"] div[class*="e594"]')
+        ? document.querySelector('div[id*="couponId"] div[class*="e594"]') : null;
+      // @ts-ignore
+      if (couponText !== null) couponText.setAttribute('coupon', couponText.textContent.split(' ').filter(e => e !== 'Details').join(' '));
     });
 
     await context.extract(productDetails);
