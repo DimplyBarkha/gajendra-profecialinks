3727803f585fd8de0497ea8eec674f92f1eb888b
madhanimportio <madhan.selvaraj-odc@partner.import.io>
2020-11-12 19:22:06 +0530

    changes in details extractor yaml file

--- src/library/product/search/stores/r/rs-online/UK/paginate.js
+++ src/library/product/search/stores/r/rs-online/UK/paginate.js
@@ -4,7 +4,8 @@ module.exports = {
   parameterValues: {
     country: 'UK',
     store: 'rs-online',
-    nextLinkSelector: '#pagnNextString, #pagnNextLink, li a[class="paginationIcon nextPage "][title]',
+    // nextLinkSelector: '#pagnNextString, #pagnNextLink, li a[class="paginationIcon nextPage "][title]',
+    nextLinkSelector: '#pagnNextString, #pagnNextLink, li a[class="paginationIcon nextPage "][title], span[class="button_1fzFH0ah"][data-qa="next"]',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'body',
