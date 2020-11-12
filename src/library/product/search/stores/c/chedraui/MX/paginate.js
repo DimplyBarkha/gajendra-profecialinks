5c1d8abad4cc5d75bdaf1d5da2479626546c60f8
rajeev <rajeev.ranjan@synerzip.com>
2020-11-13 03:11:43 +0530

    1285 #1286 search and core issue fixed

--- src/library/product/search/stores/c/chedraui/MX/paginate.js
+++ src/library/product/search/stores/c/chedraui/MX/paginate.js
@@ -4,15 +4,15 @@ module.exports = {
   parameterValues: {
     country: 'MX',
     store: 'chedraui',
-    //nextLinkSelector: 'li.pagination-next a',
+    nextLinkSelector: 'li.pagination-next a',
     // mutationSelector: null,
     // spinnerSelector: null,
     loadedSelector: 'div[class="product__list--wrapper"]',
     // ul[id="plp_display"]
     noResultsXPath: '//div[@class="not-found-text"]',
-    openSearchDefinition: {
-      template: 'https://www.chedraui.com.mx/search?text={searchTerms}&page={page-1}&pageSize={48}',
-    },
+    // openSearchDefinition: {
+    //   template: 'https://www.chedraui.com.mx/search?text={searchTerms}&page={page-1}&pageSize={48}',
+    // },
     domain: 'chedraui.com.mx',
     zipcode: "''",
   },
