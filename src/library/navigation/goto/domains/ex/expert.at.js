6dae93173336474f20cd8942f00093e3e549d6dc
osmarghyadeep <arghyadep.g@partner.import.io>
2020-11-13 05:14:13 +0530

    Updated the code to fix videos and specification

--- src/library/navigation/goto/domains/ex/expert.at.js
+++ src/library/navigation/goto/domains/ex/expert.at.js
@@ -9,10 +9,11 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameterValues, context, dependencies) => {
-    // await context.setAntiFingerprint(false);
-    // await context.setLoadAllResources(true);
-    // await context.setBlockAds(false);
-    // await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
-    await context.goto(`${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setAntiFingerprint(false);
+    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    //await context.goto(`${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
   },
 };
--- src/library/product/details/stores/e/expert/AT/extract.js
+++ src/library/product/details/stores/e/expert/AT/extract.js
@@ -34,6 +34,8 @@ module.exports = {
       if (url !== null) {
         await context.goto(`${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
       }
+    } else {
+      console.log('either input id is null or input object is null');
     }
 
     let content = null;
@@ -42,11 +44,11 @@ module.exports = {
     const link = await context.evaluate(async function () {
       return window.location.href;
     });
-
+    console.log(`link is - ${link}`);
     const apiManufCall = await context.evaluate(async function () {
       return document.querySelector('iframe#loadbeeTabContent') ? document.querySelector('iframe#loadbeeTabContent').getAttribute('src') : null;
     });
-
+    console.log(`apiManufCall is - ${apiManufCall}`);
     if (apiManufCall) {
       const obj = await sharedhelpers.goToiFrameLink(apiManufCall, link, 'body img', 'src');
       content = obj.content;
@@ -72,14 +74,50 @@ module.exports = {
     const urlLink = await context.evaluate(async function () {
       return window.location.href;
     });
-
+    console.log(`urlLink is - ${urlLink}`);
     sharedhelpers.addHiddenInfo('iio_product_url', urlLink);
 
-    let ratingReviews = await sharedhelpers.getEleByXpath('//script[contains(@type, "application/ld+json") and contains(text(), "Product")]');
+    let ratingReviews = '';
+
+    const scriptXpath = '//script[contains(@type, "application/ld+json") and contains(text(), "Product")]';
+    const isScriptLoaded = await context.evaluate(async function (xpath, reloadSec, maxTime) {
+      window.scrollTo(0,document.body.scrollHeight);
+      async function timeout(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+      }
+      //let element = document.querySelector(selector);
+      let element = document.evaluate(xpath, document, null, 7, null);
+      let elementVal = {};
+      if(element.snapshotLength > 0) {
+        elementVal = element.snapshotItem(0);
+      }
+      let count = 0;
+      while (elementVal === null) {
+        count = count + reloadSec;
+        element = document.evaluate(xpath, document, null, 7, null);
+        if(element && element.snapshotLength > 0) {
+          elementVal = element.snapshotItem(0);
+        }
+        await timeout(reloadSec);
+        if (count >= maxTime) {
+          console.log("script with rating info not found");
+          return false;
+        }
+      }
+      return true;
+    }, scriptXpath, 500, 30000);
+
+    if(isScriptLoaded) {
+      ratingReviews = await sharedhelpers.getEleByXpath('//script[contains(@type, "application/ld+json") and contains(text(), "Product")]');
+    }
 
     console.log('ratingReviews');
     console.log(ratingReviews);
-    ratingReviews = Object.keys(JSON.parse(ratingReviews)).length ? JSON.parse(ratingReviews) : null;
+    if(ratingReviews) {
+      ratingReviews = Object.keys(JSON.parse(ratingReviews)).length ? JSON.parse(ratingReviews) : null;
+    } else {
+      console.log('script containing rating is not loaded');
+    }
 
     console.log('ratingReviews');
     console.log(ratingReviews);
