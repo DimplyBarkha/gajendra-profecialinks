518ebbc56af4242a59c36349b9eeb3f4167e4976
Wayne E <bartekstafiej2@gmail.com>
2020-11-12 18:59:08 +0100

    #1353 added rank, rankOrganic and productUrl implementation

--- src/library/product/search/stores/j/jumbo/NL/extract.js
+++ src/library/product/search/stores/j/jumbo/NL/extract.js
@@ -1,4 +1,23 @@
+async function implementation (inputs, parameters, context, dependencies) {
+  const { productDetails } = dependencies;
 
+  await context.evaluate(() => {
+    const productUrlAll = document.querySelectorAll('.list-methods a.opposite');
+    let productUrl;
+    for (let i = 0; i < productUrlAll.length; i++) {
+      if (productUrlAll[i].href.includes('https')) {
+        productUrl = productUrlAll[i].href;
+      } else {
+        productUrl = 'https://www.bol.com' + productUrlAll[i].href;
+      }
+
+      document.querySelectorAll('.list-methods a.opposite')[i].setAttribute('productUrl', productUrl);
+      document.querySelectorAll('div[analytics-tag="product card"]')[i].setAttribute('rank', `${i + 1}`);
+      document.querySelectorAll('div[analytics-tag="product card"]')[i].setAttribute('rankOrganic', `${i + 1}`);
+    };
+  });
+  return await context.extract(productDetails);
+};
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +27,5 @@ module.exports = {
     domain: 'jumbo.com',
     zipcode: '',
   },
+  implementation,
 };
