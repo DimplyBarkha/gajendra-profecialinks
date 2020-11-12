648ea0966d2d43bfc5051c2d32f1b935209a4237
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-11-12 19:16:46 +0200

    add search

--- src/library/product/search/stores/a/ah/BE/paginate.js
+++ src/library/product/search/stores/a/ah/BE/paginate.js
@@ -10,7 +10,7 @@ module.exports = {
     loadedSelector: 'div[data-testhook="search-lane"]',
     noResultsXPath: null,
     openSearchDefinition: {
-      template: 'https://www.ah.be/zoeken/api/products/search?page=1&size=36&query={SearchTerms}',
+      template: 'https://www.ah.be/zoeken?query={searchTerms}&page={page}',
     },
     domain: 'ah.be',
     zipcode: '',
