1134409195fb5b848d0e6bc86a33322b5189d112
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-13 23:13:21 +0100

    created a template for search extractor

---
+++ src/library/product/search/stores/z/zalando/DE/paginate.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'DE',
+    store: 'zalando',
+    nextLinkSelector: null,
+    nextLinkXpath: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    loadedXpath: null,
+    noResultsXPath: null,
+    stopConditionSelectorOrXpath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'zalando.de',
+    zipcode: '',
+  },
+};
