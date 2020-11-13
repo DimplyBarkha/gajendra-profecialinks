dd6f35b51c6940f92a8634854fd5efef436401c1
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-13 13:16:39 +0530

    devwhiskydetails

--- src/library/product/details/stores/w/whisky/DE/extract.js
+++ src/library/product/details/stores/w/whisky/DE/extract.js
@@ -1,11 +1,11 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'DE',
     store: 'whisky',
-    transform: null,
+    transform,
     domain: 'whisky.de',
-    zipcode: '',
   },
 };
