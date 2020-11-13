bb5f2147777df3b114896aa9504c6206d38a5b21
Julia Lojek <julialojek@gmail.com>
2020-11-13 11:21:37 +0100

    petsmart search extractor

---
+++ src/library/navigation/goto/domains/pe/petsmart.ca.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'petsmart.ca',
+    timeout: null,
+    country: 'CA',
+    store: 'petsmart',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/p/petsmart/CA/extract.js
@@ -0,0 +1,46 @@
+const { transform } = require('../../../../shared');
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'CA',
+    store: 'petsmart',
+    transform: transform,
+    domain: 'petsmart.ca',
+    zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+
+    await context.evaluate(async () => {
+      const currentUrl = window.location.href;
+      document.querySelector('body').setAttribute('searchurl', currentUrl);
+
+      // rank
+      const products = document.querySelectorAll('ul#search-result-items li');
+      products.forEach((product, index) => {
+        product.setAttribute('rankorganic', `${index + 1}`);
+
+        // rating
+        let ratingValue = 0;
+        const stars = product.querySelectorAll('div.rated-stars-container img');
+        if (stars.length > 0) {
+          stars.forEach(star => {
+            let starValue = 0;
+            if (star.getAttribute('src').includes('full')) {
+              starValue = 1;
+            } else {
+              starValue = Number(star.getAttribute('src').slice(-11, -10)) / 10;
+            }
+            ratingValue += starValue;
+          });
+          const ratingAttribute = ratingValue.toFixed(1).replace('.', ',');
+          product.setAttribute('rating', ratingAttribute);
+        }
+      });
+    });
+
+    return await context.extract(productDetails, { transform });
+  },
+};
