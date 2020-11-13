c05c81e33b302ba4ed17f35bebcc0cc4ed1dabad
SS <toaranjan@gmail.com>
2020-11-13 12:02:13 +0530

    #935, 936 QA fixes

--- src/library/product/search/stores/b/beautye/IT/paginate.js
+++ src/library/product/search/stores/b/beautye/IT/paginate.js
@@ -4,15 +4,16 @@ module.exports = {
   parameterValues: {
     country: 'IT',
     store: 'beautye',
-    // nextLinkSelector: '#amasty-shopby-product-list > div:nth-child(4) > div.pages > ul > li.item.pages-item-next > a',
+    nextLinkSelector: '#amasty-shopby-product-list > div:nth-child(4) > div.pages > ul > li.item.pages-item-next > a',
     // mutationSelector: null,
     // spinnerSelector: null,
     loadedSelector: 'div.products-grid', // 'li.item.product.product-item',
     noResultsXPath: '//div[@class="message notice"]/div',
     // openSearchDefinition: null,
-    openSearchDefinition: {
-      template: 'https://www.beautye.it/search/{searchTerms}?p={page}',
-    },
+    // openSearchDefinition: {
+    //  template: 'https://www.beautye.it/catalogsearch/result/?q={searchTerms}&p={page}',
+      // template: 'https://www.beautye.it/search/{searchTerms}?p={page}',
+    // },
     domain: 'beautye.it',
     zipcode: '',
   },
