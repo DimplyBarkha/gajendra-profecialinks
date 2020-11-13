3e166b047916135b15f4090e1d1a9fa13827fcf4
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-13 15:26:07 +0100

    created a template for search and core extractor

---
+++ src/library/product/details/stores/m/mecca/AU/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'mecca.com.au',
+    prefix: null,
+    url: null,
+    country: 'AU',
+    store: 'mecca',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/m/mecca/AU/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'AU',
+    store: 'mecca',
+    domain: 'mecca.com.au',
+    zipcode: '',
+  },
+};
