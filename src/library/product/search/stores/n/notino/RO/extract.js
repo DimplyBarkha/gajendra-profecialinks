13adfd39a82107bf2a799c4e2f057579f105695a
Narendra Ghanta <narendra_g@hcl.com>
2020-11-13 17:15:05 +0530

    QA1isuues

--- src/library/product/search/stores/n/notino/RO/extract.js
+++ src/library/product/search/stores/n/notino/RO/extract.js
@@ -10,6 +10,14 @@ module.exports = {
   },
 
   implementation: async ({ inputString }, { country, store, transform: transformParam }, context, { productDetails }) => {
+    await context.evaluate(async () => {
+      while (document.querySelector('a[class="btn btn--secondary"]')) {
+        // @ts-ignore
+        document.querySelector('a[class="btn btn--secondary"] ').click();
+        // eslint-disable-next-line promise/param-names
+        await new Promise(r => setTimeout(r, 1000));
+      }
+    });
     async function addUrl () {
       function addHiddenDiv (id, content) {
         const newDiv = document.createElement('div');
@@ -24,31 +32,4 @@ module.exports = {
     await context.evaluate(addUrl);
     return await context.extract(productDetails, { transform: transformParam });
   },
-  // implementation: async (inputs, parameters, context, dependencies) => {
-  //   const { transform } = parameters;
-  //   const { productDetails } = dependencies;
-  //   await context.evaluate(() => {
-  //     function addHiddenDiv (id, content, index) {
-  //       const newDiv = document.createElement('div');
-  //       newDiv.id = id;
-  //       newDiv.textContent = content;
-  //       newDiv.style.display = 'none';
-  //       const originalDiv = document.querySelectorAll('ul#productsList li.item a')[index];
-  //       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-  //     }
-  //     const product = document.querySelectorAll('ul#productsList li.item');
-  //     const URL = window.location.href;
-  //     for (let i = 0; i < product.length; i++) {
-  //       const headerText = product[i].querySelector('ul#productsList li.item h2 span.name').textContent;
-  //       // // @ts-ignore
-  //       // aggrating = aggrating !== null ? aggrating.classList : '';
-  //       // aggrating = aggrating[2] !== undefined ? aggrating[2].split('-')[2] : '';
-  //       // // @ts-ignore
-  //       // const productUrl = product[i].querySelector('span.product-name b a').href;
-  //       addHiddenDiv('pd_productText', headerText, i);
-  //       addHiddenDiv('pd_url', URL, i);
-  //     }
-  //   });
-  //   return await context.extract(productDetails, { transform });
-  // },
 };
