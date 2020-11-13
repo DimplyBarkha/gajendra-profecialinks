a384d13577416a2b727069336882813ee2f26ac1
unknown <shalini.kumari-odc@partner.import.io>
2020-11-13 23:08:15 +0530

    kicks

---
+++ src/library/navigation/goto/domains/ki/kicks.se.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'kicks.se',
+    timeout: 60000,
+    country: 'SE',
+    store: 'kicks',
+    zipcode: '',
+  },
+};
