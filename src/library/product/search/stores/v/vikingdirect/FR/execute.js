1ccf6ed656760d9b9f927657f4f15a24f95ba0e1
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2020-11-13 09:52:09 +0100

    Inserted selectors and deployed search extractor (#1501)

--- src/library/product/search/stores/v/vikingdirect/FR/execute.js
+++ src/library/product/search/stores/v/vikingdirect/FR/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'FR',
     store: 'vikingdirect',
     domain: 'vikingdirect.fr',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.vikingdirect.fr/catalog/search.do?Ntt={searchTerms}',
+    loadedSelector: 'div.clearfix.block.top',
+    noResultsXPath: '//h1[contains(text(), "Mince ! Rien ne correspond à ")]|//div[@class="skuTopDetailsContainer"]',
     zipcode: '',
   },
 };
