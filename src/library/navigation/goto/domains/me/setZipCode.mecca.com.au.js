bb3c42f15c71c20e3e36601bc348afd887deafa2
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-11-13 23:15:55 +0100

    created a template for search extractor

---
+++ src/library/navigation/goto/domains/me/setZipCode.mecca.com.au.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'AU',
+    domain: 'mecca.com.au',
+    store: 'mecca',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/m/mecca/AU/paginate.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'AU',
+    store: 'mecca',
+    nextLinkSelector: null,
+    nextLinkXpath: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    loadedXpath: null,
+    noResultsXPath: null,
+    stopConditionSelectorOrXpath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'mecca.com.au',
+    zipcode: '',
+  },
+};
