460c6260237d5659e2dced3d11465d4894fdbc22
Bartosz Kurzawa <kurzawa.bart@gmail.com>
2020-11-13 13:15:42 +0100

    Adjusted rating field

--- src/library/product/search/stores/m/mediamarkt/CH/extract.js
+++ src/library/product/search/stores/m/mediamarkt/CH/extract.js
@@ -9,14 +9,15 @@ async function implementation (inputs, parameters, context, dependencies) {
   await context.evaluate(() => {
     // @ts-ignore
     const currency = document.querySelector('div.price-range-slider + div > label').innerText;
-    const allProducts = document.querySelectorAll('div.product-wrapper');
+    const allProducts = document.querySelectorAll('ul.products-list > li:not([class])');
     allProducts.forEach((product, index) => {
       const pictureUrl = `https:${product.querySelector('aside.product-photo > figure > a > img').getAttribute('src')}`;
+      const rating = product.querySelector('div.rating > div').getAttribute('class').match(/-(\d(-\d)?)/)[1].replace('-', '.');
       // @ts-ignore
       const price = product.querySelector('div.price-box > div').innerText.replace(/\n/g, '');
       product.setAttribute('product-picture', pictureUrl);
       product.setAttribute('product-price', `${price} ${currency}`);
-      product.setAttribute('rank', `${index + 1}`);
+      product.setAttribute('product-rating', rating);
     });
 
     const productUrl = document.querySelectorAll('h2 > a');
