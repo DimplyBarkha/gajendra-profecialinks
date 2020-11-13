1975efb6e116ff50fc6735db34406a66cbf4ff78
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-13 13:25:07 +0530

    #1535 preparing for development

--- src/library/navigation/goto/domains/ma/macro.cz.js
+++ src/library/navigation/goto/domains/ma/macro.cz.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'macro.cz',
-    timeout: null,
+    timeout: 50000,
     country: 'CZ',
     store: 'macro',
     zipcode: '',
