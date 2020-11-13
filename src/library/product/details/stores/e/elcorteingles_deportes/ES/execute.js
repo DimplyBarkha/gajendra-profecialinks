e149a23fbe242c563b3b3a71e2a264b5e315f979
Prateek3018 <69887481+Prateek3018@users.noreply.github.com>
2020-11-14 03:03:07 +0530

    updated timeout

--- src/library/product/details/stores/e/elcorteingles_deportes/ES/execute.js
+++ src/library/product/details/stores/e/elcorteingles_deportes/ES/execute.js
@@ -26,7 +26,7 @@ module.exports = {
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    }, { timeout: 30000 }, parameters.loadedSelector, parameters.noResultsXPath);
   }
 
   // TODO: Check for not found?
