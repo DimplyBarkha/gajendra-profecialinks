a2039663542481aae75fb5add4e618e41c4fdb95
Karina Miernik <karina.miernik@gmail.com>
2020-11-13 10:36:38 +0100

    #1483 xpaths added

--- src/library/product/search/stores/s/salontopper/NL/extract.js
+++ src/library/product/search/stores/s/salontopper/NL/extract.js
@@ -3,19 +3,30 @@ const { transform } = require('../../../../shared');
 async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+  await context.evaluate(async () => {
+    // @ts-ignore
+    if (window !== undefined) {
+      return window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    }
+  });
+
+  await new Promise((resolve, reject) => setTimeout(resolve, 2000));
 
   await context.evaluate(() => {
-    // function addElementToDocument (id, value) {
-    //   const catElement = document.createElement('div');
-    //   catElement.id = id;
-    //   catElement.innerText = value;
-    //   catElement.style.display = 'none';
-    //   document.body.appendChild(catElement);
-    // };
+    const prefix = 'https://www.salontopper.nl';
+    const productUrl = document.querySelectorAll('div[class*="product-gallery"] a[href*="/product"]');
+    productUrl.forEach(e => e.setAttribute('productUrl', prefix.concat(e.getAttribute('href'))));
 
-    // const manufacturer = document.querySelectorAll('div.title')
-    //   ? document.querySelectorAll('div.title') : [];
+    const imageUrl = document.querySelectorAll('div.image img');
+    imageUrl.forEach(e => e.setAttribute('image', prefix.concat(e.getAttribute('src'))));
 
+    const titles = document.querySelectorAll('div.title')
+      ? document.querySelectorAll('div.title') : [];
+    // @ts-ignore
+    [...titles].map(e => {
+      const text = e.innerText.split(' ')[0];
+      e.setAttribute('brand', text);
+    });
     function addProp (selector, iterator, propName, value) {
       document.querySelectorAll(selector)[iterator].setAttribute(propName, value);
     }
