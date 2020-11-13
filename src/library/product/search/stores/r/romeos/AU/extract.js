02735997463985b1b8dad922a3294fa9be970a61
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-11-13 08:32:16 +0100

    - Finished working on Romeos AU search extractor

--- src/library/product/search/stores/r/romeos/AU/extract.js
+++ src/library/product/search/stores/r/romeos/AU/extract.js
@@ -9,18 +9,4 @@ module.exports = {
     domain: 'martinplace.romeosonline.com.au',
     zipcode: '',
   },
-  implementation: async (inputs, parameters, context, { productDetails }) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    await context.evaluate(async () => {
-      const productsPerPage = 48;
-      const pageElem = document.querySelector('em[aria-current="page"]');
-      const pageNum = pageElem && pageElem.textContent ? parseInt(pageElem.textContent, 10) : 1;
-      const products = document.querySelectorAll('div[class="TalkerGrid__Item"]');
-      for (let i = 0; i < products.length; i++) {
-        const rankOrganic = (pageNum - 1) * productsPerPage + (i + 1);
-        products[i].setAttribute('rankOrganic', rankOrganic.toString());
-      }
-    });
-    return await context.extract(productDetails);
-  },
 };
