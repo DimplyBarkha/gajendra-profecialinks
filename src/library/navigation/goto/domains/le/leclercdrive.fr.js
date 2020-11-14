641f1f4ff457043fba10cad03805dbc132ef6e5a
Bindu Datla <bindu.datla@import.io>
2020-11-13 23:57:10 -0500

    updates to xpaths

--- src/library/navigation/goto/domains/le/leclercdrive.fr.js
+++ src/library/navigation/goto/domains/le/leclercdrive.fr.js
@@ -8,4 +8,53 @@ module.exports = {
     store: 'leclerc',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.captureRequests();
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+
+    const responseStatus = await context.goto(url, {
+      firstRequestTimeout: 60000,
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+      antiCaptchaOptions: {
+        type: 'IMAGECAPTCHA',
+      },
+    });
+    await new Promise((resolve) => setTimeout(resolve, 70000));
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
+    let captchaFrame = "iframe[_src*='captcha']:not([title]), iframe[src*='captcha']:not([title]), span.recaptcha-anchor";
+    try {
+      await context.waitForSelector(captchaFrame);
+    } catch (error) {
+      console.log('error: without underscore ', error);
+    }
+
+    console.log('captchaFrame', captchaFrame);
+    const hasCaptcha = async (selector) => {
+      return await context.evaluate(async (captchaSelector) => {
+        return Boolean(document.querySelector(captchaSelector));
+      }, selector);
+    };
+    const isCaptchaFramePresent = await hasCaptcha(captchaFrame);
+
+    if (isCaptchaFramePresent) {
+      console.log('isCaptcha', true);
+      await context.waitForNavigation(timeout);
+      // @ts-ignore
+      // eslint-disable-next-line no-undef
+      //await context.evaluateInFrame('iframe', () => grecaptcha.execute());
+      await context.solveCaptcha({ type: 'RECAPTCHA', inputElement: '.recaptcha-anchor' });
+      console.log('solved captcha, waiting for page change');
+      await context.waitForNavigation(timeout);
+    }
+   
+  },
 };
--- src/library/product/search/stores/l/leclerc/FR/execute.js
+++ src/library/product/search/stores/l/leclerc/FR/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'leclercdrive.fr',
     url: 'https://fd1-courses.leclercdrive.fr/magasin-606801-Saint-Louis/recherche.aspx?TexteRecherche={searchTerms}',
     loadedSelector: "div[id*='_ProductsList']",
-    noResultsXPath: "//div[contains(@class,'PasDeResultats')",
+    noResultsXPath: "//div[contains(@class,'PasDeResultats')]",
     zipcode: '',
   },
 };
