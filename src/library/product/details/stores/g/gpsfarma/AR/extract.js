796e4f23281731889ae30b17276e7da144072f42
ganesh <ganesh@profecialinks.com>
2020-11-14 06:53:46 +0530

    core part done

--- src/library/product/details/stores/g/gpsfarma/AR/extract.js
+++ src/library/product/details/stores/g/gpsfarma/AR/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('./format');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AR',
     store: 'gpsfarma',
-    transform: null,
+    transform,
     domain: 'gpsfarma.com',
     zipcode: '',
   },
---
+++ src/library/product/details/stores/g/gpsfarma/AR/format.js
@@ -0,0 +1,60 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+    const cleanUp = (data, context) => {
+      const clean = text => text.toString()
+        .replace(/\r\n|\r|\n/g, ' ')
+        .replace(/&amp;nbsp;/g, ' ')
+        .replace(/&amp;#160/g, ' ')
+        .replace(/\u00A0/g, ' ')
+        .replace(/\s{2,}/g, ' ')
+        .replace(/"\s{1,}/g, '"')
+        .replace(/\s{1,}"/g, '"')
+        .replace(/^ +| +$|( )+/g, ' ')
+        // eslint-disable-next-line no-control-regex
+        .replace(/[\x00-\x1F]/g, '')
+        .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }))));
+      return data;
+    };
+    for (const { group } of data) {      
+      for (const row of group) {
+        let p_sku = '';        
+        if (row.description) {
+            let info = [];          
+            row.description.forEach(item => {
+              info.push(item.text.replace(/(\s*\n\s*)+/g, ' ').trim());            
+            });
+            row.description = [{'text':info.join(' | '),'xpath':row.description[0].xpath}];
+        }
+        
+        if (row.specifications) {
+            let info = [];          
+            row.specifications.forEach(item => {
+                info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());            
+            });          
+            row.specifications = [{'text':info.join(' || '),'xpath':row.specifications[0].xpath}];          
+        }
+        
+        // if (row.nameExtended) {
+        //   row.nameExtended.forEach(item => {
+        //     let j_data = JSON.parse(item.text);
+        //     if (j_data['name'] && j_data['brand']['name']){
+        //       item.text = j_data['name'] + " - " + j_data['brand']['name'];
+        //     }            
+        //   });
+        // }
+        if (row.descriptionBullets) {
+          row.descriptionBullets = [{'text':row.descriptionBullets.length,'xpath':row.descriptionBullets[0].xpath}];
+        }                
+      }
+    }
+    return cleanUp(data);
+  };
+  
+  module.exports = { transform };
