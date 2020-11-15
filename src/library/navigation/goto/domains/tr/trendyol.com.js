abe5e609a2ad04a3a8510953bbb2c536845b08b2
sunil-synerzip <sunil.kshirasagar@synerzip.com>
2020-11-15 23:57:23 +0530

    trendyol search extractor

--- src/library/navigation/goto/domains/tr/trendyol.com.js
+++ src/library/navigation/goto/domains/tr/trendyol.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'trendyol.com',
-    timeout: null,
+    timeout: 200000,
     store: 'trendyol',
     zipcode: '',
     country: 'TR',
