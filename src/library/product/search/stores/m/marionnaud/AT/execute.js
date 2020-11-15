9163408101ac658ab687725c9d14cb9936041b6a
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-15 15:10:49 +0530

    #1626 start working on AT adn getting exection issue

--- src/library/product/search/stores/m/marionnaud/AT/execute.js
+++ src/library/product/search/stores/m/marionnaud/AT/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'marionnaud',
     domain: 'marionnaud.at',
     url: 'https://www.marionnaud.at/search?text={searchTerms}',
-    loadedSelector: 'div.container-fluid>div.more-data-loader > div.container-fluid, container-fluid--max-width',
+    loadedSelector: '',
     noResultsXPath: 'not(//div[@class="container-fluid"]/div[@class="more-data-loader"])',
     zipcode: '',
   },
