84beeecc0d539637a86a81670c30f657641b1e63
unknown <ramraut25@gmail.com>
2020-11-15 15:48:28 +0530

    Added implementation for video attribute

--- src/library/navigation/goto/domains/ne/netonnet.no.js
+++ src/library/navigation/goto/domains/ne/netonnet.no.js
@@ -8,4 +8,22 @@ module.exports = {
     store: 'netonnet',
     zipcode: '',
   },
+  implementation: async (
+    { url, zipcode, storeId },
+    parameters, context, dependencies,
+    ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    
+    await context.goto(url, {
+    timeout: timeout,
+    waitUntil: 'load',
+    checkBlocked: true,
+    });
+    },
 };
--- src/library/product/details/stores/n/netonnet/NO/extract.js
+++ src/library/product/details/stores/n/netonnet/NO/extract.js
@@ -16,17 +16,38 @@ module.exports = {
   ) => {
     await new Promise((resolve, reject) => setTimeout(resolve, 4000));
     await context.evaluate(async function () {
+
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+
+      const iframeDoc = document.querySelector('iframe.videoly-box') && document.querySelector('iframe.videoly-box').contentWindow;
+      if (iframeDoc && iframeDoc.document && iframeDoc.document.body && iframeDoc.document.body.innerHTML) {
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(iframeDoc.document.body.innerHTML, 'text/html');
+        const videoElements = htmlDoc.querySelectorAll('div.b-video-cover');
+        videoElements && videoElements.forEach(item => {
+          let vidURL = item.getAttribute('style').replace(/(.*) url((.*)\/vi\/(.*)\/(.+))/g, 'https://www.youtube.com/watch?v=$4')
+          addElementToDocument('videoUrls', vidURL);
+        });
+      }
+
+
       const overlay = document.getElementById('headingOne');
       if (overlay !== undefined) {
         overlay.click();
       }
-      
-          const collapseFive = document.querySelector('div[data-target="#collapseFive"]')      
-          collapseFive.classList.remove("collapsed");
-          document.getElementById('collapseFive').classList.add('in');
-      
+
+      const collapseFive = document.querySelector('div[data-target="#collapseFive"]')
+      collapseFive.classList.remove("collapsed");
+      document.getElementById('collapseFive').classList.add('in');
+
     });
-  
+
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.extract(productDetails, { transform });
