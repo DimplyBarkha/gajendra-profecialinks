b64fb58f89a7b383b86e15a4240dc078762074e7
Bindu Datla <bindu.datla@import.io>
2020-11-15 01:58:24 -0500

    update

--- src/library/product/details/stores/b/bijenkorf/NL/extract.js
+++ src/library/product/details/stores/b/bijenkorf/NL/extract.js
@@ -1,6 +1,6 @@
 const { transform } = require('../format');
 
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -9,21 +9,25 @@ async function implementation (
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await context.click('button.dbk-accordion__header');
-  await context.waitForSelector('div.dbk-accordion__body');
-  await context.waitForSelector('footer.dbk-footer');
-  await context.evaluate(async () => {
-    const element = document.querySelector('footer.dbk-footer');
-    if (element) {
-      element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'end',
-        inline: 'nearest',
-      });
-      await new Promise(resolve => setTimeout(resolve, 10000));
-    }
-  });
-
+  try {
+    await context.click('button.dbk-accordion__header');
+    await context.waitForSelector('div.dbk-accordion__body');
+    await context.waitForSelector('footer.dbk-footer');
+    await context.evaluate(async () => {
+      const element = document.querySelector('footer.dbk-footer');
+      if (element) {
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'end',
+          inline: 'nearest',
+        });
+        await new Promise(resolve => setTimeout(resolve, 10000));
+      }
+    });
+  }
+  catch (error) {
+    console.log(error);
+  }
   return await context.extract(productDetails, { transform });
 }
 
