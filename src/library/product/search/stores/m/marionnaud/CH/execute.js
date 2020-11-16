a49af90425d3a28d0d96116af7aa3bdc643d1c7a
Julia Lojek <julialojek@gmail.com>
2020-11-16 12:27:34 +0100

    #1587 search extractor logic and fields

--- src/library/product/search/stores/m/marionnaud/CH/execute.js
+++ src/library/product/search/stores/m/marionnaud/CH/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'marionnaud',
     domain: 'marionnaud.ch',
     url: 'https://www.marionnaud.ch/de/search?text={searchTerms}',
-    loadedSelector: 'div.product-tile',
-    noResultsXPath: null,
+    loadedSelector: 'div.more-data-loader div.product-tile img.product-tile__image',
+    noResultsXPath: '//section[contains(@data-position, "emptySearchResult")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/m/marionnaud/CH/extract.js
+++ src/library/product/search/stores/m/marionnaud/CH/extract.js
@@ -1,11 +1,62 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'CH',
     store: 'marionnaud',
-    transform: null,
+    transform: transform,
     domain: 'marionnaud.ch',
     zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+
+    // removing cookies popup
+    await context.waitForNavigation();
+    const isPopupPresent = await context.evaluate(async () => {
+      return document.querySelector('button#onetrust-accept-btn-handler');
+    });
+    if (isPopupPresent) {
+      await context.click('button#onetrust-accept-btn-handler');
+    }
+    await context.waitForNavigation();
+
+    // load more pagination implementation
+    let isLoadMoreButtonPresent = await context.evaluate(async () => {
+      const button = document.querySelector('button.more-data-loader__btn');
+      if (button) {
+        return !button.getAttribute('style');
+      } else {
+        return false;
+      }
+    });
+    let productsAmount = await context.evaluate(async () => {
+      return document.querySelectorAll('div.more-data-loader div.product-tile').length;
+    });
+    while (isLoadMoreButtonPresent && productsAmount < 150) {
+      await context.click('button.more-data-loader__btn span');
+      await context.waitForSelector('button.more-data-loader__btn', { timeout: 2000 });
+      isLoadMoreButtonPresent = await context.evaluate(async () => {
+        return !document.querySelector('button.more-data-loader__btn').getAttribute('style');
+      });
+      productsAmount = await context.evaluate(async () => {
+        return document.querySelectorAll('div.more-data-loader div.product-tile').length;
+      });
+    }
+
+    var dataRef = await context.extract(productDetails, { transform });
+
+    dataRef[0].group.forEach((row) => {
+      if (row.productUrl) {
+        row.productUrl[0].text = `https://marionnaud.ch${row.productUrl[0].text}`;
+      }
+      if (row.thumbnail) {
+        row.thumbnail[0].text = `https://marionnaud.ch${row.thumbnail[0].text}`;
+      }
+    });
+
+    return dataRef;
   },
 };
