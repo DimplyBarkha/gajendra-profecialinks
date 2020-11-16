ebf5218e02e304942d5a8f45e90e86e24d1287e4
unknown <suryakanta.s@osmosys.asia>
2020-11-16 11:52:51 +0530

    commiting changes

--- src/library/product/search/stores/b/Bigbasket_Mweb/IN/execute.js
+++ src/library/product/search/stores/b/Bigbasket_Mweb/IN/execute.js
@@ -15,29 +15,29 @@ async function implementation (
   console.log('URL :', responseStatus.url);
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
-        return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
     }, {
-        timeout: 10000,
+      timeout: 10000,
     }, parameters.loadedSelector, parameters.noResultsXPath);
   }
-    // Apply scroll
-    const applyScroll = async function (context) {
-        await context.evaluate(async function () {
-            let scrollTop = 0;
-            while (scrollTop !== 20000) {
-                const products = document.evaluate('//img[@data-sizes="auto"]/@src', document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-                const productsCount = products.snapshotLength;
-                console.log('Length: ' + productsCount);
-                await stall(1000);
-                scrollTop += 1000;
-                window.scroll(0, scrollTop);
-                if (scrollTop === 20000 || productsCount > 160) {
-                    await stall(10000);
-                    await stall(5000);
-                }
-            }
-            function stall(ms) {
-                return new Promise((resolve, reject) => {
+  // Apply scroll
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        const products = document.evaluate('//img[@data-sizes="auto"]/@src', document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const productsCount = products.snapshotLength;
+        console.log('Length: ' + productsCount);
+        await stall(1000);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000 || productsCount > 160) {
+            await stall(10000);
+            await stall(5000);
+        }
+    }
+    function stall(ms) {
+        return new Promise((resolve, reject) => {
                     setTimeout(() => {
                         resolve();
                     }, ms);
