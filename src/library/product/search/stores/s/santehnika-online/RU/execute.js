160945ba754c43348d8bfee2561a0329a8f29be9
adrian_swiderek <adrian.swiderek@tdcm.io>
2020-11-16 19:16:27 +0100

    search extractor tests added

--- src/library/product/search/stores/s/santehnika-online/RU/execute.js
+++ src/library/product/search/stores/s/santehnika-online/RU/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'santehnika-online',
     domain: 'santehnika-online.ru',
     url: 'https://santehnika-online.ru/search/?strategy=vectors_strict,zero_queries&q={searchTerms}',
-    loadedSelector: 'div[data-name*= "CatalogProducts"]',
+    loadedSelector: 'html body',
     noResultsXPath: '//div[@class="p-search-b-search-result"]',
     zipcode: '',
   },
