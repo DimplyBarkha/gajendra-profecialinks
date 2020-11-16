0170d83ec67ef82d95a056b5ac79eb4d166d42f1
Bindu Datla <bindu.datla@import.io>
2020-11-15 21:06:52 -0500

    updated to use cleanup transform

--- src/library/product/details/stores/b/baligam/IL/extract.js
+++ src/library/product/details/stores/b/baligam/IL/extract.js
@@ -1,10 +1,10 @@
-const { transform } = require('../../../../shared');
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'IL',
     store: 'baligam',
-    transform: transform,
+    transform: cleanUp,
     domain: 'baligam.co.il',
     zipcode: '',
   },
