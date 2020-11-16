d36a83fe21ad85f8085d997b972d24ca38f80a18
Sephali-23 <sephalisingh023@gmail.com>
2020-11-16 17:33:32 +0530

    latest code

--- src/library/product/details/stores/c/costco/US/extract.js
+++ src/library/product/details/stores/c/costco/US/extract.js
@@ -134,62 +134,62 @@ module.exports = {
     //     }
     //   });
     // }
-    //     async function preparePage () {
-    //       await new Promise(resolve => setTimeout(resolve, 1000));
-    //       await context.evaluate(async () => {
-    //         const iframe = document.querySelector('[title="Product Videos"]');
-    //         if (iframe) {
-    //           const video = iframe.contentWindow.document.getElementsByTagName('video');
-    //           const videoUrls = [...video].map(elm => elm.src);
-    //           document.querySelector('head').setAttribute('video', videoUrls.join(''));
-    //         } else {
-    //           const id = document.querySelector('#product-body-item-number') ? document.querySelector('#product-body-item-number').textContent.match(/(\d+)/g) : '';
-    //           const url = `https://cors-anywhere.herokuapp.com/https://sc.liveclicker.net/service/api?method=liveclicker.widget.getList&account_id=69&dim5=${id}&format=json`;
-    //           const data = await fetch(url);
-    //           if (data.status === 200) {
-    //             const json = await data.json();
+        async function preparePage () {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          await context.evaluate(async () => {
+            const iframe = document.querySelector('[title="Product Videos"]');
+            if (iframe) {
+              const video = iframe.contentWindow.document.getElementsByTagName('video');
+              const videoUrls = [...video].map(elm => elm.src);
+              document.querySelector('head').setAttribute('video', videoUrls.join(''));
+            } else {
+              const id = document.querySelector('#product-body-item-number') ? document.querySelector('#product-body-item-number').textContent.match(/(\d+)/g) : '';
+              const url = `https://cors-anywhere.herokuapp.com/https://sc.liveclicker.net/service/api?method=liveclicker.widget.getList&account_id=69&dim5=${id}&format=json`;
+              const data = await fetch(url);
+              if (data.status === 200) {
+                const json = await data.json();
 
-    //             const arr = [];
-    //             const array = json.widgets.widget;
-    //             array.forEach(item => {
-    //               const val = item.asset_id;
-    //               const url = `https://d2vxgxvhgubbj8.cloudfront.net/videos/69/${val}_1_liveclicker.mp4`;
-    //               arr.push(url);
-    //             });
-    //             let count = 0;
-    //             arr.forEach(item => {
-    //               document.querySelector('head').setAttribute(`vid${count}`, item);
-    //               count++;
-    //             });
-    //           }
-    //         }
-    //       });
-    //     }
-    //     // await new Promise(resolve => setTimeout(resolve, 50000));
-    //     // await context.evaluate(async function () {
-    //     //   const arr = [];
-    //     //   const videoLink = document.querySelector('.flix-jw') ? document.querySelector('.flix-jw').value.match(/file":"([^"]+)/)[1].replace(/^\\\/\\\//, '').replace(/\\\//g, '/') : '';
-    //     //   if (videoLink !== '') {
-    //     //     arr.push(videoLink);
-    //     //   }
-    //     //   document.querySelectorAll('#vjs_video_1_html5_api').forEach(item => {
-    //     //     const videoUrl = item.getAttribute('src');
-    //     //     arr.push(videoUrl);
-    //     //   });
-    //     //   const id = document.querySelector('#product-body-item-number') ? document.querySelector('#product-body-item-number').textContent.match(/(\d+)/g) : '';
-    //     //   if (id !== '') {
-    //     //     const url = `https://cors-anywhere.herokuapp.com/https://sc.liveclicker.net/service/api?method=liveclicker.widget.getList&account_id=69&dim5=${id}&format=json`;
-    //     //     const data = await fetch(url);
-    //     //     const json = await data.json();
-    //     //     const array = json.widgets.widget;
-    //     //     array.forEach(item => {
-    //     //       const val = item.asset_id;
-    //     //       const url = `https://d2vxgxvhgubbj8.cloudfront.net/videos/69/${val}_1_liveclicker.mp4`;
-    //     //       arr.push(url);
-    //     //     });
-    //     //   }
-    //     //   document.querySelector('body').setAttribute('videos', arr.join('|'));
-    //     // });
+                const arr = [];
+                const array = json.widgets.widget;
+                array.forEach(item => {
+                  const val = item.asset_id;
+                  const url = `https://d2vxgxvhgubbj8.cloudfront.net/videos/69/${val}_1_liveclicker.mp4`;
+                  arr.push(url);
+                });
+                let count = 0;
+                arr.forEach(item => {
+                  document.querySelector('head').setAttribute(`vid${count}`, item);
+                  count++;
+                });
+              }
+            }
+          });
+        }
+        // await new Promise(resolve => setTimeout(resolve, 50000));
+        // await context.evaluate(async function () {
+        //   const arr = [];
+        //   const videoLink = document.querySelector('.flix-jw') ? document.querySelector('.flix-jw').value.match(/file":"([^"]+)/)[1].replace(/^\\\/\\\//, '').replace(/\\\//g, '/') : '';
+        //   if (videoLink !== '') {
+        //     arr.push(videoLink);
+        //   }
+        //   document.querySelectorAll('#vjs_video_1_html5_api').forEach(item => {
+        //     const videoUrl = item.getAttribute('src');
+        //     arr.push(videoUrl);
+        //   });
+        //   const id = document.querySelector('#product-body-item-number') ? document.querySelector('#product-body-item-number').textContent.match(/(\d+)/g) : '';
+        //   if (id !== '') {
+        //     const url = `https://cors-anywhere.herokuapp.com/https://sc.liveclicker.net/service/api?method=liveclicker.widget.getList&account_id=69&dim5=${id}&format=json`;
+        //     const data = await fetch(url);
+        //     const json = await data.json();
+        //     const array = json.widgets.widget;
+        //     array.forEach(item => {
+        //       const val = item.asset_id;
+        //       const url = `https://d2vxgxvhgubbj8.cloudfront.net/videos/69/${val}_1_liveclicker.mp4`;
+        //       arr.push(url);
+        //     });
+        //   }
+        //   document.querySelector('body').setAttribute('videos', arr.join('|'));
+        // });
     return await context.extract(productDetails, { transform });
   },
 };
