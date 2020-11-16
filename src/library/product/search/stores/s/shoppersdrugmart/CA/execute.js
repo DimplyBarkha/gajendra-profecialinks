6db5181882f984564f934461c1b2960685c596ce
Ajinkya Kathe <ajinkya.kathe@synerzip.com>
2020-11-16 19:34:32 +0530

    Changes for accential

--- src/library/product/search/stores/s/shoppersdrugmart/CA/execute.js
+++ src/library/product/search/stores/s/shoppersdrugmart/CA/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'CA',
     store: 'shoppersdrugmart',
     domain: 'shoppersdrugmart.ca',
-    url: 'https://beauty.shoppersdrugmart.ca/search?text={searchTerms}',
+    url: 'https://shop.shoppersdrugmart.ca/search?text={searchTerms}',
     loadedSelector: 'section[data-testid*="product-list"] a[class*="plp__productTileWrapper"]:last-child img',
     noResultsXPath: '//p[contains(@class,"plp__contactInstructions")]',
     zipcode: '',
