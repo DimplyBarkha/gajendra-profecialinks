b4b8beeb6cbacae13371a3d895f45d2426775d57
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-11-16 16:43:10 +0200

    mondoffice config

--- src/library/product/details/stores/m/mondoffice/IT/shared.js
+++ src/library/product/details/stores/m/mondoffice/IT/shared.js
@@ -8,7 +8,7 @@ const transform = (data) => {
   data.forEach(e => {
     e.group.forEach(gr => {
       gr.online_price[0].text = gr.online_price[0].text.replace(/[^\d,]+/g, '');
-      gr.rpc[0].text = gr.rpc[0].text.replace(/[^\d,]+/g, '');
+      gr.sku[0].text = gr.sku[0].text.replace(/[^\d,]+/g, '');
       gr.retailer_product_code[0].text = gr.retailer_product_code[0].text.replace(/[^\d,]+/g, '');
       gr.online_price_currency[0].text = gr.online_price_currency[0].text.replace(/[\d\., ]/g, '').charAt(0);
       gr.products_per_page[0].text = gr.products_per_page[0].text = 0;
