6a86a70a2c8a0318da5466576adfcccd5849a94e
alscotty <alawrencescott@gmail.com>
2020-11-16 14:40:52 -0800

    add more fields

--- src/library/product/details/stores/f/farmaciasdirect/ES/execute.js
+++ src/library/product/details/stores/f/farmaciasdirect/ES/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'ES',
     store: 'farmaciasdirect',
     domain: 'farmaciasdirect.com',
-    loadedSelector: null,
+    loadedSelector: 'div.product-img img',
     noResultsXPath: "//section[contains(@class,'page-not-found')] | //font[contains(text(),'Sorry for the inconvenience')]",
     zipcode: '28001',
   },
