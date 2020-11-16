e720b986de3424fec6f76611dd48c8f34ec58943
Borys Maciuch <51968632+BorysMaciuch@users.noreply.github.com>
2020-11-16 08:32:01 +0100

    Tdcm keshop es core (#1651)
    * #1357 create extractor
    
    * #1358 add xpaths up to aggregateRating
    
    * #1357 add more xpaths
    
    * #1357 add missing xpaths
    
    * #1357 add missing bullet xpath
    
    * #1357 redeploy
    
    * #1357 fix rating
    
    * #1357 redeploy
    
    * #1357 fix rating, name
    
    * #1357 redeploy, run tests
    
    * #1357 fix rating
    
    * #1357 redeploy
    
    * #1357 fix category variantId
    
    * #1357 redeploy

--- src/library/product/details/stores/k/keshop/ES/extract.js
+++ src/library/product/details/stores/k/keshop/ES/extract.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -7,5 +6,31 @@ module.exports = {
     transform: null,
     domain: 'keshop.com',
     zipcode: '',
+  },
+  implementation: async (
+    { url },
+    { country, domain },
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(() => {
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+
+      stall(2000);
+    });
+    await context.evaluate(() => {
+      const ratingString = document.querySelector('.comment_anchor > img');
+      if (ratingString) {
+        const ratingValueNormalized = ratingString.src.match(/\d+/)[0] / 2;
+        ratingString.setAttribute('ratingNormalized', ratingValueNormalized);
+      }
+    });
+    await context.extract(dependencies.productDetails);
   },
 };
