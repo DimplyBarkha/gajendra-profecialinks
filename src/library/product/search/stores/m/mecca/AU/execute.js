c8117481ce21382afa08d9f4ec401f27bfadff00
astropaula <wojciechowska.fran@gmail.com>
2020-11-16 14:23:18 +0100

    #1590 Modified extract.yaml and url in execute.js, added rank  attribute in extract.js

--- src/library/product/search/stores/m/mecca/AU/execute.js
+++ src/library/product/search/stores/m/mecca/AU/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'AU',
     store: 'mecca',
     domain: 'mecca.com.au',
-    url: 'https://www.mecca.com.au/search?q={searchTerms}',
+    url: 'https://www.mecca.com.au/search?q="{searchTerms}"',
     loadedSelector: 'div[class="search-result-content"]',
     noResultsXPath: '//div[@class="no-hits-help"]',
     zipcode: '',
--- src/library/product/search/stores/m/mecca/AU/extract.js
+++ src/library/product/search/stores/m/mecca/AU/extract.js
@@ -1,11 +1,28 @@
+const { transform } = require('../../../../shared');
 
+async function implementation (inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async () => {
+    // add rank attribute
+    var rank = document.querySelectorAll('div[class="product-tile"]');
+
+    rank.forEach((element, index) => {
+      element.setAttribute('rank', (index + 1).toString());
+    });
+  });
+
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'AU',
     store: 'mecca',
-    transform: null,
+    transform: transform,
     domain: 'mecca.com.au',
     zipcode: '',
   },
+  implementation,
 };
