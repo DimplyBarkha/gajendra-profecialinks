c806c223cf97cf51e6d698ce2b660c455022fd22
unknown <ramraut25@gmail.com>
2020-11-16 13:01:29 +0530

    Added implementation to handle captcha and enhanced content

--- src/library/navigation/goto/domains/fn/fnac.es.js
+++ src/library/navigation/goto/domains/fn/fnac.es.js
@@ -9,20 +9,179 @@ module.exports = {
     zipcode: '',
   },implementation: async (
     { url, zipcode, storeId },
-    parameters, context, dependencies,
+    parameters,
+    context,
+    dependencies,
   ) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
-
-    await context.setBlockAds(false);
+    const timeout = parameters.timeout ? parameters.timeout : 60000;
+    await context.setJavaScriptEnabled(true);
+    await context.setCssEnabled(true);
     await context.setLoadAllResources(true);
     await context.setLoadImages(true);
-    await context.setJavaScriptEnabled(true);
-    await context.setAntiFingerprint(false);
+    await context.setBlockAds(false);
+    await context.setUseRelayProxy(false);
 
-    await context.goto(url, {
-      timeout: timeout,
-      waitUntil: 'load',
-      checkBlocked: true,
-    });
+    const gotoPage = async () => {
+      try {
+        const responseStatus = await context.goto(url, {
+          antiCaptchaOptions: {
+            provider: '2-captcha',
+            type: 'GEETEST',
+          },
+          firstRequestTimeout: 60000,
+          timeout,
+          waitUntil: 'load',
+          checkBlocked: true,
+        });
+        console.log('Status :', responseStatus.status);
+        console.log('URL :', responseStatus.url);
+        await context.waitForNavigation({ timeout: 30000 });
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      } catch (err) {
+        console.log('Error navigating to page.');
+        throw err;
+      }
+    };
+
+    const checkExistence = async (selector) => {
+      return await context.evaluate(async (sel) => {
+        return Boolean(document.querySelector(sel));
+      }, selector);
+    };
+
+    const optionalWait = async (selector) => {
+      try {
+        await context.waitForSelector(selector, { timeout });
+        console.log(`Found selector => ${selector}`);
+        return true;
+      } catch (err) {
+        console.log('Couldn\'t load the selector ' + selector);
+        return false;
+      }
+    };
+
+    await gotoPage();
+
+    const captchaSelector = 'iframe[src*="https://geo.captcha"]';
+    let isCaptchaFramePresent = await checkExistence(captchaSelector);
+
+    console.log('isCaptcha', isCaptchaFramePresent);
+
+    if (isCaptchaFramePresent) {
+      try {
+        let isHardBlocked = await context.evaluateInFrame(
+          captchaSelector,
+          function () {
+            return document.body.innerText.search('You have been blocked') > -1;
+          },
+        );
+        if (isHardBlocked) {
+          console.log('IP is hard blocked');
+          return context.reportBlocked(451, 'Blocked!');
+          // throw new Error('Blocked');
+        }
+        await context.evaluateInFrame(
+          captchaSelector,
+          function () {
+            // eslint-disable-next-line
+            const code = geetest
+              .toString()
+              .replace(/appendTo\("#([^"]+)"\)/, 'appendTo(document.getElementById("$1"))');
+            // eslint-disable-next-line
+            return eval(`(${code})()`);
+          },
+        );
+
+        await new Promise(resolve => setTimeout(resolve, 500));
+        await context.evaluateInFrame('iframe',
+          function () {
+            // @ts-ignore
+            document.querySelector('.captcha-handler').click();
+          },
+        );
+        console.log('Captcha Resolved.');
+        await new Promise(resolve => setTimeout(resolve, 500));
+        await context.waitForNavigation({ timeout: 30000 });
+        // we may be navigated to an index page after captcha solve
+        let productPageSelector = '#product-content';
+
+        if (url.includes('search')) {
+          productPageSelector = '#search-result-items';
+        }
+        // await context.waitForSelector(productPageSelector);
+        let isWaitProduct = await optionalWait(productPageSelector);
+
+        console.log('isProduct', isWaitProduct);
+
+        const cookieButtonSelector = 'body > div.optanon-alert-box-wrapper > div.optanon-alert-box-bg > div.optanon-alert-box-button-container > div.optanon-alert-box-button.optanon-button-allow > div > button';
+        const cookieButton = await optionalWait(cookieButtonSelector);
+        console.log('isCookie', cookieButton);
+
+        if (cookieButton) {
+          try {
+            console.log('Clicking on cookie accept button.');
+            await context.click(cookieButtonSelector);
+          } catch (err) {
+            console.log('Error while accepting cookies.');
+          }
+        }
+        if (!isWaitProduct) {
+          await gotoPage();
+          isCaptchaFramePresent = await checkExistence(captchaSelector);
+          console.log('SecondCaptchaFrame', isCaptchaFramePresent);
+          if (isCaptchaFramePresent) {
+            console.log('got captcha a second time');
+            isHardBlocked = await context.evaluateInFrame(
+              captchaSelector,
+              function () {
+                return document.body.innerText.search('You have been blocked') > -1;
+              },
+            );
+            if (isHardBlocked) {
+              console.log('IP is hard blocked');
+              return context.reportBlocked(451, 'Blocked!');
+              // throw new Error('Blocked');
+            }
+            await context.evaluateInFrame(
+              captchaSelector,
+              function () {
+                // eslint-disable-next-line
+                const code = geetest
+                  .toString()
+                  .replace(/appendTo\("#([^"]+)"\)/, 'appendTo(document.getElementById("$1"))');
+                // eslint-disable-next-line
+                return eval(`(${code})()`);
+              },
+            );
+
+            await new Promise(resolve => setTimeout(resolve, 500));
+            await context.evaluateInFrame('iframe',
+              function () {
+                // @ts-ignore
+                document.querySelector('.captcha-handler').click();
+              },
+            );
+            console.log('Captcha Resolved.');
+            await new Promise(resolve => setTimeout(resolve, 500));
+            await context.waitForNavigation({ timeout: 30000 });
+            isWaitProduct = await optionalWait(productPageSelector);
+            if (!isWaitProduct) {
+              await gotoPage();
+              isCaptchaFramePresent = await checkExistence(captchaSelector);
+              console.log('ThirdCaptchaFrame', isCaptchaFramePresent);
+              if (isCaptchaFramePresent) {
+                throw new Error('got captcha a third time');
+              }
+            }
+          }
+        }
+      } catch (error) {
+        console.log('error: NO CAPTCHA ENCOUNTER', error);
+        if (error.message === 'Blocked') { return context.reportBlocked(451, 'Blocked! Error - ' + error); }
+        // throw error;
+      }
+    } else {
+      console.log('NO CAPTCHA ENCOUNTER');
+    }
   },
 };
--- src/library/product/details/stores/f/fnac/ES/extract.js
+++ src/library/product/details/stores/f/fnac/ES/extract.js
@@ -1,34 +1,81 @@
 const { transform } = require('../format');
 
-async function implementation (inputs, parameters, context, dependencies) {
+async function implementation(inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+  try {
+    if (document.querySelector('#KameleoonScenarioLayerCloseButton34896')) {
+      document.querySelector('#KameleoonScenarioLayerCloseButton34896').click()
+    }
 
-  // If cookie pop up appears then clicking on accept button
-  await context.evaluate(async function () {
-    function addHiddenDiv (id, content) {
-      const newDiv = document.createElement('div');
-      newDiv.id = id;
-      newDiv.textContent = content;
-      newDiv.style.display = 'none';
-      document.body.appendChild(newDiv);
-    }
-    
-    if (document.querySelector('div.productStrate__raw')) {
-      let desc = document.querySelector('div.productStrate__raw').innerHTML;
-      desc = desc.replace(/<li>/gm, ' || ').replace(/<.*?>/gm, '').replace(/&nbsp;/g, '').trim();
-      addHiddenDiv('desc', desc);
-    }
-    
-    let desc = document.querySelector('script[type="application/ld+json"]').innerText
-    desc = JSON.parse(desc);
-    if(desc.brand) {
-      addHiddenDiv('brandText', desc.brand.name);
-    }
-  });
+  } catch (error) {
+    console.log('no close button found');
+  }
 
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  return await context.extract(productDetails, { transform });
+  try {
+    await context.waitForSelector('#Flixmedia', { timeout: 50000 });
+   
+    
+  } catch (error) {
+  console.log('no enhanced content found');
+}
+
+
+// If cookie pop up appears then clicking on accept button
+await context.evaluate(async function () {
+  function addHiddenDiv(id, content) {
+    const newDiv = document.createElement('div');
+    newDiv.id = id;
+    newDiv.textContent = content;
+    newDiv.style.display = 'none';
+    document.body.appendChild(newDiv);
+  }
+
+  if (document.querySelector('div.productStrate__raw')) {
+    let desc = document.querySelector('div.productStrate__raw').innerHTML;
+    desc = desc.replace(/<li>/gm, ' || ').replace(/<.*?>/gm, '').replace(/&nbsp;/g, '').trim();
+    addHiddenDiv('desc', desc);
+  }
+
+  let desc = document.querySelector('script[type="application/ld+json"]').innerText
+  desc = JSON.parse(desc);
+  if (desc.brand) {
+    addHiddenDiv('brandText', desc.brand.name);
+  }
+
+
+  try {
+
+
+    let scrollSelector = document.querySelector('#footer');
+    // @ts-ignore
+    let scrollLimit = scrollSelector ? scrollSelector.offsetTop : '';
+    let yPos = 0;
+    while (scrollLimit && yPos < scrollLimit) {
+      yPos = yPos + 350;
+      window.scrollTo(0, yPos);
+      scrollSelector = document.querySelector('#footer');
+      // @ts-ignore
+      scrollLimit = scrollSelector ? scrollSelector.offsetTop : '';
+      await new Promise(resolve => setTimeout(resolve, 3500));
+    }
+
+  } catch (e) {
+    console.log('No content found');
+  }
+
+
+
+
+});
+// try {
+//   await context.waitForXPath('//div[contains(@class,"inpage_selector_info dyson_introduction") or contains(@class,"inpage_block inpage_selector_feature")]');
+// } catch (error) {
+//   console.log('no video found');
+// }
+
+await new Promise((resolve) => setTimeout(resolve, 10000));
+return await context.extract(productDetails, { transform });
 }
 
 module.exports = {
