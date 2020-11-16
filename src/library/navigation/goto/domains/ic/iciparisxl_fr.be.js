803c16a46de4b1ac79db1196914542726969f41f
sivasoarlogic <siva@soarlogic.com>
2020-11-16 19:42:29 +0530

    searchupdated

--- src/library/navigation/goto/domains/ic/iciparisxl_fr.be.js
+++ src/library/navigation/goto/domains/ic/iciparisxl_fr.be.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'iciparisxl_fr.be',
-    timeout: 10000,
+    timeout: 100000,
     country: 'BE',
     store: 'iciparisxl_fr',
     zipcode: "''",
