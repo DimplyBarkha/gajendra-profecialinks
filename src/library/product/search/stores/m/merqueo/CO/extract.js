1bf9aca713b6a332c9c8c073568b3dbf1f381201
Wayne E <bartekstafiej2@gmail.com>
2020-11-17 11:40:34 +0100

    #1594 Added scroll implementation

--- src/library/product/search/stores/m/merqueo/CO/extract.js
+++ src/library/product/search/stores/m/merqueo/CO/extract.js
@@ -1,5 +1,47 @@
-async function implementation (inputs, parameters, dependencies, context) {
+async function implementation (inputs, parameters, context, dependencies) {
   const { productDetails } = dependencies;
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  await context.evaluate(() => {
+    const priceSelector = document.querySelectorAll('.mq-product-prices');
+    let price;
+    const productUrlSelector = document.querySelectorAll('.mq-product-img>div>a');
+    let productUrl;
+    const allProducts = productUrlSelector.length;
+
+    for (let i = 0; i < allProducts; i++) {
+      if (priceSelector[i]) {
+        price = priceSelector[i].textContent.replace('.', ',');
+      }
+
+      if (productUrlSelector[i].href !== undefined && productUrlSelector[i].href !== null) {
+        productUrl = 'https://merqueo.com' + productUrlSelector.href;
+      }
+
+      document.querySelectorAll('.mq-product-prices')[i].setAttribute('price', price);
+      document.querySelectorAll('.mq-product-img>div>a')[i].setAttribute('producturl', productUrl);
+      document.querySelectorAll('.mq-grid-item')[i].setAttribute('rank', `${i + 1}`);
+    }
+  });
+
+  await context.evaluate(async () => {
+    // scroll
+    function stall (ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+
+    let scrollTop = 0;
+    do {
+      await stall(1000);
+      scrollTop += 380;
+      window.scroll(0, scrollTop);
+    } while (scrollTop <= window.innerHeight);
+  });
 
   return await context.extract(productDetails);
 }
