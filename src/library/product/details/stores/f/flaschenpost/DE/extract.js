a16b5a5cca410201b33527cee6379a782b3b020b
Your Name <karthik.ravi-odc@partner.import.io>
2020-11-17 15:43:32 +0530

    karthickravi

--- src/library/product/details/stores/f/flaschenpost/DE/extract.js
+++ src/library/product/details/stores/f/flaschenpost/DE/extract.js
@@ -8,4 +8,27 @@ module.exports = {
     domain: 'flaschenpost.de',
     zipcode: '48151',
   },
-};
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async () => {
+      const usernameElements = document.querySelectorAll('#validZipcode');
+      usernameElements.forEach(username => username.value = "48151");
+      document.querySelector('div[class="fp-modal_input"]>button').click()
+      await new Promise(r => setTimeout(r, 6000));
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.className = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      
+    });
+    return await context.extract(productDetails, { transform });
+  },
+}
