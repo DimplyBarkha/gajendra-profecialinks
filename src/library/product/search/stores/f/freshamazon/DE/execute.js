c7d05e98d2ee66c75f07c0e9041247b506feb59c
unknown <leena.gavali-odc@partner.import.io>
2020-11-17 21:34:30 +0530

    fresh

--- src/library/product/search/stores/f/freshamazon/DE/execute.js
+++ src/library/product/search/stores/f/freshamazon/DE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DE',
     store: 'freshamazon',
     domain: 'freshamazon.de',
-    url: 'https://www.amazon.de/s?k={searchTerms}&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss',
+    url: 'https://www.amazon.de/s?k=%25+Aluminium&page=3&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1605628027&ref=sr_pg_3',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
