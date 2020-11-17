bb61c909626ad506da498c830ae2274f8d461fb0
prateek.s <psc3018@gmail.com>
2020-11-18 04:47:54 +0530

    fixed for enhanced content, aplus images and videos

--- src/library/product/details/stores/d/did/IE/extract.js
+++ src/library/product/details/stores/d/did/IE/extract.js
@@ -9,4 +9,19 @@ module.exports = {
     domain: 'did.ie',
     zipcode: '',
   },
+  implementation: async function implementation (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    try{
+      await context.waitForSelector('iframe[id*=flix-iframe]');
+    }catch(e){
+      console.log('no video present in enhanced content');
+    }
+    return await context.extract(productDetails, { transform });
+  }
 };
