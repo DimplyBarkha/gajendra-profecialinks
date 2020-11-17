f95d8cc0a973126ab9b1e23787d1b0aa095b7a9b
Vivek Gondil <gondilvivek@gmail.com>
2020-11-17 17:48:20 +0530

    Changes From PR.

--- src/library/product/details/stores/a/apotekhjartat/SE/extract.js
+++ src/library/product/details/stores/a/apotekhjartat/SE/extract.js
@@ -19,44 +19,55 @@ module.exports = {
     } catch (e) {
       console.error(e);
     } finally {
-      await context.evaluate(() => {
-        const repl = document.querySelectorAll('.productMainUSP > li');
-        for (let i = 0; i < repl.length; i++) {
-          if (i !== 0) {
-            repl[i].innerText = '|| ' + repl[i].innerText;
+      if (await context.evaluate(() => !window.location.href.toString().includes('soksida'))) {
+        // Common Implementation
+        await context.evaluate(() => {
+          const repl = document.querySelectorAll('.productMainUSP > li');
+          for (let i = 0; i < repl.length; i++) {
+            if (i !== 0) {
+              repl[i].innerText = '|| ' + repl[i].innerText;
+            }
           }
-        }
-      });
-      if (await context.evaluate(async () => (!window.location.href.toString().includes('soksida') && Boolean(document.querySelector('#pm-document'))))) {
-        await context.evaluate(async () => {
-          let a = document.evaluate('//ul[@class="slides"]/li[not(contains(@class,"clone")) and (position()>2)]/img', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-          let b = a.iterateNext();
-          let c = [];
-          while (b !== null) {
-            c.push(b.getAttribute('src').split('?')[0]);
-            b = a.iterateNext();
+          const tempIterator = document.evaluate('//ul[@class="slides"]/li[not(contains(@class,"clone")) and (position()>2)]/img', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+          let nodeElement = tempIterator.iterateNext();
+          const reqData = [];
+          while (nodeElement) {
+            reqData.push(nodeElement.getAttribute('src').split('?')[0]);
+            nodeElement = tempIterator.iterateNext();
           }
           const div = document.createElement('div');
           div.setAttribute('id', 'alt-img');
-          div.innerText = c.join(' | ');
+          div.innerText = reqData.join(' | ');
           document.body.append(div);
 
           const ing = document.getElementById('product-toc');
           if (ing) {
-            let secDiv = document.createElement('div');
-            secDiv.setAttribute('id', 'section');
-            document.body.append(secDiv);
-            secDiv = document.createElement('div');
-            secDiv.setAttribute('id', 'section');
+            const secDiv = document.createElement('div');
+            secDiv.setAttribute('id', 'toc');
             secDiv.append(ing);
             document.body.append(secDiv);
           }
+        });
+        // Implementation Only if PM-Doc is present
+        await context.evaluate(async () => {
           const pmDoc = document.querySelector('#pm-document');
           if (pmDoc) {
             const hs = document.querySelectorAll('#pm-document>h2');
+            hs[0].setAttribute('toc', 'start');
+            hs[1].setAttribute('toc', 'end');
+
+            let secDiv = document.createElement('div');
+            secDiv.setAttribute('id', 'toc');
+            let flag = false;
+            for (let i = 0; pmDoc.children[i].getAttribute('toc') !== 'end'; i++) {
+              if (flag) { secDiv.append(pmDoc.children[i].cloneNode(true)); }
+              if (pmDoc.children[i].getAttribute('toc') !== 'start') { flag = true; }
+            }
+            document.body.append(secDiv);
+
             hs.forEach((h) => { if (h.innerText.includes('.')) { h.setAttribute('id', 'br'); } });
             const children = pmDoc.children;
-            let secDiv = document.createElement('div');
+            secDiv = document.createElement('div');
             secDiv.setAttribute('id', 'section');
             for (let i = 0; i < children.length; i++) {
               if (children[i].id === 'br') {
@@ -67,55 +78,33 @@ module.exports = {
               secDiv.appendChild(children[i].cloneNode(true));
             }
             document.body.append(secDiv);
-          }
-          const createDiv = async function (xpath, id) {
-            a = document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-            b = a.iterateNext();
-            c = [];
-            while (b !== null) {
-              c.push(b.textContent);
-              b = a.iterateNext();
-            }
-            const div2 = document.createElement('div');
-            div2.setAttribute('id', id);
-            div2.innerText = c.join(' ');
-            document.body.append(div2);
-          };
 
-          await createDiv("//div[@id='section']//h2[contains(.,'3. ')]//following-sibling::*", 'directions');
-          await createDiv("//div[@id='section']//h2[contains(.,'2. ')]//following-sibling::*", 'warnings');
-          await createDiv("//div[@id='section']//h2[contains(.,'5. ')]//following-sibling::*", 'storage');
-        });
-      } else {
-        await context.evaluate(async () => {
-          const a = document.evaluate('//ul[@class="slides"]/li[not(contains(@class,"clone")) and (position()>2)]/img', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-          let b = a.iterateNext();
-          const c = [];
-          while (b !== null) {
-            c.push(b.getAttribute('src').split('?')[0]);
-            b = a.iterateNext();
-          }
-          const div = document.createElement('div');
-          div.setAttribute('id', 'alt-img');
-          div.innerText = c.join(' | ');
-          document.body.append(div);
+            const createDiv = async function (xpath, id) {
+              const tempIterator = document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+              let nodeElement = tempIterator.iterateNext();
+              const reqData = [];
+              while (nodeElement) {
+                reqData.push(nodeElement.textContent);
+                nodeElement = tempIterator.iterateNext();
+              }
+              const div2 = document.createElement('div');
+              div2.setAttribute('id', id);
+              div2.innerText = reqData.join(' ');
+              document.body.append(div2);
+            };
 
-          const ing = document.getElementById('product-toc');
-          if (ing) {
-            let secDiv = document.createElement('div');
-            secDiv.setAttribute('id', 'section');
-            document.body.append(secDiv);
-            secDiv = document.createElement('div');
-            secDiv.setAttribute('id', 'section');
-            secDiv.append(ing);
-            document.body.append(secDiv);
+            await createDiv("//div[@id='section']//h2[contains(.,'3. ')]//following-sibling::*", 'directions');
+            await createDiv("//div[@id='section']//h2[contains(.,'2. ')]//following-sibling::*", 'warnings');
+            await createDiv("//div[@id='section']//h2[contains(.,'5. ')]//following-sibling::*", 'storage');
           }
         });
       }
     }
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    return await context.extract(productDetails, { transform });
+    if (await context.evaluate(() => !window.location.href.toString().includes('soksida'))) {
+      const { transform } = parameters;
+      const { productDetails } = dependencies;
+      return await context.extract(productDetails, { transform });
+    }
   },
 
 };
