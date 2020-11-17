9d11c77003f7a5f28373c97d63df48cbde58a6dc
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-17 14:52:14 +0530

    #1550 fixing local issue

--- src/library/product/search/stores/m/makro/PL/format.js
+++ src/library/product/search/stores/m/makro/PL/format.js
@@ -25,7 +25,12 @@ const transform = (data) => {
     };
     for (const { group } of data) {
       var rank = 1; 
-      for (let row of group) {               
+      for (let row of group) { 
+        if(row.productUrl){
+          row.productUrl.forEach(item=>{
+            item.text="https://online.makro.pl"+item.text;
+          })
+        }              
         row.rank = [{ "text": rank }];
         row.rankOrganic = [{ "text": rank }];
         rank++;
