947a982cadcf16774a337fdd2c557d6b57cd4255
poojasable-sz-import <pooja.sable@sz.import.io>
2020-11-18 02:55:29 +0530

    Core and Variant Extractor Updates

---
+++ src/library/product/details/stores/d/dm/DE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'DE',
+    store: 'dm',
+    domain: 'dm.de',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/d/dm/DE/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'DE',
+    store: 'dm',
+    transform: null,
+    domain: 'dm.de',
+    zipcode: '',
+  },
+};
