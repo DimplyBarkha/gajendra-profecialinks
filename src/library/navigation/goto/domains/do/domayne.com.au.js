ac85430ef70bbafb4871dbf6009e8efda42461a1
himanshu.a <himanshu.a@osmosys.asia>
2020-11-17 10:49:41 +0530

    Added goto options
    To load the additional manufaturer description, but seems to be not working, also tried adding wait for selector, that is also not helpful

--- src/library/navigation/goto/domains/do/domayne.com.au.js
+++ src/library/navigation/goto/domains/do/domayne.com.au.js
@@ -1,36 +1,39 @@
 module.exports = {
-  implements: "navigation/goto",
-  parameterValues: {
-    domain: "domayne.com.au",
-    timeout: 50000,
-    country: "AU",
-    store: "domayne",
-    zipcode: "",
-  },
-  implementation: async (
-    { url, zipcode, storeId },
-    parameters,
-    context,
-    dependencies
-  ) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.setLoadImages(false);
-    await context.setLoadAllResources(false);
-    /**
-     * Adding goto in try catch as the webpage keeps on loading indefinitely
-     * even if we give huge time out and eventually throws timeout error
-     */
-    try {
-      await context.goto(url, {
-        timeout: timeout,
-        waitUntil: "load",
-        checkBlocked: true,
-      });
-    } catch (e) {
-      console.log("page load time out ----> ", e);
-    }
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-    }
-  },
+    implements: "navigation/goto",
+    parameterValues: {
+        domain: "domayne.com.au",
+        timeout: 50000,
+        country: "AU",
+        store: "domayne",
+        zipcode: "",
+    },
+    implementation: async({ url, zipcode, storeId },
+        parameters,
+        context,
+        dependencies
+    ) => {
+        const timeout = parameters.timeout ? parameters.timeout : 10000;
+        await context.setLoadImages(false);
+        await context.setLoadAllResources(false);
+        /**
+         * Adding goto in try catch as the webpage keeps on loading indefinitely
+         * even if we give huge time out and eventually throws timeout error
+         */
+        try {
+            await context.goto(url, {
+                timeout: timeout,
+                waitUntil: "load",
+                checkBlocked: true,
+                js_enabled: true,
+                css_enabled: false,
+                random_move_mouse: true,
+                antiFingerPrint: false
+            });
+        } catch (e) {
+            console.log("page load time out ----> ", e);
+        }
+        if (zipcode) {
+            await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+        }
+    },
 };
--- src/library/product/details/stores/d/domayne/AU/extract.js
+++ src/library/product/details/stores/d/domayne/AU/extract.js
@@ -7,56 +7,61 @@ const { transform } = require('../format');
  * @param { ImportIO.IContext } context
  * @param { Record<string, any> } dependencies
  */
-async function implementation (inputs, parameters, context, dependencies) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-
-  await context.evaluate(async function () {
-   
-    if (document.querySelector('#category-grid > div[data-position="1"]')) {
-        document.querySelector('#category-grid > div > div > div.photo-box > a').click();
-      }
-   
-    function addHiddenDiv (id, content) {
-      const newDiv = document.createElement('div');
-      newDiv.id = id;
-      newDiv.textContent = content;
-      newDiv.style.display = 'none';
-      document.body.appendChild(newDiv);
+async function implementation(inputs, parameters, context, dependencies) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    try {
+        await context.waitForSelector('div[class="box-description cms"] img', { timeout: 30000 });
+    } catch (error) {
+        console.log(error);
     }
 
-    // Function to fetch sku number and gtin from script tag as not available directly on DOM.
-    function fetchRatingFromScript () {
-      const scriptDataTagSelector = document.evaluate('//script[@type="application/ld+json"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-      const scriptTagData = scriptDataTagSelector ? scriptDataTagSelector.innerText : '';
-      const availability = scriptTagData ? scriptTagData.replace(/.*added_availability":"(\d+).*/gm, '$1') : '';
-      addHiddenDiv('added_availability', availability);
-    }
+    await context.evaluate(async function() {
 
-    // If images are present in description then add to manufacturerDescription else add to description
-    const descriptionSelector = document.evaluate('//*[@id="productDescription"] | //span[contains(text(), "Description")]/parent::*/following-sibling::* |  //div[contains(@class, "product-short-description short-description")]/p', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    let description = descriptionSelector ? descriptionSelector.innerText : '';
-    description = description ? description.replace(/(\n\s*){2,}/g, ' ').replace(/(\n\s*){1,}/g, ' || ') : '';
-    const manufacturerImageFlag = document.querySelector('div[class="box-description cms"] img');
-    if (manufacturerImageFlag) {
-      addHiddenDiv('added-manufacturerDesc', description);
-    } else {
-      addHiddenDiv('added-description', description);
-    }
-    fetchRatingFromScript();
-  });
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  return await context.extract(productDetails, { transform });
+        if (document.querySelector('#category-grid > div[data-position="1"]')) {
+            document.querySelector('#category-grid > div > div > div.photo-box > a').click();
+        }
+
+        function addHiddenDiv(id, content) {
+            const newDiv = document.createElement('div');
+            newDiv.id = id;
+            newDiv.textContent = content;
+            newDiv.style.display = 'none';
+            document.body.appendChild(newDiv);
+        }
+
+        // Function to fetch sku number and gtin from script tag as not available directly on DOM.
+        function fetchRatingFromScript() {
+            const scriptDataTagSelector = document.evaluate('//script[@type="application/ld+json"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            const scriptTagData = scriptDataTagSelector ? scriptDataTagSelector.innerText : '';
+            const availability = scriptTagData ? scriptTagData.replace(/.*added_availability":"(\d+).*/gm, '$1') : '';
+            addHiddenDiv('added_availability', availability);
+        }
+
+        // If images are present in description then add to manufacturerDescription else add to description
+        const descriptionSelector = document.evaluate('//*[@id="productDescription"] | //span[contains(text(), "Description")]/parent::*/following-sibling::* |  //div[contains(@class, "product-short-description short-description")]/p', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        let description = descriptionSelector ? descriptionSelector.innerText : '';
+        description = description ? description.replace(/(\n\s*){2,}/g, ' ').replace(/(\n\s*){1,}/g, ' || ') : '';
+        const manufacturerImageFlag = document.querySelector('div[class="box-description cms"] img');
+        if (manufacturerImageFlag) {
+            addHiddenDiv('added-manufacturerDesc', description);
+        } else {
+            addHiddenDiv('added-description', description);
+        }
+        fetchRatingFromScript();
+    });
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    return await context.extract(productDetails, { transform });
 }
 
 module.exports = {
-  implements: 'product/details/extract',
-  parameterValues: {
-    country: 'AU',
-    store: 'domayne',
-    transform,
-    domain: 'domayne.com.au',
-    zipcode: '',
-  },
-  implementation,
+    implements: 'product/details/extract',
+    parameterValues: {
+        country: 'AU',
+        store: 'domayne',
+        transform,
+        domain: 'domayne.com.au',
+        zipcode: '',
+    },
+    implementation,
 };
