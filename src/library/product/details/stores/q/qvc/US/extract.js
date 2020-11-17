f4d3b9b1ebe042d0bbb6ce2922793b4d2b2be8fd
astropaula <wojciechowska.fran@gmail.com>
2020-11-17 16:08:15 +0100

    created core extractor

---
+++ src/library/product/details/stores/q/qvc/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'qvc',
+    transform: null,
+    domain: 'qvc.com',
+    zipcode: '',
+  },
+};
