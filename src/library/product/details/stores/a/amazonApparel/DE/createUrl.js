16ab1b691dcddb4aa70d129fbfea9cc775f15da4
vikas kyatannawar <vikas.k@osmosys.asia>
2020-11-17 13:30:14 +0530

    updated url pattern

--- src/library/product/details/stores/a/amazonApparel/DE/createUrl.js
+++ src/library/product/details/stores/a/amazonApparel/DE/createUrl.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'amazon.de',
-    url: 'https://www.amazon.de/dp/{id}?th=1&psc=1',
+    url: 'https://www.amazon.de/dp/{id}?th=1&psc=1&showDetailProductDesc=1',
     country: 'DE',
     store: 'amazon',
   },
