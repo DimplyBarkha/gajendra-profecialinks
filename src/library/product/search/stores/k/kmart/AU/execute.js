7d16d0b62734b1e6a316a5cfa3ac830f85b4526c
unknown <leena.gavali-odc@partner.import.io>
2020-11-17 12:46:51 +0530

    kmart_QA1_search

--- src/library/product/search/stores/k/kmart/AU/execute.js
+++ src/library/product/search/stores/k/kmart/AU/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'AU',
     store: 'kmart',
     domain: 'kmart.com.au',
-    url: 'https://www.kmart.com.au/webapp/wcs/stores/servlet/SearchDisplay?searchTerm=apple&categoryId=&storeId=10701&catalogId=10102&langId=-1&beginIndex=0&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=#.plp-wrapper',
+    url: 'https://www.kmart.com.au/webapp/wcs/stores/servlet/SearchDisplay?searchTerm={searchTerms}&categoryId=&storeId=10701&catalogId=10102&langId=-1&beginIndex=0&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=#.plp-wrapper',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
--- src/library/product/search/stores/k/kmart/AU/extract.js
+++ src/library/product/search/stores/k/kmart/AU/extract.js
@@ -9,4 +9,56 @@ module.exports = {
     domain: 'kmart.com.au',
     zipcode: '',
   },
+  implementation,
+};
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addclass(xpathforpagination) {
+      var elems = document.querySelectorAll(xpathforpagination);
+      elems[0].classList.add('pagination');
+    }
+    // for rank
+    //for rank
+    function addHiddenDiv(id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll('div[class="product product_box small-6 medium-4 large-4 columns clearfix col "]')[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    let rankOrganic;
+    let url = window.location.href;
+    // let checkPageNumber1 = url.split('?')[1];
+    let checkPageNumber = url.split('&')[0];
+    try {
+      if (checkPageNumber.startsWith('page=')) {
+        rankOrganic = checkPageNumber.replace('page=', '');
+      }
+    }
+    catch (err) {
+    }
+    var dup = Number(rankOrganic);
+    dup = dup - 1;
+    if (!rankOrganic) {
+      rankOrganic = 1;
+    } else {
+      rankOrganic = (dup * 30) + 1;
+    }
+    const urlProduct = document.querySelectorAll('div[class="product product_box small-6 medium-4 large-4 columns clearfix col "]');
+    for (let i = 0; i < urlProduct.length; i++) {
+      addHiddenDiv('rankOrganic', rankOrganic++, i);
+    }
+  });
+  //rank end
+  return await context.extract(productDetails, { transform });
+
+
 };
