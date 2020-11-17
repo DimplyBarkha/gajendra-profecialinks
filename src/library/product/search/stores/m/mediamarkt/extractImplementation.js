47d72a28d768b27df48d0ee4b0286be4b5679031
IpsitaM <iamipsita07@gmail.com>
2020-11-17 22:39:00 +0530

    Updated code

--- src/library/product/search/stores/m/mediamarkt/extractImplementation.js
+++ src/library/product/search/stores/m/mediamarkt/extractImplementation.js
@@ -15,32 +15,6 @@ const implementation = async function(
             })
         }
     });
-    const applyScroll = async function(context) {
-        await context.evaluate(async function() {
-            let scrollTop = 0;
-            while (scrollTop !== 20000) {
-                await stall(4000);
-                scrollTop += 1000;
-                window.scroll(0, scrollTop);
-                if (scrollTop === 20000) {
-                    await stall(6000);
-                    break;
-                }
-            }
-
-            function stall(ms) {
-                return new Promise((resolve, reject) => {
-                    setTimeout(() => {
-                        resolve();
-                    }, ms);
-                });
-            }
-        });
-    };
-    await applyScroll(context);
-    await context.waitForSelector('img[class="loaded"]', { timeout: 80000 });
-    const delay = t => new Promise(resolve => setTimeout(resolve, t));
-    await delay(8000);
     return await context.extract(productDetails, { transform });
-};
+}
 module.exports = { implementation };
