d2d431e60f94ed5c83e35b498edd5cf3ae70c313
unknown <harsh.khare-od@partner.import.io>
2020-11-18 01:20:51 +0530

    Parfumdreams search complete

--- src/library/product/search/stores/p/parfumdreams/DE/extract.js
+++ src/library/product/search/stores/p/parfumdreams/DE/extract.js
@@ -1,4 +1,4 @@
-
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,48 +8,4 @@ module.exports = {
     domain: 'parfumdreams.de',
     zipcode: '',
   },
-  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
-    await context.evaluate(async function () {
-      function addElementToDocument(key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
-      }
-      // Method to Retrieve Xpath content of a Multiple Nodes
-      const getAllXpath = (xpath, prop) => {
-        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        const result = [];
-        for (let index = 0; index < nodeSet.snapshotLength; index++) {
-          const element = nodeSet.snapshotItem(index);
-          if (element) result.push(prop ? element[prop] : element.nodeValue);
-        }
-        return result;
-      };
-      var getXpath = (xpath, prop) => {
-        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-        let result;
-        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-        else result = elem ? elem.singleNodeValue : '';
-        return result && result.trim ? result.trim() : result;
-      };
-      function addHiddenDiv(id, content, index) {
-        const newDiv = document.createElement('div');
-        newDiv.id = id;
-        newDiv.textContent = content;
-        newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll('div[class="item clearfix no-underline ajx item-link c d-4 p-m-6  "]')[index];
-        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-      }
-
-      // var res = getAllXpath('//p[@class="item-description mbs0"]', 'nodeValue');
-      // var len = res.length;
-
-      // for(var i=0; i<len; i++){
-
-      // }
-    });
-    await context.extract(productDetails);
-    },
-    };
+};
