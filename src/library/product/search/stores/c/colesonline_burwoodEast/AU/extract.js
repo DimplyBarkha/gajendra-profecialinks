4a83762e9d8447e48fee976e740be37d42a0a3e0
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-17 16:40:14 +0530

    Search name updated

--- src/library/product/search/stores/c/colesonline_burwoodEast/AU/extract.js
+++ src/library/product/search/stores/c/colesonline_burwoodEast/AU/extract.js
@@ -15,6 +15,24 @@ async function implementation (
       newDiv.style.display = 'none';
       document.body.appendChild(newDiv);
     }
+    function addChildDiv (id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll("section[id*='product-list'] div[tile='tile'] span[class*='product-brand']")[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    const product = document.querySelectorAll("section[id*='product-list'] div[tile='tile']");
+    product.forEach((item, index) => {
+      const brand = item.querySelector("span[class*='product-brand']") ? item.querySelector("span[class*='product-brand']").innerText : '';
+      const name = item.querySelector("span[class*='product-name']") ? item.querySelector("span[class*='product-name']").innerText : '';
+      if (brand && name) {
+        addChildDiv('ii_name', brand.trim() + ' ' + name.trim(), index);
+      } else {
+        addChildDiv('ii_name', name.trim(), index);
+      }
+    });
     const searchUrl = window.location.href;
     addHiddenDiv('ii_searchUrl', searchUrl);
   });
