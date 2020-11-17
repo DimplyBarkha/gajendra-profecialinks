1d41ae9143ccfa8d2a0e73c39245dcc2fb384d78
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-17 19:01:26 +0530

    linio-Damayanthi

--- src/library/navigation/goto/domains/li/linio.com.js
+++ src/library/navigation/goto/domains/li/linio.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'linio.com',
-    timeout: null,
+    timeout: 3000000,
     country: 'PE',
     store: 'linio',
     zipcode: '',
