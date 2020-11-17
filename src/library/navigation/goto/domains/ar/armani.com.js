f3567c3fa0a202fd8d3e3a10ace8a660f5618420
rajeshprofecialinks <rajesh.g@profecialinks.com>
2020-11-17 10:45:00 +0530

    armani core feed

---
+++ src/library/navigation/goto/domains/ar/armani.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'armani.com',
+    timeout: null,
+    country: 'AU',
+    store: 'armani',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/armani/AU/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'AU',
+    store: 'armani',
+    transform: null,
+    domain: 'armani.com',
+    zipcode: '',
+  },
+};
