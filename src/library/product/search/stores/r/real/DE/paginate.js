6447ea3595f5a7b35540a891691ddd87b332cacb
unknown <guntraddi>
2020-11-17 20:14:05 +0530

    updated for search_url ---saipava

--- src/library/product/search/stores/r/real/DE/paginate.js
+++ src/library/product/search/stores/r/real/DE/paginate.js
@@ -4,8 +4,11 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'real',
-    nextLinkSelector: 'a[class="btn -default pagination"]',
+    nextLinkSelector: null,
     loadedSelector: 'div#page',
+    openSearchDefinition: {
+      template: 'https://www.real.de/item/search/?search_value={searchTerms}&page={page}',
+    },
     domain: 'real.de',
     zipcode: '',
   },
