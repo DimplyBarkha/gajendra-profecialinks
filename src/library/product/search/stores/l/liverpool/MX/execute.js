a1e257ac4b6c64dd2b1d4128b961761739061847
unknown <shalini.kumari-odc@partner.import.io>
2020-11-18 01:11:31 +0530

    liverpool

--- src/library/product/search/stores/l/liverpool/MX/execute.js
+++ src/library/product/search/stores/l/liverpool/MX/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'MX',
     store: 'liverpool',
     domain: 'liverpool.mx',
-    url: 'https://www.liverpool.com.mx/tienda?s={searchTerms}',
+    url: 'https://www.liverpool.com.mx/tienda?s=labios',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
