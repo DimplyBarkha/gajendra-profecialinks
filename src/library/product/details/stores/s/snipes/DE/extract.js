b39e9e9401fa4e0581952ca0760123441e04f9ff
sobha rani <amol.thukrul-od@partner.import.io>
2020-11-17 22:26:14 +0530

    Snipes complete IM

--- src/library/product/details/stores/s/snipes/DE/extract.js
+++ src/library/product/details/stores/s/snipes/DE/extract.js
@@ -8,4 +8,28 @@ module.exports = {
     domain: 'snipes.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      // Method to Retrieve Xpath content of a Multiple Nodes
+      var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+      var description = getXpath('//div[@class="js-target"]/a/text()', 'nodeValue');
+      var description1 = getXpath("(//div[@class='b-pdp-carousel-item']/picture/img/@alt)[1]", 'nodeValue');
+      description = description + ' ' + description1;
+      addElementToDocument('description', description);
+    });
+    await context.extract(productDetails);
+  },
 };
