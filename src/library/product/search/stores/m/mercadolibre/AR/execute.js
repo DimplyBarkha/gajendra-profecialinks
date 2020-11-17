17a2d5d89fe4f0a77d6f78f3368cd2b4d43f5bfa
Karina Miernik <karina.miernik@gmail.com>
2020-11-17 11:28:12 +0100

    #1593 added xpaths and selectors

--- src/library/product/search/stores/m/mercadolibre/AR/execute.js
+++ src/library/product/search/stores/m/mercadolibre/AR/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'AR',
     store: 'mercadolibre',
     domain: 'mercadolibre.com.ar',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://listado.mercadolibre.com.ar/{searchTerms}#D',
+    loadedSelector: 'ol[class*="search-layout--stack"]',
+    noResultsXPath: '//div[@class="ui-search"]/div[contains(@class, "rescue")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/m/mercadolibre/AR/extract.js
+++ src/library/product/search/stores/m/mercadolibre/AR/extract.js
@@ -1,11 +1,41 @@
+const { transform } = require('../../../../shared');
+
+async function implementation (inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async () => {
+    const products = document.querySelectorAll('li[class*="layout__item"]');
+    products.forEach((product, index) => {
+      // set product url
+      const productIdArr = document.querySelector('head script:not([type]):not([src])').textContent.match(/dimension49",(.*)\)/)[1].split(',');
+
+      // @ts-ignore
+      product.setAttribute('id', productIdArr[index]);
+
+      // set rank
+      product.setAttribute('rank', (index + 1).toString());
+      // set soldBys
+      // @ts-ignore
+      const soldBy = product.querySelector('p[class*="store-label"]')
+        // @ts-ignore
+        ? product.querySelector('p[class*="store-label"]').innerText.split(' ') : null;
+      if (soldBy !== null) soldBy.splice(0, 2).join(' ');
+      product.setAttribute('soldBy', soldBy);
+    });
+  });
+
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'AR',
     store: 'mercadolibre',
-    transform: null,
+    transform: transform,
     domain: 'mercadolibre.com.ar',
     zipcode: '',
   },
+  implementation,
 };
