1ef899f64419596d3b09a8552281284035f0dee7
Lech Kulesza <lmkulesz@icloud.com>
2020-11-17 12:01:23 +0100

    #1585. fixes after testing

--- src/library/product/search/stores/m/mambo/BR/execute.js
+++ src/library/product/search/stores/m/mambo/BR/execute.js
@@ -1,12 +1,11 @@
-
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
     country: 'BR',
     store: 'mambo',
     domain: 'mambo.com.br',
-    url: null,
-    loadedSelector: null,
+    url: 'https://www.mambo.com.br/searchresults?Ntt={searchTerms}&Nty=1&No=0&Nrpp=12&Rdm=294&searchType=simple&type=search',
+    loadedSelector: '.CC-shelf',
     noResultsXPath: null,
     zipcode: '',
   },
--- src/library/product/search/stores/m/mambo/BR/extract.js
+++ src/library/product/search/stores/m/mambo/BR/extract.js
@@ -3,13 +3,14 @@ const { transform } = require('../../../../shared');
 async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+  await context.waitForSelector('.ts-shelf-rate-symbols.ts-sprite');
   await context.evaluate(async function () {
     function checkForProducts (selectorForProducts) {
       return document.querySelector(selectorForProducts) ? document.querySelectorAll(selectorForProducts).length : 0;
     }
-    function scrollToEndOfElement (element) {
-      const height = element.clientHeight;
-      window.scrollTo(0, height);
+    function scrollBy (number) {
+      const height = window.scrollY;
+      window.scrollTo(0, height + number);
     }
 
     const numberOfProducts = document.querySelector('.CC-searchCountFiltros .CC-count span')
@@ -17,20 +18,20 @@ async function implementation (inputs, parameters, context, dependencies) {
       : 0;
 
     let numberOfSeenProducts;
-
     do {
-      scrollToEndOfElement(document.querySelector('.CC-shelf.shelf-block'));
-      await new Promise((resolve, reject) => setTimeout(resolve, 500));
+      scrollBy(10);
+      await new Promise((resolve, reject) => setTimeout(resolve, 5));
       numberOfSeenProducts = checkForProducts('.CC-shelf.shelf-block .shelf-product');
-      console.log(`numberOfProducts: ${numberOfProducts} ---- numberOfSeenProducts: ${numberOfSeenProducts}`);
     } while (numberOfProducts !== numberOfSeenProducts);
-
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
     const productCards = document.querySelector('.CC-shelf.shelf-block .shelf-product') ? document.querySelectorAll('.CC-shelf.shelf-block .shelf-product') : [];
 
     productCards.forEach((card, index) => {
       card.setAttribute('data-rank-number', index + 1);
       const cardRatingContainer = card.querySelector('.shelf-rating');
-      const ratingPercentage = card.querySelector('.ts-shelf-rate-symbols.ts-sprite').style.width.replace('%', '') / 100;
+      const ratingPercentage = card.querySelector('.ts-shelf-rate-symbols.ts-sprite')
+        ? card.querySelector('.ts-shelf-rate-symbols.ts-sprite').style.width.replace('%', '') / 100
+        : 0;
       const aggregateRatingNormalized = ratingPercentage * 5;
       const div = document.createElement('div');
       div.id = 'aggregateRatingNormalized';
@@ -49,6 +50,6 @@ module.exports = {
     transform,
     domain: 'mambo.com.br',
     zipcode: '',
-    implementation,
   },
+  implementation,
 };
