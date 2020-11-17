22fcc82d26c905f578c021472d0290f94486d75a
Wayne E <bartekstafiej2@gmail.com>
2020-11-17 21:21:20 +0100

    #1594 Fixed name, price rank and rankoOrganic fields/ deployed extractor

--- src/library/product/search/stores/m/merqueo/CO/extract.js
+++ src/library/product/search/stores/m/merqueo/CO/extract.js
@@ -3,31 +3,9 @@ async function implementation (inputs, parameters, context, dependencies) {
 
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  await context.evaluate(() => {
-    const priceSelector = document.querySelectorAll('.mq-product-prices');
-    let price;
-    const productUrlSelector = document.querySelectorAll('.mq-product-img>div>a');
-    let productUrl;
-    const allProducts = productUrlSelector.length;
-
-    for (let i = 0; i < allProducts; i++) {
-      if (priceSelector[i]) {
-        price = priceSelector[i].textContent.replace('.', ',');
-      }
-
-      if (productUrlSelector[i].href !== undefined && productUrlSelector[i].href !== null) {
-        productUrl = 'https://merqueo.com' + productUrlSelector.href;
-      }
-
-      document.querySelectorAll('.mq-product-prices')[i].setAttribute('price', price);
-      document.querySelectorAll('.mq-product-img>div>a')[i].setAttribute('producturl', productUrl);
-      document.querySelectorAll('.mq-grid-item')[i].setAttribute('rank', `${i + 1}`);
-    }
-  });
-
   await context.evaluate(async () => {
     // scroll
-    function stall (ms) {
+    function stall(ms) {
       return new Promise((resolve, reject) => {
         setTimeout(() => {
           resolve();
@@ -41,6 +19,28 @@ async function implementation (inputs, parameters, context, dependencies) {
       scrollTop += 380;
       window.scroll(0, scrollTop);
     } while (scrollTop <= window.innerHeight);
+  });
+
+  await context.evaluate(() => {
+    const priceSelector = document.querySelectorAll('div[class="mq-product-prices"]');
+    let price;
+    const productUrlSelector = document.querySelectorAll('div[class="mq-product-img"]>div>a');
+    let productUrl;
+    const allProducts = productUrlSelector.length;
+
+    for (let i = 0; i < allProducts; i++) {
+      if (priceSelector[i]) {
+        price = priceSelector[i].textContent.replace('.', ',');
+      }
+
+      if (productUrlSelector[i].href !== undefined && productUrlSelector[i].href !== null) {
+        productUrl = productUrlSelector[i].href;
+      }
+
+      document.querySelectorAll('.mq-product-prices')[i].setAttribute('price', price);
+      document.querySelectorAll('.mq-product-img>div>a')[i].setAttribute('producturl', productUrl);
+      document.querySelectorAll('.mq-grid-item')[i].setAttribute('rank', `${i + 1}`);
+    }
   });
 
   return await context.extract(productDetails);
