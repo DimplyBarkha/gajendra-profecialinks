43c2fe4346aea6203fcc82988dc871919a185166
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-18 15:53:14 +0530

    aptekaolmed-Damayanthi

--- src/library/product/search/stores/w/whiteaway/DK/extract.js
+++ src/library/product/search/stores/w/whiteaway/DK/extract.js
@@ -1,25 +1,4 @@
 const { transform } = require('../../../../shared');
-// async function implementation (
-//   inputs,
-//   parameters,
-//   context,
-//   dependencies,
-// ) {
-//   const { transform } = parameters;
-//   const { productDetails } = dependencies;
-//   let promise = new Promise((resolve, reject) => {
-//     setTimeout(() => resolve("done!"), 10000)
-//   });
-
-//   let result = await promise; // wait until the promise resolves (*)
-
-  // console.log(result); // "done!"
-  // window.scrollBy(0, 10000);
-  // console.log("Test");
-  // return context.extract(productDetails, { transform });
-  
-
-// }
 
 module.exports = {
   implements: 'product/search/extract',
