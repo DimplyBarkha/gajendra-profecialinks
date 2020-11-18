4b96ec63e979aa4cc081917e8728f8f70c7f635b
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-18 18:26:23 +0530

    #1711 completed development

--- src/library/navigation/goto/domains/pe/petpost.com.au.js
+++ src/library/navigation/goto/domains/pe/petpost.com.au.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'petpost.com.au',
-    timeout: null,
+    timeout: 50000,
     country: 'AU',
     store: 'petpost',
     zipcode: '',
