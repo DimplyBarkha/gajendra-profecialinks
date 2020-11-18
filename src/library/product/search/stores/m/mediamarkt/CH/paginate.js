23b3186d706dea411ae4f8e66e7f8ab4e34e3080
Bartosz Kurzawa <kurzawa.bart@gmail.com>
2020-11-18 14:11:39 +0100

    Updated noResultsXPath #1466

--- src/library/product/search/stores/m/mediamarkt/CH/paginate.js
+++ src/library/product/search/stores/m/mediamarkt/CH/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: 'div.spinner',
     loadedSelector: 'ul[class=products-list]',
-    noResultsXPath: '//div[contains(@id, "search_no_result")] | //h1[contains(text(), "404")] | //div[contains(@class, "outer-brand")]',
+    noResultsXPath: '//div[contains(@id, "search_no_result")] | //h1[contains(text(), "404")] | //div[contains(@class, "outer-brand")] | //body[contains(@class, "page-product")]',
     resultsDivSelector: null,
     openSearchDefinition: null,
     domain: 'mediamarkt.ch/fr',
