4a2750a709271bd97595f5ead95d4f26f9c48a5e
IpsitaM <iamipsita07@gmail.com>
2020-11-18 20:48:46 +0530

    Pushing code for check

--- src/library/navigation/goto/domains/be/bebitus.com.js
+++ src/library/navigation/goto/domains/be/bebitus.com.js
@@ -1,11 +1,21 @@
-
 module.exports = {
-  implements: 'navigation/goto',
-  parameterValues: {
-    domain: 'bebitus.com',
-    timeout: null,
-    country: 'ES',
-    store: 'bebitus',
-    zipcode: '',
-  },
+    implements: 'navigation/goto',
+    parameterValues: {
+        domain: 'bebitus.com',
+        timeout: null,
+        country: 'ES',
+        store: 'bebitus',
+        zipcode: '',
+    },
+    implementation: async({ url }, parameters, context, dependencies) => {
+        await context.goto(url, {
+            first_request_timeout: 60000,
+            block_ads: false,
+            load_all_resources: true,
+            embed_iframes: false,
+            images_enabled: true,
+            timeout: 200000,
+            waitUntil: 'load',
+        });
+    },
 };
