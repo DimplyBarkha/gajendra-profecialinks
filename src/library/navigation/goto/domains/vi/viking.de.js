64dcb9d28e1515d6e2b8aa8d2a84aecba3f7bb38
BorysMaciuch <borysmaciuch@gmail.com>
2020-11-18 14:30:48 +0100

    #1496 create extractor, add xpaths

--- src/library/navigation/goto/domains/vi/viking.de.js
+++ src/library/navigation/goto/domains/vi/viking.de.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'viking.de',
-    timeout: null,
+    timeout: 30000,
     country: 'DE',
     store: 'viking',
     zipcode: '',
