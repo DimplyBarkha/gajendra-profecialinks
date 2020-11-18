acf7beee18e1f9fffc260f6a18ddd77f41e25c13
dhanush <dhanush.isukapalli@od.import.io>
2020-11-18 17:21:30 +0530

    otto by kavya

---
+++ src/library/product/search/stores/p/perfumesclub/ES/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'ES',
+    store: 'perfumesclub',
+    transform: null,
+    domain: 'perfumesclub.com',
+    zipcode: '',
+  },
+};
