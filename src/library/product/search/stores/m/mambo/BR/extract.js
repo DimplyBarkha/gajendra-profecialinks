3bf8fed53ce59977c71dd44c38e0ac64edaadaf3
Lech Kulesza <lmkulesz@icloud.com>
2020-11-18 17:44:50 +0100

    #1585. fixes to make extractor faster

--- src/library/product/search/stores/m/mambo/BR/extract.js
+++ src/library/product/search/stores/m/mambo/BR/extract.js
@@ -3,18 +3,8 @@ const { transform } = require('../../../../shared');
 async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  let flag = false;
-  await context.evaluate(async function () {
-    const numberOfProducts = document.querySelector('.CC-searchCountFiltros .CC-count span')
-      ? parseInt(document.querySelector('.CC-searchCountFiltros .CC-count span').innerText)
-      : 0;
-    if (numberOfProducts > 0) {
-      flag = true;
-    }
-  });
-  if (flag) {
-    await context.waitForSelector('.ts-shelf-rate-symbols.ts-sprite');
-  }
+
+  await context.waitForSelector('.ts-shelf-rate-symbols.ts-sprite');
   await context.evaluate(async function () {
     function checkForProducts (selectorForProducts) {
       return document.querySelector(selectorForProducts) ? document.querySelectorAll(selectorForProducts).length : 0;
@@ -31,10 +21,10 @@ async function implementation (inputs, parameters, context, dependencies) {
       let numberOfSeenProducts;
       do {
         scrollBy(10);
-        await new Promise((resolve, reject) => setTimeout(resolve, 5));
+        await new Promise((resolve, reject) => setTimeout(resolve, 10));
         numberOfSeenProducts = checkForProducts('.CC-shelf.shelf-block .shelf-product');
       } while (numberOfProducts !== numberOfSeenProducts);
-      await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+      await new Promise((resolve, reject) => setTimeout(resolve, 500));
       const productCards = document.querySelector('.CC-shelf.shelf-block .shelf-product') ? document.querySelectorAll('.CC-shelf.shelf-block .shelf-product') : [];
 
       productCards.forEach((card, index) => {
