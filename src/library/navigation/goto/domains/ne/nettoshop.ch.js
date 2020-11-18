3c44b4ba76ebb954e03f45ef6db30f5964375c7d
ravitejaw324 <70552284+ravitejaw324@users.noreply.github.com>
2020-11-18 19:12:02 +0530

    18755 ds core nettoshop ch (#1737)
    * Fixed the issue for aplus_images and enhanced_content
    
    * modified manufacturerDescription code
    
    * updated the code for enhanced content - 18755
    
    Co-authored-by: sujoypal144 <sujoypal144@gmail.com>

--- src/library/navigation/goto/domains/ne/nettoshop.ch.js
+++ src/library/navigation/goto/domains/ne/nettoshop.ch.js
@@ -8,4 +8,21 @@ module.exports = {
     store: 'nettoshop',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    
+    const timeout = parameters.timeout ? parameters.timeout : 30000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
+
--- src/library/product/details/stores/n/nettoshop/CH/extract.js
+++ src/library/product/details/stores/n/nettoshop/CH/extract.js
@@ -29,7 +29,7 @@ module.exports = {
             break;
           }
         }
-        function stall (ms) {
+        function stall(ms) {
           return new Promise((resolve, reject) => {
             setTimeout(() => {
               resolve();
@@ -46,8 +46,10 @@ module.exports = {
       }, cssSelector);
     };
 
+    await applyScroll(context);
     console.log('.....waiting......');
     await context.waitForSelector(cssProductDetails, { timeout: 5000 });
+
 
     const productAvailable = await isSelectorAvailable(cssProductDetails);
     console.log(`productAvailable: ${productAvailable}`);
@@ -75,13 +77,14 @@ module.exports = {
     // specs end
 
     // video click
-    const videoSelector = 'div[data-accordion="c-youtube-embed"] button';
+    let videoList = [];
+    const videoSelectorButton = '*[data-component*="c-youtube-embed"]';;
 
     console.log('.....waiting for video.....');
     let videoAvailable;
     try {
-      await context.waitForSelector(videoSelector, { timeout: 5000 });
-      videoAvailable = await isSelectorAvailable(videoSelector);
+      await context.waitForSelector(videoSelectorButton, { timeout: 20000 });
+      videoAvailable = await isSelectorAvailable(videoSelectorButton);
       console.log('.....waiting....complete..video..');
     } catch (e) {
     }
@@ -89,10 +92,24 @@ module.exports = {
     console.log(`videoAvailable: ${videoAvailable}`);
     if (videoAvailable) {
       console.log('clicking video button');
-      await context.click(videoSelector);
-      await context.waitForNavigation({ timeout: 5000, waitUntil: 'load' });
+      await context.click(videoSelectorButton);
+      //await context.waitForNavigation({ timeout: 5000, waitUntil: 'load' });
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      await context.waitForSelector('*[class*="c-tabs__video-item"] iframe', { timeout: 5000 });
       console.log('navigation complete!!');
+      videoList = await context.evaluate(async function() {
+          const videoSelector = 'div.c-youtube-embed iframe';
+          const allVideoNodes = document.querySelectorAll(videoSelector);
+          let videoList = [];
+          allVideoNodes.forEach(q => {
+            if(q.hasAttribute('src')) {
+              videoList.push(q.getAttribute('src'));
+            }
+          });
+          return videoList;
+      })
     }
+
     // video end
     // product tour click
     // const tourSelector = 'div[class="c-product-tour__item-head"] button';
@@ -121,8 +138,84 @@ module.exports = {
     //   }
     // }
     // product tour end
+
+    const tourButtonSelector = '*[id*="tour-tab-title"]';
+    const tourSelector = '*[aria-labelledby*="tour-tab-title"]';
+    try {
+      await context.waitForSelector(tourButtonSelector, { timeout: 20000 });
+    } catch(e) {
+      console.log("tourButtonSelector is not found");
+    }
+    const tourButtonAvailable = await isSelectorAvailable(tourButtonSelector);
+    if (tourButtonAvailable) {
+      // document.querySelector(tourSelector).click();
+      await context.click(tourButtonSelector);
+      try {
+        await context.waitForSelector(tourSelector, { timeout: 10000 });
+      } catch(er) {
+        console.log("tourSelector is not found")
+      }
+      const tourContentAvailable = await isSelectorAvailable(tourSelector);
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      //await context.waitForSelector('#inpage_container img', { timeout: 5000 });
+      if (tourContentAvailable) console.log('Product tour content available');
+      console.log('.....waiting....complete..for product tour content..');
+
+    }
+
+    await context.evaluate(async function (videoList) {
+
+      // function to append the elements to DOM
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.className = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      };
+
+      if (document.querySelector('*[id*="inpage_container"]')) {
+        //for manufacturerImages
+        let xpath1 = '//div[@id="inpage_container"]//img/@src';
+        let imgList = [];
+        let imgSelector = document.evaluate(xpath1, document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        for (let index = 0; index < imgSelector.snapshotLength; index++) {
+          let element = imgSelector.snapshotItem(index);
+          let imgURL = element.nodeValue;
+          imgList.push(imgURL);
+        }
+        for (let i = 0; i < imgList.length; i++) {
+          addElementToDocument('manufactureImages', imgList[i]);
+        }
+        // getting video from enhancedContent section
+        const videoSelector = '[title*=Flix-media-video]';
+        const videoSelectorNode = document.querySelector(videoSelector);
+        if(videoSelectorNode && videoSelectorNode.hasAttribute('src')) {
+          addElementToDocument('productVideos',videoSelectorNode.getAttribute('src'));
+        }
+
+        //for manufacturerDescription
+        let xpath2 = '//div[@id="inpage_container"]//div[contains(@class,"flix-Text-block" ) or contains(@class,"inpage_ftgridtext")]//div/text()';
+        let descList = [];
+        let descSelector = document.evaluate(xpath2, document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        for (let index = 0; index < descSelector.snapshotLength; index++) {
+          let element = descSelector.snapshotItem(index);
+          let desc = element.nodeValue;
+          descList.push(desc);
+        }
+        addElementToDocument('manufactureDescription', descList.join(" "));
+      }
+      if (videoList.length > 0) {
+        for (let i = 0; i < videoList.length; i++) {
+          addElementToDocument('productVideos', videoList[i]);
+        }
+      }
+
+    }, videoList);
+
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.extract(productDetails, { transform });
   },
 };
+
