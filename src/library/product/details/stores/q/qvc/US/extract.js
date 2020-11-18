9373bc3b0c8fb96b272c8c0a6499e87d23a38915
astropaula <wojciechowska.fran@gmail.com>
2020-11-18 15:01:10 +0100

    #1478 cleanup

--- src/library/product/details/stores/q/qvc/US/extract.js
+++ src/library/product/details/stores/q/qvc/US/extract.js
@@ -9,12 +9,11 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
-    //open ingredients tab
+    // open ingredients tab
     await context.evaluate(async function () {
       const buttonIngredients = document.querySelector('div[class*="accIngredients"] a[role="button"]');
       if (buttonIngredients) buttonIngredients.click();
     });
-    
     await context.extract(productDetails, { transform });
   },
 };
