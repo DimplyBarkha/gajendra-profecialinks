2dd2da00f867fd44e69522fe9d93f6d43ddf3e68
astropaula <wojciechowska.fran@gmail.com>
2020-11-18 14:10:46 +0100

    Filled extract.yaml, modified extract.js

--- src/library/product/details/stores/q/qvc/US/extract.js
+++ src/library/product/details/stores/q/qvc/US/extract.js
@@ -1,11 +1,20 @@
-
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'qvc',
-    transform: null,
+    transform: cleanUp,
     domain: 'qvc.com',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    //open ingredients tab
+    await context.evaluate(async function () {
+      const buttonIngredients = document.querySelector('div[class*="accIngredients"] a[role="button"]');
+      if (buttonIngredients) buttonIngredients.click();
+    });
+    
+    await context.extract(productDetails, { transform });
   },
 };
