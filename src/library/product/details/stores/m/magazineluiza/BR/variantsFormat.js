e134d25242339b758c49573daafd8fe5f97e8cb6
Gajender Ramola <ramola106@gmail.com>
2020-11-18 20:32:41 +0530

    #1712 variant implemented

--- src/library/product/details/stores/m/magazineluiza/BR/variantsFormat.js
+++ src/library/product/details/stores/m/magazineluiza/BR/variantsFormat.js
@@ -8,23 +8,21 @@ const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
       if (row.variantId) {
-        row.variantId.forEach(item => {
-          const vData = JSON.parse(item.text);
-          if (vData.attributes) {
-            vData.attributes.forEach(variation => {
-              var tmpVariations = {};
-              tmpVariations.variantId = [];
-              tmpVariations.variantUrl = [];
-              tmpVariations.variant = [];
-              tmpVariations.variantId.push({ text: variation.id });
-              tmpVariations.variantUrl.push({ text: 'https://www.magazineluiza.com.br' + variation.url });
-              tmpVariations.variant.push({ text: variation.value });
-              variations.push(tmpVariations);
-            });
-          } else {
-            delete row.variantId;
-          }
-        });
+        const vData = JSON.parse(row.variantId[0].text);
+        if (vData.attributes) {
+          vData.attributes.forEach(variation => {
+            var tmpVariations = {};
+            tmpVariations.variantId = [];
+            tmpVariations.variantUrl = [];
+            tmpVariations.variant = [];
+            tmpVariations.variantId.push({ text: variation.id });
+            tmpVariations.variantUrl.push({ text: 'https://www.magazineluiza.com.br' + variation.url });
+            tmpVariations.variant.push({ text: variation.value });
+            variations.push(tmpVariations);
+          });
+        } else {
+          delete row.variantId;
+        }
       }
     }
   }
