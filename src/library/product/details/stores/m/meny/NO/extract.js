84d739ccae32d19b628248355b916285893e9e8a
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-11-18 14:53:09 +0530

    Handled product not found manually

--- src/library/product/details/stores/m/meny/NO/extract.js
+++ src/library/product/details/stores/m/meny/NO/extract.js
@@ -8,4 +8,13 @@ module.exports = {
     domain: 'meny.no',
     zipcode: '',
   },
+  implementation: async (inputs, { country, domain, transform: transformParam }, context, { productDetails }) => {
+    await context.evaluate(function (inputs) {
+      const element = document.evaluate("//p[contains(., 'Noe gikk galt') and contains(., 'ikke funnet')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (element) {
+        throw new Error('Product not found for given input');
+      }
+    });
+    return await context.extract(productDetails);
+  },
 };
