e8ace529900ff2a96247ff5f64e8568149acf2b6
prasath <r.rahul@olivedatacentre.com>
2020-11-18 06:27:47 -0800

    bringmeister prasath

---
+++ src/library/product/search/stores/k/konzum/HR/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'HR',
+    store: 'konzum',
+    domain: 'konzum.hr',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
