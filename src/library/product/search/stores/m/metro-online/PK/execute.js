50d94ec012cda8ed52f9cf3196a82807b7389077
ganesh <ganesh@profecialinks.com>
2020-11-18 14:53:11 +0530

    execute time extend

--- src/library/product/search/stores/m/metro-online/PK/execute.js
+++ src/library/product/search/stores/m/metro-online/PK/execute.js
@@ -1,4 +1,32 @@
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
+  
+  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
 
+  console.log('Checking no results', parameters.noResultsXPath);
+
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+}
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -10,4 +38,5 @@ module.exports = {
     noResultsXPath: null,//"//a[contains(@class,'blueButton yellowhover') and text()='Search Again']",
     zipcode: '',
   },
+  implementation,
 };
