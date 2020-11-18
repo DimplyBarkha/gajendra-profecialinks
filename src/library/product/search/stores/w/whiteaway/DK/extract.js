41e2dfdde8fdad3c02dc47dbce10c681afe3df1f
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-18 19:49:20 +0530

    whiteaway-Damayanthi

--- src/library/product/search/stores/w/whiteaway/DK/extract.js
+++ src/library/product/search/stores/w/whiteaway/DK/extract.js
@@ -8,4 +8,129 @@ module.exports = {
     transform: transform,
     domain: 'whiteaway.com',
   },
+  implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addclass(xpathforpagination) {
+      var elems = document.querySelectorAll(xpathforpagination);
+      elems[0].classList.add('pagination');
+    }
+
+    //for rank
+    function addHiddenDiv(id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll('div[class="srp-product-box-outer list"]')[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    let rankOrganic;
+    let url = window.location.href;
+    let checkPageNumber = url.split('&')[1];
+    // if (checkPageNumber != null) {
+    //   checkPageNumber = checkPageNumber.split("-")[0];
+    // }
+    
+    try {
+      if (checkPageNumber.startsWith('counter=')) {
+        rankOrganic = checkPageNumber.replace('counter=', '');
+      }
+    }
+    catch (err) {
+    }
+
+
+    var dup = Number(rankOrganic);
+    dup = dup - 1; 
+
+    if (!rankOrganic) {
+      rankOrganic = 1;
+    } else {
+      rankOrganic = (dup * 21) + 22;
+    }
+    const urlProduct = document.querySelectorAll('div[class="srp-product-box-outer list"]');
+    for (let i = 0; i < urlProduct.length; i++) {
+      addHiddenDiv('rankOrganic', rankOrganic++, i);
+    }
+
+
+    const aggregateRating = document.querySelectorAll("div[class='star-rating__front']")
+    for (let k = 0; k < aggregateRating.length; k++) {
+      // @ts-ignore
+      let singleRating = aggregateRating[k].style.width;
+      singleRating = singleRating.slice(0, singleRating.length - 1)
+      singleRating = (5 * singleRating) / 100;
+      singleRating = singleRating.toFixed(1);
+      addHiddenDiv('aggregateRating', singleRating, k);
+    }
+
+    // // Double Pipe Concatenation
+    // const pipeSeparatorDouble = (id, data) => {
+
+    //   var doubleSeparatorText = data.join(' || ');
+      
+    //   addElementToDocument(id, doubleSeparatorText);
+      
+    //   };
+
+    // Method to Retrieve Xpath content of a Single Node
+    // var getXpath = (xpath, prop) => {
+    //   var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+    //   let result;
+    //   if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+    //   else result = elem ? elem.singleNodeValue : '';
+    //   return result && result.trim ? result.trim() : result;
+    // };
+
+    // var pagination = getXpath("li[class="item"]', 'nodeValue');
+    // if (pagination === 'pagination__link') {
+    //   addclass('ul.pagination li:last-child a');
+    // };
+
+    // for rank
+    // function addElementToDocument(key, value) {
+    //   const catElement = document.createElement('div');
+    //   catElement.id = key;
+    //   catElement.textContent = value;
+    //   catElement.style.display = 'none';
+    //   document.body.appendChild(catElement);
+    // }
+
+    // Method to Retrieve Xpath content of a Multiple Nodes
+    // const getAllXpath = (xpath, prop) => {
+    //   const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    //   const result = [];
+    //   for (let index = 0; index < nodeSet.snapshotLength; index++) {
+    //     const element = nodeSet.snapshotItem(index);
+    //     if (element) result.push(prop ? element[prop] : element.nodeValue);
+    //   }
+    //   return result;
+    // };
+
+    // for rank
+    // const sliceURL = (data) => {
+    //   var cnt = 0;
+    //   for (let index = 0; index < data.length; index++) {
+    //     if (data[0] != 0) {
+    //       cnt++;
+    //       addElementToDocument('altImages', cnt);
+    //     }
+    //   }
+    // };
+    // var backgroundURL = getAllXpath('//span[@class="product__name__productitemno"]/text()', 'nodeValue');
+    // sliceURL(backgroundURL);
+  });
+  //rank end
+
+
+  return await context.extract(productDetails, { transform });
+} 
