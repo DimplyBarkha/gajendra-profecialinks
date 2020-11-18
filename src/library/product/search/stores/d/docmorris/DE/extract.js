a288508913977c26daf7a762dd1587227cb3aa7d
unknown <akashkumar.p@osmosys.asia>
2020-11-18 10:41:17 +0530

    fixing changes

--- src/library/product/search/stores/d/docmorris/DE/extract.js
+++ src/library/product/search/stores/d/docmorris/DE/extract.js
@@ -20,7 +20,7 @@ async function implementation (
   const { transform } = parameters;
   await context.evaluate(async function(context) {
     //next page button click
-    const seeAllSelector = document.querySelector('.pagination > ul > li:nth-child(3) > a');
+    const seeAllSelector = document.querySelector('span[class="gicon-bracketright-green"]');
     if(seeAllSelector) {
       seeAllSelector.click();
     }
