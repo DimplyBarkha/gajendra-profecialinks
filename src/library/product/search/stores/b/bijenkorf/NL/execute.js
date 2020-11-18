656aafee4d39f434deb98f017c07b9c53211128a
Vivek Gondil <gondilvivek@gmail.com>
2020-11-18 19:02:51 +0530

    Adding no result xpath

--- src/library/product/search/stores/b/bijenkorf/NL/execute.js
+++ src/library/product/search/stores/b/bijenkorf/NL/execute.js
@@ -1,13 +1,12 @@
-
 module.exports = {
-  implements: 'product/search/execute',
-  parameterValues: {
-    country: 'NL',
-    store: 'bijenkorf',
-    domain: 'bijenkorf.nl',
-    url: 'https://www.debijenkorf.nl/product-lister-page.html?SearchTerm={searchTerms}',
-    loadedSelector: 'li[data-at="lister-product-item"]:nth-last-child(1)',
-    noResultsXPath: '//h2[contains(.,"hebben helaas geen")]',
-    zipcode: '',
-  },
+    implements: 'product/search/execute',
+    parameterValues: {
+        country: 'NL',
+        store: 'bijenkorf',
+        domain: 'bijenkorf.nl',
+        url: 'https://www.debijenkorf.nl/product-lister-page.html?SearchTerm={searchTerms}',
+        loadedSelector: 'li[data-at="lister-product-item"]:nth-last-child(1)',
+        noResultsXPath: '//div[contains(@class,"dbk-search-empty")]',
+        zipcode: '',
+    },
 };
