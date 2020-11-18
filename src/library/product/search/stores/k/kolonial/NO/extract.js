4fb697bf4e719d512abffe7340ef435018be8a18
venkatsn <venkat.sn-odc@partner.import.io>
2020-11-18 15:35:48 +0530

    changes done by deepak

--- src/library/product/search/stores/k/kolonial/NO/extract.js
+++ src/library/product/search/stores/k/kolonial/NO/extract.js
@@ -1,10 +1,10 @@
-
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'NO',
     store: 'kolonial',
-    transform: null,
+    transform: cleanUp,
     domain: 'kolonial.no',
     zipcode: '',
   },
@@ -17,13 +17,18 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
-      const getXpath = (xpath, prop) => {
-        const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-        let result;
-        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-        else result = elem ? elem.singleNodeValue : '';
-        return result && result.trim ? result.trim() : result;
-      };
+      // const getXpath = (xpath, prop) => {
+      //   const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+      //   let result;
+      //   if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+      //   else result = elem ? elem.singleNodeValue : '';
+      //   return result && result.trim ? result.trim() : result;
+      // };
+      const abc= document.querySelectorAll('div[class="col-xs-6 col-sm-3 col-md-2"]');
+      for(let i=1;i<=abc.length;i++)
+      {
+        addElementToDocument('rank1',i);
+      }
     });
       await context.extract(productDetails, { transform: transformParam });
     },
