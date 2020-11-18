912b12b180e1118cd73016f36a94972676aaa560
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-18 19:25:04 +0530

    QA2 updation for IM - akshay channa

--- src/library/product/details/stores/c/coopvitality/CH/extract.js
+++ src/library/product/details/stores/c/coopvitality/CH/extract.js
@@ -28,9 +28,33 @@ module.exports = {
       for (let i=0 ; i<len; i++){
         addElementToDocument('category', z[i]);
       }
-      // addElementToDocument('category', z[1]);
-      // addElementToDocument('category', z[1]);
-      // addElementToDocument('category', z[1]);
+     
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+
+      var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+
+      var name = getXpath('//span[@data-ui-id="page-title-wrapper"]/text()', 'nodeValue');
+      if (name != null){
+        var abc = name.split(" ");
+        var zz = abc.slice(Math.max(abc.length - 2, 1))
+        zz = zz.join(" ")
+        addElementToDocument('zz', zz);
+      }
+      
 
       
 
