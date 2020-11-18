68211f5d6e8b5210b30d52e46d7242ebea65a5b4
unknown <suryakanta.giet119@gmail.com>
2020-11-18 06:08:57 +0530

    fixed issues with aplus image, enhanced content

--- src/library/product/details/stores/e/ep/AT/createUrl.js
+++ src/library/product/details/stores/e/ep/AT/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'ep.at',
     prefix: null,
-    url: null,
+    url: 'https://www.ep.at/search/?text={id}',
     country: 'AT',
     store: 'ep',
     zipcode: '',
