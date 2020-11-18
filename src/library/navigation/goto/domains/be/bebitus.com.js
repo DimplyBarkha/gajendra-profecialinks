9808f6f0e2dfae8a4efbf76ba94bea5de3ef5ca4
IpsitaM <iamipsita07@gmail.com>
2020-11-18 20:50:56 +0530

    Pushing for check

--- src/library/navigation/goto/domains/be/bebitus.com.js
+++ src/library/navigation/goto/domains/be/bebitus.com.js
@@ -7,15 +7,5 @@ module.exports = {
         store: 'bebitus',
         zipcode: '',
     },
-    implementation: async({ url }, parameters, context, dependencies) => {
-        await context.goto(url, {
-            first_request_timeout: 60000,
-            block_ads: false,
-            load_all_resources: true,
-            embed_iframes: false,
-            images_enabled: true,
-            timeout: 200000,
-            waitUntil: 'load',
-        });
-    },
+
 };
--- src/library/product/details/stores/b/bebitus/ES/extract.js
+++ src/library/product/details/stores/b/bebitus/ES/extract.js
@@ -9,27 +9,4 @@ module.exports = {
         domain: 'bebitus.com',
         zipcode: '',
     },
-    implementation: async function(
-        inputs,
-        parameters,
-        context,
-        dependencies,
-    ) {
-        const { productDetails } = dependencies;
-        const { transform } = parameters;
-
-        const iframeSelector = await context.evaluate(async() => { document.querySelector("div[class*='video-wrapper']") });
-        if (iframeSelector) {
-            await context.evaluate(async(iframeSelector) => {
-                const mainBody = document.querySelector('body');
-                const iframe = await document.querySelector(iframeSelector);
-                const iframeDoc = iframe.contentDocument;
-                const video = await iframeDoc.querySelector("div[class*='video-wrapper']>iframe");
-                const videoSrc = video.getAttribute('src');
-                mainBody.setAttribute('video-src', videoSrc);
-            }, iframeSelector);
-        }
-
-        return await context.extract(productDetails, { transform });
-    }
 };
