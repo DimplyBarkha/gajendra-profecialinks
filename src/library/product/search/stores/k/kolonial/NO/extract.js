203c772d5bbe457a2e0fbf0c805f64b2ec811bf9
venkatsn <venkat.sn-odc@partner.import.io>
2020-11-18 15:50:00 +0530

    changes done by deepak

--- src/library/product/search/stores/k/kolonial/NO/extract.js
+++ src/library/product/search/stores/k/kolonial/NO/extract.js
@@ -10,26 +10,35 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      function addElementToDocument (key, value) {
+      function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
-      // const getXpath = (xpath, prop) => {
-      //   const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-      //   let result;
-      //   if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-      //   else result = elem ? elem.singleNodeValue : '';
-      //   return result && result.trim ? result.trim() : result;
-      // };
-      const abc= document.querySelectorAll('div[class="col-xs-6 col-sm-3 col-md-2"]');
-      for(let i=1;i<=abc.length;i++)
-      {
-        addElementToDocument('rank1',i);
+      const getXpath = (xpath, prop) => {
+        const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+      function ranking(rank) {
+        const abc = document.querySelectorAll('div[class="col-xs-6 col-sm-3 col-md-2"]');
+        for (let i = 1; i <= abc.length; i++) {
+          addElementToDocument('rank1', i);
+        }
       }
+      const rank = getXpath('//h3[@class="name"]//div[@class="name-main wrap-two-lines"]', 'nodeValue');
+      //const productWeight = productWeightNode ? (productWeightNode.includes('Weight') ? productWeightNode.replace('Weight ', '') : productWeightNode) : '';
+      //addElementToDocument('addedProductWeight', productWeight);
+      // const abc = document.querySelectorAll('div[class="col-xs-6 col-sm-3 col-md-2"]');
+      // for (let i = 1; i <= abc.length; i++) {
+      //   addElementToDocument('rank1', i);
+      // }
+      ranking(rank);
     });
-      await context.extract(productDetails, { transform: transformParam });
-    },
+    await context.extract(productDetails, { transform: transformParam });
+  },
 };
