2869588c292f8524fd70e3ad1d885cbfebaf0f8f
PrinceYadav1 <prince.y@osmosys.asia>
2020-11-19 14:30:54 +0530

    Issue 1669

--- src/library/product/search/stores/d/douglas/DE/extract.js
+++ src/library/product/search/stores/d/douglas/DE/extract.js
@@ -10,8 +10,30 @@ async function implementation (
   await context.evaluate(async function () {
     document.querySelectorAll('div.rd__rating__result').forEach(rating => {
       const width = Number(rating.getAttribute('style').match(/\d+/)[0]);
-      rating.parentElement.parentElement.innerText = (width / 20).toString();
+      var string = (width / 20).toString();
+      string = string.replace(".",",");
+      rating.parentElement.parentElement.innerText = string;
     });
+    function addHiddenDiv (id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll("div.rd__productinfo")[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    const product = document.querySelectorAll("div.rd__productinfo");
+    for (let i = 0; i < product.length; i++) {
+      // @ts-ignore
+      const name1 = (product[i].querySelectorAll('span'))[0].textContent;
+      const name2 = (product[i].querySelectorAll('span'))[1].textContent;
+      const name3 = (product[i].querySelectorAll('span'))[3].textContent;
+      const name4 = (product[i].querySelectorAll('span'))[2].textContent;
+      let name = name1+" "+name2+" "+name3+" "+name4;
+      if (name) {
+        addHiddenDiv('ii_name', name, i);
+      }
+    }
   });
   return await context.extract(productDetails, { transform });
 }
