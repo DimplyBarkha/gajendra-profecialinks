f0785428871646ec5f3d84eec648f0211e11c89d
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2020-11-19 12:40:39 +0100

    Added a custom createUrl functionality

--- src/library/product/details/stores/p/planetParfum/BE/createUrl.js
+++ src/library/product/details/stores/p/planetParfum/BE/createUrl.js
@@ -9,4 +9,16 @@ module.exports = {
     store: 'planetParfum',
     zipcode: '',
   },
+  implementation: async (inputs, parameterValues, context, dependencies) => {
+    const mainUrl = 'https://www.planetparfum.com/fr/';
+    await context.goto(mainUrl, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+    await context.waitForSelector('li.search.shortcut-search-suggest');
+    await context.click('form[role=search] fieldset input[type=text]');
+    await context.waitForSelector('form[role=search] fieldset input[type=text]');
+    await context.setInputValue('form[role=search] fieldset input[type=text]', inputs.id);
+    await context.click('form[role=search] fieldset input.form-submit');
+    await context.waitForSelector('body');
+    const productUrl = await context.evaluate(async () => (document.querySelector('span[itemprop="url"]') ? document.querySelector('span[itemprop="url"]').innerText : window.location.href));
+    return productUrl;
+  },
 };
