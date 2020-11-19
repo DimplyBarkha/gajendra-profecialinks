8985309c98de4c97296b7ec2287a035b359f98bd
venkatsn <venkat.sn-odc@partner.import.io>
2020-11-19 15:06:00 +0530

    deepak medpex

--- src/library/product/search/stores/m/medpex/DE/paginate.js
+++ src/library/product/search/stores/m/medpex/DE/paginate.js
@@ -3,13 +3,13 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'medpex',
-    nextLinkSelector: null,
+    nextLinkSelector: '#searchresult > div.pagenav > table > tbody > tr > td:nth-child(6) > a',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div[id="product-list"]',
-    openSearchDefinition: {
-      template: 'https://www.medpex.de/search.do?q={searchTerms}&pn={page}',
-    },
+    //openSearchDefinition: {
+     // template: 'https://www.medpex.de/search.do?q={searchTerms}&pn={page}',
+    //},
     domain: 'medpex.de',
   },
 };
