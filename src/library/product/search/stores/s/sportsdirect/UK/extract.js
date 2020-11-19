f271b51879b2d839f8c9fe26800724bea354e55f
Lokanath sahoo <loknath.s@osmosys.asia>
2020-11-19 09:37:03 +0530

    completed the dev

--- src/library/product/search/stores/s/sportsdirect/UK/extract.js
+++ src/library/product/search/stores/s/sportsdirect/UK/extract.js
@@ -40,54 +40,54 @@ async function implementation(
       })
     }
   });
-  try {
-    await context.waitForSelector('div[data-bv-show="inline_rating"]', { timeout: 4000 });
-    console.log(`id value loaded successfully`);
-  } catch (e) {
-    console.log(`id value did not load at all`);
-  }
+  // try {
+  //   await context.waitForSelector('div[data-bv-show="inline_rating"]', { timeout: 4000 });
+  //   console.log(`id value loaded successfully`);
+  // } catch (e) {
+  //   console.log(`id value did not load at all`);
+  // }
   //Api method for fetching the rating value and review count
-  await context.evaluate(async () => {
-    const productId = document.querySelectorAll('div[data-bv-show="inline_rating"]');
+  // await context.evaluate(async () => {
+  //   const productId = document.querySelectorAll('div[data-bv-show="inline_rating"]');
 
-    const productCodes = [];
+  //   const productCodes = [];
 
-    productId.forEach((element) => {
-      let text = element.getAttribute('data-bv-productid');
-      productCodes.push(text);
-    })
+  //   productId.forEach((element) => {
+  //     let text = element.getAttribute('data-bv-productid');
+  //     productCodes.push(text);
+  //   })
 
-    const prodString = productCodes.join(',');
+  //   const prodString = productCodes.join(',');
 
-    const apistring = `https://api.bazaarvoice.com/data/statistics.json?apiversion=5.4&passkey=caiGlgNZJbkmq4vv9Aasd5JdLBg2YKJzgwEEhL0sLkQUw&stats=Reviews&filter=ContentLocale:en_GB,en*&filter=ProductId:${prodString}`;
+  //   const apistring = `https://api.bazaarvoice.com/data/statistics.json?apiversion=5.4&passkey=caiGlgNZJbkmq4vv9Aasd5JdLBg2YKJzgwEEhL0sLkQUw&stats=Reviews&filter=ContentLocale:en_GB,en*&filter=ProductId:${prodString}`;
 
-    const response = await fetch(apistring);
+  //   const response = await fetch(apistring);
 
-    const responseData = await response.json();
-    console.log(`this is the value for responseData ${responseData}`);
+  //   const responseData = await response.json();
+  //   console.log(`this is the value for responseData ${responseData}`);
 
-    const ratingArr = [];
+  //   const ratingArr = [];
 
-    const reviewArr = [];
+  //   const reviewArr = [];
 
-    const idArray = [];
+  //   const idArray = [];
 
-    responseData.Results.forEach((element) => {
-      ratingArr.push(element.ProductStatistics.ReviewStatistics.AverageOverallRating);
-      reviewArr.push(element.ProductStatistics.ReviewStatistics.TotalReviewCount);
-      idArray.push(element.ProductStatistics.ProductId);
-    })
-    console.log(`this is the value for rating${ratingArr}`);
-    console.log(`this is the value for rating${reviewArr}`);
-    console.log(`this is the value for rating${reviewArr}`);
-    idArray.forEach((id, index) => {
-      let sel = `div[data-bv-productid="${id}"]`;
-      const appendElement = document.querySelector(sel);
-      appendElement.setAttribute('rating', ratingArr[index]);
-      appendElement.setAttribute('review', reviewArr[index]);
-    })
+  //   responseData.Results.forEach((element) => {
+  //     ratingArr.push(element.ProductStatistics.ReviewStatistics.AverageOverallRating);
+  //     reviewArr.push(element.ProductStatistics.ReviewStatistics.TotalReviewCount);
+  //     idArray.push(element.ProductStatistics.ProductId);
+  //   })
+  //   console.log(`this is the value for rating${ratingArr}`);
+  //   console.log(`this is the value for rating${reviewArr}`);
+  //   console.log(`this is the value for rating${reviewArr}`);
+  //   idArray.forEach((id, index) => {
+  //     let sel = `div[data-bv-productid="${id}"]`;
+  //     const appendElement = document.querySelector(sel);
+  //     appendElement.setAttribute('rating', ratingArr[index]);
+  //     appendElement.setAttribute('review', reviewArr[index]);
+  //   })
 
-  })
+  // })
   return await context.extract(productDetails, { transform });
 }
 const { transform } = require('../shared')
