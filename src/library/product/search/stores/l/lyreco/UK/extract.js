275e611540ff7b4474f59dbd7bd57e479f0989d0
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-20 00:46:34 +0530

    id search - akshay channa

--- src/library/product/search/stores/l/lyreco/UK/extract.js
+++ src/library/product/search/stores/l/lyreco/UK/extract.js
@@ -8,4 +8,63 @@ module.exports = {
     domain: 'lyreco.com',
     zipcode: '',
   },
+  implementation,
+};
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addclass(xpathforpagination) {
+      var elems = document.querySelectorAll(xpathforpagination);
+      elems[0].classList.add('pagination');
+    }
+    function addHiddenDiv(id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll('div[id="productCmsGrid"]>div>div>div')[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    function addElementToDocument(key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+    var getXpath = (xpath, prop) => {
+      var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+      let result;
+      if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+      else result = elem ? elem.singleNodeValue : '';
+      return result && result.trim ? result.trim() : result;
+      // price
+    };
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+      }
+      return result;
+    };
+
+    var id = getAllXpath('//div[@class="item-content"]/div[@class="block_left float"]/p[@class="reference_cms"]/text()', 'nodeValue');
+   
+    if (id != null) {
+      for (var i = 0; i < id.length; i++) {
+        var abc = id[i].split(":")[1];
+        addHiddenDiv('abc', abc, i);
+      }
+    }
+    
+  });
+  return await context.extract(productDetails, { transform });
 };
