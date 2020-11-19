c76864dce6361b27a44869e006fe304efcfc0499
dhanush <dhanush.isukapalli@od.import.io>
2020-11-19 23:14:47 +0530

    lyreco by kavya

---
+++ src/library/navigation/goto/domains/ly/lyreco.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'lyreco.com',
+    timeout: null,
+    country: 'ES',
+    store: 'lyreco',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/l/lyreco/ES/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'lyreco',
+    domain: 'lyreco.com',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
