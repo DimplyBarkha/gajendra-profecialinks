29e4622a36097f5ce3cf2a3b101efb29755ee782
dhanush <dhanush.isukapalli@od.import.io>
2020-11-19 21:56:28 +0530

    otto my kavya

---
+++ src/library/product/search/stores/l/lyreco/ES/paginate.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'ES',
+    store: 'lyreco',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'lyreco.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/l/lyreco/ES/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'ES',
+    store: 'lyreco',
+    domain: 'lyreco.com',
+    zipcode: '',
+  },
+};
