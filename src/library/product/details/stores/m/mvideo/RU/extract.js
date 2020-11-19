9fd9586e41134a0bf1bc4a504236e45a3787d250
unknown <shalini.kumari-odc@partner.import.io>
2020-11-19 22:05:35 +0530

    mvideo

--- src/library/product/details/stores/m/mvideo/RU/extract.js
+++ src/library/product/details/stores/m/mvideo/RU/extract.js
@@ -1,11 +1,26 @@
-
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'RU',
     store: 'mvideo',
-    transform: null,
+    transform: transform,
     domain: 'mvideo.ru',
     zipcode: '',
   },
-};
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    const doesPopupExist = await context.evaluate(function () {
+      return Boolean(document.querySelector('ul.c-tabs__menu-list> li:nth-child(2) > a'));
+      });
+      if (doesPopupExist) {
+      await context.click('ul.c-tabs__menu-list> li:nth-child(2) > a');
+      }
+    return await context.extract(productDetails, { transform });
+  },
+}
