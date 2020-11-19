0e83d6469265b998340eed364d84bf349c472f1e
viswa <viswashankar.veginati@od.import.io>
2020-11-19 21:59:15 +0530

    bestbuy by viswa

--- src/library/product/search/stores/b/bestbuy/CA/paginate.js
+++ src/library/product/search/stores/b/bestbuy/CA/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'CA',
     store: 'bestbuy',
-    nextLinkSelector: '#root > div > div > div.x-page-content.container_3Sp8P > div.singleColumn_BCnZ4.singleColumn > div > main > div.loadMoreRow_1TEZj.loadMoreButtonContainer_35w02 > button > span',
+    nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: '#root > div > div > footer > div.middleFooterSection_2AzRo > div > div.footerRightContent_1yMpE > div > div.newsLetterInputContainer_16LLU > form > div > button > span.content_3dXxd',
+    loadedSelector: null,
     noResultsXPath: null,
-    openSearchDefinition: null,
+    // openSearchDefinition: {
+    //   template: 'https://www.bestbuy.ca/en-ca/search?search={searchTerms}&pn={page}',
+    //   },
     domain: 'bestbuy.ca/en-ca',
     zipcode: '',
   },
