96a2c3f2384de8585849296c01f7a69eada5ea7e
Shreyam <shreyam.g@osmosys.asia>
2020-11-19 19:00:45 +0530

    Added video and upc xpaths

--- src/library/product/details/stores/b/bestbuy/US/extract.js
+++ src/library/product/details/stores/b/bestbuy/US/extract.js
@@ -8,4 +8,31 @@ module.exports = {
     domain: 'bestbuy.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      let videoTab = document.querySelector("button.image-button")
+      if (videoTab) {
+        videoTab.click();
+      }
+
+      let videoLink = document.querySelector("#product-videos-tab")
+      if (videoLink) {
+        videoLink.click();
+      }
+    })
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+
+    await context.evaluate(async function () {
+      let videos = document.evaluate(`//video[@class="video-player"]//source//@src`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (videos) {
+        document.body.setAttribute('video', videos.textContent);
+      }
+
+      let upc = document.evaluate('//div[contains(@class, "spec-categories")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+      if(upc) {
+        document.body.setAttribute('upc', upc.textContent.match(/(UPC\s)(\d+)/)[2]);
+      }
+    });
+    await context.extract(productDetails);
+  },
 };
