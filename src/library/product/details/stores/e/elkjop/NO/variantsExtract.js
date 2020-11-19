f893da80fc48d249eb1cff2e01d403fe2e7a9590
irfan shaikh <irfan.shaikh@synerzip.com>
2020-11-19 23:56:31 +0530

    #988. Fixed description, rpc, enhanced content fields

--- src/library/product/details/stores/e/elkjop/NO/variantsExtract.js
+++ src/library/product/details/stores/e/elkjop/NO/variantsExtract.js
@@ -1,10 +1,11 @@
 
+const { transform } = require('./variantFormat');
 module.exports = {
   implements: 'product/details/variants/variantsExtract',
   parameterValues: {
     country: 'NO',
     store: 'elkjop',
-    transform: null,
+    transform,
     domain: 'elkjop.no',
     zipcode: '',
   },
