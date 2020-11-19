d55327a20d7b91f10494ec0ae678d5f58896177f
sushma-mangalapally <sushma.mangalapally@import.io>
2020-11-19 11:18:49 -0800

    Add transform

--- src/library/product/details/stores/d/dosfarma/ES/extract.js
+++ src/library/product/details/stores/d/dosfarma/ES/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'ES',
     store: 'dosfarma',
-    transform: null,
+    transform,
     domain: 'dosfarma.com',
     zipcode: '',
   },
