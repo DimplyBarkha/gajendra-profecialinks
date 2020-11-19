a82a4dcf4de5823ce1c633aaccd9ae522acc50a2
nilofarsh <nilofar.shaikh@synerzip.com>
2020-11-19 23:54:49 +0530

    #1674 Search extractors,xpaths and inputs added

--- src/library/product/search/stores/c/colruyt/BE/extract.js
+++ src/library/product/search/stores/c/colruyt/BE/extract.js
@@ -1,11 +1,34 @@
 
+const { transform } = require('../format.js');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const searchURL = window.location.href;
+    addHiddenDiv('added_search_url', searchURL);
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'BE',
     store: 'colruyt',
-    transform: null,
+    transform,
     domain: 'colruyt.be',
     zipcode: '',
   },
+  implementation,
 };
