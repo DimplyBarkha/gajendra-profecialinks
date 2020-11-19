3773de361f3673b440f98948145c8ebf47b68722
David Skoglund <david.skoglund@import.io>
2020-11-18 21:28:50 -0800

    updated paginate no results xpath

--- src/library/product/search/stores/a/amazon/CA/paginate.js
+++ src/library/product/search/stores/a/amazon/CA/paginate.js
@@ -11,7 +11,7 @@ module.exports = {
     openSearchDefinition: {
       template: 'https://www.amazon.ca/s?k={searchTerms}&ref=nb_sb_noss_2&page={page}',
     },
-    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
+    noResultsXPath: '//div[contains(@class, "s-result-list")]/div[@data-asin]/span[contains(@class,"no-results")]',
     domain: 'amazon.ca',
   },
 };
