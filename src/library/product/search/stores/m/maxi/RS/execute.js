84075d3bdd7ffd6b4bf5bfe7dbafa056a573fbe3
Michał Wieczorek <michal126@hotmail.com>
2020-11-19 09:49:28 +0100

    changed url in execute.js

--- src/library/product/search/stores/m/maxi/RS/execute.js
+++ src/library/product/search/stores/m/maxi/RS/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'RS',
     store: 'maxi',
     domain: 'maxi.rs',
-    url: 'https://www.maxi.rs/online/search?q={searchTerms}',
+    url: 'https://www.maxi.rs/online/search?q={searchTerms}:relevance&text=something&sort=relevance&pageNumber=0',
     loadedSelector: 'div[data-testid="search-results-list-wrapper"]',
     noResultsXPath: '//div[@class="sc-3brks3-2 iyGbUN"]',
     zipcode: '',
