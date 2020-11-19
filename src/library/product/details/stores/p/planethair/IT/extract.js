fd89f57244f01525180e4e189ea87a6bdb2c6672
astropaula <wojciechowska.fran@gmail.com>
2020-11-19 17:43:09 +0100

    Filled extract.yaml, started editing extract.js

--- src/library/product/details/stores/p/planethair/IT/extract.js
+++ src/library/product/details/stores/p/planethair/IT/extract.js
@@ -1,11 +1,29 @@
+const { cleanUp } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'IT',
     store: 'planethair',
-    transform: null,
+    transform: cleanUp,
     domain: 'planethair.it',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    
+      var data = await context.extract(productDetails, { transform });
+      for (let k = 0; k < data.length; k++) {
+        for (let i = 0; i < data[k].group.length; i++) {
+          if ('description' in data[k].group[i]) {
+            var descrString = data[k].group[i].description[0].text;
+            console.log('!!!!!!!!!');
+            console.log(typeof(descrString));
+            // descrString = descrString.split('Ingredien');
+            // console.log('!!!!!!!!!');
+            // console.log(descrString[1]);
+          }
+        }
+      }
+      return data;
   },
 };
