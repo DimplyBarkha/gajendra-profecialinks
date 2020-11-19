f623b7c1eb3e0aa7059fc15823a09b5cb318d704
srishivams <srishivams97@gmail.com>
2020-11-19 23:16:32 +0530

    handling ID as inputs

--- src/library/product/details/stores/n/n11/TR/extract.js
+++ src/library/product/details/stores/n/n11/TR/extract.js
@@ -10,6 +10,20 @@ async function implementation (
   const { productDetails } = dependencies;
   const delay = t => new Promise(resolve => setTimeout(resolve, t));
   await delay(2000);
+  const pId = inputs.id;
+  const searchUrl = `https://www.n11.com/arama?q=${pId}`;
+  await context.goto(searchUrl, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+  let prodUrl = null;
+  prodUrl = await context.evaluate(function () {
+    if (document.querySelector('section[class*="listingGroup"] ul')) { return document.querySelector('section[class*="listingGroup"] ul[class="clearfix"] a').getAttribute('href'); }
+    return null;
+  });
+  if (prodUrl !== null) {
+    await context.goto(prodUrl, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+  } else {
+    prodUrl = 'https://www.n11.com';
+    await context.goto(prodUrl, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+  }
   return await context.extract(productDetails, { transform });
 }
 
