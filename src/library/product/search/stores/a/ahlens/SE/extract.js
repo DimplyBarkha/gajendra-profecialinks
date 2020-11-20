e7bb2d3989c27f4501062fea50f3b983837694ad
Your Name <karthik.ravi-odc@partner.import.io>
2020-11-20 21:39:13 +0530

    new

--- src/library/product/search/stores/a/ahlens/SE/extract.js
+++ src/library/product/search/stores/a/ahlens/SE/extract.js
@@ -7,43 +7,48 @@ async function implementation(
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await context.evaluate(async () => {   
-      function addHiddenDiv(id, content, index) {
-        const newDiv = document.createElement('div');
-        newDiv.className = id;
-        newDiv.textContent = content;
-        newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll('div[class*=MuiCardContent-root]')[index];
-        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+  await context.evaluate(async () => {
+    function addHiddenDiv(id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.className = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll('div[class*=MuiCardContent-root]')[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    // Method to Retrieve Xpath content of a Multiple Nodes
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
       }
-      // Method to Retrieve Xpath content of a Multiple Nodes
-      const getAllXpath = (xpath, prop) => {
-        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        const result = [];
-        for (let index = 0; index < nodeSet.snapshotLength; index++) {
-          const element = nodeSet.snapshotItem(index);
-          if (element) result.push(prop ? element[prop] : element.nodeValue);
+      return result;
+    };
+    const sliceURL = (data) => {
+      for (let index = 0; index < data.length; index++) {
+        var temp;
+        if (data[index].includes(",")) {
+          var temp = data[index].replace(",", ".");
+        } else if (data[index].includes(" ")) {
+          temp = data[index].replace(" ", "");
+        } else if (data[index].includes(":-")) {
+          temp = data[index].replace(":-", "");
+        }else {
+          temp=data[index]
         }
-        return result;
-      };
-      const sliceURL = (data) => {
-        for (let index = 0; index < data.length; index++) {
-          var temp;
-          if (data[index].includes(",")) {
-            var temp = data[index].replace(",", ".");
-          } else {
-            temp = data[index].replace(" ", "");
-          }
-          addHiddenDiv('zz', temp, index);
-        }
-      };
-      var backgroundURL = getAllXpath("//*[contains(@class,'MuiCardContent-root')]/div/div/span[1]/text()", 'nodeValue');
-      sliceURL(backgroundURL);   
-      while (!!document.querySelector('#ahl-product-list-app > div > div >button')) {
-        document.querySelector('#ahl-product-list-app > div > div >button').click()
-        await new Promise(r => setTimeout(r, 6000));
+        
+        addHiddenDiv('zz', temp, index);
       }
-    
+    };
+    var backgroundURL = getAllXpath("//*[contains(@class,'MuiCardContent-root')]/div/div/span[1]/text()", 'nodeValue');
+    sliceURL(backgroundURL);
+    while (!!document.querySelector('#ahl-product-list-app > div > div >button')) {
+      document.querySelector('#ahl-product-list-app > div > div >button').click()
+      await new Promise(r => setTimeout(r, 6000));
+    }
+
   })
   return await context.extract(productDetails, { transform });
 }
