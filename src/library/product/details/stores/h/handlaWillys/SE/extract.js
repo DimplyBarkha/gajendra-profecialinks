3f0caac06a2b8a1bab9264dcdc26efbca35ca851
Rajeev <rajeev.ranjan@synerzip.com>
2020-11-20 10:03:32 +0530

    #890 price

--- src/library/product/details/stores/h/handlaWillys/SE/extract.js
+++ src/library/product/details/stores/h/handlaWillys/SE/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'SE',
     store: 'handlaWillys',
-    transform: null,
+    transform: transform,
     domain: 'willys.se',
     zipcode: '',
   },
