0534770601027d02458cf18c8b86c82faf50bf84
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-11-20 17:03:48 +0200

    kalunga IM

--- src/library/product/details/stores/k/kalunga/BR/shared.js
+++ src/library/product/details/stores/k/kalunga/BR/shared.js
@@ -12,9 +12,20 @@ const transform = (data) => {
       try {
         if (gr.rpc) gr.rpc[0].text = gr.rpc[0].text.replace(onlyNumbers, '');
         if (gr.price) gr.price[0].text = gr.price[0].text.replace(onlyNumbers, '');
-        if (gr.uniqCode) gr.uniqCode[0].text = gr.uniqCode[0].text.replace(onlyNumbers, '');
+        if (gr.retailer_product_code) gr.retailer_product_code[0].text = gr.retailer_product_code[0].text.replace(onlyNumbers, '');
         if (gr.listPrice) gr.listPrice[0].text = gr.listPrice[0].text.replace(onlyNumbers, '');
         if (gr.currency) gr.currency[0].text = gr.currency[0].text.replace(currency, '').charAt(1);
+        if (gr.sub_category) {
+          gr.sub_category.shift();
+          gr.sub_category = [{ text: gr.sub_category.map(e => e.text).join(' > ') }];
+        }
+        if (gr.brand) {
+          const mainData = JSON.parse(gr.brand[0].text);
+          if (mainData) {
+            if (mainData.brand) gr.brand[0].text = mainData.brand.name;
+            gr.sku_number[0].text = mainData.sku;
+          }
+        }
       } catch (e) {
         console.log(e);
       }
