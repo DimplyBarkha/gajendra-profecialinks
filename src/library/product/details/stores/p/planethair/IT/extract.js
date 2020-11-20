e4129b085f07ec8dd4add28f844191a265eb9d2c
astropaula <wojciechowska.fran@gmail.com>
2020-11-20 17:48:26 +0100

    #1760 Updated extract.yaml, edited data in extract.js

--- src/library/product/details/stores/p/planethair/IT/extract.js
+++ src/library/product/details/stores/p/planethair/IT/extract.js
@@ -10,17 +10,40 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
-    
+
+    await context.evaluate(async () => {
+      // add rank attribute
+      var brand = document.querySelector('div[class="brand"] a').innerText;
+      brand = brand.replace(' ', '-');
+      const element = document.createElement('a');
+      element.id = "brandLink";
+      element.href = 'https://www.planethair.it/' + brand;
+      element.style.display = 'none';
+      document.body.appendChild(element);
+    });
+
       var data = await context.extract(productDetails, { transform });
       for (let k = 0; k < data.length; k++) {
         for (let i = 0; i < data[k].group.length; i++) {
           if ('description' in data[k].group[i]) {
             var descrString = data[k].group[i].description[0].text;
-            console.log('!!!!!!!!!');
-            console.log(typeof(descrString));
-            // descrString = descrString.split('Ingredien');
-            // console.log('!!!!!!!!!');
-            // console.log(descrString[1]);
+            descrString = descrString.split('Planethair.it:');
+            descrString = descrString[0];
+            descrString = descrString.split('Ingredienti');
+            descrString = descrString[0];
+            data[k].group[i].description[0].text = descrString;
+          }
+          if ('ingredientsList' in data[k].group[i]) {
+            if (data[k].group[i].ingredientsList[0].text.includes('Ingredienti')) {
+              var ingList = data[k].group[i].ingredientsList[0].text;
+              ingList = ingList.split('Planethair.it:');
+              ingList = ingList[0];
+              ingList = ingList.split('Ingredienti');
+              ingList = ingList[1];
+              data[k].group[i].ingredientsList[0].text = ingList;
+            } else {
+              data[k].group[i].ingredientsList[0].text = '';
+            }
           }
         }
       }
