7d1a2bf911aaae77e9309384444d776e8f09a8e3
Gajender Ramola <ramola106@gmail.com>
2020-11-20 16:42:24 +0530

    #1782 initial commit

---
+++ src/library/product/details/stores/s/samsclub/MX/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'sams.com.mx',
+    prefix: null,
+    url: 'https://www.sams.com.mx/a/r/{id}',
+    country: 'MX',
+    store: 'samsclub',
+    zipcode: '',
+  },
+};
