2ea0869a15d6d819196897c7c35042348ecac751
sobha rani <amol.thukrul-od@partner.import.io>
2020-11-20 23:51:20 +0530

    Snipes complete core

--- src/library/product/details/stores/s/snipes/DE/extract.js
+++ src/library/product/details/stores/s/snipes/DE/extract.js
@@ -10,14 +10,30 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      function addElementToDocument (key, value) {
+      function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+      var pp = getAllXpath("(//div[@class='b-pdp-attribute b-pdp-attribute--size js-pdp-attributes--size']/div/a/@data-value)", 'nodeValue');
+      if (pp != null) {
+        var ab = pp.join(' | ');
+        addElementToDocument('ab', ab);
+      }
+
       // Method to Retrieve Xpath content of a Multiple Nodes
+
       var getXpath = (xpath, prop) => {
         var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
         let result;
