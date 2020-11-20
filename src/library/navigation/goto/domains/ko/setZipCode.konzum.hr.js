dc54afec098a175e57526b5edff2ce2762a95007
prasath <r.rahul@olivedatacentre.com>
2020-11-20 00:51:02 -0800

    notino_de prasath

---
+++ src/library/navigation/goto/domains/ko/setZipCode.konzum.hr.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'HR',
+    domain: 'konzum.hr',
+    store: 'konzum',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/k/konzum/HR/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'HR',
+    store: 'konzum',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'konzum.hr',
+    zipcode: '',
+  },
+};
