f3a4aaa2018eeada34ad3f71b24af497e3f17e77
mania307 <maniakalnaa@gmail.com>
2020-11-20 16:24:08 +0100

    #1502 changed availabilityText

--- src/library/product/details/stores/v/vikingdirect/NL/execute.js
+++ src/library/product/details/stores/v/vikingdirect/NL/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'vikingdirect',
     domain: 'vikingdirect.nl',
     loadedSelector: 'div[id="productPage"]',
-    noResultsXPath: '//div[@id="searchEmpty"]|//div[contains(@class, "error__404")]',
+    noResultsXPath: '//div[@id="searchEmpty"]|//div[contains(@class, "error__404")]|//div[@id="heroBanner"]',
     zipcode: '',
   },
 };
--- src/library/product/details/stores/v/vikingdirect/NL/extract.js
+++ src/library/product/details/stores/v/vikingdirect/NL/extract.js
@@ -7,24 +7,25 @@ async function implementation(inputs, parameters, context, dependencies) {
 
   await context.evaluate(async () => {
 
-    const price = document.querySelector('div[id="productDetailsPanel"] div[class="product-price-panel__price-alt-vat"]')
-      ? document.querySelector('div[id="productDetailsPanel"] div[class="product-price-panel__price-alt-vat"]').textContent : '';
-    const regex = /(\d+,\d+)/;
-    // @ts-ignore
-    document.querySelector('body').setAttribute('price', price.match(regex)[1]);
+    if (document.querySelector('div[role="alertdialog"]') === null) {
+      const price = document.querySelector('div[id="productDetailsPanel"] div[class="product-price-panel__price-alt-vat"]')
+        ? document.querySelector('div[id="productDetailsPanel"] div[class="product-price-panel__price-alt-vat"]').textContent : '';
+      const regex = /(\d+,\d+)/;
+      // @ts-ignore
+      document.querySelector('body').setAttribute('price', price.match(regex)[1]);
 
 
-
-    const products = document.querySelectorAll('span[class="product__brand-name"]')
-    const prefix = 'https://www.vikingdirect.nl';
-    // const shortPrefix = 'https:';
-    products.forEach((product, index) => {
-      // set product url
-      const brandLink = product.querySelector('a').getAttribute('href');
-      product.setAttribute('brandLink', prefix + brandLink);
-      console.log(brandLink);
-      console.log(product.setAttribute('brandLink', prefix + brandLink));
-    });
+      const products = document.querySelectorAll('span[class="product__brand-name"]')
+      const prefix = 'https://www.vikingdirect.nl';
+      products.forEach((product, index) => {
+        // set product url
+        const brandLink = product.querySelector('a').getAttribute('href');
+        product.setAttribute('brandLink', prefix + brandLink);
+      });
+      //if product is not available set availabilityText to Out of Stock
+    } else if (document.querySelector('div[role="alertdialog"]').innerText === 'Helaas is dit product niet langer beschikbaar') {
+      document.querySelector('body').setAttribute('availabilityText', 'Out of Stock');
+    }
   });
 
   return await context.extract(productDetails, { transform });
