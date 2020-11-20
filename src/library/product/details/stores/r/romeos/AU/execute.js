0904ebff913aa7671d451d1458fcd4db04653021
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-11-20 08:24:04 +0100

    #1469 - update the noResultsXpath in the core extractor

--- src/library/product/details/stores/r/romeos/AU/execute.js
+++ src/library/product/details/stores/r/romeos/AU/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'romeos',
     domain: 'martinplace.romeosonline.com.au',
     loadedXpath: '//div[@class="ModalPopup__Content"]//img[@class="product_image"]',
-    noResultsXPath: '//h1/span[@class="weak" and text()="Whoops!"]',
+    noResultsXPath: '//h1/span[@class="weak" and text()="Whoops!"] | //div[@class="MoreInfo"]//strong[text()="No longer available"]',
     zipcode: '',
   },
 };
