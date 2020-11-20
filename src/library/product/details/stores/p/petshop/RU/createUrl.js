ee778aaf4d24f437543b924be6ee620cf7f457b1
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-11-20 13:27:30 +0100

    - Add custom createUrl action to the core extractor

--- src/library/product/details/stores/p/petshop/RU/createUrl.js
+++ src/library/product/details/stores/p/petshop/RU/createUrl.js
@@ -9,4 +9,13 @@ module.exports = {
     store: 'petshop',
     zipcode: '',
   },
+  dependencies: {
+    goto: 'action:navigation/goto',
+  },
+  implementation: async ({ id }, { domain }, context, dependencies) => {
+    await dependencies.goto({ url: `https://www.${domain}/search/?q=${id}` });
+    const productHref = await context.evaluate(async () => (document.querySelector('ul.product-list > li > a') ? document.querySelector('ul.product-list > li > a').getAttribute('href') : null));
+    if (!productHref) throw new Error('Failed to create a URL for a given id');
+    return productHref;
+  },
 };
