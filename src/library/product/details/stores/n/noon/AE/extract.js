4a1203ed319a5f122eaa0bb192ec9a821bc3a7bc
prateek.s <psc3018@gmail.com>
2020-11-21 00:06:12 +0530

    fixed issue for specification

--- src/library/product/details/stores/n/noon/AE/extract.js
+++ src/library/product/details/stores/n/noon/AE/extract.js
@@ -8,5 +8,29 @@ module.exports = {
     domain: 'noon.com',
     zipcode: "''",
   },
+  implementation: async function implementation (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    let enhancedContent = await context.evaluate(()=>{
+      return Boolean(document.querySelector('div[class*="richContent"]'));
+    });
+    if(enhancedContent){
+      await context.evaluate(()=>{
+        document.querySelector('div[class*="richContent"]').scrollIntoView({behavior:"smooth"})
+      });
+      try{
+        await context.waitForSelector('div[class*="richContent"] img');
+      }catch(err){
+        console.log('no enhanced content')
+      }
+    }
+    return await context.extract(productDetails, { transform });
+  }
+  
 };
 
