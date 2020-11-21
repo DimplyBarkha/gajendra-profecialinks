add73db69ba4864e3bd2f2db4421b3078beb6aba
priyankag-trinesis <priyankag@trinesis.com>
2020-11-21 12:18:45 +0530

    changes for maxi

--- src/library/product/search/stores/m/maxi/RS/execute.js
+++ src/library/product/search/stores/m/maxi/RS/execute.js
@@ -6,8 +6,6 @@ module.exports = {
     store: 'maxi',
     domain: 'maxi.rs',
     url: 'https://www.maxi.rs/online/search?q={searchTerms}',
-    // loadedSelector: 'div.ak6cwf-7 erXDfJ',
-    // noResultsXPath: '//p/span',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
