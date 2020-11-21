44f646ac3bad7aedc8d9fbaaefda8c68e8a03e1f
Vaijat Kokate <vaijatvk@gmail.com>
2020-11-21 14:12:06 +0530

    Commit

--- src/library/product/details/stores/d/deichmann/DE/extract.js
+++ src/library/product/details/stores/d/deichmann/DE/extract.js
@@ -1,11 +1,28 @@
+const { transform } = require("../format");
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'DE',
     store: 'deichmann',
-    transform: null,
+    transform,
     domain: 'deichmann.com',
     zipcode: '',
+  },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+
+    await context.evaluate(async () =>{
+      document.querySelector(".open-size-selector").click();
+      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+      document.querySelector(".size-selection").querySelector(".active-element").querySelector("span").click();
+    })
+
+    await context.extract(productDetails, { transform });
   },
 };
