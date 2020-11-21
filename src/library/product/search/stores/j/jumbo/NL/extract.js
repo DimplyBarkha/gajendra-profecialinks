1ba67f74c3e685c19a81fed9ca1f782c4b8220a4
Wayne E <bartekstafiej2@gmail.com>
2020-11-21 11:05:14 +0100

    #1353 fixed price field

--- src/library/product/search/stores/j/jumbo/NL/extract.js
+++ src/library/product/search/stores/j/jumbo/NL/extract.js
@@ -4,14 +4,24 @@ async function implementation (inputs, parameters, context, dependencies) {
   await context.evaluate(() => {
     const productUrlAll = document.querySelectorAll('.list-methods a.opposite');
     let productUrl;
-    for (let i = 0; i < productUrlAll.length; i++) {
+    const priceArray = [];
+    let price;
+    const priceSelector = document.querySelectorAll('span.jum-product-price__current-price.d-inline-flex.align-items-start');
+    for (let i = 0; i < priceSelector.length; i++) {
+      price = priceSelector[i].textContent;
+
       if (productUrlAll[i].href.includes('https')) {
         productUrl = productUrlAll[i].href;
       } else {
-        productUrl = 'https://www.bol.com' + productUrlAll[i].href;
+        productUrl = 'https://www.jumbo.com' + productUrlAll[i].href;
       }
 
+      priceArray[0] = price.match('..$');
+      priceArray[1] = price.replace(priceArray[0], '');
+      price = priceArray[1] + ',' + priceArray[0];
+
       document.querySelectorAll('.list-methods a.opposite')[i].setAttribute('productUrl', productUrl);
+      document.querySelectorAll('div[analytics-tag="product card"]')[i].setAttribute('price', price);
       document.querySelectorAll('div[analytics-tag="product card"]')[i].setAttribute('rank', `${i + 1}`);
       document.querySelectorAll('div[analytics-tag="product card"]')[i].setAttribute('rankOrganic', `${i + 1}`);
     };
