bf3a10dc15480e1c5672be16f71533cb0c8855f5
vijay <judhishtor@profecialinks.com>
2020-11-21 16:37:34 +0530

    #1791 Deploying search

---
+++ src/library/product/details/stores/e/etos/NL/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'etos.nl',
+    prefix: null,
+    url: null,
+    country: 'NL',
+    store: 'etos',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/e/etos/NL/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'NL',
+    store: 'etos',
+    transform: null,
+    domain: 'etos.nl',
+    zipcode: '',
+  },
+};
