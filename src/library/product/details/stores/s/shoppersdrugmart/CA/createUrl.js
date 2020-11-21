7a92ef8525312889271115c2ace7b855d65c0e3a
Gajender Ramola <ramola106@gmail.com>
2020-11-21 12:35:16 +0530

    #1792 initial commit

---
+++ src/library/product/details/stores/s/shoppersdrugmart/CA/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'shoppersdrugmart.ca',
+    prefix: null,
+    url: 'https://shop.shoppersdrugmart.ca/p/{id}',
+    country: 'CA',
+    store: 'shoppersdrugmart',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/s/shoppersdrugmart/CA/format.js
@@ -0,0 +1,58 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.image) {
+        row.image.forEach(item => {
+          item.text = item.text.replace('/100/', '/800/');
+        });
+      }
+      if (row.alternateImages) {
+        row.alternateImages.forEach(item => {
+          item.text = item.text.replace('/100/', '/800/');
+        });
+      }
+      if (row.ratingCount) {
+        row.ratingCount.forEach(item => {
+          item.text = item.text.replace('Reviews', '');
+          item.text = item.text.trim();
+        });
+      }
+      if (row.ratingCount) {
+        row.ratingCount.forEach(item => {
+          item.text = item.text.trim();
+        });
+      }
+      if (row.nameExtended) {
+        if (row.brandText) {
+          row.nameExtended = [{ text: row.brandText[0].text + ' - ' + row.nameExtended[0].text }];
+        }
+      }
+    }
+  }
+  return cleanUp(data);
+};
+
+module.exports = { transform };
