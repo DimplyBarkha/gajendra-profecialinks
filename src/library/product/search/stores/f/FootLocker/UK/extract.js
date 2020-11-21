8e099986743294a50b9c14830055c2ea18ed5613
waquas ali <waquas2ali@gmail.com>
2020-11-21 15:58:01 +0530

    Loading Footlocker UK Search - Issue: #1732

--- src/library/product/search/stores/f/FootLocker/UK/extract.js
+++ src/library/product/search/stores/f/FootLocker/UK/extract.js
@@ -8,19 +8,21 @@ module.exports = {
     domain: 'footlocker.co.uk',
     zipcode: '',
   },
-  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
-    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
     const applyScroll = async function (context) {
       await context.evaluate(async function () {
         let scrollTop = 0;
         while (scrollTop !== 20000) {
-          await stall(500);
-          scrollTop += 100;
+          scrollTop += 500;
           window.scroll(0, scrollTop);
-          if (scrollTop === 20000) {
-            await stall(5000);
-            break;
-          }
+          await stall(1000);
         }
         function stall (ms) {
           return new Promise((resolve, reject) => {
@@ -32,6 +34,36 @@ module.exports = {
       });
     };
     await applyScroll(context);
-    return await context.extract(productDetails, { transform });
+  
+    async function getProductsCount (context) {
+      return context.evaluate(async function () {
+        const products = document.evaluate('//div[@class="fl-product-tile--basic"]//picture[@class="fl-picture"]/img/@srcset', document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        return products.snapshotLength;
+      });
+    }
+  
+    let productsCount = 0;
+    while (productsCount < 150) {
+      const doesLoadMoreExists = await context.evaluate(function () {
+        return Boolean(document.querySelector('.text-center > div > .fl-btn--inner'));
+      });
+  
+      if (doesLoadMoreExists) {
+        await context.evaluate(async function () {
+          console.log('Clicking on load more button');
+          document.querySelector('.text-center > div > .fl-btn--inner').click();
+          await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+        });
+        productsCount = await getProductsCount(context);
+        console.log('productsCount' + productsCount);
+        if (productsCount >= 150) {
+          break;
+        }
+        await applyScroll(context);
+      } else {
+        break;
+      }
+    }
+    return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
   },
 };
