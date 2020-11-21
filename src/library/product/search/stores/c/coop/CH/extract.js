47aa0da17bd832b380b4a86b94ddc6c86c808001
Rajeev <rajeev.ranjan@synerzip.com>
2020-11-22 01:28:07 +0530

    #1455 search

--- src/library/product/search/stores/c/coop/CH/extract.js
+++ src/library/product/search/stores/c/coop/CH/extract.js
@@ -17,7 +17,7 @@ module.exports = {
           }, ms);
         });
       }
-      
+
       let scrollTop = 0;
       while (scrollTop !== 10000) {
         await stall(500);
@@ -30,19 +30,29 @@ module.exports = {
       }
       const moreButton = document.evaluate('//a[@class="list-page__trigger"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
       if (moreButton && moreButton.singleNodeValue != null) {
-        let index = 0
-          while (index < 5) {
+        let index = 0;
+        while (index < 2) {
           await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-          try{
+          try {
             moreButton.singleNodeValue.click();
-          }catch(e) {}
+            // await new Promise((resolve, reject) => setTimeout(resolve, 500));
+            let scrollTop = 0;
+            while (scrollTop !== 20000) {
+              await stall(500);
+              scrollTop += 1000;
+              window.scroll(0, scrollTop);
+              if (scrollTop === 20000) {
+                await stall(500);
+                break;
+              }
+            }
+          } catch (e) {}
           await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-          let scrollTop = 0;
-          index ++;
+          const scrollTop = 0;
+          index++;
         }
       }
     });
     return await context.extract(productDetails, { transform });
   },
 };
-
--- src/library/product/search/stores/c/coop/CH/paginate.js
+++ src/library/product/search/stores/c/coop/CH/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     // nextLinkSelector: 'span.list-page__trigger__text',
     // mutationSelector: null,
     // spinnerSelector: null,
-    loadedSelector: 'ul.list-page__content li',
+    loadedSelector: 'ul.list-page__content',
     noResultsXPath: '//h1[contains(text(),"Leider keine Treffer")]',
     // openSearchDefinition: null,
     domain: 'coop.ch',
