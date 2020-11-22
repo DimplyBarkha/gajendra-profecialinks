a564bd1e81b4f0a85f9b75d3961119f2d9ab4191
ravitejaw324 <ravi.w@partner.import.io>
2020-11-22 13:52:29 +0530

    fixed the variantId issue

--- src/library/product/details/stores/b/binglee/AU/extract.js
+++ src/library/product/details/stores/b/binglee/AU/extract.js
@@ -8,5 +8,28 @@ module.exports = {
     transform: transform,
     domain: 'binglee.com.au',
     zipcode: '',
-  },
+  }, implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(async function () {
+      const allScriptNodes = document.querySelectorAll('script[type="application/ld+json"]');
+      let scriptText = "";
+      allScriptNodes.forEach(q => {
+        if (q.textContent && q.textContent.includes('identifier')) {
+          scriptText = q.textContent;
+        }
+      });
+      if (scriptText) {
+        scriptText = JSON.parse(scriptText);
+        if (scriptText.identifier) {
+          document.body.insertAdjacentHTML('afterbegin', `<div id="prodVarId" style="display: hidden">${scriptText.identifier}</div>`)
+        }
+      }
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+  }
 };
