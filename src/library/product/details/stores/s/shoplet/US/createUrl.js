ddd101dd3192a3ee7bfb7ef70979257a529d3213
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-23 02:36:47 +0530

    #1847 develop going on

---
+++ src/library/product/details/stores/s/shoplet/US/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'shoplet.com',
+    prefix: null,
+    url: 'https://www.shoplet.com/product/{id}/spdv',
+    country: 'US',
+    store: 'shoplet',
+    zipcode: '',
+  },
+};
