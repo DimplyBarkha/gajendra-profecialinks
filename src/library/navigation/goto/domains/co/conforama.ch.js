f432a6e3b89af57c572d5e3a47d9f4d039a9ff4e
Rajeev <rajeev.ranjan@synerzip.com>
2020-11-22 13:16:14 +0530

    #1643 image

--- src/library/navigation/goto/domains/co/conforama.ch.js
+++ src/library/navigation/goto/domains/co/conforama.ch.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'conforama.ch',
-    timeout: 80000,
+    timeout: 100000,
     country: 'CH',
     store: 'conforama',
     zipcode: '',
