2fde3f7b0ff0ace28f7db808b1389334ff1bc0ed
ganesh <ganesh@profecialinks.com>
2020-11-22 16:08:24 +0530

    deploy

---
+++ src/library/product/details/stores/m/myntra/IN/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'myntra.com',
+    prefix: null,
+    url: "https://www.myntra.com/category/brand/slug/{id}/buy",
+    country: 'IN',
+    store: 'myntra',
+    zipcode: '',
+  },
+};
