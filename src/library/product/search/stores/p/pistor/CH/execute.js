4273ad475f5fed6ed7b5a0dabc7eb12a4d1f3963
Paulina Wyrwas <paulina.p.wyrwas@gmail.com>
2020-11-23 08:40:08 +0100

    added loadedselector and noresultsxpath

--- src/library/product/search/stores/p/pistor/CH/execute.js
+++ src/library/product/search/stores/p/pistor/CH/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'pistor',
     domain: 'pistorone.ch',
     url: 'https://www.pistorone.ch/index.php/locale:de_CH/ctx:L2NhdGFsb2cy/search?add_article={searchTerms}',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'table.productlist.list',
+    noResultsXPath: '//div[@id="search_content"]//div[@class="infobox"]',
     zipcode: '',
   },
 };
