16f77c974e8d3a31728c924d3fe6fc5216e66674
avinashkadam <avinash.kadam@profecialinks.com>
2020-11-23 17:19:44 +0530

    walmart reviews

---
+++ src/library/product/reviews/stores/w/walmart/US/extract.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/reviews/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'walmart',
+    transform: null,
+    filterReviews: null,
+    domain: 'walmart.com',
+    zipcode: '',
+  },
+};
