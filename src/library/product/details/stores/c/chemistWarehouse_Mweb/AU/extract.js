2e0e684637e725957f7a6c8ce020fe1229a63d86
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-11-23 12:48:06 +0530

    xpaths added (#1460)

--- src/library/product/details/stores/c/chemistWarehouse_Mweb/AU/extract.js
+++ src/library/product/details/stores/c/chemistWarehouse_Mweb/AU/extract.js
@@ -1,11 +1,30 @@
-
+const { transform } = require('../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AU',
     store: 'chemistWarehouse_Mweb',
-    transform: null,
+    transform,
     domain: 'chemistwarehouse.com.au',
     zipcode: '',
   },
+  // implementation: async (inputs,
+  //   parameters,
+  //   context,
+  //   dependencies,
+  // ) => {
+  //   await context.evaluate(async function () {
+  //     function addHiddenDiv (id, content) {
+  //       const newDiv = document.createElement('div');
+  //       newDiv.id = id;
+  //       newDiv.textContent = content;
+  //       newDiv.style.display = 'none';
+  //       document.body.appendChild(newDiv);
+  //     }
+
+  //   });
+  //   const { transform } = parameters;
+  //   const { productDetails } = dependencies;
+  //   return await context.extract(productDetails, { transform });
+  // },
 };
