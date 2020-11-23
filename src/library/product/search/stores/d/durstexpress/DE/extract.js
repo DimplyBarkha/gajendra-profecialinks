1b6d2b9f22cae7f54f8be1842f2a63f357311175
Raghav <user.email>
2020-11-23 21:37:15 +0530

    updating yaml with boolean values to false

--- src/library/product/search/stores/d/durstexpress/DE/extract.js
+++ src/library/product/search/stores/d/durstexpress/DE/extract.js
@@ -1,12 +1,28 @@
-const { transform } = require('../../../../shared');
+const { transform } = require("../../../../shared");
 module.exports = {
-implements: 'product/search/extract',
-parameterValues: {
-country: 'DE',
-store: 'durstexpress',
-transform: transform,
-domain: 'durstexpress.de',
-zipcode: ''
-},
-
-};
+  implements: "product/search/extract",
+  parameterValues: {
+    country: "DE",
+    store: "durstexpress",
+    transform: transform,
+    domain: "durstexpress.de",
+    zipcode: "",
+  },
+}
+//   implementation: async (inputs, parameters, context, dependencies) => {
+//     const { transform } = parameters;
+//     const { productDetails } = dependencies;
+//     await context.evaluate(() => {
+//       function addElementToDocument(key, value) {
+//         const catElement = document.createElement("div");
+//         catElement.id = key;
+//         catElement.textContent = value;
+//         catElement.style.display = "none";
+//         document.body.appendChild(catElement);
+//       }
+//       const URL = window.location.href;
+//       addElementToDocument("pd_url", URL);
+//     });
+//     return await context.extract(productDetails, { transform });
+//   },
+// };
