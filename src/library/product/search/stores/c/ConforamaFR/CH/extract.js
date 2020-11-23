5abaa9c31be564a42e646638588f596593a35c1c
unknown <guntraddi>
2020-11-23 18:19:16 +0530

    search url

--- src/library/product/search/stores/c/ConforamaFR/CH/extract.js
+++ src/library/product/search/stores/c/ConforamaFR/CH/extract.js
@@ -13,32 +13,36 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(() => {
-      try {
-        function addHiddenDiv(id, content, index) {
-          const newDiv = document.createElement('div');
-          newDiv.id = id;
-          newDiv.textContent = content;
-          newDiv.style.display = 'none';
-          const originalDiv = document.querySelectorAll('article[class="box-product    "]')[index];
-          originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-        }
-        const name = document.querySelectorAll('article[class="box-product    "]');
-        // @ts-ignore
-        const URL = window.tc_vars.originalPageURL;
-        console.log(URL);
-        let price;
-        for (let i = 0; i < name.length; i++) {
-          // @ts-ignore
-          addHiddenDiv('prodName', document.querySelectorAll('article[class="box-product    "]')[i].innerText, i);
-          // @ts-ignore
-          price = document.querySelectorAll('div[class="price-product"]')[i].innerText;
-          var priceUpdated = price.replace("€", ".");
-          addHiddenDiv('price', '€ ' + priceUpdated, i);
-          addHiddenDiv('pd_url', URL, i);
-        }
-      } catch (error) {
+      function addHiddenDiv(id, content, index) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll('article[class="box-product    "]')[index];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      }
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
       }
 
+      const name = document.querySelectorAll('article[class="box-product    "]');
+      // @ts-ignore
+      const URL = window.tc_vars.originalPageURL;
+      console.log(URL);
+      let price;
+      for (let i = 0; i < name.length; i++) {
+        // @ts-ignore
+        addHiddenDiv('prodName', document.querySelectorAll('article[class="box-product    "]')[i].innerText, i);
+        // @ts-ignore
+        price = document.querySelectorAll('div[class="price-product"]')[i].innerText;
+        var priceUpdated = price.replace("€", ".");
+        addHiddenDiv('price', '€ ' + priceUpdated, i);
+      }
+      addElementToDocument('pd_url', URL);
     });
     return await context.extract(productDetails, { transform });
   },
