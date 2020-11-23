da6fedada25294d79663e74ff7f18fd14d7d4295
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-24 01:05:34 +0530

    pagination - akshay channa

--- src/library/product/search/stores/l/lenstore/IT/paginate.js
+++ src/library/product/search/stores/l/lenstore/IT/paginate.js
@@ -4,10 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'IT',
     store: 'lenstore',
-    nextLinkSelector: null,
+    nextLinkSelector: 'div[class="search-results-filter-bottom"]>div[class="search-results-filter search-results-filter--right"]>nav[class="_pagination"]>a[class="_pagination__link _pagination__link--next "]',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'body',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'lenstore.it',
