cf851f0b5176cabc9dd542098736efa04f26d549
BorysMaciuch <borysmaciuch@gmail.com>
2020-11-23 12:17:11 +0100

    #1496 QA fixes

--- src/library/product/details/stores/v/viking/DE/extract.js
+++ src/library/product/details/stores/v/viking/DE/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'DE',
     store: 'viking',
-    transform: null,
+    transform: transform,
     domain: 'viking.de',
     zipcode: '',
   },
