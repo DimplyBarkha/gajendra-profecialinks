4755a8336a75da995823fd986f3d5baeead61a1a
prasath <r.rahul@olivedatacentre.com>
2020-11-23 02:24:10 -0800

    konzum prasath

--- src/library/product/search/stores/k/konzum/HR/extract.js
+++ src/library/product/search/stores/k/konzum/HR/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'HR',
     store: 'konzum',
-    transform: null,
+    transform,
     domain: 'konzum.hr',
     zipcode: '',
   },
