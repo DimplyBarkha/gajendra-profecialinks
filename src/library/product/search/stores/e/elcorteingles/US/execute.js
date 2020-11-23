79b69570abaf9ea24cd7831c932a7b1f36a0f2a3
priyankaL <priyanka.l@osmosys.asia>
2020-11-23 11:16:23 +0530

    pushing the data

---
+++ src/library/product/search/stores/e/elcorteingles/US/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'US',
+    store: 'elcorteingles',
+    domain: 'elcorteingles.US',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/e/elcorteingles/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'elcorteingles',
+    transform: null,
+    domain: 'elcorteingles.US',
+    zipcode: '',
+  },
+};
