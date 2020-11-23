74d335536d407ccb03346d929cccad98688a55ee
Richa-rani <richa.rani@osmosys.asia>
2020-11-23 17:33:10 +0530

    timeout issue fixed

--- src/library/navigation/goto/domains/ex/expert.at.js
+++ src/library/navigation/goto/domains/ex/expert.at.js
@@ -13,7 +13,7 @@ module.exports = {
     await context.setLoadAllResources(true);
     await context.setLoadImages(true);
     await context.setAntiFingerprint(false);
-    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url, { timeout: 100000, waitUntil: 'load', checkBlocked: true });
     // await context.goto(`${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
   },
 };
