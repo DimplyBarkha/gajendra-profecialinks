d341ed12302257d4b4bae39cf6473b2a1c9bb169
sahoojudhi <judhishtor@profecialinks.com>
2020-11-23 11:36:20 +0530

    Core extractor created #1849

---
+++ src/library/product/details/stores/p/petcircle/AU/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'AU',
+    store: 'petcircle',
+    transform: null,
+    domain: 'petcircle.com.au',
+    zipcode: '',
+  },
+};
