99496dea33e53433141a146a48d72fa772d47f57
osmarghyadeep <arghyadep.g@partner.import.io>
2020-11-24 03:19:15 +0530

    Updated the code to extract videos from gallery and desc section

--- src/library/product/details/stores/p/pccomponentes/ES/extract.js
+++ src/library/product/details/stores/p/pccomponentes/ES/extract.js
@@ -28,8 +28,43 @@ module.exports = {
       }
     });
     if(prodUrlLink && !isProdPage) {
-      await context.goto(prodUrlLink, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+      console.log('productLink we are going to - ' + prodUrlLink);
+      await context.goto(prodUrlLink, { timeout: 20000, waitUntil: 'load', checkBlocked: true });
     }
+
+    try {
+
+      await context.click('div[class*="demoup-playbutton-wrapper"] div[title*="Play video"] div[class*="demoupUI-playimage"], div[class*="demoup-product-description-wrapper"] div[title*="Play video"] div[class*="demoupUI-playimage"]');
+      await context.waitForSelector('div[class*="demoupUI-playlist-slider"]');
+
+      await context.evaluate(async function() {
+        let allVideosLinkList = [];
+        allVideosLinkList = document.querySelectorAll('div[class*="demoupUI-playlist-slider"] div[class*="demoupUI-item"] img');
+        let allUrls = [];
+        if(allVideosLinkList.length > 0) {
+          for(let index = 0; index < allVideosLinkList.length; index++) {
+            if (allVideosLinkList[index].hasAttribute('src')) {
+              let thisUrl = allVideosLinkList[index].src.split('-')[0];
+              thisUrl += '.mp4';
+              allUrls.push(thisUrl);
+              console.log(thisUrl);
+            } else {
+              console.log('no src for - ' + index + 'th element');
+            }
+            document.getElementsByTagName('body')[0].setAttribute(`allvideos`, allUrls.join(' | '));
+          }
+        } else {
+          console.log('we do not have the list of videos');
+        }
+      });
+    } catch (e) {
+      console.log(e.message);
+      console.log('the div we are looking to click - may not be present');
+    }
+    
+    
+
+
     return await context.extract(productDetails, { transform: transformParam });
   }
 };
