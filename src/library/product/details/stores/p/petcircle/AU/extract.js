0d1d0c7ecf308fd721de373e0df8c15e739ddff6
sahoojudhi <judhishtor@profecialinks.com>
2020-11-23 22:56:00 +0530

    xpath and transform completed #1849

--- src/library/product/details/stores/p/petcircle/AU/extract.js
+++ src/library/product/details/stores/p/petcircle/AU/extract.js
@@ -1,10 +1,10 @@
-
+const {transform} = require('../AU/format')
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AU',
     store: 'petcircle',
-    transform: null,
+    transform,
     domain: 'petcircle.com.au',
     zipcode: '',
   },
