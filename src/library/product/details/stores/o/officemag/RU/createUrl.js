83bffeba06b368b940ed1309f2475daebbcbe7ae
Michał Wieczorek <michal126@hotmail.com>
2020-11-23 15:50:39 +0100

    filled up part of the extractor

--- src/library/product/details/stores/o/officemag/RU/createUrl.js
+++ src/library/product/details/stores/o/officemag/RU/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'officemag.ru',
     prefix: null,
-    url: null,
+    url: 'https://www.officemag.ru/catalog/goods/{id}/',
     country: 'RU',
     store: 'officemag',
     zipcode: '',
--- src/library/product/details/stores/o/officemag/RU/extract.js
+++ src/library/product/details/stores/o/officemag/RU/extract.js
@@ -1,12 +1,49 @@
-const { cleanUp } = require('../../../../shared');
+const { transform } = require('../../../../shared');
+
+async function implementation (inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async () => {
+    function addElementToDom (element, id) {
+      const div = document.createElement('div');
+      div.id = id;
+      div.innerHTML = element;
+      document.body.appendChild(div);
+    }
+
+    // add product url
+    const productUrl = window.location.href;
+    addElementToDom(productUrl, 'productUrl');
+
+    // add img url
+    const prefix = 'https://www.officemag.ru';
+    const rawImgNode = document.querySelector('a[id="photosListLink"] img');
+    if (rawImgNode) {
+      const imgUrl = rawImgNode.getAttribute('src');
+      addElementToDom(prefix + imgUrl, 'imgUrl');
+    }
+
+    // alternate imgs
+    const altImgsNode = document.querySelectorAll(
+      'a[class="ProductPhotoThumb__link js-bigImg"] img');
+    altImgsNode.forEach(element => {
+      const altImgUrl = element.getAttribute('src');
+      element.setAttribute('altImgUrl', prefix + altImgUrl);
+    });
+  });
+
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'RU',
     store: 'officemag',
-    transform: cleanUp,
+    transform: transform,
     domain: 'officemag.ru',
     zipcode: '',
   },
+  implementation,
 };
