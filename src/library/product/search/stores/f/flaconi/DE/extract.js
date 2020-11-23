52ba85b190d9e7afe64c3964fc36ac1d5a0180c6
venkatsn <venkat.sn-odc@partner.import.io>
2020-11-23 16:12:13 +0530

    deepak flaconi

--- src/library/product/search/stores/f/flaconi/DE/extract.js
+++ src/library/product/search/stores/f/flaconi/DE/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require('../../../../shared');
+const { transform } = require('../../../../shared');  
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +8,14 @@ module.exports = {
     domain: 'flaconi.de',
     zipcode: '',
   },
+  //  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+  //   await context.evaluate(async function () {
+  //       while(!!document.querySelector('body > div.wrapper.off-canvas-wrap > div.inner-wrap.row-collapse > div > div.content > div > div > div.toolbar.row > div > div > div:nth-child(3) > a')){
+  //         // @ts-ignore
+  //         document.querySelector('body > div.wrapper.off-canvas-wrap > div.inner-wrap.row-collapse > div > div.content > div > div > div.toolbar.row > div > div > div:nth-child(3) > a').click()
+  //         await new Promise(r => setTimeout(r, 6000));
+  //       }
+  //     })
+  //     await context.extract(productDetails, { transform: transformParam });
+  //   }
 };
