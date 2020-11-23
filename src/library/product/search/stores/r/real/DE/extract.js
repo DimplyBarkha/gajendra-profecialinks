c7e457b1792bf42615de25ebf211ee1f00dd89ca
unknown <guntraddi>
2020-11-23 16:54:55 +0530

    updated search_URL for real

--- src/library/product/search/stores/r/real/DE/extract.js
+++ src/library/product/search/stores/r/real/DE/extract.js
@@ -8,4 +8,25 @@ module.exports = {
     domain: 'real.de',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(() => {
+      function addHiddenDiv(id, content, index) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll('div[class="img-wrapper rd-product__image-container"]')[index];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      }
+      const URL = window.location.href;
+      const products = document.querySelectorAll('div[class="img-wrapper rd-product__image-container"]');
+      for (let i = 0; i < products.length; i++) {
+        addHiddenDiv('pd_url', URL, i);
+      }
+
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
