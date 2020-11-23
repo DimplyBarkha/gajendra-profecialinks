3f0bab7f9791e35d089f6d07b527b6f3248d1f83
sushma-mangalapally <sushma.mangalapally@import.io>
2020-11-23 12:30:37 -0800

    Add changes for goto file

--- src/library/navigation/goto/domains/fn/fnac.es.js
+++ src/library/navigation/goto/domains/fn/fnac.es.js
@@ -8,4 +8,27 @@ module.exports = {
     store: 'fnac',
     zipcode: '',
   },
+  implementation: async ({ url }, parameterValues, context, dependencies) => {
+
+    const timeout = parameterValues.timeout ? parameterValues.timeout : 60000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
+
+    try {
+      await context.waitForSelector('span.geetest_radar_tip_content', { timeout: 45000 });
+    } catch (error) {
+      console.log('No verification needed.');
+    }
+
+    const verifyAccess = await context.evaluate(function () {
+      return Boolean(document.querySelector('span.geetest_radar_tip_content'));
+    });
+
+    if (verifyAccess) {
+      await context.evaluate(function () {
+        document.querySelector('span.geetest_radar_tip_content').click();
+      });
+    }
+
+    await context.waitForSelector('span.Header__logo-img', { timeout: 45000 });
+  },
 };
