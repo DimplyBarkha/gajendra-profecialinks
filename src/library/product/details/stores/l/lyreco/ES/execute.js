b960bf2a0eb3f5ad90b5ab45c360cc2a5cf848ea
dhanush <dhanush.isukapalli@od.import.io>
2020-11-23 20:46:54 +0530

    lyreco by kavya

---
+++ src/library/product/details/stores/l/lyreco/ES/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'lyreco',
+    domain: 'lyreco.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
