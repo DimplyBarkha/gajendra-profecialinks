369d6773a9138f95903fc33168826fcc5b15c761
harishkhattri <harish.khattri@synerzip.com>
2020-11-23 19:08:44 +0530

    Extract thumbnail URL for given number of required results

--- src/library/navigation/goto/domains/ca/casinodrive.fr.js
+++ src/library/navigation/goto/domains/ca/casinodrive.fr.js
@@ -3,13 +3,13 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'casinodrive.fr',
-    timeout: 20000,
+    timeout: 30000,
     country: 'FR',
     store: 'casinodrive',
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 20000;
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
     await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
--- src/library/product/search/stores/c/casinodrive/FR/extract.js
+++ src/library/product/search/stores/c/casinodrive/FR/extract.js
@@ -6,14 +6,29 @@ async function implementation (
   context,
   dependencies,
 ) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
   await context.evaluate(async function (results) {
+    function addHiddenDiv (id, content, container) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      container.appendChild(newDiv);
+    }
+
     const productCards = document.querySelectorAll('div[class*="viewports-enabled-fop__"]');
     if (productCards && productCards.length > 0) {
       const length = (productCards.length > results) ? results : productCards.length;
       for (let i = 0; i < length; i++) {
-        if ((i % 5) === 0) {
+        if ((i % 5) === 3) {
           productCards[i].scrollIntoView({ behavior: 'smooth' });
           await new Promise(resolve => setTimeout(resolve, 500));
+        }
+        const thumbnail = productCards[i].querySelector('a[data-synthetics="bop-link"] img');
+        if (thumbnail) {
+          addHiddenDiv('ii_thumbnail', thumbnail.getAttribute('src'), productCards[i]);
         }
         const loadedCards = productCards[i].querySelector('div[class*="base__BoxCard"]');
         if (!loadedCards) {
@@ -24,8 +39,6 @@ async function implementation (
     }
   }, results);
 
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
   return await context.extract(productDetails, { transform });
 }
 
