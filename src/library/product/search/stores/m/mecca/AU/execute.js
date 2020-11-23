895fb5c736df5f0eede2e3c35b380c241ac94136
astropaula <wojciechowska.fran@gmail.com>
2020-11-23 13:29:34 +0100

    Made clean up

--- src/library/product/search/stores/m/mecca/AU/execute.js
+++ src/library/product/search/stores/m/mecca/AU/execute.js
@@ -1,30 +1,28 @@
-// async function implementation (inputs, parameters, context, dependencies) {
-//   console.log('params', parameters);
-//   const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
-  
-//   await context.goto(url);
-//   const isRightPage = await context.evaluate(async () => {
-//     return document.querySelector('div[class="container-fluid"]');
-//   });
-  
-//   if (isRightPage != null) {
-//     const url1 = 'https://www.mecca.com.au/search?q="{searchTerms}"'.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
-//     await context.goto(url1);
-//    } 
-//   if (parameters.loadedSelector) {
-//     await context.waitForFunction(function (sel, xp) {
-//       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-//     }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
-//   }
-//   console.log('Checking no results', parameters.noResultsXPath);
-//   return await context.evaluate(function (xp) {
-//     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-//     console.log(xp, r);
-//     const e = r.iterateNext();
-//     console.log(e);
-//     return !e;
-//   }, parameters.noResultsXPath);
-// };
+async function implementation (inputs, parameters, context, dependencies) {
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await context.goto(url);
+  const isRightPage = await context.evaluate(async () => {
+    return document.querySelector('div[class="container-fluid"]');
+  });
+  if (isRightPage != null) {
+    const url1 = 'https://www.mecca.com.au/search?q="{searchTerms}"'.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+    await context.goto(url1);
+   } 
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+};
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -36,5 +34,5 @@ module.exports = {
     noResultsXPath: '//div[@class="no-hits-help"]',
     zipcode: '',
   },
-  // implementation,
+  implementation,
 };
--- src/library/product/search/stores/m/mecca/AU/extract.js
+++ src/library/product/search/stores/m/mecca/AU/extract.js
@@ -1,5 +1,20 @@
-const { transform } = require('./format');
+const { transform } = require('../../../../shared');
 
+async function implementation (inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async () => {
+    // add rank attribute
+    var rank = document.querySelectorAll('div[class="product-tile"]');
+
+    rank.forEach((element, index) => {
+      element.setAttribute('rank', (index + 1).toString());
+    });
+  });
+
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,4 +24,5 @@ module.exports = {
     domain: 'mecca.com.au',
     zipcode: '',
   },
+  implementation,
 };
