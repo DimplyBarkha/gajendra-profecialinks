ecb41f185d281a2b8c027e2ac5fc130ee6da591e
viswa <viswashankar.veginati@od.import.io>
2020-11-23 21:22:24 +0530

    bestbuy by viswa

---
+++ src/library/navigation/goto/domains/ma/mall.cz.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'mall.cz',
+    timeout: 50000,
+    country: 'CZ',
+    store: 'Mall',
+    zipcode: '',
+  },
+};
