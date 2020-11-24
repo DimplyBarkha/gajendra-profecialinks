e68ec62ba71585bc442ee9c8f7b50f97ee2e7518
sangeeta <sangeta.kumari@synerzip.com>
2020-11-24 11:22:37 +0530

    search-tmall_ru #1632

--- src/library/navigation/goto/domains/tm/tmall.ru.js
+++ src/library/navigation/goto/domains/tm/tmall.ru.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'tmall.ru',
-    timeout: 50000,
+    timeout: 30000,
     country: 'RU',
     store: 'tmall',
     zipcode: '',
--- src/library/product/search/stores/s/shop-apotheke/DE/execute.js
+++ src/library/product/search/stores/s/shop-apotheke/DE/execute.js
@@ -4,7 +4,8 @@ module.exports = {
     country: 'DE',
     store: 'shop-apotheke',
     domain: 'shop-apotheke.com',
-    url: 'https://www.shop-apotheke.com/search.htm?&query={searchTerms}',
+    url: 'https://www.shop-apotheke.com/search.htm?&page={page}&query={searchTerms}&isCompetitiveProducts=y&g=y',
+    //&isCompetitiveProducts=y&g=y
     loadedSelector: 'ul div[data-qa-id="result-list-entry"]',
     noResultsXPath: '//h1[contains(.,"Kein Suchergebnis gefunden")]',
   },
