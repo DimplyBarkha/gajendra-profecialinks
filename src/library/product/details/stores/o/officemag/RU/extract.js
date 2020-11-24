f1f3d536e0af60b3dd77b6b1468014d1d65fe8b8
Michał Wieczorek <michal126@hotmail.com>
2020-11-24 11:09:51 +0100

    Filled up remaining fields (#1472)

--- src/library/product/details/stores/o/officemag/RU/extract.js
+++ src/library/product/details/stores/o/officemag/RU/extract.js
@@ -1,11 +1,11 @@
 const { transform } = require('../../../../shared');
 
-async function implementation (inputs, parameters, context, dependencies) {
+async function implementation(inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
   await context.evaluate(async () => {
-    function addElementToDom (element, id) {
+    function addElementToDom(element, id) {
       const div = document.createElement('div');
       div.id = id;
       div.innerHTML = element;
@@ -26,11 +26,23 @@ async function implementation (inputs, parameters, context, dependencies) {
 
     // alternate imgs
     const altImgsNode = document.querySelectorAll(
-      'a[class="ProductPhotoThumb__link js-bigImg"] img');
-    altImgsNode.forEach(element => {
+      'a[class="ProductPhotoThumb__link js-bigImg"] img'
+    );
+    altImgsNode.forEach((element) => {
       const altImgUrl = element.getAttribute('src');
       element.setAttribute('altImgUrl', prefix + altImgUrl);
     });
+
+    // convert rating
+    const rating = document.querySelector('span[class="Score__fill"]');
+    const regex = /\d+\.?\d+/gm;
+
+    if (rating) {
+      var regArray = rating.getAttribute('style').match(regex);
+      var value = (parseInt(regArray[0]) * 5) / 100;
+
+      rating.setAttribute('rating', value.toString().replace('.', ','));
+    }
   });
 
   return await context.extract(productDetails, { transform });
