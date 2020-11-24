e3ed4838e5e04a4940db165b210a3ca195c75e8a
avinashkadam <avinash.kadam@profecialinks.com>
2020-11-24 19:15:20 +0530

    search extractor added for migros-CH

---
+++ src/library/product/search/stores/m/migros/CH/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'CH',
+    store: 'migros',
+    domain: 'migros.ch',
+    url: 'https://shop.migros.ch/en/search?query={searchTerms}',
+    loadedSelector: 'div.product-grid-component.search',
+    noResultsXPath: null,
+    zipcode:'',
+  },
+};
---
+++ src/library/product/search/stores/m/migros/CH/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'CH',
+    store: 'migros',
+    domain: 'migros.ch',
+    zipcode: '',
+  },
+};
