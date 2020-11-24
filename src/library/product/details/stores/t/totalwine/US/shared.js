9bb4a7b7987408d78ccb8ee58ded4385c038bec1
Richa Tiwari <richa.t@osmosys.asia>
2020-11-24 16:01:54 +0545

    Created extractor for Total Wine US

--- src/library/product/details/stores/t/totalwine/US/shared.js
+++ src/library/product/details/stores/t/totalwine/US/shared.js
@@ -4,61 +4,50 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-  for (const { group } of data) {
-    for (const row of group) {
-      if (row.variantAsins) {
-        let text = '';
-        row.variantAsins.forEach(item => {
-          text += `${item.text} | `;
-        });
-        row.variantAsins = [
-          {
-            text: text.slice(0, -3),
-          },
-        ];
-      }
-      if (row.ingredientsList) {
-        let text = '';
-        row.ingredientsList.forEach(item => {
-          text += `${item.text} `;
-        });
-        row.ingredientsList = [
-          {
-            text: text.trim(),
-          },
-        ];
-      }
+    for (const { group }
+        of data) {
+        for (const row of group) {
+            if (row.variantAsins) {
+                let text = '';
+                row.variantAsins.forEach(item => {
+                    text += `${item.text} | `;
+                });
+                row.variantAsins = [{
+                    text: text.slice(0, -3),
+                }, ];
+            }
+            if (row.ingredientsList) {
+                let text = '';
+                row.ingredientsList.forEach(item => {
+                    text += `${item.text} `;
+                });
+                row.ingredientsList = [{
+                    text: text.trim(),
+                }, ];
+            }
 
-      if (row.packSize) {
-        if (row.packSize[0].text.match(/(\d+)\(/)) {
-          row.packSize = [
-            {
-              text: row.packSize[0].text.match(/(\d+)\(/)[1],
-            },
-          ];
-        } else if (row.packSize[0].text.match(/(\d+)pk/)) {
-          row.packSize = [
-            {
-              text: row.packSize[0].text.match(/(\d+)pk/)[1],
-            },
-          ];
-        } else if (row.packSize[0].text.match(/(\d+)-/)) {
-          row.packSize = [
-            {
-              text: row.packSize[0].text.match(/(\d+)-/)[1],
-            },
-          ];
-        } else {
-          row.packSize = [
-            {
-              text: '',
-            },
-          ];
+            if (row.packSize) {
+                if (row.packSize[0].text.match(/(\d+)\(/)) {
+                    row.packSize = [{
+                        text: row.packSize[0].text.match(/(\d+)\(/)[1],
+                    }, ];
+                } else if (row.packSize[0].text.match(/(\d+)pk/)) {
+                    row.packSize = [{
+                        text: row.packSize[0].text.match(/(\d+)pk/)[1],
+                    }, ];
+                } else if (row.packSize[0].text.match(/(\d+)-/)) {
+                    row.packSize = [{
+                        text: row.packSize[0].text.match(/(\d+)-/)[1],
+                    }, ];
+                } else {
+                    row.packSize = [{
+                        text: '',
+                    }, ];
+                }
+            }
         }
-      }
     }
-  }
-  return data;
+    return data;
 };
 
 module.exports = { transform };
--- src/library/product/details/stores/t/totalwine/executeImplementation.js
+++ src/library/product/details/stores/t/totalwine/executeImplementation.js
@@ -1,91 +1,91 @@
-
-const implementation = async function (
-  inputs,
-  parameters,
-  context,
-  dependencies,
+const implementation = async function(
+    inputs,
+    parameters,
+    context,
+    dependencies,
 ) {
-  let { url, id, zipcode } = inputs;
-  if (!url) {
-    if (!id) {
-      throw new Error('no id provided');
-    }
-    url = await dependencies.createUrl({ id });
-  }
-
-  try {
-    const response = await context.goto(url, { timeout: 30000, waitUntil: 'networkidle0', checkBlocked: true });
-    console.log('Response ' + JSON.stringify(response));
-    if (response.message && response.message.includes('code 403')) {
-      console.log('Response failed');
-      return context.reportBlocked(451, 'Blocked!');
-    }
-  } catch (err) {
-    console.log('Error response' + JSON.stringify(err));
-    if (err.message && err.message.includes('code 403')) {
-      console.log('403 Response');
-      return context.reportBlocked(451, 'Blocked!');
-    }
-    throw err;
-  }
-
-  if (parameters.loadedSelector) {
-    await context.waitForFunction(function (sel, xp) {
-      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 20000 }, parameters.loadedSelector, parameters.noResultsXPath);
-  }
-
-  async function getData (variantUrl) {
-    console.log('URL passed - ' + variantUrl);
-    const data = await context.evaluate(async function (reqUrl) {
-      const response = await fetch(reqUrl, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      return response.json();
-    }, variantUrl);
-    return data;
-  };
-
-  // Check if variants exists
-  const variantsExist = await context.evaluate(function () {
-    const variants = document.querySelectorAll('ul[role="listbox"][class*="reset"] > li');
-    return variants.length > 1;
-  });
-
-  if (!variantsExist) {
-    return;
-  }
-  // API call to fetch variants
-  const sku = url.match(/p\/(.+)\?s=/g)[0].replace('?s=', '').replace('p/', '');
-  const storeUniqueId = zipcode === '95825' ? 1108 : url.match(/s=(\d+)/g)[0].replace('s=', '');
-
-  try {
-    const productDetails = await getData(`https://www.totalwine.com/product/api/product/product-detail/v1/getProduct/${sku}?shoppingMethod=INSTORE_PICKUP&state=US-CA&storeId=${storeUniqueId}`);
-    console.log('API call done');
-
-    await context.evaluate(async function (details) {
-      // Add skus to DOM
-      for (let i = 0; i < details.skus.length; i++) {
-        const newDiv = document.createElement('div');
-        console.log('sku id found ' + details.skuId);
-        console.log('sku found ' + JSON.stringify(details.skus[i]));
-        if (details.skuId === details.skus[i].skuId) {
-          newDiv.setAttribute('class', 'currentItemId');
-        } else {
-          newDiv.setAttribute('class', 'itemId');
+    let { url, id, zipcode } = inputs;
+    if (!url) {
+        if (!id) {
+            throw new Error('no id provided');
         }
-        newDiv.textContent = details.skus[i].skuId;
-        newDiv.style.display = 'none';
-        document.body.appendChild(newDiv);
-      }
-    }, productDetails);
-  } catch (err) {
-    console.log('ERROR while calling the API' + err);
-    throw err;
-  }
+        url = await dependencies.createUrl({ id });
+    }
+
+    try {
+        const response = await context.goto(url, { timeout: 30000, waitUntil: 'networkidle0', checkBlocked: true });
+        console.log('Response ' + JSON.stringify(response));
+        if (response.message && response.message.includes('code 403')) {
+            console.log('Response failed');
+            return context.reportBlocked(451, 'Blocked!');
+        }
+    } catch (err) {
+        console.log('Error response' + JSON.stringify(err));
+        if (err.message && err.message.includes('code 403')) {
+            console.log('403 Response');
+            return context.reportBlocked(451, 'Blocked!');
+        }
+        throw err;
+    }
+
+    if (parameters.loadedSelector) {
+        await context.waitForFunction(function(sel, xp) {
+            return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+        }, { timeout: 20000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    }
+
+    async function getData(variantUrl) {
+        console.log('URL passed - ' + variantUrl);
+        const data = await context.evaluate(async function(reqUrl) {
+            const response = await fetch(reqUrl, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            return response.json();
+        }, variantUrl);
+        return data;
+    };
+
+    // Check if variants exists
+    const variantsExist = await context.evaluate(function() {
+        const variants = document.querySelectorAll('ul[role="listbox"][class*="reset"] > li');
+        return variants.length > 1;
+    });
+
+    if (!variantsExist) {
+        return;
+    }
+    // API call to fetch variants
+    const sku = url.match(/p\/(.+)\?s=/g)[0].replace('?s=', '').replace('p/', '');
+    const storeUniqueId = zipcode === '95825' ? 1108 : url.match(/s=(\d+)/g)[0].replace('s=', '');
+
+    try {
+        const productDetails = await getData(`https://www.totalwine.com/product/api/product/product-detail/v1/getProduct/${sku}?shoppingMethod=INSTORE_PICKUP&state=US-CA&storeId=${storeUniqueId}`);
+        console.log('API call done');
+
+        await context.evaluate(async function(details) {
+            // Add skus to DOM
+            if (details && details.sku)
+                for (let i = 0; i < details.skus.length; i++) {
+                    const newDiv = document.createElement('div');
+                    console.log('sku id found ' + details.skuId);
+                    console.log('sku found ' + JSON.stringify(details.skus[i]));
+                    if (details.skuId === details.skus[i].skuId) {
+                        newDiv.setAttribute('class', 'currentItemId');
+                    } else {
+                        newDiv.setAttribute('class', 'itemId');
+                    }
+                    newDiv.textContent = details.skus[i].skuId;
+                    newDiv.style.display = 'none';
+                    document.body.appendChild(newDiv);
+                }
+        }, productDetails);
+    } catch (err) {
+        console.log('ERROR while calling the API' + err);
+        throw err;
+    }
 };
 
 module.exports = { implementation };
