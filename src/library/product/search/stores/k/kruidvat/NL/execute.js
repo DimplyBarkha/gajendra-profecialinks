7608187c9f20cb3dd512a3c1ac090d425fb9533c
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-24 17:13:07 +0530

    linio-Damayanthi

---
+++ src/library/product/search/stores/k/kruidvat/NL/execute.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'NL',
+    store: 'kruidvat',
+    domain: 'kruidvat.nl',
+    url: 'https://www.kruidvat.nl/search?q=48+uur+werking&text=48+uur+werking&searchType=manual',
+    // url: 'https://www.kruidvat.nl/search?q={searchTerms}&text={searchTerms}&searchType=manual',
+    loadedSelector: 'body',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
