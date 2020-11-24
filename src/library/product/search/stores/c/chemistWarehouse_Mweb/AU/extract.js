6757346ead5bcabd9181111798b6f49c4020cfe5
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-11-24 16:03:15 +0530

    pagination loagic added (#1461)

--- src/library/product/search/stores/c/chemistWarehouse_Mweb/AU/extract.js
+++ src/library/product/search/stores/c/chemistWarehouse_Mweb/AU/extract.js
@@ -10,42 +10,46 @@ module.exports = {
   },
   implementation,
 };
-
-async function implementation (
-  // @ts-ignore
+async function implementation(
   inputs,
   parameters,
   context,
   dependencies,
-  ) {
+) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  async function arrBtn (){
-    return await context.evaluate(async () => {
-      let paginations = document.querySelectorAll('div.search__result__pager-list');
-      // @ts-ignore
-      let upperPagination = paginations ? paginations.querySelectorAll('button') : '';
-      let btnarr = upperPagination ? upperPagination : '';
-      console.log('Length of all', btnarr.length);
-      console.log('btnarr: ', btnarr);
-      return btnarr ? btnarr : [];
+  // try {
+  //   await context.waitForSelector("button[aria-label='close']");
+  //   await context.click("button[aria-label='close']");
+  // } catch (e) {
+  //   console.log('No pop up');
+  // }
+  let count = 0;
+  while (true) {
+    await context.extract(productDetails, { transform }, { type: 'APPEND' });
+    count += await context.evaluate(() => {
+      return document.querySelectorAll("div.search__result__product__list div.row div.col-xs-4.search__result__product").length;
     });
+    const nextLink = await context.evaluate(() => {
+      const element = document.evaluate('//div[@class="search__result__products row"]/following-sibling::div[@class="search__result__pager row"][1]//div[contains(@class, "search__result__pager-list")]//button[@class="search__result__pager-button--active search__result__pager-button"]/following-sibling::button', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (element) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    // await context.waitForXpath("//span[contains(@class,'hbmid active')]/following-sibling::a[contains(@class,'page_list')]", { timeout: 60000 });
+    if (nextLink && count < 150) {
+      await context.evaluate(async () => {
+        const element = document.evaluate('//div[@class="search__result__products row"]/following-sibling::div[@class="search__result__pager row"][1]//div[contains(@class, "search__result__pager-list")]//button[@class="search__result__pager-button--active search__result__pager-button"]/following-sibling::button', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        console.log('Elemdsfsdfdent' + element);
+        // @ts-ignore
+        element && element.click();
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      });
+    } else {
+      break;
+    }
   }
-  console.log('buttons: ', arrBtn());
-  // await context.evaluate(async () => {
-  //   let paginations = document.querySelectorAll('div.search__result__pager-list');
-  //   let firstPagination = paginations ? paginations[0] : '';
-  //   // @ts-ignore
-  //   let button = firstPagination ? firstPagination.querySelectorAll('button') : '';
-  //   for (let index = 0; index < button.length; index++) {
-  //     const element = button[index];
-  //     console.log('element: ', element);
-  //     if(element.classList.contains('search__result__pager-button--active')){
-  //       console.log("llllllllllll"+element)
-  //       element.nextSibling.click();
-        //await context.extract(productDetails, { transform }, { type: 'APPEND' });
-  //     }
-  //   }
-  // });
   return await context.extract(productDetails, { transform });
-  }
+}
