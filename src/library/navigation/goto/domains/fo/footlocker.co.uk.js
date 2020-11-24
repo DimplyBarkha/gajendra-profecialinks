e8649ec1d9407e24e33a403190b60afee953cc60
Vivek Gondil <gondilvivek@gmail.com>
2020-11-24 17:03:38 +0530

    added missing xpaths

--- src/library/navigation/goto/domains/fo/footlocker.co.uk.js
+++ src/library/navigation/goto/domains/fo/footlocker.co.uk.js
@@ -7,4 +7,14 @@ module.exports = {
     store: 'footlocker',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 50000,
+      waitUntil: 'load',
+    });
+    await context.waitForNavigation();
+  },
 };
--- src/library/product/details/stores/f/footlocker/UK/extract.js
+++ src/library/product/details/stores/f/footlocker/UK/extract.js
@@ -16,6 +16,26 @@ module.exports = {
   ) {
     const { cleanUp } = parameters;
     const { productDetails } = dependencies;
+
+    await context.evaluate(() => {
+      const getvariants = document.querySelectorAll('div.owl-stage a');
+      const variantid = [];
+      for (let i = 0; i < getvariants.length; i++) {
+        variantid.push(getvariants[i].getAttribute('data-product-click-link'));
+      }
+      const getSizeVariants = document.querySelectorAll('#fitanalytics_sizecontainer > section:nth-child(3) > div >button');
+      for (let i = 0; i < getSizeVariants.length; i++) {
+        if (getSizeVariants[i].getAttribute('data-product-size-select-item')) {
+          variantid.push(getSizeVariants[i].getAttribute('data-product-size-select-item'));
+        }
+      }
+      for (let i = 0; i < variantid.length; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('id', 'variantId');
+        div.setAttribute('data', variantid[i]);
+        document.body.append(div);
+      }
+    });
     try {
       await context.waitForSelector('.slick-list.draggable .slick-active img', { timeout: 30000 });
     } catch (err) {
