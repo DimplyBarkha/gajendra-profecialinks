cfc591a7e1f1497e002eaca3ec9a4e873b5f9669
sasz94 <sacha.bartlomiej@gmail.com>
2020-11-24 20:14:15 +0100

    Added post-extraction data modification to concatenate strings to single field (#1351)

--- src/library/product/details/stores/j/jumbo/NL/extract.js
+++ src/library/product/details/stores/j/jumbo/NL/extract.js
@@ -9,4 +9,29 @@ module.exports = {
     domain: 'jumbo.com',
     zipcode: '',
   },
+  implementation: async function implementation (inputs, parameters, context, dependencies) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+
+    const dataRef = await context.extract(productDetails, { transform });
+    const ingredientsList = dataRef[0].group[0].ingredientsList;
+    if (ingredientsList && ingredientsList.length > 1) {
+      let ingredientsText = '';
+      ingredientsList.forEach(ingredient => {
+        ingredientsText += ' -' + ingredient.text;
+      });
+      ingredientsList[0].text = ingredientsText.replace(/\n/g, ': ');
+      dataRef[0].group[0].ingredientsList = ingredientsList.splice(0, 1);
+    }
+    const description = dataRef[0].group[0].description;
+    if (description && description.length > 1) {
+      let descriptionText = '';
+      description.forEach(desc => {
+        descriptionText += ' -' + desc.text;
+      });
+      description[0].text = descriptionText.replace(/\n/g, ': ');
+      dataRef[0].group[0].description = description.splice(0, 1);
+    }
+    return dataRef;
+  },
 };
