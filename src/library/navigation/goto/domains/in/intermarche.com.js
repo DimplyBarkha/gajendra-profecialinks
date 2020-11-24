eb0998323d142485f0ba86772c572b2a69703c8f
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-11-24 17:28:16 +0530

    intermarche initial commit

--- src/library/navigation/goto/domains/in/intermarche.com.js
+++ src/library/navigation/goto/domains/in/intermarche.com.js
@@ -2,63 +2,23 @@ module.exports = {
   implements: "navigation/goto",
   parameterValues: {
     domain: "intermarche.com",
-    timeout: 50000,
-    zipcode: "",
-    store: "Intermarche",
+    timeout: null,
     country: "FR",
+    store: "intermarche",
+    zipcode: "",
   },
-  implementation: async (
-    { url, zipcode, storeId },
-    parameters,
-    context,
-    dependencies
-  ) => {
-    const timeout = parameters.timeout ? parameters.timeout : 50000;
-
-    // await context.setBlockAds(false);
-    // await context.setLoadAllResources(true);
-    // await context.setLoadImages(true);
-    // await context.setJavaScriptEnabled(true);
-    // await context.setAntiFingerprint(false);
-    // await context.setUseRelayProxy(false);
-    // await context.goto(url, {
-    // firstRequestTimeout: 40000,
-    //   timeout: timeout,
-    //     waitUntil: 'load',
-    //       checkBlocked: false,
-    //         antiCaptchaOptions: {
-    //   type: 'RECAPTCHA',
-    //   },
-    // });
-    url = `${url}#[!opt!]{"force200": true}[/!opt!]`;
+  implementation: async ({ url }, parameters, context, dependencies) => {
     await context.goto(url, {
-      firstRequestTimeout: 40000,
-      timeout: timeout,
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 100000,
       waitUntil: "load",
-      checkBlocked: false,
+      checkBlocked: true,
       antiCaptchaOptions: {
         type: "RECAPTCHA",
       },
     });
-    const captchaFrame = 'iframe[src*="https://geo.captcha"]';
-    const checkExistance = async (selector) => {
-      return await context.evaluate((captchaSelector) => {
-        return Boolean(document.querySelector(captchaSelector));
-      }, selector);
-    };
-    const isCaptchaFramePresent = await checkExistance(captchaFrame);
-    if (isCaptchaFramePresent) {
-      try {
-        console.log("isCaptcha", true);
-        await context.waitForNavigation({ timeout });
-        // @ts-ignore
-        // eslint-disable-next-line no-undef
-        await context.evaluateInFrame("iframe", () => grecaptcha.execute());
-        console.log("solved captcha, waiting for page change");
-        await context.waitForNavigation({ timeout });
-      } catch (e) {
-        console.log("could not solve captcha");
-      }
-    }
+    await context.waitForNavigation();
   },
 };
--- src/library/product/details/stores/i/intermarche/FR/execute.js
+++ src/library/product/details/stores/i/intermarche/FR/execute.js
@@ -2,10 +2,10 @@ module.exports = {
   implements: "product/details/execute",
   parameterValues: {
     country: "FR",
-    store: "Intermarche",
+    store: "intermarche",
     domain: "intermarche.com",
-    loadedSelector: '[class^="product-price--unit ProductPrice__PriceUnit"]',
-    noResultsXPath: '//*[@id="go_home"]',
+    loadedSelector: null,
+    noResultsXPath: null,
     zipcode: "",
   },
 };
