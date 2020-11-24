242f483a29156b29e7afd9f73b71f59dd3f7d301
sivasoarlogic <siva@soarlogic.com>
2020-11-24 11:04:32 +0530

    searchupdate

---
+++ src/library/navigation/goto/domains/fn/fnac.fr.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'fnac.fr',
+    timeout: 100000,
+    country: 'FR',
+    store: 'fnac',
+    zipcode: "''",
+  },
+};
