90bdd8bc430f4a681fcaa623c257bbcffd21b33d
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-24 13:40:39 +0530

    kruidvat-Damayanthi

--- src/library/product/search/stores/k/kruidvat/NL/paginate.js
+++ src/library/product/search/stores/k/kruidvat/NL/paginate.js
@@ -4,10 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'NL',
     store: 'kruidvat',
-    nextLinkSelector: 'body > div.e2-page.e2-page--scrolled > main > div.container-fluid-mobile.container-fluid-mobile--search-grid.pagecontent > div.row.no-gutters--mobile.no-gutters--tablet > div.yCmsContentSlot.col-12.col-lg-9 > div > div > e2-loading > v-root > div > v-slot > v-slot-assigned-content > div.pager.plp-paginator__pager.pager--mobile.m-bottom-medium.m-bottom-no-indent--desktop > div > e2-plp-page-selectors:nth-child(6) > v-root > a',
+    nextLinkSelector: 'body > div.e2-page.e2-page--scrolled > main > div.container-fluid-mobile.container-fluid-mobile--search-grid.pagecontent > div.row.no-gutters--mobile.no-gutters--tablet > div.yCmsContentSlot.col-12.col-lg-9 > div > div > e2-loading > v-root > div > v-slot > v-slot-assigned-content > div.pager.plp-paginator__pager.pager--mobile-bottom > div > e2-plp-page-selectors:nth-child(6) > v-root > a',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'body',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'kruidvat.nl',
