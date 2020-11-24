ba9bfc5b141512acda3d884ec81b758dec0c53e1
mania307 <maniakalnaa@gmail.com>
2020-11-24 15:01:29 +0100

    updated xpath

--- src/library/product/details/stores/s/selfridges/UK/extract.js
+++ src/library/product/details/stores/s/selfridges/UK/extract.js
@@ -1,10 +1,19 @@
+const { transform } = require('../../../../shared');
+async function implementation(inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
 
+  await context.evaluate(async () => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 750));
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'UK',
     store: 'selfridges',
-    transform: null,
+    transform: transform,
     domain: 'selfridges.com',
     zipcode: '',
   },
