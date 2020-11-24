2e01e9f0353bc13f27ab178db32e54ed698bb828
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-11-24 16:02:23 +0100

    #1569 - Continue working on the core extractor

--- src/library/product/details/stores/p/petshop/RU/extract.js
+++ src/library/product/details/stores/p/petshop/RU/extract.js
@@ -1,15 +1,15 @@
-const { cleanup } = require('../../../../shared');
+const { cleanUp } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'RU',
     store: 'petshop',
-    transform: cleanup,
+    transform: cleanUp,
     domain: 'petshop.ru',
     zipcode: '',
   },
-  implementation: async (inputs, { transform }, context, { productDetails }) => {
+  implementation: async ({ inputString }, { transform }, context, { productDetails }) => {
     // Adding an empty list to the DOM
     await context.evaluate(async () => {
       const addedList = document.createElement('ol');
@@ -25,8 +25,11 @@ module.exports = {
         descriptionButton.click();
         await new Promise((resolve) => setTimeout(resolve, 500));
       }
-      return document.evaluate('//button[div[text()="Описание"]]/following-sibling::div[position()=1]', document, null, XPathResult.STRING_TYPE, null).stringValue;
+      return document.evaluate('//button[div[text()="Описание"]]/following-sibling::div[position()=1]//div[contains(@class, "style_description")]', document, null, XPathResult.STRING_TYPE, null)
+        .stringValue;
     });
+
+    const descriptionBullets = await context.evaluate(() => document.evaluate('count(//tbody/tr[td[img]]/td[last()]/p)', document, null, XPathResult.NUMBER_TYPE, null).numberValue);
 
     // Switching to the product's composition tab
     await context.evaluate(async () => {
@@ -50,7 +53,7 @@ module.exports = {
         );
       }
       await context.evaluate(
-        async ({ variantsTotal, productDescription }) => {
+        async ({ variantsTotal, productDescription, descriptionBullets }) => {
           const addedList = document.querySelector('ol#variants_list');
           const listElem = document.createElement('li');
           const variantElem = document.evaluate('//div[contains(@class, "art") and starts-with(text(), "Арт")]', document, null, XPathResult.STRING_TYPE, null).stringValue;
@@ -64,17 +67,32 @@ module.exports = {
               ? productScript.match(/window.__PS_STATE__\['\/petshop\/components\/ps\/product-head-.+ = (.+}]})/)[1]
               : '{}';
           const productObj = JSON.parse(productObjString).product;
-          // const variantObj = productObj.offers.find((offer) => offer.artnumber === variantId);
 
           const imagesList = document.createElement('ol');
           imagesList.id = 'images';
-          const allImages = productObj && productObj.images ? productObj.images.filter((image) => image.type === 'image') : [];
+          const allImages = productObj && productObj.images ? productObj.images.filter((item) => item.type === 'image') : [];
           allImages.forEach((image) => {
             const imageElem = document.createElement('li');
             imageElem.setAttribute('src', `https:${image.bigImgSrc}`);
             imageElem.setAttribute('alt', image.alt);
+            imagesList.appendChild(imageElem);
           });
           listElem.appendChild(imagesList);
+
+          const videosList = document.createElement('ol');
+          videosList.id = 'videos';
+          const allVideos = productObj && productObj.images ? productObj.images.filter((item) => item.type === 'video' && item.youtubeCode) : [];
+          allVideos.forEach((video) => {
+            const videoElem = document.createElement('li');
+            videoElem.setAttribute('src', `https://www.youtube.com/embed/${video.youtubeCode}`);
+            videosList.appendChild(videoElem);
+          });
+          if (productObj.description.brandVideo) {
+            const videoElem = document.createElement('li');
+            videoElem.setAttribute('src', `https://www.youtube.com/embed/${productObj.description.brandVideo}`);
+            videosList.appendChild(videoElem);
+          }
+          listElem.appendChild(videosList);
 
           const name = document.querySelector('div[class^="style_info_product_name"] h1') ? document.querySelector('div[class^="style_info_product_name"] h1').textContent : '';
           const brandElem = document.querySelector('a[class*="brand_name"], a[class*="ProductBrand"]');
@@ -108,12 +126,15 @@ module.exports = {
           listElem.setAttribute('availability_text', availabilityText);
           listElem.setAttribute('coupon_text', couponText);
           listElem.setAttribute('rating_count', productObj.rating.voteCount);
-          listElem.setAttribute('aggregate_rating', productObj.rating.value.toString().replace('.', ','));
+          listElem.setAttribute('aggregate_rating', productObj.rating.value);
           listElem.setAttribute('description', productDescription);
+          listElem.setAttribute('description_bullets', descriptionBullets);
 
+          const servingText = document.evaluate('//span[(contains(. , "Пищевая ценность") or contains(. , "Добавки") and contains(. , "г"))]', document, null, XPathResult.STRING_TYPE, null)
+            .stringValue;
+          const servingSize = servingText.match(/на (\d+) ?(к?г)/) ? servingText.match(/на (\d+) ?(к?г)/)[1] : '';
+          const servingSizeUom = servingText.match(/на (\d+) ?(к?г)/) ? servingText.match(/на (\d+) ?(к?г)/)[2] : '';
           const caloriesText = document.evaluate('//div[b[contains(text(), "Энергетическая ценность") or contains(text(), "энергия")]]', document, null, XPathResult.STRING_TYPE, null).stringValue;
-          const servingSize = caloriesText.match(/(\d+) ?(г)/) ? caloriesText.match(/(\d+) ?(г)/)[1] : '';
-          const servingSizeUom = caloriesText.match(/(\d+) ?(г)/) ? caloriesText.match(/(\d+) ?(г)/)[2] : '';
           let caloriesPerServing = caloriesText.match(/г\)?: ([\d,]+)/) ? caloriesText.match(/г\)?: ([\d,]+)/)[1] : '';
           if (!caloriesPerServing) caloriesPerServing = caloriesText.match(/([\d,]+)\s[Кк]кал/) ? caloriesText.match(/([\d,]+)\s[Кк]кал/)[1] : '';
 
@@ -135,7 +156,7 @@ module.exports = {
           listElem.setAttribute('sodium_per_serving', sodiumPerServing);
           listElem.setAttribute('sodium_per_serving_uom', sodiumPerServingUom);
 
-          const carbText = document.evaluate('//tr[td[contains(. , "натрий")] and contains(. , "г") and not(contains(. , "%"))]', document, null, XPathResult.STRING_TYPE, null).stringValue;
+          const carbText = document.evaluate('//tr[td[contains(. , "углевод")] and contains(. , "г") and not(contains(. , "%"))]', document, null, XPathResult.STRING_TYPE, null).stringValue;
           const totalCarbPerServing = carbText.match(/(\d+)\s?(.+)/) ? carbText.match(/(\d+)\s?(.+)/)[1] : '';
           const totalCarbPerServingUom = carbText.match(/(\d+)\s?(.+)/) ? carbText.match(/(\d+)\s?(.+)/)[2] : '';
 
@@ -156,7 +177,13 @@ module.exports = {
           listElem.setAttribute('total_sugar_per_serving', totalSugarsPerServing);
           listElem.setAttribute('total_sugar_per_serving_uom', totalSugarsPerServingUom);
 
-          const proteinText = document.evaluate('//tr[td[contains(. , "белок") or contains(. , "белки")] and contains(. , "г") and not(contains(. , "%"))]', document, null, XPathResult.STRING_TYPE, null).stringValue;
+          const proteinText = document.evaluate(
+            '//tr[td[contains(. , "белок") or contains(. , "белки")] and contains(. , "г") and not(contains(. , "%"))]',
+            document,
+            null,
+            XPathResult.STRING_TYPE,
+            null,
+          ).stringValue;
           const proteinPerServing = proteinText.match(/(\d+)\s?(.+)/) ? proteinText.match(/(\d+)\s?(.+)/)[1] : '';
           const proteinPerServingUom = proteinText.match(/(\d+)\s?(.+)/) ? proteinText.match(/(\d+)\s?(.+)/)[2] : '';
 
@@ -177,7 +204,7 @@ module.exports = {
           listElem.setAttribute('vitamin_c_per_serving', vitaminCPerServing);
           listElem.setAttribute('vitamin_c_per_serving_uom', vitaminCPerServingUom);
 
-          const calciumText = document.evaluate('//tr[td[contains(. , "витамин C")] and not(contains(. , "%"))]', document, null, XPathResult.STRING_TYPE, null).stringValue;
+          const calciumText = document.evaluate('//tr[td[contains(. , "кальций")] and not(contains(. , "%"))]', document, null, XPathResult.STRING_TYPE, null).stringValue;
           const calciumPerServing = calciumText.match(/(\d+)\s?(.+)/) ? calciumText.match(/(\d+)\s?(.+)/)[1] : '';
           const calciumPerServingUom = calciumText.match(/(\d+)\s?(.+)/) ? calciumText.match(/(\d+)\s?(.+)/)[2] : '';
 
@@ -191,16 +218,26 @@ module.exports = {
           listElem.setAttribute('iron_per_serving', ironPerServing);
           listElem.setAttribute('iron_per_serving_uom', ironPerServingUom);
 
-          const magnesiumText = document.evaluate('//tr[td[contains(. , "железо")] and not(contains(. , "%"))]', document, null, XPathResult.STRING_TYPE, null).stringValue;
+          const magnesiumText = document.evaluate('//tr[td[contains(. , "магний")] and not(contains(. , "%"))]', document, null, XPathResult.STRING_TYPE, null).stringValue;
           const magnesiumPerServing = magnesiumText.match(/(\d+)\s?(.+)/) ? magnesiumText.match(/(\d+)\s?(.+)/)[1] : '';
           const magnesiumPerServingUom = magnesiumText.match(/(\d+)\s?(.+)/) ? magnesiumText.match(/(\d+)\s?(.+)/)[2] : '';
 
           listElem.setAttribute('magnesium_per_serving', magnesiumPerServing);
           listElem.setAttribute('magnesium_per_serving_uom', magnesiumPerServingUom);
 
+          const saltText = document.evaluate('//tr[td[contains(. , "соль")] and not(contains(. , "%"))]', document, null, XPathResult.STRING_TYPE, null).stringValue;
+          const saltPerServing = saltText.match(/(\d+)\s?(.+)/) ? saltText.match(/(\d+)\s?(.+)/)[1] : '';
+          const saltPerServingUom = saltText.match(/(\d+)\s?(.+)/) ? saltText.match(/(\d+)\s?(.+)/)[2] : '';
+
+          listElem.setAttribute('salt_per_serving', saltPerServing);
+          listElem.setAttribute('salt_per_serving_uom', saltPerServingUom);
+
+          const image360Present = !!productObj.images.find(item => item.type === '3d' && item.imagesArr.length);
+          listElem.setAttribute('image_360_present', image360Present.toString());
+
           addedList.appendChild(listElem);
         },
-        { variantsTotal, productDescription },
+        { variantsTotal, productDescription, descriptionBullets },
       );
     }
     await context.extract(productDetails, { transform });
