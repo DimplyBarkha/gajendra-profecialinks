107cf0c0b106d3415fc0b146cc3d5343514ad439
David Skoglund <david.skoglund@import.io>
2020-11-24 13:57:39 -0800

    updated  offers goto url and some inputs

--- src/library/product/offers/stores/a/amazon/ES/execute.js
+++ src/library/product/offers/stores/a/amazon/ES/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.ca',
     loadedSelector: '.olpOffer',
     noResultsXPath: '//div[@id="olpOfferList"]/div/p | //*[@id="be"]//*[not(contains(text(), "OfferListing")) and contains(text(), "ue_pty")]',
-    offerUrl: 'https://www.amazon.es/gp/offer-listing/{id}/ref=olp_f_new?ie=UTF8&overridePriceSuppression=10',
+    offerUrl: 'https://www.amazon.es/gp/offer-listing/{id}/ref=olp_f_new?ie=UTF8&overridePriceSuppression=10&f_new=true',
     zipcode: '',
   },
 };
--- src/library/product/offers/stores/a/amazon/US/execute.js
+++ src/library/product/offers/stores/a/amazon/US/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.com',
     loadedSelector: '.olpOffer',
     noResultsXPath: '//div[@id="olpOfferList"]/div/p | //*[@id="be"]//*[not(contains(text(), "OfferListing")) and contains(text(), "ue_pty")]',
-    offerUrl: 'https://www.amazon.com/gp/offer-listing/{id}/ref=olp_f_new?ie=UTF8&overridePriceSuppression=10',
+    offerUrl: 'https://www.amazon.com/gp/offer-listing/{id}/ref=olp_f_new?ie=UTF8&overridePriceSuppression=10&f_new=true',
     zipcode: '',
   },
 };
