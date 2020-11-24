b46c253a5827af47b667cb80b9128faff1fe9596
alscotty <alawrencescott@gmail.com>
2020-11-24 10:32:56 -0800

    setup extractor and pagination xpaths

--- src/library/product/reviews/stores/k/kroger/US/execute.js
+++ src/library/product/reviews/stores/k/kroger/US/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'kroger',
     domain: 'kroger.com',
     loadedSelector: 'div.ProductDetails',
-    noResultsXPath: '//div[@id="notFound"] | //h2[contains(text(),"to have a bad link")]',
+    noResultsXPath: '//div[@id="notFound"] | //h2[contains(text(),"to have a bad link")] | //div[contains(@data-bv-show,"reviews") and (@class="hidden")]',
     reviewUrl: '',
     sortButtonSelectors: null,
     zipcode: '',
