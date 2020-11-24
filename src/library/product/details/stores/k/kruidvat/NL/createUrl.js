9d88e222d04f445831f7ed7d11be425cf4b36df9
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-24 22:41:19 +0530

    kruidvat-Damayanthi

---
+++ src/library/product/details/stores/k/kruidvat/NL/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'kruidvat.nl',
+    prefix: null,
+    url: null,
+    country: 'NL',
+    store: 'kruidvat',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/k/kruidvat/NL/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'NL',
+    store: 'kruidvat',
+    transform: null,
+    domain: 'kruidvat.nl',
+    zipcode: '',
+  },
+};
