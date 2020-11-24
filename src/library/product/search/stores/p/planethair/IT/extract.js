1631ddc4e0d1e242bb7f786cff1d002b5d94bff3
Karina Miernik <karina.miernik@gmail.com>
2020-11-24 08:04:22 +0100

    #1582 iteration count changed

--- src/library/product/search/stores/p/planethair/IT/extract.js
+++ src/library/product/search/stores/p/planethair/IT/extract.js
@@ -4,6 +4,7 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
+  // pagination on scroll
   await context.evaluate(async function () {
     function stall (ms) {
       return new Promise((resolve) => {
@@ -14,8 +15,7 @@ async function implementation (inputs, parameters, context, dependencies) {
     }
     const numberOfProducts = parseInt(document.querySelector('span.ty-mainbox-title__right').textContent.match(/\d+/gm)[0]);
     let productCount = 0;
-    // const productLimit = 153;
-    for (let i = 0; i < 17; i++) {
+    for (let i = 0; i < 32; i++) {
       productCount += document.querySelectorAll('div[class="ty-column3"] > div[class*="list__item"]').length - productCount;
       window.scroll(0, document.body.scrollHeight);
       await stall(1000);
@@ -28,13 +28,13 @@ async function implementation (inputs, parameters, context, dependencies) {
   await new Promise((resolve, reject) => setTimeout(resolve, 1500));
 
   await context.evaluate(async () => {
-    // add price
+    // price
 
     const price = document.querySelectorAll('span[class*="ty-price"][id*="line"]')
       ? document.querySelectorAll('span[class*="ty-price"][id*="line"]') : [];
     price.forEach(e => e.setAttribute('price', e.innerText.replace('.', ',')));
 
-    // add rating
+    // rating
 
     const ratings = document.querySelectorAll('span.ty-nowrap.ty-stars a')
       ? document.querySelectorAll('span.ty-nowrap.ty-stars a') : [];
@@ -47,7 +47,7 @@ async function implementation (inputs, parameters, context, dependencies) {
       document.querySelectorAll(selector)[iterator].setAttribute(propName, value);
     }
 
-    // add rank
+    // rank
 
     const allProducts = document.querySelectorAll('div[class="ty-column3"] > div[class*="list__item"]');
     for (let i = 0; i < allProducts.length; i++) {
