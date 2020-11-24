7736e585801ff5774756ded1061d2b10c490f946
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-24 19:34:28 +0530

    devjdsportsdetails

--- src/library/product/details/stores/j/jdsports/UK/execute.js
+++ src/library/product/details/stores/j/jdsports/UK/execute.js
@@ -1,11 +1,10 @@
 
 module.exports = {
-  implements: 'product/search/execute',
+  implements: 'product/details/execute',
   parameterValues: {
     country: 'UK',
     store: 'jdsports',
     domain: 'jdsports.co.uk',
-    url: 'https://www.jdsports.co.uk/search/{searchTerms}/?max=72',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
---
+++ src/library/product/details/stores/j/jdsports/UK/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'UK',
+    store: 'jdsports',
+    transform: null,
+    domain: 'jdsports.co.uk',
+    zipcode: '',
+  },
+};
