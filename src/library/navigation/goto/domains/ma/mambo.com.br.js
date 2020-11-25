3f09c69d5ae6e4076f886e7859b7ba516a394834
Lech Kulesza <lmkulesz@icloud.com>
2020-11-25 16:42:38 +0100

    #1585. update extractor for new site version

--- src/library/navigation/goto/domains/ma/mambo.com.br.js
+++ src/library/navigation/goto/domains/ma/mambo.com.br.js
@@ -1,3 +1,21 @@
+async function implementation ({ url, zipcode, storeId }, parameters, context, dependencies) {
+  await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
+  await context.waitForSelector('.pageLoading');
+  let body = await context.evaluate(async function () {
+    const body = document.querySelector('.pageLoading');
+    if (body) return true;
+    else return false;
+  });
+  do {
+    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+    body = await context.evaluate(async function () {
+      const body = document.querySelector('.pageLoading');
+      if (body) return true;
+      else return false;
+    });
+  } while (body);
+  await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
+}
 
 module.exports = {
   implements: 'navigation/goto',
@@ -8,4 +26,5 @@ module.exports = {
     store: 'mambo',
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/search/stores/m/mambo/BR/extract.js
+++ src/library/product/search/stores/m/mambo/BR/extract.js
@@ -4,40 +4,30 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await context.waitForSelector('.ts-shelf-rate-symbols.ts-sprite');
   await context.evaluate(async function () {
     function checkForProducts (selectorForProducts) {
       return document.querySelector(selectorForProducts) ? document.querySelectorAll(selectorForProducts).length : 0;
     }
-    function scrollBy (number) {
-      const height = window.scrollY;
-      window.scrollTo(0, height + number);
-    }
+    // function scrollBy(number) {
+    //   const height = window.scrollY;
+    //   window.scrollTo(0, height + number);
+    // }
 
     const numberOfProducts = document.querySelector('.CC-searchCountFiltros .CC-count span')
       ? parseInt(document.querySelector('.CC-searchCountFiltros .CC-count span').innerText)
       : 0;
-    if (numberOfProducts > 0) {
+    const loadMoreBtn = document.querySelector('.CC-seeMore button') ? document.querySelector('.CC-seeMore button') : null;
+    if (numberOfProducts > 0 && loadMoreBtn) {
       let numberOfSeenProducts;
       do {
-        scrollBy(10);
-        await new Promise((resolve, reject) => setTimeout(resolve, 10));
+        loadMoreBtn.click();
+        await new Promise((resolve, reject) => setTimeout(resolve, 100));
         numberOfSeenProducts = checkForProducts('.CC-shelf.shelf-block .shelf-product');
       } while (numberOfProducts !== numberOfSeenProducts);
-      await new Promise((resolve, reject) => setTimeout(resolve, 700));
       const productCards = document.querySelector('.CC-shelf.shelf-block .shelf-product') ? document.querySelectorAll('.CC-shelf.shelf-block .shelf-product') : [];
 
       productCards.forEach((card, index) => {
         card.setAttribute('data-rank-number', index + 1);
-        const cardRatingContainer = card.querySelector('.shelf-rating');
-        const ratingPercentage = card.querySelector('.ts-shelf-rate-symbols.ts-sprite')
-          ? card.querySelector('.ts-shelf-rate-symbols.ts-sprite').style.width.replace('%', '') / 100
-          : 0;
-        const aggregateRatingNormalized = (ratingPercentage * 5).toFixed(1);
-        const div = document.createElement('div');
-        div.id = 'aggregateRatingNormalized';
-        div.innerHTML = aggregateRatingNormalized.toString().replace('.', ',');
-        cardRatingContainer.appendChild(div);
       });
     }
   });
