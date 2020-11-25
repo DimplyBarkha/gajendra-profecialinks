b16a9e70d6645a21a06bfb18688af37104c4cf10
plcpl-jitendra <73573105+plcpl-jitendra@users.noreply.github.com>
2020-11-25 17:13:20 +0530

    Committing Extractor for rewe.de.
    Committing Extractor for rewe.de.

---
+++ src/library/product/details/stores/r/rewe/DE/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'DE',
+    store: 'rewe',
+    domain: 'rewe.de',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/r/rewe/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'rewe',
+    transform: null,
+    domain: 'rewe.de',
+    zipcode: "''",
+  },
+};
