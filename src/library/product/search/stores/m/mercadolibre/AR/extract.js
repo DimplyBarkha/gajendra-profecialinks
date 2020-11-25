9548c292859c7e673d14eae5a9cc5d875b061e1b
Karina Miernik <karina.miernik@gmail.com>
2020-11-25 13:21:19 +0100

    #1593 searchurl fixed

--- src/library/product/search/stores/m/mercadolibre/AR/extract.js
+++ src/library/product/search/stores/m/mercadolibre/AR/extract.js
@@ -5,8 +5,15 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { productDetails } = dependencies;
 
   await context.evaluate(async () => {
+    function addElementToDocument (key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
     const searchUrl = window.location.href;
-    document.querySelectorAll('li[class*="layout__item"]').forEach(e => e.setAttribute('searchurl', searchUrl));
+    addElementToDocument('searchUrl', searchUrl);
     // set rank
     const products = document.querySelectorAll('li[class*="layout__item"]');
     products.forEach((product, index) => product.setAttribute('rank', `${index + 1}`));
