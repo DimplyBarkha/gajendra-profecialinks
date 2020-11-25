149d7f9da63427da0b931298382df0bd8e606d8a
prateek.s <psc3018@gmail.com>
2020-11-26 04:09:45 +0530

    fixed pagination

--- src/library/product/search/stores/c/costco/US/paginate.js
+++ src/library/product/search/stores/c/costco/US/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'costco',
-    nextLinkSelector: 'li.forward > a',
+    nextLinkSelector: null,
     // mutationSelector: null,
     // spinnerSelector: null,
     loadedSelector: 'div.product-list',
     noResultsXPath: '//h1[contains(text(),"Try another search")]',
-    // openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.costco.com/CatalogSearch?currentPage={page}&dept=All&keyword={searchTerms}'
+    },
     domain: 'costco.com',
     zipcode: '',
   },
