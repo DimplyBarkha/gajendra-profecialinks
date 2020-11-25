f5cf9659d319fe83c1a89c8bafddf687e3543e5c
Vivek Gondil <gondilvivek@gmail.com>
2020-11-25 10:48:43 +0530

    Added suppoert to input as URL

--- src/library/product/details/stores/0/0815/AT/execute.js
+++ src/library/product/details/stores/0/0815/AT/execute.js
@@ -19,35 +19,37 @@ async function implementation (
     }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
   }
 
-  await context.waitForSelector('.product-image-link');
-  const pageLink = await context.evaluate(() => document.querySelector('.product-image-link').getAttribute('href'));
-  const rpc = pageLink.split('/').pop();
-  if (id.toString() === rpc.toString()) {
-    await context.goto(pageLink, {
-      blockAds: false,
-      loadAllResources: true,
-      imagesEnabled: true,
-      timeout: 100000,
-      waitUntil: 'networkidle0',
-    });
-    await context.waitForSelector('div[class*="is-button-buy"]', { timeout: 100000 });
-    await context.evaluate(() => {
-      const div = document.evaluate("//div[contains(@class,'product-detail-buy')]//div[contains(@class,'custom-price-style') and not (contains(@class,'instead'))]", document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
-      div.innerText = div.innerText.replace(',', '.').replace('\n', '');
-      const ava2 = document.createElement('div');
-      ava2.setAttribute('id', 'availablity');
-      ava2.innerText = 'In Stock';
-      document.body.append(ava2);
-
-      const desc = document.querySelector('div[class*="custom-detail-short-description"]').innerText;
-      const name = document.querySelector('h2[class*="custom-detail-headline"]');
-      name.innerText = name.innerText + ' ' + desc;
-    });
-  } else {
-    await context.evaluate(() => {
-      document.querySelector('meta[property="og:image"]').setAttribute('content', '');
-    });
+  if (id) {
+    await context.waitForSelector('.product-image-link');
+    const pageLink = await context.evaluate(() => document.querySelector('.product-image-link').getAttribute('href'));
+    const rpc = pageLink.split('/').pop();
+    if (id.toString() === rpc.toString()) {
+      await context.goto(pageLink, {
+        blockAds: false,
+        loadAllResources: true,
+        imagesEnabled: true,
+        timeout: 100000,
+        waitUntil: 'networkidle0',
+      });
+    } else {
+      await context.evaluate(() => {
+        document.querySelector('meta[property="og:image"]').setAttribute('content', '');
+      });
+    }
   }
+  await context.waitForSelector('div[class*="is-button-buy"]', { timeout: 100000 });
+  await context.evaluate(() => {
+    const div = document.evaluate("//div[contains(@class,'product-detail-buy')]//div[contains(@class,'custom-price-style') and not (contains(@class,'instead'))]", document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
+    div.innerText = div.innerText.replace(',', '.').replace('\n', '');
+    const ava2 = document.createElement('div');
+    ava2.setAttribute('id', 'availablity');
+    ava2.innerText = 'In Stock';
+    document.body.append(ava2);
+
+    const desc = document.querySelector('div[class*="custom-detail-short-description"]').innerText;
+    const name = document.querySelector('h2[class*="custom-detail-headline"]');
+    name.innerText = name.innerText + ' ' + desc;
+  });
 }
 module.exports = {
   implements: 'product/details/execute',
