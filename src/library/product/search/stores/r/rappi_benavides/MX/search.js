c3319e966135fb6c8ff00fc066bc8faacaabd69f
adheerProfecia <peter@profecialinks.com>
2020-11-26 01:28:03 +0530

    core-rappi_benavides_MX #2037 search-rappi_benavides_mx #2039

---
+++ src/library/product/search/stores/r/rappi_benavides/MX/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'MX',
+    store: 'rappi_benavides',
+    domain: 'rappi.com.mx',
+    zipcode: '',
+  },
+};
