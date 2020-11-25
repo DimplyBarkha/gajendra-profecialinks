82a425c96ab2f91c2f96f635f7613b5a917e1388
Lokanath sahoo <loknath.s@osmosys.asia>
2020-11-25 15:04:29 +0530

    #1920 completed the development

--- src/library/product/details/stores/a/aptekagemini/PL/extract.js
+++ src/library/product/details/stores/a/aptekagemini/PL/extract.js
@@ -1,11 +1,46 @@
-
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(() => {
+    //written implementation for getting the brand
+    const script = document.querySelectorAll('script[type="application/ld+json"]');
+    const ourScript = [...script].find((element) => element.innerText.includes('brand'));
+    const jsonData = JSON.parse(ourScript && ourScript.innerText);
+    const brand = jsonData && jsonData.brand && jsonData.brand.name;
+    const seller = jsonData && jsonData.offers && jsonData.offers.seller && jsonData.offers.seller.name;
+    const sku = jsonData && jsonData.sku;
+    const gtin = jsonData && jsonData.gtin;
+    const manufacturer = jsonData && jsonData.manufacturer && jsonData.manufacturer.name;
+    //written implementation for getting the rpc
+    const rpcData = document.querySelector('gtm-data-layer-push-product-async[class*="custom-element"]') && document.querySelector('gtm-data-layer-push-product-async[class*="custom-element"]').getAttribute('data');
+    const jsonData2 = rpcData && JSON.parse(rpcData);
+    const rpc = jsonData2 && jsonData2.ecommerce && jsonData2.ecommerce.detail && jsonData2.ecommerce.detail.products[0] && jsonData2.ecommerce.detail.products[0].dimension22;
+    const appendElement = document.createElement('div');
+    appendElement.className = 'productinfos';
+    appendElement.setAttribute('brand', brand);
+    appendElement.setAttribute('seller', seller);
+    appendElement.setAttribute('sku', sku);
+    appendElement.setAttribute('gtin', gtin);
+    appendElement.setAttribute('manufacturrer', manufacturer);
+    appendElement.setAttribute('rpc', rpc)
+    document.body.append(appendElement);
+  })
+  return await context.extract(productDetails, { transform });
+}
+const { cleanUp } = require('../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'PL',
     store: 'aptekagemini',
-    transform: null,
+    transform: cleanUp,
     domain: 'aptekagemini.pl',
     zipcode: '',
   },
+  implementation,
 };
---
+++ src/library/product/details/stores/a/aptekagemini/shared.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const cleanUp = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.brandText) {
+        if (row.brandText[0].text === "null") {
+          row.brandText[0].text = '';
+        }
+      }
+      if (row.additionalDescBulletInfo) {
+        let text = '';
+        row.additionalDescBulletInfo.forEach((element) => {
+          text += ` || ${element.text}`;
+        })
+        row.additionalDescBulletInfo = [{ text: text.trim() }]
+      }
+      if (row.additionalDescBulletInfo && row.description) {
+        let text = `${row.description[0].text} ${row.additionalDescBulletInfo[0].text}`
+        row.description = [{ text }]
+      }
+    }
+  }
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+  return data;
+};
+
+module.exports = { cleanUp };
