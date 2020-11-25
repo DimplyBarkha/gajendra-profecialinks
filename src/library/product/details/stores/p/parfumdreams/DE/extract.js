2450a72620abe4247b27b603da55b9c6bd5df028
unknown <harsh.khare-od@partner.import.io>
2020-11-25 19:20:09 +0530

    Parfumdreams IM complete

--- src/library/product/details/stores/p/parfumdreams/DE/extract.js
+++ src/library/product/details/stores/p/parfumdreams/DE/extract.js
@@ -8,4 +8,35 @@ module.exports = {
     domain: 'en.parfumdreams.de',
     zipcode: '',
   },
+  implementation: async ({ inputstring }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(() => {
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+      var dec = getAllXpath('(//div[@class="variation-alt-images clearfix"]/div/@data-variation-id | //*[@id="schema-offer"]/div[1]/div/input/@value)', 'nodeValue');
+      var str = "";
+      if (dec != null) {
+        str = dec.join(" | ");
+        addElementToDocument('str', str);
+      }
+    });
+
+
+    await context.extract(productDetails);
+
+  },
+
 };
