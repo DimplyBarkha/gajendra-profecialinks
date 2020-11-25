6298c7818049f8b5e08a9d6d6e26942aacfc4445
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-11-25 11:54:45 +0100

    #1469 - Update loadedSelector in execute.js

--- src/library/product/details/stores/r/romeos/AU/execute.js
+++ src/library/product/details/stores/r/romeos/AU/execute.js
@@ -1,11 +1,10 @@
-
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
     country: 'AU',
     store: 'romeos',
     domain: 'martinplace.romeosonline.com.au',
-    loadedXpath: '//div[@class="ModalPopup__Content"]//img[@class="product_image"]',
+    loadedSelector: 'div.search_results',
     noResultsXPath: '//h1/span[@class="weak" and text()="Whoops!"] | //div[@class="MoreInfo"]//strong[text()="No longer available"]',
     zipcode: '',
   },
