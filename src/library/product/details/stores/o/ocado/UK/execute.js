d886de84391cc43125a44d2f83cf7f91a865aadb
harsha chirmade <harsha.chirmade@profeciallinks.com>
2020-11-25 12:52:34 +0530

    ocado-UK-details extractor

---
+++ src/library/product/details/stores/o/ocado/UK/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'ocado',
+    domain: 'ocado.uk',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: "''",
+  },
+};
