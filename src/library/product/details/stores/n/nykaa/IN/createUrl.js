e3c1a211813e0fc2c5e04bb59a0ea13f85fde851
Sephali-23 <sephalisingh023@gmail.com>
2020-11-25 10:06:20 +0530

    fixed variant id

--- src/library/product/details/stores/n/nykaa/IN/createUrl.js
+++ src/library/product/details/stores/n/nykaa/IN/createUrl.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'nykaa.com',
-    url: 'https://www.nykaa.com/dyson-supersonic-hair-dryer/p/{id}',
+    // url: 'https://www.nykaa.com/dyson-supersonic-hair-dryer/p/{id}',
     url: null,
     country: 'IN',
     store: 'nykaa',
