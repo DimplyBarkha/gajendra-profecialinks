561378d882605da34c5d1bd7eb920725e7994e30
vikas kyatannawar <vikas.k@osmosys.asia>
2020-11-25 12:26:09 +0530

    added walmart US offers

---
+++ src/library/product/offers/stores/w/walmart/US/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/offers/paginate',
+  parameterValues: {
+    country: 'US',
+    store: 'walmart',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'walmart.com',
+    zipcode: '',
+  },
+};
