3a54e860b7347fe7609d32a4afeccefe46623ca0
Rafał Perkowski <rafalpero1@gmail.com>
2020-11-25 17:36:32 +0100

    Created pagination and updated yaml

--- src/library/product/search/stores/p/petlove/BR/extract.js
+++ src/library/product/search/stores/p/petlove/BR/extract.js
@@ -1,27 +1,38 @@
 const { transform } = require('../../../../shared');
 
-// async function implementation (inputs, parameters, context, dependencies) {
-//   const { productDetails } = dependencies;
+async function implementation (inputs, parameters, context, dependencies) {
+  const { productDetails } = dependencies;
+  // Do a scroll down to load all products
+  await context.evaluate(async function () {
+    let scrollSelector = document.querySelector('a#show-more-products');
+    if (scrollSelector) {
+      do {
+        scrollSelector.scrollIntoView();
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        scrollSelector = document.querySelector('a#show-more-products');
+      } while (scrollSelector.getAttribute('style') === null);
+    }
+  });
 
-//   await context.evaluate(() => {
-//     const productUrlAll = document.querySelectorAll('.catalog-list-item');
-//     let productUrl;
-//     const ratingSelector = document.querySelectorAll('.catalog-list-stars float-right>span');
+  /* await context.evaluate(() => {
+    const productUrlAll = document.querySelectorAll('.catalog-list-item');
+    let productUrl;
+    const ratingSelector = document.querySelectorAll('.catalog-list-stars float-right>span');
 
-//     for (let i = 0; i < productUrlAll.length; i++) {
-//       if (productUrlAll[i].href.includes('https')) {
-//         productUrl = productUrlAll[i].href;
-//       } else {
-//         productUrl = 'https://www.petlove.com.br' + productUrlAll[i].href;
-//       }
+    for (let i = 0; i < productUrlAll.length; i++) {
+      if (productUrlAll[i].href.includes('https')) {
+        productUrl = productUrlAll[i].href;
+      } else {
+        productUrl = 'https://www.petlove.com.br' + productUrlAll[i].href;
+      }
 
-//       document.querySelectorAll('.catalog-list-item')[i].setAttribute('productUrl', productUrl);
-//       document.querySelectorAll('.catalog-list-item')[i].setAttribute('rank', `${i + 1}`);
-//       document.querySelectorAll('.catalog-list-item')[i].setAttribute('rankOrganic', `${i + 1}`);
-//     };
-//   });
-//   await context.extract(productDetails);
-// }
+      document.querySelectorAll('.catalog-list-item')[i].setAttribute('productUrl', productUrl);
+      document.querySelectorAll('.catalog-list-item')[i].setAttribute('rank', `${i + 1}`);
+      document.querySelectorAll('.catalog-list-item')[i].setAttribute('rankOrganic', `${i + 1}`);
+    };
+  }); */
+  return await context.extract(productDetails);
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -31,5 +42,5 @@ module.exports = {
     domain: 'petlove.com.br',
     zipcode: '',
   },
-  // implementation,
+  implementation,
 };
