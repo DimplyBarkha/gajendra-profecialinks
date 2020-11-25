9a1e1075cd7da44e720853240332f9ea0424531c
unknown <shalini.kumari-odc@partner.import.io>
2020-11-25 23:33:20 +0530

    kicks

---
+++ src/library/navigation/goto/domains/ki/kicks.no.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'kicks.no',
+    timeout: null,
+    country: 'NO',
+    store: 'kicks',
+    zipcode: '',
+  },
+};
---
+++ src/library/navigation/goto/domains/ki/setZipCode.kicks.no.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'NO',
+    domain: 'kicks.no',
+    store: 'kicks',
+    zipcode: '',
+  },
+};
