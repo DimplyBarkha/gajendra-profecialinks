c0fe4a9f5f3b274aa36d859e62f2bccbe327f8cc
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-11-25 23:00:13 +0530

    #1877 Added files

---
+++ src/library/product/search/stores/t/thewarehouse/NZ/paginate.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'NZ',
+    store: 'thewarehouse',
+    nextLinkSelector: 'li a.paging.next',
+    nextLinkXpath: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: 'ul#search-result-items .grid-tile',
+    loadedXpath: null,
+    noResultsXPath: '//div[@class="no-search-result"]',
+    stopConditionSelectorOrXpath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'thewarehouse.co.nz',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/search/stores/t/thewarehouse/NZ/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'NZ',
+    store: 'thewarehouse',
+    domain: 'thewarehouse.co.nz',
+    zipcode: "''",
+  },
+};
