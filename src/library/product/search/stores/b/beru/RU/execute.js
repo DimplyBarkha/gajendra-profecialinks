f4917c9c4462d8c09b2c81e83dbb44d53998e22e
shrutikab <shrutika.bongarde@synerzip.com>
2020-11-26 01:12:46 +0530

    Added xpath and paginatiom(#2022)

--- src/library/product/search/stores/b/beru/RU/execute.js
+++ src/library/product/search/stores/b/beru/RU/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'RU',
     store: 'beru',
     domain: 'pokupki.market.yandex.ru',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://pokupki.market.yandex.ru/search?text={searchTerms}',
+    loadedSelector: 'div.b_2wGkXdFPAH',
+    noResultsXPath: '//div[@class="b_y8i0GBUY25"]//div[contains(@class,"b_2lf8berAKx")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/b/beru/RU/extract.js
+++ src/library/product/search/stores/b/beru/RU/extract.js
@@ -1,11 +1,61 @@
-
+const { transform } = require('../../../../shared')
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'RU',
     store: 'beru',
-    transform: null,
+    transform,
     domain: 'pokupki.market.yandex.ru',
     zipcode: '',
   },
+  implementation
 };
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll("div.b_sEc13pK37d")[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+  // @ts-ignore
+  let rc = document.querySelectorAll('div.b_sEc13pK37d')
+  console.log(rc , 'op');
+  rc.forEach((element , i) => {
+    console.log(element.style.cssText.replace('width:', '').replace('%;', '')/20  , 'dio'
+      );
+      let name;
+      name = element.style.cssText.replace('width:', '').replace('%;', '')/20
+      addHiddenDiv('ii_name', name, i);
+
+  });
+    let scrollTop = 0;
+    while (scrollTop !== 20000) {
+      await stall(500);
+      scrollTop += 1000;
+      window.scroll(0, scrollTop);
+      if (scrollTop === 20000) {
+        await stall(5000);
+        break;
+      }
+    }
+    function stall (ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
