8a34d9abd17172f5f9f70a626dfc78e2c3114d50
Karina Miernik <karina.miernik@gmail.com>
2020-11-25 09:40:53 +0100

    #1593 id, productUrl and soldBy fixed

--- src/library/product/search/stores/m/mercadolibre/AR/extract.js
+++ src/library/product/search/stores/m/mercadolibre/AR/extract.js
@@ -7,30 +7,14 @@ async function implementation (inputs, parameters, context, dependencies) {
   await context.evaluate(async () => {
     const searchUrl = window.location.href;
     document.querySelectorAll('li[class*="layout__item"]').forEach(e => e.setAttribute('searchurl', searchUrl));
+    // set rank
     const products = document.querySelectorAll('li[class*="layout__item"]');
-    products.forEach((product, index) => {
-      // set product id
-      const allAcriptTags = document.querySelectorAll('head script:not([type]):not([src])');
-      // @ts-ignore
-      const scriptWithId = [...allAcriptTags].filter(e => e.textContent.includes('dimension49"'));
-      const productIdArr = scriptWithId[0].textContent.match(/dimension49", "(.*)"\)/)[1].split(',');
-      // @ts-ignore
-      product.setAttribute('id', productIdArr[index]);
+    products.forEach((product, index) => product.setAttribute('rank', `${index + 1}`));
+    // set soldBy
+    const soldBy = document.querySelectorAll('p[class*="store-label"]');
 
-      // set productUrl
-      const prefixUrl = 'https://articulo.mercadolibre.com.ar/MLA-';
-      product.setAttribute('productUrl', prefixUrl.concat(productIdArr[index].match(/\d+/)[0]));
-      // set rank
-      product.setAttribute('rank', (index + 1).toString());
-      // set soldBy
-      // @ts-ignore
-      const soldBy = product.querySelector('p[class*="store-label"]')
-        // @ts-ignore
-        ? product.querySelector('p[class*="store-label"]').innerText.split('por') : null;
-      if (soldBy !== null) {
-        product.setAttribute('soldBy', soldBy.pop());
-      }
-    });
+    // @ts-ignore
+    if (soldBy !== null) soldBy.forEach(e => e.setAttribute('soldBy', e.innerText.split('por').pop()));
   });
 
   return await context.extract(productDetails, { transform });
