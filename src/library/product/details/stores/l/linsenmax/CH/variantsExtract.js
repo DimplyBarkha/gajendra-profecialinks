771f64e823a3bc100e58297dc00910b17b9b3747
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-11-26 18:25:30 +0530

    Updated code

--- src/library/product/details/stores/l/linsenmax/CH/variantsExtract.js
+++ src/library/product/details/stores/l/linsenmax/CH/variantsExtract.js
@@ -1,4 +1,35 @@
 const { transform } = require('./variantsFormat');
+
+/**
+ *
+ * @param { { url: any, id: any } } inputs
+ * @param { { country: any, transform: any, store: any } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { Record<string, any> } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { variants } = dependencies;
+  await context.evaluate(async function () {
+    const canonicalUrl = document.querySelector('link[rel="canonical"]').getAttribute('href');
+    const allBlocks = document.querySelector('div.productdetail-radio div.sky-form__radio') ? document.querySelectorAll('div.productdetail-radio div.sky-form__radio') : [];
+    allBlocks.forEach((block, index) => {
+      const div = document.createElement('div');
+      div.id = 'product_page_url';
+      div.innerHTML = canonicalUrl;
+      block.appendChild(div);
+      console.log(block);
+    });
+  });
+
+  return await context.extract(variants, { transform });
+}
+
 module.exports = {
   implements: 'product/details/variants/variantsExtract',
   parameterValues: {
@@ -8,4 +39,5 @@ module.exports = {
     domain: 'linsenmax.ch',
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/details/stores/l/linsenmax/CH/variantsFormat.js
+++ src/library/product/details/stores/l/linsenmax/CH/variantsFormat.js
@@ -11,6 +11,23 @@ const transform = (data) => {
           item.text = item.text.trim();
         });
       }
+      if (row.variantUrl) {
+        if (row.variantId) {
+          row.variantUrl.forEach(item => {
+            item.text = item.text + '?unitId=' + row.variantId[0].text;
+          });
+        }
+      }
+      if (row.variantId) {
+        if (row.pId) {
+          row.variantId.forEach(item => {
+            item.text = row.pId[0].text + '-' + item.text;
+          });
+        }
+      }
+      if (row.pId) {
+        delete row.pId;
+      }
     }
   }
   return data;
