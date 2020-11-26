8ed23a5d11578845ba7215b610b41edbfea687e2
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-26 16:02:27 +0530

    Dermstore Search rank fixed

--- src/library/product/search/stores/d/dermstore/US/extract.js
+++ src/library/product/search/stores/d/dermstore/US/extract.js
@@ -15,10 +15,6 @@ async function implementation (
   }
   let count = 0;
   while (true) {
-    await context.extract(productDetails, { transform }, { type: 'APPEND' });
-    count += await context.evaluate(() => {
-      return document.querySelectorAll("div[id*='prodGrid'] div[class*='prod-widget-responsive']").length;
-    });
     const nextLink = await context.evaluate(() => {
       const element = document.evaluate("//span[contains(@class,'hbmid active')]/following-sibling::a[contains(@class,'page_list')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
       if (element) {
@@ -27,11 +23,14 @@ async function implementation (
         return false;
       }
     });
-    // await context.waitForXpath("//span[contains(@class,'hbmid active')]/following-sibling::a[contains(@class,'page_list')]", { timeout: 60000 });
-    if (nextLink && count < 150) {
+    if (nextLink && count < 144) {
+      await context.extract(productDetails, { transform }, { type: 'APPEND' });
+      count += await context.evaluate(() => {
+        return document.querySelectorAll("div[id*='prodGrid'] div[class*='prod-widget-responsive']").length;
+      });
+      console.log('Count of the product', count);
       await context.evaluate(async () => {
         const element = document.evaluate("//span[contains(@class,'hbmid active')]/following-sibling::a[contains(@class,'page_list')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        console.log('Elemdsfsdfdent' + element);
         element && element.click();
         await new Promise(resolve => setTimeout(resolve, 2000));
       });
