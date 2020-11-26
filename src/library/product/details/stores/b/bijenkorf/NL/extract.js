577308193f881a6b18e51b5041f8652a40f865b8
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-11-26 15:31:47 +0530

    fixed issue with non matching rpc and non input click

--- src/library/product/details/stores/b/bijenkorf/NL/extract.js
+++ src/library/product/details/stores/b/bijenkorf/NL/extract.js
@@ -10,17 +10,18 @@ module.exports = {
   },
 
   implementation: async (inputs, { country, domain, transform: transformParam }, context, { productDetails }) => {
-    const productDetailsLink = await context.evaluate(function (inputs) {
+    const productDetailsLink = await context.evaluate(async function (inputs) {
       const productList = document.querySelectorAll('ul.productlist__list > li a');
       for (let i = 0; i < productList.length; i++) {
         const productRpc = productList[i].getAttribute('name');
-        if (productRpc.includes(inputs.id)) {
+        if (productRpc.includes(inputs.id) || productList.length === 1) {
           return productList[i].getAttribute('href');
         } else {
           return null;
         }
       }
     }, inputs);
+
     if (productDetailsLink) {
       const url = productDetailsLink;
       await context.goto(url, {
@@ -49,44 +50,48 @@ module.exports = {
             await new Promise((resolve, reject) => setTimeout(resolve, 3000));
           }
           // @ts-ignore
-          variantsList[i].querySelector('div input').click();
-          await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-          const variantInfoSpan = document.querySelector('p.dbk-product-indicator span.dbk-product-indicator--value');
+          const variantToClick = variantsList[i].querySelector('div input');
+          if (variantToClick) {
+            // @ts-ignore
+            variantToClick.click();
+            await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+            const variantInfoSpan = document.querySelector('p.dbk-product-indicator span.dbk-product-indicator--value');
 
-          if (variantInfoSpan) {
-            const variantInfoEle = document.createElement('div');
-            variantInfoEle.setAttribute('id', 'variantInfo');
-            variantInfoEle.textContent = variantInfoSpan.textContent;
-            variantInfoEle.style.visibility = 'hidden';
-            variantsList[i].appendChild(variantInfoEle);
-          }
+            if (variantInfoSpan) {
+              const variantInfoEle = document.createElement('div');
+              variantInfoEle.setAttribute('id', 'variantInfo');
+              variantInfoEle.textContent = variantInfoSpan.textContent;
+              variantInfoEle.style.visibility = 'hidden';
+              variantsList[i].appendChild(variantInfoEle);
+            }
 
-          const skuInfoSpan = document.querySelector('span[itemprop="sku"]');
+            const skuInfoSpan = document.querySelector('span[itemprop="sku"]');
 
-          if (skuInfoSpan) {
-            const skuInfoEle = document.createElement('div');
-            skuInfoEle.setAttribute('id', 'skuInfo');
-            skuInfoEle.textContent = skuInfoSpan.textContent;
-            skuInfoEle.style.visibility = 'hidden';
+            if (skuInfoSpan) {
+              const skuInfoEle = document.createElement('div');
+              skuInfoEle.setAttribute('id', 'skuInfo');
+              skuInfoEle.textContent = skuInfoSpan.textContent;
+              skuInfoEle.style.visibility = 'hidden';
 
-            variantsList[i].appendChild(skuInfoEle);
-          }
+              variantsList[i].appendChild(skuInfoEle);
+            }
 
-          const images = document.querySelectorAll('div.dbk-image-gallery--display ul.dbk-image-carousel--list > li  img');
-          const secondaryImageCountDiv = document.createElement('div');
-          secondaryImageCountDiv.setAttribute('id', 'secondaryImageCount');
-          secondaryImageCountDiv.textContent = (images.length - 1).toString();
-          secondaryImageCountDiv.style.visibility = 'hidden';
-          variantsList[i].appendChild(secondaryImageCountDiv);
+            const images = document.querySelectorAll('div.dbk-image-gallery--display ul.dbk-image-carousel--list > li  img');
+            const secondaryImageCountDiv = document.createElement('div');
+            secondaryImageCountDiv.setAttribute('id', 'secondaryImageCount');
+            secondaryImageCountDiv.textContent = (images.length - 1).toString();
+            secondaryImageCountDiv.style.visibility = 'hidden';
+            variantsList[i].appendChild(secondaryImageCountDiv);
 
-          for (let j = 0; j < images.length; j++) {
-            const variantImageDiv = document.createElement('div');
-            variantImageDiv.setAttribute('id', 'variantImages');
-            variantImageDiv.setAttribute('href', images[j].getAttribute('src'));
-            variantImageDiv.setAttribute('alt', images[j].getAttribute('alt'));
-            variantImageDiv.setAttribute('position', j.toString());
-            variantImageDiv.style.visibility = 'hidden';
-            variantsList[i].appendChild(variantImageDiv);
+            for (let j = 0; j < images.length; j++) {
+              const variantImageDiv = document.createElement('div');
+              variantImageDiv.setAttribute('id', 'variantImages');
+              variantImageDiv.setAttribute('href', images[j].getAttribute('src'));
+              variantImageDiv.setAttribute('alt', images[j].getAttribute('alt'));
+              variantImageDiv.setAttribute('position', j.toString());
+              variantImageDiv.style.visibility = 'hidden';
+              variantsList[i].appendChild(variantImageDiv);
+            }
           }
         }
       } else {
