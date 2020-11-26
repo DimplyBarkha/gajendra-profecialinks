356cc15e1de5890cefd1e48bcf6f91d3b35a6fe7
Vivek Gondil <gondilvivek@gmail.com>
2020-11-26 23:41:02 +0530

    Added fixes from UAT

--- src/library/product/details/stores/f/footlocker/UK/extract.js
+++ src/library/product/details/stores/f/footlocker/UK/extract.js
@@ -16,28 +16,33 @@ module.exports = {
   ) {
     const { cleanUp } = parameters;
     const { productDetails } = dependencies;
-
-    await context.evaluate(() => {
-      const getvariants = document.querySelectorAll('div.owl-stage a');
-      const variantid = [];
-      for (let i = 0; i < getvariants.length; i++) {
-        variantid.push(getvariants[i].getAttribute('data-product-click-link'));
-      }
-      const getSizeVariants = document.querySelectorAll('#fitanalytics_sizecontainer > section:nth-child(3) > div >button');
-      for (let i = 0; i < getSizeVariants.length; i++) {
-        if (getSizeVariants[i].getAttribute('data-product-size-select-item')) {
-          variantid.push(getSizeVariants[i].getAttribute('data-product-size-select-item'));
-        }
-      }
-      for (let i = 0; i < variantid.length; i++) {
-        const div = document.createElement('div');
-        div.setAttribute('id', 'variantId');
-        div.setAttribute('data', variantid[i]);
-        document.body.append(div);
-      }
-    });
     try {
       await context.waitForSelector('.slick-list.draggable .slick-active img', { timeout: 30000 });
+      await context.evaluate(() => {
+        const getvariants = document.querySelectorAll('div.owl-stage a');
+        const variantid = [];
+        for (let i = 0; i < getvariants.length; i++) {
+          variantid.push(getvariants[i].getAttribute('data-product-click-link'));
+        }
+        const getSizeVariants = document.querySelectorAll('#fitanalytics_sizecontainer > section:nth-child(3) > div >button');
+        for (let i = 0; i < getSizeVariants.length; i++) {
+          if (getSizeVariants[i].getAttribute('data-product-size-select-item')) {
+            variantid.push(getSizeVariants[i].getAttribute('data-product-size-select-item'));
+          }
+        }
+        for (let i = 0; i < variantid.length; i++) {
+          const div = document.createElement('div');
+          div.setAttribute('id', 'variantId');
+          div.setAttribute('data', variantid[i]);
+          document.body.append(div);
+        }
+        const stock = document.querySelector('div[class="fl-product-error-messages"]');
+        if (stock.textContent.includes('In') || stock.textContent.includes('Less then')) {
+          stock.setAttribute('availability', 'In stock');
+        } else {
+          stock.setAttribute('availability', 'Out of stock');
+        }
+      });
     } catch (err) {
       console.log('Image did not load');
     }
