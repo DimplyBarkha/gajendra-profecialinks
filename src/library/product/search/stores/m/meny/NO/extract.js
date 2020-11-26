3a8be3fa23c845ebd09a6818bc68f69aaaf6ab1d
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-11-26 17:52:09 +0530

    fixed rank issue

--- src/library/product/search/stores/m/meny/NO/extract.js
+++ src/library/product/search/stores/m/meny/NO/extract.js
@@ -17,45 +17,45 @@ module.exports = {
     const { productDetails } = dependencies;
     const { transform } = parameters;
 
-    // const optionalWait = async (sel) => {
-    //   try {
-    //     await context.waitForSelector(sel, { timeout: 30000 });
-    //     console.log(`Selector => "${sel}" loaded`);
-    //     return true;
-    //   } catch (err) {
-    //     console.log(`Selector => ${sel} did not load.`);
-    //     return false;
-    //   }
-    // };
+    const optionalWait = async (sel) => {
+      try {
+        await context.waitForSelector(sel, { timeout: 30000 });
+        console.log(`Selector => "${sel}" loaded`);
+        return true;
+      } catch (err) {
+        console.log(`Selector => ${sel} did not load.`);
+        return false;
+      }
+    };
 
-    // const seeAll = await optionalWait("div[id='ws-search-block-products']>button[class*='ws-link']");
+    const seeAll = await optionalWait("div[id='ws-search-block-products']>button[class*='ws-link']");
 
-    // if (seeAll) {
-    //   await context.click("div[id='ws-search-block-products']>button[class*='ws-link']");
-    //   await context.waitForNavigation({ timeout: 30000, waitUntil: 'networkidle0' });
-    // }
+    if (seeAll) {
+      await context.click("div[id='ws-search-block-products']>button[class*='ws-link']");
+      await context.waitForNavigation({ timeout: 30000, waitUntil: 'networkidle0' });
+    }
 
-    // const response = await context.searchAllRequests('/episearch/1300/products.*');
+    const response = await context.searchAllRequests('/episearch/1300/products.*');
 
-    // for (const res of response) {
-    //   if (!res.responseBody || !res.responseBody.body)continue;
-    //   const searchResponse = JSON.parse(res.responseBody.body);
+    for (const res of response) {
+      if (!res.responseBody || !res.responseBody.body)continue;
+      const searchResponse = JSON.parse(res.responseBody.body);
 
-    //   if (!searchResponse.hits || !searchResponse.hits.hits) continue;
+      if (!searchResponse.hits || !searchResponse.hits.hits) continue;
 
-    //   for (const hit of searchResponse.hits.hits) {
-    //     await context.evaluate(function (param) {
-    //       const imgId = param.contentId;
-    //       const url = param.contentData && param.contentData._source && param.contentData._source.slugifiedUrl;
+      for (const hit of searchResponse.hits.hits) {
+        await context.evaluate(function (param) {
+          const imgId = param.contentId;
+          const url = param.contentData && param.contentData._source && param.contentData._source.slugifiedUrl;
 
-    //       if (!imgId || !url) return;
+          if (!imgId || !url) return;
 
-    //       const img = document.querySelector(`img[src*="${param.contentId}"]`);
-    //       console.log(`Setting URL for ${param.contentId}`);
-    //       img.setAttribute('url', `https://meny.no/varer${url}`);
-    //     }, hit);
-    //   }
-    // }
+          const img = document.querySelector(`img[src*="${param.contentId}"]`);
+          console.log(`Setting URL for ${param.contentId}`);
+          img.setAttribute('url', `https://meny.no/varer${url}`);
+        }, hit);
+      }
+    }
 
     async function getProductsCount (context) {
       return context.evaluate(async function () {
