dd10afae64cf1ba1d86c4e988792ab84fd923206
romancg <gromantsg@gmail.com>
2020-11-26 12:22:35 +0200

    #1209 update extractor

--- src/library/product/search/stores/e/euronics/IT/extract.js
+++ src/library/product/search/stores/e/euronics/IT/extract.js
@@ -1,3 +1,4 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
@@ -7,5 +8,21 @@ module.exports = {
     transform: null,
     domain: 'euronics.it',
     zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(() => {
+      const pi = 12;
+      const url = new URL(window.location.href);
+      const p = url.searchParams.get('p');
+      const page = p || 1;
+      const product = document.querySelectorAll('div.productCard.j-productCard');
+      for (let i = 0; i < product.length; i++) {
+        const rank = (page * pi) - (pi - 1) + i;
+        product[i].querySelector('#log_position').setAttribute('rank', rank);
+      }
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
