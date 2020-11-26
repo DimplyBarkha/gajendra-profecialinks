5776d6c7c9e111fa015bf4aee91c19688137ee93
Gajender Ramola <ramola106@gmail.com>
2020-11-26 16:23:43 +0530

    #2136 core extractor implemented

--- src/library/product/details/stores/o/officemag/RU/extract.js
+++ src/library/product/details/stores/o/officemag/RU/extract.js
@@ -1,60 +1,59 @@
-const { transform } = require('../../../../shared');
+// const { transform } = require('../../../../shared');
 
-async function implementation (inputs, parameters, context, dependencies) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
+// async function implementation (inputs, parameters, context, dependencies) {
+//   const { transform } = parameters;
+//   const { productDetails } = dependencies;
 
-  await context.evaluate(async () => {
-    function addElementToDom (element, id) {
-      const div = document.createElement('div');
-      div.id = id;
-      div.innerHTML = element;
-      document.body.appendChild(div);
-    }
+//   await context.evaluate(async () => {
+//     function addElementToDom (element, id) {
+//       const div = document.createElement('div');
+//       div.id = id;
+//       div.innerHTML = element;
+//       document.body.appendChild(div);
+//     }
 
-    // add product url
-    const productUrl = window.location.href;
-    addElementToDom(productUrl, 'productUrl');
+//     // add product url
+//     const productUrl = window.location.href;
+//     addElementToDom(productUrl, 'productUrl');
 
-    // add img url
-    const prefix = 'https://www.officemag.ru';
-    const rawImgNode = document.querySelector('a[id="photosListLink"] img');
-    if (rawImgNode) {
-      const imgUrl = rawImgNode.getAttribute('src');
-      addElementToDom(prefix + imgUrl, 'imgUrl');
-    }
+//     // add img url
+//     const prefix = 'https://www.officemag.ru';
+//     const rawImgNode = document.querySelector('a[id="photosListLink"] img');
+//     if (rawImgNode) {
+//       const imgUrl = rawImgNode.getAttribute('src');
+//       addElementToDom(prefix + imgUrl, 'imgUrl');
+//     }
 
-    // alternate imgs
-    const altImgsNode = document.querySelectorAll(
-      'a[class="ProductPhotoThumb__link js-bigImg"] img');
-    altImgsNode.forEach((element) => {
-      const altImgUrl = element.getAttribute('src');
-      element.setAttribute('altImgUrl', prefix + altImgUrl);
-    });
+//     // alternate imgs
+//     const altImgsNode = document.querySelectorAll(
+//       'a[class="ProductPhotoThumb__link js-bigImg"] img');
+//     altImgsNode.forEach((element) => {
+//       const altImgUrl = element.getAttribute('src');
+//       element.setAttribute('altImgUrl', prefix + altImgUrl);
+//     });
 
-    // convert rating
-    const rating = document.querySelector('span[class="Score__fill"]');
-    const regex = /\d+\.?(\d+)?/gm;
+//     // convert rating
+//     const rating = document.querySelector('span[class="Score__fill"]');
+//     const regex = /\d+\.?(\d+)?/gm;
 
-    if (rating.getAttribute('style')) {
-      var regArray = rating.getAttribute('style').match(regex);
-      var value = (parseInt(regArray[0]) * 5) / 100;
+//     if (rating.getAttribute('style')) {
+//       var regArray = rating.getAttribute('style').match(regex);
+//       var value = (parseInt(regArray[0]) * 5) / 100;
 
-      rating.setAttribute('rating', value.toString().replace('.', ','));
-    }
-  });
+//       rating.setAttribute('rating', value.toString().replace('.', ','));
+//     }
+//   });
 
-  return await context.extract(productDetails, { transform });
-}
-
+//   return await context.extract(productDetails, { transform });
+// }
+const { transform } = require('./format');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'RU',
     store: 'officemag',
-    transform: transform,
+    transform,
     domain: 'officemag.ru',
     zipcode: '',
   },
-  implementation,
 };
