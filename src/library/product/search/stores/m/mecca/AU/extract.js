f6f04b5325ed7f6d10f8f1bcb1f4292c2758444a
astropaula <wojciechowska.fran@gmail.com>
2020-11-26 12:32:10 +0100

    #1766 Set up extractor

--- src/library/product/search/stores/m/mecca/AU/extract.js
+++ src/library/product/search/stores/m/mecca/AU/extract.js
@@ -9,3 +9,31 @@ module.exports = {
     zipcode: '',
   },
 };
+const { transform } = require('../../../../shared');
+
+async function implementation (inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async () => {
+    // add rank attribute
+    var rank = document.querySelectorAll('div[class="product-tile"]');
+
+    rank.forEach((element, index) => {
+      element.setAttribute('rank', (index + 1).toString());
+    });
+  });
+
+  return await context.extract(productDetails, { transform });
+}
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'AU',
+    store: 'mecca',
+    transform: transform,
+    domain: 'mecca.com.au',
+    zipcode: '',
+  },
+  implementation,
+};
