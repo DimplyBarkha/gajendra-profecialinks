ef6805537013e1c81bb1da083293cc5ab797ca0e
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-26 16:09:49 +0530

    kruidvat-Damayanthi

--- src/library/product/search/stores/k/kruidvat/NL/paginate.js
+++ src/library/product/search/stores/k/kruidvat/NL/paginate.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'NL',
     store: 'kruidvat',
-    nextLinkSelector: 'div.pager.plp-paginator__pager.pager--mobile-bottom > div > e2-plp-page-selectors[css-modifier="pager__link pager__link--next"]',
+    // nextLinkSelector: 'div.pager.plp-paginator__pager.pager--mobile-bottom > div > e2-plp-page-selectors[css-modifier="pager__link pager__link--next"]',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'body',
