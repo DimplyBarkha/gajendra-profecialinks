5b1479613a29807d46955c64cfb4e67ad35d0317
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-11-26 18:39:32 +0530

    fixed url issue

--- src/library/product/search/stores/m/meny/NO/extract.js
+++ src/library/product/search/stores/m/meny/NO/extract.js
@@ -18,6 +18,36 @@ module.exports = {
     const { transform } = parameters;
 
     const optionalWait = async (sel) => {
+      async function getProductsCount (context) {
+        return context.evaluate(async function () {
+          const products = document.evaluate("//ul/li[contains(@class, 'ws-product-list-vertical__item')]", document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+          return products.snapshotLength;
+        });
+      }
+
+      let productsCount = 0;
+      while (productsCount < 150) {
+        const doesLoadMoreExists = await context.evaluate(function () {
+          return Boolean(document.querySelector('div.ws-search-result-full__footer button'));
+        });
+
+        if (doesLoadMoreExists) {
+          await context.evaluate(async function () {
+            console.log('Clicking on load more button');
+            document.querySelector('div.ws-search-result-full__footer button').click();
+            await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+          });
+          productsCount = await getProductsCount(context);
+          console.log('productsCount' + productsCount);
+          if (productsCount >= 150) {
+            break;
+          }
+          // await applyScroll(context);
+        } else {
+          break;
+        }
+      }
+
       try {
         await context.waitForSelector(sel, { timeout: 30000 });
         console.log(`Selector => "${sel}" loaded`);
@@ -36,6 +66,7 @@ module.exports = {
     }
 
     const response = await context.searchAllRequests('/episearch/1300/products.*');
+    console.log(JSON.stringify(response));
 
     for (const res of response) {
       if (!res.responseBody || !res.responseBody.body)continue;
@@ -51,39 +82,10 @@ module.exports = {
           if (!imgId || !url) return;
 
           const img = document.querySelector(`img[src*="${param.contentId}"]`);
+          if (!img) return;
           console.log(`Setting URL for ${param.contentId}`);
           img.setAttribute('url', `https://meny.no/varer${url}`);
         }, hit);
-      }
-    }
-
-    async function getProductsCount (context) {
-      return context.evaluate(async function () {
-        const products = document.evaluate("//ul/li[contains(@class, 'ws-product-list-vertical__item')]", document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        return products.snapshotLength;
-      });
-    }
-
-    let productsCount = 0;
-    while (productsCount < 150) {
-      const doesLoadMoreExists = await context.evaluate(function () {
-        return Boolean(document.querySelector('div.ws-search-result-full__footer button'));
-      });
-
-      if (doesLoadMoreExists) {
-        await context.evaluate(async function () {
-          console.log('Clicking on load more button');
-          document.querySelector('div.ws-search-result-full__footer button').click();
-          await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-        });
-        productsCount = await getProductsCount(context);
-        console.log('productsCount' + productsCount);
-        if (productsCount >= 150) {
-          break;
-        }
-        // await applyScroll(context);
-      } else {
-        break;
       }
     }
 
