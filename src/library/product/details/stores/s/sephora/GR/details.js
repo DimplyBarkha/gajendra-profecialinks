5be72080d8ae8d76a1143d4fdff1bd32a1646bf2
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-26 12:40:34 +0530

    #1839 completign development going for deployement

---
+++ src/library/product/details/stores/s/sephora/GR/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'GR',
+    store: 'sephora',
+    domain: 'sephora.gr',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/s/sephora/GR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'GR',
+    store: 'sephora',
+    transform: null,
+    domain: 'sephora.gr',
+    zipcode: '',
+  },
+};
