e41ca44788df983e2f774f2ebfda591f3165dcb0
Sephali-23 <sephalisingh023@gmail.com>
2020-11-26 18:49:04 +0530

    changed url

--- src/library/product/details/stores/n/nykaa/IN/createUrl.js
+++ src/library/product/details/stores/n/nykaa/IN/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'nykaa.com',
     // url: 'https://www.nykaa.com/dyson-supersonic-hair-dryer/p/{id}',
-    url: null,
+    url: 'https://www.nykaa.com/L/p/{id}',
     country: 'IN',
     store: 'nykaa',
   },
