9c1b8af22b8b2292f25d3f5f69c1245549bd4383
waquas ali <waquas2ali@gmail.com>
2020-11-26 21:34:43 +0530

    Updating changes of Rewe DE Search

--- src/library/product/search/stores/R/Rewe/DE/extract.js
+++ src/library/product/search/stores/R/Rewe/DE/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require('../../../../shared')
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -17,17 +17,16 @@ module.exports = {
       const allProducts = document.querySelectorAll('div.search-service-product');
       for (let i = 0; i < allProducts.length; i++) {
         if ('div.search-service-ProductActions > meso-data') {
-        const gtin = document.querySelectorAll('div.search-service-ProductActions > meso-data')[i].getAttribute('data-productid');
-        document.querySelectorAll('input[name="listingId"]')[i].setAttribute('url', `https://shop.rewe.de/p/${gtin}`);
-        document.querySelectorAll('input[name="listingId"]')[i].setAttribute('number', `${gtin}`);
-      }
-        else {
-          const gtin = document.querySelectorAll('.search-service-rsProductsMedia>picture>img')[i].getAttribute('src').slice(24,40);
+          const gtin = document.querySelectorAll('div.search-service-ProductActions > meso-data')[i].getAttribute('data-productid');
           document.querySelectorAll('input[name="listingId"]')[i].setAttribute('url', `https://shop.rewe.de/p/${gtin}`);
-          document.querySelectorAll('input[name="listingId"]')[i].setAttribute('number', `${gtin}`); 
+          document.querySelectorAll('input[name="listingId"]')[i].setAttribute('number', `${gtin}`);
+        } else {
+          const gtin = document.querySelectorAll('.search-service-rsProductsMedia>picture>img')[i].getAttribute('src').slice(24, 40);
+          document.querySelectorAll('input[name="listingId"]')[i].setAttribute('url', `https://shop.rewe.de/p/${gtin}`);
+          document.querySelectorAll('input[name="listingId"]')[i].setAttribute('number', `${gtin}`);
         }
-    }
+      }
     });
     return await context.extract(productDetails, { transform });
-  }
+  },
 };
