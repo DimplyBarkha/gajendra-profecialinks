1b1df9ebc7445044b6415718a4b04bbf8f7b0be3
wasiqe-a <wasiqe.a@osmosys.asia>
2020-11-26 17:37:26 +0530

    Adding changes to fix the issues wiht extractor

--- src/library/product/search/stores/p/petsuppliesplus/US/paginate.js
+++ src/library/product/search/stores/p/petsuppliesplus/US/paginate.js
@@ -8,12 +8,6 @@ module.exports = {
     loadedSelector: 'div.coveo-result-list-container',
     noResultsXPath: '//div[@class="coveo-query-summary-no-results-string"]',
     spinnerSelector: 'div.coveo-fade-out',
-    openSearchDefinition: {
-      template: 'https://petsuppliesplus.com/Search#q={searchTerms}&first={index}&sort=relevancy',
-      pageIndexMultiplier: 24,
-      pageOffset: 1,
-      pageStartNb: 0
-    },
     domain: 'petsuppliesplus.com',
   },
 }
