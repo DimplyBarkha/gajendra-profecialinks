94d630e190db415de6487f7fe9ed6a3d3f3048c4
Richa Tiwari <richa.t@osmosys.asia>
2020-11-27 17:26:36 +0545

    Fixed issue of inputKeyword not found

--- src/library/product/search/action.js
+++ src/library/product/search/action.js
@@ -1,103 +1,103 @@
 module.exports = {
-  parameters: [
-    {
-      name: 'country',
-      description: '2 letter ISO code for the country',
+    parameters: [{
+            name: 'country',
+            description: '2 letter ISO code for the country',
+        },
+        {
+            name: 'store',
+            description: 'store name',
+        },
+        {
+            name: 'domain',
+            description: 'The top private domain of the website (e.g. amazon.com)',
+        },
+        {
+            name: 'zipcode',
+            description: 'to set location',
+            optional: true,
+        },
+    ],
+    inputs: [{
+            name: 'keywords',
+            description: 'keywords to search for',
+            type: 'string',
+        },
+        {
+            name: 'Keywords',
+            description: 'keywords to search for',
+            type: 'string',
+        },
+        {
+            name: 'Brands',
+            description: 'brands to search for',
+            type: 'string',
+        },
+        {
+            name: 'results',
+            description: 'the minimum number of results required',
+            type: 'number',
+        },
+        {
+            name: 'Brands',
+            description: 'brands to search for',
+            type: 'string',
+        },
+        {
+            name: 'url',
+            description: 'domain url',
+            type: 'string',
+        },
+        {
+            name: 'URL',
+            description: 'domain url',
+        }
+    ],
+    dependencies: {
+        execute: 'action:product/search/execute',
+        paginate: 'action:navigation/paginate',
+        extract: 'action:product/search/extract',
     },
-    {
-      name: 'store',
-      description: 'store name',
+    path: './search/stores/${store[0:1]}/${store}/${country}/search',
+    implementation: async(inputs, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
+        const { keywords, Keywords, results = 150, Brands, url, URL } = inputs;
+
+        const inputKeywords = Keywords || keywords || Brands;
+
+        // TODO: consider moving this to a reusable function
+
+        const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
+
+        const resultsReturned = await execute({
+            keywords: inputKeywords,
+            zipcode: inputs.zipcode || zipcode,
+            url: inputs.url || inputs.URL,
+        });
+
+        // do the search
+
+        if (!resultsReturned) {
+            console.log('No results were returned');
+            return;
+        }
+
+        // try gettings some search results
+        const pageOne = await extract({});
+
+        let collected = length(pageOne);
+
+        console.log('Got initial number of results', collected);
+
+        // check we have some data
+        if (collected === 0) return;
+
+        let page = 2;
+        while (collected < results && await paginate({ keywords: inputKeywords, page, offset: collected })) {
+            const data = await extract({});
+            const count = length(data);
+            if (count === 0) break; // no results
+            collected += count;
+            console.log('Got more results', collected);
+            page++;
+        }
     },
-    {
-      name: 'domain',
-      description: 'The top private domain of the website (e.g. amazon.com)',
-    },
-    {
-      name: 'zipcode',
-      description: 'to set location',
-      optional: true,
-    },
-  ],
-  inputs: [
-    {
-      name: 'keywords',
-      description: 'keywords to search for',
-      type: 'string',
-    },
-    {
-      name: 'Keywords',
-      description: 'keywords to search for',
-      type: 'string',
-    },
-    {
-      name: 'Brands',
-      description: 'brands to search for',
-      type: 'string',
-    },
-    {
-      name: 'results',
-      description: 'the minimum number of results required',
-      type: 'number',
-    },
-    {
-      name: 'url',
-      description: 'input category url',
-      type: 'string',
-    },
-  ],
-  dependencies: {
-    execute: 'action:product/search/execute',
-    paginate: 'action:navigation/paginate',
-    extract: 'action:product/search/extract',
-  },
-  path: './search/stores/${store[0:1]}/${store}/${country}/search',
-  implementation: async ({ url, URL, keywords, Keywords, Brands, results = 150 }, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
-    // TODO: consider moving this to a reusable function
-    const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
-
-    url = (url) || (URL);
-    keywords = (Keywords) || (Brands) || (keywords);
-    console.log('zip:' + zipcode);
-
-    const resultsReturned = await execute({ keywords, url, zipcode });
-  // implementation: async (inputs, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
-  //   const { keywords, Keywords, results = 150, Brands } = inputs;
-
-  //   const inputKeywords = Keywords || keywords || Brands;
-
-  //   // TODO: consider moving this to a reusable function
-  //   const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
-
-  //   const resultsReturned = await execute({
-  //     keywords: inputKeywords,
-  //     zipcode: inputs.zipcode || zipcode,
-  //   });
-
-    // do the search
-
-    if (!resultsReturned) {
-      console.log('No results were returned');
-      return;
-    }
-
-    // try gettings some search results
-    const pageOne = await extract({});
-
-    let collected = length(pageOne);
-
-    console.log('Got initial number of results', collected);
-
-    // check we have some data
-    if (collected === 0) return;
-
-    let page = 2;
-    while (collected < results && await paginate({ keywords: inputKeywords, page, offset: collected })) {
-      const data = await extract({});
-      const count = length(data);
-      if (count === 0) break; // no results
-      collected += count;
-      console.log('Got more results', collected);
-      page++;
-    }
-  },
 };
