7992f30065677b7caf4baab16f31e42ccd898d3d
sivasoarlogic <siva@soarlogic.com>
2020-11-27 17:09:04 +0530

    coreupdate

---
+++ src/library/product/details/stores/p/parfumswinkel/NL/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'parfumswinkel.nl',
+    prefix: null,
+    url: null,
+    country: 'NL',
+    store: 'parfumswinkel',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/details/stores/p/parfumswinkel/NL/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'NL',
+    store: 'parfumswinkel',
+    transform: null,
+    domain: 'parfumswinkel.nl',
+    zipcode: "''",
+  },
+};
