eb202631bcf1469f21f85adc8090bd1429b01ace
rajeshProfecialinks <rajesh.g@profecialinks.com>
2020-11-27 19:30:26 +0530

    voussert core feedtype done

--- src/library/product/details/stores/v/voussert/FR/extract.js
+++ src/library/product/details/stores/v/voussert/FR/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../format')
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'FR',
     store: 'voussert',
-    transform: null,
+    transform,
     domain: 'voussert.fr',
     zipcode: '',
   },
