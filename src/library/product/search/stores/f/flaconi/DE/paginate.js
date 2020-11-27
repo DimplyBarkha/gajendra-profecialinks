81d821f36e48d06dfdd6b3023a2bba951eca0d65
venkatsn <venkat.sn-odc@partner.import.io>
2020-11-27 14:47:08 +0530

    deepak flaconi

--- src/library/product/search/stores/f/flaconi/DE/paginate.js
+++ src/library/product/search/stores/f/flaconi/DE/paginate.js
@@ -4,18 +4,18 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'flaconi',
-    nextLinkSelector: null,
+    nextLinkSelector: 'body > div.wrapper.off-canvas-wrap > div.inner-wrap.row-collapse > div > div.content > div > div > div.toolbar.row > div > div > div:nth-child(3) > a',
     nextLinkXpath: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'div.canvas-menu-wrapper',
+    loadedSelector: 'body',
     loadedXpath: null,
     noResultsXPath: null,
     stopConditionSelectorOrXpath: null,
     resultsDivSelector: null,
-    openSearchDefinition: {
-      template: 'https://www.flaconi.de/search/?q={searchTerms}&page={page}',
-      },
+    // openSearchDefinition: {
+    //   template: 'https://www.flaconi.de/search/?q={searchTerms}&page={page}',
+    //   },
     domain: 'flaconi.de',
     zipcode: '',
   },
