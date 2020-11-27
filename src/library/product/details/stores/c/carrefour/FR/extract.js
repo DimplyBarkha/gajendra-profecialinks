708971b46f5a2fed39e442dae00408e005509859
Priyanka <priyanka.l@osmosys.asia>
2020-11-27 22:00:37 +0530

    Fixed issue with carrefour.fr

--- src/library/product/details/stores/c/carrefour/FR/extract.js
+++ src/library/product/details/stores/c/carrefour/FR/extract.js
@@ -7,5 +7,21 @@ module.exports = {
     transform: null,
     domain: 'carrefour.fr',
     zipcode: '',
+    implementation: async (inputs,
+      parameters,
+      context,
+      dependencies,
+    ) => {
+      await context.evaluate(async function () {
+      // @ts-ignore
+      const productData = window.ONECF_INITIAL_STATE;
+      const purchasability = productData.search.data.attributes.availability.purchasable;
+      const availability = purchasability ? 'In stock' : 'Out of stock';
+      document.querySelector('body').setAttribute('import-purchasability', purchasability);
+      document.querySelector('body').setAttribute('import-availability', availability);
+      const { transform } = parameters;
+      const { productDetails } = dependencies;
+      return await context.extract(productDetails, { transform });
+    }
   },
 };
