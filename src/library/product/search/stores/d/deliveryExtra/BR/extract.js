60e9e10250319e50bf599394db74b5de37b3b4f5
nikita.kumari <nikita.kumari@synerzip.com>
2020-11-27 16:04:12 +0530

    cache issue in search

--- src/library/product/search/stores/d/deliveryExtra/BR/extract.js
+++ src/library/product/search/stores/d/deliveryExtra/BR/extract.js
@@ -1,4 +1,25 @@
 const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(() => {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const url = window.location.href;
+    addHiddenDiv('added-searchurl', url);
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +29,5 @@ module.exports = {
     domain: 'clubeextra.com.br',
     zipcode: '',
   },
+  implementation,
 };
