ef550de723676a65fbe0067cd21be31363f82f0a
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-11-27 10:34:56 +0530

    #1757. Core Extractor for source deliveryExtra.

---
+++ src/library/product/details/stores/d/deliveryextra/BR/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'clubeextra.com.br',
+    prefix: null,
+    url: null,
+    country: 'BR',
+    store: 'deliveryextra',
+    zipcode: '',
+  },
+};
