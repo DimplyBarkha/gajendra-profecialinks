0b8f9036b8ef9bb37b64b5de37248d51e59af417
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-11-27 16:13:30 +0530

    kruidvat-Damayanthi

--- src/library/navigation/goto/domains/kr/kruidvat.nl.js
+++ src/library/navigation/goto/domains/kr/kruidvat.nl.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'kruidvat.nl',
-    timeout: 100000,
+    timeout: 1000000,
     country: 'NL',
     store: 'kruidvat',
     zipcode: '',
--- src/library/product/search/stores/k/kruidvat/NL/paginate.js
+++ src/library/product/search/stores/k/kruidvat/NL/paginate.js
@@ -4,14 +4,15 @@ module.exports = {
   parameterValues: {
     country: 'NL',
     store: 'kruidvat',
-    nextLinkXpath: '(//a[@class="pager__link pager__link--next"]/v-slot/v-slot-assigned-content)[1]',
-    // nextLinkSelector: 'div.pager.plp-paginator__pager.pager--mobile-bottom > div > e2-plp-page-selectors[css-modifier="pager__link pager__link--next"]',
+    nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'body',
     noResultsXPath: null,
-    openSearchDefinition: null,
     domain: 'kruidvat.nl',
-    zipcode: '',
+    // openSearchDefinition: {
+      // page : '1',
+      // template: 'https://www.kruidvat.nl/search?q={aftersun}&searchType=manual&page={page}&size=20',
+    // },
   },
 };
