0844472f1dba9c373bfa937c69c914f5076f9639
Nilesh <nilesh@madasolns.com>
2020-11-27 19:24:28 +0530

    changes for core extractor

---
+++ src/library/product/details/stores/f/falabella/AR/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'falabella.com.ar',
+    prefix: null,
+    url: 'https://www.falabella.com.ar/falabella-ar/product/{id}',
+    country: 'AR',
+    store: 'falabella',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/f/falabella/AR/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'AR',
+    store: 'falabella',
+    domain: 'falabella.com.ar',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
