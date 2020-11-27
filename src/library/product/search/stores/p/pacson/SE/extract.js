e4a2559457ae2fc99416bfbdf29edaf5febe015e
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-27 18:14:45 +0530

    kick back rank - akshay channa

--- src/library/product/search/stores/p/pacson/SE/extract.js
+++ src/library/product/search/stores/p/pacson/SE/extract.js
@@ -1,10 +1,10 @@
-const { cleanUp } = require('../../../../shared');
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'SE',
     store: 'pacson',
-    transform: cleanUp,
+    transform: transform,
     domain: 'pacson.se',
     zipcode: '',
   },
