200f7de6af0c55a6318ca9b49662a37fcfb0a932
Nilesh <nilesh@madasolns.com>
2020-11-27 19:54:01 +0530

    changes for search extractor

---
+++ src/library/product/search/stores/f/falabella/AR/transform.js
@@ -0,0 +1,68 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data, context) => {
+  let nameData = "";
+  let brandData = "";
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.name) {
+        row.name.forEach(item => {
+          nameData = item.text;
+        });
+      }
+      if (row.brandText) {
+        row.brandText.forEach(item => {
+          brandData = item.text;
+        });
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace("(Oferta)", '').trim();
+          item.text = item.text.replace(/(\s*\(Precio\s+final\)\s*)+/g, '').trim();
+        });
+      }
+      row.name = [{ text: brandData + ' ' + nameData }];
+    }
+  }
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  for (const { group } of data) {
+    for (const row of group) {
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
+    }
+  }
+
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+  return data;
+};
+
+module.exports = { transform };
