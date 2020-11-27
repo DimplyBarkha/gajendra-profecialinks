0a0994914c1d0ef54e752cb6d8353628e25b7469
ganesh <ganesh@profecialinks.com>
2020-11-27 15:25:21 +0530

    deploy

---
+++ src/library/product/details/stores/p/petbarn/AU/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'AU',
+    store: 'petbarn',
+    domain: 'petbarn.com.au',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/p/petbarn/AU/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'AU',
+    store: 'petbarn',
+    transform: null,
+    domain: 'petbarn.com.au',
+    zipcode: '',
+  },
+};
