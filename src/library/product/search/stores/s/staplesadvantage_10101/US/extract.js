a4f33df48ea210621febee311bc18bb4222fa0bc
Julia Lojek <julialojek@gmail.com>
2020-11-27 14:50:17 +0100

    #729 pagination problem fixed

--- src/library/product/search/stores/s/staplesadvantage_10101/US/extract.js
+++ src/library/product/search/stores/s/staplesadvantage_10101/US/extract.js
@@ -4,33 +4,12 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  // removing iframe popup on every page of search results
-  // the popup is visible after a moment -> delaying the removal
-  await new Promise(resolve => setTimeout(resolve, 3000));
-  // await context.waitForSelector('div.truste_box_overlay', { timeout: 20000 });
-  const isPopupPresent = await context.evaluate(async () => {
-    return document.querySelector('div.truste_box_overlay');
-  });
-  if (isPopupPresent !== null) {
-    await context.evaluate(() => {
-      document.querySelector('div.truste_box_overlay').remove();
-      document.querySelector('div.truste_overlay').remove();
-    });
-  }
-
   await context.evaluate(() => {
     const searchUrl = window.location.href;
     document.querySelector('body').setAttribute('searchurl', searchUrl);
   });
 
-  var dataRef = await context.extract(productDetails, { transform });
-  // dataRef[0].group.forEach((row) => {
-  //   const rating = row.aggregateRating2;
-  //   if (rating) {
-  //     rating[0].text = rating[0].text.replace('.', ',');
-  //   }
-  // });
-  return dataRef;
+  return await context.extract(productDetails, { transform });
 }
 
 module.exports = {
