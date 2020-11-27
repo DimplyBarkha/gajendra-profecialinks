2ab1cadd4defd726946ee419f8b5c561c0f71e38
ganesh <ganesh@profecialinks.com>
2020-11-27 10:45:48 +0530

    deploy

--- src/library/product/details/stores/p/pampik/UA/extract.js
+++ src/library/product/details/stores/p/pampik/UA/extract.js
@@ -1,10 +1,10 @@
-
+const {transform} = require('./format')
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'UA',
     store: 'pampik',
-    transform: null,
+    transform,
     domain: 'pampik.com',
     zipcode: '',
   },
