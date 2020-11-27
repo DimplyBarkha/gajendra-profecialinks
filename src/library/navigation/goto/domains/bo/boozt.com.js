04c33b036c3e9cde1e3b94e5aa8ed0546187475b
Lech Kulesza <lmkulesz@icloud.com>
2020-11-27 13:41:47 +0100

    #1151 selectors fix

--- src/library/navigation/goto/domains/bo/boozt.com.js
+++ src/library/navigation/goto/domains/bo/boozt.com.js
@@ -1,3 +1,6 @@
+// async function implementation({ url, zipcode, storeId }, parameters, context, dependencies) {
+//   await context.goto(url, { timeout: 60000, waitUntil: "load", checkBlocked: true });
+// }
 
 module.exports = {
   implements: 'navigation/goto',
@@ -8,4 +11,5 @@ module.exports = {
     store: 'boozt',
     zipcode: '',
   },
+  // implementation,
 };
