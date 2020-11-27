04ff6b3d664697b2bfdc21a740f6cb80fe9acb08
Wayne E <bartekstafiej2@gmail.com>
2020-11-27 14:13:21 +0100

    #1498 Added majority of xpaths, added variants clicking funciton

--- src/library/product/details/stores/v/vikingdirect/FR/extract.js
+++ src/library/product/details/stores/v/vikingdirect/FR/extract.js
@@ -1,11 +1,40 @@
+const { cleanUp } = require('../../../../shared');
+
+async function implementation (inputs, parameters, context, dependencies) {
+  const { productDetails } = dependencies;
+  const { transform } = cleanUp;
+
+  const variantsArray = await context.evaluate(async () => {
+    const variantsSelector = document.querySelectorAll('.multiSkuDimensionValues>div');
+    const variants = variantsSelector;
+    return variants;
+  });
+
+  if (variantsArray !== null) {
+    for (let i = 0; i < variantsArray.length; i++) {
+      if (variantsArray[i + 1] === null) {
+        return await context.extract(productDetails, { transform });
+      }
+      await context.evaluate(() => {
+        variantsArray[i].click();
+      });
+      await context.extract(productDetails, { transform });
+
+      // wait for extraction
+      await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    };
+
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'FR',
     store: 'vikingdirect',
-    transform: null,
+    transform: cleanUp,
     domain: 'vikingdirect.fr',
     zipcode: '',
   },
+  implementation,
 };
