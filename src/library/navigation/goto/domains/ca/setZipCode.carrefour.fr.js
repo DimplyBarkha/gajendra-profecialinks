d4ab8c1e83603a6c87be2dbae392d32dbca4f9a6
Priyanka <priyanka.l@osmosys.asia>
2020-11-27 16:33:25 +0530

    Adding changes for Carrefour FR

---
+++ src/library/navigation/goto/domains/ca/setZipCode.carrefour.fr.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'FR',
+    domain: 'carrefour.fr',
+    store: 'carrefour',
+    zipcode: '',
+  },
+};
