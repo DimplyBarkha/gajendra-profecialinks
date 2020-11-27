3e8d7817e5027d637bb94daa8633a732d044e960
IpsitaM <iamipsita07@gmail.com>
2020-11-27 15:51:16 +0530

    Fixed lint issues

--- src/library/product/search/stores/m/metro/CA/paginate.js
+++ src/library/product/search/stores/m/metro/CA/paginate.js
@@ -1,17 +1,17 @@
 module.exports = {
-    implements: 'product/search/paginate',
-    parameterValues: {
-        country: 'CA',
-        store: 'metro',
-        nextLinkSelector: null,
-        mutationSelector: null,
-        spinnerSelector: null,
-        loadedSelector: '.tile-product__top-section__visuals__stickers',
-        noResultsXPath: '//div[@class="product-not-found"]',
-        openSearchDefinition: {
-            template: 'https://www.metro.ca/en/online-grocery/search-page-{page}?&filter={searchTerms}'
-        },
-        domain: 'metro.ca',
-        zipcode: '',
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'CA',
+    store: 'metro',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: '.tile-product__top-section__visuals__stickers',
+    noResultsXPath: '//div[@class="product-not-found"]',
+    openSearchDefinition: {
+      template: 'https://www.metro.ca/en/online-grocery/search-page-{page}?&filter={searchTerms}',
     },
+    domain: 'metro.ca',
+    zipcode: '',
+  },
 };
