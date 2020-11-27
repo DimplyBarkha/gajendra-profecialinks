b05a6442bd057730d7e186e3e103534977915105
harishkhattri <harish.khattri@synerzip.com>
2020-11-27 18:49:14 +0530

    Added pagination for the search extractor of casinodrive FR

--- src/library/navigation/goto/domains/ca/casinodrive.fr.js
+++ src/library/navigation/goto/domains/ca/casinodrive.fr.js
@@ -3,25 +3,21 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'casinodrive.fr',
-    timeout: 50000,
+    timeout: 60000,
     country: 'FR',
     store: 'casinodrive',
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    await context.goto('https://www.casino.fr/prehome/courses-en-ligne/carte-des-magasins/Bourg-en-Bresse%20(01000)', {
-      timeout: 40000,
-      waitUntil: 'load',
-      checkBlocked: true,
-      js_enabled: true,
-      css_enabled: false,
-      random_move_mouse: true,
-    });
-    await context.waitForSelector('div[class*="map-box__block-store"]:first-of-type a[class*="block-store__main-link"]');
-    await context.click('div[class*="map-box__block-store"]:first-of-type a[class*="block-store__main-link"]');
-    await context.stop();
+    // Navigate to choose store
+    await context.goto('https://www.casino.fr/prehome/courses-en-ligne/carte-des-magasins/Bourg-en-Bresse%20(01000)');
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await context.waitForSelector('div[class*="map-box__block-store"]:first-of-type a[class*="block-store__main-link"]', { timeout: 60000 });
+    await context.clickAndWaitForNavigation('div[class*="map-box__block-store"]:first-of-type a[class*="block-store__main-link"]');
+    await new Promise((resolve) => setTimeout(resolve, 10000));
 
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    // Navigate to search URL
+    const timeout = parameters.timeout ? parameters.timeout : 60000;
     await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
--- src/library/product/search/stores/c/casinodrive/FR/extract.js
+++ src/library/product/search/stores/c/casinodrive/FR/extract.js
@@ -10,6 +10,7 @@ async function implementation (
   const { productDetails } = dependencies;
 
   await context.evaluate(async function (results) {
+    await new Promise(resolve => setTimeout(resolve, 5000));
     const totalResultsNode = document.querySelector('span[class="header-article-title"]');
     let totalResults = 0;
     if (totalResultsNode) {
@@ -17,14 +18,14 @@ async function implementation (
       totalResults = parseInt(totalResultsStr, 10);
     }
     let productCards = document.querySelectorAll('div[class="lazyload"] ul[class*="prodlist"] li');
-    const length = (totalResults > results) ? results : totalResults;
     const footer = document.querySelector('div[class="footerV2"]');
+    const length = (totalResults > results) ? results : totalResults;
     while (productCards.length < length) {
       footer.scrollIntoView({ behavior: 'smooth' });
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await new Promise(resolve => setTimeout(resolve, 10000));
       productCards = document.querySelectorAll('div[class="lazyload"] ul[class*="prodlist"] li');
     }
-    productCards[length - 1].scrollIntoView({ behavior: 'smooth' });
+    productCards[0].scrollIntoView({ behavior: 'smooth' });
   }, results);
 
   return await context.extract(productDetails, { transform });
