884c75c5efe8370876da4793814609fa8d2af765
rajeshProfecialinks <rajesh.g@profecialinks.com>
2020-11-27 07:23:25 +0530

    wbmason core initial inprogress

---
+++ src/library/product/details/stores/w/wbmason/US/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'wbmason.com',
+    prefix: null,
+    url: null,
+    country: 'US',
+    store: 'wbmason',
+    zipcode: '',
+  },
+};
