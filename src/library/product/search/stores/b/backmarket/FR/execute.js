439365ab7212bf8f31902f85f393bacac7d3fb74
Wayne E <bartekstafiej2@gmail.com>
2020-11-28 13:00:40 +0100

    field extraxt.yaml with all available xpaths

--- src/library/product/search/stores/b/backmarket/FR/execute.js
+++ src/library/product/search/stores/b/backmarket/FR/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'FR',
     store: 'backmarket',
     domain: 'backmarket.fr',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.backmarket.fr/search?q={searchTerms}',
+    loadedSelector: 'div[id="main_container"]',
+    noResultsXPath: '//div[@data-test="search-landing-no-result"]',
     zipcode: "''",
   },
 };
