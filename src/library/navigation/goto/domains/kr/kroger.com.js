a184d72f72be2510f7fc4cdce61bacc8903cf8f9
vikas kyatannawar <vikas.k@osmosys.asia>
2020-11-29 00:49:05 +0530

    updated setzipcode for kroger

--- src/library/navigation/goto/domains/kr/kroger.com.js
+++ src/library/navigation/goto/domains/kr/kroger.com.js
@@ -5,16 +5,20 @@ module.exports = {
     domain: 'kroger.com',
     country: 'US',
     store: 'kroger',
+    timeout: 120000,
   },
-  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    await context.setBlockAds(false);
-    console.log('Block ads set to false for context');
-    const optUrl = `${url}#[!opt!]{"anti_fingerprint":false, "first_request_timeout": 60, "proxy":{"use_relay_proxy": false}}[/!opt!]`;
-    console.log(`going to optUrt, which is - ${optUrl}`);
-    await context.goto(optUrl, { timeout: 45000, waitUntil: 'load', checkBlocked: true });
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 30000;
+    await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
-      await dependencies.setZipCode({ url: optUrl, zipcode });
+      await dependencies.setZipCode({ url, zipcode });
+    }
+    // load details page
+    const productPage = await context.evaluate(() => !!document.querySelector('div.ProductDetails'));
+    if (productPage) {
+      await context.waitForXPath("//li[@itemprop='review']", { timeout: 10000 })
+        .catch(() => console.log('waited for reviews to load, none found '));
     }
   },
 };
