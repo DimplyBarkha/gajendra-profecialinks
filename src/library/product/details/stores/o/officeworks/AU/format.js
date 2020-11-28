63879c7f71b53329c4fd225df2f4ca6bcfaee74b
Gajender Ramola <ramola106@gmail.com>
2020-11-29 02:51:55 +0530

    #1690 brand issue resolved

--- src/library/product/details/stores/o/officeworks/AU/format.js
+++ src/library/product/details/stores/o/officeworks/AU/format.js
@@ -61,34 +61,6 @@ const transform = (data) => {
       if (row.variants) {
         row.variantCount = [{ text: row.variants.length }];
       }
-      // if (row.price) {
-      //   row.price.forEach(item => {
-      //     item.text = item.text.replace(',', '');
-      //   });
-      // }
-      // if (row.listPrice) {
-      //   row.listPrice.forEach(item => {
-      //     item.text = item.text.replace(',', '');
-      //   });
-      // }
-      // if (row.variantCount) {
-      //   row.variantCount = [{ text: row.variantCount.length }];
-      // }
-      // if (row.variants) {
-      //   var scriptJSON = JSON.parse(row.variants[0].text);
-      //   if (scriptJSON.productVariants) {
-      //     var objectsInVariants = Object.keys(scriptJSON.productVariants).length;
-      //     var varientIds = [];
-      //     for (var i = 0; i < objectsInVariants; i++) {
-      //       var keyName = Object.keys(scriptJSON.productVariants)[i];
-      //       var variants = scriptJSON.productVariants[keyName].variants;
-      //       variants.forEach(function(item, index) {
-      //         varientIds.push(item.fupid);
-      //       });
-      //     }
-      //   }
-      //   row.variants = [{ text: varientIds.join(' | ') }];
-      // }
       if (row.additionalDescBulletInfo) {
         var arrBullets = [];
         row.additionalDescBulletInfo.forEach(item => {
@@ -97,11 +69,13 @@ const transform = (data) => {
         row.additionalDescBulletInfo = [{ text: '|| ' + arrBullets.join(' || ') }];
         row.descriptionBullets = [{ text: arrBullets.length }];
       }
-      // if (row.aggregateRating) {
-      //   row.aggregateRating.forEach(item => {
-      //     item.text = (item.text * 5) / 10;
-      //   });
-      // }
+      if (row.brandLink) {
+        row.brandLink.forEach(item => {
+          if (item.text.indexOf('officeworks.com.au') < 0) {
+            item.text = 'https://www.officeworks.com.au' + item.text;
+          }
+        });
+      }
     }
   }
   return cleanUp(data);
