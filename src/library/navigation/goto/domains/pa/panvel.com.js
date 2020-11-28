572231cddfb9f1f1063ef9269ac4ef73646c41d0
adheerProfecia <peter@profecialinks.com>
2020-11-28 14:52:32 +0530

    #2277 #2778

---
+++ src/library/navigation/goto/domains/pa/panvel.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'panvel.com',
+    timeout: null,
+    country: 'BR',
+    store: 'panvel',
+    zipcode: '',
+  },
+};
