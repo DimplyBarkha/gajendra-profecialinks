1e9fcfbecb1c0563ee26f4e01d0113bdbe9aaa35
Priyanka <priyanka.l@osmosys.asia>
2020-11-28 14:10:47 +0530

    Fixed issue with carrefour.it

--- src/library/product/details/stores/c/carrefour/IT/extract.js
+++ src/library/product/details/stores/c/carrefour/IT/extract.js
@@ -8,14 +8,30 @@ module.exports = {
     domain: 'carrefour.it',
     zipcode: '',
   },
-  implementation: async (inputs,
+  implementation: async (
+    inputs,
     parameters,
     context,
     dependencies,
   ) => {
     await context.evaluate(async function () {
+      document.querySelector('body').setAttribute('import-enhanced-content', 'false');
+      let jsonData = document.querySelector('[type="application/ld+json"]');
 
+      if (jsonData) {
+        jsonData = JSON.parse(jsonData.textContent);
+        let availability = jsonData.offers.availability;
+        availability = availability.includes('InStock') ? 'In stock' : 'Out of stock';
+        document.querySelector('body').setAttribute('import-purchasability', availability);
+        let images = jsonData.image;
 
+        for (let image of images) {
+          image = image.split('?')[0].replace('.jpg', '.png');
+          let imgEl = document.createElement('import-image');
+          imgEl.setAttribute('data', image);
+          document.querySelector('body').appendChild(imgEl);
+        }
+      }
     });
     const { transform } = parameters;
     const { productDetails } = dependencies;
