0e57f30654c10da7a5e0e17a17f7dff0499c4c8f
osmarghyadeep <arghyadep.g@partner.import.io>
2020-11-29 00:33:56 +0530

    Updated the noresults-xpath

--- src/library/product/search/stores/p/payngo/IL/execute.js
+++ src/library/product/search/stores/p/payngo/IL/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'payngo.co.il',
     url: 'https://www.payngo.co.il/instantsearchplus/result?q={searchTerms}',
     loadedSelector: 'div#isp_center_container > ul#isp_search_results_container > li',
-    noResultsXPath: "//li[contains(string(), 'no results found')] | //h2[contains(string(), 'שגיאה 404')]",
+    noResultsXPath: "//li[contains(string(), 'no results found')] | //h2[contains(string(), 'שגיאה 404')] | //div[contains(@class,'results_summary_wrapper')]//*[contains(@id,'header_subtitle')][contains(.,'0  תוצאות')]|//div[contains(@class,'results_summary_wrapper')]//*[contains(@id,'header_subtitle')][contains(.,'0 results')]",
     zipcode: '',
   },
 };
