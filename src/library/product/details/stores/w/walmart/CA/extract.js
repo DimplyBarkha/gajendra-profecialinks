1a2d738fc9ac0bd2d1212a2e2e7c2c0e5633a287
rajeshProfecialinks <rajesh.g@profecialinks.com>
2020-11-28 08:23:44 +0530

    walmarttogo core feedtype inprogress

---
+++ src/library/product/details/stores/w/walmart/CA/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'CA',
+    store: 'walmart',
+    transform: null,
+    domain: 'walmart.ca',
+    zipcode: '',
+  },
+};
