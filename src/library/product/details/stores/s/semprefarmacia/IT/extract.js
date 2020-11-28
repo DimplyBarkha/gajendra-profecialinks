e611e89a61a7b17d659f9b66e2337c643acf3720
rajeshProfecialinks <rajesh.g@profecialinks.com>
2020-11-28 16:14:50 +0530

    semprefarmacia core feedtype inprogress

---
+++ src/library/product/details/stores/s/semprefarmacia/IT/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'IT',
+    store: 'semprefarmacia',
+    transform: null,
+    domain: 'semprefarmacia.it',
+    zipcode: '',
+  },
+};
