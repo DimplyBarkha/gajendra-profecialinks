7790092829b99ce1b10ed82fe0c3846a42153e8b
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-29 01:19:24 +0530

    #1862 developing

--- src/library/product/search/stores/b/bidfood/CZ/execute.js
+++ src/library/product/search/stores/b/bidfood/CZ/execute.js
@@ -16,25 +16,23 @@ async function implementation (
   console.log('params', parameters);
   const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
   await dependencies.goto({ url, zipcode: inputs.zipcode });
-  await context.waitForSelector('input#ProductsSearchControl_SearchTextBoxProd');
-  await context.setInputValue('input#ProductsSearchControl_SearchTextBoxProd',inputs.keywords);
-  await context.waitForSelector('input#ProductsSearchControl_SearchButton[type="image"]');
-  await context.click('input#ProductsSearchControl_SearchButton[type="image"]');
-  await context.waitForNavigation();
-  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-  for(let i=0;i<10;i++){
-    try{
-      await context.click('button#nextItemsBtn');
-      await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-    }catch(err){
-      console.log(err.message)
-    }
+  try{
+    await context.waitForSelector('input#ProductsSearchControl_SearchTextBoxProd');
+    await context.setInputValue('input#ProductsSearchControl_SearchTextBoxProd',inputs.keywords);
+    await context.waitForSelector('input#ProductsSearchControl_SearchButton[type="image"]');
+    await context.click('input#ProductsSearchControl_SearchButton[type="image"]');
+    await context.waitForNavigation();
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+  }catch(e){
+
   }
+  
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
     }, { timeout: 100000 }, parameters.loadedSelector, parameters.noResultsXPath);
   }
+  await new Promise((resolve, reject) => setTimeout(resolve, 9000));
   console.log('Checking no results', parameters.noResultsXPath);
   return await context.evaluate(function (xp) {
     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
