c963d7a701401152213b5377a41d264beb7e71c1
vijay <vijay.lnsel@gmail.com>
2020-11-28 22:14:29 +0530

    #2063 Developing

--- src/library/product/details/stores/b/bunnings/AU/extract.js
+++ src/library/product/details/stores/b/bunnings/AU/extract.js
@@ -1,10 +1,10 @@
-
+const {transform} = require('../AU/format')
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AU',
     store: 'bunnings',
-    transform: null,
+    transform,
     domain: 'bunnings.com.au',
     zipcode: '',
   },
