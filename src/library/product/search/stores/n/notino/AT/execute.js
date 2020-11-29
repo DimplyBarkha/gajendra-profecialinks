d97636ca8ccd9a1d4731c5c3557e9ff70b83f1d2
ganesh <ganesh@profecialinks.com>
2020-11-29 08:03:29 +0530

    add url

--- src/library/product/search/stores/n/notino/AT/execute.js
+++ src/library/product/search/stores/n/notino/AT/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'notino',
     domain: 'notino.at',
     url: 'https://www.notino.at/search.asp?exps={searchTerms}',
-    loadedSelector: "ul[id='productsList'] li[class='item'] span[class='img'] img",
+    loadedSelector: "ul[id='productsList'] li[class='item']",
     noResultsXPath: "//div[@class='collapsable search-results']//p[contains(text(),'Dem gesuchten Begriff')]",
     zipcode: '',
   },
