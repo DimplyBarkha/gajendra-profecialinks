6fdf5542306c3a843641881fb7417edb6ffb7f14
Taras Chuiko <taraschuiko@gmail.com>
2020-11-29 19:14:22 +0200

    Fixed product url

--- src/library/product/search/stores/m/mediamarkt/NL/extract.js
+++ src/library/product/search/stores/m/mediamarkt/NL/extract.js
@@ -36,6 +36,10 @@ module.exports = {
         // Gets thumbnail
         const thumbnail = `https:${product[i].querySelector('a.photo img').attributes.src.value}`;
         addHiddenDiv('mm_thumbnail', thumbnail, i);
+
+        // Get URL
+        const url = 'https://www.mediamarkt.nl' + product[i].querySelector('.product-wrapper h2 a').attributes.href.value;
+        addHiddenDiv('mm_productUrl', url, i);
       }
     });
     return await context.extract(productDetails, { transform });
