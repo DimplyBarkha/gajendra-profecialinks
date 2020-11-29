d40bfa1f6af990b21382b61bed3b5065fdbe14cd
himanshu.a <himanshu.a@osmosys.asia>
2020-11-29 12:10:56 +0530

    Handle website maintenence
    Added xpath for website mentenance in noResultXpath to avoid picking defaut valuse for every column

--- src/library/product/details/stores/e/eprice/IT/execute.js
+++ src/library/product/details/stores/e/eprice/IT/execute.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -6,7 +5,7 @@ module.exports = {
     store: 'eprice',
     domain: 'eprice.it',
     loadedSelector: null,
-    noResultsXPath: "//div[contains(@class, 'sezNegozi')]",
+    noResultsXPath: "//div[contains(@class, 'sezNegozi')] | //div[@id='txt']//*[contains(text(),'Saremo online a breve')]",
     zipcode: '',
   },
 };
