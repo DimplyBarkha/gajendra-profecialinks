82a315bc2a1f8cfdc1eb17e951af1cc57bd9d8ac
Aditi-ux <aditi@profecialinks.com>
2020-11-30 01:19:00 +0530

    Deployment

--- src/library/product/details/stores/u/ultrafarma/BR/extract.js
+++ src/library/product/details/stores/u/ultrafarma/BR/extract.js
@@ -1,5 +1,42 @@
 const { transform } = require('../format');
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  const mainUrl = await context.evaluate(async function () {
+    const button = document.querySelector('a.product-item-link');
+    const link = button.getAttribute('href');
+    return link;
+  });
+  await context.goto(mainUrl, { timeout: 1000000, waitUntil: 'load', checkBlocked: true });
+  await new Promise((resolve, reject) => setTimeout(resolve, 50000));
+  await context.evaluate(async function () {
+    let scrollTop = 0;
+    while (scrollTop <= 20000) {
+      await stall(7000);
+      scrollTop += 10000;
+      window.scroll(0, scrollTop);
+      if (scrollTop === 200000) {
+        await stall(80000);
+        break;
+      }
+    }
+    function stall (ms) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+  });
+  return await context.extract(productDetails, { transform });
+};
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -9,40 +46,5 @@ module.exports = {
     domain: 'ultrafarma.com.br',
     zipcode: "''",
   },
-  async implementation (
-    inputs,
-    parameters,
-    context,
-    dependencies,
-  ) {
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    const mainUrl = await context.evaluate(async function () {
-      const button = document.querySelector('a.product-item-link');
-      const link = button.getAttribute('href');
-      return link;
-    });
-    await context.goto(mainUrl, { timeout: 1000000, waitUntil: 'load', checkBlocked: true });
-    await new Promise((resolve, reject) => setTimeout(resolve, 50000));
-    await context.evaluate(async function () {
-      let scrollTop = 0;
-      while (scrollTop <= 20000) {
-        await stall(7000);
-        scrollTop += 10000;
-        window.scroll(0, scrollTop);
-        if (scrollTop === 200000) {
-          await stall(80000);
-          break;
-        }
-      }
-      function stall (ms) {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-    });
-    return await context.extract(productDetails, { transform });
-  },
+  implementation,
 };
