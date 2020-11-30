e61b674a9f2fea92190330f91929690848eb1d5d
sivasoarlogic <siva@soarlogic.com>
2020-11-30 11:42:25 +0530

    searchpart

---
+++ src/library/navigation/goto/domains/mi/misterspex.se.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'misterspex.se',
+    timeout: 100000,
+    country: 'SE',
+    store: 'misterspex',
+    zipcode: "''",
+  },
+};
