56daf4b0c60dd0692526576e5af7241fcc6760ba
dhanush <dhanush.isukapalli@od.import.io>
2020-11-30 19:17:08 +0530

    hondoscenter by kavya

--- src/library/product/search/stores/h/hondoscenter/GR/extract.js
+++ src/library/product/search/stores/h/hondoscenter/GR/extract.js
@@ -17,13 +17,26 @@ module.exports = {
         const { transform } = parameters;
         const { productDetails } = dependencies;
         const applyScroll = async function (context) {
-            
+
             await context.evaluate(async function () {
-                while(!!document.querySelector('div.scrolllist-pager-notification > div.button-container > span')){
+                while (!!document.querySelector('div.scrolllist-pager-notification > div.button-container > span')) {
+                    let scrollTop = 0;
+                    while (scrollTop !== 20000) {
+                        scrollTop += 1000;
+                        window.scroll(0, scrollTop);
+                        await stall(1000);
+                    }
+                    function stall(ms) {
+                        return new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                resolve();
+                            }, ms);
+                        });
+                    }
                     document.querySelector('div.scrolllist-pager-notification > div.button-container > span').click()
                     await new Promise(r => setTimeout(r, 6000));
-    }
-                
+                }
+
                 let scrollTop = 0;
                 while (scrollTop !== 20000) {
                     scrollTop += 1000;
