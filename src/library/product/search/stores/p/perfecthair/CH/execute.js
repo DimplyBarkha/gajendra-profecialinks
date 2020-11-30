61d5f55b7774e46d3ce0dc07d8321b2afee2c082
ganesh <ganesh@profecialinks.com>
2020-11-30 11:42:47 +0530

    change url format

--- src/library/product/search/stores/p/perfecthair/CH/execute.js
+++ src/library/product/search/stores/p/perfecthair/CH/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'CH',
     store: 'perfecthair',
     domain: 'perfecthair.ch',
-    url: "https://www.perfecthair.ch/en/search?sSearch={searchTerms}",
+    url: "https://www.perfecthair.ch/de/search?sSearch={searchTerms}",
     loadedSelector: 'div[class="listing--container"] div[class="listing"] div[class="product--box box--minimal"] span[class="image--media"] img',
     noResultsXPath: "//div[contains(text(),'No products matching your search')]",
     zipcode: '',
