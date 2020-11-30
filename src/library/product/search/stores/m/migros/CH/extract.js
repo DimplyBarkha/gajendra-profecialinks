5962d29128610ff5ecce7bbc801fb6360f0e07a6
harsha chirmade <harsha.chirmade@profeciallinks.com>
2020-11-30 17:59:05 +0530

    migros search updated

--- src/library/product/search/stores/m/migros/CH/extract.js
+++ src/library/product/search/stores/m/migros/CH/extract.js
@@ -1,4 +1,52 @@
-const {transform} = require('../format')
+const {transform} = require('../format');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+  // await context.evaluate(async function () {
+  //   await new Promise(resolve => setTimeout(resolve, 100));
+  //   for (let i = 0; i < 10; i++) {
+  //     document.getElementById("main").scrollBy({ top: 5000, left: 0, behaviour: 'smooth'});
+  //     // window.scrollBy({ top: 1000, left: 0, behavior: "smooth" });
+  //     await new Promise(resolve => setTimeout(resolve, 500));
+  //     console.log("scrolling");
+  //   }
+  // });
+  await context.evaluate(async () => {
+    
+    let scrolltop = document.getElementById("main").scrollTop;
+    
+    while(!!document.querySelector('div.btn-view-more-products > button')){
+      document.querySelector('div.btn-view-more-products > button').click();
+      await new Promise(r => setTimeout(r, 1000));
+    }
+    
+    document.getElementById("main").scrollBy({ top: 1000, left: 0, behavior: 'smooth'});
+    await new Promise(r => setTimeout(r, 1000));
+
+    let thmcount = document.querySelectorAll('ul.productGroup li.item img[src]').length;
+    let itemcount = document.querySelectorAll('ul.productGroup li.item').length;
+
+    while(itemcount!=thmcount){
+      let newscrolltop = document.getElementById("main").scrollTop;
+      document.getElementById("main").scrollBy({ top: newscrolltop-scrolltop, left: 0, behavior: 'smooth'});
+      scrolltop = newscrolltop;
+      await new Promise(r => setTimeout(r, 500));
+      let newthmcount = document.querySelectorAll('ul.productGroup li.item img[src]').length;
+      let newitemcount = document.querySelectorAll('ul.productGroup li.item').length;
+      if(newthmcount==newitemcount){
+        break;
+      }
+    }
+    
+  })
+  return await context.extract(productDetails);
+}
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +56,5 @@ module.exports = {
     domain: 'migros.ch',
     zipcode: '',
   },
+  implementation,
 };
