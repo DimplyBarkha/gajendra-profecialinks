ead19c3f412be31327d65530c4de76731c595816
sivasoarlogic <siva@soarlogic.com>
2020-11-30 14:41:33 +0530

    coreupdate

---
+++ src/library/product/details/stores/m/misterspex/SE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'SE',
+    store: 'misterspex',
+    domain: 'misterspex.se',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/details/stores/m/misterspex/SE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'SE',
+    store: 'misterspex',
+    transform: null,
+    domain: 'misterspex.se',
+    zipcode: "''",
+  },
+};
