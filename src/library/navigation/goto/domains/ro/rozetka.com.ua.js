efc90159935533f7f50e966eca0c9d92e3378763
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-11-30 22:20:00 +0530

    #1862 updating

--- src/library/navigation/goto/domains/ro/rozetka.com.ua.js
+++ src/library/navigation/goto/domains/ro/rozetka.com.ua.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'rozetka.com.ua',
-    timeout: null,
+    timeout: 50000,
     country: 'UA',
     store: 'rozetka',
     zipcode: '',
