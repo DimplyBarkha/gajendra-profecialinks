6617a3c14fb29e095259dc8acde883838fd370e5
sivasoarlogic <siva@soarlogic.com>
2020-11-30 18:06:16 +0530

    coreupdated

--- src/library/product/details/stores/m/misterspex/SE/format.js
+++ src/library/product/details/stores/m/misterspex/SE/format.js
@@ -29,6 +29,16 @@ const transform = (data) => {
             item.text =  Number(item.text);
           });
         }
+        if (row.image) {
+            row.image.forEach(item => {
+              item.text = "https:"+item.text;
+            });
+        }
+        if (row.alternateImages) {
+            row.alternateImages.forEach(item => {
+              item.text = "https:"+item.text;
+            });
+        }
         if (row.description) {
             let description_ar = [];
             row.description.forEach(item => {
@@ -42,13 +52,26 @@ const transform = (data) => {
             }
         }
         if (row.specifications) {
-          let specifications_ar = [];
-          row.description.forEach(item => {
-            specifications_ar.push(item.text);
-          });
-          if (specifications_ar.length) {
-            row.specifications = [{ "text": specifications_ar.join(" || "), 'xpath': row.specifications[0].xpath }];
-          }
+            var rowItem = ''
+            var rowCounter = 1
+            row.specifications.forEach(item => {
+              if((rowCounter % 2)){
+                rowItem = rowItem +  item.text 
+              } else{
+                rowItem = rowItem +  item.text + ' || '
+              }
+              rowCounter = rowCounter + 1
+            });
+            row.specifications = [{'text':rowItem, 'xpath': row.specifications[0].xpath}]
+            //console.log(row.specifications)
+        }
+        if (row.brandText) {
+            let info = [];
+            row.brandText.forEach(item => {
+              item.text = item.text.replace("{\"@type\":\"Thing\",\"name\":", "");
+              item.text = item.text.slice(1,-1);
+              info.push(item.text);
+            });
         }
         if (row.descriptionBullets) {
           row.descriptionBullets = [{'text':row.descriptionBullets.length, 'xpath':row.descriptionBullets[0].xpath}];              
