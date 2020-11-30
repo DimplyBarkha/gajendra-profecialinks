7f9dc08e42e0c5e9ef098da8191cc9be92e0ecff
Karolina <karolina.urbanska@tdcm.io>
2020-11-30 15:45:45 +0100

    Added variants logic to core extractor (#1575)

---
+++ src/library/product/details/stores/p/petstock/AU/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'AU',
+    store: 'petstock',
+    domain: 'petstock.com.au',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/p/petstock/AU/variantsExtract.js
@@ -0,0 +1,41 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { createUrl, variants } = dependencies;
+  await context.evaluate(function () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+      return newDiv;
+    }
+    const scriptHTML = document.evaluate('//script[contains(text(),"skus")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    const JSstring = scriptHTML && scriptHTML.textContent ? scriptHTML.textContent.split('var skus = ')[1].split(';')[0] : '';
+    const idArr = JSstring ? JSstring.match(/id:\s\d+/g) : '';
+    if (idArr.length !== 0) {
+      for (let i = 0; i < idArr.length; i++) {
+        const sku = idArr[i].replace(/[^\d+]/g, '');
+        const pagePrefixUrl = window.location.href.replace(/\d+$/g, '');
+        const variantUrl = pagePrefixUrl.concat(sku);
+        addHiddenDiv('variantUrl', variantUrl);
+      }
+    }
+  }, createUrl);
+  return await context.extract(variants);
+}
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'AU',
+    store: 'petstock',
+    transform: null,
+    domain: 'petstock.com.au',
+    zipcode: '',
+  },
+  implementation,
+};
