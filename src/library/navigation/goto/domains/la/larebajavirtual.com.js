f243d330923142392d879e85200f5f53ee6583c6
sobha rani <amol.thukrul-od@partner.import.io>
2020-11-30 22:24:34 +0530

    LarebajaVirtual complete search

--- src/library/navigation/goto/domains/la/larebajavirtual.com.js
+++ src/library/navigation/goto/domains/la/larebajavirtual.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'larebajavirtual.com',
-    timeout: 150000,
+    timeout: 90000,
     country: 'CO',
     store: 'larebajavirtual',
     zipcode: '',
