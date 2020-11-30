63887dfe282b4d4868ba979183c7db20c9b98c6d
Raghav <user.email>
2020-11-30 22:53:55 +0530

    updating search_url & recordXpath

--- src/library/product/search/stores/m/matas/DK/extract.js
+++ src/library/product/search/stores/m/matas/DK/extract.js
@@ -35,6 +35,14 @@ module.exports = {
         const originalDiv = document.querySelectorAll('div[class="productlist__products"]>div>div>div>div[class="product-item__price-container"]>div>span>span')[index];
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      // Method to Retrieve Xpath content of a Single Node
       var vari = getAllXpath("//div[@class='productlist__products']//div[@class='review-stars__container review-stars__container--border']/@data-js-border-offset","nodeValue");
       if (vari != null) {
         for (var i = 0; i < vari.length; i++) {
@@ -51,6 +59,12 @@ module.exports = {
         var priceUpdated = price.replace(",", ".");
         addHiddenDiv("price", priceUpdated, i);
       }
+      const URL = window.location.href;
+    try {
+      document.getElementById('pd_url').remove();
+    } catch (error) {
+    }
+    addElementToDocument('pd_url', URL);
     });
     return await context.extract(productDetails, { transform });
   },
