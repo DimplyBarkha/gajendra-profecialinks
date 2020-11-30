e063035e1f43632c750bbc9522a64cc0881359aa
mania307 <maniakalnaa@gmail.com>
2020-11-30 10:24:35 +0100

    lint fix

--- src/library/product/details/stores/b/boozt/DK/extract.js
+++ src/library/product/details/stores/b/boozt/DK/extract.js
@@ -8,33 +8,31 @@ module.exports = {
     transform: cleanUp,
     domain: 'boozt.com',
     zipcode: '',
-  }, implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
-
+  },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
     await context.extract(productDetails);
 
     const allInfo = await context.evaluate(async function () {
-
-      //taking all buttons with information about the product
+      // taking all buttons with information about the product
       const infoBtns = document.querySelectorAll('div.pp-tabs__list button');
 
       var allInfoRaw = {
-        'prodInfo': "",
-        'careTips': "",
-        'shippingInfo': ""
-      }
+        prodInfo: '',
+        careTips: '',
+        shippingInfo: '',
+      };
 
-      //iteration through all the buttons and click on them for loading the div with text
+      // iteration through all the buttons and click on them for loading the div with text
       for (let i = 0; i < infoBtns.length; i++) {
-        if (infoBtns[i].innerText === "BÆREDYGTIG FASHION" || infoBtns[i].innerText === "TØJPLEJE") {
+        if (infoBtns[i].innerText === 'BÆREDYGTIG FASHION' || infoBtns[i].innerText === 'TØJPLEJE') {
           infoBtns[i].click();
-          await new Promise((r) => setTimeout(r, 1000));
-          let careTips = document.querySelector("div.pp-tabs__content.pp-content").innerText;
+          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+          const careTips = document.querySelector('div.pp-tabs__content.pp-content').innerText;
           allInfoRaw.careTips = careTips;
-        }
-        else if (infoBtns[i].innerText === "LEVERING & RETURNERING") {
+        } else if (infoBtns[i].innerText === 'LEVERING & RETURNERING') {
           infoBtns[i].click();
-          await new Promise((r) => setTimeout(r, 1000));
-          let shippingInfo = document.querySelector("div.pp-tabs__content.pp-content > div.pp-content__section").innerText;
+          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+          const shippingInfo = document.querySelector('div.pp-tabs__content.pp-content > div.pp-content__section').innerText;
           allInfoRaw.shippingInfo = shippingInfo;
         }
       }
@@ -43,21 +41,20 @@ module.exports = {
 
     var dataRef = await context.data();
 
-    if (!("productOtherInformation" in dataRef[0].data[0].group[0])) {
+    if (!('productOtherInformation' in dataRef[0].data[0].group[0])) {
       dataRef[0].data[0].group[0].productOtherInformation = [{ text: allInfo.careTips }];
-    } else if ("text" in dataRef[0].data[0].group[0].productOtherInformation[0]) {
+    } else if ('text' in dataRef[0].data[0].group[0].productOtherInformation[0]) {
       dataRef[0].data[0].group[0].productOtherInformation[0].text = allInfo.careTips;
     } else {
       dataRef[0].data[0].group[0].productOtherInformation[0].push({ text: allInfo.careTips });
     }
 
-    if (!("shippingInfo" in dataRef[0].data[0].group[0])) {
+    if (!('shippingInfo' in dataRef[0].data[0].group[0])) {
       dataRef[0].data[0].group[0].shippingInfo = [{ text: allInfo.shippingInfo }];
-    } else if ("text" in dataRef[0].data[0].group[0].shippingInfo[0]) {
+    } else if ('text' in dataRef[0].data[0].group[0].shippingInfo[0]) {
       dataRef[0].data[0].group[0].shippingInfo[0].text = allInfo.shippingInfo;
     } else {
       dataRef[0].data[0].group[0].shippingInfo[0].push({ text: allInfo.shippingInfo });
     }
-
   },
 };
