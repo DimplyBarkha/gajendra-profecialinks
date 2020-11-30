eb189576f2bc93508cadb118e75dea6de6fc55f0
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-11-30 11:21:09 +0200

    mondoffice search

--- src/library/product/search/stores/m/mondoffice/IT/extract.js
+++ src/library/product/search/stores/m/mondoffice/IT/extract.js
@@ -1,46 +1,40 @@
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { productDetails } = dependencies;
-  await context.waitForSelector('#toolbar__all_anchor');
-  await context.evaluate(async function () {
-    function addElementToDocument (key, value) {
-      const catElement = document.createElement('div');
-      catElement.id = key;
-      catElement.textContent = value;
-      catElement.style.display = 'none';
-      document.body.appendChild(catElement);
-    }
-    addElementToDocument('search_url', location.href);
-  });
+const { transform } = require('./shared');
 
-  // Check if cookies pop-up appeared
-  const doesPopupExist = await context.evaluate(function () {
-    return Boolean(document.querySelector('#onetrust-accept-btn-handler'));
-  });
-  const doesPopupInfoExist = await context.evaluate(function () {
-    return Boolean(document.querySelector('.kamPopup__overlay'));
-  });
-
-  if (doesPopupExist) {
-    await context.click('#onetrust-accept-btn-handler');
-  }
-  if (doesPopupInfoExist) {
-    await context.click('.kamPopup__close');
-  }
-  return await context.extract(productDetails);
-}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'IT',
     store: 'mondoffice',
-    transform: null,
+    transform,
     domain: 'mondoffice.com',
     zipcode: '',
   },
-  implementation,
+  implementation: async (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(() => {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      try {
+        const text = location.search;
+        const start = text.indexOf('&SearchTerm=');
+        const end = text.indexOf('&SearchParameter');
+        addElementToDocument('url', location.href);
+        addElementToDocument('input', text.slice(start, end).replace('&SearchTerm=', ''));
+      } catch (e) {
+        console.log(e);
+      };
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
---
+++ src/library/product/search/stores/m/mondoffice/IT/shared.js
@@ -0,0 +1,28 @@
+
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  const searchTerms = [];
+  const urlCustom = [];
+  data.forEach(element => {
+    searchTerms.push(element.group.find(e => e.input));
+    urlCustom.push(element.group.find(e => e.url));
+  });
+  const filterSearch = searchTerms.filter(e => e)[0].input;
+  const url = urlCustom.filter(e => e)[0].url;
+  data.forEach(el => {
+    el.group.forEach(gr => {
+      try {
+        gr['_input'] = filterSearch;
+        gr['_url'] = url;
+      } catch (e) {
+        console.log(e);
+      }
+    });
+  });
+  return data;
+};
+module.exports = { transform };
