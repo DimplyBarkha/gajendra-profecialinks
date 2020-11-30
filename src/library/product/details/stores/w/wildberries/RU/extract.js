59bc95c55614857df7730cd73faf0546e0627e96
rajeshProfecialinks <rajesh.g@profecialinks.com>
2020-11-30 11:52:45 +0530

    wildberries core feedtype inprogress

---
+++ src/library/product/details/stores/w/wildberries/RU/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'RU',
+    store: 'wildberries',
+    transform: null,
+    domain: 'wildberries.ru',
+    zipcode: '',
+  },
+};
