cffa26e5b179ba218740a6d0150da08471ebf0db
vikas kyatannawar <vikas.k@osmosys.asia>
2020-11-30 16:27:21 +0530

    added amazon sweden offers and sellerInventory

---
+++ src/library/product/offers/stores/a/amazon/SE/offers.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/offers',
+  parameterValues: {
+    country: 'SE',
+    store: 'amazon',
+    domain: 'amazon.se',
+    zipcode: '',
+    defaultResults: null,
+    mergeType: null,
+  },
+};
