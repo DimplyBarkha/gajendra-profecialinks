2ff259d92b1f181733a9f992b2f6451d58fe4103
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-11-30 18:25:20 +0530

    devidealcleansearch

--- src/library/product/search/stores/i/idealclean/DE/execute.js
+++ src/library/product/search/stores/i/idealclean/DE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DE',
     store: 'idealclean',
     domain: 'ideal_clean.de',
-    url: 'https://www.idealclean.de/search?search={searchTerms}',
+    url: 'https://www.idealclean.de/search?limit=20&search={searchTerms}',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
