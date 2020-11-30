ec148100a63fd7e52b97f96443b3d373e3e26b9c
unknown <shalini.kumari-odc@partner.import.io>
2020-11-30 19:30:35 +0530

    lentillesmoinscheres

--- src/library/product/search/stores/l/lentillesmoinscheres/FR/execute.js
+++ src/library/product/search/stores/l/lentillesmoinscheres/FR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FR',
     store: 'lentillesmoinscheres',
     domain: 'lentillesmoinscheres.com',
-    url: 'https://www.lentillesmoinscheres.com/category.php?search_text={searchTerms)',
+    url: 'https://www.lentillesmoinscheres.com/category.php?search_text={searchTerms}',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
