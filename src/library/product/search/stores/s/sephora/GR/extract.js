8fc300c3a146bcf731eda6c5f01cd05c3d2ef1a9
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-01 23:38:02 +0530

    #1640 fixing QA1 issue

--- src/library/product/search/stores/s/sephora/GR/extract.js
+++ src/library/product/search/stores/s/sephora/GR/extract.js
@@ -1,10 +1,10 @@
-
+const {transform}=require('../GR/format');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'GR',
     store: 'sephora',
-    transform: null,
+    transform,
     domain: 'sephora.gr',
     zipcode: '',
   },
