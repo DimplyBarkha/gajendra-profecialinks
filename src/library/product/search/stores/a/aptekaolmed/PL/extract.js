f000b370efcd4402a522c017644c790e28adb04c
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-12-01 18:39:59 +0530

    aptekaolmed-Damayanthi

--- src/library/product/search/stores/a/aptekaolmed/PL/extract.js
+++ src/library/product/search/stores/a/aptekaolmed/PL/extract.js
@@ -8,4 +8,36 @@ module.exports = {
     domain: 'aptekaolmed.pl',
     zipcode: '',
   },
+  implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    let scrollTop = 0;
+    while (scrollTop !== 10000) {
+      await stall(500);
+      scrollTop += 500;
+      window.scroll(0, scrollTop);
+      if (scrollTop === 10000) {
+        await stall(500);
+        break;
+      }
+    }
+    function stall(ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+  
+});
+  return await context.extract(productDetails, { transform });
+}
+
