9b3f4728972bf7f26c5ed252f3eb9e953c6e3f9d
romancg <gromantsg@gmail.com>
2020-12-01 22:20:21 +0200

    #1209 edit rank

--- src/library/product/search/stores/e/euronics/IT/extract.js
+++ src/library/product/search/stores/e/euronics/IT/extract.js
@@ -13,14 +13,21 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(() => {
-      const pi = 12;
       const url = new URL(window.location.href);
       const p = url.searchParams.get('p');
       const page = p || 1;
       const product = document.querySelectorAll('div.productCard.j-productCard');
+      const pi = 16;
       for (let i = 0; i < product.length; i++) {
         const rank = (page * pi) - (pi - 1) + i;
         product[i].querySelector('#log_position').setAttribute('rank', rank);
+        product[i].querySelector('#log_position').setAttribute('rankOrganic', rank);
+      }
+      const productOrganic = document.querySelectorAll('.productCard__content>div.productCard.j-productCard');
+      const po = 12;
+      for (let i = 0; i < productOrganic.length; i++) {
+        const rankOrganic = (page * po) - (po - 1) + i;
+        productOrganic[i].querySelector('#log_position').setAttribute('rankOrganic', rankOrganic);
       }
     });
     return await context.extract(productDetails, { transform });
