2767264dba2d136e65027f5c394bc2c93874f290
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-12-01 18:41:04 +0530

    #1157. Enhanced content try catch fix  added.

--- src/library/product/details/stores/d/deliveryextra/BR/extract.js
+++ src/library/product/details/stores/d/deliveryextra/BR/extract.js
@@ -10,43 +10,47 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
-    const locationUrl = await context.evaluate(async () => {
-      return window.location.href;
-    });
-    var iframe = await context.evaluate(async () => {
-      const element = document.querySelector('div#standoutDivAutomatico');
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-        await new Promise((resolve) => setTimeout(resolve, 10000));
-      }
-      iframe = document.querySelector('div#standoutDivAutomatico iframe') ? document.querySelector('div#standoutDivAutomatico iframe').getAttribute('src') : null;
-      iframe = 'https:' + iframe;
-      return iframe;
-    });
-    if (iframe) {
-      await context.goto(iframe);
-      const manufactDes = await context.evaluate(() => {
-        return document.querySelector("div[class*='tab-pane active']") ? document.querySelector("div[class*='tab-pane active']").innerText : '';
+    try {
+      const locationUrl = await context.evaluate(async () => {
+        return window.location.href;
       });
-      const manufactImg = await context.evaluate(() => {
-        const arrImgSel = document.querySelectorAll("div[class*='box'] img[src]") ? Array.from(document.querySelectorAll("div[class*='box'] img[src]")) : '';
-        const img = arrImgSel.map((imgSelector) => imgSelector && imgSelector.src ? imgSelector.src : '');
-        const imgURL = img.join(' , ');
-        return imgURL;
-      });
-      await context.goto(locationUrl, { timeout: 30000 });
-      await context.evaluate((manufactDes, manufactImg) => {
-        addHiddenDiv('ii_manu', manufactDes);
-        addHiddenDiv('ii_img', manufactImg);
-        function addHiddenDiv (id, content) {
-          const newDiv = document.createElement('div');
-          newDiv.id = id;
-          newDiv.textContent = content;
-          newDiv.style.display = 'none';
-          document.body.appendChild(newDiv);
+      var iframe = await context.evaluate(async () => {
+        const element = document.querySelector('div#standoutDivAutomatico');
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+          await new Promise((resolve) => setTimeout(resolve, 10000));
         }
-      }, manufactDes, manufactImg);
-      return await context.extract(productDetails, { transform }, { type: 'APPEND' });
+        iframe = document.querySelector('div#standoutDivAutomatico iframe') ? document.querySelector('div#standoutDivAutomatico iframe').getAttribute('src') : null;
+        iframe = 'https:' + iframe;
+        return iframe;
+      });
+      if (iframe) {
+        await context.goto(iframe);
+        const manufactDes = await context.evaluate(() => {
+          return document.querySelector("div[class*='tab-pane active']") ? document.querySelector("div[class*='tab-pane active']").innerText : '';
+        });
+        const manufactImg = await context.evaluate(() => {
+          const arrImgSel = document.querySelectorAll("div[class*='box'] img[src]") ? Array.from(document.querySelectorAll("div[class*='box'] img[src]")) : '';
+          const img = arrImgSel.map((imgSelector) => imgSelector && imgSelector.src ? imgSelector.src : '');
+          const imgURL = img.join(' , ');
+          return imgURL;
+        });
+        await context.goto(locationUrl, { timeout: 30000 });
+        await context.evaluate((manufactDes, manufactImg) => {
+          addHiddenDiv('ii_manu', manufactDes);
+          addHiddenDiv('ii_img', manufactImg);
+          function addHiddenDiv (id, content) {
+            const newDiv = document.createElement('div');
+            newDiv.id = id;
+            newDiv.textContent = content;
+            newDiv.style.display = 'none';
+            document.body.appendChild(newDiv);
+          }
+        }, manufactDes, manufactImg);
+        return await context.extract(productDetails, { transform }, { type: 'APPEND' });
+      }
+    } catch (e) {
+      console.log(e);
     }
     return await context.extract(productDetails, { transform });
   },
