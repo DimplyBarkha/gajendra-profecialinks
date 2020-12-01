bddf39f034c6dfdf3a9cee3dc3894d7f7c81523e
Karolina <karolina.urbanska@tdcm.io>
2020-12-01 16:58:44 +0100

    Fixed issues with indents

--- src/library/product/search/stores/m/marionnaud/AT/extract.js
+++ src/library/product/search/stores/m/marionnaud/AT/extract.js
@@ -17,7 +17,7 @@ module.exports = {
     await new Promise((resolve, reject) => setTimeout(resolve, 3000));
     await context.evaluate(async () => {
       const totalNoOfResults = document.querySelector('div.more-data-loader__counter')
-        ? parseInt(document.querySelector('div.more-data-loader__counter').innerText.replace(/.*von\s?(\d+).*/g, '$1')) : 0;
+        ? parseInt(document.querySelector('div.more-data-loader__counter').textContent.replace(/.*von\s?(\d+).*/g, '$1')) : 0;
 
       const maxResultsToCollect = 150;
       if (totalNoOfResults > 20) {
@@ -29,21 +29,21 @@ module.exports = {
       }
     });
     await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-  await context.evaluate(async () => {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-  });
+    await context.evaluate(async () => {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    });
     await new Promise((resolve, reject) => setTimeout(resolve, 1000));
     await context.evaluate(async () => {
       const allProducts = document.querySelectorAll('div.more-data-loader__content div[class*=product-tile__wrapper]');
       for (let i = 0; i < allProducts.length && i <= 150; i++) {
-        const brand = allProducts[i].querySelector('a.product-tile__brand') ? allProducts[i].querySelector('a.product-tile__brand').innerText : '';
-        const subtitle = allProducts[i].querySelector('a.product-tile__description') ? allProducts[i].querySelector('a.product-tile__description').innerText : '';
+        const brand = allProducts[i].querySelector('a.product-tile__brand') ? allProducts[i].querySelector('a.product-tile__brand').textContent.trim() : '';
+        const subtitle = allProducts[i].querySelector('a.product-tile__description') ? allProducts[i].querySelector('a.product-tile__description').textContent.trim() : '';
         allProducts[i].setAttribute('product-tile-name', `${brand} ${subtitle}`);
-        const productUrl = allProducts[i].querySelector('div.product-tile__container a[data-product-link]') ? allProducts[i].querySelector('div.product-tile__container a[data-product-link]').href : '';
-        allProducts[i].setAttribute('product-tile-url', productUrl);
+        const productUrl = allProducts[i].querySelector('div.product-tile__container a[data-product-link]') ? allProducts[i].querySelector('div.product-tile__container a[data-product-link]').getAttribute('href') : '';
+        allProducts[i].setAttribute('product-tile-url', `https://www.marionnaud.at${productUrl}`);
         allProducts[i].setAttribute('product-tile-id', productUrl.replace(/.*\/p\/(.*)/g, '$1'));
-        const oldPrice = allProducts[i].querySelector('a.product-tile__price span.text--gray-dark') ? allProducts[i].querySelector('a.product-tile__price span.text--gray-dark').innerText : '';
-        const price = allProducts[i].querySelector('a.product-tile__price span.text--pink') ? allProducts[i].querySelector('a.product-tile__price span.text--pink').innerText : oldPrice;
+        const oldPrice = allProducts[i].querySelector('a.product-tile__price span.text--gray-dark') ? allProducts[i].querySelector('a.product-tile__price span.text--gray-dark').textContent : '';
+        const price = allProducts[i].querySelector('a.product-tile__price span.text--pink') ? allProducts[i].querySelector('a.product-tile__price span.text--pink').textContent : oldPrice;
         allProducts[i].setAttribute('product-tile-price', price);
       }
     });
