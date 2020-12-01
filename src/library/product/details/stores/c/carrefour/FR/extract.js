bca1f4429b3770016cd0c86fcfe36fdaceedea80
Himanshu-Agrahari <himanshu.a@partner.import.io>
2020-12-01 13:01:39 +0530

    Fix: Handle no-result page locally
    No result xpath implemented globally seems to be not working

--- src/library/product/details/stores/c/carrefour/FR/extract.js
+++ src/library/product/details/stores/c/carrefour/FR/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require('../FR/shared')
+const { transform } = require('../FR/shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -16,7 +16,8 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
 
-    const cssProduct = ".pdp__secondary";
+    const cssNoResult = 'h1.page-title';
+    const cssProduct = '.pdp__secondary';
     const cssProductDetails = 'div.pdp__main';
 
     const isSelectorAvailable = async (cssSelector) => {
@@ -25,6 +26,21 @@ module.exports = {
         return !!document.querySelector(selector);
       }, cssSelector);
     };
+
+    try {
+      console.log('Checking for no-result-page');
+      const isNoResultPage = await context.evaluate(function (selector) {
+        const noResultsNode = document.querySelector(selector);
+        return noResultsNode && noResultsNode.innerText && noResultsNode.innerText.includes('0 résultat pour');
+      }, cssNoResult);
+
+      if (isNoResultPage) {
+        console.log('Encountered No result page');
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
 
     console.log('.....waiting......');
     await context.waitForSelector(cssProduct, { timeout: 10000 });
@@ -47,14 +63,13 @@ module.exports = {
       try {
         await new Promise((resolve) => setTimeout(resolve, 5000));
         await context.waitForXPath('//button[@id="footer_tc_privacy_button"]', { timeout: 30000 });
-        let cookieButton = document.querySelector('button#footer_tc_privacy_button');
+        const cookieButton = document.querySelector('button#footer_tc_privacy_button');
         if (cookieButton) {
           // @ts-ignore
           cookieButton.click();
         }
       } catch (error) {
         console.log('error: ', error);
-  
       }
       async function infiniteScroll () {
         let prevScroll = document.documentElement.scrollTop;
@@ -71,12 +86,12 @@ module.exports = {
       await infiniteScroll();
       try {
         await new Promise((resolve) => setTimeout(resolve, 5000));
-      }catch(error){
-        console.log(error)
+      } catch (error) {
+        console.log(error);
       }
-      let clickLoadMore = document.querySelector('button.paragraph-truncate__see-more');
       // @ts-ignore
-      clickLoadMore = clickLoadMore ? clickLoadMore.click() : '';
+      document.querySelector('button.paragraph-truncate__see-more') && document.querySelector('button.paragraph-truncate__see-more').click();
+
       function addHiddenDiv (id, content) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
@@ -84,39 +99,36 @@ module.exports = {
         newDiv.style.display = 'none';
         document.body.appendChild(newDiv);
       }
-      let enhancedDescHTML = document.querySelector('div#flix-inpage');
-      let enhancedDesc = enhancedDescHTML ? enhancedDescHTML.innerHTML.replace(/<li.*?>/gm, ' || ').replace(/\n/gm, ' ').replace(/<script>.*?<\/script>/gm, '').replace(/<style.*?<\/style>/gm, '').replace(/<.*?>/gm, ' ').replace(/•/gm, ' ||').replace(/\s{2,}/, ' ').trim() : '';
-      addHiddenDiv('cr_enhancedContent',enhancedDesc);
-      let specTrs = document.querySelectorAll('div.secondary-details__characteristics-table table tr');
-      let finalSpecArr = [];
-      let fieldVal = '';
+      const enhancedDescHTML = document.querySelector('div#flix-inpage');
+      const enhancedDesc = enhancedDescHTML ? enhancedDescHTML.innerHTML.replace(/<li.*?>/gm, ' || ').replace(/\n/gm, ' ').replace(/<script>.*?<\/script>/gm, '').replace(/<style.*?<\/style>/gm, '').replace(/<.*?>/gm, ' ').replace(/•/gm, ' ||').replace(/\s{2,}/, ' ').trim() : '';
+      addHiddenDiv('cr_enhancedContent', enhancedDesc);
+      const specTrs = document.querySelectorAll('div.secondary-details__characteristics-table table tr');
+      const finalSpecArr = [];
       let field;
       let value;
       for (let index = 0; index < specTrs.length; index++) {
         const element = specTrs[index];
-        field = element.querySelector('td:nth-child(1)')
+        field = element.querySelector('td:nth-child(1)');
         // @ts-ignore
-        let fieldStr = field ? field.innerText : '';
+        const fieldStr = field ? field.innerText : '';
         value = element.querySelector('td:nth-child(2)');
         // @ts-ignore
-        let valueStr = value ? value.innerText : '';
-        if(fieldStr && valueStr){
-          let fieldVal = fieldStr+' : '+valueStr;
+        const valueStr = value ? value.innerText : '';
+        if (fieldStr && valueStr) {
+          const fieldVal = fieldStr + ' : ' + valueStr;
           finalSpecArr.push(fieldVal);
         }
-
       }
-      let finalSpecStr
-      if(finalSpecArr.length > 0){
+      let finalSpecStr;
+      if (finalSpecArr.length > 0) {
         finalSpecStr = finalSpecArr.join(' || ');
       }
-      addHiddenDiv('cr_specification',finalSpecStr );
+      addHiddenDiv('cr_specification', finalSpecStr);
       // @ts-ignore
-      let tc_vars = window.tc_vars;
-      let brand = tc_vars ? tc_vars.product_brand : '';
-      addHiddenDiv('cr_brand',brand);
-    })
+      const tcVars = window.tc_vars;
+      const brand = tcVars ? tcVars.product_brand : '';
+      addHiddenDiv('cr_brand', brand);
+    });
     return await context.extract(productDetails, { transform });
-  }
+  },
 };
-
