d3d8e39eca811af8d931984e4540d9c13b9f0bff
Nilesh <nilesh.b@osmosys.asia>
2020-12-01 13:33:33 +0530

    Fixed issue.

--- src/library/product/details/stores/s/shop-apotheke/DE/extract.js
+++ src/library/product/details/stores/s/shop-apotheke/DE/extract.js
@@ -12,7 +12,8 @@ module.exports = {
     await context.evaluate(async function () {
       // @ts-ignore
       const dataObj = window.dataLayer[0].product;
-      function addElementToDocument(key, value) {
+
+      function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
@@ -22,7 +23,7 @@ module.exports = {
       if (dataObj) {
         addElementToDocument('pd_id', dataObj.id);
         addElementToDocument('pd_name', dataObj.name);
-        addElementToDocument('pd_category', dataObj.categoryPath);
+        addElementToDocument('pd_category', dataObj.categoryPath ? dataObj.categoryPath : '');
         addElementToDocument('pd_quantity', dataObj.unitQuantityString + ' ' + dataObj.unit);
         addElementToDocument('pd_price', dataObj.priceBrutto);
       }
@@ -30,22 +31,16 @@ module.exports = {
       const mainDataObj = window.__PRELOADED_STATE__[0].componentInitialState.ProductVariantsInitialState;
       if (mainDataObj) {
         addElementToDocument('pd_variantId', mainDataObj.currentVariantId);
-        addElementToDocument('pd_availabilityText', mainDataObj.product.stockStatus.statusReason.trim());
         addElementToDocument('pd_sku', mainDataObj.currentVariantId);
-        addElementToDocument('pd_ratingCount', mainDataObj.product.numberOfRatings);
         addElementToDocument('pd_mpn', mainDataObj.variants[0].manufacturerCode);
         addElementToDocument('pd_image', mainDataObj.product.thumbnailURL);
-        // addElementToDocument('pd_variant_ids', mainDataObj.variants.slice(1).map(elm=>elm.id));
-        if (mainDataObj.product.rating) {
-          addElementToDocument('pd_aggregateRating', mainDataObj.product.rating);
-        }
         if (mainDataObj.variants.length > 1) {
           const variantIds = mainDataObj.variants.slice(1).map(elm => elm.id);
           addElementToDocument('pd_variant_ids', variantIds);
           addElementToDocument('pd_first_variant', dataObj.unitQuantityString + ' ' + dataObj.unit);
           addElementToDocument('pd_variant_info', mainDataObj.currentVariantId);
         } else {
-          console.log('variant not found');
+          console.log('variants not found');
         }
       }
     });
--- src/library/product/details/stores/s/shop-apotheke/shared.js
+++ src/library/product/details/stores/s/shop-apotheke/shared.js
@@ -1,53 +1,83 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-        for (const row of group) {
-            let text;
-            if (row.category) {
-                row.category.forEach(item => {
-                    if (item.text.includes('/')) {
-                        text = item.text.split('/').map(e => {
-                            return { text: e.trim() };
-                        });
-                    }
-                });
-                row.category = text;
-            }
-            if (row.variants) {
-                row.variants.forEach(item => {
-                    if (item.text.includes(',')) {
-                        text = item.text.split(',').map(e => {
-                            return { text: e.trim() };
-                        });
-                    }
-                });
-                row.variants = text;
-            }
-        }
+  for (const { group }
+    of data) {
+    for (const row of group) {
+      if (row.variants) {
+        let text;
+        row.variants.forEach(item => {
+          if (item.text.includes(',')) {
+            text = item.text.split(',').map(e => {
+              return { text: e.trim() };
+            });
+          } else {
+            text = [];
+          }
+        });
+        row.variants = text;
+      }
+
+      if (row.category) {
+        let text;
+        row.category.forEach(item => {
+          if (item.text.includes('/')) {
+            text = item.text.split('/').map(e => {
+              return { text: e.trim() };
+            });
+          } else {
+            text = [];
+          }
+        });
+        row.category = text;
+      }
+
+      if (row.price) {
+        let text = '';
+        row.price.forEach(item => {
+          text = item.text.replace('.', ',');
+        });
+        row.price = [
+          {
+            text: text,
+          },
+        ];
+      }
+
+      if (row.aggregateRating) {
+        let text = '';
+        row.aggregateRating.forEach(item => {
+          text = Number(item.text).toFixed(1).replace('.', ',');
+        });
+        row.aggregateRating = [
+          {
+            text: text,
+          },
+        ];
+      }
     }
-    const clean = text => text.toString()
-        .replace(/\r\n|\r|\n/g, ' ')
-        .replace(/&amp;nbsp;/g, ' ')
-        .replace(/&amp;#160/g, ' ')
-        .replace(/\u00A0/g, ' ')
-        .replace(/\s{2,}/g, ' ')
-        .replace(/"\s{1,}/g, '"')
-        .replace(/\s{1,}"/g, '"')
-        .replace(/^ +| +$|( )+/g, ' ')
-        // eslint-disable-next-line no-control-regex
-        .replace(/[\x00-\x1F]/g, '')
-        .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  }
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
 
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-    }))));
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
 
-    return data;
+  return data;
 };
 
 module.exports = { transform };
