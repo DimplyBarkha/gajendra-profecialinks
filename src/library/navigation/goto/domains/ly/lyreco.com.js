ecb60f67395e4cbd1f04253c07840569376ea551
ravitejaw324 <ravi.w@partner.import.io>
2020-12-01 07:51:37 +0530

    added more goto timeOut

--- src/library/navigation/goto/domains/ly/lyreco.com.js
+++ src/library/navigation/goto/domains/ly/lyreco.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'lyreco.com',
-    timeout: 12000,
+    timeout: 50000,
     country: 'IT',
     store: 'lyreco',
     zipcode: '',
