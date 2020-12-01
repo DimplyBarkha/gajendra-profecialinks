8f42aad2ceff02e7feb928cd33ac8aeeaf7e90b1
dhanush <dhanush.isukapalli@od.import.io>
2020-12-01 17:38:52 +0530

    mall by kavya

--- src/library/product/details/stores/m/mall/SK/extract.js
+++ src/library/product/details/stores/m/mall/SK/extract.js
@@ -8,4 +8,30 @@ module.exports = {
     domain: 'mall.sk',
     zipcode: '',
   },
-};
+  implementation: async ({ inputstring }, { country, domain }, context, { productDetails }) => {
+
+    await context.evaluate(() => {
+    function addElementToDocument(key, value) {
+    const catElement = document.createElement('div');
+    catElement.id = key;
+    catElement.textContent = value;
+    catElement.style.display = 'none';
+    document.body.appendChild(catElement);
+    }
+    var getXpath = (xpath, prop) => {
+      var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+      let result;
+      if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+      else result = elem ? elem.singleNodeValue : '';
+      return result && result.trim ? result.trim() : result;
+      };
+      var name = getXpath('//span[@class="rating-percent"]/text()', 'nodeValue');
+      if(name!=null){
+        var agr = name.slice(0, -2);
+        agr = (agr*5)/100;
+        addElementToDocument('agr', agr);
+      }
+    });
+    await context.extract(productDetails);
+    },
+    };
