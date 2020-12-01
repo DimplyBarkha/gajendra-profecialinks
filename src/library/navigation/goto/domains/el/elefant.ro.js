816d618d968030c5fa149a38252a022b1c6d0560
tapan-profecialinks <tapan@profecialinks.com>
2020-12-01 11:54:08 +0530

    issue fixed

--- src/library/navigation/goto/domains/el/elefant.ro.js
+++ src/library/navigation/goto/domains/el/elefant.ro.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'elefant.ro',
-    timeout: 500000,
+    timeout: 8000000,
     country: 'RO',
     store: 'elefant',
     zipcode: '',
