42c7437549f9b5ad4da430978a77bb4b707d417f
nehashende <neha.shende@synerzip.com>
2020-12-01 16:37:36 +0530

    #2522. Fixed rank and aggregateRating.

--- src/library/product/search/stores/c/cabelas/US/extract.js
+++ src/library/product/search/stores/c/cabelas/US/extract.js
@@ -1,10 +1,11 @@
-const { cleanUp } = require('../../../../shared');
+const { transform } = require('./transform');
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'cabelas',
-    transform: cleanUp,
+    transform,
     domain: 'cabelas.com',
     zipcode: '',
   },
