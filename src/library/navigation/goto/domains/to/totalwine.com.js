f56170ac26752b4a151a3ed8ec662fa13a769809
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-01 11:06:24 +0530

    updated goto

--- src/library/navigation/goto/domains/to/totalwine.com.js
+++ src/library/navigation/goto/domains/to/totalwine.com.js
@@ -9,5 +9,8 @@ module.exports = {
   implementation: async ({ url }, parameters, context, dependencies) => {
     url = `${url}#[!opt!]{"cookie_jar":[{"name":"twm-userStoreInformation","value":"ispStore~402:ifcStore~null@ifcStoreState~US-MD@method~INSTORE_PICKUP"}]}[/!opt!]`;
     await context.goto(url);
+    if (await context.evaluate(() => !!document.querySelector('img[src*="NotFoundPage"]'))) {
+      throw new Error('Error: Whoops!, Sorry we’re coming up dry.');
+    }
   },
 };
