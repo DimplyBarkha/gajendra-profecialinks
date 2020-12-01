5a3849b06b5367edce7651941b59bbb346189e85
Vivek Gondil <gondilvivek@gmail.com>
2020-12-01 22:32:41 +0530

    Added changes in extractor.

--- src/library/product/details/stores/f/footlocker/UK/extract.js
+++ src/library/product/details/stores/f/footlocker/UK/extract.js
@@ -14,16 +14,18 @@ module.exports = {
     context,
     dependencies,
   ) {
-    const { cleanUp } = parameters;
-    const { productDetails } = dependencies;
     try {
       await context.waitForSelector('.slick-list.draggable .slick-active img', { timeout: 30000 });
-      await context.evaluate(() => {
-        const getvariants = document.querySelectorAll('div.owl-stage a');
-        const variantid = [];
-        for (let i = 0; i < getvariants.length; i++) {
-          variantid.push(getvariants[i].getAttribute('data-product-click-link'));
+      await context.evaluate(async () => {
+        function getDivWithId(id){
+          const div = document.createElement('div');
+          div.setAttribute('id', id);
+          return div;
         }
+        const url = window.location.href;
+        const sku = url.split('=')[1];
+        document.body.setAttribute('variant-id', sku.toString());
+        const variantid = [];
         const getSizeVariants = document.querySelectorAll('#fitanalytics_sizecontainer > section:nth-child(3) > div >button');
         for (let i = 0; i < getSizeVariants.length; i++) {
           if (getSizeVariants[i].getAttribute('data-product-size-select-item')) {
@@ -31,22 +33,24 @@ module.exports = {
           }
         }
         for (let i = 0; i < variantid.length; i++) {
-          const div = document.createElement('div');
-          div.setAttribute('id', 'variantId');
+          const div = getDivWithId('variantId');
           div.setAttribute('data', variantid[i]);
           document.body.append(div);
         }
-        const stock = document.querySelector('div[class="fl-product-error-messages"]');
-        if (stock.textContent.includes('In') || stock.textContent.includes('Less then')) {
-          stock.setAttribute('availability', 'In stock');
-        } else {
-          stock.setAttribute('availability', 'Out of stock');
+        const query = "button[data-product-size-select-item='" + sku + "']";
+        const selectedBtn = document.querySelectorAll(query);
+        if (selectedBtn[2]) { selectedBtn[2].click(); }
+        if (!selectedBtn && !document.querySelector('section:nth-child(3) > div >button[data-form-field-target="SKU"]')) {
+          const div = getDivWithId('stock');
+          div.innerText = 'Out of Stock';
+          document.body.append(div);
         }
       });
     } catch (err) {
       console.log('Image did not load');
     }
-    // await context.waitForSelector('.fl-picture--img');
+    const { cleanUp } = parameters;
+    const { productDetails } = dependencies;
     return await context.extract(productDetails, { cleanUp });
   },
 
--- src/library/product/details/stores/f/footlocker/UK/variantsExtract.js
+++ src/library/product/details/stores/f/footlocker/UK/variantsExtract.js
@@ -9,49 +9,35 @@ module.exports = {
     zipcode: '',
   },
 
-  implementation: async ({ inputString }, { country, domain }, context, { variants }) => {
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
     await context.evaluate(() => {
-      const getvariants = document.querySelectorAll('div.owl-stage a');
       const variantid = [];
       const url = window.location.href;
       const trimmedurl = url.split('=')[0];
       let data = '';
-      for (let i = 0; i < getvariants.length; i++) {
-        data = getvariants[i].getAttribute('data-product-click-link');
-        console.log(data);
-        variantid.push(data);
-      }
       const getSizeVariants = document.querySelectorAll('#fitanalytics_sizecontainer > section:nth-child(3) > div >button');
       for (let i = 0; i < getSizeVariants.length; i++) {
         data = getSizeVariants[i].getAttribute('data-product-size-select-item');
-        const div = document.createElement('div');
-        div.className = 'variantid';
-        const getInput = document.createElement('li');
-        getInput.id = 'variant';
-        div.appendChild(getInput);
-        document.body.appendChild(div);
-        console.log(data);
         if (data) {
           variantid.push(data);
-          getInput.setAttribute('value', data);
         }
       }
-      const varianturl = [];
-      const newVariant = Array.from(new Set(variantid));
-
-      for (let i = 0; i < newVariant.length; i++) {
+      variantid.forEach((id) => {
         const div = document.createElement('div');
-        div.className = 'varianturl';
-        const getInput = document.createElement('li');
-        getInput.id = 'variant';
-        div.appendChild(getInput);
+        div.className = 'variantid';
+        div.setAttribute('id', id.toString());
+        div.setAttribute('url', trimmedurl.toString() + id.toString());
         document.body.appendChild(div);
-        varianturl.push(trimmedurl + '=' + newVariant[i]);
-        getInput.setAttribute('urlvalue', varianturl[i]);
-        getInput.setAttribute('variantid', newVariant[i]);
-      }
+      });
     });
-    await context.extract(variants);
+    const { transform } = parameters;
+    const { variants } = dependencies;
+    return await context.extract(variants, { transform });
   },
 
 };
