28b90abd558ce7f674d6f7ca47534d03c6df8606
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-01 10:08:35 +0530

    added location cookies to goto

--- src/library/navigation/goto/domains/to/totalwine.com.js
+++ src/library/navigation/goto/domains/to/totalwine.com.js
@@ -6,4 +6,8 @@ module.exports = {
     country: 'US',
     store: 'totalwine',
   },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"cookie_jar":[{"name":"twm-userStoreInformation","value":"ispStore~402:ifcStore~null@ifcStoreState~US-MD@method~INSTORE_PICKUP"}]}[/!opt!]`;
+    await context.goto(url);
+  },
 };
