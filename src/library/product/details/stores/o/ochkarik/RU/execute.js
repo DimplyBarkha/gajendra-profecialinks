e53320ba7751a27cf145ee47d22d3f0caa2e1b97
rajeshProfecialinks <rajesh.g@profecialinks.com>
2020-12-01 17:19:16 +0530

    ochkarik core feedtype inprogress

---
+++ src/library/product/details/stores/o/ochkarik/RU/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'RU',
+    store: 'ochkarik',
+    domain: 'ochkarik.ru',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/o/ochkarik/RU/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'RU',
+    store: 'ochkarik',
+    transform: null,
+    domain: 'ochkarik.ru',
+    zipcode: '',
+  },
+};
