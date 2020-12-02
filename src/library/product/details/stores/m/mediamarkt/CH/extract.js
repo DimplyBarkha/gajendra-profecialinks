7d9a0583799f3346e53a9a7b12a9cfd3e24d1912
Venkata Akhilesh Pasupuleti <41539204+VenkataAkhileshP@users.noreply.github.com>
2020-12-03 01:38:50 +0530

    Fixed price to contain , instead of .

--- src/library/product/details/stores/m/mediamarkt/CH/extract.js
+++ src/library/product/details/stores/m/mediamarkt/CH/extract.js
@@ -9,4 +9,24 @@ module.exports = {
     domain: 'mediamarkt.ch',
     zipcode: '',
   },
+  dependencies: {
+    productDetails: 'extraction:product/details/stores/${store[0:1]}/${store}/${country}/extract',
+    SharedHelpers: 'module:product/details/stores/${store[0:1]}/${store}/helpersShared',
+  },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails, Helpers, SharedHelpers }) => {
+    const sharedhelpers = new SharedHelpers(context);
+
+    const priceValue = await context.evaluate(async function () {
+      const priceEle = 'meta[property*="product:price:amount"]';
+      let price = document.querySelectorAll(priceEle) && document.querySelectorAll(priceEle)[0] && document.querySelectorAll(priceEle)[0].hasAttribute('content') ? document.querySelectorAll(priceEle)[0].getAttribute('content') : '';
+      price = price.replace('.', ',');
+      return price;
+    });
+
+    await sharedhelpers.addHiddenInfo('priceValue', priceValue);
+    
+    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+    return await context.extract(productDetails, { transform });
+  },
 };
