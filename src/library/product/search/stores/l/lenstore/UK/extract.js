c6c7e24e0d5df0d42331cc02c1165a47875a5bdc
unknown <leena.gavali-odc@partner.import.io>
2020-12-02 22:48:35 +0530

    search_url

--- src/library/product/search/stores/l/lenstore/UK/extract.js
+++ src/library/product/search/stores/l/lenstore/UK/extract.js
@@ -8,4 +8,54 @@ module.exports = {
     domain: 'lenstore.co.uk',
     zipcode: '',
   },
+  implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addclass(xpathforpagination) {
+      var elems = document.querySelectorAll(xpathforpagination);
+      elems[0].classList.add('pagination');
+    }
+    function addElementToDocument(key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+      }
+      return result;
+    };
+    function addHiddenDiv(id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll('div[class="c-product-list__box js-impression-product"]')[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    var abc = getAllXpath("//div[@class='c-product-list__box js-impression-product']/@data-ga-id", 'nodeValue');
+    for (var i = 0; i < abc.length; i++) {
+      var URL = window.location.href;
+      if (URL != null) {
+        // addElementToDocument('pd_url', URL);
+        addHiddenDiv('URL', URL, i);
+      }
+    }
+
+  });
+  return await context.extract(productDetails, { transform });
+}
