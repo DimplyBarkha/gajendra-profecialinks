ef50eec2eeff979f8d6b54dbf8ff56da683e6e2f
sahoojudhi <judhishtor@profecialinks.com>
2020-12-02 23:31:24 +0530

    Core part pushed

--- src/library/product/details/stores/c/canadiantire/CA/createUrl.js
+++ src/library/product/details/stores/c/canadiantire/CA/createUrl.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'canadiantire.ca',
-    prefix: null,
+    prefix: 'search.do?q=',
     url: 'https://canadiantire.ca/search.do?q={id}',
     country: 'CA',
     store: 'canadiantire',
