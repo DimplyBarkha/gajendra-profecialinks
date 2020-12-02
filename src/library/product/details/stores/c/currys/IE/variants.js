94ef970016e9f6cbda1f356df8550172dbb22561
Bartosz Kurzawa <kurzawa.bart@gmail.com>
2020-12-02 09:36:08 +0100

    Added and deployed variants extractor #2413

---
+++ src/library/product/details/stores/c/currys/IE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'IE',
+    store: 'currys',
+    domain: 'currys.ie',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/details/stores/c/currys/IE/variantsExtract.js
@@ -0,0 +1,42 @@
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { variants } = dependencies;
+  await context.evaluate(async () => {
+    const addElementToDocument = (key, value) => {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    };
+    const url = window.location.href;
+    const curentVariantId = url.match(/(\d+)-pdt.html/) ? url.match(/(\d+)-pdt.html/)[1] : null;
+    // @ts-ignore
+    const otherVariants = [...document.querySelectorAll('div[class="no-tablet"] li[class*=dc-product-swatch] > a')]
+      .map(el => {
+        return (el.href && el.href.match(/(\d+)-pdt.html/)) ? el.href.match(/(\d+)-pdt.html/)[1] : null;
+      }).filter(el => !!el);
+    const variantArr = [curentVariantId, ...otherVariants].filter(el => !!el);
+    console.log(variantArr);
+    for (let i = 0; i < variantArr.length; i++) {
+      addElementToDocument('product_variant', variantArr[i]);
+    }
+  });
+  return await context.extract(variants);
+}
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'IE',
+    store: 'currys',
+    transform: null,
+    domain: 'currys.ie',
+    zipcode: "''",
+  },
+  implementation,
+};
