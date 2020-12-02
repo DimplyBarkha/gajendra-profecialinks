d32523e57af88e52f13768ed4e5295238e6f26ca
prasath <r.rahul@olivedatacentre.com>
2020-12-02 04:39:03 -0800

    konzum prasath

--- src/library/product/search/stores/k/konzum/HR/execute.js
+++ src/library/product/search/stores/k/konzum/HR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'HR',
     store: 'konzum',
     domain: 'konzum.hr',
-    url: 'https://www.konzum.hr/web/search?utf8=%E2%9C%93&search%5Bscope%5D=products&filters%5BKategorije%5D%5B%5D=&search%5Bterm%5D={serachTerms}',
+    url: 'https://www.konzum.hr/web/search?utf8=%E2%9C%93&search%5Bscope%5D=products&filters%5BKategorije%5D%5B%5D=&search%5Bterm%5D={searchTerms}',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
