2785fae99d024b3008799d27c4e1d77895763f00
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2020-12-02 15:41:44 +0100

    Modified selecting variants, tests updated

--- src/library/product/details/stores/a/arnotts/IE/variantsExtract.js
+++ src/library/product/details/stores/a/arnotts/IE/variantsExtract.js
@@ -14,15 +14,30 @@ module.exports = {
       variantsList.id = 'added_variants_list';
       variantsList.style.display = 'none';
 
-      const variants = document.querySelectorAll('ul.swatches.color li a');
-      const iterations = variants.length || 1;
-      for (let i = 0; i < iterations; i++) {
-        const elem = document.createElement('li');
-        const url = variants.length && variants[i].getAttribute('href') ? variants[i].getAttribute('href') : window.location.href;
-        const id = url.match(/pid=(.*)/) ? url.match(/pid=(.*)/)[1] : '';
-        elem.setAttribute('variant_url', url);
-        elem.setAttribute('variant_id', id);
-        variantsList.appendChild(elem);
+      const variantsSize = document.querySelectorAll('select.variation-select option[data-lgimg]');
+      const variants = document.querySelectorAll('ul.swatches.color li a[href]');
+      if (variants.length && !variantsSize.length) {
+        const iterations = variants.length || 1;
+        for (let i = 0; i < iterations; i++) {
+          const elem = document.createElement('li');
+          const url = variants.length ? variants[i].getAttribute('href') : window.location.href;
+          const id = url.match(/pid=(.*)/) ? url.match(/pid=(.*)/)[1] : '';
+          elem.setAttribute('variant_url', url);
+          elem.setAttribute('variant_id', id);
+          variantsList.appendChild(elem);
+        }
+      }
+      if (!variants.length && variantsSize.length) {
+        const iterations = variantsSize.length || 1;
+        for (let i = 0; i < iterations; i++) {
+          const elem = document.createElement('li');
+          const url =
+          variantsSize.length && variantsSize[i].getAttribute('value') ? variantsSize[i].getAttribute('value') : window.location.href;
+          const id = url.match(/pid=(.*)/) ? url.match(/pid=(.*)/)[1] : '';
+          elem.setAttribute('variant_url', url);
+          elem.setAttribute('variant_id', id);
+          variantsList.appendChild(elem);
+        }
       }
       document.body.appendChild(variantsList);
     });
