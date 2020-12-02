62604033854365b0b88e397ae67e99cb7bb48033
srishivams <srishivams97@gmail.com>
2020-12-02 22:25:15 +0530

    fixed issues with pagination

--- src/library/product/search/stores/m/magasin/DK/extract.js
+++ src/library/product/search/stores/m/magasin/DK/extract.js
@@ -1,5 +1,5 @@
 const { transform } = require('./transfer');
-async function implementation(
+async function implementation (
   inputs,
   parameters,
   context,
@@ -8,28 +8,38 @@ async function implementation(
   const { transform } = parameters;
   const { productDetails } = dependencies;
   await context.evaluate(async function () {
-    function addHiddenDiv(node, id, content) {
+    function stall (ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+    function addHiddenDiv (node, id, content) {
       const newDiv = document.createElement('product-id');
       newDiv.id = id;
       newDiv.textContent = content;
       newDiv.style.display = 'none';
       node.appendChild(newDiv);
     }
-    
+
     document.querySelectorAll('product-tile').forEach(node => {
-      const attr = node.getAttribute('data-gtm-event')
+      const attr = node.getAttribute('data-gtm-event');
       if (attr) {
         var productTileObject = attr.trim();
-        var productData = JSON.parse(productTileObject); 
+        var productData = JSON.parse(productTileObject);
         if (productData && productData.ecommerce && productData.ecommerce.click && productData.ecommerce.click.products[0]) {
-          addHiddenDiv(node, productData.ecommerce.click.products[0].id, productData.ecommerce.click.products[0].id)
-        }        
+          addHiddenDiv(node, productData.ecommerce.click.products[0].id, productData.ecommerce.click.products[0].id);
+        }
       }
     });
+    if (document.querySelector('div.show-more > div.load-more > button[class="button -border"]')) {
+      document.querySelector('div.show-more > div.load-more > button[class="button -border"]').click();
+      await stall(3000);
+    }
   });
   return await context.extract(productDetails, { transform });
 }
-
 
 module.exports = {
   implements: 'product/search/extract',
