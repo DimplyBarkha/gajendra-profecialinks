8f9cb9ff9a23b031b64bdbbe3910082e28982d56
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-02 19:11:56 +0530

    fixed pagination

--- src/library/product/search/stores/t/totalwine/US/paginate.js
+++ src/library/product/search/stores/t/totalwine/US/paginate.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'totalwine',
     nextLinkSelector: 'a[aria-label*="Next page"]',
     mutationSelector: null,
-    spinnerSelector: null,
+    spinnerSelector: 'div[style="display: block;"] div[class*="Loader__Overlay-"]',
     loadedSelector: 'a[class="productImg__3fOOgAmO"] > picture > img',
     noResultsXPath: '//p[@class="resultsTitle__2yxTXNeW"]/span',
     zipcode: '',
