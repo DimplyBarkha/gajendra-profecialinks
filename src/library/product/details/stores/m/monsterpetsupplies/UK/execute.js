635ffeab5a9b56f5107fa52cabcec7bc36db805f
Michał Wieczorek <michal126@hotmail.com>
2020-12-02 12:12:39 +0100

    created template for monsterpetsupplies uk core

---
+++ src/library/product/details/stores/m/monsterpetsupplies/UK/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'monsterpetsupplies',
+    domain: 'monsterpetsupplies.co.uk',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
