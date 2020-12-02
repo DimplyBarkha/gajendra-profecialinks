21eafa699451aba8bf7496199ff0fe15252a3487
Karina Miernik <karina.miernik@gmail.com>
2020-12-02 10:31:38 +0100

    #2405 timeout and implementation added

--- src/library/navigation/goto/domains/bu/but.fr.js
+++ src/library/navigation/goto/domains/bu/but.fr.js
@@ -3,15 +3,15 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'but.fr',
-    timeout: null,
-    // timeout: 90000,
+    // timeout: null,
+    timeout: 90000,
     country: 'FR',
     store: 'but',
     zipcode: '',
   },
-  // implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-  //   const timeout = parameters.timeout ? parameters.timeout : 90000;
-  //   await context.setBlockAds(false);
-  //   await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
-  // },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 90000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
+  },
 };
