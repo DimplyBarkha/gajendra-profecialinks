c76c080310600935975d5e2f0bf7a481b63a437b
tapan-profecialinks <tapan@profecialinks.com>
2020-12-02 22:04:21 +0530

    core key implemented

--- src/library/navigation/goto/domains/pl/plein.nl.js
+++ src/library/navigation/goto/domains/pl/plein.nl.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'plein.nl',
-    timeout: 8000000,
+    timeout: 9000000,
     country: 'NL',
     store: 'plein',
     zipcode: '',
--- src/library/product/details/stores/p/plein/NL/format.js
+++ src/library/product/details/stores/p/plein/NL/format.js
@@ -3,41 +3,103 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
-    const cleanUp = (data, context) => {
-      const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-      }))));
-      return data;
-    };
-    
-    for (const { group } of data) {
-      for (let row of group) {
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
+  for (const { group } of data) {
+    for (const row of group) {
 
-        // if (row.description) {
-        //     let info = [];
-        //     row.description.forEach(item => {
-        //         info.push(item.text.replace(/(\s*\n\s*)+/g, ' | ').trim());
-        //     });
-
-        //     row.description = [{'text':info.join(' | '),'xpath':row.description[0].xpath}];
-        // }
-       
+      if (row.description) {
+        let info = [];
+        row.description.forEach(item => {
+            info.push(item.text.replace(/(\s*\n\s*)+/g, ' | ').trim());
+        });
+        row.description = [{'text':info.join(' | '),'xpath':row.description[0].xpath}];
       }
+
+      if (row.sku) {
+          row.sku.forEach(item => {
+            let data = JSON.parse(item.text);
+             if(data['sku']){
+              if(data['sku']){
+                 item.text = data['sku'];
+              }
+            }
+          });
+      }
+
+      if (row.brandText) {
+        row.brandText.forEach(item => {
+          let data = JSON.parse(item.text);
+           if(data['brand']){
+            if(data['brand']){
+               item.text = data['brand'];
+            }
+          }
+        });
     }
-    return cleanUp(data);
-  };
-  
-  module.exports = { transform };
+
+      if (row.variantId) {
+        let infovariant = [];
+        row.variantId.forEach(item => {
+          infovariant.push(item.text.trim());
+        });
+        row.variantId = [{'text':infovariant[0],'xpath':row.variantId[0].xpath}];
+      }
+
+      if (row.variantInformation) {
+        row.variantInformation.forEach(item => {
+          item.text =  item.text.replace(/(\s*\n\s*)+/g, ' | ').trim();
+        });
+      }
+
+      if (row.ratingCount) {
+        row.ratingCount.forEach(item => {
+          item.text =  item.text.match(/\d/g).join('').trim();
+        });
+      }
+
+
+      if (row.availabilityText) {
+        let infoAvailable = [];
+        row.availabilityText.forEach(item => {
+            infoAvailable.push(item.text.trim());
+        });
+        row.availabilityText = [{'text':infoAvailable[0],'xpath':row.availabilityText[0].xpath}];
+      }
+
+      if (row.aggregateRating) {  
+        let counter = 0;                 
+        row.aggregateRating.forEach(item => {
+            if(item.text.trim() == "fas fa-star"){
+                counter = counter + 1;
+            }else if(item.text.trim() == "fas fa-star-half-alt"){
+                counter = counter + 0.5;
+            }
+        });
+        row.aggregateRating = [{'text': counter.toFixed(1),'xpath': row.aggregateRating[0].xpath}]        
+      }
+
+    }
+  }
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  console.log(productCodes);
+  return data;
+};
+module.exports = { transform };
