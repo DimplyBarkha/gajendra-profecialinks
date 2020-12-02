587cbe3249a1af78b413d99653aef9b7e26e67ed
dhanush <dhanush.isukapalli@od.import.io>
2020-12-02 23:07:52 +0530

    mall by kavya

--- src/library/product/search/stores/m/mall/SK/extract.js
+++ src/library/product/search/stores/m/mall/SK/extract.js
@@ -1,11 +1,38 @@
 const { transform } = require('../../../../shared');
 module.exports = {
-  implements: 'product/search/extract',
-  parameterValues: {
-    country: 'SK',
-    store: 'mall',
-    transform: null,
-    domain: 'mall.sk',
-    zipcode: '',
-  },
+implements: 'product/search/extract',
+parameterValues: {
+country: 'SK',
+store: 'mall',
+transform: transform,
+domain: 'mall.sk',
+zipcode: '',
+},
+implementation,
 };
+async function implementation(
+inputs,
+parameters,
+context,
+dependencies,
+) {
+const { transform } = parameters;
+const { productDetails } = dependencies;
+await context.evaluate(async function () {
+
+function addElementToDocument(key, value) {
+const catElement = document.createElement('div');
+catElement.id = key;
+catElement.textContent = value;
+catElement.style.display = 'none';
+document.body.appendChild(catElement);
+}
+const URL = window.location.href;
+try {
+document.getElementById('pd_url').remove();
+} catch (error) {
+}
+addElementToDocument('pd_url', URL);
+});
+return await context.extract(productDetails, { transform });
+}
