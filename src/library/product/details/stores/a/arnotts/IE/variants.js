b889c8eb251568a6fea27476f46d15e339c2f40b
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2020-12-02 11:59:37 +0100

    Created a template for variants extractor and inserted selectors ( #2378)

---
+++ src/library/product/details/stores/a/arnotts/IE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'IE',
+    store: 'arnotts',
+    domain: 'arnotts.ie',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/arnotts/IE/variantsExtract.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'IE',
+    store: 'arnotts',
+    transform: null,
+    domain: 'arnotts.ie',
+    zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, { variants }) => {
+    await context.evaluate(async () => {
+      const variantsList = document.createElement('ol');
+      variantsList.id = 'added_variants_list';
+      variantsList.style.display = 'none';
+
+      const variants = document.querySelectorAll('ul.swatches.color li a');
+      const iterations = variants.length || 1;
+      for (let i = 0; i < iterations; i++) {
+        const elem = document.createElement('li');
+        const url = variants.length ? variants[i].getAttribute('href') : window.location.href;
+        const id = url.match(/pid=(.*)/) ? url.match(/pid=(.*)/)[1] : '';
+        elem.setAttribute('variant_url', url);
+        elem.setAttribute('variant_id', id);
+        variantsList.appendChild(elem);
+      }
+      document.body.appendChild(variantsList);
+    });
+    await context.extract(variants);
+  },
+};
