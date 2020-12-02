4039d1b3955b37d81f58f8668956b7c0d7265930
ganesh <ganesh@profecialinks.com>
2020-12-03 00:23:29 +0530

    Adding walmart

---
+++ src/library/navigation/goto/domains/wa/walmart.com.ar.js
@@ -0,0 +1,10 @@
+module.exports = {
+  implements: "navigation/goto",
+  parameterValues: {
+    domain: "walmart.com.ar",
+    timeout: 200000,
+    country: "AR",
+    store: "walmart",
+    zipcode: "",
+  },
+};
---
+++ src/library/product/search/stores/w/walmart/AR/search.js
@@ -0,0 +1,10 @@
+module.exports = {
+  implements: "product/search",
+  parameterValues: {
+    country: "AR",
+    timeout: 50000,
+    store: "walmart",
+    domain: "walmart.com.ar",
+    zipcode: "",
+  },
+};
