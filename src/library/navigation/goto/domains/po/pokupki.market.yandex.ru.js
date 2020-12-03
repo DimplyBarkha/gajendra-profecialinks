c33a6fad04ebda4f10b9d0a49a963cc26cd83e57
shrutikab <shrutika.bongarde@synerzip.com>
2020-12-03 18:40:37 +0530

    captch(#2521)

--- src/library/navigation/goto/domains/po/pokupki.market.yandex.ru.js
+++ src/library/navigation/goto/domains/po/pokupki.market.yandex.ru.js
@@ -35,7 +35,7 @@ module.exports = {
     console.log('Status :', responseStatus.status);
     console.log('URL :', responseStatus.url);
 
-    const captchaFrame = "iframe[sandbox='allow-same-origin allow-scripts']";
+    const captchaFrame = "iframe[_src*='captcha']:not([title]), iframe[src*='captcha']:not([title]), div#iframe-wrapper";
     try {
       await context.waitForSelector(captchaFrame);
     } catch (e) {
