da948066562c420594b1285b0679dffcdd8cdb0a
mania307 <maniakalnaa@gmail.com>
2020-12-03 13:55:34 +0100

    #2592 updated xpaths

--- src/library/product/search/stores/p/perfumespremium/ES/extract.js
+++ src/library/product/search/stores/p/perfumespremium/ES/extract.js
@@ -1,3 +1,29 @@
+const { transform } = require('../../../../shared');
+
+async function implementation(inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async () => {
+    function addElementToDocument(key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+    const searchUrl = window.location.href;
+    addElementToDocument('searchUrl', searchUrl);
+
+    const products = document.querySelectorAll('div[class="col-lg-4 col-md-4 col-sm-4 col-xs-6 item card-col"]');
+    products.forEach((product, index) => {
+      // set rank
+      product.setAttribute('rank', (index + 1).toString());
+    });
+  });
+
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
@@ -8,4 +34,5 @@ module.exports = {
     domain: 'perfumespremium.es',
     zipcode: '',
   },
+  implementation,
 };
