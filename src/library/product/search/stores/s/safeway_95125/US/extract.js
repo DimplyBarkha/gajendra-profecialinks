70189749c424559c2cdb05881c9ff19b44eb4de8
Akash Panda <akashkumar.p@osmosys.asia>
2020-12-03 17:24:47 +0530

    lint fix code

--- src/library/product/search/stores/s/safeway_95125/US/extract.js
+++ src/library/product/search/stores/s/safeway_95125/US/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'safeway_95125',
-    transform: null,
+    transform,
     domain: 'safeway.com',
     zipcode: '95125',
   },
