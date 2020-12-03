c30d2c74d152d558114741d28138e931a6eb5966
Karolina <karolina.urbanska@tdcm.io>
2020-12-03 12:11:55 +0100

    Added price selector (#1145)

--- src/library/product/search/stores/b/booker/UK/extract.js
+++ src/library/product/search/stores/b/booker/UK/extract.js
@@ -9,7 +9,6 @@ module.exports = {
   },
 
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
-
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     await context.evaluate(async function () {
@@ -22,36 +21,34 @@ module.exports = {
         document.body.appendChild(catElement);
       };
 
-    const keywords = document.querySelector('div#title h2').innerHTML ? (document.querySelector('div#title h2').innerHTML.match(/for\s"([^"]+)/) ? (document.querySelector('div#title h2').innerHTML.match(/for\s"([^"]+)/)[1] ? document.querySelector('div#title h2').innerHTML.match(/for\s"([^"]+)/)[1].replace(/\s+/g, '+') : null) : null) : null
-    const urlElem = `https://www.booker.co.uk/catalog/products.aspx?categoryName=Default%20Catalog&keywords=${keywords}&view=UnGrouped`
-    addElementToDocument('pageURL', '', urlElem)  
+      const keywords = document.querySelector('div#title h2').innerHTML ? (document.querySelector('div#title h2').innerHTML.match(/for\s"([^"]+)/) ? (document.querySelector('div#title h2').innerHTML.match(/for\s"([^"]+)/)[1] ? document.querySelector('div#title h2').innerHTML.match(/for\s"([^"]+)/)[1].replace(/\s+/g, '+') : null) : null) : null;
+      const urlElem = `https://www.booker.co.uk/catalog/products.aspx?categoryName=Default%20Catalog&keywords=${keywords}&view=UnGrouped`;
+      addElementToDocument('pageURL', '', urlElem);
 
-    const domain = 'https://www.booker.co.uk'
-    const productUrls = document.querySelectorAll('div.info_r1 a')
+      const domain = 'https://www.booker.co.uk';
+      const productUrls = document.querySelectorAll('div.info_r1 a');
 
-    productUrls.forEach((e) => {
-      const prodUrl = domain.concat(e.getAttribute('href'))
-      e.setAttribute('prodUrl', prodUrl)
-    });
+      productUrls.forEach((e) => {
+        const prodUrl = domain.concat(e.getAttribute('href'));
+        e.setAttribute('prodUrl', prodUrl);
+      });
 
-    const productThumbs = document.querySelectorAll('img.pi')
-    
-    productThumbs.forEach((e) => {
+      const productThumbs = document.querySelectorAll('img.pi');
+
+      productThumbs.forEach((e) => {
       // @ts-ignore
-      const prodThumbImg = e.currentSrc
-      e.setAttribute('prodThumb', prodThumbImg)
+        const prodThumbImg = e.currentSrc;
+        e.setAttribute('prodThumb', prodThumbImg);
+      });
+
+      const row = document.querySelectorAll('tr.pr');
+      row.forEach((e, index) => {
+        const n = (index + 1).toString();
+        e.setAttribute('index', n);
+      });
     });
 
-    const row = document.querySelectorAll('tr.pr')
-    row.forEach((e, index) => {
-      const n = (index + 1).toString()
-      e.setAttribute('index', n)
-    });
-  
-    });
-    
     return await context.extract(productDetails, { transform });
-    },
-  
-    };
+  },
 
+};
