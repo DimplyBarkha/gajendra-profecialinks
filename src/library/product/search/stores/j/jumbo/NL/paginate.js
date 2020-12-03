261d58013164c5bfc703a32c28972e7edfe7cd57
Wayne E <bartekstafiej2@gmail.com>
2020-12-03 11:53:36 +0100

    #1353 fixed pagination problem, and no results inputs

--- src/library/product/search/stores/j/jumbo/NL/paginate.js
+++ src/library/product/search/stores/j/jumbo/NL/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'NL',
     store: 'jumbo',
-    nextLinkSelector: 'span[class="content"] span[class="d-xs-inline d-l-none"]', // not pushed, check out if selector is fine
+    nextLinkSelector: null, // 'span[class="content"] span[class="d-xs-inline d-l-none"]', // not pushed, check out if selector is fine
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: '.rw',
     noResultsXPath: '//div[@class="error-state-wrapper text-center cl ctr"]/div[@class="server-error"]',
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.jumbo.com/producten/?offSet=100&searchTerms={searchTerms}&pageSize={page}',
+    },
     domain: 'jumbo.com',
     zipcode: '',
   },
