b6843123461ee0e5e62b5cf59b76e4939482a28c
sangeeta <sangeta.kumari@synerzip.com>
2020-12-03 14:07:09 +0530

    core-nespresso_ch/de #2620

---
+++ src/library/product/details/stores/n/nesspresso_ch_de/CH/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'CH',
+    store: 'nesspresso_ch_de',
+    domain: 'nespresso.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/n/nesspresso_ch_de/CH/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'CH',
+    store: 'nesspresso_ch_de',
+    transform: null,
+    domain: 'nespresso.com',
+    zipcode: '',
+  },
+};
