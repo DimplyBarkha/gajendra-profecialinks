c35e0b194a5cc94675592cde0012c9423d636b87
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-12-03 12:05:32 +0100

    - Finish working on the Dyson AE - Update tests

--- src/library/product/details/stores/d/dyson/AE/execute.js
+++ src/library/product/details/stores/d/dyson/AE/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'AE',
     store: 'dyson',
     domain: 'dyson.ae',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div.hero__body__inner',
+    noResultsXPath: '//p[contains(text(), "The page you’re looking for can’t be found")]',
     zipcode: '',
   },
 };
