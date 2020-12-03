7d0e0c06ac1f19c131cabcc2cd569a6c768d5b9d
Shreyam <shreyam.g@osmosys.asia>
2020-12-03 12:19:27 +0530

    Added xpath for fields

---
+++ src/library/product/details/stores/h/hayneedle/US/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'hayneedle.com',
+    prefix: null,
+    url: null,
+    country: 'US',
+    store: 'hayneedle',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/h/hayneedle/US/extract.js
@@ -0,0 +1,43 @@
+const { cleanUp } = require('../../../../shared');
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'hayneedle',
+    transform: cleanUp,
+    domain: 'hayneedle.com',
+    zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      let specifications = document.querySelector("#specificationsTarget > div > div");
+      if (specifications) {
+        let specificationText = specifications.textContent.match(/(Specifications)(.+)/);
+        if (specificationText) {
+          document.body.setAttribute('specifications', specificationText[2]);
+        }
+      }
+
+      let results = [];
+      let query = document.evaluate(`//div[@id="productInfoTarget"]//ul[@data-automation-id="bullets"]//li`, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
+      if (query) {
+        for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+          results.push(query.snapshotItem(i));
+        }
+      }
+
+      const table = document.createElement('table');
+      document.body.appendChild(table);
+      const tBody = document.createElement('tbody');
+      table.appendChild(tBody);
+
+      for (let index = 0; index < results.length; index++) {
+        const newlink = document.createElement('tr');
+        newlink.setAttribute('class', 'bullets');
+        newlink.setAttribute('bullet', results[index].textContent);
+        tBody.appendChild(newlink);
+      }
+    });
+    await context.extract(productDetails);
+  },
+};
