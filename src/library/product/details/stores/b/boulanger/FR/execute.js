881babd2c106c962a6a5a455a55a7e34df3c3274
Rafał Perkowski <rafalpero1@gmail.com>
2020-12-03 14:04:38 +0100

    updated noResultXpath

--- src/library/product/details/stores/b/boulanger/FR/execute.js
+++ src/library/product/details/stores/b/boulanger/FR/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'boulanger',
     domain: 'boulanger.com',
     loadedSelector: 'body[itemtype]',
-    noResultsXPath: "//div[@id='modal_fusion_mp']//h2[contains(text(), 'Attention')]",
+    noResultsXPath: "//div[@id='modal_fusion_mp']//h2[contains(text(), 'Attention')] | //h1[contains(text(), 'Oups')]",
     zipcode: "''",
   },
 };
