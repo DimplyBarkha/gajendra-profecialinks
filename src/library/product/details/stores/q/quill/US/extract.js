a089fd1e79641980f66a4cac1e7773bf3badebd7
Vivek Gondil <gondilvivek@gmail.com>
2020-12-03 19:27:59 +0530

    Added missing xpaths and required implementation

--- src/library/product/details/stores/q/quill/US/extract.js
+++ src/library/product/details/stores/q/quill/US/extract.js
@@ -1,9 +1,10 @@
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'quill',
-    transform: null,
+    transform: cleanUp,
     domain: 'quill.com',
     zipcode: '',
   },
@@ -14,8 +15,24 @@ module.exports = {
     dependencies,
   ) {
     await context.evaluate(async () => {
+      function getDivWithId (id) {
+        const div = document.createElement('div');
+        div.setAttribute('id', id);
+        return div;
+      }
       if (document.querySelector('div[id*="PopUp"]')) {
         try { document.querySelector('img[title*="Close Message"]').click(); } catch (e) { console.error(e); }
+
+        const videos = document.querySelectorAll('video');
+        if (videos) {
+          videos.forEach((v) => {
+            const div = getDivWithId('videos');
+            div.setAttribute('data', v.getAttribute('src'));
+          });
+        }
+        if (document.querySelector('div[class*="addToCartBtnLinks "] a[class*="button"] > span')) {
+          document.querySelector('div[class*="addToCartBtnLinks "] a[class*="button"] > span').innerText = 'In Stock';
+        }
       }
     });
     const { transform } = parameters;
