1795b38f1924d92444b40c9281cea67d8aa5617c
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-03 22:56:51 +0530

    deployed zalando uk offers

--- src/library/product/offers/stores/z/zalando/UK/extract.js
+++ src/library/product/offers/stores/z/zalando/UK/extract.js
@@ -1,5 +1,18 @@
 const { transform } = require('../../../../shared');
-const { implementation } = require('../shared');
+const { addDynamicTable, getData } = require('../sharedExtract');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productOffers } = dependencies;
+  const data = await context.evaluate(getData);
+  await context.evaluate(addDynamicTable, data, 'footer');
+  return await context.extract(productOffers, { transform });
+}
 module.exports = {
   implements: 'product/offers/extract',
   parameterValues: {
