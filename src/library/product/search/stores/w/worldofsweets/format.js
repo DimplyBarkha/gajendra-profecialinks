43cf2af8eb77085ee65cdf72ece7fddbf72b866c
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2020-12-03 14:55:53 +0530

    exctract.yml & format.js updated

--- src/library/product/search/stores/w/worldofsweets/format.js
+++ src/library/product/search/stores/w/worldofsweets/format.js
@@ -1,55 +1,69 @@
-
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
-    const cleanUp = (data, context) => {
-      const clean = text => text.toString()
-        .replace(/\r\n|\r|\n/g, ' ')
-        .replace(/&amp;nbsp;/g, ' ')
-        .replace(/&amp;#160/g, ' ')
-        .replace(/\u00A0/g, ' ')
-        .replace(/\s{2,}/g, ' ')
-        .replace(/"\s{1,}/g, '"')
-        .replace(/\s{1,}"/g, '"')
-        .replace(/^ +| +$|( )+/g, ' ')
-        // eslint-disable-next-line no-control-regex
-        .replace(/[\x00-\x1F]/g, '')
-        .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const state = context.getState();
+  console.log("state",state);
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
+  for (const { group } of data) {
+    for (const row of group) {
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+      Object.keys(row).forEach(header => row[header].forEach(el => {
         el.text = clean(el.text);
-      }))));
-      return data;
-    };
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.ratingCount) {
-          row.ratingCount.forEach(item => {
-            item.text = item.text.replace('(', ' ').trim();
-            item.text = item.text.replace(')', '').trim();
-          });
-        }
-        if (row.price) {
-          row.price.forEach(item => {
-            item.text = item.text.replace('€', ' ').trim();
-          });
-        }
-        if (row.listPrice) {
-          row.listPrice.forEach(item => {
-            item.text = item.text.replace('€', ' ').trim();
-          });
-        }
-        if (row.priceCurrency) {
-          row.priceCurrency.forEach(item => {
-            item.text = item.text.slice(-1);
-          });
-        }
+      }));
+      if (row.ratingCount) {
+        row.ratingCount.forEach(item => {
+          item.text = item.text.replace('(', ' ').trim();
+          item.text = item.text.replace(')', '').trim();
+        });
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace('€', ' ').trim();
+        });
+      }
+      if (row.listPrice) {
+        row.listPrice.forEach(item => {
+          item.text = item.text.replace('€', ' ').trim();
+        });
+      }
+      if (row.priceCurrency) {
+        row.priceCurrency.forEach(item => {
+          item.text = item.text.slice(-1);
+        });
       }
     }
-    return cleanUp(data);
-  };
-  
-  module.exports = { transform };
+    
+
+  }
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  console.log(productCodes);
+  return data;
+};
+
+
+module.exports = { transform };
+
