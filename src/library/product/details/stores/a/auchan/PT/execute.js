e8a93b2954b191ea9192f2e7eb361f83045ef6cd
Lokanath sahoo <loknath.s@osmosys.asia>
2020-12-03 20:19:42 +0530

    #1416 fixed the IM issue

--- src/library/product/details/stores/a/auchan/PT/execute.js
+++ src/library/product/details/stores/a/auchan/PT/execute.js
@@ -4,8 +4,8 @@ module.exports = {
     country: 'PT',
     store: 'auchan',
     domain: 'auchan.pt',
-    loadedSelector: 'div[class="product-item-header"] > a',
-    noResultsXPath: '//div[contains(@class,"no-results")]',
+    loadedSelector: null,
+    noResultsXPath: null,
     zipcode: '',
   },
 };
