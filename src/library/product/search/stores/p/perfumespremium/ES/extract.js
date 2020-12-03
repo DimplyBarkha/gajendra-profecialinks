9dbde0a9021d708b937d18c3d53f0c5923f71d80
mania307 <maniakalnaa@gmail.com>
2020-12-03 16:23:07 +0100

    #2592 scroll implementation

--- src/library/product/search/stores/p/perfumespremium/ES/extract.js
+++ src/library/product/search/stores/p/perfumespremium/ES/extract.js
@@ -4,6 +4,7 @@ async function implementation(inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
+
   await context.evaluate(async () => {
     function addElementToDocument(key, value) {
       const catElement = document.createElement('div');
@@ -20,6 +21,24 @@ async function implementation(inputs, parameters, context, dependencies) {
       // set rank
       product.setAttribute('rank', (index + 1).toString());
     });
+
+    let scrollTop = 0;
+    while (scrollTop !== 8000) {
+      await stall(1000);
+      scrollTop += 1000;
+      window.scroll(0, scrollTop);
+      if (scrollTop === 8000) {
+        await stall(1000);
+        break;
+      }
+    }
+    function stall(ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
   });
 
   return await context.extract(productDetails, { transform });
