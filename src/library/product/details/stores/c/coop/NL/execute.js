4ab2a6043b39e36b7f8dc00aae524f43aff1e0ff
Gajender Ramola <ramola106@gmail.com>
2020-12-03 14:26:51 +0530

    #1694 fix ingredents issue

--- src/library/product/details/stores/c/coop/NL/execute.js
+++ src/library/product/details/stores/c/coop/NL/execute.js
@@ -1,4 +1,38 @@
+/**
+ *
+ * @param { { url?: string,  id?: string, zipcode?: any, storeId?:any} } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action, createUrl: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  let { url, id, zipcode, storeId } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  await dependencies.goto({ url, zipcode, storeId });
+  try {
+    await context.click('dl.definitionList dt:nth-child(3)');
+  } catch (e) {
+    console.log(e);
+  }
+  await new Promise((resolve, reject) => setTimeout(resolve, 12000));
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
 
+  // TODO: Check for not found?
+}
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -9,4 +43,5 @@ module.exports = {
     noResultsXPath: "//h1[contains(text(),'We kunnen de pagina die je zoekt niet vinden of het product dat je')]",
     zipcode: '',
   },
+  implementation,
 };
