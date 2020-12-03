decc2ee912d2e95daae0b95f057454bfb3c68d7f
BorysMaciuch <borysmaciuch@gmail.com>
2020-12-03 11:16:19 +0100

    #1580 fix description, add shared

--- src/library/product/details/stores/p/piperitas/IT/extract.js
+++ src/library/product/details/stores/p/piperitas/IT/extract.js
@@ -1,17 +1,20 @@
+const { transform } = require('../shared');
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'IT',
     store: 'piperitas',
-    transform: null,
+    transform,
     domain: 'piperitas.com',
     zipcode: '',
   },
   implementation: async (
     { inputString },
-    { country, domain },
+    { country, domain, transform },
     context,
     { productDetails },
+
   ) => {
     await context.evaluate(async function () {
       const rating = document.querySelector('.fdt_productsealblackwhite_ratx');
@@ -20,6 +23,6 @@ module.exports = {
         rating.setAttribute('rating', ratingConverted);
       }
     });
-    return await context.extract(productDetails);
+    return await context.extract(productDetails, { transform });
   },
 };
