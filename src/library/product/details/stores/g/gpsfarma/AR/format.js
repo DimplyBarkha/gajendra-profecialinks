b9ba2f33f76738ce66489ecbe4365e14ce6cce3a
ganesh <ganesh@profecialinks.com>
2020-12-03 15:41:33 +0530

    QA fix

--- src/library/product/details/stores/g/gpsfarma/AR/format.js
+++ src/library/product/details/stores/g/gpsfarma/AR/format.js
@@ -28,7 +28,7 @@ const transform = (data) => {
         if (row.description) {
             let info = [];          
             row.description.forEach(item => {
-              info.push(item.text.replace(/(\s*\n\s*)+/g, ' ').trim());            
+              info.push(item.text.replace(/(\s*\n\s*)+/g, ', ').trim());            
             });
             row.description = [{'text':info.join(' | '),'xpath':row.description[0].xpath}];
         }
@@ -40,11 +40,52 @@ const transform = (data) => {
             });          
             row.specifications = [{'text':info.join(' || '),'xpath':row.specifications[0].xpath}];          
         }
-        
+        if (row.quantity) {          
+          row.quantity.forEach(item => {
+            var matches = /(\d+\s*ml)/isg.exec(item.text);
+            var matches1 = /(\d+\s*gr)/isg.exec(item.text);
+            var matches2 = /(\d+\s*g)/isg.exec(item.text);
+            console.log(matches);
+            if (matches){
+              item.text = matches[1];
+            }
+            else if (matches1){
+              item.text = matches1[1];
+            }
+            else if (matches2){
+              item.text = matches2[1];
+            }
+            else{
+              delete row.quantity;
+              return false;
+            }
+          });
+        }
         
         if (row.descriptionBullets) {
           row.descriptionBullets = [{'text':row.descriptionBullets.length,'xpath':row.descriptionBullets[0].xpath}];
-        }                
+        }
+
+        if (row.manufacturer) {            
+            row.manufacturer.forEach(item => {              
+              var matches = /.*window\.dataLayer\.push\((.+)\)\;.*/isg.exec(item.text);              
+              if (matches){                
+                try {
+                  let json_data = JSON.parse(matches[1]);
+                  if (json_data['product']['manufacturer']){
+                    item.text = json_data['product']['manufacturer'];
+                  }
+                } catch (error) {
+                  delete row.manufacturer;
+                  return false;
+                }
+              }
+              else{
+                delete row.manufacturer;
+                return false;
+              }
+            });
+        }
       }
     }
     return cleanUp(data);
