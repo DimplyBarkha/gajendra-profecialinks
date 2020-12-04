ffa3c92230307b58ee8ebcfbd8c099c2ab25b214
Nilesh <nilesh.b@osmosys.asia>
2020-12-04 18:11:41 +0530

    Fixed issue.

--- src/library/product/details/stores/v/voila/shared.js
+++ src/library/product/details/stores/v/voila/shared.js
@@ -4,42 +4,40 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.dietarySymbols) {
-          let text = '';
-          row.dietarySymbols.forEach(item => {
-            text = row.dietarySymbols.map(elm => elm.text).join(' | ');
-          });
-          row.dietarySymbols = [
-            {
-              text: text,
-            },
-          ];
-        } 
-  
-  
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.dietarySymbols) {
+        let text = '';
+        row.dietarySymbols.forEach(item => {
+          text = row.dietarySymbols.map(elm => elm.text).join(' | ');
+        });
+        row.dietarySymbols = [
+          {
+            text: text,
+          },
+        ];
       }
     }
-  
-    const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-  
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      el.text = clean(el.text);
-    }))));
-  
-    return data;
-  };
-  
-  module.exports = { transform };
+  }
+
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+
+  return data;
+};
+
+module.exports = { transform };
