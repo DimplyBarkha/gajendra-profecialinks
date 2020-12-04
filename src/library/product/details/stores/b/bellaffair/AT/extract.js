71cd7258882ef4aa37812a40dd0e4350dd59e142
astropaula <wojciechowska.fran@gmail.com>
2020-12-04 18:27:59 +0100

    #2393 Updated extract.yaml schema, changed few fields, added fields and data conversion to extract.js

--- src/library/product/details/stores/b/bellaffair/AT/extract.js
+++ src/library/product/details/stores/b/bellaffair/AT/extract.js
@@ -18,6 +18,23 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
+        // add alternateImages src
+        var alternativeImg = document.querySelectorAll('div[class="thumbs clear"] span:not(.active) a');
+        if (alternativeImg !== null) {
+          for (let i = 0; i < alternativeImg.length; i++) {
+          const element = document.createElement('div');
+          const parent = document.querySelector('div[class="thumbs clear"]');
+          element.id = 'altImg';
+          element.title = alternativeImg[i].href;
+          element.style.display = 'none';
+          parent.appendChild(element);
+          }
+        }
+        // add descrBullets field
+        var descBullets = document.querySelectorAll('div[class="description"] div[class="left"] li');
+        if (descBullets !== null) {
+          addElementToDocument ('bullets', descBullets.length);
+        }      
       const pdfPresent = document.querySelector('ul.downloads>li.pdf a')
         ? document.querySelector('ul.downloads>li.pdf a').getAttribute('href') : '';
       if (pdfPresent) {
@@ -39,7 +56,23 @@ module.exports = {
       const shippingInfoXpath = document.evaluate('//div[@class="block third-block"][p[contains(text(),"Versandkosten")]]', document, null, XPathResult.STRING_TYPE, null);
       const shippingInfo = shippingInfoXpath ? shippingInfoXpath.stringValue.replace(/\n{2,}/g, '\n') : '';
       if (shippingInfo) addElementToDocument('shippingInfo', shippingInfo);
-    });
-    await context.extract(productDetails);
+    }); 
+    var data = await context.extract(productDetails);
+    for (let k = 0; k < data.length; k++) {
+      for (let i = 0; i < data[k].group.length; i++) {
+        if ('description' in data[k].group[i] && data[k].group[i].description.length) {
+          for(let j = 1; j < data[k].group[i].description.length; j++){
+            data[k].group[i].description[0].text += ' ' + data[k].group[i].description[j].text;
+          }
+          data[k].group[i].description = data[k].group[i].description.slice(0, 1);
+        }
+        if ('availabilityText' in data[k].group[i]) {
+          if (data[k].group[i].availabilityText[0].text !== 'Out of Stock') {
+            data[k].group[i].availabilityText[0].text = 'In Stock';
+          }
+        }
+      }
+    }
+    return data;
   },
 };
