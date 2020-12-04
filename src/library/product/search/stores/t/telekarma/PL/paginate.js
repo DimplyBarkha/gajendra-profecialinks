33fb587d4101b726c8c8a19f5dfb5fc40a5d642a
Aditi-ux <aditi@profecialinks.com>
2020-12-05 01:25:52 +0530

    Updates

--- src/library/product/search/stores/t/telekarma/PL/paginate.js
+++ src/library/product/search/stores/t/telekarma/PL/paginate.js
@@ -4,6 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'PL',
     store: 'telekarma',
+    mutationSelector: '.d-none.d-sm-inline-block.findproducts b',
     nextLinkSelector: 'div.jumpBox input[type=image]',
     loadedSelector: 'div#srodek',
     noResultsXPath: '//div[contains(@class,"contentPadding")][contains(.,"Nie znaleziono.")]',
