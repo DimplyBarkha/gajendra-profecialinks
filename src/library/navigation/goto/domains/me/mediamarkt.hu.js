f4da12140d6d6d94cbc8c6d7d8d0d4dc0770800a
atish.shelke <atish.shelke@sz.import.io>
2020-12-05 01:17:47 +0530

    mediamarkt hu search

---
+++ src/library/navigation/goto/domains/me/mediamarkt.hu.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'mediamarkt.hu',
+    timeout: 1000000,
+    country: 'HU',
+    store: 'mediamarkt',
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/search/stores/m/mediamarkt/HU/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'HU',
+    store: 'mediamarkt',
+    domain: 'mediamarkt.hu',
+    url: 'https://www.mediamarkt.hu/hu/search.html?query={searchTerms}&searchProfile=onlineshop&channel=mmhuhu',
+    loadedSelector: 'ul[data-gtm-prop-list-name="Search result list"]',
+    noResultsXPath: '//div[@id="nincstalalat"]',
+    zipcode: "''",
+  },
+};
