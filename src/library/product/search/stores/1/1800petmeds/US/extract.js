92d0e8e808dd0a35e5afa24ea2889309854dcba5
waquas ali <waquas2ali@gmail.com>
2020-12-04 21:54:35 +0530

    Updating changes

--- src/library/product/search/stores/1/1800petmeds/US/extract.js
+++ src/library/product/search/stores/1/1800petmeds/US/extract.js
@@ -4,11 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: '1800petmeds',
-<<<<<<< HEAD
     transform,
-=======
-    transform: transform,
->>>>>>> a6a45fa11f5bf6be59c1b89b743e414effaaddde
     domain: '1800petmeds.com',
     zipcode: '',
   },
--- src/library/product/search/stores/1/1800petmeds/US/paginate.js
+++ src/library/product/search/stores/1/1800petmeds/US/paginate.js
@@ -1,6 +1,5 @@
 
 module.exports = {
-<<<<<<< HEAD
   implements: 'navigation/paginate',
   parameterValues: {
     template: null,
@@ -15,17 +14,6 @@ module.exports = {
     noResultsXPath: null,
     stopConditionSelectorOrXpath: null,
     resultsDivSelector: null,
-=======
-  implements: 'product/search/paginate',
-  parameterValues: {
-    country: 'US',
-    store: '1800petmeds',
-    nextLinkSelector: null,
-    mutationSelector: null,
-    spinnerSelector: null,
-    loadedSelector: null,
-    noResultsXPath: null,
->>>>>>> a6a45fa11f5bf6be59c1b89b743e414effaaddde
     openSearchDefinition: null,
     domain: '1800petmeds.com',
     zipcode: '',
