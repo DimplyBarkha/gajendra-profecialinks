833fa639a1d2675dd95e5e8aa59e395c57e4ecaf
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-04 11:36:11 +0530

    updated goto

--- src/library/navigation/goto/domains/kr/kroger.com.js
+++ src/library/navigation/goto/domains/kr/kroger.com.js
@@ -20,5 +20,12 @@ module.exports = {
       await context.waitForXPath("//li[@itemprop='review']", { timeout: 10000 })
         .catch(() => console.log('waited for reviews to load, none found '));
     }
+    const notAvailable = await context.evaluate((xp) => !!document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext(), '//h2[contains(.,"Product Information is unavailable at this time.")]');
+    if (notAvailable) {
+      await dependencies.setZipCode({ url, zipcode });
+      if (await context.evaluate((xp) => !!document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext(), '//h2[contains(.,"Product Information is unavailable at this time.")]')) {
+        throw new Error('Product Unavailable page.');
+      }
+    }
   },
 };
