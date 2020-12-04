de181c1fa85742d96b5aa9bbd6a76e0744d4e7f9
Harshkha <harsh.khare-od@partner.import.io>
2020-12-04 23:00:21 +0530

    IM complete

---
+++ src/library/product/details/stores/o/otto_de/DE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'otto_de',
+    domain: 'otto.de',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/o/otto_de/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'otto_de',
+    transform: null,
+    domain: 'otto.de',
+    zipcode: '',
+  },
+};
