3c1e2631ee2acbccf8717ae0e3fd1fc3b28032e9
Michał Wieczorek <michal126@hotmail.com>
2020-12-04 17:27:43 +0100

    created template for emag hu core extractor

---
+++ src/library/product/details/stores/e/emag/HU/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'emag.hu',
+    prefix: null,
+    url: null,
+    country: 'HU',
+    store: 'emag',
+    zipcode: '',
+  },
+};
