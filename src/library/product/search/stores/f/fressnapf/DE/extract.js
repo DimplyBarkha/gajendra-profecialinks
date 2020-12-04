50caa47da4caf63baa0c2089a869ef263f8d4c4e
sobha rani <amol.thukrul-od@partner.import.io>
2020-12-04 20:38:40 +0530

    fressnapf search QA complete

--- src/library/product/search/stores/f/fressnapf/DE/extract.js
+++ src/library/product/search/stores/f/fressnapf/DE/extract.js
@@ -8,4 +8,28 @@ module.exports = {
     domain: 'fressnapf.de',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async () => {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(1000);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(1000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
