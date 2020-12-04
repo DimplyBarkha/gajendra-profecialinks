4561951cc0c010b707dbc2d8575c487ef061026c
sasz94 <sacha.bartlomiej@gmail.com>
2020-12-04 14:34:18 +0100

    Deployed search extractor and run tests

--- src/library/product/search/stores/f/farmaline/BE/extract.js
+++ src/library/product/search/stores/f/farmaline/BE/extract.js
@@ -9,20 +9,4 @@ module.exports = {
     domain: 'farmaline.be',
     zipcode: '',
   },
-  implementation: async (inputs, parameters, context, dependencies) => {
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    await context.evaluate(async function () {
-      function addElementToDocument (key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
-      }
-      const searchUrl = window.location.href;
-      addElementToDocument('searchUrl', searchUrl);
-    });
-    return await context.extract(productDetails, { transform });
-  },
 };
