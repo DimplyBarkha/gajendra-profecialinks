4f09126228f8344949f381068fc29eb99e22ffb0
sasz94 <sacha.bartlomiej@gmail.com>
2020-12-04 13:40:14 +0100

    Filled more xpaths

--- src/library/product/search/stores/f/farmaline/BE/extract.js
+++ src/library/product/search/stores/f/farmaline/BE/extract.js
@@ -1,11 +1,28 @@
+const { transform } = require('../../../../shared');
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'BE',
     store: 'farmaline',
-    transform: null,
+    transform,
     domain: 'farmaline.be',
     zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const searchUrl = window.location.href;
+      addElementToDocument('searchUrl', searchUrl);
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
