3839bcddde313376d3c2ec676fcf57ebeae5564e
Mamatha-Gundapaneni <mamatha.g@osmosys.asia>
2020-12-04 18:29:50 +0530

    Updated code

--- src/library/product/details/stores/s/santediscount/FR/extract.js
+++ src/library/product/details/stores/s/santediscount/FR/extract.js
@@ -16,26 +16,17 @@ module.exports = {
   ) => {
     const { productDetails } = dependencies;
     const { transform } = parameters;
-    if (inputs.id) {
-      await context.evaluate(async function (inputs) {
-        const optionalWait = async (sel) => {
-          try {
-            await context.waitForSelector(sel, { timeout: 60000 });
-          } catch (err) {
-            console.log(`Couldn't load selector => ${sel}`);
-          }
-        };
 
-        async function getReviews(url) {
-          const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-          return response.json();
-        };
-        const isSearchPage = document.querySelector('body[class*="catalogsearch"]') ? document.querySelector('body[class*="catalogsearch"]')  : null; 
+    if (inputs.id) {
+      const optionalWait = async (sel) => {
+        try {
+          await context.waitForSelector(sel, { timeout: 60000 });
+        } catch (err) {
+          console.log(`Couldn't load selector => ${sel}`);
+        }
+      };
+      await context.evaluate(async function () {
+        const isSearchPage = document.querySelector('body[class*="catalogsearch"]') ? document.querySelector('body[class*="catalogsearch"]') : null;
         const noResultsSelector = document.querySelector('div.catalogsearch_no_result--content');
         if (noResultsSelector) {
           throw new Error('No results for this RPC');
@@ -45,22 +36,33 @@ module.exports = {
           try {
             isSelector.click();
             optionalWait('section.product-view-essential');
-            const response = await fetch(`https://api.bazaarvoice.com/data/display/0.2alpha/product/summary?PassKey=ca4sDXq7iXS9YMdnATzw4Opq9kFefC7YimHDEI3WkMLgE&productid=2024163&contentType=reviews,questions&reviewDistribution=primaryRating,recommended&rev=0&contentlocale=fr_FR`);
-              if (response.status !== 404) {
-                var json = await response.json();
-                console.log(JSON.stringify(json));
-              } else {
-                console.log('404');
-              }
-            // optionalWait('section.product-view-essential');
-            document.querySelector('section.product-view-essential').setAttribute('review',json.reviewSummary.numReviews);
           } catch (err) {
-            console.log(json.reviewSummary.numReviews);
             console.log('Not clicked' + err);
           }
         }
-      }, inputs);
+      });
     }
-    return await context.extract(productDetails, { transform });
+
+    await context.evaluate(async function (inputs) {
+      const optionalWait = async (sel) => {
+        try {
+          await context.waitForSelector(sel, { timeout: 60000 });
+        } catch (err) {
+          console.log(`Couldn't load selector => ${sel}`);
+        }
+      };
+      const response = await fetch(`https://api.bazaarvoice.com/data/display/0.2alpha/product/summary?PassKey=ca4sDXq7iXS9YMdnATzw4Opq9kFefC7YimHDEI3WkMLgE&productid=${inputs.id}&contentType=reviews,questions&reviewDistribution=primaryRating,recommended&rev=0&contentlocale=fr_FR`);
+      console.log(`https://api.bazaarvoice.com/data/display/0.2alpha/product/summary?PassKey=ca4sDXq7iXS9YMdnATzw4Opq9kFefC7YimHDEI3WkMLgE&productid=${inputs.id}&contentType=reviews,questions&reviewDistribution=primaryRating,recommended&rev=0&contentlocale=fr_FR`)
+      console.log(`MAmatha${response}`);
+      if (response.status !== 404) {
+        var json = await response.json();
+        console.log(`MAmatha${json}`);
+        document.querySelector('section.product-view-essential').setAttribute('review', json.reviewSummary.numReviews);
+      } else {
+        console.log('404');
+      }
+      optionalWait('section.product-view-essential');
+    }, inputs);
+    await context.extract(productDetails, { transform });
   },
 };
