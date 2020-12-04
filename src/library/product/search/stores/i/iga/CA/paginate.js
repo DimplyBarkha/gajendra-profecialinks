85c48796434b7d97c3af6f7a3610d8a27bb756ac
vinod chavan <vinodchavan102@gmail.com>
2020-12-04 15:05:21 +0530

    #705. iga ca search - fixed records count issue

--- src/library/product/search/stores/i/iga/CA/paginate.js
+++ src/library/product/search/stores/i/iga/CA/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'CA',
     store: 'iga',
-    nextLinkSelector: 'div[class="palm--soft--top"] ul[class*="nav nav--block pagination"] li a[class="icon--arrow-skinny-right"]',
+    nextLinkSelector: null, // 'div[class="palm--soft--top"] ul[class*="nav nav--block pagination"] li a[class="icon--arrow-skinny-right"]',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: null,
     noResultsXPath: "//h2[contains(@class,'h3-like')]/parent::div[@class='grid__item']",
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.iga.net/search?t=%7bD9CE4CBE-C8C3-4203-A58B-7CF7B830880E%7d&k={searchTerms}&page={page}',
+    },
     domain: 'iga.net',
     zipcode: '',
   },
