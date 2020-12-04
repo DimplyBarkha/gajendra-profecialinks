b53e68bcf2ea169ebd4b6dd1aac3ee0abc2d2bf2
Narendra Ghanta <narendra_g@hcl.com>
2020-12-04 15:17:05 +0530

    fixes

--- src/library/product/search/stores/d/dosfarma/ES/extract.js
+++ src/library/product/search/stores/d/dosfarma/ES/extract.js
@@ -1,5 +1,38 @@
-
 const { transform } = require('../../../../shared');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        document.querySelector('section .ebx-scroll').scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await context.waitForSelector('section[class="ebx-result-figure ebx-result__figure"] img', {}, { timeout: 50000 });
+  await applyScroll(context);
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,4 +42,6 @@ module.exports = {
     domain: 'dosfarma.com',
     zipcode: '',
   },
+  implementation,
+
 };
