85aa136cd9562fc2ffc6cd6b40e639d01e220da7
srishivams <srishivams97@gmail.com>
2020-12-04 23:09:05 +0530

    handled error

--- src/library/product/details/stores/k/kastner-oehler/AT/extract.js
+++ src/library/product/details/stores/k/kastner-oehler/AT/extract.js
@@ -10,6 +10,9 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+    let productUrl=await context.evaluate(async function(){
+      return document.URL;
+    });
     await context.evaluate(async function () {
       const cookies = document.querySelector('span.tao_button_cookie_settings');
       if (cookies) cookies.click();
@@ -54,8 +57,51 @@ module.exports = {
         // @ts-ignore
         nameExtended.content = newNameExtended;
       }
+
+    });
+    let iframeLink=await context.evaluate(async function(){
+      let iframeLink=null;
+      document.querySelector('footer').scrollIntoView({behavior: 'smooth'});
+      function stall (ms)
+      {
+      return new Promise((resolve, reject) => {
+      setTimeout(() => {
+      resolve();
+      }, ms);
+      });
+      }
+      await stall(15000);
+      if(document.querySelector('div[class*="site_inner"] iframe'))
+        iframeLink=document.querySelector('div[id*="loadBeeContainer"] iframe').getAttribute('src');
+      return iframeLink;
     });
 
+    let enhancedContent=null;
+    if(iframeLink!==null){
+      await context.goto(iframeLink, { timeout: 5000, waitUntil: 'load', checkBlocked: true });
+       enhancedContent= await context.evaluate(async function(){
+        let enhancedContent='';
+        let fontText1=document.querySelectorAll('div[class*="pic-text"] div font font');
+        for(let i=0;i<fontText1.length;i++) enhancedContent+=fontText1[i].innerText+' || ';
+        let fontText2=document.querySelectorAll('div[class*="animation-text"]  font font');
+        for(let i=0;i<fontText2.length;i++) enhancedContent+=fontText2[i].innerText+'||';
+        return enhancedContent;
+      });
+    }
+    else console.log('iframe link not loaded');
+    await context.goto(productUrl, { timeout: 5000, waitUntil: 'load', checkBlocked: true });
+    if(enhancedContent!==null){
+      await context.evaluate(async function(enhancedContent){
+        function addHiddenDiv (id, content) {
+          const newDiv = document.createElement('div');
+          newDiv.id = id;
+          newDiv.textContent = content;
+          newDiv.style.display = 'none';
+          document.body.appendChild(newDiv);
+        }
+        addHiddenDiv('enhancedContent',enhancedContent);
+      },enhancedContent)
+    }
     await context.extract(productDetails, {transform: transformParam});
   },
 };
