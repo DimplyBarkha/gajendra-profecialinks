ad4ebd1f1e6f2e1eb7417569f45c1a601a97fb1b
Michał Wieczorek <michal126@hotmail.com>
2020-12-04 17:31:55 +0100

    emag hu search cleanup (#2799)

--- src/library/product/search/stores/e/emag/HU/extract.js
+++ src/library/product/search/stores/e/emag/HU/extract.js
@@ -1,11 +1,5 @@
 const { transform } = require('../../../../shared');
 
-async function implementation (inputs, parameters, context, dependencies) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-
-  return await context.extract(productDetails, { transform });
-}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -15,5 +9,4 @@ module.exports = {
     domain: 'emag.hu',
     zipcode: '',
   },
-  implementation,
 };
