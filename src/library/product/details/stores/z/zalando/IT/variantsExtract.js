7da2972c263d6b40d060097caed0ad92670c9310
Lokanath sahoo <loknath.s@osmosys.asia>
2020-12-04 16:40:58 +0530

    created the extractor and started working

---
+++ src/library/product/details/stores/z/zalando/IT/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'IT',
+    store: 'zalando',
+    transform: null,
+    domain: 'zalando.it',
+    zipcode: "''",
+  },
+};
