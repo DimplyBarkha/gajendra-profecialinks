55901dad54337f7f7b0326aff277a3d4feb9b8ee
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2020-12-04 13:09:56 +0100

    Removed unnecessary code, inserted selectors, deployed core NZ extractor (#2474)

--- src/library/product/details/stores/d/dyson/NZ/extract.js
+++ src/library/product/details/stores/d/dyson/NZ/extract.js
@@ -19,14 +19,6 @@ async function implementation (
   await stall(5000);
 
   await context.evaluate(async function () {
-    function stall (ms) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve();
-        }, ms);
-      });
-    }
-
     function addHiddenDiv (id, content) {
       const newDiv = document.createElement('div');
       newDiv.id = id;
@@ -35,43 +27,9 @@ async function implementation (
       document.body.appendChild(newDiv);
     }
 
-    addHiddenDiv('url', window.location.href);
-    addHiddenDiv('pageTimeStamp', new Date());
-
-    if (document.querySelector('h5') && document.querySelector('h5').innerText.split(':')[1]) {
-      addHiddenDiv('sku', document.querySelector('h5').innerText.split(':')[1]);
-    } else {
-      document.querySelectorAll('small').forEach(el => {
-        if (el.innerText.includes('SKU:')) {
-          addHiddenDiv('sku', el.innerHTML.split('<br>')[0].replace('SKU: ', ''));
-        }
-      });
-    }
-
-    if (!document.getElementById('sku')) {
-      document.querySelectorAll('script').forEach(script => {
-        const matches = script.innerText.match(/productSKU: \"[0-9]+\-[0-9]+\"/);
-        if (matches && matches.length) {
-          addHiddenDiv('sku', matches[0].replace('productSKU: ', '').replace(/"/g, ''));
-        }
-      });
-    }
-
-    if (document.querySelector('.bv-off-screen')) {
-      addHiddenDiv('rating', document.querySelector('.bv-off-screen').innerText.split(' ')[0]);
-    }
-    if (document.querySelector('.dyson-bazaarvoice__reviews-link')) {
-      addHiddenDiv('reviewCount', document.querySelector('.dyson-bazaarvoice__reviews-link').innerText.split(' ')[0]);
-    } else {
-      addHiddenDiv('reviewCount', 0);
-    }
-
     document.querySelectorAll('.icon-info__body').forEach(el => {
       if (el.innerText.includes('year guarantee')) {
         addHiddenDiv('warranty', el.innerText);
-      }
-      if (el.innerText.includes('shipping')) {
-        addHiddenDiv('shippingInfo', el.innerText);
       }
     });
 
@@ -81,10 +39,6 @@ async function implementation (
           addHiddenDiv('warranty', el.innerText);
         }
       });
-    }
-
-    if (document.querySelector('.hgroup__prefix.hgroup__prefix--lead')) {
-      addHiddenDiv('name', document.querySelector('.hgroup__prefix.hgroup__prefix--lead').childNodes[0].innerText);
     }
 
     let outOfStock = false;
@@ -99,7 +53,6 @@ async function implementation (
       addHiddenDiv('availabilityText', 'In Stock');
     }
 
-    const manufacturerImages = [];
     let enhancedContent = '';
     document.querySelectorAll('.layout').forEach(el => {
       if (el.querySelector('h2') &&
@@ -107,36 +60,18 @@ async function implementation (
       el.querySelector('h2').innerText.includes('Key features') ||
       el.querySelector('h2').innerText.includes('All features'))) {
         enhancedContent += el.innerText + ' ';
-        el.querySelectorAll('img').forEach(img => {
-          manufacturerImages.push(img.getAttribute('src'));
-        });
       }
     });
-    if (document.querySelector('.spec-set__image-set__image')) {
-      document.querySelector('.spec-set__image-set__image').querySelectorAll('img').forEach(img => {
-        manufacturerImages.push(img.getAttribute('src'));
-      });
-    }
     if (enhancedContent) {
       addHiddenDiv('hasEnhancedContent', 'Yes');
     }
     addHiddenDiv('enhancedContent', enhancedContent);
-    addHiddenDiv('manufacturerImages', manufacturerImages.join(' | '));
 
-    const alternateImages = [];
     if (document.querySelector('button[data-modal="product_mini_gallery_video"]')) {
-      document.querySelector('button[data-modal="product_mini_gallery_video"]').click();
-      await stall(1000);
       if (document.querySelector('video')) {
         addHiddenDiv('videos', document.querySelector('video').querySelector('source').getAttribute('src'));
-      }
-      document.getElementById('product_mini_gallery_video').querySelectorAll('picture').forEach(pic => {
-        if (pic.querySelector('img')) {
-          alternateImages.push(pic.querySelector('img').getAttribute('src'));
-        }
-      });
-    }
-    addHiddenDiv('alternateImages', alternateImages.join(' | '));
+      };
+    };
 
     const specifications = [];
     document.querySelectorAll('.spec').forEach(spec => {
