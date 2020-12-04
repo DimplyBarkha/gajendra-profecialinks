957268b7796a48a510904b638e1550e67aad3df1
Karina Miernik <karina.miernik@gmail.com>
2020-12-04 15:15:49 +0100

    xpaths added

--- src/library/product/search/stores/e/e-bebek/TR/execute.js
+++ src/library/product/search/stores/e/e-bebek/TR/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'TR',
     store: 'e-bebek',
     domain: 'e-bebek.com',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.e-bebek.com/search?text={searchTerms}',
+    loadedSelector: 'section.product-list',
+    noResultsXPath: '//div[@class="product-detail-main bg-white static static-page"]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/e/e-bebek/TR/extract.js
+++ src/library/product/search/stores/e/e-bebek/TR/extract.js
@@ -1,11 +1,38 @@
+const { transform } = require('../../../../shared');
+async function implementation (inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async () => {
+    function addElementToDocument (id, value) {
+      const catElement = document.createElement('div');
+      catElement.id = id;
+      catElement.innerText = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    };
+    const searchUrl = window.location.href;
+    addElementToDocument('searchurl', searchUrl);
+    const products = document.querySelectorAll('section[class="product-list"] div[class*="product gtmProductClick w-100"]');
+
+    products.forEach((product, index) => {
+      product.setAttribute('rank', `${index + 1}`);
+      const prefix = 'https://www.e-bebek.com';
+      const productUrl = product.querySelector('a.product-btn');
+      if (productUrl !== undefined) product.setAttribute('producturl', prefix.concat(productUrl.getAttribute('href')));
+    });
+  });
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'TR',
     store: 'e-bebek',
-    transform: null,
+    transform: transform,
     domain: 'e-bebek.com',
     zipcode: '',
   },
+  implementation,
 };
