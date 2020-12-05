60bc2432def762e9786b619d65ceb61d8a2f3888
tapan-profecialinks <tapan@profecialinks.com>
2020-12-05 17:26:18 +0530

    issue fixed - core key

--- src/library/product/details/stores/w/woolworths/AU/format.js
+++ src/library/product/details/stores/w/woolworths/AU/format.js
@@ -33,7 +33,9 @@ const transform = (data) => {
                     if(data['sku']){
                       item.text = data['sku'];
                     }
-                  }            
+                  }else{
+                    item.text = "";
+                  }           
                 });
             } 
 
@@ -43,20 +45,36 @@ const transform = (data) => {
                     variId =  /[^/]*$/.exec(item.text)[0].split(".");
                     item.text = "" + variId[0];          
                 });
-            } 
+            }  
 
             if (row.description) {
-                row.description.forEach(item => {
-                  let data = JSON.parse(item.text);
-                  if(data['description']){
-                    if(data['description']){
-                      item.text = data['description'];
-                      item.text = item.text.replace(/<(.|\n)*?>/g, ' | ').trim();
-                    }
-                  }            
-                });
-              }  
-
+              let info = [];
+              row.description.forEach(item => {
+                  info.push(item.text.replace(/(\s*\n\s*)+/g, ' | ').trim());
+              });
+              row.description = [{'text':info.join(' | '),'xpath':row.description[0].xpath}];
+            }
+            if (row.warnings) {
+              let info = [];
+              row.warnings.forEach(item => {
+                  info.push(item.text.replace(/(\s*\n\s*)+/g, ' | ').trim());
+              });
+              row.warnings = [{'text':info.join(' | '),'xpath':row.warnings[0].xpath}];
+            }
+            if (row.directions) {
+              let info = [];
+              row.directions.forEach(item => {
+                  info.push(item.text.replace(/(\s*\n\s*)+/g, ' | ').trim());
+              });
+              row.directions = [{'text':info.join(' | '),'xpath':row.directions[0].xpath}];
+            }
+            if (row.ingredientsList) {
+              let info = [];
+              row.ingredientsList.forEach(item => {
+                  info.push(item.text.replace(/(\s*\n\s*)+/g, ' | ').trim());
+              });
+              row.ingredientsList = [{'text':info.join(' | '),'xpath':row.ingredientsList[0].xpath}];
+            }
             if (row.aggregateRating) {
                 row.aggregateRating.forEach(item => {
                   let data = JSON.parse(item.text);
@@ -64,7 +82,9 @@ const transform = (data) => {
                     if(data['aggregateRating']['ratingValue']){
                       item.text = '' + parseFloat(data['aggregateRating']['ratingValue']).toFixed(1);
                     }
-                  }            
+                  }else{
+                    item.text = "";
+                  }
                 });
             } 
 
@@ -75,7 +95,9 @@ const transform = (data) => {
                     if(data['aggregateRating']['ratingCount']){
                       item.text = data['aggregateRating']['ratingCount'];
                     }
-                  }            
+                  }else{
+                    item.text = "";
+                  }          
                 });
             }  
             if (row.gtin) {
@@ -85,9 +107,39 @@ const transform = (data) => {
                     if(data['gtin13']){
                       item.text = data['gtin13'];
                     }
-                  }            
+                  }else{
+                    item.text = "";
+                  }       
                 });
+            }
+            if (row.brandText) {
+                row.brandText.forEach(item => {
+                  let data = JSON.parse(item.text);
+                  if(data['brand']){
+                    if(data['brand']['name']){
+                      item.text = data['brand']['name'];
+                    }
+                  }else{
+                    item.text = "";
+                  }       
+                });
+            }
+            if (row.price) {
+              let info = [];
+              row.price.forEach(item => {
+                info.push(item.text);
+              });
+              row.price = [{'text':info.join(''),'xpath':row.price[0].xpath}];
             } 
+            if (row.availabilityText) {          
+              row.availabilityText.forEach(item => {
+                 if(item.text.trim().includes("Add to cart")){
+                    item.text = "In Stock";
+                 }else if(item.text.trim().includes("Unavailable")){
+                    item.text = "Out of Stock";
+                 }
+              });      
+            }
         }
     }
     return cleanUp(data);
