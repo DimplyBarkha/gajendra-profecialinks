9e0fbbee9e4db7e1d8170c6f72dcc80eb773263d
Wayne E <bartekstafiej2@gmail.com>
2020-12-05 15:21:49 +0100

    Added search extractor (#)

---
+++ src/library/product/search/stores/e/eapteka/RU/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'RU',
+    store: 'eapteka',
+    domain: 'eapteka.ru',
+    url: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: "''",
+  },
+};
