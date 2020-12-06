e23a482cd00b409d81aeaddda5a51fd658c4e6b7
sangeeta <sangeta.kumari@synerzip.com>
2020-12-06 11:02:30 +0530

    search-efarma_it #2787

--- src/library/product/search/stores/e/Efarma/IT/extract.js
+++ src/library/product/search/stores/e/Efarma/IT/extract.js
@@ -40,11 +40,11 @@ module.exports = {
           // }catch(e) {}
             // await new Promise((resolve, reject) => setTimeout(resolve, 500));
             let scrollTop = 0;
-            while (scrollTop !== 10000) {
+            while (scrollTop !== 5000) {
               await stall(500);
               scrollTop += 1000;
               window.scroll(0, scrollTop);
-              if (scrollTop === 20000) {
+              if (scrollTop === 10000) {
                 await stall(500);
                 break;
               }
