64dfac8cd01992f4b13109a5473d62fd8674c8de
sobha rani <amol.thukrul-od@partner.import.io>
2020-12-07 23:47:13 +0530

    fressnapf IM complete

---
+++ src/library/product/details/stores/f/fressnapf/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'fressnapf',
+    transform: null,
+    domain: 'fressnapf.de',
+    zipcode: '',
+  },
+};
