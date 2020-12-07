02ab4dbf27c3d72c66670124251f874bc6bac051
dubok1195 <dubovyk1195@gmail.com>
2020-12-07 11:48:37 +0200

    #821 Fix extract.js

--- src/library/product/details/stores/a/amazon/SA/extract.js
+++ src/library/product/details/stores/a/amazon/SA/extract.js
@@ -1,11 +1,11 @@
+const { transform } = require('../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
-    country: 'SA',
+    country: 'US',
     store: 'amazon',
-    transform: null,
-    domain: 'amazon.sa',
-    zipcode: '',
+    transform,
+    domain: 'amazon.com',
   },
 };
