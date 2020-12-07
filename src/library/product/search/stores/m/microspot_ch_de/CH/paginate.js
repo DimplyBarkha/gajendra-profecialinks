8d86436bd5313d45c4a6100efe898f20e5965d62
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-12-07 13:02:51 +0530

    initial search - akshay channa

---
+++ src/library/product/search/stores/m/microspot_ch_de/CH/paginate.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'CH',
+    store: 'microspot_ch_de',
+    nextLinkSelector: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    noResultsXPath: null,
+    openSearchDefinition: null,
+    domain: 'microspot.ch',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/m/microspot_ch_de/CH/search.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'CH',
+    store: 'microspot_ch_de',
+    domain: 'microspot.ch',
+    zipcode: '',
+  },
+};
