f3ce956e898ef91a62a0314dae7aa26861f0299b
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-12-07 16:03:58 +0530

    instacart search feed initial commit

--- src/library/product/search/stores/i/instacart_costco/US/execute.js
+++ src/library/product/search/stores/i/instacart_costco/US/execute.js
@@ -4,7 +4,7 @@ module.exports = {
     country: "US",
     store: "instacart_costco",
     domain: "instacart.com",
-    url: "https://www.instacart.com/store/costco/search_v3/{wine}",
+    url: "https://www.instacart.com/store/costco/search_v3/{searchTerms}",
     loadedSelector: ".rmq-926ae505.rmq-6898c4bf.item-card div a",
     noResultsXPath: null,
     zipcode: "",
