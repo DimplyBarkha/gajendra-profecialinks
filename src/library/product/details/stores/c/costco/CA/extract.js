2984fe03339746fa8db161004ccc61bf6b4f38d7
Priyanka <priyanka.l@osmosys.asia>
2020-12-07 18:19:10 +0530

    Added fixes for costco salsify

--- src/library/product/details/stores/c/costco/CA/extract.js
+++ src/library/product/details/stores/c/costco/CA/extract.js
@@ -10,5 +10,31 @@ module.exports = {
     domain: 'costco.ca',
     zipcode: 'M5V 2A5',
   },
-  implementation,
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(async function () {
+      const descEl = document.querySelector('#product_tabs .product-info-description');
+
+      if (descEl) {
+        const featureBullets = descEl.querySelectorAll('li');
+
+        for (const el of featureBullets) {
+          const newEl = document.createElement('import-feature');
+          newEl.innerText = el.innerText;
+          document.body.appendChild(newEl);
+          el.remove();
+        }
+
+        const newDescEl = document.createElement('import-description');
+        newDescEl.innerText = descEl.innerText;
+        document.body.appendChild(newDescEl);
+      }
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+  },
 };
--- src/library/product/details/stores/c/costco/US/extract.js
+++ src/library/product/details/stores/c/costco/US/extract.js
@@ -10,7 +10,39 @@ module.exports = {
     domain: 'costco.com',
     zipcode: '94209',
   },
-  implementation,
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(async function () {
+      const productNotFoundEl = document.querySelector('h1');
+
+      if (productNotFoundEl.innerText.includes('Product Not Found!')) {
+        throw new Error('Product not found');
+      }
+
+      const descEl = document.querySelector('#product_tabs .product-info-description');
+
+      if (descEl) {
+        const featureBullets = descEl.querySelectorAll('li');
+
+        for (const el of featureBullets) {
+          const newEl = document.createElement('import-feature');
+          newEl.innerText = el.innerText;
+          document.body.appendChild(newEl);
+          el.remove();
+        }
+
+        const newDescEl = document.createElement('import-description');
+        newDescEl.innerText = descEl.innerText;
+        document.body.appendChild(newDescEl);
+      }
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+  },
 
 //   implementation: async (inputs,
 //     parameters,
@@ -23,7 +55,7 @@ module.exports = {
 //  let sku = products[0].map(e => { return e.partNumber}).join(" | ")
 //  document.body.setAttribute('variantId',sku);
 //  }
- 
+
 // });
 // const { productDetails } = dependencies;
 // await context.extract(productDetails);
