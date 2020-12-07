f8f5e96ad011348bf20d94059f8555db0c96857e
osmarghyadeep <arghyadep.g@partner.import.io>
2020-12-07 06:45:52 +0530

    Tried to load the price div for some urls

--- src/library/product/details/stores/n/nfm/US/extract.js
+++ src/library/product/details/stores/n/nfm/US/extract.js
@@ -12,6 +12,45 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
 
+    try {
+      console.log('waiting for price div');
+      await context.waitForSelector('span#DT_ProductPrice', {timeout: 30000});
+    } catch(err) {
+      console.log('got some error while for the price div', err);
+    }
+
+    try {
+      await context.evaluate(async function () {
+        function addHiddenDiv (id, content) {
+          const newDiv = document.createElement('div');
+          newDiv.id = id;
+          newDiv.textContent = content;
+          newDiv.style.display = 'none';
+          document.body.appendChild(newDiv);
+        }
+        console.log('if we can get the global variable');
+        if (typeof dataModel !== 'undefined') {
+            // the variable is defined
+            console.log('data model -  the global variable is defined');
+            if(dataModel.hasOwnProperty('priceData')) {
+              if (typeof dataModel.priceData !== 'undefined' && dataModel.priceData.hasOwnProperty('price')) {
+                console.log('price present globally is - $' + dataModel.priceData.price);
+                addHiddenDiv('price', ('$' + dataModel.priceData.price));
+              } else {
+                console.log('display data has no price prop in it');
+              }
+            } else {
+              console.log('we do not have any property named as display data in dataModel');
+            }
+
+          } else {
+            console.log('data model - the global variable is not defined');
+          }
+      });
+    } catch (err) {
+      console.log('got some error while extracting price from global variable');
+    }
+
     const src = await context.evaluate(async function () {
       const iframeDoc = document.querySelector('#wcframable1-0');
       const iframe = iframeDoc ? iframeDoc.contentDocument : '';
