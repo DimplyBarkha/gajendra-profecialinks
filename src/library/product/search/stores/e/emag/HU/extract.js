4bdb68d2824dbb235f854f801d936d36035987f2
Michał Wieczorek <michal126@hotmail.com>
2020-12-07 11:44:25 +0100

    fixed price in emag search HU (#2799)

--- src/library/product/search/stores/e/emag/HU/extract.js
+++ src/library/product/search/stores/e/emag/HU/extract.js
@@ -5,6 +5,14 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { productDetails } = dependencies;
 
   await context.evaluate(async () => {
+    const prices = document.querySelectorAll('div[class="card-item js-product-data"] p[class="product-new-price"]');
+    prices.forEach(element => {
+      const price = element.textContent;
+      if (price) {
+        element.setAttribute('price', price.replace('.', ','));
+      }
+    });
+
     // replace . with , in rating
     var rating = document.querySelectorAll('div.card-item.js-product-data div.star-rating.star-rating-read');
     const regex = /\d\.?(\d+)?/gm;
