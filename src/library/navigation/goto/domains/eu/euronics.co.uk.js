fae5d1c69b5ce0ed5b11e4ceb1d69c063a2688c1
Richa Tiwari <richa.t@osmosys.asia>
2020-12-07 18:40:54 +0545

    Upated extra fields

--- src/library/navigation/goto/domains/eu/euronics.co.uk.js
+++ src/library/navigation/goto/domains/eu/euronics.co.uk.js
@@ -1,17 +1,16 @@
-
 module.exports = {
-  implements: 'navigation/goto',
-  parameterValues: {
-    country: 'US',
-    domain: 'euronics.co.uk',
-    store: 'euronics',
-  },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 50000;
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
-    console.log(zipcode);
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-    }
-  },
+    implements: 'navigation/goto',
+    parameterValues: {
+        country: 'US',
+        domain: 'euronics.co.uk',
+        store: 'euronics',
+    },
+    implementation: async({ url, zipcode, storeId }, parameters, context, dependencies) => {
+        const timeout = parameters.timeout ? parameters.timeout : 50000;
+        await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+        console.log(zipcode);
+        if (zipcode) {
+            await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+        }
+    },
 };
--- src/library/product/details/stores/e/euronics/UK/execute.js
+++ src/library/product/details/stores/e/euronics/UK/execute.js
@@ -1,9 +1,10 @@
-
 module.exports = {
-  implements: 'product/details/execute',
-  parameterValues: {
-    country: 'US',
-    store: 'euronics',
-    domain: 'euronics.co.uk',
-  },
+    implements: 'product/details/execute',
+    parameterValues: {
+        country: 'US',
+        store: 'euronics',
+        domain: 'euronics.co.uk',
+        loadedSelector: 'h1.product-name',
+        noResultsXPath: ''
+    },
 };
