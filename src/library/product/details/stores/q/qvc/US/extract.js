d3021b29c452a8d242ad8942afaa4fc52bb6b486
astropaula <wojciechowska.fran@gmail.com>
2020-12-07 14:48:46 +0100

    #1478 Added videoInformation element and ingredientsList data conversion. Changed fields in extract.yaml

--- src/library/product/details/stores/q/qvc/US/extract.js
+++ src/library/product/details/stores/q/qvc/US/extract.js
@@ -14,6 +14,37 @@ module.exports = {
       const buttonIngredients = document.querySelector('div[class*="accIngredients"] a[role="button"]');
       if (buttonIngredients) buttonIngredients.click();
     });
+    // add video information
+    await context.evaluate(async function () {
+      const addElementToDOM = (link, length) => {
+        const element = document.createElement('a');
+          const parent = document.querySelector('div[class="photoWrapper"]');
+          element.id = 'videoInfo';
+          element.href = link;
+          element.title = length; 
+          element.style.display = 'none';
+          parent.appendChild(element);
+      };
+      const videoArray = document.querySelectorAll('div[class="videoThumb"]');
+      if (videoArray.length !== 0){
+        if (videoArray.length === 1) {
+          videoArray[0].click();
+          await new Promise((resolve) => setTimeout(resolve, 4000));
+          let link = document.querySelector('div[class="ssmp-container"] video').src;
+          let length = document.querySelector('span[class="vjs-duration-display"]').innerText;
+          addElementToDOM(link, length);
+        }
+        if (videoArray.length !== 1) {
+          for (let i = 1; i < videoArray.length; i++) {
+            videoArray[i].click();
+            await new Promise((resolve) => setTimeout(resolve, 4000));
+            let link = document.querySelector('div[class="ssmp-container"] video').src;
+            let length = document.querySelector('span[class="vjs-duration-display"]').innerText;
+            addElementToDOM(link, length);
+          }
+        }
+      } 
+    });
     var data = await context.extract(productDetails, { transform });
     for (let k = 0; k < data.length; k++) {
       for (let i = 0; i < data[k].group.length; i++) {
@@ -24,6 +55,12 @@ module.exports = {
           if (data[k].group[i].availabilityText[0].text.includes('not available')) {
             data[k].group[i].availabilityText[0].text = 'Out of Stock';
           }
+        }
+        if('ingredientsList' in data[k].group[i]) {
+          for (let j = 1; j < data[k].group[i].ingredientsList.length; j++) {
+            data[k].group[i].ingredientsList[0].text += data[k].group[i].ingredientsList[j].text;
+          }
+          data[k].group[i].ingredientsList = data[k].group[i].ingredientsList.slice(0, 1);
         }
       }
     }
