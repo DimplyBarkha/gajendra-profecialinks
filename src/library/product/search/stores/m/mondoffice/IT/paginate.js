84005a72f2453df3b3f8a249e2a75641f283e588
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-12-07 13:51:35 +0200

    modoffice add product_id

--- src/library/product/search/stores/m/mondoffice/IT/paginate.js
+++ src/library/product/search/stores/m/mondoffice/IT/paginate.js
@@ -4,9 +4,6 @@ module.exports = {
   parameterValues: {
     country: 'IT',
     store: 'mondoffice',
-    // nextLinkSelector: 'a.pagination-button__next',
-    // mutationSelector: null,
-    // spinnerSelector: null,
     loadedSelector: 'div#toolbar__all_anchor',
     openSearchDefinition: {
       template: 'https://www.mondoffice.com/INTERSHOP/web/WFS/RAJA-MONDOFFICE-Site/it_IT/-/EUR/ViewParametricSearch-ProductPaging?PageNumber={page}&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}',
--- src/library/product/search/stores/m/mondoffice/IT/shared.js
+++ src/library/product/search/stores/m/mondoffice/IT/shared.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
