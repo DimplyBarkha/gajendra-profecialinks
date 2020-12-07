e6023cc4b4f74d2a59782268e63daf3620e077d5
Mahesh <mahesh.patil@profecialinks.com>
2020-12-07 18:24:37 +0530

    formated variant id

--- src/library/product/search/stores/f/fany/CZ/format.js
+++ src/library/product/search/stores/f/fany/CZ/format.js
@@ -3,20 +3,20 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {  
+const transform = (data) => {
   const cleanUp = (data, context) => {
     const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
     data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
       el.text = clean(el.text);
     }))));
@@ -25,10 +25,19 @@ const transform = (data) => {
   for (const { group } of data) {
     var rank = 1;
     for (let row of group) {
-      if(row.name){
+      if (row.name) {
         row.name.forEach(item => {
           item.text = item.text.replace(/(\s*\n\s*)+/g, ' ').trim();
         });
+      }
+      if (row.variantId) {
+        let text = '';
+        row.variantId.forEach(item => {
+          item.text = item.text.replace(/[\/]/g, ' ').trim();
+          item.text = item.text.split('_');
+          item.text = item.text[1];
+          item.text = item.text.replace(/[a-z]/g,' ').trim();
+        })
       }
       row.rank = [{ "text": rank }];
       row.rankOrganic = [{ "text": rank }];
