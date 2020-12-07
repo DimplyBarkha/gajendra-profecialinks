997a3a00cb20349ee5f8f8992f65ef766d56744a
unknown <leena.gavali-odc@partner.import.io>
2020-12-07 15:32:58 +0530

    try_search_mat

--- src/library/navigation/goto/domains/ma/mat.se.js
+++ src/library/navigation/goto/domains/ma/mat.se.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'mat.se',
-    timeout: 10000,
+    timeout: 60000,
     country: 'SE',
     store: 'mat',
     zipcode: '',
