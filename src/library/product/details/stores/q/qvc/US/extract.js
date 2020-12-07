19d80159265822032f4cb93860d0a1548ce07060
astropaula <wojciechowska.fran@gmail.com>
2020-12-07 15:05:59 +0100

    Cleanup

--- src/library/product/details/stores/q/qvc/US/extract.js
+++ src/library/product/details/stores/q/qvc/US/extract.js
@@ -18,32 +18,32 @@ module.exports = {
     await context.evaluate(async function () {
       const addElementToDOM = (link, length) => {
         const element = document.createElement('a');
-          const parent = document.querySelector('div[class="photoWrapper"]');
-          element.id = 'videoInfo';
-          element.href = link;
-          element.title = length; 
-          element.style.display = 'none';
-          parent.appendChild(element);
+        const parent = document.querySelector('div[class="photoWrapper"]');
+        element.id = 'videoInfo';
+        element.href = link;
+        element.title = length;
+        element.style.display = 'none';
+        parent.appendChild(element);
       };
       const videoArray = document.querySelectorAll('div[class="videoThumb"]');
-      if (videoArray.length !== 0){
+      if (videoArray.length !== 0) {
         if (videoArray.length === 1) {
           videoArray[0].click();
           await new Promise((resolve) => setTimeout(resolve, 4000));
-          let link = document.querySelector('div[class="ssmp-container"] video').src;
-          let length = document.querySelector('span[class="vjs-duration-display"]').innerText;
+          const link = document.querySelector('div[class="ssmp-container"] video').src;
+          const length = document.querySelector('span[class="vjs-duration-display"]').innerText;
           addElementToDOM(link, length);
         }
         if (videoArray.length !== 1) {
           for (let i = 1; i < videoArray.length; i++) {
             videoArray[i].click();
             await new Promise((resolve) => setTimeout(resolve, 4000));
-            let link = document.querySelector('div[class="ssmp-container"] video').src;
-            let length = document.querySelector('span[class="vjs-duration-display"]').innerText;
+            const link = document.querySelector('div[class="ssmp-container"] video').src;
+            const length = document.querySelector('span[class="vjs-duration-display"]').innerText;
             addElementToDOM(link, length);
           }
         }
-      } 
+      }
     });
     var data = await context.extract(productDetails, { transform });
     for (let k = 0; k < data.length; k++) {
@@ -56,7 +56,7 @@ module.exports = {
             data[k].group[i].availabilityText[0].text = 'Out of Stock';
           }
         }
-        if('ingredientsList' in data[k].group[i]) {
+        if ('ingredientsList' in data[k].group[i]) {
           for (let j = 1; j < data[k].group[i].ingredientsList.length; j++) {
             data[k].group[i].ingredientsList[0].text += data[k].group[i].ingredientsList[j].text;
           }
