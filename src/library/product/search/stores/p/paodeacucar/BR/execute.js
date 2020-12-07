43a14705190898cbbff16645c4f9c6df6b9a2a72
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2020-12-07 11:09:59 +0100

    Inserted selectors and deployed search extractor (#2586)

--- src/library/product/search/stores/p/paodeacucar/BR/execute.js
+++ src/library/product/search/stores/p/paodeacucar/BR/execute.js
@@ -5,9 +5,9 @@ module.exports = {
     country: 'BR',
     store: 'paodeacucar',
     domain: 'paodeacucar.com',
-    url: 'https://www.paodeacucar.com/busca?w=${searchTerms}&qt=12&p=1&gt=grid',
-    loadedSelector: 'div[class*="product-cardstyles__Container"]',
-    noResultsXPath: null,
+    url: 'https://www.paodeacucar.com/busca?w={searchTerms}&qt=150',
+    loadedSelector: 'div[class*="MuiGrid-spacing-xs-2"]',
+    noResultsXPath: '//p[contains(text(), "nfelizmente não encontramos nenhum produto")]|//p[contains(text(), "Produto não encontrado.")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/p/paodeacucar/BR/extract.js
+++ src/library/product/search/stores/p/paodeacucar/BR/extract.js
@@ -1,11 +1,33 @@
 
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'BR',
     store: 'paodeacucar',
-    transform: null,
+    transform,
     domain: 'paodeacucar.com',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 7000));
+    await context.evaluate(async function () {
+      const lastProductPosition = localStorage.getItem('prodCount') ? Number(localStorage.getItem('prodCount')) : 1;
+      const products = document.querySelectorAll('div[class^="product-cardstyles__Container"]');
+      for (let i = 0; i < products.length; i++) {
+        const productUrlElem = products[i].querySelector('text[class^="product-cardstyles__Text"] a');
+        const productUrl = productUrlElem ? productUrlElem.getAttribute('href') : null;
+        if (productUrl) {
+          const productLink = productUrl.includes('https://www.paodeacucar.com/') ? productUrl : 'https://www.paodeacucar.com' + productUrl;
+          products[i].setAttribute('product_link', productLink);
+        } else {
+          products[i].setAttribute('product_link', '');
+        }
+        products[i].setAttribute('rank', `${lastProductPosition + i}`);
+      }
+      localStorage.setItem('prodCount', `${lastProductPosition + products.length}`);
+    });
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    return await context.extract(productDetails, { transform });
   },
 };
