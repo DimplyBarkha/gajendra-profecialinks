4ce11fbb0c8f769b5002ead9e33fe77c9f07765a
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-07 13:36:17 +0530

    #1711 fixiing QA1 issue

--- src/library/product/search/stores/p/petpost/AU/execute.js
+++ src/library/product/search/stores/p/petpost/AU/execute.js
@@ -38,13 +38,14 @@ async function implementation (
     });
   };
   await applyScroll(context);
-  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+  await new Promise((resolve, reject) => setTimeout(resolve, 7000));
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
     }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
   }
   console.log('Checking no results', parameters.noResultsXPath);
+  await new Promise((resolve, reject) => setTimeout(resolve, 20000));
   return await context.evaluate(function (xp) {
     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
     console.log(xp, r);
