afebd74c92e743a814ede79048637388ff125bce
mania307 <maniakalnaa@gmail.com>
2020-12-07 17:05:07 +0100

    #2574 fixed description, manufacturer, weightGross, shippingDimensions, specifications, firstVariant, video

--- src/library/product/details/stores/c/currys/UK/extract.js
+++ src/library/product/details/stores/c/currys/UK/extract.js
@@ -7,8 +7,7 @@ module.exports = {
     transform: cleanUp,
     domain: 'currys.co.uk',
     zipcode: "''",
-  },
-  implementation: async ({ inputString }, { transform }, context, { productDetails }) => {
+  }, implementation: async ({ inputString }, { transform }, context, { productDetails }) => {
     async function extractVideos() {
       let videos;
       const productUrl = await context.evaluate(() => {
@@ -16,22 +15,26 @@ module.exports = {
       });
       const iframeUrl = await context.evaluate((iframeSelector) => {
         if (document.querySelector(iframeSelector)) { return document.querySelector(iframeSelector).getAttribute('src'); }
-      }, '#isitetv-media-iframe');
+      }, '#isitetv');
       if (!iframeUrl) return;
       await context.goto(iframeUrl, { timeout: 50000, waitUntil: 'networkidle0', checkBlocked: true });
-      await context.waitForXPath('//a[contains(@class,"video_link")]');
+      // await context.waitForXPath('//a[contains(@class,"isitetv_nav_item_link")]');
       try {
         videos = await context.evaluate(async function () {
           const result = [];
-          const videoElements = document.querySelectorAll('a[class*="video_link"]').length;
+          const videoElements = document.querySelectorAll('a[class*="isitetv_nav_item_link"]').length;
           console.log(videoElements + ' videos found');
-          for (let i = 0; i < videoElements; i++) {
-            // @ts-ignore
-            await document.querySelectorAll('a[class*="video_link"]')[i].click();
-            await new Promise(resolve => setTimeout(resolve, 3000));
-            if (document.querySelector('video > source').getAttribute('src')) {
-              result.push('https:' + document.querySelector('video > source').getAttribute('src'));
+          if (videoElements) {
+            for (let i = 0; i < videoElements; i++) {
+              // @ts-ignore
+              await document.querySelectorAll('a[class*="isitetv_nav_item_link"]')[i].click();
+              await new Promise(resolve => setTimeout(resolve, 3000));
+              if (document.querySelector('video > source').getAttribute('src')) {
+                result.push('https:' + document.querySelector('video > source').getAttribute('src'));
+              }
             }
+          } else if (document.querySelector('iframe#isitetv')) {
+            result.push(document.querySelector('iframe#isitetv').getAttribute('src'));
           }
           return result;
         });
@@ -63,6 +66,16 @@ module.exports = {
         document.body.appendChild(catElement);
       };
 
+      const description1 = document.querySelector('div#product-info')
+        ? document.querySelector('div#product-info').innerText : '';
+      const description2 = document.querySelectorAll('div.no-tablet div.main-desc ul.to-print li');
+      if ((description2 && description2.length) || description1) {
+        const bulletsArr = [description1];
+        description2.forEach(e => bulletsArr.push(e.textContent));
+        const concatDesc = bulletsArr.join(' || ');
+        addElementToDocument('description', concatDesc);
+      };
+
       function appendData(data) {
         const keys = Object.keys(data);
         for (let i = 0; i < keys.length; i++) {
@@ -74,6 +87,10 @@ module.exports = {
       const data = {};
       const productDigitalData = JSON.parse(document.querySelector('script[id="app.digitalData"]').textContent).product[0];
       data.availability = productDigitalData.stockStatus || '';
+      data.manufacturer = productDigitalData.manufacturer || '';
+      data.id = productDigitalData.productID || '';
+      data.sku = productDigitalData.productSKU || '';
+      if (productDigitalData.reviewRating) data.aggregateRating = (productDigitalData.reviewRating * 5) / 10;
       data.url = window.location.href;
       if (videos && videos.length) data.videos = videos;
       appendData(data);
