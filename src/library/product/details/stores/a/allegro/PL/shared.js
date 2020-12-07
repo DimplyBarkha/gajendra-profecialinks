3cdb205edff2a8610ac0ce0f31e5cc81416e260d
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-12-07 15:19:21 +0530

    fixed space issue

--- src/library/product/details/stores/a/allegro/PL/shared.js
+++ src/library/product/details/stores/a/allegro/PL/shared.js
@@ -14,7 +14,6 @@ const transform = (data) => {
     .replace(/\s{2,}/g, ' ')
     .replace(/"\s{1,}/g, '"')
     .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
   // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
