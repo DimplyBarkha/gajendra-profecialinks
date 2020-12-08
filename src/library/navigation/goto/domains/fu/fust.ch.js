637270ff72d5feb9a2736ddc45c54e2b92929b02
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-12-08 14:03:08 +0530

    #2932 Created extractor robot

--- src/library/navigation/goto/domains/fu/fust.ch.js
+++ src/library/navigation/goto/domains/fu/fust.ch.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'fust.ch',
-    timeout: 500000,
+    timeout: null,
     country: 'CH',
     store: 'fust',
     zipcode: "''",
