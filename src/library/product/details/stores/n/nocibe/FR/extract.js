ee63390814116e100e6d81dbe8f482b7b9914014
Karina Miernik <karina.miernik@gmail.com>
2020-12-08 15:31:42 +0100

    #1136 fixed videos and descBullets

--- src/library/product/details/stores/n/nocibe/FR/extract.js
+++ src/library/product/details/stores/n/nocibe/FR/extract.js
@@ -11,7 +11,11 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const checkExistance = async (selector) => {
+      return await context.evaluate(async (currentSelector) => {
+        return Boolean(document.querySelector(currentSelector));
+      }, selector);
+    };
 
     const url = await context.evaluate(async () => {
       // @ts-ignore
@@ -19,27 +23,38 @@ module.exports = {
         return window.location.href;
       }
     });
+    // extracting video
+    const iframeSelector = 'div[class*="video-inner"] iframe';
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    if (await checkExistance(iframeSelector)) {
+      const iframeUrl = await context.evaluate((iframeSelector) => {
+        return document.querySelector(iframeSelector).getAttribute('src');
+      }, iframeSelector);
+      await context.goto(iframeUrl, { timeout: 50000, waitUntil: 'networkidle0', checkBlocked: true });
+      await context.waitForXPath('//link[@rel="canonical"][contains(@href, "youtube")]');
+      const video = await context.evaluate(() => {
+        const src = ele('link[rel="canonical"][href*="youtube"]');
+        function ele (tag) {
+          return document.querySelectorAll(tag);
+        }
+        const value = [];
+        retrieve(src);
+        function retrieve (src) {
+          for (let i = 0; i < src.length; i++) {
+            value.push(src[i].getAttribute('href'));
+          }
+        }
+        return value;
+      });
 
-    const iFrameSrc = await context.evaluate(async () => {
-      // @ts-ignore
-      return document.querySelector('div[class*="video-inner"] iframe')
-        // @ts-ignore
-        ? document.querySelector('div[class*="video-inner"] iframe').src : '';
-    });
-    const iFrameContent = await context.evaluate(async () => {
-      // @ts-ignore
-      return document.querySelector('div[class*="video-inner"] iframe')
-      // @ts-ignore
-        ? document.querySelector('div[class*="video-inner"] iframe').style.height : '';
-    });
-    if (iFrameContent !== '8px' && iFrameSrc) {
-      await context.goto(iFrameSrc);
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-      await context.extract(productDetails);
-      // await context.extract('/product/details/stores/n/nocibe/FR/extract', 'APPEND');
-      // going back to default page
-      await context.goto(url);
-    };
+      await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+      await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+      await context.evaluate((video) => {
+        video = video.join(' | ');
+        document.querySelector('body').setAttribute('video-src', video);
+      }, video);
+    }
+
     await context.evaluate(async () => {
       // @ts-ignore
       if (window !== undefined) {
@@ -47,6 +62,7 @@ module.exports = {
       }
     });
     await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
     await context.evaluate(async function () {
       function addElementToDocument (key, value, src) {
         const catElement = document.createElement('div');
@@ -69,14 +85,9 @@ module.exports = {
       // @ts-ignore
       } else addElementToDocument('descriptionText', descriptionFromMeta.getAttribute('content'), '#');
 
-      const shippingNodes = document.querySelectorAll('div[class*="ship"] div[class*="zone-txt"]')
-        ? document.querySelectorAll('div[class*="ship"] div[class*="zone-txt"]') : [];
-      // @ts-ignore
-      const shippingInfo = [...shippingNodes].map(e => e.innerText).join(' || ');
-      addElementToDocument('shippingInfo', shippingInfo, '#');
-      const altImgSrc = document.querySelectorAll('div[class="prdct__gallery-img "] div[class="prdct__gallery-slide"]:not(:first-child) img')
-        ? document.querySelectorAll('div[class="prdct__gallery-img "] div[class="prdct__gallery-slide"]:not(:first-child) img') : [];
-      // @ts-ignore
+      const altImgSrc = document.querySelectorAll('div[class="prdct__top"] div[class="prdct__gallery-img "] div[class="prdct__gallery-slide"]:not(:first-child) img, div[class="prdct__top"] div[class="prdct__gallery-img  prdct__gallery-img-odd"] div[class="prdct__gallery-slide"]:not(:first-child), div[class="prdct__top"] div[class="prdct__gallery-img  soldout disabled"] div[class="prdct__gallery-slide"]:not(:first-child) img')
+        ? document.querySelectorAll('div[class="prdct__top"] div[class="prdct__gallery-img "] div[class="prdct__gallery-slide"]:not(:first-child) img, div[class="prdct__top"] div[class="prdct__gallery-img  prdct__gallery-img-odd"] div[class="prdct__gallery-slide"]:not(:first-child), div[class="prdct__top"] div[class="prdct__gallery-img  soldout disabled"] div[class="prdct__gallery-slide"]:not(:first-child) img') : [];
+        // @ts-ignore
       [...altImgSrc].map(e => addElementToDocument('altImgSrc', '', e.src));
       const manufacturerImgSrc = document.querySelectorAll('div[class*="pyramid-img"] img')
         ? document.querySelectorAll('div[class*="pyramid-img"] img') : [];
@@ -86,6 +97,17 @@ module.exports = {
         // @ts-ignore
         ? document.querySelector('div[class*="prdct__logo"] a, div[class*="prdct__banner"] a').href : '';
       addElementToDocument('brandLink', '', brandLink);
+
+      const descriptionBullets = document.querySelector('h3[class="prdct__details-title"] + p ')
+        // @ts-ignore
+        ? document.querySelector('h3[class="prdct__details-title"] + p ').innerText : '';
+      let count = 0;
+      if (descriptionBullets !== undefined && descriptionBullets !== null) {
+        descriptionBullets.split('\n').forEach(e => {
+          if (e.charAt('-') === '-') count++;
+        });
+      }
+      addElementToDocument('bulletcount', count);
     });
 
     await context.evaluate(async function () {
