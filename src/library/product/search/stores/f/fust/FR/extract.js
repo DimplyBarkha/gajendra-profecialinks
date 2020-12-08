e2b08e06ddc23b56a93f6271ff5a63c35ee2aa84
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-12-08 13:53:48 +0530

    #2911 Created robot

---
+++ src/library/product/search/stores/f/fust/FR/extract.js
@@ -0,0 +1,39 @@
+const { transform } = require('./format');
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'FR',
+    store: 'fust',
+    transform,
+    domain: 'fust.ch',
+    zipcode: "''",
+  },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop !== 20000) {
+          await stall(1000);
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 20000) {
+            await stall(1000);
+            break;
+          }
+        }
+        function stall (ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await applyScroll(context);
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    return await context.extract(productDetails, { transform });
+  },
+};
