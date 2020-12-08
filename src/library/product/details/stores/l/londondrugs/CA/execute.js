77faa3b43dbe2c971cf753096bf674b2a4cd58ac
DeepikaO <deepika.kuppireddy@od.import.io>
2020-12-08 17:44:27 +0530

    #2704 londondrugs search feed type

--- src/library/product/details/stores/l/londondrugs/CA/execute.js
+++ src/library/product/details/stores/l/londondrugs/CA/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'CA',
     store: 'londondrugs',
     domain: 'londondrugs.com',
-    loadedSelector: null,
+    loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
   },
