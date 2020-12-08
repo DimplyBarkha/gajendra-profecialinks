e4626149f0d2d6a4998866f60e07aee830648fbb
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-12-08 15:05:01 +0530

    search-monclick-damayanthi

---
+++ src/library/product/details/stores/m/monclick/IT/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'monclick.it',
+    prefix: null,
+    url: null,
+    country: 'IT',
+    store: 'monclick',
+    zipcode: '',
+  },
+};
