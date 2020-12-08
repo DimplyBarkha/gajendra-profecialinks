0fc2dd7442e94dce99108fa419d781aa8b4aea84
Gajender Ramola <ramola106@gmail.com>
2020-12-08 20:10:14 +0530

    #2854 initial commit

---
+++ src/library/product/details/stores/w/walmart/MX/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'walmart.com.mx',
+    prefix: null,
+    url: 'https://www.walmart.com.mx/a/a/a/{id}',
+    country: 'MX',
+    store: 'walmart',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/w/walmart/MX/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'MX',
+    store: 'walmart',
+    domain: 'walmart.com.mx',
+    loadedSelector: 'div.product-details_productDetails__Uunoq',
+    noResultsXPath: "//div[@class='search-bar-desktop_searchBarContainer__eBcmI']",
+    zipcode: '',
+  },
+};
