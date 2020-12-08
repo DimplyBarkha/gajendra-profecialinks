0768a31bc043964520e0b64e855685c60a31f8d6
wasiqe-a <wasiqe.a@osmosys.asia>
2020-12-08 22:52:27 +0530

    Adding changes to fix the issues with extractor

--- src/library/product/reviews/stores/e/elcorteingles/ES/extract.js
+++ src/library/product/reviews/stores/e/elcorteingles/ES/extract.js
@@ -1,11 +1,43 @@
+const { transform } = require('../shared');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productReviews } = dependencies;
+  await context.evaluate(function () {
+    const optionalWait = async (sel) => {
+      try {
+        await context.waitForSelector(sel, { timeout: 30000 });
+      } catch (err) {
+        console.log(`Couldn't load selector => ${sel}`);
+      }
+    }
+    const acceptCookies = document.querySelector('a[id="cookies-agree"]');
+    if(acceptCookies) {
+      acceptCookies.click();
+      optionalWait('//*[contains(@class, "content-list-reviews")]');
+    }
+  });
+  try {
+    await context.waitForSelector('//*[contains(@class, "content-list-reviews")]', { timeout: 30000 });
+  } catch (err) {
+    console.log(`Couldn't load selector => //*[contains(@class, "content-list-reviews")]`);
+  }
+  return await context.extract(productReviews, { transform });
+}
 
 module.exports = {
   implements: 'product/reviews/extract',
   parameterValues: {
     country: 'ES',
     store: 'elcorteingles',
-    transform: null,
+    transform,
     domain: 'elcorteingles.es',
     zipcode: '',
   },
+  implementation
 };
--- src/library/product/reviews/stores/e/elcorteingles/ES/paginate.js
+++ src/library/product/reviews/stores/e/elcorteingles/ES/paginate.js
@@ -1,4 +1,28 @@
 
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { page } = inputs;
+  const { nextLinkSelector } = parameters;
+
+  try {
+    await context.waitForSelector(nextLinkSelector, { timeout: 30000 });
+  } catch (err) {
+    console.log(`Couldn't load selector => ${nextLinkSelector}`);
+  }
+  await new Promise((resolve, reject) => setTimeout(resolve, 1000))
+  
+  await context.evaluate(function (nextLinkSelector) {
+  if (document.querySelector(nextLinkSelector)) {
+    document.querySelector(nextLinkSelector).click();
+  }});
+
+  return false;
+}
 module.exports = {
   implements: 'navigation/paginate',
   parameterValues: {
@@ -6,9 +30,9 @@ module.exports = {
     country: 'ES',
     store: 'elcorteingles',
     nextLinkSelector: 'li[class*="item-next"]>a',
-    mutationSelector: null,
     resultsDivSelector: '*[class*="content-list-reviews"]',
     domain: 'elcorteingles.es',
     zipcode: '',
   },
+  implementation
 };
