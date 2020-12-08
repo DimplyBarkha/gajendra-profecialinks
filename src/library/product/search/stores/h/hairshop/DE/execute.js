193fa76801c8901a55ffd1f61d0e3866d9e18377
Harshkha <harsh.khare-od@partner.import.io>
2020-12-08 22:15:07 +0530

    Search complete

--- src/library/product/search/stores/h/hairshop/DE/execute.js
+++ src/library/product/search/stores/h/hairshop/DE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DE',
     store: 'hairshop',
     domain: 'hair-shop.com',
-    url: 'https://www.hair-shop.com/en/catalogsearch/result/?q=gel',
+    url: 'https://www.hair-shop.com/en/catalogsearch/result/index/?p=8&q={searchTerms}',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
