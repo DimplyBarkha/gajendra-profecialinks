7f08d9ed893cc1ec57ac78a94b782fdbe1ff5260
Karolina <karolina.urbanska@tdcm.io>
2020-12-08 16:23:23 +0100

    Deployed and generated tests for search (#2800)

--- src/library/product/search/stores/e/esselungaacasa/IT/execute.js
+++ src/library/product/search/stores/e/esselungaacasa/IT/execute.js
@@ -1,3 +1,33 @@
+async function implementation (
+  { zipcode, keywords },
+  { url, loadedSelector, noResultsXPath },
+  context,
+  dependencies,
+) {
+  const homeUrl = 'https://www.esselungaacasa.it/ecommerce/nav/welcome/index.html';
+  await context.goto(homeUrl, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+
+  await context.setInputValue('input#postcode', '20141');
+  await context.click('button[type="submit"]');
+  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+
+  const destinationUrl = url.replace('{searchTerms}', encodeURIComponent(keywords));
+  await dependencies.goto({ url: destinationUrl, zipcode });
+
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate((xp) => {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/search/execute',
@@ -10,4 +40,5 @@ module.exports = {
     noResultsXPath: '//div[@ng-if="productSetCtrl.emptyProductSet"]',
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/search/stores/e/esselungaacasa/IT/extract.js
+++ src/library/product/search/stores/e/esselungaacasa/IT/extract.js
@@ -11,6 +11,41 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
     await new Promise((resolve) => setTimeout(resolve, 3000));
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    await context.evaluate(async function () {
+      function addElementToDocument (doc, key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        doc.appendChild(catElement);
+      };
+
+      const allProducts = document.querySelectorAll('div.content-item[tabindex="0"]')
+      for (let x = 0; x < allProducts.length; x++) {
+        const productId = allProducts[x].getAttribute('id');
+        addElementToDocument(allProducts[x], 'productId', productId);
+      }
+    });
     return await context.extract(productDetails, { transform });
   },
 };
