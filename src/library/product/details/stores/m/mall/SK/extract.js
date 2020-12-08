8abef2d2e1b83e389a9475c86313e54fe30a6bd4
dhanush <dhanush.isukapalli@od.import.io>
2020-12-08 17:05:03 +0530

    mall by kavya

--- src/library/product/details/stores/m/mall/SK/extract.js
+++ src/library/product/details/stores/m/mall/SK/extract.js
@@ -31,6 +31,14 @@ module.exports = {
         agr = (agr*5)/100;
         addElementToDocument('agr', agr);
       }
+      var agg = getXpath('(//div[@class="pdrt__subtitle-info"]/a/img)[1]/@alt', 'nodeValue');
+      if(agg != null){
+        addElementToDocument('agg', agg);
+      } else{
+        agg = getXpath('//article[@itemscope="itemscope"]/h1[@itemprop="name"]/text()', 'nodeValue');
+        agg = agg.split(" ")[0];
+        addElementToDocument('agg', agg);
+      }
     });
     await context.extract(productDetails);
     },
