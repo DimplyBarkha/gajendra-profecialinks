5f4ac19c9e4ebf50abf182c45d3ed45df7053eec
shubhsrv7 <subham.sourav@osmosys.asia>
2020-12-08 11:07:18 +0530

    search extractor added(#2228)

---
+++ src/library/navigation/goto/domains/ub/ubaldi.fr.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'ubaldi.fr',
+    timeout: 50000,
+    country: 'FR',
+    store: 'ubaldi',
+    zipcode: '',
+  },
+};
