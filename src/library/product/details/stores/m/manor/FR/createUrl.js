9aed106c6554ae30331fac4633a7ae85ab10a9d7
Gajender Ramola <ramola106@gmail.com>
2020-12-08 17:35:16 +0530

    #2942 initial commit

---
+++ src/library/product/details/stores/m/manor/FR/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'manor.ch',
+    prefix: null,
+    url: 'https://www.manor.ch/fr/p/{id}',
+    country: 'FR',
+    store: 'manor',
+    zipcode: '',
+  },
+};
