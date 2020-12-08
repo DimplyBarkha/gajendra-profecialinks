2226a203b393b0e202c0ab2bfecf7c74850181df
akansha.wajge@synerzip.com <akansha.wajge@synerzip.com>
2020-12-08 17:16:42 +0530

    rank Issue fixed (#1680)

--- src/library/product/search/stores/c/colesonline/AU/extract.js
+++ src/library/product/search/stores/c/colesonline/AU/extract.js
@@ -1,5 +1,6 @@
 const { transform } = require('../../../../shared');
 async function implementation (
+  // @ts-ignore
   inputs,
   parameters,
   context,
@@ -7,7 +8,57 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await context.evaluate(() => {
+  await context.evaluate(async() => {
+  //   var showProducts = document.querySelector('div.is-legal div.promo-footer a.button');
+  //   if(showProducts){
+  //     // @ts-ignore
+  //     showProducts.click();
+  //   }
+  //   await new Promise((resolve) => setTimeout(resolve, 2000));
+  // function removeClasses() {
+  //     var element = document.querySelector('form[name="ageVerificationForm"]');
+  //     // element.classList.remove("ng-pristine ng-invalid ng-invalid-required ng-valid-date ng-valid-date-not-in-future ng-valid-pattern ng-valid-maxlength");
+  //     element.setAttribute("class", "");
+  //   }
+  // removeClasses();
+  // var inputDay = document.querySelector('input#day');
+  //  if(inputDay){
+  //   // @ts-ignore
+  //   inputDay.value = 4;
+  //   // inputDay.setAttribute("class", "form-field-input ng-valid ng-valid-pattern ng-valid-maxlength ng-touched ng-not-empty ng-dirty ng-valid-parse");
+  //   inputDay.setAttribute("class", "");
+  //   inputDay.addEventListener('mouseover', function() {
+  //     console.log('Event triggered');
+  //   });
+  //   await new Promise((resolve) => setTimeout(resolve, 2000));
+  //  }
+  //  var inputMonth = document.querySelector('select#month');
+  // if(inputMonth){
+  //  // @ts-ignore
+  //  inputMonth.value = "July";
+  //  inputMonth.setAttribute("class",'');
+  // //  inputMonth.setAttribute("class",'form-field-dropdown ng-pristine ng-valid ng-not-empty ng-touched');
+  //  inputMonth.addEventListener('mouseover', function() {
+  //   console.log('Event triggered');
+  // });
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
+  // }
+  // var inputYear = document.querySelector('input#year');
+  //  if(inputYear){
+  //   // @ts-ignore
+  //   inputYear.value = 1995;
+  //   inputYear.setAttribute("class",'');
+  //   // inputYear.setAttribute("class",'form-field-input ng-valid ng-valid-pattern ng-valid-maxlength ng-not-empty ng-dirty ng-valid-parse ng-touched');
+  //   inputYear.addEventListener('mouseover', function() {
+  //     console.log('Event triggered');
+  //   });
+  //   await new Promise((resolve) => setTimeout(resolve, 2000));
+  //  }
+  //  var submitBtn = document.querySelector('p.warning-cta button[class="button button-dark"]');
+  //   if(submitBtn){
+  //     // @ts-ignore
+  //     submitBtn.click();
+  //   }
     function addHiddenDiv (id, content) {
       const newDiv = document.createElement('div');
       newDiv.id = id;
