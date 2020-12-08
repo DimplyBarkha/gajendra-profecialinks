14e0502b7ec6c0d3d7fb78516f5ab23d4f58b05f
LAPTOP-MD78TJNH\TDCM <katarzyna-godlewska1@wp.pl>
2020-12-08 12:53:31 +0100

    Fix some bugs

--- src/library/product/search/stores/e/easyparapharmacie/FR/execute.js
+++ src/library/product/search/stores/e/easyparapharmacie/FR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FR',
     store: 'easyparapharmacie',
     domain: 'www.easyparapharmacie.com',
-    url: null,
+    url: 'https://www.easyparapharmacie.com/catalogsearch/result/index/?q=%7BsearchTerms%7D',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
