433d72e1b973c9777ef3c84cbf0d9f4e3c2282f7
dhanush <dhanush.isukapalli@od.import.io>
2020-12-08 19:14:05 +0530

    ottooffice by kavya

--- src/library/product/details/stores/o/otto-office/DE/extract.js
+++ src/library/product/details/stores/o/otto-office/DE/extract.js
@@ -8,5 +8,37 @@ module.exports = {
     domain: 'otto-office.com',
     zipcode: '',
   },
-};
-
+  implementation: async ({ inputstring }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(() => {
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+      var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+      var spec = getAllXpath('//ul[@class="ooB-mb24 bullet ooX-fs14 "]/li/text()', 'nodeValue');
+      if (spec != null) {
+        var str = spec.join(" || ");
+        addElementToDocument('str', str);
+      }
+    });
+    await context.extract(productDetails);
+    },
+    };
