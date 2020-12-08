28b7854d3d4a7f5704172223e1871cebad3bf0b6
Vivek Gondil <gondilvivek@gmail.com>
2020-12-08 16:10:41 +0530

    Lint fix for review extractor

--- src/library/product/reviews/stores/s/saturn/DE/execute.js
+++ src/library/product/reviews/stores/s/saturn/DE/execute.js
@@ -1,13 +1,13 @@
 module.exports = {
-    implements: 'product/reviews/execute',
-    parameterValues: {
-        country: 'DE',
-        store: 'saturn',
-        domain: 'saturn.de',
-        loadedSelector: 'section[id="reviews"]',
-        noResultsXPath: null,
-        reviewUrl: 'https://www.saturn.de/de/product/-{id}.html',
-        sortButtonSelectors: null,
-        zipcode: '',
-    },
+  implements: 'product/reviews/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'saturn',
+    domain: 'saturn.de',
+    loadedSelector: 'section[id="reviews"]',
+    noResultsXPath: null,
+    reviewUrl: 'https://www.saturn.de/de/product/-{id}.html',
+    sortButtonSelectors: null,
+    zipcode: '',
+  },
 };
--- src/library/product/reviews/stores/s/saturn/DE/extract.js
+++ src/library/product/reviews/stores/s/saturn/DE/extract.js
@@ -1,66 +1,66 @@
-const { transform } = require("../shared");
+const { transform } = require('../shared');
 
 module.exports = {
-    implements: 'product/reviews/extract',
-    parameterValues: {
-        country: 'DE',
-        store: 'saturn',
-        transform: transform,
-        domain: 'saturn.de',
-        zipcode: '',
-    },
-    implementation: async function(
-        inputs,
-        parameters,
-        context,
-        dependencies,
-    ) {
-        await context.evaluate(async() => {
-            document.body.setAttribute('url', window.location.href);
-            let divs = document.querySelectorAll('div[data-test="mms-customer-rating"]');
-            divs.forEach((div) => {
-                let rating = 0;
-                div.childNodes.forEach((d) => { if (d.getAttribute('class').includes('iJmaKv')) { rating++ } })
-                div.setAttribute('ratings', rating.toString())
-            })
+  implements: 'product/reviews/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'saturn',
+    transform: transform,
+    domain: 'saturn.de',
+    zipcode: '',
+  },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    await context.evaluate(async () => {
+      document.body.setAttribute('url', window.location.href);
+      let divs = document.querySelectorAll('div[data-test="mms-customer-rating"]');
+      divs.forEach((div) => {
+        let rating = 0;
+        div.childNodes.forEach((d) => { if (d.getAttribute('class').includes('iJmaKv')) { rating++; } });
+        div.setAttribute('ratings', rating.toString());
+      });
 
-            divs = document.querySelectorAll('div[class*="VerifiedPurchaserIcon"]');
-            divs.forEach((div) => {
-                div.setAttribute('content', 'Yes');
-            })
+      divs = document.querySelectorAll('div[class*="VerifiedPurchaserIcon"]');
+      divs.forEach((div) => {
+        div.setAttribute('content', 'Yes');
+      });
 
-            async function removeBtn(dateSelector, btnSelector) {
-                if (!document.body.getAttribute('firstrevdate')) {
-                    let div = document.querySelector(dateSelector);
-                    console.log(div)
-                    document.body.setAttribute('firstrevdate', div.textContent);
-                }
+      async function removeBtn (dateSelector, btnSelector) {
+        if (!document.body.getAttribute('firstrevdate')) {
+          const div = document.querySelector(dateSelector);
+          console.log(div);
+          document.body.setAttribute('firstrevdate', div.textContent);
+        }
 
-                function monthDiff(d1, d2) {
-                    let months;
-                    months = (d2.getFullYear() - d1.getFullYear()) * 12;
-                    months -= d1.getMonth();
-                    months += d2.getMonth();
-                    return months <= 0 ? 0 : months;
-                }
-                const firstDate = new Date(document.body.getAttribute('firstrevdate'));
-                const divs = document.querySelectorAll(dateSelector);
-                divs.forEach((meta) => {
-                    const date = new Date(meta.textContent);
-                    if (monthDiff(date, firstDate) !== 0) {
-                        if (document.querySelector(btnSelector)) {
-                            document.querySelectorAll(btnSelector).forEach((btn) => {
-                                btn.remove();
-                            })
-                        }
-                    }
-                });
+        function monthDiff (d1, d2) {
+          let months;
+          months = (d2.getFullYear() - d1.getFullYear()) * 12;
+          months -= d1.getMonth();
+          months += d2.getMonth();
+          return months <= 0 ? 0 : months;
+        }
+        const firstDate = new Date(document.body.getAttribute('firstrevdate'));
+        const divs = document.querySelectorAll(dateSelector);
+        divs.forEach((meta) => {
+          const date = new Date(meta.textContent);
+          if (monthDiff(date, firstDate) !== 0) {
+            if (document.querySelector(btnSelector)) {
+              document.querySelectorAll(btnSelector).forEach((btn) => {
+                btn.remove();
+              });
             }
-            await removeBtn('section[id*="reviews"] div[class*="Cardstyled__StyledCardWrapper"] div[class*="FlexBox__StyledBox"]:nth-child(1) > div:nth-child(2) span', 'span[class*="ReviewsPaginationWrapper"] button');
+          }
         });
-        const { transform } = parameters;
-        const { productReviews } = dependencies;
-        return await context.extract(productReviews, { transform });
-    }
+      }
+      await removeBtn('section[id*="reviews"] div[class*="Cardstyled__StyledCardWrapper"] div[class*="FlexBox__StyledBox"]:nth-child(1) > div:nth-child(2) span', 'span[class*="ReviewsPaginationWrapper"] button');
+    });
+    const { transform } = parameters;
+    const { productReviews } = dependencies;
+    return await context.extract(productReviews, { transform });
+  },
 
 };
