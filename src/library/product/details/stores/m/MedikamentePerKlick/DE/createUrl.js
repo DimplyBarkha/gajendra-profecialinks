fe614e088aa9a4300fb225c686e33ba3577a2448
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-12-08 22:39:14 +0530

    #2891 devmedikamenteperklickdetails

---
+++ src/library/product/details/stores/m/MedikamentePerKlick/DE/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'medikamenteperklick.de',
+    prefix: null,
+    url: null,
+    country: 'DE',
+    store: 'MedikamentePerKlick',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/m/MedikamentePerKlick/DE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'MedikamentePerKlick',
+    domain: 'medikamenteperklick.de',
+    loadedSelector: 'body',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
