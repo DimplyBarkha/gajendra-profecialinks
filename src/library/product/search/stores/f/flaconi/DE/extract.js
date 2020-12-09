b402a8aa4a4caf61e006455ea8ed15208cd1e1fd
venkatsn <venkat.sn-odc@partner.import.io>
2020-12-09 16:10:21 +0530

    #2633 deepak

--- src/library/product/search/stores/f/flaconi/DE/extract.js
+++ src/library/product/search/stores/f/flaconi/DE/extract.js
@@ -8,14 +8,36 @@ module.exports = {
     domain: 'flaconi.de',
     zipcode: '',
   },
-  //  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
-  //   await context.evaluate(async function () {
-  //       while(!!document.querySelector('body > div.wrapper.off-canvas-wrap > div.inner-wrap.row-collapse > div > div.content > div > div > div.toolbar.row > div > div > div:nth-child(3) > a')){
-  //         // @ts-ignore
-  //         document.querySelector('body > div.wrapper.off-canvas-wrap > div.inner-wrap.row-collapse > div > div.content > div > div > div.toolbar.row > div > div > div:nth-child(3) > a').click()
-  //         await new Promise(r => setTimeout(r, 6000));
-  //       }
-  //     })
-  //     await context.extract(productDetails, { transform: transformParam });
-  //   }
+  implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  // await context.waitForFunction(function () {
+  //   return Boolean(document.querySelector('div[id="rd-item-grid"]') || document.evaluate('//div[@id="rd-item-grid"]', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+  // }, { timeout: 90000 });
+
+  await context.evaluate(async function () {
+    // function addclass(xpathforpagination) {
+    //   var elems = document.querySelectorAll(xpathforpagination);
+    //   elems[0].classList.add('pagination');
+    // }
+    function addElementToDocument(key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }try {
+      document.getElementById('pd_url').remove();
+    } catch (error) {
+    }
+    addElementToDocument('pd_url', URL);
+  });
+  return await context.extract(productDetails, { transform });
+}
