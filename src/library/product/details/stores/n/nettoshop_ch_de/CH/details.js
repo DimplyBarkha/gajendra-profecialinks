bb66554ffadc94afc76f6ec8a1278e5d00618b54
Sai Pavan <guntraddi>
2020-12-09 14:41:38 +0530

    #2699 Im and Core

---
+++ src/library/product/details/stores/n/nettoshop_ch_de/CH/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'CH',
+    store: 'nettoshop_ch_de',
+    domain: 'nettoshop.ch',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/n/nettoshop_ch_de/CH/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'CH',
+    store: 'nettoshop_ch_de',
+    domain: 'nettoshop.ch',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
