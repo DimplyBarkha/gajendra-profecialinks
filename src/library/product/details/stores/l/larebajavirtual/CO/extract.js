312334ff7f52e6a89eb46667335df7e4d1837224
sobha rani <amol.thukrul-od@partner.import.io>
2020-12-09 13:49:27 +0530

    laRebajaVirtual IM complete

--- src/library/product/details/stores/l/larebajavirtual/CO/extract.js
+++ src/library/product/details/stores/l/larebajavirtual/CO/extract.js
@@ -8,4 +8,28 @@ module.exports = {
     domain: 'larebajavirtual.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+      var price = getXpath("(//span[@class='ahora'])[1]/text() | //section[@class='product_detail']/div[@class='container']//div[@class='descripciones']/div[2]/div/div[@class='subtotal']/span/span/text()", 'nodeValue');
+      if (price != null) {
+        price = price.replace('.', ',');
+        addElementToDocument('price', price);
+      }
+    });
+    await context.extract(productDetails);
+  },
 };
