adb5f7184543bb8365ccd471395305edd14f6d88
Richa Tiwari <richa.t@osmosys.asia>
2020-12-09 12:54:44 +0545

    Updated CTR

--- src/library/product/details/stores/m/medimax/DE/extract.js
+++ src/library/product/details/stores/m/medimax/DE/extract.js
@@ -1,51 +1,56 @@
 const { transform } = require('../format');
 
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
+async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
 ) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-  try {
-    await context.click('button.cookies-overlay-dialog__accept-all-btn');
-  } catch (err) {
-
-  }
-  const src = await context.evaluate(async function () {
-    const iframe = document.querySelector('#loadbeeTabContent');
-    const src = iframe ? iframe.src : '';
-    return src;
-  });
-  await context.extract(productDetails, { transform });
-  if (src) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
     try {
-      await context.goto(src, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
-      await context.waitForSelector('div.wrapper.preview');
-      return await context.extract(productDetails, { type: 'MERGE_ROWS', transform });
-    } catch (error) {
-      try {
-        await context.evaluate(async function (src) {
-          window.location.assign(src);
-        }, src);
-        await context.waitForSelector('div.wrapper.preview, div#produkt');
-        return await context.extract(productDetails, { type: 'MERGE_ROWS', transform });
-      } catch (err) {
-        console.log(err);
-      }
+        await context.click('button.cookies-overlay-dialog__accept-all-btn');
+    } catch (err) {
+
     }
-  }
+    const src = await context.evaluate(async function() {
+        const iframe = document.querySelector('#loadbeeTabContent');
+        const src = iframe ? iframe.src : '';
+        return src;
+    });
+    await context.extract(productDetails, { transform });
+    if (src) {
+        try {
+            await context.goto(src, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
+            let getTable = document.querySelector('h1.compare-headline')
+            if (getTable) {
+                let table = 'Yes'
+                document.head.setAttribute('table', table);
+            }
+            await context.waitForSelector('div.wrapper.preview');
+            return await context.extract(productDetails, { type: 'MERGE_ROWS', transform });
+        } catch (error) {
+            try {
+                await context.evaluate(async function(src) {
+                    window.location.assign(src);
+                }, src);
+                await context.waitForSelector('div.wrapper.preview, div#produkt');
+                return await context.extract(productDetails, { type: 'MERGE_ROWS', transform });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
 }
 
 module.exports = {
-  implements: 'product/details/extract',
-  parameterValues: {
-    country: 'DE',
-    store: 'medimax',
-    transform,
-    domain: 'medimax.de',
-    zipcode: '',
-  },
-  implementation,
+    implements: 'product/details/extract',
+    parameterValues: {
+        country: 'DE',
+        store: 'medimax',
+        transform,
+        domain: 'medimax.de',
+        zipcode: '',
+    },
+    implementation,
 };
--- src/library/product/details/stores/m/medimax/format.js
+++ src/library/product/details/stores/m/medimax/format.js
@@ -1,68 +1,78 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  for (const { group } of data) {
-    for (const row of group) {
-      if (row.specifications) {
-        row.specifications[0].text = row.specifications[0].text.replace(/(\n\s*){6,}/g, ' | ').replace(/(\n\s*){5}/g, ' ').replace(/(\n\s*){4}/g, ' | ').replace(/:*(\n\s*){2}/g, ': ');
-      }
-      if (row.shippingDimensions) {
-        row.shippingDimensions = [{
-          text: row.shippingDimensions.reduce((item, currItem) => item ? `${item} | ${currItem.text.replace(/:*(\n\s*)/g, ': ')}` : currItem.text.replace(/:*(\n\s*)/g, ': '), ''),
-        }];
-      }
-      if (row.manufacturerDescription) {
-        row.manufacturerDescription[0].text = row.manufacturerDescription[0].text.replace(/\s*\n\s*/g, ' ');
-      }
-      if (row.videos) {
-        const videos = row.videos.map(({ text }) => text);
-        row.videos = Array.from(new Set(videos)).map(video => ({ text: video }));
-      }
-      if (row.gtin && row.gtin[0].text.length > 50) {
-        row.gtin[0].text = row.gtin[0].text.replace(/.*?ean\/(.*?)\/.*/i,'$1');
-      }
-      if (row.gtin && row.gtin[0].text.match(/EAN/i)) {
-        row.gtin[0].text = row.gtin[0].text.replace(/.*EAN:\s*([^\s]+).*/i, '$1');
-      }
-      if (row.manufacturerImages) {
-        row.manufacturerImages.forEach(img => {
-          if (img.text.includes('background-image')) {
-            img.text = img.text.replace(/.*url\('(.*)'\).*/, '$1');
-          } else if (img.text.startsWith('http')) {
-            img.text = `https:${img.text}`;
-          }
-        });
-      }
-      row.category && row.category.splice(-1);
-      if (row.availabilityText) {
-        row.availabilityText[0].text = 'In Stock';
-      } else {
-        row.availabilityText = [{
-          text: 'Out of Stock',
-        }];
-      }
+    for (const { group }
+        of data) {
+        for (const row of group) {
+
+            if (row.specifications) {
+                row.specifications[0].text = row.specifications[0].text.replace(/(\n\s*){6,}/g, ' | ').replace(/(\n\s*){5}/g, ' ').replace(/(\n\s*){4}/g, ' | ').replace(/:*(\n\s*){2}/g, ': ');
+            }
+            if (row.shippingDimensions) {
+                row.shippingDimensions = [{
+                    text: row.shippingDimensions.reduce((item, currItem) => item ? `${item} | ${currItem.text.replace(/:*(\n\s*)/g, ': ')}` : currItem.text.replace(/:*(\n\s*)/g, ': '), ''),
+                }];
+            }
+            if (row.manufacturerDescription) {
+                row.manufacturerDescription[0].text = row.manufacturerDescription[0].text.replace(/\s*\n\s*/g, ' ');
+            }
+            if (row.videos) {
+                const videos = row.videos.map(({ text }) => text);
+                row.videos = Array.from(new Set(videos)).map(video => ({ text: video }));
+            }
+            if (row.gtin && row.gtin[0].text.length > 50) {
+                row.gtin[0].text = row.gtin[0].text.replace(/.*?ean\/(.*?)\/.*/i, '$1');
+            }
+            if (row.gtin && row.gtin[0].text.match(/EAN/i)) {
+                row.gtin[0].text = row.gtin[0].text.replace(/.*EAN:\s*([^\s]+).*/i, '$1');
+            }
+            if (row.manufacturerImages) {
+                row.manufacturerImages.forEach(img => {
+                    if (img.text.includes('background-image')) {
+                        img.text = img.text.replace(/.*url\('(.*)'\).*/, '$1');
+                    } else if (img.text.startsWith('http')) {
+                        img.text = `https:${img.text}`;
+                    }
+                });
+            }
+            row.category && row.category.splice(-1);
+            if (row.availabilityText) {
+                row.availabilityText[0].text = 'In Stock';
+            } else {
+                row.availabilityText = [{
+                    text: 'Out of Stock',
+                }];
+            }
+            // if (row.hasComparisonTable) {
+            //     row.hasComparisonTable[0].text = 'Yes';
+            // } else {
+            //     row.hasComparisonTable = [{
+            //         text: 'No',
+            //     }];
+            // }
+        }
+
+
     }
-  }
-  const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-    el.text = clean(el.text);
-  }))));
-  return data;
+    const clean = text => text.toString()
+        .replace(/\r\n|\r|\n/g, ' ')
+        .replace(/&amp;nbsp;/g, ' ')
+        .replace(/&amp;#160/g, ' ')
+        .replace(/\u00A0/g, ' ')
+        .replace(/\s{2,}/g, ' ')
+        .replace(/"\s{1,}/g, '"')
+        .replace(/\s{1,}"/g, '"')
+        .replace(/^ +| +$|( )+/g, ' ')
+        // eslint-disable-next-line no-control-regex
+        .replace(/[\x00-\x1F]/g, '')
+        .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+    }))));
+    return data;
 };
 
 module.exports = { transform };
