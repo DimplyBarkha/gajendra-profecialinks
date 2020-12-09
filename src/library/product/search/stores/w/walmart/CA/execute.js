0ce5e0b250be2fe37659814de16b72796423b257
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-12-09 23:41:06 +0530

    Updated code

--- src/library/product/search/stores/w/walmart/CA/execute.js
+++ src/library/product/search/stores/w/walmart/CA/execute.js
@@ -1,3 +1,44 @@
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  { zipcode, keywords },
+  { url, loadedSelector, noResultsXPath },
+  context,
+  dependencies,
+) {
+  const destinationUrl = url.replace('{searchTerms}', encodeURIComponent(keywords));
+  await dependencies.goto({
+    url: destinationUrl,
+    zipcode: zipcode,
+    firstRequestTimeout: 60000,
+    timeout: 60000,
+    waitUntil: 'load',
+    checkBlocked: false,
+    antiCaptchaOptions: {
+      type: 'RECAPTCHA',
+    },
+  });
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate((xp) => {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/search/execute',
@@ -10,4 +51,5 @@ module.exports = {
     noResultsXPath: '//h1[@data-automation="null-results-message"]',
     zipcode: "''",
   },
+  implementation,
 };
