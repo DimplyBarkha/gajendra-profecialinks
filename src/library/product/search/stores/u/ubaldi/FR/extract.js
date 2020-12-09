5b76af8bc435afd7c1920807c1fc077b10f1f2db
shubhsrv7 <subham.sourav@osmosys.asia>
2020-12-09 17:44:13 +0530

    added implementation

--- src/library/product/search/stores/u/ubaldi/FR/extract.js
+++ src/library/product/search/stores/u/ubaldi/FR/extract.js
@@ -19,16 +19,13 @@ module.exports = {
     const applyScroll = async function (context) {
       await context.evaluate(async function () {
         let scrollTop = 0;
-        while (scrollTop !== 20000) {
-          await stall(2000);
-          scrollTop += 1000;
+        while (scrollTop !== 30000) {
+          scrollTop += 500;
           window.scroll(0, scrollTop);
-          if (scrollTop === 20000) {
-            await stall(30000);
-            break;
-          }
+          await stall(1000);
         }
-        function stall (ms) {
+        function stall (ms)
+ {
           return new Promise((resolve, reject) => {
             setTimeout(() => {
               resolve();
@@ -37,7 +34,23 @@ module.exports = {
         }
       });
     };
-    await applyScroll(context);
-    return await context.extract(productDetails, { transform });
+    async function getProductsCount (context) {
+      return context.evaluate(async function () {
+        const products = document.evaluate('//div[@id="main-liste-articles"]//div[contains(@class, "la-article")]', document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        return products.snapshotLength;
+      });
+    }
+    let productsCount = 0;
+    while (productsCount < 150) {
+      // await applyScroll(context);
+      
+        productsCount = await getProductsCount(context);
+        console.log('productsCount' + productsCount);
+        if (productsCount >= 150) {
+          break;
+        }
+        else await applyScroll(context);
+      }
+    return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
   },
 };
