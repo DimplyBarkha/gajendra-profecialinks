0967ae1123dea7e9ac5f8bc71753b59c021c3e63
Sheetal Bharara <SheetalB@hcl.com>
2020-12-09 15:36:01 +0530

    Kickback issues

--- src/library/product/details/stores/c/costco/CA/extract.js
+++ src/library/product/details/stores/c/costco/CA/extract.js
@@ -47,13 +47,15 @@ module.exports = {
 
       // xpath for description
       const featureDescInfoXpath = "//div[@class='features-container form-group']//ul/li/text()";
-      const featureDescInfo = getAllXpath(featureDescInfoXpath, 'nodeValue').join(' || ');
+      const featureDescInfo = getAllXpath(featureDescInfoXpath, 'nodeValue').join(' ||');
       const tabDescInfoXpath = "//div[@class='product-info-description']/text()";
       const tabDescInfo = getAllXpath(tabDescInfoXpath, 'nodeValue');
       const bulletsXpath = "//div[@class='product-info-description']/ul/li/text()";
-      const bulletsInfo = getAllXpath(bulletsXpath, 'nodeValue').join(' || ');
+      const bulletsInfo = getAllXpath(bulletsXpath, 'nodeValue').join(' ||');
       const modelXpath = "//div[contains(@class,'item-model-number')]//span/@data-model-number";
       let modelInfo = getXpath(modelXpath, 'nodeValue');
+      const boldText = getAllXpath('//div[@class="product-info-description"]//span[1]/text()', 'nodeValue');
+
       if (modelInfo === null) {
         const modelXpath1 = "//div[@class='product-info-description']/span[2]";
         modelInfo = getXpath(modelXpath1, 'innerText');
@@ -69,9 +71,11 @@ module.exports = {
           flag = true;
           modelInfo = element.replace('Model: ', '');
         }
+
         let info = element.replace('\n', '');
         info = info.replace('\t', '');
         info = info.trim();
+
         if (info.length > 0) {
           tabDescInfoNew.push(info);
         }
@@ -81,16 +85,26 @@ module.exports = {
       }
 
       let finalDescInfo;
-      if (featureDescInfo.length > 0) {
+      if (featureDescInfo !== null && featureDescInfo.length > 0) {
         addElementToDocument('featureBullets', featureDescInfo);
-        finalDescInfo = 'Features: || ' + featureDescInfo + ' || ' + tabDescInfoNew.join('||');
+        finalDescInfo = 'Features: ||' + featureDescInfo + ' ||' + boldText[0] + ' ||' + tabDescInfo[1];
       }
-      if (bulletsInfo.length > 0) {
+      if (bulletsInfo !== null && bulletsInfo.length > 0) {
         addElementToDocument('additionalDescBulletInfo', bulletsInfo);
-        finalDescInfo = finalDescInfo + ' || ' + bulletsInfo;
+        finalDescInfo = finalDescInfo + bulletsInfo;
       }
-      if (finalDescInfo.length > 0) {
-        finalDescInfo = finalDescInfo.replace('\n', '||');
+      if (boldText[2] !== null) {
+        if (!boldText[2].includes('★')) {
+          finalDescInfo = finalDescInfo + ' ||' + boldText[2];
+        }
+      }
+      tabDescInfo[2] = tabDescInfo[2].trim();
+      if (tabDescInfo[2].length > 0) {
+        finalDescInfo = finalDescInfo + ' ||' + tabDescInfo[2];
+      }
+
+      if (finalDescInfo !== null && finalDescInfo.length > 0) {
+        finalDescInfo = finalDescInfo.replace('\n', ' ');
         addElementToDocument('added_descriptionText', finalDescInfo);
       }
 
