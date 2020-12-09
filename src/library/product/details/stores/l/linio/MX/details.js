83cedce195a82db187b276780288fb29bb6f3af0
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-12-09 10:38:59 +0530

    #2938. Xpaths added for core extractor.

---
+++ src/library/product/details/stores/l/linio/MX/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'MX',
+    store: 'linio',
+    domain: 'linio.com.mx',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/l/linio/MX/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'MX',
+    store: 'linio',
+    transform: null,
+    domain: 'linio.com.mx',
+    zipcode: '',
+  },
+};
