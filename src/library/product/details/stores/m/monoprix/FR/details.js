4559d65c4f7f054912321ca85cdb7da3fa81ebbd
Rafał Perkowski <rafalpero1@gmail.com>
2020-12-09 13:59:36 +0100

    monoprix core initial commit

---
+++ src/library/product/details/stores/m/monoprix/FR/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'FR',
+    store: 'monoprix',
+    domain: 'monoprix.fr',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/m/monoprix/FR/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'FR',
+    store: 'monoprix',
+    transform: null,
+    domain: 'monoprix.fr',
+    zipcode: '',
+  },
+};
