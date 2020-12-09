278b2314c4ee4659fc2ab298bb4a267aae97cb4d
Vivek Gondil <gondilvivek@gmail.com>
2020-12-09 16:39:27 +0530

    Adding New Review extractor.

---
+++ src/library/product/reviews/stores/p/petsmart/US/reviews.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/reviews',
+  parameterValues: {
+    country: 'US',
+    store: 'petsmart',
+    domain: 'petsmart.com',
+    zipcode: '',
+    mergeType: null,
+  },
+};
