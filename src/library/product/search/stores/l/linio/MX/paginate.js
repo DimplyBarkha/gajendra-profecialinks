2797016cd9deebb90fe6ab8d5ef8b880e0e4f310
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-12-09 15:18:13 +0530

    #2569. searchUrl issue fixed for search extractor.

--- src/library/product/search/stores/l/linio/MX/paginate.js
+++ src/library/product/search/stores/l/linio/MX/paginate.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'MX',
     store: 'linio',
     nextLinkSelector: 'li[class*=page-item]:nth-last-child(2) a',
-    nextLinkXpath: '//li[contains(@class,"page-item") and (position()>=2 and position() = (last() - 1))]//a',
+    nextLinkXpath: null,
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div[itemtype*="http://schema.org/Product"]',
