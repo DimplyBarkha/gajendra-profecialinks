c615739187887e4800c5244e960b85bd51eb6467
Avinash <avinash.kadam@profecialinks.com>
2020-12-09 22:49:27 +0530

    removed console logs & input.yaml updated

--- src/library/product/reviews/stores/n/njoy/US/extract.js
+++ src/library/product/reviews/stores/n/njoy/US/extract.js
@@ -1,4 +1,4 @@
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -6,49 +6,30 @@ async function implementation (
 ) {
   const { transform, mergeType } = parameters;
   const { productReviews } = dependencies;
-try{
-  await context.waitForSelector('a[class="button confirmm-age"]', { timeout: 7000 });
-}
-catch(e){
-	console.log("--------");
-}
-  let agebutton = await context.evaluate(async function () {
+  try {
+    await context.waitForSelector('a[class="button confirmm-age"]', { timeout: 7000 });
+  }
+  catch (e) {
+    console.log('age selector not found');
+  }
 
+  let agebutton = await context.evaluate(async function () {
     const ageConfIframe = document.querySelector('a.button.confirmm-age');
-     if(ageConfIframe){
-     	document.querySelector('a.button.confirmm-age').click();
-    	//await context.click('a.button.confirmm-age');
-    // await context.waitForNavigation();
-  	}
+    if (ageConfIframe) {
+      document.querySelector('a.button.confirmm-age').click();
+    }
     return !!ageConfIframe;
   });
 
-  console.log(">>>>>>>>>>>>>>>>>>>>>>>", !!agebutton);
-
- 
-
   await new Promise(resolve => setTimeout(resolve, 10000));
 
-  try{
+  try {
     await context.waitForSelector('div[data-review-id]:not(.yotpo-hidden)');
   }
-  catch(e){
+  catch (e) {
     console.log('selector not found', e);
   }
 
-  await context.evaluate(async function () {
-    var parentel = document.querySelector('div[data-review-id]:not(.yotpo-hidden)');
-    if(parentel){
-      console.log("============================================================");
-      var childel = parentel.querySelector('div.content-title.yotpo-font-bold');
-      console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",childel.innerText);
-    }
-  });
-
-
-
-
-  // const mergeOptions = mergeType ? { transform, type: mergeType } : { transform };
   return await context.extract(productReviews);
 }
 
