d8a015d276e2bc07c266d8116afd9157614ee779
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-09 18:13:38 +0530

    galeriakaufhof by dhanush

--- src/library/product/search/stores/g/galeriakaufhof/DE/extract.js
+++ src/library/product/search/stores/g/galeriakaufhof/DE/extract.js
@@ -1,11 +1,27 @@
 const { transform } = require('../../../../shared');
+async function implementation (
+inputs,
+parameters,
+context,
+dependencies,
+) {
+const { transform } = parameters;
+const { productDetails } = dependencies;
+await context.evaluate(async () => {
+while(!!document.querySelector("#search-result-items > div > button")){
+document.querySelector("#search-result-items > div > button").click()
+await new Promise(r => setTimeout(r, 6000));
+}
+})
+return await context.extract(productDetails, { transform });
+}
 module.exports = {
-  implements: 'product/search/extract',
-  parameterValues: {
-    country: 'DE',
-    store: 'galeriakaufhof',
-    transform: transform,
-    domain: 'galeria.de',
-    zipcode: '',
-  },
+implements: 'product/search/extract',
+parameterValues: {
+country: 'DE',
+store: 'galeriakaufhof',
+transform: transform,
+domain: 'galeria.de',
+},
+implementation,
 };
