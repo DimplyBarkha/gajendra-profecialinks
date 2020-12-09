58aa2cbb548cf5023555bbbc8a33826647b8eb71
Harshkha <harsh.khare-od@partner.import.io>
2020-12-09 15:25:44 +0530

    search complete

--- src/library/product/search/stores/h/hairshop/DE/extract.js
+++ src/library/product/search/stores/h/hairshop/DE/extract.js
@@ -8,4 +8,61 @@ module.exports = {
     domain: 'hair-shop.com',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async () => {
+      var tempData, valueVariant, valueRating;
+      const rawData = document.querySelectorAll('script[type="application/ld+json"]');
+      for (let i = 0; i < rawData.length; i++) {
+        try {
+          // @ts-ignore
+          tempData = rawData[i].innerText;
+          tempData = JSON.parse(tempData);
+          valueRating = tempData.aggregateRating[0].ratingValue;
+          valueVariant = tempData.additionalProperty[0].value;
+          const newDiv = document.createElement('div');
+          newDiv.id = 'rating';
+          newDiv.textContent = valueRating;
+          newDiv.style.display = 'none';
+          const originalDiv = document.querySelectorAll("article[data-variationid=\"" + valueVariant + "\"]")[0];
+          // @ts-ignore
+          originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+
+        } catch (error) {
+          continue;
+        }
+      }
+      function addHiddenDiv(id, content, index) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll("#products-grid-ee2741 > li")[index];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      }
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+      // aggrreview 
+      const aggregateRating = document.querySelectorAll('div.rating');
+      //let length = aggregateRating.length
+      //for (let k = 0; k <= length; k++) {
+      // @ts-ignore
+      //let singleRating = aggregateRating;
+      //singleRating = singleRating.slice(0, singleRating.length - 1)
+      // @ts-ignore
+      //singleRating = (5 * singleRating) / 100;
+      //singleRating = singleRating.toFixed(1);
+      addHiddenDiv('aggregateRating', aggregateRating);
+    //}
+    });
+  return await context.extract(productDetails, { transform });
+},
 };
