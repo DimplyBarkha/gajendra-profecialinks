02a0e754f84dbb0a6720c26f0cbdc85ff0a3f280
Avinash <avinash.kadam@profecialinks.com>
2020-12-10 11:38:24 +0530

    changes as per pull request review

--- src/library/product/reviews/stores/z/ziipstock/US/extract.js
+++ src/library/product/reviews/stores/z/ziipstock/US/extract.js
@@ -7,19 +7,5 @@ module.exports = {
     transform: null,
     domain: 'ziipstock.com',
     zipcode: "''",
-  },
-  implementation: async (inputs,
-    parameters,
-    context,
-    dependencies) => {
-    const { productReviews } = dependencies;
-
-    await context.evaluate(async () => {
-      console.log('++++++++++++++++++++++++++++++++++++++')
-      var p = document.querySelector('div.stamped-review');
-      console.log(p.querySelector('div.stamped-review-body>h3'));
-    });
-
-    return await context.extract(productReviews);
-  },
+  }
 };
