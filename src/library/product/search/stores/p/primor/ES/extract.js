08f757f1a43f858ba7a072482c8b731b3e73d9da
unknown <leena.gavali-odc@partner.import.io>
2020-12-10 17:37:08 +0530

    primor_debugger

--- src/library/product/search/stores/p/primor/ES/extract.js
+++ src/library/product/search/stores/p/primor/ES/extract.js
@@ -1,40 +1,40 @@
 const { transform } = require('../../../../shared');
-async function implementation (
-inputs,
-parameters,
-context,
-dependencies,
+async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
 ) {
-const { transform } = parameters;
-const { productDetails } = dependencies;
-await context.evaluate(async function () {
-let scrollTop = 0;
-while (scrollTop <= 20000) {
-await stall(500);
-scrollTop += 500;
-window.scroll(0, scrollTop);
-if (scrollTop === 20000) {
-await stall(8000);
-break;
-}
-}
-function stall (ms) {
-return new Promise(resolve => {
-setTimeout(() => {
-resolve();
-}, ms);
-});
-}
-});
-return await context.extract(productDetails, { transform });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop <= 20000) {
+            await stall(500);
+            scrollTop += 500;
+            window.scroll(0, scrollTop);
+            if (scrollTop === 20000) {
+                await stall(8000);
+                break;
+            }
+        }
+        function stall(ms) {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    resolve();
+                }, ms);
+            });
+        }
+    });
+    return await context.extract(productDetails, { transform });
 }
 module.exports = {
-implements: 'product/search/extract',
-parameterValues: {
-country: 'ES',
-store: 'primor',
-transform: transform,
-domain: 'primor.eu',
-},
-implementation,
+    implements: 'product/search/extract',
+    parameterValues: {
+        country: 'ES',
+        store: 'primor',
+        transform: transform,
+        domain: 'primor.eu',
+    },
+    implementation,
 };
