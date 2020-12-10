c17733b2be446b97494c5fd00d5a7dce99cafb4a
Guillaume Vial <guillaumevial@Guiguis-mac.local>
2020-12-10 12:34:35 +0000

    lint

--- src/library/navigation/goto/domains/wa/walmart.com.ar.js
+++ src/library/navigation/goto/domains/wa/walmart.com.ar.js
@@ -1,10 +1,10 @@
 module.exports = {
-  implements: "navigation/goto",
+  implements: 'navigation/goto',
   parameterValues: {
-    domain: "walmart.com.ar",
+    domain: 'walmart.com.ar',
     timeout: 200000,
-    country: "AR",
-    store: "walmart",
-    zipcode: "",
+    country: 'AR',
+    store: 'walmart',
+    zipcode: '',
   },
 };
