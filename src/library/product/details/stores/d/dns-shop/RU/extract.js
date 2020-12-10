1d053b6527c530d9d5d306b8a5dfed705eae3c26
Himanshu-Agrahari <himanshu.a@partner.import.io>
2020-12-10 13:15:28 +0530

    Fix: Video and gallery video as null

--- src/library/product/details/stores/d/dns-shop/RU/extract.js
+++ src/library/product/details/stores/d/dns-shop/RU/extract.js
@@ -16,45 +16,6 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
 
-    await context.evaluate(async () => {
-      function addHiddenDiv (id, content) {
-        const newDiv = document.createElement('div');
-        newDiv.id = id;
-        newDiv.textContent = content;
-        newDiv.style.display = 'none';
-        document.body.appendChild(newDiv);
-      }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-      const sideTabs = document.querySelectorAll('a.product-card-tabs__title');
-      if (sideTabs) {
-        for (let i = sideTabs.length - 1; i >= 0; i--) {
-          console.log(sideTabs[i].innerText + ' is the sidetab');
-          if (sideTabs[i].innerText.includes('Обзоры')) {
-            console.log('reviews button clicked');
-            sideTabs[i].click();
-            await stall(2000);
-            const iframes = document.querySelectorAll('iframe');
-            let videoLink = null;
-            for (let i = 0; i < iframes.length; i++) {
-              if (iframes[i].getAttribute('src') !== null && iframes[i].getAttribute('src').includes('youtube')) {
-                videoLink = iframes[i].getAttribute('src');
-                break;
-              }
-            }
-            if (videoLink !== null) addHiddenDiv('moreVideos', videoLink);
-          }
-          if (sideTabs[i].innerText === 'Характеристики') {
-            sideTabs[i].click();
-          }
-        }
-      }
-    });
     try {
       await context.waitForSelector('div.product-characteristics');
       await context.evaluate(async () => {
@@ -79,20 +40,72 @@ module.exports = {
     } catch (error) {
       console.log(error);
     }
-    await context.evaluate(async () => {
-      const Button1 = document.querySelector('.owl-item:first-child .img img');
-      if (Button1) {
-        Button1.click();
-        console.log('Button1 clicked');
-      }
-    });
+
+    const mainImageCss = '.owl-item:first-child .img img';
+    const videoPageCss = '.media-viewer__titles .media-viewer__title:nth-child(2)';
+
+    const isButtonAvailable = await context.evaluate(async (mainImageCss) => {
+      return document.querySelector(mainImageCss);
+    }, mainImageCss);
+
+    if (isButtonAvailable) {
+      console.log('mainImage is available..clicking it');
+      await context.click(mainImageCss);
+    };
+
     await context.waitForSelector('.media-viewer__titles .media-viewer__title');
+    const isVideoPageAvailable = await context.evaluate(async (videoPageCss) => {
+      const btn = document.querySelector(videoPageCss);
+      return btn && btn.innerText.includes('Видео');
+    }, videoPageCss);
+
+    if (isVideoPageAvailable) {
+      console.log('VideoPage is available..clicking it');
+      await context.click(videoPageCss);
+    };
+
     await context.evaluate(async () => {
-      const Button2 = document.querySelector('.media-viewer__titles .media-viewer__title:nth-child(2)');
-      if (Button2) {
-        Button2.click();
-        console.log('Button2 clicked');
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
       }
+
+      function getVideoSrc () {
+        const iframe = document.querySelector('div[class*="media-viewer-video"] iframe');
+        const src = iframe && iframe.getAttribute('src');
+        return src !== 'null' ? src : '';
+      }
+
+      async function getVideoUrls () {
+        const allVideoNav = document.querySelectorAll('.tns-inner .tns-item img');
+        const videoUrls = [];
+
+        for (let index = 0; index < allVideoNav.length; index++) {
+          const btnCounter = index + 1;
+          console.log(`clicking ${btnCounter}th nav btn`);
+          if (index === 0) {
+            videoUrls.push(getVideoSrc());
+          } else {
+            allVideoNav[index].click();
+            await new Promise((resolve, reject) => {
+              setTimeout(() => {
+                resolve();
+              }, 3000);
+            });
+            console.log(`clicked ${btnCounter}th nav btn`);
+            videoUrls.push(getVideoSrc());
+          }
+        }
+        return videoUrls;
+      }
+
+      const videoUrls = await getVideoUrls();
+      console.log('videoUrls');
+      console.log(videoUrls);
+      videoUrls.filter(Boolean).map(url => addHiddenDiv('moreVideos', url));
     });
     return await context.extract(productDetails, { transform });
   },
