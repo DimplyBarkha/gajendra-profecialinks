a3da181609d37b01f811a621fe5923e54b220adb
sobha rani <amol.thukrul-od@partner.import.io>
2020-12-10 17:09:26 +0530

    larebajavirtual core complete

--- src/library/product/details/stores/l/larebajavirtual/CO/extract.js
+++ src/library/product/details/stores/l/larebajavirtual/CO/extract.js
@@ -17,6 +17,15 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
       var getXpath = (xpath, prop) => {
         var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
         let result;
@@ -28,6 +37,11 @@ module.exports = {
       if (price != null) {
         price = price.replace('.', ',');
         addElementToDocument('price', price);
+      }
+      var variant = getAllXpath("//div[@class='sep-dashed']/label/text()", 'nodeValue');
+      if (variant != null) {
+        var ab = variant.join(' | ');
+        addElementToDocument('variant', ab);
       }
     });
     await context.extract(productDetails);
