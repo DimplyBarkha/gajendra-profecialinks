aa8dcf6380c6a08a7ca74c49dde4cda2e4d35a8f
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-10 15:33:30 +0530

    updated settings for kroger details

--- src/library/navigation/goto/domains/kr/kroger.com.js
+++ src/library/navigation/goto/domains/kr/kroger.com.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 30000;
-    await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'networkidle0', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
       await dependencies.setZipCode({ url, zipcode });
