5c7a87c8a56c2427eb5526a5b3632e914e37abfb
nehashende <neha.shende@synerzip.com>
2020-12-10 13:55:12 +0530

    #2879. Added transformation script.

---
+++ src/library/product/details/stores/o/officedepot/FR/transform.js
@@ -0,0 +1,58 @@
+/**
+*
+* @param {ImportIO.Group[]} data
+* @returns {ImportIO.Group[]}
+*/
+const transform = (data) => {
+  const cleanUp = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+  // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.specifications) {
+        let text = '';
+        row.specifications.forEach(item => {
+          text += `${item.text} || `;
+        });
+        row.specifications = [
+          {
+            text: cleanUp(text.slice(0, -3)),
+          },
+        ];
+      }
+      if (row.aggregateRating) {
+        const ratingArr = [];
+        row.aggregateRating.forEach(item => {
+          if (item.text.includes('tpw-star tpw-star-full')) {
+            ratingArr.push(1);
+          } else ratingArr.push(0);
+        });
+        const aggregateRating = ratingArr.reduce(function (a, b) {
+          return a + b;
+        }, 0);
+        row.aggregateRating = [
+          {
+            text: cleanUp(aggregateRating),
+          },
+        ];
+      }
+    }
+  }
+
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = cleanUp(el.text);
+  }))));
+
+  return data;
+};
+module.exports = { transform };
