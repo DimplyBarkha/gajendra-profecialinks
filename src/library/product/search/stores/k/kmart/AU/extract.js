6aae8bdc57b1ae98152775f759497aed7efadddb
unknown <leena.gavali-odc@partner.import.io>
2020-12-10 19:06:29 +0530

    kmart_rank

--- src/library/product/search/stores/k/kmart/AU/extract.js
+++ src/library/product/search/stores/k/kmart/AU/extract.js
@@ -8,34 +8,93 @@ module.exports = {
     domain: 'kmart.com.au',
     zipcode: '',
   },
-  implementation: async ({ inputstring }, { country, domain }, context, { productDetails }) => {
-    await context.evaluate(() => {
-      const getAllXpath = (xpath, prop) => {
-        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        const result = [];
-        for (let index = 0; index < nodeSet.snapshotLength; index++) {
-          const element = nodeSet.snapshotItem(index);
-          if (element) result.push(prop ? element[prop] : element.nodeValue);
-        }
-        return result;
-      };
-      function addHiddenDiv(id, content, index) {
-        // @ts-ignore
-        const newDiv = document.createElement('div');
-        newDiv.id = id;
-        newDiv.textContent = content;
-        newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll('div[class="product product_box small-6 medium-4 large-4 columns clearfix col "]')[index];
-        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-      };
-      var abc = getAllXpath("//div[@class='product product_box small-6 medium-4 large-4 columns clearfix col ']/@data-attribute-productid", 'nodeValue');
+  implementation,
+
+
+};
+
+async function implementation(
+
+  inputs,
+
+  parameters,
+
+  context,
+
+  dependencies,
+
+) {
+
+  const { transform } = parameters;
+
+  const { productDetails } = dependencies;
+
+  await context.evaluate(async function () {
+
+    function addclass(xpathforpagination) {
+
+      var elems = document.querySelectorAll(xpathforpagination);
+
+      elems[0].classList.add('pagination');
+
+    }
+
+    function addHiddenDiv(id, content, index) {
+
+      const newDiv = document.createElement('div');
+
+      newDiv.id = id;
+
+      newDiv.textContent = content;
+
+      newDiv.style.display = 'none';
+
+      const originalDiv = document.querySelectorAll('div[class="product product_box small-6 medium-4 large-4 columns clearfix col "]')[index];
+
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+
+    }
+
+
+
+    const getAllXpath = (xpath, prop) => {
+
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+      const result = [];
+
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+
+        const element = nodeSet.snapshotItem(index);
+
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+
+      }
+
+      return result;
+
+    };
+
+
+
+
+
+
+
+    // price
+
+    var abc = getAllXpath("//div[@class='product product_box small-6 medium-4 large-4 columns clearfix col ']/@data-attribute-productid", 'nodeValue');
       if (abc != null) {
         for (var i = 0; i < abc.length; i++) {
           abc[i] = "P_" + abc[i]
           addHiddenDiv('id', abc[i], i);
         }
       }
-    });
-    await context.extract(productDetails);
-  },
+
+
+
+  });
+
+  return await context.extract(productDetails, { transform });
+
 };
