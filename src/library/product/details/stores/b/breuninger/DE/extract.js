273608a7ae3cc52be527b3da25d1de0df1eb4cf5
sushma-mangalapally <sushma.mangalapally@import.io>
2020-12-09 17:46:06 -0800

    Refactor code

--- src/library/product/details/stores/b/breuninger/DE/extract.js
+++ src/library/product/details/stores/b/breuninger/DE/extract.js
@@ -29,6 +29,121 @@ module.exports = {
         const myUrl = await context.evaluate(async function() {
             return document.querySelector('meta[property="og:url"]').content;
         });
+        console.log('sdsds myUrl');
+        console.log(myUrl)
+        let jsonData = await context.evaluate(async function () {
+            return JSON.parse(document.querySelector('bewerten-vue-data').getAttribute('data-bewerten-json-content'));
+        });
+
+        // manually extracting iframe url and navigating to it
+        const gotoIframe = await context.evaluate(async function() {
+            if (!(document.querySelector('iframe[id=loadbeeIframeId]'))) {
+                await new Promise((resolve, reject) => setTimeout(resolve, 100));
+            }
+            return document.querySelector('iframe[id=loadbeeIframeId]').src;
+        });
+
+        let content = null;
+        let image = null;
+        let comparisionText = null;
+        let inBoxText = null;
+        let inBoxUrls = null;
+        let videos = null;
+        await context.goto(gotoIframe);
+        await context.waitForNavigation();
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+    
+        await context.evaluate(async () => {
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+    
+          async function infiniteScroll () {
+            let prevScroll = document.documentElement.scrollTop;
+            while (true) {
+              window.scrollBy(0, document.documentElement.clientHeight);
+              await new Promise(resolve => setTimeout(resolve, 1000));
+              const currentScroll = document.documentElement.scrollTop;
+              if (currentScroll === prevScroll) {
+                break;
+              }
+              prevScroll = currentScroll;
+            }
+          }
+          await infiniteScroll();
+        });
+        // manually extracting manufacturer description and images
+        const manufacturerDescImg = await context.evaluate(async function() {
+            const descParts = document.querySelectorAll('div[class="wrapper preview"]>*');
+            const imagesRaw = document.querySelectorAll('div[class="wrapper preview"] img');
+            var description = '';
+            var images = [];
+            for (let i = 0; i < descParts.length; i++) {
+                if (descParts[i].innerText === 'Technische Daten') {
+                    break;
+                }
+                description += descParts[i].innerText;
+            }
+            for (let i = 0; i < imagesRaw.length; i++) {
+                images.push(imagesRaw[i].src);
+            }
+            images = [...new Set(images)];
+            return [description, images];
+        });
+        inBoxText = await context.evaluate(async function () {
+            const boxContent = document.querySelectorAll('.in-the-box p');
+            const boxText = [];
+            [...boxContent].forEach((element) => {
+              boxText.push(element.innerText);
+            });
+            return boxText;
+        });
+        inBoxUrls = await context.evaluate(async function () {
+            const images = document.querySelectorAll('.in-the-box img');
+            const imagesSrc = [];
+            [...images].forEach((element) => {
+                imagesSrc.push(element.getAttribute('data-src'));
+            });
+            return imagesSrc;
+        });
+
+        comparisionText = await context.evaluate(async function () {
+            return (!!document.querySelector('.compare-headline') && document.querySelector('.compare-headline').offsetHeight > 0 && document.querySelector('.compare-headline').offsetWidth) > 0;
+        });
+
+        content = await context.evaluate(async function () {
+            return document.querySelector('body').innerText;
+        });
+
+        image = await context.evaluate(async function () {
+          const images = document.querySelectorAll('div.wrapper img[data-src]');
+          const imagesSrc = [];
+          [...images].forEach((element) => {
+            imagesSrc.push(element.getAttribute('data-src'));
+          });
+          return imagesSrc;
+        });        
+        // manually extracting videos
+        videos = await context.evaluate(async function() {
+            var videosSrc = [];
+            const rawVideos = document.querySelectorAll('div[data-video]');
+            for (let i = 0; i < rawVideos.length; i++) {
+                videosSrc.push(rawVideos[i].getAttribute('data-video'));
+            }
+            return videosSrc;
+        });
+        
+        // manually extracting iframe data (specifications, weightNet)
+        const iframeData = await context.evaluate(async function() {
+            var specifications = '';
+            var weightNet = '';
+            const rawSpecifications = document.querySelectorAll('div[class*="info"]>div[class*="info"]');
+            for (let i = 0; i < rawSpecifications.length; i++) {
+                if (rawSpecifications[i].querySelector('h5').innerText == 'Gewicht') {
+                    weightNet = rawSpecifications[i].querySelector('p').innerText;
+                }
+                specifications += ` || ${rawSpecifications[i].innerText}`;
+            }
+            return { specifications: specifications, weightNet: weightNet };
+        });
         await context.goto(myUrl);
         await context.waitForNavigation();
         await new Promise((resolve, reject) => setTimeout(resolve, 2000));
@@ -49,8 +164,49 @@ module.exports = {
             await new Promise((resolve, reject) => setTimeout(resolve, 100));
         }
         // extracting data automatically
-        await context.extract(productDetails, { transform });
+        // await context.extract(productDetails, { transform });
         await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+
+        async function addHiddenInfo (elementID, content) {
+            await context.evaluate(async function (elementID, content) {
+                const newDiv = document.createElement('div');
+                newDiv.id = elementID;
+                newDiv.textContent = content;
+                newDiv.style.display = 'none';
+                document.body.appendChild(newDiv);
+            }, elementID, content);
+        }
+        if (inBoxUrls && inBoxUrls.length) {
+            inBoxUrls.forEach((element) => {
+                addHiddenInfo('ii_inBoxUrls', element);
+            });
+        }
+        addHiddenInfo('ii_comparisionText', comparisionText);
+        console.log('inBoxText');
+        console.log(inBoxText);
+
+        if (inBoxText.length) {
+            inBoxText.forEach((element) => {
+                addHiddenInfo('ii_inBoxText', element);
+            });
+        }
+
+        addHiddenInfo('ii_manufContent', content);
+        if (image && image.length) {
+            addHiddenInfo('ii_manufContentImg', image.join(' || '));
+        }
+
+        addHiddenInfo('ii_specifications', iframeData.specifications || '');
+
+        addHiddenInfo('ii_weightNet', iframeData.weightNet || '');
+
+        if (videos && videos.length) {
+            videos = [...new Set(videos)];
+            videos.forEach((element) => {
+                addHiddenInfo('ii_videos', element);
+            });
+        }
+        
         // manually extracting description
         const description = await context.evaluate(async function() {
             const descParts = document.querySelectorAll('header+div[class*="bewerten-produkt-detail"]>div[class="bewerten-textformat"]>*');
@@ -68,6 +224,8 @@ module.exports = {
             }
             return description;
         });
+
+        addHiddenInfo('ii_description', description);
         // manually extracting additional description bullet info
         const additionalDescBulletInfo = await context.evaluate(async function() {
             const bulletInfoParts = document.querySelectorAll('div>div[class="bewerten-textformat"]>ul>li');
@@ -77,116 +235,41 @@ module.exports = {
             }
             return additionalDescBulletInfo;
         });
+
+        addHiddenInfo('ii_additionalDescBulletInfo', additionalDescBulletInfo);
+
+        addHiddenInfo('ii_shippingInfo', shippingInfo);
         // manually extracting json stored data
-        const jsonData = await context.evaluate(async function() {
-            return JSON.parse(document.querySelector('bewerten-vue-data').getAttribute('data-bewerten-json-content'));
-        });
-        // manually extracting iframe url and navigating to it
-        const gotoIframe = await context.evaluate(async function() {
-            if (!(document.querySelector('iframe[id=loadbeeIframeId]'))) {
-                await new Promise((resolve, reject) => setTimeout(resolve, 100));
-            }
-            return document.querySelector('iframe[id=loadbeeIframeId]').src;
-        });
-        await context.goto(gotoIframe);
-        await context.waitForNavigation();
-        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-        // manually extracting manufacturer description and images
-        const manufacturerDescImg = await context.evaluate(async function() {
-            const descParts = document.querySelectorAll('div[class="wrapper preview"]>*');
-            const imagesRaw = document.querySelectorAll('div[class="wrapper preview"] img');
-            var description = '';
-            var images = [];
-            for (let i = 0; i < descParts.length; i++) {
-                if (descParts[i].innerText === 'Technische Daten') {
-                    break;
-                }
-                description += descParts[i].innerText;
-            }
-            for (let i = 0; i < imagesRaw.length; i++) {
-                images.push(imagesRaw[i].src);
-            }
-            images = [...new Set(images)];
-            return [description, images];
-        });
-        // manually extracting videos
-        var videos = await context.evaluate(async function() {
-            var videos = [];
-            const rawVideos = document.querySelectorAll('div[data-video]');
-            for (let i = 0; i < rawVideos.length; i++) {
-                videos.push(rawVideos[i].getAttribute('data-video'));
-            }
-            return videos;
-        });
-        // manually extracting iframe data (specifications, weightNet)
-        const iframeData = await context.evaluate(async function() {
-            var specifications = '';
-            var weightNet = '';
-            const rawSpecifications = document.querySelectorAll('div[class*="info"]>div[class*="info"]');
-            for (let i = 0; i < rawSpecifications.length; i++) {
-                if (rawSpecifications[i].querySelector('h5').innerText == 'Gewicht') {
-                    weightNet = rawSpecifications[i].querySelector('p').innerText;
-                }
-                specifications += ` || ${rawSpecifications[i].innerText}`;
-            }
-            return { specifications: specifications, weightNet: weightNet };
-        });
-        // inserting manually extracted data to output data ref
+        if (!jsonData) {
+            jsonData = await context.evaluate(async function() {
+                return JSON.parse(document.querySelector('bewerten-vue-data').getAttribute('data-bewerten-json-content'));
+            });
+        }
+
+        console.log('212121jsonData')
+
+        console.log(jsonData)
+
+        if (jsonData) {
+            addHiddenInfo('ii_sku', jsonData.artikelKey || '');
+            addHiddenInfo('ii_mainImage', jsonData.artikel && jsonData.artikel[jsonData.artikelKey] && jsonData.artikel[jsonData.artikelKey].ansichten && jsonData.artikel[jsonData.artikelKey].ansichten.length ? jsonData.artikel[jsonData.artikelKey].ansichten[0].zoomUrl : '');
+        }
+
         var dataRef = await context.data();
-        dataRef[0].data[0].group[0].specifications[0].text = iframeData.specifications;
-        dataRef[0].data[0].group[0].weightNet = [{ text: iframeData.weightNet }];
-        dataRef[0].data[0].group[0].manufacturerDescription[0].text = manufacturerDescImg[0];
-        dataRef[0].data[0].group[0].manufacturerImages = [];
-        for (let i = 0; i < manufacturerDescImg[1].length; i++) {
-            dataRef[0].data[0].group[0].manufacturerImages.push({ text: manufacturerDescImg[1][i] });
-        }
-        if ('videos' in dataRef[0].data[0].group[0]) {
-            for (let i = 0; i < dataRef[0].data[0].group[0].videos.length; i++) {
-                videos.push(dataRef[0].data[0].group[0].videos[i].text)
-            }
-        }
-        console.log(videos);
-        videos = [...new Set(videos)];
-        dataRef[0].data[0].group[0].videos = [];
-        console.log(videos);
-        for (let i = 0; i < videos.length; i++) {
-            dataRef[0].data[0].group[0].videos.push({ text: videos[i] });
-        }
-        dataRef[0].data[0].group[0].description[0].text = description;
-        dataRef[0].data[0].group[0].additionalDescBulletInfo = [{ text: additionalDescBulletInfo }];
-        dataRef[0].data[0].group[0].shippingInfo[0].text = shippingInfo;
-        dataRef[0].data[0].group[0].sku[0].text = jsonData.artikelKey;
-        if (dataRef[0].data[0].group[0].alternateImages.length + 1 === jsonData.artikel[jsonData.artikelKey].ansichten.length) {
-            for (let i = 0; i < dataRef[0].data[0].group[0].alternateImages.length; i++) {
-                dataRef[0].data[0].group[0].alternateImages[i].text = jsonData.artikel[jsonData.artikelKey].ansichten[i + 1].zoomUrl;
-                delete dataRef[0].data[0].group[0].alternateImages[i].xpath;
-            }
-            if (!('image' in dataRef[0].data[0].group[0])) {
-                dataRef[0].data[0].group[0].image = [];
-                dataRef[0].data[0].group[0].image.push({ text: ' ' });
-            } else {
-                delete dataRef[0].data[0].group[0].image[0].xpath;
-            }
-            dataRef[0].data[0].group[0].image[0].text = jsonData.artikel[jsonData.artikelKey].ansichten[0].zoomUrl;
-        }
-        var checkData = await context.evaluate(async function() {
-            let getInTheBox = document.querySelector('div.in-the-box img')
-            let inTheBoxImages = [];
-            let inTheBoxTexts = [];
-            if (getInTheBox) {
-                let getAllProducts = document.querySelectorAll('div.in-the-box div');
-                for (i = 0; i < getAllProducts.length; i++) {
-                    inTheBoxImages.push(getAllProducts[i].querySelector('img').getAttribute('src'));
-                    inTheBoxTexts.push(getAllProducts[i].querySelector('p').innerText);
-                }
-                let imagesUrl = inTheBoxImages.join(' || ')
-                let imagesText = inTheBoxTexts.join(' || ')
-                document.head.setAttribute('intheboxurl', imagesUrl);
-                document.head.setAttribute('intheboxtext', imagesText);
-            }
-            return document.querySelector('div.in-the-box img');
-        });
-        checkData;
+
+
+        console.log('212121dataRef')
+        console.log(dataRef)
+        // if (dataRef[0].data[0].group[0].alternateImages.length + 1 === jsonData.artikel[jsonData.artikelKey].ansichten.length) {    
+
+        //     if (!('image' in dataRef[0].data[0].group[0])) {
+        //         dataRef[0].data[0].group[0].image = [];
+        //         dataRef[0].data[0].group[0].image.push({ text: ' ' });
+        //     } else {
+        //         delete dataRef[0].data[0].group[0].image[0].xpath;
+        //     }
+        //     dataRef[0].data[0].group[0].image[0].text = jsonData.artikel[jsonData.artikelKey].ansichten[0].zoomUrl;
+        // }
         await new Promise((resolve, reject) => setTimeout(resolve, 6000));
         return await context.extract(productDetails, { transform });
         // const inboxdata = await context.evaluate(async function() {
