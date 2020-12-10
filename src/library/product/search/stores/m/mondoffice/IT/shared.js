5b62b7e578ff6b8486d09ea8e07736d6bb6c9bc3
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-12-10 15:17:23 +0200

    add rank field

--- src/library/product/search/stores/m/mondoffice/IT/shared.js
+++ src/library/product/search/stores/m/mondoffice/IT/shared.js
@@ -1,15 +1,29 @@
 /**
  *
  * @param {ImportIO.Group[]} data
+ * @param context
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  function findField (key, arr = data) {
+    const a = arr.map(el => el.group);
+    let obj = [];
+    a.forEach(e => {
+      e.forEach(el => {
+        if (el[key]) {
+          obj = el[key];
+        }
+      });
+    });
+    return obj;
+  }
   data.forEach(el => {
     el.group.forEach((gr, index) => {
       try {
-        gr['_input'] = gr.input;
-        gr['_url'] = gr.url;
         gr['rankOrganic'] = [{ text: index + 1 }];
+        gr['rank'] = [{ text: index + 1 }];
+        gr['_url'] = findField('url');
+        gr['_input'] = findField('input');
       } catch (e) {
         console.log(e);
       }
