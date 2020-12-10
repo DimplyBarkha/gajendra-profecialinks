037fa4451daffa223697669f5774740885867d35
Andrii Kuzmych <andriy.kuzjmych@gmail.com>
2020-12-10 21:38:44 +0200

    #820 core fix

--- src/library/product/details/stores/b/bcc/NL/extract.js
+++ src/library/product/details/stores/b/bcc/NL/extract.js
@@ -8,4 +8,17 @@ module.exports = {
     domain: 'bcc.nl',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(() => {
+      const button = document.querySelector('section.productoffer .checkout-initiator');
+      if (button) {
+        document.querySelector('h1#page_title').setAttribute('availability', 'In Stock');
+      } else {
+        document.querySelector('h1#page_title').setAttribute('availability', 'Out of Stock');
+      }
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
