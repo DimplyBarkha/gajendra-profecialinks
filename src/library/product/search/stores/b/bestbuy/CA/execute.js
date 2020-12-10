d400b208655a44cbbe6bd9bd6fc47a8f2a94c08e
viswa <viswashankar.veginati@od.import.io>
2020-12-11 01:21:58 +0530

    bestbuy  by viswa

--- src/library/product/search/stores/b/bestbuy/CA/execute.js
+++ src/library/product/search/stores/b/bestbuy/CA/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'bestbuy',
     domain: 'bestbuy.ca/en-ca',
     url: 'https://www.bestbuy.ca/en-ca/search?search={searchTerms}',
-    loadedSelector: 'div[class="x-page-content container_3Sp8P"] main',
+    loadedSelector: 'div[class="productList_31W-E"]',
     noResultsXPath: null,
     zipcode: '',
   },
--- src/library/product/search/stores/b/bestbuy/CA/extract.js
+++ src/library/product/search/stores/b/bestbuy/CA/extract.js
@@ -2,41 +2,47 @@ const { transform } = require('../../../../shared');
 module.exports = {
     implements: 'product/search/extract',
     parameterValues: {
-        country: 'CA',
-        store: 'bestbuy',
-        transform: transform,
-        domain: 'bestbuy.ca/en-ca',
-        zipcode: '',
+      country: 'CA',
+      store: 'bestbuy',
+      transform: transform,
+      domain: 'bestbuy.ca',
+      zipcode: '',
     },
-    implementation,
-};
-async function implementation(
-    inputs,
-    parameters,
-    context,
-    dependencies,
-) {
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    await context.evaluate(async function () {
-        let scrollTop = 0;
-        while (scrollTop !== 20000) {
-            await stall(1000);
-            scrollTop += 500;
-            window.scroll(0, scrollTop);
-            if (scrollTop === 20000) {
-                await stall(5000);
-                break;
-            }
-        }
+    implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+      await context.evaluate(async () => {
         function stall(ms) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve();
-                }, ms);
-            });
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
         }
-    });
-    return await context.extract(productDetails, { transform });
-    //return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
-}
+  
+        let scrollTop = 0;
+        while (scrollTop !== 10000) {
+          await stall(3000);
+          scrollTop += 500;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 10000) {
+            await stall(500);
+            break;
+          }
+        }
+        const moreButton = document.evaluate('//div[@class="loadMoreRow_1TEZj loadMoreButtonContainer_35w02"]//span[@class="content_3dXxd"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        console.log("moreButton:: ", moreButton.singleNodeValue);
+        if (moreButton && moreButton.singleNodeValue != null) {
+          let index = 0;
+          while (index < 7) {
+           
+            try {
+              moreButton.singleNodeValue.click();
+              console.log("more button clicked: ", index);
+            } catch (e) { }
+            await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+            index++;
+          }
+        }
+      });
+      return await context.extract(productDetails, { transform });
+    },
+  };
