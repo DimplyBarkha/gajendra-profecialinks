b121c1371ddd2acd6e512267e21b8b224c4a5e32
Your Name <karthik.ravi-odc@partner.import.io>
2020-12-10 16:28:19 +0530

    new

--- src/library/product/details/stores/m/macys/US/extract.js
+++ src/library/product/details/stores/m/macys/US/extract.js
@@ -35,7 +35,7 @@ module.exports = {
         var img = name.split('\"image":')[1]
         if (img != null) {
           var img = img.split(",")[0]
-          var img = img.slice(2,-1)
+          var img = img.slice(2, -1)
           addElementToDocument('image', img);
         }
       }
@@ -43,20 +43,27 @@ module.exports = {
         var ID = name.split('"productID":')[1]
         if (ID != null) {
           var ID = ID.split(",")[0]
-          var ID = ID.slice(2,-1)
+          var ID = ID.slice(2, -1)
           addElementToDocument('ID', ID);
-          }
-}
-var name = getXpath('//script[@id="productMktData"]//following-sibling::script[1]/text()', 'nodeValue');
+        }
+      }      
+      var name = getXpath('//script[@id="productMktData"]//following-sibling::script[1]/text()', 'nodeValue');
       if (name != null) {
         var upc = name.split('upcNumber')[1]
         if (upc != null) {
           var upc = upc.split(",")[0]
-          var upc = upc.slice(5,-3)
+          var upc = upc.slice(5, -3)
           addElementToDocument('upc', upc);
         }
       }
-
+      if (name != null) {
+        var des = name.split('completeName')[1]
+        if (des != null) {
+          var des = des.split(",")[0]
+          var des = des.slice(5, -3)
+          addElementToDocument('completeName', des);
+        }
+      }   
     });
 
     return await context.extract(productDetails, { transform });
