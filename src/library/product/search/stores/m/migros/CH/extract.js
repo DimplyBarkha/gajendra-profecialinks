0b81ec532d1e173c978e5268af2119476acd546d
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2020-12-10 08:57:19 +0530

    extract.js updated

--- src/library/product/search/stores/m/migros/CH/extract.js
+++ src/library/product/search/stores/m/migros/CH/extract.js
@@ -1,4 +1,41 @@
 const {transform} = require('../format');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+   await context.evaluate(async () => {
+    let scrolltop = document.getElementById("main").scrollTop;
+
+    while(!!document.querySelector('div.btn-view-more-products > button')){
+      document.querySelector('div.btn-view-more-products > button').click();
+      await new Promise(r => setTimeout(r, 1000));
+    }
+
+    document.getElementById("main").scrollBy({ top: 1000, left: 0, behavior: 'smooth'});
+    await new Promise(r => setTimeout(r, 1000));
+
+    let thmcount = document.querySelectorAll('ul.productGroup li.item img[src]').length;
+    let itemcount = document.querySelectorAll('ul.productGroup li.item').length;
+
+    while(itemcount!=thmcount){
+      let newscrolltop = document.getElementById("main").scrollTop;
+      document.getElementById("main").scrollBy({ top: newscrolltop-scrolltop, left: 0, behavior: 'smooth'});
+      scrolltop = newscrolltop;
+      await new Promise(r => setTimeout(r, 500));
+      let newthmcount = document.querySelectorAll('ul.productGroup li.item img[src]').length;
+      let newitemcount = document.querySelectorAll('ul.productGroup li.item').length;
+      if(newthmcount==newitemcount){
+        break;
+      }
+    }
+
+  })
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -6,5 +43,7 @@ module.exports = {
     store: 'migros',
     transform : transform,
     domain: 'migros.ch',
+    zipcode: '',
   },
+  implementation,
 };
