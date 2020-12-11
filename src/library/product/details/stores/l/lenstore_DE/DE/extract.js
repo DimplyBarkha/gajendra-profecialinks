dbf72a970b878d3fdcd7996875b91ae790cd471e
Harshkha <harsh.khare-od@partner.import.io>
2020-12-11 18:34:26 +0530

    IM updates complete

--- src/library/product/details/stores/l/lenstore_DE/DE/extract.js
+++ src/library/product/details/stores/l/lenstore_DE/DE/extract.js
@@ -8,8 +8,8 @@ module.exports = {
     domain: 'www.lenstore.de',
     zipcode: '',
   },
-  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
-    await context.evaluate(async function () {
+  implementation: async ({ inputstring }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(() => {
       function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
@@ -17,44 +17,112 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
-      const getXpath = (xpath, prop) => {
-        const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+
+      var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
         let result;
         if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
         else result = elem ? elem.singleNodeValue : '';
         return result && result.trim ? result.trim() : result;
       };
-      // @ts-ignore
-      const rawdata = getXpath("(//p/following::script[2])[7]/text()", 'nodeValue');
-      const jsondata = JSON.parse(rawdata);
-      //var gtin = JSON.parse(jsondata).gtin13
-      const gtin = jsondata.gtin13
-      const gtin1= JSON.stringify(gtin)
-      //jsondata.gtin13;
-      //if (gtin != null){
-      addElementToDocument('gtin', gtin1);
-      //}
 
-      const sku = jsondata.sku;
-      //if (sku != null){
-      addElementToDocument('sku', sku);
-      //}
+      //availability
+      var script = getXpath('//form[@class="js-add-to-basket-form"]/following::script[2]/text()', 'nodeValue');
+      
+      if(script != null){
+        var data = JSON.parse(script);
+        var gtin = data.gtin13;
+        if(gtin != null){
+          addElementToDocument('gtin', gtin);
+        }
+        var sku = data.sku;
+        if (sku != null){
+        addElementToDocument('sku', sku);
+        }
+        var url = data.url;
+        if (url != null){
+        addElementToDocument('url', url);
+        }
+        var productID = data.productID;
+        if (productID != null){
+        addElementToDocument('id', productID);
+        }
+        var brand = data.brand;
+        if (brand != null){
+        addElementToDocument('brand', brand);
+        }
+      }
 
-      const url = jsondata.url;
-      //if (url != null){
-      addElementToDocument('url', url);
-      //}
-
-      const productID = jsondata.productID;
-      // if (productID != null){
-      addElementToDocument('id', productID);
+      if(script != null){
+        var data1 = JSON.parse(script);
+        var rpc = data.productID;
+        if(rpc != null){
+          addElementToDocument('rpc', rpc);
+        }
+      }
+      
+      
+      // if(script != null){
+      //   var zz =  script.split('availability": "')[1];
+      //   zz = zz.split(",")[0];
+      //   var arr = zz.split("/");
+      //   zz = arr[arr.length-1];
+      //   if(zz.includes("In")){
+      //     zz = "In Stock"
+      //   }else{
+      //     zz = "Out of Stock"
+      //   }
+      //   if(zz != null){
+      //     addElementToDocument('zz', zz);
+      //   }
       // }
 
-      const brand = jsondata.brand;
-      if (brand != '') {
-        addElementToDocument('brand', brand);
+      //gtin
+      // if(script != null){
+      //   var gt = script.split('gtin13": "')[1];
+      //   gt = gt.split(",")[0];
+      //   gt = gt.slice(0,-1);
+      //   if(gt != null){
+      //     addElementToDocument('gt', gt);
+      //   }
+      // }
+
+      // //rpc
+      // if(script != null){
+      //   var rpc = script.split('productID": "')[1];
+      //   rpc = rpc.split(",")[0];
+      //   rpc = rpc.slice(0,-1);
+      //   if(rpc != null){
+      //     addElementToDocument('rpc', rpc);
+      //   }
+      // }
+
+      //description
+      // var ab = document.querySelectorAll('#relatedShift');
+      var desc = getAllXpath('//div[@class="c-product__related-content"]/p/text()', 'nodeValue');
+      if(desc != null){
+        var str = "";
+        for(var i=0; i< desc.length; i++){
+          str = str + desc[i] + " ";
+        }
+        addElementToDocument('str', str);
       }
+      
+
+
+
+
     });
-    await context.extract(productDetails, { transform: transformParam });
+    await context.extract(productDetails);
   },
 };
