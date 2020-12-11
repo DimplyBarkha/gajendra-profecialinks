10a6427cb1afc69a58c67c914af42b41027c1be2
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-12-11 22:08:18 +0530

    changes made according to QA

--- src/library/product/search/stores/h/houzz/US/paginate.js
+++ src/library/product/search/stores/h/houzz/US/paginate.js
@@ -3,16 +3,13 @@ module.exports = {
   parameterValues: {
     country: "US",
     store: "houzz",
-    nextLinkSelector: null,
+    nextLinkSelector:
+      "#hz-br__result-set-root > div.hz-card.clearfix.hz-br__result-set > div.browse-result-set-footer-wrapper > div > div.hz-pagination-bottom-wrapper.hz-track-me > div > div > a.hz-pagination-link.hz-pagination-link--next",
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: ".hz-image-container img",
     noResultsXPath: null,
-    openSearchDefinition: {
-      offset: 36,
-      template:
-        "https://www.houzz.com/products/query/{searchTerms}/nqrwns/p/{offset}",
-    },
+    openSearchDefinition: null,
     domain: "houzz.com",
     zipcode: "",
   },
