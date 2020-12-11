9075ca1f35619cbc67e6cf07e4cb5c0600b49163
RukhsarAnsari-syn <rukhsar.ansari@synerzip.com>
2020-12-11 19:51:29 +0530

    #1777. kick back issue is resolved

--- src/library/product/search/stores/C/Chronodrive/FR/extract.js
+++ src/library/product/search/stores/C/Chronodrive/FR/extract.js
@@ -12,16 +12,18 @@ module.exports = {
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     await context.evaluate(async function () {
       let scrollSelector = document.querySelector('#footer');
+      let articles = document.querySelectorAll('div[id*="productListZone"] > article:not([class*="item--suggest"])');
       // @ts-ignore
       let scrollLimit = scrollSelector ? scrollSelector.offsetTop : '';
       let yPos = 0;
-      while (scrollLimit && yPos < scrollLimit) {
+      while (scrollLimit && yPos < scrollLimit && articles && articles.length <= 150) {
         yPos = yPos + 350;
         window.scrollTo(0, yPos);
         scrollSelector = document.querySelector('#footer');
         // @ts-ignore
         scrollLimit = scrollSelector ? scrollSelector.offsetTop : '';
         await new Promise(resolve => setTimeout(resolve, 2500));
+        articles = document.querySelectorAll('div[id*="productListZone"] > article:not([class*="item--suggest"])');
       }
     });
     await new Promise(resolve => setTimeout(resolve, 20000));
