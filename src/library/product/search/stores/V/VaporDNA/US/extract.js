9b2187c7a6ad996b4547d4efb46c1b8cfad5d527
cshelake <cshelake7@gmail.com>
2020-12-11 17:13:15 +0530

    Update extract.js
    pagination

--- src/library/product/search/stores/V/VaporDNA/US/extract.js
+++ src/library/product/search/stores/V/VaporDNA/US/extract.js
@@ -1,3 +1,22 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+   await context.evaluate(async () => {
+   	var button = document.querySelector('div#age-check-prompt button#submit_birthdate');
+      button.click();
+    let scrolltop = document.getElementById("PageContainer").scrollTop;
+    var btn = document.querySelector('div.ais-infinite-hits--showmore > button');
+    while(!btn.disabled){
+      document.querySelector('div.ais-infinite-hits--showmore > button').click();
+      await new Promise(r => setTimeout(r, 1000));
+    }
+  })
+  return await context.extract(productDetails);
+}
 
 module.exports = {
   implements: 'product/search/extract',
@@ -8,4 +27,5 @@ module.exports = {
     domain: 'vapordna.com',
     zipcode: "''",
   },
+  implementation,
 };
