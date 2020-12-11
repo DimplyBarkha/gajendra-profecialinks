4e851f3c07ff3759f6e238f2ce845ca7b213ddbf
viswa <viswashankar.veginati@od.import.io>
2020-12-11 09:06:47 +0530

    bestbuy  by viswa

--- src/library/product/search/stores/b/bestbuy/CA/extract.js
+++ src/library/product/search/stores/b/bestbuy/CA/extract.js
@@ -1,48 +1,83 @@
 const { transform } = require('../../../../shared');
 module.exports = {
-    implements: 'product/search/extract',
-    parameterValues: {
-      country: 'CA',
-      store: 'bestbuy',
-      transform: transform,
-      domain: 'bestbuy.ca',
-      zipcode: '',
-    },
-    implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
-      await context.evaluate(async () => {
-        function stall(ms) {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              resolve();
-            }, ms);
-          });
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'CA',
+    store: 'bestbuy',
+    transform,
+    domain: 'bestbuy.ca',
+    zipcode: '',
+  },
+  implementation
+};
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  // await context.waitForXPath('//div[@class="productPricingContainer_3gTS3"]//meta[@itemprop="price"]');
+  // await context.evaluate(async () => {
+  //     const element = document.querySelector('footer.globalFooter_Kvg_F ');
+  //     if (element) {
+  //       element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+  //       await new Promise((resolve) => setTimeout(resolve, 10000));
+  //     }
+  //     await new Promise((resolve) => setTimeout(resolve, 10000));
+  // });
+  await context.evaluate(async () => {
+    // window.scrollTo(0,9999);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 6000));
+      // await context.waitForSelector('div[data-test="product-thumb"] a div img');
+    }catch(error){
+      console.log(error)
+    }
+    async function infiniteScroll () {
+      let prevScroll = document.documentElement.scrollTop;
+      while (true) {
+        window.scrollBy(0, document.documentElement.clientHeight);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const currentScroll = document.documentElement.scrollTop;
+        if (currentScroll === prevScroll) {
+          break;
         }
-  
-        let scrollTop = 0;
-        while (scrollTop !== 20000) {
-          await stall(3000);
-          scrollTop += 500;
-          window.scroll(0, scrollTop);
-          if (scrollTop === 20000) {
-            await stall(500);
-            break;
-          }
-        }
-        const moreButton = document.evaluate('//div[@class="loadMoreRow_1TEZj loadMoreButtonContainer_35w02"]//span[@class="content_3dXxd"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-        console.log("moreButton:: ", moreButton.singleNodeValue);
-        if (moreButton && moreButton.singleNodeValue != null) {
-          let index = 0;
-          while (index < 7) {
-           
-            try {
-              moreButton.singleNodeValue.click();
-              console.log("more button clicked: ", index);
-            } catch (e) { }
-            await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-            index++;
-          }
-        }
-      });
-      return await context.extract(productDetails, { transform });
-    },
-  };
+        prevScroll = currentScroll;
+      }
+    }
+    await infiniteScroll();
+  })
+
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 6000));
+    // await context.waitForSelector('div[data-test="product-thumb"] a div img');
+  } catch (error) {
+    console.log('error: ', error);
+    
+  }
+  return await context.extract(productDetails, { transform });
+}
+
+// async function implementation (
+//   inputs,
+//   parameters,
+//   context,
+//   dependencies,
+// ) {
+//   const { transform } = parameters;
+//   const { productDetails } = dependencies;
+//   await context.evaluate(async () => {
+//     const loadMore = (document.querySelector('button.loadMore_3AoXT'));
+//     // const endOfResult = document.querySelector('div.endOfList_b04RG');
+//     if(loadMore){
+//      while(loadMore){
+//       // @ts-ignore
+//       loadMore.click();
+//       await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+//      }
+//     }
+//   });
+//   return await context.extract(productDetails, { transform });
+// }
