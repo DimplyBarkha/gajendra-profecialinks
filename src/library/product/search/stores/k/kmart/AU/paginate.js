9a8e30ac25d08c51105d1817d3875a42c1ae6238
unknown <leena.gavali-odc@partner.import.io>
2020-12-11 18:12:32 +0530

    kmart_pagination_trial

--- src/library/product/search/stores/k/kmart/AU/paginate.js
+++ src/library/product/search/stores/k/kmart/AU/paginate.js
@@ -7,7 +7,7 @@ module.exports = {
     nextLinkSelector: 'a[class="right_arrow"]',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'div[class="product_listing_container product_listing_container_new apparel-right"]',
+    loadedSelector: null,
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'kmart.com.au',
