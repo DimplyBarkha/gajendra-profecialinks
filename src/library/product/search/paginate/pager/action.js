c4d66c8c5c04e6b2f869ab7dc461a80148979342
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-11 19:29:15 +0530

    changes

--- src/library/product/search/paginate/pager/action.js
+++ src/library/product/search/paginate/pager/action.js
@@ -30,7 +30,7 @@ async function implementation (
     await context.waitForFunction((selector) => {
       console.log(selector, document.querySelector(selector));
       return !document.querySelector(selector);
-    }, { timeout: 800000 }, spinnerSelector);
+    }, {timeout: 80000}, spinnerSelector);
     console.log('Spinner went away', spinnerSelector);
     return true;
   }
