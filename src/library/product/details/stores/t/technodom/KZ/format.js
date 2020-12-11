978a9f708b92eaffb63a019601b3b0aae2e86a7e
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-11 14:06:36 +0530

    #1867 fixing QA1 issue

--- src/library/product/details/stores/t/technodom/KZ/format.js
+++ src/library/product/details/stores/t/technodom/KZ/format.js
@@ -24,7 +24,12 @@ const transform = (data) => {
     };
     for (const { group } of data) {      
       var variantCount = 0;
-      for (let row of group) {              
+      for (let row of group) { 
+        if(row.price){
+          row.price.forEach(item=>{
+            item.text=item.text+".00";
+          })
+        }             
             if (row.description) {
               let inf =  [];
               row.description.forEach(item => {
