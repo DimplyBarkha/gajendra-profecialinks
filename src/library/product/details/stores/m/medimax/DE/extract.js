0e28a06ac1d65d7c07da4eb2fca679414416177a
osmarghyadeep <arghyadep.g@partner.import.io>
2020-12-12 03:59:08 +0530

    Updated the code to include video

--- src/library/product/details/stores/m/medimax/DE/extract.js
+++ src/library/product/details/stores/m/medimax/DE/extract.js
@@ -11,7 +11,7 @@ async function implementation(
     try {
         await context.click('button.cookies-overlay-dialog__accept-all-btn');
     } catch (err) {
-
+        console.log('cannot find or click the accept all button for cookies', err);
     }
 
     const link = await context.evaluate(function () {
@@ -19,16 +19,32 @@ async function implementation(
     });
     const src = await context.evaluate(async function() {
         const iframe = document.querySelector('#loadbeeTabContent');
-        const src = iframe ? iframe.src : '';
+        // const src = iframe ? (iframe.src||iframe._src) : '';
+        let src = '';
+        if(iframe) {
+            if(iframe.hasAttribute('src')) {
+                src = iframe.getAttribute('src');
+            } else if(iframe.hasAttribute('_src')) {
+                src = iframe.getAttribute('_src');
+            } else {
+                console.log('we do not have any src in iframe');
+            }
+        } else {
+            console.log('we do not have the iframe');
+        }
+        console.log('iframe src to go to - ' + src);
+        
         return src;
     });
     // await context.extract(productDetails, { transform });
+    let content = null;
+    let image = null;
+    let comparisionText = null;
+    let inBoxText = null;
+    let inBoxUrls = null;
+    let videoUrlIframe = [];
     if (src) {
-        let content = null;
-        let image = null;
-        let comparisionText = null;
-        let inBoxText = null;
-        let inBoxUrls = null;
+        
         try {
             await context.goto(src, { timeout: 100000, waitUntil: 'load', checkBlocked: true });
 
@@ -36,16 +52,16 @@ async function implementation(
                 await new Promise((resolve) => setTimeout(resolve, 5000));
         
                 async function infiniteScroll () {
-                let prevScroll = document.documentElement.scrollTop;
-                while (true) {
-                    window.scrollBy(0, document.documentElement.clientHeight);
-                    await new Promise(resolve => setTimeout(resolve, 1000));
-                    const currentScroll = document.documentElement.scrollTop;
-                    if (currentScroll === prevScroll) {
-                    break;
+                    let prevScroll = document.documentElement.scrollTop;
+                    while (true) {
+                        window.scrollBy(0, document.documentElement.clientHeight);
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        const currentScroll = document.documentElement.scrollTop;
+                        if (currentScroll === prevScroll) {
+                        break;
+                        }
+                        prevScroll = currentScroll;
                     }
-                    prevScroll = currentScroll;
-                }
                 }
                 await infiniteScroll();
                 await new Promise((resolve) => setTimeout(resolve, 8000));
@@ -84,58 +100,30 @@ async function implementation(
               return imagesSrc;
             });
 
+            
+            videoUrlIframe = await context.evaluate(async function () {
+                let videoElm = document.querySelectorAll('div[class*="play-btn"]');
+                let videoIframeArr = [];
+                if(videoElm.length === 0) {
+                    console.log('we do not have video in iframe or need to update the selector');
+                } else {
+                    for(let i = 0; i < videoElm.length; i++) {
+                        if(videoElm[i].hasAttribute('data-video') && !!(videoElm[i].getAttribute('data-video')) && !(videoIframeArr.includes(videoElm[i].getAttribute('data-video')))) {
+                            videoIframeArr.push(videoElm[i].getAttribute('data-video'));
+                            console.log(videoElm[i].getAttribute('data-video'));
+                        } else {
+                            console.log('we do not have this attribute anymore or this attribute is empty or it is already present');
+                        }
+                    }
+                }
+                return videoIframeArr;
+            });
+
 
             await context.goto(link, { timeout: 100000 });
             await context.waitForSelector('div#main-section', { timeout: 45000 });
 
-            async function addHiddenInfo (elementID, content) {
-                await context.evaluate(async function (elementID, content) {
-                    const newDiv = document.createElement('div');
-                    newDiv.id = elementID;
-                    newDiv.textContent = content;
-                    newDiv.style.display = 'none';
-                    document.body.appendChild(newDiv);
-                }, elementID, content);
-            }
-            if (inBoxUrls && inBoxUrls.length) {
-                inBoxUrls.forEach((element) => {
-                    addHiddenInfo('ii_inBoxUrls', element);
-                });
-            }
-            addHiddenInfo('ii_comparisionText', comparisionText ? 'Yes' : 'No');
-            console.log('inBoxText');
-            console.log(inBoxText);
-    
-            if (inBoxText.length) {
-                inBoxText.forEach((element) => {
-                    addHiddenInfo('ii_inBoxText', element);
-                });
-            }
-
-            addHiddenInfo('ii_manufContent', content);
-            if (image && image.length) {
-                addHiddenInfo('ii_manufContentImg', image.join(' || '));
-            }
-        
-            await context.waitForFunction(function (sel) {
-                return Boolean(document.querySelector(sel));
-            }, { timeout: 20000 }, 'body');
             
-            const uipdpProducts = await context.evaluate(async function () {
-                const productCarousel = [...document.querySelectorAll('div.carousel-item')];
-                const uipdpArr = [];
-                productCarousel.forEach((element) => {
-                    const brand = element.querySelector('.carousel-brand') ? element.querySelector('.carousel-brand').innerText : '';
-                    const productName = element.querySelector('.carousel-name') ? element.querySelector('.carousel-name').innerText : '';
-                    uipdpArr.push(brand + ' ' + productName);
-                });
-                return uipdpArr.join(' || ');
-            });
-            addHiddenInfo('ii_uipdp', uipdpProducts);
-            
-          
-            // return await context.extract(productDetails, { transform });
-            return await context.extract(productDetails, { type: 'MERGE_ROWS', transform });
         } catch (error) {
             try {
                 await context.evaluate(async function(src) {
@@ -148,7 +136,76 @@ async function implementation(
             }
         }
         // return await context.extract(productDetails, { transform });
+    } else {
+        console.log('we do not have the src for iframe');
     }
+
+    async function addHiddenInfo (elementID, content) {
+        await context.evaluate(async function (elementID, content) {
+            const newDiv = document.createElement('div');
+            newDiv.id = elementID;
+            newDiv.textContent = content;
+            newDiv.style.display = 'none';
+            document.body.appendChild(newDiv);
+        }, elementID, content);
+    }
+    if (inBoxUrls && inBoxUrls.length) {
+        inBoxUrls.forEach((element) => {
+            addHiddenInfo('ii_inBoxUrls', element);
+        });
+    }
+    addHiddenInfo('ii_comparisionText', comparisionText ? 'Yes' : 'No');
+    console.log('inBoxText');
+    console.log(inBoxText);
+
+    if (inBoxText && inBoxText.length) {
+        inBoxText.forEach((element) => {
+            addHiddenInfo('ii_inBoxText', element);
+        });
+    }
+
+    addHiddenInfo('ii_manufContent', content);
+    if (image && image.length) {
+        addHiddenInfo('ii_manufContentImg', image.join(' || '));
+    }
+
+    await context.waitForFunction(function (sel) {
+        return Boolean(document.querySelector(sel));
+    }, { timeout: 20000 }, 'body');
+    
+    const uipdpProducts = await context.evaluate(async function () {
+        const productCarousel = [...document.querySelectorAll('div.carousel-item')];
+        const uipdpArr = [];
+        productCarousel.forEach((element) => {
+            const brand = element.querySelector('.carousel-brand') ? element.querySelector('.carousel-brand').innerText : '';
+            const productName = element.querySelector('.carousel-name') ? element.querySelector('.carousel-name').innerText : '';
+            uipdpArr.push(brand + ' ' + productName);
+        });
+        return uipdpArr.join(' || ');
+    });
+    addHiddenInfo('ii_uipdp', uipdpProducts);
+    
+    // need to add video urls links
+    try {
+        await context.evaluate(async function (videoUrlIframe) {
+            async function addHiddenInfoAsync (elementID, content) {
+                const newDiv = document.createElement('div');
+                newDiv.id = elementID;
+                newDiv.textContent = content;
+                newDiv.style.display = 'none';
+                document.body.appendChild(newDiv); 
+            }
+            for(let i = 0; i < videoUrlIframe.length; i++) {
+                await addHiddenInfoAsync(`iframe-video-${i + 1}`, videoUrlIframe[i]);
+            }
+        }, videoUrlIframe);
+    } catch (err) {
+        console.log('error while getting the video', err)
+    }
+    
+  
+    // return await context.extract(productDetails, { transform });
+    return await context.extract(productDetails, { type: 'MERGE_ROWS', transform });
 }
 
 module.exports = {
