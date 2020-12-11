987d462d6d63eceffdfb244e72f28f60ca15e5d7
Ajinkya Kathe <ajinkya.kathe@synerzip.com>
2020-12-12 03:19:39 +0530

    Refactored code of extract js

--- src/library/product/search/stores/b/boots/UK/extract.js
+++ src/library/product/search/stores/b/boots/UK/extract.js
@@ -1,4 +1,5 @@
 const { transform } = require('../format');
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -10,6 +11,7 @@ module.exports = {
   },
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     const upcDataResults = await context.evaluate(async () => {
+
       function stall(ms) {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
@@ -19,86 +21,32 @@ module.exports = {
       }
       const upcData = {};
       let indexElement = 0;
-      const moreButton = document.evaluate('//button[@class="results-btn-viewmore"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-      if (moreButton && moreButton.singleNodeValue != null) {
-        let offset = 0
-        while (document.querySelector('button[class="results-btn-viewmore"]').getAttribute('disabled') != "") {
-          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-          try {
-            const items = document.querySelectorAll('.ais-Hits ol.ais-Hits-list li');
-            items.forEach((item, index) => {
-              try {
-                indexElement = offset + index;
-                const obj = JSON.parse(item.textContent);
+      let offset = 0;
+
+      async function collectUpdateData(offset) {
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+        try {
+          const items = document.querySelectorAll('.ais-Hits ol.ais-Hits-list li');
+          items.forEach((item, index) => {
+            try {
+              indexElement = offset + index;
+              const obj = JSON.parse(item.textContent);
+              if (Object.keys(obj).length) {
                 upcData[indexElement] = { upc: obj.upc, manufacturerModel: obj.manufacturerModel }
-              } catch (error) {
-                console.log('Error =>', error);
               }
-            });
-            offset += items.length;
-          } catch (error) {
-            console.log('IN div Error =>', error);
-          }
-          moreButton.singleNodeValue.click();
-          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-          let scrollTop = 0;
-          while (scrollTop !== 20000) {
-            await stall(500);
-            scrollTop += 1000;
-            window.scroll(0, scrollTop);
-            if (scrollTop === 20000) {
-              await stall(500);
-              break;
-            }
-          }
-        }
-        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-        try {
-          const items = document.querySelectorAll('.ais-Hits ol.ais-Hits-list li');
-          items.forEach((item, index) => {
-            try {
-              indexElement = offset + index;
-              const obj = JSON.parse(item.textContent);
-              upcData[indexElement] = { upc: obj.upc, manufacturerModel: obj.manufacturerModel }
-              //upcData.push({ index: indexElement, upc: obj.upc, manufacturerModel: obj.manufacturerModel })
+
             } catch (error) {
               console.log('Error =>', error);
             }
           });
           offset += items.length;
+          return offset;
         } catch (error) {
-          console.log('IN div Error =>', error);
+          console.log('Error while collecting update data', error);
         }
-        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-        let scrollTop = 0;
-        while (scrollTop !== 20000) {
-          await stall(500);
-          scrollTop += 1000;
-          window.scroll(0, scrollTop);
-          if (scrollTop === 20000) {
-            await stall(500);
-            break;
-          }
-        }
-      } else {
-        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-        try {
-          let offset = 0
-          const items = document.querySelectorAll('.ais-Hits ol.ais-Hits-list li');
-          items.forEach((item, index) => {
-            try {
-              indexElement = offset + index;
-              const obj = JSON.parse(item.textContent);
-              upcData[indexElement] = { upc: obj.upc, manufacturerModel: obj.manufacturerModel }
-            } catch (error) {
-              console.log('Error =>', error);
-            }
-          });
-          offset += items.length;
-        } catch (error) {
-          console.log('IN div Error =>', error);
-        }
-        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+      }
+
+      async function scrollPage() {
         let scrollTop = 0;
         while (scrollTop !== 20000) {
           await stall(500);
@@ -110,14 +58,35 @@ module.exports = {
           }
         }
       }
+
+      const moreButton = document.evaluate('//button[@class="results-btn-viewmore"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+      if (moreButton && moreButton.singleNodeValue != null) {
+        while (document.querySelector('button[class="results-btn-viewmore"]').getAttribute('disabled') != "") {
+          offset = await collectUpdateData(offset)
+          moreButton.singleNodeValue.click();
+          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+          scrollPage();
+        }
+        console.log('offset =>', offset);
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+        offset = await collectUpdateData(offset)
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+        scrollPage();
+      } else {
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+        offset = await collectUpdateData(offset);
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+        await scrollPage();
+      }
       return upcData
     });
+    console.log('upcDataResults =>', upcDataResults);
     await context.evaluate(async (upcDataResults) => {
       const liItems = document.querySelectorAll('.product_listing_container ul.grid_mode.grid li.estore_product_container');
       liItems.forEach((item, index) => {
         const productDetails = upcDataResults[index];
         const alreadyExists = liItems[index].querySelector('div#upc');
-        if (!alreadyExists) {
+        if (!alreadyExists && productDetails) {
           const upcDiv = document.createElement('div');
           upcDiv.id = 'upc';
           upcDiv.textContent = productDetails.upc;
