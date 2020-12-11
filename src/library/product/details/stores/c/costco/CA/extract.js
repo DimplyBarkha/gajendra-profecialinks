b0c1622a8596fe7acaeba300900cb172fa4a40a8
Priyanka <priyanka.l@osmosys.asia>
2020-12-11 17:38:01 +0530

    Fixed issue with ds-costco salsify

--- src/library/product/details/stores/c/costco/CA/extract.js
+++ src/library/product/details/stores/c/costco/CA/extract.js
@@ -16,21 +16,36 @@ module.exports = {
     dependencies,
   ) => {
     await context.evaluate(async function () {
-      const descEl = document.querySelector('#product_tabs .product-info-description');
+      const descEl = document.querySelector('.product-info-description');
 
       if (descEl) {
-        const featureBullets = descEl.querySelectorAll('li');
+        const enhancedContentEl1 = document.querySelector('#wc-power-page');
+        const enhancedContentEl2 = document.querySelector('#syndi_powerpage');
+        let enhancedContent = false;
 
-        for (const el of featureBullets) {
-          const newEl = document.createElement('import-feature');
-          newEl.innerText = el.innerText;
-          document.body.appendChild(newEl);
-          el.remove();
+        if (enhancedContentEl1 || enhancedContentEl2) {
+          enhancedContent = true;
         }
 
-        const newDescEl = document.createElement('import-description');
-        newDescEl.innerText = descEl.innerText;
-        document.body.appendChild(newDescEl);
+        document.body.setAttribute('import-enhanced-content', enhancedContent.toString());
+        enhancedContentEl1 ? enhancedContentEl1.remove() : null;
+        enhancedContentEl2 ? enhancedContentEl2.remove() : null;
+      }
+
+      const scriptEl = document.evaluate("//script[contains(., 'pricePerUnit')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+      if (scriptEl) {
+        let scriptText = scriptEl.innerText;
+        let products = JSON.parse(scriptText.split('var products =')[1].split("var options = ")[0].split('\n').join('').trim().slice(0, -1));
+
+        if (products[0].length > 1) {
+          for (const product of products[0]) {
+            let variantSku = product.partNumber;
+            const variantEl = document.createElement('import-variant-sku');
+            variantEl.setAttribute('data', variantSku);
+            document.body.appendChild(variantEl);
+          }
+        }
       }
     });
     const { transform } = parameters;
--- src/library/product/details/stores/c/costco/US/extract.js
+++ src/library/product/details/stores/c/costco/US/extract.js
@@ -22,6 +22,38 @@ module.exports = {
       if (productNotFoundEl.innerText.includes('Product Not Found!')) {
         throw new Error('Product not found');
       }
+
+      const descEl = document.querySelector('.product-info-description');
+
+      if (descEl) {
+        const enhancedContentEl1 = document.querySelector('#wc-power-page');
+        const enhancedContentEl2 = document.querySelector('#syndi_powerpage');
+        let enhancedContent = false;
+
+        if (enhancedContentEl1 || enhancedContentEl2) {
+          enhancedContent = true;
+        }
+
+        document.body.setAttribute('import-enhanced-content', enhancedContent.toString());
+        enhancedContentEl1 ? enhancedContentEl1.remove() : null;
+        enhancedContentEl2 ? enhancedContentEl2.remove() : null;
+      }
+
+      const scriptEl = document.evaluate("//script[contains(., 'pricePerUnit')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+      if (scriptEl) {
+        let scriptText = scriptEl.innerText;
+        let products = JSON.parse(scriptText.split('var products =')[1].split("var options = ")[0].split('\n').join('').trim().slice(0, -1));
+
+        if (products[0].length > 1) {
+          for (const product of products[0]) {
+            let variantSku = product.partNumber;
+            const variantEl = document.createElement('import-variant-sku');
+            variantEl.setAttribute('data', variantSku);
+            document.body.appendChild(variantEl);
+          }
+        }
+      }
     });
     const { transform } = parameters;
     const { productDetails } = dependencies;
