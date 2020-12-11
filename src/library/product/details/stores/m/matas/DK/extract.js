d38cbaaa0820662a0178f65c233b4128e5219081
Raghav <user.email>
2020-12-11 21:28:32 +0530

    initial commit to IM

--- src/library/product/details/stores/m/matas/DK/extract.js
+++ src/library/product/details/stores/m/matas/DK/extract.js
@@ -1,11 +1,47 @@
-
+const { transform } = require("../../../../shared");
 module.exports = {
-  implements: 'product/details/extract',
+  implements: "product/details/extract",
   parameterValues: {
-    country: 'DK',
-    store: 'matas',
+    country: "DK",
+    store: "matas",
     transform: null,
-    domain: 'matas.dk',
-    zipcode: '',
+    domain: "matas.dk",
+    zipcode: "",
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(() => {
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(
+          xpath,
+          document,
+          null,
+          XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+          null
+        );
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement("div");
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = "none";
+        document.body.appendChild(catElement);
+      }
+      // Method to Retrieve Xpath content of a Single Node
+      var price = getAllXpath('//div[@class="product-page__price"]//span[@class="nowrap"]/text()', "nodeValue");
+      if (price[0] != null) {
+        var price1 = price[0].replace(",", ".");
+        addElementToDocument("price", price1);
+      }
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
