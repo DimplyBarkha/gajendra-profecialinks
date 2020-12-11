dc5810373708ce89f2e2deb675bb13309c146dee
BorysMaciuch <borysmaciuch@gmail.com>
2020-12-11 13:28:17 +0100

    #741 add gtin

--- src/library/product/search/stores/t/teknosa/TR/extract.js
+++ src/library/product/search/stores/t/teknosa/TR/extract.js
@@ -4,15 +4,24 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
+  const addGtin = async function (context) {
+    await context.evaluate(async function () {
+      const productList = document.querySelectorAll('.product-item');
+      const eanArray = window.extend_google_tag_params.cd_product_ean;
+      productList.forEach((product, i) => product.setAttribute('ean', eanArray[i]));
+    });
+  };
+
   const addUrl = async function (context) {
     await context.evaluate(async function () {
       const url = document.location.href;
       const productList = document.querySelectorAll('.product-item');
-      productList.forEach(product => product.setAttribute('searchurl', url));
+      productList.forEach((product) => product.setAttribute('searchurl', url));
     });
   };
 
   await addUrl(context);
+  await addGtin(context);
   return await context.extract(productDetails, { transform });
 }
 
