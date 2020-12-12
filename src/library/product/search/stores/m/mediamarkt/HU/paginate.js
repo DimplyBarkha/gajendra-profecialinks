83893ae1ee63362be758511d0960f1cc2d2be31f
atish.shelke <atish.shelke@sz.import.io>
2020-12-12 15:43:45 +0530

    mediamarkt HU search added openSearchDefinition

--- src/library/product/search/stores/m/mediamarkt/HU/paginate.js
+++ src/library/product/search/stores/m/mediamarkt/HU/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'HU',
     store: 'mediamarkt',
-    nextLinkSelector: '#category div:last-child ul li.pagination-next a',
+    nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ul[data-gtm-prop-list-name="Search result list"]',
     noResultsXPath: '//div[@id="nincstalalat"]',
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.mediamarkt.hu/hu/search.html?searchParams=%2FSearch.ff%3Fquery%3D{searchTerms}%26filterTabbedCategory%3Donlineshop%26filteravailability%3D1%26channel%3Dmmhuhu%26productsPerPage%3D25%26followSearch%3D9919&searchProfile=onlineshop&query={searchTerms}&sort=&page={page}&sourceRef=INVALID',
+    },
     domain: 'mediamarkt.hu',
     zipcode: "''",
   },
