b459ffe0685734ca82ae6cb258f5de380f8536b9
Gajender Ramola <ramola106@gmail.com>
2020-12-12 21:52:51 +0530

    fix issue

--- src/library/product/details/stores/w/walmart/MX/format.js
+++ src/library/product/details/stores/w/walmart/MX/format.js
@@ -22,7 +22,6 @@ const transform = (data) => {
     }))));
     return data;
   };
-<<<<<<< HEAD
   for (const { group } of data) {
     for (const row of group) {
       if (row.brandLink) {
@@ -95,74 +94,6 @@ const transform = (data) => {
       // }
     }
   }
-=======
-  // for (const { group } of data) {
-  // for (const row of group) {
-  // if (row.alternateImages) {
-  //   row.alternateImages.splice(0, 1);
-  //   if (row.alternateImages.length === 0) {
-  //     delete row.alternateImages;
-  //   }
-  // }
-  // if (row.descriptionBullets) {
-  //   var bulletArr = [];
-  //   row.descriptionBullets.forEach(item => {
-  //     bulletArr.push(item.text.replace(/^\s*-\s*/, ''));
-  //   });
-  //   row.descriptionBullets = [{ text: '|| ' + bulletArr.join(' || ') }];
-  // }
-  // if (row.specifications) {
-  //   var arrSpecs = [];
-  //   row.specifications.forEach(item => {
-  //     item.text = item.text.replace(/\n\s+\n/, ':');
-  //     arrSpecs.push(item.text);
-  //   });
-  //   row.specifications = [{ text: arrSpecs.join(' || ') }];
-  // }
-  // if (row.price) {
-  //   row.price.forEach(item => {
-  //     item.text = item.text.replace(',', '');
-  //   });
-  // }
-  // if (row.listPrice) {
-  //   row.listPrice.forEach(item => {
-  //     item.text = item.text.replace(',', '');
-  //   });
-  // }
-  // if (row.variantCount) {
-  //   row.variantCount = [{ text: row.variantCount.length }];
-  // }
-  // if (row.variants) {
-  //   var scriptJSON = JSON.parse(row.variants[0].text);
-  //   if (scriptJSON.productVariants) {
-  //     var objectsInVariants = Object.keys(scriptJSON.productVariants).length;
-  //     var varientIds = [];
-  //     for (var i = 0; i < objectsInVariants; i++) {
-  //       var keyName = Object.keys(scriptJSON.productVariants)[i];
-  //       var variants = scriptJSON.productVariants[keyName].variants;
-  //       variants.forEach(function (item, index) {
-  //         varientIds.push(item.fupid);
-  //       });
-  //     }
-  //   }
-  //   row.variants = [{ text: varientIds.join(' | ') }];
-  // }
-  // if (row.additionalDescBulletInfo) {
-  //   var arrBullets = [];
-  //   row.price.forEach(item => {
-  //     arrBullets.push(item.text);
-  //   });
-  //   row.additionalDescBulletInfo = [{ text: '||' + arrBullets.join('||') }];
-  // }
-  // if (row.aggregateRating) {
-  //   row.aggregateRating.forEach(item => {
-  //     item.text = (item.text * 5) / 10;
-  //   });
-  // }
-  // }
-  // }
-
->>>>>>> 6b8568a21c6fd0da2cd069754f9259ad31e80289
   return cleanUp(data);
 };
 
