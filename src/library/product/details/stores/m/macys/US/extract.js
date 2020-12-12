bcdfa4efcd4ad871db45c5a6066cbe9c32e78432
Shreyam <shreyam.g@osmosys.asia>
2020-12-12 12:32:15 +0530

    Added fixes

--- src/library/product/details/stores/m/macys/US/extract.js
+++ src/library/product/details/stores/m/macys/US/extract.js
@@ -15,30 +15,37 @@ module.exports = {
   ) => {
     await context.evaluate(async function () {
       await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+      const imageLength = 1;
       let images = document.querySelectorAll('div.main-img-container ul li picture img');
       if (images.length) {
-        let image = [];
-        for (let i = 1; i < images.length; i++) {
-          image[i] = images[i].src;
+        if (images.length === imageLength) {
+          document.body.setAttribute('images', images[0].src);
         }
+        else {
+          let image = [];
+          for (let i = 1; i < images.length; i++) {
+            image[i] = images[i].src;
+          }
 
-        let uniqueImage = image.filter((c, index) => {
-          return image.indexOf(c) === index;
-        });
+          let uniqueImage = image.filter((c, index) => {
+            return image.indexOf(c) === index;
+          });
 
-        const table = document.createElement('table');
-        document.body.appendChild(table);
-        const tBody = document.createElement('tbody');
-        table.appendChild(tBody);
+          const table = document.createElement('table');
+          document.body.appendChild(table);
+          const tBody = document.createElement('tbody');
+          table.appendChild(tBody);
 
-
-        for (let index = 0; index < uniqueImage.length; index++) {
-          const newlink = document.createElement('tr');
-          newlink.setAttribute('class', 'append_image');
-          newlink.setAttribute('images', uniqueImage[index]);
-          tBody.appendChild(newlink);
+          for (let index = 0; index < uniqueImage.length; index++) {
+            const newlink = document.createElement('tr');
+            newlink.setAttribute('class', 'append_image');
+            newlink.setAttribute('images', uniqueImage[index]);
+            tBody.appendChild(newlink);
+          }
         }
       }
+      const availability = document.evaluate('//button[@data-auto="add-to-bag"]', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext() ? 'In stock' : 'Out of stock';
+      document.body.setAttribute('availability', availability);
     });
 
     await context.evaluate(async function () {
