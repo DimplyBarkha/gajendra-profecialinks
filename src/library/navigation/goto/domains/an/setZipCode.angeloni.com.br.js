9480a80dff1ee8fdefd2bc5dd8350971032a65d8
Vitalii-Petriv <vitalii.petriv-makeitease@partner.import.io>
2020-12-12 23:03:52 +0200

    initial commit

---
+++ src/library/navigation/goto/domains/an/setZipCode.angeloni.com.br.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'BR',
+    domain: 'angeloni.com.br',
+    store: 'angeloni',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/angeloni/BR/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'angeloni.com.br',
+    prefix: null,
+    url: null,
+    country: 'BR',
+    store: 'angeloni',
+    zipcode: '',
+  },
+};
