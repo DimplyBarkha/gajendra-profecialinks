93dba96d790026aa95cc5159410c30a3e8f1d20d
Avinash <avinash.kadam@profecialinks.com>
2020-12-13 11:57:12 +0530

    implementation moved to goto

--- src/library/product/reviews/stores/V/VaporDNA/US/extract.js
+++ src/library/product/reviews/stores/V/VaporDNA/US/extract.js
@@ -8,15 +8,4 @@ module.exports = {
     domain: 'vapordna.com',
     zipcode: "''",
   },
-  implementation: async (inputs,
-    parameters,
-    context,
-    dependencies) => {
-    const { productReviews } = dependencies;
-    await context.evaluate(async () => {
-      var button = document.querySelector('div#age-check-prompt button#submit_birthdate');
-      button.click();
-    });
-    return await context.extract(productReviews);
-  },
 };
