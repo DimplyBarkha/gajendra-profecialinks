ee1b1ddbca37037023acd5d5cde7e78053a574a4
unknown <pavithra.ramasamy-odc@partner.import.io>
2020-12-13 12:57:27 +0530

    devjdsportssearch

--- src/library/product/search/stores/j/jdsports/UK/execute.js
+++ src/library/product/search/stores/j/jdsports/UK/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'UK',
     store: 'jdsports',
     domain: 'jdsports.co.uk',
-    url: 'https://www.jdsports.co.uk/search/{searchTerms}/?max=72',
+    url: 'https://www.jdsports.co.uk/search/RAIN%20JACKET/?max=72',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
