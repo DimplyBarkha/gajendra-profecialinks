890b9da095e3e56c7b2b26bab9083b1140987442
osmarghyadeep <arghyadep.g@partner.import.io>
2020-12-14 04:55:19 +0530

    Added code to wait for the the loader to vanish

--- src/library/navigation/goto/domains/dy/dyson.co.uk.js
+++ src/library/navigation/goto/domains/dy/dyson.co.uk.js
@@ -8,4 +8,18 @@ module.exports = {
     store: 'dyson',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 25000;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    // await context.setUseRelayProxy(false); 
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, captureRequests: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  }
 };
--- src/library/product/search/stores/d/dyson/UK/execute.js
+++ src/library/product/search/stores/d/dyson/UK/execute.js
@@ -5,8 +5,9 @@ module.exports = {
     country: 'UK',
     store: 'dyson',
     domain: 'dyson.co.uk',
-    url: 'https://www.dyson.co.uk/search-results.html?searchText={searchTerms}&from=product#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":true,"cookies":[]}[/!opt!]',
+    url: 'https://www.dyson.co.uk/search-results.html?searchText={searchTerms}&from=product',
+    // url: 'https://www.dyson.co.uk/search-results.html?searchText={searchTerms}&from=product#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":true,"cookies":[]}[/!opt!]',
     loadedSelector: '#Products-Data .search-count',
-    noResultsXPath: '//div[@id="Products-Data"]//div[@class="search-empty"] | //link[@rel="canonical" and not(contains(@href, "search"))]',
+    noResultsXPath: '//div[@id="Products-Data"]//div[@class="search-empty"] | //link[@rel="canonical" and not(contains(@href, "search"))] | //div[contains(@class,"search-empty__container")][contains(.,"experiencing technical issues")]',
   },
 };
