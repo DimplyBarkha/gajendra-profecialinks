d55d903aed2d60e94fb4b2ba071dcddb48825cae
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-12-13 17:48:37 +0530

    #3035 extractor robot created

--- src/library/navigation/goto/domains/ma/manor.ch.js
+++ src/library/navigation/goto/domains/ma/manor.ch.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'manor.ch',
-    timeout: 500000,
+    timeout: 80000,
     country: 'CH',
     store: 'manor',
     zipcode: '',
