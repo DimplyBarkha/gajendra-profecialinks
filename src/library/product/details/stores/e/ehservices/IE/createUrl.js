d156fb635887ebc118eb1f96d099a3214c21e01a
Richa Tiwari <richa.t@osmosys.asia>
2020-12-14 11:32:25 +0530

    Updated extract.yaml

--- src/library/product/details/stores/e/ehservices/IE/createUrl.js
+++ src/library/product/details/stores/e/ehservices/IE/createUrl.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-  implements: 'product/details/createUrl',
-  parameterValues: {
-    domain: 'ehservices.co.uk',
-    prefix: null,
-    url: 'https://www.ehservices.co.uk/d/{id}',
-    country: 'IE',
-    store: 'ehservices',
-    zipcode: '',
-  },
+    implements: 'product/details/createUrl',
+    parameterValues: {
+        domain: 'ehservices.co.uk',
+        prefix: null,
+        url: null,
+        country: 'IE',
+        store: 'ehservices',
+        zipcode: '',
+    },
 };
