ad22bf72e48950966d88904bc7c619cea428afd2
tapan-profecialinks <tapan@profecialinks.com>
2020-12-14 21:46:55 +0530

    search key issue fix

--- src/library/product/search/stores/i/inkafarma/PE/execute.js
+++ src/library/product/search/stores/i/inkafarma/PE/execute.js
@@ -1,4 +1,52 @@
-
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(
+    function (sel, xp) {
+    return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    },
+    { timeout: 10000 },
+    parameters.loadedSelector, parameters.noResultsXPath
+    );
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+    let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+          resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
+  return await context.evaluate(function (xp) {
+  const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+  console.log(xp, r);
+  const e = r.iterateNext();
+  console.log(e);
+  return !e;
+  }, parameters.noResultsXPath);
+}
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -10,4 +58,5 @@ module.exports = {
     noResultsXPath: '//div[@id="noresultsBar"]//span//strong',
     zipcode: '',
   },
+  implementation,
 };
