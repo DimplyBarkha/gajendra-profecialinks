cc28b16963c00f8daf6467e0a4f936355ca9b009
David Skoglund <david.skoglund@import.io>
2020-12-14 12:19:12 -0800

    updated noResultsXpath for Dyson search UK to fail/timeout when technical issues message is displayed

--- src/library/product/search/stores/d/dyson/UK/execute.js
+++ src/library/product/search/stores/d/dyson/UK/execute.js
@@ -8,6 +8,6 @@ module.exports = {
     url: 'https://www.dyson.co.uk/search-results.html?searchText={searchTerms}&from=product',
     // url: 'https://www.dyson.co.uk/search-results.html?searchText={searchTerms}&from=product#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":true,"cookies":[]}[/!opt!]',
     loadedSelector: '#Products-Data .search-count',
-    noResultsXPath: '//div[@id="Products-Data"]//div[@class="search-empty"] | //link[@rel="canonical" and not(contains(@href, "search"))] | //div[contains(@class,"search-empty__container")][contains(.,"experiencing technical issues")]',
+    noResultsXPath: '//div[@id="Products-Data"]//div[@class="search-empty"][not(contains(.,"experiencing technical issues"))] | //link[@rel="canonical" and not(contains(@href, "search"))]',
   },
 };
