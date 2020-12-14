826969022fb4883e2293152ad83580838adfaa81
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-12-14 15:03:14 +0530

    changes made according to QA

--- src/library/product/search/stores/h/houzz/US/extract.js
+++ src/library/product/search/stores/h/houzz/US/extract.js
@@ -11,44 +11,44 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(() => {
-      function stall(ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-      if (
-        document.querySelector(
-          ".btn.btn-none.hz-universal-search-header-tip__dismiss"
-        ) != null
-      ) {
-        document
-          .querySelector(
-            ".btn.btn-none.hz-universal-search-header-tip__dismiss"
-          )
-          .click();
-      }
-      if (
-        document.querySelector(
-          "body > div.hz-modal.hz-modal--dark > div.hz-trap-focus.hz-modal__container > div > div.hz-modal__body > div > div.hz-international-redirect-modal__btns > div.hz-international-redirect-modal__continue-btn > button"
-        ) != null
-      ) {
-        document
-          .querySelector(
-            "body > div.hz-modal.hz-modal--dark > div.hz-trap-focus.hz-modal__container > div > div.hz-modal__body > div > div.hz-international-redirect-modal__btns > div.hz-international-redirect-modal__continue-btn > button"
-          )
-          .click();
-      }
-      let scrollTop = 500;
-      while (true) {
-        window.scroll(0, scrollTop);
-        stall(1000);
-        scrollTop += 500;
-        if (scrollTop === 10000) {
-          break;
-        }
-      }
+      // function stall(ms) {
+      //   return new Promise((resolve, reject) => {
+      //     setTimeout(() => {
+      //       resolve();
+      //     }, ms);
+      //   });
+      // }
+      // if (
+      //   document.querySelector(
+      //     ".btn.btn-none.hz-universal-search-header-tip__dismiss"
+      //   ) != null
+      // ) {
+      //   document
+      //     .querySelector(
+      //       ".btn.btn-none.hz-universal-search-header-tip__dismiss"
+      //     )
+      //     .click();
+      // }
+      // if (
+      //   document.querySelector(
+      //     "body > div.hz-modal.hz-modal--dark > div.hz-trap-focus.hz-modal__container > div > div.hz-modal__body > div > div.hz-international-redirect-modal__btns > div.hz-international-redirect-modal__continue-btn > button"
+      //   ) != null
+      // ) {
+      //   document
+      //     .querySelector(
+      //       "body > div.hz-modal.hz-modal--dark > div.hz-trap-focus.hz-modal__container > div > div.hz-modal__body > div > div.hz-international-redirect-modal__btns > div.hz-international-redirect-modal__continue-btn > button"
+      //     )
+      //     .click();
+      // }
+      // let scrollTop = 500;
+      // while (true) {
+      //   window.scroll(0, scrollTop);
+      //   stall(1000);
+      //   scrollTop += 500;
+      //   if (scrollTop === 10000) {
+      //     break;
+      //   }
+      // }
       function addHiddenDiv(id, content, index) {
         const newDiv = document.createElement("div");
         newDiv.id = id;
