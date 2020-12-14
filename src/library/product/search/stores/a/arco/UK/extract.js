f27cd49cffa83c0bd94546bd7bb515978dde935f
Akash Panda <akashkumar.p@osmosys.asia>
2020-12-14 13:19:27 +0530

    Commiting changes reported by the client

--- src/library/product/search/stores/a/arco/UK/extract.js
+++ src/library/product/search/stores/a/arco/UK/extract.js
@@ -1,27 +1,27 @@
 
 
-// async function implementation (
-//   inputs,
-//   parameters,
-//   context,
-//   dependencies,
-// ) {
-//   const { productDetails } = dependencies;
-//   const { transform } = parameters;
-//   await context.evaluate(async function (context) {
-//     // const isSelector = document.querySelector('div#wrapper section.tab1');
-//     // if (isSelector) {
-//     //   const url = document.querySelector('link[rel="canonical"]').getAttribute('href');
-//     //   document.querySelector('h1[itemprop="name"]').setAttribute('url', url);
-//     // }
-//     // next page button click
-//     const seeAllSelector = document.querySelector('ul> li > a > span[class="icon-arrow-right"]');
-//     if (seeAllSelector) {
-//       seeAllSelector.click();
-//     }
-//   });
-//   return await context.extract(productDetails, { transform });
-// }
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+  const { transform } = parameters;
+  await context.evaluate(async function (context) {
+    const isSelector = document.querySelector('div#infoholder');
+    if (isSelector) {
+      const url = document.querySelector('div#contentholder > div#imageholder >img').getAttribute('src');
+      document.querySelector('div#infoholder > h1').setAttribute('url', url);
+    }
+    // next page button click
+  //   const seeAllSelector = document.querySelector('ul> li > a > span[class="icon-arrow-right"]');
+  //   if (seeAllSelector) {
+  //     seeAllSelector.click();
+  //   }
+  });
+  return await context.extract(productDetails, { transform });
+}
 const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
@@ -32,4 +32,5 @@ module.exports = {
     domain: 'arco.uk',
     zipcode: '',
   },
+  implementation,
 };
