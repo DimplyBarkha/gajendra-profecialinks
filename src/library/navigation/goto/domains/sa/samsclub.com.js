c37ea74c4a6ddedd7f18f6312d72bab19ece577b
Sindhusha balla <sindhusha.b@osmosys.asia>
2020-12-14 16:34:32 +0530

    Added execute & paginate support

--- src/library/navigation/goto/domains/sa/samsclub.com.js
+++ src/library/navigation/goto/domains/sa/samsclub.com.js
@@ -3,33 +3,32 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'samsclub.com',
-    timeout: 50000,
     country: 'US',
-    // timeout: 100000,
+    timeout: 40000,
     store: 'samsclub',
   },
-//   implementation: async (
-//     { url, zipcode, storeId },
-//     parameters,
-//     context,
-//     dependencies,
-//   ) => {
-//     const timeout = parameters.timeout ? parameters.timeout : 10000;
-//     await context.setBlockAds(false);
-//     await context.setLoadAllResources(true);
-//     await context.setLoadImages(true);
-//     await context.setAntiFingerprint(false);
-//     try {
-//       await context.goto(url, {
-//         timeout: timeout,
-//         waitUntil: 'load',
-//         checkBlocked: true,
-//       });
-//     } catch (e) {
-//       console.log('page load time out ----> ', e);
-//     }
-//     if (zipcode) {
-//       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-//     }
-//   },
+  implementation: async (
+    { url, zipcode, storeId },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setAntiFingerprint(false);
+    try {
+      await context.goto(url, {
+        timeout: timeout,
+        waitUntil: 'load',
+        checkBlocked: true,
+      });
+    } catch (e) {
+      console.log('page load time out ----> ', e);
+    }
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
--- src/library/product/reviews/stores/s/samsclub/US/extract.js
+++ src/library/product/reviews/stores/s/samsclub/US/extract.js
@@ -1,59 +1,11 @@
-  const { transform } = require('../../samsclub/shared');
-  module.exports = {
-    implements: 'product/reviews/extract',
-    parameterValues: {
-      country: 'US',
-      store: 'samsclub',
-      transform,
-      domain: 'samsclub.com',
-      zipcode: '',
-    },
-  //   implementation: async (inputs, parameters, context, dependencies) => {
-  //     const { transform } = parameters;
-  //     const { productReviews } = dependencies;
-  //     await new Promise((resolve, reject) => setTimeout(resolve, 6000));
-  //     await context.evaluate(async () => {
-  //       const closePopupButton = document.querySelector('.sc-modal-content > div button ');
-  //       if (closePopupButton !== null) {
-  //         closePopupButton.click();
-  //         console.log("button clicked");
-  //       }
-  //     });
-  //     const applyScroll = async function (context) {
-  //       await context.evaluate(async function () {
-  //         let scrollTop = 0;
-  //         while (scrollTop !== 20000) {
-  //           scrollTop += 500;
-  //           window.scroll(0, scrollTop);
-  //           await stall(1000);
-  //         }
-  //         function stall(ms) {
-  //           return new Promise((resolve, reject) => {
-  //             setTimeout(() => {
-  //               resolve();
-  //             }, ms);
-  //           });
-  //         }
-  //       });
-  //       console.log("scroll");
-  //     };
-    
-  //     await applyScroll(context);
-  //     await context.evaluate(async () => {
-  //       const closePopupButton1 = document.querySelector('.sc-modal-content > div button');
-  //       if (closePopupButton1 !== null) {
-  
-  //         closePopupButton1.click(setTimeout);
-  //         console.log("button clicked");
-  //       }
-        
-  //     })
-  //     await applyScroll(context);
-
-  
-  //     context.waitForFunction((sel, xp) => {
-  //       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-  //     }, {"timeout":100000}, ".reviews-questions", "//div[@class=\"main-wrapper\"]//div[@role=\"region\"]")
-  //     return await context.extract(productReviews, { transform });
-  //   },
-  };
+const { transform } = require('../../samsclub/shared');
+module.exports = {
+  implements: 'product/reviews/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'samsclub',
+    transform,
+    domain: 'samsclub.com',
+    zipcode: '',
+  },
+};
