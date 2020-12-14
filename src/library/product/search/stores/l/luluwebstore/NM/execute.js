1bac4eff8f74bb872179a0a6c4fd8a1b6f9f78d9
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-14 18:58:44 +0530

    luluwebstore by dhanush

--- src/library/product/search/stores/l/luluwebstore/NM/execute.js
+++ src/library/product/search/stores/l/luluwebstore/NM/execute.js
@@ -1,12 +1,11 @@
-
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
-    country: 'NM',
+    country: 'AE',
     store: 'luluwebstore',
     domain: 'luluhypermarket.com',
     url: 'https://www.luluhypermarket.com/en-ae/search/?text={searchTerms}%3Arelevance',
-    loadedSelector: null,
+    loadedSelector: 'ul[class="product__listing product__grid col-xs-12"] div[class="col-lg-3 col-md-4 col-sm-6 col-xs-6 plp-product-div js-owl-carousel-reference "]',
     noResultsXPath: null,
     zipcode: '',
   },
--- src/library/product/search/stores/l/luluwebstore/NM/paginate.js
+++ src/library/product/search/stores/l/luluwebstore/NM/paginate.js
@@ -7,9 +7,11 @@ module.exports = {
     nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'ul[class="product__listing product__grid col-xs-12"] div[class="col-lg-3 col-md-4 col-sm-6 col-xs-6 plp-product-div js-owl-carousel-reference "]',
     noResultsXPath: null,
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.luluhypermarket.com/en-ae/search?q={searchTerms}%3Arelevance&page={page}',
+      },
     domain: 'luluhypermarket.com',
     zipcode: '',
   },
