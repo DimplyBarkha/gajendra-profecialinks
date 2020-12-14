097510b15a101e5841b351c50f1600fd646a1587
IpsitaM <iamipsita07@gmail.com>
2020-12-14 09:58:12 +0530

    made some changes for geo

--- src/library/navigation/goto/domains/sa/safeway.com.js
+++ src/library/navigation/goto/domains/sa/safeway.com.js
@@ -1,11 +1,18 @@
-
 module.exports = {
-  implements: 'navigation/goto',
-  parameterValues: {
-    domain: 'safeway.com',
-    timeout: 60000,
-    country: 'US',
-    store: 'safeway',
-    zipcode: '',
-  },
+    implements: 'navigation/goto',
+    parameterValues: {
+        domain: 'safeway.com',
+        timeout: 60000,
+        country: 'US',
+        store: 'safeway',
+        zipcode: '95401',
+    },
+    implementation: async({ url, zipcode, storeId }, parameters, context, dependencies) => {
+        const timeout = parameters.timeout ? parameters.timeout : 10000;
+        await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+        console.log(zipcode);
+
+        await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+
+    },
 };
--- src/library/navigation/goto/domains/sa/setZipCode.safeway.com.js
+++ src/library/navigation/goto/domains/sa/setZipCode.safeway.com.js
@@ -1,42 +1,48 @@
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
+async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
 ) {
-  const { zipcode } = inputs;
-  await context.evaluate(async () => {
-    const changeButton = document.querySelector('div [class*="reserve-nav__instore"] > button');
-    if (changeButton) {
-      console.log('entered if block');
-      changeButton.click();
-      console.log('button clicked sucessfully');
-      await new Promise(resolve => { setTimeout(resolve, 15000); });
-    }
-    console.log('sucessfully clicked the change button');
-  });
-  await context.setInputValue('div [class*="fulfillment-content__search"] > input', `${zipcode}`);
-  await context.evaluate(async () => {
-    const searchButton = document.querySelector('div [class*="fulfillment-content__search"] > span');
-    if (searchButton) {
-      searchButton.click();
-      console.log('able to click the button');
-      await new Promise(resolve => { setTimeout(resolve, 15000); });
-    }
-    const selectButton = document.querySelector('.in-store-content>.card-wrapper>.card-store:nth-child(1)>.row>div[class*="col-4"]>.caption>a');
-    if (selectButton) {
-      selectButton.click();
-    }
-    await new Promise(resolve => { setTimeout(resolve, 15000); });
-  });
+    const { zipcode } = inputs;
+    await context.evaluate(async() => {
+        const changeButton = document.querySelector('div [class*="reserve-nav__instore"] > button');
+        if (changeButton) {
+            console.log('entered if block');
+            changeButton.click();
+            console.log('button clicked sucessfully');
+            await new Promise(resolve => { setTimeout(resolve, 15000); });
+        }
+        console.log('sucessfully clicked the change button');
+    });
+    await context.setInputValue('div [class*="fulfillment-content__search"] > input', `95401`);
+    await context.evaluate(async() => {
+        const searchButton = document.querySelector('div [class*="fulfillment-content__search"] > span');
+        if (searchButton) {
+            searchButton.click();
+            console.log('able to click the button');
+            await new Promise(resolve => { setTimeout(resolve, 15000); });
+            const selectElementAll = document.querySelectorAll('div[class="card-store"]');
+            const ourElement = [...selectElementAll].find((element) => element.innerText.includes('95401'));
+            await new Promise(resolve => { setTimeout(resolve, 15000); });
+            const selectButton = ourElement.querySelector('a[class*="card-store-btn"]');
+            selectButton.click();
+        }
+
+        // const selectButton = document.querySelector('.in-store-content>.card-wrapper>.card-store:nth-child(1)>.row>div[class*="col-4"]>.caption>a');
+        // if (selectButton) {
+        //     selectButton.click();
+        // }
+        await new Promise(resolve => { setTimeout(resolve, 15000); });
+    });
 }
 module.exports = {
-  implements: 'navigation/goto/setZipCode',
-  parameterValues: {
-    country: 'US',
-    domain: 'safeway.com',
-    store: 'safeway',
-    zipcode: '',
-  },
-  implementation,
+    implements: 'navigation/goto/setZipCode',
+    parameterValues: {
+        country: 'US',
+        domain: 'safeway.com',
+        store: 'safeway',
+        zipcode: '',
+    },
+    implementation,
 };
