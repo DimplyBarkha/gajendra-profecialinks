516e017c17bfc655a8b98ceb5a8cdf2c7c02da08
Nilesh <nilesh.b@osmosys.asia>
2020-12-14 14:41:30 +0530

    Added helpful column.

--- src/library/product/reviews/stores/b/bebitus/shared.js
+++ src/library/product/reviews/stores/b/bebitus/shared.js
@@ -41,6 +41,41 @@ const transform = (data, context) => {
     return false;
   });
   context.setState({ lastReviewDate });
+  for (const { group } of data) {
+    for (const row of group) {
+         if (row.reviewRating) {
+        let text = '';
+        row.reviewRating.forEach(item => {
+          if(item.text.includes('0')) {
+            text = item.text.substr(0, 1) + ',' + item.text.substr(1, 2);
+          }else{
+            text = row.reviewRating[0].text.replace('.',',');
+          }
+        });
+        row.reviewRating = [
+          {
+            text: text,
+          },
+        ];
+      } 
+    }
+  }
+  const clean = text => text.toString()
+  .replace(/\r\n|\r|\n/g, ' ')
+  .replace(/&amp;nbsp;/g, ' ')
+  .replace(/&amp;#160/g, ' ')
+  .replace(/\u00A0/g, ' ')
+  .replace(/\s{2,}/g, ' ')
+  .replace(/"\s{1,}/g, '"')
+  .replace(/\s{1,}"/g, '"')
+  .replace(/^ +| +$|( )+/g, ' ')
+  // eslint-disable-next-line no-control-regex
+  .replace(/[\x00-\x1F]/g, '')
+  .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+  el.text = clean(el.text);
+}))));
   return data;
 };
 
