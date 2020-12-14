aafc2efa596633fba0b1af29ea84df898c8ea7b8
Vivek Gondil <gondilvivek@gmail.com>
2020-12-14 16:07:02 +0530

    lint fixed

--- src/library/product/search/stores/o/officedepot/US/paginate.js
+++ src/library/product/search/stores/o/officedepot/US/paginate.js
@@ -1,15 +1,15 @@
 module.exports = {
-    implements: 'product/search/paginate',
-    parameterValues: {
-        country: 'US',
-        store: 'officedepot',
-        nextLinkSelector: '#paginationFooter > div.next.pg_btn > a',
-        mutationSelector: null,
-        spinnerSelector: 'div[class="moreResults_Info"]',
-        loadedSelector: null,
-        noResultsXPath: '//div[@class="h3 brandColor_tp2 section no_result_break"]',
-        openSearchDefinition: null,
-        domain: 'officedepot.com',
-        zipcode: '',
-    },
+  implements: 'product/search/paginate',
+  parameterValues: {
+    country: 'US',
+    store: 'officedepot',
+    nextLinkSelector: '#paginationFooter > div.next.pg_btn > a',
+    mutationSelector: null,
+    spinnerSelector: 'div[class="moreResults_Info"]',
+    loadedSelector: null,
+    noResultsXPath: '//div[@class="h3 brandColor_tp2 section no_result_break"]',
+    openSearchDefinition: null,
+    domain: 'officedepot.com',
+    zipcode: '',
+  },
 };
