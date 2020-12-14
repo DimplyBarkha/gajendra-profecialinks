7732b16bd2ff74621a5fbacb898cd46f2826cfb1
Karina Miernik <karina.miernik@gmail.com>
2020-12-14 15:30:17 +0100

    created template for the menu extractor

---
+++ src/library/tracker/categories/stores/e/elgiganten/SE/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'SE',
+    domain: 'elgiganten.se',
+    store: 'elgiganten',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/e/elgiganten/SE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'SE',
+    store: 'elgiganten',
+    domain: 'elgiganten.se',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
