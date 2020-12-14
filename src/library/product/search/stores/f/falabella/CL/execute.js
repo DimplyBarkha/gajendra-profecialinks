a2350e31f76f86f6549e573761c930c589c11969
sahoojudhi <judhishtor@profecialinks.com>
2020-12-14 12:26:09 +0530

    #3032 Developement Done, Going for deployement

--- src/library/product/search/stores/f/falabella/CL/execute.js
+++ src/library/product/search/stores/f/falabella/CL/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'falabella',
     domain: 'falabella.com',
     url: 'https://www.falabella.com/falabella-cl/search?Ntt={searchTerms}',
-    loadedSelector: 'div[id=testId-searchResults-products]',
-    noResultsXPath: "//div[contains(@class,'no-result')]",
+    loadedSelector: 'div.jsx-1987097504,main',
+    noResultsXPath: '//div[contains(@class,"no-result")]//h3',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/f/falabella/CL/extract.js
+++ src/library/product/search/stores/f/falabella/CL/extract.js
@@ -1,47 +1,12 @@
-const { transform } = require('../format');
+const {transform}=require('../CL/format')
 
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-  const applyScroll = async function (context) {
-    console.log('calling applyScroll-----------');
-    await context.evaluate(async function () {
-      let scrollTop = 0;
-      while (scrollTop !== 20000) {
-        await stall(1000);
-        scrollTop += 1000;
-        console.log('calling applyScroll evaluate-----------', window);
-        window.scroll(0, scrollTop);
-        if (scrollTop === 20000) {
-          await stall(5000);
-          break;
-        }
-      }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-    });
-  };
-  await applyScroll(context);
-  return await context.extract(productDetails, { transform });
-}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'CL',
     store: 'falabella',
-    transform: null,
+    transform,
     domain: 'falabella.com',
     zipcode: '',
   },
-  implementation,
 };
