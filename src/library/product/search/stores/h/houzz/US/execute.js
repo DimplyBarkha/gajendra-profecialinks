b3d199c2b866575baa599f93672dc950e1720c9d
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-12-14 19:03:05 +0530

    changes made according to QA

--- src/library/product/search/stores/h/houzz/US/execute.js
+++ src/library/product/search/stores/h/houzz/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     store: "houzz",
     domain: "houzz.com",
     url: "https://www.houzz.com/products/query/{searchTerms}",
-    loadedSelector: null,
+    loadedSelector: ".hz-image-container img",
     noResultsXPath: null,
     zipcode: "",
   },
