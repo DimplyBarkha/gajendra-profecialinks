b8cbc5c453702dcabcba65410e652b10f0f46e5a
Raghav <user.email>
2020-12-14 17:20:28 +0530

    using category xpath with single output

--- src/library/product/details/stores/m/matas/DK/extract.js
+++ src/library/product/details/stores/m/matas/DK/extract.js
@@ -35,13 +35,28 @@ module.exports = {
         catElement.style.display = "none";
         document.body.appendChild(catElement);
       }
-      // Method to Retrieve Xpath content of a Single Node
-      var price = getAllXpath('//div[@class="product-page__price"]//span[@class="nowrap"]/text()', "nodeValue");
+      function addElementToDocument1(key, value, index) {
+        const catElement = document.createElement("div");
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = "none";
+        document.body.appendChild(catElement);
+      }
+      // Method to replace , with . for price
+      var price = getAllXpath('//div[@class="product-page__price"]//span[@class="nowrap"]/text() | (//span[@class="nowrap"])[1]/text()', "nodeValue");
       if (price[0] != null) {
         var price1 = price[0].replace(",", ".");
         addElementToDocument("price", price1);
       }
+      var vari = getAllXpath("(//div[@class='icon--rating icon--product-header-rating review-stars']//div[@class='review-stars__container review-stars__container--border']/@data-js-border-offset)[1]","nodeValue");
+      if (vari != null) {
+        for (var i = 0; i < vari.length; i++) {
+          var agg = (vari[i].slice(0, -1) * 5) / 100;
+          addElementToDocument1("agg", agg, i);
+        }
+      }
     });
+    
     return await context.extract(productDetails, { transform });
   },
 };
