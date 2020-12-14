03026d747b5ab1e8a539eb28c9d492f3a5255938
Karina Miernik <karina.miernik@gmail.com>
2020-12-14 15:33:00 +0100

    suffix added

---
+++ src/library/tracker/categories/stores/e/elgiganten/SE/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'elgiganten.se',
+    prefix: null,
+    suffix: 'pixel.mathtag.com/event/js?mt_id=1060219&mt_adid=171230&v1=[PRODUCT_NAME]&v2=[PRODUCT_PRICE]&v3=[PRODUCT_ID]&s1=[PAGE_URL]&s2=[CATEGORY]&s3=[SUBCATEGORY]&s4=[PRODUCT_TYPE]&s5=[PRODUCT_BRAND',
+    subdomain: null,
+    url: null,
+    country: 'SE',
+    store: 'elgiganten',
+    zipcode: '',
+  },
+};
