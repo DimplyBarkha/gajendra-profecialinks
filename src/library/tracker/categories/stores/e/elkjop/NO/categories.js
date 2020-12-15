c225f6577609afe99b102a614eb40fb2141341da
Karina Miernik <karina.miernik@gmail.com>
2020-12-15 07:58:47 +0100

    created template for the menu extractor

---
+++ src/library/tracker/categories/stores/e/elkjop/NO/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'NO',
+    domain: 'elkjop.no',
+    store: 'elkjop',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/e/elkjop/NO/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'elkjop.no',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: null,
+    country: 'NO',
+    store: 'elkjop',
+    zipcode: '',
+  },
+};
