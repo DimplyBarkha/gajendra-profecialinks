bcbbe31fa323c8edef72be3c9121d6ddca3cbc5c
adrian_swiderek <adrian.swiderek@tdcm.io>
2020-12-15 17:31:55 +0100

    falabella PE menu extractor

---
+++ src/library/tracker/categories/stores/f/falabella/PE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'PE',
+    store: 'falabella',
+    domain: 'falabella.com.pe',
+    loadedSelector: 'html body',
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
