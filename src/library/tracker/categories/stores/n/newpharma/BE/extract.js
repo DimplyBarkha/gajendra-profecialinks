e01463d56371178c8c02eab3aa1b48eeca8a8192
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 23:06:12 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/n/newpharma/BE/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'BE',
+    domain: 'newpharma.be',
+    store: 'newpharma',
+    zipcode: '',
+  },
+};
