116284998e14e203b0d3077f0b3623c027811e52
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 22:29:16 +0100

    created a template for the menu only extractor

---
+++ src/library/navigation/goto/domains/ke/keshop.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'keshop.com',
+    timeout: null,
+    country: 'ES',
+    store: 'keshop',
+    zipcode: '',
+  },
+};
