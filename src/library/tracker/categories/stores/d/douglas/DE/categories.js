c756028bfe4ac24279fcdd19fbc65b25a73ab688
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 12:21:54 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/d/douglas/DE/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'DE',
+    domain: 'douglas.de',
+    store: 'douglas',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/d/douglas/DE/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'DE',
+    domain: 'douglas.de',
+    store: 'douglas',
+    zipcode: '',
+  },
+};
