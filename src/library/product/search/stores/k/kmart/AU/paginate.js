be6c6630164ed69fcb048e8e73f0274e646f3369
unknown <leena.gavali-odc@partner.import.io>
2020-12-15 12:08:16 +0530

    kmart_try1

--- src/library/product/search/stores/k/kmart/AU/paginate.js
+++ src/library/product/search/stores/k/kmart/AU/paginate.js
@@ -4,10 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'AU',
     store: 'kmart',
-    // nextLinkSelector: 'li a[title="Next Page"]',
+    nextLinkSelector: 'li a[id="WC_SearchBasedNavigationResults_pagination_link_right_categoryResults_bottom"]',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'body',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'kmart.com.au',
