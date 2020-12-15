52983c7778d20b81b0b9afe1526211836b2078c4
Karina Miernik <karina.miernik@gmail.com>
2020-12-15 07:44:28 +0100

    url changed

--- src/library/tracker/categories/stores/e/elgiganten/SE/createUrl.js
+++ src/library/tracker/categories/stores/e/elgiganten/SE/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'elgiganten.se',
     prefix: null,
-    suffix: 'pixel.mathtag.com/event/js?mt_id=1060219&mt_adid=171230&v1=[PRODUCT_NAME]&v2=[PRODUCT_PRICE]&v3=[PRODUCT_ID]&s1=[PAGE_URL]&s2=[CATEGORY]&s3=[SUBCATEGORY]&s4=[PRODUCT_TYPE]&s5=[PRODUCT_BRAND',
+    suffix: 'catalog',
     subdomain: null,
     url: null,
     country: 'SE',
