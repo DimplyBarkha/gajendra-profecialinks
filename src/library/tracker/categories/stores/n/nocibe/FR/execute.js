b4d13b689d6b878edccc95deede692baab787f5a
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 12:12:03 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/n/nocibe/FR/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'FR',
+    store: 'nocibe',
+    domain: 'nocibe.fr',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/n/nocibe/FR/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'FR',
+    domain: 'nocibe.fr',
+    store: 'nocibe',
+    zipcode: '',
+  },
+};
