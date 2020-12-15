55d9b69069318ff0c38a824a74489867b8f480fd
Avinash <avinash.kadam@profecialinks.com>
2020-12-15 16:43:21 +0530

    unInterruptedPDP updated, removed incorrect inTheBox

--- src/library/product/details/stores/k/kastner-oehler/AT/extract.js
+++ src/library/product/details/stores/k/kastner-oehler/AT/extract.js
@@ -10,14 +10,14 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
-    let productUrl=await context.evaluate(async function(){
+    let productUrl = await context.evaluate(async function () {
       return document.URL;
     });
     await context.evaluate(async function () {
       const cookies = document.querySelector('span.tao_button_cookie_settings');
       if (cookies) cookies.click();
       await new Promise(resolve => setTimeout(resolve, 2000));
-      function addElementToDocument (key, value) {
+      function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
@@ -59,35 +59,34 @@ module.exports = {
       }
 
     });
-    let iframeLink=await context.evaluate(async function(){
-      let iframeLink=null;
-      document.querySelector('#productcarousel_3 .productcarousel__header').scrollIntoView({behavior: 'smooth'});
-      function stall (ms)
-      {
-      return new Promise((resolve, reject) => {
-      setTimeout(() => {
-      resolve();
-      }, ms);
-      });
+    let iframeLink = await context.evaluate(async function () {
+      let iframeLink = null;
+      document.querySelector('#productcarousel_3 .productcarousel__header').scrollIntoView({ behavior: 'smooth' });
+      function stall(ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
       }
       await stall(15000);
-      if(document.querySelector('div[class*="site_inner"] iframe'))
-        iframeLink=document.querySelector('div[id*="loadBeeContainer"] iframe').getAttribute('src');
+      if (document.querySelector('div[class*="site_inner"] iframe'))
+        iframeLink = document.querySelector('div[id*="loadBeeContainer"] iframe').getAttribute('src');
       return iframeLink;
     });
 
-    let enhancedContent=null;
-    if(iframeLink!==null){
+    let enhancedContent = null;
+    if (iframeLink !== null) {
       await context.goto(iframeLink, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
       await context.waitForSelector('.module.header img');
-       enhancedContent= await context.evaluate(async function(){
-        let video= document.querySelector('.play-btn.centered.desktop').getAttribute('data-video');
-        let enhancedContent= document.querySelector('.pic-text-modul').innerText;
+      enhancedContent = await context.evaluate(async function () {
+        let video = document.querySelector('.play-btn.centered.desktop').getAttribute('data-video');
+        let enhancedContent = document.querySelector('.pic-text-modul').innerText;
         let aplusImagesArray = document.querySelectorAll('.pic-text img');
         let aplusImagesSrc = [];
-        if(aplusImagesArray.length > 0 ) {
+        if (aplusImagesArray.length > 0) {
           aplusImagesArray.forEach(image => {
-          aplusImagesSrc.push(image.getAttribute('data-src')+' ||');
+            aplusImagesSrc.push(image.getAttribute('data-src') + ' ||');
           })
         }
         // let enhancedContent='';
@@ -95,26 +94,35 @@ module.exports = {
         // for(let i=0;i<fontText1.length;i++) enhancedContent+=fontText1[i].innerText+' || ';
         // let fontText2=document.querySelectorAll('div[class*="animation-text"]  font font');
         // for(let i=0;i<fontText2.length;i++) enhancedContent+=fontText2[i].innerText+'||';
-        return {enhancedContents:enhancedContent,videos:video,aplusImages:aplusImagesSrc};
+        return { enhancedContents: enhancedContent, videos: video, aplusImages: aplusImagesSrc };
       });
     }
     else console.log('iframe link not loaded');
     await context.goto(productUrl, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
-    if(enhancedContent!==null){
-      await context.evaluate(async function(enhancedContent){
-        function addHiddenDiv (id, content) {
+    if (enhancedContent !== null) {
+      await context.evaluate(async function (enhancedContent) {
+        function addHiddenDiv(id, content) {
           const newDiv = document.createElement('div');
           newDiv.id = id;
           newDiv.textContent = content;
           newDiv.style.display = 'none';
           document.body.appendChild(newDiv);
         }
-        addHiddenDiv('enhancedContent',enhancedContent.enhancedContents);
-        addHiddenDiv('video',enhancedContent.videos);
-        addHiddenDiv('aplusImage',enhancedContent.aplusImages);
-      },enhancedContent)
+        addHiddenDiv('enhancedContent', enhancedContent.enhancedContents);
+        addHiddenDiv('video', enhancedContent.videos);
+        addHiddenDiv('aplusImage', enhancedContent.aplusImages);
+      }, enhancedContent)
     }
     await context.waitForSelector('.en_lazy_load');
-    await context.extract(productDetails, {transform: transformParam});
+
+    await context.evaluate(async function () {
+      let similarProduct = document.querySelector('div.productcarousel__inner');
+      if (similarProduct) {
+        document.querySelector("div.productcarousel__inner").scrollIntoView()
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      }
+    });
+
+    await context.extract(productDetails, { transform: transformParam });
   },
 };
