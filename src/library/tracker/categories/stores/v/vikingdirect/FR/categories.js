ec1fdf378de40451e1d2a8f1c15f9939ff6a0ac4
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 22:33:05 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/v/vikingdirect/FR/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'FR',
+    domain: 'vikingdirect.fr',
+    store: 'vikingdirect',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/v/vikingdirect/FR/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'FR',
+    domain: 'vikingdirect.fr',
+    store: 'vikingdirect',
+    zipcode: '',
+  },
+};
