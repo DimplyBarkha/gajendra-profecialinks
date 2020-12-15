9172d8bf8ec204a06b472acaccbf4055c99c6766
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-15 17:34:00 +0530

    updated kroger paginate

--- src/library/product/search/stores/k/kroger/US/paginate.js
+++ src/library/product/search/stores/k/kroger/US/paginate.js
@@ -3,7 +3,8 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'kroger',
-    loadedSelector: '.PaginateItems',
+    // loadedSelector: '.PaginateItems',
+    loadedSelector: '[data-testid="SearchGridHeader-title"]',
     nextLinkSelector: 'button[aria-label="Next page"]:not([disabled])',
     openSearchDefinition: null,
     noResultsXpath: '//*[contains(.,"Sorry, there are currently no items to display.")]',
