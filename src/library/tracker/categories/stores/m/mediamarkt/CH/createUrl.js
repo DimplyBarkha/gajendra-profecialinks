8f6fcbb0cff35af52d7f6b52b64dc30c85eba1d1
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 23:02:05 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/m/mediamarkt/CH/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'mediamarkt.ch',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: null,
+    country: 'CH',
+    store: 'mediamarkt',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/m/mediamarkt/CH/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'CH',
+    domain: 'mediamarkt.ch',
+    store: 'mediamarkt',
+    zipcode: '',
+  },
+};
