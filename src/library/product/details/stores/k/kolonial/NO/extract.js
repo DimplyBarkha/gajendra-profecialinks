4091b20bf2307598fe282b418aba2e7bd23feaa2
venkatsn <venkat.sn-odc@partner.import.io>
2020-12-15 14:18:18 +0530

    deepak

--- src/library/product/details/stores/k/kolonial/NO/extract.js
+++ src/library/product/details/stores/k/kolonial/NO/extract.js
@@ -1,23 +1,11 @@
-const { transform } = require('../../../../shared');
+const {cleanUp} = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'NO',
     store: 'kolonial',
-    transform: transform,
+    transform: cleanUp,
     domain: 'kolonial.no',
     zipcode: '',
-  },
-  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
-    await context.evaluate(async function () {
-      function addElementToDocument (key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
-      }
-    });
-    await context.extract(productDetails, { transform: transformParam });
   },
 };
