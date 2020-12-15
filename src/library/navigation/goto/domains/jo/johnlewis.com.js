c53265953a134674672a9633322cef4ed09533ee
sushma-mangalapally <sushma.mangalapally@import.io>
2020-12-15 10:54:58 -0800

    Modify goto function

--- src/library/navigation/goto/domains/jo/johnlewis.com.js
+++ src/library/navigation/goto/domains/jo/johnlewis.com.js
@@ -8,4 +8,24 @@ module.exports = {
     store: 'johnlewis',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    //const timeout = parameters.timeout ? parameters.timeout : 10000;
+
+    //await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setBlockAds(false);
+    await context.setLoadImages(true);
+    //await context.setFirstRequestTimeout(60000);
+    await context.goto(url, { timeout: 75000, waitUntil: 'load', checkBlocked: true });
+    /*
+    await context.setBlockAds(false);
+    await context.goto(`${url}`, {
+      anti_fingerprint: true,
+      discard_CSP_header: false,
+      timeout: 100000,
+      waitUntil: 'load',
+    });
+
+    */
+  },
 };
