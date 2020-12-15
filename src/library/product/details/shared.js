f1fc02c24a190010d378d6818e7aee5e63dbd4d2
Himanshu-Agrahari <himanshu.a@partner.import.io>
2020-12-15 15:31:24 +0530

    Fix: Tranasofm function issue
    Handle case if we have any word insdide double quote within a string
    
    Ex: ' my name is Dyson  and' get converted to ' my name is Dyson and' instead of ' my name isDysonand'

--- src/library/product/details/shared.js
+++ src/library/product/details/shared.js
@@ -10,12 +10,13 @@ const cleanUp = (data, context) => {
     .replace(/&amp;#160/g, ' ')
     .replace(/\u00A0/g, ' ')
     .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
+  // .replace(/"\s{1,}/g, '"')
+  // .replace(/\s{1,}"/g, '"')
     .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
+  // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
+    .trim();
   data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
     el.text = clean(el.text);
   }))));
