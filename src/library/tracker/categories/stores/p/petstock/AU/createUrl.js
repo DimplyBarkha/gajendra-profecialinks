ad91507c4c5296049c7e5db8aed6d693987af152
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 22:53:52 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/p/petstock/AU/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'petstock.com.au',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: null,
+    country: 'AU',
+    store: 'petstock',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/p/petstock/AU/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'AU',
+    domain: 'petstock.com.au',
+    store: 'petstock',
+    zipcode: '',
+  },
+};
