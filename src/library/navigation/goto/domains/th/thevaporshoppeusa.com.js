3b7eddcf4c34fbb151b4ee1fe183ddf0bf6fa896
alscotty <alawrencescott@gmail.com>
2020-12-15 10:37:03 -0800

    update loaded and noResults, adjust goto logic

--- src/library/navigation/goto/domains/th/thevaporshoppeusa.com.js
+++ src/library/navigation/goto/domains/th/thevaporshoppeusa.com.js
@@ -12,11 +12,11 @@ module.exports = {
     const url = `${inputs.url}`;
     await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
 
-    const pageLoader = (url.includes("search?")) ? 'div.product-listing.row' : 'div#pageContent';
+    const pageLoader = (url.includes('search?')) ? 'div.product-listing.row' : 'div#pageContent';
 
     const ageBtn = 'button#bouncer_modal_submit';
-    const cssReviews = 'div[class="tt-tabs__head"] ul > li:nth-child(2) span';
-    const cssReviewsRow = 'div.spr-content div.spr-review';
+    // const cssReviews = 'div[class="tt-tabs__head"] ul > li:nth-child(2) span';
+    // const cssReviewsRow = 'div.spr-content div.spr-review';
     const isSelectorAvailable = async (cssSelector) => {
       return await context.evaluate(function (selector) {
         return !!document.querySelector(selector);
@@ -28,12 +28,12 @@ module.exports = {
       await context.click(ageBtn);
     }
 
-    if (!url.includes("search?")) {
-      const reviewsAvailable = await isSelectorAvailable(cssReviews);
-      if (reviewsAvailable) {
-        await context.click(cssReviews);
-      }
-      await context.waitForSelector(cssReviewsRow, { timeout: 5000 });
-    }
-  }
+    // if (!url.includes("search?")) {
+    //   const reviewsAvailable = await isSelectorAvailable(cssReviews);
+    //   if (reviewsAvailable) {
+    //     await context.click(cssReviews);
+    //   }
+    //   await context.waitForSelector(cssReviewsRow, { timeout: 5000 });
+    // }
+  },
 };
