e484e243313b89ce57545c855f1c07193b916001
hari <harikrishnapalla47@gmail.com>
2020-12-15 13:08:47 +0530

    ferguson modified files

--- src/library/product/details/stores/f/ferguson/US/extract.js
+++ src/library/product/details/stores/f/ferguson/US/extract.js
@@ -11,6 +11,13 @@ module.exports = {
 
   implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
     const productUrl = await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
       const getXpath = (xpath, prop) => {
         const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
         let result;
@@ -18,6 +25,12 @@ module.exports = {
         else result = elem ? elem.singleNodeValue : '';
         return result && result.trim ? result.trim() : result;
       };
+      const scriptText = getXpath('//script[@class="js-dataLayer"]', 'innerText');
+      var reg = "('inStock' :)(.+)";
+      var found = scriptText.match(reg);
+      // console.log('scriptText' + found);
+      // var scriptTextObj = JSON.parse(scriptText);
+      addElementToDocument('added_inStock', found);
       const url = getXpath('//ul[@class="fg-search-results"]//li[1]//div[@class="rc-fg-search-header"]//a/@href', 'nodeValue');
       return url;
     });
