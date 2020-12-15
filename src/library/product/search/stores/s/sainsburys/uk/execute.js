4c430c46d7c1abaed330049ec2962f7259621295
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-15 19:39:48 +0530

    sainsburys by dhanush

--- src/library/product/search/stores/s/sainsburys/uk/execute.js
+++ src/library/product/search/stores/s/sainsburys/uk/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'sainsburys.co.uk',
     url: 'https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]={searchTerms}',
     loadedSelector: 'section[class="ln-o-section ln-o-section"] ul[class="ln-o-grid ln-o-grid--matrix ln-o-grid--equal-height"] li[class="pt-grid-item ln-o-grid__item ln-u-6/12@xs ln-u-3/12@md ln-u-2/12@xl"]',
-    noResultsXPath: null,
+    noResultsXPath: '(//div[contains(@class,"si__no-results")])[1]',
     zipcode: '',
   },
 };
