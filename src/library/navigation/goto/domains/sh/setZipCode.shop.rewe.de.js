fb06723a45c272a5f97d62e451dd86719d07a261
Mamatha-Gundapaneni <mamatha.g@osmosys.asia>
2020-12-15 22:02:21 +0530

    Updated code

--- src/library/navigation/goto/domains/sh/setZipCode.shop.rewe.de.js
+++ src/library/navigation/goto/domains/sh/setZipCode.shop.rewe.de.js
@@ -7,4 +7,40 @@ module.exports = {
     store: 'rewe',
     zipcode: '',
   },
+  implementation: async ({ id, URL, RPC, SKU, zipcode, storeId, postcode }, { country, domain, timeout }, context, dependencies) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 60000));
+    await context.evaluate(async function () {
+      const isCookieSelector = document.querySelector('button[id="uc-btn-accept-banner"]');
+      if (isCookieSelector) {
+        isCookieSelector.click();
+      }
+    });
+
+    await context.waitForSelector('div.search-service-rsTiles div.search-service-product a');
+    const newUrl = await context.evaluate(function () {
+      const tempUrl = document.querySelector('div.search-service-rsTiles div.search-service-product a');
+      if (tempUrl) {
+        return `https://shop.rewe.de${tempUrl.getAttribute('href')}`;
+      }
+    });
+
+    console.log(`mamatha storeId${storeId}`);
+    const url = newUrl;
+    console.log('Mamatha1' + url);
+    await context.waitForSelector('button.gbmc-header-link');
+    await context.click('button.gbmc-header-link');
+    await context.evaluate(async function () {
+      const isCookieSelector = document.querySelector('button[id="uc-btn-accept-banner"]');
+      if (isCookieSelector) {
+        isCookieSelector.click();
+      }
+    });
+    await context.waitForSelector('a.gbmc-change-zipcode-link');
+    await context.click('a.gbmc-change-zipcode-link');
+    await context.waitForSelector('input[data-testid="zip-code-input"]');
+    await context.setInputValue('input[data-testid="zip-code-input"]', `${storeId}`);
+    await context.waitForSelector('button[data-testid="service-btn"]');
+    await context.click('button[data-testid="service-btn"]');
+    await context.waitForNavigation(url, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+  },
 };
--- src/library/navigation/goto/domains/sh/shop.rewe.de.js
+++ src/library/navigation/goto/domains/sh/shop.rewe.de.js
@@ -8,27 +8,56 @@ module.exports = {
     store: 'rewe',
     zipcode: '',
   },
-  // implementation: async ({ url }, parameterValues, context, dependencies) => {
-  //   // eslint-disable-next-line
-  //   // const js_enabled = true; // Math.random() > 0.7;
-  //   // console.log('js_enabled', js_enabled); ;
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.captureRequests();
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
 
-  //   const isCaptcha = async () => {
-  //     return await context.evaluate(async function () {
-  //       const captchaEl = document.evaluate('//div[@id="cf-hcaptcha-container"]//iframe', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-  //       if (captchaEl.snapshotLength) {
-  //         return 'true';
-  //       } else {
-  //         return 'false';
-  //       }
-  //     });
-  //   };
-  //   try {
-  //     if (isCaptcha) {
-  //       document.querySelector('div[id="cf-hcaptcha-container"] iframe').click();
-  //     }
-  //   } catch (err) {
-  //     console.group('Captcha not solved' + err);
-  //   }
-  // },
+    const responseStatus = await context.goto(url, {
+      firstRequestTimeout: 60000,
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+      antiCaptchaOptions: {
+        type: 'RECAPTCHA',
+      },
+    });
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
+    const captchaFrame = 'div#cf-hcaptcha-container iframe';
+    try {
+      await context.waitForSelector(captchaFrame);
+    } catch (error) {
+      console.log('error: without undescore ', error);
+    }
+
+    console.log('captchaFrame', captchaFrame);
+    const hasCaptcha = async (selector) => {
+      return await context.evaluate(async (captchaSelector) => {
+        return Boolean(document.querySelector(captchaSelector));
+      }, selector);
+    };
+    const isCaptchaFramePresent = await hasCaptcha(captchaFrame);
+
+    if (isCaptchaFramePresent) {
+      console.log('isCaptcha', true);
+      await context.waitForNavigation(timeout);
+      await context.evaluateInFrame(captchaFrame, function () {
+        // @ts-ignore
+        document.querySelector(captchaFrame).click();
+      });
+      await context.solveCaptcha({ type: 'RECAPTCHA', inputElement: captchaFrame });
+    }
+
+    const hasAcceptLink = await context.evaluate((selector) => !!document.querySelector(selector), 'div#cf-hcaptcha-container iframe');
+    if (hasAcceptLink) {
+      await context.click('div#cf-hcaptcha-container iframe');
+    }
+  },
 };
