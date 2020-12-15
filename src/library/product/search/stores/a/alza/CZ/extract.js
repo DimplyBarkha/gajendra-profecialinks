e4a93b2c83bb4e4a14d1387df17a83484c5b426f
ravitejaw324 <ravi.w@partner.import.io>
2020-12-15 07:40:56 +0530

    added a blocked condition

--- src/library/product/search/stores/a/alza/CZ/extract.js
+++ src/library/product/search/stores/a/alza/CZ/extract.js
@@ -7,5 +7,26 @@ module.exports = {
     transform,
     domain: 'alza.cz',
     zipcode: '',
-  },
+  }, implementation
 };
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  const redirectedToBlockedPage = await context.evaluate(async function() {
+    if(document.querySelector('img[src*=ErrorStates]')) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  if(redirectedToBlockedPage) {
+    throw new context.reportBlocked(302,'Redirected to Blocked page');
+  }
+  return await context.extract(productDetails, { transform });
+}
