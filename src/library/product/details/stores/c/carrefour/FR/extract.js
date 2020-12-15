4fa2bc4367699a1fc0d2c30dff1970a867b9e339
Priyanka <priyanka.l@osmosys.asia>
2020-12-15 16:27:02 +0530

    Fixed issue with carrefour.fr

--- src/library/product/details/stores/c/carrefour/FR/extract.js
+++ src/library/product/details/stores/c/carrefour/FR/extract.js
@@ -21,6 +21,18 @@ module.exports = {
       document.querySelector('body').setAttribute('import-purchasability', purchasability);
       document.querySelector('body').setAttribute('import-availability', availability);
       document.querySelector('body').setAttribute('import-enhanced-content', 'false');
+
+      let imageEls = document.evaluate("//div[@class='pdp-hero'][count(*)>2]/div[@class='pdp-hero__thumbs']/a/img/@src | //div[@class='pdp-hero'][count(*)=2]/div[@id='data-produit-image']//img/@src", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+
+      for (let i = 0; i < imageEls.snapshotLength; i++) {
+        const imageUrl = imageEls.snapshotItem(i).textContent.replace(/(\/media\/)(43x43|540x540)(\/.*.jpg)/g, "https://www.carrefour.fr$11500x1500$3");
+
+        if (imageUrl) {
+          const imgEl = document.createElement('import-image');
+          imgEl.setAttribute('data', imageUrl);
+          document.body.appendChild(imgEl);
+        }
+      }
     });
     const { transform } = parameters;
     const { productDetails } = dependencies;
