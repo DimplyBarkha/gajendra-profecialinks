8f4c23d565610bfb1572c6a05c3e3d119fa8b318
Wayne E <bartekstafiej2@gmail.com>
2020-12-15 19:28:44 +0100

    Fixed no productUrl problem (#1351)

--- src/library/product/search/stores/j/jumbo/NL/extract.js
+++ src/library/product/search/stores/j/jumbo/NL/extract.js
@@ -6,7 +6,12 @@ async function implementation (inputs, parameters, context, dependencies) {
 
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
+  // do {
   await context.evaluate(async () => {
+    function addProp (iterator, name, value) {
+      document.querySelectorAll('div[analytics-tag="product card"]')[iterator].setAttribute(name, value);
+    }
+
     const productUrlAll = document.querySelectorAll('.list-methods a.opposite');
     let productUrl;
     const priceArray = [];
@@ -21,19 +26,31 @@ async function implementation (inputs, parameters, context, dependencies) {
       priceArray[1] = price.replace(priceArray[0], '');
       price = priceArray[1] + ',' + priceArray[0];
 
-      document.querySelectorAll('div[analytics-tag="product card"]')[i].setAttribute('productUrl', productUrl);
-      document.querySelectorAll('div[analytics-tag="product card"]')[i].setAttribute('price', price);
-      document.querySelectorAll('div[analytics-tag="product card"]')[i].setAttribute('rank', `${i + 1}`);
+      addProp(i, 'productUrl', productUrl);
+      addProp(i, 'price', price);
+      addProp(i, 'rank', `${i + 1}`);
     }
+
+    let searchUrl;
+
+    if (priceSelector.length === 0) {
+      searchUrl = null;
+    } else {
+      searchUrl = window.location.href;
+    }
+    document.querySelector('body').setAttribute('searchurl', searchUrl);
   });
 
-  await context.extract(productDetails, { transform });
+  //   await context.extract(productDetails, { transform });
 
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  //   await new Promise((resolve) => setTimeout(resolve, 3000));
 
-  await context.evaluate(() => {
-    document.querySelector('span.d-xs-inline.d-l-none').click();
-  });
+  //   await context.evaluate(() => {
+  //     document.querySelector('span.d-xs-inline.d-l-none').click();
+  //   });
+  // } while (await context.evaluate(() => {
+  //   return document.querySelector('span.d-xs-inline.d-l-none');
+  // }) !== null);
 
   return await context.extract(productDetails, { transform });
 }
--- src/library/product/search/stores/j/jumbo/NL/paginate.js
+++ src/library/product/search/stores/j/jumbo/NL/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'NL',
     store: 'jumbo',
-    nextLinkSelector: 'span.d-xs-inline.d-l-none',
+    // nextLinkSelector: 'span.d-xs-inline.d-l-none',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: '.rw',
     noResultsXPath: '//div[@class="error-state-wrapper text-center cl ctr"]/div[@class="server-error"]',
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.jumbo.com/producten/?offSet={offset}&searchTerms={searchTerms}&pageSize=25',
+    },
     domain: 'jumbo.com',
     zipcode: '',
   },
