4430dd8a099c3cbd86b92eacd54995fb5f6fb8fb
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 12:18:14 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/a/asos/UK/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'UK',
+    domain: 'asos.com',
+    store: 'asos',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/a/asos/UK/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'UK',
+    domain: 'asos.com',
+    store: 'asos',
+    zipcode: '',
+  },
+};
