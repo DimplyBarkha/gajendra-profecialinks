5c923ab96665cb42db7203a281da433f04599235
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 12:19:56 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/d/docmorris/DE/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'DE',
+    domain: 'docmorris.de',
+    store: 'docmorris',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/d/docmorris/DE/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'DE',
+    domain: 'docmorris.de',
+    store: 'docmorris',
+    zipcode: '',
+  },
+};
