0ae1ed0efe90b00e4eaeeab0c2f387ea2e5413dd
unknown <ramraut25@gmail.com>
2020-12-15 11:19:08 +0530

    Added timeout

--- src/library/navigation/goto/domains/em/emaxme.com.js
+++ src/library/navigation/goto/domains/em/emaxme.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'emaxme.com',
-    timeout: null,
+    timeout: 60000,
     country: 'AE',
     store: 'emax',
     zipcode: '',
