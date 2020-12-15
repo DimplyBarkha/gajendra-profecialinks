46956b1e8a17aeb0b82b48d82a3668b16ab6f77d
Venkata Akhilesh Pasupuleti <41539204+VenkataAkhileshP@users.noreply.github.com>
2020-12-16 00:26:05 +0530

    Increased timeout for loadedSelector and added validation for API response

--- src/library/product/details/stores/e/elcorteingles/ES/execute.js
+++ src/library/product/details/stores/e/elcorteingles/ES/execute.js
@@ -1,4 +1,28 @@
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  let { url, id, zipcode, storeId } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  await dependencies.goto({ url, zipcode, storeId });
+
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 60000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+
+  // TODO: Check for not found?
+}
+
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -10,4 +34,5 @@ module.exports = {
     // loadedSelector: 'img[class="js-zoom-to-modal-image"]',
     // noResultsXPath: '//span[contains(.,"No hemos")]',
   },
+  implementation,
 };
