9790448ffbeeb1be810211e1b95015744e0b2232
sahoojudhi <judhishtor@profecialinks.com>
2020-12-16 01:03:11 +0530

    bug fixing

--- src/library/product/details/stores/m/musgravemarketplace/IE/createUrl.js
+++ src/library/product/details/stores/m/musgravemarketplace/IE/createUrl.js
@@ -3,8 +3,8 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'musgravemarketplace.ie',
-    prefix: null,
-    url: null,
+    prefix: 'easyorder/infopr3?parm=8643676205258067704000&srchTrms=1&catSrchTrms=',
+    url: 'https://order.musgravemarketplace.ie/easyorder/infopr3?parm=8643676205258067704000&srchTrms=1&catSrchTrms={id}',
     country: 'IE',
     store: 'musgravemarketplace',
     zipcode: '',
