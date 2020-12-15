170b77014792ebe8b798b7e415b84d7bb52f63c4
Michał Wieczorek <michal126@hotmail.com>
2020-12-15 09:09:51 +0100

    Created template for menu only

---
+++ src/library/tracker/categories/stores/h/hulpmiddelwereld/NL/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'NL',
+    domain: 'hulpmiddelwereld.nl',
+    store: 'hulpmiddelwereld',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/h/hulpmiddelwereld/NL/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'NL',
+    domain: 'hulpmiddelwereld.nl',
+    store: 'hulpmiddelwereld',
+    zipcode: '',
+  },
+};
