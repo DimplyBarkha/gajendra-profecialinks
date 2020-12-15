c21c4c97fe0356d3e5021b4abe79e2f73edcbb59
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 13:12:46 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/w/waitrose/UK/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'UK',
+    domain: 'waitrose.com',
+    store: 'waitrose',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/w/waitrose/UK/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'waitrose',
+    domain: 'waitrose.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
