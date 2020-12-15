368c1c80dc6406852d48644f7d881b4902013338
pkujawa <p.kujawa2@op.pl>
2020-12-15 10:13:41 +0100

    #2584. noResultsXpath for wrong RPCs added

--- src/library/product/details/stores/p/paodeacucar/BR/execute.js
+++ src/library/product/details/stores/p/paodeacucar/BR/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'paodeacucar',
     domain: 'paodeacucar.com',
     loadedSelector: null,
-    noResultsXPath: null,
+    noResultsXPath: '//div[@class="MuiGrid-root gridstyles-sc-6scn59-0 laucrw MuiGrid-container"]//p[text()[contains(.,"Infelizmente não encontramos nenhum produto como esse por aqui, aconselhamos a tentar novamente ou procurar em outra modalidade.")]]',
     zipcode: '',
   },
 };
