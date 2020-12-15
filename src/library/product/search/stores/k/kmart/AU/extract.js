f02cb4183180abf264e60fc47165f7a7ac0a5782
unknown <leena.gavali-odc@partner.import.io>
2020-12-15 15:50:19 +0530

    kmart_scroll

--- src/library/product/search/stores/k/kmart/AU/extract.js
+++ src/library/product/search/stores/k/kmart/AU/extract.js
@@ -1,4 +1,60 @@
 const { transform } = require('../../../../shared');
+const implementation = async function (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        scrollTop += 200;
+        window.scroll(0, scrollTop);
+        await stall(1000);
+      }
+      function stall(ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+      function addHiddenDiv(id, content, index) {
+        // @ts-ignore
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll('div[class="product product_box small-6 medium-4 large-4 columns clearfix col "]')[index];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      };
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+  
+        }
+        return result;
+  
+      };
+      var abc = getAllXpath("//div[@class='product product_box small-6 medium-4 large-4 columns clearfix col ']/@data-attribute-productid", 'nodeValue');
+        if (abc != null) {
+          for (var i = 0; i < abc.length; i++) {
+            abc[i] = "P_" + abc[i]
+            addHiddenDiv('id', abc[i], i);
+          }
+        }
+    });
+  };
+  await applyScroll(context);
+
+  return await context.extract(productDetails, { transform });
+};
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,92 +65,4 @@ module.exports = {
     zipcode: '',
   },
   implementation,
-
-
-};
-
-async function implementation(
-
-  inputs,
-
-  parameters,
-
-  context,
-
-  dependencies,
-
-) {
-
-  const { transform } = parameters;
-
-  const { productDetails } = dependencies;
-
-  await context.evaluate(async function () {
-
-    function addclass(xpathforpagination) {
-
-      var elems = document.querySelectorAll(xpathforpagination);
-
-      elems[0].classList.add('pagination');
-
-    }
-
-    function addHiddenDiv(id, content, index) {
-
-      const newDiv = document.createElement('div');
-
-      newDiv.id = id;
-
-      newDiv.textContent = content;
-
-      newDiv.style.display = 'none';
-
-      const originalDiv = document.querySelectorAll('div[class="product product_box small-6 medium-4 large-4 columns clearfix col "]')[index];
-
-      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-
-    }
-
-
-
-    const getAllXpath = (xpath, prop) => {
-
-      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-
-      const result = [];
-
-      for (let index = 0; index < nodeSet.snapshotLength; index++) {
-
-        const element = nodeSet.snapshotItem(index);
-
-        if (element) result.push(prop ? element[prop] : element.nodeValue);
-
-      }
-
-      return result;
-
-    };
-
-
-
-
-
-
-
-    // price
-
-    var abc = getAllXpath("//div[@class='product product_box small-6 medium-4 large-4 columns clearfix col ']/@data-attribute-productid", 'nodeValue');
-      if (abc != null) {
-        for (var i = 0; i < abc.length; i++) {
-          abc[i] = "P_" + abc[i]
-          addHiddenDiv('id', abc[i], i);
-        }
-      }
-
-
-
-  });
-
-  return await context.extract(productDetails, { transform });
-
 };
--- src/library/product/search/stores/k/kmart/AU/paginate.js
+++ src/library/product/search/stores/k/kmart/AU/paginate.js
@@ -7,7 +7,7 @@ module.exports = {
     nextLinkSelector: 'li a[id="WC_SearchBasedNavigationResults_pagination_link_right_categoryResults_bottom"]',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'body',
+    loadedSelector: 'div[class="grid_mode"]',
     noResultsXPath: null,
     openSearchDefinition: null,
     domain: 'kmart.com.au',
