d900d5a08a9796d5dbfe183ce0532b9c0dfc3411
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-15 12:08:34 +0100

    created a template for the menu only extractor

---
+++ src/library/navigation/goto/domains/gr/groceries.morrisons.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'groceries.morrisons.com',
+    timeout: null,
+    country: 'UK',
+    store: 'morrisons',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/m/morrisons/UK/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'UK',
+    domain: 'groceries.morrisons.com',
+    store: 'morrisons',
+    zipcode: '',
+  },
+};
