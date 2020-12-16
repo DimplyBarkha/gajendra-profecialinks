cb9f5fb8a9567053660c9cc96bedddecd1ef1b40
Avinash <avinash.kadam@profecialinks.com>
2020-12-16 18:55:20 +0530

    issue resolved-productUrl are without domain

--- src/library/product/search/stores/z/ziipstock/US/extract.js
+++ src/library/product/search/stores/z/ziipstock/US/extract.js
@@ -1,10 +1,23 @@
+const transform = (data) => {
+	for (const { group } of data) {
+		for (const row of group) {
+      if (row.productUrl) {
+				let text = '';
+				row.productUrl.forEach(item => {
+						item.text = 'https://ziipstock.com' + item.text;
+				})
+      }
+		}
+	}
+	return data;
+};
 
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'ziipstock',
-    transform: null,
+    transform,
     domain: 'ziipstock.com',
     zipcode: "''",
   },
