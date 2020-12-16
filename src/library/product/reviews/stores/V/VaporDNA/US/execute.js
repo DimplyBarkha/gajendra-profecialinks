b6f5f0d7ed0f8dc8152a3bf4917c7d98602027d0
cshelake <cshelake7@gmail.com>
2020-12-16 19:50:23 +0530

    fix noresultxpath

--- src/library/product/reviews/stores/V/VaporDNA/US/execute.js
+++ src/library/product/reviews/stores/V/VaporDNA/US/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'vapordna',
     domain: 'vapordna.com',
     loadedSelector: 'div#shopify-section-product-template > div.product-section:first-child div.stamped-content',
-    noResultsXPath: null,
+    noResultsXPath: '//h1[contains(text(),"Page Not Found")] | //div[contains(text(),"Be the first to review this item")]',
     reviewUrl: null,
     sortButtonSelectors: null,
     zipcode: "''",
