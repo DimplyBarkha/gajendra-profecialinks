0e258e84c2b046318372624e02268eef9ab3341f
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 07:46:15 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/m/mytime/DE/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'mytime.de',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: null,
+    country: 'DE',
+    store: 'mytime',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/m/mytime/DE/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'DE',
+    domain: 'mytime.de',
+    store: 'mytime',
+    zipcode: '',
+  },
+};
