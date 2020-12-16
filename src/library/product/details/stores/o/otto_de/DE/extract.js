1cd0ae8a4e0aef9a386a6a66533613cf732235d3
Harshkha <harsh.khare-od@partner.import.io>
2020-12-16 14:42:52 +0530

    IM complete

--- src/library/product/details/stores/o/otto_de/DE/extract.js
+++ src/library/product/details/stores/o/otto_de/DE/extract.js
@@ -8,4 +8,31 @@ module.exports = {
     domain: 'otto.de',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const getXpath = (xpath, prop) => {
+        const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+      // @ts-ignore
+      const rawdata = getXpath("//div[@class='prd_price__main']/span/span[@id='reducedPriceAmount']/text()",'nodeValue');
+      if (rawdata!=null){
+       var price='€'+rawdata
+       addElementToDocument('price', price);
+      }
+
+      
+    });
+    await context.extract(productDetails, { transform: transformParam });
+  },
 };
