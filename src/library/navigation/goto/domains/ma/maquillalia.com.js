4af76da2986607a226becd5c9e54d3cc328449cb
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 10:54:05 +0100

    created a template for the menu only extractor

---
+++ src/library/navigation/goto/domains/ma/maquillalia.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'maquillalia.com',
+    timeout: null,
+    country: 'ES',
+    store: 'maquillalia',
+    zipcode: '',
+  },
+};
