2cc551553936161647f332a030d59e46520eec81
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-12-16 14:31:12 +0100

    - Finishing working on the Sears US menu extractor - Generating tests

--- src/library/tracker/categories/stores/s/sears/US/extract.js
+++ src/library/tracker/categories/stores/s/sears/US/extract.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -6,5 +5,44 @@ module.exports = {
     domain: 'sears.com',
     store: 'sears',
     zipcode: '',
+  },
+  implementation: async (inputs, properties, context, { productMenu }) => {
+    await context.evaluate(async () => {
+      const addedCatList = document.createElement('ul');
+      addedCatList.id = 'added_categories_list';
+      addedCatList.style.display = 'none';
+      document.body.appendChild(addedCatList);
+
+      const allViewAllUrls = document.querySelectorAll('div#sitemap ul.list-links.column > li > a');
+      for (let i = 0; i < allViewAllUrls.length; i++) {
+        console.log(allViewAllUrls[i].textContent);
+        const categoryUrl = allViewAllUrls[i].href;
+        const doc = await fetch(categoryUrl)
+          .then((resp) => resp.text())
+          .then((resp) => {
+            const parser = new DOMParser();
+            return parser.parseFromString(resp, 'text/html');
+          })
+          .catch((err) => {
+            console.log(err);
+            throw new Error('Failed to fetch the category data');
+          });
+        const allCategoryUrls = document.evaluate(
+          './/div[@id="sitemap"]//section//a[not(@class)]',
+          doc,
+          null,
+          XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+          null,
+        );
+        for (let j = 0; j < allCategoryUrls.snapshotLength; j++) {
+          const item = allCategoryUrls.snapshotItem(j);
+          const listItem = document.createElement('li');
+          listItem.setAttribute('category', item.textContent);
+          listItem.setAttribute('categoryUrl', item.href);
+          addedCatList.appendChild(listItem);
+        }
+      }
+    });
+    await context.extract(productMenu);
   },
 };
