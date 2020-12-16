0a3ad498b8711fdad71570742d5bc989584cb823
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-12-16 14:39:11 +0100

    Extractor built

--- src/library/tracker/categories/stores/s/sharafdg/AE/extract.js
+++ src/library/tracker/categories/stores/s/sharafdg/AE/extract.js
@@ -2,9 +2,48 @@
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
-    country: 'AE',
-    domain: 'uae.sharafdg.com',
-    store: 'sharafdg',
+    country: 'DE',
+    domain: 'parfumdreams.de',
+    store: 'parfumdreams',
     zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { productMenu } = dependencies;
+
+    await context.evaluate(async () => {
+      function addHiddenDiv (id, content, parentDiv = null) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        if (!content) content = '';
+        newDiv.textContent = content;
+        if (parentDiv) {
+          parentDiv.appendChild(newDiv);
+        } else {
+          document.body.appendChild(newDiv);
+        }
+        return newDiv;
+      }
+
+      const categories = document.querySelectorAll('div.mega-menu-wrap:nth-child(-n+7) > ul > li');
+
+      categories.forEach((category) => {
+        const categoryName = category.querySelector('a').textContent;
+        const subCategories = category.querySelectorAll('ul > li[class*="columns"]');
+
+        subCategories.forEach((subCategory) => {
+          const subCategoryName = subCategory.querySelector('a').textContent;
+          const elements = subCategory.querySelectorAll('ul > li');
+
+          elements.forEach((element) => {
+            element = element.querySelector('a');
+            const newDiv = addHiddenDiv('categories');
+            const names = [categoryName, subCategoryName, element.textContent];
+            names.forEach(name => addHiddenDiv('category', name, newDiv));
+            addHiddenDiv('categoryUrl', `https://uae.sharafdg.com/${element.getAttribute('href')}`, newDiv);
+          });
+        });
+      });
+    });
+    return await context.extract(productMenu);
   },
 };
