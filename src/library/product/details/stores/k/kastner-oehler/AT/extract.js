2d624af49cb401e70c53edc23be655cf77af407c
ravitejaw324 <ravi.w@partner.import.io>
2020-12-16 12:37:22 +0530

    fixed the issue with enhanced content

--- src/library/product/details/stores/k/kastner-oehler/AT/extract.js
+++ src/library/product/details/stores/k/kastner-oehler/AT/extract.js
@@ -10,10 +10,64 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+
+    
     const productUrl = await context.evaluate(async function () {
       return document.URL;
     });
-    await context.evaluate(async function () {
+    
+    //getting enhacedContent from iframe.
+    const iframeUrl = await context.evaluate(async function() {
+      let gtin = document.querySelector('span[itemprop*=gtin]') ? document.querySelector('span[itemprop*=gtin]').innerText.trim() : "";
+      if(!gtin) {
+        console.log("Enhaced Content not found");
+        return null;
+      }
+      return `https://service.loadbee.com/ean/${gtin}/de_DE?css=default&template=default&button=default&data=%7B%22shop%22%3A%22www.kastner-oehler.at%22%2C%22source%22%3A%22inpage%22%2C%22api%22%3A%22hKxdndaN6V7hJ6SwV2RyLs9M3gGt73re%22%7D`;
+    });
+    let enhancedContent = {};
+    if(iframeUrl) {
+      await context.goto(iframeUrl, {
+        block_ads: false,
+        load_all_resources: true,
+        images_enabled: true,
+        timeout: 50000,
+        waitUntil: 'load',
+      });
+      enhancedContent = await context.evaluate(async function() {
+        let content = {};
+        content.images = [];
+        let imageNodes = document.querySelectorAll('img[class=pic]');
+        imageNodes.forEach(img => {
+          if(img.hasAttribute('src')) {
+            content.images.push(img.getAttribute('src'));
+          }
+        });
+        content.manufacturerDescription = document.body.innerText.trim().replace(/\n|\s\s+/g," ");
+        content.videos = [];
+        let videoNodes = document.querySelectorAll('*[data-video]');
+        videoNodes.forEach(vid => {
+          if(vid.hasAttribute('data-video')) {
+            if(!content.videos.includes(vid.getAttribute('data-video'))) {
+            content.videos.push(vid.getAttribute('data-video'))
+            }
+          }
+        })
+
+        return content;
+      })
+    }
+
+    //going back to product page
+    await context.goto(productUrl, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 50000,
+      waitUntil: 'load',
+    });
+
+    await context.evaluate(async function (enhancedContent) {
       const cookies = document.querySelector('span.tao_button_cookie_settings');
       if (cookies) cookies.click();
       await new Promise(resolve => setTimeout(resolve, 2000));
@@ -31,6 +85,18 @@ module.exports = {
         bulletInfo.forEach(e => {
           descBulletInfo.push(e.innerText);
         });
+      }
+
+      addElementToDocument('enhancedContent', enhancedContent.manufacturerDescription);
+      if(enhancedContent.videos) {
+        enhancedContent.videos.forEach(q => {
+          addElementToDocument('video', q);
+        })
+      }
+      if(enhancedContent.images) {
+        enhancedContent.images.forEach(q => {
+          addElementToDocument("manufacturerImages", q);
+        })
       }
       addElementToDocument('desc_bullets', descBulletInfo.join('||'));
       const descInfo = document.querySelectorAll('span[itemprop="description"] p');
@@ -58,83 +124,7 @@ module.exports = {
         // @ts-ignore
         nameExtended.content = newNameExtended;
       }
-    });
-
-    async function autoScroll () {
-      await context.evaluate(async function () {
-        await new Promise((resolve, reject) => {
-          var totalHeight = 0;
-          var distance = 100;
-          var timer = setInterval(() => {
-            var scrollHeight = document.body.scrollHeight;
-            window.scrollBy(0, distance);
-            totalHeight += distance;
-            console.log('===== Scrolling =====');
-            if (totalHeight >= scrollHeight) {
-              clearInterval(timer);
-              resolve();
-            }
-          }, 100);
-        });
-      });
-    }
-
-    await autoScroll();
-    const iFrameCss = 'div[id*="loadBeeContainer"] iframe';
-    try {
-      await context.waitForSelector(iFrameCss, { timeout: 30000 });
-      console.log(`iFrameCss: ${iFrameCss} loaded..`);
-    } catch (error) {
-      console.log(`iFrameCss: ${iFrameCss} not loaded..`, error);
-    }
-
-    const iframeLink = await context.evaluate(async function (iFrameCss) {
-      let iframeLink = null;
-      document.querySelector('#productcarousel_3 .productcarousel__header').scrollIntoView({ behavior: 'smooth' });
-      if (document.querySelector('div[class*="site_inner"] iframe')) { iframeLink = document.querySelector(iFrameCss).getAttribute('src'); }
-      return iframeLink;
-    }, iFrameCss);
-
-    let enhancedContent = null;
-    console.log(`Iframe link: ${iframeLink}`);
-    if (iframeLink !== null) {
-      await context.goto(iframeLink, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
-      await context.waitForSelector('.module.header img');
-      enhancedContent = await context.evaluate(async function () {
-        const video = document.querySelector('.play-btn.centered.desktop').getAttribute('data-video');
-        const enhancedContent = document.querySelector('.pic-text-modul').innerText;
-        const aplusImagesArray = document.querySelectorAll('.pic-text img');
-        const aplusImagesSrc = [];
-        if (aplusImagesArray.length > 0) {
-          aplusImagesArray.forEach(image => {
-            aplusImagesSrc.push(image.getAttribute('data-src') + ' ||');
-          });
-        }
-        // let enhancedContent='';
-        // let fontText1=document.querySelectorAll('div[class*="pic-text"] div font font');
-        // for(let i=0;i<fontText1.length;i++) enhancedContent+=fontText1[i].innerText+' || ';
-        // let fontText2=document.querySelectorAll('div[class*="animation-text"]  font font');
-        // for(let i=0;i<fontText2.length;i++) enhancedContent+=fontText2[i].innerText+'||';
-        return { enhancedContents: enhancedContent, videos: video, aplusImages: aplusImagesSrc };
-      });
-    } else console.log('iframe link not loaded');
-
-    await context.goto(productUrl, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
-    if (enhancedContent !== null) {
-      await context.evaluate(async function (enhancedContent) {
-        function addHiddenDiv (id, content) {
-          const newDiv = document.createElement('div');
-          newDiv.id = id;
-          newDiv.textContent = content;
-          newDiv.style.display = 'none';
-          document.body.appendChild(newDiv);
-        }
-        addHiddenDiv('enhancedContent', enhancedContent.enhancedContents);
-        addHiddenDiv('video', enhancedContent.videos);
-        addHiddenDiv('aplusImage', enhancedContent.aplusImages);
-      }, enhancedContent);
-    }
-    await context.waitForSelector('.en_lazy_load');
+    }, enhancedContent);
     await context.extract(productDetails, { transform: transformParam });
   },
 };
