e59e39e4cf861dae01827a679941ece97df4913f
Harish <harish.sathavara@kcsitglobal.com>
2020-12-16 12:38:51 +0530

    Add some fields

--- src/library/product/details/stores/d/drogaraia/BR/extract.js
+++ src/library/product/details/stores/d/drogaraia/BR/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('./transform');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'BR',
     store: 'drogaraia',
-    transform: null,
+    transform: transform,
     domain: 'drogaraia.com.br',
     zipcode: '',
   },
