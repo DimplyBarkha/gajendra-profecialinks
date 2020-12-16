cf175f26b67c2e8990a045c70d471f1692de78a2
ravitejaw324 <ravi.w@partner.import.io>
2020-12-16 14:52:24 +0530

    increase navingation timeOut

--- src/library/navigation/goto/domains/dr/drankdozijn.nl.js
+++ src/library/navigation/goto/domains/dr/drankdozijn.nl.js
@@ -7,7 +7,7 @@ module.exports = {
     store: 'drankdozijn',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    const timeout = parameters.timeout ? parameters.timeout : 40000;
     await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true });
     // Hack to wait until details are binded to the DOM
     await new Promise((resolve, reject) => setTimeout(resolve, 5000));
