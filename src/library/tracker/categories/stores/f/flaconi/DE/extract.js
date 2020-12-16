f2c128f18f79fbdaca53444000252e1b9c895db1
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2020-12-16 08:13:14 +0100

    Cleaned up the code

--- src/library/tracker/categories/stores/f/flaconi/DE/extract.js
+++ src/library/tracker/categories/stores/f/flaconi/DE/extract.js
@@ -1,19 +1,3 @@
-
-async function implementation (inputs, parameters, context, dependencies) {
-  const { productMenu } = dependencies;
-
-  console.log(productMenu);
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-  const allCategories = await context.evaluate(async () => document.querySelectorAll('nav#main-navigation ul li a[data-webtrekk-link-id="header.subnav"]').length);
-
-  if (allCategories) {
-
-  } else {
-    throw new Error('No categories found');
-  }
-  return await context.extract(productMenu);
-}
-
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -22,8 +6,4 @@ module.exports = {
     store: 'flaconi',
     zipcode: '',
   },
-  dependencies: {
-    productMenu: 'extraction:tracker/categories/stores/${store[0:1]}/${store}/${country}/extract',
-  },
-  implementation,
 };
