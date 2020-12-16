899f0bc68899fda6a9f68906c6cf6fe6711644f3
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 09:08:04 +0100

    created a template for the menu only extractor

---
+++ src/library/navigation/goto/domains/ly/setZipCode.lyreco.com.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'UK',
+    domain: 'lyreco.com',
+    store: 'lyreco',
+    zipcode: '',
+  },
+};
