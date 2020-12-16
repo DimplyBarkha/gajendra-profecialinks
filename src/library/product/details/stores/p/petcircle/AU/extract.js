144c4328b5f407c9dc21324815d645d44fcfcd63
sahoojudhi <judhishtor@profecialinks.com>
2020-12-16 21:03:44 +0530

    Core run by RPC fixed

--- src/library/product/details/stores/p/petcircle/AU/extract.js
+++ src/library/product/details/stores/p/petcircle/AU/extract.js
@@ -8,4 +8,21 @@ module.exports = {
     domain: 'petcircle.com.au',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const timeout = 45000;
+    const redirectProductDetails = async () => {
+      try {
+        await context.waitForSelector('h3.product-name>a', { timeout });
+      } catch (err) {
+        console.log('Product with RPC didn\'t load');
+      }
+    };
+    
+    await redirectProductDetails();
+    await context.clickAndWaitForNavigation('h3.product-name>a', { timeout, waitUntil: 'load' });
+    
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.extract(productDetails, { transform });
+  },
 };
