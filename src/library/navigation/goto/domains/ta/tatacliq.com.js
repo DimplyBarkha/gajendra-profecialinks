3f5992b3add48bbd66bcd78d99d4e2e43f694311
unknown <ramraut25@gmail.com>
2020-12-16 15:15:49 +0530

    Updated xpath for coupon attribute

--- src/library/navigation/goto/domains/ta/tatacliq.com.js
+++ src/library/navigation/goto/domains/ta/tatacliq.com.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true });
+    await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true, load_all_resources: true });
     console.log(zipcode);
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
@@ -16,7 +16,7 @@ module.exports = {
     await context.evaluate(async function () {
       if (document.URL === 'https://www.tatacliq.com/') {
         console.log('Navigated to home page, redirecting to the product page');
-        await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true });
+        await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true, load_all_resources: true });
       }
     });
   },
