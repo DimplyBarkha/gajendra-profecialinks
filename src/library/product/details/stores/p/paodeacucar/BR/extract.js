9df10e8f0208107310ddf5578ff1578f9cbac1de
pkujawa <p.kujawa2@op.pl>
2020-12-16 11:53:01 +0100

    #2584. cleanUp instead of Transform

--- src/library/product/details/stores/p/paodeacucar/BR/extract.js
+++ src/library/product/details/stores/p/paodeacucar/BR/extract.js
@@ -1,6 +1,6 @@
-const { transform } = require('../../../../shared');
+const { cleanUp } = require('../../../../shared');
 const implementation = async (inputs, parameters, context, dependencies) => {
-  const { transform } = parameters;
+  const { cleanUp } = parameters;
   const { productDetails } = dependencies;
   await context.waitForSelector('.product-cardstyles__Container-sc-1uwpde0-1.eaVrql');
   async function firstItemLink() {
@@ -13,14 +13,14 @@ const implementation = async (inputs, parameters, context, dependencies) => {
   if (url !== null) {
     await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
   }
-  return await context.extract(productDetails, { transform });
+  return await context.extract(productDetails, { cleanUp });
 };
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'BR',
     store: 'paodeacucar',
-    transform: transform,
+    transform: cleanUp,
     domain: 'paodeacucar.com',
     zipcode: '',
   },
