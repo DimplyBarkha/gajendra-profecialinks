2945fc559e245099e97c37ad59a899414f3479b8
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 10:52:33 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/m/manor/CH/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'manor.ch',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: null,
+    country: 'CH',
+    store: 'manor',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/m/manor/CH/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'CH',
+    domain: 'manor.ch',
+    store: 'manor',
+    zipcode: '',
+  },
+};
