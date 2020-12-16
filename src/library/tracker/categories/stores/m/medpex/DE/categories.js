e245b4319f1beac2a4d2966e36f514fd255e4830
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 11:58:59 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/m/medpex/DE/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'DE',
+    domain: 'medpex.de',
+    store: 'medpex',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/m/medpex/DE/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'DE',
+    domain: 'medpex.de',
+    store: 'medpex',
+    zipcode: '',
+  },
+};
