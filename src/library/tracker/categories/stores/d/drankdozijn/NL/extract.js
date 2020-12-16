ea4293bb840722c5c182904c61e48a0fc566cec9
sasz94 <sacha.bartlomiej@gmail.com>
2020-12-16 14:29:24 +0100

    Fixed menu NL extractor

--- src/library/tracker/categories/stores/d/drankdozijn/NL/extract.js
+++ src/library/tracker/categories/stores/d/drankdozijn/NL/extract.js
@@ -1,14 +1,3 @@
-async function implementation (inputs, parameters, context, dependencies) {
-  const { productMenu } = dependencies;
-  let dataRef = await context.extract(productMenu);
-  const array = [];
-  for (let i = 0; i < dataRef.length; i++) {
-    array.push(...dataRef[i].group);
-  }
-  dataRef[0].group = array;
-  dataRef = dataRef.splice(1, dataRef.length - 1);
-  return dataRef;
-}
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -17,5 +6,45 @@ module.exports = {
     store: 'drankdozijn',
     zipcode: '',
   },
-  implementation,
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { productMenu } = dependencies;
+
+    const isSiteMapDivPresent = await context.evaluate(async () => {
+      return document.querySelector('li.has-dropdown') !== null;
+    });
+
+    if (isSiteMapDivPresent) {
+      await context.evaluate(async () => {
+        function addHiddenDiv (id, content, parentDiv = null) {
+          const newDiv = document.createElement('div');
+          newDiv.dataset.id = id;
+          if (!content) content = '';
+          newDiv.textContent = content;
+          newDiv.style.display = 'none';
+          if (parentDiv) {
+            parentDiv.appendChild(newDiv);
+          } else {
+            document.body.appendChild(newDiv);
+          }
+          return newDiv;
+        }
+
+        const mainCategoryContainers = document.querySelectorAll('li[class="has-dropdown"]');
+        mainCategoryContainers.forEach(mainCategory => {
+          const categoryContainers = mainCategory.querySelectorAll('div.dropdown > div.w_max > ul:first-child > li a');
+          categoryContainers.forEach(category => {
+            if (category) {
+              const subWrapper = addHiddenDiv('cat-wrapper');
+              addHiddenDiv('cat', mainCategory.querySelector('a span') ? mainCategory.querySelector('a span').textContent : '', subWrapper);
+              addHiddenDiv('cat', category.textContent, subWrapper);
+              addHiddenDiv('cat-url', category.getAttribute('href'), subWrapper);
+            }
+          });
+        });
+      });
+    } else {
+      throw new Error('No categories found');
+    }
+    return await context.extract(productMenu);
+  },
 };
