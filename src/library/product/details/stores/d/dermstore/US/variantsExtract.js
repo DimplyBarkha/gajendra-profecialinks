f8c402fda96f674b748074ba70b34146edc481ff
nilofarsh <nilofar.shaikh@synerzip.com>
2020-12-16 13:20:42 +0530

    #2623 Internal QA issues fixed for core & IM

--- src/library/product/details/stores/d/dermstore/US/variantsExtract.js
+++ src/library/product/details/stores/d/dermstore/US/variantsExtract.js
@@ -8,4 +8,38 @@ module.exports = {
     domain: 'dermstore.com',
     zipcode: '',
   },
+  implementation,
 };
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { variants } = dependencies;
+  await context.evaluate(async (parentInput) => {
+    function addElementToDocument (key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+    try {
+      const colorVariantSelector = document.querySelector('div.colorVariant');
+      const sizeSelector = document.querySelector('ul#sizeOptions');
+      // @ts-ignore
+      if (!(colorVariantSelector || sizeSelector)) {
+        const variantId1 = prod_details_array.prod_id;
+        addElementToDocument('added_variantId1', variantId1);
+        const variantUrl1 = window.location.href;
+        addElementToDocument('added_variantUrl1', variantUrl1);
+      }
+    } catch (error) {
+      console.log('variants are present....');
+    }
+  });
+  return await context.extract(variants, { transform });
+}
