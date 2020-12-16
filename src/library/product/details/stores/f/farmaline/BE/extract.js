f9eed089567b55c437cb3600aa98dff19c2e16cf
Harish <harish.sathavara@kcsitglobal.com>
2020-12-16 18:02:00 +0530

    Intial commit

---
+++ src/library/product/details/stores/f/farmaline/BE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'BE',
+    store: 'farmaline',
+    transform: null,
+    domain: 'farmaline.be',
+    zipcode: '',
+  },
+};
