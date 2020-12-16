ece4c4f0d36c3e38d2107097f283c63f18e49cb1
Nishant Sinha <nishant.s@osmosys.asia>
2020-12-16 22:31:09 +0530

    Increased goto action timeout

--- src/library/navigation/goto/domains/ab/ab.gr.js
+++ src/library/navigation/goto/domains/ab/ab.gr.js
@@ -3,9 +3,19 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'ab.gr',
-    timeout: null,
+    timeout: 60000,
     zipcode: '',
     store: 'ab',
     country: 'GR',
+  },
+   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    console.log('IN GOTO');
+    const timeout = parameters.timeout ? parameters.timeout : 120000;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setFirstRequestTimeout(60000);
+    await context.setUseRelayProxy(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
   },
 };
