78b84e7bac465c234072265482ae79d200ff6451
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 07:49:38 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/s/superdrug/UK/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'superdrug.com',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: null,
+    country: 'UK',
+    store: 'superdrug',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/s/superdrug/UK/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'UK',
+    domain: 'superdrug.com',
+    store: 'superdrug',
+    zipcode: '',
+  },
+};
