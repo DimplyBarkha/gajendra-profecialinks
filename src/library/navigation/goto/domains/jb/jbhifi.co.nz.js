3a59429e3793e6c32591c3c785cda0053080e2c4
unknown <ramraut25@gmail.com>
2020-12-16 15:13:49 +0530

    modified goto to fix timeout issue

--- src/library/navigation/goto/domains/jb/jbhifi.co.nz.js
+++ src/library/navigation/goto/domains/jb/jbhifi.co.nz.js
@@ -9,15 +9,24 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.setAntiFingerprint(false);
-    await context.setLoadAllResources(true);
-    await context.setBlockAds(false);
-    // url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
-    console.log(zipcode);
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-    }
+    // const timeout = parameters.timeout ? parameters.timeout : 10000;
+    // await context.setAntiFingerprint(false);
+    // await context.setLoadAllResources(true);
+    // await context.setBlockAds(false);
+    // // url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+    // await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    // console.log(zipcode);
+    // if (zipcode) {
+    //   await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    // }
+    
+    url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 100000,
+      waitUntil: 'load',
+    });
   },
 };
