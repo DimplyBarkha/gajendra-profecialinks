062546a7d8dce5505e5a3f709d2d46dd5ccd44a6
Your Name <karthik.ravi-odc@partner.import.io>
2020-12-16 17:08:01 +0530

    new

--- src/library/product/details/stores/j/jpg/FR/execute.js
+++ src/library/product/details/stores/j/jpg/FR/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'jpg',
     domain: 'jpg.fr',
     loadedSelector: 'body',
-    noResultsXPath: null,
+    noResultsXPath: '//section[@class="error-pages__container col mb-8"]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/j/jpg/FR/extract.js
+++ src/library/product/search/stores/j/jpg/FR/extract.js
@@ -15,8 +15,32 @@ module.exports = {
   ) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
-    
+    const applyScroll = async function (context) {
     await context.evaluate(async () => {
+      try {
+        // @ts-ignore
+      document.querySelector('button[id=onetrust-accept-btn-handler]').click()
+      await new Promise(r => setTimeout(r, 6000));
+      } catch (error) {
+        
+      }     
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        await stall(1000);
+      }
+      function stall(ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
+  await context.evaluate(async function () {
       function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
