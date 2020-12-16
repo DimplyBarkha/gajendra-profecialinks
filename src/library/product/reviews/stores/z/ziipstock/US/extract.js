651c2c7e9edb2502b70b4967501a1ca9c0a9034b
cshelake <cshelake7@gmail.com>
2020-12-16 16:27:18 +0530

    fix rating

--- src/library/product/reviews/stores/z/ziipstock/US/extract.js
+++ src/library/product/reviews/stores/z/ziipstock/US/extract.js
@@ -1,3 +1,27 @@
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productReviews } = dependencies;
+  await context.evaluate(async function () {
+    const ratings = document.querySelectorAll('.stamped-review-header-starratings');
+    if (ratings) {
+      Array.from(ratings).forEach(rating => {
+        const stars = rating.querySelectorAll('i.stamped-fa');
+        let ratingVal = 0;
+
+        [...stars].forEach(star => {
+          if (star.getAttribute('class').includes('stamped-fa-star')) ++ratingVal;
+        });
+        rating.setAttribute('custom_rating', ratingVal);
+      });
+    }
+  });
+
+  return await context.extract(productReviews);
+}
 
 module.exports = {
   implements: 'product/reviews/extract',
@@ -8,4 +32,8 @@ module.exports = {
     domain: 'ziipstock.com',
     zipcode: "''",
   },
+  implementation,
+ 
 };
+
+
