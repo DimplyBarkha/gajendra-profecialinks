90bf1368c258ee99b2502e8b294486e3751de132
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 09:39:04 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/l/lyreco/FR/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'FR',
+    domain: 'lyreco.com',
+    store: 'lyreco',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/l/lyreco/FR/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'FR',
+    store: 'lyreco',
+    domain: 'lyreco.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
