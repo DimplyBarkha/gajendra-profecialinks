c1eda5fd280b3f87120d6f73920cb89a054575d5
cshelake <cshelake7@gmail.com>
2020-12-16 15:49:46 +0530

    fix brand issue

--- src/library/product/reviews/stores/V/VaporDNA/US/extract.js
+++ src/library/product/reviews/stores/V/VaporDNA/US/extract.js
@@ -8,4 +8,29 @@ module.exports = {
     domain: 'vapordna.com',
     zipcode: "''",
   },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies) => {
+    const { productReviews } = dependencies;
+
+    await context.evaluate(async () => {
+     function addHiddenDiv (elementID, content) {
+        const newDiv = document.createElement('div');
+        newDiv.className = elementID;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }			
+      
+      if (document.querySelector('.product-single__meta')) {
+      		var url = window.location.href;
+      		var brand = null;
+      		brand = url.match(/Logic|Juul/);	
+          addHiddenDiv("vapordna_brand", brand);
+      }
+    });
+
+    return await context.extract(productReviews);
+  },
 };
