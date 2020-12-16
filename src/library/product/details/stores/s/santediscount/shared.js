11893d894fe0c7fffa3358cc6c57cd7553283e74
wasiqe-a <wasiqe.a@osmosys.asia>
2020-12-16 14:51:13 +0530

    Adding fixes found in UAT

--- src/library/product/details/stores/s/santediscount/shared.js
+++ src/library/product/details/stores/s/santediscount/shared.js
@@ -3,27 +3,8 @@
 * @param {ImportIO.Group[]} data
 * @returns {ImportIO.Group[]}
 */
+
 const transform = (data) => {
-  for (const { group } of data) {
-    for (const row of group) {
-      if (row.warnings) {
-        let text = '';
-        row.warnings.forEach(item => {
-          text = text + (text ? ' ' : '') + item.text;
-        });
-        row.warnings = [{ text }];
-      }
-
-      if (row.subCategory.length) {
-        row.subCategory[0].text = row.subCategory[0].text.split('/').join('>');
-      }
-
-      if (row.listPrice.length) {
-        row.listPrice[0].text = row.listPrice[0].text.split(',').join('.');
-      }
-    }
-  }
-
   // Clean up data
   const clean = text => text.toString()
     .replace(/\r\n|\r|\n/g, ' ')
@@ -37,6 +18,30 @@ const transform = (data) => {
     // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.warnings) {
+        let text = '';
+        row.warnings.forEach(item => {
+          text = text + (text ? ' ' : '') + item.text;
+        });
+        row.warnings = [{ text }];
+      }
+
+      if (row.subCategory) {
+        row.subCategory[0].text = row.subCategory[0].text.split('/').join('>');
+      }
+
+      if (row.listPrice) {
+        row.listPrice[0].text = row.listPrice[0].text.split(',').join('.');
+      }
+
+      if (row.description) {
+        row.description[0].text = clean(row.description[0].text).replace(/(.*?)Description\s:\s(.*?)Indications\s:\s(.*)/g, '$2');
+      }
+    }
+  }
 
   data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
     el.text = clean(el.text);
