3a6545113314be41890e4072df51755bd2a6cd32
sujoypal144 <sujoypal144@gmail.com>
2020-12-16 15:08:56 +0530

    fixed the issue for timeout error.

--- src/library/navigation/goto/domains/pa/payngo.co.il.js
+++ src/library/navigation/goto/domains/pa/payngo.co.il.js
@@ -3,9 +3,19 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'payngo.co.il',
-    timeout: 60000,
+    //timeout: 60000,
     country: 'IL',
     store: 'payngo',
     zipcode: '',
+  },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 100000,
+      waitUntil: 'load',
+    });
   },
 };
