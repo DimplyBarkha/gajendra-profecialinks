aae035d2ffa75c2ac61e13f9aada436e5e6691c8
sobha rani <amol.thukrul-od@partner.import.io>
2020-12-16 23:18:09 +0530

    instacart_publix search complete

--- src/library/navigation/auth/domains/in/instacart.com/doLogin.js
+++ src/library/navigation/auth/domains/in/instacart.com/doLogin.js
@@ -3,12 +3,44 @@ module.exports = {
   implements: 'navigation/auth/doLogin',
   parameterValues: {
     domain: 'instacart.com',
-    usernameSelector: null,
-    passwordSelector: null,
-    buttonSelector: null,
-    loggedInSelector: null,
+    usernameSelector: '#nextgen-authenticate\\.all\\.log_in_email',
+    passwordSelector: '#nextgen-authenticate\\.all\\.log_in_password',
+    buttonSelector: '.rmq-a5d52242 div:nth-child(6) button',
+    loggedInSelector: '.css-mtzz8n',
     country: 'US',
     store: 'instacart_publix',
     zipcode: '',
+  },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { username, password } = inputs;
+    const { usernameSelector, passwordSelector, buttonSelector, loggedInSelector } = parameters;
+    // alert("username "+username);
+    await context.click('button[type="button"]');
+    // console.log("button clicked");
+    await context.waitForNavigation({ timeout: 50000, waitUntil: 'load' });
+    var userName = username.value;
+    console.log('Username' + username);
+    // var userName = (<HTMLInputElement>document.getElementById('#nextgen-authenticate.all.log_in_email')).value = 'panchal.drmita85@yahoo.com';
+    console.log('userName :' + userName);
+    await context.setInputValue(usernameSelector, username);
+    console.log('checked');
+    // await context.click(buttonSelector);
+    // if (loggedInSelector) {
+    //   await context.waitForFunction(function (sel) {
+    //     return Boolean(document.querySelector(sel));
+    //   }, { timeout: 10000 }, loggedInSelector);
+    // }
+    await context.setInputValue(passwordSelector, password);
+    await context.waitForNavigation({ timeout: 200000, waitUntil: 'load' });
+    await context.click('button[type="submit"]');
+    await context.waitForNavigation({ timeout: 50000, waitUntil: 'load' });
+    await context.evaluate(function () {
+      document.forms[0].submit();
+    });
   },
 };
--- src/library/navigation/auth/domains/in/instacart.com/gotoLogin.js
+++ src/library/navigation/auth/domains/in/instacart.com/gotoLogin.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/auth/gotoLogin',
   parameterValues: {
     domain: 'instacart.com',
-    loginPage: null,
+    loginPage: 'https://www.instacart.com/',
     country: 'US',
     store: 'instacart_publix',
     zipcode: '',
