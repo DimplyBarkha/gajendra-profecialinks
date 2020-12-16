888290fc26be489eed9967dc16ad7a0125aba247
mania307 <maniakalnaa@gmail.com>
2020-12-16 10:07:38 +0100

    vikingdirect NL menu-only extractor

--- src/library/tracker/categories/stores/v/vikingdirect/NL/extract.js
+++ src/library/tracker/categories/stores/v/vikingdirect/NL/extract.js
@@ -1,4 +1,33 @@
+async function implementation(inputs, parameters, context, dependencies) {
+  const { productMenu } = dependencies;
+  await context.evaluate(async function () {
+    function addElementToDocument(key) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
 
+    function addChildDiv(parentKey, key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.getElementById(parentKey).appendChild(catElement);
+    }
+
+    const allCategories = document.evaluate('//ul[contains(@class,"site-navigation__list--level-1")]//li[contains(@class, "site-navigation__list-item")]/a|//ul[contains(@class,"site-navigation__list--level-0")]/li[contains(@class, "site-navigation__list-item--has-children")]/a',
+      document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    let categoryDivName;
+    for (let i = 0; i < allCategories.snapshotLength; i++) {
+      categoryDivName = 'added_category_' + i;
+      addElementToDocument(categoryDivName);
+      addChildDiv(categoryDivName, 'category_name', allCategories.snapshotItem(i).textContent);
+      addChildDiv(categoryDivName, 'category_url', allCategories.snapshotItem(i).href);
+    }
+  });
+  return await context.extract(productMenu);
+}
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -7,4 +36,8 @@ module.exports = {
     store: 'vikingdirect',
     zipcode: '',
   },
+  dependencies: {
+    productMenu: 'extraction:tracker/categories/stores/${store[0:1]}/${store}/${country}/extract',
+  },
+  implementation,
 };
