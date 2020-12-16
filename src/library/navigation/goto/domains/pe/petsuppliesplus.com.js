3182c64b22a428fcc2bf4c202bc6e9088475f295
wasiqe-a <wasiqe.a@osmosys.asia>
2020-12-16 21:11:05 +0530

    Fixed the issues wiht setting store

--- src/library/navigation/goto/domains/pe/petsuppliesplus.com.js
+++ src/library/navigation/goto/domains/pe/petsuppliesplus.com.js
@@ -8,9 +8,10 @@ module.exports = {
     store: 'petsuppliesplus',
     zipcode: '',
   },
-  implementation: async ({ url }, parameters, context, dependencies) => {
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
     const URL = `${url}#[!opt!]{"block_ads":true,"first_request_timeout":30,"load_timeout":30,"load_all_resources":true}[/!opt!]`;
     await context.goto(URL, { timeout: 50000, waitUntil: 'load' });
     await context.waitForNavigation({ timeout: 50000, waitUntil: 'networkidle0' });
+    await dependencies.setZipCode({ url, zipcode: '60440-2380' });
   },
 };
--- src/library/navigation/goto/domains/pe/setZipCode.petsuppliesplus.com.js
+++ src/library/navigation/goto/domains/pe/setZipCode.petsuppliesplus.com.js
@@ -1,3 +1,57 @@
+async function implementation (
+  inputs, parameters, context, dependencies,
+) {
+  const { zipcode } = inputs;
+
+  await context.evaluate(async function () {
+    const suggetionModel = document.querySelector('h5.suggested-store-close');
+    if (suggetionModel) {
+      suggetionModel.click();
+    }
+  });
+
+  const getCurrentZip = async () => {
+    return await context.evaluate(async function () {
+      const element = document.querySelector('a#headerDropMenuPhoneNumber');
+      if (element) {
+        return element.getAttribute('href').split(':')[1];
+      }
+      return null;
+    });
+  };
+
+  const findButtonWithStoreSelect = async () => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+    await context.evaluate(function () {
+      const mystore = document.querySelector('button.shopButton');
+      if (mystore) mystore.click();
+    });
+  };
+
+  const changeZip = async (wantedZip) => {
+    await context.click('button.change-store-header');
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+
+    await context.setInputValue('input#SearchBox', wantedZip);
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+
+    await context.click('span#SearchStoreSubmit');
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+    await findButtonWithStoreSelect();
+    await new Promise((resolve, reject) => setTimeout(resolve, 8000));
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+  };
+
+  const currentZip = await getCurrentZip();
+  console.log(`Want zip: ${zipcode}, got zip: ${currentZip}`);
+
+  if (currentZip !== zipcode) {
+    console.log('Trying to change zip');
+    await changeZip(zipcode);
+  }
+
+  await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+}
 
 module.exports = {
   implements: 'navigation/goto/setZipCode',
@@ -7,4 +61,5 @@ module.exports = {
     store: 'petsuppliesplus',
     zipcode: '',
   },
+  implementation,
 };
