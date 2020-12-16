1481ceae35c8037d05bcb2e8aa77dace60aaaee3
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 09:15:37 +0100

    created a template for the menu only extractor

---
+++ src/library/navigation/goto/domains/sn/snipes.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'snipes.com',
+    timeout: null,
+    country: 'DE',
+    store: 'snipes',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/s/snipes/DE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'DE',
+    store: 'snipes',
+    domain: 'snipes.com',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
