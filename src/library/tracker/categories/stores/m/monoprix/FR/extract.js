694bf6c431278d868f2b4d844ec5fbef14b299ef
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 07:33:35 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/m/monoprix/FR/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'FR',
+    domain: 'monoprix.fr',
+    store: 'monoprix',
+    zipcode: '',
+  },
+};
