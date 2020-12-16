a8673ecc1f9771adbe9a63726a9bb8600d90d4ac
klskoczylas <klaudia.skoczylas1@wp.pl>
2020-12-16 08:29:40 +0100

    created a template for the menu only extractor

---
+++ src/library/tracker/categories/stores/p/promofarma/ES/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'ES',
+    domain: 'promofarma.com',
+    store: 'promofarma',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/p/promofarma/ES/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'ES',
+    domain: 'promofarma.com',
+    store: 'promofarma',
+    zipcode: '',
+  },
+};
