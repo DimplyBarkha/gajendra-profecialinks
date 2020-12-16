647450b06247a686f476b3d0f37b80cbe641f6b1
romancg <gromantsg@gmail.com>
2020-12-16 19:19:25 +0200

    #822 fix remote data scraping

--- src/library/product/details/stores/e/euronics/IT/createUrl.js
+++ src/library/product/details/stores/e/euronics/IT/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'euronics.it',
     prefix: null,
-    url: 'https://www.euronics.it/{id}/',
+    url: 'https://www.euronics.it/{id}/#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":true,"cookies":[]}[/!opt!]',
     country: 'IT',
     store: 'euronics',
     zipcode: '',
