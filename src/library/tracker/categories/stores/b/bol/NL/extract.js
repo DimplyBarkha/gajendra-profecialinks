362de10fc8c05b9b3a923b46024f223a7f5c0d49
Michał Wieczorek <michal126@hotmail.com>
2020-12-17 13:02:31 +0100

    finished menu extractor

--- src/library/tracker/categories/stores/b/bol/NL/extract.js
+++ src/library/tracker/categories/stores/b/bol/NL/extract.js
@@ -1,3 +1,32 @@
+async function implementation (inputs, parameters, context, dependencies) {
+  const { productMenu } = dependencies;
+
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content, parentDiv = null) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      if (parentDiv) {
+        parentDiv.appendChild(newDiv);
+      } else {
+        document.body.appendChild(newDiv);
+      }
+      return newDiv;
+    }
+
+    const categories = document.querySelectorAll('a[data-test="navigation-list-item"]');
+    categories.forEach(category => {
+      if (!category.getAttribute('href').startsWith('https')) {
+        const newCatDiv = addHiddenDiv('categories', '');
+        addHiddenDiv('category', category.textContent, newCatDiv);
+        addHiddenDiv('categoryUrl', category.getAttribute('href'), newCatDiv);
+      }
+    });
+  });
+
+  return await context.extract(productMenu);
+}
 
 module.exports = {
   implements: 'tracker/categories/extract',
@@ -7,4 +36,5 @@ module.exports = {
     store: 'bol',
     zipcode: '',
   },
+  implementation,
 };
