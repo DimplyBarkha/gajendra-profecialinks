0ca42163804c3a397e8bad7112442e22f523244c
sahoojudhi <judhishtor@profecialinks.com>
2020-12-18 01:22:23 +0530

    #3265 Development done, going for deployement

--- src/library/product/details/stores/f/falabella/CL/format.js
+++ src/library/product/details/stores/f/falabella/CL/format.js
@@ -24,60 +24,107 @@ const transform = (data) => {
     };
     for (const { group } of data) {
       for (let row of group) {
-        if (row.description) {
-          row.description.forEach(item => {
-            item.text = item.text.replace(/(\s?\n)+/g, ' ').trim();
-          });
-        }        
-        if (row.specifications) {
-          let info = [];          
-          row.specifications.forEach(item => {
-            info.push(item.text.replace(/(\s*\n\s*)+/g, ' : ').trim());            
-          });          
-          row.specifications = [{'text':info.join(' || '),'xpath':row.specifications[0].xpath}];          
-        }        
-        if (row.additionalDescBulletInfo) {
-            row.additionalDescBulletInfo.forEach(item => {
-              item.text = item.text.replace(/(\s*\n\s*)+/g, ' || ').trim();
-            });
-        }
-        if (row.listPrice) {
-            row.listPrice.forEach(item => {
-              item.text = item.text.replace(/(\s*\(Precio\s+final\)\s*)+/g, '').trim();
-            });
-        }
-        if (row.videos) {
-          row.videos.forEach(item => {
-            if(item.text == ''){
-              delete row.videos;
-            }
-          });
-        }
         if (row.aggregateRatingText) {
           row.aggregateRatingText.forEach(item => {
             item.text = item.text.replace(/(\s?\n)+/g, ' | ').trim();
           });
         }
-        if (row.shippingInfo) {
-          row.shippingInfo.forEach(item => {
-            item.text = item.text.replace(/(\s?\n)+/g, ' | ').trim();
-          });
+        if(row.price) {
+          row.price.forEach(item=>{
+            var str = item.text;
+            str = str.split(".");
+            if(str.length > 2) {
+              str = str.join(".");
+              str = str.replace(".","");
+              str = str.split(".");
+              str = str.join(",");
+            }
+            else {
+              str = str.join(",");
+            }
+            item.text = str;
+          })
         }
-        if (row.availabilityText) {
-            row.availabilityText.forEach(item => {
-                if (item.text == 'Out Of Stock'){
-                    delete row.quantity;
-                }
+        if(row.listPrice) {
+          row.listPrice.forEach(item=>{
+            var str = item.text;
+            str = str.split(".");
+            if(str.length > 2) {
+              str = str.join(".");
+              str = str.replace(".","");
+              str = str.split(".");
+              str = str.join(",");
+            }
+            else {
+              str = str.join(",");
+            }
+            item.text = str;
+          })
+        }
+        if(row.aggregateRating) {
+          row.aggregateRating.forEach(item=>{
+            item.text = item.text.replace(".",",");
+          })
+        }
+        if(row.specifications) {
+          let count = 0, array = [], tmp;
+          row.specifications.forEach(item=>{
+            if(count == 0) {
+              tmp = item.text;
+              count = 1;
+            }
+            else if(count == 1) {
+              var str = item.text.replace(".",",");
+              tmp = tmp+" : "+str;
+              array.push(tmp);
+              tmp = '';
+              count = 0;
+            }
+          })
+          row.specifications=[{"text":array.join(' || ')}];
+        }
+        if(row.description) {
+          var array = [];
+          row.description.forEach(item=>{
+            item.text = item.text.replace(".",",");
+            array.push(item.text);
+          })
+          for(var i = 0; i < array.length; i++) {
+            if(array[i] == ':') {
+              array[i+1] = array[i+1]+" ||";
+            }
+          }
+          row.description=[{"text":"|| "+array.join(' ')}];
+        }
+        if(row.manufacturerDescription) {
+          var array = [];
+          row.manufacturerDescription.forEach(item=>{
+            array.push(item.text);
+          })
+          row.manufacturerDescription = [{"text":array.join(" ")}];
+        }
+        if(row.variants){
+          var array = [];
+          row.variants.forEach(item=>{
+            let tmp=JSON.parse(item.text);
+            let variantObj = tmp.props.pageProps.productData.variants;
+            variantObj.forEach(item => {
+              array.push(item.id);
             });
+          });
+          row.variants = [{"text":array.join(" | ")}];
         }
-        if (row.promotion) {
-            let info = [];          
-            row.promotion.forEach(item => {
-              info.push(item.text);            
-            });          
-            row.promotion = [{'text':info.join(' | '),'xpath':row.promotion[0].xpath}];          
-        }        
-
+        if(row.variantCount){
+          var array = [];
+          row.variantCount.forEach(item=>{
+            let tmp=JSON.parse(item.text);
+            let variantObj = tmp.props.pageProps.productData.variants;
+            variantObj.forEach(item => {
+              array.push(item.id);
+            });
+          });
+          row.variantCount = [{"text":array.length}];
+        }
       }
     }
     return cleanUp(data);
