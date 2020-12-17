f4b2478b5c9c9e2b39dbc0558e3251caa6d75687
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2020-12-18 00:36:21 +0530

    #3263 Created extractor robot

---
+++ src/library/navigation/goto/domains/fn/fnac.es.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'fnac.es',
+    timeout: null,
+    country: 'ES',
+    store: 'fnac',
+    zipcode: "''",
+  },
+};
