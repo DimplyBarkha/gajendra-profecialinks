7d13526004a716b4b104a17f46dee66013e36da3
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-17 11:44:07 +0530

    luluwebstore

--- src/library/product/search/stores/l/luluwebstore/NM/paginate.js
+++ src/library/product/search/stores/l/luluwebstore/NM/paginate.js
@@ -8,9 +8,9 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ul[class="product__listing product__grid col-xs-12"] div[class="col-lg-3 col-md-4 col-sm-6 col-xs-6 plp-product-div js-owl-carousel-reference "]',
-    openSearchDefinition: {
-      template: 'https://www.luluhypermarket.com/en-ae/search?q={searchTerms}%3Arelevance&page={page}',
-      },
+    // openSearchDefinition: {
+    //   template: 'https://www.luluhypermarket.com/en-ae/search?q={searchTerms}%3Arelevance&page={page}',
+    //   },
     domain: 'luluhypermarket.com',
     zipcode: '',
   },
