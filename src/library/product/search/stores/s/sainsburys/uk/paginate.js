203171d951881313ad4c68f5cf1561ce590ea725
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-17 13:42:26 +0530

    sainsburys

--- src/library/product/search/stores/s/sainsburys/uk/paginate.js
+++ src/library/product/search/stores/s/sainsburys/uk/paginate.js
@@ -8,9 +8,9 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'section[class="ln-o-section ln-o-section"] ul[class="ln-o-grid ln-o-grid--matrix ln-o-grid--equal-height"] li[class="pt-grid-item ln-o-grid__item ln-u-6/12@xs ln-u-3/12@md ln-u-2/12@xl"]',
-    openSearchDefinition: {
-      template: 'https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]={searchTerms}&pageNumber={page}',
-      },
+    // openSearchDefinition: {
+    //   template: 'https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]={searchTerms}&pageNumber={page}',
+    //   },
     domain: 'sainsburys.co.uk',
     noResultsXPath: null,
     zipcode: '',
