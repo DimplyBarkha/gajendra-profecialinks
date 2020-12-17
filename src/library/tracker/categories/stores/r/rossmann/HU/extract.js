a1ed55a3c1a2162c75a4e9e7dc4818944d2d0e19
amstr166 <adamgrabowski166@gmail.com>
2020-12-17 12:44:50 +0100

    Rossmann HU menu extractor

--- src/library/tracker/categories/stores/r/rossmann/HU/extract.js
+++ src/library/tracker/categories/stores/r/rossmann/HU/extract.js
@@ -3,8 +3,46 @@ module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
     country: 'HU',
-    domain: 'rossmann.hu',
+    domain: 'shop.rossmann.hu',
     store: 'rossmann',
     zipcode: "''",
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { productMenu } = dependencies;
+
+    await context.evaluate(async () => {
+      function addHiddenDiv (id, content, parentDiv = null) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        if (parentDiv) {
+          parentDiv.appendChild(newDiv);
+        } else {
+          document.body.appendChild(newDiv);
+        }
+        return newDiv;
+      }
+      // categories
+      const categories = document.querySelectorAll('li[class="level0"]');
+
+      categories.forEach((category) => {
+        const categoryName = category.querySelector('a').textContent;
+        const subCategories = category.querySelectorAll('div[class="submenu level0"]');
+
+        subCategories.forEach((subCategory) => {
+          const subCategoryName = subCategory.querySelector('li[class="level1"] > a').textContent;
+          const elements = subCategory.querySelectorAll('a');
+
+          elements.forEach((element) => {
+            const newDiv = addHiddenDiv('categories');
+            const names = [categoryName, subCategoryName, element.textContent];
+            names.forEach(name => addHiddenDiv('category', name, newDiv));
+            addHiddenDiv('categoryUrl', element.getAttribute('href'), newDiv);
+          });
+        });
+      });
+    });
+    return await context.extract(productMenu);
   },
 };
