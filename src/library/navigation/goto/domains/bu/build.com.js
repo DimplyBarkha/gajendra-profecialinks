4593a9661abe81ba849a94546c73c73c722cbdbf
Shreyam <shreyam.g@osmosys.asia>
2020-12-17 20:35:59 +0530

    updated field for salsify

--- src/library/navigation/goto/domains/bu/build.com.js
+++ src/library/navigation/goto/domains/bu/build.com.js
@@ -3,9 +3,16 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'build.com',
-    timeout: 9999999,
+    timeout: 50000,
     country: 'US',
     store: 'build',
     zipcode: '',
+  },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"first_request_timeout":50000,"force200":true}[/!opt!]`;
+    context.setBlockAds(false);
+    context.setLoadAllResources(true);
+    context.setAntiFingerprint(false);
+    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true });
   },
 };
