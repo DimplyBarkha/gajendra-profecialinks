a2f4c645d9e0a61bdc0b3b769b625b635e70b953
unknown <shalini.kumari-odc@partner.import.io>
2020-12-17 20:50:55 +0530

    mvideo

--- src/library/product/search/stores/m/mvideo/RU/execute.js
+++ src/library/product/search/stores/m/mvideo/RU/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'RU',
     store: 'mvideo',
     domain: 'mvideo.ru',
-    url: 'https://www.mvideo.ru/product-list-page-cls?q={searchTerms}',
+    url: 'https://www.mvideo.ru/product-list-page-cls?q=Bosch+пылесос&region_id=1&limit=12&category_id=cat2_cis_0000000639',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
