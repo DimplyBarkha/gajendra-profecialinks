3ea0bdf48771aea2d15b4038a4a7accbdfc0de52
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-12-17 17:04:12 +0200

    search amazon

--- src/library/product/search/stores/a/amazon/SA/extract.js
+++ src/library/product/search/stores/a/amazon/SA/extract.js
@@ -1,10 +1,45 @@
+const { transform } = require('./shared');
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'SA',
     store: 'amazon',
-    transform: null,
+    transform,
     domain: 'amazon.sa',
     zipcode: '',
+  },
+  implementation: async (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.waitForSelector('.rush-component.s-latency-cf-section');
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop !== 20000) {
+          await stall(500);
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 20000) {
+            await stall(5000);
+            break;
+          }
+        }
+        function stall (ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await applyScroll(context);
+    return await context.extract(productDetails, { transform });
   },
 };
