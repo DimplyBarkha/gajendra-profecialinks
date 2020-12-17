7fb6d07ca18ed029fa604c985a89d5282d30da60
Vivek Gondil <gondilvivek@gmail.com>
2020-12-17 16:20:26 +0530

    Added Fix

--- src/library/product/details/stores/c/carrefour/BR/extract.js
+++ src/library/product/details/stores/c/carrefour/BR/extract.js
@@ -8,4 +8,30 @@ module.exports = {
     domain: 'carrefour.com.br',
     zipcode: '',
   },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    await context.evaluate(async () => {
+      const a = document.querySelectorAll('script');
+      let productId;
+      a.forEach((b) => {
+        const number = b.textContent.toString().match(/"params":{"id":"(\d+)"/g);
+        if (number) {
+          productId = number[0].match(/\d+/g)[0];
+        }
+      });
+
+      fetch('https://www.carrefour.com.br/api/catalog_system/pub/products/search?fq=productId:' + productId)
+        .then(response => response.json())
+        .then(data => { document.body.setAttribute('desc', data[0].description); });
+    });
+
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+  },
+
 };
