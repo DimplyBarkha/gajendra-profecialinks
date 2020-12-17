019d828bc8d40ab44f609b6595665d7b6129b48b
osmosysprakhar <prakhar.s@osmosys.asia>
2020-12-18 01:14:39 +0530

    fixed brandText and category

--- src/library/product/details/stores/i/innovasport/MX/extract.js
+++ src/library/product/details/stores/i/innovasport/MX/extract.js
@@ -9,4 +9,16 @@ module.exports = {
     domain: 'innovasport.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      const dataLayer = document.evaluate(`//script[@type="application/ld+json"][contains(.,'brand')]`,document).iterateNext();
+      if(dataLayer){
+        const url = dataLayer.textContent.replace(/(.*)(brand":{"@type":".*","url":")(.*)?("})/,"$3");
+        const urlSplit = url.split('/');
+        var category =  `${urlSplit[3]} , ${urlSplit[4]} , ${urlSplit[5]} ,  ${urlSplit[6]}` ;     
+        document.body.setAttribute('category',category);
+      }
+    });
+  return await context.extract(productDetails);
+  }
 };
