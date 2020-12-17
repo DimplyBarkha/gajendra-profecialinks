209d87bddb45379a6ed18e5f0e2b920ea8abc9d1
sahoojudhi <judhishtor@profecialinks.com>
2020-12-17 20:26:47 +0530

    Core bugfix - removed extra space and updated RPC code in extract js

--- src/library/product/details/stores/p/petcircle/AU/extract.js
+++ src/library/product/details/stores/p/petcircle/AU/extract.js
@@ -1,4 +1,32 @@
 const {transform} = require('../AU/format')
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  console.log("inputs:: ", inputs);
+  const { url, id } = inputs;
+  console.log("parameters:: ", parameters);
+  if (id) {
+    await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    await context.waitForXPath('//h3[@class="product-name"]/a/@href');
+    await context.waitForSelector('h3.product-name a');
+    console.log('everything fine !!!');
+    await context.evaluate(() => {
+      let firstItem = document.querySelector('h3.product-name a');
+      if (firstItem instanceof HTMLElement) {
+        console.log(firstItem)
+        firstItem.click();
+      }
+    });
+  }
+  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,22 +36,23 @@ module.exports = {
     domain: 'petcircle.com.au',
     zipcode: '',
   },
-  implementation: async (inputs, parameters, context, dependencies) => {
-    const { url, id } = inputs;
-    const timeout = 60000;
-    if(id){
-      const redirectProductDetails = async () => {
-        try {
-          await context.waitForSelector('h3.product-name>a', { timeout });
-        } catch (err) {
-          console.log('Product with RPC didn\'t load');
-        }
-      };
-      await redirectProductDetails();
-      await context.clickAndWaitForNavigation('h3.product-name>a', { timeout, waitUntil: 'load' });
-    }
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    await context.extract(productDetails, { transform });
-  },
+  implementation,
+  // implementation: async (inputs, parameters, context, dependencies) => {
+  //   const { url, id } = inputs;
+  //   const timeout = 60000;
+  //   if(id){
+  //     const redirectProductDetails = async () => {
+  //       try {
+  //         await context.waitForSelector('h3.product-name>a', { timeout });
+  //       } catch (err) {
+  //         console.log('Product with RPC didn\'t load');
+  //       }
+  //     };
+  //     await redirectProductDetails();
+  //     await context.clickAndWaitForNavigation('h3.product-name>a', { timeout, waitUntil: 'load' });
+  //   }
+  //   const { transform } = parameters;
+  //   const { productDetails } = dependencies;
+  //   await context.extract(productDetails, { transform });
+  // },
 };
