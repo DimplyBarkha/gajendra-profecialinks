e65f8d4f72cf9171d6f35abef74ee9a6ca911afe
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-12-17 20:24:38 +0530

    extract.yaml file updated

--- src/library/product/details/stores/i/instacart_costco/US/extract.js
+++ src/library/product/details/stores/i/instacart_costco/US/extract.js
@@ -21,6 +21,22 @@ module.exports = {
         newDiv.style.display = "none";
         document.body.appendChild(newDiv);
       }
+      let scrollTop = 500;
+      function stall(ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+      while (true) {
+        window.scroll(0, scrollTop);
+        stall(1000);
+        scrollTop += 500;
+        if (scrollTop === 10000) {
+          break;
+        }
+      }
       if (window.location.href != null) {
         var url = window.location.href;
         if (url.includes("item_")) {
