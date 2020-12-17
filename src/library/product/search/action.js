fbbbc592fedd8bd6f9fd988e23f008b0cf3af981
srishivams <srishivams97@gmail.com>
2020-12-17 17:14:36 +0530

    increased results threshold for search extractor

--- src/library/product/search/action.js
+++ src/library/product/search/action.js
@@ -46,10 +46,10 @@ module.exports = {
     extract: 'action:product/search/extract',
   },
   path: './search/stores/${store[0:1]}/${store}/${country}/search',
-  implementation: async ({ keywords, Keywords, Brands, results = 150 }, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
+  implementation: async ({ keywords, Keywords, Brands, results = 300 }, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
     // TODO: consider moving this to a reusable function
 
-    results = (results) ? results : defaultResults;
+    results = (results) || defaultResults;
     console.log('No of results were returned' + results);
     const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
 
