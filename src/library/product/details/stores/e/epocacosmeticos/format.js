3cfb39f4b59f8b8733cf1b2088e53912fef56711
Pashya <pashya@profecialinks.com>
2020-12-17 13:23:54 +0530

    Fixes

--- src/library/product/details/stores/e/epocacosmeticos/format.js
+++ src/library/product/details/stores/e/epocacosmeticos/format.js
@@ -3,7 +3,19 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
   for (const { group } of data) {
     for (const row of group) {
       if (row.category) {
@@ -23,25 +35,28 @@ const transform = (data) => {
       }
       if (row.description) {
         const descriptionArr = row.description.map((item) => {
-          return typeof (item.text) === 'string' ? item.text.replace(/\n/g, '') : '|';
+          return clean(item.text);
         });
-        row.description = [{ text: descriptionArr.join('|'), xpath: row.description[0].xpath }];
+        row.description = [{ text: descriptionArr.join('||'), xpath: row.description[0].xpath }];
       }
       if (row.alternateImages) {
         row.alternateImages.shift();
       }
       if (row.specifications) {
         const specificationsArr = row.specifications.map((item) => {
-          return typeof (item.text) === 'string' ? item.text.replace(/www.youtube.com\/embed\/[\w\-/_]*/g, '').replace('Video', '').replace('https://', '').replace(/\n \n \n \n \n \n \n/g, '|').replace(/\n \n \n \n/g, '|').replace(/\n \n/g, ':').replace(/\n/g, ' ').replace('Concentração Concentração', 'Concentração').replace('Imagem Imagem', '').replace('Gênero Gênero', 'Gênero') : '|';
+          return clean(typeof (item.text) === 'string' ? item.text.replace(/www.youtube.com\/embed\/[\w\-/_]*/g, '').replace('Video', '').replace('https://', '').replace(/\n \n \n \n \n \n \n/g, '||').replace(/\n \n \n \n/g, '|').replace(/\n \n/g, ':').replace(/\n/g, ' ').replace('Concentração Concentração', 'Concentração').replace('Imagem Imagem', '').replace('Gênero Gênero', 'Gênero') : '||');
         });
         row.specifications = [{ text: specificationsArr.join('|'), xpath: row.specifications[0].xpath }];
       }
       if (row.manufacturerDescription) {
         const manufacturerDescriptionArr = row.manufacturerDescription.map((item) => {
-          return typeof (item.text) === 'string' ? item.text.replace(/\n \n \n \n \n \n \n/g, ' | ').replace(/\n \n \n \n/g, '|').replace(/\n \n/g, ':').replace(/\n/g, ' ') : '|';
+          return clean(typeof (item.text) === 'string' ? item.text.replace(/\n \n \n \n \n \n \n/g, ' || ').replace(/\n \n \n \n/g, '||').replace(/\n \n/g, ':').replace(/\n/g, ' ') : '||');
         });
-        row.manufacturerDescription = [{ text: manufacturerDescriptionArr.join('|'), xpath: row.manufacturerDescription[0].xpath }];
+        row.manufacturerDescription = [{ text: manufacturerDescriptionArr.join('||'), xpath: row.manufacturerDescription[0].xpath }];
       }
+      // if (row.additionalDescBulletInfo && row.additionalDescBulletInfo[0] && row.additionalDescBulletInfo[0].text.length > 1) {
+      //   row.additionalDescBulletInfo[0].text = row.additionalDescBulletInfo[0].text.startsWith(' || ') ? row.additionalDescBulletInfo[0].text : ' || ' + row.additionalDescBulletInfo[0].text;
+      // }
       if (row.manufacturerImages) {
         const manufacturerImagesArr = row.manufacturerImages.map((item) => {
           return item.text;
@@ -51,10 +66,11 @@ const transform = (data) => {
       if (row.descriptionBullets) {
         row.descriptionBullets = [{ text: row.descriptionBullets.length, xpath: row.descriptionBullets[0].xpath }];
       }
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
     }
   }
-
   return data;
 };
-
 module.exports = { transform };
