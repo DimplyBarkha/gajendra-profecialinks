aeee5b187a002805745e12811acaa5c4a77a8e55
Rafał Perkowski <rafalpero1@gmail.com>
2020-12-17 15:26:58 +0100

    Built extractor and ran tests

--- src/library/tracker/categories/stores/i/idealclean/DE/extract.js
+++ src/library/tracker/categories/stores/i/idealclean/DE/extract.js
@@ -7,4 +7,10 @@ module.exports = {
     store: 'idealclean',
     zipcode: '',
   },
+  dependencies: {
+    productMenu: 'extraction:tracker/categories/stores/${store[0:1]}/${store}/${country}/extract',
+  },
+  implementation: async (inputs, properties, context, { productMenu }) => {
+    await context.extract(productMenu);
+  },
 };
