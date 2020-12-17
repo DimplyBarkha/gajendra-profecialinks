0ef2dad6d131acc789ae981643f71c79b03c65c5
Michał Wieczorek <michal126@hotmail.com>
2020-12-17 14:34:33 +0100

    finished medikamenteperklick menu only

--- src/library/tracker/categories/stores/m/medikamente-per-klick/DE/extract.js
+++ src/library/tracker/categories/stores/m/medikamente-per-klick/DE/extract.js
@@ -1,4 +1,32 @@
+async function implementation (inputs, parameters, context, dependencies) {
+  const { productMenu } = dependencies;
 
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content, parentDiv = null) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      if (parentDiv) {
+        parentDiv.appendChild(newDiv);
+      } else {
+        document.body.appendChild(newDiv);
+      }
+      return newDiv;
+    }
+
+    // categories
+    const mainCategories = document.querySelectorAll('div[class="smallBoxCategories"] li[class="submenu01"]');
+    mainCategories.forEach((categoryNode, index) => {
+      const category = categoryNode.querySelector('a');
+      const newCatDiv = addHiddenDiv('categories', '');
+      addHiddenDiv('category', category.textContent, newCatDiv);
+      addHiddenDiv('categoryUrl', category.getAttribute('href'), newCatDiv);
+    });
+  });
+
+  return await context.extract(productMenu);
+}
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -7,4 +35,5 @@ module.exports = {
     store: 'medikamente-per-klick',
     zipcode: '',
   },
+  implementation,
 };
