7cc990a95812607e542a9f02c600a79f4ae3ceed
pkujawa <p.kujawa2@op.pl>
2020-12-17 22:07:27 +0100

    #3131. Menu extractor

--- src/library/navigation/goto/domains/pa/paodeacucar.com.js
+++ src/library/navigation/goto/domains/pa/paodeacucar.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'paodeacucar.com',
-    timeout: null,
+    timeout: 50000,
     country: 'BR',
     store: 'paodeacucar',
     zipcode: '',
--- src/library/tracker/categories/stores/p/paodeacucar/BR/extract.js
+++ src/library/tracker/categories/stores/p/paodeacucar/BR/extract.js
@@ -1,4 +1,35 @@
 
+async function implementation(inputs, parameters, context, dependencies) {
+  const { productMenu } = dependencies;
+
+  await context.evaluate(async function () {
+    function addHiddenDiv(id, content, parentDiv = null) {
+      const newDiv = document.createElement("div");
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = "none";
+      if (parentDiv) {
+        parentDiv.appendChild(newDiv);
+      } else {
+        document.body.appendChild(newDiv);
+      }
+      return newDiv;
+    }
+
+    const mainCategories = document.querySelectorAll(
+      ".category-menustyles__Section-sc-1f1zulv-7.gKUKvM:nth-of-type(4) div:not(:first-of-type)"
+    );
+    mainCategories.forEach((category) => {
+      const mainCategoryName = category.textContent;
+      const fixedCategoryName = mainCategoryName.toLowerCase().replaceAll(' ', '-').normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      const categoryUrl = 'https://www.paodeacucar.com/categoria/' + fixedCategoryName;
+      const newDiv = addHiddenDiv("categories", "");
+      addHiddenDiv("category", mainCategoryName, newDiv);
+      addHiddenDiv("categoryUrl", categoryUrl, newDiv);
+    });
+  });
+  return await context.extract(productMenu);
+}
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -7,4 +38,5 @@ module.exports = {
     store: 'paodeacucar',
     zipcode: '',
   },
+  implementation,
 };
