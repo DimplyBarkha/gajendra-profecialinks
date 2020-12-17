63be1ade2080bc7fa602b6031bcc5a4d6f873de1
Piotr Wieleba <wieleba.piotr@gmail.com>
2020-12-17 11:42:18 +0100

    - Finish working on the Lentillesmoinscheres menu extractor - Generate tests

--- src/library/tracker/categories/stores/l/lentillesmoinscheres/FR/extract.js
+++ src/library/tracker/categories/stores/l/lentillesmoinscheres/FR/extract.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -6,5 +5,34 @@ module.exports = {
     domain: 'lentillesmoinscheres.com',
     store: 'lentillesmoinscheres',
     zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, { productMenu }) => {
+    await context.evaluate(async () => {
+      const addedUrlsList = document.createElement('ol');
+      addedUrlsList.id = 'added_urls_list';
+      addedUrlsList.style.display = 'none';
+      document.body.appendChild(addedUrlsList);
+
+      const allMenuUrls = document.evaluate(
+        '//div[@id="responsiveMenuCategs"]/div/ul//li[not(contains(@class, "visible-xs")) and not(contains(@class, "hidden"))]/a[@href and @title]',
+        document,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null,
+      );
+
+      const addedUrlsArr = [];
+      for (let i = 0; i < allMenuUrls.snapshotLength; i++) {
+        const elem = allMenuUrls.snapshotItem(i);
+        if (!addedUrlsArr.includes(elem.href)) {
+          addedUrlsArr.push(elem.href);
+          const listItem = document.createElement('li');
+          listItem.setAttribute('category', elem.textContent || elem.getAttribute('title'));
+          listItem.setAttribute('categoryUrl', elem.href);
+          addedUrlsList.appendChild(listItem);
+        }
+      }
+    });
+    await context.extract(productMenu);
   },
 };
