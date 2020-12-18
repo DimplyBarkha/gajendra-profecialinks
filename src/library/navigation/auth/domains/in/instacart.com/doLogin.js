27c8d8cd078c921e90b5bdc602225c9820171006
Raghav <user.email>
2020-12-18 18:05:25 +0530

    initial commit for auth extractor action

---
+++ src/library/navigation/auth/domains/in/instacart.com/doLogin.js
@@ -0,0 +1,39 @@
+async function implementation(inputs, parameters, context, dependencies) {
+  const { username, password } = inputs;
+  const {
+    usernameSelector,
+    passwordSelector,
+    buttonSelector,
+    loggedInSelector,
+  } = parameters;
+  if (context.click('button[type="button"]')) {
+    await new Promise((resolve, reject) => setTimeout(resolve, 8000));
+    await context.waitForNavigation({ timeout: 50000, waitUntil: "load" });
+    if (usernameSelector) {
+      await context.setInputValue(usernameSelector, username);
+    }
+    if (passwordSelector) {
+      await context.setInputValue(passwordSelector, password);
+    }
+  }
+  await context.click('button[type="submit"]');
+  await context.waitForNavigation({ timeout: 50000, waitUntil: "load" });
+  await context.evaluate(function () {
+    document.forms[0].submit();
+  });
+  await context.waitForNavigation({ timeout: 100000, waitUntil: "load" });
+}
+module.exports = {
+  implements: "navigation/auth/doLogin",
+  parameterValues: {
+    domain: "instacart.com",
+    usernameSelector: '#nextgen-authenticate\\.all\\.log_in_email',
+    passwordSelector: '#nextgen-authenticate\\.all\\.log_in_password',
+    buttonSelector: '.rmq-a5d52242 div:nth-child(6) button',
+    loggedInSelector: 'body.store-brand-instacart store-home',
+    country: "US",
+    store: "instacart_albertsons",
+    zipcode: "83704",
+  },
+  implementation,
+};
---
+++ src/library/navigation/auth/domains/in/instacart.com/postLogin.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/auth/postLogin',
+  parameterValues: {
+    domain: 'instacart.com',
+    country: 'US',
+    store: 'instacart_albertsons',
+    zipcode: '83704',
+  },
+};
