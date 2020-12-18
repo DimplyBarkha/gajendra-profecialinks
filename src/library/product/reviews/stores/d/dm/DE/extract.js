39cb67bbdabb434296eb8a12013d641efc2b9cf7
shrutikab <shrutika.bongarde@synerzip.com>
2020-12-18 16:41:49 +0530

    added(#3078)

--- src/library/product/reviews/stores/d/dm/DE/extract.js
+++ src/library/product/reviews/stores/d/dm/DE/extract.js
@@ -37,10 +37,13 @@ async function implementation (
       });
     }
   });
+  await context.evaluate(async function () {
   let xpath = document.querySelector('span.bv-content-btn-pages-next');
   if(xpath){
     xpath.click();
   }
+});
+
 
   return await context.extract(productReviews);
 }
