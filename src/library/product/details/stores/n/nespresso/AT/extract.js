5dec6f43ab0a352da9b8f8cf1b0342db89b6ba0e
unknown <sangeta.kumari@synerzip.com>
2020-12-19 01:52:08 +0530

    core-nespresso_at #3283 search-nespresso_at #3284

--- src/library/product/details/stores/n/nespresso/AT/extract.js
+++ src/library/product/details/stores/n/nespresso/AT/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'AT',
     store: 'nespresso',
-    transform: null,
+    transform: transform,
     domain: 'nespresso.com',
     zipcode: '',
   },
---
+++ src/library/product/details/stores/n/nespresso/shared.js
@@ -0,0 +1,129 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const transform = (data) => {
+  // Default transform function
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.description) {
+        let text = '';
+        row.description.forEach(item => {
+          text += item.text.replace(/\s{2,}/g, ' ').replace(/\n/g, ' ').trim();
+        });
+        row.description = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if (row.image) {
+        const img = [];
+        row.image.forEach(item => {
+          if (item.text.indexOf('https:') === -1) {
+            item.text = `https:${item.text}`;
+          }
+        });
+      }
+      if (row.specifications) {
+        let text = '';
+        row.specifications.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        });
+        row.specifications = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+      }
+      if (row.manufacturerDescription) {
+        let text = '';
+        row.manufacturerDescription.forEach(item => {
+          text += item.text.replace(/\s{2,}/g, ' ').replace(/\n/g, ' ').trim();
+        });
+        row.manufacturerDescription = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if ((!row.energyEfficiency || !row.energyEfficiency.length) && row.energyEfficiency1) {
+        console.log('energyEfficiency1', row.energyEfficiency1);
+        row.energyEfficiency = row.energyEfficiency1;
+        console.log('energyEfficiency', row.energyEfficiency);
+      }
+      if ((!row.weightNet || !row.weightNet.length) && row.weightNet1) {
+        console.log('weightNet1', row.weightNet1);
+        row.weightNet = row.weightNet1;
+        console.log('weightNet', row.weightNet);
+      }
+      if ((!row.manufacturerImages || !row.manufacturerImages.length) && row.manufacturerImages1) {
+        console.log('manufacturerImages1', row.manufacturerImages1);
+        row.manufacturerImages = row.manufacturerImages1;
+        console.log('manufacturerImages', row.manufacturerImages);
+      }
+
+      if (row.variant) {
+        let text = '';
+        row.variant.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} | `;
+        });
+        row.variant = [
+          {
+            text: text.slice(0, -2),
+          },
+        ];
+      }
+      if (row.additionalDescBulletInfo) {
+        let text = '';
+        row.additionalDescBulletInfo.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ':')} || `;
+        });
+        row.additionalDescBulletInfo = [
+          {
+            text: text.slice(0, -2),
+          },
+        ];
+      }
+      if (row.allergyAdvice) {
+        let text = '';
+        row.allergyAdvice.forEach(item => {
+          text += item.text.replace(/\n/g, '');
+        });
+        row.allergyAdvice = [
+          {
+            text: text,
+          },
+        ];
+      }
+
+      if (row.allergens) {
+        let text = '';
+        row.allergens.forEach(item => {
+          text = text + (text ? ' ' : '') + item.text;
+        });
+        row.allergens = [{ text }];
+      }
+    }
+  }
+  return data;
+};
+
+module.exports = { transform };
