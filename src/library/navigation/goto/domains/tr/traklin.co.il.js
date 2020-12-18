dcf15c68df81e46021a85134ac1326504917e8a6
sujoypal144 <sujoypal144@gmail.com>
2020-12-18 10:15:25 +0530

    fixed the timeout issue.

--- src/library/navigation/goto/domains/tr/traklin.co.il.js
+++ src/library/navigation/goto/domains/tr/traklin.co.il.js
@@ -8,4 +8,14 @@ module.exports = {
     store: 'traklin',
     zipcode: '',
   },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 60000,
+      waitUntil: 'load',
+    });
+  },
 };
