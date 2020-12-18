17c503c4831178a0c594b6a6f84b9d8ccdd8ec99
Rajeev <rajeev.ranjan@synerzip.com>
2020-12-18 15:11:44 +0530

    #2620

--- src/library/product/details/stores/n/nesspresso_ch_de/CH/createUrl.js
+++ src/library/product/details/stores/n/nesspresso_ch_de/CH/createUrl.js
@@ -3,8 +3,8 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'nespresso.com',
-    prefix: null,
-    url: null,
+    // prefix: null,
+    // url: null,
     country: 'CH',
     store: 'nesspresso_ch_de',
     zipcode: '',
