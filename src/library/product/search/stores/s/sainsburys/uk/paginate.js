aa04e872a8726c1861cbf329d6c3981b5b567541
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-18 16:56:04 +0530

    sainsburys by dhanush

--- src/library/product/search/stores/s/sainsburys/uk/paginate.js
+++ src/library/product/search/stores/s/sainsburys/uk/paginate.js
@@ -7,7 +7,7 @@ module.exports = {
     nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'section[class="ln-o-section ln-o-section"] ul[class="ln-o-grid ln-o-grid--matrix ln-o-grid--equal-height"] li[class="pt-grid-item ln-o-grid__item ln-u-6/12@xs ln-u-3/12@md ln-u-2/12@xl"]',
+    // loadedSelector: 'section[class="ln-o-section ln-o-section"] ul[class="ln-o-grid ln-o-grid--matrix ln-o-grid--equal-height"] li[class="pt-grid-item ln-o-grid__item ln-u-6/12@xs ln-u-3/12@md ln-u-2/12@xl"]',
     openSearchDefinition: {
       template: 'https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]={searchTerms}&pageNumber={page}',
       },
