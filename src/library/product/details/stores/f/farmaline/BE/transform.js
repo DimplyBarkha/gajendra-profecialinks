aa19f885cc91c7b9c65123e542d123740a93ba02
Harish <harish.sathavara@kcsitglobal.com>
2020-12-18 20:02:31 +0530

    Add some fields

--- src/library/product/details/stores/f/farmaline/BE/transform.js
+++ src/library/product/details/stores/f/farmaline/BE/transform.js
@@ -1,30 +1,71 @@
-/* eslint-disable no-useless-escape */
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-    const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
+  // Default transform function
+  const clean = (text) =>
+    text
+      .toString()
+      .replace(/\r\n|\r|\n/g, " ")
+      .replace(/&amp;nbsp;/g, " ")
+      .replace(/&amp;#160/g, " ")
+      .replace(/\u00A0/g, " ")
+      .replace(/\s{2,}/g, " ")
       .replace(/"\s{1,}/g, '"')
       .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
+      .replace(/^ +| +$|( )+/g, " ")
       // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-
-      data.forEach((obj) =>
-        obj.group.forEach((row) =>
-        Object.keys(row).forEach((header) =>
-          row[header].forEach((el) => {
+      .replace(/[\x00-\x1F]/g, "")
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, " ");
+  data.forEach((obj) =>
+    obj.group.forEach((row) =>
+      Object.keys(row).forEach((header) =>
+        row[header].forEach((el) => {
           el.text = clean(el.text);
-      }))));
-    return data;
-  };
-  
-  module.exports = { transform };
+        })
+      )
+    )
+  );
+
+  for (const { group } of data) {
+    for (const row of group) {
+      try {
+        if (row.manufacturerName) {
+          let x = row.manufacturerName[0].text
+          var ret = x.replace('dataLayer = ','');
+          var semi = ret.replace(';','');
+          var obj = JSON.parse(semi);
+          
+          row.manufacturerName = [
+            { text: obj[0].product.manufacturer },
+          ];
+        }
+
+        // if (row.availabilityText) {
+        //   if (row.availabilityText[0].text == "Op voorraad") {
+        //     row.availabilityText = [{ text: "In stock" }];
+        //   } else {
+        //     row.availabilityText = [{ text: "Out of stock" }];
+        //   }
+        // } else {
+        //   row.availabilityText = [{ text: "Out of stock" }];
+        // }
+
+        // if (row.manufacturerName) {
+        //   var baseObject = JSON.parse(row.aggregateRating[0].text);
+        //   row.aggregateRating = [
+        //     { text: baseObject.aggregateRating.ratingValue },
+        //   ];
+        // }
+
+      } catch (exception) {
+        console.log("Error in transform", exception);
+      }
+    }
+  }
+  return data;
+};
+
+module.exports = { transform };
