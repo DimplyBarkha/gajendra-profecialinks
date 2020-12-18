0baaeb548e2a7d7014b89acf4121f8b302f08b09
pkujawa <p.kujawa2@op.pl>
2020-12-18 12:29:44 +0100

    #3131. Formatting

--- src/library/tracker/categories/stores/p/paodeacucar/BR/extract.js
+++ src/library/tracker/categories/stores/p/paodeacucar/BR/extract.js
@@ -1,13 +1,13 @@
 
-async function implementation(inputs, parameters, context, dependencies) {
+async function implementation (inputs, parameters, context, dependencies) {
   const { productMenu } = dependencies;
 
   await context.evaluate(async function () {
-    function addHiddenDiv(id, content, parentDiv = null) {
-      const newDiv = document.createElement("div");
+    function addHiddenDiv (id, content, parentDiv = null) {
+      const newDiv = document.createElement('div');
       newDiv.id = id;
       newDiv.textContent = content;
-      newDiv.style.display = "none";
+      newDiv.style.display = 'none';
       if (parentDiv) {
         parentDiv.appendChild(newDiv);
       } else {
@@ -17,15 +17,15 @@ async function implementation(inputs, parameters, context, dependencies) {
     }
 
     const mainCategories = document.querySelectorAll(
-      ".category-menustyles__Section-sc-1f1zulv-7.gKUKvM:nth-of-type(4) div:not(:first-of-type)"
+      '.category-menustyles__Section-sc-1f1zulv-7.gKUKvM:nth-of-type(4) div:not(:first-of-type)',
     );
     mainCategories.forEach((category) => {
       const mainCategoryName = category.textContent;
-      const fixedCategoryName = mainCategoryName.toLowerCase().replaceAll(' ', '-').normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      const fixedCategoryName = mainCategoryName.toLowerCase().replaceAll(' ', '-').normalize('NFD').replace(/[\u0300-\u036f]/g, '');
       const categoryUrl = 'https://www.paodeacucar.com/categoria/' + fixedCategoryName;
-      const newDiv = addHiddenDiv("categories", "");
-      addHiddenDiv("category", mainCategoryName, newDiv);
-      addHiddenDiv("categoryUrl", categoryUrl, newDiv);
+      const newDiv = addHiddenDiv('categories', '');
+      addHiddenDiv('category', mainCategoryName, newDiv);
+      addHiddenDiv('categoryUrl', categoryUrl, newDiv);
     });
   });
   return await context.extract(productMenu);
