37a1b474a241c829093d91befc2c62096ce7e6f4
rajeshProfecialinks <rajesh.g@profecialinks.com>
2020-12-18 19:33:23 +0530

    canadian tire fr core inprogress

---
+++ src/library/product/details/stores/c/canadiantire/FR/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'canadiantire.ca/fr.html',
+    prefix: null,
+    url: null,
+    country: 'FR',
+    store: 'canadiantire',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/c/canadiantire/FR/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'FR',
+    store: 'canadiantire',
+    domain: 'canadiantire.ca/fr.html',
+    zipcode: '',
+  },
+};
