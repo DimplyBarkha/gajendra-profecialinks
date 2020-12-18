235ca89c79cf7a3cfdfccb4eb4050b2ea885d06d
amstr166 <adamgrabowski166@gmail.com>
2020-12-18 12:02:44 +0100

    Working on menu extractor

--- src/library/tracker/categories/stores/o/otto/DE/extract.js
+++ src/library/tracker/categories/stores/o/otto/DE/extract.js
@@ -7,4 +7,42 @@ module.exports = {
     store: 'otto',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { productMenu } = dependencies;
+
+    await context.evaluate(async () => {
+      function addHiddenDiv (id, content, parentDiv = null) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        if (parentDiv) {
+          parentDiv.appendChild(newDiv);
+        } else {
+          document.body.appendChild(newDiv);
+        }
+        return newDiv;
+      }
+      // categories
+      const categories = document.querySelectorAll('ul[class="nav_navi-list"] > li');
+
+      categories.forEach((category) => {
+        const categoryName = category.querySelector('a[class="nav_navi-elem--level-1"]').textContent;
+        const subCategories = category.querySelectorAll('ul[class="nav_navi-list"]');
+
+        subCategories.forEach((subCategory) => {
+          const subCategoryName = subCategory.querySelector('li[class="nav_navi-elem"] > a').textContent;
+          const elements = subCategory.querySelectorAll('a');
+
+          elements.forEach((element) => {
+            const newDiv = addHiddenDiv('categories');
+            const names = [categoryName, subCategoryName, element.textContent];
+            names.forEach(name => addHiddenDiv('category', name, newDiv));
+            addHiddenDiv('categoryUrl', element.getAttribute('href'), newDiv);
+          });
+        });
+      });
+    });
+    return await context.extract(productMenu);
+  },
 };
