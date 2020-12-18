45f0c7070fb8ca5e55c6366618c7dee9a4b726d8
Dima Povarchuk <povarchukdima007@gmail.com>
2020-12-18 22:47:43 +0200

    initial commit

--- src/library/product/details/stores/m/misterspex/DE/extract.js
+++ src/library/product/details/stores/m/misterspex/DE/extract.js
@@ -1,4 +1,4 @@
-
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -7,5 +7,18 @@ module.exports = {
     transform: null,
     domain: 'misterspex.de',
     zipcode: '',
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(() => {
+      const button = document.querySelector('section.productoffer .checkout-initiator');
+      if (button) {
+        document.querySelector('h1#page_title').setAttribute('availability', 'In Stock');
+      } else {
+        document.querySelector('h1#page_title').setAttribute('availability', 'Out of Stock');
+      }
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
