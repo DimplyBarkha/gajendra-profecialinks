0f7ba80ad9bc2326f26be1eae602cf606a14b609
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2020-12-19 00:38:50 +0530

    extract.yaml file updated

--- src/library/product/details/stores/i/instacart_costco/US/extract.js
+++ src/library/product/details/stores/i/instacart_costco/US/extract.js
@@ -40,6 +40,149 @@ module.exports = {
             addHiddenDiv("ii_procode", product_code[1]);
           }
         }
+        if (
+          document
+            .evaluate(
+              '//li/strong[contains(text(),"Total Fat")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext() != null
+        ) {
+          var fat = document
+            .evaluate(
+              '//li/strong[contains(text(),"Total Fat")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext().data;
+          var total_fat = fat.replace("g", "");
+          addHiddenDiv("ii_fat", total_fat);
+        }
+        if (
+          document
+            .evaluate(
+              '//div/li[contains(text(),"Saturated Fat")]/text()[3]',
+              document
+            )
+            .iterateNext() != null
+        ) {
+          var sat_fat = document
+            .evaluate(
+              '//div/li[contains(text(),"Saturated Fat")]/text()[3]',
+              document
+            )
+            .iterateNext().data;
+          var total_sat_fat = sat_fat.replace("g", "");
+
+          addHiddenDiv("ii_satfat", total_sat_fat);
+        }
+
+        if (
+          document
+            .evaluate(
+              '//li/strong[contains(text(),"Cholesterol")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext() != null
+        ) {
+          var Cholesterol = document
+            .evaluate(
+              '//li/strong[contains(text(),"Cholesterol")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext().data;
+          var total_Cholesterol = Cholesterol.replace("mg", "");
+
+          addHiddenDiv("ii_Cholesterol", total_Cholesterol);
+        }
+
+        if (
+          document
+            .evaluate(
+              '//li/strong[contains(text(),"Sodium")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext() != null
+        ) {
+          var Sodium = document
+            .evaluate(
+              '//li/strong[contains(text(),"Sodium")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext().data;
+          var total_Sodium = Sodium.replace("mg", "");
+
+          addHiddenDiv("ii_Sodium", total_Sodium);
+        }
+
+        if (
+          document
+            .evaluate(
+              '(//div[@class="rmq-97f207b6"]/div/div)[2]/div/ul/div/li/strong[contains(text(),"Total Carbohydrate")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext() != null
+        ) {
+          var Carbohydrate = document
+            .evaluate(
+              '(//div[@class="rmq-97f207b6"]/div/div)[2]/div/ul/div/li/strong[contains(text(),"Total Carbohydrate")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext().data;
+          var total_Carbohydrate = Carbohydrate.replace("g", "");
+
+          addHiddenDiv("ii_Carbohydrate", total_Carbohydrate);
+        }
+
+        if (
+          document
+            .evaluate(
+              '//div/li[contains(text(),"Dietary Fiber")]/text()[3]',
+              document
+            )
+            .iterateNext() != null
+        ) {
+          var Fiber = document
+            .evaluate(
+              '//div/li[contains(text(),"Dietary Fiber")]/text()[3]',
+              document
+            )
+            .iterateNext().data;
+          var total_Fiber = Fiber.replace("g", "");
+
+          addHiddenDiv("ii_Fiber", total_Fiber);
+        }
+
+        if (
+          document
+            .evaluate('//div/li[contains(text(),"Sugars")]/text()[3]', document)
+            .iterateNext() != null
+        ) {
+          var Sugars = document
+            .evaluate('//div/li[contains(text(),"Sugars")]/text()[3]', document)
+            .iterateNext().data;
+          var total_Sugars = Sugars.replace("g", "");
+
+          addHiddenDiv("ii_Sugars", total_Sugars);
+        }
+
+        if (
+          document
+            .evaluate(
+              '//li/strong[contains(text(),"Protein")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext() != null
+        ) {
+          var Protein = document
+            .evaluate(
+              '//li/strong[contains(text(),"Protein")]/following-sibling::text()[2]',
+              document
+            )
+            .iterateNext().data;
+          var total_Protein = Protein.replace("g", "");
+
+          addHiddenDiv("ii_Protein", total_Protein);
+        }
       }
       addHiddenDiv("ii_gram", "g");
       addHiddenDiv("ii_mg", "mg");
