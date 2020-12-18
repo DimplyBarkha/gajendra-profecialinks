dd78996386e265b813e08e72c4b0385c521afbee
ahmad8931 <ahmad@profecialinks.com>
2020-12-19 02:19:31 +0530

    brand name filter

--- src/library/product/reviews/stores/t/thevaporshoppeusa/US/extract.js
+++ src/library/product/reviews/stores/t/thevaporshoppeusa/US/extract.js
@@ -8,4 +8,29 @@ module.exports = {
     domain: 'thevaporshoppeusa.com',
     zipcode: "''",
   },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies) => {
+    const { productReviews } = dependencies;
+
+    await context.evaluate(async () => {
+      function addHiddenDiv (elementID, content) {
+        const newDiv = document.createElement('div');
+        newDiv.className = elementID;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+
+      if (document.querySelector('.product-single__meta')) {
+      		var url = window.location.href;
+      		var brand = null;
+      		brand = url.match(/Vuse|Juul|juul|vuse/);
+        addHiddenDiv('vapordna_brand', brand);
+      }
+    });
+
+    return await context.extract(productReviews);
+  },
 };
