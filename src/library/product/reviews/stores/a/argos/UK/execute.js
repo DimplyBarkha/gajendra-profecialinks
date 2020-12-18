1ccab5daba7db22c502542d9231cbf85937f7152
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-12-18 17:32:52 +0530

    #3270. Review Extractor for source argos.

---
+++ src/library/product/reviews/stores/a/argos/UK/execute.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'product/reviews/execute',
+  parameterValues: {
+    country: 'UK',
+    store: 'argos',
+    domain: 'argos.co.uk',
+    loadedSelector: 'div#reviews',
+    noResultsXPath: '//div[contains(@class,"ErrorPagestyles__Column-sc-1xry310-2 chdxit")]',
+    reviewUrl: 'https://www.argos.co.uk/product/{id}',
+    sortButtonSelectors: null,
+    zipcode: '',
+  },
+};
