f1de3d3562162dad3f272f1d3186c9691d5188bb
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-12-18 18:24:29 +0530

    search-monclick-core-damayanthi

--- src/library/product/search/stores/m/monclick/IT/execute.js
+++ src/library/product/search/stores/m/monclick/IT/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'IT',
     store: 'monclick',
     domain: 'monclick.it',
-    url: 'https://www.monclick.it/risultati_ricerca?fh_view_size=50&fh_sort_by=-orderby_piupopolari,-_match_rate&fh_location=//root/it_IT/$s={searchTerms}/prontaconsegna%3d%7bpronta20consegna%7d&testo=Dyson vacuum',
+    url: 'https://www.monclick.it/Catalogo/SearchDispatcher.axd?testo={searchTerms}',
     // line:''
     loadedSelector: 'body',
     noResultsXPath: null,
