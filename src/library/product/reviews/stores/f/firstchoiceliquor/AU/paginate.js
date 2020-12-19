8a913692b41149efa320637d1be03fbe552e82f2
sivasoarlogic <siva@soarlogic.com>
2020-12-19 16:14:31 +0530

    reviewupdate

---
+++ src/library/product/reviews/stores/f/firstchoiceliquor/AU/paginate.js
@@ -0,0 +1,20 @@
+module.exports = {
+  implements: 'navigation/paginate',
+  parameterValues: {
+    template: null,
+    country: 'AU',
+    store: 'firstchoiceliquor',
+    nextLinkSelector: null,
+    nextLinkXpath: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    loadedXpath: null,
+    noResultsXPath: null,
+    stopConditionSelectorOrXpath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'firstchoiceliquor.com.au',
+    zipcode: '',
+  },
+};
