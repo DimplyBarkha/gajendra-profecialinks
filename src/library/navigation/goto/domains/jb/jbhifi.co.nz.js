16b3217200ae54153dc97937ded1ac0a14bbeb4f
Nishant Sinha <nishant.s@osmosys.asia>
2020-12-19 20:51:02 +0530

    Fixed first request timeout error

--- src/library/navigation/goto/domains/jb/jbhifi.co.nz.js
+++ src/library/navigation/goto/domains/jb/jbhifi.co.nz.js
@@ -3,18 +3,19 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'jbhifi.co.nz',
-    timeout: 60000,
+    timeout: 80000,
     country: 'NZ',
     store: 'jbhifi',
     zipcode: '',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    const timeout = parameters.timeout ? parameters.timeout : 100000;
     await context.setAntiFingerprint(false);
     await context.setLoadAllResources(true);
     await context.setBlockAds(false);
     await context.setLoadImages(true);
+    await context.setFirstRequestTimeout(60000);
     // url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
-    await context.goto(url, { firstRequestTimeout: 60000, timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
   },
 };
