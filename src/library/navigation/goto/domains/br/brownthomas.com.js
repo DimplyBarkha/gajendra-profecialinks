616824296a3b5870a329de7ddf00f043090ac16f
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-19 18:52:49 +0530

    updated updp

--- src/library/navigation/goto/domains/br/brownthomas.com.js
+++ src/library/navigation/goto/domains/br/brownthomas.com.js
@@ -9,7 +9,7 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url }, { country, domain, timeout }, context, dependencies) => {
-    await context.goto(`${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout, waitUntil: 'load', checkBlocked: true });
+    await context.goto(`${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout, waitUntil: 'networkidle0', checkBlocked: true });
     const newUrl = await context.evaluate(function (url) {
       const isSelector = document.querySelector('div[id="primary"] div.slot-panels-container div.asset:nth-last-child(1) a.asset-link');
       if (isSelector) {
@@ -17,6 +17,25 @@ module.exports = {
       }
     }, url);
     url = newUrl || url;
-    await context.goto(`${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout, waitUntil: 'load', checkBlocked: true });
+    await context.goto(`${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout, waitUntil: 'networkidle0', checkBlocked: true });
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
+    }
+    await autoScroll(context);
   },
 };
