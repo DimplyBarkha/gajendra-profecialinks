8497ef773d366028733b37e2abbcc87f3f2e882d
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2020-12-19 18:21:46 +0530

    Updated exctract.js

--- src/library/product/search/stores/m/migros/CH/extract.js
+++ src/library/product/search/stores/m/migros/CH/extract.js
@@ -1,10 +1,11 @@
 const { transform } = require('../format');
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
   dependencies,
 ) {
+  const { transform } = parameters;
   const { productDetails } = dependencies;
   await context.evaluate(async () => {
     let scrolltop = document.getElementById('main').scrollTop;
@@ -12,24 +13,6 @@ async function implementation (
     while (document.querySelector('div.btn-view-more-products > button')) {
       document.querySelector('div.btn-view-more-products > button').click();
       await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
-
-    document.getElementById('main').scrollBy({ top: 1000, left: 0, behavior: 'smooth' });
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    const thmcount = document.querySelectorAll('ul.productGroup li.item img[src]').length;
-    const itemcount = document.querySelectorAll('ul.productGroup li.item').length;
-
-    while (itemcount != thmcount) {
-      const newscrolltop = document.getElementById('main').scrollTop;
-      document.getElementById('main').scrollBy({ top: newscrolltop - scrolltop, left: 0, behavior: 'smooth' });
-      scrolltop = newscrolltop;
-      await new Promise(r => setTimeout(r, 500));
-      const newthmcount = document.querySelectorAll('ul.productGroup li.item img[src]').length;
-      const newitemcount = document.querySelectorAll('ul.productGroup li.item').length;
-      if (newthmcount == newitemcount) {
-        break;
-      }
     }
   });
   return await context.extract(productDetails, { transform });
@@ -39,9 +22,11 @@ module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     store: 'migros',
-    transform: null,
+    transform: transform,
     domain: 'migros.ch',
     zipcode: '',
   },
-  // implementation,
+  implementation,
 };
+
+
