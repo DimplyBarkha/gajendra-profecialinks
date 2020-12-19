1007aa184fb7d7631e5fb5f3c33d6f71f25d7395
prateek.s <psc3018@gmail.com>
2020-12-19 06:12:04 +0530

    fixed for description bullets

--- src/library/product/details/stores/a/ab/GR/extract.js
+++ src/library/product/details/stores/a/ab/GR/extract.js
@@ -8,4 +8,31 @@ module.exports = {
     transform: cleanUp,
     domain: 'ab.gr',
   },
+  implementation: async function implementation (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(()=>{
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+        }    
+        let descriptionBulletPointsExists = document.evaluate(`//div[contains(@class,'ShowMoreLess')]//p[contains(text(),'•')]/br`,document).iterateNext();
+        let descriptionBulletCount;
+        if(descriptionBulletPointsExists) {
+        descriptionBulletCount = document.evaluate(`count(//div[contains(@class,'ShowMoreLess')]//p[contains(text(),'•')]/br)+1`,document).numberValue;      
+        } else {        
+        descriptionBulletCount = 0 ;
+        }     
+        addHiddenDiv('descriptionBulletCount' , descriptionBulletCount);
+    });
+    return await context.extract(productDetails, { transform });
+  }
 };
