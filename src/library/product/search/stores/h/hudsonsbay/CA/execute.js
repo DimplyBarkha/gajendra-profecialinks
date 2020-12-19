86955f4ad61ad5a20ee99b8126c9379cea90f6ac
nehashende <neha.shende@synerzip.com>
2020-12-19 12:13:53 +0530

    Added details page xpath to noResultsXpath.

--- src/library/product/search/stores/h/hudsonsbay/CA/execute.js
+++ src/library/product/search/stores/h/hudsonsbay/CA/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'thebay.com',
     url: 'https://www.thebay.com/search?q={searchTerms}&lang=en_CA&start=0&sz=24',
     loadedSelector: '.product.bfx-disable-product.standard',
-    noResultsXPath: '//span[contains(text(), "weren’t able to find any results")]',
+    noResultsXPath: '//span[contains(text(), "weren’t able to find any results")] | //div[contains(@class, "product-detail product-wrapper")]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/h/hudsonsbay/CA/paginate.js
+++ src/library/product/search/stores/h/hudsonsbay/CA/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: '.product.bfx-disable-product.standard',
-    noResultsXPath: '//span[contains(text(), "weren’t able to find any results")]',
+    noResultsXPath: '//span[contains(text(), "weren’t able to find any results")] | //div[contains(@class, "product-detail product-wrapper")]',
     openSearchDefinition: {
       indexOffset: 0,
       template: 'https://www.thebay.com/search?q={searchTerms}&lang=en_CA&start={offset}&sz=24',
