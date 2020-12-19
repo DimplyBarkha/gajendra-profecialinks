31c4b7220250012135e7c50d40e9d61487b92210
Prakhar Singh <prakhar.s@OSMOSYS.LOCAL>
2020-12-19 21:29:02 +0530

    added options and proxy

--- src/library/navigation/goto/domains/on/onlinetrade.ru.js
+++ src/library/navigation/goto/domains/on/onlinetrade.ru.js
@@ -7,5 +7,15 @@ module.exports = {
     country: 'RU',
     store: 'onlinetrade',
     zipcode: '',
-  },
+  }, implementation: async ({ url , zipcode, storeId} , parameters ,context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 40000;
+    await context.captureRequests();
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true });
+  }
 };
