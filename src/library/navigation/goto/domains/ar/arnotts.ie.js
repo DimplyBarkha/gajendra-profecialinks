177e1ecd69c0e5a0c2005522fa9d021b0909e696
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-19 19:27:27 +0530

    updated goto

--- src/library/navigation/goto/domains/ar/arnotts.ie.js
+++ src/library/navigation/goto/domains/ar/arnotts.ie.js
@@ -13,6 +13,9 @@ module.exports = {
     await context.setLoadAllResources(true);
     await context.setAntiFingerprint(false);
     const timeout = parameters.timeout ? parameters.timeout : 30000;
-    await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true });
+    const response = await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true });
+    if(Math.abs(response.status / 100) === 4){
+      await context.reportBlock('Blocked: ', response.status);
+    }
   },
 };
