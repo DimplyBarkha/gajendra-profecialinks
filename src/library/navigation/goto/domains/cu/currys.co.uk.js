447d1513707a670e5e2cc46af2d83655c590b545
Prakhar Singh <prakhar.s@OSMOSYS.LOCAL>
2020-12-20 20:07:47 +0530

    Added timeout and optional tag

--- src/library/navigation/goto/domains/cu/currys.co.uk.js
+++ src/library/navigation/goto/domains/cu/currys.co.uk.js
@@ -3,9 +3,13 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'currys.co.uk',
-    timeout: null,
+    timeout: 30000,
     country: 'UK',
     store: 'currys',
     zipcode: '',
+  },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"force200": true}[/!opt!]`;
+    await context.goto(url);
   },
 };
