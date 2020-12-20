ef7fd5462e1e9138489dc14f353c6d9d4cfa74d2
Nilesh <nilesh@madasolns.com>
2020-12-21 01:37:51 +0530

    fixed pagination

--- src/library/product/search/stores/c/check24/DE/paginate.js
+++ src/library/product/search/stores/c/check24/DE/paginate.js
@@ -1,18 +1,18 @@
 
 module.exports = {
-  implements: 'product/search/paginate',
+  implements: 'navigation/paginate',
   parameterValues: {
     country: 'DE',
     store: 'check24',
     // nextLinkSelector: 'button:enabled.btn-next',
-    nextLinkSelector: null,
+    nextLinkSelector: "button[class='btn-next']:not(:disabled)",
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: null,
     noResultsXPath: '//div[@class="fakeresult_headline"]',
-    openSearchDefinition: {
-      template: 'https://shopping.check24.de/suche.html?query={searchTerms}&page={page}',
-    },
+    // openSearchDefinition: {
+    //   template: 'https://shopping.check24.de/suche.html?query={searchTerms}&page={page}',
+    // },
     domain: 'check24.de',
     zipcode: '',
   },
