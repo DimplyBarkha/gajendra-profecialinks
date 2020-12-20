50b3fd4bdedcf89aff26845bca04f04d8a208a23
Bindu Datla <bindu.datla@import.io>
2020-12-20 18:43:01 -0500

    updated for timeout

--- src/library/navigation/goto/domains/ga/garbarino.com.js
+++ src/library/navigation/goto/domains/ga/garbarino.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'garbarino.com',
-    timeout: 90000,
+    timeout: 120000,
     country: 'AR',
     store: 'garbarino',
     zipcode: '',
