030e9ba1c40ce93b1e16d17b64298e4e1b9d18f2
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-20 14:41:27 +0530

    #3314 fixing local qa

--- src/library/product/search/stores/s/shufersal/IL/execute.js
+++ src/library/product/search/stores/s/shufersal/IL/execute.js
@@ -1,4 +1,36 @@
 
+/**
+ *
+ * @param { { keywords: string, zipcode: string, storeID: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  { url, loadedSelector, noResultsXPath },
+  context,
+  dependencies,
+) {
+  const { keywords } = inputs;
+  const destinationUrl = url.replace('{searchTerms}', encodeURIComponent(keywords));
+  await dependencies.goto({ ...inputs, url: destinationUrl });
+  await new Promise((resolve, reject) => setTimeout(resolve, 8000));
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate((xp) => {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
+
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -9,5 +41,5 @@ module.exports = {
     loadedSelector: 'div#wrapper.wrapper',
     noResultsXPath: null,
     zipcode: '',
-  },
+  },implementation,
 };
