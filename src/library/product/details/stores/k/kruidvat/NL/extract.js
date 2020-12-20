bde23184cfc0eea2d3b4d88dc83b8dc3aa5570ce
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-12-20 22:24:59 +0530

    im-kruidvat-damayanthi

--- src/library/product/details/stores/k/kruidvat/NL/extract.js
+++ src/library/product/details/stores/k/kruidvat/NL/extract.js
@@ -8,4 +8,10 @@ module.exports = {
     domain: 'kruidvat.nl',
     zipcode: '',
   },
+  // implementation: async ({ url }, parameters, context, dependencies) => {
+  //   context.setBlockAds(false);
+  //   context.setLoadAllResources(true);
+  //   context.setAntiFingerprint(false);
+  //   await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true });
+  // },
 };
