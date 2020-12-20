551204f1c2c0af9fc2968efe7e13891daa3c0aaa
Himanshu-Agrahari <himanshu.a@partner.import.io>
2020-12-20 09:47:42 +0530

    Fix: Images missing
    Fix: Alternate images missing completely
    
    Fix: image is missing completely
    
    Fix: secondaryImageTotal is missing completely
    
    Not fixed: imageAlt is missing completely [Because no Alt is available right now for the images, hence can't get it]

--- src/library/product/details/stores/d/dns-shop/RU/transform.js
+++ src/library/product/details/stores/d/dns-shop/RU/transform.js
@@ -1,8 +1,8 @@
 /**
-*
-* @param {ImportIO.Group[]} data
-* @returns {ImportIO.Group[]}
-*/
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
 const transform = (data) => {
   const cleanUp = text => text.toString()
     .replace(/\r\n|\r|\n/g, ' ')
@@ -13,19 +13,26 @@ const transform = (data) => {
     .replace(/"\s{1,}/g, '"')
     .replace(/\s{1,}"/g, '"')
     .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
+  // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
 
-  for (const { group } of data) {
+  for (const { group }
+    of data) {
     for (const row of group) {
       if (row.alternateImages) {
         if (row.alternateImages.length > 0) {
-          row.secondaryImageTotal = [
-            {
+          // if alternate image doesn't includes main image, count will be equal to lengt
+          if (!row.alternateImages.map(ele => ele.text).includes(row.image[0].text)) {
+            row.secondaryImageTotal = [{
+              text: row.alternateImages.length,
+            }];
+          } else {
+            // if alternate images includes main image, count will be less by 1 of total
+            row.secondaryImageTotal = [{
               text: row.alternateImages.length - 1,
-            },
-          ];
+            }];
+          }
         }
       }
       if (row.description) {
@@ -33,77 +40,63 @@ const transform = (data) => {
         row.description.forEach(item => {
           text += `${item.text} `;
         });
-        row.description = [
-          {
-            text: cleanUp(text.slice(0, -3)),
-          },
-        ];
+        row.description = [{
+          text: cleanUp(text.slice(0, -3)),
+        }];
       }
       if (row.shippingDimensions) {
         let text = '';
         row.shippingDimensions.forEach(item => {
           text += `${item.text}`;
         });
-        row.shippingDimensions = [
-          {
-            text: cleanUp(text),
-          },
-        ];
+        row.shippingDimensions = [{
+          text: cleanUp(text),
+        }];
       }
       if (row.manufacturerDescription) {
         let text = '';
         row.manufacturerDescription.forEach(item => {
           text += `${item.text}`;
         });
-        row.manufacturerDescription = [
-          {
-            text: cleanUp(text),
-          },
-        ];
+        row.manufacturerDescription = [{
+          text: cleanUp(text),
+        }];
       }
       if (row.specifications) {
         let text = '';
         row.specifications.forEach(item => {
           text += `${item.text} || `;
         });
-        row.specifications = [
-          {
-            text: cleanUp(text.slice(0, -3)),
-          },
-        ];
+        row.specifications = [{
+          text: cleanUp(text.slice(0, -3)),
+        }];
       }
       if (row.shippingWeight) {
         let text = '';
         row.shippingWeight.forEach(item => {
           text = item.text;
         });
-        row.shippingWeight = [
-          {
-            text: cleanUp(text),
-          },
-        ];
+        row.shippingWeight = [{
+          text: cleanUp(text),
+        }];
       }
       if (row.nameExtended) {
         let text = '';
         row.nameExtended.forEach(item => {
           text = item.text.replace('Характеристики ', '');
         });
-        row.nameExtended = [
-          {
-            text: cleanUp(text),
-          },
-        ];
+        row.nameExtended = [{
+          text: cleanUp(text),
+        }];
       }
       if (row.name) {
         let text = '';
         row.name.forEach(item => {
           text = item.text.replace('Характеристики ', '');
         });
-        row.name = [
-          {
-            text: cleanUp(text),
-          },
-        ];
+        row.name = [{
+          text: cleanUp(text),
+        }];
       }
     }
   }
