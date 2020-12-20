a91f400b0c76dd607fd1323b066df87730bc8d87
Bindu Datla <bindu.datla@import.io>
2020-12-20 15:32:07 -0500

    price format change

--- src/library/product/search/stores/g/garbarino/shared.js
+++ src/library/product/search/stores/g/garbarino/shared.js
@@ -4,54 +4,63 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-    const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-    const state = context.getState();
-    let orgRankCounter = state.orgRankCounter || 0;
-    let rankCounter = state.rankCounter || 0;
-    const productCodes = state.productCodes || [];
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.productUrl) {
-            row.productUrl.forEach(item => {
-                if (!item.text.match('https://')) {
-                  item.text = `https://www.garbarino.com${item.text}`;
-                }
-              });
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.productUrl) {
+        row.productUrl.forEach(item => {
+          if (!item.text.match('https://')) {
+            item.text = `https://www.garbarino.com${item.text}`;
           }
-          if (row.thumbnail) {
-            row.thumbnail.forEach(item => {
-                if (!item.text.match('https://')) {
-                  item.text = `https:${item.text}`;
-                }
-              });
-          }
-        rankCounter += 1;
-        if (!row.sponsored) {
-          orgRankCounter += 1;
-          row.rankOrganic = [{ text: orgRankCounter }];
-        }
-        row.rank = [{ text: rankCounter }];
-        Object.keys(row).forEach(header => row[header].forEach(el => {
-          el.text = clean(el.text);
-        }));
+        });
       }
+      if (row.thumbnail) {
+        row.thumbnail.forEach(item => {
+          if (!item.text.match('https://')) {
+            item.text = `https:${item.text}`;
+          }
+        });
+      }
+      if (row.listPrice) {
+        row.listPrice.forEach(item => {
+          item.text = item.text.replace(".", "");
+        });
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace(".", "");
+        });
+      }
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
     }
-    context.setState({ rankCounter });
-    context.setState({ orgRankCounter });
-    context.setState({ productCodes });
-    console.log(productCodes);
-    return data;
-  };
-  module.exports = { transform };
-  
+  }
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  console.log(productCodes);
+  return data;
+};
+module.exports = { transform };
