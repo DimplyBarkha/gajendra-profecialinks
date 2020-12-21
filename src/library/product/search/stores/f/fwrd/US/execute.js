320f5190268f7f651fe5f3e87d3be2bd2e3c780f
Sashi Datla <sashi.datla@import.io>
2020-12-21 04:01:19 -0500

    Updated execute.js to handle queryParams for listing

--- src/library/product/search/stores/f/fwrd/US/execute.js
+++ src/library/product/search/stores/f/fwrd/US/execute.js
@@ -1,3 +1,29 @@
+async function implementation (
+  inputs,
+  { url, loadedSelector, noResultsXPath },
+  context,
+  dependencies,
+) {
+  
+  const { keywords } = inputs;
+  const destinationUrl = url.replace('{queryParams}', keywords);
+  await dependencies.goto({ ...inputs, url: destinationUrl });
+
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate((xp) => {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
+
 
 module.exports = {
   implements: 'product/search/execute',
@@ -5,9 +31,10 @@ module.exports = {
     country: 'US',
     store: 'fwrd',
     domain: 'fwrd.com',
-    url: 'https://www.fwrd.com/category-shoes/3f40a9/?navsrc=main&sortBy=popularity',
+    url: 'https://www.fwrd.com/{queryParams}/?navsrc=main&sortBy=popularity',
     loadedSelector: '#plp-product-list ul.product-grids li',
     noResultsXPath: null,
     zipcode: '',
   },
+  implementation,
 };
