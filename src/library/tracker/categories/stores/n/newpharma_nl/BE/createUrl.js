73ac4c76ed2e964fbcb7c36474ceb15eabdf08f0
Michał Wieczorek <michal126@hotmail.com>
2020-12-21 14:54:54 +0100

    finished menu only newpharma_nl be extractor

--- src/library/tracker/categories/stores/n/newpharma_nl/BE/createUrl.js
+++ src/library/tracker/categories/stores/n/newpharma_nl/BE/createUrl.js
@@ -6,7 +6,7 @@ module.exports = {
     prefix: null,
     suffix: null,
     subdomain: null,
-    url: null,
+    url: 'https://www.newpharma.be/apotheek/catalog/',
     country: 'BE',
     store: 'newpharma_nl',
     zipcode: '',
