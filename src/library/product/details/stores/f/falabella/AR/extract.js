f56af75b8c0e8be9dfdcd2693c4ba2c21831b760
Nilesh <nilesh@madasolns.com>
2020-12-21 22:15:21 +0530

    added variant extractor

--- src/library/product/details/stores/f/falabella/AR/extract.js
+++ src/library/product/details/stores/f/falabella/AR/extract.js
@@ -10,6 +10,27 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+    const selector = 'section.pdp-detail-section div.color-swatch-container ul.swatch--container button.colorSwatch-medium';
+    const variantsLength = await context.evaluate(async (selector) => {
+      const variants = document.querySelectorAll(selector);
+      return variants ? variants.length : 0;
+    }, selector);
+
+    // console.log('variantsLength', variantsLength);
+
+    if (variantsLength > 1) {
+      for (let index = 0; index < variantsLength; index++) {
+        if (index > 0) {
+          await context.evaluate((selector, index) => {
+            const variants = document.querySelectorAll(selector);
+            variants[index].click();
+          }, selector, index);
+        }
+        if (index !== variantsLength - 1) {
+          await context.extract(productDetails, { transform }, { type: 'APPEND' });
+        }
+      };
+    }
     return await context.extract(productDetails, { transform });
   },
 };
