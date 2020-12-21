b8cf3fe4c4f9dd0f19f6b262aa6a5b5e311e36f0
unknown <shalini.kumari-odc@partner.import.io>
2020-12-21 14:59:15 +0530

    mvideo

--- src/library/product/search/stores/m/mvideo/RU/execute.js
+++ src/library/product/search/stores/m/mvideo/RU/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'RU',
     store: 'mvideo',
     domain: 'mvideo.ru',
-    url: 'https://www.mvideo.ru/product-list-page-cls?q=Bosch+пылесос&region_id=1&limit=12&category_id=cat2_cis_0000000639',
+    url: 'https://www.mvideo.ru/product-list-page-cls?q=стиральная машина',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
