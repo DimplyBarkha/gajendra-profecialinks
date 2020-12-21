58adabc719f76d37a6654f289ad28ad61b9177f7
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-21 15:34:11 +0530

    updated witb and goto

--- src/library/navigation/goto/domains/wa/wallashops.co.il.js
+++ src/library/navigation/goto/domains/wa/wallashops.co.il.js
@@ -16,13 +16,34 @@ module.exports = {
     dependencies,
   ) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
     await context.goto(url, {
       timeout: timeout,
       waitUntil: 'load',
       checkBlocked: true,
+      blockAds: false,
     });
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
     }
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
+    }
+    await autoScroll(context);
   },
 };
