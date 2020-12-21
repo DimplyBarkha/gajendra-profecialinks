54533a5b5144e3e503aa3a4073f54c0f53288974
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2020-12-21 10:04:42 +0530

    updated

--- src/library/product/details/stores/f/fastenal/US/extract.js
+++ src/library/product/details/stores/f/fastenal/US/extract.js
@@ -1,11 +1,41 @@
+// const { transform } = require('./format');
+// module.exports = {
+//   implements: 'product/details/extract',
+//   parameterValues: {
+//     country: 'US',
+//     store: 'fastenal',
+//     transform: transform,
+//     domain: 'fastenal.com',
+//     zipcode: '',
+//   },
+// };
+
+
 const { transform } = require('./format');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'fastenal',
-    transform: transform,
     domain: 'fastenal.com',
-    zipcode: '',
+    transform
+
+  },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+   // await new Promise(resolve => setTimeout(resolve, 10000));
+    await context.evaluate(async function () {
+
+      var url = window.location.href;
+      document.body.setAttribute('productUrl', url);
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
   },
 };
+
+
