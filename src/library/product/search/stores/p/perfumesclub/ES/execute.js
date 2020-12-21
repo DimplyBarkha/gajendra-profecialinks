2217730e1fe14b27d3bb10ba065d26ee2842edcd
viswa <viswashankar.veginati@od.import.io>
2020-12-21 12:51:30 +0530

    Perfumesclub by Viswa

---
+++ src/library/product/search/stores/p/perfumesclub/ES/execute.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  implements: 'product/search/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'perfumesclub',
+    domain: 'perfumesclub.com',
+    url: 'https://www.perfumesclub.com/',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
