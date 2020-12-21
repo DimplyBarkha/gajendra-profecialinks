017e95101f94df1a5c19020a9cc36b393c301c87
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-12-21 19:15:26 +0530

    Added 404 selector for execute

--- src/library/product/details/stores/d/douglas/DE/execute.js
+++ src/library/product/details/stores/d/douglas/DE/execute.js
@@ -6,6 +6,6 @@ module.exports = {
     store: 'douglas',
     domain: 'douglas.de',
     loadedSelector: 'div.rd__product-header__title__main',
-    noResultsXPath: '//main[@id="rd__not-found-page"]|//div[@class="rd__error-content__headline"]',
+    noResultsXPath: '//main[@id="rd__not-found-page"] | //div[@class="rd__error-content__headline"] | //div[contains(@class, "div.rd__error-content__image")]',
   },
 };
