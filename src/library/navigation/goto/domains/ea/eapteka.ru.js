4227c107f7a05f1083aea47cb022c03a69bb718b
pkujawa <p.kujawa2@op.pl>
2020-12-21 21:03:07 +0100

    #3142. eApteka RU menu extractor

--- src/library/navigation/goto/domains/ea/eapteka.ru.js
+++ src/library/navigation/goto/domains/ea/eapteka.ru.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'eapteka.ru',
-    timeout: null,
+    timeout: 50000,
     country: 'RU',
     store: 'eapteka',
     zipcode: '',
--- src/library/tracker/categories/stores/e/eapteka/RU/extract.js
+++ src/library/tracker/categories/stores/e/eapteka/RU/extract.js
@@ -1,4 +1,43 @@
+async function implementation (inputs, parameters, context, dependencies) {
+  const { productMenu } = dependencies;
 
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content, parentDiv = null) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      if (parentDiv) {
+        parentDiv.appendChild(newDiv);
+      } else {
+        document.body.appendChild(newDiv);
+      }
+      return newDiv;
+    }
+
+    const mainCategories = document.querySelectorAll(
+      '.container.header__nav-list .header__nav-item > a',
+    );
+    mainCategories.forEach((category) => {
+      // @ts-ignore
+      const mainCategoryName = category.textContent;
+      if (category.nextElementSibling) {
+        const subCategories = category.nextElementSibling.querySelectorAll(
+          '.row ul > li > a',
+        );
+        subCategories.forEach((subCategory) => {
+          const subCategoryTitle = subCategory.textContent;
+          const newDiv = addHiddenDiv('categories', '');
+          addHiddenDiv('category', mainCategoryName, newDiv);
+          addHiddenDiv('category', subCategoryTitle, newDiv);
+          addHiddenDiv('categoryUrl', subCategory.href, newDiv);
+        });
+      }
+    });
+  },
+  );
+  return await context.extract(productMenu);
+}
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -7,4 +46,5 @@ module.exports = {
     store: 'eapteka',
     zipcode: '',
   },
+  implementation,
 };
