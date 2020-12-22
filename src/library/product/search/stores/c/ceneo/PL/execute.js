6a8f573604e19556dca8e64a58037062dc1b9295
ravitejaw324 <ravi.w@partner.import.io>
2020-12-22 06:57:24 +0530

    added loaded and no results selectors

--- src/library/product/search/stores/c/ceneo/PL/execute.js
+++ src/library/product/search/stores/c/ceneo/PL/execute.js
@@ -8,8 +8,8 @@ module.exports = {
     url: 'https://www.ceneo.pl/;szukaj-{searchTerms}',
     // url: 'https://www.ceneo.pl/Odswiezacze_powietrza;szukaj-{searchTerms};0191.htm',
     // 'https://www.ceneo.pl/Mleka_modyfikowane;szukaj-{searchTerms};0191.htm',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div[class*=category-list-body] div[class*=js_category-list-item]',
+    noResultsXPath: '//div[contains(@class,"card__body") and contains(.,"nie znaleziono")]',
     zipcode: '',
   },
 };
