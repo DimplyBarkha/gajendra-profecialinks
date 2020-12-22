4201d884f40b0b7bf9231361c8163b8aef267ec4
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-22 16:41:37 +0530

    updated goto, updated witb

--- src/library/navigation/goto/domains/ab/abt.com.js
+++ src/library/navigation/goto/domains/ab/abt.com.js
@@ -12,10 +12,29 @@ module.exports = {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
     await context.setBlockAds(false);
     await context.setFirstRequestTimeout(60000);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load' });
+    await context.goto(url, { timeout: timeout, waitUntil: 'networkidle0', block_ads: false });
     console.log(zipcode);
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
     }
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
+    }
+    await autoScroll(context);
   },
 };
