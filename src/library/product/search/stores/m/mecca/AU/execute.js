bbd0574dea1b73ed38d048bdf87f715ea01d8fd0
astropaula <wojciechowska.fran@gmail.com>
2020-12-22 18:13:53 +0100

    Rebuiled extractor

--- src/library/product/search/stores/m/mecca/AU/execute.js
+++ src/library/product/search/stores/m/mecca/AU/execute.js
@@ -1,14 +1,13 @@
 async function implementation (inputs, parameters, context, dependencies) {
-  console.log('params', parameters);
   const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
   await context.goto(url);
-  const isRightPage = await context.evaluate(async () => {
-    return document.querySelector('div[class="container-fluid"]');
-  });
-  if (isRightPage != null) {
-    const url1 = 'https://www.mecca.com.au/search?q="{searchTerms}"'.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
-    await context.goto(url1);
-  }
+  // const isRightPage = await context.evaluate(async () => {
+  //   return document.querySelector('div[class="container-fluid"]');
+  // });
+  // if (isRightPage != null) {
+  //   const url1 = 'https://www.mecca.com.au/search?q="{searchTerms}"'.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  //   await context.goto(url1);
+  // }
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
@@ -30,7 +29,7 @@ module.exports = {
     store: 'mecca',
     domain: 'mecca.com.au',
     url: 'https://www.mecca.com.au/search?q={searchTerms}',
-    loadedSelector: 'div[class="search-result-content"]',
+    loadedSelector: 'div[class="search-result-content"], div[class="shop-grid"]',
     noResultsXPath: '//div[@class="no-hits-help"]',
     zipcode: '',
   },
--- src/library/product/search/stores/m/mecca/AU/paginate.js
+++ src/library/product/search/stores/m/mecca/AU/paginate.js
@@ -7,7 +7,7 @@ module.exports = {
     nextLinkSelector: 'a[class="page-next"], a[rel="next"]',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'div[class="search-result-content"]',
+    loadedSelector: 'div[class="search-result-content"], div[class="shop-grid"]',
     noResultsXPath: '//div[@class="no-hits-help"]',
     resultsDivSelector: null,
     openSearchDefinition: null,
