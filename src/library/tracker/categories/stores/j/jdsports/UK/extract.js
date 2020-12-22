5087b51417cbde95d253562005c49aa86fbfaf28
LAPTOP-MD78TJNH\TDCM <katarzyna-godlewska1@wp.pl>
2020-12-22 16:13:28 +0100

    generate test, remove commented code in extract.js

--- src/library/tracker/categories/stores/j/jdsports/UK/extract.js
+++ src/library/tracker/categories/stores/j/jdsports/UK/extract.js
@@ -43,35 +43,6 @@ async function implementation (inputs, parameters, context, dependencies) {
         });
       }
     });
-    // const categoryArr = [];
-    // const subCategories = [];
-
-    // const categories = document.querySelectorAll('ul#nav-menu > li > a')
-    // categories.forEach(element => {
-    //   if (element.innerText === 'JD Now' || element.innerText === 'Offers' || element.innerText === 'JD Blog') {
-    //   } else {
-    //     const categoryName = element.innerText;
-    //     const categoryUrl = element.href;
-    //     const subCategories = element.querySelectorAll('div.nav-panel > div > div > ul > li > a');
-
-    //     subCategories.forEach((subCategory) => {
-    //       // const subCategoriesList = subCategory.innerHTML;
-    //       const subCategoryUrl = subCategory.href;
-
-    //       categoryName.forEach((category) => {
-    //         const newDiv = addHiddenDiv('categories', '');
-    //         addHiddenDiv('category', category, newDiv);
-    //         addHiddenDiv('categoryUrl', categoryUrl, newDiv);
-
-    //         subCategories.forEach((subCategory) => {
-    //           addHiddenDiv('categoryUrl', subCategoryUrl, newDiv);
-    //           addHiddenDiv('category', subCategory.innerHTML, newDiv);
-    //         });
-    //       });
-    //     },
-    //     );
-    //   }
-    // });
   });
   return await context.extract(productMenu);
 }
