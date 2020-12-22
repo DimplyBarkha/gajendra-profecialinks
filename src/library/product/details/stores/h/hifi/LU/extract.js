3fb9cd808b9560e9ebc08a00c042b96b09475a68
Himanshu-Agrahari <himanshu.a@partner.import.io>
2020-12-22 15:39:50 +0530

    Make chagne in previous commit

--- src/library/product/details/stores/h/hifi/LU/extract.js
+++ src/library/product/details/stores/h/hifi/LU/extract.js
@@ -24,7 +24,6 @@ module.exports = {
     const seeMoreProductInfoCSS = 'a.brand-content-link'; // anchor tag
     const iframeDivContainerCss = '#loadbeeTabContentId'; // div has API key to make call to fetch iframe url
     const iframeIdCSS = '#loadbeeIframeId'; // iframe id css
-    const noInformationAvailableXpath = `//div[contains(@class, 'content-provider-content')]//*[contains(text(), 'aucune information supplémentaire est disponible')]`;
 
     const waitForIframeDiv = async (loadbeeIframeIdCSS) => {
       try {
@@ -35,29 +34,21 @@ module.exports = {
       }
     };
 
-    const checkNoInfo = async (xpath) => await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, xpath);
-    // no info => false else true
     try {
       await context.waitForSelector(seeMoreProductInfoCSS, { timeout: 10000 });
       console.log('seeMoreProduct button found, clicking it...');
       await context.click(seeMoreProductInfoCSS); // loads iframe upon click
 
-      const isNoInfoavailable = await checkNoInfo(noInformationAvailableXpath);
-      if (!isNoInfoavailable) {
-        await waitForIframeDiv(iframeIdCSS); // wait for iframe load
-        await context.evaluate(async (iframeDivContainerCss, seeMoreProductInfoCSS) => {
-          const iframeDiv = document.querySelector(iframeDivContainerCss);
-          if (iframeDiv) return; // check if iframe div loaded, return
-
-          const seeMoreAtag = document.querySelector(seeMoreProductInfoCSS);
-          if (seeMoreAtag) { // else click see more info node again
-            console.log(`clicking again seeMoreAtag : ${seeMoreAtag}`);
-            seeMoreAtag.click();
-          }
-        }, iframeDivContainerCss, seeMoreProductInfoCSS);
-      } else {
-        console.log('Additional info not available')
-      }
+      await waitForIframeDiv(iframeIdCSS); // wait for iframe load
+      await context.evaluate(async (iframeDivContainerCss, seeMoreProductInfoCSS) => {
+        const iframeDiv = document.querySelector(iframeDivContainerCss);
+        if (iframeDiv) return; // check if iframe div loaded, return
+        const seeMoreAtag = document.querySelector(seeMoreProductInfoCSS);
+        if (seeMoreAtag) { // else click see more info node again
+          console.log(`clicking again seeMoreAtag : ${seeMoreAtag}`);
+          seeMoreAtag.click();
+        }
+      }, iframeDivContainerCss, seeMoreProductInfoCSS);
     } catch (err) {
       console.log(`no seeMoreProductInfoCSS link found : CSS => ${seeMoreProductInfoCSS}`);
     }
@@ -67,106 +58,106 @@ module.exports = {
     let aplusImages = '';
     let videos = '';
 
-    if (!await checkNoInfo(noInformationAvailableXpath)) {
-      await waitForIframeDiv(iframeIdCSS);
-      // get iframge link src
-      src = await context.evaluate(async function (loadbeeIframeIdCSS) {
-        const iframe = document.querySelector(loadbeeIframeIdCSS);
-        // @ts-ignore
-        const src = iframe ? iframe.src : '';
-        return src;
-      }, iframeIdCSS);
+    await waitForIframeDiv(iframeIdCSS);
+    // get iframge link src
+    src = await context.evaluate(async function (loadbeeIframeIdCSS) {
+      const iframe = document.querySelector(loadbeeIframeIdCSS);
+      // @ts-ignore
+      const src = iframe ? iframe.src : '';
+      return src;
+    }, iframeIdCSS);
 
-      // getting iframe url from API
-      if (!src) {
-        console.log('Trying to get iframe url from API');
-        try {
-          await context.waitForSelector(iframeDivContainerCss, { timeout: 30000 });
-          console.log('iframeDivContainerCss selector found');
-          await context.evaluate(async (iframeDivContainerCss) => {
-            const div = document.querySelector(iframeDivContainerCss);
-            const dataSet = div && div.dataset;
-            console.log(`dataset: ${JSON.stringify(dataSet)}`);
-            const gtin = dataSet && dataSet.loadbeeGtin;
-            const apiKey = dataSet && dataSet.loadbeeApikey;
-            if (!(gtin && apiKey)) return;
+    // getting iframe url from API
+    if (!src) {
+      console.log('Trying to get iframe url from API');
+      try {
+        await context.waitForSelector(iframeDivContainerCss, { timeout: 10000 });
+        console.log('iframeDivContainerCss selector found');
+        await context.evaluate(async (iframeDivContainerCss) => {
+          const div = document.querySelector(iframeDivContainerCss);
+          const dataSet = div && div.dataset;
+          console.log(`dataset: ${JSON.stringify(dataSet)}`);
+          const gtin = dataSet && dataSet.loadbeeGtin;
+          const apiKey = dataSet && dataSet.loadbeeApikey;
+          if (!(gtin && apiKey)) return;
 
-            const apiUrl = `https://availability.loadbee.com/v3/EAN/${gtin}/fr_BE?template=default&apiKey=${apiKey}`;
-            console.log(`apiUrl: ${apiUrl}`);
-            const data = await fetch(apiKey);
-            console.log(`API status: ${data.status}`);
-            if (data.status === 404) return;
-            const jsonData = await data.json();
-            console.log(`JSON from API: ${JSON.stringify(jsonData)}`);
-            src = jsonData.url;
-            console.log(`Iframe URL: ${src}`);
-          }, iframeDivContainerCss);
-        } catch (error) {
-          console.log('src not found from API as well');
-          console.log(error);
-        }
+          const apiUrl = `https://availability.loadbee.com/v3/EAN/${gtin}/fr_BE?template=default&apiKey=${apiKey}`;
+          console.log(`apiUrl: ${apiUrl}`);
+
+          const data = await fetch(apiKey);
+          console.log(`API status: ${data.status}`);
+
+          if (data.status === 404) return;
+          const jsonData = await data.json();
+          console.log(`JSON from API: ${JSON.stringify(jsonData)}`);
+
+          src = jsonData.url;
+          console.log(`Iframe URL: ${src}`);
+        }, iframeDivContainerCss);
+      } catch (error) {
+        console.log(`Div with API key not found, API will not hit.. ${iframeDivContainerCss}`);
+        console.log('src not found from API as well');
+        console.log(error);
       }
-      // navigate to iframe src if available
-      if (src) {
-        try {
-          await context.goto(src, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
-          await context.waitForSelector('div.wrapper.preview');
-          enhancedContent = await context.evaluate(async function () {
-            let enhancedContent = '';
-            if (document.querySelectorAll('div.pic-text')) {
-              const characterstics = document.querySelectorAll('div.pic-text');
-              // @ts-ignore
-              for (let i = 0; i < characterstics.length; i++) { enhancedContent += characterstics[i].innerText + ' || '; }
-            }
-            if (document.querySelectorAll('div.animation-text')) {
-              const moreCharacterstics = document.querySelectorAll('div.animation-text');
-              // @ts-ignore
-              for (let i = 0; i < moreCharacterstics.length; i++) { enhancedContent += moreCharacterstics[i].innerText + ' || '; }
-            }
-            if (document.querySelectorAll('div.info')) {
-              const attributeSections = document.querySelectorAll('div.info');
-              // @ts-ignore
-              for (let i = 0; i < attributeSections.length; i++) { enhancedContent += attributeSections[i].innerText + ' || '; }
-            }
-            if (document.querySelectorAll('div.info2')) {
-              const moreAttributeSections = document.querySelectorAll('div.info2');
-              // @ts-ignore
-              for (let i = 0; i < moreAttributeSections.length; i++) { enhancedContent += moreAttributeSections[i].innerText + ' || '; }
-            }
-            return enhancedContent;
-          });
+    }
+    // navigate to iframe src if available
+    if (src) {
+      try {
+        await context.goto(src, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
+        await context.waitForSelector('div.wrapper.preview');
+        enhancedContent = await context.evaluate(async function () {
+          let enhancedContent = '';
+          if (document.querySelectorAll('div.pic-text')) {
+            const characterstics = document.querySelectorAll('div.pic-text');
+            // @ts-ignore
+            for (let i = 0; i < characterstics.length; i++) { enhancedContent += characterstics[i].innerText + ' || '; }
+          }
+          if (document.querySelectorAll('div.animation-text')) {
+            const moreCharacterstics = document.querySelectorAll('div.animation-text');
+            // @ts-ignore
+            for (let i = 0; i < moreCharacterstics.length; i++) { enhancedContent += moreCharacterstics[i].innerText + ' || '; }
+          }
+          if (document.querySelectorAll('div.info')) {
+            const attributeSections = document.querySelectorAll('div.info');
+            // @ts-ignore
+            for (let i = 0; i < attributeSections.length; i++) { enhancedContent += attributeSections[i].innerText + ' || '; }
+          }
+          if (document.querySelectorAll('div.info2')) {
+            const moreAttributeSections = document.querySelectorAll('div.info2');
+            // @ts-ignore
+            for (let i = 0; i < moreAttributeSections.length; i++) { enhancedContent += moreAttributeSections[i].innerText + ' || '; }
+          }
+          return enhancedContent;
+        });
 
-          aplusImages = await context.evaluate(async function () {
-            let aplusImages = '';
-            if (document.querySelectorAll('img')) {
-              const allImages = document.querySelectorAll('img');
-              for (let i = 0; i < allImages.length; i++) {
-                if (allImages[i].hasAttribute('src')) {
-                  aplusImages += allImages[i].getAttribute('src') + ' || ';
-                }
+        aplusImages = await context.evaluate(async function () {
+          let aplusImages = '';
+          if (document.querySelectorAll('img')) {
+            const allImages = document.querySelectorAll('img');
+            for (let i = 0; i < allImages.length; i++) {
+              if (allImages[i].hasAttribute('src')) {
+                aplusImages += allImages[i].getAttribute('src') + ' || ';
               }
             }
-            return aplusImages;
-          });
-          videos = await context.evaluate(async function () {
-            let videos = '';
-            if (document.querySelector('div[class="header-content desktop"] div[class="play-btn centered desktop"]')) {
-              if (document.querySelector('div[class="header-content desktop"] div[class="play-btn centered desktop"]').hasAttribute('data-video')) { videos = document.querySelector('div[class="header-content desktop"] div[class="play-btn centered desktop"]').getAttribute('data-video'); }
-            }
-            return videos;
-          });
-        } catch (err) { }
-      } else {
-        console.log('Iframe src not found, will not navigate, still at original page');
-      }
+          }
+          return aplusImages;
+        });
+        videos = await context.evaluate(async function () {
+          let videos = '';
+          if (document.querySelector('div[class="header-content desktop"] div[class="play-btn centered desktop"]')) {
+            if (document.querySelector('div[class="header-content desktop"] div[class="play-btn centered desktop"]').hasAttribute('data-video')) { videos = document.querySelector('div[class="header-content desktop"] div[class="play-btn centered desktop"]').getAttribute('data-video'); }
+          }
+          return videos;
+        });
+      } catch (err) { }
     } else {
-      console.log(`No additional info were found, will not navigate...`);
+      console.log('Iframe src not found, will not navigate, still at original page');
     }
 
     // checking if page already navigated to src/iframe url, if not no need to reload/naviagate
     if (src) await context.goto(prodUrl, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
     await context.evaluate(async function (enhancedContent, aplusImages, videos) {
-      function addElementToDocument(key, value) {
+      function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
@@ -191,7 +182,7 @@ module.exports = {
       }
       console.log(`The value of video is: ${video}`);
 
-      function addHiddenDiv(id, content) {
+      function addHiddenDiv (id, content) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
         newDiv.textContent = content;
@@ -205,7 +196,7 @@ module.exports = {
 
     // pdEan, pd_rating, sku, description
     await context.evaluate(async function () {
-      function addElementToDocument(key, value) {
+      function addElementToDocument (key, value) {
         console.log(`${key} : ${value}`);
         const catElement = document.createElement('div');
         catElement.id = key;
