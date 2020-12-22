e1e5f1390c3a2a84d2a4b44307df774fcd8d9090
srishivams <srishivams97@gmail.com>
2020-12-22 21:43:08 +0530

    retrieved manufacturer desc, images and videos

--- src/library/product/details/stores/f/fnac/PT/extract.js
+++ src/library/product/details/stores/f/fnac/PT/extract.js
@@ -33,6 +33,52 @@ async function implementation (inputs, parameters, context, dependencies) {
       addHiddenDiv('added_brandText', brandText);
     }
     fetchBrandFromScript();
+    const scrollToElement = document.querySelector('div#flix-location-content');
+    console.log('scroll element ' + scrollToElement);
+    scrollToElement.scrollIntoView({ behavior: 'smooth' });
+    function stall (ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+    await stall(3000);
+    if (document.querySelectorAll('div.flix-std-row')) {
+      const flixRows = document.querySelectorAll('div.flix-std-row');
+      let enhancedContent = '';
+      for (let i = 0; i < flixRows.length; i++) {
+        if (flixRows[i].querySelector('div[class*="flix-std-title"]')) { enhancedContent += flixRows[i].querySelector('div[class*="flix-std-title"]').innerText + '||'; }
+        if (flixRows[i].querySelector('div[class*="flix-std-desc"]')) { enhancedContent += flixRows[i].querySelector('div[class*="flix-std-desc"]').innerText + '||'; }
+      }
+      if (document.querySelectorAll('div[class*="flix-std-table"] div[class*="flix-std-desc"]')) {
+        const moreInfo = document.querySelectorAll('div[class*="flix-std-table"] div[class*="flix-std-desc"]');
+        for (let i = 0; i < moreInfo.length; i++) enhancedContent += moreInfo[i].innerText + '||';
+        const specsData = document.querySelectorAll('table[class*="flix-std-specs-table"] td');
+        for (let i = 0; i < specsData.length; i++) {
+          enhancedContent += specsData[i].querySelector('div[class*="flix-value"]').innerText + '||';
+          enhancedContent += specsData[i].querySelector('div[class*="flix-title"]').innerText + '||';
+        }
+      }
+      let aplusImage = '';
+      if (document.querySelectorAll('div[class*="flix-std-row"]')) {
+        const imageList = document.querySelectorAll('div[class*="flix-std-row"]');
+        for (let i = 0; i < imageList.length; i++) {
+          if (imageList[i].querySelector('img')) {
+            if (imageList[i].querySelector('img').hasAttribute('srcset')) aplusImage += imageList[i].querySelector('img').getAttribute('srcset') + ' || ';
+          }
+        }
+      }
+      let vidLink = '';
+      if (document.querySelector('iframe[title*="media-video"]')) {
+        vidLink = document.querySelector('iframe[title*="media-video"]').getAttribute('src');
+      }
+
+      addHiddenDiv('enhancedContent', enhancedContent);
+      addHiddenDiv('aplusImage', aplusImage);
+      addHiddenDiv('vidLink', vidLink);
+      console.log(enhancedContent + ' is enhanced content ');
+    }
   });
   await new Promise((resolve) => setTimeout(resolve, 10000));
   return await context.extract(productDetails, { transform });
