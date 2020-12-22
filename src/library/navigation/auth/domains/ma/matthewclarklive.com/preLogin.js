4d17e3231e61015b8bc68a68b4295a0ed5952fa8
Your Name <karthik.ravi-odc@partner.import.io>
2020-12-22 19:05:16 +0530

    new

--- src/library/navigation/auth/domains/ma/matthewclarklive.com/preLogin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  implements: 'navigation/auth/preLogin',
-  parameterValues: {
-    domain: 'matthewclarklive.com',
-    country: 'UK',
-    store: 'matthewclarklive',
-    zipcode: '',
-  },
-};
