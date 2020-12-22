f9f39f4096706dcbd85a52dfecfac1eff665da77
John Felix J <johnfelix.josep@hcl.com>
2020-12-22 18:53:33 +0530

    Amazon MX retailer updates

---
+++ src/library/navigation/goto/domains/am/amazon.com.mx.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'amazon.com.mx',
+    timeout: null,
+    country: 'MX',
+    store: 'amazon',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/amazon/MX/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'amazon.com.mx',
+    prefix: null,
+    url: 'https://www.amazon.com.mx/dp/{id}?th=1&psc=1',
+    country: 'MX',
+    store: 'amazon',
+    zipcode: '',
+  },
+};
