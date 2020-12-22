1b4e09f79658d25589d9c8d0834f74b3404e79b0
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-12-22 15:06:33 +0200

    mercadolibre images

--- src/library/product/search/stores/m/mercadolibre/MX/execute.js
+++ src/library/product/search/stores/m/mercadolibre/MX/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'MX',
     store: 'mercadolibre',
     domain: 'mercadolibre.com.mx',
-    url: 'https://listado.mercadolibre.com.mx/{searchTerms}',
+    url: 'https://listado.mercadolibre.com.mx/{searchTerms}/#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":true,"cookies":[]}[/!opt!]',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
