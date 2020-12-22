521bd6ea34a0890bd5edd3e6a6693dfa51c7c3bd
Nilesh <nilesh.b@osmosys.asia>
2020-12-22 17:03:21 +0530

    Fixed alternate image issue.

--- src/library/product/details/stores/s/saturn/DE/extract.js
+++ src/library/product/details/stores/s/saturn/DE/extract.js
@@ -24,7 +24,7 @@ async function implementation(inputs, parameters, context, dependencies) {
   }
 
   // adding rating count with API since value seems to vary during run.
-  async function getRating () {
+  async function getRating() {
     const productId = window.location.pathname.match(/([^-]+).html$/)[1];
     const API = `https://www.saturn.de/api/v1/graphql?operationName=GetSelectProduct&variables={"hasMarketplace":false,"id":"${productId}"}&extensions={"pwa":{"salesLine":"Saturn","country":"DE","language":"de"},"persistedQuery":{"version":1,"sha256Hash":"bf21cd23afefaf0e92d339815ffe1da9ed05b7fdbeb5f00dcf5478e5abdfee89"}}`;
     const response = await fetch(API);
@@ -91,6 +91,12 @@ async function implementation(inputs, parameters, context, dependencies) {
     }
     // Call the function to get the images
     await clickImages('div > div > picture > img[alt]');
+
+
+    var clickArrow = document.querySelector('div[ data-test="mms-th-gallery"] div[direction="next"]');
+    if (clickArrow) {
+      clickArrow.click();
+    }
   });
 
   await gDelay(2000);
@@ -105,12 +111,15 @@ async function implementation(inputs, parameters, context, dependencies) {
       const inf = JSON.parse(request.responseBody.body);
       const vidUrl = inf.cliplist.clip.clipurl;
       videoUrls.push(vidUrl);
+      console.log("Got video");
+      console.log(videoUrls.length);
       console.log('VIDEO URL :  ', vidUrl);
     }
   }
 
   await context.evaluate(function (urls) {
     const videosAll = urls.join(' | ');
+    
     document.body.setAttribute('video-url', videosAll);
   }, videoUrls);
 
