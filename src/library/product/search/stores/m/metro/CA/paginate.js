3db2f67ec73c84ba7ddbfc715604d1443e24842c
IpsitaM <iamipsita07@gmail.com>
2020-12-22 10:57:52 +0530

    made changes according to uat

--- src/library/product/search/stores/m/metro/CA/paginate.js
+++ src/library/product/search/stores/m/metro/CA/paginate.js
@@ -1,17 +1,19 @@
 module.exports = {
-  implements: 'product/search/paginate',
-  parameterValues: {
-    country: 'CA',
-    store: 'metro',
-    nextLinkSelector: null,
-    mutationSelector: null,
-    spinnerSelector: null,
-    loadedSelector: '.tile-product__top-section__visuals__stickers',
-    noResultsXPath: '//div[@class="product-not-found"]',
-    openSearchDefinition: {
-      template: 'https://www.metro.ca/en/online-grocery/search-page-{page}?&filter={searchTerms}',
+    implements: 'product/search/paginate',
+    parameterValues: {
+        country: 'CA',
+        store: 'metro',
+        nextLinkSelector: `div[class*='ppn--pagination'] > a:last-child:not(.disabled)`,
+        mutationSelector: null,
+        spinnerSelector: null,
+        loadedSelector: '.tile-product__top-section__visuals__stickers',
+        noResultsXPath: '//div[@class="product-not-found"]',
+        openSearchDefinition: null,
+        // openSearchDefinition: {
+        //     template: 'https://www.metro.ca/en/online-grocery/search-page-{page}?&filter={searchTerms}',
+        //     pageOffset: 1,
+        // },
+        domain: 'metro.ca',
+        zipcode: '',
     },
-    domain: 'metro.ca',
-    zipcode: '',
-  },
 };
