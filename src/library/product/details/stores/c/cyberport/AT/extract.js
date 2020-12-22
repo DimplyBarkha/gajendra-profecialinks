388964504a569a5a7f21e00168c526643f1296dd
unknown <ramraut25@gmail.com>
2020-12-22 08:01:47 +0530

    Fixed an interaction error with click() property

--- src/library/product/details/stores/c/cyberport/AT/extract.js
+++ src/library/product/details/stores/c/cyberport/AT/extract.js
@@ -1,4 +1,4 @@
-const{transform} = require('./transform');
+const { transform } = require('./transform');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,47 +8,49 @@ module.exports = {
     domain: 'cyberport.at',
     zipcode: '',
   },
-  implementation: async ({ inputString }, { country, domain ,transform: transformParam}, context, { productDetails }) => {     
-    
-    await context.evaluate(async function () {       
-     const productInfo = preFetchProductDetails();
-       addEleToDoc('divhiddenproductAvgRating', productInfo['productAvgRating']);
-       addEleToDoc('divhiddenproductRatingCount', productInfo['productRatingCount']);
-       addEleToDoc('divhiddenproductGrossPrice', productInfo['productGrossPrice'].analyticsFormat);
-      function preFetchProductDetails () {
-        let productInfo = findProductDetails('//section//@data-productinfofordatalayer');                        
-        productInfo = JSON.parse(productInfo.textContent);   
+  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+
+    await context.evaluate(async function () {
+      const productInfo = preFetchProductDetails();
+      if (productInfo && productInfo !== undefined) {
+        addEleToDoc('divhiddenproductAvgRating', productInfo['productAvgRating']);
+        addEleToDoc('divhiddenproductRatingCount', productInfo['productRatingCount']);
+        addEleToDoc('divhiddenproductGrossPrice', productInfo['productGrossPrice'].analyticsFormat);
+      }
+      function preFetchProductDetails() {
+        let productInfo = findProductDetails('//section//@data-productinfofordatalayer');
+        productInfo = productInfo && JSON.parse(productInfo.textContent);
         return productInfo;
-      }      
-            
-      function findProductDetails (xpath) {        
-        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;                        
+      }
+
+      function findProductDetails(xpath) {
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         const productDetails = element;
         return productDetails;
       }
 
-      function addEleToDoc (key, value) {
+      function addEleToDoc(key, value) {
         const prodEle = document.createElement('div');
         prodEle.id = key;
         prodEle.textContent = value;
         prodEle.style.display = 'none';
         document.body.appendChild(prodEle);
-      }      
+      }
 
-      if(document.querySelector('div[class="clipThumbnail"]')){
+      if (document.querySelector('div[class="clipThumbnail"]') && document.querySelector('div[class="clipThumbnail"]') !== undefined) {
         document.querySelector('div[class="clipThumbnail"]').click();
         function delay(time) {
-          return new Promise(function(resolve) { 
-              setTimeout(resolve, time)
+          return new Promise(function (resolve) {
+            setTimeout(resolve, time)
           });
-       }
-       await delay(2000);
+        }
+        await delay(2000);
         document.querySelector('div[id="clipVideo"] img[class*="Playbtn"]').click();
-        videoSrc=document.querySelector('div[id="clipVideo"] video source').getAttribute('src');
-        console.log(videoSrc+' is video link');
-        addEleToDoc('videoLink',videoSrc);
+        videoSrc = document.querySelector('div[id="clipVideo"] video source').getAttribute('src');
+        console.log(videoSrc + ' is video link');
+        addEleToDoc('videoLink', videoSrc);
       }
     });
-       return await context.extract(productDetails, { transform: transformParam });       
-  }, 
+    return await context.extract(productDetails, { transform: transformParam });
+  },
 };
