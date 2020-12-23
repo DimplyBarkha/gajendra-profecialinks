8e07b9ee1d5104c8d6a97bfee104988dd9bd5d0a
Rajeev <rajeev.ranjan@synerzip.com>
2020-12-23 16:49:07 +0530

    #2559

--- src/library/product/details/stores/l/linenchest_fr_ca/CA/execute.js
+++ src/library/product/details/stores/l/linenchest_fr_ca/CA/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'CA',
     store: 'linenchest_fr_ca',
     domain: 'linenchest.com',
-    loadedSelector: null,
-    noResultsXPath: null,
+    // loadedSelector: null,
+    noResultsXPath: '//p[contains(.,"Le produit que vous recherchez semble ne plus être offert")]',
     zipcode: '',
   },
 };
