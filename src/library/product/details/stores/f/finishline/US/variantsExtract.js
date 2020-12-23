f6e7e09a7bccc93ab2648c9e0e73e30922aaf45f
Bindu Datla <bindu.datla@import.io>
2020-12-23 10:02:35 -0500

    updated for variants extractor

---
+++ src/library/product/details/stores/f/finishline/US/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'US',
+    store: 'finishline',
+    transform: null,
+    domain: 'finishline.com',
+    zipcode: "''",
+  },
+};
