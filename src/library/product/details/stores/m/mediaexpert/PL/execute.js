c511f9818535df2b9e1061049dc8f24ca0bc179e
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2020-12-23 17:10:33 +0530

    moved navigation to execute.js

--- src/library/product/details/stores/m/mediaexpert/PL/execute.js
+++ src/library/product/details/stores/m/mediaexpert/PL/execute.js
@@ -9,4 +9,59 @@ module.exports = {
     noResultsXPath: '//div[contains(@class, "is-noResults")]',
     zipcode: '',
   },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const searchUrl = `https://www.mediaexpert.pl/search?query[menu_item]=&query[querystring]=${inputs.id}`;
+    await context.goto(searchUrl, {
+      timeout: 30000,
+      waitUntil: 'load',
+      checkBlocked: true,
+      js_enabled: true,
+      css_enabled: false,
+      random_move_mouse: true,
+    });
+
+    const productDetailsLink = await context.evaluate(function (inputs) {
+      const productList = document.querySelectorAll('div.c-grid_col.is-grid-col-1');
+      for (let i = 0; i < productList.length; i++) {
+        const productCodeEle = productList[i].querySelector('div.is-productCode');
+        if (productCodeEle) {
+          const productCode = productCodeEle.textContent.trim();
+          if (productCode.includes(inputs.id) || productList.length === 1) {
+            const productDetailsEle = productList[i].querySelector('div.c-offerBox_photo a');
+            return productDetailsEle ? productDetailsEle.getAttribute('href') : null;
+          }
+        }
+      }
+    }, inputs);
+    if (productDetailsLink) {
+      console.log('found product');
+      const url = `https://www.mediaexpert.pl${productDetailsLink}`;
+      await context.goto(url, {
+        timeout: 60000,
+        waitUntil: 'load',
+        checkBlocked: true,
+        js_enabled: true,
+        css_enabled: false,
+        random_move_mouse: true,
+      });
+    } else {
+      const noProductsFound = await context.evaluate(function (inputs) {
+        const noResults = document.querySelector('div.is-noResults');
+        return noResults;
+      });
+      if (noProductsFound || noProductsFound === undefined) {
+        try {
+          throw new Error('Product not found for the given input');
+        } catch (e) {
+          console.log('Error while throwing custom error');
+        }
+      }
+    }
+    await context.waitForNavigation({ timeout: 60000, waitUntil: 'networkidle0' });
+  },
 };
--- src/library/product/details/stores/m/mediaexpert/PL/extract.js
+++ src/library/product/details/stores/m/mediaexpert/PL/extract.js
@@ -11,41 +11,6 @@ module.exports = {
     noResultsXPath: null,
   },
   implementation: async (inputs, { country, domain, transform: transformParam }, context, { productDetails }) => {
-    const productDetailsLink = await context.evaluate(function (inputs) {
-      const productList = document.querySelectorAll('div.c-grid_col.is-grid-col-1');
-      for (let i = 0; i < productList.length; i++) {
-        const productCodeEle = productList[i].querySelector('div.is-productCode');
-        if (productCodeEle) {
-          const productCode = productCodeEle.textContent.trim();
-          if (productCode.includes(inputs.id)) {
-            const productDetailsEle = productList[i].querySelector('div.c-offerBox_photo a');
-            return productDetailsEle ? productDetailsEle.getAttribute('href') : null;
-          }
-        }
-      }
-    }, inputs);
-    if (productDetailsLink) {
-      console.log('found product');
-      const url = `https://www.mediaexpert.pl${productDetailsLink}`;
-      await context.goto(url, {
-        timeout: 60000,
-        waitUntil: 'load',
-        checkBlocked: true,
-        js_enabled: true,
-        css_enabled: false,
-        random_move_mouse: true,
-      });
-    } else {
-      const noProductsFound = await context.evaluate(function (inputs) {
-        const noResults = document.querySelector('div.is-noResults');
-        return noResults;
-      });
-      if (noProductsFound) {
-        throw new Error('Product not found for the given input');
-      }
-    }
-
-    await context.waitForNavigation({ timeout: 60000, waitUntil: 'networkidle0' });
     await context.evaluate(async function () {
       const applyScroll = async function (context) {
         let scrollTop = 0;
