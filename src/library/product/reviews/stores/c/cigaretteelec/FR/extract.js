f02fbfa3e8085286954b6a62685fbac88be34942
alscotty <alawrencescott@gmail.com>
2020-12-22 16:55:23 -0800

    add reviews pagination

--- src/library/product/reviews/stores/c/cigaretteelec/FR/extract.js
+++ src/library/product/reviews/stores/c/cigaretteelec/FR/extract.js
@@ -1,11 +1,24 @@
+const { transform } = require('../../../../shared');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productReviews } = dependencies;
+  return await context.extract(productReviews, { transform, type: 'MERGE_ROWS' });
+}
 
 module.exports = {
   implements: 'product/reviews/extract',
   parameterValues: {
     country: 'FR',
     store: 'cigaretteelec',
-    transform: null,
+    transform,
     zipcode: '',
     domain: 'cigaretteelec.fr',
   },
+  implementation,
 };
