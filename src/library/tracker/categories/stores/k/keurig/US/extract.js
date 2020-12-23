427fe86eeab1c4fe27e694c0393677288a05e61c
mania307 <maniakalnaa@gmail.com>
2020-12-23 14:48:38 +0100

    rebuilt menu extractor

--- src/library/tracker/categories/stores/k/keurig/US/extract.js
+++ src/library/tracker/categories/stores/k/keurig/US/extract.js
@@ -1,19 +1,65 @@
-
 async function implementation(inputs, parameters, context, dependencies) {
   const { productMenu } = dependencies;
+
   await context.evaluate(async function () {
-    function addProp(selector, iterator, propName, value) {
-      document.querySelectorAll(selector)[iterator].setAttribute(propName, value);
-    }
-    const allCategories = document.querySelectorAll('ul.top-nav-items ul.Lc li.sub-category>a, ul.top-nav-items li.top-header>a');
-    if (allCategories.length) {
-      for (let i = 0; i < allCategories.length; i++) {
-        addProp('ul.top-nav-items ul.Lc li.sub-category>a, ul.top-nav-items li.top-header>a',
-          i, 'category_url', allCategories[i].href);
+    function addHiddenDiv(id, content, parentDiv = null) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      if (parentDiv) {
+        parentDiv.appendChild(newDiv);
+      } else {
+        document.body.appendChild(newDiv);
       }
+      return newDiv;
     }
+
+    // categories
+    const mainCategories = document.querySelectorAll('ul.top-nav-items>li.top-header');
+    mainCategories.forEach(categoryNode => {
+      const category = categoryNode.querySelector('a');
+      const url = categoryNode.querySelector('a') ? categoryNode.querySelector('a').getAttribute('href') : null;
+      const newCatDiv = addHiddenDiv('categories', '');
+      if (url !== null) {
+        const link = url.startsWith('http') ? url : 'https://www.keurig.com' + url;
+        addHiddenDiv('category', category.textContent, newCatDiv);
+        addHiddenDiv('categoryUrl', link, newCatDiv);
+      }
+
+      // subcategories
+      const subCategories = categoryNode.querySelectorAll('div.exp-67 div.section');
+      subCategories.forEach(subCategory => {
+        const subCategoryName = subCategory.querySelector('h6:first-child') ? subCategory.querySelector('h6:first-child').textContent : null;
+        const newSubCatDiv = addHiddenDiv('categories', '');
+        // const url = subCategory.querySelector('a.at_subNavHeadingLink:first-child') ? subCategory.querySelector('a.at_subNavHeadingLink:first-child').getAttribute('href') : null;
+        if (subCategoryName) {
+          addHiddenDiv('category', category.textContent, newSubCatDiv);
+          addHiddenDiv('category', subCategoryName, newSubCatDiv);
+        }
+
+
+
+        // subsubcategories
+        const subsubCategories = subCategory.querySelectorAll('ul>li.sub-category>a');
+        subsubCategories.forEach(subsubCategory => {
+          const subsubCategoryName = subsubCategory.textContent;
+          const newSubSubCatDiv = addHiddenDiv('categories', '');
+          const url = subsubCategory.getAttribute('href').startsWith('http') ? subsubCategory.getAttribute('href') : 'https://www.keurig.com' + subsubCategory.getAttribute('href');
+          if (url) {
+            addHiddenDiv('category', category.textContent, newSubSubCatDiv);
+            addHiddenDiv('category', subCategoryName, newSubSubCatDiv);
+            addHiddenDiv('category', subsubCategoryName, newSubSubCatDiv);
+            addHiddenDiv('categoryUrl', url, newSubSubCatDiv);
+          }
+
+        });
+      });
+    });
   });
+
   return await context.extract(productMenu);
+
 }
 module.exports = {
   implements: 'tracker/categories/extract',
