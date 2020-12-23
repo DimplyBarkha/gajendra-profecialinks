f4e6112d4e464121dc9dd2b9728cbf3a92531c24
Vishwas Sinha <vishwas_s@hcl.com>
2020-12-23 13:42:05 +0530

    code commit for AmazonLG IN

---
+++ src/library/product/details/stores/a/amazonLg/IN/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'amazon.in',
+    prefix: null,
+    country: 'IN',
+    store: 'amazonLg',
+    url: 'https://amazon.in/dp/{id}?th=1&psc=1&showDetailProductDesc=1',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/amazonLg/IN/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'IN',
+    store: 'amazonLg',
+    domain: 'amazon.in',
+    zipcode: '',
+  },
+};
