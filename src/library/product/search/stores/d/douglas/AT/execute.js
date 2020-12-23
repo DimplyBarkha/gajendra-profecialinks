ab82bfdc11535a97d8ae974a52a2c6d83d645197
abhisek.d <abhisek.d@osmosys.asia>
2020-12-24 00:30:29 +0530

    added changes for getting the data using API

--- src/library/product/search/stores/d/douglas/AT/execute.js
+++ src/library/product/search/stores/d/douglas/AT/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'douglas',
     domain: 'douglas.at',
     noResultsXPath: null,
-    url: 'https://www.douglas.at/jsapi/v2/products/search?currentPage=0&pageSize=48&query={searchTerms}&fields=FULL&crealyticsSpaEnabled=true#[!opt!]{"type":"json"}[/!opt!]',
+    url: 'https://www.douglas.at/jsapi/v2/products/search?currentPage=0&pageSize=180&query={searchTerms}&fields=FULL&crealyticsSpaEnabled=true#[!opt!]{"type":"json"}[/!opt!]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/d/douglas/AT/paginate.js
+++ src/library/product/search/stores/d/douglas/AT/paginate.js
@@ -10,8 +10,8 @@ module.exports = {
     loadedSelector: null,
     noResultsXPath: null,
     openSearchDefinition: {
-      indexOffset: 20,
-      template: 'https://www.douglas.at/jsapi/v2/products/search?currentPage={indexOffset}&pageSize=48&query={searchTerms}&fields=FULL&crealyticsSpaEnabled=true#[!opt!]{"type":"json"}[/!opt!]',
+      offset: 24,
+      template: 'https://www.douglas.at/jsapi/v2/products/search?currentPage={offset}&pageSize=180&query={searchTerms}&fields=FULL&crealyticsSpaEnabled=true#[!opt!]{"type":"json"}[/!opt!]',
     },
     domain: 'douglas.at',
     zipcode: '',
