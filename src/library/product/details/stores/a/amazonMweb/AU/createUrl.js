123db61877424fa863664d701630fd8815090020
Abhishek Kumar Mishra <abhishekkumar_mishra@hcl.com>
2020-12-23 11:39:55 +0530

    amazonmweb local commit

---
+++ src/library/product/details/stores/a/amazonMweb/AU/createUrl.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+    domain: 'amazon.com.au',
+    prefix: null,
+    url: 'https://www.amazon.com.au/dp/{id}?th=1&psc=1',
+    country: 'AU',
+    store: 'amazonMweb',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/amazonMweb/AU/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'AU',
+    store: 'amazonMweb',
+    domain: 'amazon.com.au',
+    zipcode: '',
+  },
+};
