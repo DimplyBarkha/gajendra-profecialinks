9ff3203bd92948227aaf7b1c0c20946af5991a06
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-23 17:44:06 +0530

    updated witb

--- src/library/product/details/stores/t/technopark/format.js
+++ src/library/product/details/stores/t/technopark/format.js
@@ -5,17 +5,18 @@
  */
 const transform = (data) => {
   const clean = text => text.toString()
-  .replace(/\r\n|\r|\n/g, ' ')
-  .replace(/&amp;nbsp;/g, ' ')
-  .replace(/&amp;#160/g, ' ')
-  .replace(/\u00A0/g, ' ')
-  .replace(/\s{2,}/g, ' ')
-  .replace(/"\s{1,}/g, '"')
-  .replace(/\s{1,}"/g, '"')
-  .replace(/^ +| +$|( )+/g, ' ')
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
   // eslint-disable-next-line no-control-regex
-  .replace(/[\x00-\x1F]/g, '')
-  .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
+    .trim();
 
   for (const { group } of data) {
     for (const row of group) {
@@ -94,12 +95,15 @@ const transform = (data) => {
         row.price[0].text = row.price[0].text.replace(/\s/, '');
         console.log('row.price[0].text', row.price[0].text);
       }
+      if (row.inTheBoxText) {
+        row.inTheBoxText.forEach(elm => { elm.text = elm.text.replace(/(в комплекте$|да$)/, ''); });
+      }
     }
   }
   data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
     el.text = clean(el.text);
   }))));
-  
+
   return data;
 };
 
