039cfb33463a869376d5e9955b3698774d3bf76e
sivasoarlogic <siva@soarlogic.com>
2020-12-23 17:22:09 +0530

    UPDP issue fixed

--- src/library/navigation/goto/domains/ma/mamaguru.co.il.js
+++ src/library/navigation/goto/domains/ma/mamaguru.co.il.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'mamaguru.co.il',
-    timeout: null,
+    timeout: 100000,
     country: 'IL',
     store: 'mamaguru',
     zipcode: '',
