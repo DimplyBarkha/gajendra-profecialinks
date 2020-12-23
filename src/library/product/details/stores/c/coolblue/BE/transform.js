6179d291f9229944dcb81032136b7fa391287480
amit joshi <ajstyl2.4.2@gmail.com>
2020-12-23 18:02:18 +0530

    UAT failed issue fixed

---
+++ src/library/product/details/stores/c/coolblue/BE/transform.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * @param {ImportIO.Group[]} data
+ * @returns {ImportIO.Group[]}
+ */
+const cleanUp = (data, context) => {
+  for (const { group } of data) {
+    for (let row of group) {
+      if (row.aggregateRating) {
+        const ratingValue = row.aggregateRating[0].text;
+        row.aggregateRating = [{ text: ratingValue.replace('.', ',') }];
+      }
+    }
+  }
+  const clean = (text) =>
+    text
+      .toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  data.forEach((obj) =>
+    obj.group.forEach((row) =>
+      Object.keys(row).forEach((header) =>
+        row[header].forEach((el) => {
+          el.text = clean(el.text);
+        })
+      )
+    )
+  );
+
+  return data;
+};
+
+module.exports = { cleanUp };
