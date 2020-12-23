2731f140ee6a36eb0168b72b91186a3f79b1220d
MADHAN KUMAR SELVARAJ <madhan.selvaraj-odc@partner.import.io>
2020-12-23 21:06:31 +0530

    #2683 Update in details extractor.yaml file

--- src/library/product/details/stores/r/rs-online/UK/extract.js
+++ src/library/product/details/stores/r/rs-online/UK/extract.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -7,5 +6,26 @@ module.exports = {
     transform: null,
     domain: 'uk.rs-online.com',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop !== 20000) {
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          await stall(1000);
+        }
+        function stall(ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await applyScroll(context);
+    await context.extract(productDetails);
   },
 };
