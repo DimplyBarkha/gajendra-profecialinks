1f722c3c40df3744817a14b5323024208a59a3a5
astropaula <wojciechowska.fran@gmail.com>
2020-12-23 11:08:43 +0100

    Modified rank collection

--- src/library/product/search/stores/m/mecca/AU/extract.js
+++ src/library/product/search/stores/m/mecca/AU/extract.js
@@ -6,7 +6,7 @@ async function implementation (inputs, parameters, context, dependencies) {
 
   await context.evaluate(async () => {
     // add rank attribute
-    var rank = document.querySelectorAll('div[class="product-tile"]');
+    var rank = document.querySelectorAll('div[class="product-tile"], li[class="col-xs-6 col-sm-4 col-md-3 grid-product"]');
 
     rank.forEach((element, index) => {
       element.setAttribute('rank', (index + 1).toString());
