8c69dfe91ef044f2e135c557d4ddd11287da3fd6
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-12-23 18:35:31 +0530

    #3270. Cookie popup issue fixed.

--- src/library/product/reviews/stores/a/argos/UK/extract.js
+++ src/library/product/reviews/stores/a/argos/UK/extract.js
@@ -1,10 +1,10 @@
-// const { transform } = require('./format');
+const { transform } = require('./format');
 module.exports = {
   implements: 'product/reviews/extract',
   parameterValues: {
     country: 'UK',
     store: 'argos',
-    transform: null,
+    transform,
     domain: 'argos.co.uk',
     zipcode: '',
   },
@@ -14,41 +14,25 @@ module.exports = {
     context,
     dependencies,
   ) => {
-    // const basicDetails = await context.evaluate(() => {
-    //   const productRows = document.querySelectorAll('div[class="css-1dbjc4n"] > div[class="css-1dbjc4n r-18u37iz r-tzz3ar"]');
-    //   let gotoUrl = '';
-    //   let skuId = '';
-    //   if (productRows && productRows.length > 0) {
-    //     const products = productRows[0].querySelectorAll('div[class="css-1dbjc4n r-18u37iz r-tzz3ar"]');
-    //     if (products && products.length > 0) {
-    //       const productLink = products[0].querySelector('a');
-    //       if (productLink) {
-    //         const productUrl = productLink.getAttribute('href');
-    //         skuId = window.location.href.split('?')[1].split('=')[1];
-    //         gotoUrl = `https://www.cvs.com${productUrl}?skuid=${skuId}`;
-    //       }
-    //     }
-    //   }
-    //   return { gotoUrl, skuId };
-    // });
+    try {
+      console.log('Clicking on cookies button.');
+      await context.waitForSelector('button[id*="consent_prompt_submit"]', { timeout: 10000 });
+      await context.evaluate(function () {
+        document.querySelector('button[id*="consent_prompt_submit"]').click();
+      });
+    } catch (err) {
+      console.log('Clicking on cookies button failed.');
+    }
 
-    // if (basicDetails.gotoUrl) {
-    //   await context.goto(basicDetails.gotoUrl, { timeout: 60000 });
-    // }
-
-    // await new Promise(resolve => setTimeout(resolve, 10000));
-    // const cookieCheck = await context.evaluate(async () => {
-    //   const cookie = document.querySelector('div[class*="consent_prompt explicit_consent"]') ? document.querySelector('div[class*="consent_prompt explicit_consent"]') : '';
-    //   if (cookie && cookie.innerHTML.length) {
-    //     await new Promise(resolve => setTimeout(resolve, 10000));
-    //     await context.waitForSelector('div[class*="consent_prompt explicit_consent"]');
-    //     await context.click('button[id*="consent_prompt_submit"]');
-    //   }
-    // });
-
-    await new Promise(resolve => setTimeout(resolve, 10000));
-    await context.click('a[data-test*="reviews-flag-link"]');
-    await context.waitForSelector('div[id*="reviews"]');
+    try {
+      await context.waitForSelector('a[data-test*="reviews-flag-link"', { timeout: 10000 });
+      await context.evaluate(function () {
+        document.querySelector('a[data-test*="reviews-flag-link"').click();
+      });
+      await context.waitForSelector('div[id*="reviews"]');
+    } catch (err) {
+      console.log(err);
+    }
 
     const checkIfReviewIsFromLast30Days = (lastDate, reviewDate) => {
       const timestamp = new Date(lastDate).getTime() - (30 * 24 * 60 * 60 * 1000);
@@ -116,50 +100,6 @@ module.exports = {
       allReviewsCount = await getAllReviewsCount();
       lastReviewDate = await getLastReviewDate();
     }
-
-    // await context.evaluate(() => {
-    //   const addHiddenDiv = (id, content) => {
-    //     const newDiv = document.createElement('div');
-    //     newDiv.id = id;
-    //     newDiv.textContent = content;
-    //     newDiv.style.display = 'none';
-    //     document.body.appendChild(newDiv);
-    //   };
-
-    // const schemaJsonSscript = document.querySelector('script[id="schema-json-ld"]');
-    // if (schemaJsonSscript) {
-    //   const schemaJson = JSON.parse(schemaJsonSscript.textContent);
-    //   const brand = schemaJson[0].brand;
-    //   if (brand) {
-    //     addHiddenDiv('ii_brand', brand);
-    //   }
-    // }
-    // if (basicDetails.skuId) {
-    //   addHiddenDiv('ii_sku', basicDetails.skuId);
-    // }
-    // if (basicDetails.gotoUrl) {
-    //   addHiddenDiv('ii_productUrl', basicDetails.gotoUrl);
-    // }
-    // });
-
-    // const readMoreCount = await context.evaluate(async () => {
-    //   const readMoreButtons = document.querySelectorAll('div[class="css-901oao r-jw6ls8"]');
-    //   if (readMoreButtons && readMoreButtons.length > 0) {
-    //     readMoreButtons.forEach((button, index) => {
-    //       if (button.textContent.indexOf('Read More') >= 0) {
-    //         button.setAttribute('id', `ii_ReadMore${index}`);
-    //       }
-    //     });
-    //     return readMoreButtons.length;
-    //   }
-    //   return 0;
-    // });
-
-    // if (readMoreCount) {
-    //   for (let i = 0; i < readMoreCount; i++) {
-    //     await context.click(`div[id="ii_ReadMore${i}"]`);
-    //   }
-    // }
 
     await new Promise(resolve => setTimeout(resolve, 5000));
 
