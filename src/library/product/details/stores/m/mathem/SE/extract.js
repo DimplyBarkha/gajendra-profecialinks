bb7f9b1f912e525f6369a0bf0330358b375bdf00
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-23 17:51:20 +0530

    mathem

--- src/library/product/details/stores/m/mathem/SE/extract.js
+++ src/library/product/details/stores/m/mathem/SE/extract.js
@@ -25,16 +25,13 @@ async function implementation(
       newDiv.id = id;
       newDiv.textContent = content;
       newDiv.style.display = 'none';
-      const originalDiv = document.querySelectorAll("i[class='fa fa-star']")[index];
+      const originalDiv = document.querySelectorAll("span[class='product-name']")[index];
       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
     }
     let firstChildNode;
-    let finalaggregateRating;
-    const aggregateRating = document.querySelectorAll("div[class='rating']")
-    for (let k = 0; k < aggregateRating.length; k++) {
-      firstChildNode = aggregateRating[k].getElementsByClassName('fa fa-star').length
-      addHiddenDiv('aggregateRating', firstChildNode, k);
-    }
+    const aggregateRating = document.querySelectorAll("div[class='full-width'] i[class='fa fa-star']").length
+      addHiddenDiv('aggregateRating',aggregateRating ,0);
+    // }
   });
   //rank end
   return await context.extract(productDetails, { transform });
