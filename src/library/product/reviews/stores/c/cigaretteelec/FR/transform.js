4f5b60c6028867ed33421bba8558c5f69b8c325a
alscotty <alawrencescott@gmail.com>
2020-12-22 17:38:44 -0800

    add xpaths to reviews

---
+++ src/library/product/reviews/stores/c/cigaretteelec/FR/transform.js
@@ -0,0 +1,34 @@
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+  // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
+    .trim();
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.reviewDate) {
+        const dateArr = row.reviewDate[0].text.split('/');
+        const day = dateArr[0];
+        const month = dateArr[1];
+        const year = dateArr[2];
+        row.reviewDate = [{ text: `${year}-${month}-${day}` }];
+      }
+
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
+    }
+  }
+  return data;
+};
+module.exports = {
+  transform,
+};
