3dbd09b41b61ca9213bc2be2ceadf4851fe8c237
ravitejaw324 <ravi.w@partner.import.io>
2020-12-24 14:04:33 +0530

    added an implementation to get SKU from DOM

--- src/library/product/search/stores/k/kogan/AU/extract.js
+++ src/library/product/search/stores/k/kogan/AU/extract.js
@@ -1,7 +1,7 @@
 //const { transform } = require('../../../../shared');
 const { transform } = require('../shared');
 
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -11,18 +11,17 @@ async function implementation (
   const { productDetails } = dependencies;
 
   await context.evaluate(async function () {
-    let tempUrl ='';
-    let tempCheck = document.evaluate('//link[@rel="canonical"]//@href', document).iterateNext();      
-    if(tempCheck !=null)
-    {
-      tempUrl = document.evaluate('//link[@rel="canonical"]//@href', document).iterateNext().textContent.trim();                        
+    let tempUrl = '';
+    let tempCheck = document.evaluate('//link[@rel="canonical"]//@href', document).iterateNext();
+    if (tempCheck != null) {
+      tempUrl = document.evaluate('//link[@rel="canonical"]//@href', document).iterateNext().textContent.trim();
     }
-    
+
     if (tempUrl.indexOf('kogan.com') === -1) {
       tempUrl = "https://www.kogan.com" + tempUrl;
     }
-    
-    function addHiddenDiv (node, id, content) {
+
+    function addHiddenDiv(node, id, content) {
       const newDiv = document.createElement('tempWebUrl-id');
       newDiv.id = id;
       newDiv.textContent = content;
@@ -30,10 +29,26 @@ async function implementation (
       node.appendChild(newDiv);
     }
 
+    try {
+      let scriptData = k3; //k3 is a variable declared in a script tag.
+      if (scriptData.bootstrapData && scriptData.bootstrapData.results && scriptData.bootstrapData.results.objects) {
+        const prodData = scriptData.bootstrapData.results.objects;
+        const allNodes = document.querySelectorAll('._3dbuB._2TkM7'); //all product rows
+        let iter = 0;
+        for (iter = 0; iter < prodData.length; iter++) {
+          if (prodData[iter].sku && allNodes[iter]) {
+            allNodes[iter].insertAdjacentHTML('afterbegin', `<div id="sku" style="display: none">${prodData[iter].sku}</div>`);
+          }
+        }
+      } 
+    } catch(er) {
+      console.log("ERROR!! :"+er.message);
+    }
+
     let i = 0;
-    document.querySelectorAll('div.rs-infinite-scroll > div._3dbuB').forEach(node => {    
-      console.log(node);      
-      addHiddenDiv(node,'tempWebUrl',tempUrl);
+    document.querySelectorAll('div.rs-infinite-scroll > div._3dbuB').forEach(node => {
+      console.log(node);
+      addHiddenDiv(node, 'tempWebUrl', tempUrl);
       i++;
     });
   });
@@ -48,6 +63,6 @@ module.exports = {
     transform,
     domain: 'kogan.com',
     zipcode: '',
-  },    
+  },
   implementation
 };
