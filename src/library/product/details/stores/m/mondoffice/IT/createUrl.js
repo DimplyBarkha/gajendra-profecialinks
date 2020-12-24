862f5601427136d5f31f94625dd0b48d6a8220c1
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-12-24 13:19:25 +0200

    add sku field

--- src/library/product/details/stores/m/mondoffice/IT/createUrl.js
+++ src/library/product/details/stores/m/mondoffice/IT/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'mondoffice.com',
     prefix: null,
-    url: 'https://www.mondoffice.com/',
+    url: 'https://www.mondoffice.com/INTERSHOP/web/WFS/RAJA-MONDOFFICE-Site/it_IT/-/EUR/ViewParametricSearch-ProductPaging?PageSize=9999&SortingAttribute=&ViewType=1&SearchTerm={id}',
     country: 'IT',
     store: 'mondoffice',
     zipcode: '',
