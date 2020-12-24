977ac0739e31afc8581497f4ddb74acf6ca836a2
osmakash <akashkumar.p@osmosys.asia>
2020-12-24 09:59:37 +0530

    commiting the changes made on API

--- src/library/product/search/stores/d/douglas/AT/paginate.js
+++ src/library/product/search/stores/d/douglas/AT/paginate.js
@@ -10,8 +10,7 @@ module.exports = {
     loadedSelector: null,
     noResultsXPath: null, 
     openSearchDefinition: {
-      offset: 1,
-      template: 'https://www.douglas.at/jsapi/v2/products/search?currentPage={offset}&pageSize=180&query={searchTerms}&fields=FULL&crealyticsSpaEnabled=true#[!opt!]{"type":"json"}[/!opt!]',
+      template: 'https://www.douglas.at/jsapi/v2/products/search?currentPage={page}&pageSize=180&query={searchTerms}&fields=FULL&crealyticsSpaEnabled=true#[!opt!]{"type":"json"}[/!opt!]',
     },
     domain: 'douglas.at',
     zipcode: '',
