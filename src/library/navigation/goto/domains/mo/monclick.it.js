e830c713b8d2c5eebf462ad0c368def2566d1d8d
Richa Tiwari <richa.t@osmosys.asia>
2020-12-24 17:20:34 +0530

    Added code for CTR

--- src/library/navigation/goto/domains/mo/monclick.it.js
+++ src/library/navigation/goto/domains/mo/monclick.it.js
@@ -1,20 +1,16 @@
-
 module.exports = {
-  implements: 'navigation/goto',
-  parameterValues: {
-    domain: 'monclick.it',
-    timeout: null,
-    country: 'IT',
-    store: 'monclick',
-    zipcode: '',
-  },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
-    try{
-      await context.waitForSelector('section.mk-product-page')
-    }catch(err){
-      console.log('product page did not load')
-    }
-  },
+    implements: 'navigation/goto',
+    parameterValues: {
+        domain: 'monclick.it',
+        timeout: null,
+        country: 'IT',
+        store: 'monclick',
+        zipcode: '',
+    },
+    implementation: async(inputs, parameterValues, context, dependencies) => {
+        let url = `${inputs.url}`;
+
+        url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+        await context.goto(url, { timeout: parameterValues.timeout });
+    },
 };
--- src/library/product/details/stores/m/monclick/IT/extract.js
+++ src/library/product/details/stores/m/monclick/IT/extract.js
@@ -1,29 +1,41 @@
 const { transform } = require('./format');
 module.exports = {
-  implements: 'product/details/extract',
-  parameterValues: {
-    country: 'IT',
-    store: 'monclick',
-    transform,
-    domain: 'monclick.it',
-    zipcode: '',
-  },
-  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
-    try {
-      await new Promise(resolve => setTimeout(resolve, 5000));
-      await context.click('div[class*="image-content-gallery"] li[class*="gallery-video"]');
-      await context.waitForSelector('div[class*=product-gallery-overlay]');
-      await context.evaluate(async () => {
-        const element = document.querySelector('footer#footer');
-        if (element) {
-          element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-          await new Promise((resolve) => setTimeout(resolve, 10000));
+    implements: 'product/details/extract',
+    parameterValues: {
+        country: 'IT',
+        store: 'monclick',
+        transform,
+        domain: 'monclick.it',
+        zipcode: '',
+    },
+    implementation: async({ url }, { country, domain, transform }, context, { productDetails }) => {
+        try {
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            await context.click('div[class*="image-content-gallery"] li[class*="gallery-video"]');
+            await context.waitForSelector('div[class*=product-gallery-overlay]');
+            await context.evaluate(async() => {
+                const element = document.querySelector('footer#footer');
+                if (element) {
+                    element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+                    await new Promise((resolve) => setTimeout(resolve, 10000));
+                }
+                const syndiPowerpage = document.querySelector('.syndigo-shadowed-powerpage');
+                let hasComparisonTable = 'No';
+                if (syndiPowerpage) {
+                    console.log('Shadow root!');
+                    const headings = Array.from(syndiPowerpage.querySelectorAll('h2'));
+                    headings.forEach(h2 => {
+                        if (h2.innerText.includes('Tabella di comparazione')) {
+                            hasComparisonTable = 'Yes';
+                        }
+                    });
+                    document.body.setAttribute('has-comparison-table', hasComparisonTable);
+                }
+            });
+        } catch (e) {
+            console.log(e);
         }
-      });
-    } catch (e) {
-      console.log(e);
-    }
-    await new Promise(resolve => setTimeout(resolve, 10000));
-    return await context.extract(productDetails, { transform });
-  },
+        await new Promise(resolve => setTimeout(resolve, 10000));
+        return await context.extract(productDetails, { transform });
+    },
 };
