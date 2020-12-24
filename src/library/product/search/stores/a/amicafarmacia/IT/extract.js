a875ab516d73fe8d1f82785ed646528a5dc682a6
harishkhattri <harish.khattri@synerzip.com>
2020-12-24 16:43:44 +0530

    Fixed issues with search url and product code

--- src/library/product/search/stores/a/amicafarmacia/IT/extract.js
+++ src/library/product/search/stores/a/amicafarmacia/IT/extract.js
@@ -1,6 +1,6 @@
 const { transform } = require('./format');
 
-async function implementation(
+async function implementation (
   inputs,
   parameters,
   context,
@@ -21,7 +21,7 @@ async function implementation(
           break;
         }
       }
-      function stall(ms) {
+      function stall (ms) {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
             resolve();
@@ -31,89 +31,20 @@ async function implementation(
     });
   };
 
-  const getSKU = async function (context) {
-    const mainUrl = await context.evaluate(async function () {
-      return document.URL;
-    });
-    console.log('mainUrl', mainUrl);
-    await applyScroll(context);
-  
-    var results = await context.evaluate(async function () {
-      const result = [];
-      (document.querySelectorAll('button.stock.unavailable')).forEach((elem) => {
-        result.push({
-          url: elem.parentElement.getAttribute('href'),
-          code: ''
-        });
-      });
-      return result;
-    });
-  
-    for (var i = 0; i < results.length; i++) {
-      console.log(`URL: ${results[i].url}`);
-      await context.goto(results[i].url, {
-        timeout: 10000000,
-        waitUntil: 'load',
-        checkBlocked: true,
-        js_enabled: true,
-        css_enabled: false,
-        random_move_mouse: true,
-      });
-      const productCode = await context.evaluate(async function () {
-        const productCode = document.querySelector("[itemprop='sku']").textContent;
-        return productCode;
-      });
-      results[i].code = productCode;
-      console.log(`SKU: ${productCode}`);
-      await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-    };
-  
-    await context.goto(mainUrl, { timeout: 1000000, waitUntil: 'load', checkBlocked: true });
-    await applyScroll(context);
-    await context.evaluate(async function (results) {
-      try {
-        var index = 0;
-        (document.querySelectorAll('button.stock.unavailable')).forEach((node) => {
-          const newDiv = document.createElement('input');
-          newDiv.type = 'hidden';
-          newDiv.name = 'product';
-          newDiv.value = results[0][index].code;
-          node.parentElement.appendChild(newDiv);
-          index++;
-        });
-      } catch (error) {
-        console.log('Error: ', error);
-      }
-    }, [results]);
-  };
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    addHiddenDiv('ii_searchUrl', window.location.href);
+  });
 
-  const getPageURL = async function (context) {
-    await context.evaluate(async function () {
-      let URL = window.location.href;
-      function addHiddenDiv (id, content, index) {
-        const newDiv = document.createElement('div');
-        newDiv.id = id;
-        newDiv.textContent = content;
-        newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll('div.product-item-info')[index];
-        originalDiv.appendChild(newDiv);
-        console.log("child appended " + index);
-      }
-      const product = document.querySelectorAll('div.product-item-info');
-      // select query selector and loop and add div
-      for (let i = 0; i < product.length; i++) {
-        addHiddenDiv('page_url', URL, i);
-      }
-    });
-  };
-  
-  await getSKU(context);
-  await new Promise((resolve, reject) => setTimeout(resolve, 6000));
   await applyScroll(context);
   await new Promise((resolve, reject) => setTimeout(resolve, 6000));
-  await getPageURL(context);
-  await new Promise((resolve, reject) => setTimeout(resolve, 6000));
-  return await context.extract(productDetails, {transform});
+  return await context.extract(productDetails, { transform });
 }
 
 module.exports = {
@@ -125,5 +56,5 @@ module.exports = {
     domain: 'amicafarmacia.com',
     zipcode: "''",
   },
-  implementation
+  implementation,
 };
--- src/library/product/search/stores/a/amicafarmacia/IT/format.js
+++ src/library/product/search/stores/a/amicafarmacia/IT/format.js
@@ -4,15 +4,15 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-    for (const { group } of data) {
-        for (const row of group) {            
-            if (row.productUrl) {
-                row.productUrl = [{text: row.productUrl[0].text, xpath: row.productUrl[0].xpath}];
-            }
-        }        
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.productUrl) {
+        row.productUrl = [{ text: row.productUrl[0].text, xpath: row.productUrl[0].xpath }];
+      }
     }
+  }
 
-    const clean = text => text.toString()
+  const clean = text => text.toString()
     .replace(/\r\n|\r|\n/g, ' ')
     .replace(/&amp;nbsp;/g, ' ')
     .replace(/&amp;#160/g, ' ')
@@ -25,30 +25,30 @@ const transform = (data, context) => {
     .replace(/[\x00-\x1F]/g, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
 
-    const state = context.getState();
-    let orgRankCounter = state.orgRankCounter || 0;
-    let rankCounter = state.rankCounter || 0;
-    for (const { group } of data) {
-      for (const row of group) {
-        rankCounter += 1;
-        if (!row.sponsored) {
-          orgRankCounter += 1;
-          row.rankOrganic = [{ text: orgRankCounter }];
-        }
-        row.rank = [{ text: rankCounter }];
-        Object.keys(row).forEach(header => row[header].forEach(el => {
-          el.text = clean(el.text);
-        }));
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  for (const { group } of data) {
+    for (const row of group) {
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
       }
-    }
-    context.setState({ rankCounter });
-    context.setState({ orgRankCounter });
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      if (typeof el.text!=='undefined') {  
+      row.rank = [{ text: rankCounter }];
+      Object.keys(row).forEach(header => row[header].forEach(el => {
         el.text = clean(el.text);
-      }
-    }))));
-    return data;
-};  
+      }));
+    }
+  }
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    if (typeof el.text !== 'undefined') {
+      el.text = clean(el.text);
+    }
+  }))));
+  return data;
+};
 
 module.exports = { transform };
