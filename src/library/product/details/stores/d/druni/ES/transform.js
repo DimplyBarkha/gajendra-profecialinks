4f6d7aa9bfeebedcd95d3312e2c09f0ef937da8b
kcs-mohammedbhanpurawala <mohammed.bhanpurawala@kcsitglobal.com>
2020-12-24 16:07:36 +0530

    druni category commit

--- src/library/product/details/stores/d/druni/ES/transform.js
+++ src/library/product/details/stores/d/druni/ES/transform.js
@@ -48,6 +48,14 @@ const transform = (data, context) => {
             row.availabilityText = [{ text: "In stock" }];
           }
         }
+        if(row.category){
+          let x = row.category[0].text
+          var rat = x.replace('window.dataLayer = window.dataLayer || []; window.dataLayer.push({"ecommerce":{"currencyCode":"EUR"},"pageType":"catalog_product_view","list":"detail"}); window.dataLayer.push(', '');
+          var semi = rat.replace(`); (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NQDSNPL');`,``);
+          var obj = JSON.parse(semi);
+          row.category = [{ text: obj.product.category }];
+          
+        }
       } catch (exception) {
         console.log("Error in transform", exception);
       }
