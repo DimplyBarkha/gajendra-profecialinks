c93b7b73337b889fabf1e85da82332f1c9e2408a
banderovskyi <roman1004b@gmail.com>
2020-12-24 12:59:10 +0200

    Implement a menu extractor

--- src/library/tracker/categories/stores/m/misterspex/DE/extract.js
+++ src/library/tracker/categories/stores/m/misterspex/DE/extract.js
@@ -1,4 +1,32 @@
+async function implementation (inputs, parameters, context, dependencies) {
+  const { productMenu } = dependencies;
+  await context.evaluate(async function () {
+    function addHiddenElem (className, content, elemTag = 'div', parentDiv = null) {
+      const newDiv = document.createElement(elemTag);
+      newDiv.className = className;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      if (parentDiv) {
+        parentDiv.appendChild(newDiv);
+      } else {
+        document.body.appendChild(newDiv);
+      }
+      return newDiv;
+    }
+    
+    const pageUrl = window.location.href.replace(/.$/, '');
+    const catsLinks = document.querySelectorAll('#nav-wrapper>div:first-child a.ButtonRoot-sc-apcbcy, #nav-wrapper>div:first-child a.LinkRoot-sc-17sm7bh');
 
+    catsLinks.forEach(catItem => {
+      const catName = catItem.textContent;
+      const catLink = catItem.getAttribute('href');
+      addHiddenElem('categoryName', catName);
+      addHiddenElem('categoryUrl', `${pageUrl}${catLink}`);
+    });
+  });
+  // await context.waitForXPath(`//div[contains(@class, "idsLoop-25")]`);
+  return await context.extract(productMenu);
+}
 module.exports = {
   implements: 'tracker/categories/extract',
   parameterValues: {
@@ -7,4 +35,5 @@ module.exports = {
     store: 'misterspex',
     zipcode: '',
   },
+  implementation
 };
