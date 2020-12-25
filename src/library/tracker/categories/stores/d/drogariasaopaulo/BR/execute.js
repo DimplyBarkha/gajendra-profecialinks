e28c6a73600a4cd6cc5c98c89a17c13278d850f8
Harish <harish.sathavara@kcsitglobal.com>
2020-12-25 11:18:16 +0530

    initial commit for menu only

---
+++ src/library/tracker/categories/stores/d/drogariasaopaulo/BR/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'BR',
+    store: 'drogariasaopaulo',
+    domain: 'drogariasaopaulo.com.br',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
