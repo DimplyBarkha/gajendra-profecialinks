c6cce645bf08f46f2b356ed2504db490e342c914
unknown <ramraut25@gmail.com>
2020-12-25 08:51:59 +0530

    Updated loaded selector and NoResultsXpath

--- src/library/product/search/stores/s/smithandcaugheys/NZ/execute.js
+++ src/library/product/search/stores/s/smithandcaugheys/NZ/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'smithandcaugheys',
     domain: 'smithandcaugheys.co.nz',
     url: 'https://www.smithandcaugheys.co.nz/search.aspx?q={searchTerms}',
-    loadedSelector: 'article[data-productid]',
-    noResultsXPath: '//div[contains(text(),"Sorry no results found")]',
+    loadedSelector: 'div.products article[data-productid]',
+    noResultsXPath: '//div[contains(text(),"Sorry no results found")] | //section[@id="ProductDisplay"]',
     zipcode: '',
   },
 };
