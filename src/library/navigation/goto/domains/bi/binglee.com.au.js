645753ac65d977ac9579837ee43a8ff80cb3296d
Avinash <avinash.kadam@profecialinks.com>
2020-12-27 11:11:12 +0530

    setBlockAds added

--- src/library/navigation/goto/domains/bi/binglee.com.au.js
+++ src/library/navigation/goto/domains/bi/binglee.com.au.js
@@ -1,11 +1,19 @@
-
 module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'binglee.com.au',
-    timeout: 500000,
+    timeout: 20000,
     country: 'AU',
     store: 'binglee',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
