67ea9a6591d422e3e353a0ca9b0c2abba207296d
sasz94 <sacha.bartlomiej@gmail.com>
2020-12-28 15:18:09 +0100

    Changed proxy & timeout & fixed reducing function & fixed xpaths (#1351)

--- src/library/navigation/goto/domains/ju/jumbo.com.js
+++ src/library/navigation/goto/domains/ju/jumbo.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'jumbo.com',
-    timeout: 20000,
+    timeout: 30000,
     country: 'NL',
     store: 'jumbo',
     zipcode: '',
--- src/library/product/details/stores/j/jumbo/NL/extract.js
+++ src/library/product/details/stores/j/jumbo/NL/extract.js
@@ -12,26 +12,29 @@ module.exports = {
   implementation: async function implementation (inputs, parameters, context, dependencies) {
     const { transform } = parameters;
     const { productDetails } = dependencies;
+    await context.evaluate(async () => {
+      const bulletTextElements = document.querySelectorAll('section.jum-additional-info.row li');
+      bulletTextElements.forEach(el => {
+        el.setAttribute('bullet', `|| ${el.textContent}`);
+      });
+    });
     const dataRef = await context.extract(productDetails, { transform });
-    function reduceInfoToOneField (field) {
+    function reduceInfoToOneField (field, separator = ' ') {
       if (field && field.length > 1) {
         let fieldText = '';
         field.forEach(element => {
-          fieldText += ' -' + element.text;
+          fieldText += element.text + separator;
         });
-        field[0].text = fieldText.replace(/\n/g, ': ');
+        field[0].text = fieldText.slice(0, -separator.length);
         return field.splice(1);
       }
     }
     const ingredientsList = dataRef[0].group[0].ingredientsList;
     const description = dataRef[0].group[0].description;
     reduceInfoToOneField(description);
-    reduceInfoToOneField(ingredientsList);
+    reduceInfoToOneField(ingredientsList, ', ');
     if (dataRef[0].group[0].variantId) {
       dataRef[0].group[0].variantId[0].text = dataRef[0].group[0].variantId[0].text.match(/:"(\w+)"/)[1];
-    }
-    if (dataRef[0].group[0].brandText) {
-      dataRef[0].group[0].brandText[0].text = dataRef[0].group[0].brandText[0].text.match(/\/\/w?w?w?\.?(.+)\./)[1];
     }
     if (dataRef[0].group[0].quantity) {
       dataRef[0].group[0].quantity[0].text = dataRef[0].group[0].quantity[0].text.match(/(\d+\s?\w+)$/)[1];
