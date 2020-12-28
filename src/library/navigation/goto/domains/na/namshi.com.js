9030639a4befd65770540597e0c44c93a682d5b7
shantanuDevlper <shantanu.kulkarni@profecialinks.com>
2020-12-28 11:13:34 +0530

    changes for UDPDP & intheboxtext

--- src/library/navigation/goto/domains/na/namshi.com.js
+++ src/library/navigation/goto/domains/na/namshi.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'namshi.com',
-    timeout: null,
+    timeout: 800000,
     country: 'AE',
     store: 'namshi',
     zipcode: "''",
