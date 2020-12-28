a1d09e5bc7435fe2860c44e954bf6299331300ac
Karthickravi <karthik.ravi-odc@partner.import.io>
2020-12-28 09:17:25 +0530

    new

--- src/library/product/search/stores/p/promofarma/ES/extract.js
+++ src/library/product/search/stores/p/promofarma/ES/extract.js
@@ -15,26 +15,33 @@ module.exports = {
   ) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
-    await context.evaluate(() => {
+    await context.evaluate(async () => {
+      try {
+        // @ts-ignore
+        document.querySelector('button[id="onetrust-accept-btn-handler"]').click()
+        await new Promise(r => setTimeout(r, 10000));
+
+      } catch (error) {
+
+      }
+      var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
       function addHiddenDiv(id, content, index) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
         newDiv.textContent = content;
         newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll('article[data-qa-ta="couponItem"]')[index];
-        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-      }
-      function addHiddenDivrating(id, content, index) {
-        const newDiv = document.createElement('div');
-        newDiv.id = id;
-        newDiv.textContent = content;
-        newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll('article[data-qa-ta="couponItem"]')[index];
+        const originalDiv = document.querySelectorAll('div[class="rating-box"] div[class="rating-stars"]')[index];
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
       function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
-        catElement.id = key;
+        catElement.className = key;
         catElement.textContent = value;
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
@@ -48,23 +55,17 @@ module.exports = {
         }
         return result;
       };
-      var getXpath = (xpath, prop) => {
-        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-        let result;
-        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-        else result = elem ? elem.singleNodeValue : '';
-        return result && result.trim ? result.trim() : result;
-      };
-      var rating = getAllXpath('//div[@itemprop="aggregateRating"]/meta[@itemprop="ratingValue"]/@content');
-      for (var i = 0; i < rating.length; i++) {
-        (rating[i / 2]);
-      }
 
-      console.log(rating = rating.map(x => x / 2))
-      for (var j = 0; j < rating.length; j++) {
-        addHiddenDivrating('ratings', rating[j], j)
-      }
+      const aggregateRating = document.querySelectorAll('div[class="rating-box"] div[class="rating-stars"]')
+      for (let k = 0; k < aggregateRating.length; k++) {
+        // @ts-ignore
+        let singleRating = aggregateRating[k].style.width;
+        singleRating = singleRating.slice(0, singleRating.length - 1)
+        singleRating = (5 * singleRating) / 100;
+        singleRating = singleRating.toFixed(1);
+        addHiddenDiv('aggregateRating', singleRating, k);
 
+      }
 
     });
     return await context.extract(productDetails, { transform });
