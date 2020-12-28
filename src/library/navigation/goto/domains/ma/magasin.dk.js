3e387778facbb213079eced8c821382476e1a6f0
sivasoarlogic <siva@soarlogic.com>
2020-12-28 14:06:33 +0530

    QAfixs

--- src/library/navigation/goto/domains/ma/magasin.dk.js
+++ src/library/navigation/goto/domains/ma/magasin.dk.js
@@ -8,4 +8,13 @@ module.exports = {
     store: 'magasin',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 100000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
