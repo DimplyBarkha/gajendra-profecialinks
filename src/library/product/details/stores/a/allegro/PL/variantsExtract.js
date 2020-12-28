36f3ea738868c6c33da40c8a2f7219c2924535fb
srishivams <srishivams97@gmail.com>
2020-12-28 16:31:08 +0530

    added new regex for variant id

--- src/library/product/details/stores/a/allegro/PL/variantsExtract.js
+++ src/library/product/details/stores/a/allegro/PL/variantsExtract.js
@@ -18,19 +18,31 @@ module.exports = {
     const { variants } = dependencies;
     try {
       await context.evaluate(() => {
-        const variants = document.evaluate(
-          '//meta[@property="og:url"]/@content | //div[@data-analytics-view-label="offerVariants"]//a/@href',
-          document,
-          null,
-          XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-          null,
-        );
-        for (let i = 0; i < variants.snapshotLength; i++) {
-          const div = document.createElement('div');
-          div.id = 'product-variants';
-          div.innerText = variants.snapshotItem(i).textContent;
-          document.body.append(div);
+        // const variants = document.evaluate(
+        //   '//meta[@property="og:url"]/@content | //div[@data-analytics-view-label="offerVariants"]//a/@href',
+        //   document,
+        //   null,
+        //   XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        //   null,
+        // );
+        // for (let i = 0; i < variants.snapshotLength; i++) {
+        //   const div = document.createElement('div');
+        //   div.id = 'product-variants';
+        //   div.innerText = variants.snapshotItem(i).textContent;
+        //   document.body.append(div);
+        // }
+        let url = null;
+        if (document.querySelector('meta[property="og:url"]')) {
+          url = document.querySelector('meta[property="og:url"]').getAttribute('content');
+        } else if (document.querySelector('div[data-analytics-view-label="offerVariants"] a')) {
+          url = document.querySelector('div[data-analytics-view-label="offerVariants"] a').getAttribute('href');
         }
+        if (url.includes('//allegro.pl')) url = url.replace(/(.+-)(\d+)(\??.+)/g, '$2');
+        else url = url.replace(/(.+-i)(\d+)(.+)/g, '$2');
+        const div = document.createElement('div');
+        div.id = 'product-variants';
+        div.innerText = url;
+        document.body.append(div);
       });
     } catch (e) {
       console.log(e.message);
