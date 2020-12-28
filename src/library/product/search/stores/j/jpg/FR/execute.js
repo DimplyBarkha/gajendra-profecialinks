3d6a45cb9b222fce93f655faa4408028d7bd2a58
Your Name <karthik.ravi-odc@partner.import.io>
2020-12-28 21:28:31 +0530

    new

--- src/library/product/search/stores/j/jpg/FR/execute.js
+++ src/library/product/search/stores/j/jpg/FR/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'jpg.fr',
     url: "https://www.jpg.fr/INTERSHOP/web/WFS/RAJA-JPG-Site/fr_FR/-/EUR/ViewParametricSearch-SimpleOfferSearch?PageNumber=0&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}&SearchParameter=",   
     loadedSelector: 'body',
-    noResultsXPath: null,
+    noResultsXPath: '//header[@class="search-noresult my-3"]/h3/text()',
     zipcode: '',
   },
 };
