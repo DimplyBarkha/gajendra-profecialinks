d4bdbfac98fba859ca3b37fdf69ed86e44b12026
BorysMaciuch <borysmaciuch@gmail.com>
2020-12-28 12:42:49 +0100

    #1163 fix rating, promotion

--- src/library/product/details/stores/b/bruneau/FR/extract.js
+++ src/library/product/details/stores/b/bruneau/FR/extract.js
@@ -1,4 +1,3 @@
-
 const { transform } = require('../shared');
 
 module.exports = {
@@ -18,17 +17,35 @@ module.exports = {
   ) => {
     await context.evaluate(() => {
       const zoom = document.querySelector('a.cursor-zoom-in');
-      const pdf = document.querySelector('button.document');
-      const image360 = document.querySelector('button.view-3D-button');
-
       if (zoom) {
         zoom.setAttribute('zoom', 'yes');
       }
+      const pdf = document.querySelector('button.document');
+
       if (pdf) {
         pdf.setAttribute('pdf', 'yes');
       }
+      const image360 = document.querySelector('button.view-3D-button');
       if (image360) {
         image360.setAttribute('image360', 'yes');
+      }
+    });
+
+    await context.evaluate(() => {
+      const rating = document.querySelector('.rating > span');
+      const ratingAlt = document.querySelector('.rating-container > span');
+
+      if (rating) {
+        if (rating.title.length === 1) {
+          rating.title =
+          rating.setAttribute('title', rating.title += ',0');
+        }
+      }
+      if (ratingAlt) {
+        if (ratingAlt.title.length === 1) {
+          ratingAlt.title =
+          ratingAlt.setAttribute('title', ratingAlt.title += ',0');
+        }
       }
     });
     await context.extract(dependencies.productDetails);
