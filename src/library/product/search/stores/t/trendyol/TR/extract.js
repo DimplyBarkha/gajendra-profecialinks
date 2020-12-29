3a36878adebaf9cea18f32adbcf5ead10e23cb44
Vivek Gondil <gondilvivek@gmail.com>
2020-12-29 14:23:28 +0530

    Added fix from UAT

--- src/library/product/search/stores/t/trendyol/TR/extract.js
+++ src/library/product/search/stores/t/trendyol/TR/extract.js
@@ -1,143 +1,61 @@
 const { transform } = require('./transform');
 module.exports = {
-  implements: 'product/search/extract',
-  parameterValues: {
-    country: 'TR',
-    store: 'trendyol',
-    transform,
-    zipcode: '',
-    domain: 'trendyol.com',
-  },
-  implementation: async (
-    inputs,
-    parameters,
-    context,
-    dependencies,
-  ) => {
-    const checkExistance = async (selector) => {
-      return await context.evaluate(async (currentSelector) => {
-        return await Boolean(document.querySelector(currentSelector));
-      }, selector);
-    };
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    const delay = t => new Promise(resolve => setTimeout(resolve, t));
-    await context.evaluate(async function () {
-      // @ts-ignore
-      const getRating = [...document.querySelectorAll('div.ratings')];
-      getRating.forEach(ele => {
-        var stars = [...ele.querySelectorAll('div.full')];
-        var sum = 0;
-        stars.forEach(element => {
-          var addition = element.getAttribute('style').match(/(\d+)/)[1];
-          sum = sum + Number(addition);
+    implements: 'product/search/extract',
+    parameterValues: {
+        country: 'TR',
+        store: 'trendyol',
+        transform,
+        zipcode: '',
+        domain: 'trendyol.com',
+    },
+    implementation: async(
+        inputs,
+        parameters,
+        context,
+        dependencies,
+    ) => {
+        await context.evaluate(async() => {
+            document.body.setAttribute('link', window.location.href)
+
+            function stall(ms) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve();
+                    }, ms);
+                });
+            }
+
+            const applyScroll = async function(lengthToScroll) {
+                let scrollTop = 0;
+                while (scrollTop !== 30000) {
+                    await stall(500);
+                    scrollTop += lengthToScroll;
+                    window.scrollBy(0, lengthToScroll);
+                    if (document.querySelectorAll('div.p-card-wrppr').length === parseInt(document.querySelector('.dscrptn').innerHTML.match(/(?!<!--\s-->)(\d+)(?=<!--\s-->)/g)[0]) || document.querySelectorAll('div.p-card-wrppr').length >= 150) {
+                        await stall(1000);
+                        break;
+                    }
+                }
+            };
+            await applyScroll(399);
+            console.log("lenght of produts: " + document.querySelectorAll('div.p-card-wrppr').length)
+            const cardsRating = document.querySelectorAll('div.p-card-wrppr div.ratings div.full');
+            cardsRating.forEach(element => {
+                element.setAttribute("rating", (parseInt(element.getAttribute("style").match(/(?!(width:))(\d+)(?:[%]?;)/g)[0].match(/\d+/g)[0]) / 100).toString())
+            });
+            const ratingDiv = document.querySelectorAll("div.ratings");
+            ratingDiv.forEach((element) => {
+                let rating = 0.0;
+                const nodes = element.childNodes;
+                for (let i = 0; i < 5; i++) {
+                    rating += parseFloat(nodes[i].childNodes[1].getAttribute("rating"));
+                };
+                element.setAttribute('rating', rating.toString())
+            })
+
         });
-        var convertedfull = (((sum / 500) * 100) / 20).toFixed(1).replace(/(\d+)(\.)(\d+)/, '$1,$3');
-        ele.setAttribute('sum', convertedfull);
-      });
-    });
-    if (await checkExistance('.prdct-cntnr-wrppr > div:nth-child(13)')) {
-      await context.evaluate(() => {
-        document.querySelector('.prdct-cntnr-wrppr > div:nth-child(13)').scrollIntoView({ behavior: 'smooth' });
-      });
-    }
-    // await context.evaluate(async function () {
-    //   const URL = window.location.href;
-    //   function addHiddenDiv(id, content, index) {
-    //     const newDiv = document.createElement('div');
-    //     newDiv.id = id;
-    //     newDiv.textContent = content;
-    //     newDiv.style.display = 'none';
-    //     const originalDiv = document.querySelectorAll('div div.p-card-wrppr')[index];
-    //     originalDiv.appendChild(newDiv);
-    //     console.log('child appended ' + index);
-    //   }
-    //   const product = document.querySelectorAll('div div.p-card-wrppr');
-    //   // select query selector and loop and add div
-    //   for (let i = 0; i < product.length; i++) {
-    //     addHiddenDiv('page_url', URL, i);
-    //   }
-    // });
-
-    const mainUrl = await context.evaluate(async function () {
-      return document.URL;
-    });
-    await new Promise((resolve, reject) => setTimeout(resolve, 2000));
-
-    const applyScroll = async function (context, lengthToScroll) {
-      await context.evaluate(async function (lengthToScroll) {
-        let scrollTop = 0;
-        while (scrollTop !== 20000) {
-          await stall(1000);
-          scrollTop += lengthToScroll;
-          window.scroll(0, scrollTop);
-          if (scrollTop === 20000) {
-            await stall(1000);
-            break;
-          }
-        }
-        function stall(ms) {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              resolve();
-            }, ms);
-          });
-        }
-      }, [lengthToScroll]);
-    };
-    await applyScroll(context, 500);
-
-    var results = await context.evaluate(async function () {
-      const result = [];
-      (document.querySelectorAll('div.prdct-cntnr-wrppr div a')).forEach((elem) => {
-        result.push({
-          url: elem.getAttribute('href'),
-          code: '',
-        });
-      });
-      return result;
-    });
-
-    const maxCount = results.length > 150 ? 150 : results.length;
-
-    for (var i = 0; i < maxCount; i++) {
-      const detailPageUrl = results[i].url.includes('https://www.trendyol.com/') ? results[i].url : `https://www.trendyol.com/${results[i].url}`;
-      await context.goto(detailPageUrl, {
-        timeout: 10000000,
-        waitUntil: 'load',
-        checkBlocked: true,
-        js_enabled: true,
-        css_enabled: false,
-        random_move_mouse: true,
-      });
-      await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-      const productCode = await context.evaluate(async function () {
-        const productCode = window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.productCode;
-        return productCode;
-      });
-      results[i].code = productCode;
-      await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-    };
-    // var productcodes = [];
-    // results.map((item) => {
-    //   productcodes.push(item.code);
-    // });
-
-    await context.goto(mainUrl, { timeout: 1000000, waitUntil: 'load', checkBlocked: true });
-    //await applyScroll(context);
-    await context.evaluate(async function (results) {
-      try {
-        var index = 0;
-        (document.querySelectorAll('div.prdct-cntnr-wrppr div a')).forEach((node) => {
-          node.setAttribute('data-product-code', results[0][index].code);
-          index++;
-        });
-      } catch (error) {
-        console.log('Error: ', error);
-      }
-    }, [results]);
-    await applyScroll(context, 100);
-    await context.waitForSelector('div.srch-prdcts-cntnr img');
-    return await context.extract(productDetails, { transform });
-  },
+        const { transform } = parameters;
+        const { productDetails } = dependencies;
+        return await context.extract(productDetails, { transform });
+    },
 };
---
+++ src/library/product/search/stores/t/trendyol/TR/shared.js
@@ -0,0 +1,133 @@
+function addDynamicTable(jsonData, appendSelector) {
+    function generateDynamicTable(jsonData) {
+        const dataLength = jsonData.length;
+
+        if (dataLength > 0) {
+            const table = document.createElement("table");
+            table.style.width = "100%";
+            table.setAttribute("border", "1");
+            table.setAttribute("cellspacing", "0");
+            table.setAttribute("cellpadding", "5");
+
+            const col = [];
+            for (let i = 0; i < dataLength; i++) {
+                for (const key in jsonData[i]) {
+                    if (col.indexOf(key) === -1) {
+                        col.push(key);
+                    }
+                }
+            }
+            const tHead = document.createElement("thead");
+            tHead.setAttribute("bgcolor", "#CCC4F5");
+            tHead.style.color = "black";
+            const hRow = document.createElement("tr");
+
+            for (let i = 0; i < col.length; i++) {
+                const th = document.createElement("th");
+                th.innerHTML = col[i];
+                hRow.appendChild(th);
+            }
+            tHead.appendChild(hRow);
+            table.appendChild(tHead);
+
+            const tBody = document.createElement("tbody");
+
+            for (let i = 0; i < dataLength; i++) {
+                const bRow = document.createElement("tr");
+                for (let j = 0; j < col.length; j++) {
+                    const td = document.createElement("td");
+                    table.style.padding = "5px";
+                    table.style.margin = "5px auto";
+                    td.setAttribute("class", col[j]);
+                    if (
+                        jsonData[i][col[j]] &&
+                        (jsonData[i][col[j]] !== "null" ||
+                            jsonData[i][col[j]] !== "undefined")
+                    ) {
+                        if (typeof jsonData[i][col[j]] === "object") {
+                            if (Array.isArray(jsonData[i][col[j]])) {
+                                const table = generateDynamicTable(jsonData[i][col[j]]);
+                                table && td.append(table);
+                            } else {
+                                const table = generateDynamicTable([jsonData[i][col[j]]]);
+                                table && td.append(table);
+                            }
+                        } else {
+                            td.innerText = jsonData[i][col[j]];
+                        }
+                    }
+                    bRow.appendChild(td);
+                    bRow.style.padding = "5px";
+                }
+                tBody.appendChild(bRow);
+            }
+            table.appendChild(tBody);
+            return table;
+        }
+    }
+
+    const table = generateDynamicTable(jsonData);
+    const container = document.createElement("div");
+    container.setAttribute("id", "added-table");
+    container.setAttribute("style", "overflow:auto");
+    container.innerHTML = "";
+    container.appendChild(table);
+    document.querySelector(appendSelector).append(container);
+}
+
+async function getData() {
+    async function getPartnerDetails(sku) {
+        async function getJsonObject() {
+            const API = window.location.href;
+            const res = await fetch(API);
+            const html = await res.text();
+            const doc = new DOMParser().parseFromString(html, 'text/html');
+            return Array.from(doc.querySelectorAll('script')).find((elm) =>
+                elm.innerText.includes('deliveryOptions'),
+            ).innerText;
+        }
+
+        const json = await getJsonObject();
+        let seller = Object.entries(JSON.parse(json).graphqlCache).filter(item => item[1].data.product).filter(item => item[1].data.product.hasOwnProperty('fulfillmentLabel'))[0][1].data.product.simples.filter(item => item.sku === `${sku}`)[0].fulfillmentLabel
+        return (seller && seller.label[1] && seller.label[1].text) ? seller.label[1].text : ''
+    }
+    const jsonData = JSON.parse(
+        document
+        .querySelector('[id="z-vegas-pdp-props"]')
+        .innerText.match(/(\[{.+}\])\]>$/)[1],
+    );
+    const {
+        id,
+        name,
+        reviewsCount,
+        roundedAverageStarRating,
+    } = jsonData[0].model.articleInfo;
+    let image = '';
+    try {
+        image = jsonData[0].model.articleInfo.media.images[0].sources.detail;
+    } catch (err) {
+        console.log('err getting image');
+    }
+    let brand = '';
+    try {
+        brand = jsonData[0].model.articleInfo.brand.name;
+    } catch (err) {
+        console.log('err getting brand');
+    }
+    const units = jsonData[0].model.articleInfo.units;
+    for (const unit of units) {
+        unit.merchantName = await getPartnerDetails(unit.id);
+    }
+    let res = [{
+        productId: id,
+        name,
+        reviewsCount,
+        roundedAverageStarRating,
+        image,
+        brand,
+        units,
+    }, ]
+    return res
+}
+
+module.exports = { addDynamicTable, getData };
