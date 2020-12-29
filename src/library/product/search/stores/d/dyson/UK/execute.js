21d376d204472693282634a4ef5644616ad6d4fa
Himanshu-Agrahari <himanshu.a@partner.import.io>
2020-12-29 12:11:23 +0530

    Remove loaded selector
    Since we are not waiting to load the page
    
    We are making API call to fetch data

--- src/library/product/search/stores/d/dyson/UK/execute.js
+++ src/library/product/search/stores/d/dyson/UK/execute.js
@@ -7,7 +7,8 @@ module.exports = {
     domain: 'dyson.co.uk',
     url: 'https://www.dyson.co.uk/search-results.html?searchText={searchTerms}&from=product',
     // url: 'https://www.dyson.co.uk/search-results.html?searchText={searchTerms}&from=product#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"proxy":{"use_relay_proxy":false},"load_all_resources":true,"enable_cache":false,"discard_CSP_header":true,"cookies":[]}[/!opt!]',
-    loadedSelector: '#Products-Data .search-count',
+    // loadedSelector: '#Products-Data .search-count',
+    loadedSelector: null,
     noResultsXPath: '//div[@id="Products-Data"]//div[@class="search-empty"][not(contains(.,"experiencing technical issues"))] | //link[@rel="canonical" and not(contains(@href, "search"))]',
   },
 };
