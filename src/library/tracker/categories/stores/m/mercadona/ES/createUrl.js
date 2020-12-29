39896769761390ea50f2095f64c7605a07cf08c1
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-29 20:00:51 +0530

    just checking for media with Anil Bist

---
+++ src/library/tracker/categories/stores/m/mercadona/ES/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'mercadona.es',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: 'https://tienda.mercadona.es/categories',
+    country: 'ES',
+    store: 'mercadona',
+    zipcode: '46008',
+  },
+};
---
+++ src/library/tracker/categories/stores/m/mercadona/ES/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'ES',
+    store: 'mercadona',
+    domain: 'mercadona.es',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '46008',
+  },
+};
