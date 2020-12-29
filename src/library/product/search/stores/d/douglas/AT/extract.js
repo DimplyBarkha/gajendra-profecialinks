fe2f42be5ed9292acdd38dfabb0220cdd1ec1042
osmakash <akashkumar.p@osmosys.asia>
2020-12-29 17:36:43 +0530

    comiting changes

--- src/library/product/search/stores/d/douglas/AT/extract.js
+++ src/library/product/search/stores/d/douglas/AT/extract.js
@@ -1,19 +1,20 @@
-
-async function implementation(
+const { transform } = require('./transform');
+async function implementation (
   inputs,
   parameters,
   context,
   dependencies,
 ) {
-  await context.evaluate(async function(context) {
-  
-  const checkBlankPage = document.querySelector('td[class="code depth_1"]')
-  if (checkBlankPage === null) {
-    throw new Error('Blank Page')
-  }
-});
+  const { productDetails } = dependencies;
+  const { transform } = parameters;
+  await context.evaluate(async function (context) {
+    const checkBlankPage = document.querySelector('td[class="code depth_1"]')
+      if (checkBlankPage === null) {
+        throw new Error('Blank Page')
+      }
+  });
+  return await context.extract(productDetails, { transform });
 }
-const { transform } = require('./transform');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
