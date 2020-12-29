8c7bcad63658f94aa43f2bb728b1a79e6c7a6abb
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-12-30 00:57:50 +0530

    linio-search--Damayanthi

--- src/library/product/search/stores/l/linio/PE/paginate.js
+++ src/library/product/search/stores/l/linio/PE/paginate.js
@@ -5,12 +5,15 @@ module.exports = {
     country: 'PE',
     store: 'linio',
     mutationSelector: null,
+    // nextLinkSelector: ('body > div.wrapper.container-fluid > main > div.catalogue-container > div.catalogue-product-section.col-lg-10.col-md-9 > nav > ul > li:nth-child(6) > a > span' or 'body > div.wrapper.container-fluid > main > div.catalogue-container > div.catalogue-product-section.col-lg-10.col-md-9 > nav > ul > li:nth-child(6) > a > span'),
+    // nextLinkXpath: '//ul/li[@class="page-item"]/a[@class="page-link"]/following::li[1]/a/span',
+    nextLinkXpath: '//ul/li[@class="page-item"][last()]/preceding::li[1]/a/span',
     spinnerSelector: null,
     loadedSelector: 'body',
     noResultsXPath: null,
-    openSearchDefinition: {
-      template: 'https://www.linio.com.pe/search?scroll=&q={searchTerms}&page={page}',
-    },
+    // openSearchDefinition: {
+    //   template: 'https://www.linio.com.pe/search?scroll=&q={searchTerms}&page={page}',
+    // },
     domain: 'linio.com',
     zipcode: '',
   },
