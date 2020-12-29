30f746725f249b1ee732c803536825d6c6ceb8e4
Your Name <karthik.ravi-odc@partner.import.io>
2020-12-29 20:35:56 +0530

    new

--- src/library/product/search/stores/a/ahlens/SE/extract.js
+++ src/library/product/search/stores/a/ahlens/SE/extract.js
@@ -15,19 +15,36 @@ module.exports = {
   ) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
-    await context.evaluate(async () => {
-      while (!!document.querySelector('#ahl-product-list-app > div > div >button')) {
-        // @ts-ignore
-        document.querySelector('#ahl-product-list-app > div > div >button').click()
-        await new Promise(r => setTimeout(r, 6000));
-        function addElementToDocument(key, value) {
-          const catElement = document.createElement('div');
-          catElement.className = key;
-          catElement.textContent = value;
-          catElement.style.display = 'none';
-          document.body.appendChild(catElement);
+    const applyScroll = async function (context) {
+      await context.evaluate(async () => {
+        let scrollTop = 0;
+        while (scrollTop !== 16000) {
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          await stall(1000);
         }
+        function stall(ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+
+    };
+    await applyScroll(context);
+
+
+    await context.evaluate(async function () {
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.className = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
       }
+      // }
       function addHiddenDiv(id, content, index) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
@@ -53,20 +70,21 @@ module.exports = {
           if (element) result.push(prop ? element[prop] : element.nodeValue);
         }
         return result;
-      };     
+      };
       var rating = getAllXpath("//*[contains(@class,'MuiCardContent-root')]//div/div//span[contains(text(),'kr')][1]/text()");
-      for (let index = 0; index < rating.length; index++) {        
+      for (let index = 0; index < rating.length; index++) {
         if (rating[index].includes(":")) {
           var temp = rating[index].replace(":", ".");
-        } else if (rating[index].includes(",")){
+        } else if (rating[index].includes(",")) {
           var temp = rating[index].replace(",", ".");
         } else {
           temp = rating[index].replace(":-", ".");
-        }           
+        }
         addHiddenDivPrice('price', temp, index);
       }
 
     });
     return await context.extract(productDetails, { transform });
   },
+
 }
