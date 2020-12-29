6bc9ed155c30f42ed8f51731a8a436679ce1c2b9
sivasoarlogic <siva@soarlogic.com>
2020-12-29 11:19:12 +0530

    reviewupdate

---
+++ src/library/product/reviews/stores/w/wildberries/RU/execute.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'product/reviews/execute',
+  parameterValues: {
+    country: 'RU',
+    store: 'wildberries',
+    domain: 'wildberries.ru',
+    loadedSelector: null,
+    noResultsXPath: null,
+    reviewUrl: null,
+    sortButtonSelectors: null,
+    zipcode: "''",
+  },
+};
---
+++ src/library/product/reviews/stores/w/wildberries/RU/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/reviews/extract',
+  parameterValues: {
+    country: 'RU',
+    store: 'wildberries',
+    transform: null,
+    domain: 'wildberries.ru',
+    zipcode: "''",
+  },
+};
