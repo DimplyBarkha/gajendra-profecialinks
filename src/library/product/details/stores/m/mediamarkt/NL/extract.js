fe1234ca083d05a608b1909e5d36fe7f646db03d
Lokanath sahoo <loknath.s@osmosys.asia>
2020-12-29 17:52:34 +0530

    fixing UAT issues

--- src/library/product/details/stores/m/mediamarkt/NL/extract.js
+++ src/library/product/details/stores/m/mediamarkt/NL/extract.js
@@ -65,28 +65,28 @@ module.exports = {
         var specificationsItems = Array.from(specificationGroup.children);
         specificationsItems.forEach((item, index) => {
           // Getting weight
-          if (item.innerText === 'Gewicht:') {
-            weight = specificationsItems[index + 1].innerText;
+          if (item && item.innerText === 'Gewicht:') {
+            weight = specificationsItems[index] && specificationsItems[index + 1].innerText;
           }
           // Getting color
-          if (item.innerText === 'Kleur:') {
-            color = specificationsItems[index + 1].innerText;
+          if (item && item.innerText === 'Kleur:') {
+            color = specificationsItems[index] && specificationsItems[index + 1].innerText;
           }
           // Getting warranty
-          if (item.innerText === 'Fabrieksgarantie:') {
-            warranty = specificationsItems[index + 1].innerText;
+          if (item && item.innerText === 'Fabrieksgarantie:') {
+            warranty = specificationsItems[index] && specificationsItems[index + 1].innerText;
           }
           // Getting mpc
-          if (item.innerText === 'Manufacturer Part Number (MPN):' || item.innerText.includes('Modelnaam')) {
-            mpc = specificationsItems[index + 1].innerText;
+          if (item && item.innerText === 'Manufacturer Part Number (MPN):' || item.innerText.includes('Modelnaam')) {
+            mpc = specificationsItems[index] && specificationsItems[index + 1].innerText;
           }
           // Getting energy
-          if (item.innerText === 'Energie-efficiëntieklasse:') {
-            energy = specificationsItems[index + 1].innerText;
+          if (item && item.innerText === 'Energie-efficiëntieklasse:') {
+            energy = specificationsItems[index] && specificationsItems[index + 1].innerText;
           }
           // Getting shipping dimensions
-          if (item.innerText.includes('Verpakkingsvolume') || item.innerText.includes('Afmetingen')) {
-            shippingDimensions = specificationsItems[index + 1].innerText;
+          if (item && item.innerText.includes('Verpakkingsvolume') || item.innerText.includes('Afmetingen')) {
+            shippingDimensions = specificationsItems[index] && specificationsItems[index + 1].innerText;
           }
         });
       });
@@ -108,32 +108,6 @@ module.exports = {
       addElementToDocument('mm_skuCode', sku);
       addElementToDocument('mm_retailerProductCode', sku);
 
-      // Getting number of customer reviews
-      // const reviewsElement = document.querySelector('.bv_numReviews_text');
-      // if (reviewsElement) {
-      //   addElementToDocument('mm_numberOfCustomerReviews', reviewsElement.innerText.replace(/\(|\)/g, ''));
-      // }
-
-      // Gets aggregate rating
-      // if (document.querySelector('div[itemprop=ratingValue]')) {
-      //   addElementToDocument('mm_aggregateRating', document.querySelector('div[itemprop=ratingValue]').innerText.replace('.', ','));
-      // }
-
-      // Getting images
-      // const images = Array.from(document.querySelectorAll('ul.thumbs li a:not(.thumb--play-video-btn)'));
-      // const image = `https:${images[0].dataset.preview}`;
-      // const alternativeImages = images.reduce((accumulator, link, i) => i > 0 ? accumulator + `${i !== 1 ? ' | ' : ''}https:${link.dataset.preview}` : '', '');
-      // addElementToDocument('mm_image', image);
-      // addElementToDocument('mm_alternateImages', alternativeImages);
-
-      // Getting category
-      // const breadcrumbs = Array.from(document.querySelectorAll('ul.breadcrumbs li a'));
-      // addElementToDocument('mm_category', breadcrumbs[1].innerText);
-
-      // Getting subcategory
-      // const subcategories = breadcrumbs.reduce((accumulator, category, i) => i > 1 ? accumulator + `${i > 2 ? ' > ' : ''}${category.innerText}` : '', '');
-      // addElementToDocument('mm_subCategory', subcategories);
-
       // Checking if in stock
       if (document.querySelector('.label-instock')) {
         addElementToDocument('mm_availabilityText', 'In Stock');
@@ -150,7 +124,7 @@ module.exports = {
       // Getting description
       const descriptionElement = Array.from(document.querySelectorAll('#omschrijving p'));
       if (descriptionElement) {
-        addElementToDocument('mm_description', descriptionElement.map(element => element.innerText.replace(/\r?\n/g, ' ').slice(0, -1)).join(' '));
+        addElementToDocument('mm_description', descriptionElement.map(element => element && element.innerText.replace(/\r?\n/g, ' ').slice(0, -1)).join(' '));
       }
     });
     return await context.extract(productDetails, { transform });
