d377eeaf20276e7374683ff4125e65be80074727
sivasoarlogic <siva@soarlogic.com>
2020-12-29 19:11:45 +0530

    QAfixs

--- src/library/navigation/goto/domains/la/lastprice.co.il.js
+++ src/library/navigation/goto/domains/la/lastprice.co.il.js
@@ -8,5 +8,19 @@ module.exports = {
     store: 'lastprice',
     zipcode: '',
   },
-
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.captureRequests();
+    await context.setBlockAds(false);
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
