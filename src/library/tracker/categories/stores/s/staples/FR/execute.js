2517f79a1db0966d4fb48204eb3fe336f3d9ad74
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-29 19:45:48 +0530

    #3399 develop done

---
+++ src/library/tracker/categories/stores/s/staples/FR/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'FR',
+    store: 'staples',
+    domain: 'jpg.fr',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
