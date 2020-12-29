b8e8f2a8ff8ee1dab54efcc8e7eb3b7bb29b7f11
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-29 19:27:04 +0530

    #3397 develop done

---
+++ src/library/tracker/categories/stores/s/sephora/IT/extract.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories/extract',
+  parameterValues: {
+    country: 'IT',
+    domain: 'sephora.com',
+    store: 'sephora',
+    zipcode: '',
+  },
+};
