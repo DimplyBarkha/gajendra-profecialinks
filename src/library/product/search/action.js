cb962f3b9f6c06715689e0e75e9e6fba57a27878
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-12-29 23:37:45 +0530

    lyko--Damayanthi

--- src/library/product/search/action.js
+++ src/library/product/search/action.js
@@ -1,106 +1,190 @@
+// module.exports = {
+//   parameters: [
+//     {
+//       name: 'country',
+//       description: '2 letter ISO code for the country',
+//     },
+//     {
+//       name: 'store',
+//       description: 'store name',
+//     },
+//     {
+//       name: 'domain',
+//       description: 'The top private domain of the website (e.g. amazon.com)',
+//     },
+//     {
+//       name: 'zipcode',
+//       description: 'to set location',
+//       optional: true,
+//     },
+//     {
+//       name: 'storeID',
+//       description: 'Id of the store',
+//       type: 'string',
+//       optional: true,
+//     },
+//   ],
+//   inputs: [
+//     {
+//       name: 'keywords',
+//       description: 'keywords to search for',
+//       type: 'string',
+//     },
+//     {
+//       name: 'Keywords',
+//       description: 'keywords to search for',
+//       type: 'string',
+//     },
+//     {
+//       name: 'Brands',
+//       description: 'brands to search for',
+//       type: 'string',
+//     },
+//     {
+//       name: 'results',
+//       description: 'the minimum number of results required',
+//       type: 'number',
+//     },
+//     {
+//       name: 'Brands',
+//       description: 'brands to search for',
+//       type: 'string',
+//     },
+//   ],
+//   dependencies: {
+//     execute: 'action:product/search/execute',
+//     paginate: 'action:navigation/paginate',
+//     extract: 'action:product/search/extract',
+//   },
+//   path: './search/stores/${store[0:1]}/${store}/${country}/search',
+//   implementation: async (inputs, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
+//     const { keywords, Keywords, results = 150, Brands } = inputs;
+
+//     const inputKeywords = Keywords || keywords || Brands;
+
+//     // TODO: consider moving this to a reusable function
+//     const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
+
+//     const resultsReturned = await execute({
+//       ...inputs,
+//       keywords: inputKeywords,
+//       zipcode: inputs.zipcode || zipcode,
+//     });
+
+//     // do the search
+
+//     if (!resultsReturned) {
+//       console.log('No results were returned');
+//       return;
+//     }
+
+//     // try gettings some search results
+//     const pageOne = await extract({});
+
+//     let collected = length(pageOne);
+
+//     console.log('Got initial number of results', collected);
+
+//     // check we have some data
+//     if (collected === 0) {
+//       return;
+//     }
+// // <<<<<<< HEAD
+//     let page = 1;
+//     while (collected < results && await paginate({ keywords, page, offset: collected })) {
+// // =======
+//     let page = 2;
+//     while (collected < results && await paginate({ keywords: inputKeywords, page, offset: collected })) {
+// // >>>>>>> ba1530b472d6acd392c50f4a7fc78f140e0bac06
+//       const data = await extract({});
+//       const count = length(data);
+//       if (count === 0) {
+//         break; // no results
+//       }
+//       collected += count;
+//       console.log('Got more results', collected);
+//       page++;
+//     }
+//   },
+// };
 module.exports = {
   parameters: [
-    {
-      name: 'country',
-      description: '2 letter ISO code for the country',
-    },
-    {
-      name: 'store',
-      description: 'store name',
-    },
-    {
-      name: 'domain',
-      description: 'The top private domain of the website (e.g. amazon.com)',
-    },
-    {
-      name: 'zipcode',
-      description: 'to set location',
-      optional: true,
-    },
-    {
-      name: 'storeID',
-      description: 'Id of the store',
-      type: 'string',
-      optional: true,
-    },
+  {
+  name: 'country',
+  description: '2 letter ISO code for the country',
+  },
+  {
+  name: 'store',
+  description: 'store name',
+  },
+  {
+  name: 'domain',
+  description: 'The top private domain of the website (e.g. amazon.com)',
+  },
+  {
+  name: 'zipcode',
+  description: 'to set location',
+  optional: true,
+  },
   ],
   inputs: [
-    {
-      name: 'keywords',
-      description: 'keywords to search for',
-      type: 'string',
-    },
-    {
-      name: 'Keywords',
-      description: 'keywords to search for',
-      type: 'string',
-    },
-    {
-      name: 'Brands',
-      description: 'brands to search for',
-      type: 'string',
-    },
-    {
-      name: 'results',
-      description: 'the minimum number of results required',
-      type: 'number',
-    },
-    {
-      name: 'Brands',
-      description: 'brands to search for',
-      type: 'string',
-    },
+  {
+  name: 'keywords',
+  description: 'keywords to search for',
+  type: 'string',
+  },
+  {
+  name: 'Keywords',
+  description: 'keywords to search for',
+  type: 'string',
+  },
+  {
+  name: 'Brands',
+  description: 'brands to search for',
+  type: 'string',
+  },
+  {
+  name: 'results',
+  description: 'the minimum number of results required',
+  type: 'number',
+  },
   ],
   dependencies: {
-    execute: 'action:product/search/execute',
-    paginate: 'action:navigation/paginate',
-    extract: 'action:product/search/extract',
+  execute: 'action:product/search/execute',
+  paginate: 'action:product/search/paginate',
+  extract: 'action:product/search/extract',
   },
   path: './search/stores/${store[0:1]}/${store}/${country}/search',
-  implementation: async (inputs, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
-    const { keywords, Keywords, results = 150, Brands } = inputs;
-
-    const inputKeywords = Keywords || keywords || Brands;
-
-    // TODO: consider moving this to a reusable function
-    const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
-
-    const resultsReturned = await execute({
-      ...inputs,
-      keywords: inputKeywords,
-      zipcode: inputs.zipcode || zipcode,
-    });
-
-    // do the search
-
-    if (!resultsReturned) {
-      console.log('No results were returned');
-      return;
-    }
-
-    // try gettings some search results
-    const pageOne = await extract({});
-
-    let collected = length(pageOne);
-
-    console.log('Got initial number of results', collected);
-
-    // check we have some data
-    if (collected === 0) return;
-
-<<<<<<< HEAD
-    let page = 1;
-    while (collected < results && await paginate({ keywords, page, offset: collected })) {
-=======
-    let page = 2;
-    while (collected < results && await paginate({ keywords: inputKeywords, page, offset: collected })) {
->>>>>>> ba1530b472d6acd392c50f4a7fc78f140e0bac06
-      const data = await extract({});
-      const count = length(data);
-      if (count === 0) break; // no results
-      collected += count;
-      console.log('Got more results', collected);
-      page++;
-    }
+  implementation: async ({ keywords, Keywords, Brands, results = 150 }, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
+  // TODO: consider moving this to a reusable function
+  const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
+  keywords = (Keywords) || (Brands) || (keywords);
+  console.log('zip:' + zipcode);
+  // do the search
+  const resultsReturned = await execute({ keywords, zipcode });
+  if (!resultsReturned) {
+  console.log('No results were returned');
+  return;
+  }
+  // try gettings some search results
+  const pageOne = await extract({});
+  let collected = length(pageOne);
+  console.log('Got initial number of results', collected);
+  // check we have some data
+  if (collected === 0) {
+  return;
+  }
+  let page = 1;
+  while (collected < results && await paginate({ keywords, page, offset: collected })) {
+  const data = await extract({});
+  const count = length(data);
+  if (count === 0) {
+  // no results
+  break;
+  }
+  collected += count;
+  console.log('Got more results', collected);
+  page++;
+  }
   },
-};
+  };
--- src/library/product/search/stores/l/lyko/SE/extract.js
+++ src/library/product/search/stores/l/lyko/SE/extract.js
@@ -7,12 +7,30 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await context.evaluate(async () => {
+  await context.evaluate(async function () {
     while(!!document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button")){
       // @ts-ignore
       document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button").click()
       await new Promise(r => setTimeout(r, 10000));
     }
+      let scrollTop = 0;
+      while (scrollTop !== 100000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 100000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    
   })
   return await context.extract(productDetails, { transform });
 }
