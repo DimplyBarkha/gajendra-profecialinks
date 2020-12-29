51c83995f7a56fd5d5160630968fd4828002cb3a
David Skoglund <david.skoglund@import.io>
2020-12-28 16:15:31 -0800

    rebuilt to use search API

--- src/library/product/search/stores/s/shopmylocal/AU/execute.js
+++ src/library/product/search/stores/s/shopmylocal/AU/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'AU',
     store: 'shopmylocal',
     domain: 'shopmylocal.com.au',
-    url: 'https://www.shopmylocal.com.au/s/-/-/new-south-wales/Sydney?query={searchTerms}',
+    url: 'https://www.shopmylocal.com.au/api/int/s?query={searchTerms}&search_distance=10&published_to=marketplace&location_slug=St-Ives&region_name=New+South+Wales#[!opt!]{"type":"json"}[/!opt!]',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
