c29fe7ca8f7c3510943899419527d750e922d801
shivaraj <>
2020-12-29 13:41:45 +0530

    UDPA Fixed

--- src/library/navigation/goto/domains/me/melectronics.ch.js
+++ src/library/navigation/goto/domains/me/melectronics.ch.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'melectronics.ch',
-    timeout: 50000,
+    timeout: 90000,
     country: 'CH',
     store: 'melectronics',
     zipcode: '',
--- src/library/product/details/stores/m/melectronics/CH/extract.js
+++ src/library/product/details/stores/m/melectronics/CH/extract.js
@@ -1,4 +1,4 @@
-const {transform} = require('../format')
+const { transform } = require('../format')
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -7,31 +7,53 @@ module.exports = {
     transform,
     domain: 'melectronics.ch',
     zipcode: '',
-  }, implementation: async ({ inputString }, { country, domain, transform: transformParam}, context, { productDetails }) => {
+  }, implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+
     try {
       await context.waitForSelector(".detail-showcase--additional-img-box img[alt='Video']");
-    } catch(er) {
+    } catch (er) {
       console.log("No video selector");
     }
-    await context.evaluate(async function() {
-      if(document.querySelector(".detail-showcase--additional-img-box img[alt='Video']")) {
+    await context.evaluate(async function () {
+      if (document.querySelector(".detail-showcase--additional-img-box img[alt='Video']")) {
         //@ts-ignore
         document.querySelector(".detail-showcase--additional-img-box img[alt='Video']").click();
         await new Promise((resolve, reject) => setTimeout(resolve, 4000));
       }
     })
     //fixing for gtin
-    try{
-      await context.evaluate(()=>{
+    try {
+      await context.evaluate(() => {
         //@ts-ignore
-        const dataFromScript = document.evaluate('//script[@type="application/ld+json"][contains(.,"gtin")]', document,null, 7, null).snapshotItem(0).innerText;
+        const dataFromScript = document.evaluate('//script[@type="application/ld+json"][contains(.,"gtin")]', document, null, 7, null).snapshotItem(0).innerText;
         const jsonData = JSON.parse(dataFromScript);
         const gtin = jsonData.gtin13;
         document.querySelector('body').setAttribute('gtin', gtin);
       })
-    }catch(e){
+    } catch (e) {
       console.log('gtin not present');
     }
-  await context.extract(productDetails, { transform: transformParam });
+
+
+    await context.evaluate(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+
+      async function infiniteScroll() {
+          let prevScroll = document.documentElement.scrollTop;
+          while (true) {
+              window.scrollBy(0, document.documentElement.clientHeight);
+              await new Promise(resolve => setTimeout(resolve, 1000));
+              const currentScroll = document.documentElement.scrollTop;
+              if (currentScroll === prevScroll) {
+                  break;
+              }
+              prevScroll = currentScroll;
+          }
+      }
+      await infiniteScroll();
+      await new Promise((resolve) => setTimeout(resolve, 8000));
+  });
+
+    await context.extract(productDetails, { transform: transformParam });
   }
 };
