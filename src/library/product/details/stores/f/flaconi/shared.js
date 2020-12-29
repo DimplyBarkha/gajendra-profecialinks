6423206c561096b29138ab2b80174d66a653e35b
sushma-mangalapally <sushma.mangalapally@import.io>
2020-12-29 09:38:54 -0800

    Remove unused comment

--- src/library/product/details/stores/f/flaconi/shared.js
+++ src/library/product/details/stores/f/flaconi/shared.js
@@ -71,34 +71,6 @@ const transform = (data) => {
         // row.variantUrl = row.variantUrl+'#sku='+row.variantId[item].text;
         // });
       }
-      /* Why did we do this?
-      if (row.variantId) {
-        const length = row.variantId.length;
-        const firstEle = row.variantId[length - 1].text;
-        console.log('firstEle: ', firstEle);
-        row.variantId.forEach(item => {
-        // @ts-ignore
-          item.text = firstEle;
-        });
-        if (row.sku) {
-          row.sku.forEach(item => {
-          // @ts-ignore
-            item.text = firstEle;
-          });
-        }
-
-        if (row.asin) {
-          row.asin.forEach(item => {
-            item.text = firstEle;
-          });
-        }
-
-        if (row.gtin) {
-          row.gtin.forEach(item => {
-            item.text = firstEle;
-          });
-        }
-      } */
       if (row.category) {
         row.category.pop();
       }
