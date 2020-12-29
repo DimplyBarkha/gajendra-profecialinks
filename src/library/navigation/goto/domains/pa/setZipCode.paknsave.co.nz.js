86813e990d79157f9a35a23683129ff5c4def630
BorysMaciuch <borysmaciuch@gmail.com>
2020-12-29 14:20:08 +0100

    Fix setZipCode

--- src/library/navigation/goto/domains/pa/setZipCode.paknsave.co.nz.js
+++ src/library/navigation/goto/domains/pa/setZipCode.paknsave.co.nz.js
@@ -6,23 +6,38 @@ module.exports = {
     store: "paknsave",
     zipcode: "''",
   },
-  implementation: async (
-    inputs,
-    parameterValues,
-    context,
-    dependencies
-  ) => {
-    const storeId  = inputs.zipcode
-    console.log('storeid', storeId)
-    await context.waitForSelector('.fs-selected-store__name.u-bold')
-    await context.click('.fs-selected-store__name.u-bold')
-    await context.waitForSelector('#change-store')
-    await context.click('#change-store')
-    await context.waitForSelector('.m-form__input.fs-store-search__input.fs-search-autocomplete__input')
-    await context.evaluate( async(storeId) => document.querySelector('.m-form__input.fs-store-search__input.fs-search-autocomplete__input').setAttribute('value', storeId), storeId)
-    await context.click('.fs-search-autocomplete__search-btn')
+  implementation: async (inputs, parameterValues, context, dependencies) => {
+    const storeId = inputs.zipcode;
+    console.log("storeid", storeId);
     await new Promise((r) => setTimeout(r, 3000));
-    await context.click('.fs-store-search__card-ui.m-fulfillment-selectstore__select > button')
-      
+    await context.waitForSelector(".fs-selected-store__name.u-bold");
+    const popup = await context.evaluate(() =>
+      document.querySelector("fs-tooltip__close-btn > i")
+    );
+    if (popup) {
+      await context.click("fs-tooltip__close-btn > i");
     }
-  }
+    await context.click(".fs-selected-store__name.u-bold");
+    await context.waitForSelector("#change-store");
+    await context.click("#change-store");
+    await context.waitForSelector(
+      ".m-form__input.fs-store-search__input.fs-search-autocomplete__input"
+    );
+    await context.setInputValue(
+      ".m-form__input.fs-store-search__input.fs-search-autocomplete__input",
+      storeId
+    );
+    await context.waitForSelector(".fs-search-autocomplete__search-term");
+    await context.click(".fs-search-autocomplete__search-term");
+    await new Promise((r) => setTimeout(r, 3000));
+    await context.click(
+      ".fs-store-search__card-ui.m-fulfillment-selectstore__select > .btn.btn--primary.btn--large.select-store"
+    );
+    await context.waitForSelector(
+      ".fs-book-slot-notice__action.fs-book-slot-notice__action-less-preferred.u-bold"
+    );
+    await context.click(
+      ".fs-book-slot-notice__action.fs-book-slot-notice__action-less-preferred.u-bold"
+    );
+  },
+};
