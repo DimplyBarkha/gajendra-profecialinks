e09d033148e1484f9ba32f7cb74db0b266984380
xeilen <xeilen1112@gmail.com>
2020-12-29 16:43:19 +0200

    -added func to get correct search terms

--- src/library/product/search/stores/m/mercadona/ES/extract.js
+++ src/library/product/search/stores/m/mercadona/ES/extract.js
@@ -19,9 +19,36 @@ module.exports = {
     await context.waitForSelector('.section');
     await context.evaluate(async () => {
       const productUrl = 'https://tienda.mercadona.es/product/';
-      const searchWord = location.search.includes('%20')
-        ? location.search.replace('?query=', '').replace('%20', ' ')
-        : location.search.replace('?query=', '');
+
+      function getCorrectSearchTerms (searchTerms) {
+        searchTerms = searchTerms.replace('?query=', '');
+        if (searchTerms.includes('%20')) {
+          searchTerms = searchTerms.replace(/%20/g, ' ');
+        }
+        if (searchTerms.includes('%C3%B1')) {
+          searchTerms = searchTerms.replace(/%C3%B1/g, 'ñ');
+        }
+        if (searchTerms.includes('%C3%A1')) {
+          searchTerms = searchTerms.replace(/%C3%A1/g, 'á');
+        }
+        if (searchTerms.includes('%C3%A9')) {
+          searchTerms = searchTerms.replace(/%C3%A9/g, 'é');
+        }
+        if (searchTerms.includes('%C3%AD')) {
+          searchTerms = searchTerms.replace(/%C3%AD/g, 'í');
+        }
+        if (searchTerms.includes('%C3%B3')) {
+          searchTerms = searchTerms.replace(/%C3%B3/g, 'ó');
+        }
+        if (searchTerms.includes('%C3%BA')) {
+          searchTerms = searchTerms.replace(/%C3%BA/g, 'ú');
+        }
+        if (searchTerms.includes('%C3%BC')) {
+          searchTerms = searchTerms.replace(/%C3%BC/g, 'ü');
+        }
+
+        return searchTerms;
+      }
 
       const url = 'https://7uzjkl1dj0-dsn.algolia.net/1/indexes/products_prod_vlc1_es/query?x-algolia-agent=Algolia%20for%20JavaScript%20(3.35.1)%3B%20Browser&x-algolia-application-id=7UZJKL1DJ0&x-algolia-api-key=9d8f2e39e90df472b4f2e559a116fe17';
       const response = await fetch(url, {
@@ -29,7 +56,7 @@ module.exports = {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ query: searchWord }),
+        body: JSON.stringify({ query: getCorrectSearchTerms(location.search) }),
       }).then(res => res.json())
         .then(items => items.hits);
 
@@ -44,8 +71,6 @@ module.exports = {
       const products = document.querySelectorAll('.product-cell');
       console.log(products);
       products.forEach((product, index) => {
-        addHiddenDiv('position-helper', location.search.replace('?query=', ''), product);
-        addHiddenDiv('helper-url', location.href, product);
         addHiddenDiv('helper-id', response[index].id, product);
         addHiddenDiv('helper-productUrl', productUrl + response[index].id, product);
       });
