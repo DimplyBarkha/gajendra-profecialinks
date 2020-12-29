86a05df426c1026606598f9a8545532a237301e8
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-29 13:51:03 +0530

    #3370 deploy done

---
+++ src/library/tracker/media/stores/t/technodom/KZ/media.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'tracker/media',
+  parameterValues: {
+    country: 'KZ',
+    store: 'technodom',
+    domain: 'technodom.kz',
+    zipcode: '',
+    storeID: null,
+  },
+};
