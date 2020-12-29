a415d2d93459ed37868aeb37e381f041c3277172
Abhishek Kumar Mishra <abhishekkumar_mishra@hcl.com>
2020-12-29 11:03:13 +0530

    Added codes for AmazonPrimeNow DE

---
+++ src/library/product/details/stores/a/amazonPrimeNow/DE/extract.js
@@ -0,0 +1,21 @@
+const { implementation } = require('../../../a/amazon/US/extract');
+const { transform } = require('../../../../sharedAmazon/transformNew');
+
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'amazonPrimeNow',
+    transform: transform,
+    domain: 'primenow.amazon.de',
+    zipcode: '10115',
+  },
+  dependencies: {
+    productDetails: 'extraction:product/details/stores/${store[0:1]}/${store}/${country}/extract',
+    Helpers: 'module:helpers/helpers',
+    AmazonHelp: 'module:helpers/amazonHelp',
+    goto: 'action:navigation/goto',
+  },
+  implementation,
+};
