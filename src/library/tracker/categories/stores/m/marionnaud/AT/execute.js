a035d17398cde78ab0da6b1e6ec55181c2e88af0
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-29 18:10:07 +0530

    #3393 developing done

---
+++ src/library/tracker/categories/stores/m/marionnaud/AT/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'AT',
+    store: 'marionnaud',
+    domain: 'marionnaud.at',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
