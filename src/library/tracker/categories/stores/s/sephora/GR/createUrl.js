d81f178079f2f716c9861c74aa6e9bbceb6aaadc
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-29 19:36:55 +0530

    #3397 develop done

---
+++ src/library/tracker/categories/stores/s/sephora/GR/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'sephora.com',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: null,
+    country: 'GR',
+    store: 'sephora',
+    zipcode: '',
+  },
+};
