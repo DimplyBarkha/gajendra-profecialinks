3d94ac4de5a8fa34d0a833eef6464e1974047241
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-29 13:46:28 +0530

    #3370 develop goign on

---
+++ src/library/tracker/categories/stores/t/technodom/KZ/categories.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'tracker/categories',
+  parameterValues: {
+    country: 'KZ',
+    domain: 'technodom.kz',
+    store: 'technodom',
+    zipcode: '',
+  },
+};
---
+++ src/library/tracker/categories/stores/t/technodom/KZ/createUrl.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  implements: 'tracker/categories/createUrl',
+  parameterValues: {
+    domain: 'technodom.kz',
+    prefix: null,
+    suffix: null,
+    subdomain: null,
+    url: null,
+    country: 'KZ',
+    store: 'technodom',
+    zipcode: '',
+  },
+};
