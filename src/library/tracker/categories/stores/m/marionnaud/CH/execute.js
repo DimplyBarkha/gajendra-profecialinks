6e9c4947011e6e8a7f017344ffc7c30ef405acb0
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-29 18:57:57 +0530

    #3395 develop done

---
+++ src/library/tracker/categories/stores/m/marionnaud/CH/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'tracker/categories/execute',
+  parameterValues: {
+    country: 'CH',
+    store: 'marionnaud',
+    domain: 'marionnaud.ch',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
