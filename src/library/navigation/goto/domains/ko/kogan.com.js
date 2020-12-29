b8b4d89381584bc03526c8b150a8cd232654ce53
sivasoarlogic <siva@soarlogic.com>
2020-12-29 18:28:57 +0530

    Qafixs

--- src/library/navigation/goto/domains/ko/kogan.com.js
+++ src/library/navigation/goto/domains/ko/kogan.com.js
@@ -11,6 +11,12 @@ module.exports = {
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
     await context.setBlockAds(false);
+    await context.captureRequests();
+    await context.setBlockAds(false);
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
     await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
