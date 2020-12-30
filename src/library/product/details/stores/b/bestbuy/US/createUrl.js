532c8ffd9275857af77c023c77212ef849c30753
PR <pravinr@synerzipune.local>
2020-12-30 22:53:28 +0530

    rebase

--- src/library/product/details/stores/b/bestbuy/US/createUrl.js
+++ src/library/product/details/stores/b/bestbuy/US/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'bestbuy.com',
     prefix: null,
-    url: 'https://www.bestbuy.com/site/{id}.p',
+    url: 'https://www.bestbuy.com/site/{id}.p?intl=nosplash',
     country: 'US',
     store: 'bestbuy',
     zipcode: '',
