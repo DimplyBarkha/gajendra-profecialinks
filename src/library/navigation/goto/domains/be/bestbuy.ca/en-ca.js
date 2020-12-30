b543bb9cef94ee9f00169525175e974896a514f0
PR <pravinr@synerzipune.local>
2020-12-30 22:49:42 +0530

    rebase

--- src/library/navigation/goto/domains/be/bestbuy.ca/en-ca.js
+++ src/library/navigation/goto/domains/be/bestbuy.ca/en-ca.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'bestbuy.ca/en-ca',
-    timeout: '100000',
+    timeout: '80000',
     country: 'CA',
     store: 'bestbuy',
     zipcode: '',
