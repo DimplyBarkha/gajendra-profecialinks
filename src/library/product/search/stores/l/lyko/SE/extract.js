839d23caab0500f410ce46c0090b7c5d4fb7f0ee
unknown <mailto:sirisha.thotla-odc@partner.import.io>
2020-12-30 18:10:18 +0530

    lyko-search--Damayanthi

--- src/library/product/search/stores/l/lyko/SE/extract.js
+++ src/library/product/search/stores/l/lyko/SE/extract.js
@@ -7,31 +7,37 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await context.evaluate(async function () {
-    while(!!document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button")){
-      // @ts-ignore
-      document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button").click()
-      await new Promise(r => setTimeout(r, 10000));
-    }
-      let scrollTop = 0;
-      while (scrollTop !== 1000) {
-        await stall(500);
-        scrollTop += 1000;
-        window.scroll(0, scrollTop);
-        if (scrollTop === 1000) {
-          await stall(500);
-          break;
-        }
-      }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
+  // await context.evaluate(async function () {
     
-  })
+      
+  //     while(!!document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button")){
+  //       // @ts-ignore
+  //       document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button").click()
+        
+  //       await new Promise(r => setTimeout(r, 10000));
+  //     }
+
+    
+  // })
+  await context.evaluate(async function () {
+  let scrollTop = 0;
+  while (scrollTop !== 1000) {
+    await stall(500);
+    scrollTop += 1000;
+    window.scroll(0, scrollTop);
+    if (scrollTop === 1000) {
+      await stall(500);
+      break;
+    }
+  }
+  function stall (ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, ms);
+    });
+  }
+})
   return await context.extract(productDetails, { transform });
 }
 
