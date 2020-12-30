8910b1f62af3d2bb7667dd23ad17c2834a356f4a
venkatsn <venkat.sn-odc@partner.import.io>
2020-12-30 14:32:18 +0530

    #2641

--- src/library/product/search/stores/m/meijer/US/execute.js
+++ src/library/product/search/stores/m/meijer/US/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'meijer.com',
     url: 'https://www.meijer.com/shop/en/search/?q={searchTerms}&page=0',
     loadedSelector: 'body',
-    noResultsXPath: '//div[@class="search-empty row "]',
+    noResultsXPath: null,//'//div[@class="search-empty row "]',
     zipcode: '',
   },
 };
