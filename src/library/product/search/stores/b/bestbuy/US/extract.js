6545a8b66987415f8cbb3cd4e8580321878b4085
PR <pravinr@synerzipune.local>
2020-12-30 22:53:59 +0530

    rebase

--- src/library/product/search/stores/b/bestbuy/US/extract.js
+++ src/library/product/search/stores/b/bestbuy/US/extract.js
@@ -8,4 +8,28 @@ module.exports = {
     domain: 'bestbuy.com',
     zipcode: '',
   },
+  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+    await context.evaluate(async () => {
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+
+      let scrollTop = 0;
+      while (scrollTop !== 5000) {
+        await stall(1000);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 5000) {
+          await stall(1000);
+          break;
+        }
+      }
+      
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
--- src/library/product/search/stores/b/bestbuy/US/paginate.js
+++ src/library/product/search/stores/b/bestbuy/US/paginate.js
@@ -4,10 +4,10 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'bestbuy',
-    nextLinkSelector: 'a.sku-list-page-next',
+    nextLinkSelector: 'a.sku-list-page-next[aria-disabled="false"]',
     mutationSelector: null, //"ol.paging-list",
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'ol.sku-item-list',
     noResultsXPath: "//div[contains(@class,'no-result')]",
     openSearchDefinition: null,
     domain: 'bestbuy.com',
