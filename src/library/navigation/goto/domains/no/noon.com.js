fa1a4e0b87973cc34b415007dd79c458f7508672
shantanuDevlper <shantanu.kulkarni@profecialinks.com>
2020-12-30 11:00:02 +0530

    changes for intheboxtext

--- src/library/navigation/goto/domains/no/noon.com.js
+++ src/library/navigation/goto/domains/no/noon.com.js
@@ -3,13 +3,13 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'noon.com',
-    timeout: 800000,
+    timeout: 2000000,
     country: 'SA',
     store: 'noon',
     zipcode: '',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 800000;
+    const timeout = parameters.timeout ? parameters.timeout : 2000000;
     await context.setBlockAds(false);
     await context.setLoadAllResources(true);
     await context.setLoadImages(true);
