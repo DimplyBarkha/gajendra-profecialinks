2e12e6be81fa3c616605401fcb4dbab139c6e7db
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2020-12-30 17:01:17 +0200

    ah be

--- src/library/product/search/stores/a/ah/BE/shared.js
+++ src/library/product/search/stores/a/ah/BE/shared.js
@@ -4,19 +4,9 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-  const searchTerms = [];
-  const urlCustom = [];
-  data.forEach(element => {
-    searchTerms.push(element.group.find(e => e.input));
-    urlCustom.push(element.group.find(e => e.url));
-  });
-  const filterSearch = searchTerms.filter(e => e)[0].input;
-  const url = urlCustom.filter(e => e)[0].url;
   data.forEach(el => {
     el.group.forEach((gr, index) => {
       gr['rankOrganic'] = [{ text: index + 1 }];
-      gr['_input'] = url;
-      gr['_url'] = filterSearch;
       if (gr && gr.productUrl && gr.productUrl.length) {
         gr.productUrl[0].text = 'https://www.ah.be' + gr.productUrl[0].text;
         gr['id'] = [{ text: 'wi' + gr.productUrl[0].text.match(/\d+/g).join() }];
