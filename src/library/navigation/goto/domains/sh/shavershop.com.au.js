c16c8b81b1af87fd62b967961c7a57ca96eccd6b
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2020-12-30 11:38:13 +0530

    extract.yml updated

--- src/library/navigation/goto/domains/sh/shavershop.com.au.js
+++ src/library/navigation/goto/domains/sh/shavershop.com.au.js
@@ -1,11 +1,19 @@
-
 module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'shavershop.com.au',
-    timeout: 100000,
+    timeout: 500000,
     country: 'AU',
     store: 'shavershop',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
