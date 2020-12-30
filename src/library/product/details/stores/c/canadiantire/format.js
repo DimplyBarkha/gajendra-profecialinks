a2ee90ee0f5f1a9a7be69de527c9a92e45d93860
rajeshProfecialinks <rajesh.g@profecialinks.com>
2020-12-30 17:19:44 +0530

    canadiantire CA en core feedtype QA fixed

--- src/library/product/details/stores/c/canadiantire/format.js
+++ src/library/product/details/stores/c/canadiantire/format.js
@@ -212,6 +212,11 @@ const transform = (data) => {
                 });
                 row.materials = [{ 'text': info.join(' | ') }];
             }
+            if (row.sku) {
+                row.sku.forEach(item => {
+                    item.text = item.text.substring(0, item.text.length-1);
+                });
+            }
         }
     }
     return cleanUp(data);
