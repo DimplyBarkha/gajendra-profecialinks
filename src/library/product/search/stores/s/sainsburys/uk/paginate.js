35d9e6443f3c9c8e1c461a47ba1c818bb6833e1b
Dhanush <dhanush.isukapalli@od.import.io>
2020-12-30 20:05:41 +0530

    sainsburys

--- src/library/product/search/stores/s/sainsburys/uk/paginate.js
+++ src/library/product/search/stores/s/sainsburys/uk/paginate.js
@@ -4,15 +4,15 @@ module.exports = {
   parameterValues: {
     country: 'uk',
     store: 'sainsburys',
-    nextLinkSelector: null,
+    nextLinkSelector: 'li[class="ln-c-pagination__item ln-c-pagination__item--next"] > *:not(.is-disabled)',
     mutationSelector: null,
     spinnerSelector: null,
     // loadedSelector: 'section[class="ln-o-section ln-o-section"] ul[class="ln-o-grid ln-o-grid--matrix ln-o-grid--equal-height"] li[class="pt-grid-item ln-o-grid__item ln-u-6/12@xs ln-u-3/12@md ln-u-2/12@xl"]',
-    openSearchDefinition: {
-      template: 'https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]={searchTerms}&pageNumber={page}&pageSize=90',
-      },
+    // openSearchDefinition: {
+    //   template: 'https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]={searchTerms}&pageNumber={page}&pageSize=90',
+    // },
     domain: 'sainsburys.co.uk',
-    noResultsXPath: null,
+    noResultsXPath: '(//div[contains(@class,"si__no-results")])[1]',
     zipcode: '',
   },
 };
