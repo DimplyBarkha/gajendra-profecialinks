f54e027f72f3fc837d34b4c63baad8ee8f55f73a
Gajender Ramola <ramola106@gmail.com>
2020-12-30 16:08:03 +0530

    fix issues

--- src/library/product/details/stores/t/thegoodguys/AU/createUrl.js
+++ src/library/product/details/stores/t/thegoodguys/AU/createUrl.js
@@ -2,7 +2,7 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'thegoodguys.com.au',
-    url: 'https://www.thegoodguys.com.au/SearchDisplay?categoryId=&storeId=900&searchTerm={}',
+    url: 'https://www.thegoodguys.com.au/SearchDisplay?categoryId=&storeId=900&searchTerm={id}',
     country: 'AU',
     store: 'thegoodguys',
     zipcode: '',
