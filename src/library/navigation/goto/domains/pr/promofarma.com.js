3619b87429acb9e10108822934a74782fc67e8b5
Karthickravi <karthik.ravi-odc@partner.import.io>
2020-12-30 17:54:07 +0530

    new

--- src/library/navigation/goto/domains/pr/promofarma.com.js
+++ src/library/navigation/goto/domains/pr/promofarma.com.js
@@ -8,4 +8,30 @@ module.exports = {
     store: 'promofarma',
     zipcode: '',
   },
+  implementation: async (
+    { url },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    const responseStatus = await context.goto(url, {
+      firstRequestTimeout: 10000,
+      timeout: timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+      antiCaptchaOptions: {
+        type: 'RECAPTCHA',
+      },
+    });
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);    
+  }
 };
--- src/library/product/details/stores/p/promofarma/ES/extract.js
+++ src/library/product/details/stores/p/promofarma/ES/extract.js
@@ -10,16 +10,32 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      // @ts-ignore
-      document.querySelector('div[class="text-center"]>button').click()
-      await new Promise(r => setTimeout(r, 6000));
+      try {
+        // @ts-ignore
+        document.querySelector('div[class="banner-actions-container"]>button').click()
+        await new Promise(r => setTimeout(r, 10000));
+      } catch (error) {
+
+      }
+      try {
+        // @ts-ignore
+        document.querySelector('div[class="text-center"]>button').click()
+        await new Promise(r => setTimeout(r, 10000));
+      } catch (error) {
+      }
+      try {
+        // @ts-ignore
+        document.querySelector('div[class="abtasty-modal__close abtasty-modal__close--inside"]').click()
+        await new Promise(r => setTimeout(r, 10000));
+      } catch (error) {
+      }
       function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
-        }
+      }
       // Method to Retrieve Xpath content of a Single Node
       var getXpath = (xpath, prop) => {
         var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
@@ -33,8 +49,31 @@ module.exports = {
         URL2 = "Yes"
       } else {
         URL2 = "No"
+
+        addElementToDocument('zoom', URL2);
       }
-      addElementToDocument('zoom', URL2);
+      try {
+        // @ts-ignore
+        var man1 = document.querySelector('p[data-qa-ta="couponInfo"]').innerText
+        // @ts-ignore
+        var man2 = document.querySelector('div[id="content-description"]').innerText
+
+        var man = man1 + '||' + man2
+
+        addElementToDocument('manu', man);
+
+      } catch (error) {
+
+      }
+      try {
+        var video = getXpath("(//div[@id='description']/div[@id='wrapper-description']/div//@data-content)", 'nodeValue');
+        var video = video.split('src="')[1]
+        var video = video.split(" ")[0]
+        var video = video.slice(0, -1)
+        addElementToDocument('video', video);
+      } catch (error) {
+        
+      }
     });
     await context.extract(productDetails);
   },
