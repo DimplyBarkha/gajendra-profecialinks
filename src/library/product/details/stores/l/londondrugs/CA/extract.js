ba3df44809567cced60e6fd9aa400e3595f399ab
DeepikaO <deepika.kuppireddy@od.import.io>
2020-12-30 23:55:31 +0530

    #2704 londondrugs details feed type

--- src/library/product/details/stores/l/londondrugs/CA/extract.js
+++ src/library/product/details/stores/l/londondrugs/CA/extract.js
@@ -1,4 +1,41 @@
+// const { transform } = require('../../../../../details/shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addElementToDocument (id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+      }
+      return result;
+    };
+    // Double Pipe Concatenation
+    const pipeSeparatorDouble = (id, data) => {
+      var doubleSeparatorText = data.join(' || ');
+      addElementToDocument(id, doubleSeparatorText);
+    };
 
+    // XPATH Data Extraction For Additional Description Bullet
+    const addDescBulletInfo = getAllXpath("//div[@id='pdp-overview']", 'nodeValue');
+    pipeSeparatorDouble('addDescBulletInfo', addDescBulletInfo);
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,4 +45,5 @@ module.exports = {
     domain: 'londondrugs.com',
     zipcode: '',
   },
+  implementation,
 };
