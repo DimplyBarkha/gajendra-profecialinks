9b053a5662d15309032907c8c9e460eb77e8ffb7
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-31 18:22:06 +0530

    updated execute

--- src/library/product/details/stores/m/mediaexpert/PL/execute.js
+++ src/library/product/details/stores/m/mediaexpert/PL/execute.js
@@ -15,7 +15,6 @@ module.exports = {
     context,
     dependencies,
   ) {
-    
     const searchUrl = `https://www.mediaexpert.pl/search?query[menu_item]=&query[querystring]=${inputs.id}`;
     await context.goto(searchUrl, {
       timeout: 30000,
@@ -25,8 +24,7 @@ module.exports = {
       css_enabled: false,
       random_move_mouse: true,
     });
-    
-    
+
     const productDetailsLink = await context.evaluate(function (inputs) {
       const productList = document.querySelectorAll('div.c-grid_col.is-grid-col-1');
       for (let i = 0; i < productList.length; i++) {
@@ -52,22 +50,22 @@ module.exports = {
         random_move_mouse: true,
       });
     } else {
-      const noProductsFound = await context.evaluate(function (inputs) {
+      return await context.evaluate(function (inputs) {
         const noResults = document.querySelector('div.is-noResults');
-        return noResults;
+        return !noResults;
       });
-      if (noProductsFound || noProductsFound === undefined) {
-        console.log('Checking no results', parameters.noResultsXPath);
-        return await context.evaluate(function (xp) {
-          const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-          console.log(xp, r);
-          const e = r.iterateNext();
-          console.log(e);
-          return !e;
-        }, parameters.noResultsXPath);
-      }
+      // if (noProductsFound) {
+      //   console.log('Checking no results', parameters.noResultsXPath);
+      //   return await context.evaluate(function (xp) {
+      //     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+      //     console.log(xp, r);
+      //     const e = r.iterateNext();
+      //     console.log(e);
+      //     return !e;
+      //   }, parameters.noResultsXPath);
+      // }
     }
     await context.waitForNavigation({ timeout: 60000, waitUntil: 'networkidle0' });
-    
+    return true;
   },
 };
