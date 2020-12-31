40cf090102207996604a37e982c270a44e9e93c6
cshelake <cshelake7@gmail.com>
2020-12-31 13:03:25 +0530

    review & search

--- src/library/navigation/goto/domains/am/amazon.fr.js
+++ src/library/navigation/goto/domains/am/amazon.fr.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'amazon.fr',
-    timeout: null,
+    timeout: 50000,
     country: 'FR',
     store: 'amazon',
     zipcode: "''",
