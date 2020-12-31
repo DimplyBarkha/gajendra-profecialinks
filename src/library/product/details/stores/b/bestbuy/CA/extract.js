5f6bccd6d092d5c06e91cab8b7fa295269edd6cd
Mamatha-Gundapaneni <mamatha.g@osmosys.asia>
2020-12-31 10:48:56 +0530

    Added code

--- src/library/product/details/stores/b/bestbuy/CA/extract.js
+++ src/library/product/details/stores/b/bestbuy/CA/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require('../../../../shared');
+const { transform } = require('./shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -6,7 +6,7 @@ module.exports = {
     store: 'bestbuyCA',
     transform: transform,
     domain: 'bestbuy.ca/en-ca',
-    zipcode: '',    
+    zipcode: '',
   },
   implementation: async (inputs,
     parameters,
@@ -24,10 +24,9 @@ module.exports = {
     try {
       await context.waitForXPath('//h3[text()="Warranty"]/parent::div/following-sibling::div');
     } catch (error) {
-      console.log("Warranty element not found");
+      console.log('Warranty element not found');
     }
     await context.evaluate(async function () {
-      
       function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
@@ -35,83 +34,84 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
-     let detailsAndSpecsTextArr = [];
-     let warrantyTextArr = [];
-     let detailsAndSpecsText = document.querySelectorAll('div.itemContainer_20kXj');
-     for (let index = 0; index < detailsAndSpecsText.length; index++) {
-       let field = detailsAndSpecsText[index].querySelector('div.itemName_37zd4');
-       let specs = detailsAndSpecsText[index].querySelector('div.itemValue_XPfaq');
-       // @ts-ignore
-       let fieldSpec = field.innerText + ' : '+specs.innerText;
-       if(fieldSpec.includes("Warranty")){
-        warrantyTextArr.push(fieldSpec);
-       }
-       detailsAndSpecsTextArr.push(fieldSpec);
-     }
-     // @ts-ignore
-     warrantyTextArr = warrantyTextArr.join(' | ');
-     addElementToDocument('bb_warranty', warrantyTextArr);
-     // @ts-ignore
-     detailsAndSpecsTextArr = detailsAndSpecsTextArr.join(' || ');
-     addElementToDocument('bb_specifications', detailsAndSpecsTextArr);
-     //---------------------------------------------------------------
-     let dimension = document.querySelectorAll('div.smallSizeContainer_1UtWF div.itemContainer_20kXj');
-     let dimensionTxtField = '';
-     let originTxtField = '';
-     let dimensionTxtFieldVal = '';
-     let originTxtFieldVal = '';
-     let dimensionTxt = '';
-     let originTxt = '';
-     let dimensionTxtArr =[];
-     let originTxtArr=[];
-     let netWeightTxt = '';
-     let colorTxt = '';
-     for (let index = 0; index < dimension.length; index++) {
-       const element = dimension[index];
-       // @ts-ignore
-       let elementTxt = element.innerText;
-       if((elementTxt === 'Width')||(elementTxt === 'Height') || (elementTxt === 'Depth')|| (elementTxt === 'Width (Inches)') || (elementTxt === 'Height (Inches)') || (elementTxt === 'Depth (Inches)')){
+      let detailsAndSpecsTextArr = [];
+      let warrantyTextArr = [];
+      const detailsAndSpecsText = document.querySelectorAll('div.itemContainer_20kXj');
+      for (let index = 0; index < detailsAndSpecsText.length; index++) {
+        const field = detailsAndSpecsText[index].querySelector('div.itemName_37zd4');
+        const specs = detailsAndSpecsText[index].querySelector('div.itemValue_XPfaq');
         // @ts-ignore
-        dimensionTxtField = element.innerText;
+        const fieldSpec = field.innerText + ' : ' + specs.innerText;
+        if (fieldSpec.includes('Warranty')) {
+          warrantyTextArr.push(fieldSpec);
+        }
+        detailsAndSpecsTextArr.push(fieldSpec);
+      }
+      // @ts-ignore
+      warrantyTextArr = warrantyTextArr.join(' | ');
+      addElementToDocument('bb_warranty', warrantyTextArr);
+      // @ts-ignore
+      detailsAndSpecsTextArr = detailsAndSpecsTextArr.join(' || ');
+      addElementToDocument('bb_specifications', detailsAndSpecsTextArr);
+      // ---------------------------------------------------------------
+      const dimension = document.querySelectorAll('div.smallSizeContainer_1UtWF div.itemContainer_20kXj');
+      let dimensionTxtField = '';
+      let originTxtField = '';
+      let dimensionTxtFieldVal = '';
+      let originTxtFieldVal = '';
+      let dimensionTxt = '';
+      const originTxt = '';
+      const dimensionTxtArr = [];
+      const originTxtArr = [];
+      let netWeightTxt = '';
+      let colorTxt = '';
+      for (let index = 0; index < dimension.length; index++) {
+        const element = dimension[index];
         // @ts-ignore
-        dimensionTxtFieldVal = element.nextSibling.innerText;
-        dimensionTxt = dimensionTxtField+' : '+dimensionTxtFieldVal;
-        dimensionTxtArr.push(dimensionTxt);
-       }
-       if((elementTxt === 'Country of Origin')){
+        const elementTxt = element.innerText;
+        if ((elementTxt === 'Width') || (elementTxt === 'Height') || (elementTxt === 'Depth') || (elementTxt === 'Width (Inches)') || (elementTxt === 'Height (Inches)') || (elementTxt === 'Depth (Inches)')) {
         // @ts-ignore
-        originTxtField = element.innerText;
+          dimensionTxtField = element.innerText;
+          // @ts-ignore
+          dimensionTxtFieldVal = element.nextSibling.innerText;
+          dimensionTxt = dimensionTxtField + ' : ' + dimensionTxtFieldVal;
+          dimensionTxtArr.push(dimensionTxt);
+        }
+        if ((elementTxt === 'Country of Origin')) {
         // @ts-ignore
-        originTxtFieldVal = element.nextSibling.innerText;
-        originTxtArr.push(originTxtFieldVal);
-       }
-       if((elementTxt === 'Weight') || (elementTxt==='Weight (lbs)')){
+          originTxtField = element.innerText;
+          // @ts-ignore
+          originTxtFieldVal = element.nextSibling.innerText;
+          originTxtArr.push(originTxtFieldVal);
+        }
+        if ((elementTxt === 'Weight') || (elementTxt === 'Weight (lbs)')) {
         // @ts-ignore
-        netWeightTxt = element.nextSibling.innerText;
-       }
-       if((elementTxt === 'Colour') || (elementTxt === 'Band Colour')){
+          netWeightTxt = element.nextSibling.innerText;
+        }
+        if ((elementTxt === 'Colour') || (elementTxt === 'Band Colour')) {
         // @ts-ignore
-        colorTxt = element.nextSibling.innerText;
-       }
-     }
-     addElementToDocument('bb_colour', colorTxt);
-     addElementToDocument('bb_weightNet', netWeightTxt);
-     addElementToDocument('bb_countryOfOrigin', originTxtArr.join(' | '));
-     addElementToDocument('bb_dimension', dimensionTxtArr.join(' | '));
+          colorTxt = element.nextSibling.innerText;
+        }
+      }
+      addElementToDocument('bb_colour', colorTxt);
+      addElementToDocument('bb_weightNet', netWeightTxt);
+      addElementToDocument('bb_countryOfOrigin', originTxtArr.join(' | '));
+      addElementToDocument('bb_dimension', dimensionTxtArr.join(' | '));
     });
-     //-----------------------------------------------------------------
-     await context.evaluate(async function () {
+    // -----------------------------------------------------------------
+    await context.evaluate(async function () {
       const buttonSelector1 = 'button#moreInfo';
       let available = document.querySelector(buttonSelector1);
       // @ts-ignore
       available = available ? available.click() : '';
-     });
-     try {
-     await context.waitForSelector('div.productDescription_1F7nr');
-     } catch (error) {
-       console.log("Product description not loaded");
-     }
-     await context.evaluate(async function () {
+    });
+    try {
+      await context.waitForSelector('div.productDescription_1F7nr');
+    } catch (error) {
+      console.log('Product description not loaded');
+    }
+    await context.evaluate(async function () {
+      document.querySelectorAll('div#moreInfo ul li').forEach((ele) => ele.textContent = `|| ${ele.textContent}`);
       function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
@@ -119,19 +119,19 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
-     let description = document.querySelector('div[itemprop="description"]');
+      let description = document.querySelector('div[itemprop="description"]');
       // @ts-ignore
       description = description ? description.innerText : '';
       let descArr = [];
-      let additionalBulletInfo = [];
+      const additionalBulletInfo = [];
       // @ts-ignore
-      if(description !== ''){
+      if (description !== '') {
         descArr.push(description);
       }
       let bulletsDescription = document.querySelectorAll('div.productDescription_1F7nr ul li');
       console.log('bulletsDescription: ', bulletsDescription);
       let bulletCount = bulletsDescription.length;
-      if(bulletsDescription.length !== 0){
+      if (bulletsDescription.length !== 0) {
         bulletCount = bulletsDescription.length;
         for (let index = 0; index < bulletsDescription.length; index++) {
           let element = bulletsDescription[index];
@@ -142,13 +142,13 @@ module.exports = {
         }
         addElementToDocument('bb_descriptionBulletsCount', bulletCount);
         // @ts-ignore
-      descArr = descArr.join(' | ');
-      addElementToDocument('bb_descriptionBullets', descArr);
-      addElementToDocument('bb_addDescriptionBulletsInfo', additionalBulletInfo.join(' | '));
+        descArr = descArr.join(' | ');
+        addElementToDocument('bb_descriptionBullets', descArr);
+        addElementToDocument('bb_addDescriptionBulletsInfo', additionalBulletInfo.join(' | '));
       }
       let count = 0;
 
-      if(bulletsDescription.length == 0){
+      if (bulletsDescription.length == 0) {
         bulletsDescription = document.querySelectorAll('div.productDescription_1F7nr p');
         bulletCount = bulletsDescription.length;
         // if(bulletsDescription.length == 1){
@@ -159,28 +159,27 @@ module.exports = {
           // @ts-ignore
           element = element ? element.innerText.replace(/•/, ' | ') : '';
           // @ts-ignore
-          if(element.includes(' | ')){
+          if (element.includes(' | ')) {
             additionalBulletInfo.push(element);
-           count++;
+            count++;
           }
-          
+
           descArr.push(element);
         }
 
-      let desc = descArr.join(' ');
-      // @ts-ignore
-      descArr = descArr.join(' ');
-      addElementToDocument('bb_descriptionBullets', descArr);
+        const desc = descArr.join(' ');
+        // @ts-ignore
+        descArr = descArr.join(' ');
+        addElementToDocument('bb_descriptionBullets', descArr);
 
-      addElementToDocument('bb_descriptionBulletsCount', count);
-      addElementToDocument('bb_addDescriptionBulletsInfo', additionalBulletInfo.join(' | '));
-      console.log('count: ', count);
+        addElementToDocument('bb_descriptionBulletsCount', count);
+        addElementToDocument('bb_addDescriptionBulletsInfo', additionalBulletInfo.join(' | '));
+        console.log('count: ', count);
       }
-      
     });
-      //------------------------------------------------------------
-      // await context.waitForSelector('img[class="middle_1qXv8"]');
-      await context.evaluate(async function () {
+    // ------------------------------------------------------------
+    // await context.waitForSelector('img[class="middle_1qXv8"]');
+    await context.evaluate(async function () {
       function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
@@ -188,28 +187,28 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
-     let videoLinkArr = [];
-     let videos = document.querySelectorAll('img[class="middle_1qXv8"]');
-     for (let index = 0; index < videos.length; index++) {
-       // @ts-ignore
-       let element = videos[index];
-       element = element.previousElementSibling;
-       // @ts-ignore
-       element = element ? element.src : '';
-       console.log('element: ', element);
-       // @ts-ignore
-       if(element.includes('https://i1.ytimg.com/vi/')){
-       // @ts-ignore
-       let videoLink = element.replace(/https:\/\/i1\.ytimg\.com\/vi\/(.*)\/default.jpg/,'$1');
-       videoLink = 'https://www.youtube.com/watch?v='+videoLink+'&feature=youtu.be';
-       videoLinkArr.push(videoLink)
-       }
-     }
-     // @ts-ignore
-     videoLinkArr = videoLinkArr.join(' | ');
-     addElementToDocument('bb_videos', videoLinkArr);
+      let videoLinkArr = [];
+      const videos = document.querySelectorAll('img[class="middle_1qXv8"]');
+      for (let index = 0; index < videos.length; index++) {
+        // @ts-ignore
+        let element = videos[index];
+        element = element.previousElementSibling;
+        // @ts-ignore
+        element = element ? element.src : '';
+        console.log('element: ', element);
+        // @ts-ignore
+        if (element.includes('https://i1.ytimg.com/vi/')) {
+          // @ts-ignore
+          let videoLink = element.replace(/https:\/\/i1\.ytimg\.com\/vi\/(.*)\/default.jpg/, '$1');
+          videoLink = 'https://www.youtube.com/watch?v=' + videoLink;
+          videoLinkArr.push(videoLink);
+        }
+      }
+      // @ts-ignore
+      videoLinkArr = videoLinkArr.join(' | ');
+      addElementToDocument('bb_videos', videoLinkArr);
     });
-     //------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------
     //  await context.evaluate(async function () {
     //   const buttonSelector2 = 'button#whatsIncluded';
     //   let available = document.querySelector(buttonSelector2);
@@ -221,7 +220,7 @@ module.exports = {
     //  } catch (error) {
     //   console.log("whatsIncluded description not loaded");
     //  }
-     //------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
     //  await context.evaluate(async function () {
     //  let otherInfoArr = [];
     //  let otherInfo = document.querySelector('button#whatsIncluded');
@@ -236,16 +235,16 @@ module.exports = {
     //  // @ts-ignore
     //  otherInfoArr = otherInfoArr.join('');
     //  addElementToDocument('bb_productOtherInformation', otherInfoArr);
-     //------------------------------------------------------------------------------
-  //    function addElementToDocument (key, value) {
-  //     const catElement = document.createElement('div');
-  //     catElement.id = key;
-  //     catElement.textContent = value;
-  //     catElement.style.display = 'none';
-  //     document.body.appendChild(catElement);
-  //   }
-  //  });
-   
+    // ------------------------------------------------------------------------------
+    //    function addElementToDocument (key, value) {
+    //     const catElement = document.createElement('div');
+    //     catElement.id = key;
+    //     catElement.textContent = value;
+    //     catElement.style.display = 'none';
+    //     document.body.appendChild(catElement);
+    //   }
+    //  });
+
     return await context.extract(productDetails, { transform });
   },
 };
--- src/library/product/details/stores/b/bestbuy/CA/shared.js
+++ src/library/product/details/stores/b/bestbuy/CA/shared.js
@@ -1,195 +1,182 @@
 const transform = (data) => {
-    const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-  
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.shippingDimensions) {
-          row.shippingDimensions.forEach(item => {
-            const locText = item.text;
-            if (locText.indexOf('###') > 0) {
-              item.text = locText.substring(0, locText.indexOf('###'));
-            } else if (locText.indexOf('###') === 0) {
-              item.text = locText.replace('###', '');
-            }
-            console.log(item.text);
-          });
-        }
-        if (row.specifications) {
-          const specs = [];
-          let newTxt = '';
-          let cnt = 0;
-          row.specifications.forEach(item => {
-            specs[0] = item;
-            item.text = item.text.replace(/(\s?\n)+/g, ' ');
-            if (cnt > 0) newTxt = newTxt + ' || ' + item.text;
-            else newTxt = newTxt + item.text;
-            cnt++;
-          });
-          specs.forEach(item => {
-            item.text = newTxt;
-          });
-          row.specifications = specs;
-        }
-        if (row.variantUrl) {
-          const variantUrls = [];
-          let dupUrl = '';
-          let urls = [];
-          row.variantUrl.forEach(item => {
-            console.log('item:: ', item.text);
-            urls = row.variantUrl.filter(it => item.text === it.text);
-            if (urls && urls.length === 1) {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.shippingDimensions) {
+        row.shippingDimensions.forEach(item => {
+          const locText = item.text;
+          if (locText.indexOf('###') > 0) {
+            item.text = locText.substring(0, locText.indexOf('###'));
+          } else if (locText.indexOf('###') === 0) {
+            item.text = locText.replace('###', '');
+          }
+          console.log(item.text);
+        });
+      }
+
+      if (row.variantUrl) {
+        const variantUrls = [];
+        let dupUrl = '';
+        let urls = [];
+        row.variantUrl.forEach(item => {
+          console.log('item:: ', item.text);
+          urls = row.variantUrl.filter(it => item.text === it.text);
+          if (urls && urls.length === 1) {
+            variantUrls.push(item);
+          } else {
+            if (dupUrl !== item.text) {
+              dupUrl = item.text;
               variantUrls.push(item);
-            } else {
-              if (dupUrl !== item.text) {
-                dupUrl = item.text;
-                variantUrls.push(item);
-              }
             }
-          });
-          row.variantUrl = variantUrls;
-        }
-  
-        if (row.videos) {
-          const video = [];
-          let dupUrl = '';
-          let urls = [];
-          row.videos.forEach(item => {
-            console.log('item:: ', item.text);
-            urls = row.videos.filter(it => item.text === it.text);
-            if (urls && urls.length === 1) {
+          }
+        });
+        row.variantUrl = variantUrls;
+      }
+
+      if (row.videos) {
+        const video = [];
+        let dupUrl = '';
+        let urls = [];
+        row.videos.forEach(item => {
+          console.log('item:: ', item.text);
+          urls = row.videos.filter(it => item.text === it.text);
+          if (urls && urls.length === 1) {
+            video.push(item);
+          } else {
+            if (dupUrl !== item.text) {
+              dupUrl = item.text;
               video.push(item);
-            } else {
-              if (dupUrl !== item.text) {
-                dupUrl = item.text;
-                video.push(item);
-              }
             }
-          });
-          row.videos = video;
-        }
-  
-        if (row.manufacturerDescription) {
-          const manufacturerDescriptions = [];
-          let dupUrl = '';
-          let urls = [];
-          row.manufacturerDescription.forEach(item => {
-            console.log('item:: ', item.text);
-            urls = row.manufacturerDescription.filter(it => item.text === it.text);
-            if (urls && urls.length === 1) {
+          }
+        });
+        row.videos = video;
+      }
+
+      if (row.manufacturerDescription) {
+        const manufacturerDescriptions = [];
+        let dupUrl = '';
+        let urls = [];
+        row.manufacturerDescription.forEach(item => {
+          console.log('item:: ', item.text);
+          urls = row.manufacturerDescription.filter(it => item.text === it.text);
+          if (urls && urls.length === 1) {
+            manufacturerDescriptions.push(item);
+          } else {
+            if (dupUrl !== item.text) {
+              dupUrl = item.text;
               manufacturerDescriptions.push(item);
-            } else {
-              if (dupUrl !== item.text) {
-                dupUrl = item.text;
-                manufacturerDescriptions.push(item);
-              }
             }
-          });
-          row.manufacturerDescription = manufacturerDescriptions;
-        }
-  
-        if (row.manufacturerImages) {
-          const manufacturerImage = [];
-          let dupUrl = '';
-          let urls = [];
-          row.manufacturerImages.forEach(item => {
-            console.log('item:: ', item.text);
-            urls = row.manufacturerImages.filter(it => item.text === it.text);
-            if (urls && urls.length === 1) {
+          }
+        });
+        row.manufacturerDescription = manufacturerDescriptions;
+      }
+
+      if (row.manufacturerImages) {
+        const manufacturerImage = [];
+        let dupUrl = '';
+        let urls = [];
+        row.manufacturerImages.forEach(item => {
+          console.log('item:: ', item.text);
+          urls = row.manufacturerImages.filter(it => item.text === it.text);
+          if (urls && urls.length === 1) {
+            manufacturerImage.push(item);
+          } else {
+            if (dupUrl !== item.text) {
+              dupUrl = item.text;
               manufacturerImage.push(item);
-            } else {
-              if (dupUrl !== item.text) {
-                dupUrl = item.text;
-                manufacturerImage.push(item);
-              }
             }
-          });
-          row.manufacturerImages = manufacturerImage;
-        }
-  
-        if (row.description) {
-          let text = '';
-          row.description.forEach(item => {
-            text += `${item.text.replace(/\n \n/g, ':')} `;
-          });
-          row.description = [
-            {
-              text: text.slice(0, -4),
-            },
-          ];
-        }
-        if (row.variants) {
-          let text = '';
-          row.variants.forEach(item => {
-            text += `${item.text.replace(/\n \n/g, ' ')} | `;
-          });
-          row.variants = [
-            {
-              text: text.slice(0, -3),
-            },
-          ];
-        }
-        if (row.variantId) {
-          const variantIds = [];
-          let dup = '';
-          let urls = [];
-          row.variantId.forEach(item => {
-            urls = row.variantId.filter(it => item.text === it.text);
-            if (urls && urls.length === 1) {
+          }
+        });
+        row.manufacturerImages = manufacturerImage;
+      }
+
+      if (row.variants) {
+        let text = '';
+        row.variants.forEach(item => {
+          text += `${item.text.replace(/\n \n/g, ' ')} | `;
+        });
+        row.variants = [
+          {
+            text: text.slice(0, -3),
+          },
+        ];
+      }
+      if (row.variantId) {
+        const variantIds = [];
+        let dup = '';
+        let urls = [];
+        row.variantId.forEach(item => {
+          urls = row.variantId.filter(it => item.text === it.text);
+          if (urls && urls.length === 1) {
+            variantIds.push(item);
+          } else {
+            if (dup !== item.text) {
+              dup = item.text;
               variantIds.push(item);
-            } else {
-              if (dup !== item.text) {
-                dup = item.text;
-                variantIds.push(item);
-              }
             }
-          });
-          row.variantId = variantIds;
+          }
+        });
+        row.variantId = variantIds;
+      }
+
+      if (row.additionalDescBulletInfo) {
+        let text = '';
+        row.additionalDescBulletInfo.forEach(item => {
+          text = text + (text ? ' ' : ' ') + item.text;
+        });
+        row.additionalDescBulletInfo = [{ text }];
+      }
+
+      if (row.description) {
+        let text = '';
+        row.description.forEach(item => {
+          text = text + (text ? ' ' : ' ') + item.text;
+        });
+        row.description = [{ text }];
+      }
+
+      if (row.alternateImages) {
+        const images = row.alternateImages.filter(img => !img.text.match('#'));
+        row.alternateImages = images;
+      }
+
+      if (row.alternateImages) {
+        let text = '';
+        row.alternateImages.forEach(item => {
+          text = text + (text ? ' | ' : ' ') + item.text;
+        });
+        row.alternateImages = [{ text }];
+      }
+
+      if (row.specifications) {
+        let text = '';
+        for (let i = 0; i < row.specifications.length; i++) {
+          text += `${row.specificationsLabel[i].text} : ${row.specifications[i].text} || `;
         }
-  
-        if (row.description) {
-          const descs = [];
-          let newTxt = '';
-          let cnt = 0;
-          row.description.forEach(item => {
-            descs[0] = item;
-            item.text = item.text.replace(/(\s?\n)+/g, ' ').trim();
-            if (cnt > 0) newTxt = newTxt + '||' + item.text;
-            else newTxt = newTxt + item.text;
-            cnt++;
-          });
-          descs.forEach(item => {
-            item.text = newTxt;
-          });
-          row.description = descs;
-        }
-        if (row.aggregateRating) {
-          row.aggregateRating = [
-            {
-              text: row.aggregateRating[0].text.replace('.', ','),
-            },
-          ];
-        }
-        if (row.alternateImages) {
-          const images = row.alternateImages.filter(img => !img.text.match('#'));
-          row.alternateImages = images;
-        }
-      
+        row.specifications = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+        delete row.specificationsLabel;
       }
     }
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      el.text = clean(el.text);
-    }))));
-  
-    return data;
-  };
-  module.exports = { transform };
+  }
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+    el.text = el.text.trim();
+  }))));
+  return data;
+};
+module.exports = { transform };
