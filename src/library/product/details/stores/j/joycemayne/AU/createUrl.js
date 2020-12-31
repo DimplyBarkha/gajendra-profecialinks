1d03f8562a7eee363ba062e179bdeb4090efe059
vikas kyatannawar <vikas.k@osmosys.asia>
2020-12-31 14:05:09 +0530

    updated create url and no results

--- src/library/product/details/stores/j/joycemayne/AU/createUrl.js
+++ src/library/product/details/stores/j/joycemayne/AU/createUrl.js
@@ -9,11 +9,11 @@ async function implementation (
   if (parameters.url) {
     const url = parameters.url.replace('{id}', encodeURIComponent(id));
     await dependencies.goto({ url });
-    return await context.evaluate(async function () {
-      if (document.querySelector('div[id="category-grid"] div:first-child a')) {
-        return document.querySelector('div[id="category-grid"] div:first-child a').href;
-      }
-    });
+    return await context.evaluate(async function (id) {
+      const response = await fetch(`https://www.joycemayne.com.au/ajaxwishlist/cart/add/product/${id}/isAjax/1/`);
+      const json = await response.json();
+      return json.url || 'https://www.joycemayne.com.au/catalogsearch/result/?q=errorpage';
+    }, id);
   }
 }
 module.exports = {
@@ -24,7 +24,7 @@ module.exports = {
   parameterValues: {
     domain: 'joycemayne.com.au',
     prefix: null,
-    url: 'https://www.joycemayne.com.au/catalogsearch/result/?q={id}',
+    url: 'https://www.joycemayne.com.au',
     country: 'AU',
     store: 'joycemayne',
     zipcode: '',
