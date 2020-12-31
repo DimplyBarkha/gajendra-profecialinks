f1a2ace3de2033ea34763057affd972df3aeb341
David Skoglund <david.skoglund@import.io>
2020-12-31 09:46:20 -0800

    updated paginate configs in order to  user goto  for pagination vs click

--- src/library/product/search/stores/a/amazon/CA/paginate.js
+++ src/library/product/search/stores/a/amazon/CA/paginate.js
@@ -1,17 +1,17 @@
-
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'CA',
     store: 'amazon',
-    nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
-    spinnerSelector: 'div.s-result-list-placeholder:not(.aok-hidden)',
+    domain: 'amazon.ca',
+    // nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
+    // spinnerSelector: 'div.s-result-list-placeholder:not(.aok-hidden)',
     // Use openSearchDefinition if nextLink has navigation issues.
+    stopConditionSelectorOrXpath: 'ul.a-pagination>li.a-last.a-disabled',
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
     openSearchDefinition: {
       template: 'https://www.amazon.ca/s?k={searchTerms}&ref=nb_sb_noss_2&page={page}',
     },
-    noResultsXPath: '//div[contains(@class, "s-result-list")]/div[@data-asin]/span[contains(@class,"no-results")]',
-    domain: 'amazon.ca',
+    noResultsXPath: '//div[contains(@class, "s-result-list")]/div[@data-asin]/span[contains(@class,"no-results")]'
   },
 };
