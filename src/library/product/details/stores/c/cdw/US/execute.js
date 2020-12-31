1d228ed9fd509774d3d79cfdfcafd6e5d76ac6c0
Judhisthira Sahoo <judhishtor@profecialinks.com>
2020-12-31 12:02:06 +0530

    #969 fiixign kick back issue

--- src/library/product/details/stores/c/cdw/US/execute.js
+++ src/library/product/details/stores/c/cdw/US/execute.js
@@ -1,3 +1,33 @@
+/**
+ *
+ * @param { { url?: string,  id?: string, zipcode?: any, storeId?:any} } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action, createUrl: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  let { url, id, zipcode, storeId } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  await dependencies.goto({ url, zipcode, storeId });
+  await new Promise((resolve, reject) => setTimeout(resolve, 8000));
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+
+  // TODO: Check for not found?
+}
 
 module.exports = {
   implements: 'product/details/execute',
@@ -9,4 +39,5 @@ module.exports = {
     noResultsXPath: null,
     zipcode: '',
   },
+  implementation
 };
