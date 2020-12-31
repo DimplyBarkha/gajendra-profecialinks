a04c511e08f243652a99864c152b9be8520c64a3
srishivams <srishivams97@gmail.com>
2020-12-31 17:08:22 +0530

    fixed no product issue

--- src/library/product/details/stores/d/domayne/format.js
+++ src/library/product/details/stores/d/domayne/format.js
@@ -6,7 +6,6 @@
 const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
-
       if (row.termsAndConditions) {
         row.termsAndConditions = [{ text: 'Yes' }];
       } else {
@@ -69,10 +68,10 @@ const transform = (data) => {
 
       if (row.inTheBoxUrl) {
         row.inTheBoxUrl.forEach((inTheBoxUrlItem) => {
-          inTheBoxUrlItem.text = inTheBoxUrlItem.text.replace(/\,.*/, '');
+          inTheBoxUrlItem.text = inTheBoxUrlItem.text.replace(/,.*/, '');
           inTheBoxUrlItem.text = inTheBoxUrlItem.text.replace('200w', '');
-          inTheBoxUrlItem.text = 'I'+inTheBoxUrlItem.text;
-          inTheBoxUrlItem.text = inTheBoxUrlItem.text.slice(1,-1);
+          inTheBoxUrlItem.text = 'I' + inTheBoxUrlItem.text;
+          inTheBoxUrlItem.text = inTheBoxUrlItem.text.slice(1, -1);
         });
       }
 
@@ -81,25 +80,25 @@ const transform = (data) => {
           if (aggregateRatingItem.text.includes('no rating')) {
             aggregateRatingItem.text = '';
           } else {
-            aggregateRatingItem.text = aggregateRatingItem.text.replace(/(.*?) out of.*/gm, '$1')
+            aggregateRatingItem.text = aggregateRatingItem.text.replace(/(.*?) out of.*/gm, '$1');
           }
         });
       }
       const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
+        .replace(/\r\n|\r|\n/g, ' ')
+        .replace(/&amp;nbsp;/g, ' ')
+        .replace(/&amp;#160/g, ' ')
+        .replace(/\u00A0/g, ' ')
+        .replace(/\s{2,}/g, ' ')
+        .replace(/"\s{1,}/g, '"')
+        .replace(/\s{1,}"/g, '"')
+        .replace(/^ +| +$|( )+/g, ' ')
       // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      el.text = clean(el.text);
-    }))));
+        .replace(/[\x00-\x1F]/g, '')
+        .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }))));
     }
   }
   return data;
