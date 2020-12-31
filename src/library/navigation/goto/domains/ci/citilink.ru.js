aed284e323a5d95275cf1ed637b83ac91e2547f5
shrutikab <shrutika.bongarde@synerzip.com>
2020-12-31 16:28:44 +0530

    added new changes(#2323)

--- src/library/navigation/goto/domains/ci/citilink.ru.js
+++ src/library/navigation/goto/domains/ci/citilink.ru.js
@@ -8,4 +8,18 @@ module.exports = {
     store: 'citilink',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
