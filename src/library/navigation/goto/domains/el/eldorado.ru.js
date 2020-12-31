2c812b5bcb98a458740cfce3efb4a678bb9e7c5a
manoj.d <manoj.d@osmosys.asia>
2020-12-31 18:46:20 +0530

    update xpath for attributes

--- src/library/navigation/goto/domains/el/eldorado.ru.js
+++ src/library/navigation/goto/domains/el/eldorado.ru.js
@@ -3,9 +3,16 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'eldorado.ru',
-    timeout: null,
+    timeout: 50000,
     country: 'RU',
     store: 'eldorado',
     zipcode: '',
+  },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"first_request_timeout":50000,"force200":true}[/!opt!]`;
+    context.setBlockAds(false);
+    context.setLoadAllResources(true);
+    context.setAntiFingerprint(false);
+    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true });
   },
 };
--- src/library/product/details/stores/e/eldorado/format.js
+++ src/library/product/details/stores/e/eldorado/format.js
@@ -4,112 +4,116 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    const cleanUp = (data, context) => {
-        let dataStr = JSON.stringify(data);
-        console.log('INSIDE OF CLEANUP');
-        dataStr = dataStr
-            .replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
-            .replace(/&amp;nbsp;/g, ' ')
-            .replace(/&amp;#160/g, ' ')
-            .replace(/\\u00A0/g, ' ')
-            .replace(/\s{2,}/g, ' ')
-            .replace(/"\s{1,}/g, '"')
-            .replace(/\s{1,}"/g, '"')
-            .replace(/^ +| +$|( )+/g, ' ')
-            // eslint-disable-next-line no-control-regex
-            .replace(/[^\x00-\x7F]/g, '');
+  const cleanUp = (data, context) => {
+    let dataStr = JSON.stringify(data);
+    console.log('INSIDE OF CLEANUP');
+    dataStr = dataStr
+      .replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+      .replace(/[^\x00-\x7F]/g, '');
 
-        return JSON.parse(dataStr);
-    };
-    for (const { group } of data) {
-        for (let row of group) {
-            // if (row.image) {
-            //   row.image.forEach((str) => {
-            //     str.text = str.text.replace(/380x240/g, "380x500");
-            //   });
-            // }
-            if (row.alternateImages) {
-                row.alternateImages.shift();
-                // row.alternateImages.forEach((str) => {
-                //   str.text = str.text.replace(/380x240/g, "380x500");
-                // });
-            }
-            if (row.manufacturerImages) {
-                row.manufacturerImages.shift();
-                row.manufacturerImages.forEach((str) => {
-                    str.text = 'https://static.eldorado.ru/' + str.text;
-                });
-            }
-            if (row.specifications) {
-                const formattedSpecifications = row.specifications
-                    .map((item, index) =>
-                        index == row.specifications.length - 1 ? item.text : index % 2 === 0 ? item.text + ' : ' : item.text + ' || ',
-                    )
-                    .join('');
-                row.specifications = [];
-                row.specifications.push({ text: '' });
-                row.specifications[0].text = formattedSpecifications;
-            }
-            if (row.description) {
-                const description = row.description.map((str) => {
-                    return str.text.replace(/\n/g, ' ');
-                }).join('');
-                row.description = [];
-                row.description.push({ text: '' });
-                row.description[0].text = description;
-            }
-            if (row.manufacturerDescription) {
-                const manufacturerDescription = row.manufacturerDescription
-                    .map((str, index) => {
-                        str.text = str.text.replace(/\n/g, ' ');
-                        return index == row.manufacturerDescription.length - 1 ? str.text : str.text + ' | ';
-                    })
-                    .join('');
-                row.manufacturerDescription = [{ text: '' }];
-                row.manufacturerDescription[0].text = manufacturerDescription;
-            }
-            if (row.shippingInfo) {
-                const shippingInfo = row.shippingInfo.map((str) => {
-                    return str.text.replace(/\n/g, '');
-                });
+    return JSON.parse(dataStr);
+  };
+  for (const { group } of data) {
+    for (let row of group) {
+      // if (row.image) {
+      //   row.image.forEach((str) => {
+      //     str.text = str.text.replace(/380x240/g, "380x500");
+      //   });
+      // }
+      if (row.alternateImages) {
+        row.alternateImages.shift();
+        // row.alternateImages.forEach((str) => {
+        //   str.text = str.text.replace(/380x240/g, "380x500");
+        // });
+      }
+      if (row.manufacturerImages) {
+        // row.manufacturerImages.shift();
+        row.manufacturerImages.forEach((str) => {
+          if (str.text.includes('upload')) {
+            str.text = 'https://static.eldorado.ru/' + str.text;
+          } else if (str.text.includes('syndication')) {
+            str.text = 'https:' + str.text;
+          }
+        });
+      }
+      if (row.specifications) {
+        const formattedSpecifications = row.specifications
+          .map((item, index) =>
+            index == row.specifications.length - 1 ? item.text : index % 2 === 0 ? item.text + ' : ' : item.text + ' || ',
+          )
+          .join('');
+        row.specifications = [];
+        row.specifications.push({ text: '' });
+        row.specifications[0].text = formattedSpecifications;
+      }
+      if (row.description) {
+        const description = row.description.map((str) => {
+          return str.text.replace(/\n/g, ' ');
+        }).join('');
+        row.description = [];
+        row.description.push({ text: '' });
+        row.description[0].text = description;
+      }
+      if (row.manufacturerDescription) {
+        const manufacturerDescription = row.manufacturerDescription
+          .map((str, index) => {
+            str.text = str.text.replace(/\n/g, ' ');
+            return index == row.manufacturerDescription.length - 1 ? str.text : str.text + ' | ';
+          })
+          .join('');
+        row.manufacturerDescription = [{ text: '' }];
+        row.manufacturerDescription[0].text = manufacturerDescription;
+      }
+      if (row.shippingInfo) {
+        const shippingInfo = row.shippingInfo.map((str) => {
+          return str.text.replace(/\n/g, '');
+        });
 
-                row.shippingInfo[0].text = shippingInfo;
-            }
-            if (row.availabilityText) {
-                row.availabilityText.forEach((str) => {
-                    str.text = str.text == 'true' ? 'In Stock' : 'Out of Stock';
-                });
-            }
-            if (row.Image360Present) {
-                const Image360Present = row.Image360Present.map((str) => {
-                    return str.text == 'true' ? 'Yes' : 'No';
-                }).join('');
+        row.shippingInfo[0].text = shippingInfo;
+      }
+      if (row.availabilityText) {
+        row.availabilityText.forEach((str) => {
+          str.text = str.text == 'true' ? 'In Stock' : 'Out of Stock';
+        });
+      }
+      if (row.Image360Present) {
+        const Image360Present = row.Image360Present.map((str) => {
+          return str.text == 'true' ? 'Yes' : 'No';
+        }).join('');
 
-                row.Image360Present[0].text = Image360Present;
-            }
-            if (row.technicalInformationPdfPresent) {
-                const technicalInformationPdfPresent = row.technicalInformationPdfPresent
-                    .map((str) => {
-                        return str.text == 'true' ? 'Yes' : 'No';
-                    })
-                    .join('');
+        row.Image360Present[0].text = Image360Present;
+      }
+      if (row.technicalInformationPdfPresent) {
+        const technicalInformationPdfPresent = row.technicalInformationPdfPresent
+          .map((str) => {
+            return str.text == 'true' ? 'Yes' : 'No';
+          })
+          .join('');
 
-                row.technicalInformationPdfPresent[0].text = technicalInformationPdfPresent;
-            }
-            if (row.category) {
-                row.category.shift();
-                // const category = row.category
-                //   .map((category) => {
-                //     return category.text.split(">").splice(1).join(",");
-                //   })
-                //   .join("");
+        row.technicalInformationPdfPresent[0].text = technicalInformationPdfPresent;
+      }
+      if (row.category) {
+        row.category.shift();
+        // const category = row.category
+        //   .map((category) => {
+        //     return category.text.split(">").splice(1).join(",");
+        //   })
+        //   .join("");
 
-                // row.category[0].text = category;
-            }
-            row = cleanUp(row);
-        }
+        // row.category[0].text = category;
+      }
+      row = cleanUp(row);
     }
-    return data;
+  }
+  return data;
 };
 
 module.exports = { transform };
