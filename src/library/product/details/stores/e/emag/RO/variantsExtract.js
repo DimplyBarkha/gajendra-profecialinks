73f492ed8f5a1bcff831041ae6059f1e617c0117
sasz94 <sacha.bartlomiej@gmail.com>
2020-12-31 15:55:44 +0100

    Fixed sku and variantId extraction accordingly to UAT TOOL (#2969)

--- src/library/product/details/stores/e/emag/RO/variantsExtract.js
+++ src/library/product/details/stores/e/emag/RO/variantsExtract.js
@@ -14,17 +14,25 @@ async function implementation (
       document.body.appendChild(newDiv);
       return newDiv;
     }
-
-    const variantElements = document.querySelectorAll('div.product-highlights-wrapper ul.page-product-options-group.list-inline a');
     const skuArr = [];
+    let uniqueVariantIds = [];
+    const variantElements = document.querySelectorAll('div.product-highlights-wrapper ul.page-product-options-group.list-inline a');
+    var scriptText = document.evaluate('//script[contains(.,"offer_id")]', document, null, XPathResult.STRING_TYPE, null).stringValue;
+    const variantIds = scriptText.match(/offer_id":(.+?),/g);
+    if (variantIds) {
+      uniqueVariantIds = variantIds.filter(function onlyUnique (value, index, self) {
+        return self.indexOf(value) === index;
+      });
+    }
+
     variantElements.forEach(element => {
       skuArr.push(element.getAttribute('href').match(/pd\/(.+)\//)[1]);
     });
-    if (skuArr.length !== 0) {
+    if (skuArr.length !== 0 && uniqueVariantIds.length !== 0) {
       for (let i = 0; i < skuArr.length; i++) {
         const pagePrefixUrl = window.location.href.replace(/pd\/.+\//g, `pd/${skuArr[i]}/`);
         addHiddenDiv('variantUrl', pagePrefixUrl);
-        addHiddenDiv('variantId', skuArr[i]);
+        addHiddenDiv('variantId', uniqueVariantIds[i]);
       }
     }
   }, createUrl);
