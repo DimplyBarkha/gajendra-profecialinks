ed4f470462bf4d4d286d84cb35a234c9574a8cd0
sujoypal144 <sujoypal144@gmail.com>
2021-01-01 14:23:48 +0530

    fixed the issue with timeout.

--- src/library/navigation/goto/domains/jo/joycemayne.com.au.js
+++ src/library/navigation/goto/domains/jo/joycemayne.com.au.js
@@ -8,14 +8,24 @@ module.exports = {
     store: 'joycemayne',
     zipcode: '',
   },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
-    // await context.setAntiFingerprint(false);
-    await context.setLoadAllResources(true);
-    await context.setBlockAds(false);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-    }
+  // implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+  //   const timeout = parameters.timeout ? parameters.timeout : 10000;
+  //   // await context.setAntiFingerprint(false);
+  //   await context.setLoadAllResources(true);
+  //   await context.setBlockAds(false);
+  //   await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
+  //   if (zipcode) {
+  //     await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+  //   }
+  // },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 80000,
+      waitUntil: 'load',
+    });
   },
 };
