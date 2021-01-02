158019fe1d3765e3d16da65615971fde238e3189
Rekha Takalkar <rekha.takalkar@profecialinks.com>
2021-01-02 16:23:47 +0530

    updated file

--- src/library/navigation/goto/domains/bo/bol.com.js
+++ src/library/navigation/goto/domains/bo/bol.com.js
@@ -8,4 +8,15 @@ module.exports = {
     store: 'bol',
     zipcode: '',
   },
+   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 20000;
+    await context.setLoadAllResources(true);
+    await context.setAntiFingerprint(false);
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, first_request_timeout: 60, load_timeout: 60 });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
