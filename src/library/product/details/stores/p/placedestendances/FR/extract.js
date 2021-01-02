84fb11a8512a70ed9d58c33857f9b3fd4e52e0d7
sivasoarlogic <siva@soarlogic.com>
2021-01-02 17:55:48 +0530

    QAfixs

--- src/library/product/details/stores/p/placedestendances/FR/extract.js
+++ src/library/product/details/stores/p/placedestendances/FR/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('../format');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'FR',
     store: 'placedestendances',
-    transform: null,
+    transform,
     domain: 'placedestendances.com/fr/fr',
     zipcode: '',
   },
