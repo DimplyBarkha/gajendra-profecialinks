ca943237bfb3b0482136a677d400baca2d1b393a
Himanshu-Agrahari <himanshu.a@partner.import.io>
2021-01-02 15:49:34 +0530

    Fix: Tag change for all the columns
    Remove Unwanted code in paginate.js
    
    Add Test extractor to test the source
    
    Fix all attribute's xpath and get some from JSON available

--- src/library/product/search/stores/h/holmesproducts/US/extract.js
+++ src/library/product/search/stores/h/holmesproducts/US/extract.js
@@ -1,5 +1,63 @@
 const { transform } = require('./format');
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+  const { transform } = parameters;
+
+  const cssAllProduct = '.product-tile';
+
+  await context.evaluate(async (cssAllProduct) => {
+    function addHiddenDiv (id, content, parent) {
+      console.log(`inputs: ${JSON.stringify({
+        id: id,
+        content: content,
+        parent: parent
+      })}`);
+
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      parent.appendChild(newDiv);
+    }
+
+    const allProducts = document.querySelectorAll(cssAllProduct);
+    if(!allProducts.length) {
+      console.log('No products are found');
+      console.log(`cssAllProduct: ${cssAllProduct}`);
+      return false;
+    }
+    
+    allProducts.forEach(product => {
+      const productJson = product && product.dataset && product.dataset.analyticsData && JSON.parse(product.dataset.analyticsData);
+
+      // product is the parent div found from querySelectorAll
+      // We are setting the attribute inside there respective parents
+      const brand = productJson.brand;
+      brand? addHiddenDiv('brand', brand, product) : console.log('Brand detail not found');
+
+      const category = productJson.category;
+      category? addHiddenDiv('category', category, product) : console.log('category detail not found');
+
+      const id = productJson.id;
+      id? addHiddenDiv('id', id, product) : console.log('id detail not found');
+
+      const name = productJson.name;
+      name? addHiddenDiv('name', name, product) : console.log('name detail not found');
+
+      const sku = productJson.product_sku;
+      sku? addHiddenDiv('sku', sku, product) : console.log('sku detail not found');
+    })
+  }, cssAllProduct);
+
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -9,4 +67,5 @@ module.exports = {
     domain: 'holmesproducts.com',
     zipcode: '',
   },
+  implementation
 };
--- src/library/product/search/stores/h/holmesproducts/US/paginate.js
+++ src/library/product/search/stores/h/holmesproducts/US/paginate.js
@@ -7,6 +7,7 @@ module.exports = {
     nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
+    stopConditionSelectorOrXpath: null,
     loadedSelector: 'ul[id*="search-result-items"]',
     noResultsXPath: '//div[contains(@class,"responsive-slot no-hits-container")] | //div[contains(@id,"primary")]/p[contains(text(),"resulted in no products")] | //span[contains(@class,"search-result-count") and contains(.,"no product search result")] | //div[contains(@class, "search-tips")]',
     openSearchDefinition: null,
