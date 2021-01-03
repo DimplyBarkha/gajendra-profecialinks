ae4122022071c0f9ee071fa8531062ff3528e946
Avinash Kadam <avinash.kadam@profecialinks.com>
2021-01-03 19:42:57 +0530

    Updated loaded selector

--- src/library/product/details/stores/m/microspot/CH/execute.js
+++ src/library/product/details/stores/m/microspot/CH/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'CH',
     store: 'microspot',
     domain: 'microspot.ch',
-    loadedSelector: 'div[class~=demoup-container] img[property="image"]',
+    loadedSelector: 'div#TOP_CONTENT_ANCHOR',
     noResultsXPath: '//h2[contains(text(),"Dieses Produkt ist leider nicht mehr verfügba")] | //*[contains(text(),"Aber so schnell geben Sie doch nicht auf, ode")]',
     zipcode: '',
   },
