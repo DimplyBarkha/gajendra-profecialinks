4543199fea98e8362cf200267ab17d30e136a957
rajeshProfecialinks <rajesh.g@profecialinks.com>
2021-01-03 12:57:57 +0530

    wbmason core feedtype add url id in createUrl file

--- src/library/product/details/stores/w/wbmason/US/createUrl.js
+++ src/library/product/details/stores/w/wbmason/US/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'wbmason.com',
     prefix: null,
-    url: null,
+    url: 'https://www.wbmason.com/ProductDetail.aspx?ItemID={id}',
     country: 'US',
     store: 'wbmason',
     zipcode: '',
