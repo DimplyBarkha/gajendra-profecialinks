2865ebb85b03350a8fbd3e5cf3471acee6c3bb87
Venkata Akhilesh Pasupuleti <41539204+VenkataAkhileshP@users.noreply.github.com>
2021-01-04 21:16:06 +0530

    Fixed issue with searchURL

--- src/library/product/search/stores/t/totalwine_95825/US/extract.js
+++ src/library/product/search/stores/t/totalwine_95825/US/extract.js
@@ -17,20 +17,10 @@ module.exports = {
     const { productDetails } = dependencies;
     const { transform } = parameters;
     await context.evaluate(async () => {
-      const closeButton = document.querySelector('button[class="backButton__2KmT2m2I"]');
-      if (closeButton) {
-        closeButton.click();
-        console.log('able to click the close button');
-        await new Promise(resolve => { setTimeout(resolve, 10000); });
-      } else {
-        console.log('not able to click the close button');
-      }
-      const searchUrl = window.location.href;
-      const appendElements = document.querySelectorAll('article[class="productCard__2nWxIKmi"]');
-      if (appendElements.length) {
-        appendElements.forEach((element) => {
-          element.setAttribute('searchurl', searchUrl);
-        });
+      const currentPageURL = window.location.href;
+      const bodyElement = document.querySelector('body');
+      if (bodyElement) {
+        bodyElement.setAttribute('current_page_url', currentPageURL);
       }
     });
     return await context.extract(productDetails, { transform });
