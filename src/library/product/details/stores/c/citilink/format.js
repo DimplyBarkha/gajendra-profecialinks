d8ed695347cd4533a364cf10e51dfae4305c5267
shrutikab <shrutika.bongarde@synerzip.com>
2021-01-04 14:43:51 +0530

    new change(#2323)

--- src/library/product/details/stores/c/citilink/format.js
+++ src/library/product/details/stores/c/citilink/format.js
@@ -10,8 +10,8 @@ const transform = (data) => {
     .replace(/&amp;#160/g, ' ')
     .replace(/\u00A0/g, ' ')
     .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
+    // .replace(/"\s{1,}/g, '"')
+    // .replace(/\s{1,}"/g, '"')
     .replace(/^ +| +$|( )+/g, ' ')
     // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
@@ -55,7 +55,7 @@ const transform = (data) => {
   }
 
   data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-    // el.text = clean(el.text);
+     el.text = clean(el.text);
   }))));
 
   return data;
