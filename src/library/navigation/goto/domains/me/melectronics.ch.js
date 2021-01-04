2604649bf62035e9c620e5af0b9e8ca3a29645e3
vikas kyatannawar <vikas.k@osmosys.asia>
2021-01-04 16:01:42 +0530

    fixed goto and loading issues, updated image, witb

--- src/library/navigation/goto/domains/me/melectronics.ch.js
+++ src/library/navigation/goto/domains/me/melectronics.ch.js
@@ -12,25 +12,25 @@ module.exports = {
     // let url = `${inputs.url}`;
     // await context.setBlockAds(false);
     // url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
-    await context.goto(url);
-    // async function autoScroll (page) {
-    //   await page.evaluate(async () => {
-    //     await new Promise((resolve, reject) => {
-    //       var totalHeight = 0;
-    //       var distance = 100;
-    //       var timer = setInterval(() => {
-    //         var scrollHeight = document.body.scrollHeight;
-    //         window.scrollBy(0, distance);
-    //         totalHeight += distance;
+    await context.goto(url, { timeout: 90000, waitUntil: 'networkidle0', block_ads: false });
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
 
-    //         if (totalHeight >= scrollHeight) {
-    //           clearInterval(timer);
-    //           resolve();
-    //         }
-    //       }, 100);
-    //     });
-    //   });
-    // }
-    // await autoScroll(context);
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
+    }
+    await autoScroll(context);
   },
 };
