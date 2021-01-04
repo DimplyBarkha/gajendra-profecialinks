76655b008e51502c61285cb78280255b8c7f5895
ravitejaw324 <ravi.w@partner.import.io>
2021-01-04 09:45:06 +0530

    modified loaded selector

--- src/library/product/details/stores/d/domayne/AU/execute.js
+++ src/library/product/details/stores/d/domayne/AU/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'domayne',
     domain: 'domayne.com.au',
     // loadedSelector: null,
-    loadedSelector: 'div#category-grid',
+    loadedSelector: 'div#category-grid, div.product-view-sales',
     noResultsXPath: '//h1[contains(.,"0 items found")]',
     zipcode: '',
   },
