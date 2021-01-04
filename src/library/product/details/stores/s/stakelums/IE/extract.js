7313905a8e806ce8978d48963fe648194c6df7a0
shivaraj <>
2021-01-04 21:23:21 +0530

    iframe code added for CTR and inTheBox

--- src/library/product/details/stores/s/stakelums/IE/extract.js
+++ src/library/product/details/stores/s/stakelums/IE/extract.js
@@ -28,14 +28,14 @@ module.exports = {
         return result && result.trim ? result.trim() : result;
       };
 
-      function addHiddenDiv (id, content) {
+      function addHiddenDiv(id, content) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
         newDiv.textContent = content;
         newDiv.style.display = 'none';
         document.body.appendChild(newDiv);
       }
-      function stall (ms) {
+      function stall(ms) {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
             resolve();
@@ -120,7 +120,7 @@ module.exports = {
       if (productInformation.length > 0) {
         const specificationOption2 = productInformation.join('||');
         addHiddenDiv('added_productInformation', productInformation.join('|'));
-     
+
       }
 
       const inTheBoxUrlArr = [];
@@ -129,9 +129,9 @@ module.exports = {
         const links = item.split(' ')[0];
         inTheBoxUrlArr.push(links);
       });
-       addHiddenDiv('added_inTheBoxUrl', inTheBoxUrlArr.join(' || '));
-    
-      function findXpathArr (xpath) {
+      addHiddenDiv('added_inTheBoxUrl', inTheBoxUrlArr.join(' || '));
+
+      function findXpathArr(xpath) {
         const element = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
         let node = element.iterateNext();
         const value = [];
@@ -142,7 +142,135 @@ module.exports = {
         return value;
       }
 
+      let compareTable = document.querySelector('.inpage_selector_comparison > div');
+      if (compareTable) {
+        addHiddenDiv(`isCompareTable-`, 'Yes');
+      }
+
     });
+
+    const link = await context.evaluate(function () {
+      return window.location.href;
+    });
+
+    const src = await context.evaluate(async function () {
+      const iframe = document.querySelector('#eky-dyson-iframe');
+      // const src = iframe ? (iframe.src||iframe._src) : '';
+      let src = '';
+      if (iframe) {
+        if (iframe.hasAttribute('src')) {
+          src = iframe.getAttribute('src');
+        } else if (iframe.hasAttribute('_src')) {
+          src = iframe.getAttribute('_src');
+        } else {
+          console.log('we do not have any src in iframe');
+        }
+      }
+
+      return src;
+    });
+    // let content = null;
+    if (src) {
+
+      try {
+        await context.goto(src, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+
+        const witbData = await context.evaluate(async () => {
+          const getInTheBox = document.querySelector('div.eky-accesory-container img');
+          // const getInTheBoxWB = document.querySelector('div.inpage_selector_InTheBox');
+          const getInTheBoxVideo = document.querySelector('div.eky-container-full');
+          const inBoxUrls = [];
+          const inBoxText = [];
+          if (getInTheBoxVideo) {
+            const getAllProductsVideo = document.querySelectorAll('div.tns-inner > div.my-slider>div.eky-relative-wrapper.tns-normal');
+            for (let i = 0; i < getAllProductsVideo.length; i++) {
+              inBoxUrls.push(getAllProductsVideo[i].querySelector('div.eky-header-video-container>video').getAttribute('src'));
+              inBoxText.push(getAllProductsVideo[i].querySelector('div.eky-overlay>div.lax>h1').innerText);
+            }
+          }
+          if (getInTheBox) {
+            const getAllProducts = document.querySelectorAll('div.eky-accesory-container > div.eky-accessory');
+            for (let i = 0; i < getAllProducts.length; i++) {
+              inBoxUrls.push(getAllProducts[i].querySelector('img').getAttribute('src'));
+              inBoxText.push(getAllProducts[i].querySelector('div').innerText);
+            }
+          }
+
+
+          // if(getInTheBoxWB){
+          //   const getAllProductsWB = document.querySelectorAll('div.inpage_selector_InTheBox>div.flix-std-container-fluid>div.flix-std-table>div.flix-box-modules>div.flix-std-table>div.flix-std-table-cell');
+          //   for (let i = 0; i < getAllProductsWB.length; i++) {
+          //     inBoxUrls.push(getAllProductsWB[i].querySelector('div.flix-background-image>img').getAttribute('data-flixsrcset'));
+          //     inBoxText.push(getAllProductsWB[i].querySelector('div.flix-std-content>div.flix-std-desc>span').innerText);
+          //   }
+          // }
+          return { inBoxText, inBoxUrls };
+        });
+
+
+        await context.goto(link, { timeout: 5000, waitUntil: 'load', checkBlocked: true });
+        await context.waitForSelector('#inpage_container');
+
+        await context.evaluate(async (witbData) => {
+
+
+          function addHiddenDiv(id, content) {
+            const newDiv = document.createElement('div');
+            newDiv.id = id;
+            newDiv.textContent = content;
+            newDiv.style.display = 'none';
+            document.body.appendChild(newDiv);
+          }
+
+
+          const { inBoxText = [], inBoxUrls = [] } = witbData;
+          addHiddenDiv(`inTheBoxText-`, inBoxText.join(' || '));
+          addHiddenDiv(`inTheBoxUrl-`, inBoxUrls.join(' || '));
+          // for (let i = 0; i < inBoxUrls.length; i++) {
+          //   addHiddenDiv(`inTheBoxUrl-${i}`, inBoxUrls[i]);
+          //   // if (inBoxText[i]) {
+          //   //   addHiddenDiv(`inTheBoxText-${i}`, inBoxText[i] );
+          //   // }
+          // }      
+        }, witbData);
+
+        await context.evaluate(async () => {
+
+
+          function addHiddenDiv(id, content) {
+            const newDiv = document.createElement('div');
+            newDiv.id = id;
+            newDiv.textContent = content;
+            newDiv.style.display = 'none';
+            document.body.appendChild(newDiv);
+          }
+
+
+          let compareTable = document.querySelector('.inpage_selector_comparison > div');
+          if (compareTable) {
+            addHiddenDiv(`isCompareTable-`, 'Yes');
+          }
+        });
+
+
+        // await context.waitForSelector('div#main-section', { timeout: 45000 });
+      } catch (error) {
+        try {
+          await context.evaluate(async function (src) {
+            window.location.assign(src);
+          }, src);
+          await context.waitForSelector('div.eky-container-full');
+
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      // return await context.extract(productDetails, { transform });
+    } else {
+      console.log('we do not have the src for iframe');
+
+    }
+
     await context.extract(productDetails, { transform: transformParam });
   },
 };
