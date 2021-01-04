b8177f9d3f70e2230dd3be283e74357cf22390d8
BorysMaciuch <borysmaciuch@gmail.com>
2021-01-04 11:42:02 +0100

    Add retailer_id, change inputs from storeId to Postcode

--- src/library/navigation/goto/domains/al/setZipCode.alcampo.es.js
+++ src/library/navigation/goto/domains/al/setZipCode.alcampo.es.js
@@ -8,18 +8,18 @@ module.exports = {
     zipcode: "''",
   },
   implementation: async (inputs, parameterValues, context, dependencies) => {
-    const storeId = inputs.zipcode;
-    await new Promise((r) => setTimeout(r, 3000));
+    const storeId = inputs.Postcode;
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
     const popup = await context.evaluate(() =>
       document.querySelector('.cc_b_ok.auxOKButton.cookie-button'),
     );
     if (popup) {
       await context.click('.cc_b_ok.auxOKButton.cookie-button');
     }
-    await new Promise((r) => setTimeout(r, 3000));
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
     await context.waitForSelector('#header-container .titleNameBlock .nameMyStore');
     await context.click('#header-container .titleNameBlock .nameMyStore');
-    await new Promise((r) => setTimeout(r, 3000));
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
     await context.waitForSelector('#header-container .botonera.botonera-noLogged:nth-of-type(2) .secondary-button.black-secondary');
     await context.click('#header-container .botonera.botonera-noLogged:nth-of-type(2) .secondary-button.black-secondary');
     await context.waitForSelector(
