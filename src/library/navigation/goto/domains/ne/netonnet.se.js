58f02975f8da12e69e8e3d84fa29ee96b1652b7b
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2021-01-04 22:41:47 +0530

    updated netonnet.se.js

--- src/library/navigation/goto/domains/ne/netonnet.se.js
+++ src/library/navigation/goto/domains/ne/netonnet.se.js
@@ -8,9 +8,9 @@ module.exports = {
     store: 'netonnet',
     zipcode: '',
   },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+  // implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
 
-    url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
-    await context.goto(url);
-  },
+  //   url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+  //   await context.goto(url);
+  // },
 };
