af722da460b7d05de6537e194c42e5d181057ecd
sahoojudhi <judhishtor@profecialinks.com>
2021-01-04 10:22:17 +0530

    Fixed the WITB issue

--- src/library/product/details/stores/p/payngo/IL/extract.js
+++ src/library/product/details/stores/p/payngo/IL/extract.js
@@ -9,4 +9,59 @@ module.exports = {
     domain: 'payngo.co.il',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+
+      //const getInTheBox = document.querySelector('#MainContent_Properties_productDetails').childNodes;
+      let getInTheBox=document.evaluate('//div/p[*[contains(text(),"?? ????? ")]] | //div/p[*[contains(text(),"??????? ??????")]] | //div[*[contains(text(),"?? ?????")]] | //div[*[contains(text(),"????? ?????:")]] ',document,null,XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue;
+      console.log("getInTheBox:",!!getInTheBox);
+        if (getInTheBox) {
+            let getInTheBoxNodes=getInTheBox.childNodes;
+	          const getAllProducts = Array.from(getInTheBoxNodes).filter(elem=>elem.nodeType===3 || elem.nodeType===1)
+            let takeTextFlag=false;
+            let brCountFlag=0;
+            let startTextFlag=false;
+            console.log("In for loop length:",getAllProducts.length);
+            for (let i = 0; i < getAllProducts.length; i++) {
+                console.log("brCountFlag:",brCountFlag);
+                
+                if(getAllProducts[i].textContent.trim().includes("?? ?????") || getAllProducts[i].textContent.trim().includes("??????? ??????") || getAllProducts[i].textContent.trim().includes("?? ?????") || getAllProducts[i].textContent.trim().includes("????? ?????:")){
+                  console.log("Found matche.....................");
+                  brCountFlag=0;
+                  takeTextFlag=true;
+                }else if(takeTextFlag){
+                  if(brCountFlag===2){
+                    takeTextFlag=false;
+                  }
+                  else if(getAllProducts[i].textContent!=""){
+                    brCountFlag=0;
+                    startTextFlag=true;
+
+                    addElementToDocument(`inTheBoxText-${i}`, getAllProducts[i].textContent);
+                    if(getAllProducts[i].textContent.trim().includes("????.")){
+                      takeTextFlag=false;
+                    }
+                    console.log("textContent:::",getAllProducts[i].textContent);
+                  }else{
+                    if(startTextFlag){
+                      brCountFlag++;
+                    }
+                    
+                  }
+                }
+                
+            }
+        }
+
+    });
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    await context.extract(productDetails, { transform });
+  },
 };
