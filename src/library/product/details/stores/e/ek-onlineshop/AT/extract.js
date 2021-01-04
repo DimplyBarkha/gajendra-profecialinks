bceea1edcff022f5554f23f53f5a3b93d60fa250
ravitejaw324 <ravi.w@partner.import.io>
2021-01-04 10:54:31 +0530

    added a fix for enhancedContent

--- src/library/product/details/stores/e/ek-onlineshop/AT/extract.js
+++ src/library/product/details/stores/e/ek-onlineshop/AT/extract.js
@@ -34,8 +34,72 @@ async function implementation(
     });
   };
 
-
+  await applyScroll(context);
   await context.setBlockAds(false);
+  try {
+    await context.waitForSelector('span[id*=inpage-data]', {timeout: 20000});
+  } catch(er) {
+    console.log("coundn't find flix id");
+  }
+
+  const flixid = await context.evaluate(async function() {
+      const flixIdNode = document.querySelector('span[id*=inpage-data]');
+      if(flixIdNode) {
+        return flixIdNode.getAttribute('id').match(/\d+/g)[0];
+      } else {
+        return null;
+      }
+  });
+
+  if(flixid) {
+    await context.goto(`https://media.flixcar.com/delivery/inpage/show/14387/at/${flixid}/`, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 20000,
+      waitUntil: 'load',
+    });
+
+    const enhancedContent = await context.evaluate(async function() {
+      let content = {};
+      content.description =  document.body.innerText;
+      content.images = [];
+      content.videos = [];
+
+      const imagesNodes = document.querySelectorAll('div[class*=feature-image] img');
+      imagesNodes.forEach(q => {
+        if(q.hasAttribute('data-flixsrcset')) {
+          let matches = q.getAttribute('data-flixsrcset').match(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g)[0]; //url regex
+          content.images.push(matches)
+        }
+      })
+
+      const videoNodes = document.querySelectorAll('iframe[title*=video]');
+      videoNodes.forEach(q => {
+        if(q.hasAttribute('src')) {
+          content.videos.push(q.getAttribute('src'));
+        }
+      });
+      return content;
+    });
+
+    await context.goto(pageUrl, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
+    await context.evaluate(async function(enhancedContent) {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+
+      addHiddenDiv('manufacturerDescription', enhancedContent.description);
+      addHiddenDiv('manufacturerImages', enhancedContent.images.join(" | "));
+      addHiddenDiv('videos', enhancedContent.videos.join(" | "));
+
+    }, enhancedContent)
+  }
+  
   //await context.setLoadAllResources(true); 
   //await context.setLoadImages(true);
   //await context.setJavaScriptEnabled(true); 
@@ -52,10 +116,7 @@ async function implementation(
   // elementID = 'manuf';
   // content = text;
 
-  // going back to product page
-  await context.goto(pageUrl, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
-  await applyScroll(context);
-  await context.waitForSelector('#flix-inpage', {timeout: 30000});
+
 
   return context.extract(productDetails, { transform });
 }
