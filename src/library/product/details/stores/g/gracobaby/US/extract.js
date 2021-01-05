bbb265bbb0deb57bc636150b6ce9a1246279a631
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-01-05 14:55:44 +0530

    Minor Issues

--- src/library/product/details/stores/g/gracobaby/US/extract.js
+++ src/library/product/details/stores/g/gracobaby/US/extract.js
@@ -1,17 +1,17 @@
-
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'gracobaby',
-    transform: null,
+    cleanUp,
     domain: 'gracobaby.com',
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
       // Java Script Code for adding new Div
-      function addHiddenDiv (id, content) {
+      function addHiddenDiv(id, content) {
         const newDiv = document.createElement('div');
         newDiv.className = id;
         newDiv.textContent = content;
@@ -49,47 +49,75 @@ module.exports = {
         var doubleSeparatorText = data.join(' - ');
         addHiddenDiv(id, doubleSeparatorText);
       };
+      try {
+        const addDescBulletInfo = getAllXpath("//ul[@id='collapsible-pdp-details-2']/li/text()|//ul[@id='collapsible-pdp-details-4']/li/text()", 'nodeValue');
+        pipeSeparatorDouble('addDescBulletInfo', addDescBulletInfo);
+      } catch (error) {
 
-      const addDescBulletInfo = getAllXpath("//ul[@id='collapsible-pdp-details-2']/li/text()|//ul[@id='collapsible-pdp-details-4']/li/text()", 'nodeValue');
-      pipeSeparatorDouble('addDescBulletInfo', addDescBulletInfo);
+      }
+      try {
+        const addvariantInfo = getAllXpath("//div[contains(@class,'color-attribute')]/span[contains(@class,'js-nonretail')]/@data-attr-value", 'nodeValue');
+        commaSeparatorDouble('addvariantInfo', addvariantInfo);
+      } catch (error) {
 
-      const addvariantInfo = getAllXpath("//div[contains(@class,'color-attribute')]/span[contains(@class,'js-nonretail')]/@data-attr-value", 'nodeValue');
-      commaSeparatorDouble('addvariantInfo', addvariantInfo);
+      }
+      try {
+        const adddescriptionInfo = getAllXpath("//div[@id='collapsible-pdp-details-1']/text() | //ul[@id='collapsible-pdp-details-2']/li/text()", 'nodeValue');
+        commaSeparatorDouble('adddescriptionInfo', adddescriptionInfo);
+      } catch (error) {
 
-      const adddescriptionInfo = getAllXpath("//div[@id='collapsible-pdp-details-1']/text() | //ul[@id='collapsible-pdp-details-2']/li/text()", 'nodeValue');
-      commaSeparatorDouble('adddescriptionInfo', adddescriptionInfo);
+      }
+      try {
+        const addwarningInfo = getAllXpath("//ul[@id='collapsible-pdp-details-4']/li/text()", 'nodeValue');
+        commaSeparatorDouble('addwarningInfo', addwarningInfo);
+      } catch (error) {
 
-      const addwarningInfo = getAllXpath("//ul[@id='collapsible-pdp-details-4']/li/text()", 'nodeValue');
-      commaSeparatorDouble('addwarningInfo', addwarningInfo);
+      }
+      try {
+        const addManufacturerDescInfo = getAllXpath("//ul[@id='collapsible-pdp-details-2']/li/text()", 'nodeValue');
+        commaSeparatorDouble('addManufacturerDescInfo', addManufacturerDescInfo);
+      } catch (error) {
 
-      const addManufacturerDescInfo = getAllXpath("//ul[@id='collapsible-pdp-details-2']/li/text()", 'nodeValue');
-      commaSeparatorDouble('addManufacturerDescInfo', addManufacturerDescInfo);
+      }
+      try {
+        const addProductDescInfo = getAllXpath("//div[@class='d-md-none col-sm-12']/h5/text()|//span[@class='color-display-value order-3 ml-1']/text()", 'nodeValue');
+        hiphenSeparatorDouble('addProductDescInfo', addProductDescInfo);
+      } catch (error) {
 
-      const addSubCategoryInfo = getAllXpath("//ol[@class='breadcrumb']/li[3]/a/text() | //ol[@class='breadcrumb']/li[4]/a/text()", 'nodeValue');
-      angelSeparatorDouble('addSubCategoryInfo', addSubCategoryInfo);
+      }
+      try {
+        const addDirectionInfo = getAllXpath("//div[@id='collapsible-pdp-details-6']/span/text()", 'nodeValue');
+        pipeSeparatorDouble('addDirectionInfo', addDirectionInfo);
+      } catch (error) {
 
-      const addProductDescInfo = getAllXpath("//div[@class='d-md-none col-sm-12']/h5/text()|//span[@class='color-display-value order-3 ml-1']/text()", 'nodeValue');
-      hiphenSeparatorDouble('addProductDescInfo', addProductDescInfo);
+      }
+      try {
+        const addvideo1Info = getAllXpath("//div[contains(@class,'pdp-thumb-img d-flex')]/@data-video-server-url", 'nodeValue');
+        const addvideo2Info = getAllXpath("//div[contains(@class,'pdp-thumb-img d-flex')]/@data-asset", 'nodeValue');
+        addHiddenDiv('addvideoInfo', addvideo1Info[0] + addvideo2Info[0]);
+      } catch (error) {
 
-      const addDirectionInfo = getAllXpath("//div[@id='collapsible-pdp-details-6']/span/text()", 'nodeValue');
-      pipeSeparatorDouble('addDirectionInfo', addDirectionInfo);
+      }
 
-      const addvideo1Info = getAllXpath("//div[contains(@class,'pdp-thumb-img d-flex')]/@data-video-server-url", 'nodeValue');
-      const addvideo2Info = getAllXpath("//div[contains(@class,'pdp-thumb-img d-flex')]/@data-asset", 'nodeValue');
-      addHiddenDiv('addvideoInfo', addvideo1Info[0] + addvideo2Info[0]);
-
-      // Java Script Code to retrieve Xpath for availability
-      var xpathavailability = function (xpathToExecute) {
-        var data = '';
-        var nodesSnapshot = document.evaluate(xpathToExecute, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        data = nodesSnapshot.snapshotItem(0).textContent;
-        data = data.substring(data.indexOf('dimension51":') + 14);
-        data = data.substring(0, data.indexOf(',') - 1);
-        addHiddenDiv('graco_availability', data);
-      };
       const addavailabilityInfo = getAllXpath("//ul[@id='collapsible-pdp-details-3']/li[contains(text(),'Product')]/text()", 'nodeValue');
       pipeSeparatorDouble('graco_dimensions', addavailabilityInfo);
-      xpathavailability('//span[@class="product-object"]/@data-product');
+
+
+      try {
+        const sku = getAllXpath('//script[@type="application/ld+json" and contains(text(),"sku")]/text()', 'nodeValue');
+        const finalSKU = JSON.parse(sku[0]);
+        addHiddenDiv('sku', finalSKU.sku);
+      } catch (error) {
+
+      }
+      try {
+        const availability = getAllXpath('//script[@type="application/ld+json" and contains(text(),"InStock")]/text()', 'nodeValue');
+        if (availability.length > 0) {
+          addHiddenDiv('graco_availability', 'graco_availability');
+        }
+      } catch (error) {
+
+      }
     });
     return await context.extract(productDetails);
   },
--- src/library/product/search/stores/g/gracobaby/US/extract.js
+++ src/library/product/search/stores/g/gracobaby/US/extract.js
@@ -1,10 +1,10 @@
-const { cleanUp } = require('../../../../shared');
+const { transform } = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'gracobaby',
-    transform: cleanUp,
+    transform,
     domain: 'gracobaby.com',
     zipcode: '',
   },
