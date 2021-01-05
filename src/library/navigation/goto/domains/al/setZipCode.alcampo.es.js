6d8cf7602340bb2f45fc491ddc425f9a167037a8
BorysMaciuch <borysmaciuch@gmail.com>
2021-01-05 15:22:15 +0100

    Change setInputValue to setAttribute in setZipCode.js, deploy and test extractor

--- src/library/navigation/goto/domains/al/setZipCode.alcampo.es.js
+++ src/library/navigation/goto/domains/al/setZipCode.alcampo.es.js
@@ -10,25 +10,27 @@ module.exports = {
   implementation: async (inputs, parameterValues, context, dependencies) => {
     const storeId = inputs.Postcode;
     await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    
+    await context.waitForSelector('#header-container .titleNameBlock .nameMyStore');
     const popup = await context.evaluate(() =>
       document.querySelector('.cc_b_ok.auxOKButton.cookie-button'),
     );
     if (popup) {
       await context.click('.cc_b_ok.auxOKButton.cookie-button');
     }
-    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-    await context.waitForSelector('#header-container .titleNameBlock .nameMyStore');
+    await context.click('#subNavContainer')
     await context.click('#header-container .titleNameBlock .nameMyStore');
-    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
     await context.waitForSelector('#header-container .botonera.botonera-noLogged:nth-of-type(2) .secondary-button.black-secondary');
     await context.click('#header-container .botonera.botonera-noLogged:nth-of-type(2) .secondary-button.black-secondary');
     await context.waitForSelector(
       '#postalCode',
     );
-    await context.setInputValue(
-      '#postalCode',
-      storeId,
-    );
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    await context.evaluate((storeId) => {
+      const input = document.querySelector('#postalCode')
+      input.setAttribute('value', storeId)
+
+    }, storeId)
     await context.waitForSelector('#changePostalCodeButton');
     await context.click('#changePostalCodeButton');
   },
