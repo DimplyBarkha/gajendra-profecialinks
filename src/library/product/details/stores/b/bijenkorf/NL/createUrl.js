db3006a1511273e448f6a217d0231ff8277b28cb
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2021-01-05 14:21:27 +0530

    reverted create url logic

--- src/library/product/details/stores/b/bijenkorf/NL/createUrl.js
+++ src/library/product/details/stores/b/bijenkorf/NL/createUrl.js
@@ -4,8 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'debijenkorf.nl',
     prefix: null,
-    // url: 'https://www.debijenkorf.nl/product-lister-page.html?SearchTerm={id}',
-    url: 'https://ceres-catalog.debijenkorf.nl/catalog/product/show?productVariantCode={id}&cached=false&locale=nl_NL&api-version=2.34',
+    url: 'https://www.debijenkorf.nl/product-lister-page.html?SearchTerm={id}',
     country: 'NL',
     store: 'bijenkorf',
     zipcode: '',
