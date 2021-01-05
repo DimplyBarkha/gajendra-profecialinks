800165b1054ade2b6508010236af1ef382abca28
LAPTOP-MD78TJNH\TDCM <katarzyna-godlewska1@wp.pl>
2021-01-05 14:28:34 +0100

    add geo xpaths and implementation to setZipCode

--- src/library/navigation/goto/domains/ma/setZipCode.martinplace.romeosonline.com.au.js
+++ src/library/navigation/goto/domains/ma/setZipCode.martinplace.romeosonline.com.au.js
@@ -7,4 +7,20 @@ module.exports = {
     store: 'romeos',
     zipcode: '',
   },
+
+  implementation: async (inputs, parameterValues, context, dependencies) => {
+    const storeId = inputs.Postcode;
+    console.log('storeid', storeId);
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    await context.click('div.store-chooser > a');
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    if (storeId === 'North Adelaide') {
+    // @ts-ignore
+      await context.click(('.StoreChooser__List').children[0]);
+    } else if (storeId === 'Martin Place') {
+      await context.click(('.StoreChooser__List').children[1]);
+    } else if (storeId === 'North Sydney') {
+      await context.click(('.StoreChooser__List').children[2]);
+    }
+  },
 };
