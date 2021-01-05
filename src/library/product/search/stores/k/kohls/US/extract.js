8f7dd4e8980f5a77d2d4d95341d807db496532ea
sasz94 <sacha.bartlomiej@gmail.com>
2021-01-05 11:28:22 +0100

    Fixed searchUrl & Added pending for sponsored products to show on website (#1362)

--- src/library/product/search/stores/k/kohls/US/extract.js
+++ src/library/product/search/stores/k/kohls/US/extract.js
@@ -13,11 +13,24 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(async function () {
-      const allProducts = document.querySelectorAll('li.products_grid ');
-      let x;
-      for (x = 0; allProducts.length - 1 >= x; x++) {
-        allProducts[x].setAttribute('count', `${x + 1}`);
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
       }
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      // wait for loading sponsored products
+      await stall(2000);
+      const searchUrl = window.location.href;
+      addElementToDocument('searchUrl', searchUrl);
     });
     return await context.extract(productDetails, { transform });
   },
