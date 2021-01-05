845672e9ecb4a96b5b6d2bb9cb49b8dbb54caf91
srishivams <srishivams97@gmail.com>
2021-01-05 21:44:25 +0530

    fixed error encountered for some urls

--- src/library/product/details/stores/a/allegro/PL/extract.js
+++ src/library/product/details/stores/a/allegro/PL/extract.js
@@ -15,50 +15,61 @@ module.exports = {
       const newurl = `https://allegro.pl/listing?string=${inputs.id}`;
       await context.goto(newurl, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
     }
-    const applyScroll = async function (context) {
-      await context.evaluate(async function () {
-        function addHiddenDiv (id, content) {
-          const newDiv = document.createElement('div');
-          newDiv.id = id;
-          newDiv.textContent = content;
-          newDiv.style.display = 'none';
-          document.body.appendChild(newDiv);
-        }
-        let scrollTop = 0;
-        while (scrollTop !== 20000) {
-          await stall(500);
-          scrollTop += 1000;
-          window.scroll(0, scrollTop);
-          if (scrollTop === 20000) {
-            await stall(5000);
-            break;
-          }
-        }
-        // if(document.URL.includes('archiwum.allegro'))
-        function stall (ms) {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              resolve();
-            }, ms);
-          });
-        }
-        addHiddenDiv('prodUrl', document.URL);
-      });
-    };
-    await applyScroll(context);
-    const stopExtraction = await context.evaluate(async function () {
-      if (document.querySelector('div.opbox-listing p')) {
-        // @ts-ignore
-        if (document.querySelector('div.opbox-listing p').innerText.includes('Czy na pewno szukasz')) {
-          return true;
+    const isProdPage = await context.evaluate(async function () {
+      let prodPage = true;
+      if (document.querySelector('div[data-box-name="items-v3"] div.opbox-listing')) {
+        if (document.querySelector('div[data-box-name="items-v3"] div.opbox-listing').innerText.includes('Czy na pewno szukasz')) {
+          prodPage = false;
         }
       }
-      return false;
+      return prodPage;
     });
-    if (stopExtraction !== true) {
-      return await context.extract(productDetails, { transform: transformParam });
-    } else {
-      throw new Error('Not a product page');
+    if (isProdPage === true) {
+      const applyScroll = async function (context) {
+        await context.evaluate(async function () {
+          function addHiddenDiv (id, content) {
+            const newDiv = document.createElement('div');
+            newDiv.id = id;
+            newDiv.textContent = content;
+            newDiv.style.display = 'none';
+            document.body.appendChild(newDiv);
+          }
+          let scrollTop = 0;
+          while (scrollTop !== 20000) {
+            await stall(500);
+            scrollTop += 1000;
+            window.scroll(0, scrollTop);
+            if (scrollTop === 20000) {
+              await stall(5000);
+              break;
+            }
+          }
+          // if(document.URL.includes('archiwum.allegro'))
+          function stall (ms) {
+            return new Promise((resolve, reject) => {
+              setTimeout(() => {
+                resolve();
+              }, ms);
+            });
+          }
+          addHiddenDiv('prodUrl', document.URL);
+        });
+      };
+      await applyScroll(context);
+      const stopExtraction = await context.evaluate(async function () {
+        if (document.querySelector('div.opbox-listing p')) {
+        // @ts-ignore
+          if (document.querySelector('div.opbox-listing p').innerText.includes('Czy na pewno szukasz')) {
+            return true;
+          }
+        }
+        return false;
+      });
+      if (stopExtraction !== true) {
+        return await context.extract(productDetails, { transform: transformParam });
+      } else {
+        throw new Error('Not a product page');
+      }
     }
   },
 };
