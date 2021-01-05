961dac6fdd80e82a88b3c7f4da50f44fccac54da
Karina Miernik <karina.miernik@gmail.com>
2021-01-05 09:32:54 +0100

    #1279 fixes according to t1 file

--- src/library/navigation/goto/domains/za/zalando.de.js
+++ src/library/navigation/goto/domains/za/zalando.de.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'zalando.de',
-    timeout: null,
+    timeout: 50000,
     country: 'DE',
     store: 'zalando',
     zipcode: '',
--- src/library/product/search/stores/z/zalando/DE/execute.js
+++ src/library/product/search/stores/z/zalando/DE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DE',
     store: 'zalando',
     domain: 'zalando.de',
-    url: 'https://zalando.de/katalog/?q={searchTerms}',
+    url: 'https://zalando.de/herren/?q={searchTerms}',
     loadedSelector: 'div[class*="cat_catalog"]',
     noResultsXPath: '//div[contains(@class, "noSearch")]//span[contains(text(), "Du hast gesucht nach")] | //div[contains(@class, "404")] | //div[contains(@class, "error")]',
     zipcode: '',
