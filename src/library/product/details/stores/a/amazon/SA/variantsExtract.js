3cfc9f598edb96f7320f2f2daead26c6829a5bf0
banderovskyi <roman1004b@gmail.com>
2021-01-05 11:43:56 +0200

    Init variant extractor

---
+++ src/library/product/details/stores/a/amazon/SA/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'SA',
+    store: 'amazon',
+    transform: null,
+    domain: 'amazon.sa',
+    zipcode: '',
+  },
+};
