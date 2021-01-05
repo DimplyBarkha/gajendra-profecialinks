941ef9ab393bfba07367959454e6a70ce97999b8
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2021-01-05 13:28:05 +0530

    recreating the code

--- src/library/product/details/stores/b/bijenkorf/NL/createUrl.js
+++ src/library/product/details/stores/b/bijenkorf/NL/createUrl.js
@@ -4,7 +4,8 @@ module.exports = {
   parameterValues: {
     domain: 'debijenkorf.nl',
     prefix: null,
-    url: 'https://www.debijenkorf.nl/product-lister-page.html?SearchTerm={id}',
+    // url: 'https://www.debijenkorf.nl/product-lister-page.html?SearchTerm={id}',
+    url: 'https://ceres-catalog.debijenkorf.nl/catalog/product/show?productVariantCode={id}&cached=false&locale=nl_NL&api-version=2.34',
     country: 'NL',
     store: 'bijenkorf',
     zipcode: '',
