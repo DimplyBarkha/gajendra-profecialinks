f92a4f4e80846dedf333eb0b43a84eb2e999e716
wasiqe-a <wasiqe.a@osmosys.asia>
2021-01-06 17:57:59 +0530

    Adding additional fields

--- src/library/navigation/goto/domains/bc/bcc.nl.js
+++ src/library/navigation/goto/domains/bc/bcc.nl.js
@@ -8,4 +8,8 @@ module.exports = {
     store: 'bcc',
     zipcode: '',
   },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    const URL = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+    await context.goto(URL, { timeout: 50000, waitUntil: 'networkidle0' });
+  },
 };
--- src/library/product/details/stores/b/bcc/NL/extract.js
+++ src/library/product/details/stores/b/bcc/NL/extract.js
@@ -7,17 +7,31 @@ module.exports = {
     store: 'bcc',
     transform,
     domain: 'bcc.nl',
-  }, implementation: async (
+  },
+  implementation: async (
     inputs,
     parameters,
     context,
     dependencies,
   ) => {
+    try {
+      await context.waitForSelector('div#cookiewallmodal', { timeout: 20000 });
+      await context.evaluate(async function () {
+        const cookies = document.querySelector('button.cookiewall__accept-btn');
+        if (cookies) {
+          cookies.click();
+        }
+      });
+      await context.waitForSelector('section#wfinance-terms table', { timeout: 20000 });
+    } catch (error) {
+      console.log('@@There was an error accepting cookies@@' + JSON.stringify(error));
+    }
+
     await context.evaluate(async function () {
-     const getBulletsPoint =  document.querySelectorAll('div[data-title="Productinformatie"] ul li');
-     if(getBulletsPoint.length){
-      getBulletsPoint.forEach((ele)=> ele.textContent = `|| ${ele.textContent}` );
-     }
+      const getBulletsPoint = document.querySelectorAll('div[data-title="Productinformatie"] ul li');
+      if (getBulletsPoint.length) {
+        getBulletsPoint.forEach((ele) => ele.textContent = `|| ${ele.textContent}`);
+      }
     });
     const { transform } = parameters;
     const { productDetails } = dependencies;
