563448b9bc7393348a21bcbf96c8184264262760
nehashende <neha.shende@synerzip.com>
2021-01-06 20:54:37 +0530

    UAT tool issue fixes.

--- src/library/product/details/stores/d/darty/format.js
+++ src/library/product/details/stores/d/darty/format.js
@@ -22,36 +22,46 @@ const transform = (data) => {
   }))));
   for (const { group } of data) {
     for (const row of group) {
-      // if (row.description) {
-      //   let text = '';
-      //   if (row.additionalDescBulletInfo) {
-      //     text = row.additionalDescBulletInfo.reduce((item, currentItem) => `${item} || ${currentItem.text}`, '').trim();
-      //   }
-      //   if (text) {
-      //     row.description = [{
-      //       text: text + ' | ' + row.description[0].text.replace(/\s*\n\s*/g, ' '),
-      //     },
-      //     ];
-      //   } else {
-      //     row.description = [{
-      //       text: row.description[0].text.replace(/\s*\n\s*/g, ' '),
-      //     },
-      //     ];
-      //   }
-      // }
       if (row.description) {
-        const descriptionArr = row.description.map((item) => {
-          return item.text;
-        });
-        row.description = [{ text: descriptionArr.join('|'), xpath: row.description[0].xpath }];
+        let text = '';
+        if (row.additionalDescBulletInfo) {
+          text = row.additionalDescBulletInfo.reduce((item, currentItem) => `${item} || ${currentItem.text}`, '').trim();
+        }
+        if (text) {
+          row.description = [{
+            text: text + ' | ' + row.description[0].text.replace(/\s*\n\s*/g, ' '),
+          },
+          ];
+        } else {
+          row.description = [{
+            text: row.description[0].text.replace(/\s*\n\s*/g, ' '),
+          },
+          ];
+        }
       }
+      // if (row.description) {
+      //   const descriptionArr = row.description.map((item) => {
+      //     return item.text;
+      //   });
+      //   row.description = [{ text: descriptionArr.join('|'), xpath: row.description[0].xpath }];
+      // }
       if (row.manufacturerDescription) {
         row.manufacturerDescription[0].text = row.manufacturerDescription[0].text.replace(/\s*\n\s*/g, ' ');
       }
       if (row.videos) {
         row.videos.forEach(video => {
           if (video.text.includes('file')) {
-            video.text = video.text.replace(/.*?"file":"(.*?)".*/, '$1');
+            video.text = video.text.replace(/.*?"file":"(.*?)".*/, '$1').replace(/\\/g, '');
+          }
+          if (!video.text.startsWith('http')) {
+            video.text = `https:${video.text}`;
+          }
+        });
+      }
+      if (row.galleryVideos) {
+        row.galleryVideos.forEach(video => {
+          if (video.text.includes('file')) {
+            video.text = video.text.replace(/.*?"file":"(.*?)".*/, '$1').replace(/\\/g, '');
           }
           if (!video.text.startsWith('http')) {
             video.text = `https:${video.text}`;
