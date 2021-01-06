b8666fe91aed425c319973da11f5d927895ba593
Nishant Sinha <nishant.s@osmosys.asia>
2021-01-07 00:24:21 +0530

    Updated the xpaths and selectors

--- src/library/product/details/stores/b/bilkatogo/DK/extract.js
+++ src/library/product/details/stores/b/bilkatogo/DK/extract.js
@@ -12,7 +12,7 @@ module.exports = {
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     await new Promise(resolve => setTimeout(resolve, 5000));
     await context.evaluate(async () => {
-      function addElementToDocument (key, value) {
+      function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
@@ -77,20 +77,51 @@ module.exports = {
         addElementToDocument('finalPrice', finalPrice);
       }
 
-      const unitPrice = document.querySelector('p[class*=\'separated-list\']>span:last-child')
-        ? document.querySelector('p[class*=\'separated-list\']>span:last-child').innerText.trim() : '';
-      if (unitPrice) {
-        // eslint-disable-next-line no-useless-escape
-        const unitPriceRegex = /([\d,]+)\/?([\d,]+)?/g;
-        // eslint-disable-next-line no-useless-escape
-        const unitPriceUomRegex = /[^\.]+\.$/g;
-        const unitPriceString = unitPriceRegex.exec(unitPrice)[1] + ' DKK';
-        const unitPriceUomString = unitPriceUomRegex.exec(unitPrice)[0];
-        addElementToDocument('pricePerUnit', unitPriceString);
-        addElementToDocument('pricePerUnitUom', unitPriceUomString);
+      // const unitPrice = document.querySelector('p[class*=\'separated-list\']>span:last-child')
+      //   ? document.querySelector('p[class*=\'separated-list\']>span:last-child').innerText.trim() : '';
+      // if (unitPrice) {
+      //   // eslint-disable-next-line no-useless-escape
+      //   const unitPriceRegex = /([\d,]+)\/?([\d,]+)?/g;
+      //   // eslint-disable-next-line no-useless-escape
+      //   const unitPriceUomRegex = /[^\.]+\.$/g;
+      //   const unitPriceString = unitPriceRegex.exec(unitPrice)[1] + ' DKK';
+      //   const unitPriceUomString = unitPriceUomRegex.exec(unitPrice)[0];
+      let pricePerUnit = document.querySelector('div[class="product-sub-title"]');
+      let pricePerUnitSliced = '', pricePerUnitUOM = '';
+      let pricePerUnitTxt = pricePerUnit.innerText;
+      let strt = 0, end = 0, endUOM = 0;
+      for (let i = 0; i < pricePerUnitTxt.length; i++) {
+        if (pricePerUnitTxt.charAt(i) === '/' && strt === 0)
+          strt = i;
+        else if (pricePerUnitTxt.charAt(i) === '/' && end === 0)
+          end = i;
+        else if (pricePerUnitTxt.charAt(i) === 'L' && end !== 0) {
+          endUOM = i;
+          break;
+        }
+      }
+
+      pricePerUnitSliced = pricePerUnitTxt.substring(strt + 1, end).trim();
+      pricePerUnitUOM = pricePerUnitTxt.substring(strt + 1, endUOM + 1).trim();
+      addElementToDocument('pricePerUnit', pricePerUnitSliced);
+      addElementToDocument('pricePerUnitUom', pricePerUnitUOM);
+      let brandText = '', index = 0;
+      for (let i = pricePerUnitTxt.length; i >= 0; i--) {
+        if (pricePerUnitTxt.charAt(i) == '/') { index = i; break; }
+      }
+      brandText = pricePerUnitTxt.substring(index, pricePerUnitTxt.length);
+      if(brandText.length > 4){
+        addElementToDocument('product_brand', brandText);
+      } 
+
+      let expansionPanels = document.querySelectorAll('div.expansion-panel');
+      if (document.querySelectorAll('div.expansion-panel')) {
+        let expansionPanels = document.querySelectorAll('div.expansion-panel');
+        for (let i = 0; i < expansionPanels.length; i++) {
+          expansionPanels[i].querySelector('div.header-button').click();
+        }
       }
     });
-
     await context.extract(productDetails, { transform });
   },
 };
