e8b49f8daf7c85ecc646bf996c0b02e67678a00c
hari <harikrishnapalla47@gmail.com>
2021-01-06 13:51:39 +0530

    amazonPrimePantry search feed modified files

--- src/library/product/search/stores/a/amazonPrimePantry/IN/paginate.js
+++ src/library/product/search/stores/a/amazonPrimePantry/IN/paginate.js
@@ -4,20 +4,20 @@ module.exports = {
   parameterValues: {
     country: 'IN',
     store: 'amazonPrimePantry',
+    domain: 'amazon.in',
     nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
-    nextLinkXpath: null,
-    mutationSelector: null,
+    // nextLinkXpath: null,
+    // mutationSelector: null,
     spinnerSelector: 'div.s-result-list-placeholder:not(.aok-hidden)',
     // Use openSearchDefinition if nextLink has navigation issues.
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
-    loadedXpath: null,
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
-    stopConditionSelectorOrXpath: null,
-    resultsDivSelector: null,
+    // loadedXpath: null,
+    // stopConditionSelectorOrXpath: null,
+    // resultsDivSelector: null,
     openSearchDefinition: {
-      template: 'https://www.amazon.in/s?k={searchTerms}&ref=nb_sb_noss_2&page={page}',
+      template: 'https://www.amazon.in/s?k={searchTerms}&i=pantry&ref=nb_sb_noss&page={page}',
     },
-    domain: 'amazon.in',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[@cel_widget_id="MAIN-TOP_BANNER_MESSAGE" and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
     zipcode: '',
   },
 };
