07bb55c96be4e4f53db77cd6f1d47b59f052149c
Bindu Datla <bindu.datla@import.io>
2021-01-05 19:31:09 -0500

    updated when no variants

--- src/library/product/details/stores/n/namshi/variantsFormat.js
+++ src/library/product/details/stores/n/namshi/variantsFormat.js
@@ -10,7 +10,7 @@ const transform = (data) => {
       if (row.variantId) {
         row.variantId.forEach(item => {
           const vData = JSON.parse(item.text);
-          if (vData.variations) {
+          if (vData.variations && vData.variations.length > 0) {
             vData.variations.forEach(variation => {
               const tmpVariations = {};
               tmpVariations.variantId = [];
@@ -22,15 +22,23 @@ const transform = (data) => {
               variations.push(tmpVariations);
             });
           } else {
-            delete row.variantId;
+            const tmpVariations = {};
+            tmpVariations.variantId = [];
+            tmpVariations.variantUrl = [];
+            tmpVariations.variant = [];
+            tmpVariations.variantId.push({ text: vData.sku, xpath: item.xpath });
+            tmpVariations.variantUrl.push({ text: 'https://en-ae.namshi.com/' + vData.link, xpath: item.xpath });              
+            variations.push(tmpVariations);
           }
         });
       }
     }
+    
   }
   if (variations.length) {
     data[0].group = variations;
   }
+  
   return data;
 };
 
