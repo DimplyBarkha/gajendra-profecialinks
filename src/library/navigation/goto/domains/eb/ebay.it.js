d42f77bd744c6795d9e0ebfcf557b2b3695bfca5
sahoojudhi <judhishtor@profecialinks.com>
2021-01-07 14:22:11 +0530

    WITB and CTR fixed

--- src/library/navigation/goto/domains/eb/ebay.it.js
+++ src/library/navigation/goto/domains/eb/ebay.it.js
@@ -3,9 +3,19 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'ebay.it',
-    timeout: 100000,
+    timeout: 2400000,
     country: 'IT',
     store: 'ebay',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    await context.setLoadAllResources(true);  
+    }
+  },    
 };
