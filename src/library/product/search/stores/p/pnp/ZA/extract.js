2b8880c77b11e8a58069b69a01f65c955154027b
Rafał Perkowski <rafalpero1@gmail.com>
2021-01-07 11:04:11 +0100

    #1583 fixed id

--- src/library/product/search/stores/p/pnp/ZA/extract.js
+++ src/library/product/search/stores/p/pnp/ZA/extract.js
@@ -9,13 +9,37 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await context.evaluate(() => {
+  await context.evaluate(async function () {
+    function getHTML (url, callback) {
+      if (!window.XMLHttpRequest) return;
+      var xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        if (callback && typeof callback === 'function') {
+          callback(this.responseXML);
+        }
+      };
+      xhr.open('GET', url);
+      xhr.responseType = 'document';
+      xhr.send();
+    }
     // Creating Rank/RankOrganic and searchUrl
     const searchUrl = window.location.href;
     const allProducts = document.querySelectorAll('ul.col-md-12.product-listing.product-grid.product-grid > div > div');
     for (let x = 0; allProducts.length - 1 >= x; x++) {
       allProducts[x].setAttribute('count', `${x + 1}`);
       allProducts[x].setAttribute('searchurl', searchUrl);
+      // Create ID via XML request
+      const productUrl = allProducts[x].querySelector('a');
+      getHTML(`https://www.pnp.co.za/${productUrl}`, function (response) {
+        const id = response.querySelectorAll('div.col-sm-5.col-lg-8');
+        id.forEach(element => {
+          const regex = /\d+$/;
+          const idMatch = element.innerText.match(regex);
+          if (idMatch) {
+            allProducts[x].setAttribute('idxhr', idMatch[0]);
+          }
+        });
+      });
     }
     // Adding , separator to price
     const allProductsPrice = document.querySelectorAll('div.currentPrice  ');
