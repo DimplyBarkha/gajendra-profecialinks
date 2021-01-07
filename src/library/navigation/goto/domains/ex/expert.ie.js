951a0b47ccf21eba9e1344c96690d31f1682fd18
srishivams <srishivams97@gmail.com>
2021-01-07 21:04:28 +0530

    retrieved alternate images

--- src/library/navigation/goto/domains/ex/expert.ie.js
+++ src/library/navigation/goto/domains/ex/expert.ie.js
@@ -9,9 +9,24 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameterValues, context, dependencies) => {
-    // await context.setAntiFingerprint(false);
-    // await context.setLoadAllResources(true);
-    // await context.setBlockAds(false);
-    await context.goto(`${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    // // await context.setAntiFingerprint(false);
+    // // await context.setLoadAllResources(true);
+    // // await context.setBlockAds(false);
+    // await context.goto(`${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"load_timeout":60,"load_all_resources":true}[/!opt!]`, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+    url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 250000,
+      waitUntil: 'load',
+      checkBlocked: true,
+      embed_iframes: true,
+    });
   },
 };
--- src/library/product/details/stores/e/expert/IE/extract.js
+++ src/library/product/details/stores/e/expert/IE/extract.js
@@ -37,8 +37,43 @@ module.exports = {
         console.log('No videos loading.');
       }
     }
-    await context.evaluate(() => {
+    try {
+      await context.waitForSelector('div[class*="fotorama__nav__shaft"]', { timeout: 85000 });
+    } catch (error) {
+      console.log('Alternate images not loading');
+    }
+    await context.evaluate(async function () {
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+      await stall(4500);
       Array.from(document.querySelectorAll('#product_description div.value > ul > li')).forEach(elm => { elm.textContent = '|| ' + elm.textContent; });
+      let bigImageSrc = null; let picId = null;
+      if (document.querySelector('div[class*="fotorama__stage__frame"]')) {
+        const bigImage = document.querySelector('div[class*="fotorama__stage__frame"] img');
+        bigImageSrc = bigImage.getAttribute('src');
+        picId = bigImageSrc.replace(/(.+)(\/cache\/)(\w+)(\/)(.+)/g, '$3');
+      } else { console.log('frame not present'); }
+      if (document.querySelectorAll('div[class*="fotorama__thumb"] img')) {
+        const photoThumbs = document.querySelectorAll('div[class*="fotorama__thumb"] img');
+        for (let i = 1; i < photoThumbs.length; i++) {
+          let miniPic = photoThumbs[i].getAttribute('src');
+          const miniPicId = miniPic.replace(/(.+)(\/cache\/)(\w+)(\/)(.+)/g, '$3');
+          miniPic = miniPic.replace(miniPicId, picId);
+          addElementToDocument('alternateImages', miniPic);
+        }
+      }
     });
     return await context.extract(productDetails, { transform: transformParam });
   },
