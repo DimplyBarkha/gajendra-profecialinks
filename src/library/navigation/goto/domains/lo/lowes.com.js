d9456c7111b4ed12b578c86a2e6f0ef656d92c23
irfan shaikh <irfan.shaikh@synerzip.com>
2021-01-07 19:13:15 +0530

    Added optatg options to load enhanced content.

--- src/library/navigation/goto/domains/lo/lowes.com.js
+++ src/library/navigation/goto/domains/lo/lowes.com.js
@@ -10,7 +10,8 @@ module.exports = {
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
     await context.setBlockAds(false);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    const optTagUrl = `${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"enable_cache":false,"discard_CSP_header":true}[/!opt!]`;
+    await context.goto(optTagUrl, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
