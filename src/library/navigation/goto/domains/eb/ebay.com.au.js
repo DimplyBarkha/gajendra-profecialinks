4bb5fcbfafc84b87493a539d8d0c86a065242e94
sahoojudhi <judhishtor@profecialinks.com>
2021-01-07 16:42:03 +0530

    UPDP and WITB issue fixed

--- src/library/navigation/goto/domains/eb/ebay.com.au.js
+++ src/library/navigation/goto/domains/eb/ebay.com.au.js
@@ -3,24 +3,19 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'ebay.com.au',
-    timeout: 60000,
+    timeout: 240000,
     country: 'AU',
     store: 'ebay',
     zipcode: '',
   },
-   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    // url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
-    const timeout = parameters.timeout ? parameters.timeout : 90000;
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 60000;
     await context.setBlockAds(false);
-    // await context.setLoadAllResources(true);
-    // await context.setLoadImages(true);
-    // await context.setJavaScriptEnabled(true);
-    await context.setAntiFingerprint(false);
-    // await context.setUseRelayProxy(false);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, first_request_timeout: 60, load_timeout:60 });
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    await context.setLoadAllResources(true);  
     }
-  },
+  }, 
 };
