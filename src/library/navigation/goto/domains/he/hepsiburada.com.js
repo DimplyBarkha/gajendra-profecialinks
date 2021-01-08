237e0fc1aa86d0b82798215c50d2e13be20df244
Shreyam <shreyam.g@osmosys.asia>
2021-01-08 09:55:36 +0530

    Added custom fields

--- src/library/navigation/goto/domains/he/hepsiburada.com.js
+++ src/library/navigation/goto/domains/he/hepsiburada.com.js
@@ -7,4 +7,81 @@ module.exports = {
     store: 'hepsiburada',
     zipcode: '',
   },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    async function getHtml(url) {
+      const element = document.querySelectorAll('div.flix-std-table div.flix-std-content');
+      let inBox = [];
+      if (element) {
+        for (let i = 0; i < element.length; i++) {
+          inBox.push(element[i].textContent.replace(/\s\s/g, ''));
+        }
+      }
+      return inBox;
+    }
+
+    async function inBoxUrl(url) {
+      await new Promise((resolve, reject) => setTimeout(resolve, 30000));
+      const element = document.querySelectorAll('div.inpage_selector_InTheBox div.flix-background-image img');
+      let inBox = [];
+      if (element) {
+        for (let i = 0; i < element.length; i++) {
+          inBox.push(element[i].srcset);
+        }
+      }
+      return inBox;
+    }
+
+    async function addHtml(html, id) {
+      const div = document.createElement('div');
+      div.setAttribute('id', id);
+      div.innerHTML = html;
+      const scripts = div.getElementsByTagName('script');
+      let i = scripts.length;
+      while (i--) {
+        scripts[i].parentNode.removeChild(scripts[i]);
+      }
+      document.body.append(div);
+    }
+
+    async function getEnhancedContentLink() {
+      try {
+        const element = document.querySelector('#flix-iframe');
+        return element.src;
+      } catch (err) {
+        console.log('failed to get gtin', err);
+        return false;
+      }
+    }
+
+    async function addEnhancedContent() {
+      const enhancedContentLink = await context.evaluate(getEnhancedContentLink);
+      if (enhancedContentLink) {
+        await context.goto(enhancedContentLink, {
+          timeout,
+          waitUntil: 'load',
+          checkBlocked: true,
+        });
+        await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+        const html = await context.evaluate(getHtml, enhancedContentLink);
+        const inBoxManufactureUrl = await context.evaluate(inBoxUrl, enhancedContentLink);
+        await context.goto(url);
+        await context.evaluate(addHtml, html, 'inboxtext');
+        await context.evaluate(addHtml, inBoxManufactureUrl, 'inboxurl');
+      }
+    }
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    url = url + '#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]';
+    await context.captureRequests();
+    await context.goto(url, {
+      timeout,
+      waitUntil: 'load',
+      checkBlocked: true,
+    });
+    await addEnhancedContent();
+  },
 };
--- src/library/product/details/stores/h/hepsiburada/TR/extract.js
+++ src/library/product/details/stores/h/hepsiburada/TR/extract.js
@@ -26,7 +26,54 @@ module.exports = {
       const data = document.querySelector('#productDescriptionContent') ? document.querySelector('#productDescriptionContent').textContent.trim() : '';
       var bullets = JSON.stringify(data.match(/•|\*/gi) ? data.match(/•|\*/gi).length : 0);
       document.body.setAttribute('bullet_count', bullets);
+      const table = document.createElement('table');
+      document.body.appendChild(table);
+      const tBody = document.createElement('tbody');
+      table.appendChild(tBody);
+      let inBoxUrl = document.querySelector("#inboxurl");
+      if (inBoxUrl && inBoxUrl.textContent) {
+        let inBoxUrlArr = inBoxUrl.textContent.split(',');
+        for (let index = 0; index < inBoxUrlArr.length; index++) {
+          const newlink = document.createElement('tr');
+          newlink.setAttribute('class', 'in_boxurl');
+          newlink.setAttribute('inboxurl', 'https:' + inBoxUrlArr[index]);
+          tBody.appendChild(newlink);
+        }
+      }
+      let inBoxText = document.querySelector("#inboxtext");
+      if (inBoxText && inBoxText.textContent) {
+        let inBoxTextArr = inBoxText.textContent.split(',');
+        for (let index = 0; index < inBoxTextArr.length; index++) {
+          const newlink = document.createElement('tr');
+          newlink.setAttribute('class', 'in_boxtext');
+          newlink.setAttribute('inboxtext', inBoxTextArr[index]);
+          tBody.appendChild(newlink);
+        }
+      }
     });
+    // Page scroll
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop !== 20000) {
+          await stall(500);
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 20000) {
+            await stall(5000);
+            break;
+          }
+        }
+        function stall(ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await applyScroll(context);
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.extract(productDetails, { transform });
