81a15bf044c75aa23e58b458bf671614fc122260
Karthickravi <karthik.ravi-odc@partner.import.io>
2021-01-08 19:15:20 +0530

    search_url add

--- src/library/product/details/stores/p/promofarma/ES/extract.js
+++ src/library/product/details/stores/p/promofarma/ES/extract.js
@@ -36,6 +36,15 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
       // Method to Retrieve Xpath content of a Single Node
       var getXpath = (xpath, prop) => {
         var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
@@ -56,9 +65,8 @@ module.exports = {
         // @ts-ignore
         var man1 = document.querySelector('p[data-qa-ta="couponInfo"]').innerText
         // @ts-ignore
-        var man2 = document.querySelector('div[id="content-description"]').innerText
-
-        var man = man1 + '||' + man2
+        var man2 = document.querySelector('div[id="content-description"]').innerText        
+        var man = man1 + ' || ' + man2
 
         addElementToDocument('manu', man);
 
@@ -74,6 +82,55 @@ module.exports = {
       } catch (error) {
         
       }
+      var URL2 = getXpath("//div[@class='box-white']//h3[contains(text(),'Modo de empleo')]//following-sibling::p//text()", 'nodeValue');
+      var URL3 = getAllXpath("//div[@class='box-white']//h3[contains(text(),'Modo de empleo')]//following-sibling::ul//text()", 'nodeValue');
+      var URL4 = getAllXpath("//div[@class='box-white']//h4[contains(text(),'Consejo Farmacéutico')]//following-sibling::div/div[1]//text()", 'nodeValue');
+      var URL5 = []
+      var URL7 = []
+      var URL6 = ""
+      var URL8 = ""
+      if(URL4.length>=1){    
+      for (var i = 0 ; i<URL4.length; i++){
+        URL4[i]=URL4[i].trim();
+        if(URL4[i].length >=1){
+          URL5.push(URL4[i]);          
+          }
+        }  
+          URL6 = "||"+ URL5.join(","); 
+          if (URL6.includes(":,")){
+            // @ts-ignore
+            URL6 = URL6.replaceAll(":,",":");
+        }
+        if (URL6.includes(".,")){
+          // @ts-ignore
+          URL6 = URL6.replaceAll(".,",".");
+      }
+    }
+    if(URL3.length>=1){    
+      for (var i = 0 ; i<URL3.length; i++){
+        URL3[i]=URL3[i].trim();
+        if(URL3[i].length >=1){
+          URL7.push(URL3[i]);          
+          }
+        } 
+        URL8 = URL7.join(",");   
+        if (URL8.includes(":,")){
+          // @ts-ignore
+          URL8 = URL8.replaceAll(":,",":");
+      }
+      if (URL8.includes(".,")){
+        // @ts-ignore
+        URL8 = URL8.replaceAll(".,",".");
+    }      
+    }
+          var old = ""
+        if (URL2 != null) old = old + URL2.trim();
+        if (URL8 != null) old = old + URL8;
+        if (URL6 != null) old = old + URL6;
+        addElementToDocument('direction', old);
+                 
+    
+    
     });
     await context.extract(productDetails);
   },
