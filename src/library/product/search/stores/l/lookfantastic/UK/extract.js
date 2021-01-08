5693c492f7e1a01fab27a92d22870cd8f03e606c
venkatsn <venkat.sn-odc@partner.import.io>
2021-01-08 16:17:29 +0530

    #2636

--- src/library/product/search/stores/l/lookfantastic/UK/extract.js
+++ src/library/product/search/stores/l/lookfantastic/UK/extract.js
@@ -8,4 +8,42 @@ module.exports = {
     domain: 'lookfantastic.com',
     zipcode: ''
   },
+  implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addclass(xpathforpagination) {
+      var elems = document.querySelectorAll(xpathforpagination);
+      elems[0].classList.add('pagination');
+    }
+    function addElementToDocument(key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+    // Method to Retrieve Xpath content of a Single Node
+    var getXpath = (xpath, prop) => {
+      var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+      let result;
+      if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+      else result = elem ? elem.singleNodeValue : '';
+      return result && result.trim ? result.trim() : result;
+    };
+    const URL = window.location.href;
+    try {
+      document.getElementById('pd_url').remove();
+    } catch (error) {
+    }
+    addElementToDocument('pd_url', URL);
+  });
+  return await context.extract(productDetails, { transform });
+}
