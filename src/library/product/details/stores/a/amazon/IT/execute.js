91b0af3ef40aaf88204b6e959c16734f705e807d
vikas kyatannawar <vikas.k@osmosys.asia>
2021-01-08 22:41:01 +0530

    updated noresults xpthas, updated SA variant extractor

--- src/library/product/details/stores/a/amazon/IT/execute.js
+++ src/library/product/details/stores/a/amazon/IT/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'amazon',
     domain: 'amazon.it',
     loadedSelector: '#productTitle, [data-automation-id=title], [id*=Title] h1, h1[class*=title],  h1[id*=title]',
-    noResultsXPath: '//a[contains(@href, "dogsofamazon") and not(contains(@href, "503"))]',
+    noResultsXPath: '//a[contains(@href, "dogsofamazon") and not(contains(@href, "503"))] | //a[@href="/ref=cs_404_logo"]/@href',
     zipcode: '20019',
   },
 };
--- src/library/product/details/stores/a/amazon/US/execute.js
+++ src/library/product/details/stores/a/amazon/US/execute.js
@@ -5,6 +5,6 @@ module.exports = {
     store: 'amazon',
     domain: 'amazon.com',
     loadedSelector: '#productTitle, [data-automation-id=title], [id*=Title] h1, h1[class*=title],  h1[id*=title]',
-    noResultsXPath: '//a[contains(@href, "dogsofamazon") and not(contains(@href, "503"))]',
+    noResultsXPath: '//a[contains(@href, "dogsofamazon") and not(contains(@href, "503"))] | //a[@href="/ref=cs_404_logo"]/@href',
   },
 };
