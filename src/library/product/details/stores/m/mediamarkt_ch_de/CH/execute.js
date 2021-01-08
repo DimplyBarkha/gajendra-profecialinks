72bcc35b24c3e1ada568f907b24713d9e60b5f7f
Julia Lojek <julialojek@gmail.com>
2021-01-08 10:39:26 +0100

    #2248 code fixed according to uat tool

--- src/library/product/details/stores/m/mediamarkt_ch_de/CH/execute.js
+++ src/library/product/details/stores/m/mediamarkt_ch_de/CH/execute.js
@@ -5,8 +5,38 @@ module.exports = {
     country: 'CH',
     store: 'mediamarkt_ch_de',
     domain: 'mediamarkt.ch',
-    loadedSelector: '#product-details',
+    loadedSelector: 'aside#product-sidebar img',
     noResultsXPath: '//div[contains(@id, "search_no_result")] | //h1[contains(text(), "404")] | //div[contains(@class, "outer-brand")] | //span[@class="offline-text"]',
     zipcode: '',
+  },
+  implementation: async (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    let { url, id, zipcode, storeId } = inputs;
+    if (!url) {
+      if (!id) {
+        throw new Error('no id provided');
+      }
+      url = await dependencies.createUrl({ id });
+    }
+    url = url.replace('/fr/', '/de/');
+    await dependencies.goto({ url, zipcode, storeId });
+    await context.waitForNavigation();
+    const currentUrl = await context.evaluate(async () => {
+      return window.location.href;
+    });
+    if (currentUrl.includes('/fr/')) {
+      url = currentUrl.replace('/fr/', '/de/');
+      await dependencies.goto({ url, zipcode, storeId });
+    }
+
+    if (parameters.loadedSelector) {
+      await context.waitForFunction(function (sel, xp) {
+        return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+      }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    }
   },
 };
--- src/library/product/details/stores/m/mediamarkt_ch_de/CH/extract.js
+++ src/library/product/details/stores/m/mediamarkt_ch_de/CH/extract.js
@@ -12,11 +12,16 @@ module.exports = {
   implementation: async ({ inputString }, { transform }, context, { productDetails }) => {
     var extractedData = await context.extract(productDetails, { transform });
 
+    var productUrl = extractedData[0].group[0].productUrl;
+    if (productUrl) {
+      productUrl[0].text = productUrl[0].text.replace('/fr/', '/de/');
+    }
+
     var descriptions = extractedData[0].group[0].description;
     if (descriptions && descriptions.length > 1) {
       let fullDescription = '';
       descriptions.forEach(description => {
-        fullDescription += description.text + ' ';
+        fullDescription += description.text + ' | ';
       });
       descriptions[0].text = fullDescription;
       descriptions.splice(1);
@@ -33,6 +38,18 @@ module.exports = {
     var pricePerUnitUom = extractedData[0].group[0].pricePerUnitUom;
     if (pricePerUnitUom) {
       pricePerUnitUom[0].text = pricePerUnitUom[0].text.match(/pro (.+) =/)[1];
+    }
+
+    var calories = extractedData[0].group[0].caloriesPerServing;
+    if (calories && calories.length > 1) {
+      var caloriesDataArr = [];
+      var caloriesFinalData = '';
+      calories.forEach(info => {
+        caloriesDataArr.push(info.text);
+      });
+      caloriesFinalData = caloriesDataArr.join(' | ');
+      calories[0].text = caloriesFinalData;
+      calories.splice(1);
     }
   },
 };
