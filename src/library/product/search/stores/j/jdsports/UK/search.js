6d6c09f08adba4fb24544bb9c45d2ce505ecd125
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-01-08 22:58:59 +0530

    #2667 devjdsportssearch

--- src/library/product/search/stores/j/jdsports/UK/search.js
+++ src/library/product/search/stores/j/jdsports/UK/search.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'product/search',
   parameterValues: {
     country: 'UK',
-    store: 'jdsports',
+    store: 'JDSports',
     domain: 'jdsports.co.uk',
     zipcode: '',
   },
