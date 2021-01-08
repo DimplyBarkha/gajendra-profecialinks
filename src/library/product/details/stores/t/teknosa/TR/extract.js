004d431b39032b387cca7976c3bec076fb67d28d
BorysMaciuch <borysmaciuch@gmail.com>
2021-01-08 14:09:54 +0100

    #741 fix price

--- src/library/product/details/stores/t/teknosa/TR/extract.js
+++ src/library/product/details/stores/t/teknosa/TR/extract.js
@@ -9,4 +9,27 @@ module.exports = {
     domain: 'teknosa.com',
     zipcode: '',
   },
+  implementation: async (
+    { url },
+    { country, domain, transform },
+    context,
+    { productDetails },
+  ) => {
+    await context.evaluate(() => {
+      const scriptPrice = document.querySelector('#schemaJSON');
+      /*eslint-disable */
+      const regexPrice = /((?<="lowPrice\"\:\s\").+?(?="))/;
+      /*eslint-disable */
+      const regexListPrice = /((?<="highPrice\"\:\s\").+?(?="))/;
+      const currency = 'TL';
+      const price = scriptPrice.innerText.match(regexPrice);
+      const listPrice = scriptPrice.innerText.match(regexListPrice);
+      listPrice[0] = listPrice[0].replace('.', ',');
+      scriptPrice.setAttribute('correctPrice', price[0] + ' ' + currency);
+      if (price[0] !== listPrice[0]) {
+        scriptPrice.setAttribute('correctListPrice', listPrice[0] + ' ' + currency);
+      }
+    });
+    await context.extract(productDetails, { transform });
+  },
 };
