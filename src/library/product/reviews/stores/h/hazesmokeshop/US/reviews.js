ab7784655b0613a7975a1a84820edfd08c80e52d
Avinash Kadam <avinash.kadam@profecialinks.com>
2021-01-08 23:36:22 +0530

    Bat hazesmokeshop sr 2 (#3435)
    * search and review
    
    * inputs
    
    * removed escape char
    
    * example input added
    
    * deployed
    
    Co-authored-by: ahmad8931 <ahmad@profecialinks.com>
    Co-authored-by: shivaraj <>

---
+++ src/library/product/reviews/stores/h/hazesmokeshop/US/reviews.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/reviews',
+  parameterValues: {
+    country: 'US',
+    store: 'hazesmokeshop',
+    domain: 'hazesmokeshop.ca',
+    zipcode: '',
+    mergeType: null,
+  },
+};
