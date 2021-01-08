5aa1b66d25a9f24bcc9c0191e3d64d9162dd0631
Gajender Ramola <ramola106@gmail.com>
2021-01-08 15:17:15 +0530

    fix issues

--- src/library/product/details/stores/f/fnac/FR/execute.js
+++ src/library/product/details/stores/f/fnac/FR/execute.js
@@ -1,4 +1,38 @@
+/**
+ *
+ * @param { { url?: string,  id?: string, zipcode?: any, storeId?:any} } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action, createUrl: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  let { url, id, zipcode, storeId } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  await dependencies.goto({ url, zipcode, storeId });
+  try {
+    await context.waitForSelector('div .articleList div.Article-item article.Article-itemGroup');
+    await context.clickAndWaitForNavigation('div .articleList div.Article-item article.Article-itemGroup');
+  } catch (e) {
+    console.log(e);
+  }
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
 
+  // TODO: Check for not found?
+}
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -9,4 +43,5 @@ module.exports = {
     noResultsXPath: '//p[contains(@class,"firstline")]',
     zipcode: '',
   },
+  implementation,
 };
