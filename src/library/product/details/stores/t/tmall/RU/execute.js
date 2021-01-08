e330e3776709fb2403f9d41dda132cc0ec67ac4c
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-01-09 01:25:34 +0530

    fixing QA

--- src/library/product/details/stores/t/tmall/RU/execute.js
+++ src/library/product/details/stores/t/tmall/RU/execute.js
@@ -20,13 +20,20 @@ async function implementation (
   }
   await dependencies.goto({ url, zipcode, storeId });
   await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+  
 
   try {
-    await context.waitForSelector('div#product-detail li[ae_button_type="tab_specs"]',{timeout:1500});
-    await context.click('div#product-detail li[ae_button_type="tab_specs"]',{timeout:1000});
+    await context.waitForSelector('div#product-detail li[ae_button_type="tab_specs"]', { timeout: 1500 });
+    //await context.click('div#product-detail li[ae_button_type="tab_specs"]',{timeout:1000});
+    await context.evaluate(function(){
+      document.querySelector('div#product-detail li[ae_button_type="tab_specs"]').click();
+    })
+    await new Promise((resolve, reject) => setTimeout(resolve, 2000));
   }catch (e) {
     console.log(e);
   }
+
+  
   
   try{
     const deleteiFrame=await context.evaluate(async function(){
