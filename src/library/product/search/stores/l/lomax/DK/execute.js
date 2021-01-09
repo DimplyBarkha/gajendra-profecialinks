4c14b02685b12c49ff8343c6212b7774bae34dae
navya golla <navya.golla@od.import.io>
2021-01-09 17:25:56 +0530

    search feed type

--- src/library/product/search/stores/l/lomax/DK/execute.js
+++ src/library/product/search/stores/l/lomax/DK/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DK',
     store: 'lomax',
     domain: 'lomax.dk',
-    url: "https://www.lomax.dk/soeg/?q={searchTerms}",
+    url: "https://www.lomax.dk/soeg/?q=Dug",
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
