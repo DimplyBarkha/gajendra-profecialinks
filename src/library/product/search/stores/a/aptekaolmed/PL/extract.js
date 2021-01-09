9248facd38a7dfd194053c281ab9337d07837426
unknown <madhan.selvaraj-odc@partner.import.io>
2021-01-09 08:32:54 +0530

    search-apteokalmed

--- src/library/product/search/stores/a/aptekaolmed/PL/extract.js
+++ src/library/product/search/stores/a/aptekaolmed/PL/extract.js
@@ -36,6 +36,32 @@ async function implementation(
         }, ms);
       });
     }
+        function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+        }
+        // Method to Retrieve Xpath content of a Multiple Nodes
+        const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+        };
+        var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+        };
+        var backgroundURL = getAllXpath('//span[@class="product-name"]//text()', 'nodeValue');
+        var brand = getAllXpath('//a[@itemprop="brand"]//text()', 'nodeValue');
   
 });
   return await context.extract(productDetails, { transform });
