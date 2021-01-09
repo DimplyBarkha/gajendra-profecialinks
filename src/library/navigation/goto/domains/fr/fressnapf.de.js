a422bd0ac5f3a4f0e6f709f6be5ced806e313608
sobha rani <amol.thukrul-od@partner.import.io>
2021-01-09 15:01:44 +0530

    fressnapf search complete

--- src/library/navigation/goto/domains/fr/fressnapf.de.js
+++ src/library/navigation/goto/domains/fr/fressnapf.de.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'fressnapf.de',
-    timeout: 70000,
+    timeout: 90000,
     country: 'DE',
     store: 'fressnapf',
     zipcode: '',
