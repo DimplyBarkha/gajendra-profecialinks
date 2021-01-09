710c614838f121c56732c276c5ff394268a5426a
sobha rani <amol.thukrul-od@partner.import.io>
2021-01-09 20:24:06 +0530

    fressnapf search complete

--- src/library/product/search/stores/f/fressnapf/DE/extract.js
+++ src/library/product/search/stores/f/fressnapf/DE/extract.js
@@ -25,6 +25,34 @@ const implementation = async function (
     });
   };
   await applyScroll(context);
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll("div[class='grid']>div>a")[index];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+      }
+      return result;
+    };
+    var data = getAllXpath("//div[@class='grid']/div/a/@href", 'nodeValue');
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].includes('-')) {
+        var zz = data[i].split('-');
+        var id = zz[zz.length - 1];
+        id = id.slice(0, -1);
+        addHiddenDiv('RPC', id, i);
+      }
+    }
+  });
   return await context.extract(productDetails, { transform });
 };
 module.exports = {
