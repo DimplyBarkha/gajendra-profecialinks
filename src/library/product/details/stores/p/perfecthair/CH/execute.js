34b74950ad99ebed8f548eef0e8b6ba84f0223b2
Satyam Rathod <satyam.rathod@synerzip.com>
2021-01-09 20:00:06 +0530

    Fixed issues with execution

--- src/library/product/details/stores/p/perfecthair/CH/execute.js
+++ src/library/product/details/stores/p/perfecthair/CH/execute.js
@@ -1,30 +1,3 @@
-/**
- *
- * @param { { url?: string,  id?: string, zipcode?: any, storeId?:any} } inputs
- * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { goto: ImportIO.Action, createUrl} } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  console.log('params', parameters);
-  let { url, id, zipcode, storeId } = inputs;
-  if (!url) {
-    if (!id) {
-      throw new Error('no id provided');
-    }
-    url = await dependencies.createUrl({ id });
-  }
-  await dependencies.goto({ url, zipcode: inputs.zipcode });
-  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-
-  console.log('Checking no results', parameters.noResultsXPath);
-  
-}
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -34,6 +7,5 @@ module.exports = {
     loadedSelector: 'div[class *="product--image-container"] img',
     noResultsXPath: null,
     zipcode: '',
-  },
-  implementation,
+  }
 };
