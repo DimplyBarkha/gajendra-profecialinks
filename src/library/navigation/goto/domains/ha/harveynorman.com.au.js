74b4a89d12d983a67653d8858689d671333e5e21
sivasoarlogic <siva@soarlogic.com>
2021-01-09 14:22:46 +0530

    QAfixs

--- src/library/navigation/goto/domains/ha/harveynorman.com.au.js
+++ src/library/navigation/goto/domains/ha/harveynorman.com.au.js
@@ -8,4 +8,13 @@ module.exports = {
     store: 'harveynorman',
     zipcode: "''",
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
