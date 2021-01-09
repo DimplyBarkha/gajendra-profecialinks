b423c43a30e75550635d4da338af813472e37873
unknown <madhan.selvaraj-odc@partner.import.io>
2021-01-09 07:11:04 +0530

    search-frys

--- src/library/navigation/goto/domains/fr/frys.com.js
+++ src/library/navigation/goto/domains/fr/frys.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'frys.com',
-    timeout: 2000000,
+    timeout: 1000000,
     country: 'US',
     store: 'frys',
     zipcode: '',
