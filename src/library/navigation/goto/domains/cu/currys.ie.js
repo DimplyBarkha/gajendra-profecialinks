98dbeb20b48361e33604bd4378f3ad94360b81e6
Nishant Sinha <nishant.s@osmosys.asia>
2021-01-10 00:00:10 +0530

    Fixed issue with coupon, price and promotion

--- src/library/navigation/goto/domains/cu/currys.ie.js
+++ src/library/navigation/goto/domains/cu/currys.ie.js
@@ -1,4 +1,5 @@
 
+
 module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
@@ -7,5 +8,21 @@ module.exports = {
     country: 'IE',
     store: 'currys',
     zipcode: 'D02TX94',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    console.log('IN GOTO');
+    const timeout = parameters.timeout ? parameters.timeout : 60000;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setFirstRequestTimeout(60000);
+    await context.setUseRelayProxy(false);
+    await context.setJavaScriptEnabled(true);
+
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
