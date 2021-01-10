e126820995af3e809554bfe62b2b19027e73d80e
nehashende <neha.shende@synerzip.com>
2021-01-10 12:40:58 +0530

    Captcha code updated.

--- src/library/navigation/goto/domains/da/darty.com.js
+++ src/library/navigation/goto/domains/da/darty.com.js
@@ -53,7 +53,7 @@ module.exports = {
         function () {
         // @ts-ignore
           if (document.querySelector('.captcha-handler')) {
-            document.querySelector('.captcha-handler').click();  
+            document.querySelector('.captcha-handler').click();
           }
           if (document.querySelector('.geetest_radar_tip_content')) {
             document.querySelector('.geetest_radar_tip_content').click();
@@ -61,9 +61,56 @@ module.exports = {
         },
       );
       await new Promise(resolve => setTimeout(resolve, 10000));
-      await context.waitForSelector('#produit > div.product_head');
     } catch (error) {
       console.log('error: NO CPATCHA ENCOUNTER', error);
+    }
+    try {
+      await context.waitForSelector('#produit > div.product_head');
+    } catch (error) {
+      const responseStatus = await context.goto(`${url}#[!opt!]{"block_ads":false,"anti_fingerprint":false,"first_request_timeout":60,"load_timeout":30,"load_all_resources":true,"enable_cache":false,"discard_CSP_header":true}[/!opt!]`, {
+        antiCaptchaOptions: {
+          type: 'GEETEST',
+        },
+        firstRequestTimeout: 60000,
+        timeout: timeout,
+        waitUntil: 'load',
+        checkBlocked: false,
+      });
+      console.log('Status :', responseStatus.status);
+      console.log('URL :', responseStatus.url);
+
+      await context.waitForNavigation({ timeout: 30000 });
+      try {
+        await context.evaluateInFrame('iframe',
+          function () {
+          // @ts-ignore
+            const code = geetest
+              .toString()
+              .replace(
+                /appendTo\("#([^"]+)"\)/,
+                'appendTo(document.getElementById("$1"))',
+              );
+            return eval(`(${code})('/captcha/geetest');`);
+          },
+        );
+
+        await new Promise(resolve => setTimeout(resolve, 500));
+        await context.evaluateInFrame('iframe',
+          function () {
+          // @ts-ignore
+            if (document.querySelector('.captcha-handler')) {
+              document.querySelector('.captcha-handler').click();
+            }
+            if (document.querySelector('.geetest_radar_tip_content')) {
+              document.querySelector('.geetest_radar_tip_content').click();
+            }
+          },
+        );
+        await new Promise(resolve => setTimeout(resolve, 10000));
+        await context.waitForSelector('#produit > div.product_head');
+      } catch (error) {
+        console.log('error: FAILED TO SOLVE CAPTCHA AFTER RETRY', error);
+      }
     }
   },
 };
