05ed4c2e8c940a8ac6d3de1f2011b8c6907af095
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-01-11 00:09:12 +0530

    #2658 devwhiskysearch

--- src/library/product/search/stores/w/whisky/DE/execute.js
+++ src/library/product/search/stores/w/whisky/DE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'DE',
     store: 'whisky',
     domain: 'whisky.de',
-    url: 'https://www.whisky.de/shop/index.php?cl=search&searchparam={searchTerms}',
+    url: 'http://www.whisky.de/shop/index.php?lang=0&cl=search&searchparam={searchTerms}',
     loadedSelector:'body',
     noResultsXPath: null,
     zipcode: '',
