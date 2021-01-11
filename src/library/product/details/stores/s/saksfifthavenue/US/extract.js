2cac97d628554f0bcbb9b4bb79888042823e1263
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-11 13:22:15 -0800

    Make changes after testing

--- src/library/product/details/stores/s/saksfifthavenue/US/extract.js
+++ src/library/product/details/stores/s/saksfifthavenue/US/extract.js
@@ -12,6 +12,7 @@ module.exports = {
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
       const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+      const isVisible = (element) => (!!(element).offsetWidth || (element).offsetHeight);
 
       async function addAttributes (node) {
         const inventory = document.evaluate('//div[contains(@class, "product-availability")]//ul[contains(@class, "availability-msg")]|//div[@id="waitlist"][not(contains(@class, "d-none"))]//div[contains(@class, "soldout")]', document, null, XPathResult.STRING_TYPE, null);
@@ -24,12 +25,14 @@ module.exports = {
         }
       }
 
-      for (let i = 0; i < document.querySelectorAll('ul.size-attribute li').length; i++) {
-        const element = document.querySelectorAll('ul.size-attribute li')[i];
-        element.querySelector('a').click();
-        await delay(5000);
-        const elm = element.querySelector('a');
-        await addAttributes(elm);
+      for (let i = 0; i < document.querySelectorAll('ul.size-attribute li[value="0"]').length; i++) {
+        const element = document.querySelectorAll('ul.size-attribute li[value="0"]')[i];
+        if (isVisible(element) === true) {
+          element.querySelector('a').click();
+          await delay(5000);
+          const elm = element.querySelector('a');
+          await addAttributes(elm);
+        }
       }
     });
     await context.extract(productDetails);
