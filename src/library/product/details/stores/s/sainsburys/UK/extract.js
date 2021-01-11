2a5710c6323c56c435c7700373b595329c4bdc20
Avinash <avinash.kadam@profecialinks.com>
2021-01-11 23:13:25 +0530

    priceCurrency updated

--- src/library/product/details/stores/s/sainsburys/UK/extract.js
+++ src/library/product/details/stores/s/sainsburys/UK/extract.js
@@ -1,5 +1,5 @@
 const { transform } = require('./format');
-async function implementation(
+async function implementation (
   inputs,
   parameters,
   context,
@@ -7,25 +7,38 @@ async function implementation(
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+  try {
+    await context.waitForSelector('script[type="application/ld+json"]', { timeout: 10000 });
+  } catch (e) {
+    console.log('script not loaded');
+  }
   await context.evaluate(async () => {
-    function addHiddenDiv(elementID, content) {
+    function addHiddenDiv (elementID, content) {
       const newDiv = document.createElement('div');
       newDiv.className = elementID;
       newDiv.textContent = content;
       newDiv.style.display = 'none';
-      const element = document.querySelector('div.ln-o-page__body')
+      const element = document.querySelector('div.ln-o-page__body');
       element.appendChild(newDiv);
     }
 
+    try {
+      const script = document.querySelector('script[type="application/ld+json"]');
+      const scriptContent = JSON.parse(script.textContent);
+      var priceCurrency = scriptContent.offers.priceCurrency;
+      addHiddenDiv('custom_priceCurrency', priceCurrency);
+    } catch (e) {
+      console.log('error in read script');
+    }
+
     const urlParams = new URLSearchParams(window.location.search);
-    const custom_sku = urlParams.get('productId');
+    const customSku = urlParams.get('productId');
     var url = window.location.href;
-    addHiddenDiv("custom_sku", custom_sku);
-    addHiddenDiv("product_custom_url", url);
+    addHiddenDiv('custom_sku', customSku);
+    addHiddenDiv('product_custom_url', url);
   });
   return await context.extract(productDetails, { transform });
 }
-
 
 module.exports = {
   implements: 'product/details/extract',
--- src/library/product/details/stores/s/sainsburys/UK/format.js
+++ src/library/product/details/stores/s/sainsburys/UK/format.js
@@ -4,121 +4,114 @@
  * @returns {ImportIO.Group[]}
  */
 const url1 = require('url');
- const transform = (data) => {
-    // Default transform function
-    const clean = text => text.toString()
-        .replace(/\r\n|\r|\n/g, ' ')
-        .replace(/&amp;nbsp;/g, ' ')
-        .replace(/&amp;#160/g, ' ')
-        .replace(/\u00A0/g, ' ')
-        .replace(/\s{2,}/g, ' ')
-        .replace(/"\s{1,}/g, '"')
-        .replace(/\s{1,}"/g, '"')
-        .replace(/^ +| +$|( )+/g, ' ')
-        // eslint-disable-next-line no-control-regex
-        .replace(/[\x00-\x1F]/g, '')
-        .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-    }))));
+const transform = (data) => {
+  // Default transform function
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
 
-    for (const { group }  of data) {
-        for (const row of group) {
-
-        //   if (row.price) {
-        //     let currency = '';
-        //     row.price.forEach(item => {
-        //       //currency = (item.text).includes('£'); 
-        //       item.text = item.text.replace('£', ' ').trim();
-        //     });
-        //  //   row.priceCurrency = [ { text: ( currency ) ? 'GBP':''} ]
-        //   }
-          // if (row.listPrice) {
-          //   row.listPrice.forEach(item => {
-          //     item.text = item.text.replace('£', ' ').trim();
-          //   });
-          // }
-          if (row.priceCurrency) {
-            row.priceCurrency.forEach(item => {
-              item.text = item.text.includes('£') ? 'GBP': item.text;
-            });
+  for (const { group } of data) {
+    for (const row of group) {
+      // if (row.price) {
+      //   row.price.forEach(item => {
+      //     item.text = item.text.replace('£', ' ').trim();
+      //   });
+      // }
+      // if (row.listPrice) {
+      //   row.listPrice.forEach(item => {
+      //     item.text = item.text.replace('£', ' ').trim();
+      //   });
+      // }
+      if (row.priceCurrency) {
+        row.priceCurrency.forEach(item => {
+          item.text = item.text.includes('£') ? 'GBP' : item.text;
+        });
+      }
+      if (row.availabilityText) {
+        row.availabilityText.forEach(item => {
+          item.text = ('Add' === item.text) ? 'In stock' : 'Out of stock';
+        });
+      }
+      if (row.shownImages) {
+        let text = '';
+        let element = '';
+        let images = [];
+        row.shownImages.forEach((item, index) => {
+          element = item.text.split(',');
+          if (0 == index) {
+            text = element[element.length - 2].trim().split(' ')[0];
+          } else {
+            text = element[element.length - 1].trim().split(' ')[0];
           }
-
-          if (row.availabilityText) {
-            row.availabilityText.forEach(item => {
-              item.text =  ('Add' === item.text ) ? 'in stock' : 'Out of stock';
-            });
+          images.push({ text: text });
+        });
+        row.shownImages = images;
+      }
+      if (row.highQualityImages) {
+        let text1 = '';
+        let element1 = '';
+        let images1 = [];
+        row.highQualityImages.forEach((item, index) => {
+          element1 = item.text.split(',');
+          if (0 == index) {
+            text1 = element1[element1.length - 2].trim().split(' ')[0];
+          } else {
+            text1 = element1[element1.length - 1].trim().split(' ')[0];
           }
-          if (row.shownImages) {
-            let text = '';
-            let element = '';
-            let images = [];
-            row.shownImages.forEach( (item, index ) => {
-              element = item.text.split(',');
-              if( 0 == index ) {
-                text = element[element.length-2].trim().split(' ')[0];
-              } else {
-                text = element[element.length-1].trim().split(' ')[0];
-              }
-
-              images.push({text:text });
-            });
-            row.shownImages = images;
-          }
-          if (row.highQualityImages) {
-            let text1 = '';
-            let element1 = '';
-            let images1 = [];
-            row.highQualityImages.forEach((item, index) => {
-              element1 = item.text.split(',');
-              if( 0 == index ) {
-                text1 = element1[element1.length-2].trim().split(' ')[0];
-              } else {
-                text1 = element1[element1.length-1].trim().split(' ')[0];
-              }
-              images1.push({text:text1 });
-            });
-            row.highQualityImages = images1;
-          }
-          if (row.aggregateRating) {
-            row.aggregateRating.forEach(item => {
-              let text = parseFloat( item.text.replace(/[^\d.-]/g, '') ).toFixed(1);
-              item.text = text;
-            });
-          }
-          if (row.ratingCount ) {
-            row.ratingCount.forEach(item => {
-              item.text = parseInt( item.text.replace(/[^\d-]/g, '') );
-            });
-          }
-          if (row.countryOfOrigin ) {
-            row.countryOfOrigin.forEach(item => {
-              item.text = item.text.replace(/\s*/g, '');
-              item.text = item.text.replace('Countryoforigin:', '');
-            });
-          }
-          if (row.product_grocery_allergens ) {
-            row.product_grocery_allergens.forEach(item => {
-              item.text = item.text.replace('Dietary Information', '');
-            });
-          }
-          if (row.product_grocery_calories_per_serving ) {
-            row.product_grocery_calories_per_serving.forEach(item => {
-              item.text = item.text.split('/')[1];
-            });
-          }
-          if (row.legalDisclaimer ) {
-            let arrDisclaimer = [];
-            row.legalDisclaimer.forEach(item => {
-              arrDisclaimer.push( item.text);
-            });
-            let disclaimer = arrDisclaimer.join(' ');
-            row.legalDisclaimer = [{ text: disclaimer }];
-          }
-
-        }
+          images1.push({ text: text1 });
+        });
+        row.highQualityImages = images1;
+      }
+      if (row.aggregateRating) {
+        row.aggregateRating.forEach(item => {
+          let text = parseFloat(item.text.replace(/[^\d.-]/g, '')).toFixed(1);
+          item.text = text;
+        });
+      }
+      if (row.ratingCount) {
+        row.ratingCount.forEach(item => {
+          item.text = parseInt(item.text.replace(/[^\d-]/g, ''));
+        });
+      }
+      if (row.countryOfOrigin) {
+        row.countryOfOrigin.forEach(item => {
+          item.text = item.text.replace(/\s*/g, '');
+          item.text = item.text.replace('Countryoforigin:', '');
+        });
+      }
+      if (row.product_grocery_allergens) {
+        row.product_grocery_allergens.forEach(item => {
+          item.text = item.text.replace('Dietary Information', '');
+        });
+      }
+      if (row.product_grocery_calories_per_serving) {
+        row.product_grocery_calories_per_serving.forEach(item => {
+          item.text = item.text.split('/')[1];
+        });
+      }
+      if (row.legalDisclaimer) {
+        let arrDisclaimer = [];
+        row.legalDisclaimer.forEach(item => {
+          arrDisclaimer.push(item.text);
+        });
+        let disclaimer = arrDisclaimer.join(' ');
+        row.legalDisclaimer = [{ text: disclaimer }];
+      }
     }
-    return data;
+  }
+  return data;
 };
 
 module.exports = { transform };
