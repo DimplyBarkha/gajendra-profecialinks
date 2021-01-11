6af14d85b7d0c1df5162ea9b38c658062551971b
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-11 11:07:17 -0800

    Add changes to take on variant sizes

--- src/library/product/details/stores/s/saksfifthavenue/US/extract.js
+++ src/library/product/details/stores/s/saksfifthavenue/US/extract.js
@@ -9,4 +9,35 @@ module.exports = {
     domain: 'saksfifthavenue.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      // Get additional product info
+      const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+      async function addAttributes(node) {
+        const inventory = document.evaluate('//div[contains(@class, "product-availability")]//ul[contains(@class, "availability-msg")]|//div[@id="waitlist"][not(contains(@class, "d-none"))]//div[contains(@class, "soldout")]', document, null, XPathResult.STRING_TYPE, null);
+        if (inventory && inventory.stringValue) {
+          node.setAttribute('inventory', inventory.stringValue);
+        }
+        const availText = document.evaluate('//button[contains(@class, "add-to-cart btn")][not(@disabled)]/@class', document, null, XPathResult.STRING_TYPE, null);
+        if (availText && availText.stringValue) {
+          node.setAttribute('availability', 'true');
+        }
+      }
+      for (let i = 0; i < document.querySelectorAll('ul.size-attribute li').length; i++) {
+        const element = document.querySelectorAll('ul.size-attribute li')[i];
+        element.querySelector('a').click();
+        await delay(5000);
+        const elm = element.querySelector('a');
+        console.log(document.querySelector('ul.availability-msg li div') ? document.querySelector('ul.availability-msg li div').innerText : '')
+        await addAttributes(elm);
+      }
+      // [...document.querySelectorAll('ul.size-attribute li')].map(element => {
+      //   element.querySelector('a').click();
+      //   const elm = element.querySelector('a');
+      //   await addAttributes(elm);
+      // });
+    });
+    await context.extract(productDetails);
+  },
 };
