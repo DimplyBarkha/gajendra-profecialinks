c078032b31eee51e867a1035605295f4cc6d0ba7
Akansha Wajge <akansha.wajge@synerzip.com>
2021-01-11 15:37:57 +0530

    bestbuy pagination issue (#1643)

--- src/library/product/search/stores/b/bestbuy/US/paginate.js
+++ src/library/product/search/stores/b/bestbuy/US/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'bestbuy',
-    nextLinkSelector: 'a.sku-list-page-next[aria-disabled="false"]',
+    // nextLinkSelector: 'a.sku-list-page-next[aria-disabled="false"]',
     mutationSelector: null, //"ol.paging-list",
     spinnerSelector: null,
     loadedSelector: 'ol.sku-item-list',
-    noResultsXPath: "//div[contains(@class,'no-result')]",
-    openSearchDefinition: null,
+    noResultsXPath: "//div[contains(@class,'no-result')] | //h1[@class='heading VPT-title' and text()='We’re sorry, something went wrong.']",
+    openSearchDefinition: {
+      template: 'https://www.bestbuy.com/site/searchpage.jsp?st={searchTerms}&cp={page}',
+    },
     domain: 'bestbuy.com',
     zipcode: '',
   },
