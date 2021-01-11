ac43b6e5b995f49d914481c2ee50ce205ec0dd92
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-01-11 20:51:21 +0530

    updated core xpaths

--- src/library/product/details/stores/r/real/DE/extract.js
+++ src/library/product/details/stores/r/real/DE/extract.js
@@ -11,44 +11,17 @@ module.exports = {
   implementation: async (inputs, parameters, context, dependencies) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
-    
+
     await context.evaluate(async function () {
 
-      function addElementToDocument(key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
+      function addElementToDocument(id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll('h1[class="rd-title"]')[0];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
-      try {
-        // XPATH Data Extraction For Aggregate Rating
-        // @ts-ignore
-        const aggregateRating = window.dataLayer[0].product.rating_average;
-        addElementToDocument('addedAggregateRating', aggregateRating);
-
-      } catch (error) {
-
-      }
-
-      try {
-        // @ts-ignore
-        // XPATH Data Extraction For Shipping Info
-        const deliveryDuration = window.dataLayer[0].product.offers[0].delivery_duration;
-        // @ts-ignore
-        const ShippingCost = window.dataLayer[0].product.offers[0].shipping_cost;
-        addElementToDocument('shippingInfo', 'delivery Duration:' + deliveryDuration + ', Shipping Charges:' + ShippingCost);
-      } catch (error) {
-
-      }
-      try {
-        // @ts-ignore
-        const sku = window.dataLayer[0].product.id;
-        addElementToDocument('sku', sku);
-      } catch (error) {
-
-      }
-
       // Method to Retrieve Xpath content of a Multiple Nodes
       const getAllXpath = (xpath, prop) => {
         const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -59,111 +32,44 @@ module.exports = {
         }
         return result;
       };
+
+      try {
+        // XPATH Data Extraction For Aggregate Rating
+        // @ts-ignore
+        const aggregateRating = getAllXpath('//span[@class="rd-reviews__total-rating-number"]/text()','nodeValue');
+        let FinalaggregateRating = aggregateRating[0].replace(/\s\s+/g, '');
+        addElementToDocument('addedAggregateRating', FinalaggregateRating.split(' ')[0]);
+
+      } catch (error) {
+
+      }
+
       try {
         // @ts-ignore
-        const description = document.querySelector('div[class="rd-product-description"]').innerText
+        const sku = window.location.href;
+        addElementToDocument('sku', sku.replace(/([A-Za-z])|[^\w\s]/gi, ""));
+      } catch (error) {
+
+      }
+
+      
+      try {
+        const price = getAllXpath('//div[@class="rd-buybox__price"]/text()', 'nodeValue');
+        const cents = getAllXpath('//div[@class="rd-buybox__price"]/@cents', 'nodeValue');
+        const finalPrice = price[0].replace(/\s|[,]/g, '');
+        addElementToDocument('price', '€ ' + finalPrice + '.' + cents);
+      } catch (error) {
+
+      }
+
+      try {
+        // @ts-ignore
+        let description = document.querySelector('div[class="rd-product-description__top-accordion-content-description"]').innerText
+        description = description.replace(/\s/g, ' ');
+        description = description.replace(/\s\s+/g, ' ');
         addElementToDocument('description', description);
       } catch (error) {
 
-      }
-
-      try {
-        var temp;
-        const sliceURL = (data) => {
-          for (let index = 0; index < data.length; index++) {
-            if (data[index].includes('background-image: url')) {
-              temp = data[index];
-              temp = temp.replace('100x100', '1024x1024');
-              addElementToDocument('altImages', temp.split('("').join('")').split('")')[1]);
-            }
-          }
-        };
-        var backgroundURL = getAllXpath("//div[contains(@cs-id,'image-gallery-thumbnail') and not(contains(@class,'active'))]/@style", 'nodeValue');
-        sliceURL(backgroundURL);
-      } catch (error) {
-
-      }
-
-      try {
-        // @ts-ignore
-        var data = document.querySelectorAll('div.rd-product-sidebar__info-card')[0].dataset.overlayAttr;
-        var updatedData = JSON.parse(data);
-        const sellerName = updatedData.childProps.name;
-        const sellerOnRealSince = updatedData.childProps.sellerOnRealSince;
-        var shipping_details = sellerName + " verkauft auf real.de seit: " + sellerOnRealSince;
-        addElementToDocument('shipping_details', shipping_details);
-      } catch (error) {
-
-      }
-      try {
-        // @ts-ignore
-        var fullText = document.querySelector("div[class='rd-product-description__text']").innerText;
-        fullText = fullText.replace(/\n/g, "_____");
-        const seperateText = fullText.split('_____');
-        var index;
-        for (index = 0; index < seperateText.length; index++) {
-          if (seperateText[index].includes("Paketgewicht")) {
-            addElementToDocument('weightGross', seperateText[index]);
-            break;
-          }
-        }
-        var height, diameter, maxCapacity, package_weight, Alcohol, Material;
-        for (index = 0; index < seperateText.length; index++) {
-          if (seperateText[index].includes("Höhe")) {
-            height = seperateText[index];
-          }
-          if (seperateText[index].includes("Durchmesser")) {
-            diameter = seperateText[index];
-          }
-          if (seperateText[index].includes("Maximale Füllmenge")) {
-            maxCapacity = seperateText[index];
-          }
-          if (seperateText[index].includes("Paketgewicht")) {
-            package_weight = seperateText[index];
-          }
-          if (seperateText[index].includes("Alkoholgehalt")) {
-            Alcohol = seperateText[index];
-          }
-          if (seperateText[index].includes("Material") || seperateText[index].includes("Material")) {
-            Material = seperateText[index];
-          }
-        }
-      } catch (error) {
-
-      }
-
-      try {
-        if (height.length > 0 || diameter.length > 0 || maxCapacity.length > 0) {
-          addElementToDocument('shippingDimensions', height + ', ' + diameter + ', ' + maxCapacity);
-        }
-
-      } catch (error) {
-      }
-      try {
-        if (package_weight.length > 0) {
-          addElementToDocument('shippingWeight', package_weight);
-        }
-      } catch (error) {
-      }
-      try {
-        if (Alcohol.length > 0) {
-          addElementToDocument('Alcohol', Alcohol);
-        }
-      } catch (error) {
-      }
-      try {
-        if (Material.length > 0) {
-          addElementToDocument('Material', Material);
-        }
-      } catch (error) {
-      }
-      try {
-        // @ts-ignore
-        const size = document.querySelector('h1[class="rd-product-detail__title"]').innerText;
-        if (size.includes('0,7 l')) {
-          addElementToDocument('size', '0.7 Liters');
-        }
-      } catch (error) {
       }
     });
     return await context.extract(productDetails, { transform });
