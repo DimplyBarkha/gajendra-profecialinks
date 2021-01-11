5597fc1587c1435007ecb645f41f08f2c43f880f
Avinash <avinash.kadam@profecialinks.com>
2021-01-11 13:00:38 +0530

    for testing results value changed to 200

--- src/library/product/reviews/action.js
+++ src/library/product/reviews/action.js
@@ -12,7 +12,7 @@ async function implementation (
   context,
   { execute, extract, paginate },
 ) {
-  const { URL: url, RPC, SKU, date: dateOrigin = null, days = 30, results = 10000 } = inputs;
+  const { URL: url, RPC, SKU, date: dateOrigin = null, days = 30, results = 200 } = inputs;
   const id = RPC || SKU || inputs.id;
   const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
 
