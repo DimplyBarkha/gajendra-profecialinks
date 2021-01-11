48bcebef735ef433af69813eb77c8471cd1679ee
sobha rani <amol.thukrul-od@partner.import.io>
2021-01-11 16:26:53 +0530

    laRebajaVirtual search complete

--- src/library/product/search/stores/l/larebajavirtual/CO/paginate.js
+++ src/library/product/search/stores/l/larebajavirtual/CO/paginate.js
@@ -10,9 +10,9 @@ module.exports = {
     spinnerSelector: null,
     loadedSelector: '#id-productos-list , .next a',
     noResultsXPath: null,
-    openSearchDefinition: {
-      template: 'https://www.larebajavirtual.com/catalogo/buscar/ajax/id-productos-list/subMenuCategory/on/busqueda/{searchTerms}/codigoProducto_page/{page}?ajax=id-productos-list&pageSize=500',
-    },
+    // openSearchDefinition: {
+    //   template: 'https://www.larebajavirtual.com/catalogo/buscar/ajax/id-productos-list/subMenuCategory/on/busqueda/{searchTerms}/codigoProducto_page/{page}?ajax=id-productos-list&pageSize=150',
+    // },
     domain: 'larebajavirtual.com',
     zipcode: '',
   },
