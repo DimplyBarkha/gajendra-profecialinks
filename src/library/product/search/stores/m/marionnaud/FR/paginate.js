40844786de840c43d05b0adf0bedbfd3bfa42e1a
Dhanush <dhanush.isukapalli@od.import.io>
2021-01-12 14:29:24 +0530

    marionnaud by dhanush

--- src/library/product/search/stores/m/marionnaud/FR/paginate.js
+++ src/library/product/search/stores/m/marionnaud/FR/paginate.js
@@ -4,15 +4,15 @@ module.exports = {
   parameterValues: {
     country: 'FR',
     store: 'marionnaud',
-    // nextLinkSelector: 'a[class="page-link next"]',
-    nextLinkSelector: null,
+    nextLinkSelector: 'a[class="page-link next"]',
+    // nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ul[class="product-listing product-grid"] li',
     noResultsXPath: null,
-    openSearchDefinition: {
-      template: 'https://www.marionnaud.fr/search?q={searchTerms}%3Arank-desc&sort=&page={page}&pageSize=100',
-      },
+    // openSearchDefinition: {
+    //   template: 'https://www.marionnaud.fr/search?q={searchTerms}%3Arank-desc&sort=&page={page}&pageSize=100',
+    //   },
     domain: 'marionnaud.fr',
     zipcode: '',
   },
