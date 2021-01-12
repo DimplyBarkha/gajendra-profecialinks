9b4b99cf36df1c168ac19458e5cd2fee275fcc31
romancg <gromantsg@gmail.com>
2021-01-12 18:00:22 +0200

    #1209 update rank

--- src/library/product/search/stores/e/euronics/IT/extract.js
+++ src/library/product/search/stores/e/euronics/IT/extract.js
@@ -19,14 +19,14 @@ module.exports = {
       const product = document.querySelectorAll('div.productCard.j-productCard');
       const pi = 16;
       for (let i = 0; i < product.length; i++) {
-        const rank = (page * pi) - (pi - 1) + i;
+        const rank = (page * pi) - pi + i;
         product[i].querySelector('#log_position').setAttribute('rank', rank);
         product[i].querySelector('#log_position').setAttribute('organic', rank);
       }
       const productOrganic = document.querySelectorAll('.productCard__content>div.productCard.j-productCard');
       const po = 12;
       for (let i = 0; i < productOrganic.length; i++) {
-        const rankOrganic = (page * po) - (po - 1) + i;
+        const rankOrganic = (page * po) - po + i;
         productOrganic[i].querySelector('#log_position').setAttribute('organic', rankOrganic);
       }
     });
