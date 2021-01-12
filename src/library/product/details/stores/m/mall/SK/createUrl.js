713a1b9b386aaac17c73afe6a4b8ada27c44099c
dhanush <dhanush.isukapalli@od.import.io>
2021-01-12 18:28:51 +0530

    mall by kavya

--- src/library/product/details/stores/m/mall/SK/createUrl.js
+++ src/library/product/details/stores/m/mall/SK/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'mall.sk',
     prefix: null,
-    url: null,
+    url: 'https://www.mall.sk/hladaj?s={id}',
     country: 'SK',
     store: 'mall',
     zipcode: '',
