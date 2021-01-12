0444c8da82e0148c99975296976408a14c1a0041
osmarghyadeep <arghyadep.g@partner.import.io>
2021-01-12 07:42:11 +0530

    Updated fix for videos

--- src/library/product/details/stores/f/fnac/ES/extract.js
+++ src/library/product/details/stores/f/fnac/ES/extract.js
@@ -69,6 +69,84 @@ async function implementation (inputs, parameters, context, dependencies) {
   const manufacturerDescription = await getManufacturerDescription(cssManufacturerDescription);
   await setValueInDivToDOM('manufacturerDescription', manufacturerDescription);
 
+  try {
+    await context.evaluate(async () => {
+      async function infiniteScroll () {
+        let prevScroll = document.documentElement.scrollTop;
+        while (true) {
+          window.scrollBy(0, document.documentElement.clientHeight);
+          await new Promise(resolve => setTimeout(resolve, 2000));
+          const currentScroll = document.documentElement.scrollTop;
+          if (currentScroll === prevScroll) {
+            break;
+          }
+          prevScroll = currentScroll;
+        }
+      }
+      await infiniteScroll();
+      console.log('done scrolling');
+    });
+  } catch(err) {
+    console.log('got some error while scrolling -', err.message);
+  }
+
+  let videoDivInfoElmXpath = '//div[contains(@class,"VideoPlayerPlaylist-list")]//div[@data-video-count > 0]';
+  try {
+    context.waitForXPath(videoDivInfoElmXpath);
+    console.log('video input elm loaded');
+  } catch(err) {
+    console.log('error while waiting for video input elm', err.message);
+    console.log('waiting again');
+    try {
+      context.waitForXPath(videoDivInfoElmXpath);
+      console.log('video input elm loaded');
+    } catch(error) {
+      console.log('error while waiting for video input elm -- again', error.message);
+    }
+  }
+
+  let allVideos = [];
+  try {
+    allVideos = await context.evaluate(async (videoDivInfoElmXpath) => {
+      let videoInfoElm = document.evaluate(videoDivInfoElmXpath, document, null, 7, null);
+      let allVidArr = [];
+      if(videoInfoElm && (videoInfoElm.snapshotLength > 0)) {
+        let thisVideoInfoElement = videoInfoElm.snapshotItem(0);
+        let totalVideoCount = thisVideoInfoElement.hasAttribute('data-video-count') ? thisVideoInfoElement.getAttribute('data-video-count') : '';
+        totalVideoCount = parseInt(totalVideoCount);
+        for(let i = 0; i < totalVideoCount; i++) {
+          let thisUrl = thisVideoInfoElement.hasAttribute(`data-video-${i}-url`) ? thisVideoInfoElement.getAttribute(`data-video-${i}-url`) : '';
+          console.log(thisUrl);
+          allVidArr.push(thisUrl);
+        }
+  
+      } else {
+        console.log('we do not have the video info element', videoDivInfoElmXpath);
+      }
+      return allVidArr;
+    },
+    videoDivInfoElmXpath);
+  } catch(err) {
+    console.log('issue while getting videos', err.message);
+  }
+
+  if(allVideos.length > 0) {
+    await context.evaluate(async (allVideos) => {
+      async function addElementToDocumentAsync (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        document.body.appendChild(catElement);
+      }
+
+      console.log(allVideos.join(' || '));
+      await addElementToDocumentAsync('galleryvideos', allVideos.join(' || '));
+    },
+    allVideos);
+  } else {
+    console.log('no videos -- found');
+  }
+
   // await new Promise((resolve) => setTimeout(resolve, 10000));
   return await context.extract(productDetails, { transform });
 }
