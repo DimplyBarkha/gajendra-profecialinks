090185b3829d89b270db0fd9198c577386f48299
venkatsn <venkat.sn-odc@partner.import.io>
2021-01-12 22:47:21 +0530

    #2639

--- src/library/product/search/stores/m/medpex/DE/extract.js
+++ src/library/product/search/stores/m/medpex/DE/extract.js
@@ -11,13 +11,20 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(() => {
-      function addHiddenDiv (id, content, index) {
+      function addHiddenDiv(id, content, index) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
         newDiv.textContent = content;
         newDiv.style.display = 'none';
         const originalDiv = document.querySelectorAll('div#product-list div.product-list-entry.data-tracking-product form')[index];
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      }
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
       }
       const product = document.querySelectorAll('div#product-list div.data-tracking-product');
       const URL = window.location.href;
@@ -29,13 +36,19 @@ module.exports = {
         // @ts-ignore
         const productUrl = product[i].querySelector('span.product-name b a').href;
         // @ts-ignore
-        const price=document.querySelectorAll('span.normal-price')[i].innerText;
+        const price = document.querySelectorAll('span.normal-price')[i].innerText;
         addHiddenDiv('pd_productUrl', productUrl, i);
         addHiddenDiv('pd_url', URL, i);
         addHiddenDiv('pd_aggregateRating', aggrating, i);
-        addHiddenDiv('price',price,i)
+        addHiddenDiv('price', price, i)
 
       }
+      const URL1 = window.location.href;
+      try {
+        document.getElementById('pd_url').remove();
+      } catch (error) {
+      }
+      addElementToDocument('pd_url', URL1);
     });
     return await context.extract(productDetails, { transform });
   },
