bab3318738b5d99dfbf4c9d04371aaba4f83ecb8
wasiqe-a <wasiqe.a@osmosys.asia>
2021-01-12 21:38:26 +0530

    Adding custom fields

--- src/library/product/details/stores/m/mediaworld/IT/extract.js
+++ src/library/product/details/stores/m/mediaworld/IT/extract.js
@@ -1,4 +1,4 @@
-const { cleanUp } = require('../../../../shared');
+const { cleanUp } = require('../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -51,6 +51,84 @@ module.exports = {
     } catch (error) {
       console.log('error');
     }
+    const iframeLink = 'iframe[id="eky-dyson-iframe"]';
+
+    const optionalWait = async (sel) => {
+      try {
+        await context.waitForSelector(sel, { timeout: 20000 });
+      } catch (err) {
+        console.log(`Couldn't load selector => ${sel}`);
+      }
+    };
+
+    await optionalWait(iframeLink);
+
+    const isSelectorAvailable = async (iframeLink) => {
+      console.log(`Is selector available: ${iframeLink}`);
+      return await context.evaluate(function (iframeLink) {
+        return !!document.querySelector(iframeLink);
+      }, iframeLink);
+    };
+    const selectorAvailable = await isSelectorAvailable(iframeLink);
+
+    const getSrc = async (iframeLink) => {
+      console.log(`Is selector available: ${iframeLink}`);
+      return await context.evaluate(function (iframeLink) {
+        console.log(document.querySelector(iframeLink).getAttribute('src'));
+        return document.querySelector(iframeLink).getAttribute('src');
+      }, iframeLink);
+    };
+
+    if (selectorAvailable) {
+      const getData = await getSrc(iframeLink);
+      console.log('IFRAME SOURCE', getData);
+
+      const currentPage = await context.evaluate(function () { return window.location.href; });
+      const timeout = 30000;
+      await context.goto(getData, { timeout, waitUntil: 'load', checkBlocked: true });
+      await context.waitForNavigation({ timeout: 10000, waitUntil: 'load' });
+      const enhancedContentInfo = await context.evaluate(function () {
+        const imageEls = document.querySelectorAll('div.eky-accessory img');
+        const text = document.querySelectorAll('div.eky-accessory div');
+        const images = [];
+        const content = [];
+        Array.from(imageEls).forEach(el => {
+          // @ts-ignore
+          images.push(el.src);
+        });
+
+        Array.from(text).forEach(el => {
+          content.push(el.textContent);
+        });
+
+        return { images, content };
+      });
+
+      console.log('Navigating back to product page.');
+      await context.goto(currentPage, { timeout, waitUntil: 'load', checkBlocked: true });
+      await context.waitForNavigation({ timeout: 10000, waitUntil: 'networkidle0' });
+
+      await context.evaluate(function (content) {
+        try {
+          let text = '';
+          let images = '';
+          content.content.forEach(t => {
+            text += t ? `${t} ||` : t;
+          });
+          content.images.forEach(t => {
+            images += t ? ` || ${t}` : t;
+          });
+          console.log('Adding content to body');
+          const body = document.querySelector('body');
+          body.setAttribute('images', images);
+          const element = document.querySelector('div');
+          element.setAttribute('text', text);
+        } catch (error) {
+          console.log(`error while appending the data ${JSON.stringify(error)}`);
+        }
+      }, enhancedContentInfo);
+    }
+
     await context.evaluate(async function () {
       function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
