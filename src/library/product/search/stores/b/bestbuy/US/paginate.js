637bf4a53ee10e7636f649bc9e5b89b600c05345
Akansha Wajge <akansha.wajge@synerzip.com>
2021-01-12 16:07:40 +0530

    pagination issue fixed

--- src/library/product/search/stores/b/bestbuy/US/paginate.js
+++ src/library/product/search/stores/b/bestbuy/US/paginate.js
@@ -4,14 +4,16 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'bestbuy',
-    // nextLinkSelector: 'a.sku-list-page-next[aria-disabled="false"]',
+    nextLinkXpath: '//a[contains(@class,"sku-list-page-next") and contains(@aria-disabled,"false")]',
+    nextLinkSelector: 'a.sku-list-page-next[aria-disabled="false"]',
+    //nextLinkSelector: 'a.sku-list-page-next',
     mutationSelector: null, //"ol.paging-list",
     spinnerSelector: null,
     loadedSelector: 'ol.sku-item-list',
     noResultsXPath: "//div[contains(@class,'no-result')] | //h1[@class='heading VPT-title' and text()='We’re sorry, something went wrong.']",
-    openSearchDefinition: {
-      template: 'https://www.bestbuy.com/site/searchpage.jsp?st={searchTerms}&cp={page}',
-    },
+    // openSearchDefinition: {
+    //   template: 'https://www.bestbuy.com/site/searchpage.jsp?st={searchTerms}&cp={page}',
+    // },
     domain: 'bestbuy.com',
     zipcode: '',
   },
