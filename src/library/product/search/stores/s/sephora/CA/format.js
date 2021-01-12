e24eca23c39f130f1be12a25c29268dc79b8eaf6
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-01-12 19:22:20 +0530

    fixing UAT issue

--- src/library/product/search/stores/s/sephora/CA/format.js
+++ src/library/product/search/stores/s/sephora/CA/format.js
@@ -3,49 +3,56 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {  
-    const cleanUp = (data, context) => {
-      const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const state = context.getState();
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
+  for (const { group } of data) {
+    for (const row of group) {
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+      Object.keys(row).forEach(header => row[header].forEach(el => {
         el.text = clean(el.text);
-      }))));
-      return data;
-    };
-    for (const { group } of data) {
-      var rank = 1;
-      for (let row of group) {      
-        if(row.thumbnail){
-            row.thumbnail.forEach(item=>{
-                item.text="https://www.sephora.com/"+item.text;
-            })
-        }
-        if(row.id){
-            row.id.forEach(item=>{
-                let idAr=item.text.split(' ');
-                item.text=idAr[1];
-            })
-        }
-        if(row.aggregateRating){
-            row.aggregateRating.forEach(item=>{
-                item.text=item.text.replace(' stars','');
-            })
-        }         
-        row.rank = [{ "text": rank }];
-        row.rankOrganic = [{ "text": rank }];
-        rank++;
+      }));
+      if(row.thumbnail){
+        row.thumbnail.forEach(item=>{
+            item.text="https://www.sephora.com/"+item.text;
+        })
+      }
+      if(row.id){
+          row.id.forEach(item=>{
+              let idAr=item.text.split(' ');
+              item.text=idAr[1];
+          })
+      }
+      if(row.aggregateRating){
+          row.aggregateRating.forEach(item=>{
+              item.text=item.text.replace(' stars','');
+          })
       }
     }
-    return cleanUp(data);
-  };
-  module.exports = { transform };
+  }
+  context.setState({ rankCounter });
+  context.setState({ orgRankCounter });
+  context.setState({ productCodes });
+  console.log(productCodes);
+  return data;
+};
+module.exports = { transform };
