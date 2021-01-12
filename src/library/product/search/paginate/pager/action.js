a64c1708a44253d4d45ea4121409d1d4d90d6d87
Rekha Takalkar <rekha.takalkar@profecialinks.com>
2021-01-12 10:26:06 +0530

    updated files

--- src/library/product/search/paginate/pager/action.js
+++ src/library/product/search/paginate/pager/action.js
@@ -47,7 +47,7 @@ async function implementation (
 
   if (nextLinkSelector) {
     console.log('Clicking', nextLinkSelector);
-    await context.clickAndWaitForNavigation(nextLinkSelector, {}, { timeout: 20000 });
+    await context.clickAndWaitForNavigation(nextLinkSelector, {}, { timeout: 200000 });
     if (loadedSelector) {
       await context.waitForSelector(loadedSelector, { timeout: 20000 });
     }
