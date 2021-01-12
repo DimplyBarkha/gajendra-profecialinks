6bcf07e7e7832f01d0ffadddc4821a8f4fa7d317
Sephali-23 <sephalisingh023@gmail.com>
2021-01-12 10:23:51 +0530

    Added custom fields

--- src/library/product/details/stores/h/haar-shop/CH/extract.js
+++ src/library/product/details/stores/h/haar-shop/CH/extract.js
@@ -9,4 +9,22 @@ module.exports = {
     domain: 'haar-shop.ch',
     zipcode: "''",
   },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { productDetails } = dependencies;
+    const { transform } = parameters;
+
+    try {
+      await context.waitForSelector('div[id="inpage_container"] div[class="inpage_selector_InTheBox"] span', { timeout: 30000 });
+      console.log('selector of inTheBox exist');
+    } catch (e) {
+      console.log("selector of inTheBox doesn't exist");
+    }
+    return await context.extract(productDetails, { transform });
+  
+  },
 };
