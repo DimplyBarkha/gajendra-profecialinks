72f9cf5816e5f82d2bdde9eac0ab9a4b41b231e9
venkatsn <venkat.sn-odc@partner.import.io>
2021-01-12 15:16:14 +0530

    #2639

--- src/library/product/search/stores/m/medpex/DE/paginate.js
+++ src/library/product/search/stores/m/medpex/DE/paginate.js
@@ -3,13 +3,13 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'medpex',
-    nextLinkSelector: null,//'#searchresult > div.pagenav > table > tbody > tr > :last-child a',
+    nextLinkSelector: '#searchresult > div.pagenav > table > tbody > tr > :last-child a',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div #main',
-    openSearchDefinition: {
-     template: 'https://www.medpex.de/search.do?q={searchTerms}&pn={page}',
-    },
+    // openSearchDefinition: {
+    //  template: 'https://www.medpex.de/search.do?q={searchTerms}&pn={page}',
+    // },
     domain: 'medpex.de',
   },
 };
