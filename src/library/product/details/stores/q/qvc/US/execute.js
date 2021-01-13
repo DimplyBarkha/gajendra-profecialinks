1b143935ed5189c231af6fcce905883d07b58c37
astropaula <wojciechowska.fran@gmail.com>
2021-01-13 16:18:32 +0100

    added id input llogic

--- src/library/product/details/stores/q/qvc/US/execute.js
+++ src/library/product/details/stores/q/qvc/US/execute.js
@@ -1,4 +1,30 @@
+const implementation = async ({ url, id, zipcode, storeId }, { loadedSelector, noResultsXPath }, context, dependencies) => {
+  if (id) {
+    id = id.toString().split('-')[0];
+    await context.goto('https://www.qvc.com/');
+    await context.setInputValue('input#txtMastheadSearch', id);
+    await context.click('input[id="btnMastheadSearch"]');
+    await context.waitForNavigation();
+    url = await context.evaluate(async () => { return window.location.href; });
+  }
+  
+  if (!url) {
+    if (!id) throw new Error('No id provided');
+  }
+  await dependencies.goto({ url, zipcode, storeId });
 
+  if (loadedSelector) {
+    await context.waitForFunction(
+      (selector, xpath) => {
+        return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
+      },
+      { timeout: 10000 },
+      loadedSelector,
+      noResultsXPath,
+    );
+  }
+  return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
+};
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -9,4 +35,5 @@ module.exports = {
     noResultsXPath: '//div[@class="col-tn-12"]/ul/li | //li[@class="active"]/h1[contains(text(), "Sold Out")]',
     zipcode: '',
   },
+  implementation,
 };
