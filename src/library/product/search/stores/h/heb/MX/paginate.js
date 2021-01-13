44a18b0e0cf7bbcc38278a672e64a4a77e1f3c23
Jagruti Chaudhari <jagruti.chaudhary@od.import.io>
2021-01-13 21:18:35 +0530

    heb_mx

--- src/library/product/search/stores/h/heb/MX/paginate.js
+++ src/library/product/search/stores/h/heb/MX/paginate.js
@@ -4,13 +4,15 @@ module.exports = {
   parameterValues: {
     country: 'MX',
     store: 'heb',
-    nextLinkSelector: null,
+    //nextLinkSelector: 'li[class="item pages-item-next"] a[class="action next"]',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'body',
     noResultsXPath: null,
     resultsDivSelector: null,
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.heb.com.mx/catalogsearch/result/index/?p=page&q={searchTerms}',
+      },
     domain: 'heb.com',
     zipcode: '',
   },
