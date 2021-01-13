2ef6cdd31a450a9d4a8f7b784f82c776f3ee0403
xeilen <xeilen1112@gmail.com>
2021-01-13 13:04:49 +0200

    -added variant extractor

---
+++ src/library/product/details/stores/b/bcc/NL/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'NL',
+    store: 'bcc',
+    transform: null,
+    domain: 'bcc.nl',
+    zipcode: '',
+  },
+};
