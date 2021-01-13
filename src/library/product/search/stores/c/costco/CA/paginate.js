5d34cabb169f92de368d03db6893c74343b20a26
Sheetal Bharara <SheetalB@hcl.com>
2021-01-13 14:19:27 +0530

    Fixes for Rank issue

--- src/library/product/search/stores/c/costco/CA/paginate.js
+++ src/library/product/search/stores/c/costco/CA/paginate.js
@@ -4,11 +4,11 @@ module.exports = {
   parameterValues: {
     country: 'CA',
     store: 'costco',
-    nextLinkSelector: null,
+    nextLinkSelector: 'li.forward a i.co-arrow-right',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div.product-list',
+    noResultsXPath: '//h1[contains(text(),"Try another search")]',
     openSearchDefinition: {
       template: 'https://www.costco.ca/CatalogSearch?currentPage={page}&dept=All&keyword={searchTerms}',
     },
