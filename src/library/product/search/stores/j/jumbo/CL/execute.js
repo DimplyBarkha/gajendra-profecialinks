9aca7c9ca32ee4047183e36ac9625f18b18f9f0a
adrian_swiderek <adrian.swiderek@tdcm.io>
2021-01-13 14:59:46 +0100

    jumbo CL search - 'Could not load webpage. Response: 404' error fix

--- src/library/product/search/stores/j/jumbo/CL/execute.js
+++ src/library/product/search/stores/j/jumbo/CL/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'CL',
     store: 'jumbo',
     domain: 'jumbo.cl',
-    url: 'https://www.jumbo.cl/busca?ft={searchTerms}',
+    url: 'https://www.jumbo.cl/busqueda?ft={searchTerms}',
     loadedSelector: 'ul.shelf-list',
     noResultsXPath: '//div[contains(@class, "error-404-empty-message")]',
     zipcode: '',
