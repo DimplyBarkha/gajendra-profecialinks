a57168b2b29d316fcde881ba46a1fe9f8c7595eb
Guillaume-Vial <61331399+Guillaume-Vial@users.noreply.github.com>
2021-01-13 14:18:13 +0000

    Update action.js
    Added captureRequests: true

--- src/library/navigation/goto/action.js
+++ src/library/navigation/goto/action.js
@@ -42,7 +42,7 @@ module.exports = {
   implementation: async (inputs, parameters, context, dependencies) => {
     const { timeout = 10000 } = parameters;
     const { url, zipcode, storeId } = inputs;
-    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true, captureRequests: true });
 
     // patch for synchronicity issue between json decoring and goto result
     if (url.split('[!opt!]')[1] && url.split('[!opt!]')[1].includes('"type":"json"')) {
