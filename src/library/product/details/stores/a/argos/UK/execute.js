d1376119d71932522b13b34f5accbf1cf30c2c5b
Venkata Akhilesh P <akhilesh.p@partner.import.io>
2021-01-13 16:23:07 +0530

    Handling null URL input

--- src/library/product/details/stores/a/argos/UK/execute.js
+++ src/library/product/details/stores/a/argos/UK/execute.js
@@ -1,3 +1,32 @@
+const implementation = async (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) => {
+  let { url, id, zipcode } = inputs;
+  if (!url) {
+    if (!id) {
+      throw new Error('no id provided');
+    }
+    url = await dependencies.createUrl({ id });
+  }
+  if((url === "null") || (!url)) url = `https://www.argos.co.uk/product/${id}`;
+  await dependencies.goto({ url, zipcode });
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 20000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+};
 
 module.exports = {
   implements: 'product/details/execute',
@@ -9,4 +38,5 @@ module.exports = {
     noResultsXPath: '//h1[@data-test="error-page-message-title"]',
     zipcode: 'SE19PD',
   },
+  implementation,
 };
