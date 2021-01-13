291345cf1cf6cf6ddd59739bdbb7584e22f82f79
Sheetal Bharara <SheetalB@hcl.com>
2021-01-13 19:14:41 +0530

    fixes for searchURL

--- src/library/product/search/stores/c/costco/CA/extract.js
+++ src/library/product/search/stores/c/costco/CA/extract.js
@@ -12,6 +12,19 @@ async function implementation (
   } catch (err) {
     console.log('Something happened while accepting the modal button.');
   }
+  async function addUrl () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const url = window.location.href;
+    addHiddenDiv('added-searchurl', url);
+  }
+  await context.evaluate(addUrl);
+
   const { transform } = parameters;
   const { productDetails } = dependencies;
   return await context.extract(productDetails, { transform });
