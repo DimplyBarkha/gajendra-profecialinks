f9e90812a4516249dd726ae1b9ea4d47c046b275
Ajinkya Kathe <ajinkya.kathe@synerzip.com>
2021-01-14 22:00:39 +0530

    Solved kick back issue - for gitin and product description

--- src/library/product/details/stores/e/epocacosmeticos/BR/extract.js
+++ src/library/product/details/stores/e/epocacosmeticos/BR/extract.js
@@ -1,5 +1,5 @@
 const { transform } = require('../format');
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
@@ -12,18 +12,29 @@ async function implementation (
   });
   if (variantLength > 1) {
     // await preparePageForCommonElement(0, variantLength);
-    await context.evaluate(async () => {
+    const gtinArray = await context.evaluate(async () => {
       const skuDiv = document.createElement('div');
+      let gtinArray = [];
       skuDiv.id = 'selectedSKU';
       document.body.appendChild(skuDiv);
+      const gtin = document.createElement('div');
+      gtin.id = 'gtin-data';
+      document.body.appendChild(gtin);
+      const scriptData = document.evaluate('//script[contains(text(), "vtex.events.addData")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (scriptData) {
+        gtinArray = JSON.parse(scriptData.innerText.match(/\"productEans":(.*),"skuStocks/)[1]);
+      }
+      return gtinArray
     });
+
     for (let j = 0; j < variantLength; j++) {
-      await context.evaluate(async (j) => {
+      await context.evaluate(async (j, gtinArray) => {
         const sku = document.querySelectorAll('meta[itemprop="sku"]')[j].getAttribute('content');
-        console.log('sku====>', sku);
         document.querySelector('#selectedSKU').setAttribute('data-sku', sku);
+        document.querySelector('#gtin-data').setAttribute('data-gtin', gtinArray[j])
         return document.querySelectorAll('ul.topic li label')[j].click();
-      }, j);
+      }, j, gtinArray);
+      await new Promise((resolve, reject) => setTimeout(resolve, 3000));
       // await context.click(`ul.topic li label`);
       console.log('Inside variants', j);
       // await preparePage(j, variantLength);
@@ -31,11 +42,13 @@ async function implementation (
     }
   } else {
     await context.evaluate(async () => {
+
       const skuDiv = document.createElement('div');
       skuDiv.id = 'selectedSKU';
       document.body.appendChild(skuDiv);
       const sku = document.querySelector('meta[itemprop="sku"]').getAttribute('content');
-      console.log('sku====>', sku);
+      const gtinValue = document.querySelector('meta[itemprop="gtin13"]').getAttribute('content');
+      document.querySelector('#gtin-data').setAttribute('data-gtin', gtinValue)
       document.querySelector('#selectedSKU').setAttribute('data-sku', sku);
     });
   }
