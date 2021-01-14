4e8877a8bd8c7b2898f049eb12016e9c8066f95f
romancg <gromantsg@gmail.com>
2021-01-14 18:02:22 +0200

    #1209 update rank

--- src/library/product/search/stores/e/euronics/IT/extract.js
+++ src/library/product/search/stores/e/euronics/IT/extract.js
@@ -5,30 +5,20 @@ module.exports = {
   parameterValues: {
     country: 'IT',
     store: 'euronics',
-    transform: null,
+    transform,
     domain: 'euronics.it',
     zipcode: '',
   },
-  implementation: async (inputs, parameters, context, dependencies) => {
+  implementation: async (
+      inputs,
+      parameters,
+      context,
+      dependencies,
+  ) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
-    await context.evaluate(() => {
-      const url = new URL(window.location.href);
-      const p = url.searchParams.get('p');
-      const page = p || 1;
-      const product = document.querySelectorAll('div.productCard.j-productCard');
-      const pi = 16;
-      for (let i = 0; i < product.length; i++) {
-        const rank = (page * pi) - pi + i;
-        product[i].querySelector('#log_position').setAttribute('rank', rank);
-        product[i].querySelector('#log_position').setAttribute('organic', rank);
-      }
-      const productOrganic = document.querySelectorAll('.productCard__content>div.productCard.j-productCard');
-      const po = 12;
-      for (let i = 0; i < productOrganic.length; i++) {
-        const rankOrganic = (page * po) - po + i;
-        productOrganic[i].querySelector('#log_position').setAttribute('organic', rankOrganic);
-      }
+    await context.evaluate(async function () {
+
     });
     return await context.extract(productDetails, { transform });
   },
