0af4c918603b0559a8ad306e3ccd712ef33e6ea6
astropaula <wojciechowska.fran@gmail.com>
2021-01-14 17:18:02 +0100

    Added sku and gtin data extraction

--- src/library/product/search/stores/b/boozt/DK/extract.js
+++ src/library/product/search/stores/b/boozt/DK/extract.js
@@ -8,6 +8,16 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+  const info = await context.evaluate(async function () {
+    const info = document.querySelector('script[type="application/ld+json"]') ? JSON.parse(document.querySelector('script[type="application/ld+json"]').textContent) : '';
+    return info;
+  });
+  const skuArray = [];
+  const gtinArray = [];
+  for (let i = 0; i < info.itemListElement.length; i++) {
+    skuArray.push(info.itemListElement[i].item.sku);
+    gtinArray.push(info.itemListElement[i].item.gtin8);
+  };
   var data = await context.extract(productDetails, { transform });
   for (let i = 0; i < data[0].group.length; i++) {
     if (data[0].group[i].price) {
@@ -15,8 +25,13 @@ async function implementation (
         data[0].group[i].price[0].text = data[0].group[i].price[0].text.replace(/fra /, '');
       }
     }
+    if (data[0].group[i].id) {
+      data[0].group[i].id[0].text = skuArray[i];
+    }
+    if (data[0].group[i].gtin) {
+      data[0].group[i].gtin[0].text = gtinArray[i];
+    }
   }
-
   return data;
 }
 module.exports = {
