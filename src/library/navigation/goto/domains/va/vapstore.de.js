ca7c6954a369299866315ef41a07bce86be93992
Avinash <avinash.kadam@profecialinks.com>
2021-01-14 18:56:05 +0530

    goto url updated for review

--- src/library/navigation/goto/domains/va/vapstore.de.js
+++ src/library/navigation/goto/domains/va/vapstore.de.js
@@ -8,4 +8,19 @@ module.exports = {
     store: 'vapstore',
     zipcode: "''",
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    const { url, zipcode, storeId } = inputs;
+    if (!url.includes('navi.php')) {
+      let newUrl = url + '?ratings_nItemsPerPage=-1&ratings_nSortByDir=1#tab-votes';
+      await context.goto(newUrl, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    }
+    else {
+      await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    }
+    console.log(zipcode);
+    if (zipcode || storeId) {
+      await dependencies.setZipCode(inputs);
+    }
+  }
 };
