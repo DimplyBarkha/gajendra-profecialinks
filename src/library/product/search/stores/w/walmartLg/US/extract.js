074000d7575968201323820a9d78a7c54728dfe8
Shekhar Gupta <shekhar.gupta@hcl.com>
2021-01-14 12:01:23 +0530

    added walmart lg search

---
+++ src/library/product/search/stores/w/walmartLg/US/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'US',
+    store: 'walmartLg',
+    transform: null,
+    domain: 'walmart.com',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/search/stores/w/walmartLg/US/search.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search',
+  parameterValues: {
+    country: 'US',
+    store: 'walmartLg',
+    domain: 'walmart.com',
+    zipcode: '',
+    storeID: null,
+  },
+};
