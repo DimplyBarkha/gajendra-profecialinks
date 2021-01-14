ed9b9d24a43db0ae6fb78847859ae7d41167cbc2
vikas kyatannawar <vikas.k@osmosys.asia>
2021-01-14 20:16:47 +0530

    updated goto to handle captcha, refactored code from execute to createUrl

--- src/library/product/details/stores/w/waitrose/UK/createUrl.js
+++ src/library/product/details/stores/w/waitrose/UK/createUrl.js
@@ -1,4 +1,30 @@
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { id } = inputs;
+  if (parameters.url) {
+    const url = parameters.url.replace('{id}', encodeURIComponent(id));
+
+    await dependencies.goto({ url });
+    const detailsPage = await context.evaluate(async () => {
+      if (document.querySelector('div[id="introAgreeButton"] span[class="CwaK9"] span[class="RveJvd snByac"]') !== null) {
+        document.querySelector('div[id="introAgreeButton"] span[class="CwaK9"] span[class="RveJvd snByac"]').click();
+      }
+      if (document.querySelector('div[class="bErdLd aID8W wwYr3"]') !== null && document.querySelector('div[class="t7xA6 m114nf aID8W"]') !== null) {
+        document.querySelector('div[class="bErdLd aID8W wwYr3"]').remove();
+        document.querySelector('div[class="t7xA6 m114nf aID8W"]').remove();
+      }
+      if (document.querySelector('div[class="yuRUbf"] a') !== null) {
+        return document.querySelector('div[class="yuRUbf"] a').getAttribute('href');
+      }
+    });
+    return detailsPage || url;
+  }
+}
 module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
@@ -9,4 +35,8 @@ module.exports = {
     url: 'https://www.google.com/search?q=site:/www.waitrose.com%20{id}',
     zipcode: '',
   },
+  dependencies: {
+    goto: 'action:navigation/goto',
+  },
+  implementation,
 };
--- src/library/product/details/stores/w/waitrose/UK/execute.js
+++ src/library/product/details/stores/w/waitrose/UK/execute.js
@@ -1,45 +1,3 @@
-
-const implementation = async ({ url, id, zipcode, storeId }, { loadedSelector, noResultsXPath }, context, dependencies) => {
-  if (!url) {
-    if (!id) throw new Error('No id provided');
-    else url = await dependencies.createUrl({ id });
-  }
-  await dependencies.goto({ url, zipcode, storeId });
-  const captcha = await context.evaluate(async () => {return document.querySelector('div[title="We don\'t support frames at the moment."]')});
-  if (captcha) {
-    await context.solveCaptcha({
-      type: 'RECAPTCHA',
-      inputElement:'input[id="recaptcha-token"]',
-    });
-  }
-  const detailsPage = await context.evaluate(async () => {
-    if (document.querySelector('div[id="introAgreeButton"] span[class="CwaK9"] span[class="RveJvd snByac"]') !== null) {
-      document.querySelector('div[id="introAgreeButton"] span[class="CwaK9"] span[class="RveJvd snByac"]').click();
-    }
-    if (document.querySelector('div[class="bErdLd aID8W wwYr3"]') !== null && document.querySelector('div[class="t7xA6 m114nf aID8W"]') !== null) {
-      document.querySelector('div[class="bErdLd aID8W wwYr3"]').remove();
-      document.querySelector('div[class="t7xA6 m114nf aID8W"]').remove();
-    }
-    if (document.querySelector('div[class="yuRUbf"] a') !== null) {
-      return document.querySelector('div[class="yuRUbf"] a').getAttribute('href');
-    }
-  });
-  if (detailsPage) {
-    await context.goto(detailsPage);
-    await context.waitForNavigation();
-  }
-  if (loadedSelector) {
-    await context.waitForFunction(
-      (selector, xpath) => {
-        return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
-      },
-      { timeout: 10000 },
-      loadedSelector,
-      noResultsXPath,
-    );
-  }
-  return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
-};
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -50,5 +8,4 @@ module.exports = {
     noResultsXPath: '//button[@aria-label="Load more"] | //div[@class="noResults___2M4HP"] | //h1[contains(text(), "404 Not Found")] | //ul[@style="margin-left:1.3em;margin-bottom:2em"] | //h1[contains(text(), "Offer(s)")] | //pdf-viewer',
     zipcode: '',
   },
-  implementation,
 };
