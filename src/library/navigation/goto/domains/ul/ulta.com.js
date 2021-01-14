17082c7f9a57ad3410e2206fc481bcba8809e0d3
vikas kyatannawar <vikas.k@osmosys.asia>
2021-01-14 10:51:28 +0530

    updated goto

--- src/library/navigation/goto/domains/ul/ulta.com.js
+++ src/library/navigation/goto/domains/ul/ulta.com.js
@@ -8,13 +8,38 @@ module.exports = {
     store: 'ulta',
     zipcode: '',
   },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 90000;
+  // implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+  //   const timeout = parameters.timeout ? parameters.timeout : 90000;
+  //   await context.setBlockAds(false);
+  //   await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, cookies: [] });
+  //   console.log(zipcode);
+  //   if (zipcode) {
+  //     await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+  //   }
+  // },
+  implementation: async (inputs, parameterValues, context, dependencies) => {
+    let url = `${inputs.url}`;
     await context.setBlockAds(false);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, cookies: [] });
-    console.log(zipcode);
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+    await context.goto(url, { waitUntil: 'networkidle0', block_ads: false });
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
     }
+    await autoScroll(context);
   },
 };
