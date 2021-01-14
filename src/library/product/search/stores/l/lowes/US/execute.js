0a766a32d31c6a7314b8060030cd8e0633d6f64d
shrutikab <shrutika.bongarde@synerzip.com>
2021-01-14 19:27:26 +0530

    Added changes(#1100)

--- src/library/product/search/stores/l/lowes/US/execute.js
+++ src/library/product/search/stores/l/lowes/US/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'lowes',
     domain: 'lowes.com',
     url: 'https://www.lowes.com/search?searchTerm={searchTerms}',
-    loadedSelector: 'div.pl>div',
-    noResultsXPath: '//h1[contains(text(),"NO RESULTS FOUND FOR")] | //div[@id="mainContent"] | //div[contains(@class , "ProductDetailsWrapper")] | //div[contains(@class ,"grid-container")]',
+    loadedSelector: 'section#main',
+    noResultsXPath: '//h1[contains(text(),"NO RESULTS FOUND FOR")] | //div[@id="mainContent"] | //div[contains(@class ,"grid-container")]',
   },
 };
