e7a84ccefbfd6672b926215971d0d81dbabffd20
adheer <sangeta.kumari@synerzip.com>
2021-01-15 00:00:26 +0530

    kickback issues

--- src/library/navigation/goto/domains/be/belezanaweb.com.br.js
+++ src/library/navigation/goto/domains/be/belezanaweb.com.br.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'belezanaweb.com.br',
-    timeout: null,
+    timeout: 30000,
     country: 'BR',
     store: 'belezanaweb',
     zipcode: '',
--- src/library/product/search/stores/b/belezanaweb/BR/extract.js
+++ src/library/product/search/stores/b/belezanaweb/BR/extract.js
@@ -1,4 +1,4 @@
-const {transform} = require('../format')
+const { transform } = require('../format');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -7,5 +7,56 @@ module.exports = {
     transform,
     domain: 'belezanaweb.com.br',
     zipcode: '',
+  },
+  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
+    await context.evaluate(async () => {
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(3000);
+        scrollTop += 500;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(100);
+          break;
+        }
+      }
+      const moreButton = document.evaluate('//button[contains(@class,"btn-load-more")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+      console.log('moreButton:: ', moreButton.singleNodeValue);
+      if (moreButton && moreButton.singleNodeValue != null) {
+        let index = 0;
+        while (index < 6) {
+          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+          try {
+            moreButton.singleNodeValue.click();
+            // }catch(e) {}
+            // await new Promise((resolve, reject) => setTimeout(resolve, 500));
+            let scrollTop = 0;
+            while (scrollTop !== 10000) {
+              await stall(500);
+              scrollTop += 1000;
+              window.scroll(0, scrollTop);
+              if (scrollTop === 10000) {
+                await stall(500);
+                break;
+              }
+            }
+          } catch (e) {}
+          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+          // let scrollTop = 0;
+          // index ++;
+          //const scrollTop = 0;
+          index++;
+        }
+      }
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
