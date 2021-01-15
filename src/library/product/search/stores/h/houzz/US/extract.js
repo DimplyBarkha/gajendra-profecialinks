ea3b7727597c4462200aca8a369b59693514cd71
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2021-01-15 15:17:18 +0530

    changes made according to QA

--- src/library/product/search/stores/h/houzz/US/extract.js
+++ src/library/product/search/stores/h/houzz/US/extract.js
@@ -24,27 +24,33 @@ module.exports = {
           ".btn.btn-none.hz-universal-search-header-tip__dismiss"
         ) != null
       ) {
-        document
-          .querySelector(
-            ".btn.btn-none.hz-universal-search-header-tip__dismiss"
-          )
-          .click();
+        try{
+          document.querySelector(".btn.btn-none.hz-universal-search-header-tip__dismiss").click();
+        }catch(err){
+          console.log(err)
+        }
+        
       }
       if (
         document.querySelector(
           "body > div.hz-modal.hz-modal--dark > div.hz-trap-focus.hz-modal__container > div > div.hz-modal__body > div > div.hz-international-redirect-modal__btns > div.hz-international-redirect-modal__continue-btn > button"
         ) != null
       ) {
-        document
+        try{
+          document
           .querySelector(
             "body > div.hz-modal.hz-modal--dark > div.hz-trap-focus.hz-modal__container > div > div.hz-modal__body > div > div.hz-international-redirect-modal__btns > div.hz-international-redirect-modal__continue-btn > button"
           )
           .click();
+        }catch(err){
+          console.log(err)
+        }
+
       }
       let scrollTop = 500;
       while (true) {
         window.scroll(0, scrollTop);
-        stall(1000);
+        stall(500);
         scrollTop += 500;
         if (scrollTop === 10000) {
           break;
@@ -61,11 +67,14 @@ module.exports = {
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
       var link = document.getElementsByClassName("hz-product-card__link")
-      for (let i = 0; i < link.length; i++) {
-        console.log("Loop is working");
-        const searchURL = window.location.href.split("?")[0]
-        addHiddenDiv("ii_searchURL", searchURL, i);
+      if (link != null){
+        for (let i = 0; i < link.length; i++) {
+          console.log("Loop is working");
+          const searchURL = window.location.href.split("?")[0]
+          addHiddenDiv("ii_searchURL", searchURL, i);
+        }
       }
+
     });
     return await context.extract(productDetails, { transform });
   }
