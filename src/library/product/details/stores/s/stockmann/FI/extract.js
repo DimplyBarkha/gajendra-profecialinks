90b30a227b9d1be5b65beabd0b3af6e6872e8202
vikas kyatannawar <vikas.k@osmosys.asia>
2021-01-15 18:03:01 +0530

    updated goto

--- src/library/product/details/stores/s/stockmann/FI/extract.js
+++ src/library/product/details/stores/s/stockmann/FI/extract.js
@@ -9,31 +9,4 @@ module.exports = {
     domain: 'stockmann.com',
     zipcode: '',
   },
-  implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
-     const applyScroll = async function (context) {
-    await context.evaluate(async function () {
-      let scrollTop = 0;
-      while (scrollTop !== 20000) {
-        scrollTop += 500;
-        window.scroll(0, scrollTop);
-        await stall(1500);
-      }
-      function stall(ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-    });
-    try {
-      await context.waitForSelector('class="product-slider"', { timeout: 200000 });
-  } catch (error) {
-      console.log("Manufacturer Description did not loaded....");
-  }
-  };
-    await applyScroll(context);
-    return await context.extract(productDetails, { transform });
-  },
-  
 };
