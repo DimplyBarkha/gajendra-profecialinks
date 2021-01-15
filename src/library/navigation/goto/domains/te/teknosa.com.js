81264277715719573d038b57490d46da88787c40
Mamatha-Gundapaneni <mamatha.g@osmosys.asia>
2021-01-15 14:24:48 +0530

    Updated code

--- src/library/navigation/goto/domains/te/teknosa.com.js
+++ src/library/navigation/goto/domains/te/teknosa.com.js
@@ -10,9 +10,10 @@ module.exports = {
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
     url = `${url}#[!opt!]{"first_request_timeout":50000,"force200":true}[/!opt!]`;
+    await context.captureRequests();
     context.setBlockAds(false);
     context.setLoadAllResources(true);
     context.setAntiFingerprint(false);
-    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true });
+    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true, load_timeout: 0 });
   },
 };
--- src/library/product/details/stores/t/teknosa/TR/extract.js
+++ src/library/product/details/stores/t/teknosa/TR/extract.js
@@ -41,6 +41,26 @@ module.exports = {
         }
       }
     });
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop <= 20000) {
+      await stall(500);
+      scrollTop += 1000;
+      window.scroll(0, scrollTop);
+      if (scrollTop === 20000) {
+      await stall(8000);
+      break;
+      }
+      }
+      function stall (ms)
+       {
+      return new Promise(resolve => {
+      setTimeout(() => {
+      resolve();
+      }, ms);
+      });
+      }
+      });
     await context.extract(productDetails, { transform });
   },
 };
