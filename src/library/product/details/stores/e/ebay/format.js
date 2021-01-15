f00ba5f4e4ab7801cb730ba3679f912445d5ecd4
srishivams <srishivams97@gmail.com>
2021-01-15 15:47:44 +0530

    fixed specifications

--- src/library/product/details/stores/e/ebay/format.js
+++ src/library/product/details/stores/e/ebay/format.js
@@ -7,15 +7,25 @@ const transform = (data) => {
   for (const { group }
     of data) {
     for (const row of group) {
+      const uniqueArr = [];
       if (row.specifications) {
+        for (let i = 0; i < row.specifications.length; i++) {
+          if (!uniqueArr.includes(row.specifications[i].text)) uniqueArr.push(row.specifications[i].text);
+          // row.specifications=null;
+          // row.specifications=uniqueArr;
+        }
         // for(let i=0;i<row.specifications.length;i++){
         //   if(i!==0){
         //   if(row.specifications[i].text===row.specifications[i-1].text)
         //     row.specifications.splice(i,i+1);
         //   }
         // }
+
+        // row.specifications = [{
+        //   text: row.specifications.reduce((item, currItem) => item ? `${item} || ${currItem.text.replace(/:(\s*\n\s*)+/g, ': ').replace(/(\s*\n\s*)+/, ' || ')}` : currItem.text.replace(/:(\s*\n\s*)/g, ': ').replace(/(\s*\n\s*)+/, ' || '), ''),
+        // }];
         row.specifications = [{
-          text: row.specifications.reduce((item, currItem) => item ? `${item} || ${currItem.text.replace(/:(\s*\n\s*)+/g, ': ').replace(/(\s*\n\s*)+/, ' || ')}` : currItem.text.replace(/:(\s*\n\s*)/g, ': ').replace(/(\s*\n\s*)+/, ' || '), ''),
+          text: uniqueArr.reduce((item, currItem) => item ? `${item} || ${currItem.replace(/:(\s*\n\s*)+/g, ': ').replace(/(\s*\n\s*)+/, ' || ')}` : currItem.replace(/:(\s*\n\s*)/g, ': ').replace(/(\s*\n\s*)+/, ' || '), ''),
         }];
       }
       if (row.unInterruptedPDP) {
