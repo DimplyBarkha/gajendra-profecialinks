bbb8871df1df19a1c85310645837c951c5a350a2
osmakash <akashkumar.p@osmosys.asia>
2021-01-15 12:34:24 +0530

    adding custom fields

--- src/library/product/details/stores/s/stockmann/FI/extract.js
+++ src/library/product/details/stores/s/stockmann/FI/extract.js
@@ -1,4 +1,19 @@
 const { transform } = require('./format');
+async function implementation (
+  inputs,
+  { transform: transformParam },
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+  try {
+    await context.click('body > div.cookie-dialog-screen.open > div > div > div.buttons > button');
+    console.log("buttton clicked")
+    }  catch(err) {
+     console.log("couldn't click");
+    }
+  return await context.extract(productDetails, {transform});
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,4 +23,5 @@ module.exports = {
     domain: 'stockmann.com',
     zipcode: '',
   },
+  implementation,
 };
