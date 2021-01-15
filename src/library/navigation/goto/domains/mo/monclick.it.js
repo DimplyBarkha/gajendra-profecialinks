abef56cd45b459684693370fccbb4b20a9a906cf
osmarghyadeep <arghyadep.g@partner.import.io>
2021-01-16 00:00:19 +0530

    Updated the code to get weightNet issue

--- src/library/navigation/goto/domains/mo/monclick.it.js
+++ src/library/navigation/goto/domains/mo/monclick.it.js
@@ -9,8 +9,15 @@ module.exports = {
     },
     implementation: async(inputs, parameterValues, context, dependencies) => {
         let url = `${inputs.url}`;
+        await context.setBlockAds(false);
+        await context.setLoadAllResources(true);
+        await context.setLoadImages(true);
+        await context.setJavaScriptEnabled(true);
+        await context.setAntiFingerprint(false);
+        await context.setUseRelayProxy(false);
 
-        url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+        // url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
         await context.goto(url, { timeout: parameterValues.timeout });
+        await new Promise(resolve => setTimeout(resolve, 15000));
     },
 };
--- src/library/product/details/stores/m/monclick/IT/extract.js
+++ src/library/product/details/stores/m/monclick/IT/extract.js
@@ -9,10 +9,69 @@ module.exports = {
         zipcode: '',
     },
     implementation: async({ url }, { country, domain, transform }, context, { productDetails }) => {
-        try {
+        
+        await new Promise(resolve => setTimeout(resolve, 5000));
+            await context.evaluate(async() => {
+                async function infiniteScroll () {
+                    let prevScroll = document.documentElement.scrollTop;
+                    while (true) {
+                        window.scrollBy(0, document.documentElement.clientHeight);
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                        const currentScroll = document.documentElement.scrollTop;
+                        if (currentScroll === prevScroll) {
+                        break;
+                        }
+                        prevScroll = currentScroll;
+                    }
+                }
+                await infiniteScroll();
+            });
             await new Promise(resolve => setTimeout(resolve, 5000));
-            await context.click('div[class*="image-content-gallery"] li[class*="gallery-video"]');
-            await context.waitForSelector('div[class*=product-gallery-overlay]');
+            try {
+                await context.click('div[class*="image-content-gallery"] li[class*="gallery-video"]');
+                await context.waitForSelector('div[class*=product-gallery-overlay]');
+            } catch (e) {
+                console.log(e.message);
+            }
+            
+            try {
+                await context.waitForSelector('.syndigo-shadowed-powerpage');
+            } catch(err) {
+                console.log('got some error while waiting', err.message);
+                console.log('waiting again!!');
+                try {
+                    await context.waitForSelector('.syndigo-shadowed-powerpage');
+                } catch(err) {
+                    console.log('got some error while waiting -- again', err.message);
+                }
+            }
+            await context.evaluate(async() => {
+                let parentDiv = document.querySelectorAll('div[class*="syndi_powerpage"]')[0];
+                let isShadowRootLoaded = false;
+                let maxWait = 60000;
+                let thisTime = 0;
+                while(!isShadowRootLoaded) {
+                    parentDiv = document.querySelectorAll('div[class*="syndi_powerpage"]')[0];
+                    if(parentDiv && parentDiv.shadowRoot) {
+                        isShadowRootLoaded = true;
+                        console.log('found shadowRoot');
+                    } else {
+                        thisTime += 2000;
+                    }
+                    if(thisTime > maxWait) {
+                        console.log('waited for a long time!!');
+                        break;
+                    }
+                    await new Promise(resolve => setTimeout(resolve, 2000));
+                }
+                parentDiv = document.querySelectorAll('div[class*="syndi_powerpage"]')[0];
+                if(parentDiv && parentDiv.shadowRoot) {
+                    console.log('shadow root loaded!!');
+                } else {
+                    console.log('still not loaded');
+                }
+            });
+
             await context.evaluate(async() => {
                 const element = document.querySelector('footer#footer');
                 if (element) {
@@ -30,11 +89,24 @@ module.exports = {
                         }
                     });
                     document.body.setAttribute('has-comparison-table', hasComparisonTable);
+                    let weight = '';
+                    try {
+                        let weightNetDiv = document.querySelectorAll('div[class*="syndi_powerpage"]')[0];
+                        weightNetDiv = weightNetDiv.shadowRoot.querySelector('div[class*="syndi_powerpage"]');
+                        let weightElm = document.evaluate('(.//table//tbody//th[contains(.,"Peso")]/..//td)[1]', weightNetDiv, null, 7, null);
+                        
+                        if(weightElm.snapshotItem(0).textContent) {
+                            weight = weightElm.snapshotItem(0).textContent.trim();
+                        } else {
+                            console.log('weight is not present');
+                        }
+                    } catch(err) {
+                        console.log('we got some erro while getting net weight', err.message);
+                    }
+                    console.log(weight);
+                    document.body.setAttribute('weightnet', weight);
                 }
             });
-        } catch (e) {
-            console.log(e);
-        }
         await new Promise(resolve => setTimeout(resolve, 10000));
         return await context.extract(productDetails, { transform });
     },
