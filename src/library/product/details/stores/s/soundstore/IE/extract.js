5764f5dbbc8ae9c1f692bc0baeb85564c4abfc6c
prateek.s <psc3018@gmail.com>
2021-01-15 19:12:26 +0530

    fixed enhanced content

--- src/library/product/details/stores/s/soundstore/IE/extract.js
+++ src/library/product/details/stores/s/soundstore/IE/extract.js
@@ -11,6 +11,96 @@ module.exports = {
   },
 
   implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+    // enhanced content
+    const delay = t => new Promise(resolve => setTimeout(resolve, t));
+    const checkExistance = async (selector) => {
+      try {
+        await context.waitForSelector(selector);
+      } catch (err) {
+        console.log(selector, 'not present');
+      }
+      return await context.evaluate(async (currentSelector) => {
+        return await Boolean(document.querySelector(currentSelector));
+      }, selector);
+    };
+    const currentUrl = await context.evaluate(() => {
+      return document.querySelector('meta[property="og:url"]').getAttribute('content');
+    });
+    const iframeSelector = '.eky-container-limited>iframe';
+    if (await checkExistance(iframeSelector)) {
+      const iframeUrl = await context.evaluate((iframeSelector) => {
+        return document.querySelector(iframeSelector).getAttribute('src');
+      }, iframeSelector);
+      await context.goto(iframeUrl, { timeout: 50000, waitUntil: 'networkidle0', checkBlocked: true });
+      await context.waitForXPath('//video');
+      await context.waitForSelector('div[class="eky-row left-padding divider-line"] img');
+      const video = await context.evaluate(() => {
+        const src = ele('video');
+        function ele (tag) {
+          return document.querySelectorAll(tag);
+        }
+        let value = [];
+        retrieve(src);
+        function retrieve (src) {
+          for (let i = 0; i < src.length; i++) {
+            value.push(src[i].currentSrc);
+          }
+        }
+        value = [...new Set(value)];
+        return value;
+      });
+
+      await context.evaluate(() => {
+        const scrollTo = document.querySelector('#specifications');
+        scrollTo.scrollIntoView({ behavior: 'smooth' });
+      });
+      await context.waitForSelector('div[class="eky-row left-padding divider-line"] img');
+      await delay(15000);
+      const images = await context.evaluate(() => {
+        const src = document.querySelectorAll('div[class="eky-row left-padding divider-line"] img');
+        let value = [];
+        retrieve(src);
+        function retrieve (src) {
+          for (let i = 0; i < src.length; i++) {
+            value.push(src[i].currentSrc);
+          }
+        }
+        value = [...new Set(value)];
+        return value;
+      });
+
+      const desc = await context.evaluate(() => {
+        const src = document.querySelectorAll('h1,h2,h3,h4,p,div.eky-accessory>div');
+        const value = [];
+        retrieve(src);
+        function retrieve (src) {
+          for (let i = 0; i < src.length; i++) {
+            value.push(src[i].innerText);
+          }
+        }
+        return value;
+      });
+      await context.goto(currentUrl, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+      await context.evaluate((video) => {
+        video = video.join(' | ');
+        document.querySelector('body').setAttribute('video-src', video);
+      }, video);
+      await context.evaluate((images) => {
+        images = images.join(' | ');
+        console.log(images);
+        const len = images.length;
+        if (images[len - 2] === '|') {
+          console.log('removing | ');
+          images = images.slice(0, len - 3);
+        }
+        document.querySelector('body').setAttribute('img-src', images);
+      }, images);
+      await context.evaluate((desc) => {
+        desc = desc.join(' ');
+        document.querySelector('body').setAttribute('desc', desc);
+      }, desc);
+    }
+
     await context.evaluate(async function () {
       function addElementToDocument (key, value) {
         const catElement = document.createElement('div');
