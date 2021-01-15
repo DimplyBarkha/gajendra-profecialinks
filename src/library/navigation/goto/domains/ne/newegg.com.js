7d88965e62eeb3f671e71330ccdc0e7a2f7c35df
Gayatri Devhade <gayatri.devhade-tm@partner.import.io>
2021-01-15 18:36:41 +0530

    code without captcha code

--- src/library/navigation/goto/domains/ne/newegg.com.js
+++ src/library/navigation/goto/domains/ne/newegg.com.js
@@ -1,4 +1,4 @@
-// const { parameterValues } = require("../am/amazon.com");
+
 
 module.exports = {
   implements: 'navigation/goto',
@@ -17,95 +17,86 @@ module.exports = {
   ) => {
     const timeout = parameters.timeout ? parameters.timeout : 30000;
 
-    //   const responseStatus = await context.goto(url, {
-    //     firstRequestTimeout: 60000,
-    //     timeout: 30000,
-    //     waitUntil: 'load',
-    //     checkBlocked: false,
-    //     antiCaptchaOptions: {
-    //       type: 'RECAPTCHA',
-    //     },
-    //   });
-    //   console.log('status : ', responseStatus.status);
-    //   console.log('URL : ', responseStatus.url);
+      const responseStatus = await context.goto(url, {
+        firstRequestTimeout: 60000,
+        timeout: 30000,
+        waitUntil: 'load',
+        checkBlocked: false,
+        antiCaptchaOptions: {
+          type: 'RECAPTCHA',
+        },
+      });
+      console.log('status : ', responseStatus.status);
+      console.log('URL : ', responseStatus.url);
 
-    // const captchaFrame='iframe[title="recaptcha challenge"]';
-    // // console.log(captchaFrame);
-    //   const maxRetries = 3;
-    //   let numberOfCaptchas = 0;
+    const captchaFrame='iframe[title="recaptcha challenge"]';
+    // console.log(captchaFrame);
+      const maxRetries = 3;
+      let numberOfCaptchas = 0;
 
-    //   try{
-    //     await context.waitForSelector(captchaFrame);
-    //   }catch(e){
-    //     console.log('error:', e);
-    //   }
+      try{
+        await context.waitForSelector(captchaFrame);
+      }catch(e){
+        console.log('error:', e);
+      }
 
-    //   const checkExistance = async(selector) =>{
-    //     return await context.evaluate(async (captchaSelector) => {
-    //       return Boolean(document.querySelector(captchaSelector));
-    //     }, selector);
-    //   };
-    //   const checkRedirection= async () =>{
-    //     try {
-    //       await context.waitForNavigation({ timeout });
-    //       await context.waitForSelector('div.items-grid-view > div > div > a img', { timeout });
-    //       console.log('Redirected to another page.');
-    //       return true;
-    //     } catch (e) {
-    //       console.log('error: without undescore ', e);
-    //       return false;
-    //     }
-    //   };
+      const checkExistance = async(selector) =>{
+        return await context.evaluate(async (captchaSelector) => {
+          return Boolean(document.querySelector(captchaSelector));
+        }, selector);
+      };
+      const checkRedirection= async () =>{
+        try {
+          await context.waitForNavigation({ timeout });
+          await context.waitForSelector('div.items-grid-view > div > div > a img', { timeout });
+          console.log('Redirected to another page.');
+          return true;
+        } catch (e) {
+          console.log('error: without undescore ', e);
+          return false;
+        }
+      };
 
-    //   let isCaptchaFramePresent = await checkExistance(captchaFrame);
-    //   console.log("isCaptcha:"+ isCaptchaFramePresent);
+      let isCaptchaFramePresent = await checkExistance(captchaFrame);
+      console.log("isCaptcha:"+ isCaptchaFramePresent);
 
-    //   while (isCaptchaFramePresent && 0 < 3) {
-    //     console.log('isCaptcha', true);
-    //     ++numberOfCaptchas;
-    //     await context.waitForNavigation({ timeout });
-    //     try {
-    //       console.log(`Trying to solve captcha - count [${numberOfCaptchas}]`);
-    //       await context.evaluateInFrame('iframe', () => grecaptcha.execute());  //could't run locally
-    //       console.log('solved captcha, waiting for page change');
-    //       await context.waitForNavigation({ timeout });
-    //       const redirectionSuccess = await checkRedirection();
+      while (isCaptchaFramePresent && numberOfCaptchas < 3) {
+        console.log('isCaptcha', true);
+        ++numberOfCaptchas;
+        await context.waitForNavigation({ timeout });
+        try {
+          console.log(`Trying to solve captcha - count [${numberOfCaptchas}]`);
+          await context.evaluateInFrame('iframe', () => grecaptcha.execute());  //could't run locally
+          console.log('solved captcha, waiting for page change');
+          await context.waitForNavigation({ timeout });
+          const redirectionSuccess = await checkRedirection();
 
-    //       if (redirectionSuccess) {
-    //         await context.evaluate((url) => {
-    //           window.location.href = url;
-    //         }, url);
-    //         await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
-    //         break;
-    //       } else {
-    //         await context.evaluate((url) => {
-    //           // eslint-disable-next-line no-unused-expressions
-    //           window.location.reload;
-    //         });
-    //         await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
-    //       }
+          if (redirectionSuccess) {
+            await context.evaluate((url) => {
+              window.location.href = url;
+            }, url);
+            await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
+            break;
+          } else {
+            await context.evaluate((url) => {
+              // eslint-disable-next-line no-unused-expressions
+              window.location.reload;
+            });
+            await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
+          }
 
-    //       isCaptchaFramePresent = await checkExistance(captchaFrame);
-    //     } catch (e) {
-    //       console.log('error: without undescore ', e);
-    //     }
-    //   }
+          isCaptchaFramePresent = await checkExistance(captchaFrame);
+        } catch (e) {
+          console.log('error: without undescore ', e);
+        }
+      }
 
-    //   isCaptchaFramePresent = await checkExistance(captchaFrame);
+      isCaptchaFramePresent = await checkExistance(captchaFrame);
 
-    //   if (isCaptchaFramePresent) {
-    //     throw new Error('Failed to solve captcha');
-    //   }
+      if (isCaptchaFramePresent) {
+        throw new Error('Failed to solve captcha');
+      }
 
-    //   const videoFrame = '#wcframable1-0';
-    //       console.log(videoFrame);
-    //         try{
-    //           var link = await context.waitForSelector(videoFrame,{ timeout : 40000});
-    //         }catch(e){
-    //           console.log('error:', e);
-    //         }
-
-    // await context.waitForSelector('#wcframable1-0',{ timeout : 40000});
     await context.evaluate(async () => {
       function addHiddenDiv (id, content) {
         const newDiv = document.createElement('div');
@@ -132,21 +123,6 @@ module.exports = {
       }
       if (document.querySelector('div.wc-reset > ul > li > div > div > img.wc-media.wc-video')) addHiddenDiv('video', links.join(' | '));
       addHiddenDiv('video', link);
-
-      //   const specification = [];
-      //   if(document.querySelector('table.table-horizontal > tbody > tr')){
-      //     document.querySelectorAll('table.table-horizontal > tbody > tr').forEach(e =>{
-      //       specification.push(e.textContent);
-      //   });
-      //   }
-      //   addHiddenDiv('specification',specification.join(' || '));
-
-      //   const bulletInfo =
-      //   (document.querySelector('div.product-bullets > ul') &&
-      //     document.querySelector('div.product-bullets > ul').textContent
-      //       .split(/[\n•]/)
-      //       .join('||'));
-      //   addHiddenDiv('bulletInfo', bulletInfo);
     });
   },
 
--- src/library/product/search/stores/n/newegg/US/extract.js
+++ src/library/product/search/stores/n/newegg/US/extract.js
@@ -10,123 +10,6 @@ async function implementation (
 
 ) {
   const { transform } = parameters;
-  // const { productDetails } = dependencies;
-
-  const timeout = parameters.timeout ? parameters.timeout : 15000;
-
-  // await context.evaluate(async function () {
-  //   var captchaBtn = document.querySelector('.recaptcha-checkbox-border');
-  //   // document.querySelector('#recaptcha-anchor');
-  //   console.log(captchaBtn,"%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-  //   if(captchaBtn){
-  //     // var btn = document
-  //     console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-  //     captchaBtn.click();
-  //     console.log("Click itTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
-  //   }
-  // });
-
-  // const captchaFrame = 'iframe[title="recaptcha challenge"]';
-  // div[id="g-recaptcha"]> div > div > iframe[name="a-6y6x1xg2d1jh"]
-  // console.log(url,"@@@@@@2");
-  await context.waitForSelector('iframe[title="recaptcha challenge"]', { timeout: 15000 });
-  const captchaFrame = 'iframe[title="recaptcha challenge"]';
-  console.log('&&&&&&&&&&&&');
-  if (captchaFrame !== null) {
-    // const responseStatus = await context.goto( {
-    //   firstRequestTimeout: 170000,
-    //   timeout: 170000,
-    //   waitUntil: 'load',
-    //   checkBlocked: false,
-    //   antiCaptchaOptions: {
-    //     type: 'RECAPTCHA',
-    //   // recaptcha
-    //   },
-    // });
-    // console.log('status : ', responseStatus.status);
-
-    // const captchaFrame = "iframe[src*='captcha']:not([title])";
-    // const captchaFrame = 'div[id="g-recaptcha"]';
-    // await context.waitForSelector(captchaFrame);
-    const maxRetries = 4;
-    let numberOfCaptchas = 0;
-
-    try {
-      await context.waitForSelector(captchaFrame);
-    } catch (e) {
-      console.log('error:', e);
-    }
-
-    const checkExistance = async (selector) => {
-      return await context.evaluate(async (captchaSelector) => {
-        return Boolean(document.querySelector(captchaSelector));
-      }, selector);
-    };
-    const checkRedirection = async () => {
-      try {
-        await context.waitForNavigation({ timeout });
-        await context.waitForXPath('//div[@class="page-content"]', { timeout });
-        console.log('Redirected to another page.');
-        return true;
-      } catch (e) {
-        console.log('error: without undescore ', e);
-        return false;
-      }
-    };
-
-    let isCaptchaFramePresent = await checkExistance(captchaFrame);
-    console.log('isCaptcha:' + isCaptchaFramePresent);
-
-    while (isCaptchaFramePresent && numberOfCaptchas < 4) {
-      console.log('isCaptcha', true);
-      ++numberOfCaptchas;
-      await context.waitForNavigation({ timeout });
-      try {
-        console.log(`Trying to solve captcha - count [${numberOfCaptchas}]`);
-        // @ts-ignore
-        await context.evaluateInFrame('iframe', () => grecaptcha.execute()); // could't run locally
-        console.log('solved captcha, waiting for page change');
-        await context.waitForNavigation({ timeout });
-        const redirectionSuccess = await checkRedirection();
-
-        if (redirectionSuccess) {
-          // await context.evaluate((url) => {
-          //   window.location.href = url;
-          // }, url);
-          await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
-          break;
-        } else {
-          await context.evaluate(() => {
-            // eslint-disable-next-line no-unused-expressions
-            window.location.reload;
-          });
-          await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
-        }
-
-        isCaptchaFramePresent = await checkExistance(captchaFrame);
-      } catch (e) {
-        console.log('error: without undescore ', e);
-      }
-    }
-
-    isCaptchaFramePresent = await checkExistance(captchaFrame);
-
-    if (isCaptchaFramePresent) {
-      throw new Error('Failed to solve captcha');
-    }
-  }
-  // });
-  // await context.evaluate(async function () {
-  //   var captchaBtn = document.querySelector('#recaptcha-anchor');
-  //   console.log(captchaBtn,"%%%%%%%%%%%%%%%%%%%%%%%%%%%%Extracttttttttt");
-  //   if(captchaBtn){
-  //     // var btn = document
-  //     console.log("@@@@@@@@@@@@@@@@@@@@@@@");
-  //     captchaBtn.click();
-  //     console.log("Click itTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
-  //   }
-  // });
-
   async function addUrl () {
     function addHiddenDiv (id, content) {
       const newDiv = document.createElement('div');
@@ -155,7 +38,6 @@ module.exports = {
   },
   implementation,
   dependencies: {
-    // goto: 'action:navigation/goto',
     productDetails: 'extraction:product/search/stores/${store[0:1]}/${store}/${country}/extract',
   },
 };
