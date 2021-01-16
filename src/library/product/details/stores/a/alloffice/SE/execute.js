366404e58ece2bacc9930df4d08a8431fdb0313e
osmarghyadeep <arghyadep.g@partner.import.io>
2021-01-16 17:56:17 +0530

    Updated the code to handle listing page

--- src/library/product/details/stores/a/alloffice/SE/execute.js
+++ src/library/product/details/stores/a/alloffice/SE/execute.js
@@ -4,8 +4,10 @@ module.exports = {
     country: 'SE',
     store: 'alloffice',
     domain: 'alloffice.se',
-    loadedSelector: 'button[data-test-id="add-to-cart-button"]',
-    noResultsXPath: '//font[contains(text(),"no hits")] | //font[contains(text(),"try another")] | //*[contains(text(),"Kan inte köpas i webbutiken")]',
+    //loadedSelector: 'button[data-test-id="add-to-cart-button"]',
+    loadedXpath: '//h1[contains(.,"Sökresultat för")] | //button[contains(@data-test-id,"add-to-cart-button")]',
+    noResultsXPath: '//div[contains(@id,"content-container")][contains(.,"Din sökning gav inga träffar. Vänligen försök igen med en annan sökterm eller använd kategorierna i menyn.")]',
+    // noResultsXPath: '//font[contains(text(),"no hits")] | //font[contains(text(),"try another")] | //*[contains(text(),"Kan inte köpas i webbutiken")]',
     zipcode: '',
   },
 };
--- src/library/product/details/stores/a/alloffice/SE/extract.js
+++ src/library/product/details/stores/a/alloffice/SE/extract.js
@@ -14,6 +14,49 @@ module.exports = {
     context,
     dependencies,
   ) {
+
+    // need to check if this is a product page or a listing page -- 
+    let listingPageXpath = '//a[contains(@data-test-id,"product-link")][//h1[contains(.,"Sökresultat för")]]';
+    let listingPageLoaded = false;
+    let sku = inputs.id;
+    try {
+      console.log(sku);
+      console.log('waiting for page load!');
+      await context.waitForXPath(listingPageXpath);
+      listingPageLoaded = true;
+      console.log('waiting done!!');
+    } catch(err) {
+      console.log('error while loading the page!!', err.message);
+      try {
+        console.log('waiting for page load! again');
+        await context.waitForXPath(listingPageXpath);
+        listingPageLoaded = true;
+        console.log('waiting done!!');
+      } catch(error) {
+        console.log('error while loading the page!! again', error.message);
+      }
+    }
+
+    if(listingPageLoaded) {
+      console.log('we have a listing page here!!');
+      await context.evaluate(async (listingPageXpath, sku) => {
+        let allElms = document.evaluate(listingPageXpath, document, null, 7, null);
+        for(let i = 0; i < allElms.snapshotLength; i++) {
+          if(allElms.snapshotItem(i) && allElms.snapshotItem(i).hasAttribute('href') && allElms.snapshotItem(i).getAttribute('href').includes(sku)) {
+            console.log(allElms.snapshotItem(i).getAttribute('href'));
+            allElms.snapshotItem(i).click();
+            console.log('clicked on the prod');
+            break;
+          }
+        }
+      },
+      listingPageXpath,
+      sku);
+    } else {
+      console.log('we might have product page!!');
+    }
+    await new Promise(resolve => setTimeout(resolve, 15000));
+    
     await context.evaluate(() => {
       const rpc = document.querySelector('meta[property="product:retailer_item_id"]').getAttribute('content');
       const imgs = document.querySelectorAll('img');
