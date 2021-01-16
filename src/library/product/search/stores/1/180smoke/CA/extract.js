66bc582f998806d41b81cf22b7890f9250fff8ad
ahmad8931 <ahmad@profecialinks.com>
2021-01-16 19:32:27 +0530

    optimization of search result and implementation of product family and range

--- src/library/product/search/stores/1/180smoke/CA/extract.js
+++ src/library/product/search/stores/1/180smoke/CA/extract.js
@@ -1,3 +1,26 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    const brandName = window.location.href.replace("https://www.180smoke.ca/search?q=", "");
+    const manufactureres = document.querySelectorAll('div.manufacturer > div.form-item-content > div.form-check ');
+    if (manufactureres) {
+      Array.from(manufactureres).forEach(manufacturer => {
+        var selectBrand = manufacturer.querySelector('label > div').innerHTML;
+        if(String(selectBrand).includes(brandName.toUpperCase())){
+          manufacturer.querySelector('input').click();
+        }
+      });
+    }
+  });
+  await new Promise(r => setTimeout(r, 500));
+ 
+  return await context.extract(productDetails);
+}
 
 module.exports = {
   implements: 'product/search/extract',
@@ -8,4 +31,5 @@ module.exports = {
     domain: '180smoke.ca',
     zipcode: '',
   },
+  implementation,
 };
