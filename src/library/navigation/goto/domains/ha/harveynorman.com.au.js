3e9de961839f07ddd216fe0e7aa48945599b7b61
tapan-profecialinks <tapan@profecialinks.com>
2021-01-16 22:57:32 +0530

    issue fixed

--- src/library/navigation/goto/domains/ha/harveynorman.com.au.js
+++ src/library/navigation/goto/domains/ha/harveynorman.com.au.js
@@ -8,13 +8,16 @@ module.exports = {
     store: 'harveynorman',
     zipcode: "''",
   },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
-    await context.setBlockAds(false);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
-    console.log(zipcode);
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-    }
-  },
+  
+  /* ----- commented because data is not coming for some urls eg- "https://www.harveynorman.com.au/miele-g-4263-scvi-active-60cm-fully-integrated-dishwasher.html"---- */
+
+  // implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+  //   const timeout = parameters.timeout ? parameters.timeout : 10000;
+  //   await context.setBlockAds(false);
+  //   await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+  //   console.log(zipcode);
+  //   if (zipcode) {
+  //     await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+  //   }
+  // },
 };
