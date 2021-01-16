515ab4b24c356f1598eed8a848bb5e26d70ce7ba
osmarghyadeep <arghyadep.g@partner.import.io>
2021-01-16 19:28:14 +0530

    increased the timeout

--- src/library/product/details/stores/0/0815/AT/execute.js
+++ src/library/product/details/stores/0/0815/AT/execute.js
@@ -16,7 +16,7 @@ async function implementation (
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    }, { timeout: 25000 }, parameters.loadedSelector, parameters.noResultsXPath);
   }
 
   if (id) {
