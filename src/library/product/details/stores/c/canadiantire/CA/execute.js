970f85f32e22d3b59b1c1d2d33bf825bf0050006
Vivek Gondil <gondilvivek@gmail.com>
2021-01-16 09:33:47 +0530

    Added changes for trench 3

--- src/library/product/details/stores/c/canadiantire/CA/execute.js
+++ src/library/product/details/stores/c/canadiantire/CA/execute.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-  implements: 'product/details/execute',
-  parameterValues: {
-    country: 'CA',
-    store: 'canadiantire',
-    domain: 'canadiantire.ca',
-    loadedSelector: null,
-    noResultsXPath: null,
-    zipcode: '',
-  },
+    implements: 'product/details/execute',
+    parameterValues: {
+        country: 'CA',
+        store: 'canadiantire',
+        domain: 'canadiantire.ca',
+        loadedSelector: 'div.pdp-image-carousel,div.pdp-product-image-and-buy-box__inner',
+        noResultsXPath: '//font[contains(text(),"The page you’re looking for isn’t here. Visit our")]',
+        zipcode: '',
+    },
 };
--- src/library/product/details/stores/c/canadiantire/CA/extract.js
+++ src/library/product/details/stores/c/canadiantire/CA/extract.js
@@ -1,11 +1,37 @@
-const {transform} = require('../format')
+const { transform } = require('../format')
 module.exports = {
-  implements: 'product/details/extract',
-  parameterValues: {
-    country: 'CA',
-    store: 'canadiantire',
-    transform,
-    domain: 'canadiantire.ca',
-    zipcode: '',
-  },
+    implements: 'product/details/extract',
+    parameterValues: {
+        country: 'CA',
+        store: 'canadiantire',
+        transform,
+        domain: 'canadiantire.ca',
+        zipcode: '',
+    },
+    implementation: async function(
+        inputs,
+        parameters,
+        context,
+        dependencies,
+    ) {
+        const { transform } = parameters;
+        const { productDetails } = dependencies;
+        await context.evaluate(async() => {
+            await new Promise((resolve, reject) => {
+                var totalHeight = 0;
+                var distance = 100;
+                var timer = setInterval(() => {
+                    var scrollHeight = document.body.scrollHeight;
+                    window.scrollBy(0, distance);
+                    totalHeight += distance;
+
+                    if (totalHeight >= scrollHeight) {
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 100);
+            });
+        });
+        return await context.extract(productDetails, { transform });
+    }
 };
