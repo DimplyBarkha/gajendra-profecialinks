832ccad639da180180ff0029cde0782424ca6e84
Prakhar Singh <prakhar.s@OSMOSYS.LOCAL>
2021-01-17 02:59:04 +0530

    Added captcha code

--- src/library/navigation/goto/domains/ko/kogan.com.js
+++ src/library/navigation/goto/domains/ko/kogan.com.js
@@ -17,10 +17,78 @@ module.exports = {
     await context.setLoadAllResources(true);
     await context.setLoadImages(true);
     await context.setJavaScriptEnabled(true);
-    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    
+    const gotoOptions = {
+      firstRequestTimeout: 60000,
+      timeout: timeout, 
+      waitUntil: 'load',
+      checkBlocked: true,
+      antiCaptchaOptions: {
+        type: 'RECAPTCHA',
+      },
+    };
     console.log(zipcode);
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+    const responseStatus = await context.goto(url, gotoOptions);
+    console.log('Status :', responseStatus.status);
+    console.log('URL :', responseStatus.url);
+
+    const captchaFrame = "iframe[_src*='captcha']:not([title]), iframe[src*='captcha']:not([title])";
+    const maxRetries = 3;
+    let numberOfCaptchas = 0;
+
+    try {
+      await context.waitForSelector(captchaFrame);
+    } catch(e) {
+      console.log("Didn't find Captcha.");
+    }
+
+    const checkExistance = async (selector) => {
+      return await context.evaluate(async (captchaSelector) => {
+        return Boolean(document.querySelector(captchaSelector));
+      }, selector);
+    };
+
+    const checkRedirection = async () => {
+      const newurl = await context.evaluate(() => window.location.href);
+      return !newurl.includes(url);
+    };
+
+    let isCaptchaFramePresent = await checkExistance(captchaFrame);
+    console.log("isCaptcha:"+ isCaptchaFramePresent);
+
+    while (isCaptchaFramePresent && numberOfCaptchas < maxRetries) {
+      console.log('isCaptcha', true);
+      ++numberOfCaptchas;
+      await context.waitForNavigation({ timeout });
+      try {
+        console.log(`Trying to solve captcha - count [${numberOfCaptchas}]`);
+        // @ts-ignore
+        // eslint-disable-next-line no-undef
+        await context.evaluateInFrame('iframe', () => grecaptcha.execute());
+        console.log('solved captcha, waiting for page change');
+        await context.waitForNavigation({ timeout });
+        const redirectionSuccess = await checkRedirection();
+
+        if (redirectionSuccess) {
+          console.log('Page was redirected');
+          await context.goto(url, gotoOptions);
+          await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
+          break;
+        }
+
+        isCaptchaFramePresent = await checkExistance(captchaFrame);
+      } catch (e) {
+        console.log('Captcha did not load');
+      }
+    }
+
+    isCaptchaFramePresent = await checkExistance(captchaFrame);
+
+    if (isCaptchaFramePresent) {
+      throw new Error('Failed to solve captcha');
     }
   },
 };
