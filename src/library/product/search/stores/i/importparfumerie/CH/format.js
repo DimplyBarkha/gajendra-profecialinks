681310d29b413390339c15559240da0a03d81678
sivasoarlogic <siva@soarlogic.com>
2021-01-16 22:01:55 +0530

    QAfixs

--- src/library/product/search/stores/i/importparfumerie/CH/format.js
+++ src/library/product/search/stores/i/importparfumerie/CH/format.js
@@ -4,34 +4,28 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
+  const cleanUp = (data) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+    // .replace(/"\s{1,}/g, '"')
+    // .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
     // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
   const state = context.getState();
-  let orgRankCounter = state.orgRankCounter || 0;
-  let rankCounter = state.rankCounter || 0;
-  const productCodes = state.productCodes || [];
+  let rank = state.rank || 1;
   for (const { group } of data) {
     for (const row of group) {
-      rankCounter += 1;
-      let skyIdStr='';
-      if (!row.sponsored) {
-        orgRankCounter += 1;
-        row.rankOrganic = [{ text: orgRankCounter }];
-      }
-      row.rank = [{ text: rankCounter }];
-      Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-      }));
       if (row.productUrl) {
         row.productUrl.forEach(item => {
           item.text = 'https://www.impo.ch' + item.text;
@@ -69,12 +63,11 @@ const transform = (data, context) => {
           item.text = item.text.replace(/\s\n/g, '').trim();
         });
       }
+      row.rank = row.rankOrganic = [{ text: rank }];
+      rank++;
     }
   }
-  context.setState({ rankCounter });
-  context.setState({ orgRankCounter });
-  context.setState({ productCodes });
-  console.log(productCodes);
-  return data;
+  context.setState({ rank });
+  return cleanUp(data);
 };
 module.exports = { transform };
