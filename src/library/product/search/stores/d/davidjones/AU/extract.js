75fed7b6be477196f7e3a408084f313e46b12e0b
avinashkadam <avinash.kadam@profecialinks.com>
2021-01-17 19:04:48 +0530

    search pagination issue resolved

--- src/library/product/search/stores/d/davidjones/AU/extract.js
+++ src/library/product/search/stores/d/davidjones/AU/extract.js
@@ -8,4 +8,28 @@ module.exports = {
     domain: 'davidjones.com',
     zipcode: '',
   },
+  implementation,
 };
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    let count = document.querySelectorAll('div.products.sli_container  div[data-tb-sid="st_result-container-wrapper"]').length;
+    while (count <= 150) {
+      if (document.querySelector('div.sli_infinite_wrapper > button.sli_load_more_button:not([disabled])')) {
+        document.querySelector('div.sli_infinite_wrapper > button.sli_load_more_button').click();
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        count = document.querySelectorAll('div.products.sli_container  div[data-tb-sid="st_result-container-wrapper"]').length;   
+      } else {
+        break;
+      }      
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
--- src/library/product/search/stores/d/davidjones/AU/paginate.js
+++ src/library/product/search/stores/d/davidjones/AU/paginate.js
@@ -4,8 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'AU',
     store: 'davidjones',
-    nextLinkSelector: 'div.sli_infinite_wrapper > button.sli_load_more_button',
-    spinnerSelector: 'div[data-tb-sid*="st_infinite-spinner"][style*="display: block;"]',
+    nextLinkSelector: null,
     loadedSelector: 'div#sli_content_wrapper',
     domain: 'davidjones.com',
     zipcode: '',
