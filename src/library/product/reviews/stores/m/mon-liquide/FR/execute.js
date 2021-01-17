48e55e6495eb9bfeb10006520d5c54ebf09cbd4f
Avinash <avinash.kadam@profecialinks.com>
2021-01-17 12:58:41 +0530

    loadedSelector & input updated

--- src/library/product/reviews/stores/m/mon-liquide/FR/execute.js
+++ src/library/product/reviews/stores/m/mon-liquide/FR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FR',
     store: 'mon-liquide',
     domain: 'mon-liquide.fr',
-    loadedSelector: 'div.tab-content div#idTabavisverifies div#ajax_comment_content',
+    loadedSelector: 'div#columns',
     noResultsXPath: '//span[@class="eo_notemoyenne"][not(div)]',
     reviewUrl: null,
     sortButtonSelectors: null,
