90579b80b8d1682b389355dbfaec895b67f04adb
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2021-01-17 19:07:52 +0530

    Fix product count isue

--- src/library/product/search/stores/c/carrefour/AE/execute.js
+++ src/library/product/search/stores/c/carrefour/AE/execute.js
@@ -7,26 +7,13 @@ async function implementation (
   console.log('params', parameters);
   const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
   await dependencies.goto({ url, zipcode: inputs.zipcode });
+
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
     }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
   }
   console.log('Checking no results', parameters.noResultsXPath);
-  let page = 1;
-  while(1){
-    try {
-      await context.waitForSelector('button.ltr-1upsixo')
-      await context.click('button.ltr-1upsixo');
-    }catch (e) {
-      console.log(e);
-      break;
-    }
-    page++;
-    if(page > 20){
-      break;
-    }
-  }
 
   return await context.evaluate(function (xp) {
     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
