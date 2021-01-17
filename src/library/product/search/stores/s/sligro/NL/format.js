f2d9b7e88922b4aa6ff2f5f5724392ef3fffd685
tapan-profecialinks <tapan@profecialinks.com>
2021-01-17 22:23:52 +0530

    issue fixed

--- src/library/product/search/stores/s/sligro/NL/format.js
+++ src/library/product/search/stores/s/sligro/NL/format.js
@@ -4,30 +4,23 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  const cleanUp = (data, context) => {
-    const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
     // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      el.text = clean(el.text);
-    }))));
-    return data;
-  };
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
   const state = context.getState();
   let orgRankCounter = state.orgRankCounter || 0;
   let rankCounter = state.rankCounter || 0;
   const productCodes = state.productCodes || [];
   for (const { group } of data) {
-    let rank = 1;
     for (const row of group) {
       if (row.productUrl) {
         row.productUrl.forEach(item => {
@@ -39,14 +32,21 @@ const transform = (data, context) => {
           item.text = 'https://www.sligro.nl' + item.text.replace('/small.png', '/large.jpeg');
         });
       }
-      row.rank = row.rankOrganic = [{ text: rank }];
-      rank++;
+      rankCounter += 1;
+      if (!row.sponsored) {
+        orgRankCounter += 1;
+        row.rankOrganic = [{ text: orgRankCounter }];
+      }
+      row.rank = [{ text: rankCounter }];
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
     }
   }
   context.setState({ rankCounter });
   context.setState({ orgRankCounter });
   context.setState({ productCodes });
-  return cleanUp(data);
+  console.log(productCodes);
+  return data;
 };
-
 module.exports = { transform };
