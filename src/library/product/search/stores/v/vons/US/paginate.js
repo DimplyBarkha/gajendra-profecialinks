105978bdc485e71fe0fc1636943082233aa05635
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-01-17 20:09:35 +0530

    #2650 devvonssearch

--- src/library/product/search/stores/v/vons/US/paginate.js
+++ src/library/product/search/stores/v/vons/US/paginate.js
@@ -6,7 +6,7 @@ module.exports = {
   nextLinkSelector: null,
   mutationSelector: null,
   spinnerSelector: null,
-  loadedSelector: null,
+  loadedSelector: 'body',
   noResultsXPath: null,
   openSearchDefinition: null,
   domain: 'vons.com',
