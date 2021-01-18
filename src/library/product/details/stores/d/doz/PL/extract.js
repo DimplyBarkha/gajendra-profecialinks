60155bbd0e847111b839e1be75322082f35a29fc
nilofarsh <nilofar.shaikh@synerzip.com>
2021-01-18 21:02:34 +0530

    --amend

--- src/library/product/details/stores/d/doz/PL/extract.js
+++ src/library/product/details/stores/d/doz/PL/extract.js
@@ -1,25 +1,5 @@
 const { transform } = require('../shared');
-async function implementation (inputs, parameters, context, dependencies) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
 
-  const mainUrl = await context.evaluate(() => {
-    return window.location.href;
-  });
-  let pageNotFound = '';
-  const mainUrlWithDomain = mainUrl.includes('https://www.doz.pl') ? mainUrl : 'https://www.doz.pl' + mainUrl;
-
-  pageNotFound = await context.evaluate(() => {
-    const pageNotFoundSelector = document.evaluate('//title[contains(text(),"Page not found")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-    return pageNotFoundSelector || '';
-  });
-  console.log(pageNotFound);
-  if (pageNotFound) {
-    await context.goto(mainUrlWithDomain, { timeout: 5000, waitUntil: 'load', checkBlocked: true });
-  }
-  return await context.extract(productDetails, { transform });
-}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -29,5 +9,4 @@ module.exports = {
     domain: 'doz.pl',
     zipcode: '',
   },
-  implementation,
 };
