7e34c1e525c90616bebe379a931dc65602d84613
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2021-01-18 22:08:31 +0530

    changes made according to QA

--- src/library/product/search/stores/h/houzz/US/extract.js
+++ src/library/product/search/stores/h/houzz/US/extract.js
@@ -12,50 +12,6 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(() => {
-      function stall(ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-      if (
-        document.querySelector(
-          ".btn.btn-none.hz-universal-search-header-tip__dismiss"
-        ) != null
-      ) {
-        try{
-          document.querySelector(".btn.btn-none.hz-universal-search-header-tip__dismiss").click();
-        }catch(err){
-          console.log(err)
-        }
-        
-      }
-      if (
-        document.querySelector(
-          "body > div.hz-modal.hz-modal--dark > div.hz-trap-focus.hz-modal__container > div > div.hz-modal__body > div > div.hz-international-redirect-modal__btns > div.hz-international-redirect-modal__continue-btn > button"
-        ) != null
-      ) {
-        try{
-          document
-          .querySelector(
-            "body > div.hz-modal.hz-modal--dark > div.hz-trap-focus.hz-modal__container > div > div.hz-modal__body > div > div.hz-international-redirect-modal__btns > div.hz-international-redirect-modal__continue-btn > button"
-          )
-          .click();
-        }catch(err){
-          console.log(err)
-        }
-
-      }
-      let scrollTop = 500;
-      while (true) {
-        window.scroll(0, scrollTop);
-        stall(500);
-        scrollTop += 500;
-        if (scrollTop === 10000) {
-          break;
-        }
-      }
       function addHiddenDiv(id, content, index) {
         const newDiv = document.createElement("div");
         newDiv.id = id;
