7bf38f7944d0b9e548a6b8ecb06c125509d293aa
nilofarsh <nilofar.shaikh@synerzip.com>
2021-01-18 15:32:03 +0530

    Uat issues fixed for search

--- src/library/product/search/stores/d/doz/PL/extract.js
+++ src/library/product/search/stores/d/doz/PL/extract.js
@@ -1,4 +1,44 @@
 const { transform } = require('../../../../shared');
+async function implementation (
+  { results },
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    let scrollTop = 0;
+    while (scrollTop <= 20000) {
+      await stall(500);
+      scrollTop += 1000;
+      window.scroll(0, scrollTop);
+      if (scrollTop === 20000) {
+        await stall(8000);
+        break;
+      }
+    }
+    function stall (ms) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+  });
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const searchURL = window.location.href;
+    addHiddenDiv('added_search_url', searchURL);
+  });
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/search/extract',
@@ -9,4 +49,5 @@ module.exports = {
     domain: 'doz.pl',
     zipcode: '',
   },
+  implementation,
 };
