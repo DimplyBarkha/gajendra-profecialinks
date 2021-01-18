267b748c9962b94bd13e3f198f8624ca5f9d9c41
nilofarsh <nilofar.shaikh@synerzip.com>
2021-01-18 19:01:17 +0530

    --amend

--- src/library/product/details/stores/d/doz/PL/extract.js
+++ src/library/product/details/stores/d/doz/PL/extract.js
@@ -1,5 +1,28 @@
 const { transform } = require('../shared');
+async function implementation (inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
 
+  const mainUrl = await context.evaluate(() => {
+    return window.location.href;
+  });
+  let mainUrlWithDomain = '';
+  mainUrlWithDomain = mainUrl.includes('https://www.doz.pl') ? mainUrl : 'https://www.doz.pl' + mainUrl;
+  if (mainUrlWithDomain) {
+    await context.goto(mainUrlWithDomain, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
+  }
+  await context.evaluate(async function (sku) {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    addHiddenDiv('sku', searchURL);
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -9,4 +32,5 @@ module.exports = {
     domain: 'doz.pl',
     zipcode: '',
   },
+  implementation,
 };
