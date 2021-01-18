89918aa8e673fd5f4442834a4b30bc8b1873a8b4
navya golla <navya.golla@od.import.io>
2021-01-18 22:08:08 +0530

    #3448

--- src/library/navigation/goto/domains/lo/lomax.dk.js
+++ src/library/navigation/goto/domains/lo/lomax.dk.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'lomax.dk',
-    timeout: 80000,  
+    timeout: 100000,  
     country: 'DK',
     store: 'lomax',
     zipcode: '',
