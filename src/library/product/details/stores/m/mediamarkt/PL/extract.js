0d5a6be58102dabada06a8a22b6018281b3ca2f6
osmarghyadeep <arghyadep.g@partner.import.io>
2021-01-18 20:26:24 +0530

    updated fix for name, listPrice, enhancedContent, videos, manufacturerImages

--- src/library/product/details/stores/m/mediamarkt/PL/extract.js
+++ src/library/product/details/stores/m/mediamarkt/PL/extract.js
@@ -9,4 +9,165 @@ module.exports = {
     domain: 'mediamarkt.pl',
     zipcode: '',
   },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+
+    let enhancedContentVidSel = 'iframe[src*="youtube"], iframe[ _src*="youtube"]';
+    let videosLoaded = false;
+    console.log('waiting for 10 secs!!');
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    
+    console.time('scrolling');
+    await context.evaluate(async () => {
+      async function infiniteScroll () {
+        let prevScroll = document.documentElement.scrollTop;
+        while (true) {
+          window.scrollBy(0, document.documentElement.clientHeight);
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          const currentScroll = document.documentElement.scrollTop;
+          if (currentScroll === prevScroll) {
+            break;
+          }
+          prevScroll = currentScroll;
+        }
+      }
+      await infiniteScroll();
+  
+    });
+    console.timeEnd('scrolling');
+    console.log('done scrolling!');
+
+    try {
+      await context.waitForSelector(enhancedContentVidSel);
+      console.log('videos loaded');
+      videosLoaded = true;
+    } catch(err) {
+      console.log('had some error while waiting for video div', err.message);
+    }
+
+    videosLoaded = await context.evaluate(async (enhancedContentVidSel) => {
+      let allVidElms = document.querySelectorAll(enhancedContentVidSel);
+      if(allVidElms.length > 0) {
+        console.log(`we have a total of ${allVidElms.length}`);
+        return true;
+      }
+      return false;
+    },
+    enhancedContentVidSel);
+    
+    console.log('videosLoaded', videosLoaded);
+    if(!videosLoaded) {
+      console.log('not loaded yet!! trying waiting again');
+      try {
+        await context.waitForSelector(enhancedContentVidSel);
+        console.log('videos loaded');
+        videosLoaded = await context.evaluate(async (enhancedContentVidSel) => {
+          let allVidElms = document.querySelectorAll(enhancedContentVidSel);
+          if(allVidElms.length > 0) {
+            console.log(`we have a total of ${allVidElms.length}`);
+            return true;
+          }
+          return false;
+        },
+        enhancedContentVidSel);
+      } catch(err) {
+        console.log('had some error while waiting for video div', err.message);
+      }
+    }
+
+    console.log('videosLoaded', videosLoaded);
+    if(!videosLoaded) {
+      console.log('cannot help it!! videos are not there');
+    }
+
+    if(videosLoaded) {
+      await context.evaluate(async (enhancedContentVidSel) => {
+        async function addElementToDocumentAsync (key, value) {
+          const catElement = document.createElement('div');
+          catElement.id = key;
+          catElement.textContent = value;
+          document.body.appendChild(catElement);
+        }
+        let videoArr = [];
+        let allVideosElm = document.querySelectorAll(enhancedContentVidSel);
+        if(allVideosElm && allVideosElm.length > 0) {
+          for(let i = 0; i < allVideosElm.length; i++) {
+            let thisElm = allVideosElm[i];
+            let thisVidUrl = thisElm.getAttribute('src');
+            if(!thisVidUrl) {
+              thisVidUrl = thisElm.getAttribute('_src');
+            }
+            console.log('url - ' + thisVidUrl);
+            videoArr.push(thisVidUrl);
+          }
+        } else {
+          console.log('we do not have videos in enhanced content');
+        }
+
+        await addElementToDocumentAsync('videos', videoArr.join(' || '));
+      },
+      enhancedContentVidSel);
+    }
+
+    let manufacDescSelArr = ['div[id*="description"] div[class*="product"]', 'div[id="af-product-card"]', 'div[class*="b-offerRWD_description "]'];
+    let showMoreBtnXPath = '//a[contains(@class,"m-btn")][contains(.,"Rozwiń opis")]';
+    let isShowMorePrsent = false;
+    try {
+      await context.waitForXPath(showMoreBtnXPath);
+      console.log('we have the btn');
+      await context.evaluate(async (showMoreBtnXPath) => {
+        let elm = document.evaluate(showMoreBtnXPath, document, null, 7, null);
+        if(elm && elm.snapshotLength > 0) {
+          elm.snapshotItem(0).click();
+        }
+      }, showMoreBtnXPath);
+    }catch(err) {
+      console.log('had some error while clicking the load more btn', err.message);
+    }
+
+    
+    await context.evaluate(async (manufacDescSelArr) => {
+      async function addElementToDocumentAsync (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        document.body.appendChild(catElement);
+      }
+
+      let manufacDescText = '';
+      let elm = [];
+      let thisPageSel = ''
+      for(let i = 0; i < manufacDescSelArr.length; i++) {
+        elm = document.querySelectorAll(manufacDescSelArr[i]);
+        if(elm && elm.length > 0 && elm[0] && elm[0].innerText) {
+          thisPageSel = manufacDescSelArr[i];
+          break;
+        }
+      }
+      let allImages = [];
+      if(elm && elm[0]) {
+        manufacDescText = elm[0].innerText;
+        allImages = elm[0].querySelectorAll('img');
+      }
+      console.log(manufacDescText);
+      console.log('this page has ' + thisPageSel + ' for manufacturerDesc');
+      let allImagesUrl = [];
+      for(let i = 0; i < allImages.length; i++) {
+        console.log(allImages[i].getAttribute('src'));
+        allImagesUrl.push('https:' + allImages[i].getAttribute('src'));
+      }
+      await addElementToDocumentAsync('manufacdesc', manufacDescText);
+      await addElementToDocumentAsync('manufacdescimgs', allImagesUrl.join(' || '));
+    },
+    manufacDescSelArr);
+    
+
+    return await context.extract(productDetails, { transform });
+  }
 };
