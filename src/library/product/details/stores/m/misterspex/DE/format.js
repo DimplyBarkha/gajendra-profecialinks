accdea0e3bc0d142227e14677b62ac9bbc42b4f0
Dima Povarchuk <povarchukdima007@gmail.com>
2021-01-18 15:43:39 +0200

    initial commit

--- src/library/product/details/stores/m/misterspex/DE/format.js
+++ src/library/product/details/stores/m/misterspex/DE/format.js
@@ -91,7 +91,6 @@ const transform = (data) => {
           let info = [];
           row.availabilityText.forEach(item => {
             item.text = item.text.replace(/In den Warenkorb/, "In stock");
-            item.text = item.text.replace(/ /, "Out of stock");
             info.push(item.text);
           });
         };
