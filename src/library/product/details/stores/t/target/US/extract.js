8a81e1474afe0076ad8771bee738e408a27f6f24
Richa Tiwari <richa.t@osmosys.asia>
2021-01-18 12:17:58 +0530

    Updated WITB

--- src/library/product/details/stores/t/target/US/extract.js
+++ src/library/product/details/stores/t/target/US/extract.js
@@ -795,7 +795,7 @@ async function implementation(
             console.log('Shadow root!');
             const headings = Array.from(syndiPowerpage.shadowRoot.querySelectorAll('h2'));
             headings.forEach(h2 => {
-                if (h2.innerText.includes('In the box') || h2.innerText.includes('In The Box') || h2.innerText.includes('in the box')) {
+                if (h2.innerText.includes('In the box') || h2.innerText.includes('In The Box') || h2.innerText.includes('in the box') || h2.innerText.includes('In the Box')) {
                     const parent = h2.parentElement;
                     const inTheBoxEls = parent.querySelectorAll('.syndigo-featureset-feature');
                     inTheBoxEls.forEach(el => {
