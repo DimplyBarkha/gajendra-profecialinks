9decbd3e613c88fdf2f2eb36c6f5aed2455ef073
Gajender Ramola <ramola106@gmail.com>
2021-01-18 20:40:39 +0530

    fix issues

--- src/library/product/search/stores/t/tesco/IE/extract.js
+++ src/library/product/search/stores/t/tesco/IE/extract.js
@@ -9,4 +9,29 @@ module.exports = {
     domain: 'tesco.ie',
     zipcode: '',
   },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(function name () {
+      const searchUrl = window.location.href.replace('%20', ' ');
+      const lastPageUrl = document.querySelector('div#search-url');
+      if (lastPageUrl) {
+        // @ts-ignore
+        lastPageUrl.innerText = searchUrl;
+      } else {
+        const hiddenSearchDiv = document.createElement('div');
+        hiddenSearchDiv.id = 'search-url';
+        hiddenSearchDiv.style.display = 'none';
+        hiddenSearchDiv.textContent = searchUrl;
+        document.body.appendChild(hiddenSearchDiv);
+      }
+    });
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return await context.extract(productDetails, { transform });
+  },
 };
