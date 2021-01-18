535598a860bf12c34b0c83f3f7f032850de2fbb1
Rekha Takalkar <rekha.takalkar@profecialinks.com>
2021-01-18 13:20:46 +0530

    upated file

--- src/library/navigation/goto/domains/co/continente.pt.js
+++ src/library/navigation/goto/domains/co/continente.pt.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'continente.pt',
-    timeout: 30000,
+    timeout: 100000,
     country: 'PT',
     store: 'continente',
     zipcode: '',
