6e619c461fe50352d5b30e648d53224c17dec718
dhanush <dhanush.isukapalli@od.import.io>
2021-01-18 19:39:36 +0530

    hondoscenter by kavya

--- src/library/product/search/stores/h/hondoscenter/GR/extract.js
+++ src/library/product/search/stores/h/hondoscenter/GR/extract.js
@@ -1,28 +1,4 @@
 const { transform } = require('../../../../shared');
-async function implementation(
-    inputs,
-    parameters,
-    context,
-    dependencies,
-) {
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    await context.evaluate(async () => {
-        // while(!!document.querySelector('#search-grid_0 > div.col-12.bloom-load-wrapper > button')){
-        // document.querySelector('#search-grid_0 > div.col-12.bloom-load-wrapper > button').click()
-        try{
-        document.querySelector('#cookies-modal-id > div > div.button-container > a.accept-btn.js-accept').click()
-        await new Promise(r => setTimeout(r, 6000));
-        // }
-        }
-        catch(error)
-        {
-
-        }
-        
-    })
-    return await context.extract(productDetails, { transform });
-}
 module.exports = {
     implements: 'product/search/extract',
     parameterValues: {
@@ -32,5 +8,39 @@ module.exports = {
         domain: 'hondoscenter.com',
         zipcode: '',
     },
-    // implementation,
+    implementation,
 };
+async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
+) {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async function () {
+        function addElementToDocument(key, value) {
+            const catElement = document.createElement('div');
+            catElement.id = key;
+            catElement.textContent = value;
+            catElement.style.display = 'none';
+            document.body.appendChild(catElement);
+        }
+
+        var getXpath = (xpath, prop) => {
+            var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+            let result;
+            if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+            else result = elem ? elem.singleNodeValue : '';
+            return result && result.trim ? result.trim() : result;
+        };
+
+        var upc = getXpath('//td[contains(@class,"Properties")]/table/tbody/tr/td[contains(@class,"UPCs")]/text()', 'nodeValue');
+        if (upc != null) {
+            upc = upc.split(" ")[0];
+            addElementToDocument('upc', upc);
+        }
+});
+return await context.extract(productDetails, { transform });
+};
+
