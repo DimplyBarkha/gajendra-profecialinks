33ba580f8a85dfba8fbe3c73084bcae8b46ce98d
Julia Lojek <julialojek@gmail.com>
2021-01-18 10:32:12 +0100

    #1598 added logic for cases when searching returns one product as product details page

--- src/library/product/search/stores/m/musgravemarketplace/IE/execute.js
+++ src/library/product/search/stores/m/musgravemarketplace/IE/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'musgravemarketplace',
     domain: 'musgravemarketplace.ie',
     url: 'https://order.musgravemarketplace.ie/easyorder/infopr3?parm=8218640604502041964300&srchTrms=1&catSrchTrms={searchTerms}',
-    loadedSelector: 'form#products-list th a img',
+    loadedSelector: 'form#products-list th a img, div#product_image',
     noResultsXPath: '//h2[contains(text(), "Forbidden")]',
     zipcode: '',
   },
--- src/library/product/search/stores/m/musgravemarketplace/IE/extract.js
+++ src/library/product/search/stores/m/musgravemarketplace/IE/extract.js
@@ -16,17 +16,26 @@ module.exports = {
     await context.evaluate(async () => {
       const currentUrl = window.location.href;
       const products = document.querySelectorAll('form#products-list table[class*="list_item"]');
-      products.forEach(product => {
-        product.setAttribute('searchurl', currentUrl);
-      });
+      if (products && products.length > 0) {
+        products.forEach(product => {
+          product.setAttribute('searchurl', currentUrl);
+        });
+      }
+      const isOnSingleProductPage = document.querySelector('div#product_image');
+      if (isOnSingleProductPage) {
+        document.querySelector('body').setAttribute('searchurl', currentUrl);
+      }
     });
 
     var extractedData = await context.extract(productDetails, { transform });
 
     extractedData.forEach(page => {
       page.group.forEach(row => {
+        if (row.productUrl && !row.productUrl[0].text.includes('https')) {
+          row.productUrl[0].text = 'https://order.musgravemarketplace.ie' + row.productUrl[0].text;
+        }
         if (row.thumbnail) {
-          row.thumbnail[0].text = row.thumbnail[0].text.replace('tn', 'lr');
+          row.thumbnail[0].text = row.thumbnail[0].text.replace('tn', 'hr');
         }
       });
     });
