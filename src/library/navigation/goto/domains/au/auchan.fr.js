fb62a814658bcf941876dff1a9367087ffdd6e54
PrinceYadav1 <prince.y@osmosys.asia>
2021-01-18 21:50:34 +0530

    update for custom fields

--- src/library/navigation/goto/domains/au/auchan.fr.js
+++ src/library/navigation/goto/domains/au/auchan.fr.js
@@ -21,5 +21,23 @@ module.exports = {
     await context.setLoadImages(true);
     const inputUrl = `${url}#[!opt!]{"discard_CSP_header":true, "block_ads": false}[/!opt!]`;
     await context.goto(inputUrl, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
+    }
+    await autoScroll(context);
   },
 };
