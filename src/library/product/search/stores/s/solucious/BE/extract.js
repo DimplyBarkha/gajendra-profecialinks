a64040efe54fa0cc71b7612ef1b501995983fe63
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-01-18 20:21:41 +0530

    fixing

--- src/library/product/search/stores/s/solucious/BE/extract.js
+++ src/library/product/search/stores/s/solucious/BE/extract.js
@@ -8,4 +8,31 @@ module.exports = {
     domain: 'solucious.be',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        let scrollTop = 0;
+        while (scrollTop !== 20000) {
+          await stall(1000);
+          scrollTop += 500;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 20000) {
+            await stall(1000);
+            break;
+          }
+        }
+        function stall (ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await applyScroll(context);
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    return await context.extract(productDetails, { transform });
+  },
 };
