ddf85c293397f543f8df97a6ba111434fc8c7fb6
vikas kyatannawar <vikas.k@osmosys.asia>
2021-01-18 15:55:05 +0530

    added dyson extractor

--- src/library/navigation/goto/domains/ha/harveynorman.com.au.js
+++ src/library/navigation/goto/domains/ha/harveynorman.com.au.js
@@ -8,10 +8,10 @@ module.exports = {
     store: 'harveynorman',
     zipcode: "''",
   },
-  
-  /* ----- commented because data is not coming for some urls eg- "https://www.harveynorman.com.au/miele-g-4263-scvi-active-60cm-fully-integrated-dishwasher.html"---- */
 
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+  /* ----- commented because data is not coming for some urls eg- "https://www.harveynorman.com.au/miele-g-4263-scvi-active-60cm-fully-integrated-dishwasher.html"---- */
+  /* There is an issue with got which is causing timeout error from goto without. Need to fix it. */
+  /* implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
     await context.setBlockAds(false);
     await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
@@ -19,25 +19,25 @@ module.exports = {
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
     }
-    await context.goto(url, { waitUntil: 'networkidle0', block_ads: false })
-  async function autoScroll (page) {
-    await page.evaluate(async () => {
-      await new Promise((resolve, reject) => {
-        var totalHeight = 0;
-        var distance = 100;
-        var timer = setInterval(() => {
-          var scrollHeight = document.body.scrollHeight;
-          window.scrollBy(0, distance);
-          totalHeight += distance;
+    await context.goto(url, { waitUntil: 'networkidle0', block_ads: false });
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
 
-          if (totalHeight >= scrollHeight) {
-            clearInterval(timer);
-            resolve();
-          }
-        }, 100);
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
       });
-    });
-  }
-  await autoScroll(context);
-},
+    }
+    await autoScroll(context);
+  }, */
 };
