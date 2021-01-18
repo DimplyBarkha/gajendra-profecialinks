83d0865bdadb46c34f4eb5af485777ad7ad4e281
Sephali-23 <sephalisingh023@gmail.com>
2021-01-18 17:27:04 +0530

    Added custom fields

--- src/library/product/details/stores/m/myer/AU/extract.js
+++ src/library/product/details/stores/m/myer/AU/extract.js
@@ -8,4 +8,22 @@ module.exports = {
     domain: 'myer.com.au',
     zipcode: '',
   },
+  implementation: async function (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { productDetails } = dependencies;
+    const { transform } = parameters;
+    try {
+      await context.waitForSelector('div[class="rec_name"]', { timeout: 30000 });
+      await new Promise((resolve, reject) => setTimeout(resolve, 90000));
+      console.log('selector of inTheBox exist');
+    } catch (e) {
+      console.log("selector of inTheBox doesn't exist");
+    }
+    return await context.extract(productDetails, { transform });
+
+  },
 };
