0c9648f614ba25b51c4e3ac5cf8592022c2d728b
Manas Ranjan <manas.n@osmosys.asia>
2021-01-18 18:08:13 +0530

    Made a few changes for checking and solving captchas.

--- src/library/navigation/goto/domains/ne/newegg.com.js
+++ src/library/navigation/goto/domains/ne/newegg.com.js
@@ -9,7 +9,7 @@ module.exports = {
     store: 'newegg',
     zipcode: '',
   },
- /* implementation: async (
+  implementation: async (
     { url },
     parameters,
     context,
@@ -17,113 +17,19 @@ module.exports = {
   ) => {
     const timeout = parameters.timeout ? parameters.timeout : 30000;
 
-      const responseStatus = await context.goto(url, {
-        firstRequestTimeout: 60000,
-        timeout: 30000,
-        waitUntil: 'load',
-        checkBlocked: false,
-        antiCaptchaOptions: {
-          type: 'RECAPTCHA',
-        },
-      });
-      console.log('status : ', responseStatus.status);
-      console.log('URL : ', responseStatus.url);
-
-    const captchaFrame='iframe[title="recaptcha challenge"]';
-    // console.log(captchaFrame);
-      const maxRetries = 3;
-      let numberOfCaptchas = 0;
-
-      try{
-        await context.waitForSelector(captchaFrame);
-      }catch(e){
-        console.log('error:', e);
-      }
-
-      const checkExistance = async(selector) =>{
-        return await context.evaluate(async (captchaSelector) => {
-          return Boolean(document.querySelector(captchaSelector));
-        }, selector);
-      };
-      const checkRedirection= async () =>{
-        try {
-          await context.waitForNavigation({ timeout });
-          await context.waitForSelector('div.items-grid-view > div > div > a img', { timeout });
-          console.log('Redirected to another page.');
-          return true;
-        } catch (e) {
-          console.log('error: without undescore ', e);
-          return false;
-        }
-      };
-
-      let isCaptchaFramePresent = await checkExistance(captchaFrame);
-      console.log("isCaptcha:"+ isCaptchaFramePresent);
-
-      while (isCaptchaFramePresent && numberOfCaptchas < 3) {
-        console.log('isCaptcha', true);
-        ++numberOfCaptchas;
-        await context.waitForNavigation({ timeout });
-        try {
-          console.log(`Trying to solve captcha - count [${numberOfCaptchas}]`);
-          await context.evaluateInFrame('iframe', () => grecaptcha.execute());  //could't run locally
-          console.log('solved captcha, waiting for page change');
-          await context.waitForNavigation({ timeout });
-          const redirectionSuccess = await checkRedirection();
-
-          if (redirectionSuccess) {
-            await context.evaluate((url) => {
-              window.location.href = url;
-            }, url);
-            await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
-            break;
-          } else {
-            await context.evaluate((url) => {
-              // eslint-disable-next-line no-unused-expressions
-              window.location.reload;
-            });
-            await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
-          }
-
-          isCaptchaFramePresent = await checkExistance(captchaFrame);
-        } catch (e) {
-          console.log('error: without undescore ', e);
-        }
-      }
-
-      isCaptchaFramePresent = await checkExistance(captchaFrame);
-
-      if (isCaptchaFramePresent) {
-        throw new Error('Failed to solve captcha');
-      }
-
-    await context.evaluate(async () => {
-      function addHiddenDiv (id, content) {
-        const newDiv = document.createElement('div');
-        newDiv.id = id;
-        newDiv.textContent = content;
-        newDiv.style.display = 'none';
-        document.body.appendChild(newDiv);
-      }
-      if (document.getElementsByTagName('video')) {
-        const vLink = parent.document.getElementById('wcframable1-0');
-        if (vLink) {
-          var videoLink = vLink.contentWindow.document.getElementsByTagName('video').vjs_video_1_html5_api;
-          // var link = '';
-          if (videoLink) {
-            var link = videoLink.src;
-          }
-        }
-      }
-      const links = [];
-      if (document.querySelector('div.wc-reset > ul > li > div > div > img.wc-media.wc-video')) {
-        document.querySelectorAll('div.wc-reset > ul > li > div > div > img.wc-media.wc-video').forEach(e => {
-          links.push(e.getAttribute('wcobj'));
-        });
-      }
-      if (document.querySelector('div.wc-reset > ul > li > div > div > img.wc-media.wc-video')) addHiddenDiv('video', links.join(' | '));
-      addHiddenDiv('video', link);
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    const responseStatus = await context.goto(url, {
+      firstRequestTimeout: 60000,
+      timeout: 30000,
+      waitUntil: 'load',
+      checkBlocked: false,
+      antiCaptchaOptions: {
+        type: 'RECAPTCHA',
+      },
     });
+    
+    console.log('status : ', responseStatus.status);
+    console.log('URL : ', responseStatus.url);
   },
-*/
 };
--- src/library/product/search/stores/n/newegg/US/extract.js
+++ src/library/product/search/stores/n/newegg/US/extract.js
@@ -1,7 +1,6 @@
-
 const { transform } = require('./transform');
 
-async function implementation (
+async function implementation(
   inputs,
   parameters,
 
@@ -10,8 +9,28 @@ async function implementation (
 
 ) {
   const { transform } = parameters;
-  async function addUrl () {
-    function addHiddenDiv (id, content) {
+
+  const optionalWaiter = async (selector) => {
+    try {
+      await context.waitForSelector(selector, { timeout: 15000 });
+      return true;
+    } catch (e) {
+      console.log(`Selector didn't load => ${selector}`);
+      return false;
+    }
+  };
+
+  const captcha = await optionalWaiter('#g-recaptcha');
+
+  if (captcha) {
+    await context.evaluateInFrame('iframe[src*="recaptcha/api2"], iframe[_src*="recaptcha/api2"]', () => grecaptcha.execute());
+
+    console.log('Captcha solved, waiting for product to load.');
+    await optionalWaiter('div.items-grid-view > div > div > a img');
+  }
+
+  async function addUrl() {
+    function addHiddenDiv(id, content) {
       const newDiv = document.createElement('div');
       newDiv.id = id;
       newDiv.textContent = content;
