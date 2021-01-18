b3e723daf523ac03523439c2d6bfec5b6821f62c
rajeshProfecialinks <rajesh.g@profecialinks.com>
2021-01-18 18:43:15 +0530

    UAT product distribution fail issue fix

--- src/library/product/search/stores/m/myntra/IN/execute.js
+++ src/library/product/search/stores/m/myntra/IN/execute.js
@@ -1,3 +1,66 @@
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
+  await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+  try{
+    //
+    await context.waitForSelector('button#onetrust-accept-btn-handler');
+    await new Promise((resolve, reject) => setTimeout(resolve, 500));
+    await context.click('button#onetrust-accept-btn-handler');
+  }catch(e){
+    
+  }
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
+  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  await new Promise((resolve, reject) => setTimeout(resolve, 9000));
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/search/execute',
@@ -10,4 +73,5 @@ module.exports = {
     noResultsXPath: "//img[@class='index-notFoundImage']",
     zipcode: '',
   },
+  implementation,
 };
