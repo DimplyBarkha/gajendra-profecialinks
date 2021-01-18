d0c133b1bb0c98d0298f6f8879d108cb9b486376
osmakash <akashkumar.p@osmosys.asia>
2021-01-18 12:48:36 +0530

    Adding the WTIB content

--- src/library/navigation/goto/domains/ha/harveynorman.com.au.js
+++ src/library/navigation/goto/domains/ha/harveynorman.com.au.js
@@ -11,13 +11,33 @@ module.exports = {
   
   /* ----- commented because data is not coming for some urls eg- "https://www.harveynorman.com.au/miele-g-4263-scvi-active-60cm-fully-integrated-dishwasher.html"---- */
 
-  // implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-  //   const timeout = parameters.timeout ? parameters.timeout : 10000;
-  //   await context.setBlockAds(false);
-  //   await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
-  //   console.log(zipcode);
-  //   if (zipcode) {
-  //     await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
-  //   }
-  // },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+    await context.goto(url, { waitUntil: 'networkidle0', block_ads: false })
+  async function autoScroll (page) {
+    await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+        var totalHeight = 0;
+        var distance = 100;
+        var timer = setInterval(() => {
+          var scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+
+          if (totalHeight >= scrollHeight) {
+            clearInterval(timer);
+            resolve();
+          }
+        }, 100);
+      });
+    });
+  }
+  await autoScroll(context);
+},
 };
