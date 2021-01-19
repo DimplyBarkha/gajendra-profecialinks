56ee90f108943ad1b49e3a369326b03a4d2ee913
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-18 16:53:51 -0800

    Add fix to load UPDP

--- src/library/navigation/goto/domains/wa/walmart.ca.js
+++ src/library/navigation/goto/domains/wa/walmart.ca.js
@@ -8,4 +8,20 @@ module.exports = {
     store: 'walmart',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+
+    context.setBlockAds(false);
+    context.setLoadAllResources(true);
+    context.setAntiFingerprint(false);
+    await context.setJavaScriptEnabled(true);
+
+    // await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true});
+    await context.goto(url, {
+      firstRequestTimeout: 1000000,
+      timeout,
+      waitUntil: 'load',
+      checkBlocked: false,
+    });
+  },
 };
--- src/library/product/details/stores/w/walmart/CA/extract.js
+++ src/library/product/details/stores/w/walmart/CA/extract.js
@@ -8,4 +8,25 @@ module.exports = {
     domain: 'walmart.ca',
     zipcode: '',
   },
+  dependencies: {
+    goto: 'action:navigation/goto',
+    createUrl: 'action:product/details/createUrl',
+    productDetails: 'extraction:product/details/stores/${store[0:1]}/${store}/${country}/extract',
+    helperModule: 'module:helpers/helpers',
+  },
+  // @ts-ignore
+  implementation: async ({ parentInput }, { country, domain, transform: transformParam }, context, dependencies) => {
+    const { helperModule: { Helpers } } = dependencies;
+    const helper = new Helpers(context);
+    // scroll to bottom of page
+    await context.evaluate(async () => {
+      await window.scrollTo(0, document.body.scrollHeight);
+    });
+    // scroll to iframe
+    await context.evaluate((selector) => {
+      const updp = document.querySelector(selector);
+      if (updp) updp.scrollIntoView();
+    }, 'div[data-automation="RichRelevanceCarousel"]');
+    return await context.extract(dependencies.productDetails, { transform: transformParam });
+  },
 };
