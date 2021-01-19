f3b394a2d42886a825309e514d3f7b4cef622cf4
waquas ali <waquas2ali@gmail.com>
2021-01-19 20:38:47 +0530

    updating changes...

--- src/library/navigation/goto/domains/ap/aptekagemini.pl.js
+++ src/library/navigation/goto/domains/ap/aptekagemini.pl.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'aptekagemini.pl',
-    timeout: 50000,
+    timeout: 500000,
     country: 'PL',
     store: 'aptekagemini',
     zipcode: '',
