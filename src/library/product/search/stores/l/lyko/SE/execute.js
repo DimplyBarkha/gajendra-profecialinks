161c94aa6c8e0fdc6601d565e24b82df34ee879f
unknown <madhan.selvaraj-odc@partner.import.io>
2021-01-19 15:11:39 +0530

    search-lyko

--- src/library/product/search/stores/l/lyko/SE/execute.js
+++ src/library/product/search/stores/l/lyko/SE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'SE',
     store: 'lyko',
     domain: 'lyko.com',
-    url: 'https://lyko.com/sv/sok?q={searchTerms}',
+    url: 'https://lyko.com/sv/sok?q={searchTerms}&sortBy=popularity&count=150&infinitescroll=1',
     // url: 'https://lyko.com/sv/sok?q=after+shave',
     loadedSelector: 'body',
     noResultsXPath: null,
--- src/library/product/search/stores/l/lyko/SE/extract.js
+++ src/library/product/search/stores/l/lyko/SE/extract.js
@@ -7,13 +7,33 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await context.evaluate(async () => {
-    while(!!document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button")){
-      // @ts-ignore
-      document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button").click()
-      await new Promise(r => setTimeout(r, 10000));
+  // await context.evaluate(async () => {
+  //   while(!!document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button")){
+  //     // @ts-ignore
+  //     document.querySelector("#container > div > div:nth-child(1) > div > main > div > div:nth-child(1) > div._2megZ9._3-XKWb > button").click()
+  //     await new Promise(r => setTimeout(r, 10000));
+  //   }
+  // })
+  const applyScroll = async function (context) {
+  await context.evaluate(async function () {
+    let scrollTop = 0;
+    while (scrollTop !== 1000) {
+      scrollTop += 500;
+      window.scroll(0, scrollTop);
+      await stall(1000);
+    }
+    function stall(ms) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
     }
   })
+}
+  await applyScroll(context);
+
+  
   return await context.extract(productDetails, { transform });
 }
 
