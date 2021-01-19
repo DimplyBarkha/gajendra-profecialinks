4d244e13475e12449460389c7bac06e58ab60a57
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-01-19 12:24:20 +0100

    #2599 - Applying fixes to the core extractor - Updating tests

--- src/library/product/details/stores/m/monsterpetsupplies/UK/extract.js
+++ src/library/product/details/stores/m/monsterpetsupplies/UK/extract.js
@@ -1,12 +1,13 @@
+// @ts-nocheck
 const { cleanUp } = require('../../../../shared');
 
-async function implementation ({ url, id }, { transform }, context, { productDetails }) {
+async function implementation({ url, id }, { transform }, context, { productDetails }) {
   const prodId = id;
   await context.evaluate(async (prodId) => {
     var getSiblings = function (elem) {
       // Setup siblings array and get the first sibling
-      var siblings = [];
-      var sibling = elem.parentNode.firstChild;
+      const siblings = [];
+      let sibling = elem.parentNode.firstChild;
 
       // Loop through each sibling and push to the array
       while (sibling) {
@@ -15,36 +16,50 @@ async function implementation ({ url, id }, { transform }, context, { productDet
         }
         sibling = sibling.nextSibling;
       }
-
       return siblings;
     };
 
-    function addElementToDom (element, id) {
+    function addElementToDom(element, id) {
       const div = document.createElement('div');
       div.id = id;
       div.innerHTML = element;
       document.body.appendChild(div);
     }
 
-    const skuNode = document.querySelector('meta[content="' + prodId + '"]');
+    const skuNode = document.querySelector(`meta[content="${prodId}"]`);
     const productNode = getSiblings(skuNode);
     addElementToDom(prodId, 'sku');
     productNode.forEach((element) => {
-      addElementToDom(
-        element.getAttribute('content'),
-        element.getAttribute('itemprop'));
+      addElementToDom(element.getAttribute('content'), element.getAttribute('itemprop'));
     });
 
     const parentProdNode = skuNode.parentElement.parentElement;
     parentProdNode.setAttribute('target', 'to-add');
 
     const availRegExp = /http:\/\/schema\.org\/(.+)/;
-    const elem = document.querySelector('div[id="availability"]');
-
-    if (elem) {
-      const availRaw = elem.textContent.match(availRegExp)[1];
-      elem.textContent = availRaw === 'InStock' ? 'In Stock' : 'Out Of Stock';
+    const availabilityElem = document.querySelector('div[id="availability"]');
+    if (availabilityElem) {
+      const availRaw = availabilityElem.textContent.match(availRegExp)[1];
+      availabilityElem.textContent = availRaw === 'InStock' ? 'In Stock' : 'Out Of Stock';
     }
+
+    const variantElem = document.evaluate(
+      `//div[@class="price-wrap" and contains(. , "${prodId}")]`,
+      document,
+      null,
+      XPathResult.ANY_UNORDERED_NODE_TYPE,
+      null,
+    ).singleNodeValue;
+    const nameElem = document.querySelector('div[id="name"]');
+    const nameMatch = nameElem && nameElem.textContent.match(/((\d+)([,.]?\d+)?\s?\w{1,2})/);
+    if (nameMatch) addElementToDom(nameElem.textContent.match(/((\d+)([,.]?\d+)?\s?\w{1,2})/)[1], 'quantity');
+
+    const descriptionElem = document.querySelector('div[itemprop="description"]');
+    const description = descriptionElem ? descriptionElem.innerText.replace(/\n+/g, ' ').trim() : '';
+    addElementToDom(description, 'description');
+
+    const ingredients = document.evaluate('//div[@itemprop="description"]//p[contains(. , "Ingredients")]/following-sibling::p[position() = 1]', document, null, XPathResult.STRING_TYPE, null).stringValue;
+    addElementToDom(ingredients, 'ingredients');
 
     document.querySelector('img[class="zoomImg"]') ? addElementToDom('Yes', 'Zoom') : addElementToDom('No', 'Zoom');
   }, prodId);
