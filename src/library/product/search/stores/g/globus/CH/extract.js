147692b2b469f6de18bc7b4c973ee41b099a4a59
avinashkadam <avinash.kadam@profecialinks.com>
2021-01-19 23:01:53 +0530

    scroller issue

--- src/library/product/search/stores/g/globus/CH/extract.js
+++ src/library/product/search/stores/g/globus/CH/extract.js
@@ -7,30 +7,49 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  const applyScroll = async function (context) {
-    console.log('calling applyScroll-----------');
-    await context.evaluate(async function () {
-      let scrollTop = 0;
-      while (scrollTop !== 30000) {
-        await stall(1000);
-        scrollTop += 2000;
-        console.log('calling applyScroll evaluate-----------', window);
-        window.scroll(0, scrollTop);
-        if (scrollTop === 30000) {
-          await stall(5000);
-          break;
-        }
+  // const applyScroll = async function (context) {
+  //   console.log('calling applyScroll-----------');
+  //   await context.evaluate(async function () {
+  //     let scrollTop = 0;
+  //     while (scrollTop !== 20000) {
+  //       await stall(1000);
+  //       scrollTop += 1000;
+  //       console.log('calling applyScroll evaluate-----------', window);
+  //       window.scroll(0, scrollTop);
+  //       if (scrollTop === 20000) {
+  //         await stall(5000);
+  //         break;
+  //       }
+  //     }
+  //     function stall (ms) {
+  //       return new Promise((resolve, reject) => {
+  //         setTimeout(() => {
+  //           resolve();
+  //         }, ms);
+  //       });
+  //     }
+  //   });
+  // };
+  // await applyScroll(context);
+ await context.evaluate(async () => {
+   await new Promise((resolve) => setTimeout(resolve, 5000));
+
+      async function infiniteScroll() {
+          let prevScroll = document.documentElement.scrollTop;
+          while (true) {
+              window.scrollBy(0, document.documentElement.clientHeight);
+              await new Promise(resolve => setTimeout(resolve, 1000));
+              const currentScroll = document.documentElement.scrollTop;
+              if (currentScroll === prevScroll) {
+                  break;
+              }
+              prevScroll = currentScroll;
+          }
       }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
+      await infiniteScroll();
+      await new Promise((resolve) => setTimeout(resolve, 8000));
+
     });
-  };
-  await applyScroll(context);
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
