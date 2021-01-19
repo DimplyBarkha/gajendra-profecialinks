827550128e9f6505c5e42baf72876d4603df1923
alscotty <alawrencescott@gmail.com>
2021-01-19 14:04:45 -0800

    update to get first url also

--- src/library/product/listing/stores/r/reviews/UK/extract.js
+++ src/library/product/listing/stores/r/reviews/UK/extract.js
@@ -1,5 +1,40 @@
 const { transform } = require('../../../../shared');
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform, urlTemplate, resultsCountSelector, numberResultPerPage, regExpForIdFromUrl } = parameters;
+  const { productDetails, Helpers: { Helpers } } = dependencies;
+  const helper = new Helpers(context);
+
+  const resultsCount = await context.evaluate((resultsCountSelector) => {
+    return document.querySelector(resultsCountSelector).textContent.replace(',', '');
+  }, resultsCountSelector);
+
+  const totalPages = Number(resultsCount) / numberResultPerPage;
+
+  const currentUrl = await context.evaluate(() => {
+    return window.location.href;
+  });
+  const urlArray = [currentUrl];
+
+  const itemId = regExpForIdFromUrl ? String(currentUrl).match(regExpForIdFromUrl)[0] : '';
+
+  for (let i = 1; i < totalPages; i++) {
+    urlArray.push(urlTemplate
+      .replace('{id}', itemId)
+      .replace('{page}', i));
+  }
+
+  await helper.addArrayToDocument('my-urls', urlArray);
+  await helper.addItemToDocument('my-results-count', resultsCount);
+
+  return await context.extract(productDetails, { transform });
+}
+
 module.exports = {
   implements: 'product/listing/extract',
   parameterValues: {
@@ -13,4 +48,5 @@ module.exports = {
     domain: 'reviews.co.uk',
     zipcode: '',
   },
+  implementation,
 };
