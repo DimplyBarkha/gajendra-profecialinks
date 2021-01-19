1ca9be6306635b901aad3b0997467d99e7def2cd
Lokanath sahoo <loknath.s@osmosys.asia>
2021-01-19 20:19:27 +0530

    fixed the UAT issues

--- src/library/product/search/stores/r/rossmann/DE/paginate.js
+++ src/library/product/search/stores/r/rossmann/DE/paginate.js
@@ -4,11 +4,12 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'rossmann',
-    openSearchDefinition: {
-      template: 'https://www.rossmann.de/de/search?q={searchTerms}%3Arelevance&page={index}&pageSize=60#',
-      pageIndexMultiplier: 1,
-      pageStartNb: 0,
-    },
+    // openSearchDefinition: {
+    //   template: 'https://www.rossmann.de/de/search?q={searchTerms}%3Arelevance&page={index}&pageSize=60#',
+    //   pageIndexMultiplier: 1,
+    //   pageStartNb: 0,
+    // },
+    nextLinkSelector: 'div.rm-siteselect__wrapper--product > div[class*="pagination--bottom"] li[class*="next"]>a>span',
     domain: 'rossmann.de',
     zipcode: '',
   },
