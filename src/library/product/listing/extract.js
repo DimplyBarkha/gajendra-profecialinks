c4edb81b8d36faf0188488b55ccc8e2238fee9d1
alscotty <alawrencescott@gmail.com>
2021-01-19 09:54:03 -0800

    add rest to listing templates

--- src/library/product/listing/extract.js
+++ src/library/product/listing/extract.js
@@ -5,38 +5,68 @@
  * @param { ImportIO.IContext } context
  * @param { Record<string, any> } dependencies
  */
-async function implementation(
-    inputs,
-    parameters,
-    context,
-    dependencies,
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
 ) {
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    return await context.extract(productDetails, { transform });
+  const { transform, urlTemplate, resultsCountSelector, numberResultPerPage } = parameters;
+  const { productDetails, Helpers } = dependencies;
+  const helper = new Helpers(context);
+
+  const resultsCount = await context.evaluate((resultsCountSelector) => {
+    return document.querySelector(resultsCountSelector).replace(',', '');
+  }, resultsCountSelector);
+
+  const totalPages = Number(resultsCount) / numberResultPerPage;
+
+  const urlArray = [];
+
+  for (let i = 1; i < totalPages; i++) {
+    urlArray.push(urlTemplate.replace('{page}', i));
+  }
+
+  await helper.addArrayToDocument('my-urls', urlArray);
+  await helper.addItemToDocument('my-results-count', resultsCount);
+
+  return await context.extract(productDetails, { transform });
 }
 
 module.exports = {
-    parameters: [
-        {
-            name: 'country',
-            description: '2 letter ISO code for the country',
-        },
-        {
-            name: 'store',
-            description: 'store name',
-        },
-        {
-            name: 'transform',
-            description: 'transform function for the extraction',
-            optional: true,
-        },
-    ],
-    inputs: [
-    ],
-    dependencies: {
-        productDetails: 'extraction:product/listing/stores/${store[0:1]}/${store}/${country}/extract',
+  parameters: [
+    {
+      name: 'country',
+      description: '2 letter ISO code for the country',
     },
-    path: './stores/${store[0:1]}/${store}/${country}/extract',
-    implementation,
+    {
+      name: 'store',
+      description: 'store name',
+    },
+    {
+      name: 'urlTemplate',
+      description: 'template for desired urls, with {page}',
+    },
+    {
+      name: 'resultsCountSelector',
+      description: 'selector for total number of results',
+    },
+    {
+      name: 'numberResultPerPage',
+      description: 'set as number of results shown per page',
+    },
+    {
+      name: 'transform',
+      description: 'transform function for the extraction',
+      optional: true,
+    },
+  ],
+  inputs: [
+  ],
+  dependencies: {
+    Helpers: 'module:helpers/helpers',
+    productDetails: 'extraction:product/listing/stores/${store[0:1]}/${store}/${country}/extract',
+  },
+  path: './stores/${store[0:1]}/${store}/${country}/extract',
+  implementation,
 };
