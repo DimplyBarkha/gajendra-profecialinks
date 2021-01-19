952be9ac8193062d39ec2212ee1207a59180f543
Gajender Ramola <ramola106@gmail.com>
2021-01-19 11:08:19 +0530

    fix issues

--- src/library/product/search/stores/h/haar-shop/CH/format.js
+++ src/library/product/search/stores/h/haar-shop/CH/format.js
@@ -3,8 +3,8 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
-  const cleanUp = (data, context) => {
+const transform = (data, context) => {
+  const cleanUp = (data) => {
     const clean = text => text.toString()
       .replace(/\r\n|\r|\n/g, ' ')
       .replace(/&amp;nbsp;/g, ' ')
@@ -22,8 +22,9 @@ const transform = (data) => {
     }))));
     return data;
   };
+  const state = context.getState();
+  let rank = state.rank || 1;
   for (const { group } of data) {
-    let rank = 1;
     for (const row of group) {
       // if (row.thumbnail) {
       //   row.thumbnail.forEach(item => {
@@ -34,6 +35,7 @@ const transform = (data) => {
       rank++;
     }
   }
+  context.setState({ rank });
   return cleanUp(data);
 };
 
