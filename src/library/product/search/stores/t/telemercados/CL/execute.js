dd24b558bf570042a333db8f84925b783939db11
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2021-01-19 19:57:03 +0530

    Fixed issue

--- src/library/product/search/stores/t/telemercados/CL/execute.js
+++ src/library/product/search/stores/t/telemercados/CL/execute.js
@@ -15,51 +15,21 @@ async function implementation (
   console.log('params', parameters);
   const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
   await dependencies.goto({ url, zipcode: inputs.zipcode });
-  const applyScroll = async function (context) {
-    await context.evaluate(async function () {
-      let scrollTop = 0;
-      while (scrollTop !== 20000) {
-        await stall(500);
-        scrollTop += 1000;
-        window.scroll(0, scrollTop);
-        if (scrollTop === 20000) {
-          await stall(5000);
-          break;
-        }
-      }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-    });
-  };
 
-  try{
-    await context.waitForSelector('button[data-dismiss="modal"].btn-custom, left-btn',{timeout:1000});
-    await context.click('button[data-dismiss="modal"].btn-custom, left-btn',{timeout:1000});
-  }catch(e){
+  try {
+    await context.waitForSelector('button[data-dismiss="modal"].btn-custom, left-btn', { timeout: 1000 });
+    await context.click('button[data-dismiss="modal"].btn-custom, left-btn', { timeout: 1000 });
+  } catch (e) {
 
   }
 
-  try{
-    await context.waitForSelector('div.pe-optin-6_cancelBtnWrapper>div',{timeout:1000});
-    await context.click('div.pe-optin-6_cancelBtnWrapper>div',{timeout:1000});
-  }catch(e){
+  try {
+    await context.waitForSelector('div.pe-optin-6_cancelBtnWrapper>div', { timeout: 1000 });
+    await context.click('div.pe-optin-6_cancelBtnWrapper>div', { timeout: 1000 });
+  } catch (e) {
 
   }
 
-  for(let i=0;i<5;i++){
-    try{
-      await applyScroll(context);
-      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
-    }catch(e){
-
-    }
-  }
-  
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
