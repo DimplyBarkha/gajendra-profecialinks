e1b2e46b58aae3c473f46dd26e45f5ca38d63898
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-01-20 21:24:42 +0530

    added new extractor with correct store name

---
+++ src/library/product/details/stores/l/LensOnline_NL/BE/execute.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'product/details/execute',
+  parameterValues: {
+    country: 'BE',
+    store: 'LensOnline_NL',
+    domain: 'lensonline.nl',
+    loadedSelector: null,
+    noResultsXPath: null,
+    zipcode: '',
+  },
+};
