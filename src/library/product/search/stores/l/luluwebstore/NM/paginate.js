76197586dfa19ad6a2b2e004a0f30f3233b8754d
Dhanush <dhanush.isukapalli@od.import.io>
2021-01-20 15:10:51 +0530

    luluwebstore_NM by dhanush

--- src/library/product/search/stores/l/luluwebstore/NM/paginate.js
+++ src/library/product/search/stores/l/luluwebstore/NM/paginate.js
@@ -11,7 +11,7 @@ module.exports = {
     spinnerSelector: null,
     // loadedSelector: 'ul[class="product__listing product__grid col-xs-12"] div[class="col-lg-3 col-md-4 col-sm-6 col-xs-6 plp-product-div js-owl-carousel-reference "]',
     loadedXpath: null,
-    noResultsXPath: "//div[contains(@class,'search-empty')]",
+    // noResultsXPath: "//div[contains(@class,'search-empty')]",
     stopConditionSelectorOrXpath: null,
     resultsDivSelector: null,
     openSearchDefinition: null,
