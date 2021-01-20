dab203b77dd1491749fa9368821c031f0ff0aed4
chetan-profecia <chetan.shelake@profecialinks.com>
2021-01-20 17:20:45 +0530

    brand search filter implement

--- src/library/product/reviews/action.js
+++ src/library/product/reviews/action.js
@@ -1,7 +1,7 @@
 
 /**
  *
- * @param { { URL: string, id: any, RPC: string, SKU: string, date: any, days: number, results} } inputs
+ * @param { { URL: string, id: any, RPC: string, SKU: string, date: any, days: number, results, Brands: any} } inputs
  * @param { { store: any, domain: any, country: any, zipcode: any, mergeType: any } } parameters
  * @param { ImportIO.IContext } context
  * @param { { execute: ImportIO.Action, extract: ImportIO.Action, paginate: ImportIO.Action } } dependencies
@@ -12,14 +12,14 @@ async function implementation (
   context,
   { execute, extract, paginate },
 ) {
-  const { URL: url, RPC, SKU, date: dateOrigin = null, days = 30, results = 10000 } = inputs;
+  const { URL: url, RPC, SKU, date: dateOrigin = null, days = 30, results = 10000, Brands } = inputs;
   const id = RPC || SKU || inputs.id;
   const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
 
   const date = new Date(days ? new Date().setDate(new Date().getDate() - days) : dateOrigin);
   console.log(`Date Limit: "${date}"`);
 
-  const resultsReturned = await execute({ url, id, zipcode, date, days });
+  const resultsReturned = await execute({ url, id, zipcode, date, days, Brands });
 
   if (!resultsReturned) {
     console.log('No results were returned');
