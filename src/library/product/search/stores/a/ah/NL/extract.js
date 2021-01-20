ce42c4a29eb7e60884446c1e8630b4f47dc3193e
Mamatha-Gundapaneni <mamatha.g@osmosys.asia>
2021-01-20 15:23:11 +0530

    Updated code

--- src/library/product/search/stores/a/ah/NL/extract.js
+++ src/library/product/search/stores/a/ah/NL/extract.js
@@ -18,6 +18,10 @@ module.exports = {
     const { productDetails } = dependencies;
     const { transform } = parameters;
     await context.evaluate(() => {
+      const cookies = document.querySelector('button[id="accept-cookies"]');
+      if (cookies) {
+        cookies.click();
+      }
       const searchUrl = window.location.href;
       // @ts-ignore
       const appendElements = [...document.querySelectorAll('article[data-testhook="product-card"]')];
