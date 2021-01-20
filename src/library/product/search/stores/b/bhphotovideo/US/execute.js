7cae20ee212e3b63cf12d155999b93a8551c5f5e
astropaula <wojciechowska.fran@gmail.com>
2021-01-20 17:08:21 +0100

    Changed noResultsXpath

--- src/library/product/search/stores/b/bhphotovideo/US/execute.js
+++ src/library/product/search/stores/b/bhphotovideo/US/execute.js
@@ -38,7 +38,7 @@ module.exports = {
     domain: 'bhphotovideo.com',
     url: 'https://www.bhphotovideo.com/c/search?Ntt={searchTerms}&N=0&InitialSearch=yes&sts=ma',
     loadedSelector: 'div[data-selenium="miniProductPage"]',
-    noResultsXPath: '//div[@class="message_FdUqQ7Ey65cURVJ_vVe-f"] | //h1[@class="title_2Tkgx8jFMHLoxqcKbZDI7v"] | //section[@class="body-404"]',
+    noResultsXPath: '//h1[@class="title_2Tkgx8jFMHLoxqcKbZDI7v"] | //section[@class="body-404"]',
     zipcode: '',
   },
   implementation,
