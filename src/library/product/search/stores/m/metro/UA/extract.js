e8379703f6874fedf5dd1eb7aeba9411753ba5d3
Bartosz Kurzawa <kurzawa.bart@gmail.com>
2021-01-20 11:27:17 +0100

    Adjusted fields

--- src/library/product/search/stores/m/metro/UA/extract.js
+++ src/library/product/search/stores/m/metro/UA/extract.js
@@ -10,8 +10,12 @@ async function implementation (inputs, parameters, context, dependencies) {
     const prefix = 'https://metro.zakaz.ua';
     allProducts.forEach((product) => {
       const productUrl = product.querySelector('a.product-tile').getAttribute('href');
+      product.setAttribute('search-url', window.location.href);
       product.setAttribute('product-url', prefix.concat(productUrl));
-      product.setAttribute('product-id', product.querySelector('a.product-tile').getAttribute('data-productKey'));
+      product.setAttribute('product-id', product.querySelector('a.product-tile').getAttribute('data-productKey').replace('metro', ''));
+      // @ts-ignore
+      product.setAttribute('product-price', product.querySelector('div[data-marker="Discounted Price"] > span[class*=Price__value]')
+        ? product.querySelector('div[data-marker="Discounted Price"] > span[class*=Price__value]').textContent.replace('.', ',') : '');
     });
   });
   return await context.extract(productDetails, { transform });
