5b543ec5dd1bb02bb612600b86f86735610e3a1a
Gajender Ramola <ramola106@gmail.com>
2021-01-20 16:52:46 +0530

    fix ixxues

--- src/library/product/search/stores/l/linsenmax/CH/format.js
+++ src/library/product/search/stores/l/linsenmax/CH/format.js
@@ -3,16 +3,16 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
-  const cleanUp = (data, context) => {
+const transform = (data, context) => {
+  const cleanUp = (data) => {
     const clean = text => text.toString()
       .replace(/\r\n|\r|\n/g, ' ')
       .replace(/&amp;nbsp;/g, ' ')
       .replace(/&amp;#160/g, ' ')
       .replace(/\u00A0/g, ' ')
       .replace(/\s{2,}/g, ' ')
-      // .replace(/"\s{1,}/g, '"')
-      // .replace(/\s{1,}"/g, '"')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
       .replace(/^ +| +$|( )+/g, ' ')
       // eslint-disable-next-line no-control-regex
       .replace(/[\x00-\x1F]/g, '')
@@ -22,7 +22,8 @@ const transform = (data) => {
     }))));
     return data;
   };
-  let rank = 1;
+  const state = context.getState();
+  let rank = state.rank || 1;
   for (const { group } of data) {
     for (const row of group) {
       if (row.productUrl) {
@@ -40,6 +41,7 @@ const transform = (data) => {
       rank++;
     }
   }
+  context.setState({ rank });
   return cleanUp(data);
 };
 
