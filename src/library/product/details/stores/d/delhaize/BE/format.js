e7de69594ec04e56d551ec68ee179555a6abfe1f
Navjyot Gill <navjyotg@synerzipune.local>
2021-01-20 15:30:13 +0530

    issues fixed

--- src/library/product/details/stores/d/delhaize/BE/format.js
+++ src/library/product/details/stores/d/delhaize/BE/format.js
@@ -1,37 +1,127 @@
+
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {  
-    const cleanUp = (data, context) => {
-      const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
+const transform = (data) => {
+  const cleanUp = (data, context) => {
+    let dataStr = JSON.stringify(data);
+    console.log('INSIDE OF CLEANUP');
+    dataStr = dataStr.replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
       .replace(/&amp;nbsp;/g, ' ')
       .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
+      .replace(/\\u00A0/g, ' ')
       .replace(/\s{2,}/g, ' ')
       .replace(/"\s{1,}/g, '"')
       .replace(/\s{1,}"/g, '"')
       .replace(/^ +| +$|( )+/g, ' ')
       // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-      data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-      }))));
-      return data;
-    };
-    for (const { group } of data) {
-        for (let row of group) {              
-            if(row.image){
-                row.image.forEach(item => {
-                    var spltImg = item.text.split('?');
-                    item.text = 'https:'+spltImg[0]; 
-                })
-            }         
-        }
-    }
-    return cleanUp(data);
+      .replace(/[^\x00-\x7F]/g, '');
+
+    return JSON.parse(dataStr);
   };
-  module.exports = { transform };
+  for (const { group } of data) {
+    for (let row of group) {
+      if (row.caloriesPerServing) {
+        let text = '';
+        row.caloriesPerServing.forEach(item => {
+          text += `${item.text.replace(/mg|g/, '')}`;
+        });
+        row.caloriesPerServing = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if (row.totalFatPerServing) {
+        let text = '';
+        row.totalFatPerServing.forEach(item => {
+          text += `${item.text.replace(/mg|g/, '')}`;
+        });
+        row.totalFatPerServing = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if (row.saturatedFatPerServing) {
+        let text = '';
+        row.saturatedFatPerServing.forEach(item => {
+          text += `${item.text.replace(/mg|g/, '')}`;
+        });
+        row.saturatedFatPerServing = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if (row.saltPerServing) {
+        let text = '';
+        row.saltPerServing.forEach(item => {
+          text += `${item.text.replace(/mg|g/, '')}`;
+        });
+        row.saltPerServing = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if (row.totalCarbPerServing) {
+        let text = '';
+        row.totalCarbPerServing.forEach(item => {
+          text += `${item.text.replace(/mg|g/, '')}`;
+        });
+        row.totalCarbPerServing = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if (row.totalSugarsPerServing) {
+        let text = '';
+        row.totalSugarsPerServing.forEach(item => {
+          text += `${item.text.replace(/mg|g/, '')}`;
+        });
+        row.totalSugarsPerServing = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if (row.proteinPerServing) {
+        let text = '';
+        row.proteinPerServing.forEach(item => {
+          text += `${item.text.replace(/mg|g/, '')}`;
+        });
+        row.proteinPerServing = [
+          {
+            text: text,
+          },
+        ];
+      }
+      if(row.availabilityText){
+        row.availabilityText.forEach(item => {
+          item.text = item.text.replace(/\n/g, " ").replace(/\s\s+/g, ' ');
+        });
+
+      }
+      if (row.calciumPerServing) {
+        let text = '';
+        row.calciumPerServing.forEach(item => {
+          text += `${item.text.replace(/mg|g/, '')}`;
+        });
+        row.calciumPerServing = [
+          {
+            text: text,
+          },
+        ];
+      }
+      row = cleanUp(row);
+    }
+    
+  }
+  return data;
+};
+
+module.exports = { transform };
