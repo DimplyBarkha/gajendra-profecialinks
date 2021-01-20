94acdc2802742a458b419aeeafa008d810ca7f43
Vivek Gondil <gondilvivek@gmail.com>
2021-01-20 22:48:23 +0530

    Added the changes for T7

--- src/library/product/details/stores/d/dis-chem/ZA/execute.js
+++ src/library/product/details/stores/d/dis-chem/ZA/execute.js
@@ -5,51 +5,33 @@ module.exports = {
     store: 'dis-chem',
     domain: 'dischem.co.za',
     loadedSelector: '.product-image-photo',
-    noResultsXPath: null,
+    noResultsXPath: '//div[contains(@class,"message info empty")]',
     zipcode: '',
   },
-  implementation: async ({ url, id, zipcode, storeId }, { loadedSelector, noResultsXPath }, context, dependencies) => {
+  implementation: async (inputs, { loadedSelector, noResultsXPath }, context, dependencies) => {
+    const { url, id } = inputs;
+    let builtUrl;
     if (!url) {
       if (!id) throw new Error('No id provided');
-      else url = await dependencies.createUrl({ id });
+      else builtUrl = await dependencies.createUrl(inputs);
     }
-    await dependencies.goto({ url, zipcode, storeId });
-
+    await dependencies.goto({ ...inputs, url: builtUrl || url });
     if (id) {
-      await context.waitForSelector('li[class="item product product-item"] img');
-      const selectedUrl = await context.evaluate(async (id) => {
-        const selected = document.querySelector('li[class="item product product-item"] a:nth-child(2)');
-        if (selected) {
-          if (parseInt(selected.getAttribute('data-sku')).toString() === id) {
-            return selected.getAttribute('href');
-          } else {
-            document.body.setAttribute('loaded', 'false');
-          }
-        } else {
-          return null;
-        }
-      }, id.toString());
-      if (selectedUrl) {
-        await context.goto(selectedUrl, {
-          block_ads: false,
-          load_all_resources: true,
-          images_enabled: true,
-          timeout: 50000,
-          waitUntil: 'load',
-        });
-      } else {
-        return false;
+      await context.waitForFunction((selector, xpath) => {
+        return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
+      }, { timeout: 10000 }, 'li[class="item product product-item"] a:nth-child(2)', noResultsXPath);
+      if (await context.evaluate((xpath) => document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath)) {
+        return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
       }
+      let newUrl = await context.evaluate(() => document.querySelector('li[class="item product product-item"] a:nth-child(2)').getAttribute('href'));
+      newUrl = `${newUrl}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+      await context.goto(newUrl, { waitUntil: 'networkidle0', block_ads: false });
     }
 
     if (loadedSelector) {
-      await context.waitForFunction(
-        (selector, xpath) => {
-          return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
-        }, { timeout: 10000 },
-        loadedSelector,
-        noResultsXPath,
-      );
+      await context.waitForFunction((selector, xpath) => {
+        return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
+      }, { timeout: 10000 }, loadedSelector, noResultsXPath);
     }
     return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
   },
--- src/library/product/details/stores/d/dis-chem/ZA/extract.js
+++ src/library/product/details/stores/d/dis-chem/ZA/extract.js
@@ -1,9 +1,10 @@
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'ZA',
     store: 'dis-chem',
-    transform: null,
+    transform: cleanUp,
     domain: 'dischem.co.za',
     zipcode: '',
   },
@@ -13,6 +14,25 @@ module.exports = {
     context,
     dependencies,
   ) {
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
+    }
+    await autoScroll(context);
     context.evaluate(async () => {
       const meta = document.querySelector('meta[itemprop="availability"]');
       if (meta.getAttribute('content') === 'https://schema.org/InStock') {
