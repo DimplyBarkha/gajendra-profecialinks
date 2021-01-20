276340e1d9aac210784e9cba1c40d4fe983eb70f
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-01-20 13:29:21 +0530

    #2667 devjdsportssearch

--- src/library/product/search/stores/j/jdsports/UK/paginate.js
+++ src/library/product/search/stores/j/jdsports/UK/paginate.js
@@ -7,7 +7,7 @@ module.exports = {
     nextLinkXpath: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector:null,
+    loadedSelector:'body',
     loadedXpath: null,
     noResultsXPath: null,
     stopConditionSelectorOrXpath:null,
