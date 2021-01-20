002bc95db32785e94cf090fc3684b57e7c49e0c9
Kunal Tapkir <kunal.tapkir@synerzip.com>
2021-01-20 15:04:26 +0530

    #1006. UAT kickback issues fixed for core extractor.

--- src/library/product/details/stores/d/douglas/shared.js
+++ src/library/product/details/stores/d/douglas/shared.js
@@ -5,6 +5,19 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const clean = text => text && text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+  // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
   for (const { group } of data) {
     for (const row of group) {
       if (row.description) {
@@ -14,6 +27,9 @@ const transform = (data) => {
       }
       if ((!row.name || !row.name.length) && row.name1) {
         row.name = row.name1;
+      }
+      if ((!row.name || !row.name.length) && (!row.name1 || !row.name1.length) && row.name2) {
+        row.name = row.name2;
       }
       if (row.shippingInfo) {
         row.shippingInfo.forEach(item => {
@@ -29,6 +45,7 @@ const transform = (data) => {
         row.nameExtended.forEach(item => {
           item.text = `${item.text} ${qunatityTxt}`;
         });
+        row.nameExtended[0].text = row.nameExtended[0].text.trim();
       }
 
       if (row.name) {
@@ -77,6 +94,9 @@ const transform = (data) => {
       };
     }
   }
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
   return data;
 };
 
