d67600806e996ba5e76a621e8ecd7c6fa6f98844
Navjyot Gill <navjyotg@synerzipune.local>
2021-01-20 18:41:04 +0530

    cleanup issue fixed

--- src/library/product/details/stores/d/delhaize/BE/format.js
+++ src/library/product/details/stores/d/delhaize/BE/format.js
@@ -1,28 +1,11 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-  const cleanUp = (data, context) => {
-    let dataStr = JSON.stringify(data);
-    console.log('INSIDE OF CLEANUP');
-    dataStr = dataStr.replace(/(?:\\r\\n|\\r|\\n)/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[^\x00-\x7F]/g, '');
-
-    return JSON.parse(dataStr);
-  };
   for (const { group } of data) {
-    for (let row of group) {
+    for (const row of group) {
       if (row.caloriesPerServing) {
         let text = '';
         row.caloriesPerServing.forEach(item => {
@@ -100,12 +83,7 @@ const transform = (data) => {
           },
         ];
       }
-      if(row.availabilityText){
-        row.availabilityText.forEach(item => {
-          item.text = item.text.replace(/\n/g, " ").replace(/\s\s+/g, ' ');
-        });
-
-      }
+      
       if (row.calciumPerServing) {
         let text = '';
         row.calciumPerServing.forEach(item => {
@@ -117,10 +95,26 @@ const transform = (data) => {
           },
         ];
       }
-      row = cleanUp(row);
     }
-    
   }
+
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
+
   return data;
 };
 
