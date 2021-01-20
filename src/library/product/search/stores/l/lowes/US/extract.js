3548a6bab9a8c4f14066cea30bbeab16d0cc7dc5
irfan shaikh <irfan.shaikh@synerzip.com>
2021-01-20 19:22:15 +0530

    Updated logic for lowes us search extractor.

--- src/library/product/search/stores/l/lowes/US/extract.js
+++ src/library/product/search/stores/l/lowes/US/extract.js
@@ -7,6 +7,7 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
+
   const applyScroll = async function (context) {
     await context.evaluate(async function () {
       let scrollTop = 0;
@@ -30,41 +31,27 @@ async function implementation (
   };
   await applyScroll(context);
   await context.evaluate(async function () {
-    const URL = window.location.href;
-    function addHiddenDiv (id, content, index) {
+    function addHiddenDiv (id, content) {
       const newDiv = document.createElement('div');
       newDiv.id = id;
       newDiv.textContent = content;
       newDiv.style.display = 'none';
-      const originalDiv = document.querySelectorAll('article[data-selector=" splp-prd-tl-dsktp"]')[index];
-      originalDiv.appendChild(newDiv);
-      console.log('child appended ' + index);
+      document.body.appendChild(newDiv);
     }
-    const product = document.querySelectorAll('article[data-selector=" splp-prd-tl-dsktp"]');
-    // select query selector and loop and add div
-    for (let i = 0; i < product.length; i++) {
-      addHiddenDiv('page_url', URL, i);
-    }
-    function addHiddenDivDetails (id, content, index) {
-      const newDiv = document.createElement('div');
-      newDiv.id = id;
-      newDiv.textContent = content;
-      newDiv.style.display = 'none';
-      const originalDiv = document.querySelector('div.style__ProductDetailsWrapper-PDP__sc-18s9jld-0');
-      originalDiv.appendChild(newDiv);
-    }
-    const wholeDiv = document.querySelector('div.style__ProductDetailsWrapper-PDP__sc-18s9jld-0');
-    console.log('s', wholeDiv);
-    if (wholeDiv) {
-      const url = window.location.href;
-      addHiddenDivDetails('Prod_url', url);
-      addHiddenDivDetails('Search_url', url);
-      const thumbnailDetails = document.querySelector('img.met-epc-item');
-      // @ts-ignore
-      thumbnailDetails && thumbnailDetails.src && addHiddenDivDetails('thumbnailDetails', thumbnailDetails.src);
-      const bits = url.split('/');
-      const idDetails = bits[bits.length - 1];
-      addHiddenDivDetails('idDetails', idDetails);
+    const row = document.querySelectorAll('div[class="items"] div[class="tile_group"]');
+    for (let index = 0; index < row.length; index++) {
+      const element = row[index];
+      console.log(element);
+      console.log(index);
+      const array = element.children;
+      for (let ind = 0; ind < array.length; ind++) {
+        const innerElement = array[ind];
+        const attValue = innerElement.getAttribute('data-tile');
+        if (!document.getElementById(`pd_row${attValue}`)) {
+          addHiddenDiv(`pd_row${attValue}`, '');
+        }
+        document.querySelector(`div[id="pd_row${attValue}"]`).appendChild(innerElement);
+      }
     }
   });
   return await context.extract(productDetails, { transform });
