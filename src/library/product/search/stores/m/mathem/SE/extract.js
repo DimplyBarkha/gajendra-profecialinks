0b823cbd5279445b5723767a962664f7746b5da2
Dhanush <dhanush.isukapalli@od.import.io>
2021-01-20 12:38:02 +0530

    mathem by dhanush

--- src/library/product/search/stores/m/mathem/SE/extract.js
+++ src/library/product/search/stores/m/mathem/SE/extract.js
@@ -1,4 +1,10 @@
 const { transform } = require('../../../../shared');
+/**
+ * @param {any} inputs
+ * @param {{ transform: any; }} parameters
+ * @param {{ waitForSelector?: any; click?: any; evaluate?: any; extract?: any; context?: any; }} context
+ * @param {{ productDetails: any; }} dependencies
+ */
 async function implementation (
 inputs,
 parameters,
@@ -7,10 +13,20 @@ dependencies,
 ) {
 const { transform } = parameters;
 const { productDetails } = dependencies;
+const { waitForSelector } = context;
+await context.waitForSelector('div[class="two-col"] div button[class="btn full-width"]');
+await context.click('div[class="two-col"] div button[class="btn full-width"]');
 await context.evaluate(async () => {
 while(!!document.querySelector(("#content-main > mh-search-result > mh-tabs > mh-tab.active > mh-product-list > section > mathem-fetch-more-btn > section > button > i"))){
 document.querySelector(("#content-main > mh-search-result > mh-tabs > mh-tab.active > mh-product-list > section > mathem-fetch-more-btn > section > button > i")).click()
-await new Promise(r => setTimeout(r, 60000));
+await new Promise(r => setTimeout(r, 6000));
+}
+const doesPopupExist = await context.evaluate(function () {
+  return Boolean(document.querySelector('div[class="two-col"] div button[class="btn full-width"]'));
+});
+
+if (doesPopupExist) {
+  await context.click('div[class="two-col"] div button[class="btn full-width"]');
 }
 })
 return await context.extract(productDetails, { transform });
@@ -26,3 +42,15 @@ module.exports = {
   },
   implementation
  };
+//  async function implementation(
+//   inputs,
+//   parameters,
+//   context,
+//   dependencies,
+//   ) {
+//     // const { transform } = parameters;
+//     // const { productDetails } = dependencies;
+// await context.waitForSelector('cookiesSelector');
+// await context.click('cookiesSelector');
+// await context.waitForSelector('pageFullyLoadedSelector');
+//   }
--- src/library/product/search/stores/s/sainsburys/uk/extract.js
+++ src/library/product/search/stores/s/sainsburys/uk/extract.js
@@ -17,38 +17,39 @@ async function implementation(
   context,
   dependencies,
 ) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
+  // const { transform } = parameters;
+  // const { productDetails } = dependencies;
   await context.evaluate(async function () {
-    //for rank
-    function addHiddenDiv(id, content, index) {
-      const newDiv = document.createElement('div');
-      newDiv.id = id;
-      newDiv.textContent = content;
-      newDiv.style.display = 'none';
-      const originalDiv = document.querySelectorAll('a[class="pt__link"]')[index];
-      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-    }
-    let firstChildNode;
-    let finalaggregateRating;
-    const aggregateRating = document.querySelectorAll("div[class='star-rating']")
-    for (let k = 0; k < aggregateRating.length; k++) {
-      let secondChildNode, thirdChildNode = 0;
-      firstChildNode = aggregateRating[k].childNodes;
-      for (let j = 0; j < firstChildNode.length; j++) {
-        secondChildNode = firstChildNode[j].firstChild;
-        console.log(secondChildNode)
-        // @ts-ignore
-        if (secondChildNode.childNodes.length) {
-          // @ts-ignore
-          thirdChildNode = thirdChildNode + secondChildNode.firstChild.firstChild.width.animVal.value;
-          console.log(thirdChildNode)
-          finalaggregateRating= thirdChildNode/20
-        }
-      }
-      addHiddenDiv('aggregateRating', finalaggregateRating, k);
-    }
-  });
+  //   //for rank
+  //   function addHiddenDiv(id, content, index) {
+  //     const newDiv = document.createElement('div');
+  //     newDiv.id = id;
+  //     newDiv.textContent = content;
+  //     newDiv.style.display = 'none';
+  //     const originalDiv = document.querySelectorAll('a[class="pt__link"]')[index];
+  //     originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+  //   }
+  //   let firstChildNode;
+  //   let finalaggregateRating;
+  //   const aggregateRating = document.querySelectorAll("div[class='star-rating']")
+  //   for (let k = 0; k < aggregateRating.length; k++) {
+  //     let secondChildNode, thirdChildNode = 0;
+  //     firstChildNode = aggregateRating[k].childNodes;
+  //     for (let j = 0; j < firstChildNode.length; j++) {
+  //       secondChildNode = firstChildNode[j].firstChild;
+  //       console.log(secondChildNode)
+  //       // @ts-ignore
+  //       if (secondChildNode.childNodes.length) {
+  //         // @ts-ignore
+  //         thirdChildNode = thirdChildNode + secondChildNode.firstChild.firstChild.width.animVal.value;
+  //         console.log(thirdChildNode)
+  //         finalaggregateRating= thirdChildNode/20
+  //       }
+  //     }
+  //     addHiddenDiv('aggregateRating', finalaggregateRating, k);
+  //   }
+  // });
   //rank end
-  return await context.extract(productDetails, { transform });
+  // return await context.extract(productDetails, { transform });
+})
 }
