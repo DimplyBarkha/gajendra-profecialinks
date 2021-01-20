cc7f7ba76fd51dd8d9f4fcace3383789085b7e2b
Karina Miernik <karina.miernik@gmail.com>
2021-01-20 15:28:28 +0100

    #1154 noresult removed

--- src/library/product/search/stores/b/boxed/US/execute.js
+++ src/library/product/search/stores/b/boxed/US/execute.js
@@ -7,7 +7,8 @@ module.exports = {
     domain: 'boxed.com',
     url: 'https://www.boxed.com/products/search/{searchTerms}',
     loadedSelector: 'div[class*="ab4-less"] > ul[class*="g-product-list"]',
-    noResultsXPath: '//div[contains(@class, "33b-less")]',
+    noResultsXPath: null,
+    // '//span[contains(text(), "Oh no!")]',
     zipcode: '',
   },
 };
