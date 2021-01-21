0d766baf921d95c7216fa9dd9df9ad9690486bc1
Nilesh <nilesh.b@osmosys.asia>
2021-01-21 21:14:38 +0530

    Fixed issue.

--- src/library/product/details/stores/o/ocado/UK/extract.js
+++ src/library/product/details/stores/o/ocado/UK/extract.js
@@ -24,25 +24,29 @@ module.exports = {
       const checkHeader = document.querySelector('div.bop-nutritionData__origin  table thead tr th');
       let caloriesPerServing = '';
       if (checkHeader) {
-        const cal1 = document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(1) td:nth-child(2)') && document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(1) td:nth-child(2)').textContent.replace(/\//g, '');
-        const cal2 = document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(2) td:nth-child(2)') && document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(2) td:nth-child(2)').textContent;
-        caloriesPerServing = cal1 + ' / ' + cal2;
-
+          const cal1 = document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(1) td:nth-child(2)')
+          && document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(1) td:nth-child(2)').textContent.replace(/\//g, '');
+          const cal2 = document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(2) td:nth-child(2)')
+          && document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(2) td:nth-child(2)').textContent;
+          caloriesPerServing = cal1 + ' / ' + cal2;
+      
       } else {
-        const cal1 = document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(2) td:nth-child(2)') && document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(2) td:nth-child(2)').textContent.replace(/\//g, '');
-        const cal2 = document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(3) td:nth-child(2)') && document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(3) td:nth-child(2)').textContent;
-        caloriesPerServing = cal1 + ' / ' + cal2;
-
+          const cal1 = document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(2) td:nth-child(2)')
+          && document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(2) td:nth-child(2)').textContent.replace(/\//g, '');
+          const cal2 = document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(3) td:nth-child(2)')
+          && document.querySelector('div.bop-nutritionData__origin  table tr:nth-child(3) td:nth-child(2)').textContent;
+          caloriesPerServing = cal1 + ' / ' + cal2;
+      
       }
-
+      
       var checkcaloriesPerServing = caloriesPerServing.match(/(\d+)\s?g/g);
-      if (checkcaloriesPerServing) {
-        caloriesPerServing = caloriesPerServing.replace(/(.+)(kJ)\s?\s?(.+)(kcal)(.+)/g, '$1$2/$3$4');
-
+      if (checkcaloriesPerServing || checkcaloriesPerServing === null) {
+          caloriesPerServing = caloriesPerServing.replace(/(.+)(kJ)\s?\s?(.+)(kcal)(.+)/g, '$1$2/$3$4')
+      
       }
       const productName = document.querySelector('header.bop-title');
       if (productName) {
-        productName.setAttribute('calories', caloriesPerServing.replace('(', ''));
+          productName.setAttribute('calories', caloriesPerServing.replace(/(.+)(kJ)(.+)(kcal)/g, '$1$2/$3$4').replace('(', ' / ').replace('(.+)(kJ)\s?\s?(.+)(kcal)(.+)', '$1$2/$3$4').replace('//', ' / ').replace(' / /', ' / ').replace('/ /', ' / ').replace('null / null', ''));
       }
     });
     const { transform } = parameters;
