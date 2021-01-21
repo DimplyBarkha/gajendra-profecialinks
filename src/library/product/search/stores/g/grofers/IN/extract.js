24443850995995371e9171d7ed775675c405b262
venkatsn <venkat.sn-odc@partner.import.io>
2021-01-21 18:20:51 +0530

    #3092

--- src/library/product/search/stores/g/grofers/IN/extract.js
+++ src/library/product/search/stores/g/grofers/IN/extract.js
@@ -9,4 +9,27 @@ module.exports = {
     domain: 'grofers.com',
     zipcode: "''",
   },
+  implementation: async ({ inputString }, { country, domain, transform }, context, { productDetails }) => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    const applyScroll = async function (context) {
+      await context.evaluate(async function () {
+        const recordSelector = 'div.products  div.plp-product';
+        let count = document.querySelectorAll(recordSelector).length;
+        let currScroll = document.documentElement.scrollTop;
+        while (count < 150) {
+          const oldScroll = currScroll;
+          window.scrollBy(0, 500);
+          await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+          currScroll = document.documentElement.scrollTop;
+          count = document.querySelectorAll(recordSelector).length;
+          if (oldScroll === currScroll) {
+            break;
+          }
+        }
+      });
+    };
+    await applyScroll(context);
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    return await context.extract(productDetails, { transform });
+  },
 };
