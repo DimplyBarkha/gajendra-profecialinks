b47c8ca23e4e0b0add550f79dde4408e29f90b83
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-20 18:52:31 -0800

    Reduce goto logic

--- src/library/navigation/goto/domains/in/instacart.com.js
+++ src/library/navigation/goto/domains/in/instacart.com.js
@@ -14,8 +14,10 @@ module.exports = {
     console.log('1211parameterValues')
     console.log(parameterValues)
     const mainUrl = zipcode ? `https://${parameterValues.domain}/store/home?current_zip_code=${zipcode}` : '`https://www.instacart.com/store/home';
-    await context.goto(mainUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
+    // await context.goto(mainUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
 
+    const inputUrl = zipcode ? `https://www.instacart.com/store/${storeId}/storefront?current_zip_code=${zipcode}` : `https://www.instacart.com/store/${storeId}/storefront`;
+    await context.goto(inputUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
 
     const onMainPage = await context.evaluate(async function () {
       return !!document.querySelector('div[aria-owns="location-chooser-listbox"]');
@@ -26,13 +28,12 @@ module.exports = {
 
       await context.setInputValue('div[aria-owns="location-chooser-listbox"] input', zipcode);
 
-      await new Promise((resolve) => setTimeout(resolve, 35000));
+      // await new Promise((resolve) => setTimeout(resolve, 35000));
       await context.click('button[type="submit"]');
       context.waitForNavigation();
-      await context.goto(mainUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
+      // await context.goto(mainUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
 
       if (onMainPage) {
-
         const newLink = await context.evaluate(async function () {
           const header = document.querySelectorAll('div[class^="rmq-c486ae44"] h2');
           let shop = null;
@@ -47,16 +48,15 @@ module.exports = {
           return null;
         });
         await context.goto(newLink, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
-        await context.goto(mainUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
+        await context.goto(inputUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
       }
 
-      await context.waitForSelector('div.store-wrapper', { timeout: 15000 });
+      // await context.waitForSelector('div.store-wrapper', { timeout: 15000 });
     }
 
-    const inputUrl = zipcode ? `https://www.instacart.com/store/${storeId}/storefront?current_zip_code=${zipcode}` : `https://www.instacart.com/store/${storeId}/storefront`;
-    console.log('inpewewutUrl')
-    console.log(inputUrl)
-    await context.goto(inputUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
+    // console.log('inpewewutUrl')
+    // console.log(inputUrl)
+    // await context.goto(inputUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
 
     await context.waitForSelector(`span[data-identifier="store_info"] a[href^="${storeId}"]`, { timeout: 15000 });
 
