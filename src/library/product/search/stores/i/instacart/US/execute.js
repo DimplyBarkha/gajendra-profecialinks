212d23b47242f0fc119120df221bfe985feb9811
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-21 13:36:25 -0800

    Fix search

--- src/library/product/search/stores/i/instacart/US/execute.js
+++ src/library/product/search/stores/i/instacart/US/execute.js
@@ -1,3 +1,25 @@
+async function implementation (
+  inputs,
+  { url, loadedSelector, noResultsXPath },
+  context,
+  dependencies,
+) {
+  const { keywords, query, storeId } = inputs;
+  console.log(url);
+  const destinationUrl = url
+    .replace('{searchTerms}', encodeURIComponent(keywords))
+    .replace('{storeId}', storeId)
+    .replace('{queryParams}', query);
+  await dependencies.goto({ ...inputs, url: destinationUrl });
+
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log(`noResultsXPath: ${noResultsXPath}`);
+  return await context.evaluate((xp) => !document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext(), noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/search/execute',
@@ -5,9 +27,10 @@ module.exports = {
     country: 'US',
     store: 'instacart',
     domain: 'instacart.com',
-    url: null,
+    url: 'https://www.instacart.com/store/{storeId}/search_v3/{searchTerms}',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
   },
+  implementation,
 };
