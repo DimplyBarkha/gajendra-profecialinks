cecf92ecd506fb05e61256229f3f40d0c4e66e7d
Your Name <karthik.ravi-odc@partner.import.io>
2021-01-21 16:57:17 +0530

    new

--- src/library/product/search/stores/j/jpg/FR/execute.js
+++ src/library/product/search/stores/j/jpg/FR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FR',
     store: 'jpg',
     domain: 'jpg.fr',
-    url: "https://www.jpg.fr/INTERSHOP/web/WFS/RAJA-JPG-Site/fr_FR/-/EUR/ViewParametricSearch-SimpleOfferSearch?PageNumber=0&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}&SearchParameter=",   
+    url: "https://www.jpg.fr/INTERSHOP/web/WFS/RAJA-JPG-Site/fr_FR/-/EUR/ViewParametricSearch-ProductPaging?PageNumber=0&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}",   
     loadedSelector: 'body',
     noResultsXPath: '//header[@class="search-noresult my-3"]/h3/text()',
     zipcode: '',
--- src/library/product/search/stores/j/jpg/FR/paginate.js
+++ src/library/product/search/stores/j/jpg/FR/paginate.js
@@ -10,7 +10,7 @@ module.exports = {
     loadedSelector: 'body',
     noResultsXPath: '//header[@class="search-noresult my-3"]/h3/text()',
     openSearchDefinition: {
-      template: 'https://www.jpg.fr/INTERSHOP/web/WFS/RAJA-JPG-Site/fr_FR/-/EUR/ViewParametricSearch-ProductPaging?PageNumber={page}&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}&SearchParameter=',
+      template: 'https://www.jpg.fr/INTERSHOP/web/WFS/RAJA-JPG-Site/fr_FR/-/EUR/ViewParametricSearch-ProductPaging?PageNumber={page}&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}',
       },
     domain: 'jpg.fr',
     zipcode: '',
