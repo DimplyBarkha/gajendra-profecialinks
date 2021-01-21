1992107a80575407a397e4ec63404c0db7fbd20c
ahmad8931 <ahmad@profecialinks.com>
2021-01-21 11:12:03 +0530

    product range, brand and pice issue in title

--- src/library/product/reviews/stores/m/mon-liquide/FR/extract.js
+++ src/library/product/reviews/stores/m/mon-liquide/FR/extract.js
@@ -25,6 +25,15 @@ const transform = (data, context) => {
       }));
     }
   }
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.brand) {
+        if(row.brand.length>1){
+          row.brand=row.brand[1];
+        }
+      }
+    }
+  }
   return data;
 };
 module.exports = {
