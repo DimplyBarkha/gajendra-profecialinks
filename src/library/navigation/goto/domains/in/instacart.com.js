44fa99863957b10a1e65839134805a0e588a9a22
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-20 18:43:11 -0800

    Add changes for navigation

--- src/library/navigation/goto/domains/in/instacart.com.js
+++ src/library/navigation/goto/domains/in/instacart.com.js
@@ -11,11 +11,55 @@ module.exports = {
 
   implementation: async ({ url, zipcode, storeId }, parameterValues, context, dependencies) => {
     await context.stop();
-    const inputUrl = `${url}/store/${storeId}`;
-    await context.goto(inputUrl, { timeout: 30000, waitUntil: 'load', checkBlocked: false });
+    console.log('1211parameterValues')
+    console.log(parameterValues)
+    const mainUrl = zipcode ? `https://${parameterValues.domain}/store/home?current_zip_code=${zipcode}` : '`https://www.instacart.com/store/home';
+    await context.goto(mainUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
 
-    if (zipcode) {
-      await dependencies.setZipCode({ zipcode: zipcode });
+
+    const onMainPage = await context.evaluate(async function () {
+      return !!document.querySelector('div[aria-owns="location-chooser-listbox"]');
+    });
+
+    if (onMainPage) {
+      await context.waitForSelector('div[aria-owns="location-chooser-listbox"] input', { timeout: 45000 });
+
+      await context.setInputValue('div[aria-owns="location-chooser-listbox"] input', zipcode);
+
+      await new Promise((resolve) => setTimeout(resolve, 35000));
+      await context.click('button[type="submit"]');
+      context.waitForNavigation();
+      await context.goto(mainUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
+
+      if (onMainPage) {
+
+        const newLink = await context.evaluate(async function () {
+          const header = document.querySelectorAll('div[class^="rmq-c486ae44"] h2');
+          let shop = null;
+          [...header].forEach((node) => {
+            if (node.textContent.includes('New on Instacart')) {
+              shop = node;
+            }
+          });
+          if (shop && shop.nextSibling) {
+            return shop.nextSibling.querySelector('ul li a').href;
+          }
+          return null;
+        });
+        await context.goto(newLink, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
+        await context.goto(mainUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
+      }
+
+      await context.waitForSelector('div.store-wrapper', { timeout: 15000 });
     }
+
+    const inputUrl = zipcode ? `https://www.instacart.com/store/${storeId}/storefront?current_zip_code=${zipcode}` : `https://www.instacart.com/store/${storeId}/storefront`;
+    console.log('inpewewutUrl')
+    console.log(inputUrl)
+    await context.goto(inputUrl, { timeout: 35000, waitUntil: 'load', checkBlocked: false });
+
+    await context.waitForSelector(`span[data-identifier="store_info"] a[href^="${storeId}"]`, { timeout: 15000 });
+
+    await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: false });
   },
 };
--- src/library/navigation/goto/domains/in/setZipCode.instacart.com.js
+++ src/library/navigation/goto/domains/in/setZipCode.instacart.com.js
@@ -7,4 +7,32 @@ module.exports = {
     store: 'instacart',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { zipcode } = inputs;
+
+    async function changeLocation (zipcode) {
+      await context.click('div[data-identifier="availability_chooser"] button');
+      await context.waitForSelector('div[data-identifier="availability_chooser"] button[aria-expanded="true"]', { timeout: 15000 });
+      // await context.setInputValue('div#location-chooser input', zipcode);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+
+      await context.evaluate(async function (zipcode) {
+        const button = document.querySelector('div#location-chooser input');
+        if (button) {
+          button.value = zipcode;
+          button.click();
+        }
+      }, zipcode);
+      await new Promise((resolve) => setTimeout(resolve, 35000));
+      await context.click('div[data-identifier="availability_chooser"] button[data-arrow-navigation="true"]');
+      context.waitForNavigation();
+    }
+    const changedLocationZipCode = await context.evaluate(function () {
+      return document.querySelector('div[data-identifier="availability_chooser"] button span') ? document.querySelector('div[data-identifier="availability_chooser"] button span').textContent : '';
+    });
+
+    if (changedLocationZipCode != zipcode) {
+      await changeLocation(zipcode);
+    }
+  },
 };
