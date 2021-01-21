8c9d74dcbd52839f9d154d4f46e3d12f2a913ede
mania307 <maniakalnaa@gmail.com>
2021-01-21 12:28:32 +0100

    updated description, nameExtended, directions

--- src/library/product/details/stores/b/boozt/DK/extract.js
+++ src/library/product/details/stores/b/boozt/DK/extract.js
@@ -13,7 +13,7 @@ module.exports = {
     await context.evaluate(async () => {
       // await new Promise((resolve, reject) => setTimeout(resolve, 3000));
 
-      function addElementToDocument (id, value, key) {
+      function addElementToDocument(id, value, key) {
         const catElement = document.createElement('div');
         catElement.id = id;
         catElement.innerText = value;
@@ -23,6 +23,34 @@ module.exports = {
       };
       const availability = document.querySelector("meta[property='og:availability']") ? document.querySelector("meta[property='og:availability']").getAttribute('content') : '';
       addElementToDocument('availability', availability === 'instock' ? 'In Stock' : 'Out Of Stock');
+
+      // getting nameExtended
+      const brand = document.querySelector('a[data-track-id=pdp_brandname]') ? document.querySelector('a[data-track-id=pdp_brandname]').innerText : null;
+      const name = document.querySelector('h1.pp-info__name') ? document.querySelector('h1.pp-info__name').innerText : null;
+      const script = document.querySelector('script[data-js-react-on-rails-store="store"]') ? document.querySelector('script[data-js-react-on-rails-store="store"]').innerText : null;
+      const scriptToString = JSON.parse(script);
+      const size = scriptToString.product.current.size_detail;
+      const color = scriptToString.product.current.colourDetail;
+
+      //getting firstVariant
+      const variantSize = document.querySelector('div.pp-size-selector__label') ? document.querySelector('div.pp-size-selector__label') : null;
+      const variantColor = document.querySelector('p.pp-colour-selector__label') ? document.querySelector('p.pp-colour-selector__label span').innerText : null;
+      if (variantSize !== null) {
+        addElementToDocument('variant-information', 'Size');
+        addElementToDocument('first-var', size);
+      } else if (variantColor !== null) {
+        addElementToDocument('variant-information', 'Color');
+        addElementToDocument('first-var', color);
+      }
+
+      //setting nameExtended
+      if (variantColor && !variantSize) {
+        addElementToDocument('nameext', brand + ' ' + name + ' ' + variantColor)
+      } else if (variantSize && !variantColor) {
+        addElementToDocument('nameext', brand + ' ' + name + ' ' + size)
+      } else if (!variantColor && !variantSize) {
+        addElementToDocument('nameext', brand + ' ' + name + ' ' + color)
+      }
     });
     await context.extract(productDetails);
 
