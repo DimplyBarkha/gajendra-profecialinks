7478bdf50b6ba9e4467458eba7113c177d86dd54
Karina Miernik <karina.miernik@gmail.com>
2021-01-21 16:55:09 +0100

    #3299 NightmareError fixed

--- src/library/product/search/stores/e/e-bebek/TR/extract.js
+++ src/library/product/search/stores/e/e-bebek/TR/extract.js
@@ -3,30 +3,16 @@ const { transform } = require('../../../../shared');
 async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  await new Promise((resolve, reject) => setTimeout(resolve, 4000));
 
   await context.evaluate(async () => {
-    function addElementToDocument (id, value) {
-      const catElement = document.createElement('div');
-      catElement.id = id;
-      catElement.innerText = value;
-      catElement.style.display = 'none';
-      document.body.appendChild(catElement);
-    };
     const searchUrl = window.location.href;
-    addElementToDocument('searchurl', searchUrl);
-    function stall (ms) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve();
-        }, ms);
-      });
-    }
-    // waits for script loaded
-    await stall(4000);
+
     const products = document.querySelectorAll('section[class="product-list"] div[class*="product gtmProductClick w-100"]');
 
     products.forEach((product, index) => {
+      product.setAttribute('searchurl', searchUrl);
       const prefix = 'https://www.e-bebek.com';
       const productUrl = product.querySelector('a.product-btn');
       if (productUrl !== null && productUrl !== undefined) product.setAttribute('producturl', prefix.concat(productUrl.getAttribute('href')));
