ce2c12b91232e3788136c0302990b9968b932f39
Gajender Ramola <ramola106@gmail.com>
2021-01-21 14:57:44 +0530

    fix

--- src/library/product/search/stores/w/woolworths/AU/format.js
+++ src/library/product/search/stores/w/woolworths/AU/format.js
@@ -4,33 +4,28 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  const cleanUp = (data) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
   const state = context.getState();
-  let orgRankCounter = state.orgRankCounter || 0;
-  let rankCounter = state.rankCounter || 0;
-  const productCodes = state.productCodes || [];
+  let rank = state.rank || 1;
   for (const { group } of data) {
     for (const row of group) {
-      rankCounter += 1;
-      if (!row.sponsored) {
-        orgRankCounter += 1;
-        row.rankOrganic = [{ text: orgRankCounter }];
-      }
-      row.rank = [{ text: rankCounter }];
-      Object.keys(row).forEach(header => row[header].forEach(el => {
-        el.text = clean(el.text);
-      }));
       if (row.aggregateRating) {
         let counter = 0;
         row.aggregateRating.forEach(item => {
@@ -59,12 +54,12 @@ const transform = (data, context) => {
           item.text = item.text.replace('/medium/', '/large/');
         });
       }
+      row.rank = row.rankOrganic = [{ text: rank }];
+      rank++;
     }
   }
-  context.setState({ rankCounter });
-  context.setState({ orgRankCounter });
-  context.setState({ productCodes });
-  console.log(productCodes);
-  return data;
+  context.setState({ rank });
+  return cleanUp(data);
 };
+
 module.exports = { transform };
