cd98d030b09e45d34d09d87eb97a56f3024af707
sobha rani <amol.thukrul-od@partner.import.io>
2021-01-21 13:24:23 +0530

    fressnapf complete

--- src/library/product/details/stores/f/fressnapf/DE/extract.js
+++ src/library/product/details/stores/f/fressnapf/DE/extract.js
@@ -10,12 +10,21 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      function addElementToDocument (key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
+      function addHiddenDiv (id, content, index) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll("div[class='section-content']>div[class='product-overview']>div>div>div>div>div>div>div>div>div")[index];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      }
+      function addHiddenDiv1 (id, content, index) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll("div[class='pvsil-title']")[0];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
       const getAllXpath = (xpath, prop) => {
         const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -26,19 +35,19 @@ module.exports = {
         }
         return result;
       };
-      var alternateimg = getAllXpath("(//div[@class='section-content']/div[@class='product-overview']/div/div/div/div/div/div/div/div/div/div/img/@src)", 'nodeValue');
+      var alternateimg = getAllXpath("(//div[@class='section-content']/div[@class='product-overview']/div/div/div/div/div/div/div/div/div/div/img)", 'nodeValue');
       if (alternateimg != null) {
         if (alternateimg.length > 1) {
           alternateimg.shift();
         }
         for (var i = 0; i < alternateimg.length; i++) {
-          addElementToDocument('abc', alternateimg[i]);
+          addHiddenDiv('abc', alternateimg[i], i);
         }
       }
       var variant = getAllXpath("(//div[@class='product-variant-selector'])[2]/div/label/span/div/div[@class='pvsil-title']/text()", 'nodeValue');
       if (variant != null) {
         var ab = variant.join(' | ');
-        addElementToDocument('variant', ab);
+        addHiddenDiv1('variant', ab, 0);
       }
     });
     await context.extract(productDetails);
