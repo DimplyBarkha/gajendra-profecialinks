aeea9d65268c8965f205d389c2d304b0e6330307
Shreyam <shreyam.g@osmosys.asia>
2021-01-21 12:02:52 +0530

    Added fix for salsify fields

--- src/library/product/search/stores/l/loblaw/CA/extract.js
+++ src/library/product/search/stores/l/loblaw/CA/extract.js
@@ -40,54 +40,27 @@ module.exports = {
         return products.snapshotLength;
       });
     }
-    let productsCount = 0;
-    while (productsCount < 150) {
-      const doesLoadMoreExists = await context.evaluate(function () {
-        return Boolean(document.querySelector('div[class="load-more-button"] > button'));
-      });
-      if (doesLoadMoreExists) {
-        await context.evaluate(async function () {
-          console.log('Clicking on load more button');
-          document.querySelector('div[class="load-more-button"] > button').click();
-          await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-        });
-        productsCount = await getProductsCount(context);
-        console.log('productsCount' + productsCount);
-        if (productsCount >= 150) {
-          break;
-        }
-        await applyScroll(context);
-      } else {
-        break;
-      }
-    }
+    // let productsCount = 0;
+    // while (productsCount <= 48) {
+    //   const doesLoadMoreExists = await context.evaluate(function () {
+    //     return Boolean(document.querySelector('div[class="load-more-button"] > button'));
+    //   });
+    //   if (doesLoadMoreExists) {
+    //     await context.evaluate(async function () {
+    //       console.log('Clicking on load more button');
+    //       document.querySelector('div[class="load-more-button"] > button').click();
+    //       await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    //     });
+    //     productsCount = await getProductsCount(context);
+    //     console.log('productsCount' + productsCount);
+    //     if (productsCount >= 48) {
+    //       break;
+    //     }
+    //     await applyScroll(context);
+    //   } else {
+    //     break;
+    //   }
+    // }
     return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
   },
 };
-  // implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
-  //   await new Promise((resolve, reject) => setTimeout(resolve, 6000));
-  //   const applyScroll = async function (context) {
-  //     await context.evaluate(async function () {
-  //       let scrollTop = 0;
-  //       while (scrollTop !== 20000) {
-  //         await stall(500);
-  //         scrollTop += 500;
-  //         window.scroll(0, scrollTop);
-  //         if (scrollTop === 20000) {
-  //           await stall(2000);
-  //           break;
-  //         }
-  //       }
-  //       function stall(ms) {
-  //         return new Promise((resolve, reject) => {
-  //           setTimeout(() => {
-  //             resolve();
-  //           }, ms);
-  //         });
-  //       }
-  //      // console.log("scroll");
-  //     });
-  //   };
-  //   await applyScroll(context);
-  //   return await context.extract(productDetails, { transform });
-  // }
