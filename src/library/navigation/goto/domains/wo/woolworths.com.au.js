783e248f5447ba211c509428d8aa240d6540b669
jeet kothiyal (jeet) <jitendra@profecialinks.com>
2021-01-21 17:25:33 +0530

    Fixed pagination issue

--- src/library/navigation/goto/domains/wo/woolworths.com.au.js
+++ src/library/navigation/goto/domains/wo/woolworths.com.au.js
@@ -3,9 +3,17 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'woolworths.com.au',
-    timeout: 50000,
+    timeout: 20000,
     country: 'AU',
     store: 'woolworths',
     zipcode: "''",
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
--- src/library/product/search/stores/w/woolworths/AU/execute.js
+++ src/library/product/search/stores/w/woolworths/AU/execute.js
@@ -1,3 +1,34 @@
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
+
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/search/execute',
@@ -6,8 +37,9 @@ module.exports = {
     store: 'woolworths',
     domain: 'woolworths.com.au',
     url: 'https://www.woolworths.com.au/Shop/Search?searchTerm={searchTerms}',
-    loadedSelector: 'div.ng-trigger-staggerFadeInOut',
+    loadedSelector: 'div.ng-trigger-staggerFadeInOut div.product-grid--tile',
     noResultsXPath: '//span[contains(text(),"Unfortunately, we could")]',
     zipcode: "''",
   },
+  implementation,
 };
