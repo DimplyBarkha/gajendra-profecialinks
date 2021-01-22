06aa2d4b5bca2cb23036879f34c861d3a6f7c5e5
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-01-22 18:03:26 +0530

    #2650 devvonssearch

--- src/library/product/search/stores/v/vons/US/extract.js
+++ src/library/product/search/stores/v/vons/US/extract.js
@@ -11,6 +11,7 @@ async function implementation (
     while(!!document.querySelector('#search-grid_0 > div.col-12.bloom-load-wrapper > button')){
       document.querySelector('#search-grid_0 > div.col-12.bloom-load-wrapper > button').click()
       await new Promise(r => setTimeout(r, 8000));
+      await new Promise((resolve, reject) => setTimeout(resolve, 10000));
     }
   })
   return await context.extract(productDetails, { transform });
