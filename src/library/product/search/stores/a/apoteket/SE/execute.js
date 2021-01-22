573d725aab10961668c9f2a65ae75cdefcfadfcb
Suryakanta Sahoo <suryakanta.s@osmosys.asia>
2021-01-22 17:53:34 +0530

    fixed many check issues

--- src/library/product/search/stores/a/apoteket/SE/execute.js
+++ src/library/product/search/stores/a/apoteket/SE/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'apoteket',
     domain: 'apoteket.se',
     url: 'https://www.apoteket.se/sok/?q={searchTerms}',
-    loadedSelector: 'div.product-grid__items div.grid-item',
+    loadedSelector: 'div.product-grid__items',
     noResultsXPath: '//div[@class="product-grid__loader" and contains(text(), "Inga produkter hittades")]',
     zipcode: '',
   },
