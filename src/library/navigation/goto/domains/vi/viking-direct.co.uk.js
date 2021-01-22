367e2add67b2ceed18223d36e101b3ae12d5a62c
Karolina <karolina.urbanska@tdcm.io>
2021-01-22 13:46:17 +0100

    Added force200 to goto (#1267)

--- src/library/navigation/goto/domains/vi/viking-direct.co.uk.js
+++ src/library/navigation/goto/domains/vi/viking-direct.co.uk.js
@@ -8,22 +8,19 @@ module.exports = {
     store: 'viking',
     zipcode: '',
   },
-  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    await context.setBlockAds(false);
-    await context.setAntiFingerprint(false);
-    await context.goto(url,
-      {
-        first_request_timeout: 60000,
-        block_ads: false,
-        timeout: 60000,
-        waitUntil: 'load',
-        load_all_resources: true,
-        images_enabled: true,
-        checkBlocked: true,
-      });
-    console.log(zipcode);
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
     if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+      url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true, "cookie_jar":[{"name":"invCheckPostalCode","value":${zipcode}}]}[/!opt!]`;
+    } else {
+      url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
     }
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 50000,
+      waitUntil: 'load',
+    });
+    await context.waitForNavigation();
   },
 };
