ab3178853f2f7d2726c21c9da9f5640ee2679909
Brinda <brinda.allamsetty@od.import.io>
2021-01-22 17:10:15 +0530

    frisco

--- src/library/product/search/stores/f/frisco/PL/extract.js
+++ src/library/product/search/stores/f/frisco/PL/extract.js
@@ -10,29 +10,29 @@ module.exports = {
   dependencies: {
     productDetails: 'extraction:product/search/stores/${store[0:1]}/${store}/${country}/extract',
   },
- 
+
   implementation: async ({ inputString }, { country, store, transform: transformParam }, context, dependencies) => {
-   // await new Promise((resolve, reject) => setTimeout(resolve, 8000));
-  //   async function getID() {
-  //     function addHiddenDiv(id, content, index) {
-  //       const newDiv = document.createElement('div');
-  //       newDiv.id = id;
-  //       newDiv.textContent = content;
-  //       newDiv.style.display = 'none';
-  //       const originalDiv = document.querySelectorAll('td.imageUrl.depth_2')[index];
-  //       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-  //     }
-  //    // var product = getAllXpath('//td[@class="productId depth_1"]', 'nodeValue');
-  //     const product = document.querySelectorAll('td.productId.depth_1');
-  //     console.log("product length :::"+ product.length);
-  //     if (product.length >= 1) {
-  //       for (var i = 0; i < product.length; i++) {
-  //         // @ts-ignore
-  //         addHiddenDiv('id', product[i].innerText, i);
-  //       }
-  //     }
-  //   }
-  //   await context.evaluate(getID);
+    await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    //   async function getID() {
+    //     function addHiddenDiv(id, content, index) {
+    //       const newDiv = document.createElement('div');
+    //       newDiv.id = id;
+    //       newDiv.textContent = content;
+    //       newDiv.style.display = 'none';
+    //       const originalDiv = document.querySelectorAll('td.imageUrl.depth_2')[index];
+    //       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    //     }
+    //    // var product = getAllXpath('//td[@class="productId depth_1"]', 'nodeValue');
+    //     const product = document.querySelectorAll('td.productId.depth_1');
+    //     console.log("product length :::"+ product.length);
+    //     if (product.length >= 1) {
+    //       for (var i = 0; i < product.length; i++) {
+    //         // @ts-ignore
+    //         addHiddenDiv('id', product[i].innerText, i);
+    //       }
+    //     }
+    //   }
+    //   await context.evaluate(getID);
     return await context.extract(dependencies.productDetails, { transform: transform });
   }
 };
