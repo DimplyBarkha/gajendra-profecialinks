551ab7a2549c5968f76cfc1b99dc9100b7a937ff
PradyumnaD29 <pradyumna.deshpande@synerzip.com>
2021-01-22 17:51:33 +0530

    #3459 search updated nespresso

--- src/library/product/search/stores/b/bestbuy/FR/extract.js
+++ src/library/product/search/stores/b/bestbuy/FR/extract.js
@@ -1,4 +1,6 @@
-const {transform}=require('../FR/format');
+/* eslint-disable no-unmodified-loop-condition */
+const { transform } = require('../../../../shared');
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +10,30 @@ module.exports = {
     domain: 'bestbuy.ca',
     zipcode: '',
   },
+  implementation,
 };
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async () => {
+    while (!document.querySelector('button[class*="loadMore"]') && !document.querySelector('div[class*="endOfList"]')) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+    while (true) {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      if (document.querySelector('div[class*="endOfList"]')) {
+        break;
+      }
+      // @ts-ignore
+      document.querySelector('button[class*="loadMore"]').click();
+    }
+    window.scrollTo(0, document.querySelector('div[class*="endOfList"]').offsetTop);
+  });
+  return await context.extract(productDetails, { transform });
+}
