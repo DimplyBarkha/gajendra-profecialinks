e54447eb976d60d3be2009559fcca5282cdd7908
Gajender Ramola <ramola106@gmail.com>
2021-01-22 17:14:58 +0530

    fix

--- src/library/product/search/stores/m/magazineluiza/BR/format.js
+++ src/library/product/search/stores/m/magazineluiza/BR/format.js
@@ -3,16 +3,16 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
-  const cleanUp = (data, context) => {
+const transform = (data, context) => {
+  const cleanUp = (data) => {
     const clean = text => text.toString()
       .replace(/\r\n|\r|\n/g, ' ')
       .replace(/&amp;nbsp;/g, ' ')
       .replace(/&amp;#160/g, ' ')
       .replace(/\u00A0/g, ' ')
       .replace(/\s{2,}/g, ' ')
-      // .replace(/"\s{1,}/g, '"')
-      // .replace(/\s{1,}"/g, '"')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
       .replace(/^ +| +$|( )+/g, ' ')
       // eslint-disable-next-line no-control-regex
       .replace(/[\x00-\x1F]/g, '')
@@ -22,12 +22,13 @@ const transform = (data) => {
     }))));
     return data;
   };
+  const state = context.getState();
+  let rank = state.rank || 1;
   for (const { group } of data) {
-    let rank = 1;
     for (const row of group) {
       if (row.productUrl) {
         row.productUrl.forEach(item => {
-          if (item.text.indexOf('https') != 0) {
+          if (item.text.indexOf('https') !== 0) {
             item.text = 'https:' + item.text;
           }
         });
@@ -35,7 +36,7 @@ const transform = (data) => {
       if (row.thumbnail) {
         row.thumbnail.forEach(item => {
           item.text = item.text.replace('210x210', '618x463');
-          if (item.text.indexOf('https') != 0) {
+          if (item.text.indexOf('https') !== 0) {
             item.text = 'https:' + item.text;
           }
         });
@@ -49,7 +50,9 @@ const transform = (data) => {
       rank++;
     }
   }
+  context.setState({ rank });
   return cleanUp(data);
 };
 
 module.exports = { transform };
+
