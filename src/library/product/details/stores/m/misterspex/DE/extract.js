fa4395cd1b7259cfc54bf804035e9003c3cadf7f
Taras Chuiko <taraschuiko@gmail.com>
2021-01-22 07:37:33 +0200

    Added availability

--- src/library/product/details/stores/m/misterspex/DE/extract.js
+++ src/library/product/details/stores/m/misterspex/DE/extract.js
@@ -8,4 +8,19 @@ module.exports = {
     domain: 'misterspex.de',
     zipcode: "''",
   },
+  implementation: async ({ url }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+
+      // description Block
+      addHiddenDiv('spex_availability', document.getElementById('addToBasketButtonText') ? 'In Stock' : 'out of stock');
+    });
+    return await context.extract(productDetails);
+  },
 };
