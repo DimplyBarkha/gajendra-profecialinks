f7591fe3422d371970d940a32767ed44c335640b
John Felix J <johnfelix.josep@hcl.com>
2021-01-22 21:36:04 +0530

    Coppel fixes

--- src/library/navigation/goto/domains/co/coppel.com.js
+++ src/library/navigation/goto/domains/co/coppel.com.js
@@ -8,4 +8,16 @@ module.exports = {
     store: 'coppel',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 1000000;
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setBlockAds(false);
+    await context.setLoadImages(true);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
