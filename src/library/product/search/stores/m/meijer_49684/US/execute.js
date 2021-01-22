4012fa9f8b015d5b5e01caa9e7fd0714b59f4c93
venkatsn <venkat.sn-odc@partner.import.io>
2021-01-22 16:57:24 +0530

    #2642

--- src/library/product/search/stores/m/meijer_49684/US/execute.js
+++ src/library/product/search/stores/m/meijer_49684/US/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'meijer_49684',
     domain: 'meijer.com',
     url: 'https://www.meijer.com/shop/en/search/?q={searchTerms}&page=0',
-    loadedSelector: 'body',
-    noResultsXPath: null,//"//div[@class='search-empty row ']",
+    loadedSelector: "body",
+    noResultsXPath: "//div[@class='search-empty row ']",
     zipcode: '',
   },
 };
