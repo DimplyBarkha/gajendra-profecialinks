61df8c23eef70bb73baa832024d49b4a394bf4a4
Brinda <brinda.allamsetty@od.import.io>
2021-01-22 18:35:49 +0530

    frisco

--- src/library/product/search/stores/f/frisco/PL/extract.js
+++ src/library/product/search/stores/f/frisco/PL/extract.js
@@ -7,32 +7,10 @@ module.exports = {
     transform: transform,
     domain: 'frisco.pl',
   },
-  dependencies: {
-    productDetails: 'extraction:product/search/stores/${store[0:1]}/${store}/${country}/extract',
-  },
-
-  implementation: async ({ inputString }, { country, store, transform: transformParam }, context, dependencies) => {
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
     await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-    //   async function getID() {
-    //     function addHiddenDiv(id, content, index) {
-    //       const newDiv = document.createElement('div');
-    //       newDiv.id = id;
-    //       newDiv.textContent = content;
-    //       newDiv.style.display = 'none';
-    //       const originalDiv = document.querySelectorAll('td.imageUrl.depth_2')[index];
-    //       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-    //     }
-    //    // var product = getAllXpath('//td[@class="productId depth_1"]', 'nodeValue');
-    //     const product = document.querySelectorAll('td.productId.depth_1');
-    //     console.log("product length :::"+ product.length);
-    //     if (product.length >= 1) {
-    //       for (var i = 0; i < product.length; i++) {
-    //         // @ts-ignore
-    //         addHiddenDiv('id', product[i].innerText, i);
-    //       }
-    //     }
-    //   }
-    //   await context.evaluate(getID);
-    return await context.extract(dependencies.productDetails, { transform: transform });
+    return await context.extract(productDetails, { transform });
   }
 };
