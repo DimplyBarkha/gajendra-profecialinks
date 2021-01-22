b1432c8ff8f64dd30e0a7ce2929cbe11256a2719
Dhanush <dhanush.isukapalli@od.import.io>
2021-01-22 20:23:57 +0530

    mathem By Dhanush

--- src/library/product/search/stores/m/mathem/SE/extract.js
+++ src/library/product/search/stores/m/mathem/SE/extract.js
@@ -1,36 +1,67 @@
 const { transform } = require('../../../../shared');
-/**
- * @param {any} inputs
- * @param {{ transform: any; }} parameters
- * @param {{ waitForSelector?: any; click?: any; evaluate?: any; extract?: any; context?: any; }} context
- * @param {{ productDetails: any; }} dependencies
- */
-async function implementation (
-inputs,
-parameters,
-context,
-dependencies,
+const implementation = async function (
+  inputs,
+  parameters,
+  context,
+  dependencies,
 ) {
-const { transform } = parameters;
-const { productDetails } = dependencies;
-const { waitForSelector } = context;
-await context.waitForSelector('div[class="two-col"] div button[class="btn full-width"]');
-await context.click('div[class="two-col"] div button[class="btn full-width"]');
-await context.evaluate(async () => {
-while(!!document.querySelector(("#content-main > mh-search-result > mh-tabs > mh-tab.active > mh-product-list > section > mathem-fetch-more-btn > section > button > i"))){
-document.querySelector(("#content-main > mh-search-result > mh-tabs > mh-tab.active > mh-product-list > section > mathem-fetch-more-btn > section > button > i")).click()
-await new Promise(r => setTimeout(r, 6000));
-}
-// const doesPopupExist = await context.evaluate(function () {
-//   return Boolean(document.querySelector('div[class="two-col"] div button[class="btn full-width"]'));
-// });
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  const { waitForSelector } = context;
+  await context.waitForSelector('div[class="two-col"] div button[class="btn full-width"]');
+  await context.click('div[class="two-col"] div button[class="btn full-width"]');
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        await stall(1000);
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
 
-// if (doesPopupExist) {
-//   await context.click('div[class="two-col"] div button[class="btn full-width"]');
-// }
-})
-return await context.extract(productDetails, { transform });
-}
+  async function getProductsCount (context) {
+    return context.evaluate(async function () {
+      const products = document.evaluate('//figure//img[@class="lazyload"]/@src', document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      return products.snapshotLength;
+    });
+  }
+
+  let productsCount = 0;
+  while (productsCount < 150) {
+    const doesLoadMoreExists = await context.evaluate(function () {
+      return Boolean(document.querySelector('#content-main > mh-search-result > mh-tabs > mh-tab.active > mh-product-list > section > mathem-fetch-more-btn > section > button > i'));
+    });
+
+    if (doesLoadMoreExists) {
+      await context.evaluate(async function () {
+        console.log('Clicking on load more button');
+        document.querySelector('#content-main > mh-search-result > mh-tabs > mh-tab.active > mh-product-list > section > mathem-fetch-more-btn > section > button > i').click();
+        await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+      });
+      productsCount = await getProductsCount(context);
+      console.log('productsCount' + productsCount);
+      if (productsCount >= 150) {
+        break;
+      }
+      await applyScroll(context);
+    } else {
+      break;
+    }
+  }
+
+  return await context.extract(productDetails, { transform });
+};
+
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -42,15 +73,3 @@ module.exports = {
   },
   implementation
  };
-//  async function implementation(
-//   inputs,
-//   parameters,
-//   context,
-//   dependencies,
-//   ) {
-//     // const { transform } = parameters;
-//     // const { productDetails } = dependencies;
-// await context.waitForSelector('cookiesSelector');
-// await context.click('cookiesSelector');
-// await context.waitForSelector('pageFullyLoadedSelector');
-//   }
