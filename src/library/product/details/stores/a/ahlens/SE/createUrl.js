8f565e2bc850cb11b1625273ec1ed992249944f7
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-01-22 22:35:16 +0530

    updated code

--- src/library/product/details/stores/a/ahlens/SE/createUrl.js
+++ src/library/product/details/stores/a/ahlens/SE/createUrl.js
@@ -11,39 +11,19 @@ module.exports = {
   },
   implementation,
 };
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
   dependencies,
 ) {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
   const { id } = inputs;
-  if (parameters.url) {
-    const url = parameters.url.replace('{id}', encodeURIComponent(id));
-    return url;
-  }
-  var iframeApiResUrl = await context.evaluate(async function () {
-    let url = parameters.url;
-    async function getData(url = "") {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      console.log('response.json:------------------------------- ', response.json);
-      return response.json();
-    };
-    console.log('sai')
-    console.log(url)
-    const iframeApiRes = await getData(url);
-    let urlIframe = iframeApiRes ? iframeApiRes.uri : null;
-    return urlIframe;
+  var firstURL = parameters.url.replace('{id}', encodeURIComponent(id));
+  await context.goto(firstURL);
+  var finalURL = await context.evaluate(async function () {
+    let wholeData = document.querySelector('pre').innerText;
+    let jsonwholeData = JSON.parse(wholeData);
+    return jsonwholeData.product.uri;
   });
-  if (iframeApiResUrl) {
-    console.log('iframe: ', iframeApiResUrl);
-    await context.goto(iframeApiResUrl, { timeout: 60000 });
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-  }
+  return finalURL;
 }
