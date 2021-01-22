a5702bc86adc5173e8654107c228b5143790a4d2
alscotty <alawrencescott@gmail.com>
2021-01-21 17:51:30 -0800

    move goto to nj

---
+++ src/library/navigation/goto/domains/nj/njoy.com.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'njoy.com',
+    timeout: 50000,
+    country: 'US',
+    store: 'njoy',
+    zipcode: "''",
+  },
+};
