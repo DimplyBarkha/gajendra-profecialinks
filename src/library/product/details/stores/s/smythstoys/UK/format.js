59ae2c41dd4b9c29acf5d2bde9b184b5cd526da6
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2021-01-22 12:15:46 +0530

    updated extract.yaml

--- src/library/product/details/stores/s/smythstoys/UK/format.js
+++ src/library/product/details/stores/s/smythstoys/UK/format.js
@@ -1,13 +1,34 @@
 
+
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
   for (const { group } of data) {
     for (const row of group) {
+      console.log("hi format")
       if (row.description) {
+
         let desc = '';
         row.description.forEach(item => {
           desc += `${item.text}`;
@@ -18,27 +39,32 @@ const transform = (data) => {
           },
         ];
       }
+
+      if (row.highQualityImages) {
+         row.highQualityImages.forEach(item => {
+          item.text = item.text.replace('thumbnail','original').trim();
+        });
+
+      }
+
+      if (row.shortDescription) {
+
+        let shortDesc = '';
+        row.shortDescription.forEach(item => {
+          shortDesc += `${item.text}`;
+        });
+        row.shortDescription = [
+          {
+            text: shortDesc
+          },
+        ];
+      }
+
+
+
     }
   }
-  // Clean up data
-  const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-
-  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-    el.text = clean(el.text);
-  }))));
-
-  return data;
+  return cleanUp(data);
 };
 
 module.exports = { transform };
