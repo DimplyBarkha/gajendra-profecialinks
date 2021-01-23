8ffbb28f2eb1cbcce442c0173b7fa5fb1f252a59
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2021-01-23 11:12:32 +0530

    updated

--- src/library/navigation/goto/domains/di/dickssportinggoods.com.js
+++ src/library/navigation/goto/domains/di/dickssportinggoods.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'dickssportinggoods.com',
-    timeout: '30000',
+    timeout: '60000',
     country: 'UK',
     store: 'dickssportinggoods',
     zipcode: "''",
--- src/library/product/details/stores/d/dickssportinggoods/UK/shared.js
+++ src/library/product/details/stores/d/dickssportinggoods/UK/shared.js
@@ -8,105 +8,109 @@ const transform = (data) => {
   for (const { group } of data) {
     for (const row of group) {
 
-      if (row.aggregateRating) {
-        let text = '';
-        row.aggregateRating.forEach(item => {
+      // if (row.aggregateRating) {
+      //   let text = '';
+      //   row.aggregateRating.forEach(item => {
 
-          text = item.value;
-        });
-        row.aggregateRating = [
-          {
-            text: text,
-          },
-        ];
-      }
-      if (row.ratingCount) {
-        let text = '';
-        row.ratingCount.forEach(item => {
-          console.log("item",item);
-          text = item.value;
-        });
-        row.ratingCount = [
-          {
-            text: text,
-          },
-        ];
-      }
+      //     text = item.value;
+      //   });
+      //   row.aggregateRating = [
+      //     {
+      //       text: text,
+      //     },
+      //   ];
+      // }
+      // if (row.ratingCount) {
+      //   let text = '';
+      //   row.ratingCount.forEach(item => {
+      //     console.log("item", item);
+      //     text = item.value;
+      //   });
+      //   row.ratingCount = [
+      //     {
+      //       text: text,
+      //     },
+      //   ];
+      // }
 
-       if (row.packSize) {
-         console.log("row.packSize",row.packSize)
-        let text = '';
-        row.packSize.forEach(item => {
-          console.log("item",item);
-          let text1 = item.text;
-          text = text1.slice(11);
-          console.log("text",text);
-        });
-        row.packSize = [
-          {
-            text: text,
-          },
-        ];
-       }
-       if (row.availabilityText) {
+      // if (row.packSize) {
+      //   console.log("row.packSize", row.packSize)
+      //   let text = '';
+      //   row.packSize.forEach(item => {
+      //     console.log("item", item);
+      //     let text1 = item.text;
+      //     text = text1.slice(11);
+      //     console.log("text", text);
+      //   });
+      //   row.packSize = [
+      //     {
+      //       text: text,
+      //     },
+      //   ];
+      // }
+
+      if (row.availabilityText) {
         row.availabilityText.forEach(item => {
-          if (item.text == "true"){
+          if (item.text == "true") {
             item.text = "In Stock";
           }
-          else{
-             item.text = "Out Of Stock";
+          else {
+            item.text = "Out Of Stock";
           }
 
         })
       }
-if(row.sku){
-  row.sku.forEach(item =>{
-     item.text = item.text.replace('SKU:', '');
-  })
-}
-if(row.countryOfOrigin){
-  row.countryOfOrigin.forEach(item =>{
-     item.text = item.text.replace('Country of Origin :', '');
-  })
-}
-if(row.brandText){
-  row.brandText.forEach(item =>{
-     item.text = item.text.replace('Brand :', '');
-  })
-}
-       if (row.descriptionBullets) {
-        let text = '';
-        row.descriptionBullets.forEach(item => {
-          console.log("item",item);
-          text = item.value;
-        });
-        row.descriptionBullets = [
-          {
-            text: text,
-          },
-        ];
+      if (row.sku) {
+        console.log('row.sku',row.sku);
+        row.sku.forEach(item => {
+          console.log('item.text',item.text);
+          item.text = item.text.replace('SKU:', '');
+          console.log('item.text1',item.text);
+        })
       }
+      if (row.countryOfOrigin) {
+        row.countryOfOrigin.forEach(item => {
+          item.text = item.text.replace('Country of Origin :', '');
+        })
+      }
+      if (row.brandText) {
+        row.brandText.forEach(item => {
+          item.text = item.text.replace('Brand :', '');
+        })
+      }
+      // if (row.descriptionBullets) {
+      //   let text = '';
+      //   row.descriptionBullets.forEach(item => {
+      //     console.log("item", item);
+      //     text = item.value;
+      //   });
+      //   row.descriptionBullets = [
+      //     {
+      //       text: text,
+      //     },
+      //   ];
+      // }
 
 
     }
   }
-   // Clean up data
-   const clean = text => text.toString()
-   .replace(/\r\n|\r|\n/g, ' ')
-   .replace(/&amp;nbsp;/g, ' ')
-   .replace(/&amp;#160/g, ' ')
-   .replace(/\u00A0/g, ' ')
-   .replace(/\s{2,}/g, ' ')
-   .replace(/"\s{1,}/g, '"')
-   .replace(/\s{1,}"/g, '"')
-   .replace(/^ +| +$|( )+/g, ' ')
-   // eslint-disable-next-line no-control-regex
-   .replace(/[\x00-\x1F]/g, '')
-   .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  // Clean up data
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
 
- data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-   el.text = clean(el.text);
- }))));
+  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+    el.text = clean(el.text);
+  }))));
 
   return data;
 };
