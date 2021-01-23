1e3c18742a34cf2edd0f1d668689e169a8dced19
Brinda <brinda.allamsetty@od.import.io>
2021-01-23 23:26:42 +0530

    frisco

--- src/library/product/search/stores/f/frisco/PL/execute.js
+++ src/library/product/search/stores/f/frisco/PL/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'frisco',
     domain: 'frisco.pl',
     url: 'https://commerce.frisco.pl/api/offer/products/query?includeCategories=true&pageIndex=1&search={searchTerms}&deliveryMethod=Van&pageSize=150&language=pl&facetCount=150&includeWineFacets=false#[!opt!]{"type":"json"}[/!opt!]',
-    loadedSelector: 'body',
-    noResultsXPath: null,
+    loadedSelector: 'td.products.depth_0 > table > tbody > tr',
+    noResultsXPath: 'div.alert.alert-danger',
   },
 };
