0e174020321c26945b86702c91ab14f0951fa793
alscotty <alawrencescott@gmail.com>
2021-01-22 16:10:17 -0800

    testing captcha solution

--- src/library/navigation/goto/domains/go/google.com.js
+++ src/library/navigation/goto/domains/go/google.com.js
@@ -8,4 +8,26 @@ module.exports = {
     country: 'US',
     store: 'google',
   },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    
+    await context.goto(url, { 
+      timeout: 15000,
+      js_enabled: true,
+      css_enabled: false,
+      random_move_mouse: true,
+     })
+      // .catch((err) => {
+      //   let captchaTypeText = err.data.captchaType;
+      //   console.log(`Recaptcha hit? ${captchaTypeText}`);
+      //   console.log(`${err.message}`)
+      // });
+
+      let test = await context.evaluate(()=>{
+        return "Test";
+      })
+
+      // doesn't reach this code:
+      console.log(test);
+
+  },
 };
