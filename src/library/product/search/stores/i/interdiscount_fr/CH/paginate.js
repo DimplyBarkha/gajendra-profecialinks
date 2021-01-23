27f95ba70cddacb6cf8768fffc5d0a204fa4f8d9
PR <pravinr@synerzipune.local>
2021-01-23 20:21:55 +0530

    interdiscount_fr/CH paginate issue fixed

--- src/library/product/search/stores/i/interdiscount_fr/CH/paginate.js
+++ src/library/product/search/stores/i/interdiscount_fr/CH/paginate.js
@@ -1,15 +1,17 @@
 
 module.exports = {
-  implements: 'product/search/paginate',
+  implements: 'navigation/paginate',
   parameterValues: {
     country: 'CH',
     store: 'interdiscount_fr',
-    nextLinkSelector: 'li[class="l-Be8I"]:last-child a._3XbJWN',
+    // nextLinkSelector: 'li[class="l-Be8I"]:last-child a._3XbJWN div',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div._3oe9VX',
     //noResultsXPath: '//h1[@class="uIyEJC _35UgO3 _1EbEh9"]',
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: '//www.interdiscount.ch/fr/search?search={searchTerms}&page={page}',
+    },
     domain: 'interdiscount.ch',
     zipcode: '',
   },
