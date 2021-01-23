34e142c8874a75df5d88818029f3393c8581d556
Wayne E <bartekstafiej2@gmail.com>
2021-01-23 11:22:31 +0100

    Fixed noResultXpath (#1141)

--- src/library/product/search/stores/a/alcampo/ES/paginate.js
+++ src/library/product/search/stores/a/alcampo/ES/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div.span-20.last.slideGridDiv3',
-    noResultsXPath: '//div[@class="span-24"] | //div[@class="page-not-found-content"] | //div[@class="title_holder titleNoResult"]',
+    noResultsXPath: '//div[@class="page-not-found-content"] | //div[@class="title_holder titleNoResult"]',
     openSearchDefinition: null,
     domain: 'alcampo.es',
     zipcode: '',
