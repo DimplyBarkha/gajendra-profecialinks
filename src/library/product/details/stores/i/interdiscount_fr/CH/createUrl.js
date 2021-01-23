4ee801fe99790fa413d20a80c68a87f3a34802d4
Rajeev <rajeev.ranjan@synerzip.com>
2021-01-23 12:19:14 +0530

    fixes

--- src/library/product/details/stores/i/interdiscount_fr/CH/createUrl.js
+++ src/library/product/details/stores/i/interdiscount_fr/CH/createUrl.js
@@ -3,8 +3,9 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'interdiscount.ch',
-    // prefix: 'de',
-    url: 'https://www.interdiscount.ch/fr/--p{id}',
+    prefix: null,
+    url: null,
+    // url: 'https://www.interdiscount.ch/fr/--p{id}',
     country: 'CH',
     store: 'interdiscount_fr',
     zipcode: '',
--- src/library/product/details/stores/i/interdiscount_fr/CH/execute.js
+++ src/library/product/details/stores/i/interdiscount_fr/CH/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'CH',
     store: 'interdiscount_fr',
     domain: 'interdiscount.ch',
-    loadedSelector: 'div[class~="demoup-container"] img',
+    // loadedSelector: 'div[class~="demoup-container"] img',
     noResultsXPath: '//h1[contains(text(),"Seite nicht gefunden (404)")] | //*[contains(text(),"Dieses Produkt ist leider nicht mehr verfügbar")]',
     zipcode: '',
   },
