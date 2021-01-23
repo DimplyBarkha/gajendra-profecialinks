f2892cde544302053dc8c55e37fc91cf2278de32
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2021-01-23 16:53:52 +0530

    updated

--- src/library/product/search/stores/s/sephora/BR/extract.js
+++ src/library/product/search/stores/s/sephora/BR/extract.js
@@ -1,12 +1,38 @@
 
+
+// const {transform} = require('../format')
+// module.exports = {
+//   implements: 'product/search/extract',
+//   parameterValues: {
+//     country: 'BR',
+//     store: 'sephora',
+//     transform,
+//     domain: 'sephora.com.br',
+//     zipcode: '',
+//   },
+// };
 const {transform} = require('../format')
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'BR',
     store: 'sephora',
-    transform,
+    transform: null,
     domain: 'sephora.com.br',
-    zipcode: '',
+  },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(async function () {
+      let popUps = document.querySelector('div.box-list-button div.button-progress');
+      if (popUps) {
+        popUps.click();
+      }
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
