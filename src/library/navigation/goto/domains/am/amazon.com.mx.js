8449c6b9910263ed2d856011ed4b93fe0475df56
John Felix J <johnfelix.josep@hcl.com>
2021-01-23 14:53:07 +0530

    fixes

--- src/library/navigation/goto/domains/am/amazon.com.mx.js
+++ src/library/navigation/goto/domains/am/amazon.com.mx.js
@@ -1,20 +1,9 @@
-
 module.exports = {
-<<<<<<< HEAD
-  implements: 'navigation/goto',
-  parameterValues: {
-    domain: 'amazon.com.mx',
-    timeout: null,
-    country: 'MX',
-    store: 'amazon',
-    zipcode: '',
-=======
   extends: 'navigation/goto/domains/am/amazon',
   parameterValues: {
     domain: 'amazon.com.mx',
     country: 'MX',
     store: 'amazon',
     timeout: 90000,
->>>>>>> bab5e9660a1bf92fee78ef0923e7cc6f6cdec1d9
   },
 };
--- src/library/navigation/goto/domains/am/setZipCode.amazon.com.au.js
+++ src/library/navigation/goto/domains/am/setZipCode.amazon.com.au.js
@@ -4,11 +4,7 @@ module.exports = {
   parameterValues: {
     country: 'AU',
     domain: 'amazon.com.au',
-<<<<<<< HEAD
     store: 'amazonMweb',
-=======
-    store: 'amazon',
->>>>>>> bab5e9660a1bf92fee78ef0923e7cc6f6cdec1d9
     zipcode: '',
   },
 };
