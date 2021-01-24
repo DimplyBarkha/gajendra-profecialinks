eae9d916307c365fbe0aecff323eafbd376ffb83
adheer <sangeta.kumari@synerzip.com>
2021-01-24 18:50:26 +0530

    Kick back isuues

--- src/library/product/details/stores/r/rappi/CO/extract.js
+++ src/library/product/details/stores/r/rappi/CO/extract.js
@@ -1,4 +1,28 @@
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  console.log("inputs:: ", inputs);
+  const { url, id } = inputs;
+  console.log("parameters:: ", parameters);
+  if (id) {
+    await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    await context.waitForXPath('//article//a');
+    await context.waitForSelector('article> a');
+    console.log('everything fine !!!');
+    await context.evaluate(() => {
+      const firstItem = document.querySelector('article> a');
+      firstItem.click();
+    });
+  }
 
+  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,4 +32,5 @@ module.exports = {
     domain: 'rappi.com.co',
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/details/stores/r/rappi_farmatodo/CO/extract.js
+++ src/library/product/details/stores/r/rappi_farmatodo/CO/extract.js
@@ -1,11 +1,48 @@
+const { transform } = require('../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  try {
+    await context.waitForSelector('div.paragraph a.ng-star-inserted');
+    await context.click('a.ng-star-inserted');
+  } catch (error) {
+    console.log('cookie pop up not loded', error);
+  }
+  await context.evaluate(async () => {
+    function addHiddenDiv (vidurl, content) {
+      const newDiv = document.createElement('div');
+      newDiv.setAttribute('data-vidurl', vidurl);
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+
+  //   if (document.querySelector('div.paragraph a')) {
+  //     // @ts-ignore
+  //     document.querySelector('a.ng-star-inserted').click();
+  //     await new Promise(resolve => setTimeout(resolve, 1000));
+  //   } else if (document.querySelector('div.tab-specs-row')) {
+  //     // @ts-ignore
+  //     document.querySelector('li#tab-more-info').click();
+  //     await new Promise(resolve => setTimeout(resolve, 1000));
+  //   }
+  });
+  return await context.extract(productDetails, { transform });
+}
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'CO',
     store: 'rappi_farmatodo',
-    transform: null,
+    transform: transform,
     domain: 'rappi.com.co',
     zipcode: '',
   },
+  //implementation,
 };
