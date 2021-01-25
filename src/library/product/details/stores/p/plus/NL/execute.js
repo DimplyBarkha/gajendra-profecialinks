42530c90c83ea1f72d9ebd6be2ce89553330113e
Vivek Gondil <gondilvivek@gmail.com>
2021-01-25 16:51:43 +0530

    Added the no result xpath

--- src/library/product/details/stores/p/plus/NL/execute.js
+++ src/library/product/details/stores/p/plus/NL/execute.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-  implements: 'product/details/execute',
-  parameterValues: {
-    country: 'NL',
-    store: 'plus',
-    domain: 'plus.nl',
-    loadedSelector: '.ish-productList-item img',
-    noResultsXPath: null,
-    zipcode: '',
-  },
+    implements: 'product/details/execute',
+    parameterValues: {
+        country: 'NL',
+        store: 'plus',
+        domain: 'plus.nl',
+        loadedSelector: '.ish-productList-item img',
+        noResultsXPath: '//*[@class="ish-search-noResults-title"]',
+        zipcode: '',
+    },
 };
