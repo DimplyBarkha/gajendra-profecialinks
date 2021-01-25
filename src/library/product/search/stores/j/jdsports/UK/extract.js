cc985d027963238275fbc1aba82cb7a69804b2ba
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-01-25 19:37:09 +0530

    #2667 devjdsportssearch

--- src/library/product/search/stores/j/jdsports/UK/extract.js
+++ src/library/product/search/stores/j/jdsports/UK/extract.js
@@ -1,4 +1,21 @@
 const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async () => {
+    // while(!!document.querySelector('#search-grid_0 > div.col-12.bloom-load-wrapper > button')){
+      // document.querySelector('#search-grid_0 > div.col-12.bloom-load-wrapper > button').click()
+      // await new Promise(r => setTimeout(r, 8000));
+      await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    
+  })
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -7,6 +24,5 @@ module.exports = {
     transform: transform,
     domain: 'jdsports.co.uk',
   },
-  
+  implementation,
 };
-
