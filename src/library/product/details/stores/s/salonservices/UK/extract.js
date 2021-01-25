d542b5f06635a8dd99c2b0a143c2fa1253025c06
waquas ali <waquas2ali@gmail.com>
2021-01-26 00:05:49 +0530

    updating changes...

--- src/library/product/details/stores/s/salonservices/UK/extract.js
+++ src/library/product/details/stores/s/salonservices/UK/extract.js
@@ -13,6 +13,25 @@ module.exports = {
     context,
     dependencies,
   ) => {
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
+    }
+    await autoScroll(context);
     await context.evaluate(async function () {
       function addTempDiv (id, data) {
         const tempDiv = document.createElement('div');
