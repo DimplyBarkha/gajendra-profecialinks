80a110a54a6111d0c4d50cbdccefc1f884cbd8dc
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-25 11:25:26 -0800

    Modify inthebox xpath

--- src/library/product/details/stores/t/trademe/NZ/extract.js
+++ src/library/product/details/stores/t/trademe/NZ/extract.js
@@ -1,4 +1,32 @@
 const { transform } = require('../format');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addHiddenDiv (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const elList = document.querySelectorAll("div.tm-marketplace-listing-body__item p");
+    let inTheBox = null;
+    elList.forEach(function(el) {
+      if (el.innerHTML.indexOf("in the box") !== -1) {
+        inTheBox = el;
+      }
+    });
+    const inTheBoxText = inTheBox ? inTheBox.nextElementSibling.textContent.replace(/\n/g, ' || ') : '';
+    addHiddenDiv('ii_inTheBoxText', inTheBoxText);
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,4 +36,5 @@ module.exports = {
     domain: 'trademe.co.nz',
     zipcode: "''",
   },
+  implementation,
 };
