1e6b870bee797b043f4ff47649be16146167de30
Raghav <user.email>
2021-01-25 21:58:00 +0530

    updating custom code for price to update . with , and searchUrl code

--- src/library/product/search/stores/l/lider/CL/extract.js
+++ src/library/product/search/stores/l/lider/CL/extract.js
@@ -1,12 +1,60 @@
-
 const { transform } = require("../../../../shared");
 module.exports = {
-  implements: 'product/search/extract',
+  implements: "product/search/extract",
   parameterValues: {
-    country: 'CL',
-    store: 'lider',
+    country: "CL",
+    store: "lider",
     transform: transform,
-    domain: 'lider.cl',
-    zipcode: '',
+    domain: "lider.cl",
+    zipcode: "",
+  },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.evaluate(() => {
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(
+          xpath,
+          document,
+          null,
+          XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+          null
+        );
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+      function addHiddenDiv1(id, content, index) {
+        const newDiv = document.createElement("div");
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = "none";
+        const originalDiv = document.querySelectorAll('span[class="price-sell"]')[index];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      }
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement("div");
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = "none";
+        document.body.appendChild(catElement);
+      }
+      var price = getAllXpath("//span[@class='price-sell']//text()","nodeValue");
+      if (price != null) {
+        for (var i = 0; i < price.length; i++) {
+          var price1 = price[i].replace("." , ",");
+          addHiddenDiv1("price", price1, i);
+        }
+      }
+      const URL = window.location.href;
+      try {
+        document.getElementById("pd_url").remove();
+      } catch (error) {}
+      addElementToDocument("pd_url", URL);
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
