7991f4a03831ac914e3daa4476d76f430b63108c
Bartosz Kurzawa <kurzawa.bart@gmail.com>
2021-01-25 13:08:14 +0100

    Adjusted pagination

--- src/library/product/search/stores/p/pharmasimple/BE/paginate.js
+++ src/library/product/search/stores/p/pharmasimple/BE/paginate.js
@@ -4,13 +4,15 @@ module.exports = {
   parameterValues: {
     country: 'BE',
     store: 'pharmasimple',
-    nextLinkSelector: 'li:not([class*="disabled"]) i.icon-angle-right',
+    nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'ul[class="product_list grid row"] div.product-container',
     noResultsXPath: '//p[@class="alert alert-warning"]',
     resultsDivSelector: null,
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://pharmasimple.com/module/ambjolisearch/jolisearch?search_query={searchTerms}&p={page}',
+    },
     domain: 'pharmasimple.com',
     zipcode: '',
   },
