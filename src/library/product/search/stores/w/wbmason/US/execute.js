c4c677bbd401e0628a0c9af485ec11217d62c9db
rajeshProfecialinks <rajesh.g@profecialinks.com>
2021-01-25 17:34:46 +0530

    UAT rank, row count, pagination fix

--- src/library/product/search/stores/w/wbmason/US/execute.js
+++ src/library/product/search/stores/w/wbmason/US/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'wbmason',
     domain: 'wbmason.com',
     url: "https://www.wbmason.com/SearchResults.aspx?Keyword={searchTerms}&sc=BM&fi=1&fr=1",
-    loadedSelector: null,
+    loadedSelector: 'div.search-rightcol div#ctl00_ContentPlaceholder1_ProductList_pnlProductsGrid div.grid-row div',
     noResultsXPath: '//span[@id="ctl00_ContentPlaceholder1_NoResultsUC_NoResultsText"]',
     zipcode: '',
   },
