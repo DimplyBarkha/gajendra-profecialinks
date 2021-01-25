d2a4d3e0c045bfada9cb0c559385974b15c51eac
Your Name <karthik.ravi-odc@partner.import.io>
2021-01-25 19:39:12 +0530

    zip

--- src/library/product/details/stores/j/jpg/FR/extract.js
+++ src/library/product/details/stores/j/jpg/FR/extract.js
@@ -1,10 +1,10 @@
-const { transform } = require('../../../../shared');
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'FR',
     store: 'jpg',
-    transform: transform,
+    transform: cleanUp,
     domain: 'jpg.fr',
     zipcode: '',
   },
