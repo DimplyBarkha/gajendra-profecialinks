2d845b0105a3735cdbfed9b8016c1ea2d312c2e8
avinashkadam <avinash.kadam@profecialinks.com>
2021-01-26 12:08:17 +0530

    added url with id for product url missing issue

--- src/library/product/details/stores/o/officeworks/AU/createUrl.js
+++ src/library/product/details/stores/o/officeworks/AU/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'officeworks.com.au',
     prefix: null,
-    url: null,
+    url: 'https://www.officeworks.com.au/shop/officeworks/p/{id}',
     country: 'AU',
     store: 'officeworks',
     zipcode: '',
