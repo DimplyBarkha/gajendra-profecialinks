d5b4a09036cc75873dcaabcc415b741a58d62a42
Gajender Ramola <ramola106@gmail.com>
2021-01-26 22:47:27 +0530

    fix

--- src/library/product/search/stores/t/tottus/CL/format.js
+++ src/library/product/search/stores/t/tottus/CL/format.js
@@ -3,18 +3,18 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
-  const cleanUp = (data, context) => {
+const transform = (data, context) => {
+  const cleanUp = (data) => {
     const clean = text => text.toString()
       .replace(/\r\n|\r|\n/g, ' ')
       .replace(/&amp;nbsp;/g, ' ')
       .replace(/&amp;#160/g, ' ')
       .replace(/\u00A0/g, ' ')
       .replace(/\s{2,}/g, ' ')
-    // .replace(/"\s{1,}/g, '"')
-    // .replace(/\s{1,}"/g, '"')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
       .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
+      // eslint-disable-next-line no-control-regex
       .replace(/[\x00-\x1F]/g, '')
       .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
     data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
@@ -22,8 +22,9 @@ const transform = (data) => {
     }))));
     return data;
   };
+  const state = context.getState();
+  let rank = state.rank || 1;
   for (const { group } of data) {
-    let rank = 1;
     for (const row of group) {
       if (row.thumbnail) {
         row.thumbnail.forEach(item => {
@@ -39,7 +40,10 @@ const transform = (data) => {
       rank++;
     }
   }
+  context.setState({ rank });
   return cleanUp(data);
 };
 
 module.exports = { transform };
+
+
