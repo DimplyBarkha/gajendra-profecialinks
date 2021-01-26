badfb58d10bc6d5f186b5114eb5fea60c0109f4f
Himanshu-Agrahari <himanshu.a@partner.import.io>
2021-01-26 14:10:16 +0530

    Remove commented code

--- src/library/product/details/stores/f/flaconi/DE/extract.js
+++ src/library/product/details/stores/f/flaconi/DE/extract.js
@@ -8,47 +8,6 @@ module.exports = {
     domain: 'flaconi.de',
   },
   implementation,
-  // implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
-  //   await context.evaluate(async function () {
-  //     function addElementToDocument (key, value) {
-  //       const catElement = document.createElement('div');
-  //       catElement.id = key;
-  //       catElement.textContent = value;
-  //       catElement.style.display = 'none';
-  //       document.body.appendChild(catElement);
-  //     }
-  //     function addVideoElementToDocument (key, arr) {
-  //       const catElement = document.createElement('div');
-  //       catElement.id = key;
-  //       for (let i = 0; i < arr.length; i++) {
-  //         const videoElement = document.createElement('a');
-  //         videoElement.href = arr[i].href;
-  //         catElement.appendChild(videoElement);
-  //       }
-  //       catElement.style.display = 'none';
-  //       document.body.appendChild(catElement);
-  //     }
-
-  //     const directionelement1 = document.evaluate("//div[contains(@class, 'instruction-content')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  //     const directionelement2 = document.evaluate("//div[contains(@class, 'instruction')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  //     if (directionelement1) {
-  //       addElementToDocument('fl_directioninfo', directionelement1.innerText);
-  //     } else if (directionelement2) {
-  //       addElementToDocument('fl_directioninfo', directionelement2.innerText);
-  //     }
-
-  //     const colorlement = document.evaluate("//ul[@id='makeup-color-list']/li[1]//span/@style", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  //     if (colorlement && colorlement.value.indexOf('background-color') > -1) {
-  //       const colorCode = colorlement.value.slice(colorlement.value.indexOf('#') + 1);
-  //       addElementToDocument('fl_colorcode', colorCode);
-  //     }
-  //     const videoarr = document.querySelectorAll('div.lazyYoutube > a[title=""]');
-  //     if (videoarr && videoarr.length) {
-  //       addVideoElementToDocument('pd_video', videoarr);
-  //     }
-  //   });
-  //   await context.extract(productDetails);
-  // },
 };
 async function implementation (
   // @ts-ignore
@@ -111,35 +70,7 @@ async function implementation (
         }
       }
     }
-    // Code for updp
-    // await context.evaluate(() => {
-    //     const updp = document.evaluate(
-    //         '//div[contains(@class,"related-products")]//a',
-    //         document,
-    //         null,
-    //         XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-    //         null
-    //     );
-    //     let text = '';
-    //     const brandText = []
-    //     const nameExt = []
-    //     const type = []
-    //     for (let i = 0; i < updp.snapshotLength; i++) {
-    //         const z = updp.snapshotItem(i);
-    //         // @ts-ignore
-    //         const p = z.querySelector('span.brand').innerText;
-    //         // @ts-ignore
-    //         const q = z.querySelector('span.series').innerText;
-    //         // @ts-ignore
-    //         const r = z.querySelector('span.type').innerText;
-    //         brandText.push(p);
-    //         nameExt.push(q);
-    //         type.push(r);
-    //         text += text ? ` || ${brandText[i]} ${nameExt[i]} ${type[i]}` : `${brandText[i]} ${nameExt[i]} ${type[i]}`;
 
-    //     }
-    //     document.body.setAttribute("updp-name", text);
-    // })
     const addToCartBtn = document.querySelectorAll('div.add-to-cart button');
     let availability = 'In Stock';
     if (addToCartBtn.length === 1) {
@@ -154,52 +85,6 @@ async function implementation (
     }
     addElementToDocument('fl_availabilityText', availability);
     // ---------------------------------------------
-    //  let description = document.querySelector("div[class='description-content']");
-    //   // @ts-ignore
-    //   description = description ? description.innerText : '';
-    //   let descArr = [];
-    //   // @ts-ignore
-    //   if(description !== ''){
-    //     descArr.push(description);
-    //   }
-    //   // let bulletsDescription = document.querySelectorAll("div.product-description ul li");
-    //   let bulletsDescription = document.querySelectorAll("ul.product-properties-list li");
-    //   console.log('bulletsDescription: ', bulletsDescription);
-    //   let bulletCount = bulletsDescription.length;
-    //   addElementToDocument('bb_descriptionBulletsCount', bulletCount);
-    //   for (let index = 0; index < bulletsDescription.length; index++) {
-    //     let element = bulletsDescription[index];
-    //     // @ts-ignore
-    //     element = element ? element.innerText.replace(/(\s*[\r\n]\s*)+/g, ' ') : '';
-    //     descArr.push(element);
-    //   }
-    //   // @ts-ignore
-    //   descArr = descArr.join(' || ');
-    //   addElementToDocument('bb_descriptionBullets', descArr);
-    // ---------------------------------------------
-
-    /* Old description */
-    // let description = document.querySelector("div[itemprop='description']");
-    // // @ts-ignore
-    // description = description ? description.innerHTML : '';
-    // let descArr = [];
-    // // @ts-ignore
-    // if (description !== '') {
-    //   // @ts-ignore
-    //   description = description ? description.replace(/<li>/gm, ' || ').replace(/<.*?>/gm, '').replace(/\n/gm, ' ').replace(/\s{2,}/, ' ').trim() : '';
-    //   descArr.push(description);
-    // }
-    // // let bulletsDescription = document.querySelectorAll("div.product-description ul li");
-    // const bulletsDescription = document.querySelectorAll('ul.product-properties-list li');
-    // for (let index = 0; index < bulletsDescription.length; index++) {
-    //   let element = bulletsDescription[index];
-    //   // @ts-ignore
-    //   element = element ? element.innerText.replace(/(\s*[\r\n]\s*)+/g, ' ') : '';
-    //   descArr.push(element);
-    // }
-    // // @ts-ignore
-    // descArr = descArr.join(' || ');
-    // addElementToDocument('bb_descriptionBullets', descArr);
 
     const description = document.querySelector(".product-description div[itemprop='description']");
     let descText = '';
