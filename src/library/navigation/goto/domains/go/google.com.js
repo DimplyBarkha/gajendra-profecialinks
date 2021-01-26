47f9a2e8fd4e6a8a1296f152fdfcc01cb87fcc10
alscotty <alawrencescott@gmail.com>
2021-01-25 17:38:23 -0800

    add rest of captcha logic

--- src/library/navigation/goto/domains/go/google.com.js
+++ src/library/navigation/goto/domains/go/google.com.js
@@ -9,41 +9,82 @@ module.exports = {
     store: 'google',
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+
     await context.goto(url, {
       timeout: 15000,
       checkBlocked: false,
       js_enabled: true,
       css_enabled: false,
       random_move_mouse: true,
+      antiCaptchaOptions: {
+        type: 'RECAPTCHA'
+      }
     });
     const recaptchaPresent = await context.evaluate(() => {
       const recaptchaIframe = document.querySelector('div#recaptcha');
       return !!recaptchaIframe;
+    });
+    const iframeFound = await context.evaluate(() => {
+      const iframe = document.querySelector('iframe');
+      return !!iframe;
     });
 
     const captchaSolver = async () => {
       await context.solveCaptcha({ type: 'RECAPTCHA', inputElement: 'div#recaptcha' });
     };
 
-    const currentUrl = await context.evaluate(()=>{
-      return window.location.href;
-    })
+    const currentUrl = async () =>{
+       return await context.evaluate(()=> {
+        return window.location.href
+      });
+    }
 
     if (recaptchaPresent) {
       console.log('Hit recaptcha, attempting to solve..');
-      const maxAttempts = 4;
+      console.log(`iframe present: ${iframeFound}`)
+      
+      const maxAttempts = 5;
       let solved = false;
       let currentAttempts = 1;
-
+      
       while (!solved && (currentAttempts < maxAttempts)) {
         console.log(`on attempt: ${currentAttempts}`);
-        await captchaSolver();
-        await context.waitForNavigation({ timeout: 10000, waitUntil: 'load' });
-        console.log(currentUrl)
-        await new Promise((resolve,reject) => setTimeout(resolve, 3000));
-        if (!recaptchaPresent) {
-          solved = true;
+        await context.click('div.captcha-handler',{ timeout: 4000 })
+        .then(async()=>await new Promise(res=>setTimeout(res,2000)))
+        .catch(()=>console.log('No click required'));
+        
+        if (iframeFound){
+          // @ts-ignore      // eslint-disable-next-line no-undef  
+          await context.evaluateInFrame('iframe', () => grecaptcha.execute())
+        } else {
+          await captchaSolver()
         }
+        if (await context.evaluate(() => {
+          return !!document.querySelector('div[captchastatus=ok]');
+        })){
+          console.log('Captcha solved! Submitting form..')
+          await context.evaluate(()=>{
+            document.getElementById('captcha-form').submit();
+          });
+          await new Promise((resolve,reject) => setTimeout(resolve, 7000));
+          let captchaGone = await context.evaluate(()=>{
+            return !!document.querySelector('div#recaptcha');
+          })
+
+          console.log(`Is captcha really gone? ${captchaGone}`)
+          let thisUrl = await currentUrl();
+          console.log(thisUrl)
+          if(captchaGone && !(thisUrl.includes('sorry'))){
+            solved= true;
+          }
+        }
+
+        // await context.waitForNavigation({ timeout: 10000, waitUntil: 'load' });
+        console.log(await currentUrl())
+        await new Promise((resolve,reject) => setTimeout(resolve, 3000));
+    
         currentAttempts++;
       }
 
