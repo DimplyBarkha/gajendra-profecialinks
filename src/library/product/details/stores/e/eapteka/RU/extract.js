0899b048150670caceabdb4f6539d1688b4c0891
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-01-26 12:43:16 +0100

    #2963 - Applying UAT tool errors fixes - Updating tests

--- src/library/product/details/stores/e/eapteka/RU/extract.js
+++ src/library/product/details/stores/e/eapteka/RU/extract.js
@@ -14,20 +14,47 @@ module.exports = {
       const addedElem = document.createElement('div');
       addedElem.id = 'added_elem';
 
-      const productName = document.querySelector('div.new-offer-box h1') ? document.querySelector('div.new-offer-box h1').textContent : '';
+      const productName = document.querySelector('div.new-offer-box h1')
+        ? document.querySelector('div.new-offer-box h1').textContent
+        : '';
       const quantity = productName.match(/(\d+) шт.*?$/) ? productName.match(/(\d+) шт.*?$/)[1] : '';
       const packSize = productName.match(/(\d+)х\d+ шт.*?$/) ? productName.match(/(\d+)х\d+ шт.*?$/)[1] : '';
 
       addedElem.setAttribute('quantity', quantity);
       addedElem.setAttribute('pack_size', packSize);
 
-      const warningsElements = document.querySelectorAll('div#instruction_PREGNANCY_USE, div#instruction_CONTRAINDICATIONS, div#instruction_SIDE_EFFECTS, div#instruction_INTERACTION, div#instruction_OVERDOSE');
-      const warnings = Array.from(warningsElements).map(elem => elem.textContent).join(' | ');
+      let price = document
+        .evaluate(
+          '//div[contains(@class, "offer-card__info")]//span[contains(@class, "offer-tools__price")]/@data-price',
+          document,
+          null,
+          XPathResult.STRING_TYPE,
+          null,
+        )
+        .stringValue.trim();
+      if (!price) {
+        const prodObjText = document.querySelector('div#bitem')
+          ? document.querySelector('div#bitem').getAttribute('data-ga-offer')
+          : '{}';
+        const prodObj = JSON.parse(prodObjText);
+        price = prodObj.price;
+      }
+      addedElem.setAttribute('price', `${price} руб.`);
+
+      const warningsElements = document.querySelectorAll(
+        'div#instruction_PREGNANCY_USE > div, div#instruction_CONTRAINDICATIONS > div, div#instruction_SIDE_EFFECTS > div, div#instruction_INTERACTION > div, div#instruction_OVERDOSE > div, div#instruction_SPECIAL_INSTRUCTIONS > div',
+      );
+      const warnings = Array.from(warningsElements)
+        .map((elem) => elem.textContent)
+        .join(' | ');
 
       addedElem.setAttribute('warnings', warnings);
 
       const ratingElem = document.querySelector('div.offer-card__rating div.rating');
-      const rating = ratingElem && ratingElem.getAttribute('class').match(/rating s(\d+)/) ? ratingElem.getAttribute('class').match(/rating s(\d+)/)[1] : '';
+      const rating =
+        ratingElem && ratingElem.getAttribute('class').match(/rating s(\d+)/)
+          ? ratingElem.getAttribute('class').match(/rating s(\d+)/)[1]
+          : '';
       addedElem.setAttribute('aggregate_rating', rating);
 
       const manufacturerImagesList = document.createElement('ul');
@@ -44,7 +71,13 @@ module.exports = {
         }
       }
 
-      const availabilityElem = document.evaluate('//p[contains(@class, "offer-tools__status") and contains(@class, "good")]', document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
+      const availabilityElem = document.evaluate(
+        '//p[contains(@class, "offer-tools__status") and contains(@class, "good")]',
+        document,
+        null,
+        XPathResult.ANY_UNORDERED_NODE_TYPE,
+        null,
+      ).singleNodeValue;
       addedElem.setAttribute('availability_text', availabilityElem ? 'In Stock' : 'Out Of Stock');
 
       document.body.appendChild(manufacturerImagesList);
