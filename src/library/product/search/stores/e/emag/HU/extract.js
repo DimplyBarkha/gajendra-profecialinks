3ed62968b2da64017525c836981c66ecea43c034
Karina Miernik <karina.miernik@gmail.com>
2021-01-27 15:09:57 +0100

    #2799 issue fixed

--- src/library/product/search/stores/e/emag/HU/extract.js
+++ src/library/product/search/stores/e/emag/HU/extract.js
@@ -12,7 +12,14 @@ async function implementation (inputs, parameters, context, dependencies) {
         element.setAttribute('price', price.replace('.', ','));
       }
     });
-
+    // id
+    const ids = document.querySelectorAll('div[class="card-item js-product-data"] div[class="card-toolbox"] > button[data-productid]');
+    ids.forEach(e => {
+      if (e.getAttribute('data-product') !== null) {
+        const id = JSON.parse(e.getAttribute('data-product')).pnk;
+        e.setAttribute('product-sku', id);
+      }
+    });
     // replace . with , in rating
     var rating = document.querySelectorAll('div.card-item.js-product-data div.star-rating.star-rating-read');
     const regex = /\d\.?(\d+)?/gm;
@@ -27,11 +34,19 @@ async function implementation (inputs, parameters, context, dependencies) {
     // reducing results to 150 and set rank
     const url = window.location.href;
     const allProducts = document.querySelectorAll('div[class="card-item js-product-data"]');
-    if (url.includes('/p3')) {
-      allProducts.forEach((e, i) => {
-        e.setAttribute('rank', `${i}+1`);
-        if (i > 29) e.setAttribute('trim', '');
-      });
+    allProducts.forEach((e, i) => e.setAttribute('rank', `${i + 1}`));
+    const last = allProducts.length.toString();
+    if (!url.includes('/p2') || !url.includes('/p3') || !url.includes('/p4')) sessionStorage.setItem('item1', last);
+    if (url.includes('/p2')) sessionStorage.setItem('item2', last);
+    if (url.includes('/p3')) sessionStorage.setItem('item3', last);
+    if (url.includes('/p4')) sessionStorage.setItem('item4', last);
+    if (sessionStorage.getItem('item4') === null && sessionStorage.getItem('item3') !== null) {
+      const trim = 150 - (parseInt(sessionStorage.getItem('item1')) + parseInt(sessionStorage.getItem('item2')));
+      allProducts.forEach((e, i) => i > trim ? e.setAttribute('trim', '') : '');
+    }
+    if (sessionStorage.getItem('item4') !== null) {
+      const trim = 150 - (parseInt(sessionStorage.getItem('item1')) + parseInt(sessionStorage.getItem('item2')) + parseInt(sessionStorage.getItem('item3')));
+      allProducts.forEach((e, i) => i > trim ? e.setAttribute('trim', '') : '');
     }
   });
 
