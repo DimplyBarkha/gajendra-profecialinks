44b4946c7f0b6364e072619235b07efd64fcf74f
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-01-27 12:28:38 +0530

    chnages

---
+++ src/library/product/details/stores/f/Freshamazon/DE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'DE',
+    store: 'Freshamazon',
+    domain: 'amazon.de',
+    zipcode: '10243',
+  },
+};
---
+++ src/library/product/details/stores/f/Freshamazon/DE/variantsExtract.js
@@ -0,0 +1,17 @@
+const { implementation } = require('../../../../sharedAmazon/variantExtract');
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'DE',
+    store: 'Freshamazon',
+    transform: null,
+    domain: 'amazon.de',
+    zipcode: '10243',
+  },
+  dependencies: {
+    Helpers: 'module:helpers/helpers',
+    AmazonHelp: 'module:helpers/amazonHelp',
+    variants: 'extraction:product/details/stores/${store[0:1]}/${store}/${country}/variantsExtract',
+  },
+  implementation,
+};
