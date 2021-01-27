0d4987059ccc7beee6d4b94231dda7c600d28fb2
Vivek Gondil <gondilvivek@gmail.com>
2021-01-27 12:20:57 +0530

    Added changes for IM_Search

--- src/library/product/search/stores/m/mediamarkt/PL/paginate.js
+++ src/library/product/search/stores/m/mediamarkt/PL/paginate.js
@@ -1,10 +1,12 @@
 module.exports = {
-  implements: 'product/search/paginate',
-  parameterValues: {
-    country: 'PL',
-    store: 'mediamarkt',
-    nextLinkSelector: 'div[class="b-listing_toolBarPagination"] > nav > a[class*="pagination_next"] , a[class*="is-nextPage"]>i',
-    domain: 'mediamarkt.pl',
-    zipcode: '',
-  },
+    implements: 'product/search/paginate',
+    parameterValues: {
+        country: 'PL',
+        store: 'mediamarkt',
+        nextLinkSelector: 'div[class="b-listing_toolBarPagination"] > nav > a[class*="pagination_next"] , a[class*="is-nextPage"]>i',
+        loadedSelector: 'div.m-offerBox ',
+        noResultsXPath: '//div[@class="s-search_empty"]',
+        domain: 'mediamarkt.pl',
+        zipcode: '',
+    },
 };
