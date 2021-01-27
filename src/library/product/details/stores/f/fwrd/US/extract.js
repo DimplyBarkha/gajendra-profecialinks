0adbc876dd269930571636143e3855a522b6174f
alscotty <alawrencescott@gmail.com>
2021-01-27 14:14:22 -0800

    update listprice and recordxpath

--- src/library/product/details/stores/f/fwrd/US/extract.js
+++ src/library/product/details/stores/f/fwrd/US/extract.js
@@ -1,10 +1,11 @@
+const { transform } = require('./transform');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'fwrd',
-    transform: null,
+    transform,
     domain: 'fwrd.com',
     zipcode: '',
   },
