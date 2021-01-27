2856b7a85ac31e65b7a04cdbda488f30065c2ded
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-26 17:26:41 -0800

    Change loop

--- src/library/product/details/stores/s/saksfifthavenue/US/extract.js
+++ src/library/product/details/stores/s/saksfifthavenue/US/extract.js
@@ -20,10 +20,10 @@ module.exports = {
       const isVisible = (element) => (!!(element).offsetWidth || (element).offsetHeight);
 
       async function addAttributes (node) {
-        const inventory = document.evaluate('//div[contains(@class, "product-availability")]//ul[contains(@class, "availability-msg")]|//div[@id="waitlist"][not(contains(@class, "d-none"))]//div[contains(@class, "soldout")]', document, null, XPathResult.STRING_TYPE, null);
-        if (inventory && inventory.stringValue) {
-          node.setAttribute('inventory', inventory.stringValue);
-        }
+        // const inventory = document.evaluate('//div[contains(@class, "product-availability")]//ul[contains(@class, "availability-msg")]|//div[@id="waitlist"][not(contains(@class, "d-none"))]//div[contains(@class, "soldout")]', document, null, XPathResult.STRING_TYPE, null);
+        // if (inventory && inventory.stringValue) {
+        //   node.setAttribute('inventory', inventory.stringValue);
+        // }
         const availText = document.evaluate('//button[contains(@class, "add-to-cart btn")][not(@disabled)]/@class', document, null, XPathResult.STRING_TYPE, null);
         if (availText && availText.stringValue) {
           node.setAttribute('availability', 'true');
@@ -33,10 +33,11 @@ module.exports = {
       for (let i = 0; i < document.querySelectorAll('ul.size-attribute li').length; i++) {
         const element = document.querySelectorAll('ul.size-attribute li')[i];
         if (isVisible(element) === true) {
-          element.querySelector('a').click();
+          // element.querySelector('a').click();
           await delay(5000);
           const elm = element.querySelector('a');
-          await addAttributes(elm);
+          elm.setAttribute('availability', (element.className.includes('unselectable') ? 'false' : 'true'));
+          // await addAttributes(elm);
         }
       }
     });
