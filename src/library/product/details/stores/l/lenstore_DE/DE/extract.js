a8e10de2e9e476543c3ea39490e9fdc5618c82dd
Harshkha <harsh.khare-od@partner.import.io>
2021-01-27 20:19:04 +0530

    CORE complete

--- src/library/product/details/stores/l/lenstore_DE/DE/extract.js
+++ src/library/product/details/stores/l/lenstore_DE/DE/extract.js
@@ -35,7 +35,41 @@ module.exports = {
         else result = elem ? elem.singleNodeValue : '';
         return result && result.trim ? result.trim() : result;
       };
-      
+      var aval = getXpath('//span[@class="error u-error"]/text()', 'nodeValue');
+      var lim = getXpath('//p[@id="limitedinfo"]/text()', 'nodeValue');
+      if (aval != null) {
+        var availability = "Out of Stock"
+        addElementToDocument("availability", availability);
+      } else if (lim != null) {
+        var availability = "Limited availability"
+        addElementToDocument("availability", availability);
+      } else {
+        var availability = "In Stock"
+        addElementToDocument("availability", availability);
+      }
+      //descedit
+      // var desc = getXpath('(//div[@class="clear"]/p/text())[1] | //div[@id="relatedShift"]/text()', 'nodeValue');
+      // let str2 = desc
+      // let regexp2 = /(.*)Produkt Details|(.*)Produktinformation|(.*)Produktdetails/;
+      // let result2 = str2.match(regexp2)
+      // addElementToDocument('desc', result2[1])
+      var disc = getXpath('//div[@id="relatedShift"]/text()','nodeValue');
+      if (disc != null) {
+        if (disc.includes('Produkt Details')){
+          var newdisc=disc.split("Produkt Details")
+          var nn= newdisc[newdisc.length - 1]
+          addElementToDocument("desc", nn);
+        } else if (disc.includes('Produktinformation')){
+          var newdisc=disc.split("Produktinformation")
+          var nn= newdisc[newdisc.length - 1]
+          addElementToDocument("desc", nn);
+        } else {
+          var newdisc=disc.split("Produktdetails")
+          var nn= newdisc[newdisc.length - 1]
+          addElementToDocument("desc", nn);
+        }
+      }
+      //p[@id='limitedinfo']/text()
       // var dec = getXpath("//div[@id='productRight']/text()", 'nodeValue');
       // var str = "";
       // if (dec != null) {
@@ -56,6 +90,24 @@ module.exports = {
       try {
         if (script != null) {
           var data = JSON.parse(script);
+          try {
+            var rate = data.aggregateRating.reviewCount;
+            if (rate != null) {
+              addElementToDocument('rate', rate);
+            }
+          }
+          catch (error) {
+
+          }
+          try {
+            var rate2 = data.aggregateRating.ratingValue;
+            if (rate2 != null) {
+              addElementToDocument('rate2', rate2);
+            }
+          }
+          catch (error) {
+
+          }
 
           try {
             var gtin = data.gtin13;
