4f64e0c65d3c8a12dfb19782cdaf88e8f943671d
Vivek Gondil <gondilvivek@gmail.com>
2021-01-27 11:32:07 +0530

    Added changes for IM search Bijenkorf

--- src/library/product/search/stores/b/bijenkorf/NL/extract.js
+++ src/library/product/search/stores/b/bijenkorf/NL/extract.js
@@ -1,42 +1,44 @@
 const { transform } = require('../NL/shared');
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
+async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
 ) {
-  const { productDetails } = dependencies;
-  const { transform } = parameters;
-  async function autoScroll (page) {
-    await page.evaluate(async () => {
-      await new Promise((resolve, reject) => {
-        var totalHeight = 0;
-        var distance = 100;
-        var timer = setInterval(() => {
-          var scrollHeight = document.body.scrollHeight;
-          window.scrollBy(0, distance);
-          totalHeight += distance;
+    const { productDetails } = dependencies;
+    const { transform } = parameters;
+    async function autoScroll(page) {
+        await page.evaluate(async() => {
+            await new Promise((resolve, reject) => {
+                var totalHeight = 0;
+                var distance = 100;
+                var timer = setInterval(() => {
+                    var scrollHeight = document.body.scrollHeight;
+                    window.scrollBy(0, distance);
+                    totalHeight += distance;
 
-          if (totalHeight >= scrollHeight) {
-            clearInterval(timer);
-            resolve();
-          }
-        }, 50);
-      });
-    });
-  }
-  await autoScroll(context);
-  return await context.extract(productDetails, { transform });
+                    if (totalHeight >= scrollHeight) {
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 50);
+            });
+        });
+    }
+    await autoScroll(context);
+    if (await context.evaluate(() => { return !document.evaluate('//div[contains(@class,"dbk-search-empty")]', document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue })) {
+        return await context.extract(productDetails, { transform });
+    }
 }
 
 module.exports = {
-  implements: 'product/search/extract',
-  parameterValues: {
-    country: 'NL',
-    store: 'bijenkorf',
-    transform,
-    domain: 'bijenkorf.nl',
-    zipcode: '',
-  },
-  implementation,
+    implements: 'product/search/extract',
+    parameterValues: {
+        country: 'NL',
+        store: 'bijenkorf',
+        transform,
+        domain: 'bijenkorf.nl',
+        zipcode: '',
+    },
+    implementation,
 };
