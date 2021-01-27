e308b4ddc86271e8f23977edd4674b281f12ee72
rajeshProfecialinks <rajesh.g@profecialinks.com>
2021-01-27 16:26:35 +0530

    corrected search url, corrected pagination code

--- src/library/product/search/stores/z/zalando/FR/execute.js
+++ src/library/product/search/stores/z/zalando/FR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FR',
     store: 'zalando',
     domain: 'zalando.fr',
-    url: 'https://www.zalando.fr/femme/?q={searchTerms}',
+    url: 'https://www.zalando.fr/homme/?q={searchTerms}',
     loadedSelector: 'div[class="_8P5KBX"] div[class="adFHlH _0xLoFW _7ckuOK mROyo1"]',
     noResultsXPath: '//div[@class="cat_container-1Py_q"]/div[@class="cat_noSearchResultsHeadline-1Q-I5"]',
     zipcode: '',
