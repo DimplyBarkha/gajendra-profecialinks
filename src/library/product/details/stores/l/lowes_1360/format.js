5ecb88a698442f7cef2414badbee893ee5770678
Ajinkya Kathe <ajinkya.kathe@synerzip.com>
2021-01-27 23:12:19 +0530

    Minor changes

--- src/library/product/details/stores/l/lowes_1360/format.js
+++ src/library/product/details/stores/l/lowes_1360/format.js
@@ -15,7 +15,7 @@ const transform = (data) => {
     .replace(/"\s{1,}/g, '"')
     .replace(/\s{1,}"/g, '"')
     .replace(/^ +| +$|( )+/g, ' ')
-  // eslint-disable-next-line no-control-regex
+    // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
   data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
@@ -124,6 +124,14 @@ const transform = (data) => {
       //     },
       //   ];
       // }
+      if (row.inTheBoxText) {
+        const witb = Array.from(new Set(row.inTheBoxText.map(elm => elm.text.trim())));
+        row.inTheBoxText = witb.map(text => ({ text }));
+      }
+      if (row.inTheBoxUrl) {
+        const witb = Array.from(new Set(row.inTheBoxUrl.map(elm => elm.text.trim())));
+        row.inTheBoxUrl = witb.map(text => ({ text }));
+      }
     }
   }
   return data;
