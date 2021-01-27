c9d6335589901d9628129356baf21881f7c4bbfd
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2021-01-27 21:35:13 +0530

    added dummy url to match t1 data for variants extractor

--- src/library/product/details/stores/n/nykaa/IN/variantsExtract.js
+++ src/library/product/details/stores/n/nykaa/IN/variantsExtract.js
@@ -16,55 +16,75 @@ module.exports = {
   ) {
     const { variants } = dependencies;
     const { transform } = parameters;
-    await context.evaluate(() => {
-      const variants = document.querySelectorAll('ul[class*="options-colors"]>li>label>span');
-      const variantColorArr = [];
-      if (variants.length) {
-        variants.forEach((element) => {
-          element.click();
-          variantColorArr.push(window.location.href);
-        });
-      }
-      const sizeVariants = document.querySelectorAll('div[class*="sizes"]>ul>li>label>span');
-      const sizeArr = [];
-      if (sizeVariants.length) {
-        sizeVariants.forEach((element) => {
-          element.click();
-          sizeArr.push(window.location.href);
-        });
-      }
-      const wholeVariants = [...variantColorArr, ...sizeArr];
-      console.log(variantColorArr)
-      console.log(sizeArr)
-      console.log(wholeVariants)
-      if (wholeVariants.length == 0) {
-        const appendDiv = document.createElement('div');
-        appendDiv.className = 'variantinfo';
-        appendDiv.setAttribute('varianturl', window.location.href);
-        const defaultUrl = window.location.href;
-        let skuId = defaultUrl && defaultUrl.match(/(skuId=)(\d+)/g) && defaultUrl.match(/(skuId=)(\d+)/g)[0];
-        if (skuId == undefined || skuId == null) {
-          skuId = defaultUrl.match(/p\/\d+/g)[0];
+    await context.evaluate((inputs) => {
+      const is404 = document.querySelector('p.page_nt_found');
+      if (!is404) {
+        const variants = document.querySelectorAll('ul[class*="options-colors"]>li>label>span');
+        const variantColorArr = [];
+        if (variants.length) {
+          variants.forEach((element) => {
+            element.click();
+            variantColorArr.push(window.location.href);
+          });
         }
-        const id = skuId && skuId.match(/(\d+)/g) && skuId.match(/(\d+)/g)[0];
-        appendDiv.setAttribute('variantid', id);
-        document.querySelector('body').appendChild(appendDiv);
-      } else {
-        const variantId = [];
-        wholeVariants.forEach((element) => {
-          const skuId = element.match(/(skuId=)(\d+)/g)[0];
-          const id = skuId.match(/(\d+)/g)[0];
-          variantId.push(id);
-        });
-        variantId.forEach((element, index) => {
+        const sizeVariants = document.querySelectorAll('div[class*="sizes"]>ul>li>label>span');
+        const sizeArr = [];
+        if (sizeVariants.length) {
+          sizeVariants.forEach((element) => {
+            element.click();
+            sizeArr.push(window.location.href);
+          });
+        }
+        const wholeVariants = [...variantColorArr, ...sizeArr];
+        console.log(variantColorArr)
+        console.log(sizeArr)
+        console.log(wholeVariants)
+        if (wholeVariants.length == 0) {
           const appendDiv = document.createElement('div');
-          appendDiv.className = 'variantinfos';
-          appendDiv.setAttribute('variantid', variantId[index]);
-          appendDiv.setAttribute('varianturl', wholeVariants[index]);
-          document.body.append(appendDiv);
-        });
+          appendDiv.className = 'variantinfo';
+          appendDiv.setAttribute('varianturl', window.location.href);
+          const defaultUrl = window.location.href;
+          let skuId = defaultUrl && defaultUrl.match(/(skuId=)(\d+)/g) && defaultUrl.match(/(skuId=)(\d+)/g)[0];
+          if (skuId == undefined || skuId == null) {
+            skuId = defaultUrl.match(/p\/\d+/g)[0];
+          }
+          const id = skuId && skuId.match(/(\d+)/g) && skuId.match(/(\d+)/g)[0];
+          appendDiv.setAttribute('variantid', id);
+          document.querySelector('body').appendChild(appendDiv);
+        } else {
+          const variantId = [];
+          wholeVariants.forEach((element) => {
+            const skuId = element.match(/(skuId=)(\d+)/g)[0];
+            const id = skuId.match(/(\d+)/g)[0];
+            variantId.push(id);
+          });
+          variantId.forEach((element, index) => {
+            const appendDiv = document.createElement('div');
+            appendDiv.className = 'variantinfos';
+            appendDiv.setAttribute('variantid', variantId[index]);
+            appendDiv.setAttribute('varianturl', wholeVariants[index]);
+            document.body.append(appendDiv);
+          });
+          if (inputs && inputs.url) {
+            const urlArray = inputs.url.split('?');
+            if (urlArray.length > 0) {
+              const baseUrl = urlArray[0];
+              console.log(baseUrl);
+              let skuId = baseUrl.match(/p\/\d+/g)[0];
+              if (skuId) {
+                skuId = skuId.match(/(\d+)/g)[0];
+                const appendUrl = `${baseUrl}?skuId=${skuId}`;
+                const appendDiv = document.createElement('div');
+                appendDiv.className = 'variantinfo';
+                appendDiv.setAttribute('varianturl', appendUrl);
+                appendDiv.setAttribute('variantid', skuId);
+                document.querySelector('body').appendChild(appendDiv);
+              }
+            }
+          }
+        }
       }
-    });
+    }, inputs);
     return await context.extract(variants, { transform });
   },
 };
