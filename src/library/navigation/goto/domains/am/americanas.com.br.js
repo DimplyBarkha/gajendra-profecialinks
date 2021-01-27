0d14f30693337af16e8342fe81a3ed7a106c9c18
Karolina <karolina.urbanska@tdcm.io>
2021-01-27 16:58:23 +0100

    Updated goto

--- src/library/navigation/goto/domains/am/americanas.com.br.js
+++ src/library/navigation/goto/domains/am/americanas.com.br.js
@@ -1,25 +1,23 @@
+
 module.exports = {
-  implements: "navigation/goto",
+  implements: 'navigation/goto',
   parameterValues: {
-    domain: "americanas.com.br",
-    timeout: null,
-    country: "BR",
-    store: "americanas",
-    zipcode: "",
+    domain: 'americanas.com.br',
+    timeout: 60000,
+    country: 'BR',
+    store: 'americanas',
+    zipcode: '',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    await context.setFirstRequestTimeout(60000);
+    const timeout = parameters.timeout;
     await context.setBlockAds(false);
     await context.setLoadAllResources(true);
-    await context.setLoadImages(true);
-    await context.setJavaScriptEnabled(true);
     await context.setAntiFingerprint(false);
-    await context.goto(url, {
-      block_ads: false,
-      timeout: 60000,
-      waitUntil: "load",
-      load_all_resources: true,
-      images_enabled: true,
-    });
+    await context.setLoadImages(true);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode });
+    }
   },
 };
