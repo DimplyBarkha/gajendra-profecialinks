7d1a3d3b95f61a0201141e6bda7384371404794b
adheer <sangeta.kumari@synerzip.com>
2021-01-28 00:07:03 +0530

    kick-back issue

--- src/library/product/details/stores/n/naivas/KE/extract.js
+++ src/library/product/details/stores/n/naivas/KE/extract.js
@@ -1,4 +1,4 @@
-//const { transform } = require('../format');
+// const { transform } = require('../format');
 
 async function implementation (
   inputs,
@@ -8,25 +8,25 @@ async function implementation (
 ) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  // console.log("inputs:: ", inputs);
-  // //const {url, id } = inputs;
-  // console.log("parameters:: ", parameters);
-  //if(id){
-  await new Promise((resolve, reject) => setTimeout(resolve, 20000));
-  //await context.waitForXPath('//div//span[contains(text(),"Embakasi/Syokimau")]');
-
-  // await context.waitForSelector('div.head span[_ngcontent-naivas-app-c15]');
-  // console.log('everything fine !!!');
-  // await context.evaluate(() => {
-  //   const firstItem = document.querySelector('div.head span[_ngcontent-naivas-app-c15]');
-  //   firstItem.click();
-  // });
-  // try {
-  //   await context.waitForSelector('button.loc-btn>span:first-child')
-  //   await context.click('button.loc-btn>span:first-child')
-  // } catch (error) {
-  console.log('cookie pop up not loded');
-  //}
+  console.log('inputs:: ', inputs);
+  const { url, id } = inputs;
+  console.log('parameters:: ', parameters);
+  if (id) {
+    await new Promise((resolve, reject) => setTimeout(resolve, 20000));
+    await context.waitForXPath('//div//span[contains(text(),"Embakasi/Syokimau")]');
+    await context.waitForSelector('div.head span[_ngcontent-naivas-app-c15]');
+    console.log('everything fine !!!');
+    await context.evaluate(() => {
+      const firstItem = document.querySelector('div.head span[_ngcontent-naivas-app-c15]');
+      firstItem.click();
+    });
+  }
+  try {
+    await context.waitForSelector('span.block-display.text-center.buyNowBtn.txt-center a[_ngcontent-naivas-app-c11]');
+    await context.click('a[_ngcontent-naivas-app-c11] span');
+  } catch (error) {
+    console.log('cookie pop up not loded');
+  }
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
