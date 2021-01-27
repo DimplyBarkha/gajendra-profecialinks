65b9dde41f52f4c7bb442a623c6e91e6ef9f7019
PradyumnaD29 <pradyumna.deshpande@synerzip.com>
2021-01-27 16:50:28 +0530

    #3466 core extractor updated nespresso

--- src/library/product/details/stores/b/bestbuy/FR/execute.js
+++ src/library/product/details/stores/b/bestbuy/FR/execute.js
@@ -5,29 +5,6 @@
  * @param { ImportIO.IContext } context
  * @param { { goto: ImportIO.Action, createUrl: ImportIO.Action} } dependencies
  */
-const implementation = async (inputs, { loadedSelector, noResultsXPath }, context, dependencies) => {
-  const { url, id } = inputs;
-  let builtUrl;
-  if (!url) {
-    if (!id) throw new Error('No id provided');
-    else builtUrl = await dependencies.createUrl(inputs);
-  }
-  await dependencies.goto({ ...inputs, url: builtUrl || url });
-
-  await new Promise((resolve, reject) => setTimeout(resolve, 8000));
-  
-  if (loadedSelector) {
-    await context.waitForFunction(
-      (selector, xpath) => {
-        return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
-      },
-      { timeout: 10000 },
-      loadedSelector,
-      noResultsXPath,
-    );
-  }
-  return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
-};
 
 module.exports = {
   implements: 'product/details/execute',
@@ -35,8 +12,8 @@ module.exports = {
     country: 'FR',
     store: 'bestbuy',
     domain: 'bestbuy.ca',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div[class*="product-detail"]',
+    noResultsXPath: '//div[@id="french-container"]',
     zipcode: '',
-  },implementation,
+  },
 };
--- src/library/product/details/stores/b/bestbuy/FR/format.js
+++ src/library/product/details/stores/b/bestbuy/FR/format.js
@@ -3,53 +3,140 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {  
+const transform = (data) => {
   const cleanUp = (data, context) => {
     const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
     // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
     data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
       el.text = clean(el.text);
     }))));
     return data;
   };
   for (const { group } of data) {
-    for (let row of group) {
-      if(row.additionalDescBulletInfo){
-        let inf=[];
-        row.additionalDescBulletInfo.forEach(item=>{
-          inf.push(item.text);
-        })
-        row.additionalDescBulletInfo=[{"text":"|| "+inf.join(' || ')}];
+    for (const row of group) {
+      if (row.description) {
+        let text = '';
+        row.description.forEach(item => {
+          text = text + (text === '' ? '' : ' | ') + item.text;
+        });
+
+        if (row.additionalDescBulletInfo) {
+          row.additionalDescBulletInfo.forEach(item => {
+            text = text + ' || ' + item.text;
+          });
+        }
+        row.description = [{ text }];
       }
-      if(row.image){
-        row.image.forEach(item=>{
-          item.text=item.text.replace('/500x500/','/1500x1500/');
-        })
+
+      if (row.manufacturerDescription) {
+        const manufacturerDescriptions = [];
+        let dupUrl = '';
+        let urls = [];
+        row.manufacturerDescription.forEach(item => {
+          console.log('item:: ', item.text);
+          urls = row.manufacturerDescription.filter(it => item.text === it.text);
+          if (urls && urls.length === 1) {
+            manufacturerDescriptions.push(item);
+          } else {
+            if (dupUrl !== item.text) {
+              dupUrl = item.text;
+              manufacturerDescriptions.push(item);
+            }
+          }
+        });
+        row.manufacturerDescription = manufacturerDescriptions;
       }
-      if(row.alternateImages){
-        row.alternateImages.forEach(item=>{
-          item.text=item.text.replace('/100x100/','/1500x1500/');
-        })
+
+      if (row.manufacturerImages) {
+        const manufacturerImage = [];
+        let dupUrl = '';
+        let urls = [];
+        row.manufacturerImages.forEach(item => {
+          console.log('item:: ', item.text);
+          urls = row.manufacturerImages.filter(it => item.text === it.text);
+          if (urls && urls.length === 1) {
+            manufacturerImage.push(item);
+          } else {
+            if (dupUrl !== item.text) {
+              dupUrl = item.text;
+              manufacturerImage.push(item);
+            }
+          }
+        });
+        row.manufacturerImages = manufacturerImage;
       }
-      if(row.aggregateRating){
-        row.aggregateRating.forEach(item=>{
-          item.text=item.text.replace('.',',');
-        })
+
+      if (row.image) {
+        row.image.forEach(item => {
+          item.text = item.text.replace('/500x500/', '/1500x1500/');
+        });
       }
-      if(row.price){
-        row.price.forEach(item=>{
-          item.text=item.text.replace('.',',');
-        })
+
+      if (row.alternateImages) {
+        row.alternateImages.forEach(item => {
+          item.text = item.text.replace('/100x100/', '/1500x1500/');
+        });
+      }
+
+      if (row.aggregateRating) {
+        row.aggregateRating.forEach(item => {
+          item.text = item.text.replace('.', ',');
+        });
+      }
+
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace('.', ',');
+        });
+      }
+
+      if (row.name) {
+        if (row.brandText) {
+          if (!row.name[0].text.toLowerCase().startsWith(row.brandText[0].text.toLowerCase())) {
+            const text = row.name[0].text;
+            row.name = [
+              {
+                text: row.brandText[0].text + ' - ' + text,
+              },
+            ];
+          }
+        }
+      }
+
+      if (row.nameExtended) {
+        if (row.brandText) {
+          if (!row.nameExtended[0].text.toLowerCase().startsWith(row.brandText[0].text.toLowerCase())) {
+            const text = row.nameExtended[0].text;
+            row.nameExtended = [
+              {
+                text: row.brandText[0].text + ' - ' + text,
+              },
+            ];
+          }
+        }
+      }
+
+      if (row.specifications) {
+        let text = '';
+        for (let i = 0; i < row.specifications.length; i++) {
+          text += `${row.specificationsLabel[i].text} : ${row.specifications[i].text} || `;
+        }
+        row.specifications = [
+          {
+            text: text.slice(0, -4),
+          },
+        ];
+        delete row.specificationsLabel;
       }
     }
   }
