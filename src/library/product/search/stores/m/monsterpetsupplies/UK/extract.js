da51d20841fe2682f8d8db061742f20ea54a9f44
sasz94 <sacha.bartlomiej@gmail.com>
2021-01-28 14:38:17 +0100

    Fixed corrupted thumbnail urls by removing unnecessary parts (#2601)

--- src/library/product/search/stores/m/monsterpetsupplies/UK/extract.js
+++ src/library/product/search/stores/m/monsterpetsupplies/UK/extract.js
@@ -21,6 +21,19 @@ module.exports = {
         });
       }
     });
-    return await context.extract(productDetails, { transform });
+    const extractedData = await context.extract(productDetails, { transform });
+
+    const re = /upload(\/.+\d)\//;
+    // thumbnail urls are corrupted by unnecessary text, we fix them by removing unnecessary part of url
+    extractedData.forEach(page => {
+      page.group.forEach(row => {
+        if (row.thumbnail && (row.thumbnail[0].text.includes('f_auto') || row.thumbnail[0].text.includes('c_scale'))) {
+          console.log(row.thumbnail[0].text);
+          row.thumbnail[0].text = row.thumbnail[0].text.replace(re.exec(row.thumbnail[0].text)[1], '');
+        }
+      });
+    });
+
+    return extractedData;
   },
 };
