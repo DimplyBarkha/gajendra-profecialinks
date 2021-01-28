78fe1a08b796e1800d8e87abfdeed54cadf0e81f
adheer <sangeta.kumari@synerzip.com>
2021-01-28 15:09:28 +0530

    Kick back issues
    rebase

--- src/library/navigation/goto/domains/me/mechta.kz.js
+++ src/library/navigation/goto/domains/me/mechta.kz.js
@@ -3,9 +3,19 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'mechta.kz',
-    timeout: 40000,
+    timeout: 80000,
     country: 'KZ',
     store: 'mechta',
     zipcode: "''",
   },
+  // implementation: async ({ url }, parameters, context, dependencies) => {
+  //   url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+  //   await context.goto(url, {
+  //     block_ads: false,
+  //     load_all_resources: true,
+  //     images_enabled: true,
+  //     timeout: 100000,
+  //     waitUntil: 'load',
+  //   });
+  // },
 };
--- src/library/product/details/stores/m/mechta/KZ/format.js
+++ src/library/product/details/stores/m/mechta/KZ/format.js
@@ -24,11 +24,43 @@ const transform = (data) => {
   };
   for (const { group } of data) {
     for (const row of group) {
-      if (row.image) {
-        row.image = [{ text: 'https://www.mechta.kz' + row.image[0].text }];
-      }
-      if (row.imageAlt) {
-        row.imageAlt = [{ text: row.imageAlt[0].text }];
+      // if (row.image) {
+      //   row.image = [{ text: 'https://www.mechta.kz' + row.image[0].text }];
+      // }
+      // if (row.imageAlt) {
+      //   row.imageAlt = [{ text: row.imageAlt[0].text }];
+      // }
+      // if (row.description) {
+      //   const descs = [];
+      //   let newTxt = '';
+      //   let cnt = 0;
+      //   row.description.forEach(item => {
+      //     descs[0] = item;
+      //     item.text = item.text.replace(/(\s?\n)+/g, ' ').trim();
+      //     if (cnt > 0) newTxt = newTxt + '||' + item.text;
+      //     else newTxt = newTxt + item.text;
+      //     cnt++;
+      //   });
+      //   descs.forEach(item => {
+      //     item.text = newTxt;
+      //   });
+      //   row.description = descs;
+      // }
+      if (row.specifications) {
+        const specs = [];
+        let newTxt = '';
+        let cnt = 0;
+        row.specifications.forEach(item => {
+          specs[0] = item;
+          item.text = item.text.replace(/(\s?\n)+/g, ' ');
+          if (cnt > 0) newTxt = newTxt + ' || ' + item.text;
+          else newTxt = newTxt + item.text;
+          cnt++;
+        });
+        specs.forEach(item => {
+          item.text = newTxt;
+        });
+        row.specifications = specs;
       }
       if (row.alternateImages) {
         row.alternateImages.splice(0, 1);
@@ -72,12 +104,35 @@ const transform = (data) => {
           item.text = item.text.replace(',', '.');
         });
       }
-      if (row.quantity) {
-        row.quantity.forEach(item => {
-          item.text = item.text.trim();
-          item.text = item.text.replace(/\s+.+/, '');
-          item.text = item.text.trim();
+      // if (row.brand) {
+      //   row.brand.forEach(item => {
+      //     item.text = item.text.trim();
+      //     item.text = item.text.replace(/\s+.+/, '');
+      //     item.text = item.text.trim();
+      //   });
+      // }
+      if ((!row.brand || !row.brand.length) && row.brand1) {
+        console.log('brand1',row.brand1);
+        row.brand = row.brand1;
+        console.log("brand", row.brand);
+      }
+      if (row.videos) {
+        const unInterruptedPDPs = [];
+        let dup = '';
+        let urls = [];
+        row.videos.forEach(item => {
+          // console.log('item:: ', item.text);
+          urls = row.videos.filter(it => item.text === it.text);
+          if (urls && urls.length === 1) {
+            unInterruptedPDPs.push(item);
+          } else {
+            if (dup !== item.text) {
+              dup = item.text;
+              unInterruptedPDPs.push(item);
+            }
+          }
         });
+        row.videos = unInterruptedPDPs;
       }
       if (row.specificationsKey && row.specificationsValue) {
         var arrSpecs = [];
