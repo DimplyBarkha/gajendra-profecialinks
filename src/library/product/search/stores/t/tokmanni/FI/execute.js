b7ec7a2dfd5d6d3ddb363ea7f173a8d5d835f1ef
SHALINI <shalini.kumari-odc@partner.import.io>
2021-01-28 20:54:42 +0530

    tokmanni2

--- src/library/product/search/stores/t/tokmanni/FI/execute.js
+++ src/library/product/search/stores/t/tokmanni/FI/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FI',
     store: 'tokmanni',
     domain: 'tokmanni.fi',
-    url: 'https://eucs11.ksearchnet.com/cloud-search/n-search/search?ticket=klevu-15488592134928913&term={searchTerms}&paginationStartsFrom=0&sortPrice=false&ipAddress=undefined&analyticsApiKey=klevu-15488592134928913&showOutOfStockProducts=true&klevuFetchPopularTerms=false&klevu_priceInterval=500&fetchMinMaxPrice=true&klevu_multiSelectFilters=true&noOfResults=60&klevuSort=rel&enableFilters=true&filterResults=&visibility=search&category=KLEVU_PRODUCT&sv=2216&lsqt=&responseType=json#[!opt!]{"type":"json"}[/!opt!]',
+    url: 'https://www.tokmanni.fi/search/?q={searchTerms}',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
