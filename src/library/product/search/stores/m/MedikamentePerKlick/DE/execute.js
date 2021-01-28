8805f79c27532edfc60f1b6ceb55dbcadc67f3d8
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-01-28 23:28:54 +0530

    #2890 devmedikamenteperklicksearch

--- src/library/product/search/stores/m/MedikamentePerKlick/DE/execute.js
+++ src/library/product/search/stores/m/MedikamentePerKlick/DE/execute.js
@@ -5,8 +5,10 @@ module.exports = {
     country: 'DE',
     store: 'MedikamentePerKlick',
     domain: 'medikamente-per-klick.de',
-    url: 'https://www.medikamente-per-klick.de/keywordsearch?sortBy=default&VIEW_SIZE=150&VIEW_INDEX=0&clearSearch=N&SEARCH_STRING={searchTerms}',
+    url: 'https://www.medikamente-per-klick.de/keywordsearch?sortBy=default&VIEW_SIZE=20&VIEW_INDEX=0&clearSearch=N&SEARCH_STRING={searchTerms}&SEARCH_CATEGORY_ID=&anb=',
     loadedSelector: 'div.productsList',
     noResultsXPath: null,
   },
 };
+// https://www.medikamente-per-klick.de/keywordsearch?sortBy=default&VIEW_SIZE=150&VIEW_INDEX=0&clearSearch=N&SEARCH_STRING={searchTerms}
+
