574403c02ada71ec3bd51e4e6b213bdc0930a7d0
John Felix J <johnfelix.josep@hcl.com>
2021-01-28 09:47:49 +0530

    connection search fixes

--- src/library/product/search/stores/c/connection/US/extract.js
+++ src/library/product/search/stores/c/connection/US/extract.js
@@ -8,4 +8,32 @@ module.exports = {
     domain: 'connection.com',
     zipcode: '',
   },
+  implementation: async ({ inputString }, { country, store, transform: transformParam }, context, { productDetails }) => {
+    async function productUrl () {
+     function addHiddenDiv (key, value) {
+        const catElement = document.createElement('div');
+        catElement.className = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+      const producturl = getAllXpath("//table[@class='table']/tbody/tr[@class='product-container']/td/div[@class='product-name-list']/a/@href", 'nodeValue');
+      console.log('Producturl ==>',producturl);
+      for (let a = 0; a < producturl.length; a++) {
+        addHiddenDiv('added_producturl', 'https://www.connection.com'+producturl[a]);
+        console.log('addedurl ==>','https://www.connection.com'+producturl[a]);
+      }
+    }
+    await context.evaluate(productUrl);
+    return await context.extract(productDetails, { transform: transformParam });
+  },
 };
--- src/library/product/search/stores/c/connection/US/paginate.js
+++ src/library/product/search/stores/c/connection/US/paginate.js
@@ -8,9 +8,11 @@ module.exports = {
     spinnerSelector: null,
     loadedSelector: null,
     noResultsXPath: null,
+    // stopConditionSelectorOrXpath: 'button[class="nav-pagin-item--next"]',
+    //stopConditionSelectorOrXpath: '//div[@data-sel="nav-pagin"]//button[position()=last() and @class="nav-pagin-item--active"]',
     openSearchDefinition:  {
       page: 1,
-      template: 'https://www.connection.com/product/searchpage?SearchType=1&term={searchTerms}&pageNumber={page}&url=https%3A%2F%2Fwww.connection.com%2FIPA%2FShop%2FProduct%2FSearch&mode=List',
+      template: 'https://www.connection.com/product/searchpage?SearchType=1&term={searchTerms}&pageNumber={page}&pageSize=12&url=https%3A%2F%2Fwww.connection.com%2FIPA%2FShop%2FProduct%2FSearch&mode=List',
     },
     domain: 'connection.com',
   },
