44a4211d79a4e440594bffb850f2a2880a60935e
Vivek Gondil <gondilvivek@gmail.com>
2021-01-28 18:03:28 +0530

    Added cookies

--- src/library/product/details/stores/u/unieuro/IT/execute.js
+++ src/library/product/details/stores/u/unieuro/IT/execute.js
@@ -1,3 +1,34 @@
+const implementation = async(inputs, { loadedSelector, noResultsXPath }, context, dependencies) => {
+    const { url, id } = inputs;
+    let builtUrl;
+    if (!url) {
+        if (!id) throw new Error('No id provided');
+        else builtUrl = await dependencies.createUrl(inputs);
+    }
+
+    await context.goto({ url: builtUrl || url }, {
+        timeout: 50000,
+        waitUntil: 'load',
+        checkBlocked: true,
+        block_ads: false,
+    });
+    await context.evaluate(() => {
+        document.cookie = 'flixgvid=flix60124ae8000000.95115196';
+        document.cookie = 'privacyAccepted=1';
+    });
+    await dependencies.goto({...inputs, url: builtUrl || url });
+
+    if (loadedSelector) {
+        await context.waitForFunction(
+            (selector, xpath) => {
+                return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
+            }, { timeout: 10000 },
+            loadedSelector,
+            noResultsXPath,
+        );
+    }
+    return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
+};
 module.exports = {
     implements: 'product/details/execute',
     parameterValues: {
@@ -8,4 +39,5 @@ module.exports = {
         loadedSelector: '.container',
         noResultsXPath: '//div[@id="no-results-message"]',
     },
+    implementation,
 };
