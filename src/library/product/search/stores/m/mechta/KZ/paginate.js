4b620bc9194fde2b2148d716faebacab04b7c075
PR <pravinr@synerzipune.local>
2021-01-28 17:26:27 +0530

    search fixes

--- src/library/product/search/stores/m/mechta/KZ/paginate.js
+++ src/library/product/search/stores/m/mechta/KZ/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     // mutationSelector: 'button.q-btn.q-btn-item.non-selectable.no-outline.q-btn--standard.q-btn--rectangle.bg-primary.text-white.q-btn--actionable.q-focusable.q-hoverable.q-btn--no-uppercase.q-btn--wrap',
     // spinnerSelector: 'div[style*="preload_circle_mechta.svg"]',
     loadedSelector: 'div.hoverCard-child.bg-white',
-    noResultsXPath: '//div[@class="j_detectum_search" and contains(text(),"По текущему запросу нет товаров")]',
+    noResultsXPath: '//div[@class="bg-color10 q-mt-xl q-pt-sm"]/div[contains(., "ничего не найдено")]',
     resultsDivSelector: null,
     openSearchDefinition: null,
     domain: 'mechta.kz',
