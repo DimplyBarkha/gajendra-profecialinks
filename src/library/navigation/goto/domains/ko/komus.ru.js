47e5c87305049b06ba3fc3c43105f2d62217bf8b
Your Name <karthik.ravi-odc@partner.import.io>
2021-01-28 14:34:46 +0530

    await

--- src/library/navigation/goto/domains/ko/komus.ru.js
+++ src/library/navigation/goto/domains/ko/komus.ru.js
@@ -8,4 +8,21 @@ module.exports = {
     store: 'komus',
     zipcode: '',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { timeout = 10000 } = parameters;
+
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+
+
+    const { url, zipcode, storeId } = inputs;
+    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true, captureRequests: true });
+    // patch for synchronicity issue between json decoring and goto result    
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    
+  },
 };
