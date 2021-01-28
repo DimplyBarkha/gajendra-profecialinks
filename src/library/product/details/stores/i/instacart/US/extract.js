4adf4cfc72ef3e8306498eef5e9691545bd4cba9
sushma-mangalapally <sushma.mangalapally@import.io>
2021-01-28 11:17:09 -0800

    Add changes

--- src/library/product/details/stores/i/instacart/US/extract.js
+++ src/library/product/details/stores/i/instacart/US/extract.js
@@ -1,10 +1,10 @@
-const { transform } = require('../../../../shared');
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'instacart',
-    transform,
+    transform: cleanUp,
     domain: 'instacart.com',
     zipcode: '',
   },
@@ -14,10 +14,27 @@ module.exports = {
     dependencies,
   ) => {
     await context.evaluate(async function () {
-     let othersellerName = document.evaluate(`//main[@id="main-content"]/div[contains(@class, 'rmq')]/div[contains(@class, 'rmq')]/section[2]/div/div[1]/h3 | //main[@id="main-content"]/div[contains(@class, 'rmq')]/div[contains(@class, 'rmq')]/section[1]/a[contains(.,'Shop similar items at')]`, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
-     if(othersellerName) {
-       document.body.setAttribute('othersellername', othersellerName.textContent.replace('Shop similar items at ',""));
-     }
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+
+      const othersellerName = document.evaluate('//main[@id="main-content"]/div[contains(@class, \'rmq\')]/div[contains(@class, \'rmq\')]/section[2]/div/div[1]/h3 | //main[@id="main-content"]/div[contains(@class, \'rmq\')]/div[contains(@class, \'rmq\')]/section[1]/a[contains(.,\'Shop similar items at\')]', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
+      if (othersellerName) {
+        document.body.setAttribute('othersellername', othersellerName.textContent.replace('Shop similar items at ', ''));
+      }
+      //  const variantFlavor = document.querySelector('ul[aria-labelledby*="variant-selections"] li button[aria-label*="select"]');
+      //  const nameExtendedWithVariant = document.evaluate(`concat(//div[contains(@class, "itemModalHeader")]//h2[contains(@class, "item-title")], " - ", //ul[contains(@aria-labelledby, "variant-selections")]//li//button[contains(@aria-label, "select")])`, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
+      //  const name = document.evaluate(`//div[contains(@class, "itemModalHeader")]//h2[contains(@class, "item-title")]`, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext();
+
+    //  if(nameExtendedWithVariant) {
+    //   addHiddenDiv('ii_nameExtended', nameExtendedWithVariant);
+    //  } else if (name) {
+    //    addHiddenDiv('ii_nameExtended', name);
+    //  }
     });
     const { transform } = parameters;
     const { productDetails } = dependencies;
