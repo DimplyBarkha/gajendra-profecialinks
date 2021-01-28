5781c5dc4b21e802ecbf5b6b4f5dc100a44c40cd
unknown <leena.gavali-odc@partner.import.io>
2021-01-28 23:50:02 +0530

    primor_core_UAT

--- src/library/product/details/stores/p/primor/ES/extract.js
+++ src/library/product/details/stores/p/primor/ES/extract.js
@@ -30,6 +30,17 @@ async function implementation(
       catElement.style.display = 'none';
       document.body.appendChild(catElement);
     }
+
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+      }
+      return result;
+    };
+
     var getXpath = (xpath, prop) => {
       var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
       let result;
@@ -44,13 +55,56 @@ async function implementation(
       desc1 = desc1.replace(",", ".")
       addElementToDocument('desc1', desc1);
     }
+
+    //ingredients//
+
+    var inglist = getXpath('//td/span[contains(text(),"Ingredientes")]/text()', 'nodeValue');
+    var inglist2 = getAllXpath('//td/span[contains(text(),"Ingredientes")]/parent::td/p/text()', 'nodeValue');
+
+    if (inglist != null) {
+      var data = "";
+      for (var i = 0; i < inglist2.length; i++) {
+        data = data + inglist2[i];
+      }
+      var ingredient = inglist + ' ' + data;
+      addElementToDocument('ingredient', ingredient);
+    }
+
     // list-price
     var desc2 = getXpath('//p[@id="old_price"]/span[@id="old_price_display"]/text()', 'nodeValue');
 
     if (desc2 != null) {
-      desc2 = desc2.replace(",", ".").split(": ")[1]
+      if (desc2.includes(":")) {
+        desc2 = desc2.split(": ")[1]
+      }
+      desc2 = desc2.replace(",", ".")
       addElementToDocument('desc2', desc2);
     }
+
+
+    // product Description
+    var brand1 = getXpath('//span[@itemprop="brand"]/text()', 'nodeValue');
+    var brand2 = getAllXpath('//h1[@itemprop="name"]/text()', 'nodeValue');
+    var brand3 = getXpath('//span[@class="checked"]/following::span[1]/text()', 'nodeValue');
+
+    if (brand2.length >= 1) {
+      var temp = "";
+      for (var i=0; i<brand2.length; i++){
+        temp = temp + " "  + brand2[i]
+      }
+    }
+    var final = "";
+    if (brand1 != null){
+      final = final + brand1;
+    }
+    if (temp != null){
+      final = final + temp;
+    }
+    if (brand3 != null){
+      final = final + " - " + brand3;
+    }
+    addElementToDocument('product_desc', final);
+
     // promotion
     var desc3 = getXpath('//p[@id="reduction_percent"]/span[@id="reduction_percent_display"]/text()', 'nodeValue');
 
@@ -59,37 +113,37 @@ async function implementation(
       addElementToDocument('desc3', desc3);
     }
     // Method to Retrieve Xpath content of a Multiple Nodes
-    const getAllXpath = (xpath, prop) => {
-      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-      const result = [];
-      for (let index = 0; index < nodeSet.snapshotLength; index++) {
-        const element = nodeSet.snapshotItem(index);
-        if (element) result.push(prop ? element[prop] : element.nodeValue);
-      }
-      return result;
-    };
+    // const getAllXpath = (xpath, prop) => {
+    //   const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    //   const result = [];
+    //   for (let index = 0; index < nodeSet.snapshotLength; index++) {
+    //     const element = nodeSet.snapshotItem(index);
+    //     if (element) result.push(prop ? element[prop] : element.nodeValue);
+    //   }
+    //   return result;
+    // };
     // Double Pipe Concatenation
     // const pipeSeparatorDouble1 = (id, data) => {
     //   // var doubleSeparatorText = data.join(' || ');
     //   addElementToDocument('id', data);
     // }
 
-      // XPATH Data Extraction For Additional Description Bullet
-      const addDescBulletInfo1 = getAllXpath("(//div[@itemprop='description']/p/strong/text())[1] | //div[@itemprop='description']/p/span/text() | //div[@itemprop='description']/p/text()", 'nodeValue');
-      if (addDescBulletInfo1 != null){
-        var abc = addDescBulletInfo1.join(" || ");
-        addElementToDocument('id', abc);
-        
-      }
-      var rating = getXpath('//div[@itemprop="aggregateRating"]/meta[@itemprop="ratingValue"]//@content', 'nodeValue');
-      if (rating != null){
+    // XPATH Data Extraction For Additional Description Bullet
+    const addDescBulletInfo1 = getAllXpath("(//div[@itemprop='description']/p/strong/text())[1] | //div[@itemprop='description']/p/span/text() | //div[@itemprop='description']/p/text()", 'nodeValue');
+    if (addDescBulletInfo1 != null) {
+      var abc = addDescBulletInfo1.join(" || ");
+      addElementToDocument('id', abc);
 
-        rating = rating/2;
-        addElementToDocument('rating', rating);
-      }
+    }
+    var rating = getXpath('//div[@itemprop="aggregateRating"]/meta[@itemprop="ratingValue"]//@content', 'nodeValue');
+    if (rating != null) {
+
+      rating = rating / 2;
+      addElementToDocument('rating', rating);
+    }
 
 
-    });
+  });
   //rank end
   return await context.extract(productDetails, { transform });
 
