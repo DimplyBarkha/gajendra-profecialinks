afac95a55e1919f6095377575068c45ec525acbc
adheer <sangeta.kumari@synerzip.com>
2021-01-28 15:09:28 +0530

    Kick back issues

--- src/library/product/details/stores/m/mechta/KZ/execute.js
+++ src/library/product/details/stores/m/mechta/KZ/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'KZ',
     store: 'mechta',
     domain: 'mechta.kz',
-    loadedSelector: 'div.aa_catalogdetail',
-    noResultsXPath: "//font[contains(text(),'Элемент не найден')]",
+    // loadedSelector: 'div.aa_catalogdetail',
+    // noResultsXPath: "//font[contains(text(),'Элемент не найден')]",
     zipcode: '',
   },
 };
