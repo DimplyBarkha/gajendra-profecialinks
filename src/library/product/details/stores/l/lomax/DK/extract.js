21568fa7f32df91c156c228f2f4aff1bf2b61db2
navya golla <navya.golla@od.import.io>
2021-01-28 12:06:44 +0530

    #3461

--- src/library/product/details/stores/l/lomax/DK/extract.js
+++ src/library/product/details/stores/l/lomax/DK/extract.js
@@ -10,35 +10,49 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
     await context.evaluate(async function () {
-    function addElementToDocument(key, value) {
-    const catElement = document.createElement('div');
-    catElement.id = key;
-    catElement.textContent = value;
-    catElement.style.display = 'none';
-    document.body.appendChild(catElement);
-    }
-    const getXpath = (xpath, prop) => {
-    const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-    let result;
-    if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-    else result = elem ? elem.singleNodeValue : '';
-    return result && result.trim ? result.trim() : result;
-    };
-    const aggr = getXpath("//div[@class='container productcard']/@data-gtm-rating", 'nodeValue');
-    try {
-    if (aggr != null) {
-    let str = aggr
-    var a=str.replace('.',',')
-    addElementToDocument('aggr', a)
-    }
-    }
-    catch (error) {
-    }
-    const price = getXpath('//meta[@property="product:pretax_price:amount"]/@content', 'nodeValue');
-    // @ts-ignore
-    var onlinePrice = price.concat(" kr.")
-    addElementToDocument('price', onlinePrice)
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      const getXpath = (xpath, prop) => {
+        const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+      const aggr = getXpath("//div[@class='container productcard']/@data-gtm-rating", 'nodeValue');
+      try {
+        if (aggr != null) {
+          let str = aggr
+          var a = str.replace('.', ',')
+          addElementToDocument('aggr', a)
+        }
+      }
+      catch (error) {
+      }
+      const price = getXpath('//meta[@property="product:pretax_price:amount"]/@content', 'nodeValue');
+      // @ts-ignore
+      try {
+        if (price != null) {
+          let str = price
+          var b = str.replace('.', ',')
+          var c = price.concat(" kr,")
+          b = c;
+          // @ts-ignore
+          addElementToDocument('price', b)
+          // let onlinePrice = price.concat(" kr,")
+          // addElementToDocument('price', onlinePrice)
+        }
+      }
+      catch (error) {
+      }
+      // let onlinePrice = price.concat(" kr,")
+      // addElementToDocument('price', onlinePrice)
     });
     await context.extract(productDetails, { transform: transformParam });
-    },
-    };
+  },
+};
