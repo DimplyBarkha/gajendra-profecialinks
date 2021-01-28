ca97f19a4a870f66511c26d29d2b8ebc495f896d
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2021-01-28 14:14:45 +0100

    Modified Creating Url

--- src/library/product/details/stores/p/planetParfum/BE/createUrl.js
+++ src/library/product/details/stores/p/planetParfum/BE/createUrl.js
@@ -4,9 +4,14 @@ module.exports = {
   parameterValues: {
     domain: 'planetparfum.com',
     prefix: null,
-    url: 'https://www.planetparfum.com/fr/searchresults?q={id}',
+    url: null,
     country: 'BE',
     store: 'planetParfum',
     zipcode: '',
+  },
+  implementation: async (inputs, parameterValues, context, dependencies) => {
+    const providedInput = inputs.id.match(/[\d_]+/) ? inputs.id.match(/[\d_]+/)[0] : inputs.id;
+    const productUrl = `https://www.planetparfum.com/fr/searchresults?q=${providedInput}`;
+    return productUrl;
   },
 };
