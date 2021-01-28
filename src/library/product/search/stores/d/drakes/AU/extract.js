402deb0cb8139ce6af7dc93b9b87da8355fc1091
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-01-28 10:08:24 +0100

    #1468 - Applying fixes to 'id' extraction - Updating tests

--- src/library/product/search/stores/d/drakes/AU/extract.js
+++ src/library/product/search/stores/d/drakes/AU/extract.js
@@ -10,4 +10,25 @@ module.exports = {
     domain: 'drakes.com.au',
     zipcode: '',
   },
+  implementation: async (inputs, { transform }, context, { productDetails }) => {
+    await context.evaluate(async () => {
+      const searchUrl = window.location.href;
+      document.body.setAttribute('search_url', searchUrl);
+
+      const allProducts = document.querySelectorAll('div.TalkerGrid__Item');
+      for (let i = 0; i < allProducts.length; i++) {
+        const prodElem = allProducts[i];
+        const prodUrl = prodElem.querySelector('div.talker__imagery a')
+          ? prodElem.querySelector('div.talker__imagery a').href
+          : '';
+        let id = prodUrl.match(/.+\/(.+)/) ? prodUrl.match(/.+\/(.+)/)[1] : '';
+        if (id.length > 60) {
+          const idArr = id.split('-');
+          id = idArr.map((item) => item.substr(0, 1).toUpperCase()).join('');
+        }
+        prodElem.setAttribute('added_id', id);
+      }
+    });
+    return await context.extract(productDetails, { transform });
+  },
 };
