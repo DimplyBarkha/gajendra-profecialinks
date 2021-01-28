6b82564b4b272edfdc3e30c7fba21a5ab28a6849
astropaula <wojciechowska.fran@gmail.com>
2021-01-28 17:59:30 +0100

    Added fix to createUrl

--- src/library/product/details/stores/w/waitrose/UK/createUrl.js
+++ src/library/product/details/stores/w/waitrose/UK/createUrl.js
@@ -6,11 +6,12 @@ async function implementation (
   dependencies,
 ) {
   const { id } = inputs;
+  const productCode = inputs.id;
   if (parameters.url) {
     const url = parameters.url.replace('{id}', encodeURIComponent(id));
 
     await dependencies.goto({ url });
-    const detailsPage = await context.evaluate(async () => {
+    const detailsPage = await context.evaluate(async (productCode) => {
       if (document.querySelector('div[id="introAgreeButton"] span[class="CwaK9"] span[class="RveJvd snByac"]') !== null) {
         document.querySelector('div[id="introAgreeButton"] span[class="CwaK9"] span[class="RveJvd snByac"]').click();
       }
@@ -18,10 +19,16 @@ async function implementation (
         document.querySelector('div[class="bErdLd aID8W wwYr3"]').remove();
         document.querySelector('div[class="t7xA6 m114nf aID8W"]').remove();
       }
-      if (document.querySelector('div[class="yuRUbf"] a') !== null) {
-        return document.querySelector('div[class="yuRUbf"] a').getAttribute('href');
+      if (document.querySelectorAll('div[class="yuRUbf"] a') !== null) {
+        console.log(productCode);
+        for (let i = 0; i < document.querySelectorAll('div[class="yuRUbf"] a').length; i++) {
+          if (document.querySelectorAll('div[class="yuRUbf"] a')[i].getAttribute('href').includes(`/${productCode}`)) {
+            return document.querySelectorAll('div[class="yuRUbf"] a')[i].getAttribute('href');
+          }
+        }
+        return 'https://www.waitrose.com/';
       }
-    });
+    }, productCode);
     return detailsPage || url;
   }
 }
