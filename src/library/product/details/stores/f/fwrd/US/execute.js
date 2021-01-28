3cb515beca42ef9d3d86e9d8c44f2902458d573f
alscotty <alawrencescott@gmail.com>
2021-01-28 11:24:05 -0800

    set locale via query string

--- src/library/product/details/stores/f/fwrd/US/execute.js
+++ src/library/product/details/stores/f/fwrd/US/execute.js
@@ -1,4 +1,22 @@
 
+const implementation = async (inputs, { loadedSelector, noResultsXPath }, context, dependencies) => {
+  const { url, id } = inputs;
+ 
+  await dependencies.goto({ ...inputs, url: url + '&lang=en&currency=USD&countryCode=US' });
+
+  if (loadedSelector) {
+    await context.waitForFunction(
+      (selector, xpath) => {
+        return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
+      },
+      { timeout: 10000 },
+      loadedSelector,
+      noResultsXPath,
+    );
+  }
+  return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
+};
+
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -9,4 +27,5 @@ module.exports = {
     noResultsXPath: null,
     zipcode: '',
   },
+  implementation,
 };
