00fffc7856a02c1546500884eab9e1f3da6fc43e
osmakash <akashkumar.p@osmosys.asia>
2021-01-28 16:01:56 +0530

    Pushing code to report the proxy issue

--- src/library/navigation/goto/domains/of/officedepot.com.js
+++ src/library/navigation/goto/domains/of/officedepot.com.js
@@ -8,4 +8,13 @@ module.exports = {
     store: 'officedepot',
     zipcode: '',
   },
+  implementation: async ({ url }, { country, domain }, context, dependencies) => {
+    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: false });
+    const elem = await context.evaluate(function () {
+      return Boolean(document.querySelector('main > h1'));
+    });
+    if(elem){
+      return context.reportBlocked(404, 'Blocked!');
+    }
+  }
 };
--- src/library/product/search/stores/o/officedepot/US/execute.js
+++ src/library/product/search/stores/o/officedepot/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     store: 'officedepot',
     domain: 'officedepot.com',
     url: 'https://www.officedepot.com/catalog/search.do?Ntt={searchTerms}',
-    loadedSelector: '#productView',
+    loadedSelector: null,
     noResultsXPath: '//div[@class="h3 brandColor_tp2 section no_result_break"] | //h1[contains(.,"We are sorry")]',
     zipcode: '',
   },
