98c28475ba2a5628ddbf5d0a2351c379827c59e0
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-01-29 12:12:51 +0100

    - Fixing an issue with pagination

--- src/library/product/search/stores/r/romeos/AU/paginate.js
+++ src/library/product/search/stores/r/romeos/AU/paginate.js
@@ -1,16 +1,18 @@
-
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'AU',
     store: 'romeos',
-    nextLinkSelector: 'a[rel="next"]',
+    nextLinkSelector: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'div[class="TalkerGrid__Item"]',
+    loadedSelector: 'div.shopping-list.search_results',
     noResultsXPath: '//div[@class="search-results__empty-message"]',
     resultsDivSelector: null,
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      pageStartNb: 1,
+      template: 'https://martinplace.romeosonline.com.au/search?page={page}&q={searchTerms}&utf8=%E2%9C%93',
+    },
     domain: 'martinplace.romeosonline.com.au',
     zipcode: '',
   },
