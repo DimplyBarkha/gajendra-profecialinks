33da838ffa9dfade67203bdf44412feddea19bbf
alscotty <alawrencescott@gmail.com>
2021-01-29 15:12:13 -0800

    testing locale w/ cookie

--- src/library/navigation/goto/domains/ji/jiomart.com.js
+++ src/library/navigation/goto/domains/ji/jiomart.com.js
@@ -13,27 +13,51 @@ module.exports = {
 
     console.log(zipcode);
     if (zipcode){
+      await context.goto(`https://www.jiomart.com/mst/rest/v1/pin/${zipcode}`, {waitUntil: 'load'});
       const locationCookie = await context.evaluate(async(zipcode)=>{
-        const pinInfoResponse = await fetch(`https://www.jiomart.com/mst/rest/v1/pin/${zipcode}`);
+        const response = JSON.parse(document.querySelector('body pre').textContent);
 
-        if (pinInfoResponse.status == 200) {
-          const responseData = await pinInfoResponse.json();
-        
-          const pincode = responseData.result.pin || zipcode;
-          const city = responseData.result.city;
-          const stateCode = responseData.result.state_code;
-  
-          return `cookie: nms_mgo_pincode = ${pincode}; nms_mgo_city = ${city}; nms_mgo_state_code = ${stateCode}`;
-        }
-      },zipcode);
-      await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true, headers: locationCookie });
+        const pincode = response.result.pin || zipcode;
+        const city = response.result.city;
+        const stateCode = response.result.state_code;
+
+        return `nms_mgo_pincode = ${pincode}; nms_mgo_city = ${city}; nms_mgo_state_code = ${stateCode}`
+      });
+
+      
+        await context.goto(url + `#[!opt!]{"cookie_jar":[ 
+          {"name":"nms_mgo_pincode","value":"456001"},
+          {"name":"nms_mgo_city","value":"Ujjain"},
+          {"name":"nms_mgo_state_code","value":"MP"},
+        ]}[/!opt!]`, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true, 
+      });
+      //   await context.goto(url + `#[!opt!]{"cookie_jar": [{ "name": "cookie", "value": ${locationCookie}}]
+      // } [/!opt!]`, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true, 
+      //    headers: { pin: '456001', cookie: locationCookie},
+      // });
 
     } else {
-
       await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: true});
-
     }
-
 
   },
 };
+
+
+// const pinInfoResponse = await fetch(`https://www.jiomart.com/mst/rest/v1/pin/${zipcode}`, {
+//   headers: {
+//     accept: '*/*',
+//     'accept-encoding': 'gzip,deflate,br',
+//   },
+// });
+
+// if (pinInfoResponse.status == 200) {
+//   const responseData = await pinInfoResponse.json();
+
+//   const pincode = responseData.result.pin || zipcode;
+//   const city = responseData.result.city;
+//   const stateCode = responseData.result.state_code;
+
+//   return `cookie: nms_mgo_pincode = ${pincode}; nms_mgo_city = ${city}; nms_mgo_state_code = ${stateCode}`;
+// }
+//       }, zipcode);
