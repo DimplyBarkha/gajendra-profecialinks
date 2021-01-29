21459889e005e98a71c87bb944257445c6d65674
sobha rani <amol.thukrul-od@partner.import.io>
2021-01-30 00:21:34 +0530

    fressnapf complete

--- src/library/product/search/stores/l/larebajavirtual/CO/execute.js
+++ src/library/product/search/stores/l/larebajavirtual/CO/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'larebajavirtual',
     domain: 'larebajavirtual.com',
     url: 'https://www.larebajavirtual.com/catalogo/buscar/subMenuCategory/on/busqueda/{searchTerms}?ajax=id-productos-list&pageSize=150',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div[class="container-fluid"]>div>div[class="col-md-10 side"]',
+    noResultsXPath: 'img[alt="lo sentimos no se han encontrado resultados"]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/l/larebajavirtual/CO/paginate.js
+++ src/library/product/search/stores/l/larebajavirtual/CO/paginate.js
@@ -4,12 +4,11 @@ module.exports = {
   parameterValues: {
     country: 'CO',
     store: 'larebajavirtual',
-    // nextLinkSelector: '.next a',
-    // nextLinkXpath: "//li[@class='next']/a",
+    nextLinkSelector: 'li[class="next"]>a',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: '#id-productos-list , .next a',
-    noResultsXPath: null,
+    loadedSelector: 'div[class="container-fluid"]>div>div[class="col-md-10 side"]',
+    noResultsXPath: 'img[alt="lo sentimos no se han encontrado resultados"]',
     // openSearchDefinition: {
     //   template: 'https://www.larebajavirtual.com/catalogo/buscar/ajax/id-productos-list/subMenuCategory/on/busqueda/{searchTerms}/codigoProducto_page/{page}?ajax=id-productos-list&pageSize=150',
     // },
