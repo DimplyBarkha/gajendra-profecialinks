5aabeaeb2e97d599b72253a5893ea0d3f64d8141
Karina Miernik <karina.miernik@gmail.com>
2021-01-29 09:37:26 +0100

    #3299 kickback issue fixed

--- src/library/product/search/stores/e/e-bebek/TR/extract.js
+++ src/library/product/search/stores/e/e-bebek/TR/extract.js
@@ -6,34 +6,28 @@ module.exports = {
   parameterValues: {
     country: 'TR',
     store: 'e-bebek',
-    transform,
+    transform: transform,
     domain: 'e-bebek.com',
     zipcode: '',
   },
   implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
     await new Promise((resolve, reject) => setTimeout(resolve, 6000));
 
     await context.evaluate(async () => {
-      const searchUrl = window.location.href;
-
-      const products = document.querySelectorAll('section[class="product-list"] div[class*="product gtmProductClick w-100"]');
-
-      products.forEach((product, index) => {
-        product.setAttribute('searchurl', searchUrl);
-        product.setAttribute('rank', `${index + 1}`);
-        const prefix = 'https://www.e-bebek.com';
-        const productUrl = product.querySelector('a.product-btn');
-        if (productUrl !== null && productUrl !== undefined) product.setAttribute('producturl', prefix.concat(productUrl.getAttribute('href')));
-        // const id = product.querySelector('form[action*="wishlist"] > input');
-        const id = product.querySelector('a')
-          ? product.querySelector('a').getAttribute('href') : null;
-        if (id !== null && id !== undefined) product.setAttribute('sku', id.split('-p-').pop().replace('/', ''));
-        if (product.innerText.includes('Promosyonlu')) product.setAttribute('sponsored', 'true');
-        if (!product.innerText.includes('Promosyonlu')) product.setAttribute('rankorganic', `${index + 1}`);
-      });
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      };
+      const searchUrl = document.querySelector('link[rel="alternate"][hreflang="tr"]')
+        ? document.querySelector('link[rel="alternate"][hreflang="tr"]').getAttribute('href') : '';
+      addElementToDocument('searchurl', searchUrl);
     });
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
+
     return await context.extract(productDetails, { transform });
   },
 };
