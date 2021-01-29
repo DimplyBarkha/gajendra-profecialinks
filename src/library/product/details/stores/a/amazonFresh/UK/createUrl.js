f56bdce02c5a771ebad1cb14299cad8d12890f2c
Balaji Venkatesan <balaji-venkatesan@hcl.com>
2021-01-29 15:41:18 +0530

    freshamazon_UK_search

--- src/library/product/details/stores/a/amazonFresh/UK/createUrl.js
+++ src/library/product/details/stores/a/amazonFresh/UK/createUrl.js
@@ -5,6 +5,7 @@ module.exports = {
     domain: 'amazon.co.uk',
     prefix: null,
     url: 'https://www.amazon.co.uk/dp/product/{id}?s=amazonfresh&th=1&psc=1&showDetailProductDesc=1',
+    // url: 'https://amazon.com/dp/product/{id}/ref=sr_1_1&dchild=1&fpw=alm&s=amazonfresh?th=1',
     country: 'UK',
     store: 'amazonFresh',
     // zipcode: '',
