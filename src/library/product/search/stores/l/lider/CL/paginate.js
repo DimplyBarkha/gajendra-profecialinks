920868a7d8bb73b2887233e1e3ed3c1be86b45cc
Raghav <user.email>
2021-01-29 15:43:56 +0530

    updating search url to retreive less data, paginatation, row xpaths & xpath for prod id

--- src/library/product/search/stores/l/lider/CL/paginate.js
+++ src/library/product/search/stores/l/lider/CL/paginate.js
@@ -4,14 +4,13 @@ module.exports = {
   parameterValues: {
     country: 'CL',
     store: 'lider',
-    nextLinkSelector: null,
-    // '#paginationBox > nav > ul.pagination.pull-right > li:last-child:not(.disabled) > a',
+    nextLinkSelector: '#paginationBox > nav > ul.pagination.pull-right > li:last-child:not(.disabled) > a',
     // 'ul[class="pagination pull-right"]>li:nth-child(4)>a',
     // 'ul[class="pagination pull-right"]>li>a'
     // nextLinkXPath: "(//ul[@class='pagination pull-right']/li/a/@href)[3]"
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: 'body',
     noResultsXPath: null,
     resultsDivSelector: null,
     openSearchDefinition: null,
