631f61edbecb2d483b755bcffb2dae54d215dca0
chetan-profecia <chetan.shelake@profecialinks.com>
2021-01-29 12:05:38 +0530

    search extractor push

--- src/library/product/search/stores/q/qoo10/JP/extract.js
+++ src/library/product/search/stores/q/qoo10/JP/extract.js
@@ -1,4 +1,63 @@
 
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+
+const { productDetails } = dependencies;
+    await context.evaluate(async () => {
+
+    	function stall (ms) {
+	        return new Promise((resolve, reject) => {
+	          setTimeout(() => {
+	            resolve();
+	          }, ms);
+	        });
+	    }
+
+        const applyScroll = async function () {
+	      let scrollTop = window.scrollY;
+	      scrollTop = scrollTop + 5000
+	      let scroll = window.scrollY;
+	      while (scroll !== scrollTop) {
+	        await stall(100);
+	        scroll += 500;
+	        console.log("====================================");
+	        console.log(scroll);
+	        window.scroll(0, scroll);
+	        if (scroll === scrollTop) {
+	          await stall(300);
+	          break;
+	        }
+	      }
+	      
+	  	};	
+	  	
+
+      const el = document.querySelector('div.pagingInfo div.btn_more');
+      const totleCount = el.getAttribute("totalpage");
+      const moreBtn = document.querySelector('div.pagingInfo a#btn_more_item');
+      if (el) {
+        new Promise(resolve => {
+          setTimeout(() => {
+            resolve();
+          }, 2000);
+        });
+        if (el && totleCount && moreBtn) {
+		  	var i=0;
+		  	while(i <= totleCount){
+		    	await applyScroll();
+		    	i++;
+			}
+		  }  
+      }
+
+    });
+
+    return await context.extract(productDetails);
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +67,5 @@ module.exports = {
     domain: 'qoo10.jp',
     zipcode: '',
   },
+  implementation
 };
