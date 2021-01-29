801b4c25c07fb07309eae58b96d99f4d725da75f
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-01-29 17:18:57 +0530

    updated execute.js for search since not working for im search

--- src/library/product/search/stores/m/manor/CH/execute.js
+++ src/library/product/search/stores/m/manor/CH/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'manor',
     domain: 'manor.ch',
     url: 'https://www.manor.ch/search/text#/q/{searchTerms}?offset=0',
-    loadedSelector: 'div#epoq_resultrows div.epoq_resultrow',
+    loadedSelector: 'div[class="o-producttile__image js-product-image"]',
     noResultsXPath: '//div[@class="m-textcomponent"]//p[contains(text(),"Nous n’avons malheureusement trouvé aucun résultat")] | //h4//b[contains(text(),"Willkommen in der Welt von Manor!")]',
     zipcode: '',
   },
