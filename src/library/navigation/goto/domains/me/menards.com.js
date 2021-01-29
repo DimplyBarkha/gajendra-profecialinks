14b4051632f86f67bb7e2baf1510423ebf71d512
unknown <leena.gavali-odc@partner.import.io>
2021-01-29 16:20:10 +0530

    menards_UAT_searchterms3

--- src/library/navigation/goto/domains/me/menards.com.js
+++ src/library/navigation/goto/domains/me/menards.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'menards.com',
-    timeout: 90000,
+    timeout: 120000,
     country: 'US',
     store: 'menards',
     zipcode: '',
