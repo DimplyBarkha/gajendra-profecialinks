5fb726b3cfafc9b10263c1ccfe52bed720a3ff55
dhanush <dhanush.isukapalli@od.import.io>
2021-01-29 20:56:12 +0530

    hondoscenter by kavya

--- src/library/product/search/stores/h/hondoscenter/GR/execute.js
+++ src/library/product/search/stores/h/hondoscenter/GR/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'hondoscenter',
     domain: 'hondoscenter.com',
     url: 'https://www.hondoscenter.com/CachedServices/api/Product/GetScrolledProductList?search-for={searchTerms}&lang=en&templateCode=search_products&configCode=&itemId=88338&itemPath=1755772414&pageNumber=1&pageSize=150&sortKey=default&fullPath=%2C1%2C126315%2C2%2C88276%2C88338%2C#[!opt!]{"type":"json"}[/!opt!]',
-    loadedSelector: 'body',
-    noResultsXPath: null,
+    loadedSelector: 'td.Items.depth_1 > table > tbody > tr > td.LISTITEM_9872649.depth_2 > table > tbody > tr',
+    noResultsXPath: 'div.alert.alert-danger',
     zipcode: '',
   },
 };
