940548c02966e25031691214df19ab6948c74ba9
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-01-29 15:06:38 +0100

    - Trying out new proxy settings - Updating tests

--- src/library/product/details/stores/e/eapteka/RU/execute.js
+++ src/library/product/details/stores/e/eapteka/RU/execute.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -8,5 +7,33 @@ module.exports = {
     loadedSelector: 'div.offer-card',
     noResultsXPath: '//div[@class="sec-error"]',
     zipcode: '',
+  },
+  implementation: async (inputs, { loadedSelector, noResultsXPath }, context, dependencies) => {
+    const { url, id } = inputs;
+    let builtUrl;
+    if (!url) {
+      if (!id) throw new Error('No id provided');
+      else builtUrl = await dependencies.createUrl(inputs);
+    }
+    await context.setBlockAds(false);
+    await dependencies.goto({ ...inputs, url: builtUrl || url });
+
+    if (loadedSelector) {
+      await context.waitForFunction(
+        (selector, xpath) => {
+          return !!(
+            document.querySelector(selector) ||
+            document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue
+          );
+        },
+        { timeout: 10000 },
+        loadedSelector,
+        noResultsXPath,
+      );
+    }
+    return await context.evaluate(
+      (xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue,
+      noResultsXPath,
+    );
   },
 };
