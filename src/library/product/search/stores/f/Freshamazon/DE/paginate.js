bb9c87f017db8d2772a434adb26cf93fae7a7cef
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-01-30 09:42:46 +0530

    updated paginate

--- src/library/product/search/stores/f/Freshamazon/DE/paginate.js
+++ src/library/product/search/stores/f/Freshamazon/DE/paginate.js
@@ -5,18 +5,18 @@ module.exports = {
     template: null,
     country: 'DE',
     store: 'Freshamazon',
-    nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
+    // nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
     nextLinkXpath: null,
     mutationSelector: null,
-    spinnerSelector: null,
+    // spinnerSelector: 'div.s-result-list-placeholder:not(.aok-hidden)',
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
     loadedXpath: null,
     noResultsXPath: '//span[contains(text(),"Keine Ergebnisse")]',
     stopConditionSelectorOrXpath: null,
     resultsDivSelector: null,
-    // openSearchDefinition: {
-    //   template: 'https://www.amazon.de/s?k={searchTerms}&i=amazonfresh&__mk_de_DE=ÅMÅŽÕÑ&ref=nb_sb_noss&page={page}'
-    // },
+    openSearchDefinition: {
+      template: 'https://www.amazon.de/s?k={searchTerms}&i=amazonfresh&__mk_de_DE=ÅMÅŽÕÑ&ref=nb_sb_noss&page={page}'
+    },
     domain: 'amazon.de',
     zipcode: '10243',
   },
