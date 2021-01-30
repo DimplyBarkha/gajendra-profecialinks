705a448ca0d2edb01b823fbb59277b49c7d54a28
ahmad8931 <ahmad@profecialinks.com>
2021-01-30 18:10:26 +0530

    review sort by changed

--- src/library/product/reviews/stores/a/amazon/IT/execute.js
+++ src/library/product/reviews/stores/a/amazon/IT/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.it',
     loadedSelector: 'div#cm_cr-review_list',
     noResultsXPath: '//td[@valign="middle"]/b[contains(text(),"Cerchi qualcosa in particolare?")]',
-    reviewUrl: 'https://www.amazon.it/product-reviews/{id}/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews',
+    reviewUrl: 'https://www.amazon.it/product-reviews/{id}/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&sortBy=recent',
     sortButtonSelectors: null,
     zipcode: '',
   },
--- src/library/product/reviews/stores/a/amazon/JP/execute.js
+++ src/library/product/reviews/stores/a/amazon/JP/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'amazon.co.jp',
     loadedSelector: 'div#cm_cr-review_list',
     noResultsXPath: '//td/b[contains(text(),"Looking for something?")]',
-    reviewUrl: 'https://www.amazon.co.jp/-/en/product-reviews/{id}/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews',
+    reviewUrl: 'https://www.amazon.co.jp/-/en/product-reviews/{id}/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&sortBy=recent',
     sortButtonSelectors: null,
     zipcode: '',
   },
