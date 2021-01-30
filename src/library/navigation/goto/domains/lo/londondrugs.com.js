a359ae24c020edc059d0eb487705cf5796dfd1d8
DeepikaO <deepika.kuppireddy@od.import.io>
2021-01-30 18:54:58 +0530

    #2704 londondrugs details feed type

--- src/library/navigation/goto/domains/lo/londondrugs.com.js
+++ src/library/navigation/goto/domains/lo/londondrugs.com.js
@@ -1,56 +1,45 @@
-
 module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'londondrugs.com',
-    timeout: 90000,
+    timeout: 20000,
     country: 'CA',
     store: 'londondrugs',
     zipcode: '',
   },
-  implementation: async (
-    { url, zipcode },
-    parameters,
-    context,
-    dependencies,
+  implementation: async ({ url, zipcode, storeId },
+    parameters, context, dependencies,
   ) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    const timeout = parameters.timeout ? parameters.timeout : 30000;
+    const maxRetries = 3;
+    var lastResponseData;
+    var isCaptchaFramePresent;
 
+    url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+    let numberOfCaptchas = 0;
+    await context.captureRequests();
+    await context.setAntiFingerprint(false);
     await context.setBlockAds(false);
     await context.setLoadAllResources(true);
     await context.setLoadImages(true);
     await context.setJavaScriptEnabled(true);
-    await context.setAntiFingerprint(false);
     await context.setUseRelayProxy(false);
-    await context.setCssEnabled(true);
 
-    const inputUrl = '${url}#[!opt!]{"discard_CSP_header":true, "block_ads": false}[/!opt!]';
-    await context.goto(inputUrl, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
-    console.log(zipcode);
-    if (zipcode) {
-      await dependencies.setZipCode({ url: url, zipcode: zipcode });
-    }
-    const responseStatus = await context.goto(url, {
-      firstRequestTimeout: 60000,
-      timeout: timeout,
-      waitUntil: 'load',
-      checkBlocked: false,
-      antiCaptchaOptions: {
-        type: 'RECAPTCHA',
-      },
-    });
-    console.log('Status :', responseStatus.status);
-    console.log('URL :', responseStatus.url);
+    // await context.goto(url, {
+    //   firstRequestTimeout: 60000,
+    //   timeout: timeout,
+    //   waitUntil: 'networkidle0',
+    //   checkBlocked: true,
+    //   block_ads: false,
+    //   load_all_resources: true,
+    //   images_enabled: true,
+    //   load_timeout: 0,
+    //   antiCaptchaOptions: {
+    //     type: 'RECAPTCHA',
+    //   },
+    // });
 
-    const captchaFrame = "iframe[_src*='captcha']:not([title]), iframe[src*='captcha']:not([title])";
-    const maxRetries = 3;
-    let numberOfCaptchas = 0;
-
-    try {
-      await context.waitForSelector(captchaFrame);
-    } catch (e) {
-      console.log("Didn't find Captcha.");
-    }
+    const captchaFrame = 'iframe[src*="https://geo.captcha"]';
 
     const checkExistance = async (selector) => {
       return await context.evaluate(async (captchaSelector) => {
@@ -58,9 +47,69 @@ module.exports = {
       }, selector);
     };
 
+    const run = async () => {
+      lastResponseData = context.goto(url, {
+        firstRequestTimeout: 60000,
+        waitUntil: 'load',
+        checkBlocked: true,
+        js_enabled: true,
+        css_enabled: true,
+        antiCaptchaOptions: {
+          type: 'RECAPTCHA',
+        },
+      });
+      isCaptchaFramePresent = await checkExistance(captchaFrame);
+
+      while (isCaptchaFramePresent && numberOfCaptchas < maxRetries) {
+        console.log('isCaptcha', true);
+        ++numberOfCaptchas;
+        await context.waitForNavigation({ timeout });
+        try {
+          console.log(`Trying to solve captcha - count [${numberOfCaptchas}]`);
+          // @ts-ignore
+          // eslint-disable-next-line no-undef
+          await context.evaluateInFrame('iframe', () => grecaptcha.execute());
+          console.log('solved captcha, waiting for page change');
+          await context.waitForNavigation({ timeout });
+          const redirectionSuccess = await checkRedirection();
+          if (!redirectionSuccess) {
+            throw new Error('ERROR: Failed to load product details page');
+          }
+          if (redirectionSuccess) {
+            await context.evaluate((url) => {
+              window.location.href = url;
+            }, url);
+            await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
+            break;
+          } else {
+            await context.evaluate((url) => {
+              // eslint-disable-next-line no-unused-expressions
+              window.location.reload;
+            });
+            await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
+          }
+
+          isCaptchaFramePresent = await checkExistance(captchaFrame);
+        } catch (e) {
+          console.log('Captcha did not load');
+          console.log(JSON.stringify(lastResponseData));
+          // @ts-ignore
+          return context.reportBlocked(lastResponseData.code, 'Blocked: Could not solve CAPTCHA, attempts=' + numberOfCaptchas);
+        }
+      }
+    };
+
+    try {
+      await run();
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      // @ts-ignore
+      return context.reportBlocked(lastResponseData.code, 'Blocked: Could not solve CAPTCHA, attempts=' + numberOfCaptchas);
+    }
+
     const checkRedirection = async () => {
       try {
-        await context.waitForSelector('h2.pdp-title', { timeout });
+        await context.waitForSelector('#my-account-option, span[itemprop="productID"]', { timeout });
         console.log('Redirected to another page.');
         return true;
       } catch (e) {
@@ -69,46 +118,33 @@ module.exports = {
       }
     };
 
-    let isCaptchaFramePresent = await checkExistance(captchaFrame);
-    console.log('isCaptcha:' + isCaptchaFramePresent);
-
-    while (isCaptchaFramePresent && numberOfCaptchas < maxRetries) {
-      console.log('isCaptcha', true);
-      ++numberOfCaptchas;
-      await context.waitForNavigation({ timeout });
-      try {
-        console.log(`Trying to solve captcha - count [${numberOfCaptchas}]`);
-        // @ts-ignore
-        // eslint-disable-next-line no-undef
-        await context.evaluateInFrame('iframe', () => grecaptcha.execute());
-        console.log('solved captcha, waiting for page change');
-        await context.waitForNavigation({ timeout });
-        const redirectionSuccess = await checkRedirection();
-
-        if (redirectionSuccess) {
-          await context.evaluate((url) => {
-            window.location.href = url;
-          }, url);
-          await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
-          break;
-        } else {
-          await context.evaluate((url) => {
-            // eslint-disable-next-line no-unused-expressions
-            window.location.reload;
-          });
-          await context.waitForNavigation({ timeout, waitUntil: 'networkidle0' });
-        }
-
-        isCaptchaFramePresent = await checkExistance(captchaFrame);
-      } catch (e) {
-        console.log('Captcha did not load');
-      }
-    }
+    // let isCaptchaFramePresent = await checkExistance(captchaFrame);
 
     isCaptchaFramePresent = await checkExistance(captchaFrame);
 
     if (isCaptchaFramePresent) {
-      throw new Error('Failed to solve captcha');
+      console.log('Failed to load captcha');
+      // @ts-ignore
+      return context.reportBlocked(lastResponseData.code, 'Blocked: Could not solve CAPTCHA, attempts=' + numberOfCaptchas);
     }
+    async function autoScroll (page) {
+      await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      });
+    }
+    await autoScroll(context);
   },
 };
