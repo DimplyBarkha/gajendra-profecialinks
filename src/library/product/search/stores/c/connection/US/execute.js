282e117da4b577600181648a9f0eb82c1236e6cb
John Felix J <johnfelix.josep@hcl.com>
2021-01-30 10:02:01 +0530

    connection search fixes

--- src/library/product/search/stores/c/connection/US/execute.js
+++ src/library/product/search/stores/c/connection/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'connection',
     domain: 'connection.com',
-    url: 'https://www.connection.com/IPA/Shop/Product/Search?SearchType=1&term={searchTerms}',
+    url: 'https://www.connection.com/product/searchpage?SearchType=1&term={searchTerms}&pageNumber=1&pageSize=12&url=https%3A%2F%2Fwww.connection.com%2FIPA%2FShop%2FProduct%2FSearch&mode=List',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
