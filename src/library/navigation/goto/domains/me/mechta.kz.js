d3a4c1310a7ad2fc25a39db26697b3039734a18f
adheer <sangeta.kumari@synerzip.com>
2021-01-31 03:12:01 +0530

    kick-back issue
    rebase

--- src/library/navigation/goto/domains/me/mechta.kz.js
+++ src/library/navigation/goto/domains/me/mechta.kz.js
@@ -3,19 +3,19 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'mechta.kz',
-    timeout: 80000,
+    timeout: 90000,
     country: 'KZ',
     store: 'mechta',
     zipcode: "''",
   },
-  // implementation: async ({ url }, parameters, context, dependencies) => {
-  //   url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
-  //   await context.goto(url, {
-  //     block_ads: false,
-  //     load_all_resources: true,
-  //     images_enabled: true,
-  //     timeout: 100000,
-  //     waitUntil: 'load',
-  //   });
-  // },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 100000,
+      waitUntil: 'load',
+    });
+  },
 };
