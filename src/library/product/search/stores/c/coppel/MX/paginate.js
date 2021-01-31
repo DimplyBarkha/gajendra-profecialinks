60203f6d55b1a0245caf473d95ade7f3a1a34d7e
John Felix J <johnfelix.josep@hcl.com>
2021-01-31 09:14:48 +0530

    coppel fixes

--- src/library/product/search/stores/c/coppel/MX/paginate.js
+++ src/library/product/search/stores/c/coppel/MX/paginate.js
@@ -9,7 +9,10 @@ module.exports = {
     spinnerSelector: null,
     loadedSelector: null,
     noResultsXPath: null,
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      offset: 12,
+      template: 'https://www.coppel.com/SearchDisplay?categoryId=&storeId=12761&catalogId=10001&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex={offset}&pageSize=12&searchTerm={searchTerms}#facet:&productBeginIndex:12&orderBy:&pageView:list&minPrice:&maxPrice:&pageSize:&',
+    },
     domain: 'coppel.com',
     zipcode: '',
   },
