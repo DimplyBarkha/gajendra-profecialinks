c09151cd98221bc921a6f82d58221a92ad1c0c9d
tapan-profecialinks <tapan@profecialinks.com>
2021-01-31 12:47:43 +0530

    retailer product code added.

--- src/library/product/details/stores/d/davidjones/AU/extract.js
+++ src/library/product/details/stores/d/davidjones/AU/extract.js
@@ -8,4 +8,30 @@ module.exports = {
     domain: 'davidjones.com',
     zipcode: '',
   },
+  implementation: async (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const variantCount = await context.evaluate(async function () {
+      return document.querySelectorAll('ul > li.size-0 > input').length;
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.extract(productDetails, { transform });
+    for (let index = 2; index <= variantCount; index++) {
+      try {
+        await context.click(`ul > li.size-0:nth-child(${index})`);
+        await new Promise(resolve => setTimeout(resolve, 500));
+        if (variantCount !== index) {
+          await context.extract(productDetails, { type: 'APPEND', transform });
+        } else {
+          return await context.extract(productDetails, { type: 'APPEND', transform });
+        }
+      } catch (error) {
+        console.log('Error While itrerating over the variants',error);
+      }
+    }
+  },
 };
