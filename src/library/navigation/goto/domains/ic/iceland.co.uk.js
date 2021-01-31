b886731ef35cccd306b1aacff8a9c1569f24379c
dhanush <dhanush.isukapalli@od.import.io>
2021-01-31 11:13:57 +0530

    iceland by kavya

--- src/library/navigation/goto/domains/ic/iceland.co.uk.js
+++ src/library/navigation/goto/domains/ic/iceland.co.uk.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'iceland.co.uk',
-    timeout: null,
+    timeout: 60000,
     country: 'UK',
     store: 'iceland',
     zipcode: '',
--- src/library/product/details/stores/i/iceland/UK/createUrl.js
+++ src/library/product/details/stores/i/iceland/UK/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'iceland.co.uk',
     prefix: null,
-    url: null,
+    url: 'https://www.iceland.co.uk/p/iceland-thick-sliced-tiger-bloomer-800g/65937.html#q={id}',
     country: 'UK',
     store: 'iceland',
     zipcode: '',
