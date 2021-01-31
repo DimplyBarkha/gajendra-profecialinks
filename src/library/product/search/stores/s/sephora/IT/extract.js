99a3c745b758580e4a00c6ddcc230071af107fbe
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2021-01-31 21:53:23 +0530

    updated extract.js

--- src/library/product/search/stores/s/sephora/IT/extract.js
+++ src/library/product/search/stores/s/sephora/IT/extract.js
@@ -1,4 +1,4 @@
-const {transform}=require('../IT/format')
+const { transform } = require('../IT/format')
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +8,30 @@ module.exports = {
     domain: 'sephora.it',
     zipcode: '',
   },
+  implementation
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    let count = document.querySelectorAll(`ul#search-result-items > li`).length;
+    let scrollElement = document.querySelector('div.infinite-scroll-placeholder');
+    while (scrollElement && count <= 150) {
+      scrollElement.scrollIntoView({ behavior: 'smooth', block: 'end' })
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      let newCount = document.querySelectorAll(`ul#search-result-items > li`).length;
+      if (newCount === count) {
+        break;
+      }
+      else {
+        count = newCount;
+      }
+    }
+  })
+  return await context.extract(productDetails, { transform });
+}
