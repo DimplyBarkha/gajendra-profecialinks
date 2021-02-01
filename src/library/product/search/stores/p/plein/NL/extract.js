8ecadf5c7a9dfe8a88b0156abc3b79347ac97cbd
adheer <sangeta.kumari@synerzip.com>
2021-02-01 20:51:10 +0530

    kickback issues

--- src/library/product/search/stores/p/plein/NL/extract.js
+++ src/library/product/search/stores/p/plein/NL/extract.js
@@ -1,20 +1,48 @@
 const { transform } = require('./shared');
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
 async function implementation (
   inputs,
   parameters,
   context,
   dependencies,
 ) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-  try {
-    await context.waitForSelector('div.modal-footer')
-    await context.click('div.modal-footer>a.btn-success')
-    //await context.click('div.modal-footer > button')
-  } catch (error) {
-    console.log('cookie pop up not loded', error);
-  }
-  return await context.extract(productDetails, { transform });
+  console.log('params', parameters);
+  const applyScroll = async function (context) {
+    await context.evaluate(async function () {
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+  };
+  await applyScroll(context);
+
+  // return await context.evaluate(function (xp) {
+  //   const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+  //   console.log(xp, r);
+  //   const e = r.iterateNext();
+  //   console.log(e);
+  //   return !e;
+  // }, parameters.noResultsXPath);
 }
 module.exports = {
   implements: 'product/search/extract',
@@ -25,5 +53,5 @@ module.exports = {
     domain: 'plein.nl',
     zipcode: "''",
   },
-  implementation,
+  // implementation,
 };
--- src/library/product/search/stores/p/plein/NL/paginate.js
+++ src/library/product/search/stores/p/plein/NL/paginate.js
@@ -4,16 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'NL',
     store: 'plein',
-    // nextLinkSelector: 'li:nth-last-child(2) a',
-    // nextLinkXpath: '//a[contains(text(),">")]', 
-    // loadedXpath: '//div[contains(@class,"product-view-container")]',
-    nextLinkXpath: '//a[contains(text(),">")]',
-    loadedSelector: 'div.product-view-container',
-    noResultsXPath: '//div[contains(text()," Tip")]',
-    // openSearchDefinition: {
-    //   template: 'https://www.plein.nl/zoeken?search={searchTerms}&p={page}', 
-    // },    
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: 'div[class*="product-view-container"]',
+    noResultsXPath: '//div[contains(text(),"Probeer eens een merknaam of beschrijving van je product in te typen.")]',
+    openSearchDefinition: {
+      template: 'https://www.plein.nl/zoeken?search={searchTerms}&p={page}',
+    },
+    domain: 'plein.nl',
+    zipcode: "''",
   },
-  domain: 'plein.nl',
-  zipcode: "''",
 };
