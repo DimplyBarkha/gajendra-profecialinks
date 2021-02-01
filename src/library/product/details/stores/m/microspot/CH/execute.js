291ae1ad0500acfaed671e6d2de244a3c63a5d42
sushma-mangalapally <sushma.mangalapally@import.io>
2021-02-01 14:06:31 -0800

    Add changes before merge

--- src/library/product/details/stores/m/microspot/CH/execute.js
+++ src/library/product/details/stores/m/microspot/CH/execute.js
@@ -5,8 +5,10 @@ module.exports = {
     country: 'CH',
     store: 'microspot',
     domain: 'microspot.ch',
-    loadedSelector: 'div#TOP_CONTENT_ANCHOR',
-    noResultsXPath: '//h2[contains(text(),"Dieses Produkt ist leider nicht mehr verfügba")] | //*[contains(text(),"Aber so schnell geben Sie doch nicht auf, ode")]',
+    // loadedSelector: 'div#TOP_CONTENT_ANCHOR',
+    loadedSelector: 'div#container-productdetailPrice',
+    // noResultsXPath: '//h2[contains(text(),"Dieses Produkt ist leider nicht mehr verfügba")]|//*[contains(text(),"Aber so schnell geben Sie doch nicht auf, ode")]',
+    noResultsXPath: '//h2[contains(text(),"Dieses Produkt ist leider nicht mehr verfügba")] | //*[contains(text(),"Aber so schnell geben Sie doch nicht auf, ode")] | //div[@id="container-productlist"]//a[contains(@id, "container-product-0-item")]',
     zipcode: '',
   },
 };
