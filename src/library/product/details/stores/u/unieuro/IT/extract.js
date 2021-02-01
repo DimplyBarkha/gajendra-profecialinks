837379667bf9b72bde94653ee97ae3c2de8405f1
Vivek Gondil <gondilvivek@gmail.com>
2021-02-01 10:00:06 +0530

    Added the API support

--- src/library/product/details/stores/u/unieuro/IT/extract.js
+++ src/library/product/details/stores/u/unieuro/IT/extract.js
@@ -16,27 +16,50 @@ async function implementation(
     await context.evaluate(async function() {
         const ecDiv = document.querySelector('div[id="inpage_container"]');
         if (!ecDiv) {
-            async function add() {
-                try {
-                    const jsApi = document.querySelector('#flix-minisite no-script, #flix-inpage > script').getAttribute('src');
-                    let response = await fetch(jsApi);
+            async function addEnhancedContent() {
+                const jsApi = document.querySelector('[src*="ws.cnetcontent.com"]') && document.querySelector('[src*="ws.cnetcontent.com"]').src;
+                let jsApi2 = document.querySelector('#flix-minisite no-script, #flix-inpage > script') && document.querySelector('#flix-minisite no-script, #flix-inpage > script').getAttribute('src');
+                if (jsApi) {
+                    const clean = text => text.toString()
+                        .replace(/\r\n|\r|\n/g, ' ')
+                        .replace(/&amp;nbsp;/g, ' ')
+                        .replace(/&amp;#160/g, ' ')
+                        .replace(/\u00A0/g, ' ')
+                        .replace(/\s{2,}/g, ' ')
+                        .replace(/"\s{1,}/g, '"')
+                        .replace(/\s{1,}"/g, '"')
+                        .replace(/^ +| +$|( )+/g, ' ')
+                        // eslint-disable-next-line no-control-regex
+                        .replace(/[\x00-\x1F]/g, '')
+                        .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+                    const response = await fetch(jsApi);
+                    const text = await response.text();
+                    const text2 = clean(text);
+                    const array = text2.match(/"htmlBlocks"\s*:\s*(\[.+])\s*,\s*"sites"/)[1];
+                    const html = unescape(array.match(/ccs-inline-content","html":"(.+)"/)[1].replace(/\\n/g, '')).replace(/\\"/g, '"').replace(/\\'/g, "'");
+                    const div = document.createElement('div');
+                    div.id = 'enhanced-content';
+                    div.innerHTML = html;
+                    document.body.append(div);
+                } else {
+                    const ean = document.querySelector('div[data-module="productdetail"]').getAttribute('data-productdetail-productean');
+                    jsApi2 = `https://media.flixcar.com/delivery/js/inpage/353/it/ean/${ean}`;
+                    let response = await fetch(jsApi2);
+                    if (response.status !== 200) throw Error('Enhanced content API Failed');
                     const js = await response.text();
-                    eval(js);
-                } catch (error) {
-                    console.log(error);
-                }
-                const id = flixJsCallbacks.pid;
-                response = await fetch(`https://media.flixcar.com/delivery/inpage/show/620/pt/${id}/json`);
-                const html = JSON.parse((await response.text()).match(/^\((.+)\)$/)[1]).html;
-                if (html) {
-                    let manuDiv = document.createElement('div');
-                    manuDiv.setAttribute('id', 'ecDiv');
-                    manuDiv.innerHTML = html
-                    document.body.append(manuDiv);
+                    if (js.match(/flixJsCallbacks.pid\s*='([^']+)/)) {
+                        const id = js.match(/flixJsCallbacks.pid\s*='([^']+)/)[1];
+                        response = await fetch(`https://media.flixcar.com/delivery/inpage/show/353/it/${id}/json`);
+                        const html = JSON.parse((await response.text()).match(/^\((.+)\)$/)[1]).html;
+                        const div = document.createElement('div');
+                        div.id = 'enhanced-content';
+                        div.innerHTML = html;
+                        document.body.append(div);
+                    }
                 }
             }
             try {
-                await add();
+                await addEnhancedContent();
             } catch (error) {
                 console.log(error);
             }
