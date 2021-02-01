7d631514a676e69d0b038e9724fbdf5a4d7d388b
adheer <sangeta.kumari@synerzip.com>
2021-02-01 20:51:10 +0530

    kickback issues

--- src/library/product/details/stores/p/plein/NL/extract.js
+++ src/library/product/details/stores/p/plein/NL/extract.js
@@ -1,22 +1,22 @@
 const { transform } = require('./format');
 
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-  try {
-    await context.waitForSelector('div.owl-item.active img.owl-lazy')
-    //await context.click('div.modal-footer>a.btn-success')
-    //await context.click('div.modal-footer > button')
-  } catch (error) {
-    console.log('cookie pop up not loded', error);
-  }
-  return await context.extract(productDetails, { transform });
-}
+// async function implementation (
+//   inputs,
+//   parameters,
+//   context,
+//   dependencies,
+// ) {
+//   const { transform } = parameters;
+//   const { productDetails } = dependencies;
+//   try {
+//     await context.waitForSelector('div.owl-item.active img.owl-lazy');
+//     //await context.click('div.modal-footer>a.btn-success')
+//     //await context.click('div.modal-footer > button')
+//   } catch (error) {
+//     console.log('cookie pop up not loded', error);
+//   }
+//   return await context.extract(productDetails, { transform });
+// }
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -26,5 +26,39 @@ module.exports = {
     domain: 'plein.nl',
     zipcode: '',
   },
-  implementation,
+  // implementation,
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    await context.evaluate(async () => {
+      const loadMoreEle = document.querySelector('button.btn.btn-link.product-details-toggle');
+      if (loadMoreEle) {
+        // @ts-ignore
+        loadMoreEle.click();
+      }
+
+      let scrollTop = 0;
+      while (scrollTop !== 20000) {
+        await stall(500);
+        scrollTop += 1000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(5000);
+          break;
+        }
+      }
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.extract(productDetails, { transform });
+  },
 };
--- src/library/product/details/stores/p/plein/NL/format.js
+++ src/library/product/details/stores/p/plein/NL/format.js
@@ -1,105 +1,82 @@
+
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data, context) => {
-  const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/"\s{1,}/g, '"')
-    .replace(/\s{1,}"/g, '"')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-  const state = context.getState();
-  let orgRankCounter = state.orgRankCounter || 0;
-  let rankCounter = state.rankCounter || 0;
-  const productCodes = state.productCodes || [];
+const transform = (data) => {
+  const cleanUp = (data, context) => {
+    const clean = text => text.toString()
+      .replace(/\r\n|\r|\n/g, ' ')
+      .replace(/&amp;nbsp;/g, ' ')
+      .replace(/&amp;#160/g, ' ')
+      .replace(/\u00A0/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .replace(/"\s{1,}/g, '"')
+      .replace(/\s{1,}"/g, '"')
+      .replace(/^ +| +$|( )+/g, ' ')
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x00-\x1F]/g, '')
+      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
+    return data;
+  };
   for (const { group } of data) {
     for (const row of group) {
-
       if (row.description) {
-        let info = [];
+        const description_ar = [];
         row.description.forEach(item => {
-            info.push(item.text.replace(/(\s*\n\s*)+/g, ' | ').trim());
+          // description_ar.push(item.text);
+          item.text = item.text.replace(/\n \n/g, '|');
         });
-        row.description = [{'text':info.join(' | '),'xpath':row.description[0].xpath}];
+        // if (description_ar.length) {
+        //   row.description = [{ "text": description_ar.join(" || "), 'xpath': row.description[0].xpath }];
+        // }
       }
 
-      if (row.sku) {
-          row.sku.forEach(item => {
-            let data = JSON.parse(item.text);
-             if(data['sku']){
-              if(data['sku']){
-                 item.text = data['sku'];
-              }
-            }
-          });
-      }
-
-      if (row.brandText) {
-        row.brandText.forEach(item => {
-          let data = JSON.parse(item.text);
-           if(data['brand']){
-            if(data['brand']){
-               item.text = data['brand'];
-            }
+      if (row.aggregateRating) {
+        row.aggregateRating.forEach(item => {
+          item.text = Number(item.text);
+          // console.log("row.halfStarCount: ", row.halfStarCount)
+          if (row.halfStarCount) {
+            // console.log("aggregateRating item.text: ",  item.text);
+            item.text = Number(item.text) + Number(row.halfStarCount[0].text / 2);
+            item.text = item.text.toString().replace('.', ',');
+            // console.log("aggregateRating item.text: ",  item.text);
           }
         });
-    }
-
-      if (row.variantId) {
-        let infovariant = [];
-        row.variantId.forEach(item => {
-          infovariant.push(item.text.trim());
-        });
-        row.variantId = [{'text':infovariant[0],'xpath':row.variantId[0].xpath}];
+        row.aggregateRatingText = [{
+          text: row.aggregateRating[0].text,
+        }];
       }
-
-      if (row.variantInformation) {
-        row.variantInformation.forEach(item => {
-          item.text =  item.text.replace(/(\s*\n\s*)+/g, ' | ').trim();
+      if (row.category) {
+        const info = [];
+        row.category.forEach(item => {
+          info.push(item.text.trim());
         });
+        if (info.length) {
+          row.category = [];
+          info.forEach(item => {
+            row.category.push({ text: item });
+          });
+        }
       }
-
-      if (row.ratingCount) {
-        row.ratingCount.forEach(item => {
-          item.text =  item.text.match(/\d/g).join('').trim();
-        });
-      }
-
-
-      if (row.availabilityText) {
-        let infoAvailable = [];
-        row.availabilityText.forEach(item => {
-            infoAvailable.push(item.text.trim());
-        });
-        row.availabilityText = [{'text':infoAvailable[0],'xpath':row.availabilityText[0].xpath}];
-      }
-
-      if (row.aggregateRating) {  
-        let counter = 0;                 
-        row.aggregateRating.forEach(item => {
-            if(item.text.trim() == "fas fa-star"){
-                counter = counter + 1;
-            }else if(item.text.trim() == "fas fa-star-half-alt"){
-                counter = counter + 0.5;
-            }
-        });
-        row.aggregateRating = [{'text': counter.toFixed(1),'xpath': row.aggregateRating[0].xpath}]        
-      }
-
+      // if (row.specifications) {
+      //   const specifications_ar = [];
+      //   row.specifications.forEach(item => {
+      //     specifications_ar.push(item.text);
+      //   });
+      //   if (specifications_ar.length) {
+      //     row.specifications = [{ text: specifications_ar.join(' || '), xpath: row.specifications[0].xpath }];
+      //   }
+      // }
+      // if (row.descriptionBullets) {
+      //   row.descriptionBullets = [{'text':row.descriptionBullets.length, 'xpath':row.descriptionBullets[0].xpath}];
+      // }
     }
   }
-  context.setState({ rankCounter });
-  context.setState({ orgRankCounter });
-  context.setState({ productCodes });
-  console.log(productCodes);
-  return data;
+  return cleanUp(data);
 };
 module.exports = { transform };
