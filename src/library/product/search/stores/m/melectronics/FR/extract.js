654a337a5be7cf2475f7e7142166839b97302c62
Ajinkya Kathe <ajinkya.kathe@synerzip.com>
2021-02-01 12:11:43 +0530

    Fixed pagingation issue

--- src/library/product/search/stores/m/melectronics/FR/extract.js
+++ src/library/product/search/stores/m/melectronics/FR/extract.js
@@ -10,30 +10,42 @@ module.exports = {
   },
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     await context.evaluate(async () => {
-      function stall (ms) {
+      function stall(ms) {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
             resolve();
           }, ms);
         });
       }
+
+      function findPos(obj) {
+        var curtop = 0;
+        if (obj.offsetParent) {
+          do {
+            curtop += obj.offsetTop;
+          } while (obj = obj.offsetParent);
+          return [curtop];
+        }
+      }
+
       const moreButton = document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
       if (moreButton && moreButton.singleNodeValue != null) {
         let count = 0;
         let scrollTop = 0;
         let lastScrollValue = 0;
         while (document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue && document.evaluate("//div[contains(@class,'DlNoVpsv8MABrtE8wLdel')]//a", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength <= 160) {
-          console.log('count == ', count++);
-          console.log('records count ===> ', document.evaluate("//div[contains(@class,'DlNoVpsv8MABrtE8wLdel')]//a", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength);
+          let scrollTop = 0;
           await new Promise((resolve, reject) => setTimeout(resolve, 1000));
           document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
           await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-          while (scrollTop !== 5000) {
+          // @ts-ignore
+          while (scrollTop <= findPos(document.evaluate('//section[contains(@class, "footer--shops")]//div[@class="footer--container"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue)[0] && document.evaluate("//div[contains(@class,'DlNoVpsv8MABrtE8wLdel')]//a", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength <= 160) {
             await stall(300);
             lastScrollValue = scrollTop;
-            scrollTop += 500;
+            scrollTop += 1000;
             window.scroll(lastScrollValue, scrollTop);
-            if (scrollTop === 5000) {
+            // @ts-ignore
+            if (scrollTop >= findPos(document.evaluate('//section[contains(@class, "footer--shops")]//div[@class="footer--container"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue)[0]) {
               await stall(2000);
               break;
             }
