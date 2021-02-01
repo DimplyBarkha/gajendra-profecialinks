22b50efb8c77989deec4d380a895bae9a7d493ba
vikas kyatannawar <vikas.k@osmosys.asia>
2021-02-01 20:07:16 +0530

    removed custom goto, updated createUrl

--- src/library/navigation/goto/domains/of/officedepot.com.js
+++ src/library/navigation/goto/domains/of/officedepot.com.js
@@ -8,8 +8,4 @@ module.exports = {
     store: 'officedepot',
     zipcode: '',
   },
-  implementation: async ({ url }, parameters, context, dependencies) => {
-    const URL = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
-    await context.goto(URL, { timeout: 50000, waitUntil: 'networkidle0' });
-  },
 };
--- src/library/product/details/stores/o/officedepot/US/createUrl.js
+++ src/library/product/details/stores/o/officedepot/US/createUrl.js
@@ -3,8 +3,7 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'officedepot.com',
-    prefix: 'a/products',
-    url: null,
+    url: 'https://www.officedepot.com/a/products/{id}',
     country: 'US',
     store: 'officedepot',
     zipcode: '',
