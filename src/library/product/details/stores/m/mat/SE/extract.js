990acdc5c95f1dfd67b306a3ecef77a33447374b
unknown <leena.gavali-odc@partner.import.io>
2021-02-01 14:49:52 +0530

    UAT_core_staging_run_

--- src/library/product/details/stores/m/mat/SE/extract.js
+++ src/library/product/details/stores/m/mat/SE/extract.js
@@ -44,92 +44,137 @@ module.exports = {
       }
       //calories//
       const abc2 = getXpath("//div[@class='box clearfix mobileNone']/div/p/strong[contains(text(),'Näringsvärde:')]/parent::p/text()", 'nodeValue');
-      if (abc2.includes('Kilokalori')) {
-        var ll = abc2.split('Kilokalori')[0];
-        var tt = ll.split(' ');
-        var nn = tt[tt.length - 2];
-        addElementToDocument('calory', nn);
-      }
-      else if (abc2.includes('kilojoule/kilokalori')) {
-        var pp = abc2.split('kilojoule/kilokalori')[0];
-        var aa = pp.split(' ');
-        var nn = aa[aa.length - 2];
-        addElementToDocument('calory', nn);
+      if (abc2 != null) {
+        if (abc2.includes('Kilokalori')) {
+          var ll = abc2.split('Kilokalori')[0];
+          var tt = ll.split(' ');
+          var nn = tt[tt.length - 2];
+          addElementToDocument('calory', nn);
+        }
+        else if (abc2.includes('Kilojoule')) {
+          var pp = abc2.split('Kilojoule')[0];
+          var aa = pp.split(' ');
+          var nn = aa[aa.length - 2];
+          addElementToDocument('calory', nn);
+        }
+        else if (abc2.includes('kilokalori/kilojoule')) {
+          var pp = abc2.split('kilokalori/kilojoule')[0];
+          var aa = pp.split(' ');
+          var nn = aa[aa.length - 2];
+          addElementToDocument('calory', nn);
+        }
+        else if (abc2.includes('kilojoule/kilokalori')) {
+          var pp = abc2.split('kilojoule/kilokalori')[0];
+          var aa = pp.split(' ');
+          var nn = aa[aa.length - 2];
+          addElementToDocument('calory', nn);
+        }
       }
 
       //protin//
       const abc3 = getXpath("//div[@class='box clearfix mobileNone']/div/p/strong[contains(text(),'Näringsvärde:')]/parent::p/text()", 'nodeValue');
-      if (abc3.includes('protein ')) {
-        var ll = abc3.split('protein ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('protin', nn);
-      }
-      else if (abc3.includes('Protein ')) {
-        var ll = abc3.split('Protein ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('protin', nn);
+      if (abc3 != null) {
+        if (abc3.includes('protein ')) {
+          var ll = abc3.split('protein ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('protin', nn);
+        }
+        else if (abc3.includes('Protein ')) {
+          var ll = abc3.split('Protein ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('protin', nn);
+        }
       }
 
       //fat//
       const abc4 = getXpath("//div[@class='box clearfix mobileNone']/div/p/strong[contains(text(),'Näringsvärde:')]/parent::p/text()", 'nodeValue');
-      if (abc4.includes('fett ')) {
-        var ll = abc3.split('fett ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('fat', nn);
+      if (abc4 != null) {
+        if (abc4.includes('Fett ')) {
+          var ll = abc3.split('Fett ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('fat', nn);
+        }
+        else if (abc4.includes('fett ')) {
+          var ll = abc3.split('fett ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('fat', nn);
+        }
       }
 
       //sodium//
       const abc11 = getXpath("//div[@class='box clearfix mobileNone']/div/p/strong[contains(text(),'Näringsvärde:')]/parent::p/text()", 'nodeValue');
-      if (abc11.includes('Natrium ')) {
-        var ll = abc11.split('Natrium ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('sodium', nn);
+      if (abc11 != null) {
+        if (abc11.includes('Natrium ')) {
+          var ll = abc11.split('Natrium ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('sodium', nn);
+        }
       }
 
       //fiber//
       const abc9 = getXpath("//div[@class='box clearfix mobileNone']/div/p/strong[contains(text(),'Näringsvärde:')]/parent::p/text()", 'nodeValue');
-      if (abc9.includes('fiber ')) {
-        var ll = abc9.split('fiber ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('fiber', nn);
+      if (abc9 != null) {
+        if (abc9.includes('Fiber ')) {
+          var ll = abc9.split('Fiber ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('fiber', nn);
+        }
+        else if (abc9.includes('fiber ')) {
+          var ll = abc9.split('fiber ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('fiber', nn);
+        }
       }
 
       //saturated//
       const abc7 = getXpath("//div[@class='box clearfix mobileNone']/div/p/strong[contains(text(),'Näringsvärde:')]/parent::p/text()", 'nodeValue');
-      if (abc7.includes('mättat fett ')) {
-        var ll = abc3.split('mättat fett ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('saturated', nn);
+      if (abc7 != null) {
+        if (abc7.includes('fleromättat fett ')) {
+          var ll = abc3.split('fleromättat fett ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('saturated', nn);
+        } else if (abc7.includes('mättat fett ')) {
+          var ll = abc3.split('mättat fett ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('saturated', nn);
+        }
       }
 
       //sugar//
       const abc6 = getXpath("//div[@class='box clearfix mobileNone']/div/p/strong[contains(text(),'Näringsvärde:')]/parent::p/text()", 'nodeValue');
-      if (abc6.includes('sockerarter ')) {
-        var ll = abc3.split('sockerarter ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('sugar', nn);
+      if (abc6 != null) {
+        if (abc6.includes('sockerarter ')) {
+          var ll = abc3.split('sockerarter ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('sugar', nn);
+        }
       }
 
       //carbs//
       const abc5 = getXpath("//div[@class='box clearfix mobileNone']/div/p/strong[contains(text(),'Näringsvärde:')]/parent::p/text()", 'nodeValue');
-      if (abc5.includes('kolhydrat ')) {
-        var ll = abc5.split('kolhydrat ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('carb', nn);
-      }
-      else if (abc5.includes('Kolhydrat ')) {
-        var ll = abc5.split('Kolhydrat ')[1];
-        var tt = ll.split(' ');
-        var nn = tt[0];
-        addElementToDocument('carb', nn);
+      if (abc5 != null) {
+        if (abc5.includes('kolhydrat ')) {
+          var ll = abc5.split('kolhydrat ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('carb', nn);
+        }
+        else if (abc5.includes('Kolhydrat ')) {
+          var ll = abc5.split('Kolhydrat ')[1];
+          var tt = ll.split(' ');
+          var nn = tt[0];
+          addElementToDocument('carb', nn);
+        }
       }
 
       // @ts-ignore
