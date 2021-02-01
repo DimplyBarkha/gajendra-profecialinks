db1d698a273b86be5fda1f17e203e9b4b8fbcca9
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-02-01 17:12:35 +0530

    #1711 uat issue

--- src/library/product/search/stores/p/petpost/AU/execute.js
+++ src/library/product/search/stores/p/petpost/AU/execute.js
@@ -24,30 +24,13 @@ async function implementation (
     await context.evaluate(async function (inputs) {
       console.log('tmpInput after context', inputs);
       let keywords12=inputs.keywords;
-      let scrollTop = 0;
-      /*while (scrollTop !== 20000) {
-        await stall(1500);
-        scrollTop += 500;
-        window.scroll(0, scrollTop);
-        if (scrollTop === 20000) {
-          await stall(5000);
-          break;
-        }
-      }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }*/
 
       const hiddenDataDiv = document.createElement('div');
       hiddenDataDiv.id = 'jdCustomDivJSONData';
       hiddenDataDiv.style.display = 'none';
       document.body.appendChild(hiddenDataDiv);
       const allData = await fetch(`https://api.searchspring.net/api/search/search.json?siteId=14dsyu&resultsFormat=native&page=1&resultsPerPage=150&q=${keywords12}`).then(r => r.json());
-      
+      await new Promise((resolve, reject) => setTimeout(resolve, 1000));
       if (allData && allData.results) {
         allData.results.forEach(item => {
           //console.log('item',item);
@@ -59,44 +42,11 @@ async function implementation (
           const jdCustomDivJSONDataMain = document.querySelector('div#jdCustomDivJSONData');
           jdCustomDivJSONDataMain.appendChild(hiddenDataDiv);
         });
-        /*data = allData.results.map(({ id, url, }) => ({ id, url }));
-        const hiddenDataDiv = document.createElement('div');
-        hiddenDataDiv.id = 'jdCustomDivJSONData';
-        hiddenDataDiv.style.display = 'none';
-        hiddenDataDiv.textContent = JSON.stringify(data);
-        document.body.appendChild(hiddenDataDiv);*/
       }
     },inputs);
   };
   await applyScroll(context);
-  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-  /*try{
-    await context.waitForSelector('div.-mx-2>div>button',{timeout:500});
-    await context.click('div.-mx-2>div>button');
-    await new Promise((resolve, reject) => setTimeout(resolve, 500));
-    await applyScroll(context);
-  }catch(e){
-
-  }
-
-  try{
-    await context.waitForSelector('div.-mx-2>div>button',{timeout:500});
-    await context.click('div.-mx-2>div>button');
-    await new Promise((resolve, reject) => setTimeout(resolve, 500));
-    await applyScroll(context);
-  }catch(e){
-
-  }
-
-  try{
-    await context.waitForSelector('div.-mx-2>div>button',{timeout:500});
-    await context.click('div.-mx-2>div>button');
-    await new Promise((resolve, reject) => setTimeout(resolve, 500));
-    await applyScroll(context);
-  }catch(e){
-
-  }*/
-
+  await new Promise((resolve, reject) => setTimeout(resolve, 1500));
 
   if (parameters.loadedSelector) {
     await context.waitForFunction(function (sel, xp) {
