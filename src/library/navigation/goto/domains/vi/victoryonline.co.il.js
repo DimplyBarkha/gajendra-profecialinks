6f4a318fbd8c64f0149d9349e83481a04ec12e64
sasz94 <sacha.bartlomiej@gmail.com>
2021-02-01 19:20:40 +0100

    Implemented 2-step navigation to product page to load valid price (#1493)

--- src/library/navigation/goto/domains/vi/victoryonline.co.il.js
+++ src/library/navigation/goto/domains/vi/victoryonline.co.il.js
@@ -8,4 +8,27 @@ module.exports = {
     store: 'victoryonline',
     zipcode: '',
   },
+  implementation: async (
+    { url },
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const timeout = parameters.timeout ? parameters.timeout : 30000;
+
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    await context.goto('https://www.victoryonline.co.il/', {
+      firstRequestTimeout: 60000,
+      timeout: timeout,
+      waitUntil: 'networkidle0',
+      checkBlocked: false,
+    });
+    await context.waitForNavigation();
+    await context.waitForSelector('div#HPPanel', { timeout: 30000 });
+  },
 };
--- src/library/product/details/stores/v/victoryonline/IL/execute.js
+++ src/library/product/details/stores/v/victoryonline/IL/execute.js
@@ -9,4 +9,38 @@ module.exports = {
     noResultsXPath: "//div[@id='ListTextContent']",
     zipcode: '',
   },
+  implementation: async (inputs, { loadedSelector, noResultsXPath }, context, dependencies) => {
+    const { url, id } = inputs;
+    let builtUrl;
+    if (!url) {
+      if (!id) throw new Error('No id provided');
+      else builtUrl = await dependencies.createUrl(inputs);
+    }
+
+    await dependencies.goto({ ...inputs, url: builtUrl || url });
+    context.goto(url, {
+      firstRequestTimeout: 60000,
+      timeout: 60000,
+      waitUntil: 'load',
+      checkBlocked: false,
+    });
+    await context.waitForNavigation();
+    if (loadedSelector) {
+      await context.waitForFunction(
+        (selector, xpath) => {
+          return !!(
+            document.querySelector(selector) ||
+            document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue
+          );
+        },
+        { timeout: 10000 },
+        loadedSelector,
+        noResultsXPath,
+      );
+    }
+    return await context.evaluate(
+      (xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue,
+      noResultsXPath,
+    );
+  },
 };
