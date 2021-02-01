fb1fd8a537565d0dda52b35f20c7c9aa507b14a2
venkatsn <venkat.sn-odc@partner.import.io>
2021-02-01 17:45:01 +0530

    #2642

---
+++ src/library/product/details/stores/l/lookfantastic/UK/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'UK',
+    store: 'lookfantastic',
+    transform: null,
+    domain: 'lookfantastic.com',
+    zipcode: '',
+  },
+};
