318ec4e0bb4035205a52f558bc474f030cabc70f
Prashant <prashant.wakchaure@synerzip.com>
2021-02-01 11:28:49 +0530

    fixes

--- src/library/product/search/stores/m/melectronics/FR/extract.js
+++ src/library/product/search/stores/m/melectronics/FR/extract.js
@@ -10,40 +10,47 @@ module.exports = {
   },
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     await context.evaluate(async () => {
-      function stall(ms) {
+      function stall (ms) {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
             resolve();
           }, ms);
         });
       }
-      async function infiniteScroll() {
-        let prevScroll = document.documentElement.scrollTop;
-
-        while (document.evaluate("//div[contains(@class,'DlNoVpsv8MABrtE8wLdel')]//a", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength <= 160) {
-          window.scrollBy(0, document.documentElement.clientHeight);
-          await new Promise(resolve => setTimeout(resolve, 4000));
-          const currentScroll = document.documentElement.scrollTop;
-          if (currentScroll === prevScroll) {
-            break;
-          }
-          prevScroll = currentScroll;
-        }
-      }
       const moreButton = document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
       if (moreButton && moreButton.singleNodeValue != null) {
+        let count = 0;
+        let scrollTop = 0;
+        let lastScrollValue = 0;
         while (document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue && document.evaluate("//div[contains(@class,'DlNoVpsv8MABrtE8wLdel')]//a", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength <= 160) {
+          console.log('count == ', count++);
+          console.log('records count ===> ', document.evaluate("//div[contains(@class,'DlNoVpsv8MABrtE8wLdel')]//a", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength);
           await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-          const isLimitReached = document.evaluate("//div[contains(@class,'DlNoVpsv8MABrtE8wLdel')]//a", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength <= 160
-          if (isLimitReached) {
-            break;
+          document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
+          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+          while (scrollTop !== 5000) {
+            await stall(300);
+            lastScrollValue = scrollTop;
+            scrollTop += 500;
+            window.scroll(lastScrollValue, scrollTop);
+            if (scrollTop === 5000) {
+              await stall(2000);
+              break;
+            }
           }
-          moreButton.singleNodeValue.click();
-          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-          await infiniteScroll();
         }
       } else {
-        await infiniteScroll();
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+        let scrollTop = 0;
+        while (scrollTop !== 5000) {
+          await stall(500);
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          if (scrollTop === 5000) {
+            await stall(500);
+            break;
+          }
+        }
       }
     });
     return await context.extract(productDetails, { transform });
