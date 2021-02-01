837c2065a923a8a4dc9402526759f42d85a131c8
Prashant <prashant.wakchaure@synerzip.com>
2021-02-01 17:29:13 +0530

    image Urls fixes

--- src/library/product/search/stores/m/melectronics/FR/extract.js
+++ src/library/product/search/stores/m/melectronics/FR/extract.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   implementation: async ({ url }, { country, domain, transform }, context, { productDetails }) => {
     await context.evaluate(async () => {
-      function stall(ms) {
+      function stall (ms) {
         return new Promise((resolve, reject) => {
           setTimeout(() => {
             resolve();
@@ -18,7 +18,7 @@ module.exports = {
         });
       }
 
-      function findPos(obj) {
+      function findPos (obj) {
         var curtop = 0;
         if (obj.offsetParent) {
           do {
@@ -30,11 +30,9 @@ module.exports = {
 
       const moreButton = document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
       if (moreButton && moreButton.singleNodeValue != null) {
-        let count = 0;
-        let scrollTop = 0;
         let lastScrollValue = 0;
+        let scrollTop = 0;
         while (document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue && document.evaluate("//div[contains(@class,'DlNoVpsv8MABrtE8wLdel')]//a", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength <= 160) {
-          let scrollTop = 0;
           await new Promise((resolve, reject) => setTimeout(resolve, 1000));
           document.evaluate('//div[@class="p-product-listing--item p-product-listing--item__buttons"]//button[@class="btn btn__md btn__secondary"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
           await new Promise((resolve, reject) => setTimeout(resolve, 1000));
@@ -51,11 +49,22 @@ module.exports = {
             }
           }
         }
+        while (scrollTop !== scrollTop + 10000) {
+          await stall(2000);
+          lastScrollValue = scrollTop;
+          scrollTop += 1000;
+          window.scroll(lastScrollValue, scrollTop);
+          // @ts-ignore
+          if (scrollTop !== scrollTop + 10000) {
+            await stall(2000);
+            break;
+          }
+        }
       } else {
         await new Promise((resolve, reject) => setTimeout(resolve, 1000));
         let scrollTop = 0;
-        while (scrollTop !== 5000) {
-          await stall(500);
+        while (scrollTop !== 6000) {
+          await stall(1000);
           scrollTop += 1000;
           window.scroll(0, scrollTop);
           if (scrollTop === 5000) {
