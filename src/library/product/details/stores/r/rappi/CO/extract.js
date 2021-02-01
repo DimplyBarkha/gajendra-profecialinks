62266159f55a36eb5d92b5ec8aa6f0b8360dbf99
adheer <sangeta.kumari@synerzip.com>
2021-02-01 16:27:59 +0530

    kick-back issue

--- src/library/product/details/stores/r/rappi/CO/extract.js
+++ src/library/product/details/stores/r/rappi/CO/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('./shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'CO',
     store: 'rappi',
-    transform: null,
+    transform: transform,
     domain: 'rappi.com.co',
     zipcode: '',
   },
