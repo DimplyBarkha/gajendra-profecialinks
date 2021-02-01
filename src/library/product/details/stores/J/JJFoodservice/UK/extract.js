97a3abbe33909c6b206b1c0d8e938d5a716aa084
Rahulolive <rahul.rao-od@partner.import.io>
2021-02-01 21:01:22 +0530

    rahul JJFoodservice

--- src/library/product/details/stores/J/JJFoodservice/UK/extract.js
+++ src/library/product/details/stores/J/JJFoodservice/UK/extract.js
@@ -28,6 +28,18 @@ module.exports = {
       const originalDiv = document.querySelectorAll('#jjfoods > div > div.sc-bdVaJa.bLdRfG > main > div.sc-jOVcOr.kDJWxr > div > div.sc-imAxmJ.hFVJzy > div.sc-esoVGF.bLMvps > h1')[0];
       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
+      // var getXpath = (xpath, prop) => {
+      //   var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+      //   let result;
+      //   if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+      //   else result = elem ? elem.singleNodeValue : '';
+      //   return result && result.trim ? result.trim() : result;
+      //   };  
+      //   var aval = getXpath('(//a[@data-rw="product--add-button"])[1]', 'nodeValue');
+      //   if(aval != null){
+      //   aval = "In Stock";
+      //   addHiddenDiv('aval', aval);
+      //   }    
     var cat = getAllXpath('(//div[@class="sc-dTsoBL kFLVeC"]/span/a[position()>1])/text()[1]', 'nodeValue');
     for (let i=0;i<cat.length;i++){
       addHiddenDiv("cat", cat[i]);
