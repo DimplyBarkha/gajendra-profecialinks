7d8455d220c849ef1c5d8a8fadee3584df024be2
banderovskyi <roman1004b@gmail.com>
2021-02-01 12:55:30 +0200

    Add brands to core

--- src/library/product/details/stores/a/angeloni/BR/extract.js
+++ src/library/product/details/stores/a/angeloni/BR/extract.js
@@ -12,31 +12,16 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(async () => {
-      document.querySelector(('.box-produto a')).removeAttribute('onClick');
-      document.querySelector(('.box-produto a')).removeEventListener('click', () => {
-      }, false);
+      if (document.querySelector('.box-produto a')) {
+        document.querySelector(('.box-produto a')).removeAttribute('onClick');
+        document.querySelector(('.box-produto a')).removeEventListener('click', () => {
+        }, false);
+      }
     });
     // await new Promise((resolve, reject) => setTimeout(resolve, 100000));
     await context.waitForSelector('.box-produto a');
     await context.clickAndWaitForNavigation('.box-produto a', {}, {});
     await context.evaluate(async () => {
-      // let scrollTop = 0;
-      // while (scrollTop <= 20000) {
-      //   await stall(100);
-      //   scrollTop += 1000;
-      //   window.scroll(0, scrollTop);
-      //   if (scrollTop === 20000) {
-      //     await stall(3000);
-      //     break;
-      //   }
-      // }
-      // function stall(ms) {
-      //   return new Promise((resolve) => {
-      //     setTimeout(() => {
-      //       resolve();
-      //     }, ms);
-      //   });
-      // }
       function addElementToDocument (key, value) {
         const createdElem = document.querySelector(`#${key}`);
         if (!createdElem) {
@@ -52,24 +37,23 @@ module.exports = {
       let jsonDataText = '';
       jsonDataNodes.forEach(jsonDataNode => {
         const jsonDataNodeText = jsonDataNode.textContent;
-        jsonDataNodeText.includes('price') ? jsonDataText = jsonDataNodeText : '';
+        jsonDataText = jsonDataNodeText.includes('price') ? jsonDataNodeText : '';
       });
-      const jsonDataObject = JSON.parse(jsonDataText);
-      console.log(jsonDataObject);
+      const jsonDataObject = jsonDataText ? JSON.parse(jsonDataText) : null;
       if (jsonDataObject) {
         // pirce
         addElementToDocument('ag-price', jsonDataObject.offers.price);
         // availabilityText
         if (jsonDataObject.offers.availability) {
           jsonDataObject.offers.availability.includes('OutOfStock')
-            ? addElementToDocument('ag-availability', 'Out of Stock')
-            : addElementToDocument('ag-availability', 'In Stock');
+            ? addElementToDocument('ag-availability', 'Out of stock')
+            : addElementToDocument('ag-availability', 'In stock');
         }
       }
 
       // zoom
       const mainImg = document.querySelector('.box-galeria img');
-      if (mainImg.classList.contains('zoom')) {
+      if (mainImg && mainImg.classList.contains('zoom')) {
         addElementToDocument('ag-zoom', 'Yes');
       }
 
@@ -82,8 +66,30 @@ module.exports = {
           addElementToDocument('ag-desc-bullets', bulletsMatch.length);
         }
       }
+
+      const descrInfoTitles = document.querySelectorAll('h2.titulo');
+      let descrInfoArr;
+      descrInfoTitles.forEach(descrInfoTitle => {
+        if (descrInfoTitle.textContent.includes('Informações Adicionais')) {
+          const descrInfoNode = descrInfoTitle.nextElementSibling;
+          const descrInfoInner = descrInfoNode.innerHTML;
+          descrInfoArr = descrInfoInner.replace(/&nbsp;/gm, '').split('<br>');
+        }
+      });
+      if (descrInfoArr) {
+        descrInfoArr.forEach(descrItem => {
+          function getInfoItem (infoItem, infoStr, divId) {
+            if (infoItem.toLowerCase().includes(infoStr)) {
+              const textValue = descrItem.replace(/.* - /, '');
+              addElementToDocument(divId, textValue);
+              return textValue;
+            }
+          }
+
+          getInfoItem(descrItem, 'marca', 'ag-brand');
+        });
+      }
     });
     return await context.extract(productDetails, { transform });
   },
 };
-
