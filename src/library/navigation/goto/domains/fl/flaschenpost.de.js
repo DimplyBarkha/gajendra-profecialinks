11d4c88af5862d4ece049e31a0f78dc430fffc6a
Your Name <karthik.ravi-odc@partner.import.io>
2021-02-01 18:16:01 +0530

    await

--- src/library/navigation/goto/domains/fl/flaschenpost.de.js
+++ src/library/navigation/goto/domains/fl/flaschenpost.de.js
@@ -10,16 +10,11 @@ module.exports = {
   },
   implementation: async (inputs, parameters, context, dependencies) => {
     const { timeout = 10000 } = parameters;
-    await context.setBlockAds(false);
-    await context.setLoadAllResources(true);
-    await context.setLoadImages(true);
-    await context.setJavaScriptEnabled(true);
-    await context.setAntiFingerprint(false);
-    await context.setUseRelayProxy(false);
-
     const { url, zipcode, storeId } = inputs;
     await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true, captureRequests: true });
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
-  }
+    // patch for synchronicity issue between json decoring and goto result
+    if (url) {
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+  },
 };
