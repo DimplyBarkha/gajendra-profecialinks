5064e9719441979eaa56053d42284882b6e9dfeb
SHALINI <shalini.kumari-odc@partner.import.io>
2021-02-01 14:41:57 +0530

    liverpool

--- src/library/product/search/stores/l/liverpool/MX/extract.js
+++ src/library/product/search/stores/l/liverpool/MX/extract.js
@@ -24,28 +24,32 @@ module.exports = {
                 originalDiv.parentNode.insertBefore(newDiv, originalDiv);
             }
             var productdata = document.evaluate('//script[@class="next-head"][3]', document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent;
-            var productInfoJson = JSON.parse(productdata);
-            console.log(productInfoJson)
-            // var brandBlock1 = document.querySelector('head > script:nth-child(99)');
-            // @ts-ignore
-            const lengthArray = productInfoJson.itemListElement.length;
-            // @ts-ignore
-            const itemList = productInfoJson.itemListElement;
-            alert(itemList[0].item)
-            for (let i = 1; i <= lengthArray; i++) {
-                if (Object.keys(itemList[i].item.aggregateRating).length > 1) {
-                    const finalValue = itemList[i].item.aggregateRating.ratingValue
-                    const url = itemList[i].item.url
-                    addHiddenDiv('finalValue', finalValue, i);
-                    addHiddenDiv('url', url, i);
-                    i++;
+            if (productdata != null) {
+                var productInfoJson = JSON.parse(productdata);
+                console.log(productInfoJson)
+                // var brandBlock1 = document.querySelector('head > script:nth-child(99)');
+                // @ts-ignore
+                const lengthArray = productInfoJson.itemListElement.length;
+                // @ts-ignore
+                const itemList = productInfoJson.itemListElement;
+                alert(itemList[0].item)
+                for (let i = 1; i <= lengthArray; i++) {
+                    if (Object.keys(itemList[i].item.aggregateRating).length > 1) {
+                        const finalValue = itemList[i].item.aggregateRating.ratingValue
+                        const url = itemList[i].item.url
+                        addHiddenDiv('finalValue', finalValue, i);
+                        addHiddenDiv('url', url, i);
+                        i++;
+                    }
+                    else {
+                        i++;
+                    }
+                    // var finalValue = itemList[i].item.url
+                    // console.log("test" + finalValue)
                 }
-                else{
-                    i++;
-                }
-                // var finalValue = itemList[i].item.url
-                // console.log("test" + finalValue)
+
             }
+
             // @ts-ignore
             //console.log("json" + JSON.parse(brandBlock1.innerText).itemListElement[1].item.aggregateRating.ratingValue);
             // addHiddenDiv('finalValue', finalValue, i);
