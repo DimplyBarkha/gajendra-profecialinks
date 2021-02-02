e1216da6687c3a6f23600a32f9133bc6fee02190
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-02 22:28:23 +0530

    updated this to code to check for the id among products

--- src/library/product/details/stores/0/0815/AT/execute.js
+++ src/library/product/details/stores/0/0815/AT/execute.js
@@ -14,28 +14,101 @@ async function implementation (
   await dependencies.goto({ url, zipcode, storeId });
 
   if (parameters.loadedSelector) {
-    await context.waitForFunction(function (sel, xp) {
-      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 25000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    try {
+      await context.waitForFunction(function (sel, xp) {
+        return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+      }, { timeout: 25000 }, parameters.loadedSelector, parameters.noResultsXPath);
+    } catch(err) {
+      console.log('we had some issue while waiting', err.message);
+    }
   }
 
-  if (id) {
-    await context.waitForSelector('.product-image-link');
-    const pageLink = await context.evaluate(() => document.querySelector('.product-image-link').getAttribute('href'));
-    const rpc = pageLink.split('/').pop();
-    if (id.toString() === rpc.toString()) {
-      await context.goto(pageLink, {
-        blockAds: false,
-        loadAllResources: true,
-        imagesEnabled: true,
-        timeout: 100000,
-        waitUntil: 'networkidle0',
-      });
-    } else {
-      await context.evaluate(() => {
-        document.querySelector('meta[property="og:image"]').setAttribute('content', '');
-      });
+  let thisPageHasProdId = false;
+  let allNameSel = 'a[class*="product-titel"]';
+  let allNamesLoaded = false;
+  try {
+    await context.waitForSelector(allNameSel);
+    console.log('got some names');
+    allNamesLoaded = true;
+  } catch(err) {
+    console.log('got some error while waiting for names to load on the search page', err.message);
+  }
+
+  if(allNamesLoaded) {
+    thisPageHasProdId = await context.evaluate(async (allNameSel, id) => {
+      console.log('allNameSel', allNameSel);
+      console.log('id', id);
+      let allNamesElms = document.querySelectorAll(allNameSel);
+      let thisPageHasProdId = false;
+      if(allNamesElms) {
+        for(let i = 0; i < allNamesElms.length; i++) {
+          let url = allNamesElms[i].getAttribute('href');
+          console.log('url', url);
+          let thisId = url.split('/').pop();
+          if(thisId.toString() === id.toString()) {
+            thisPageHasProdId = true;
+            break;
+          }
+        }
+      }
+      return thisPageHasProdId;
+    }, allNameSel, id);
+  }
+
+  if(!thisPageHasProdId) {
+    console.log("this page does not have the product id mentioned in the input");
+    return thisPageHasProdId;
+  }
+
+  let gotTheProdId = false;
+  let allProdCount = await context.evaluate(async (allNameSel)=> {
+    console.log('allNameSel', allNameSel);
+    let allNamesElms = document.querySelectorAll(allNameSel);
+    if(allNamesElms) {
+      return allNamesElms.length;
     }
+    return 0;
+  }, allNameSel);
+
+  if (id) {
+    let i = 0;
+    while(allProdCount > i && (!gotTheProdId)) {
+      const pageLink = await context.evaluate(async (i) => {return (document.querySelectorAll('.product-image-link')[i].getAttribute('href'))}, i);
+      const rpc = pageLink.split('/').pop();
+      if (id.toString() === rpc.toString()) {
+        await context.goto(pageLink, {
+          blockAds: false,
+          loadAllResources: true,
+          imagesEnabled: true,
+          timeout: 100000,
+          waitUntil: 'networkidle0',
+        });
+        gotTheProdId = true;
+        try {
+          await context.waitForSelector('div[class*="is-button-buy"]', { timeout: 100000 });
+        } catch(err) {
+          console.log('we got some error while waiting for add to cart btn', err.message);
+          try {
+            await context.waitForSelector('div[class*="is-button-buy"]', { timeout: 100000 });
+          } catch(error) {
+            console.log('we got some error while waiting for add to cart btn - again', error.message);
+          }
+        }
+      } else {
+        await context.evaluate(() => {
+          document.querySelector('meta[property="og:image"]').setAttribute('content', '');
+        });
+      }
+      i++;
+    }
+    //await context.waitForSelector('.product-image-link');
+    
+  }
+
+  console.log('gotTheProdId', gotTheProdId);
+  if(!gotTheProdId) {
+    console.log("this page has the id but we could not get the page link");
+    return gotTheProdId;
   }
   await context.waitForSelector('div[class*="is-button-buy"]', { timeout: 100000 });
   await context.evaluate(() => {
--- src/library/product/details/stores/0/0815/AT/extract.js
+++ src/library/product/details/stores/0/0815/AT/extract.js
@@ -8,4 +8,95 @@ module.exports = {
     domain: '0815.at',
     zipcode: '',
   },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+
+    let skuScriptXpath = "//script[contains(@type,'application')][contains(.,'sku')]";
+    let gotSku = false;
+    let sku = '';
+    let skuDivXpath = '//div[contains(@class,"product-detail-main")]//div[contains(text(),"EAN")]//following-sibling::div';
+    try {
+      await context.waitForXPath(skuDivXpath);
+      gotSku = true;
+    } catch(err) {
+      console.log('we got some error while waiting for div containing ean', err.message);
+      try {
+        await context.waitForXPath(skuDivXpath);
+        gotSku = true;
+      } catch(err) {
+        console.log('we got some error while waiting for div containing ean, again', err.message);
+      }
+    }
+
+    if(gotSku) {
+      sku = await context.evaluate(async (skuDivXpath) => {
+        console.log('skuDivXpath', skuDivXpath);
+        let skuElm = document.evaluate(skuDivXpath, document, null, 7, null);
+        if(skuElm && skuElm.snapshotLength > 0) {
+          let sku = skuElm.snapshotItem(0).textContent;
+          if(sku) {
+            return sku.trim();
+          }
+        }
+        return '';
+      }, skuDivXpath);
+    }
+
+    if(!gotSku) {
+      try {
+        await context.waitForXPath(skuScriptXpath);
+        gotSku = true;
+      } catch(err) {
+        console.log('we got some error while waiting for div containing ean', err.message);
+        try {
+          await context.waitForXPath(skuScriptXpath);
+          gotSku = true;
+        } catch(err) {
+          console.log('we got some error while waiting for div containing ean, again', err.message);
+        }
+      }
+
+      if(gotSku) {
+        sku = await context.evaluate(async (skuScriptXpath) => {
+          console.log('skuScriptXpath', skuScriptXpath);
+          let skuElm = document.evaluate(skuScriptXpath, document, null, 7, null);
+          if(skuElm && skuElm.snapshotLength > 0) {
+            let skuText = skuElm.snapshotItem(0).textContent;
+            if(skuText) {
+              let sku =  skuText.trim();
+              let regex = /(.+)sku([":\s]+)([\d\w]+)(",)(.+)/g;
+              sku = sku.replace(regex, '$3');
+              console.log('sku', sku);
+              return sku;
+            }
+          }
+          return '';
+        }, skuScriptXpath);
+      }
+  
+    }
+
+    console.log('gotSku', gotSku);
+    console.log('finally - sku', sku);
+
+    await context.evaluate(async (sku) => {
+      console.log('finally - sku', sku);
+      async function addElementToDocumentAsync (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        document.body.appendChild(catElement);
+      }
+
+      await addElementToDocumentAsync('sku', sku);
+    }, sku);
+
+
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.extract(productDetails, { transform });
+  }
 };
