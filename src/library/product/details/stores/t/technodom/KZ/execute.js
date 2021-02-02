f1042d8795014b2d31dafdc32aeb34fa1d9a25f4
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-02-02 11:57:20 +0530

    #1866 fixing

--- src/library/product/details/stores/t/technodom/KZ/execute.js
+++ src/library/product/details/stores/t/technodom/KZ/execute.js
@@ -57,8 +57,8 @@ module.exports = {
     country: 'KZ',
     store: 'technodom',
     domain: 'technodom.kz',
-    loadedSelector: 'main.CategoryPage',
-    noResultsXPath: '//div[@class="r46t__results__title__wrap"]/div[@id="r46t-count"]/span[text()=0]',
+    loadedSelector: null,
+    noResultsXPath: null,
     zipcode: '',
   },
 };
