bb97d6b43d50e46cacca185932ada623699b8e10
Your Name <karthik.ravi-odc@partner.import.io>
2021-02-02 20:26:14 +0530

    variant

--- src/library/product/search/stores/j/jpg/FR/paginate.js
+++ src/library/product/search/stores/j/jpg/FR/paginate.js
@@ -8,9 +8,9 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'section[class="page-category__products-cards"] article',
-    noResultsXPath: '//header[@class="search-noresult my-3"]/h3/text()',
+    noResultsXPath: '//header[@class="search-noresult my-3"]/h3/text()|//ol[@class="page-breadcrumb"]',
     openSearchDefinition: {
-      pageStartNb: 1,
+      pageStartNb: 0,
       template: 'https://www.jpg.fr/INTERSHOP/web/WFS/RAJA-JPG-Site/fr_FR/-/EUR/ViewParametricSearch-ProductPaging?PageNumber={page}&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}',
       },
     domain: 'jpg.fr',
