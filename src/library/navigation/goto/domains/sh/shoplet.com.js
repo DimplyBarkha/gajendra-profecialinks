0215b4ce9f71b6a32fd211cbecf3d385a447844c
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-02-02 14:41:32 +0530

    #1837 fixing

--- src/library/navigation/goto/domains/sh/shoplet.com.js
+++ src/library/navigation/goto/domains/sh/shoplet.com.js
@@ -9,16 +9,30 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    await context.setBypassCSP(true);
+    await context.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36");
     const timeout = parameters.timeout ? parameters.timeout : 10000;
     const resp=await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false,firstRequestTimeout:60000,antiCaptchaOptions:{type: 'RECAPTCHA'} });
     console.log('response at goto:',resp);
+    await new Promise((resolve) => setTimeout(resolve, 10000));
     if(resp.status==200){
-      let hasCaptchaExists= await context.evaluate(function(){
-        return document.querySelector('div#captcha-container.captcha');
-      });
-      if(hasCaptchaExists){
-        
-        //await context.evaluateInFrame('iframe', () => grecaptcha.execute());
+      const captchaFrame = "iframe[_src*='captcha']:not([title]), iframe[src*='captcha']:not([title]), div.g-recaptcha";
+      try {
+        await context.waitForSelector(captchaFrame);
+      } catch (error) {
+        console.log('error: without undescore ', error);
+      } 
+      if (isCaptchaFramePresent) {
+        console.log('isCaptcha', true);
+        await context.waitForNavigation({ timeout });
+        // @ts-ignore
+        // eslint-disable-next-line no-undef
         try {
           await context.evaluateInFrame('iframe', () => grecaptcha.execute());
           await new Promise((resolve) => setTimeout(resolve, 8000));
@@ -33,6 +47,8 @@ module.exports = {
             throw Error ('grecaptcha is not working!!');
           }
         }
+        console.log('solved captcha, waiting for page change');
+        await context.waitForNavigation({ timeout });
       }
     }
     console.log(zipcode);
