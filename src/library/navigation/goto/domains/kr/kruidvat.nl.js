fe803f4adacb6502ddec2abbeb289bfbaaee2558
unknown <madhan.selvaraj-odc@partner.import.io>
2021-02-02 21:08:34 +0530

    im-kruidvat

--- src/library/navigation/goto/domains/kr/kruidvat.nl.js
+++ src/library/navigation/goto/domains/kr/kruidvat.nl.js
@@ -3,9 +3,15 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'kruidvat.nl',
-    timeout: 1000000,
+    timeout: 10000,
     country: 'NL',
     store: 'kruidvat',
     zipcode: '',
+  },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    context.setBlockAds(false);
+    context.setLoadAllResources(true);
+    context.setAntiFingerprint(false);
+    await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true });
   },
 };
