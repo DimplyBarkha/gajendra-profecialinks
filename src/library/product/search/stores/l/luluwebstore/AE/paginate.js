fd96a501d46a05252241c42e5b13829ad96fa6c2
Dhanush <dhanush.isukapalli@od.import.io>
2021-02-02 21:55:03 +0530

    Luluwebstore_AE By Dhanush

--- src/library/product/search/stores/l/luluwebstore/AE/paginate.js
+++ src/library/product/search/stores/l/luluwebstore/AE/paginate.js
@@ -9,7 +9,7 @@ module.exports = {
     nextLinkXpath: '//div[@class="pagination"]//a[@rel="next"]',
     mutationSelector: null,
     spinnerSelector: null,
-    // loadedSelector: 'ul[class="product__listing product__grid col-xs-12"] div[class="col-lg-3 col-md-4 col-sm-6 col-xs-6 plp-product-div js-owl-carousel-reference "]',
+    loadedSelector: 'ul[class="product__listing product__grid col-xs-12"] div[class="col-lg-3 col-md-4 col-sm-6 col-xs-6 plp-product-div js-owl-carousel-reference "]',
     loadedXpath: null,
     noResultsXPath: null,
     stopConditionSelectorOrXpath: null,
