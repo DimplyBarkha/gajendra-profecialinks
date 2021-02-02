55f1dec6cd7cd77cb9c37a748e2f8151f7a262d4
Harshkha <harsh.khare-od@partner.import.io>
2021-02-02 18:23:54 +0530

    Search complete

--- src/library/product/search/stores/p/parfumdreams/DE/extract.js
+++ src/library/product/search/stores/p/parfumdreams/DE/extract.js
@@ -55,12 +55,15 @@ module.exports = {
       //   }
       //   catch (error) { }
       // }
-      try {
+      const URL = window.location.href
+      if (URL.includes('Aftershave')) {
+
         // @ts-ignore
-        let ean = document.querySelectorAll("div.main-content > script:nth-child(6)");
-        // @ts-ignore
-        let jsonData = ean[0].innerText
+        let ean = document.querySelectorAll("#content-wrapper > div.cw.full > div.main-content > script:nth-child(6)");
+        
         try {
+          // @ts-ignore
+          let jsonData = ean[0].innerText
           if (jsonData != null) {
             //let jsonData=ean[0].innerText;
             let splitJsonData = jsonData.split('"impressions":')[1];
@@ -75,16 +78,105 @@ module.exports = {
           }
         }
         catch (error) { }
-
       }
-      catch (error) {
-        if (error instanceof TypeError) {
-          let ean = document.querySelectorAll("#right-column > script:nth-child(4)");
+      else if (URL.includes('Shu-Uemura')) {
+
+        // @ts-ignore
+        let ean = document.querySelectorAll("#content-wrapper > div > div.page-category.main-content > strong > script:nth-child(2)");
+        
+        
+        try {
+          // @ts-ignore
+          let jsonData = ean[0].innerText
+          if (jsonData != null) {
+            //let jsonData=ean[0].innerText;
+            let splitJsonData = jsonData.split('"impressions":')[1];
+            var a = splitJsonData.match(/position/g).length; console.log(a)
+            let splitJsonData2 = splitJsonData.split('"position":' + a)[0];
+            splitJsonData2 = splitJsonData2 + '"position":' + a + '}]';
+            splitJsonData2 = JSON.parse(splitJsonData2);
+            for (var i = 0; i < splitJsonData2.length; i++) {
+              addHiddenDiv('ean', splitJsonData2[i].ean, i);
+              addHiddenDiv('id', splitJsonData2[i].simpleId, i);
+            }
+          }
+        }
+        catch (error) { }
+      }
+      else if (URL.includes('sonnenöl')){
+        try {
+          let ean = document.querySelectorAll("#content-wrapper > div > div.main-content > strong > strong > script:nth-child(2)");
+          // @ts-ignore
+          let jsonData = ean[0].innerText
+          if (jsonData != null) {
+            //let jsonData=ean[0].innerText;
+            let splitJsonData = jsonData.split('"impressions":')[1];
+            var a = splitJsonData.match(/position/g).length; console.log(a)
+            let splitJsonData2 = splitJsonData.split('"position":' + a)[0];
+            splitJsonData2 = splitJsonData2 + '"position":' + a + '}]';
+            splitJsonData2 = JSON.parse(splitJsonData2);
+            for (var i = 0; i < splitJsonData2.length; i++) {
+              addHiddenDiv('ean', splitJsonData2[i].ean, i);
+              addHiddenDiv('id', splitJsonData2[i].simpleId, i);
+            }
+          }
+        }
+        catch (error) { }
+      }
+      else if (URL.includes('haar%u00f6l')) {
+
+        // @ts-ignore
+        let ean = document.querySelectorAll("#content-wrapper > div.cw.full > div.page-category.main-content > div.chanel.clearfix > script:nth-child(6)");
+        
+        try {
+          // @ts-ignore
+          let jsonData = ean[0].innerText
+          if (jsonData != null) {
+            //let jsonData=ean[0].innerText;
+            let splitJsonData = jsonData.split('"impressions":')[1];
+            var a = splitJsonData.match(/position/g).length; console.log(a)
+            let splitJsonData2 = splitJsonData.split('"position":' + a)[0];
+            splitJsonData2 = splitJsonData2 + '"position":' + a + '}]';
+            splitJsonData2 = JSON.parse(splitJsonData2);
+            for (var i = 0; i < splitJsonData2.length; i++) {
+              addHiddenDiv('ean', splitJsonData2[i].ean, i);
+              addHiddenDiv('id', splitJsonData2[i].simpleId, i);
+            }
+          }
+        }
+        catch (error) { }
+      }
+      else if (URL.includes('redken')) {
+
+        // @ts-ignore
+        let ean = document.querySelectorAll("#content-wrapper > div > div.page-category.main-content > b > script:nth-child(2)");
+        
+        try {
+          // @ts-ignore
+          let jsonData = ean[0].innerText
+          if (jsonData != null) {
+            //let jsonData=ean[0].innerText;
+            let splitJsonData = jsonData.split('"impressions":')[1];
+            var a = splitJsonData.match(/position/g).length; console.log(a)
+            let splitJsonData2 = splitJsonData.split('"position":' + a)[0];
+            splitJsonData2 = splitJsonData2 + '"position":' + a + '}]';
+            splitJsonData2 = JSON.parse(splitJsonData2);
+            for (var i = 0; i < splitJsonData2.length; i++) {
+              addHiddenDiv('ean', splitJsonData2[i].ean, i);
+              addHiddenDiv('id', splitJsonData2[i].simpleId, i);
+            }
+          }
+        }
+        catch (error) { }
+      }
+      else {
+        try {
+          // @ts-ignore
+          let ean = document.querySelectorAll("div.main-content > script:nth-child(6)");
           // @ts-ignore
           let jsonData = ean[0].innerText
           try {
             if (jsonData != null) {
-              //Gesichtsreiniger
               //let jsonData=ean[0].innerText;
               let splitJsonData = jsonData.split('"impressions":')[1];
               var a = splitJsonData.match(/position/g).length; console.log(a)
@@ -96,15 +188,38 @@ module.exports = {
                 addHiddenDiv('id', splitJsonData2[i].simpleId, i);
               }
             }
+          }
+          catch (error) { }
 
+        }
+        catch (error) {
+          if (error instanceof TypeError) {
+            let ean = document.querySelectorAll("#right-column > script:nth-child(4)");
+            // @ts-ignore
+            let jsonData = ean[0].innerText
+            try {
+              if (jsonData != null) {
+                //Gesichtsreiniger
+                //let jsonData=ean[0].innerText;
+                let splitJsonData = jsonData.split('"impressions":')[1];
+                var a = splitJsonData.match(/position/g).length; console.log(a)
+                let splitJsonData2 = splitJsonData.split('"position":' + a)[0];
+                splitJsonData2 = splitJsonData2 + '"position":' + a + '}]';
+                splitJsonData2 = JSON.parse(splitJsonData2);
+                for (var i = 0; i < splitJsonData2.length; i++) {
+                  addHiddenDiv('ean', splitJsonData2[i].ean, i);
+                  addHiddenDiv('id', splitJsonData2[i].simpleId, i);
+                }
+              }
+
+            }
+
+            catch (error) { }
           }
 
-          catch (error) { }
+
         }
-
-
       }
-
 
 
     });
