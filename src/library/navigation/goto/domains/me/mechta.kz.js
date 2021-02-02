48efea7e64e3bfc706c2d23d92808ba2f2a9adbb
adheer <sangeta.kumari@synerzip.com>
2021-02-02 15:36:04 +0530

    kickback issues

--- src/library/navigation/goto/domains/me/mechta.kz.js
+++ src/library/navigation/goto/domains/me/mechta.kz.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'mechta.kz',
-    timeout: 90000,
+    timeout: 70000,
     country: 'KZ',
     store: 'mechta',
     zipcode: "''",
