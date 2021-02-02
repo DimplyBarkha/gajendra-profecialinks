cc7ccca46aa2f767adf877d22718e027a47d7774
Your Name <karthik.ravi-odc@partner.import.io>
2021-02-02 21:22:59 +0530

    variant

--- src/library/product/search/stores/j/jpg/FR/paginate.js
+++ src/library/product/search/stores/j/jpg/FR/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'section[class="page-category__products-cards"] article',
-    noResultsXPath: '//header[@class="search-noresult my-3"]/h3/text()|//ol[@class="page-breadcrumb"]',
+    noResultsXPath: '//header[@class="search-noresult my-3"]/h3/text()|//ol[@class="page-breadcrumb"]//span[@class="js-dropdown-button"]',
     openSearchDefinition: {
       pageStartNb: 0,
       template: 'https://www.jpg.fr/INTERSHOP/web/WFS/RAJA-JPG-Site/fr_FR/-/EUR/ViewParametricSearch-ProductPaging?PageNumber={page}&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}',
