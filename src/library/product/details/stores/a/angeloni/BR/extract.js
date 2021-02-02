9480fd12d8708ad1aba1498f47126479dc9709df
banderovskyi <roman1004b@gmail.com>
2021-02-02 10:30:21 +0200

    Add scroll

--- src/library/product/details/stores/a/angeloni/BR/extract.js
+++ src/library/product/details/stores/a/angeloni/BR/extract.js
@@ -22,6 +22,23 @@ module.exports = {
     await context.waitForSelector('.box-produto a');
     await context.clickAndWaitForNavigation('.box-produto a', {}, {});
     await context.evaluate(async () => {
+      let scrollTop = 0;
+      while (scrollTop <= 20000) {
+        await stall(100);
+        scrollTop += 2000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 30000) {
+          await stall(1000);
+          break;
+        }
+      }
+      function stall(ms) {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
       function addElementToDocument (key, value) {
         const createdElem = document.querySelector(`#${key}`);
         if (!createdElem) {
