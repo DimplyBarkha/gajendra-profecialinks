582ca23ad0e0746fde899c3eb593873947ba69f1
adheer <sangeta.kumari@synerzip.com>
2021-02-02 18:37:24 +0530

    kickback issues

--- src/library/product/details/stores/p/plein/NL/execute.js
+++ src/library/product/details/stores/p/plein/NL/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'NL',
     store: 'plein',
     domain: 'plein.nl',
-    loadedSelector: 'div.owl-item.active img.owl-lazy',
-    noResultsXPath: '//div[@class="no-page"]//h2[text()="Sorry, we kunnen de pagina niet vinden"]',
+    //loadedSelector: 'div.owl-item.active img.owl-lazy',
+    noResultsXPath: '//div[@class="container"]/div[contains(.,"Probeer eens een merknaam of beschrijving van je product in te typen.")]',
     zipcode: '',
   },
 };
--- src/library/product/details/stores/p/plein/NL/extract.js
+++ src/library/product/details/stores/p/plein/NL/extract.js
@@ -1,22 +1,36 @@
 const { transform } = require('./format');
 
-// async function implementation (
-//   inputs,
-//   parameters,
-//   context,
-//   dependencies,
-// ) {
-//   const { transform } = parameters;
-//   const { productDetails } = dependencies;
-//   try {
-//     await context.waitForSelector('div.owl-item.active img.owl-lazy');
-//     //await context.click('div.modal-footer>a.btn-success')
-//     //await context.click('div.modal-footer > button')
-//   } catch (error) {
-//     console.log('cookie pop up not loded', error);
-//   }
-//   return await context.extract(productDetails, { transform });
-// }
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  console.log("inputs:: ", inputs);
+  const {url, id } = inputs;
+  console.log("parameters:: ", parameters);
+  // try {
+  //   await context.waitForSelector('div.owl-item.active img.owl-lazy');
+  //   // await context.click('div.modal-footer>a.btn-success')
+  //   // await context.click('div.modal-footer > button')
+  // } catch (error) {
+  //   console.log('cookie pop up not loded', error);
+  // }
+  if (id) {
+    await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    await context.waitForXPath('//div[@class="clear product-grid-view"]//a[@class="umbrella"]');
+
+    await context.waitForSelector('div.row a.umbrella');
+    console.log('everything fine !!!');
+    await context.evaluate(() => {
+      const firstItem = document.querySelector('div.row a.umbrella');
+      firstItem.click();
+    });
+  }
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -26,39 +40,39 @@ module.exports = {
     domain: 'plein.nl',
     zipcode: '',
   },
-  // implementation,
-  implementation: async (inputs,
-    parameters,
-    context,
-    dependencies,
-  ) => {
-    await context.evaluate(async () => {
-      const loadMoreEle = document.querySelector('button.btn.btn-link.product-details-toggle');
-      if (loadMoreEle) {
-        // @ts-ignore
-        loadMoreEle.click();
-      }
-
-      let scrollTop = 0;
-      while (scrollTop !== 20000) {
-        await stall(500);
-        scrollTop += 1000;
-        window.scroll(0, scrollTop);
-        if (scrollTop === 20000) {
-          await stall(5000);
-          break;
-        }
-      }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-    });
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    await context.extract(productDetails, { transform });
-  },
+  implementation,
 };
+// implementation: async (inputs,
+//   parameters,
+//   context,
+//   dependencies,
+// ) => {
+//   await context.evaluate(async () => {
+//     const loadMoreEle = document.querySelector('button.btn.btn-link.product-details-toggle');
+//     if (loadMoreEle) {
+//       // @ts-ignore
+//       loadMoreEle.click();
+//     }
+
+//     let scrollTop = 0;
+//     while (scrollTop !== 20000) {
+//       await stall(500);
+//       scrollTop += 1000;
+//       window.scroll(0, scrollTop);
+//       if (scrollTop === 20000) {
+//         await stall(5000);
+//         break;
+//       }
+//     }
+//     function stall (ms) {
+//       return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//           resolve();
+//         }, ms);
+//       });
+//     }
+//   });
+//   const { transform } = parameters;
+//   const { productDetails } = dependencies;
+//   await context.extract(productDetails, { transform });
+// },
