8dc7e7cdacaccbd1cbe07e66c5c7a790d13179b2
adheer <sangeta.kumari@synerzip.com>
2021-02-02 19:35:28 +0530

    kickback issues

--- src/library/product/details/stores/n/naszezoo/PL/execute.js
+++ src/library/product/details/stores/n/naszezoo/PL/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'naszezoo',
     domain: 'naszezoo.pl',
     loadedSelector: null,
-    noResultsXPath: '//p[.="Nie znaleziono produktów spełniających podane kryteria."]',
+    noResultsXPath: '//p[.="Nie znaleziono produktów spełniających podane kryteria."] | //p[.="Ten produkt jest niedostępny."]',
     zipcode: '',
   },
 };
