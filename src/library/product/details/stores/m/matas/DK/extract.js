d7bd9549f01af90ade748beb5507c7f6e2c69248
Raghav <user.email>
2021-02-02 23:51:49 +0530

    Updated record xpath & custom code in extract.js, removed it for price,commented single values false

--- src/library/product/details/stores/m/matas/DK/extract.js
+++ src/library/product/details/stores/m/matas/DK/extract.js
@@ -42,17 +42,26 @@ module.exports = {
         catElement.style.display = "none";
         document.body.appendChild(catElement);
       }
+      function addHiddenDiv(id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.className = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll("span[class='breadcrumb__text js-csquare-currentBreadcrumbText']")[0];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+        // document.body.appendChild(newDiv);
+        }
       // Method to replace , with . for price
-      var price = getAllXpath('//div[@class="product-page__price"]//span[@class="nowrap"]/text() | (//span[@class="nowrap"])[1]/text()', "nodeValue");
-      if (price[0] != null) {
-        var price1 = price[0].replace(",", ".");
-        addElementToDocument("price", price1);
-      }
+      // var price = getAllXpath('//div[@class="product-page__price"]//span[@class="nowrap"]/text() | (//span[@class="nowrap"])[1]/text()', "nodeValue");
+      // if (price[0] != null) {
+      //   var price1 = price[0].replace(",", ".");
+      //   addHiddenDiv("price", price1);
+      // }
       var vari = getAllXpath("(//div[@class='icon--rating icon--product-header-rating review-stars']//div[@class='review-stars__container review-stars__container--border']/@data-js-border-offset)[1]","nodeValue");
       if (vari != null) {
         for (var i = 0; i < vari.length; i++) {
           var agg = (vari[i].slice(0, -1) * 5) / 100;
-          addElementToDocument1("agg", agg, i);
+          addHiddenDiv("agg", agg);
         }
       }
     });
