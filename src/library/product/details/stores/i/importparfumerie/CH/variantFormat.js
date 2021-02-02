627c0f3cdee078470004737cf2ac20ec9e128b72
sivasoarlogic <siva@soarlogic.com>
2021-02-02 13:27:48 +0530

    QAfixs

--- src/library/product/details/stores/i/importparfumerie/CH/variantFormat.js
+++ src/library/product/details/stores/i/importparfumerie/CH/variantFormat.js
@@ -4,23 +4,15 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {    
-    for (const { group } of data) {
-        for (const row of group) {                
-            if (row.variantUrl) {
-                row.variantUrl.forEach(item => {
-                    item.text = "https://www.impo.ch/de/p/"+item.text;
-                });
-                if (row.variantUrl.length > 1) {
-                  row.variantUrl.shift();
-                }
-            }
-            if (row.variantId) {
-              if (row.variantId.length > 1) {
-                row.variantId.shift();
-              }
-            }
-        }
-    }    
-    return data;
-  };
-  module.exports = { transform };
+  for (const { group } of data) {
+      for (const row of group) {                
+          if (row.variantUrl) {
+              row.variantUrl.forEach(item => {
+                  item.text = "https://www.impo.ch/de/p/"+item.text;
+              });
+          }
+      }
+  }    
+  return data;
+};
+module.exports = { transform };
