3860c814f9b9d3b48c206ad737577613c8173631
ahmad8931 <ahmad@profecialinks.com>
2021-02-02 22:38:27 +0530

    phase 2 new schema key name updated

--- src/library/product/search/stores/c/coupang/KO/execute.js
+++ src/library/product/search/stores/c/coupang/KO/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'KO',
     store: 'coupang',
     domain: 'coupang.com',
-    url: 'https://www.coupang.com/np/search?q={searchTerms}&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=',
+    url: 'https://www.coupang.com/np/search?q={searchTerms}&channel=recent',
     loadedSelector: 'div.search-content.search-content-with-feedback  ul#productList',
     noResultsXPath: '//div[@class="search-content search-content-with-feedback search-empty-content"]//p//span[@class="message"]',
     zipcode: '',
