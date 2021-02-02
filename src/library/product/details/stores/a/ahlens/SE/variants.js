ccced26ff9c405da39415b77f57b5d6fc00f0e29
Your Name <karthik.ravi-odc@partner.import.io>
2021-02-02 16:02:10 +0530

    variant

---
+++ src/library/product/details/stores/a/ahlens/SE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'SE',
+    store: 'ahlens',
+    domain: 'ahlens.se',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/a/ahlens/SE/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'SE',
+    store: 'ahlens',
+    transform: null,
+    domain: 'ahlens.se',
+    zipcode: '',
+  },
+};
