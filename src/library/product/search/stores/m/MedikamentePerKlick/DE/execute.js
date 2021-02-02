684fd1f16e269874643dd74dd9fb0437a1b587a6
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-02-02 20:10:25 +0530

    #2658 devwhiskysearch

--- src/library/product/search/stores/m/MedikamentePerKlick/DE/execute.js
+++ src/library/product/search/stores/m/MedikamentePerKlick/DE/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'MedikamentePerKlick',
     domain: 'medikamente-per-klick.de',
     url: 'https://www.medikamente-per-klick.de/keywordsearch?sortBy=default&VIEW_SIZE=20&VIEW_INDEX=0&clearSearch=N&SEARCH_STRING={searchTerms}&SEARCH_CATEGORY_ID=&anb=',
-    loadedSelector: null,
+    loadedSelector: '#searchList > div.productsList',
     noResultsXPath: null,
   },
 };
--- src/library/product/search/stores/m/MedikamentePerKlick/DE/paginate.js
+++ src/library/product/search/stores/m/MedikamentePerKlick/DE/paginate.js
@@ -9,7 +9,7 @@ module.exports = {
     nextLinkXpath: null,
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: null,
+    loadedSelector: '#searchList > div.productsList',
     loadedXpath: null,
     noResultsXPath: null,
     stopConditionSelectorOrXpath: null,
