ad78e26ce30053d97d3a57457ab6784c69f2c83e
Prateek3018 <prateek.s@osmosys.asia>
2021-02-02 19:02:49 +0530

    firstRequest Timeout increased

--- src/library/navigation/goto/domains/se/sephora.nz.js
+++ src/library/navigation/goto/domains/se/sephora.nz.js
@@ -8,4 +8,14 @@ module.exports = {
     store: 'sephora',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.captureRequests();
+    await context.goto(url, { firstRequestTimeout: 90000, timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
