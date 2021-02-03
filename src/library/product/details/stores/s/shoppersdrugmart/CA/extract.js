b98dce1de808672ab9b83da8983e64d618e0dffb
rajeshProfecialinks <rajesh.g@profecialinks.com>
2021-02-03 20:45:23 +0530

    variant data fix

--- src/library/product/details/stores/s/shoppersdrugmart/CA/extract.js
+++ src/library/product/details/stores/s/shoppersdrugmart/CA/extract.js
@@ -8,4 +8,31 @@ module.exports = {
     domain: 'shoppersdrugmart.ca',
     zipcode: '',
   },
+  implementation: async (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const variantCount = await context.evaluate(async function () {
+      return document.querySelectorAll('div.swatches-group > ul > li > a').length;
+    });
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    await context.extract(productDetails, { transform:transform });
+    for (let index = 2; index <= variantCount; index++) {
+      try {
+        await context.click(`div.swatches-group > ul > li:nth-child(${index})`);
+        await new Promise(resolve => setTimeout(resolve, 500));
+        if (variantCount !== index) {
+          await context.evaluate(() => document.querySelectorAll('div[data-wps-popup-close-intent]').forEach(elm => elm.click()));
+          await context.extract(productDetails, { type: 'APPEND', transform });
+        } else {
+          return await context.extract(productDetails, { type: 'APPEND', transform });
+        }
+      } catch (error) {
+        console.log('Error While itrerating over the variants',error);
+      }
+    }
+  },
 };
