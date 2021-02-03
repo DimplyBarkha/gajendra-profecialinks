a4ab9c284dceecc15b503768acac20b580142e73
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-03 16:13:47 +0530

    updated code

--- src/library/product/details/stores/g/gracobaby/US/extract.js
+++ src/library/product/details/stores/g/gracobaby/US/extract.js
@@ -9,6 +9,29 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    const locationUrl = await context.evaluate(async () => {
+      return window.location.href;
+    });
+    await context.evaluate(async function () {
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+
+      const sku = getAllXpath('//script[@type="application/ld+json" and contains(text(),"sku")]/text()', 'nodeValue');
+      const finalSKU = JSON.parse(sku[0]);
+      let Finalsku = finalSKU.sku.replace('SAP_', '');
+      let Frameurl = "https://content.syndigo.com/page/006eb996-6362-42ff-99b0-389a3502c5cc/{1}.json?u=90B1B9E8-46FB-4520-BCEB-1428D0F74052&prtnid=006eb996-6362-42ff-99b0-389a3502c5cc&siteid=006eb996-6362-42ff-99b0-389a3502c5cc&pageid=1988647&s=1612341006904&v=v1.0.122&visitid=2C26B62D-FCAC-4700-9D1A-9CF983D94051&ref=&r=0.6235173545457959&pageurl={2}"
+      let FinalFrameUrl = Frameurl
+        .replace('{1}', Finalsku)
+        .replace('{2}', locationUrl);
+      context.goto(FinalFrameUrl)
+    });
     await context.evaluate(async function () {
       // Java Script Code for adding new Div
       function addHiddenDiv(id, content) {
@@ -166,7 +189,7 @@ module.exports = {
         // @ts-ignore
         let finalImages = [...new Set(alternateImages)];
         for (let i = 0; i < finalImages.length; i++) {
-          addHiddenDiv('alternateImages', finalImages[i].replace(/\?wid=180&hei=180/g,''));
+          addHiddenDiv('alternateImages', finalImages[i].replace(/\?wid=180&hei=180/g, ''));
         }
 
       } catch (error) {
