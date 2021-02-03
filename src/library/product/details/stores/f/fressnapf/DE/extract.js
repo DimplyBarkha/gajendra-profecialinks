a79c542d156b55ca8d9798e1de7e2a0e52a85de9
sobha rani <amol.thukrul-od@partner.import.io>
2021-02-03 19:24:22 +0530

    fressnapf complete

--- src/library/product/details/stores/f/fressnapf/DE/extract.js
+++ src/library/product/details/stores/f/fressnapf/DE/extract.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      function addHiddenDiv (id, content, index) {
+      function addHiddenDiv(id, content, index) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
         newDiv.textContent = content;
@@ -18,12 +18,20 @@ module.exports = {
         const originalDiv = document.querySelectorAll("div[class='section-content']>div[class='product-overview']>div>div>div>div>div>div>div>div>div")[0];
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
-      function addHiddenDiv1 (id, content, index) {
+      // function addHiddenDiv1 (id, content, index) {
+      //   const newDiv = document.createElement('div');
+      //   newDiv.id = id;
+      //   newDiv.textContent = content;
+      //   newDiv.style.display = 'none';
+      //   const originalDiv = document.querySelectorAll("div[class='pvsil-title']")[0];
+      //   originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      // }
+      function addHiddenDiv2(id, content, index) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
         newDiv.textContent = content;
         newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll("div[class='pvsil-title']")[0];
+        const originalDiv = document.querySelectorAll('body[class="nuxt-ready"]>script:first-of-type')[0];
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
       const getAllXpath = (xpath, prop) => {
@@ -48,12 +56,43 @@ module.exports = {
       } catch (error) {
 
       }
+      // try {
+      //   var variant = getAllXpath("(//div[@class='product-variant-selector'])[2]/div/label/span/div/div[@class='pvsil-title']/text()", 'nodeValue');
+      //   if (variant != null && variant.length > 0) {
+      //     var ab = variant.join(' | ');
+      //     addHiddenDiv1('variant', ab, 0);
+      //   }
+      // } catch (error) {
+
+      // }
       try {
-        var variant = getAllXpath("(//div[@class='product-variant-selector'])[2]/div/label/span/div/div[@class='pvsil-title']/text()", 'nodeValue');
-        if (variant != null && variant.length > 0) {
-          var ab = variant.join(' | ');
-          addHiddenDiv1('variant', ab, 0);
+        // @ts-ignore
+        var firstVariant = window.__NUXT__.data[0].product.baseOptions[0].options[0].code;
+        addHiddenDiv2('firstVariant', firstVariant, 0);
+      } catch (error) {
+
+      }
+      try {
+        // @ts-ignore
+        var length = window.__NUXT__.data[0].product.baseOptions[0].options.length;
+        var variantCount = length - 1;
+        addHiddenDiv2('variantCount', variantCount, 0);
+        var variant = '';
+        for (var j = 0; j < length; j++) {
+          // @ts-ignore
+          variant = variant + window.__NUXT__.data[0].product.baseOptions[0].options[j].code;
+          if (j + 1 < length) {
+            variant = variant + ' || ';
+          }
         }
+        addHiddenDiv2('variant', variant, 0);
+      } catch (error) {
+
+      }
+      try {
+        // @ts-ignore
+        var upcgtin = window.__NUXT__.data[0].product.ean;
+        addHiddenDiv2('upcgtin', upcgtin, 0);
       } catch (error) {
 
       }
