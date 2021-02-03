bce7c96dedec4d464df41ce1cc64c185571c82ea
MADHAN KUMAR SELVARAJ <madhan.selvaraj-odc@partner.import.io>
2021-02-03 16:36:02 +0530

    #2678 Update in extractor.yaml file

--- src/library/product/search/stores/r/rs-online/UK/execute.js
+++ src/library/product/search/stores/r/rs-online/UK/execute.js
@@ -6,7 +6,6 @@ module.exports = {
     store: 'rs-online',
     domain: 'uk.rs-online.com',
     url: 'https://uk.rs-online.com/web/c/?sra=oss&r=t&searchTerm={searchTerms}',
-    // url: 'https://uk.rs-online.com/web/c/?sra=oss&r=t&searchTerm=Air%20freshener',
     loadedSelector: 'body',
     noResultsXPath: null,
     zipcode: '',
