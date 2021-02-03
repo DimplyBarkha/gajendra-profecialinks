51f40c728948fd2e4ddfcee6ff27b4524063641b
sivasoarlogic <siva@soarlogic.com>
2021-02-03 16:14:08 +0530

    QAfixs

--- src/library/product/details/stores/t/thebay/CA/extract.js
+++ src/library/product/details/stores/t/thebay/CA/extract.js
@@ -15,19 +15,19 @@ module.exports = {
     dependencies,
   ) => {
     const variantCount = await context.evaluate(async function () {
-      return document.querySelectorAll('ul.selection-list.ps.ps--active-y > li > div.item').length;
+      return document.querySelectorAll('ul.selection-list.ps > li').length;
     });
     const { transform } = parameters;
     const { productDetails } = dependencies;
-    await context.extract(productDetails, { transform:transform[0] });
+    await context.extract(productDetails, { transform });
     for (let index = 2; index <= variantCount; index++) {
       try {
-        await context.click(`'ul.selection-list.ps.ps--active-y > li:nth-child(${index})`);
+        await context.click(`ul.selection-list.ps > li:nth-child(${index})`);
         await new Promise(resolve => setTimeout(resolve, 500));
         if (variantCount !== index) {
-          await context.extract(productDetails, { type: 'APPEND', transform:transform[0]  });
+          await context.extract(productDetails, { type: 'APPEND', transform  });
         } else {
-          return await context.extract(productDetails, { type: 'APPEND', transform:transform[1]  });
+          return await context.extract(productDetails, { type: 'APPEND', transform  });
         }
       } catch (error) {
         console.log('Error While itrerating over the variants',error);
