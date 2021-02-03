f1663d7fb97ed2b6a84e435038c10db342820463
Harshkha <harsh.khare-od@partner.import.io>
2021-02-03 21:38:51 +0530

    Core updated

--- src/library/product/details/stores/o/otto_de/DE/extract.js
+++ src/library/product/details/stores/o/otto_de/DE/extract.js
@@ -34,33 +34,58 @@ module.exports = {
         return result && result.trim ? result.trim() : result;
       };
       // @ts-ignore
-      const rpc=getXpath("//li[@class='p_selected ']/@data-variationid/text()", 'nodeValue');
-      if(rpc!=null){
-        var rpcid=rpc
+      const URL = window.location.href
+      try {
+        var rpc = URL.split('/#variationId=')[1]
         addElementToDocument('rpcid', rpc)
       }
-      else{
-        const rpc2=getXpath("//div[@data-trigger='productDetailView']/@data-variationid/text()", 'nodeValue');
-        var rpcid=rpc2
-        addElementToDocument('rpcid', rpc2)
+      catch (error) {
+
       }
-      
+      // const rpc=getXpath("//li[@class='p_selected ']/@data-variationid/text()", 'nodeValue');
+      // if(rpc!=null){
+      //   var rpcid=rpc
+      //   addElementToDocument('rpcid', rpc)
+      // }
+      // else{
+      //   const rpc2=getXpath("//div[@data-trigger='productDetailView']/@data-variationid/text()", 'nodeValue');
+      //   var rpcid=rpc2
+      //   addElementToDocument('rpcid', rpc2)
+      // }
+
       const rawdata = getXpath("//div[@class='prd_price__main']/span/span[@id='reducedPriceAmount']/@content|//div[@class='prd_price__main']/span/span[@id='normalPriceAmount']/@content", 'nodeValue');
       if (rawdata != null) {
         var nr = rawdata//.replace('.', ',')
         var price = '€' + nr
         addElementToDocument('price', price);
       }
-      var variant = getAllXpath("//div[@class='reco_cinema reco_productlineCinema']/div[@class='reco_cinema__container']/ul/li/@data-variation-id", 'nodeValue');
-      if (variant != null) {
-        var ab = variant.join(' | ');
-        addElementToDocument('variant', ab);
-      }
-  //     // const perunit = getXpath("//span[@class='prd_price__normAmount']/text()", 'nodeValue');
-  //     // if (perunit != null) {
-  //     //   var npu = perunit.replace(',', '.')
-  //     //   addElementToDocument('perunit', npu);
-  //     // }
+      // @ts-ignore
+      const aa = document.querySelector('section[class="prd_section"] div[class*=\'prd_section\']').innerText
+      if(aa!=null){
+      // @ts-ignore
+      var desc1=aa.replace('Artikelbeschreibung','')
+        addElementToDocument('desc1', desc1);
+      } 
+      const rawjson = getXpath("//script[@id='productDataJson']/text()", 'nodeValue');
+      
+      var jsondata = JSON.parse(rawjson);
+      var sku=jsondata.id;
+      var a = jsondata.sortedVariationIds;
+      
+      var variants = a.join(' | ');
+      addElementToDocument('sku', sku);
+      addElementToDocument('variant', variants);
+      addElementToDocument('cts', a.length);
+      // var variant = getAllXpath("//div[@class='reco_cinema reco_productlineCinema']/div[@class='reco_cinema__container']/ul/li/@data-variation-id", 'nodeValue');
+      // if (variant != null) {
+      //   var ab = variant.join(' | ');
+      //   addElementToDocument('variant', ab);
+      // }
+      //     // const perunit = getXpath("//span[@class='prd_price__normAmount']/text()", 'nodeValue');
+      //     // if (perunit != null) {
+      //     //   var npu = perunit.replace(',', '.')
+      //     //   addElementToDocument('perunit', npu);
+      //     // }
       const weight = getXpath("(//*[contains(text(),'Gewicht')]//parent::span//parent::td//parent::tr//td[2])[1]/text()", 'nodeValue');
       if (weight != null) {
         try {
