539306de090ef56a0586223363a2db5fa44967d4
Your Name <karthik.ravi-odc@partner.import.io>
2021-02-04 23:13:23 +0530

    rpc

--- src/library/product/details/stores/j/jpg/FR/createUrl.js
+++ src/library/product/details/stores/j/jpg/FR/createUrl.js
@@ -4,7 +4,7 @@ module.exports = {
   parameterValues: {
     domain: 'jpg.fr',
     prefix: null,
-    url: null,
+    url: 'https://www.jpg.fr/INTERSHOP/web/WFS/RAJA-JPG-Site/fr_FR/-/EUR/ViewParametricSearch-ProductPaging?PageNumber=0&PageSize=20&SortingAttribute=&ViewType=1&SearchTerm={id}',
     country: 'FR',
     store: 'jpg',
     zipcode: '',
