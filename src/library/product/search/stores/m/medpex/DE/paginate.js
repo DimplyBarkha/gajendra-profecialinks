6ff481d61e4d118feb00cc235851d53e94359b47
venkatsn <venkat.sn-odc@partner.import.io>
2021-02-04 18:25:51 +0530

    #2639

--- src/library/product/search/stores/m/medpex/DE/paginate.js
+++ src/library/product/search/stores/m/medpex/DE/paginate.js
@@ -8,7 +8,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     //noResultsXPath: '//div[@id="messageContainer"]/table[@class="error"]',
-    //loadedSelector: 'div[class="product-list-entry data-tracking-product"]',
+    loadedSelector: 'div[class="product-list-entry data-tracking-product"]',
     // openSearchDefinition: {
     //  template: 'https://www.medpex.de/search.do?q={searchTerms}&pn={page}',
     // },
