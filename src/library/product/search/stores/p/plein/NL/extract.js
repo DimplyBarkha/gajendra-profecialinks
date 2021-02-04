dec4a4ad672df46c11f54648acf1f50470210b4b
adheer <sangeta.kumari@synerzip.com>
2021-02-05 02:51:14 +0530

    kickback issues

--- src/library/product/search/stores/p/plein/NL/extract.js
+++ src/library/product/search/stores/p/plein/NL/extract.js
@@ -1,48 +1,32 @@
 const { transform } = require('./shared');
-/**
- *
- * @param { { keywords: string, zipcode: string } } inputs
- * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { goto: ImportIO.Action} } dependencies
- */
 async function implementation (
   inputs,
   parameters,
   context,
   dependencies,
 ) {
-  console.log('params', parameters);
-  const applyScroll = async function (context) {
-    await context.evaluate(async function () {
-      let scrollTop = 0;
-      while (scrollTop !== 20000) {
-        await stall(500);
-        scrollTop += 1000;
-        window.scroll(0, scrollTop);
-        if (scrollTop === 20000) {
-          await stall(5000);
-          break;
-        }
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    let scrollTop = 0;
+    while (scrollTop <= 20000) {
+      await stall(500);
+      scrollTop += 1000;
+      window.scroll(0, scrollTop);
+      if (scrollTop === 20000) {
+        await stall(8000);
+        break;
       }
-      function stall (ms) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve();
-          }, ms);
-        });
-      }
-    });
-  };
-  await applyScroll(context);
-
-  // return await context.evaluate(function (xp) {
-  //   const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-  //   console.log(xp, r);
-  //   const e = r.iterateNext();
-  //   console.log(e);
-  //   return !e;
-  // }, parameters.noResultsXPath);
+    }
+    function stall (ms) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+  });
+  return await context.extract(productDetails, { transform });
 }
 module.exports = {
   implements: 'product/search/extract',
--- src/library/product/search/stores/p/plein/NL/paginate.js
+++ src/library/product/search/stores/p/plein/NL/paginate.js
@@ -5,12 +5,13 @@ module.exports = {
     country: 'NL',
     store: 'plein',
     mutationSelector: null,
+    nextLinkSelector: 'ul.pagination>li:nth-last-child(2)>a',
     spinnerSelector: null,
     loadedSelector: 'div[class*="product-view-container"]',
     noResultsXPath: '//div[contains(text(),"Probeer eens een merknaam of beschrijving van je product in te typen.")]',
-    openSearchDefinition: {
-      template: 'https://www.plein.nl/zoeken?search={searchTerms}&p={page}',
-    },
+    // openSearchDefinition: {
+    //   template: 'https://www.plein.nl/zoeken?search={searchTerms}&p={page}',
+    // },
     domain: 'plein.nl',
     zipcode: "''",
   },
