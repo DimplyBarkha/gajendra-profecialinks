13b15821be5685a5df783b3d072e2cffa2709b51
adheer <sangeta.kumari@synerzip.com>
2021-02-04 19:22:16 +0530

    kickback issues

--- src/library/product/details/stores/p/plein/NL/extract.js
+++ src/library/product/details/stores/p/plein/NL/extract.js
@@ -29,6 +29,72 @@ async function implementation (
       firstItem.click();
     });
   }
+  async function fetchManufacturerContentIframe (url) {
+    await context.goto(url);
+    return await context.evaluate(async function () {
+      const manufacturerDescription = document.body.innerText;
+      const manufacturerImagesList = document.querySelectorAll('div.box img');
+      const manufacturerImageArray = [];
+      let gtin = '';
+      try {
+        const gtinPath = '//strong[contains(text(),"EAN:")]/../following-sibling::td[1]';
+        const gtinText = document.evaluate(gtinPath, document, null, XPathResult.STRING_TYPE, null).stringValue;
+        if (gtinText) gtin = gtinText;
+      } catch (err) { }
+      for (let i = 0; i < manufacturerImagesList.length; i++) {
+        const imgUrl = manufacturerImagesList[i].getAttribute('src');
+        imgUrl && manufacturerImageArray.push(imgUrl);
+      }
+      return { manufacturerDescription, gtin, manufacturerImageArray };
+    });
+  }
+  async function checkmanufacturerContent () {
+    return await context.evaluate(async function () {
+      const manufacturerIFrameSelector = document.evaluate('//div[@class="owl-custom-theme"]/iframe', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      const manufacturerIFrameSrc = manufacturerIFrameSelector ? manufacturerIFrameSelector.src : '';
+      if (manufacturerIFrameSrc) {
+        return manufacturerIFrameSrc;
+      } else {
+        return false;
+      }
+    });
+  }
+
+  // Function to add manufacturer content and description to DOM
+  async function addContentToDOM (manContentObj, manufacturerContentLink) {
+    await context.evaluate(async function ([manContentObj, manufacturerContentLink]) {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        document.body.appendChild(newDiv);
+      }
+      if (manufacturerContentLink) {
+        // Adding manufacturer images to DOM
+        for (let i = 0; i < manContentObj.manufacturerImageArray.length; i++) {
+          addHiddenDiv('added-manufacturer-images-' + i, manContentObj.manufacturerImageArray[i]);
+        }
+        addHiddenDiv('gtin', manContentObj.gtin);
+
+        addHiddenDiv('added-manufacturer-description', manContentObj.manufacturerDescription);
+      }
+    }, [manContentObj, manufacturerContentLink]);
+  }
+  const pageUrl = await context.evaluate(async () => {
+    return window.location.href;
+  });
+
+  console.log('pageUrl ==', pageUrl);
+  const manufacturerContentLink = await checkmanufacturerContent();
+  let manContentObj;
+  if (manufacturerContentLink) {
+    manContentObj = await fetchManufacturerContentIframe(manufacturerContentLink);
+    await context.goto(pageUrl);
+  }
+  await addContentToDOM(manContentObj, manufacturerContentLink);
+
+  await new Promise((resolve) => setTimeout(resolve, 5000))
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
