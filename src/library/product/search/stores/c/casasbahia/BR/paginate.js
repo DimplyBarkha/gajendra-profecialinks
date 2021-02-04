40055b99a93badb98648547ca160bc723a92ffbd
adheer <sangeta.kumari@synerzip.com>
2021-02-05 00:22:52 +0530

    kickback issues

--- src/library/product/search/stores/c/casasbahia/BR/paginate.js
+++ src/library/product/search/stores/c/casasbahia/BR/paginate.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'casasbahia',
     // nextLinkSelector: 'div[class*="LoadMore__Wrapper"] button',
     // mutationSelector: 'div[class*="Loader__LoaderIcon"]',
-    spinnerSelector: 'div[class*="Loader__LoaderIcon"]',
+    // spinnerSelector: 'div[class*="Loader__LoaderIcon"]',
     loadedSelector: 'ul[class*="ProductsGrid__ProductsGridWrapper"] li',
     noResultsXPath: '//h2[contains(@class,"NoResultsMessage")]',
     openSearchDefinition: null,
