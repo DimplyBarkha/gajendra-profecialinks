19c493d88ed5fa480b1547277064b3f3edbc5c6d
ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>
2021-02-04 21:40:21 +0530

    Updated execute.js to handle both rpc and URL

--- src/library/product/details/stores/b/bijenkorf/NL/execute.js
+++ src/library/product/details/stores/b/bijenkorf/NL/execute.js
@@ -15,40 +15,49 @@ module.exports = {
     context,
     dependencies,
   ) {
-    const searchUrl = `https://ceres-catalog.debijenkorf.nl/catalog/product/show?productVariantCode=${inputs.id}&cached=false&locale=nl_NL&api-version=2.34`;
-    try {
-      await context.goto(searchUrl, {
-        timeout: 30000,
-        waitUntil: 'load',
-        checkBlocked: true,
-        js_enabled: true,
-        css_enabled: false,
-        random_move_mouse: true,
-      });
-    } catch (e) {
-      console.log(e);
-    }
+    let productDetailsLink = null;
 
-    const productDetailsLink = await context.evaluate(function () {
-      let url = null;
-      let htmlContent = document.querySelector('pre').textContent;
-      if (htmlContent) {
-        htmlContent = JSON.parse(htmlContent);
-        if (htmlContent.data && htmlContent.data.product && htmlContent.data.product.currentVariantProduct) {
-          console.log(htmlContent.data.product.currentVariantProduct.url);
-          url = htmlContent.data.product.currentVariantProduct.url;
-          return url;
-        } else if (htmlContent.data && htmlContent.data.product) {
-          console.log(htmlContent.data && htmlContent.data.product.url);
-          url = htmlContent.data && htmlContent.data.product.url;
+    async function navigateWithProductId () {
+      if (inputs.id) {
+        const searchUrl = `https://ceres-catalog.debijenkorf.nl/catalog/product/show?productVariantCode=${inputs.id}&cached=false&locale=nl_NL&api-version=2.34`;
+        try {
+          await context.goto(searchUrl, {
+            timeout: 30000,
+            waitUntil: 'load',
+            checkBlocked: true,
+            js_enabled: true,
+            css_enabled: false,
+            random_move_mouse: true,
+          });
+        } catch (e) {
+          console.log(e);
         }
+        productDetailsLink = await context.evaluate(function () {
+          let url = null;
+          let htmlContent = document.querySelector('pre').textContent;
+          if (htmlContent) {
+            htmlContent = JSON.parse(htmlContent);
+            if (htmlContent.data && htmlContent.data.product && htmlContent.data.product.currentVariantProduct) {
+              console.log(htmlContent.data.product.currentVariantProduct.url);
+              url = htmlContent.data.product.currentVariantProduct.url;
+              return url;
+            } else if (htmlContent.data && htmlContent.data.product) {
+              console.log(htmlContent.data && htmlContent.data.product.url);
+              url = htmlContent.data && htmlContent.data.product.url;
+            }
+          }
+          return `https:${url}`;
+        });
+      } else if (inputs.url) {
+        productDetailsLink = inputs.url;
       }
-      return url;
-    });
+    }
+    await navigateWithProductId();
+
     console.log("product url", productDetailsLink);
     if (productDetailsLink) {
-      const url = `https:${productDetailsLink}`;
-      const finalUrl = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+      // const url = `https:${productDetailsLink}`;
+      const finalUrl = `${productDetailsLink}`;
       await context.setBlockAds(false);
       await context.setLoadAllResources(true);
       await context.setLoadImages(true);
