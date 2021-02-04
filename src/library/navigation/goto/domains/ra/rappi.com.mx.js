366988466dcc8c586576aa0caf8b238951e7adca
unknown <ramraut25@gmail.com>
2021-02-05 04:47:46 +0530

    Added proxy for search and details

--- src/library/navigation/goto/domains/ra/rappi.com.mx.js
+++ src/library/navigation/goto/domains/ra/rappi.com.mx.js
@@ -8,4 +8,19 @@ module.exports = {
     // store: 'rappi',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 30000;
+    
+    await context.setCssEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setJavaScriptEnabled(true);
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode });
+    }
+  },
 };
