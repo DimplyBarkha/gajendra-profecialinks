4207b6ade4322873974049eb215a453f596fbc1f
banderovskyi <roman1004b@gmail.com>
2021-02-04 15:22:23 +0200

    Change pageProducts

--- src/library/product/search/stores/m/mondoffice/IT/execute.js
+++ src/library/product/search/stores/m/mondoffice/IT/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'mondoffice',
     domain: 'mondoffice.com',
     // url: 'https://www.mondoffice.com/INTERSHOP/web/WFS/RAJA-MONDOFFICE-Site/it_IT/-/EUR/ViewParametricSearch-ProductPaging?PageSize=9999&SortingAttribute=&ViewType=1&SearchTerm={searchTerms}',
-    url: 'https://www.mondoffice.com/INTERSHOP/web/WFS/RAJA-MONDOFFICE-Site/it_IT/-/EUR/ViewParametricSearch-SimpleOfferSearch?SearchTerm={searchTerms}&SearchCat=natural',
+    url: 'https://www.mondoffice.com/INTERSHOP/web/WFS/RAJA-MONDOFFICE-Site/it_IT/-/EUR/ViewParametricSearch-ProductPaging?PageSize=150&SearchTerm={searchTerms}&SearchCat=natural',
     loadedSelector: 'div#toolbar__all_anchor',
     noResultsXPath: "//ul[@class='search-noresult__list']/li",
     zipcode: '',
