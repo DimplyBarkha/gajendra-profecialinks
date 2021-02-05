b80de784571c3481e6b332d9173eca5097bf091d
Abhishek Kumar Mishra <abhishekkumar_mishra@hcl.com>
2021-02-05 18:35:44 +0530

    solved pagination issue

--- src/library/product/search/stores/a/amazonLg/IN/paginate.js
+++ src/library/product/search/stores/a/amazonLg/IN/paginate.js
@@ -1,38 +1,19 @@
-
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'IN',
     store: 'amazonLg',
-    nextLinkSelector: null,
+    domain: 'amazon.in',
+    nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
     nextLinkXpath: null,
     mutationSelector: null,
-    spinnerSelector: null,
-    loadedSelector: null,
-    loadedXpath: null,
-    noResultsXPath: null,
-    stopConditionSelectorOrXpath: null,
-    resultsDivSelector: null,
-    openSearchDefinition: null,
-    domain: 'amazon.in',
-    zipcode: '',
-  },
+    spinnerSelector: 'div.s-result-list-placeholder:not(.aok-hidden)',
+    loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+    openSearchDefinition: {
+      template: 'https://www.amazon.in/s?k={searchTerms}&rh=p_89%3ALG&dc&qid=1602841162&rnid=2528832011&ref=sr_nr_p_89_3&page={page}',
+      },
+      noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
+    },
 };
 
-// module.exports = {
-//   implements: 'product/search/paginate',
-//   parameterValues: {
-//     country: 'IN',
-//     store: 'amazonLg',
-//     domain: 'amazon.in',
-//     nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
-//     spinnerSelector: 'div.s-result-list-placeholder:not(.aok-hidden)',
-//     // Use openSearchDefinition if nextLink has navigation issues.
-//     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
-//     openSearchDefinition: {
-//       template: 'https://amazon.in/s?k={searchTerms}&rh=p_89%3ALG&dc&qid=1602841162&rnid=2528832011&ref=sr_nr_p_89_3&page={page}',
-//     },
-//     noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
-//   },
-// };
 
