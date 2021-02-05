d69563da8e16249684400d0b1d3f0fe1a21e4fc1
venkatsn <venkat.sn-odc@partner.import.io>
2021-02-05 10:42:26 +0530

    #2639

--- src/library/product/search/stores/m/medpex/DE/execute.js
+++ src/library/product/search/stores/m/medpex/DE/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'medpex',
     domain: 'medpex.de',
     url: 'https://www.medpex.de/search.do?q={searchTerms}&pn=1',
-    loadedSelector: 'div#product-list  div.product-list-entry.data-tracking-product',
+    loadedSelector: 'div.product-list-entry.data-tracking-product',
     noResultsXPath: '//div[@id="messageContainer"]/table[@class="error"]',
   },
 };
--- src/library/product/search/stores/m/medpex/DE/paginate.js
+++ src/library/product/search/stores/m/medpex/DE/paginate.js
@@ -3,12 +3,12 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'medpex',
-    nextLinkSelector: '#searchresult > div.pagenav > table > tbody > tr > td:nth-last-child(1)>a ',
+    nextLinkSelector: '#searchresult > div.pagenav > table > tbody > tr > td:nth-last-child(2)>a',
     //nextLinkXpath: '//div[@class="pagenav"]//tr//td//a[contains(text(),">")]',
     mutationSelector: null,
     spinnerSelector: null,
     //noResultsXPath: '//div[@id="messageContainer"]/table[@class="error"]',
-    loadedSelector: 'div[class="product-list-entry data-tracking-product"]',
+    loadedSelector: 'div.product-list-entry.data-tracking-product',
     // openSearchDefinition: {
     //  template: 'https://www.medpex.de/search.do?q={searchTerms}&pn={page}',
     // },
