03ca097cd0b4fb9dbe1e1019f60d3f47be64163c
Navjyot Gill <navjyotg@synerzipune.local>
2021-02-05 12:56:33 +0530

    issues fixed

--- src/library/product/details/stores/a/apoteket/shared.js
+++ src/library/product/details/stores/a/apoteket/shared.js
@@ -4,6 +4,19 @@
 * @returns {ImportIO.Group[]}
 */
 const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
+  
     for (const { group } of data) {
       for (const row of group) {
         if (row.image) {
@@ -32,9 +45,17 @@ const transform = (data, context) => {
               item.text = item.text.replace(/\//g,'');
             })
           }
+          if(row.brandText&&row.brandText.length>1){
+
+            row.brandText=[{text:row.brandText[0].text}]
+          }
           if (row.description) {
             row.description.forEach(item => {
               item.text = item.text.trim();
+              if(item.text[0]==='|' &&item.text[1]==='|'){
+                item.text=item.text.substring(2,item.text.length-1)
+                item.text = item.text.trim();
+              }
             })
           }
           if (row.name) {
@@ -128,6 +149,9 @@ const transform = (data, context) => {
           }
       }
     }
+    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
+      el.text = clean(el.text);
+    }))));
     return data;
   };
   
