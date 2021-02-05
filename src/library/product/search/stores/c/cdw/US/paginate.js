626c1a3b029a4d6223238a8542478a3e197bb93c
adheer <sangeta.kumari@synerzip.com>
2021-02-05 18:39:25 +0530

    kickback issues

--- src/library/product/search/stores/c/cdw/US/paginate.js
+++ src/library/product/search/stores/c/cdw/US/paginate.js
@@ -4,13 +4,15 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'cdw',
-    nextLinkSelector: 'a[aria-label*="Go to next page 2"]',
+    // nextLinkSelector: 'a[aria-label*="Go to next page 2"]',
     //nextLinkSelector: 'div.search-pagination-list-container>a[aria-label="Next Page"]',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div.search-results',
     noResultsXPath: '//div[@class="no-results-error-message"]',
-    openSearchDefinition: null,
+    openSearchDefinition: {
+      template: 'https://www.cdw.com/search/?key={searchTerms}&pcurrent={page}',
+    },
     domain: 'cdw.com',
     zipcode: '',
   },
