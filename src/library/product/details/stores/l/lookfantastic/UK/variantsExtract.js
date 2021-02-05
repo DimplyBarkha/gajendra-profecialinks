c9c36850412af321a4c39c1d8d2aa170ed7ec340
venkatsn <venkat.sn-odc@partner.import.io>
2021-02-05 14:56:38 +0530

    #2636

--- src/library/product/details/stores/l/lookfantastic/UK/variantsExtract.js
+++ src/library/product/details/stores/l/lookfantastic/UK/variantsExtract.js
@@ -1,14 +1,23 @@
-const { cleanUp } = require('../../../../shared');
+
 module.exports = {
   implements: 'product/details/variants/variantsExtract',
   parameterValues: {
     country: 'UK',
     store: 'lookfantastic',
-    transform: cleanUp,
+    transform: null,
     domain: 'lookfantastic.com',
     zipcode: '',
   },
-  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+  implementation,
+};
+  async function implementation(
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    const { transform } = parameters;
+    const { variants } = dependencies;
     await context.evaluate(async function () {
       function addHiddenDiv(id, content, index) {
         const newDiv = document.createElement('div');
@@ -20,19 +29,30 @@ module.exports = {
       }
       try {
         var len = document.querySelectorAll('ul[class="productVariations_colorList"] li').length
+        var ids=[];
+          var urls=[];
         for (let i = 0; i < len; i++) {
           // @ts-ignore
           var id = document.querySelectorAll('ul[class="productVariations_colorList"] li button')[i].click()
           await new Promise(r => setTimeout(r, 3000));
           let variantId1 = document.querySelector('div.productVariations div').getAttribute('data-child-id')
-          console.log("hai", variantId1, i);
-          addHiddenDiv("variantId", variantId1, i)
+          //addHiddenDiv("variantId", variantId1, i)
+          ids.push(variantId1);
+          let url=document.querySelector('link[rel="canonical"]').getAttribute('href');
+          urls.push(url+"?variation="+variantId1) 
         }
+        for(let i=0;i<ids.length;i++)
+        {
+          addHiddenDiv("variantId", ids[i], i);
+          addHiddenDiv("variantUrl",urls[i],i)
+        }
+        //console.log(ids);
+        //console.log(urls);
       } catch (error) {
 
       }
     });
-    await context.extract(productDetails);
-  },
-};
+    return await context.extract(variants, { transform });
+  }
+
 
