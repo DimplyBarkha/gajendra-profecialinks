594efdc1f2fa3e54729f8c5605851775c71c393b
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-05 18:26:55 +0530

    updated paginate

--- src/library/product/search/stores/g/gracobaby/US/execute.js
+++ src/library/product/search/stores/g/gracobaby/US/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'US',
     store: 'gracobaby',
     domain: 'gracobaby.com',
-    url: 'https://www.gracobaby.com/search?q={searchTerms}&start=0&sz=1000&view=product',
+    url: 'https://www.gracobaby.com/search?q={searchTerms}&start=0&sz=24&view=product',
     loadedSelector: '#maincontent',
     noResultsXPath: null,
     zipcode: '',
