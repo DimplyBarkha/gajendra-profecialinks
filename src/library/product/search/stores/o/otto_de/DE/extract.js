5c1c6ea7fafc3a3bfd9d8ba880823d7a0663f18d
Harshkha <harsh.khare-od@partner.import.io>
2021-02-05 12:47:55 +0530

    Search updated

--- src/library/product/search/stores/o/otto_de/DE/extract.js
+++ src/library/product/search/stores/o/otto_de/DE/extract.js
@@ -14,11 +14,11 @@ module.exports = {
     const { productDetails } = dependencies;
     await context.evaluate(async () => {
       let scrollTop = 0;
-      while (scrollTop !== 20000) {
+      while (scrollTop !== 1000) {
         await stall(1000);
-        scrollTop += 2000;
+        scrollTop += 1700;
         window.scroll(0, scrollTop);
-        if (scrollTop === 30000) {
+        if (scrollTop === 1700) {
          await stall(2000);
           break;
         }
@@ -56,7 +56,8 @@ module.exports = {
         newDiv.id = id;
         newDiv.textContent = content;
         newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll('#san_resultSection > article')[index];
+        //const originalDiv = document.querySelectorAll('#san_resultSection > article')[index];
+        const originalDiv = document.querySelectorAll("script[type='application/ld+json']")[index];
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
       const getAllXpath = (xpath, prop) => {
@@ -69,34 +70,47 @@ module.exports = {
         return result;
       };
 
-      // var aa = document.querySelectorAll("script[type='application/ld+json']");
-      // if (aa != null) {
-      //   for (var i = 0; i < aa.length; i++) {
-      //     // @ts-ignore
-      //     var rawjson = JSON.parse(aa[i].innerText)
-      //     //var newname = rawjson.name
-      //     addHiddenDiv('newname', rawjson.name, i)
-      //     addHiddenDiv('price', rawjson.offers[0].price, i)
-      //     //addHiddenDiv('newname', rawjson.name, i)
-      //     //addHiddenDiv('newname', rawjson.name, i)
+      var aa = document.querySelectorAll("script[type='application/ld+json']");
+      if (aa != null) {
+        for (var i = 0; i < aa.length; i++) {
+          // @ts-ignore
+          var rawjson = JSON.parse(aa[i].innerText)
+          //var newname = rawjson.name
+          addHiddenDiv('newname', rawjson.name, i)
+          addHiddenDiv('price', rawjson.offers[0].price, i)
+          addHiddenDiv('img', rawjson.image, i)
+          addHiddenDiv('url', 'https://www.otto.de'+rawjson.url, i)
+          addHiddenDiv('brand', rawjson.brand, i)
+          var prodid=rawjson.url.split('/#variationId=')[1]
+          addHiddenDiv('prodid', prodid, i)
+          try{
+            addHiddenDiv('aggr', rawjson.aggregateRating[0].ratingValue, i)
+          }
+          catch(error){}
+          try{
+            addHiddenDiv('review', rawjson.aggregateRating[0].reviewCount, i)
+          }
+          catch(error){}
+          
+
+        }
+      }
+
+      // price 
+      // var price = getAllXpath("//span[@class='value']/text()", 'nodeValue');
+      // if (price != null) {
+      //   for (var i = 0; i < price.length; i++) {
+      //     price[i] = price[i].replace(",", ".")
+      //     addHiddenDiv('price', price[i], i);
       //   }
       // }
-
-      price 
-      var price = getAllXpath("//span[@class='value']/text()", 'nodeValue');
-      if (price != null) {
-        for (var i = 0; i < price.length; i++) {
-          price[i] = price[i].replace(",", ".")
-          addHiddenDiv('price', price[i], i);
-        }
-      }
-      var prodid = getAllXpath("//article[@class='product']/@data-facetted-variation-ids", 'nodeValue');
-      if (prodid != null) {
-        for (var i = 0; i < prodid.length; i++) {
-          prodid[i] = prodid[i].split(",")[0]
-          addHiddenDiv('prodid', prodid[i], i);
-        }
-      }
+      // var prodid = getAllXpath("//article[@class='product']/@data-facetted-variation-ids", 'nodeValue');
+      // if (prodid != null) {
+      //   for (var i = 0; i < prodid.length; i++) {
+      //     prodid[i] = prodid[i].split(",")[0]
+      //     addHiddenDiv('prodid', prodid[i], i);
+      //   }
+      // }
 
     });
     return await context.extract(productDetails, { transform });
