367e9c48f0a46f8168ecbb3b403a4e33a68a3602
Vivek Gondil <gondilvivek@gmail.com>
2021-02-05 19:59:00 +0530

    Updated the noresultxpath deployed new extractor

--- src/library/product/reviews/stores/t/tesco/UK/execute.js
+++ src/library/product/reviews/stores/t/tesco/UK/execute.js
@@ -1,12 +1,11 @@
-
 module.exports = {
-  implements: 'product/reviews/execute',
-  parameterValues: {
-    country: 'UK',
-    store: 'tesco',
-    domain: 'tesco.com',
-    loadedSelector: 'div#review-data',
-    noResultsXPath: '//h2[contains(.,"No reviews")] | //p[contains(.,"We’re really sorry")] | //section[contains(@class, "error-container")]',
-    reviewUrl: 'https://www.tesco.com/groceries/en-GB/products/{id}#review-data',
-  },
+    implements: 'product/reviews/execute',
+    parameterValues: {
+        country: 'UK',
+        store: 'tesco',
+        domain: 'tesco.com',
+        loadedSelector: 'div#review-data',
+        noResultsXPath: '//h2[contains(.,"No reviews")] | //p[contains(.,"We’re really sorry")] | //section[contains(@class, "error-container")] | //div[contains(@class,"signin-register")]',
+        reviewUrl: 'https://www.tesco.com/groceries/en-GB/products/{id}#review-data',
+    },
 };
