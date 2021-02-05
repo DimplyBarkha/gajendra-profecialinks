358cf33ad32344890bb594dfd0d501dd19be2b9c
Priyanka <priyanka.l@osmosys.asia>
2021-02-05 10:30:12 +0530

    Fixed issue with rossmann rd

--- src/library/product/reviews/stores/r/rossmann/shared.js
+++ src/library/product/reviews/stores/r/rossmann/shared.js
@@ -3,45 +3,35 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data, context) => {
-  function checkIfReviewIsFromLast30Days (lastDate, reviewDate) {
-    console.log('lastDate' + lastDate);
-    console.log('reviewDate' + reviewDate);
-    const timestamp = new Date(lastDate).getTime() - (30 * 24 * 60 * 60 * 1000);
-    console.log('timestamp' + timestamp);
-    console.log(new Date(reviewDate).getTime());
-    if (new Date(reviewDate).getTime() >= timestamp) {
-      console.log('True');
-      return true;
+
+const transform = (data) => {
+  console.log('Validity initiated');
+  console.log(JSON.stringify(data));
+  for (let { group } of data) {
+    const newRows = [];
+    for (const row of group) {
+      if (row.reviewDate) {
+        // Validate date if below 30 days
+        const validityDate = new Date();
+        validityDate.setMonth(validityDate.getMonth() - 1 );
+        const reviewDate = new Date(row.reviewDate[0].raw);
+
+        if (reviewDate.getTime() >= validityDate.getTime()) {
+          newRows.push(row);
+        }
+      }
     }
-    console.log('false');
-    return false;
+    delete data[0].group;
+    data[0].group = newRows;
+    data[0].rows = newRows.length;
+
+    if (!newRows.length) {
+      // If no valid rows are present then remove other data.
+      data.pop();
+    }
   }
 
-  const state = context.getState();
-  let lastReviewDate = state.lastReviewDate || null;
-
-  data = data.filter(function (item, index, data) {
-    console.log('group length before' + item.group.length);
-    item.group = item.group.filter(function (row) {
-      if (!lastReviewDate) {
-        lastReviewDate = row.reviewDate[0].text;
-      }
-      if (checkIfReviewIsFromLast30Days(lastReviewDate, row.reviewDate[0].text)) {
-        return true;
-      }
-      return false;
-    });
-
-    console.log('group length after' + item.group.length);
-    item.rows = item.group.length;
-    if (item.group.length !== 0) {
-      return true;
-    }
-    return false;
-  });
-  context.setState({ lastReviewDate });
   return data;
-};
+}
 
 module.exports = { transform };
