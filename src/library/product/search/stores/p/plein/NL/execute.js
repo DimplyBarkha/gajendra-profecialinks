4e992b6c21ff1e6de70db92f3925cb07aba59266
adheer <sangeta.kumari@synerzip.com>
2021-02-05 22:59:05 +0530

    kickback issues

--- src/library/product/search/stores/p/plein/NL/execute.js
+++ src/library/product/search/stores/p/plein/NL/execute.js
@@ -4,9 +4,9 @@ module.exports = {
     country: 'NL',
     store: 'plein',
     domain: 'plein.nl',
-    url: 'https://www.plein.nl/zoeken?search={searchTerms}&p=1',
+    url: 'https://www.plein.nl/zoeken?search={searchTerms}',
     loadedSelector: 'div.product-view-container',
-    noResultsXPath: '//div[contains(text(),"Probeer eens een merknaam of beschrijving van je product in te typen.")]',
+    // noResultsXPath: '//div[contains(text(),"Probeer eens een merknaam of beschrijving van je product in te typen.")]',
     zipcode: "''",
   },  
 };
--- src/library/product/search/stores/p/plein/NL/paginate.js
+++ src/library/product/search/stores/p/plein/NL/paginate.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'NL',
     store: 'plein',
     mutationSelector: null,
-    nextLinkSelector: 'ul.pagination>li:nth-last-child(2)>a',
+    nextLinkSelector: 'ul.pagination>li.active+li>a',
     spinnerSelector: null,
     loadedSelector: 'div[class*="product-view-container"]',
     noResultsXPath: '//div[contains(text(),"Probeer eens een merknaam of beschrijving van je product in te typen.")]',
