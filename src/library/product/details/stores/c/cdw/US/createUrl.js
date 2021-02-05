6d78b0d89850ac723aae9751dc8abf59107398a1
adheer <sangeta.kumari@synerzip.com>
2021-02-05 19:55:09 +0530

    kickback issues

--- src/library/product/details/stores/c/cdw/US/createUrl.js
+++ src/library/product/details/stores/c/cdw/US/createUrl.js
@@ -5,6 +5,7 @@ module.exports = {
     domain: 'cdw.com',
     prefix: null,
     url: 'https://www.cdw.com/product/details/{id}',
+    // url: 'https://www.cdw.com/search/?key={id}',
     country: 'US',
     store: 'cdw',
     zipcode: '',
