a4219f1233d409ad78703110a7fb5f220e67e950
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-02-05 09:43:28 +0100

    #2113 - Adding changing language to german - Updating pagination selectors - Updating tests

--- src/library/product/search/stores/z/zalando_mweb/DE/extract.js
+++ src/library/product/search/stores/z/zalando_mweb/DE/extract.js
@@ -1,4 +1,4 @@
-
+// @ts-nocheck
 const { transform } = require('../../../../shared');
 
 module.exports = {
@@ -11,6 +11,33 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputString }, { country, store, transform }, context, { productDetails }) => {
+    // checking for selected language and changing to German if required
+    const englishSelected = await context.evaluate(async () => !!document.querySelector('a[title="Choose language"]'));
+    if (englishSelected) {
+      console.log('Changing language');
+      await context.click('a[title="Choose language"]');
+      await context.waitForSelector('div.z-navicat-header_modalContent', { timeout: 10000 });
+      const changingLanguage = await context.evaluate(async () => {
+        const deutschLabel = document.evaluate(
+          '//label[@class="z-navicat-header_radioItem"][contains(. , "Deutsch")]',
+          document,
+          null,
+          XPathResult.ANY_UNORDERED_NODE_TYPE,
+          null,
+        ).singleNodeValue;
+        if (deutschLabel) {
+          deutschLabel.click();
+          await new Promise((resolve) => setTimeout(resolve, 2000));
+          return !!document.querySelector('div.z-navicat-header_modalContent button[class*="Primary"]');
+        }
+      });
+      if (changingLanguage) {
+        await context.click('div.z-navicat-header_modalContent button[class*="Primary"]');
+        await context.waitForNavigation({ timeout: 10000, waitUntil: 'load' });
+      }
+      console.log('Finished changing language');
+    }
+
     await context.evaluate(async () => {
       const scriptText = document.querySelector('script[id="z-nvg-cognac-props"]').textContent;
       const scriptObj = JSON.parse(scriptText.match(/<!\[CDATA\[(.+)\]\]>/)[1]);
@@ -26,7 +53,10 @@ module.exports = {
         productElem.setAttribute('sku', product.sku);
         productElem.setAttribute('name', product.name);
         productElem.setAttribute('product_url', `https://m-en.zalando.de/${product.url_key}.html`);
-        productElem.setAttribute('thumbnail', `https://img01.ztat.net/article/${product.media.find((item) => item.role === 'DEFAULT').path}`);
+        productElem.setAttribute(
+          'thumbnail',
+          `https://img01.ztat.net/article/${product.media.find((item) => item.role === 'DEFAULT').path}`,
+        );
         productElem.setAttribute('price', product.price.promotional);
         productElem.setAttribute('manufacturer', product.brand_name);
 
