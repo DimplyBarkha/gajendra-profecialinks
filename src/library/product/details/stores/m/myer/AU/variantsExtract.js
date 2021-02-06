ec8c93c27ad2fd8bd0fede436b7fbdb774701efc
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-02-06 15:24:36 +0530

    fixing variant UAT issue

--- src/library/product/details/stores/m/myer/AU/variantsExtract.js
+++ src/library/product/details/stores/m/myer/AU/variantsExtract.js
@@ -8,12 +8,53 @@ async function implementation (
   const { transform } = parameters;
   const { variants } = dependencies;
 
-  await context.waitForSelector('div[data-automation="colour-select-container"]>div[class="select-wrapper"]>button[data-automation="select-colour"]');
-  await context.click('div[data-automation="colour-select-container"]>div[class="select-wrapper"]>button[data-automation="select-colour"]');
+  try{
+    await context.waitForSelector('div[data-automation="colour-select-container"]>div[class="select-wrapper"]>button[data-automation="select-colour"]');
+    await context.click('div[data-automation="colour-select-container"]>div[class="select-wrapper"]>button[data-automation="select-colour"]');
+    ////div[@class="drawers-container"]//ul[@data-automation="drawer-body"]/li
+  }catch(e){
+    
+  }
+  
   await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+  //await context.evaluate(function () {
+    //document.body.setAttribute('currentURL',window.location.href.replace(/#.*/,''));
+  //})*/
+
   await context.evaluate(function () {
-    document.body.setAttribute('currentURL',window.location.href.replace(/#.*/,''));
-  })
+    function addHiddenDiv(className, content) {
+      const newDiv = document.createElement('div');
+      newDiv.className = className;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      return newDiv;
+    }
+    let url = window.location.href.replace(/#.*/,'');
+    const outerDiv = addHiddenDiv('variants_outer', '');
+    if (document.querySelector('div.drawers-container ul[data-automation="drawer-body"]>li')) {
+      document.querySelectorAll('div.drawers-container ul[data-automation="drawer-body"]>li').forEach(variant => {
+        const skuId = variant.querySelector('img')?variant.querySelector('img').src.split('_Colour_').pop().split('_sw_')[0]:'';
+        const skudiv = addHiddenDiv('sku_id', skuId);
+        let skuURL = variant.querySelector('button')?variant.querySelector('button').getAttribute('data-automation'):'';
+        const skuUrl = addHiddenDiv('sku_url', `${url}?color=${skuURL}`);
+        const innerDiv = addHiddenDiv('variants_ul', '');
+        innerDiv.appendChild(skudiv);
+        innerDiv.appendChild(skuUrl);
+        outerDiv.appendChild(innerDiv);
+      });
+    } else {
+      const sku = document.querySelector('p[data-automation="product-part-number"] span');
+      const skuId = sku ? sku.innerText : '';
+      const skudiv = addHiddenDiv('sku_id', skuId);
+      let skuUrl = addHiddenDiv('sku_url', url);
+      const innerDiv = addHiddenDiv('variants_ul', '');
+      innerDiv.appendChild(skudiv);
+      innerDiv.appendChild(skuUrl);
+      outerDiv.appendChild(innerDiv);
+    }
+    document.body.appendChild(outerDiv);
+  });
+
   return await context.extract(variants, { transform });
   
 }
@@ -24,7 +65,7 @@ module.exports = {
   parameterValues: {
     country: 'AU',
     store: 'myer',
-    transform,
+    transform:null,
     domain: 'myer.com.au',
     zipcode: "''",
   },
