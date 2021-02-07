81a650c0d642b1816afee7d1c493586c89d02a39
Anushka2410 <44680746+Anushka2410@users.noreply.github.com>
2021-02-07 13:29:28 +0530

    updated the loadedSelector

--- src/library/product/search/stores/e/expert/DE/paginate.js
+++ src/library/product/search/stores/e/expert/DE/paginate.js
@@ -6,7 +6,7 @@ module.exports = {
     nextLinkSelector: 'a.arrow > i.fa-angle-right',
     mutationSelector: null,
     spinnerSelector: 'div.btLoader-overlay',
-    loadedSelector: 'div.widget-ArticlePPM, div.widget-Grid--content',
+    loadedSelector: 'div[class*="widget-Hyperlink"]',
     noResultsXPath: '//img[contains(@alt, "Keine Suchergebnisse")]',
     openSearchDefinition: null,
     domain: 'expert.de',
