9e9f3e0c30236ccf79d101c49391a9798d23b6dd
PR <pravinr@synerzipune.local>
2021-02-07 23:42:39 +0530

    search fixes

--- src/library/product/search/stores/c/chedraui/MX/paginate.js
+++ src/library/product/search/stores/c/chedraui/MX/paginate.js
@@ -1,19 +1,22 @@
 module.exports = {
-  implements: 'product/search/paginate',
+  implements: 'navigation/paginate',
   parameterValues: {
     country: 'MX',
     store: 'chedraui',
-    nextLinkSelector: 'li.pagination-next a',
+    // nextLinkSelector: 'li[class="pagination-next"] a',
+    // nextLinkXpath: '//li[@class="pagination-next"]/a',
     // mutationSelector: null,
     // spinnerSelector: null,
     // loadedSelector: 'div[class="product__list--wrapper"]',
     loadedSelector: 'ul[class="product__listing product__grid"]',
     // ul[id="plp_display"]
     noResultsXPath: '//div[@class="not-found-text"]',
-    // openSearchDefinition: {
-    //   template: 'https://www.chedraui.com.mx/search?q={searchTerms}%3Arelevance&page={page}&pageSize=24', 
-    //   //'https://www.chedraui.com.mx/search?text={searchTerms}&page={page-1}&pageSize={48}',
-    // },
+    openSearchDefinition: {
+      pageStartNb: 0,
+      template: 'https://www.chedraui.com.mx/search?q={searchTerms}%3Arelevance&page={page}&pageSize=24'
+      // 'https://www.chedraui.com.mx/search?q={searchTerms}%3Arelevance&page={page}&pageSize=24', 
+      //'https://www.chedraui.com.mx/search?text={searchTerms}&page={page-1}&pageSize={48}',
+    },
     domain: 'chedraui.com.mx',
     zipcode: "''",
   },
