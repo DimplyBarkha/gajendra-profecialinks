44fa93ad6aef4867dacc3b515cd1d248c62ed9ab
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-07 21:40:10 +0530

    updated the code to check for api response

--- src/library/product/search/stores/w/worten/ES/extract.js
+++ src/library/product/search/stores/w/worten/ES/extract.js
@@ -15,36 +15,72 @@ async function implementation (
     await timeout(5000);
     const id = window.location.href.match('[^=]$')[0];
     const searchTerm = window.location.href.replace(new RegExp('(.+query=)(.+)(&so.+)', 'g'), '$2');
-    const response = await fetch('https://www.worten.es/api/search/0/products/_search', {
-      headers: {
-        accept: 'application/json, text/plain, */*',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        'content-type': 'application/json;charset=UTF-8',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-      },
-      referrer: `https://www.worten.es/search?query=${searchTerm}&sortBy=relevance&hitsPerPage=24&page=${id}`,
-      referrerPolicy: 'no-referrer-when-downgrade',
-      body: `{"requests":[{"indexName":"search-prd-wes-products_rank1","params":"query=${searchTerm}&attributesToHighlight=%5B%5D&attributesToRetrieve=%5B%22*%22%2C%22-facets%22%5D&clickAnalytics=true&facets=price_range%2Ccategory_tree_lbls.level2%2Cbrand&hitsPerPage=24&page=${id - 1}"},{"indexName":"search-prd-wes-products_rank1","params":"query=%20black%20game&attributesToHighlight=%5B%5D&attributesToRetrieve=%5B%22*%22%2C%22-facets%22%5D&facets=price_range%2Ccategory_tree_lbls.level2%2Cbrand&hitsPerPage=0&page=0"}]}`,
-      method: 'POST',
-      mode: 'cors',
-      credentials: 'include',
-    });
-    const json = await response.json();
-    const arr = json.results;
-    const dataArr = arr[0].hits;
-    dataArr.forEach(item => {
-      if (item.rating_bazaar) {
-        document.querySelectorAll('#products-list-block > div').forEach(val => {
-          const sku = val.getAttribute('data-sku');
-          if (sku === item.sku) {
-            const rating = parseFloat(item.rating_bazaar).toFixed(2);
-            val.setAttribute('aggregaterating', rating.toString().replace('.', ','));
+    let response = {};
+    let json = {};
+    let apiDataFetched = false;
+    try {
+      response = await fetch('https://www.worten.es/api/search/0/products/_search', {
+        headers: {
+          accept: 'application/json, text/plain, */*',
+          'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+          'content-type': 'application/json;charset=UTF-8',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+        },
+        referrer: `https://www.worten.es/search?query=${searchTerm}&sortBy=relevance&hitsPerPage=24&page=${id}`,
+        referrerPolicy: 'no-referrer-when-downgrade',
+        body: `{"requests":[{"indexName":"search-prd-wes-products_rank1","params":"query=${searchTerm}&attributesToHighlight=%5B%5D&attributesToRetrieve=%5B%22*%22%2C%22-facets%22%5D&clickAnalytics=true&facets=price_range%2Ccategory_tree_lbls.level2%2Cbrand&hitsPerPage=24&page=${id - 1}"},{"indexName":"search-prd-wes-products_rank1","params":"query=%20black%20game&attributesToHighlight=%5B%5D&attributesToRetrieve=%5B%22*%22%2C%22-facets%22%5D&facets=price_range%2Ccategory_tree_lbls.level2%2Cbrand&hitsPerPage=0&page=0"}]}`,
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+      });
+      json = await response.json();
+      apiDataFetched = true;
+    } catch(err) {
+      console.log('got some error while fetching data from the api', err.message)
+    }
+    console.log('apiDataFetched', apiDataFetched);
+    if(apiDataFetched) {
+      if(json && json.hasOwnProperty('results') && Array.isArray(json.results)) {
+        const arr = json.results;
+        if(arr.length > 0) {
+          if(typeof(arr[0]) === 'object' && arr[0].hasOwnProperty('hits')) {
+            const dataArr = arr[0].hits;
+            dataArr.forEach(item => {
+              if (item.rating_bazaar) {
+                document.querySelectorAll('#products-list-block > div').forEach(val => {
+                  const sku = val.getAttribute('data-sku');
+                  if (sku === item.sku) {
+                    const rating = parseFloat(item.rating_bazaar).toFixed(2);
+                    val.setAttribute('aggregaterating', rating.toString().replace('.', ','));
+                  }
+                });
+              }
+            });
+          } else {
+            console.log('either 1st element of json.results is not an object or there is not hits property in it')
           }
-        });
+          
+        } else {
+          console.log('array in json.results is empty');
+        }
+      } else {
+        console.log('either there is no property as results in json or json.results is not an array');
       }
-    });
+    } else {
+      // need to somehow get rating info
+      document.querySelectorAll('#products-list-block > div').forEach(val => {
+        let aggRatingFulStarSel = 'div[itemprop*="aggregateRating"] span[class*="active"]';
+        let aggRatingHalfStarSel = 'div[itemprop*="aggregateRating"] span[class*="half"]';
+        let fullStarElms = val.querySelectorAll(aggRatingFulStarSel);
+        let halfStarElms = val.querySelectorAll(aggRatingHalfStarSel);
+        let aggregateRating = fullStarElms.length - (halfStarElms.length*(0.5));
+        console.log('aggregateRating', aggregateRating);
+        val.setAttribute('aggregaterating', aggregateRating.toString().replace('.', ','));
+      });
+    }
+    
   });
   return await context.extract(productDetails, { transform });
 }
