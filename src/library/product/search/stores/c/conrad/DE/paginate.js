3d60c7b3429d452055144263637f24c4a33a58cc
Ajinkya Kathe <ajinkya.kathe@synerzip.com>
2021-02-08 21:57:30 +0530

    Fixed search missing search term kickback issue

--- src/library/product/search/stores/c/conrad/DE/paginate.js
+++ src/library/product/search/stores/c/conrad/DE/paginate.js
@@ -3,8 +3,8 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'conrad',
-    nextLinkSelector: 'ul.pagination__list li:last-child button.pagination__button',
-    mutationSelector: 'p.resultsListHeadline',
+    nextLinkSelector: 'ul.pagination__list li:last-child button[class="pagination__button"]',
+    mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: null,
     noResultsXPath: '//main//div[@class="searchView"]//div[contains(@class, "resultsListTitle--zeroResultPage")]/h1',
