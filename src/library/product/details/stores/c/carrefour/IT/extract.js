ee27df4e03ee07a605f237cd944602bc15e554e7
shwetakedari10 <60596662+shwetakedari10@users.noreply.github.com>
2021-02-08 18:26:53 +0530

    commiting the carrefour IT

---
+++ src/library/product/details/stores/c/carrefour/IT/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/extract',
+  parameterValues: {
+    country: 'IT',
+    store: 'carrefour',
+    transform: null,
+    domain: 'carrefour.it',
+    zipcode: '',
+  },
+};
