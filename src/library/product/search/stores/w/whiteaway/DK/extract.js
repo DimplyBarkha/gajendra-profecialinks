bdf29fa36ffdcee3996b457bcf6f999161ead5c6
unknown <madhan.selvaraj-odc@partner.import.io>
2021-02-08 16:12:17 +0530

    search-whiteaway

--- src/library/product/search/stores/w/whiteaway/DK/extract.js
+++ src/library/product/search/stores/w/whiteaway/DK/extract.js
@@ -20,9 +20,19 @@ async function implementation(
   const { productDetails } = dependencies;
   const applyScroll = async function (context) {
   await context.evaluate(async function () {
+    try{
+      // @ts-ignore
+      document.querySelector('#coiPage-1 > div.coi-banner__page-footer > div.coi-button-group > button.coi-banner__accept').click()
+      await new Promise(r => setTimeout(r, 6000));
+      // }
+      }
+      catch(error)
+      {
+  
+      }
     
     let scrollTop = 0;
-    while (scrollTop !== 10000) {
+    while (scrollTop !== 50000) {
       scrollTop += 1000;
       window.scroll(0, scrollTop);
       await stall(1000);
@@ -36,8 +46,39 @@ async function implementation(
     }
   })
 }
+  // await applyScroll(context);
+  async function getProductsCount (context) {
+    return context.evaluate(async function () {
+      const products = document.evaluate('//div[@class="srp-product-box-outer list"]', document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      return products.snapshotLength;
+    });
+  }
+  let productsCount = 0;
+  productsCount = await getProductsCount(context);
+      console.log('productsCount' + productsCount);
+      if (productsCount >= 150) {
+        // @ts-ignore
+        // break;
+      }
+      await applyScroll(context);
+  // let productsCount = 0;
+  // while (productsCount < 150) {
+  //   const doesLoadMoreExists = await context.evaluate(function () {
+  //     return Boolean(document.querySelector('div.srp-product-box-outer.list'));
+  //   });
+  //   productsCount = await getProductsCount(context);
+  //   console.log('productsCount' + productsCount);
+  //   if (productsCount >= 150) {
+  //     break;
+  //   }
+  // }
+  //   else {
+  //   break;
+  // }
   await applyScroll(context);
+
   try{
+    // @ts-ignore
     document.querySelector('#coiPage-1 > div.coi-banner__page-footer > div.coi-button-group > button.coi-banner__accept').click()
     await new Promise(r => setTimeout(r, 6000));
     // }
