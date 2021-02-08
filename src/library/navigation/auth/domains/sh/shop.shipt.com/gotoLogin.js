68d29b478d87a66e700c3d6901a8b2c62ffa55b1
Julia Lojek <julialojek@gmail.com>
2021-02-08 14:01:26 +0100

    search extractor finished

--- src/library/navigation/auth/domains/sh/shop.shipt.com/gotoLogin.js
+++ src/library/navigation/auth/domains/sh/shop.shipt.com/gotoLogin.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/auth/gotoLogin',
   parameterValues: {
     domain: 'shop.shipt.com',
-    loginPage: null,
+    loginPage: 'https://shop.shipt.com/login',
     country: 'US',
     store: 'shipt_publix_37205',
     zipcode: '37205',
---
+++ src/library/product/search/stores/s/shipt_publix_37205/US/paginate.js
@@ -0,0 +1,21 @@
+module.exports = {
+  implements: 'navigation/paginate',
+  parameterValues: {
+    template: null,
+    country: 'US',
+    store: 'shipt_publix_37205',
+    nextLinkSelector: null,
+    nextPageUrlSelector: null,
+    nextLinkXpath: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    loadedXpath: null,
+    noResultsXPath: null,
+    stopConditionSelectorOrXpath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'shop.shipt.com',
+    zipcode: '37205',
+  },
+};
