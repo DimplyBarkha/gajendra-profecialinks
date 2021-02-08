b8e389dc1994df33facda9828f69012f7dc9b6fc
Mamatha-Gundapaneni <mamatha.g@osmosys.asia>
2021-02-08 15:57:52 +0530

    Updated yaml

--- src/library/product/search/stores/t/tesco/UK/extract.js
+++ src/library/product/search/stores/t/tesco/UK/extract.js
@@ -12,7 +12,7 @@ async function implementation(
   await context.evaluate(async function () {
     let scrollTop = 0;
     while (scrollTop <= 20000) {
-      await stall(500);
+      await stall(600);
       scrollTop += 100;
       window.scroll(0, scrollTop);
       if (scrollTop === 20000) {
@@ -37,35 +37,34 @@ async function implementation(
       }
     };
     optionalWait('li.product-list--list-item:nth-last-child(1) a img[src*="https"]');
-
   });
-  await context.evaluate(async function () {
-    let thumbanial = document.querySelectorAll('a[class*="product-image-wrapper"] img')
-     const imageArray = [];
-    const imageArray1 = [];
-    thumbanial.forEach((element) => {
-      imageArray.push(element.getAttribute('src'))
-      imageArray1.push(element.getAttribute('srcset'))
-    })
-    const imageUrl = [];
+  // await context.evaluate(async function () {
+  //   let thumbanial = document.querySelectorAll('a[class*="product-image-wrapper"] img')
+  //    const imageArray = [];
+  //   const imageArray1 = [];
+  //   thumbanial.forEach((element) => {
+  //     imageArray.push(element.getAttribute('src'))
+  //     imageArray1.push(element.getAttribute('srcset'))
+  //   })
+  //   const imageUrl = [];
 
-    for (let i = 0; i < imageArray1.length; i++) {
-      if (imageArray1[i] != null) {
-        imageUrl.push(imageArray1[i].split(',')[0].split(' ')[0]);
-      } else {
-        imageUrl.push(imageArray[i]);
-      }
-    }
-    const appendDiv = document.querySelectorAll('div[data-auto-id');
+  //   for (let i = 0; i < imageArray1.length; i++) {
+  //     if (imageArray1[i] != null) {
+  //       imageUrl.push(imageArray1[i].split(',')[0].split(' ')[0]);
+  //     } else {
+  //       imageUrl.push(imageArray[i]);
+  //     }
+  //   }
+  //   const appendDiv = document.querySelectorAll('div[data-auto-id');
 
-    for (let i = 0; i < appendDiv.length; i++) {
-      let imageAllUrl = '';
-      if (imageUrl != null) {
-        imageAllUrl = imageUrl[i];
-      }
-      appendDiv[i].setAttribute('imagedetail', imageAllUrl);
-    }
-  })
+  //   for (let i = 0; i < appendDiv.length; i++) {
+  //     let imageAllUrl = '';
+  //     if (imageUrl != null) {
+  //       imageAllUrl = imageUrl[i];
+  //     }
+  //     appendDiv[i].setAttribute('imagedetail', imageAllUrl);
+  //   }
+  // })
   return await context.extract(productDetails, { transform });
 }
 
