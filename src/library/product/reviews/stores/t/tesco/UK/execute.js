a54c54b557fcef1baf4a65c473fb189dc0462981
Vivek Gondil <gondilvivek@gmail.com>
2021-02-08 17:17:46 +0530

    Added fix in no result xpath

--- src/library/product/reviews/stores/t/tesco/UK/execute.js
+++ src/library/product/reviews/stores/t/tesco/UK/execute.js
@@ -5,7 +5,7 @@ module.exports = {
         store: 'tesco',
         domain: 'tesco.com',
         loadedSelector: 'div#review-data',
-        noResultsXPath: '//h2[contains(.,"No reviews")] | //p[contains(.,"We’re really sorry")] | //section[contains(@class, "error-container")] | //div[contains(@class,"signin-register")]',
+        noResultsXPath: '//h2[contains(.,"No reviews")] | //p[contains(.,"We’re really sorry")] | //section[contains(@class, "error-container")] | //div[contains(@class,"signin-register")] | //*[contains(.,"Quick and convenient shopping")]',
         reviewUrl: 'https://www.tesco.com/groceries/en-GB/products/{id}#review-data',
     },
 };
