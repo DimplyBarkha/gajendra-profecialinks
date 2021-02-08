dde784f0e5d47b8a31d9233260cdf77cd66d10e0
venkatsn <venkat.sn-odc@partner.import.io>
2021-02-08 20:34:17 +0530

    #2637

--- src/library/product/details/stores/m/medpex/DE/createUrl.js
+++ src/library/product/details/stores/m/medpex/DE/createUrl.js
@@ -2,8 +2,8 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'medpex.de',
-    prefix: null,//'search.do?q=',
-    url: null,//'https://medpex.de/search.do?q={id}',
+    prefix: 'search.do?q=',
+    url: 'https://medpex.de/search.do?q={id}',
     country: 'DE',
     store: 'medpex',
   },
