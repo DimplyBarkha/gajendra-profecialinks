922313c4c1639d504706310dcb9f8b78fa31162a
Navjyot Gill <navjyotg@synerzipune.local>
2021-02-08 11:01:25 +0530

    waitForXpath added

--- src/library/product/details/stores/d/delhaize/BE/extract.js
+++ src/library/product/details/stores/d/delhaize/BE/extract.js
@@ -8,4 +8,18 @@ module.exports = {
     domain: 'delhaize.be',
     zipcode: '',
   },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    try {
+      await context.waitForXPath('//div[contains(@class,"ShowMoreLess__content")]//h3[contains(text(), "Gebru")]/following::p[1]')
+    } catch (error) {
+      console.log('Error in Loading Instructions :',error)
+    }
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
+    return await context.extract(productDetails, { transform });
+  },
 };
