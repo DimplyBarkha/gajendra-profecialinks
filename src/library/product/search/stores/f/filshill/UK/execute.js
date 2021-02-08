9a4b1d726e1e445ca799eaf1c3ce66ecd69a4292
John Felix J <johnfelix.josep@hcl.com>
2021-02-08 12:34:40 +0530

    filshill fixes

--- src/library/product/search/stores/f/filshill/UK/execute.js
+++ src/library/product/search/stores/f/filshill/UK/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'UK',
     store: 'filshill',
     domain: 'filshill.co.uk',
-    url: 'https://sales.filshill.co.uk/products/gridlistsearch.asp?product_desc={searchTerms}',
+    url: 'https://sales.filshill.co.uk/products/gridlistsearch.asp?Cols=4&chkBrand=&product_desc={searchTerms}&chkDept=&search=&chkPM=&chkSubGroup=&chSize=&DeptCode=&prodgroup=&gridsort=rank&ItemPP=128',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
