a346910991a456d51b6566de82e1e13a264ecada
bindudatla9 <47576613+bindudatla9@users.noreply.github.com>
2021-02-08 07:08:26 -0500

    Reviews paginate action update (#3481)
    * [Osmosys] Carrefour es core (#221)
    
    * Migration completed.
    
    * Added missed columns
    
    * Fixed issue.
    
    * Fixed all the issue.
    
    * Updated, price per unit, price per unit uom, allergy advice and schema. Updated tests.
    
    * Fixed all the issue.
    
    * Fixed lint issue.
    
    * Fixed all the issue.
    
    * Fixed issue.
    
    * Resolved comments.
    
    * Resolved issue.
    
    * Resolved all the issue.
    
    * Resolved comments.
    
    * reduced code.
    
    * Added transform function
    
    * updated config file.
    
    * Resolved all the comments
    
    * Resolved comments.
    
    * added ts-ignore
    
    * Fixed category issue.
    
    * Removed unwanted columns.
    
    * Fixed new line issue.
    
    * Fixed all issue.
    
    * Fixed all the issue.
    
    Co-authored-by: Vikas Kyatannawar <vikas.k@osmosys.asia>
    Co-authored-by: vikas kyatannawar <49167922+vikas-kyatannawar@users.noreply.github.com>
    Co-authored-by: Cori Fowler <cori.fowler@import.io>
    
    * updated reviews template
    
    * fixed variable rename
    
    * updated to remove console logs
    
    Co-authored-by: osmnilesh <49181438+osmnilesh@users.noreply.github.com>
    Co-authored-by: Vikas Kyatannawar <vikas.k@osmosys.asia>
    Co-authored-by: vikas kyatannawar <49167922+vikas-kyatannawar@users.noreply.github.com>
    Co-authored-by: Cori Fowler <cori.fowler@import.io>

--- src/library/navigation/paginate/action.js
+++ src/library/navigation/paginate/action.js
@@ -17,6 +17,9 @@
  *  spinnerSelector: string,
  *  stopConditionSelectorOrXpath: string,
  *  resultsDivSelector: string,
+ *  dateSelector: string,
+ *  datePattern: string,
+ *  dateReplacePattern: string
  *  openSearchDefinition: { template: string, indexOffset?: number, pageOffset?: number, pageIndexMultiplier?: number, pageStartNb?: number }
  * }} parameters
  * @param { ImportIO.IContext } context
@@ -32,7 +35,7 @@ async function implementation (
   dependencies,
 ) {
   const { id, date, keywords, page, offset } = inputs;
-  const { nextPageUrlSelector, stopConditionSelectorOrXpath, nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, loadedXpath, resultsDivSelector, spinnerSelector, openSearchDefinition, nextLinkXpath } = parameters;
+  const { nextPageUrlSelector, stopConditionSelectorOrXpath, nextLinkSelector, loadedSelector, noResultsXPath, mutationSelector, loadedXpath, resultsDivSelector, dateSelector, datePattern, dateReplacePattern, spinnerSelector, openSearchDefinition, nextLinkXpath } = parameters;
 
   let nextLink;
 
@@ -52,6 +55,31 @@ async function implementation (
     }, stopConditionSelectorOrXpath);
     // @ts-ignore
     if (conditionIsTrue) return false;
+  }
+
+  if (dateSelector) {
+    const stopDateFound = await context.evaluate((sel, stopDate, datePattern, dateReplacePattern) => {
+      try {
+        const isThere = document.querySelectorAll(sel);
+        if (isThere[isThere.length - 1]) {
+          let pageDateStr = isThere[isThere.length - 1].textContent;
+          if (datePattern && dateReplacePattern) {
+            const pattern = new RegExp(datePattern, 'g');
+            pageDateStr = pageDateStr.replace(pattern, dateReplacePattern);
+          }
+
+          if (new Date(pageDateStr).getTime() < new Date(stopDate).getTime()) {
+            return true;
+          }
+        }
+        return false;
+      } catch (error) {
+        return error.message;
+      }
+    }, dateSelector, date, datePattern, dateReplacePattern);
+    // @ts-ignore
+    console.log(stopDateFound);
+    if (stopDateFound) return false;
   }
 
   if (nextLinkSelector) {
