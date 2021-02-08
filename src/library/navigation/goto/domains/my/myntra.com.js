3f5c467d6d1324f3140d786479c5fff1e0b3ee25
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-08 23:18:46 +0530

    updated code to fix the issue of timeout and not able to parse text to json

--- src/library/navigation/goto/domains/my/myntra.com.js
+++ src/library/navigation/goto/domains/my/myntra.com.js
@@ -3,9 +3,36 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'myntra.com',
-    timeout: null,
+    timeout: 120000,
     country: 'IN',
     store: 'myntra',
     zipcode: '',
+  },
+  implementation: async (inputs, parameterValues, context, dependencies) => {
+    // USING OPT TAGS > anti_fingerprint), to avoid blocking
+    // #[!opt!]{"anti_fingerprint":false}[/!opt!]
+    let timeOut = null;
+    if(parameterValues.timeout) {
+      timeOut = parameterValues.timeout;
+    }
+    // const url = `${inputs.url}#[!opt!]{"anti_fingerprint":false}[/!opt!]`;
+    const url = `${inputs.url}`;
+    await context.setBlockAds(false);
+    await context.setFirstRequestTimeout(100000);
+    await context.setCssEnabled(true);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    let response = await context.goto(url,
+      {
+        timeout: 150000,
+        waitUntil: 'networkidle0',
+      }
+    );
+
+    console.log('status', response.status);
+    // await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true });
   },
 };
--- src/library/product/details/stores/m/myntra/IN/extract.js
+++ src/library/product/details/stores/m/myntra/IN/extract.js
@@ -32,7 +32,44 @@ module.exports = {
       });
     };
     await applyScroll(context);
-    await context.evaluate(async function () {
+    let idXpath = '//*[contains(text(),"Product Code:")]//*';
+    let idPresent = false;
+    try {
+      await context.waitForXPath(idXpath);
+      idPresent = true;
+    } catch(err) {
+      console.log('got some error while waiting for id to load', err.message);
+    }
+
+    async function scrollToRecGlobal (node) {
+      await context.evaluate(async (node) => {
+        let elm = document.evaluate(node, document, null, 7, null);
+        let element = {};
+        if(elm && elm.snapshotLength > 0) {
+          element = elm.snapshotItem(0);
+        } else {
+          element = null;
+        }
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+          await new Promise((resolve) => {
+            setTimeout(resolve, 5000);
+          });
+        }
+      }, node);
+    }
+
+    if(!idPresent) {
+      await scrollToRecGlobal(idXpath);
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      try {
+        await context.waitForXPath(idXpath);
+        idPresent = true;
+      } catch(err) {
+        console.log('got some error while waiting for id to load', err.message);
+      }
+    }
+    await context.evaluate(async function (idXpath, idPresent) {
       function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
@@ -60,15 +97,124 @@ module.exports = {
       }
       addElementToDocument('desc_bullets', descBulletInfo.join(' || '));
 
-      const data = document.querySelectorAll('script[type="application/ld+json"]');
-      const json = data && data[1] && data[1].innerText ? JSON.parse(data[1].innerText) : '';
-      if (json) {
-        if (json.offers && json.offers.availability === 'InStock') {
-          addElementToDocument('availability', 'In Stock');
-        } else addElementToDocument('availability', 'Out of Stock');
-        addElementToDocument('sku', json.sku);
-        addElementToDocument('mpc', json.mpn);
+      let gotAllDataFromScript = false;
+      try {
+        const data = document.querySelectorAll('script[type="application/ld+json"]');
+        const json = data && data[1] && data[1].innerText ? JSON.parse(data[1].innerText) : '';
+        if (json) {
+          if (json.offers && json.offers.availability === 'InStock') {
+            addElementToDocument('availability', 'In Stock');
+          } else addElementToDocument('availability', 'Out of Stock');
+          addElementToDocument('sku', json.sku);
+          addElementToDocument('mpc', json.mpn);
+          gotAllDataFromScript = true;
+        }
+      } catch(err) {
+        console.log('got some error while getting data from script', err.message);
       }
+
+      let sku = '';
+      let mpc = '';
+      let availability = '';
+      async function scrollToRec (node) {
+        let elm = document.evaluate(node, document, null, 7, null);
+        let element = {};
+        if(elm && elm.snapshotLength > 0) {
+          element = elm.snapshotItem(0);
+        } else {
+          element = null;
+        }
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+          await new Promise((resolve) => {
+            setTimeout(resolve, 5000);
+          });
+        }
+      }
+      console.log('gotAllDataFromScript', gotAllDataFromScript);
+      if(!gotAllDataFromScript) {
+        // input url - https://www.myntra.com/8119769
+        // api url - 
+        await scrollToRec(idXpath);
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        let id = '';
+        console.log('idPresent', idPresent);
+        if(idPresent) {
+          let idElm = document.evaluate(idXpath, document, null, 7, null);
+          if(idElm && idElm.snapshotLength > 0) {
+            id = idElm.snapshotItem(0).textContent;
+            if(id) {
+              id = id.trim();
+            }
+          }
+
+          let resp = {};
+          let jsonResp = {};
+          let gotRespOk = false;
+          if(id) {
+            try {
+              resp = await fetch(`https://www.myntra.com/gateway/v2/product/${id}`);
+              jsonResp = await resp.json();
+              gotRespOk = true;
+            } catch(err) {
+              console.log('got some error while getting data from the api', err.message);
+            }
+          }
+          
+          if(gotRespOk) {
+            if(Object.keys(jsonResp).length > 0) {
+              try {
+                if(Array.isArray(jsonResp.style.sizes) && jsonResp.style.sizes.length > 0) {
+                  sku = jsonResp.style.sizes[0].skuId;
+                }
+                mpc = sku;
+                if(jsonResp.style.flags.outOfStock) {
+                  availability = "Out of Stock";
+                } else {
+                  availability = "In Stock";
+                }
+
+                addElementToDocument('availability', availability);
+                addElementToDocument('sku', sku);
+                addElementToDocument('mpc', mpc);
+              } catch(err) {
+                console.log('got some errro while extracting data from api', err.message);
+              }
+            } else {
+              console.log('we have the api responded successfully but it is empty');
+            }
+          } else {
+            console.log('either the response was not ok or it could not be parsed into json');
+          }
+
+        } else {
+          console.log('we do not have the id yet to call tha api');
+        }
+        
+      }
+
+      if(!availability) {
+        console.log('still do not have availability');
+        let availabilityXpath = '//div[contains(@class,"add-to-bag")]';
+        console.log('availabilityXpath', availabilityXpath);
+        let elm = document.evaluate(availabilityXpath, document, null, 7, null);
+        let text = '';
+        if(elm && elm.snapshotLength > 0) {
+          text = elm.snapshotItem(0).textContent;
+          if(text) {
+            text = text.trim();
+            console.log('current text', text);
+            if(text.toLowerCase().includes("add to bag")) {
+              text = "In Stock";
+            } else {
+              text = "Out of Stock";
+            }
+          }
+        }
+        availability = text;
+        addElementToDocument('availability', availability);
+      }
+      
 
       const specsXpath = document.evaluate("//h4[contains(text(), 'pecifications')]/following-sibling::div[@class='index-tableContainer']/div[@class='index-row']", document, null, XPathResult.ANY_TYPE, null);
       // eslint-disable-next-line prefer-const
@@ -98,7 +244,7 @@ module.exports = {
         console.log("ingredients section not present.");
       }
 
-    });
+    }, idXpath, idPresent);
     await context.extract(productDetails, { transform });
   },
 };
