e46a4dba38baa6ed1d383c9f82e0f738e66b49d6
Your Name <karthik.ravi-odc@partner.import.io>
2021-02-08 22:50:08 +0530

    new

--- src/library/navigation/goto/domains/jp/jpg.fr.js
+++ src/library/navigation/goto/domains/jp/jpg.fr.js
@@ -3,9 +3,21 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'jpg.fr',
-    timeout: 50000,
+    timeout: 100000,
     country: 'FR',
     store: 'jpg',
     zipcode: '',
+  },
+  implementation: async ({ url, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 1000000;
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+
+    const URL = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true, "cookie_jar":[{"name":"invCheckPostalCode","value":"${parameters.zipcode}"}]}[/!opt!]`;
+
+    await context.goto(URL, { timeout: timeout, waitUntil: 'networkidle0', checkBlocked: true });
+
   },
 };
