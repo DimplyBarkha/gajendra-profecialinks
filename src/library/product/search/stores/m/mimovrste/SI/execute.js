151a00b906d2d12e7db4d998adbe8357cd133a3f
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-08 16:04:03 +0530

    updated the code to create the url properly

--- src/library/product/search/stores/m/mimovrste/SI/execute.js
+++ src/library/product/search/stores/m/mimovrste/SI/execute.js
@@ -10,4 +10,25 @@ module.exports = {
     noResultsXPath: null, // '//div[@class="msg msg--indent-medium msg--warning"]',
     zipcode: '',
   },
+  implementation: async function implementation (
+    inputs,
+    { url, loadedSelector, noResultsXPath },
+    context,
+    dependencies,
+  ) {
+    const { keywords, query } = inputs;
+    console.log(url);
+    const destinationUrl = url
+      .replace('{searchTerms}', `${encodeURIComponent(keywords)}`)
+      .replace('{queryParams}', `${encodeURIComponent(query)}`);
+    await dependencies.goto({ ...inputs, url: destinationUrl });
+  
+    if (loadedSelector) {
+      await context.waitForFunction(function (sel, xp) {
+        return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+      }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+    }
+    console.log(`noResultsXPath: ${noResultsXPath}`);
+    return await context.evaluate((xp) => !document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext(), noResultsXPath);
+  },
 };
