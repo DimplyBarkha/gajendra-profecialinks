89c47945bb27dd6baafaf1fb27a1084c44005731
radhakrishna-osm <66716612+radhakrishna-osm@users.noreply.github.com>
2021-02-09 21:57:45 +0530

    fixed issues regarding ticket 20464 (#3485)
    Co-authored-by: ajayradhakrishna <ajayradhakrishna.t@osmosys.asia>

--- src/library/product/details/stores/b/brownthomas/IE/execute.js
+++ src/library/product/details/stores/b/brownthomas/IE/execute.js
@@ -9,4 +9,17 @@ module.exports = {
     noResultsXPath: '//p[contains(@class,"no-hits-content-results")]',
     zipcode: '',
   },
+  implementation: async (inputs, parameterValues, context, dependencies) => {
+    let url = `${inputs.url}`;
+    await context.setBlockAds(false);
+    url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true}[/!opt!]`;
+    await context.goto(url, { waitUntil: 'networkidle0', block_ads: false });
+    return await context.evaluate(function (xp) {
+      const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+      console.log(xp, r);
+      const e = r.iterateNext();
+      console.log(e);
+      return !e;
+    }, parameterValues.noResultsXPath);
+  },
 };
