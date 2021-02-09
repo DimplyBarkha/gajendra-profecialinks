34a6d26aa448e83c5a2b9e4ca9e98fc79a50e887
unknown <madhan.selvaraj-odc@partner.import.io>
2021-02-09 23:01:04 +0530

    im-kruidvat

--- src/library/product/details/createUrl.js
+++ src/library/product/details/createUrl.js
@@ -10,36 +10,36 @@ module.exports = {
   },
   };
 
-/**
- *
- * @param { { id: any } } inputs
- * @param { { domain: string, prefix?: string, suffix?: string, url?: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { } } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { id } = inputs;
-  const { domain, prefix, suffix } = parameters;
+// /**
+//  *
+//  * @param { { id: any } } inputs
+//  * @param { { domain: string, prefix?: string, suffix?: string, url?: string } } parameters
+//  * @param { ImportIO.IContext } context
+//  * @param { { } } dependencies
+//  */
+// async function implementation (
+//   inputs,
+//   parameters,
+//   context,
+//   dependencies,
+// ) {
+//   const { id } = inputs;
+//   const { domain, prefix, suffix } = parameters;
 
-  if (parameters.url) {
-    const url = parameters.url.replace('{id}', encodeURIComponent(id));
-    return url;
-  }
-  let gotoUrl = `https://${domain}`;
-  if (prefix) {
-    gotoUrl += `/${prefix}`;
-  }
-  gotoUrl += `/${id}`;
-  if (suffix) {
-    gotoUrl += `/${suffix}`;
-  }
-  return gotoUrl;
-}
+//   if (parameters.url) {
+//     const url = parameters.url.replace('{id}', encodeURIComponent(id));
+//     return url;
+//   }
+//   let gotoUrl = `https://${domain}`;
+//   if (prefix) {
+//     gotoUrl += `/${prefix}`;
+//   }
+//   gotoUrl += `/${id}`;
+//   if (suffix) {
+//     gotoUrl += `/${suffix}`;
+//   }
+//   return gotoUrl;
+// }
 
 module.exports = {
   parameters: [
@@ -67,5 +67,5 @@ module.exports = {
   ],
   dependencies: {},
   path: './stores/${store[0:1]}/${store}/${country}/createUrl',
-  implementation,
+  // implementation,
 };
