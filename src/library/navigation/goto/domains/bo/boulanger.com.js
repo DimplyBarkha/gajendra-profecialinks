81d9891bb1819391ac766582c42dcdd3638807af
Rafał Perkowski <rafalpero1@gmail.com>
2021-02-09 13:34:58 +0100

    #778 changes according to Jira

--- src/library/navigation/goto/domains/bo/boulanger.com.js
+++ src/library/navigation/goto/domains/bo/boulanger.com.js
@@ -12,22 +12,22 @@ module.exports = {
     const url = `${inputs.url}`;
     await context.setBlockAds(false);
     // await context.goto(url, { timeout: 90000, waitUntil: 'networkidle0', block_ads: false });
-    const isBlocked = async () => {
-      return await context.evaluate(async function () {
-        const errorXpath = document.evaluate("//title[contains(text(), 'Error')] | //title[contains(text(), 'Access Denied')]", document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        if (errorXpath.snapshotLength) {
-          return 'true';
-        } else {
-          return 'false';
-        }
-      });
-    };
-    const promise2 = async () => {
-      await new Promise(resolve => setTimeout(resolve, 30000));
-      if (await isBlocked() === 'true') {
-        return await context.reportBlocked();
+    const response = await context.goto(url, {
+      timeout: 10000,
+      waitUntil: 'load',
+      checkBlocked: false,
+      load_timeout: 6000, // not too long so it can timeout
+      first_redirect_timeout: 10000,
+    });
+    try {
+      await context.waitForSelector('body[itemtype], h1.product-title__main');
+    } catch (e) {
+      console.log(e);
+      console.log(response.status);
+      if (response.status !== 404) {
+        await context.reportBlocked(response.status);
+        throw new Error(`Got blocked with status: ${response.status}`);
       }
-    };
-    Promise.all([context.goto(url, { timeout: 90000, waitUntil: 'networkidle0', block_ads: false }), promise2]);
+    }
   },
 };
