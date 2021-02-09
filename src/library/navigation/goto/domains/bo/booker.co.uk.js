379386f667e05031dcd940943364c863e589bd53
Karolina <karolina.urbanska@tdcm.io>
2021-02-09 13:05:58 +0100

    Fixed goto due to suspended credentials (#1143)

--- src/library/navigation/goto/domains/bo/booker.co.uk.js
+++ src/library/navigation/goto/domains/bo/booker.co.uk.js
@@ -12,17 +12,33 @@ module.exports = {
   implementation: async (inputs, parameterValues, context, dependencies) => {
     const url = `${inputs.url}`;
     await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true });
-    const LogoutBtn = await context.evaluate(async () => {
-      return document.querySelector('a.logout-btn');
+    // Functionality deployed for search extractor
+    // const LogoutBtn = await context.evaluate(async () => {
+    //   return document.querySelector('a.logout-btn');
+    // });
+    // if (!LogoutBtn) {
+    //   await context.waitForSelector('input#CustomerNumber');
+    //   await context.setInputValue('input#CustomerNumber', '703636209');
+    //   await context.click('input#CustomerNumber+button');
+    //   await context.waitForSelector('form[action="/login"]');
+    //   await context.setInputValue('input#Email', 'russell.kirkham@unilever.com');
+    //   await context.setInputValue('input#Password', 'george');
+    //   await context.click('button.login-btn');
+    //   await context.waitForSelector('div#navbarMenuItems');
+    //   await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+    // }
+    // Functionality deployed for core extractor
+    const branchLocator = await context.evaluate(async () => {
+      return document.querySelector('header#shopping-header-desktop a[href*=branch]');
     });
-    if (!LogoutBtn) {
-      await context.waitForSelector('input#CustomerNumber');
-      await context.setInputValue('input#CustomerNumber', '703636209');
-      await context.click('input#CustomerNumber+button');
-      await context.waitForSelector('form[action="/login"]');
-      await context.setInputValue('input#Email', 'russell.kirkham@unilever.com');
-      await context.setInputValue('input#Password', 'george');
-      await context.click('button.login-btn');
+    if (!branchLocator) {
+      await context.waitForSelector('input#postcode');
+      await context.setInputValue('input#postcode', 'SY23 3JQ');
+      await context.click('div#logo-and-login+nav div#postcode-input button.btn-branch-search');
+      await context.waitForSelector('main#booker_branch_locator');
+      await context.click('div#search-results li.list-group-item:nth-child(1) a.view-branch');
+      await context.waitForSelector('main#booker_branch_locator');
+      await context.click('a[href*=categories]');
       await context.waitForSelector('div#navbarMenuItems');
       await context.goto(url, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
     }
