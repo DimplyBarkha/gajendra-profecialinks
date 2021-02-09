9738f60a8d35cb72332d7b10f949357dee21f526
unknown <madhan.selvaraj-odc@partner.import.io>
2021-02-09 22:16:55 +0530

    im-kruidvat

--- src/library/product/details/createUrl.js
+++ src/library/product/details/createUrl.js
@@ -1,3 +1,14 @@
+module.exports = {
+  implements: 'product/details/createUrl',
+  parameterValues: {
+  domain: 'kruidvat.nl',
+  prefix: null,
+  url: 'https://www.kruidvat.nl/p/{id}',
+  country: 'NL',
+  store: 'kruidvat',
+  zipcode: '',
+  },
+  };
 
 /**
  *
