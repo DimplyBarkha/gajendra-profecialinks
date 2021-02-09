bba0f88d6411bdda5cafe1d84f447984c642ee52
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-09 20:38:21 +0530

    updated the code to check for product in case we land on a product listing page

--- src/library/product/details/stores/e/elgiganten/SE/extract.js
+++ src/library/product/details/stores/e/elgiganten/SE/extract.js
@@ -54,6 +54,70 @@ async function implementation (
     console.log('No recommended products ');
   }
 
+  // need to check if this is a listing page or not
+
+  let thisIsListingPage = false;
+  let prodRowSel = 'div[id*="searchProductsInfo"] div[class*="product-list"] div[class="mini-product-content"]';
+  try {
+    await context.waitForSelector(prodRowSel);
+    thisIsListingPage = true;
+  } catch(err) {
+    console.log('got some error while waiting for listing page', err.message);
+  }
+  console.log('thisIsListingPage', thisIsListingPage);
+
+  if(thisIsListingPage) {
+
+    await context.evaluate(async (prodRowSel, idToCheck) => {
+      console.log('we need to check prodRowSel', prodRowSel);
+      console.log('idToCheck', idToCheck);
+      let allProds = document.querySelectorAll(prodRowSel);
+      let prodFound = false;
+      if(allProds && allProds.length > 0) {
+        console.log('got these many prods', allProds.length);
+        for(let i = 0; i < allProds.length; i++) {
+          let thisProd = allProds[i];
+          let thisIdElm = thisProd.querySelector('div[class*="product-number sku"]');
+          let thisId = '';
+          if(thisIdElm) {
+            thisId = thisIdElm.innerText;
+            console.log('thisId', thisId);
+            if(thisId.toString() === idToCheck.toString()) {
+              let prodNameElm = thisProd.querySelector('a[class*="product-name"]');
+              if(prodNameElm) {
+                prodNameElm.click();
+                prodFound = true;
+                await new Promise(resolve => setTimeout(resolve, 10000));
+                break;
+              }
+            }
+          }
+        }
+
+        console.log('prodFound', prodFound);
+
+      } else {
+        console.log('do not have any product here in this page');
+      }
+
+    }, prodRowSel, inputs.id);
+
+  }
+
+  let detailsPageSel = 'div.product-detail-page';
+  try {
+    await context.waitForSelector(detailsPageSel);
+    console.log('got the prod page');
+  } catch(err) {
+    console.log('got some error while waiting for details page', err.message);
+    try {
+      await context.waitForSelector(detailsPageSel);
+      console.log('got the prod page');
+    } catch(error) {
+      console.log('got some error while waiting for details page', error.message);
+    }
+  }
+
   async function scrollToRec (node) {
     await context.evaluate(async function (node) {
       var element = (document.querySelector(node)) ? document.querySelector(node) : null;
@@ -91,26 +155,36 @@ async function implementation (
       await new Promise(resolve => setTimeout(resolve, 1000));
     }
 
-    const sku = document.querySelector('p[data-product-sku]').getAttribute('data-product-sku');
+    const sku = document.querySelector('p[data-product-sku]') ? document.querySelector('p[data-product-sku]').getAttribute('data-product-sku'): null;
     // @ts-ignore
-    const name = document.querySelector('h1.product-title').innerText;
+    const name = document.querySelector('h1.product-title') ? document.querySelector('h1.product-title').innerText : null;
     const vidApiUrl = `https://dapi.videoly.co/1/videos/0/407/?SKU=${sku}&productTitle=${name}&hn=www.elgiganten.se`;
-    const videoApi = await fetch(vidApiUrl,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
+    let videoApi = {};
+    try {
+      videoApi = await fetch(vidApiUrl,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          method: 'GET',
         },
-        method: 'GET',
-      },
-    ).then(x => x.json());
+      ).then(x => x.json());
+    } catch(err) {
+      console.log('got some error while getting data from api', err.message);
+    }
 
-    const video = videoApi.items;
-    let videoUrl;
-    video.forEach(vid => {
-      videoUrl = `https://youtu.be/${vid.videoId}`;
-      addHiddenDiv('vidURL', videoUrl);
-    });
+    if(videoApi.hasOwnProperty('items')) {
+      const video = videoApi.items;
+      let videoUrl;
+      video.forEach(vid => {
+        videoUrl = `https://youtu.be/${vid.videoId}`;
+        addHiddenDiv('vidURL', videoUrl);
+      });
+    } else {
+      console.log('there is not items attr in api response - need to check how to fetch the api');
+    }
+    
   });
   try {
     await context.click('[data-template="ProductMoreInformationTab"]');
