16d339155e296d475ff093dbe4dca087c9b24a23
Gangadhar Dasari <d.gangadhar@olivedatacentre.com>
2021-02-09 22:27:45 +0530

    #3483 paginated

--- src/library/product/search/stores/l/lojashampoo_pt/PT/extract.js
+++ src/library/product/search/stores/l/lojashampoo_pt/PT/extract.js
@@ -8,4 +8,55 @@ module.exports = {
     domain: 'lojashampoo.pt',
     zipcode: '',
   },
+  implementation,
+};
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async function () {
+    function addclass(xpathforpagination) {
+      var elems = document.querySelectorAll(xpathforpagination);
+      elems[0].classList.add('pagination');
+    }
+    // function addHiddenDiv(id, content, index) {
+    //   const newDiv = document.createElement('div');
+    //   newDiv.id = id;
+    //   newDiv.textContent = content;
+    //   newDiv.style.display = 'none';
+    //   const originalDiv = document.querySelectorAll('div[class="NETREVIEWS_PRODUCT_STARS"]>div')[index];
+    //   originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    // }
+    function addElementToDocument(key, value) {
+      const catElement = document.createElement('div');
+      catElement.className = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+      }
+      return result;
+    };
+
+    var rating = getAllXpath('//div[@class="NETREVIEWS_PRODUCT_STARS"]/div/text()', 'nodeValue');
+    if (rating.length >= 1) {
+      for (var i = 0; i < rating.length; i++) {
+        rating[i] = rating[i].split("/")[0];
+        // addHiddenDiv('rating', rating[i], i);
+        addElementToDocument('rating', rating[i]);
+      }
+    }
+  });
+  return await context.extract(productDetails, { transform });
 };
