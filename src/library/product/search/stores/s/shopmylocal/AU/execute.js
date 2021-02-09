111d4e721012e7cbaf0b02ad9b03ae7605fbee39
Himanshu-Agrahari <himanshu.a@partner.import.io>
2021-02-09 13:27:20 +0530

    Update no-result-xpath

--- src/library/product/search/stores/s/shopmylocal/AU/execute.js
+++ src/library/product/search/stores/s/shopmylocal/AU/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'shopmylocal.com.au',
     url: 'https://www.shopmylocal.com.au/api/int/s?query={searchTerms}&search_distance=10&published_to=marketplace&location_slug=St-Ives&region_name=New+South+Wales#[!opt!]{"type":"json"}[/!opt!]',
     loadedSelector: null,
-    noResultsXPath: null,
+    noResultsXPath: `//div[contains(@class,'AdvertSearch-noResults') and contains(.,"We couldn't find any results matching your search")]`,
     zipcode: '',
   },
 };
