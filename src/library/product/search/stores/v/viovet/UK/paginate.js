47f9348ba51b591e9cb05a4e3ea8b8695c64d6b0
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-02-09 13:02:09 +0100

    - Get back to old pagination - Update tests

--- src/library/product/search/stores/v/viovet/UK/paginate.js
+++ src/library/product/search/stores/v/viovet/UK/paginate.js
@@ -4,16 +4,16 @@ module.exports = {
   parameterValues: {
     country: 'UK',
     store: 'viovet',
-    // nextLinkSelector: 'a[class="next"]',
+    nextLinkSelector: 'a[class="next"]',
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'span[class="_results-view-option"]',
-    noResultsXPath: '//h3[contains(. , "Here is a list of our most popular products:")] | //h1[contains(. , "Page Not Found")]',
+    noResultsXPath: '//h3[contains(. , "Here is a list of our most popular products:")] | //h1[contains(. , "Page Not Found") or contains(. , "did not match any products")]',
     resultsDivSelector: null,
-    openSearchDefinition: {
-      template: 'https://www.viovet.co.uk/advanced_search_result.php?keywords={searchTerms}&page={page}',
-      pageStartNb: 1,
-    },
+    // openSearchDefinition: {
+    //   template: 'https://www.viovet.co.uk/advanced_search_result.php?keywords={searchTerms}&page={page}',
+    //   pageStartNb: 1,
+    // },
     domain: 'viovet.co.uk',
     zipcode: '',
   },
