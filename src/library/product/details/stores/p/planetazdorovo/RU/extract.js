476c71730a4f9e990a9700a577a17f8029c3fb22
Ajinkya Kathe <ajinkya.kathe@synerzip.com>
2021-02-10 02:06:20 +0530

    Fixed kick back missing product code issue

--- src/library/product/details/stores/p/planetazdorovo/RU/extract.js
+++ src/library/product/details/stores/p/planetazdorovo/RU/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require("./format");
+const { transform } = require("../format");
 
 module.exports = {
   implements: "product/details/extract",
@@ -9,4 +9,17 @@ module.exports = {
     domain: "planetazdorovo.ru",
     zipcode: "",
   },
+  implementation: async ({ inputString }, { country, domain }, context, { productDetails }) => {
+    const firstItemLink = await context.evaluate(async function () {
+      const listPresent = document.querySelector('div.card-list');
+      if (listPresent) {
+        return document.querySelectorAll('div.card-list div.card-list__row div.card-list__element a.product-card__image')[0] && document.querySelectorAll('div.card-list div.card-list__row div.card-list__element a.product-card__image')[0].href
+      }
+    });
+    if (firstItemLink) {
+      await context.goto(firstItemLink, { timeout: 10000, waitUntil: 'load', checkBlocked: true });
+      context.waitForNavigation();
+    }
+    await context.extract(productDetails);
+  }
 };
