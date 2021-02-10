2c41ab79c5bd816e5ad45086f0e07e632b93e783
BorysMaciuch <borysmaciuch@gmail.com>
2021-02-10 10:27:55 +0100

    Fix rank

--- src/library/product/search/stores/t/thewhiskyexchange/UK/extract.js
+++ src/library/product/search/stores/t/thewhiskyexchange/UK/extract.js
@@ -1,31 +1,11 @@
 const { transform } = require('../../../../shared');
 
-async function implementation(inputs, parameters, context, dependencies) {
+async function implementation (inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-
-  await context.evaluate(async() => {
-    function addProp(selector, iterator, propName, value) {
-      document
-        .querySelectorAll(selector)
-        [iterator].setAttribute(propName, value);
-    }
-    const allProducts = document.querySelectorAll(
-      'div.item'
-    );
-    for (let i = 0; i < allProducts.length; i++) {
-      addProp(
-        'div.item',
-        i,
-        'rankorganic',
-        `${i + 1}`
-      );
-    }
-  });
-
   await context.evaluate(async () => {
-    function stall(ms) {
+    function stall (ms) {
       return new Promise((resolve, reject) => {
         setTimeout(() => {
           resolve();
@@ -53,9 +33,9 @@ module.exports = {
   parameterValues: {
     country: 'UK',
     store: 'thewhiskyexchange',
-    transform: null,
+    transform,
     domain: 'thewhiskyexchange.com',
     zipcode: '',
   },
-  implementation
+  implementation,
 };
