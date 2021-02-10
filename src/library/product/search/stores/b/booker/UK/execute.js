2da64db4573db2ce68b8510a552bdb15ba145949
Suryakanta Sahoo <suryakanta.s@osmosys.asia>
2021-02-10 22:45:34 +0530

    updated noResultXpath

--- src/library/product/search/stores/b/booker/UK/execute.js
+++ src/library/product/search/stores/b/booker/UK/execute.js
@@ -7,6 +7,6 @@ module.exports = {
     domain: 'booker.co.uk',
     url: 'https://www.booker.co.uk/products/product-list?categoryName=&keywords={searchTerms}&view=UnGrouped&sortField=SearchRelevancy&SortDirection=Ascending&multi=False&pageIndex=0',
     loadedSelector: 'main.inner-list',
-    noResultsXPath: '//p[contains(text(),"search returned no results")] | //h1[contains(text(),"The website is undergoing essential maintenance")] | //h1[contains(text(),"Sorry, this page does not exist")]',
+    noResultsXPath: '//p[contains(text(),"search returned no results")] | //h1[contains(text(),"The website is undergoing essential maintenance")] | //h1[contains(text(),"Sorry, this page does not exist")]|//*[contains(text(),"Sorry, your search returned no results")]',
   },
 };
