6afe4a92b75cb9e315cb345825b0529b34729dd5
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-10 15:16:22 +0530

    updated variants

--- src/library/product/details/stores/o/otto-office/DE/variantsExtract.js
+++ src/library/product/details/stores/o/otto-office/DE/variantsExtract.js
@@ -19,18 +19,32 @@ async function implementation(
   const { transform } = parameters;
   const { variants } = dependencies;
   await context.evaluate(async function () {
-      function addElementToDocument(key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
+    function addElementToDocument(key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+    // Method to Retrieve Xpath content of a Multiple Nodes
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
       }
-  // @ts-ignore
-  let URL = windows.location.href;
-  addElementToDocument('URL', URL);
+      return result;
+    };
+    // @ts-ignore
+    let URL = window.location.href;
+    addElementToDocument('URL', URL);
 
-
-});
-return await context.extract(variants, { transform });
+    // @ts-ignore
+    var subURLS = getAllXpath('//div[@id="item-color-choice-container"]/a//img/@src', 'nodeValue');
+    for (let i = 0; i < subURLS.length; i++) {
+      addElementToDocument('URL', 'https://www.otto-office.com/de/' + subURLS[i].replace(/(.+ART_)(\d+)(([A-Z]+)?)(_+.+)/g, '$2$3'));
+    }
+  });
+  return await context.extract(variants, { transform });
 }
