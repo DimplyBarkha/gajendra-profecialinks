32f5c88afe53d2542c606a7571b76164cdc0bfd3
adheer <sangeta.kumari@synerzip.com>
2021-02-10 13:36:59 +0530

    kick back issues

--- src/library/navigation/goto/domains/be/belezanaweb.com.br.js
+++ src/library/navigation/goto/domains/be/belezanaweb.com.br.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'belezanaweb.com.br',
-    timeout: 60000,
+    timeout: 80000,
     country: 'BR',
     store: 'belezanaweb',
     zipcode: '',
