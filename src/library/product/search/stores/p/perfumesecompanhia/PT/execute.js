31273870e2b886270e91c0fcbbde45adff1d4e5f
Julia Lojek <julialojek@gmail.com>
2021-02-10 13:19:28 +0100

    #2589 noResultsXPath changed, pagiation logic fixed

--- src/library/product/search/stores/p/perfumesecompanhia/PT/execute.js
+++ src/library/product/search/stores/p/perfumesecompanhia/PT/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'perfumesecompanhia.pt',
     url: 'https://www.perfumesecompanhia.pt/pt/procurar/?q={searchTerms}',
     loadedSelector: 'div#containerResultsFilter div.active[id] div.preview-img img',
-    noResultsXPath: '//div[contains(text(), "0 resultados")] | //div[@class="banner"]/div[@data_analytics] | //div[@id="containerResultsFilter"]//map[@name] | //div[@id="ACT-body"] | //div[@id="containerResultsFilter"]//link[@type="text/css"]',
+    noResultsXPath: '//div[contains(text(), " 0 resultados")] | //div[@id="containerResultsFilter" and not(//div[contains(@class, " active")])] | //div[@id="containerResultsFilter"]//map[@name] | //div[@id="ACT-body"] | //div[@id="containerResultsFilter"]//link[@type="text/css"]',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/p/perfumesecompanhia/PT/extract.js
+++ src/library/product/search/stores/p/perfumesecompanhia/PT/extract.js
@@ -33,8 +33,12 @@ async function implementation (
   let clicksAmount = 0;
 
   while (isLoadMoreButtonPresent && productsAmount < 150 && clicksAmount < 20) {
-    await context.click('button#btnVerMais');
-    // await context.waitForXPath(`(//div[@id="containerResultsFilter"]//div[contains(@class, " active") and @id])[${productsAmount + 1}]`);
+    await context.evaluate(async () => {
+      const button = document.querySelector('button#btnVerMais');
+      // @ts-ignore
+      if (button) button.click();
+    });
+    await new Promise(resolve => setTimeout(resolve, 1500));
     isLoadMoreButtonPresent = await checkIfLoadMoreButtonIsPresent();
     productsAmount = await countProducts();
     clicksAmount++;
@@ -43,26 +47,30 @@ async function implementation (
   await context.evaluate(async () => {
     const searcUrl = window.location.href;
     const products = document.querySelectorAll('div#containerResultsFilter div.active[id]');
-    products.forEach(product => {
-      // full name
-      const collectData = (selector) => {
-        return product.querySelector(selector) ? product.querySelector(selector).textContent : '';
-      };
-      const brand = collectData('h3');
-      const name = collectData('h2');
-      const info = collectData('div.tipo');
-      const variant = collectData('li > div.mini-tab.active');
-      const fullName = `${brand} ${name} ${info} ${variant}`.trim();
-      product.setAttribute('fullname', fullName);
-      // search url
-      product.setAttribute('searchurl', searcUrl);
-      // rating
-      const rating = product.querySelector('div.star-ratings-css-top');
-      if (rating && rating.getAttribute('style')) {
-        const ratingPercentage = rating.getAttribute('style').match(/width: ([\d.]+)%/) ? Number(rating.getAttribute('style').match(/width: ([\d.]+)%/)[1]) : 0;
-        if (ratingPercentage) {
-          const ratingValue = (ratingPercentage / 20).toFixed(1).replace('.', ',');
-          product.setAttribute('rating', ratingValue);
+    products.forEach((product, index) => {
+      if (index < 150) {
+        // attribute to mark only first 150 products for the yaml file
+        product.setAttribute('tocollect', '');
+        // full name
+        const collectData = (selector) => {
+          return product.querySelector(selector) ? product.querySelector(selector).textContent : '';
+        };
+        const brand = collectData('h3');
+        const name = collectData('h2');
+        const info = collectData('div.tipo');
+        const variant = collectData('li > div.mini-tab.active');
+        const fullName = `${brand} ${name} ${info} ${variant}`.trim();
+        product.setAttribute('fullname', fullName);
+        // search url
+        product.setAttribute('searchurl', searcUrl);
+        // rating
+        const rating = product.querySelector('div.star-ratings-css-top');
+        if (rating && rating.getAttribute('style')) {
+          const ratingPercentage = rating.getAttribute('style').match(/width: ([\d.]+)%/) ? Number(rating.getAttribute('style').match(/width: ([\d.]+)%/)[1]) : 0;
+          if (ratingPercentage) {
+            const ratingValue = (ratingPercentage / 20).toFixed(1).replace('.', ',');
+            product.setAttribute('rating', ratingValue);
+          }
         }
       }
     });
