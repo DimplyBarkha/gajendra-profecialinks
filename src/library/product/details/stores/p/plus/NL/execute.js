a98a37768acbea1ef5d0c462fdd7a639e139cb17
Vivek Gondil <gondilvivek@gmail.com>
2021-02-10 17:32:34 +0530

    Updated the Loaded selector

--- src/library/product/details/stores/p/plus/NL/execute.js
+++ src/library/product/details/stores/p/plus/NL/execute.js
@@ -4,7 +4,7 @@ module.exports = {
         country: 'NL',
         store: 'plus',
         domain: 'plus.nl',
-        loadedSelector: '.ish-productList-item img',
+        loadedSelector: '.ish-productList-item img, .ish-product-photo img',
         noResultsXPath: '//*[@class="ish-search-noResults-block"]',
         zipcode: '',
     },
