46962062e31f5b61cea9e4f7999f03e2bf1e1192
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-10 14:44:37 +0530

    upfdated code

--- src/library/product/details/stores/m/medpex/DE/createUrl.js
+++ src/library/product/details/stores/m/medpex/DE/createUrl.js
@@ -7,4 +7,34 @@ module.exports = {
     country: 'DE',
     store: 'medpex',
   },
+  implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { id } = inputs;
+  const { domain, prefix, suffix } = parameters;
+
+  if (parameters.url) {
+    const url = parameters.url.replace('{id}', encodeURIComponent(id));
+    // return url;
+    await context.goto(url, { timeout: 20000, waitUntil: 'load', checkBlocked: true });
+    const finalURL = await context.evaluate(async function () {
+      let searchURL = document.querySelector('span[class="product-name"] a').getAttribute('href');
+      return 'https://www.medpex.de' + searchURL;
+    });
+    return finalURL;
+  }
+  let gotoUrl = `https://${domain}`;
+  if (prefix) {
+    gotoUrl += `/${prefix}`;
+  }
+  gotoUrl += `/${id}`;
+  if (suffix) {
+    gotoUrl += `/${suffix}`;
+  }
+  return gotoUrl;
+}
---
+++ src/library/product/details/stores/m/medpex/DE/variants.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details/variants',
+  parameterValues: {
+    country: 'DE',
+    store: 'medpex',
+    domain: 'medpex.de',
+    zipcode: '',
+  },
+};
