aee29db0f1e19817bb2825c95ab2d590b9f7bf48
prasath <r.rahul@olivedatacentre.com>
2021-02-10 04:28:33 -0800

    notino_de prasath

--- src/library/product/details/stores/n/notino.de/DE/execute.js
+++ src/library/product/details/stores/n/notino.de/DE/execute.js
@@ -1,43 +1,11 @@
 module.exports = {
-  implements: 'product/details/extract',
+  implements: 'product/details/execute',
   parameterValues: {
     country: 'DE',
     store: 'notino',
-    transform: null,
     domain: 'notino.de',
+    loadedSelector: null,
+    noResultsXPath: null,
     zipcode: '',
   },
-  implementation,
-};
-async function implementation(
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-  await context.evaluate(async function () {
-    function addElementToDocument(key, value) {
-      const catElement = document.createElement('div');
-      catElement.id = key;
-      catElement.textContent = value;
-      catElement.style.display = 'none';
-      document.body.appendChild(catElement);
-    }
-    var a = ''
-    // @ts-ignore
-    const c = document.querySelector('script[type="application/ld+json"]').innerText;
-    console.log(c, 'ggggggggggggggg')
-    // @ts-ignore
-    if (c.includes('OutOfStock')) {
-      a = "Out of Stock"
-    }
-    else {
-      a = "In Stock"
-    }
-    addElementToDocument('a', a);
-  });
-  return await context.extract(productDetails, { transform });
-  // return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
-}
+  };
