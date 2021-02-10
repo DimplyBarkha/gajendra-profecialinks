58e80344cf48e06c84887d9803cf2cab3a408162
prasath <r.rahul@olivedatacentre.com>
2021-02-10 02:55:01 -0800

    loblaws prasath

--- src/library/product/details/stores/n/notino.de/DE/extract.js
+++ src/library/product/details/stores/n/notino.de/DE/extract.js
@@ -8,4 +8,39 @@ module.exports = {
     domain: 'notino.de',
     zipcode: '',
   },
+  implementation,
 };
+async function implementation(
+inputs,
+parameters,
+context,
+dependencies,
+) {
+const { transform } = parameters;
+const { productDetails } = dependencies;
+await context.evaluate(async function () {
+  function addElementToDocument(key, value) {
+    const catElement = document.createElement('div');
+    catElement.id = key;
+    catElement.textContent = value;
+    catElement.style.display = 'none';
+    document.body.appendChild(catElement);
+    }
+
+  // @ts-ignore
+  var a=''
+  const c = document.querySelector('script[type="application/ld+json"]').innerText;
+  if(c.includes('OutOfStock')){
+    a="Out of Stock"
+
+  }
+  else{
+    a= "In Stock"
+  }
+  addElementToDocument('a', a);
+
+
+});
+return await context.extract(productDetails, { transform });
+// return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
+}
