cc468d184bb9853a10852bf7119efeebdbd0c8f2
Gangadhar Dasari <d.gangadhar@olivedatacentre.com>
2021-02-10 17:47:08 +0530

    #3483 Aggregate rating field

--- src/library/product/search/stores/l/lojashampoo_pt/PT/extract.js
+++ src/library/product/search/stores/l/lojashampoo_pt/PT/extract.js
@@ -28,7 +28,7 @@ async function implementation(
       newDiv.id = id;
       newDiv.textContent = content;
       newDiv.style.display = 'none';
-      const originalDiv = document.querySelectorAll('div[class="product-block item-default"]')[index];
+      const originalDiv = document.querySelectorAll('div[class="product-block item-default"]>div')[index];
       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
     }
     function addElementToDocument(key, value) {
@@ -38,6 +38,13 @@ async function implementation(
       catElement.style.display = 'none';
       document.body.appendChild(catElement);
     }
+    var getXpath = (xpath, prop) => {
+      var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+      let result;
+      if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+      else result = elem ? elem.singleNodeValue : '';
+      return result && result.trim ? result.trim() : result;
+    };
 
     const getAllXpath = (xpath, prop) => {
       const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
@@ -53,11 +60,22 @@ async function implementation(
     if (rating.length >= 1) {
       for (var i = 0; i < rating.length; i++) {
         rating[i] = rating[i].split("/")[0];
-        // addHiddenDiv('rating', rating[i], i);
-        addElementToDocument('rating', rating[i]);
+        addHiddenDiv('rating', rating[i], i);
+        // addElementToDocument('rating', rating[i]);
       }
     }
     // addElementToDocument('rating', "rating Text");
+
+    // Price
+    var price = getAllXpath('//span[@class="price-new"]//text()', 'nodeValue');
+    if (price != null) {
+      for (var i = 0; i < price.length; i++) {
+        price[i] = price[i].match(/(\d+)/gm).join(',');
+        addElementToDocument('price', price[i]);
+      }
+
+
+    }
   });
   return await context.extract(productDetails, { transform });
 };
