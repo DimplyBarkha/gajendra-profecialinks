021184c36b9f7430274f78b3a7c27ac69cf9dffd
pavitra r <pavithra.ramasamy-odc@partner.import.io>
2021-02-10 20:05:39 +0530

    updated recordXpath - akshay channa

--- src/library/product/search/stores/m/marionnaud/FR/extract.js
+++ src/library/product/search/stores/m/marionnaud/FR/extract.js
@@ -22,7 +22,7 @@ implementation: async (
       newDiv.id = id;
       newDiv.textContent = content;
       newDiv.style.display = 'none';
-      const originalDiv = document.querySelectorAll("li[class='col-lg-3 col-md-3 col-sm-4']")[index];
+      const originalDiv = document.querySelectorAll("li[class='col-lg-3 col-md-3 col-sm-4'] div[class=' productMainLink']")[index];
       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
     }
     const price = document.querySelectorAll('div.productMainLink div.infoTextCarousel div span.lineinner');
