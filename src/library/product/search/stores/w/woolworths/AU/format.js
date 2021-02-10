5f7a0dced909fad6bd9d5b526b5dde9d3b351955
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-02-11 00:02:01 +0530

    fixing UAT

--- src/library/product/search/stores/w/woolworths/AU/format.js
+++ src/library/product/search/stores/w/woolworths/AU/format.js
@@ -4,33 +4,33 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  const cleanUp = (data) => {
-    const clean = text => text.toString()
-      .replace(/\r\n|\r|\n/g, ' ')
-      .replace(/&amp;nbsp;/g, ' ')
-      .replace(/&amp;#160/g, ' ')
-      .replace(/\u00A0/g, ' ')
-      .replace(/\s{2,}/g, ' ')
-      .replace(/"\s{1,}/g, '"')
-      .replace(/\s{1,}"/g, '"')
-      .replace(/^ +| +$|( )+/g, ' ')
-      // eslint-disable-next-line no-control-regex
-      .replace(/[\x00-\x1F]/g, '')
-      .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
-    data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(header => row[header].forEach(el => {
-      el.text = clean(el.text);
-    }))));
-    return data;
-  };
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ');
   const state = context.getState();
-  let rank = state.rank || 1;
-  let orgRankCounter = state.orgRankCounter || 1;
+  let orgRankCounter = state.orgRankCounter || 0;
+  let rankCounter = state.rankCounter || 0;
+  const productCodes = state.productCodes || [];
   for (const { group } of data) {
     for (const row of group) {
+      rankCounter += 1;
       if (!row.sponsored) {
         orgRankCounter += 1;
         row.rankOrganic = [{ text: orgRankCounter }];
       }
+      row.rank = [{ text: rankCounter }];
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
       if (row.aggregateRating) {
         let counter = 0;
         row.aggregateRating.forEach(item => {
@@ -59,13 +59,12 @@ const transform = (data, context) => {
           item.text = item.text.replace('/medium/', '/large/');
         });
       }
-      row.rank = [{ text: rank }];
-      rank++;
     }
   }
-  context.setState({ rank });
+  context.setState({ rankCounter });
   context.setState({ orgRankCounter });
-  return cleanUp(data);
+  context.setState({ productCodes });
+  console.log(productCodes);
+  return data;
 };
-
 module.exports = { transform };
