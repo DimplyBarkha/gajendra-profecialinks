ab4f085852f816c32d40750b5fed889d88fa62a2
PR <pravinr@synerzipune.local>
2021-02-10 11:40:53 +0530

    search kick-back fixes

--- src/library/product/search/stores/e/eroski/ES/paginate.js
+++ src/library/product/search/stores/e/eroski/ES/paginate.js
@@ -1,13 +1,14 @@
 
 module.exports = {
-  implements: 'product/search/paginate',
+  implements: 'navigation/paginate',
   parameterValues: {
     country: 'ES',
     store: 'eroski',
     // nextLinkSelector: null,
     // mutationSelector: null,
     // spinnerSelector: null,
-    loadedSelector: 'div[id=productListZone] >div',
+    // loadedSelector: 'div[id=productListZone] >div',
+    loadedSelector: 'div[id=productListZone] > div.product-item-lineal',
     noResultsXPath: '//p[@class="lineal-products-no-products"]',
     // openSearchDefinition: null,
     domain: 'supermercado.eroski.es',
