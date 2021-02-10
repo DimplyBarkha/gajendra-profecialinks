67eaf3c8468f3dc671015d4c01779ba65e7614d6
venkatsn <venkat.sn-odc@partner.import.io>
2021-02-10 21:37:54 +0530

    meiejr_49684

--- src/library/product/details/stores/m/meijer_49684/US/createUrl.js
+++ src/library/product/details/stores/m/meijer_49684/US/createUrl.js
@@ -3,8 +3,8 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'meijer.com',
-    prefix: 'search/?text=',
-    url: 'https://www.meijer.com/shop/en/search/?text={id}',
+    prefix: null,
+    url: null,
     country: 'US',
     store: 'meijer_49684',
     zipcode: '',
