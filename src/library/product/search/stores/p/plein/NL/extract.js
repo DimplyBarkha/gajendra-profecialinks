7ca1aab54264b5e2ae32a7d327c78762a023e090
adheer <sangeta.kumari@synerzip.com>
2021-02-11 02:13:42 +0530

    kick back issues

--- src/library/product/search/stores/p/plein/NL/extract.js
+++ src/library/product/search/stores/p/plein/NL/extract.js
@@ -1,33 +1,33 @@
-const { transform } = require('./shared');
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-  await context.evaluate(async function () {
-    let scrollTop = 0;
-    while (scrollTop <= 20000) {
-      await stall(500);
-      scrollTop += 1000;
-      window.scroll(0, scrollTop);
-      if (scrollTop === 20000) {
-        await stall(1000);
-        break;
-      }
-    }
-    function stall (ms) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve();
-        }, ms);
-      });
-    }
-  });
-  return await context.extract(productDetails, { transform });
-}
+const { transform } = require('../shared');
+// async function implementation (
+//   inputs,
+//   parameters,
+//   context,
+//   dependencies,
+// ) {
+//   const { transform } = parameters;
+//   const { productDetails } = dependencies;
+//   await context.evaluate(async function () {
+//     let scrollTop = 0;
+//     while (scrollTop <= 20000) {
+//       await stall(500);
+//       scrollTop += 1000;
+//       window.scroll(0, scrollTop);
+//       if (scrollTop === 20000) {
+//         await stall(1000);
+//         break;
+//       }
+//     }
+//     function stall (ms) {
+//       return new Promise(resolve => {
+//         setTimeout(() => {
+//           resolve();
+//         }, ms);
+//       });
+//     }
+//   });
+//   return await context.extract(productDetails, { transform });
+// }
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -37,5 +37,5 @@ module.exports = {
     domain: 'plein.nl',
     zipcode: "''",
   },
-  implementation,
+  // implementation,
 };
--- src/library/product/search/stores/p/plein/NL/paginate.js
+++ src/library/product/search/stores/p/plein/NL/paginate.js
@@ -5,13 +5,13 @@ module.exports = {
     country: 'NL',
     store: 'plein',
     mutationSelector: null,
-    nextLinkSelector: 'ul.pagination>li.active+li>a',
+    // nextLinkSelector: 'ul.pagination>li.active+li>a',
     spinnerSelector: null,
     loadedSelector: 'div[class*="product-view-container"]',
     noResultsXPath: '//div[contains(text(),"Probeer eens een merknaam of beschrijving van je product in te typen.")]',
-    // openSearchDefinition: {
-    //   template: 'https://www.plein.nl/zoeken?search={searchTerms}&p={page}',
-    // },
+    openSearchDefinition: {
+      template: 'https://www.plein.nl/zoeken?search={searchTerms}&p={page}',
+    },
     domain: 'plein.nl',
     zipcode: "''",
   },
