82c006ee76b80b6a858aa44a173ce47e3dacf1e1
malgorzata-kozera <malgorzata.kozera5@gmail.com>
2021-02-10 07:49:03 +0100

    Created template for reviews extractor

---
+++ src/library/product/reviews/stores/w/windeln/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/reviews/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'windeln',
+    transform: null,
+    domain: 'windeln.de',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/reviews/stores/w/windeln/DE/paginate.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  implements: 'navigation/paginate',
+  parameterValues: {
+    template: null,
+    country: 'DE',
+    store: 'windeln',
+    nextLinkSelector: null,
+    nextPageUrlSelector: null,
+    nextLinkXpath: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    loadedXpath: null,
+    noResultsXPath: null,
+    stopConditionSelectorOrXpath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'windeln.de',
+    zipcode: '',
+  },
+};
