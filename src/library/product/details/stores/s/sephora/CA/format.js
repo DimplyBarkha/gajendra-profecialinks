03fb871bc342a0e092bc23b6e10ad108075e7d37
alscotty <alawrencescott@gmail.com>
2021-02-11 14:40:51 -0800

    fixes for CA details per QA

--- src/library/product/details/stores/s/sephora/CA/format.js
+++ src/library/product/details/stores/s/sephora/CA/format.js
@@ -24,20 +24,34 @@ const transform = (data) => {
     return data;
   };
   for (const { group } of data) {
-    let tmpSku = '';
+    const tmpSku = '';
     for (const row of group) {
       let fstImg = ''; let fstImgAlt = ''; const restImg = []; let skuArr = []; let skuStr = '';
-      if (row.variantId) {
-        row.variantId.forEach(item => {
-          if (item.text.indexOf('ITEM') == -1) {
+      // if (row.variantId) {
+      //   row.variantId.forEach(item => {
+      //     if (item.text.indexOf('ITEM') == -1) {
 
-          } else {
-            tmpSku = item.text.replace('ITEM ', '');
-          }
-        });
-        row.variantId = [{ text: tmpSku }];
-        row.firstVariant = [{ text: tmpSku }];
+      //     } else {
+      //       tmpSku = item.text.replace('ITEM ', '');
+      //     }
+      //   });
+      //   row.variantId = [{ text: tmpSku }];
+      //   row.firstVariant = [{ text: tmpSku }];
+      // }
+
+      if (row.privacyPolicy) {
+        row.privacyPolicy = [{ text: row.privacyPolicy[0].text }];
       }
+      if (row.variantInformation) {
+        row.variantInformation = [{ text: row.variantInformation[0].text }];
+      }
+      if (row.termsAndConditions) {
+        row.termsAndConditions = [{ text: row.termsAndConditions[0].text }];
+      }
+      if (row.customerServiceAvailability) {
+        row.customerServiceAvailability = [{ text: row.customerServiceAvailability[0].text }];
+      }
+
       if (row.image) {
         row.image.forEach(item => {
           if (fstImg == '') {
@@ -70,26 +84,8 @@ const transform = (data) => {
           brnd = item.text;
         });
       }
-      if (row.nameExtended) {
-        row.nameExtended.forEach(item => {
-          if (brnd != '') {
-            item.text = brnd + ' - ' + item.text;
-          }
-        });
-      }
       if (row.quantity) {
-        let rowDelete;
-        row.quantity.forEach(item => {
-          if (item.text.indexOf('SIZE') == -1) {
-            rowDelete = true;
-          } else {
-            item.text = item.text.replace('SIZE ', '');
-            rowDelete = false;
-          }
-        });
-        if (rowDelete == true) {
-          delete row.quantity;
-        }
+        row.quantity = [{ text: row.quantity[0].text.replace('Size', '').replace('SIZE', '') }];
       }
       if (row.ratingCount) {
         row.ratingCount.forEach(item => {
@@ -101,56 +97,19 @@ const transform = (data) => {
           item.text = item.text.replace(' stars', '');
         });
       }
-      if (row.directions) {
-        let directionsStr = '';
-        row.directions.forEach(item => {
-          if (directionsStr = '') {
-            directionsStr = item.text;
-          } else {
-            directionsStr = directionsStr + ' ' + item.text;
-          }
-        });
-        row.directions = [{ text: directionsStr }];
-      }
-      if (row.directions) {
-        let directionsStr = '';
-        row.directions.forEach(item => {
-          if (directionsStr = '') {
-            directionsStr = item.text;
-          } else {
-            directionsStr = directionsStr + ' ' + item.text;
-          }
-        });
-        row.directions = [{ text: directionsStr }];
-      }
-      if (row.ingredientsList) {
-        let no2 = 0; const inf = []; let tmp = '';
-        row.ingredientsList.forEach(item => {
-          if (no2 == 0) {
-            tmp = item.text;
-            no2 = 1;
-          } else if (no2 == 1) {
-            tmp = tmp + ' : ' + item.text;
-            inf.push(tmp);
-            tmp = '';
-            no2 = 0;
-          }
-        });
-        row.ingredientsList = [{ text: inf.join(' | ') }];
-      }
-      if (row.variantInformation) {
-        row.variantInformation.forEach(item => {
-          item.text = item.text.replace(' - Selected', '');
-        });
-      }
-      if (row.variants) {
-        const inf = [];
-        row.variants.forEach(item => {
-          const tmpArr = item.text.replace('/productimages/sku/s', '').split('+');
-          inf.push(tmpArr[0]);
-        });
-        row.variants = [{ text: inf.join(' | ') }];
-      }
+      // if (row.variantInformation) {
+      //   row.variantInformation.forEach(item => {
+      //     item.text = item.text.replace(' - Selected', '');
+      //   });
+      // }
+      // if (row.variants) {
+      //   const inf = [];
+      //   row.variants.forEach(item => {
+      //     const tmpArr = item.text.replace('/productimages/sku/s', '').split('+');
+      //     inf.push(tmpArr[0]);
+      //   });
+      //   row.variants = [{ text: inf.join(' | ') }];
+      // }
     }
   }
   return cleanUp(data);
