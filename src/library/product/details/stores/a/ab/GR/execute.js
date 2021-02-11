aeedfe6d4d19de28718343762ea8aa891ed90d4b
sujoypal144 <sujoypal144@gmail.com>
2021-02-12 04:01:53 +0530

    Modified the xpaths for the attributes.

--- src/library/product/details/stores/a/ab/GR/execute.js
+++ src/library/product/details/stores/a/ab/GR/execute.js
@@ -5,7 +5,9 @@ module.exports = {
     country: 'GR',
     store: 'ab',
     domain: 'ab.gr',
-    loadedSelector: 'h1.page-title',
-    noResultsXPath: '//div[contains(@class,"ErrorPage")] | //meta[contains(@content,"errorPage")]',
+    // loadedSelector: 'h1.page-title',
+    loadedSelector: 'h1.page-title , #__next',
+    // noResultsXPath: '//div[contains(@class,"ErrorPage")] | //meta[contains(@content,"errorPage")]',
+    noResultsXPath: '//div[contains(@class,"ErrorPage")] | //meta[contains(@content,"errorPage")] | //div[contains(@class,"NoSearchResultsMessage")]',
   },
 };
