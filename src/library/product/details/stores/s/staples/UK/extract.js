6f3436cefe5ca9790137d3b56bafc689e3b0a5fb
Amit Mahapatra <amit.mahapatra@hc.import.io>
2021-02-11 19:14:54 +0530

    added js code for mpc, color, weightNet

--- src/library/product/details/stores/s/staples/UK/extract.js
+++ src/library/product/details/stores/s/staples/UK/extract.js
@@ -1,19 +1,19 @@
 
 // @ts-ignore
-const { transform } = require('../../../../shared');
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'UK',
     store: 'staples',
-    transform: transform,
+    transform: cleanUp,
     domain: 'staples.co.uk',
     zipcode: '',
   },
   // @ts-ignore
   implementation: async ({ inputString }, { country, store, transform: transformParam }, context, { productDetails }) => {
     await context.evaluate(async function () {
-      function addElementToDocument (key, value) {
+      function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
         catElement.textContent = value;
@@ -29,6 +29,27 @@ module.exports = {
       };
       const price = getXpath('//div[@class="formRow"]//span[@id="SkuPriceUpdate"]//text()', 'nodeValue');
       const pricecurrency = getXpath('//div[@class="formRow"]//span[@content="GBP"]//text()', 'nodeValue');
+      const weightNet = getXpath('//table[@class="table specficsWrapper"]//tbody//tr//td//span[contains(text(),"Weight")]//following-sibling::text()', 'nodeValue');
+      // console.log('weightNet: ', weightNet);
+      if (weightNet !== null) {
+        const finalWeightNet = weightNet.replace(':', '');
+        // console.log('Final', finalWeightNet);
+        addElementToDocument('added_weightNet', finalWeightNet);
+      }
+      const color = getXpath('//table[@class="table specficsWrapper"]//tbody//tr//td//span[contains(text(),"Colour")]//following-sibling::text()', 'nodeValue');
+      // console.log('weightNet: ', weightNet);
+      if (color !== null) {
+        const finalColor = color.replace(':', '');
+        // console.log('Final', finalWeightNet);
+        addElementToDocument('added_color', finalColor);
+      }
+      const mpcpath = getXpath('//*[@id="PageInner"]/script[2]/text()', 'nodeValue');
+      if (mpcpath) {
+        const jsonObj = JSON.parse(mpcpath);
+        var mpc = jsonObj.model;
+        var mpc2 = mpc.split(':');
+        addElementToDocument('mpc_added', mpc2[1]);
+      }
       addElementToDocument('added_onlinepricecurrency', pricecurrency + price);
       if (document.querySelector('input[class="accept-all-cookies"]')) {
         // @ts-ignore
