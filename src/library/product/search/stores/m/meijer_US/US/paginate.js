8f41fca63e1561e40f66ad142a5a13d802074adc
Brinda <brinda.allamsetty@od.import.io>
2021-02-11 10:28:24 +0530

    search updated

--- src/library/product/search/stores/m/meijer_US/US/paginate.js
+++ src/library/product/search/stores/m/meijer_US/US/paginate.js
@@ -9,7 +9,7 @@ module.exports = {
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: null,
-    loadedXpath: '//div[@class="product-item"]',
+    loadedXpath: null,
     noResultsXPath: null,
     stopConditionSelectorOrXpath: null,
     resultsDivSelector: null,
