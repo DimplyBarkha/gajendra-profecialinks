cb5b1d13b8f04c0c1fa7f2bd2d1708c463f5c81d
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-11 16:57:47 +0530

    updated brand

--- src/library/product/details/stores/m/medpex/DE/extract.js
+++ src/library/product/details/stores/m/medpex/DE/extract.js
@@ -24,9 +24,13 @@ module.exports = {
         return result && result.trim ? result.trim() : result;
       };
       try {
-        const brand = getXpath('//span[@class="productDetails-name"]//text()', 'nodeValue');
-        var brand1 = brand.split(" ");
-        addElementToDocument('brand1', brand1[0])
+        const brand = getXpath('//span[@itemprop="brand"]/text()', 'nodeValue');
+        addElementToDocument('brand1', brand);
+        if (brand == null) {
+          const brand = getXpath('//span[@class="productDetails-name"]//text()', 'nodeValue');
+          var brand1 = brand.split(" ");
+          addElementToDocument('brand1', brand1[0])
+        }
       }
       catch (e) {
 
