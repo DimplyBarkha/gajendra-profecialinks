04c0de447d6702a64ed0f4bbb080334c96329502
Jeet2804 <jitendra.singh@vyomlabs.com>
2021-02-11 14:52:24 +0530

    #1345 datart

---
+++ src/library/navigation/goto/domains/da/datart.cz.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'datart.cz',
+    timeout: null,
+    jsonToTable: null,
+    country: 'CZ',
+    store: 'datart',
+    zipcode: '',
+  },
+};
