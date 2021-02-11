e85c0c66c0b8ca3f78a2cf14aff049e04c091335
Karina Miernik <karina.miernik@gmail.com>
2021-02-11 12:58:43 +0100

    created reviews extractor for mytime RD

---
+++ src/library/product/reviews/stores/m/mytime/DE/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/reviews/extract',
+  parameterValues: {
+    country: 'DE',
+    store: 'mytime',
+    transform: null,
+    domain: 'mytime.de',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/reviews/stores/m/mytime/DE/paginate.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  implements: 'navigation/paginate',
+  parameterValues: {
+    template: null,
+    country: 'DE',
+    store: 'mytime',
+    nextLinkSelector: null,
+    nextLinkXpath: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    loadedXpath: null,
+    noResultsXPath: null,
+    stopConditionSelectorOrXpath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'mytime.de',
+    zipcode: '',
+  },
+};
