d720f52238bbbfe112446c8e99fd5d66fd90fcff
Shekhar Gupta <shekhar.gupta@hcl.com>
2021-02-11 11:12:32 +0530

    fix for goto

--- src/library/navigation/goto/domains/am/amazon.com.au.js
+++ src/library/navigation/goto/domains/am/amazon.com.au.js
@@ -1,11 +1,18 @@
-
 module.exports = {
-  implements: 'navigation/goto',
+  extends: 'navigation/goto/domains/am/amazon',
   parameterValues: {
-    domain: 'amazon.com.au',
-    timeout: 50000,
     country: 'AU',
+    domain: 'amazon.com.au',
     store: 'amazon',
-    zipcode: '',
   },
 };
+// module.exports = {
+//   implements: 'navigation/goto',
+//   parameterValues: {
+//     domain: 'amazon.com.au',
+//     timeout: 50000,
+//     country: 'AU',
+//     store: 'amazon',
+//     zipcode: '',
+//   },
+// };
