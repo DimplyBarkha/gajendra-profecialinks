18e76e4a49827f1bba1f4598afab7890a4316b01
alscotty <alawrencescott@gmail.com>
2021-02-10 17:54:17 -0800

    update captcha logic, working!

--- src/library/navigation/goto/domains/go/google.com.js
+++ src/library/navigation/goto/domains/go/google.com.js
@@ -19,90 +19,73 @@ module.exports = {
       checkedBlocked: true,
       js_enabled: true,
       random_move_mouse: true,
+      checkBlocked: false,
+      antiCaptchaOptions: {
+        provider: 'anti-captcha',
+        type: 'RECAPTCHA',
+        autoSubmit: true,
+      }
     });
 
-    // checkBlocked: false,
-    // antiCaptchaOptions: {
-    //   type: 'RECAPTCHA'
-    // }
 
-    // const recaptchaPresent = await context.evaluate(() => {
-    //   const recaptchaIframe = document.querySelector('div#recaptcha');
-    //   return !!recaptchaIframe;
-    // });
-    // const iframeFound = await context.evaluate(() => {
-    //   const iframe = document.querySelector('iframe');
-    //   return !!iframe;
-    // });
+    const recaptchaPresent = await context.evaluate(() => {
+      const recaptchaIframe = document.querySelector('div#recaptcha');
+      return !!recaptchaIframe;
+    });
+    const iframeFound = await context.evaluate(() => {
+      const iframe = document.querySelector('iframe');
+      return !!iframe;
+    });
 
-    // const captchaSolver = async () => {
-    //   await context.solveCaptcha({ type: 'RECAPTCHA', inputElement: 'div#recaptcha' });
-    // };
+    const captchaSolver = async () => {
+      await context.solveCaptcha({ type: 'RECAPTCHA', inputElement: 'div#recaptcha' });
+    };
 
-    // const currentUrl = async () =>{
-    //    return await context.evaluate(()=> {
-    //     return window.location.href
-    //   });
-    // }
+    const currentUrl = async () =>{
+       return await context.evaluate(()=> {
+        return window.location.href
+      });
+    }
 
-    //   const captchaSolved = async () =>{
-    //     return await context.evaluate(() => {
-    //       return !!document.querySelector('div[captchastatus=ok]');
-    //     })
-    //   }
+      if (recaptchaPresent) {
+        console.log('Hit recaptcha, attempting to solve..');
+        console.log(`iframe present: ${iframeFound}`)
 
-    //   if (recaptchaPresent) {
-    //     console.log('Hit recaptcha, attempting to solve..');
-    //     console.log(`iframe present: ${iframeFound}`)
+        const maxAttempts = 3;
+        let solved = false;
+        let currentAttempts = 1;
 
-    //     const maxAttempts = 3;
-    //     let solved = false;
-    //     let currentAttempts = 1;
+        while (!solved && (currentAttempts < maxAttempts)) {
+          console.log(`on attempt: ${currentAttempts}`);
 
-    //     while (!solved && (currentAttempts < maxAttempts)) {
-    //       console.log(`on attempt: ${currentAttempts}`);
-    //       // Removed click, resolve captcha w/ solveCaptcha instead
-    //       // await context.click('div.captcha-handler',{ timeout: 4000 })
-    //       // .then(async()=>await new Promise(res=>setTimeout(res,2000)))
-    //       // .catch(()=>console.log('No click required'));
+          if (iframeFound){
+            // @ts-ignore      // eslint-disable-next-line no-undef
+            await context.evaluateInFrame('iframe', () => grecaptcha.execute())
+          } else {
+            await captchaSolver()
+          }
 
-    //       if (iframeFound){
-    //         // @ts-ignore      // eslint-disable-next-line no-undef
-    //         await context.evaluateInFrame('iframe', () => grecaptcha.execute())
-    //       } else {
-    //         await captchaSolver()
-    //       }
-    //       if (await captchaSolved()){
-    //         console.log('Captcha solved! Submitting form..')
-    //         await context.evaluate(()=>{
-    //           document.getElementById('captcha-form').submit();
-    //         });
-    //         await new Promise((resolve,reject) => setTimeout(resolve, 5000));
-    //         let captchaGone = await context.evaluate(()=>{
-    //           return !!document.querySelector('div#recaptcha');
-    //         })
+            await new Promise((resolve,reject) => setTimeout(resolve, 5000));
 
-    //         console.log(`Is captcha really gone? ${captchaGone}`)
-    //         let thisUrl = await currentUrl();
-    //         console.log(thisUrl)
-    //         if(captchaGone && !(thisUrl.includes('sorry'))){
-    //           solved= true;
-    //         }
-    //       }
+            let thisUrl = await currentUrl();
+            console.log(thisUrl)
+            if(!(thisUrl.includes('sorry'))){
+              console.log('Captcha solved, and redirected successfully!')
+              solved= true;
+            }
 
-    //       // await context.waitForNavigation({ timeout: 10000, waitUntil: 'load' });
-    //       console.log(await currentUrl())
-    //       await new Promise((resolve,reject) => setTimeout(resolve, 6000));
+          // await context.waitForNavigation({ timeout: 10000, waitUntil: 'load' });
+          console.log(await currentUrl())
+          await new Promise((resolve,reject) => setTimeout(resolve, 6000));
 
-    //       currentAttempts++;
-    //     }
+          currentAttempts++;
+        }
 
-  //     await context.checkBlocked()
-  //       .catch((err) => {
-  //         throw new Error(`Error:${err.message}, failed after ${currentAttempts - 1} attempts`);
-  //       });
-  //     console.log('Solved!');
-  //   }
-  // },
+      await context.checkBlocked()
+        .catch((err) => {
+          throw new Error(`Error:${err.message}, failed after ${currentAttempts - 1} attempts`);
+        });
+      console.log('Solved!');
+    }
   },
 };
