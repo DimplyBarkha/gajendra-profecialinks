f4baa69bd8b634d7241d0eb79f4a0c64b89585dd
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2021-02-11 13:36:31 +0530

    extract.js updated

--- src/library/product/details/stores/s/samsclub/US/extract.js
+++ src/library/product/details/stores/s/samsclub/US/extract.js
@@ -34,16 +34,34 @@ async function implementation(
   await context.evaluate(async function () {
     await new Promise((resolve, reject) => setTimeout(resolve, 30000));
     let getManufatureImageArray = [];
+    let getManufatureImageZoomArray =[];
+    let getManufatureTextArray = [];
     let getContent = document.querySelector('iframe#wcframable1-1');
     const getManufatureImage = getContent.contentDocument.querySelectorAll('div.wc-media-available img');
+    const getManufatureText = getContent.contentDocument.querySelectorAll('div.wc-media-available');
     if(getManufatureImage){
       for (let i = 0; i < getManufatureImage.length; i++) {
         let urls = getManufatureImage[i].src;
         getManufatureImageArray.push(urls);
-        // getManufatureImageArray.join("|");
-
       }
     console.log("inBoxArray >>>>>>>>>>>>>>>>", getManufatureImageArray);
+    }
+
+    if(getManufatureText){
+      for (let i = 0; i < getManufatureText.length; i++) {
+        let text = getManufatureText [i].innerText;
+        getManufatureTextArray.push(text);
+      }
+    console.log("inBoxArrayText>>>>>>>>>>>>>>>>", getManufatureTextArray);
+    }
+
+    const getManufatureImageZoom = getContent.contentDocument.querySelectorAll('div.zoomableContainer img')
+    if(getManufatureImageZoom){
+      for (let i1 = 0; i1 < getManufatureImageZoom.length; i1++) {
+        let urls1 = getManufatureImageZoom[i1].src;
+        getManufatureImageZoomArray.push(urls1);
+      }
+    console.log("inBoxArrayZoom >>>>>>>>>>>>>>>>", getManufatureImageZoomArray);
     }
 
 
@@ -55,7 +73,10 @@ async function implementation(
       document.body.appendChild(newDiv);
     }
 
-    addHiddenDiv('getManufatureImageArray1',getManufatureImageArray);
+    addHiddenDiv('getManufatureImageArray1',getManufatureImageArray.join('  || '));
+    addHiddenDiv('getManufatureImageZoomArray1',getManufatureImageZoomArray.join('  || '));
+    addHiddenDiv('getManufatureTextArray1',getManufatureTextArray.join('  || '));
+
 
   });
   return await context.extract(productDetails, { transform });
