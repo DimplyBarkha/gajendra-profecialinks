080efb8fa8a80cca5eb3e720bf68ed375c8f9ff1
Riddhi Halani <riddhi.halani@mail.codetrade.io>
2021-02-11 17:47:03 +0530

    Initial commit

---
+++ src/library/navigation/goto/domains/in/interspar.at.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'interspar.at',
+    timeout: null,
+    jsonToTable: null,
+    country: 'AT',
+    store: 'interspar',
+    zipcode: '',
+  },
+};
