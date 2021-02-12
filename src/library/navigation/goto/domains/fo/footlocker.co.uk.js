c123c58119a14692a1249d87ca7d4825d3b0bbcd
IpsitaM07 <ipsita.m@osmosys.asia>
2021-02-12 15:42:02 +0530

    Worked on core and variant extractor

--- src/library/navigation/goto/domains/fo/footlocker.co.uk.js
+++ src/library/navigation/goto/domains/fo/footlocker.co.uk.js
@@ -7,4 +7,14 @@ module.exports = {
     store: 'FootLocker',
     zipcode: '',
   },
+  implementation: async ({ url }, parameters, context, dependencies) => {
+    url = `${url}#[!opt!]{"first_request_timeout":90000, "force200": true}[/!opt!]`;
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 100000,
+      waitUntil: 'load',
+    });
+  },
 };
--- src/library/product/details/stores/f/footlocker/UK/variantsExtract.js
+++ src/library/product/details/stores/f/footlocker/UK/variantsExtract.js
@@ -15,29 +15,50 @@ module.exports = {
     context,
     dependencies,
   ) {
+    // await context.evaluate(() => {
+    //   const variantid = [];
+    //   const url = window.location.href;
+    //   const trimmedurl = url.split('=')[0];
+    //   let data = '';
+    //   const getSizeVariants = document.querySelectorAll('#fitanalytics_sizecontainer > section:nth-child(3) > div >button');
+    //   for (let i = 0; i < getSizeVariants.length; i++) {
+    //     data = getSizeVariants[i].getAttribute('data-product-size-select-item');
+    //     if (data) {
+    //       variantid.push(data);
+    //     }
+    //   }
+    //   variantid.forEach((id) => {
+    //     const div = document.createElement('div');
+    //     div.className = 'variantid';
+    //     div.setAttribute('id', id.toString());
+    //     div.setAttribute('url', trimmedurl.toString() + id.toString());
+    //     document.body.appendChild(div);
+    //   });
+    // });
+    await new Promise((resolve, reject) => setTimeout(resolve, 6000));
     await context.evaluate(() => {
-      const variantid = [];
-      const url = window.location.href;
-      const trimmedurl = url.split('=')[0];
-      let data = '';
-      const getSizeVariants = document.querySelectorAll('#fitanalytics_sizecontainer > section:nth-child(3) > div >button');
-      for (let i = 0; i < getSizeVariants.length; i++) {
-        data = getSizeVariants[i].getAttribute('data-product-size-select-item');
-        if (data) {
-          variantid.push(data);
-        }
+      try{
+      const colorvariantElements = document.querySelectorAll('div[class*="owl-stage-outer"]>div>div>a');
+      const colorvariantUrl = [];
+      colorvariantElements.forEach((element) => {
+      // @ts-ignore
+      colorvariantUrl.push(element && element.href);
+    })
+    colorvariantUrl.push(window.location.href);
+    console.log('Color variants found');
+    for (let i = 0; i < colorvariantUrl.length; i++) {
+    const div = document.createElement('div');
+    div.className = 'varianturl';
+    div.setAttribute('url',colorvariantUrl[i]);
+    document.body.appendChild(div);
+    }
       }
-      variantid.forEach((id) => {
-        const div = document.createElement('div');
-        div.className = 'variantid';
-        div.setAttribute('id', id.toString());
-        div.setAttribute('url', trimmedurl.toString() + id.toString());
-        document.body.appendChild(div);
-      });
+    catch(e){
+      console.log('No color variants');
+    }
     });
     const { transform } = parameters;
     const { variants } = dependencies;
     return await context.extract(variants, { transform });
   },
-
 };
