fb747367f9dac55ac6daec9a1287001581aad61f
venkatsn <venkat.sn-odc@partner.import.io>
2021-02-12 19:56:21 +0530

    medpex de

--- src/library/product/search/stores/m/medpex/DE/execute.js
+++ src/library/product/search/stores/m/medpex/DE/execute.js
@@ -5,7 +5,30 @@ module.exports = {
     country: 'DE',
     store: 'medpex',
     domain: 'medpex.de',
-    url: 'https://www.medpex.de/search.do?method=similarity&q={searchTerms}',
+    url: 'https://www.medpex.de/search.do?q={searchTerms}',
     loadedSelector: 'div[id="product-list"] form',
   },
+  implementation,
 };
+async function implementation (
+  inputs,
+  { url, loadedSelector, noResultsXPath },
+  context,
+  dependencies,
+) {
+  const { keywords, query } = inputs;
+  console.log(url);
+  const destinationUrl = url
+    .replace('{searchTerms}', keywords.replace(/\ü/g, '%FC'))
+    //.replace('{searchTerms}', keywords.replace(/ü/g, '%FC'))
+    .replace('{queryParams}', query);
+  await dependencies.goto({ ...inputs, url: destinationUrl });
+
+  if (loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+  console.log(`noResultsXPath: ${noResultsXPath}`);
+  return await context.evaluate((xp) => !document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext(), noResultsXPath);
+}
