a4a229bd15330090e1149fdb9e84d7f42d519d13
Jagruti Chaudhari <jagruti.chaudhary@od.import.io>
2021-02-12 22:57:59 +0530

    heb_mx

--- src/library/product/details/stores/h/heb/MX/extract.js
+++ src/library/product/details/stores/h/heb/MX/extract.js
@@ -4,8 +4,30 @@ module.exports = {
   parameterValues: {
     country: 'MX',
     store: 'heb',
-    transform: null,
+    transform: cleanUp,
     domain: 'heb.com',
     zipcode: '',
+  },
+  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+      function addElementToDocument(key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      try {
+        // @ts-ignore
+      let ingrediants=document.querySelectorAll("div[class='product-tab-content desc']")[0].innerText;
+      let split_ingrediants=ingrediants.split('INGREDIENTES')[1];
+      addElementToDocument('ingrediants', split_ingrediants)
+        
+      } catch (error) {
+        
+      }
+      
+    });
+    await context.extract(productDetails, { transform: transformParam });
   },
 };
