b6e5583506f98e0b0d18d49dd201ac30ffce51de
romancg <gromantsg@gmail.com>
2021-02-12 14:49:28 +0200

    metro ua

---
+++ src/library/navigation/goto/domains/me/metro.zakaz.ua.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'metro.zakaz.ua',
+    timeout: null,
+    jsonToTable: null,
+    country: 'UA',
+    store: 'metro',
+    zipcode: "''",
+  },
+};
---
+++ src/library/navigation/goto/domains/me/setZipCode.metro.zakaz.ua.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'navigation/goto/setZipCode',
+  parameterValues: {
+    country: 'UA',
+    domain: 'metro.zakaz.ua',
+    store: 'metro',
+    zipcode: "''",
+  },
+};
