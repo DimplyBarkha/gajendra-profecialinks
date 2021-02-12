77726e883a49c9022ff362d343fd5886062dcea0
Sindhusha balla <sindhusha.b@osmosys.asia>
2021-02-12 20:36:42 +0530

    Added 3 retries for captcha solving

--- src/library/navigation/goto/domains/co/courir.com.js
+++ src/library/navigation/goto/domains/co/courir.com.js
@@ -40,7 +40,7 @@ module.exports = {
       await context.waitForNavigation({ timeout: 30000 });
     };
 
-    const checkExistance = async (selector) => {
+    const checkExistence = async (selector) => {
       return await context.evaluate(async (sel) => {
         return Boolean(document.querySelector(sel));
       }, selector);
@@ -63,13 +63,13 @@ module.exports = {
 
     const captchaSelector = 'iframe[src*="https://geo.captcha"]';
     await optionalWait(captchaSelector, 60000);
-    const isCaptchaFramePresent = await checkExistance(captchaSelector);
+    const isCaptchaFramePresent = await checkExistence(captchaSelector);
 
     console.log('isCaptcha', isCaptchaFramePresent);
 
     if (isCaptchaFramePresent) {
       try {
-        const isHardBlocked = await context.evaluateInFrame(captchaSelector, function () {
+        let isHardBlocked = await context.evaluateInFrame(captchaSelector, function () {
           if (document.body.innerText.search('Vous avez été bloqué') > -1 || document.body.innerText.search('You have been blocked') > -1) {
             return true;
           }
@@ -81,81 +81,151 @@ module.exports = {
           throw new Error('Blocked');
         }
 
-        await context.evaluateInFrame(
-          captchaSelector,
-          function () {
-            // @ts-ignore
-            const code = geetest
-              .toString()
-              .replace(/appendTo\("#([^"]+)"\)/, 'appendTo(document.getElementById("$1"))');
-            return eval(`(${code})()`);
-          },
-        );
+        async function handleCaptchaClick () {
+          await context.evaluateInFrame(
+            captchaSelector,
+            function () {
+              // @ts-ignore
+              const code = geetest
+                .toString()
+                .replace(/appendTo\("#([^"]+)"\)/, 'appendTo(document.getElementById("$1"))');
+              return eval(`(${code})()`);
+            },
+          );
 
-        await delay(500);
-        await context.evaluateInFrame(captchaSelector, function () {
-          // @ts-ignore
-          document.querySelector('.captcha-handler').click();
-        });
-
-
-
-        // Wait for iframe to disappear
-        let iframeStillExists = await checkExistance(captchaSelector);
-        let isCaptchaSolved = await context.evaluateInFrame(captchaSelector, function () {
-          return Boolean(document.querySelector('div[captchastatus="ok"]'));
-        });
-
-        let isCaptchaFailed = await context.evaluateInFrame(captchaSelector, function () {
-          return Boolean(document.querySelector('div[captchastatus="fail"]'));
-        });
-
-        let counter = 0;
-        while (iframeStillExists && !isCaptchaSolved && !isCaptchaFailed) {
-          iframeStillExists = await checkExistance(captchaSelector);
-          isCaptchaSolved = await context.evaluateInFrame(captchaSelector, function () {
-            return Boolean(document.querySelector('div[captchastatus="ok"]'));
-          });
-          isCaptchaFailed = await context.evaluateInFrame(captchaSelector, function () {
-            return Boolean(document.querySelector('div[captchastatus="fail"]'));
-          });
           await delay(500);
-          ++counter;
+          await context.evaluateInFrame(captchaSelector, function () {
+            // @ts-ignore
+            document.querySelector('.captcha-handler').click();
+          });
+        }
 
-          // Waiting for a minute before throwing an error
-          if (counter === 120 || isCaptchaFailed) {
-            throw new Error('CAPTCHA_FAIL');
+        async function waitForIFrameToDisappear () {
+          let iframeStillExists = await checkExistence(captchaSelector);
+          let isCaptchaSolved = false;
+          let isCaptchaFailed = false;
+          let counter = 0;
+          while (iframeStillExists) {
+            try {
+              isCaptchaSolved = await context.evaluateInFrame(captchaSelector, function () {
+                return Boolean(document.querySelector('div[captchastatus="ok"]'));
+              });
+              if (isCaptchaSolved) {
+                break;
+              }
+            } catch (err) {
+              console.log('Error while breaking the loop when captcha solved');
+            }
+
+            iframeStillExists = await checkExistence(captchaSelector);
+            if (iframeStillExists) {
+              try {
+                isCaptchaFailed = await context.evaluateInFrame(captchaSelector, function () {
+                  return Boolean(document.querySelector('div[captchastatus="fail"]'));
+                });
+              } catch (err) {
+                console.log('Error while breaking the loop when captcha failed to solved');
+              }
+              if (isCaptchaFailed) {
+                throw new Error('CAPTCHA_FAIL');
+              }
+            }
+
+            await delay(500);
+            ++counter;
+
+            // Waiting for a minute before throwing an error
+            if (counter === 120) {
+              throw new Error('CAPTCHA_FAIL');
+            }
           }
+        }
+
+        async function handleCookieClick () {
+          const cookieButtonSelector = 'body > div.optanon-alert-box-wrapper > div.optanon-alert-box-bg > div.optanon-alert-box-button-container > div.optanon-alert-box-button.optanon-button-allow > div > button';
+          const cookieButton = await optionalWait(cookieButtonSelector, 30000);
+          console.log('isCookie', cookieButton);
+
+          if (cookieButton) {
+            try {
+              console.log('Clicking on cookie accept button.');
+              await context.click(cookieButtonSelector);
+            } catch (err) {
+              console.log('Error while accepting cookies.');
+            }
+          }
+        }
+
+        await handleCaptchaClick();
+
+        try {
+          // Wait for iframe to disappear
+          await waitForIFrameToDisappear();
+        } catch (err) {
+          throw err;
         }
 
         console.log('Captcha Resolved.');
-
         await context.waitForNavigation({ timeout: 30000 });
         // we may be navigated to an index page after captcha solve
-        const productPageSelector = '#product-content';
+        const productPageSelector = '#search-result-items, #product-content';
         const isWaitProduct = await optionalWait(productPageSelector, 30000);
 
         console.log('isProduct', isWaitProduct);
-        const cookieButtonSelector = 'body > div.optanon-alert-box-wrapper > div.optanon-alert-box-bg > div.optanon-alert-box-button-container > div.optanon-alert-box-button.optanon-button-allow > div > button';
-        const cookieButton = await optionalWait(cookieButtonSelector, 30000);
-        console.log('isCookie', cookieButton);
 
-        if (cookieButton) {
-          try {
-            console.log('Clicking on cookie accept button.');
-            await context.click(cookieButtonSelector);
-          } catch (err) {
-            console.log('Error while accepting cookies.');
-          }
-        }
+        await handleCookieClick();
 
         if (!isWaitProduct) {
           await gotoPage();
-          const isCaptchaFramePresent = await checkExistance(captchaSelector);
+          const isCaptchaFramePresent = await checkExistence(captchaSelector);
           console.log('SecondCaptchaFrame', isCaptchaFramePresent);
           if (isCaptchaFramePresent) {
-            throw new Error('got captcha a second time');
+            await handleCaptchaClick();
+            try {
+              // Wait for iframe to disappear
+              await waitForIFrameToDisappear();
+            } catch (err) {
+              throw err;
+            }
+
+            console.log('Second Captcha Resolved.');
+            await context.waitForNavigation({ timeout: 30000 });
+            // we may be navigated to an index page after captcha solve
+            const productPageSelector = '#search-result-items, #product-content';
+            const isWaitProduct = await optionalWait(productPageSelector, 30000);
+
+            console.log('isProduct', isWaitProduct);
+            await handleCookieClick();
+            if (!isWaitProduct) {
+              await gotoPage();
+              const isCaptchaFramePresent = await checkExistence(captchaSelector);
+              console.log('ThirdCaptchaFrame', isCaptchaFramePresent);
+              if (isCaptchaFramePresent) {
+                await handleCaptchaClick();
+                try {
+                  // Wait for iframe to disappear
+                  await waitForIFrameToDisappear();
+                } catch (err) {
+                  throw err;
+                }
+                console.log('Third Captcha Resolved.');
+                await context.waitForNavigation({ timeout: 30000 });
+                // we may be navigated to an index page after captcha solve
+                const productPageSelector = '#search-result-items, #product-content';
+                const isWaitProduct = await optionalWait(productPageSelector, 30000);
+                console.log('isProduct', isWaitProduct);
+                await handleCookieClick();
+                if (!isWaitProduct) {
+                  await gotoPage();
+                  const isCaptchaFramePresent = await checkExistence(captchaSelector);
+                  if (isCaptchaFramePresent) {
+                    throw new Error('got captcha a forth time');
+                  }
+                }
+              }
+            }
           }
+
         }
       } catch (error) {
         console.log('CAPTCHA error: ', error);
