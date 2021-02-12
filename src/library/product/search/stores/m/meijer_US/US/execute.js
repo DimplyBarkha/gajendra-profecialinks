261d6253d2cf6c0d792baa4a4def90a20e46f6ed
Brinda <brinda.allamsetty@od.import.io>
2021-02-12 20:33:46 +0530

    search updated

--- src/library/product/search/stores/m/meijer_US/US/execute.js
+++ src/library/product/search/stores/m/meijer_US/US/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'meijer_US',
     domain: 'meijer.com',
     url: 'https://www.meijer.com/shop/en/search/?q={searchTerms}&page=0',
-    loadedSelector: 'div[class="product-tile-container"] div[class="product-item"]',
+    loadedXpath: '//div[@class="product-item"]',
     noResultsXPath: '//div[@class="search-empty row "]',
     zipcode: '',
   },
