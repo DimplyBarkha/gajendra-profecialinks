aca190075f044a926da90d33aa375999728ac8a0
nehashende <neha.shende@synerzip.com>
2021-02-12 16:11:50 +0530

    Updated variant extractor code.

--- src/library/product/details/stores/b/bestbuy/US/variantsExtract.js
+++ src/library/product/details/stores/b/bestbuy/US/variantsExtract.js
@@ -13,12 +13,24 @@ async function implementation(
       newDiv.style.display = 'none';
       return newDiv;
     }
+    function addHiddenDivs (id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      document.body.appendChild(newDiv);
+    }
+    const variantUrls = [];
+    const variantUrlArr = [];
     let url = window.location.href;
     const outerDiv = addHiddenDiv('variants_outer', '');
     if (document.querySelector('div.shop-product-variations a.carousel-anchor')) {
       document.querySelectorAll('div.shop-product-variations a.carousel-anchor').forEach(variant => {
         const innerDiv = addHiddenDiv('variants_ul', '');
-        const skuId = variant.href.replace(/.*skuId=(.*)/, '$1');
+        let skuId = variant.href.replace(/.*skuId=(.*)/, '$1');
+        if (skuId.match(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)) {
+          skuId = skuId.match(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)[0];
+        }
         const skudiv = addHiddenDiv('sku_id', skuId);
         let sku = variant.href;
         if (sku.includes('?')) {
@@ -31,10 +43,41 @@ async function implementation(
         innerDiv.appendChild(skuUrl);
         outerDiv.appendChild(innerDiv);
       });
+    } else {
+      const allScripts = document.querySelectorAll('script');
+      if (allScripts) {
+        for (let i = 0; i < allScripts.length; i++) {
+          if (allScripts[i].innerText.includes('seoPdpUrl')) {
+            variantUrls.push(allScripts[i].innerText.match(/seoPdpUrl\\":\\"(.*?)\\",/g));
+          }
+        }
+        if (variantUrls[0]) {
+          variantUrlArr.push(...variantUrls[0]);
+        }
+        if (variantUrlArr.length) {
+          for (let i = 0; i < variantUrlArr.length; i++) {
+            let variantUrl = variantUrlArr[i].replace(/seoPdpUrl\\":\\"/g, '').replace(/\\"/g, '').replace(/\\/g, '').replace(/u002Fu002F/, '//').replace(/u002Fu002F/g, '').replace(/u002F/g, '/').replace(',', '');
+            if (variantUrl.includes('?')) {
+              variantUrl = `${variantUrl}&intl=nosplash`;
+            } else {
+              variantUrl = `${variantUrl}?intl=nosplash`;
+            }
+            let variantId = variantUrlArr[i].replace(/.*skuId=(.*)/, '$1').replace(/\\/g, '').replace('/', '').replace(',', '').replace('"', '');
+            if (variantId.match(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)) {
+              variantId = variantId.match(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)[0];
+            }
+            addHiddenDivs('sku_url2', variantUrl);
+            addHiddenDivs('sku_id2', variantId);
+          };
+        }
+      }
     }
     const innerDiv = addHiddenDiv('variants_ul', '');
     let sku = window.location.href
-    const skuId = sku.replace(/.*skuId=(.*)/, '$1');
+    let skuId = sku.replace(/.*skuId=(.*)/, '$1');
+    if (skuId.match(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)) {
+      skuId = skuId.match(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)[0];
+    }
     const skudiv = addHiddenDiv('sku_id', skuId.replace(/#(.*)/g, ''));
     if (sku.includes('?')) {
       sku = `${sku.replace(/#(.*)/g, '')}&intl=nosplash`;
