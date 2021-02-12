114a13da414a394ff25feb6eb81940387aded322
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-12 15:57:46 +0530

    Added code to avoid loading issues in future

--- src/library/navigation/goto/domains/ac/aceuae.com.js
+++ src/library/navigation/goto/domains/ac/aceuae.com.js
@@ -3,9 +3,30 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'aceuae.com',
-    timeout: 50000,
+    timeout: 120000,
     zipcode: '',
     country: 'UAE',
     store: 'acehardware',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.setFirstRequestTimeout(100000);
+    await context.setCssEnabled(true);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    // await context.goto(url,
+    //   {
+    //     timeout: 150000,
+    //     waitUntil: 'networkidle0',
+    //   });
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
