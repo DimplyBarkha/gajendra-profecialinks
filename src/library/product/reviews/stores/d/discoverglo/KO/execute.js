ba69dd08b387a193f08bc8da1db1a68b637937d6
Avinash <avinash.kadam@profecialinks.com>
2021-02-12 19:14:51 +0530

    added condition for querySelector

--- src/library/product/reviews/stores/d/discoverglo/KO/execute.js
+++ src/library/product/reviews/stores/d/discoverglo/KO/execute.js
@@ -46,13 +46,21 @@ async function implementation(
     }
     if (document.querySelector('input#juminsag')) {
       document.getElementById('juminsag').value = '19840101';
-      if (document.querySelector('button#entrance')) {
-        document.querySelector('button#entrance').click();
-      }
+      // if (document.querySelector('button#entrance')) {
+      //   document.querySelector('button#entrance').click();
+      // }
     }
   });
 
-  await context.waitForNavigation({ timeout: 10000, waitUntil: 'load' });
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  try {
+    await context.waitForSelector('button#entrance', { timeout: 1000 });
+    await context.click('button#entrance');
+  } catch (e) {
+    console.log('cookies not loaded');
+  }
+
+  await context.waitForNavigation({ timeout: 2000, waitUntil: 'load' });
 
   await context.evaluate(async function () {
     function addHiddenDiv(id, reviewData) {
@@ -66,39 +74,42 @@ async function implementation(
     const url = window.location.href;
     const urlParams = new URLSearchParams(window.location.search);
     const page = Number(urlParams.get('page'));
-    const reviewString = document.querySelector('div.review_topicon a span').textContent;
-    const totalReviewsCount = Number(reviewString.replace(/,/g, ''));
-    if (totalReviewsCount > 0 && page > 0) {
-      // const itemStr = url.match(/(\d+).html/g)[0].replace('.html', '');
-      const itemStr = url.match(/(?<=--)(.*)(?=.html)/g)[0];
-      const apiUrl = `https://www.discoverglo.co.kr/store/ajax.cmt.list.php?pdtIdx=${itemStr}&cmt2page=${page}`;
-      console.log(`PAGE#:${page}`);
-      console.log('itemStr#: ', itemStr);
+    if (document.querySelector('div.review_topicon a span')) {
+      const reviewString = document.querySelector('div.review_topicon a span').textContent;
+      const totalReviewsCount = Number(reviewString.replace(/,/g, ''));
+      if (totalReviewsCount > 0 && page > 0) {
+        // const itemStr = url.match(/(\d+).html/g)[0].replace('.html', '');
+        const itemStr = url.match(/(?<=--)(.*)(?=.html)/g)[0];
+        const apiUrl = `https://www.discoverglo.co.kr/store/ajax.cmt.list.php?pdtIdx=${itemStr}&cmt2page=${page}`;
+        console.log(`PAGE#:${page}`);
+        console.log('itemStr#: ', itemStr);
 
-      const response = await fetch(apiUrl, {
-        headers: {
-          accept: '*/*',
-          'accept-language': 'en-US,en;q=0.9',
-          'sec-fetch-dest': 'script',
-          'sec-fetch-mode': 'no-cors',
-          'sec-fetch-site': 'cross-site',
-        },
-        referrer: url,
-        referrerPolicy: 'no-referrer-when-downgrade',
-        body: null,
-        method: 'GET',
-        mode: 'cors',
-      });
-      if (!response || response.status !== 200) {
-        return false;
-      } else {
-        const data = await response.json();
-        console.log('API called! Adding data..');
-        if (data) {
-          addHiddenDiv('my-reviews', data.msg);
+        const response = await fetch(apiUrl, {
+          headers: {
+            accept: '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'sec-fetch-dest': 'script',
+            'sec-fetch-mode': 'no-cors',
+            'sec-fetch-site': 'cross-site',
+          },
+          referrer: url,
+          referrerPolicy: 'no-referrer-when-downgrade',
+          body: null,
+          method: 'GET',
+          mode: 'cors',
+        });
+        if (!response || response.status !== 200) {
+          return false;
+        } else {
+          const data = await response.json();
+          console.log('API called! Adding data..');
+          if (data) {
+            addHiddenDiv('my-reviews', data.msg);
+          }
         }
       }
     }
+
   });
 
   async function checkDate() {
