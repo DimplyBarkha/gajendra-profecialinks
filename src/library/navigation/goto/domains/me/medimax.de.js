053654e84409e1cee82a08d06f154ceadabfb044
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-12 16:48:24 +0530

    updated the code to avoid loading issues

--- src/library/navigation/goto/domains/me/medimax.de.js
+++ src/library/navigation/goto/domains/me/medimax.de.js
@@ -3,9 +3,30 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'medimax.de',
-    timeout: null,
+    timeout: 120000,
     country: 'DE',
     store: 'medimax',
     zipcode: '',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 100000;
+    await context.setBlockAds(false);
+    await context.setFirstRequestTimeout(100000);
+    await context.setCssEnabled(true);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    // await context.goto(url,
+    //   {
+    //     timeout: 150000,
+    //     waitUntil: 'networkidle0',
+    //   });
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
--- src/library/product/search/stores/m/medimax/DE/execute.js
+++ src/library/product/search/stores/m/medimax/DE/execute.js
@@ -10,4 +10,38 @@ module.exports = {
     noResultsXPath: '//h1[@class="search-no-hits-headline"]',
     zipcode: '',
   },
+  implementation: async function implementation (
+    inputs,
+    parameters,
+    context,
+    dependencies,
+  ) {
+    await new Promise(resolve => setTimeout(resolve, 4000));
+    try {
+      await context.click('button.cookies-overlay-dialog__accept-all-btn');
+    } catch (err) {
+      console.log(err);
+    }
+
+    console.log('params', parameters);
+    const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+    await dependencies.goto({ url, zipcode: inputs.zipcode });
+    if (parameters.loadedSelector) {
+      try {
+        await context.waitForFunction(function (sel, xp) {
+          return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+        }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+      } catch(err) {
+        console.log('we got some error while waiting', err.message);
+      }
+    }
+    console.log('Checking no results', parameters.noResultsXPath);
+    return await context.evaluate(function (xp) {
+      const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+      console.log(xp, r);
+      const e = r.iterateNext();
+      console.log(e);
+      return !e;
+    }, parameters.noResultsXPath);
+  }
 };
