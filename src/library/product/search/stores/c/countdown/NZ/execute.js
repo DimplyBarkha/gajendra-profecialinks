b70d85747dc6cbbf05eef4d1380e5b1da909a7e2
sivasoarlogic <siva@soarlogic.com>
2021-02-12 13:24:25 +0530

    QAfixs

--- src/library/product/search/stores/c/countdown/NZ/execute.js
+++ src/library/product/search/stores/c/countdown/NZ/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'NZ',
     store: 'countdown',
     domain: 'countdown.co.nz',
-    url: 'https://shop.countdown.co.nz/shop/searchproducts?search={searchTerms}',
+    url: 'https://shop.countdown.co.nz/shop/searchproducts?search={searchTerms}&page=1&size=120',
     loadedSelector: '//wnz-search[@class="ng-star-inserted"]',
     noResultsXPath: '//search-no-results[@class="ng-star-inserted"]',
     zipcode: '',
