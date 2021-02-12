185c17f4ce17bdbdf077167eb5c046e541000828
Abhishek Kumar Mishra <abhishekkumar_mishra@hcl.com>
2021-02-12 11:24:57 +0530

    change the extractor for AmazonLg

--- src/library/product/search/stores/a/amazonLg/IN/execute.js
+++ src/library/product/search/stores/a/amazonLg/IN/execute.js
@@ -1,11 +1,25 @@
+// module.exports = {
+//   implements: 'product/search/execute',
+//   parameterValues: {
+//     country: 'IN',
+//     store: 'amazonLg',
+//     domain: 'amazon.in',
+//     url: 'https://www.amazon.in/s?k={searchTerms}&rh=p_89%3ALG',
+//     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+//     noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
+//     zipcode: '',
+//   },
+// };
+
+
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
     country: 'IN',
     store: 'amazonLg',
     domain: 'amazon.in',
-    url: 'https://www.amazon.in/s?k={searchTerms}&rh=p_89%3ALG',
-    loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+    url: 'https://www.amazon.in/s?k={searchTerms}&ref=nb_sb_noss_2&dc',
+    loadedSelector: 'div[data-asin], section.ebx-empathy-x__body',
     noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
     zipcode: '',
   },
--- src/library/product/search/stores/a/amazonLg/IN/paginate.js
+++ src/library/product/search/stores/a/amazonLg/IN/paginate.js
@@ -1,21 +1,37 @@
+// module.exports = {
+//   implements: 'product/search/paginate',
+//   parameterValues: {
+//     country: 'IN',
+//     store: 'amazonLg',
+//     nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
+//     nextLinkXpath: null,
+//     mutationSelector: null,
+//     spinnerSelector: 'div.s-result-list-placeholder:not(.aok-hidden)',
+//     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
+//     loadedXpath: null,
+//     noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
+//     stopConditionSelectorOrXpath: null,
+//     resultsDivSelector: null,
+//     openSearchDefinition: {
+//       template: 'https://www.amazon.in/s?k={searchTerms}&rh=p_89%3ALG&dc&qid=1602841162&rnid=2528832011&ref=sr_nr_p_89_3&page={page}',
+//     },
+//     domain: 'amazon.in',
+//     zipcode: '',
+//   },
+// };
+
 module.exports = {
   implements: 'product/search/paginate',
   parameterValues: {
     country: 'IN',
     store: 'amazonLg',
     nextLinkSelector: '#pagnNextString, #pagnNextLink, ul.a-pagination li.a-last:not(.a-disabled) a',
-    nextLinkXpath: null,
-    mutationSelector: null,
     spinnerSelector: 'div.s-result-list-placeholder:not(.aok-hidden)',
+    // Use openSearchDefinition if nextLink has navigation issues.
     loadedSelector: 'div[data-asin][data-component-type=s-search-result]',
-    loadedXpath: null,
-    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
-    stopConditionSelectorOrXpath: null,
-    resultsDivSelector: null,
     openSearchDefinition: {
-      template: 'https://www.amazon.in/s?k={searchTerms}&rh=p_89%3ALG&dc&qid=1602841162&rnid=2528832011&ref=sr_nr_p_89_3&page={page}',
+      template: 'https://www.amazon.in/s?k={searchTerms}&ref=nb_sb_noss_2&page={page}',
     },
-    domain: 'amazon.in',
-    zipcode: '',
+    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
   },
 };
