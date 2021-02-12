4e8bbef4de8a725ed2c02b737ba1f6887ad0a8dd
Amit Ranjan <amit.ranjan@RFLX-AmitMacBook.local>
2021-02-12 15:08:46 +0530

    cleanit supply search fiixes

--- src/library/product/search/stores/c/cleanitsupply/US/execute.js
+++ src/library/product/search/stores/c/cleanitsupply/US/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'cleanitsupply',
     domain: 'cleanitsupply.com',
     url: 'https://www.cleanitsupply.com/nsearch.aspx?keywords={searchTerms}&ip=60',
-    loadedSelector: 'ul.small-block-grid-2.medium-block-grid-3.large-block-grid-4.text-center.item-grid li',
+    loadedSelector: 'ul.small-block-grid-2.medium-block-grid-3.large-block-grid-4.text-center.item-grid li.item',
     noResultsXPath: '//span[contains(text()," Your search did not return an exact match.")]',
     zipcode: '',
   },
--- src/library/product/search/stores/c/cleanitsupply/US/extract.js
+++ src/library/product/search/stores/c/cleanitsupply/US/extract.js
@@ -1,4 +1,23 @@
 const { transform } = require('../shared');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await new Promise(resolve => setTimeout(resolve, 4000));
+  await context.evaluate(async function () {
+    const overlay = document.querySelector('div.bx-slab div.bx-wrap div.bx-row-submit-custom button.bx-button[data-click="close"]');
+    if (overlay) {
+      overlay.click();
+      await new Promise(resolve => setTimeout(resolve, 2000));
+    }
+  });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +27,5 @@ module.exports = {
     domain: 'cleanitsupply.com',
     zipcode: '',
   },
+  implementation,
 };
