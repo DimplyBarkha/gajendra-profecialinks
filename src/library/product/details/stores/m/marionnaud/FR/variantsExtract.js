d98ac718519a542598c1dd049cf5a8ccad4ffd2c
pavitra r <pavithra.ramasamy-odc@partner.import.io>
2021-02-12 18:34:00 +0530

    created variant extractor - akshay channa

---
+++ src/library/product/details/stores/m/marionnaud/FR/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'FR',
+    store: 'marionnaud',
+    transform: null,
+    domain: 'marionnaud.fr',
+    zipcode: '',
+  },
+};
