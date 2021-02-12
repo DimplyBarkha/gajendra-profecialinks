330478446b2fb06ba46e9e3832b3bc93c446cdda
srishivams <srishivams97@gmail.com>
2021-02-12 11:51:33 +0530

    fixed issue with product page navigation

--- src/library/product/details/stores/j/jumia/EG/extract.js
+++ src/library/product/details/stores/j/jumia/EG/extract.js
@@ -1,5 +1,4 @@
 const { transform } = require('./shared');
-
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -8,5 +7,23 @@ module.exports = {
     transform,
     domain: 'jumia.com',
     zipcode: '',
+  },
+  implementation: async (inputs,
+    parameters,
+    context,
+    dependencies,
+  ) => {
+    const { productDetails } = dependencies;
+    await context.evaluate(async function () {
+      // const { transform } = parameters;
+      if (document.querySelector('div[id="catalog-listing"]')) { // checking if a listing page
+        console.log('we are on listing page');
+        if (document.querySelector('div[class*="-paxs row"]') && document.querySelector('div[class*="-paxs row"] article[class*="prd"]')) {
+          console.log('we are inside listing div');
+          document.querySelector('div[class*="-paxs row"] article[class*="prd"] a[class*="core"]').click();
+        }
+      }
+    });
+    return await context.extract(productDetails, { transform });
   },
 };
