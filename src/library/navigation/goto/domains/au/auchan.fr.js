60916b2c5c6ab707f5421130b8e9c29b3cd5c7fa
nilofarsh <nilofar.shaikh@synerzip.com>
2021-02-12 22:41:03 +0530

    --amend

--- src/library/navigation/goto/domains/au/auchan.fr.js
+++ src/library/navigation/goto/domains/au/auchan.fr.js
@@ -17,6 +17,15 @@ module.exports = {
     await context.setLoadImages(true);
     await context.setBypassCSP(true);
     await context.setUseRelayProxy(false);
-    await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: false });
+    const lastResponseData = await context.goto(url, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: false });
+    try {
+      console.log('lastResponseData.status', lastResponseData.status);
+      if (lastResponseData.status === 404) {
+        console.log('Blocked: ' + lastResponseData.status);
+        return context.reportBlocked(lastResponseData.status, 'Blocked: ' + lastResponseData.status);
+      }
+    } catch (error) {
+      console.log('ResponseData status is not available');
+    }
   },
 };
