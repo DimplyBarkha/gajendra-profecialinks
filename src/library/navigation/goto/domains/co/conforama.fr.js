c33c6a5b39922f37aac1e6f91a82bfc56e30038b
sahoojudhi <judhishtor@profecialinks.com>
2021-02-12 19:07:45 +0530

    Enhanced Content

--- src/library/navigation/goto/domains/co/conforama.fr.js
+++ src/library/navigation/goto/domains/co/conforama.fr.js
@@ -9,4 +9,13 @@ module.exports = {
     first_request_timeout: 90000,
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  },
 };
