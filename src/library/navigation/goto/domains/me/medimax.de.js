a34a727f961f1d29de15f5b7b55c9769b7590a6c
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-12 22:12:57 +0530

    Added code to avoid loading issue

--- src/library/navigation/goto/domains/me/medimax.de.js
+++ src/library/navigation/goto/domains/me/medimax.de.js
@@ -3,14 +3,26 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'medimax.de',
-    timeout: 20000,
+    timeout: 120000,
     country: 'DE',
     store: 'medimax',
     zipcode: '',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    const timeout = parameters.timeout ? parameters.timeout : 100000;
     await context.setBlockAds(false);
+    await context.setFirstRequestTimeout(100000);
+    await context.setCssEnabled(true);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    // await context.goto(url,
+    //   {
+    //     timeout: 150000,
+    //     waitUntil: 'networkidle0',
+    //   });
     await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
     console.log(zipcode);
     if (zipcode) {
