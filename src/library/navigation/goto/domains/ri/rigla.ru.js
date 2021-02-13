c9c8a458bc78126fcce2c74763218ce404c609a4
Suryakanta Sahoo <suryakanta.s@osmosys.asia>
2021-02-13 20:31:33 +0530

    fixed loading of page issue

--- src/library/navigation/goto/domains/ri/rigla.ru.js
+++ src/library/navigation/goto/domains/ri/rigla.ru.js
@@ -3,9 +3,18 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'rigla.ru',
-    timeout: 50000,
+    timeout: 60000,
     country: 'RU',
     store: 'rigla',
     zipcode: "''",
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 100000;
+    await context.setAntiFingerprint(false);
+    await context.setLoadAllResources(true);
+    await context.setBlockAds(false);
+    await context.setLoadImages(true);
+    await context.setFirstRequestTimeout(60000);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    },
 };
