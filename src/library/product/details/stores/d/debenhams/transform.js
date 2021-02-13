5795e4ce185f72c1045dfdbc803f6df24ee20fcd
Sweeti Kothari <sweeti@fluentcode.io>
2021-02-13 13:56:35 +0530

    extra space removed

--- src/library/product/details/stores/d/debenhams/transform.js
+++ src/library/product/details/stores/d/debenhams/transform.js
@@ -5,7 +5,7 @@
  */
 const transform = (data) => {
     const clean = (text) =>
-      text
+        text
         .toString()
         .replace(/\r\n|\r|\n/g, " ")
         .replace(/&amp;nbsp;/g, " ")
@@ -18,34 +18,36 @@ const transform = (data) => {
         // eslint-disable-next-line no-control-regex
         .replace(/[\x00-\x1F]/g, "")
         .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, " ");
-    for (const { group } of data) {
-      for (const row of group) {
+    for (const { group }
+        of data) {
+        for (const row of group) {
 
-        if(row.alternateImages) {
-            row.alternateImages = row.alternateImages.map(x => ({
-                ...x,
-                text: `https:${x.text}`.replace("w=130", "w=1500").replace("h=130", "h=1500")
+            if (row.alternateImages) {
+                row.alternateImages = row.alternateImages.map(x => ({
+                    ...x,
+                    text: `https:${x.text}`.replace("w=130", "w=1500").replace("h=130", "h=1500")
+                }));
+            }
+            if (row.image) {
+                row.image.forEach(element => {
+                    element.text = `https:${element.text}`
+                });
+            }
+            if (row.variantInformation) {
+                row.variantInformation.forEach(x => {
+                    x.text = x.text.split(",").join(" / ");
+                });
+            }
+            if (row.brandText) {
+                row.brandText = row.brandText.toString().trim()
+            }
+            Object.keys(row).forEach(header => row[header].forEach(el => {
+                el.text = clean(el.text);
             }));
         }
-        if (row.image) {
-          row.image.forEach(element => {
-            element.text = `https:${element.text}`
-          });
-        }
-        if (row.variantInformation) {
-          row.variantInformation.forEach(x => {
-            x.text = x.text.split(",").join(" / ");
-          });
-        }
-
-        Object.keys(row).forEach(header => row[header].forEach(el => {
-          el.text = clean(el.text);
-        }));
-      }
     }
-  
+
     return data;
-  };
-  
-  module.exports = { transform };
-  
+};
+
+module.exports = { transform };
