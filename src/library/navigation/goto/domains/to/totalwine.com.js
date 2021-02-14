66ccf8ab20ec834bc7ea57fde5a58b6c7f94707c
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-14 19:04:11 +0530

    updated the code for noresultXpath and goto options

--- src/library/navigation/goto/domains/to/totalwine.com.js
+++ src/library/navigation/goto/domains/to/totalwine.com.js
@@ -3,9 +3,25 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'totalwine.com',
-    timeout: 50000,
+    timeout: 120000,
     zipcode: '',
     store: 'totalwine',
     country: 'US',
+  },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    await context.setBlockAds(false);
+    await context.setFirstRequestTimeout(100000);
+    await context.setCssEnabled(true);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
