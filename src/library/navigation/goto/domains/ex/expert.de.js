e62b55a983b99c4fabf4b1c1f704d0652af7acec
Himanshu-Agrahari <himanshu.a@partner.import.io>
2021-02-14 15:41:35 +0530

    Fix issue of error not in the right zipcode
    JIRA: https://import-io.atlassian.net/browse/BUGS-2123

--- src/library/navigation/goto/domains/ex/expert.de.js
+++ src/library/navigation/goto/domains/ex/expert.de.js
@@ -6,28 +6,9 @@ module.exports = {
     timeout: 35000,
     country: 'DE',
     store: 'expert',
-    // suffix: '/ahlen',
-    zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameterValues, context, dependencies) => {
-    // USING OPT TAGS > anti_fingerprint), to avoid blocking
-    // #[!opt!]{"anti_fingerprint":false}[/!opt!]
-    // await context.setBlockAds(false);
-    // await context.goto(`${url}`, {
-    //   anti_fingerprint: true,
-    //   discard_CSP_header: true,
-    //   timeout: 100000,
-    //   waitUntil: 'load',
-    // });
-    // await context.setBlockAds(false);
-    // await context.goto(`${url}#[!opt!]{"anti_fingerprint":true, "discard_CSP_header":false}[/!opt!]`, {
-    //       images_enabled: true,
-    //       timeout: 100000,
-    //       waitUntil: 'load',
-    // })
     let location = '';
-
-
     if (zipcode === 'ahlen') {
       location = '/ahlen';
     } else if (zipcode === 'saarwellingen') {
@@ -40,42 +21,44 @@ module.exports = {
       location = '/mannheim1';
     }
 
-    // await context.setBlockAds(false);
-    await context.goto(`https://www.expert.de${location}`, {
-      block_ads: false,
-      anti_fingerprint: true,
-      discard_CSP_header: false,
-      timeout: 300000,
-      waitUntil: 'load',
-    });
-
-    if (zipcode && zipcode.length) {
-      // const zipcodeHref = document.querySelector('a[class^="widget-ExpertLogo--link"]') ? document.querySelector('a[class^="widget-ExpertLogo--link"]').getAttribute('href') : '';
-      await context.waitForSelector('a[class^="widget-ExpertLogo-link"][href="' + location + '"]', { timeout: 75000 });
-    } else {
-      const ifSpecialist = await context.evaluate(function () {
-        return document.querySelector('a[class^="widget-ExpertLogo-link"]') ? (document.querySelector('a[class^="widget-ExpertLogo-link"]').href === 'https://www.expert.de/') : false;
+    if (zipcode) { // if we have zipcode, goto specific location to set zipcode on website
+      console.log('Zipcode passed => ', zipcode);
+      // await context.setBlockAds(false);
+      const _url = `https://www.expert.de${location}`;
+      console.log('GOTO Url to set zipcode => ', _url);
+      await context.goto(_url, {
+        block_ads: false,
+        anti_fingerprint: true,
+        discard_CSP_header: false,
+        timeout: 300000,
+        waitUntil: 'load',
       });
 
-      if (!ifSpecialist) {
-        throw new Error('Not in right zipcode');
+      if (zipcode && zipcode.length) {
+        // const zipcodeHref = document.querySelector('a[class^="widget-ExpertLogo--link"]') ? document.querySelector('a[class^="widget-ExpertLogo--link"]').getAttribute('href') : '';
+        await context.waitForSelector('a[class^="widget-ExpertLogo-link"][href="' + location + '"]', { timeout: 75000 });
+      } else {
+        const ifSpecialist = await context.evaluate(function () {
+          return document.querySelector('a[class^="widget-ExpertLogo-link"]') ? (document.querySelector('a[class^="widget-ExpertLogo-link"]').href === 'https://www.expert.de/') : false;
+        });
+
+        if (!ifSpecialist) {
+          throw new Error('Not in right zipcode');
+        }
       }
+    } else {
+      // if no zipcode is passed
+      console.log('No zipcode was passed')
     }
 
-    // if (zipcode.length) {
-    //   const splitUrl = url.split('https://www.expert.de/');
-    //   const firstPart = splitUrl[0];
-    //   const secondPart = splitUrl[0];
-    // }
-    // const ahlen = 'ahlen';
-    // const saarwellingen = 'saarwellingen';
-    // const mannheim = 'Mannheim1';
-    // const nuernberg = 'nuernberg1';
-    // const loerrach = 'loerrach';
-    // location = `/${ahlen}`;
-
-    // await context.setBlockAds(false);
-    await context.goto(`${url}`, {
+    /**
+     * If zipcode was passed, we set zipcode by making 1st GOTO above
+     * Then goto to searh url
+     * 
+     * If no zipcode was passed, goto to default zipcode 
+     */
+    console.log('Making normal goto => ', url);
+    await context.goto(url, {
       block_ads: false,
       anti_fingerprint: true,
       discard_CSP_header: false,
