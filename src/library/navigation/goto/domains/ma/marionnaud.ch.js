05fb18d6b19db25e5c44cfaf2fd1cf93d39715d0
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-14 17:24:27 +0530

    Added code to work throw error if status is not 200

--- src/library/navigation/goto/domains/ma/marionnaud.ch.js
+++ src/library/navigation/goto/domains/ma/marionnaud.ch.js
@@ -8,4 +8,16 @@ module.exports = {
     store: 'marionnaud',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+    let resp = await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true });
+    console.log('resp.status', resp.status);
+    if(resp.status.toString() !== '200') {
+      throw new Error(`got status --> ${resp.status} - Hence throwing custom error`);
+    }
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
+  }
 };
