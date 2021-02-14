161094095dd7806798dc2d0b2682661d8a4d0914
Rishabh Tiwari <rishabh.tiwari-od@partner.import.io>
2021-02-14 20:55:34 +0530

    changes in detail page

--- src/library/product/details/stores/i/intermarche/FR/extract.js
+++ src/library/product/details/stores/i/intermarche/FR/extract.js
@@ -1,78 +1,28 @@
-const { cleanUp } = require("../../../../shared");
+const { transform } = require('./format');
 module.exports = {
   implements: "product/details/extract",
   parameterValues: {
     country: "FR",
     store: "intermarche",
-    transform: cleanUp,
+    transform,
     domain: "intermarche.com",
     zipcode: "",
   },
-  implementation: async (
-    { url },
-    { country, domain },
-    context,
-    dependencies
-  ) => {
-    await context.evaluate(() => {
-      function findLabel(productObj, label) {
-        const value = productObj[label];
-        if (Array.isArray(value)) {
-          return {
-            label: value.reduce((prevVal, currentVal) => {
-              return prevVal ? prevVal + "," + currentVal : currentVal;
-            }, ""),
-          };
-        } else if (value) {
-          return { label: value };
-        }
-        return null;
-      }
-      function addHiddenDiv(id, content) {
-        const newDiv = document.createElement("div");
+  implementation: async ({ parentInput }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+    await context.evaluate(function (parentInput) {
+      function addHiddenDiv (id, content) {
+        const newDiv = document.createElement('div');
         newDiv.id = id;
         newDiv.textContent = content;
-        newDiv.style.display = "none";
+        newDiv.style.display = 'none';
         document.body.appendChild(newDiv);
       }
-      function findAndInsertLabel(obj, labelName, outputName) {
-        const result = findLabel(obj, labelName);
-        if (result != null) {
-          addHiddenDiv("ii_" + outputName, result.label);
-        }
+      const prodUrl = window.location.href.split('product/');
+      if (prodUrl[1]) {
+        addHiddenDiv('ii_url', prodUrl[1]);
       }
-
-
-      var energy = document.getElementsByClassName(
-        "NutritionalTable__TableCell-sc-49ty6u-8 gUJdBI"
-      );
-      console.log(energy);
-      if (energy[0] != null) {
-        var energy_num = energy[0].innerText;
-        var energy_num = energy[0].innerText.match(/([\d.]+) *kcal/)[1];
-        addHiddenDiv("ii_" + "energy", energy_num);
-        addHiddenDiv("ii_" + "gram", "g");
-        addHiddenDiv("ii_" + "energykilo", "kcal");
-      }
-      if (window.location.href != null) {
-        var url = window.location.href;
-        var id = url.split("/product/");
-        addHiddenDiv("ii_" + "productid", id[1]);
-      }
-      if (
-        document.querySelector(
-          ".styled__ProductConditionnement-rc4bd7-2.kvnQKM"
-        ) != null
-      ) {
-        var size = document.querySelectorAll(
-          ".styled__ProductConditionnement-rc4bd7-2.kvnQKM"
-        );
-        var t = size[2].innerText;
-        var s = t.split(" de ");
-        var p = s.reverse();
-        addHiddenDiv("ii_" + "size", p[0]);
-      }
-    });
-    await context.extract(dependencies.productDetails);
+      addHiddenDiv('ii_parentInput', parentInput);
+    }, parentInput);
+    return await context.extract(productDetails, { transform: transformParam });
   },
 };
