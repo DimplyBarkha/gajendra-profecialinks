f0a31e4b00080bef528bef2fef7b7b7ab4e7c06e
BorysMaciuch <borysmaciuch@gmail.com>
2021-02-15 14:17:04 +0100

    Add setzipcode code

--- src/library/navigation/goto/domains/gr/groceries.asda.com.js
+++ src/library/navigation/goto/domains/gr/groceries.asda.com.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
@@ -6,4 +5,38 @@ module.exports = {
     country: 'UK',
     store: 'asda',
   },
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 10000;
+
+    const url = `https://www.asda.com/login`;
+    const zipcode = inputs.zipcode;
+    const storeId = inputs.storeID;
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, antiCaptchaOptions: { type: 'RECAPTCHA' } });
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    await context.setInputValue('div[class="input-box"] input.email-phone-input', 'asdageorandom@randomstring.com');
+    //await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    await context.waitForSelector('div[class="input-box"] input#password');
+    await context.setInputValue('div[class="input-box"] input#password', 'Random123');
+    //await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    await context.waitForSelector('button[type="submit"]');
+    await context.click('button[type="submit"]');
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    await context.evaluate(async function () {
+      if (document.querySelector('#px-captcha > div.g-recaptcha')) {
+        await context.solveCaptcha({
+          type: 'RECAPTCHA',
+          inputElement: '#px-captcha > div.g-recaptcha',
+          autoSubmit: true,
+        });
+        console.log('solved captcha, waiting for page change');
+        console.log('Captcha vanished');
+      } else {
+        console.log("Captcha not found");
+      }
+    });
+    console.log(zipcode);
+    if (zipcode || storeId) {
+      await dependencies.setZipCode(inputs);
+    }
+  }
 };
--- src/library/navigation/goto/domains/gr/setZipCode.groceries.asda.com.js
+++ src/library/navigation/goto/domains/gr/setZipCode.groceries.asda.com.js
@@ -1,4 +1,69 @@
+async function implementation(inputs, { country, domain, timeout }, context, dependencies) {
 
+  const url2 = `https://groceries.asda.com/search/${inputs.URL}`;
+  await context.goto(url2, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+  await context.evaluate(async function () {
+    if (document.querySelector('div.no-result')) {
+      throw Error('We cant find any results for "Product Code", sorry about that.');
+    }
+  });
+  await context.waitForSelector('div.search-page-content__products-tab-content ul li a[data-auto-id="linkProductTitle"]');
+  const newUrl = await context.evaluate(function () {
+    const tempUrl = document.querySelector('div.search-page-content__products-tab-content ul li a[data-auto-id="linkProductTitle"]');
+    if (tempUrl) {
+      return `https://groceries.asda.com${tempUrl.getAttribute('href')}`;
+    }
+  });
+  const url = newUrl;
+  //const url = `${newUrl}#[!opt!]{"cookie_jar":[{"name":"STORE_ID","value":${inputs.storeID}]}[/!opt!]`;
+  await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+  const url1 = `https://groceries.asda.com/checkout/book-slot/collect?origin=/search/${inputs.URL}`;
+  await context.goto(url1, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+  await new Promise((resolve, reject) => setTimeout(resolve, 20000));
+  await context.evaluate(async function () {
+    if (document.querySelector('button[data-auto-id=btnContinue]')) {
+      document.querySelector('button[data-auto-id=btnContinue]').click();
+    }
+  });
+  await context.waitForSelector('button[data-auto-id="linkChange"]');
+  await context.click('button[data-auto-id="linkChange"]');
+  await new Promise((resolve, reject) => setTimeout(resolve, 20000));
+  await context.waitForSelector('input[data-auto-id="textFieldPostcode"]');
+  await context.setInputValue('input[data-auto-id="textFieldPostcode"]', `${inputs.zipcode}`);
+  await context.waitForSelector('button[data-auto-id="btnCheckPostCode"]');
+  await context.click('button[data-auto-id="btnCheckPostCode"]');
+  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+
+  const storeid = inputs.storeID;
+  const isStore = async () => {
+    return await context.evaluate(async (storeid) => {
+      return !document.querySelector(`input[value="${storeid}"]`);
+    }, storeid);
+  };
+
+  if (await isStore()) {
+    await context.goto(url, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+  } else {
+    try { await context.waitForXPath(`//*[@id="${inputs.storeID}"]`, { timeout: 10000 }); } catch (error) { console.log('Sorry selected store id not present'); };
+    try { await context.click(`input[value="${inputs.storeID}"]`, { timeout: 10000 }); } catch (error) { console.log('Sorry selected store id not present'); };
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    const retailerName = await context.evaluate(function () {
+      const temp = document.querySelector("div.collection-point__container strong");
+      if (temp) {
+        return temp.textContent;
+      }
+    });
+    
+    await context.goto(url, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+    async function addHtml(html, id) {
+      const div = document.createElement('div');
+      div.setAttribute('id', id);
+      div.innerHTML = html;
+      document.body.append(div);
+    };
+    await context.evaluate(addHtml, retailerName, 'retailername');
+  }
+}
 module.exports = {
   implements: 'navigation/goto/setZipCode',
   parameterValues: {
@@ -7,4 +72,5 @@ module.exports = {
     store: 'asda',
     zipcode: '',
   },
+  implementation,
 };
