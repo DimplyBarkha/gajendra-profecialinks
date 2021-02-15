893921593534ed7afcbdfad00b4844cc413b7a30
Shreyam <shreyam.g@osmosys.asia>
2021-02-15 12:33:39 +0530

    Updated signin

--- src/library/navigation/goto/domains/gr/setZipCode.groceries.asda.com.js
+++ src/library/navigation/goto/domains/gr/setZipCode.groceries.asda.com.js
@@ -14,44 +14,12 @@ async function implementation(inputs, { country, domain, timeout }, context, dep
       return `https://groceries.asda.com${tempUrl.getAttribute('href')}`;
     }
   });
-  console.log(`mamatha storeId${inputs.StoreID}`);
-  console.log(`mamatha postcode${inputs.Postcode}`);
+  console.log(`mamatha storeId${inputs.storeID}`);
+  console.log(`mamatha postcode${inputs.zipcode}`);
   const url = newUrl;
-  //const url = `${newUrl}#[!opt!]{"cookie_jar":[{"name":"STORE_ID","value":${inputs.StoreID}]}[/!opt!]`;
+  //const url = `${newUrl}#[!opt!]{"cookie_jar":[{"name":"STORE_ID","value":${inputs.storeID}]}[/!opt!]`;
   console.log('MAmatha1' + url);
-  // await context.waitForSelector('a[data-auto-id="btnSign"]');
-  // await context.click('a[data-auto-id="btnSign"]');
-  // await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-  // await context.setInputValue('div[class="input-box"] input.email-phone-input', 'randomstring@randomstring.com');
-  // //await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-  // await context.waitForSelector('div[class="input-box"] input#password');
-  // await context.setInputValue('div[class="input-box"] input#password', 'Change123');
-  // //await new Promise((resolve, reject) => setTimeout(resolve, 5000));
-  // await context.waitForSelector('button[type="submit"]');
-  // await context.click('button[type="submit"]');
-  // // await context.waitForSelector('');
-  // await context.evaluate(async function () {
-  //   if (document.querySelector('#px-captcha > div.g-recaptcha')) {
-  //     await context.solveCaptcha({
-  //       type: 'RECAPTCHA',
-  //       inputElement: '#px-captcha > div.g-recaptcha',
-  //       autoSubmit: true,
-  //     });
-  //     console.log('solved captcha, waiting for page change');
-  //     console.log('Captcha vanished');
-  //   } else {
-  //     console.log("Captcha not found");
-  //   }
-  // });
   await new Promise((resolve, reject) => setTimeout(resolve, 6000));
-  // await context.evaluate(async function () {
-  //   if (document.querySelector('div.mini-trolley__slot-expiry-messge')) {
-  //     document.querySelector('button[data-auto-id=btnBookSlot]').click();
-  //   } else {
-  //     //await context.waitForSelector('a[data-auto-id="linkChangeSlot"]');
-  //     document.querySelector('a[data-auto-id="linkChangeSlot"]').click();
-  //   }
-  // });
   const url1 = `https://groceries.asda.com/checkout/book-slot/collect?origin=/search/${inputs.URL}`;
   await context.goto(url1, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
   await new Promise((resolve, reject) => setTimeout(resolve, 20000));
@@ -64,7 +32,7 @@ async function implementation(inputs, { country, domain, timeout }, context, dep
   await context.click('button[data-auto-id="linkChange"]');
   await new Promise((resolve, reject) => setTimeout(resolve, 20000));
   await context.waitForSelector('input[data-auto-id="textFieldPostcode"]');
-  await context.setInputValue('input[data-auto-id="textFieldPostcode"]', `${inputs.Postcode}`);
+  await context.setInputValue('input[data-auto-id="textFieldPostcode"]', `${inputs.zipcode}`);
   await context.waitForSelector('button[data-auto-id="btnCheckPostCode"]');
   await context.click('button[data-auto-id="btnCheckPostCode"]');
   await new Promise((resolve, reject) => setTimeout(resolve, 10000));
@@ -75,17 +43,43 @@ async function implementation(inputs, { country, domain, timeout }, context, dep
   //   }
   // });
   //await context.waitForXPath(`//*[@id="${storeId}"]`);
-  try { await context.waitForXPath(`//*[@id="${inputs.StoreID}"]`, { timeout: 10000 }); } catch (error) { console.log('Sorry selected store id not present'); }
-  //document.querySelector("#\\34 220")
-  //await context.waitForSelector(`input[id='${storeId}']`);
-  await context.click(`input[value="${inputs.StoreID}"]`);
-  // await context.waitForSelector('button[data-auto-id="cncAvailableSlot"]');
-  // await context.click('button[data-auto-id="cncAvailableSlot"]');
-  // await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-  // await context.waitForSelector('button[data-auto-id="btnChangeSlot"]');
-  // await context.click('button[data-auto-id="btnChangeSlot"]');
-  await new Promise((resolve, reject) => setTimeout(resolve, 10000));
-  await context.goto(url, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+
+  const storeid = inputs.storeID;
+  console.log(storeid)
+  const isStore = async () => {
+    return await context.evaluate(async (storeid) => {
+      return !document.querySelector(`input[value="${storeid}"]`);
+    }, storeid);
+  };
+
+  if (await isStore()) {
+    await context.goto(url, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+  } else {
+    try { await context.waitForXPath(`//*[@id="${inputs.storeID}"]`, { timeout: 10000 }); } catch (error) { console.log('Sorry selected store id not present'); };
+    try { await context.click(`input[value="${inputs.storeID}"]`, { timeout: 10000 }); } catch (error) { console.log('Sorry selected store id not present'); };
+    await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+    const retailerName = await context.evaluate(function () {
+      const temp = document.querySelector("div.collection-point__container strong");
+      if (temp) {
+        return temp.textContent;
+      }
+    });
+
+    // await context.waitForSelector('button[data-auto-id="cncAvailableSlot"]');
+    // await context.click('button[data-auto-id="cncAvailableSlot"]');
+    // await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    // await context.waitForSelector('button[data-auto-id="btnChangeSlot"]');
+    // await context.click('button[data-auto-id="btnChangeSlot"]');
+    // await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    await context.goto(url, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+    async function addHtml(html, id) {
+      const div = document.createElement('div');
+      div.setAttribute('id', id);
+      div.innerHTML = html;
+      document.body.append(div);
+    };
+    await context.evaluate(addHtml, retailerName, 'retailername');
+  }
 }
 module.exports = {
   implements: 'navigation/goto/setZipCode',
