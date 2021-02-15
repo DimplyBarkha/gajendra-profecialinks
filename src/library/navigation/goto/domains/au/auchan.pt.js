30203d85fb1f2de7352c3f03c837dd5e58fbddab
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-15 21:05:43 +0530

    updated the code - now it will go to the product page by url and not by clicking

--- src/library/navigation/goto/domains/au/auchan.pt.js
+++ src/library/navigation/goto/domains/au/auchan.pt.js
@@ -3,18 +3,27 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'auchan.pt',
-    timeout: 50000,
+    timeout: 120000,
     country: 'PT',
     store: 'auchan',
     zipcode: '',
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
     url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+    let timeout = parameters.timeout;
+    await context.setBlockAds(false);
+    await context.setFirstRequestTimeout(100000);
+    await context.setCssEnabled(true);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
     await context.goto(url, {
       block_ads: false,
       load_all_resources: true,
       images_enabled: true,
-      timeout: 600000,
+      timeout: timeout,
       waitUntil: 'load',
     });
   },
--- src/library/product/details/stores/a/auchan/PT/extract.js
+++ src/library/product/details/stores/a/auchan/PT/extract.js
@@ -25,13 +25,19 @@ async function implementation(
   }
   const addOptionalWait = async (selector) => {
     try {
-      await context.waitForSelector(selector, { timeout: 50000 });
+      await context.waitForSelector(selector, { timeout: 30000 });
       console.log(`${selector} loaded successfully`);
     } catch (e) {
-      console.log(`${selector} did not load at all`);
+      console.log(`${selector} did not load at all`, e.message);
+      try {
+        await context.waitForSelector(selector, { timeout: 30000 });
+        console.log(`${selector} loaded successfully`);
+      } catch (er) {
+        console.log(`${selector} did not load at all`, er.message);
+      }
     }
   };
-  addOptionalWait('div[class="product-item-header"] > a');
+  await addOptionalWait('div[class="product-item-header"] > a');
   const checkExistance = async (selector) => {
     return await context.evaluate((selector) => {
       return Boolean(document.querySelector(selector));
@@ -40,11 +46,32 @@ async function implementation(
   const navigationSelector = 'div[class="product-item-header"] > a';
   await new Promise(resolve => setTimeout(resolve, 30000));
   const isPresent = await checkExistance(navigationSelector);
+  console.log('isPresent', isPresent);
   if (isPresent) {
-    await context.click(navigationSelector);
-    await context.waitForNavigation({ timeout: 30000 });
-    await context.waitForSelector('span[class="item-price"]', { timeout: 30000 });
-    console.log('navigation happened successfully');
+    // await context.click(navigationSelector);
+    let gotoProdUrl = await context.evaluate(async (navigationSelector) => {
+      console.log('need to get the prod url from', navigationSelector);
+      let elm = document.querySelectorAll(navigationSelector);
+      let thisUrl = '';
+      if(elm && elm.length > 0) {
+        if(elm[0].hasAttribute('href')) {
+          thisUrl = 'https://www.auchan.pt' + elm[0].getAttribute('href');
+        }
+      }
+      console.log('got this prod url', thisUrl);
+      return thisUrl;
+    }, navigationSelector);
+
+    if(gotoProdUrl) {
+      await context.goto(gotoProdUrl, { timeout: 30000 });
+    }
+    try {
+      await context.waitForNavigation({ timeout: 30000 });
+      await context.waitForSelector('span[class="item-price"]', { timeout: 30000 });
+      console.log('navigation happened successfully');
+    } catch(err) {
+      console.log('got some error while going to the prod page', err.message);
+    }
   }
   const cookiesSelector = 'button[id="cookies-accept"]';
   const isCookiePresent = await checkExistance(cookiesSelector);
