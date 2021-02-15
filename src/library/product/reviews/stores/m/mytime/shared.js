1d93b61a0e572d9ea1fb09278f9dbe7e3103be5d
Karina Miernik <karina.miernik@gmail.com>
2021-02-15 14:15:36 +0100

    console logs edited

--- src/library/product/reviews/stores/m/mytime/shared.js
+++ src/library/product/reviews/stores/m/mytime/shared.js
@@ -35,14 +35,14 @@ const transform = (data, context) => {
   const todayDate = new Date().toDateString();
 
   data = data.filter(function (item) {
-    console.log('group length before' + item.group.length);
+    console.log('group length before ' + item.group.length);
     item.group = item.group.filter(function (row) {
       if (checkIfReviewIsFromLast30Days(todayDate, row.reviewDate[0].text)) {
         return true;
       }
       return false;
     });
-    console.log('group length after' + item.group.length);
+    console.log('group length after ' + item.group.length);
     item.rows = item.group.length;
     if (item.group.length !== 0) {
       return true;
