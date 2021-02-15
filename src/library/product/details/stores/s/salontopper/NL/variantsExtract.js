de96c33b3e529dbf9f8720e41494fc9d405129a8
Karina Miernik <karina.miernik@gmail.com>
2021-02-15 13:58:47 +0100

    #1482 improved variants

--- src/library/product/details/stores/s/salontopper/NL/variantsExtract.js
+++ src/library/product/details/stores/s/salontopper/NL/variantsExtract.js
@@ -8,4 +8,46 @@ module.exports = {
     domain: 'salontopper.nl',
     zipcode: '',
   },
+
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { variants } = dependencies;
+
+    await context.evaluate(function () {
+      function addHiddenDiv (id, variantId, url, parentDiv = null) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.setAttribute('variantId', variantId);
+        newDiv.setAttribute('href', url);
+        newDiv.style.display = 'none';
+
+        if (parentDiv) {
+          parentDiv.appendChild(newDiv);
+        } else {
+          document.body.appendChild(newDiv);
+        }
+        return newDiv;
+      }
+
+      const variants = document.querySelectorAll('select.autoredirect > option');
+      if (variants.length > 1) {
+        variants.forEach(e => {
+          const link = e.getAttribute('value');
+          if (link !== null) {
+            const href = 'https://www.salontopper.nl'.concat(link);
+            const newDiv = addHiddenDiv('variants', '');
+            addHiddenDiv('variant', href.match(/\d+$/)[0], href, newDiv);
+          }
+        });
+      }
+      if (variants.length === 0) {
+        const url = document.evaluate('//link[@rel="alternate"][@hreflang="nl-nl"]/@href', document, null, XPathResult.STRING_TYPE, null).stringValue.trim();
+        if (url !== '') {
+          const id = url.match(/\d+$/)[0];
+          const newDiv = addHiddenDiv('variants', '');
+          addHiddenDiv('variant', id, url, newDiv);
+        }
+      }
+    });
+    await context.extract(variants);
+  },
 };
