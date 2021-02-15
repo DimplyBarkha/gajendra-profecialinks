28a7663df1f39c8212f3a33163fdff8f5a3d46ac
irfan shaikh <irfan.shaikh@synerzip.com>
2021-02-15 11:54:37 +0530

    Handle brands as an input.

--- src/library/product/search/stores/c/carrefour/ES/search.js
+++ src/library/product/search/stores/c/carrefour/ES/search.js
@@ -6,4 +6,43 @@ module.exports = {
     store: 'carrefour',
     domain: 'carrefour.es',
   },
+  implementation: async ({ keywords, Keywords, Brands, results = 150 }, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
+    // TODO: consider moving this to a reusable function
+    const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
+
+    keywords = (Keywords) || (keywords) || (Brands);
+    console.log('zip:' + zipcode);
+    // do the search
+    const resultsReturned = await execute({ keywords, zipcode });
+
+    if (!resultsReturned) {
+      console.log('No results were returned');
+      return;
+    }
+
+    // try gettings some search results
+    const pageOne = await extract({});
+
+    let collected = length(pageOne);
+
+    console.log('Got initial number of results', collected);
+
+    // check we have some data
+    if (collected === 0) {
+      return;
+    }
+
+    let page = 2;
+    while (collected < results && await paginate({ keywords, page, offset: collected })) {
+      const data = await extract({});
+      const count = length(data);
+      if (count === 0) {
+        // no results
+        break;
+      }
+      collected += count;
+      console.log('Got more results', collected);
+      page++;
+    }
+  },
 };
