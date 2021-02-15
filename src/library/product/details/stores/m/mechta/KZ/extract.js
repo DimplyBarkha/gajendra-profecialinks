c1e085b60ee37a39dcd5a8aaf574b9174c0976b9
PR <pravinr@synerzipune.local>
2021-02-15 23:16:04 +0530

    fixed color, shipping dimention

--- src/library/product/details/stores/m/mechta/KZ/extract.js
+++ src/library/product/details/stores/m/mechta/KZ/extract.js
@@ -1,13 +1,16 @@
-const { transform } = require('./format');
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
+const { transform } = require("./format");
+async function implementation(inputs, parameters, context, dependencies) {
   const { transform } = parameters;
   const { productDetails } = dependencies;
   await context.evaluate(async function () {
+    function stall(ms) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, ms);
+      });
+    }
+
     let scrollTop = 0;
     while (scrollTop <= 20000) {
       await stall(500);
@@ -17,25 +20,28 @@ async function implementation (
         await stall(80000);
         break;
       }
+     
     }
-    function stall (ms) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve();
-        }, ms);
-      });
+    const link = document.querySelector(
+      'div[class*="q-tab relative-position self-stretch flex flex-center text-center q-tab--inactive q-tab--no-caps q-focusable q-hoverable cursor-pointer"]'
+    );
+    console.log("link:: ", link);
+    if (link != null) {
+      link.click();
     }
+    
   });
+
   return await context.extract(productDetails, { transform });
 }
 module.exports = {
-  implements: 'product/details/extract',
+  implements: "product/details/extract",
   parameterValues: {
-    country: 'KZ',
-    store: 'mechta',
+    country: "KZ",
+    store: "mechta",
     transform,
-    domain: 'mechta.kz',
-    zipcode: '',
+    domain: "mechta.kz",
+    zipcode: "",
   },
   implementation,
 };
