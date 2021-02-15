94a7b6fdfe1969444f4be9e900240599290afae7
Tasos Bitsios <tasos.bitsios@import.io>
2021-02-15 15:38:28 +0100

    darty navigator refactor

--- src/library/navigation/goto/domains/da/darty.com.js
+++ src/library/navigation/goto/domains/da/darty.com.js
@@ -23,54 +23,56 @@ module.exports = {
     await context.setAntiFingerprint(false);
     await context.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36');
 
-    const responseStatus = await context.goto(`${url}`, {
-      antiCaptchaOptions: {
-        provider: 'anti-captcha',
-        type: 'GEETEST',
-        autoSubmit: true,
-      },
-      firstRequestTimeout: 60000,
-      timeout: timeout,
-      waitUntil: 'load',
-      checkBlocked: false,
-    });
-    console.log('Status :', responseStatus.status);
-    console.log('URL :', responseStatus.url);
-
-    await context.waitForNavigation({ timeout: 30000 });
-    try {
-      await context.evaluateInFrame('iframe',
+    async function hasCaptcha() {
+      return context.evaluateInFrame('iframe',
         function () {
-          // @ts-ignore
-          const code = geetest
-            .toString()
-            .replace(
-              /appendTo\("#([^"]+)"\)/,
-              'appendTo(document.getElementById("$1"))',
-            );
-          return eval(`(${code})('/captcha/geetest');`);
-        },
+          return !!window.geetest;
+        }
       );
-
-      await new Promise(resolve => setTimeout(resolve, 500));
-      await context.evaluateInFrame('iframe',
-        function () {
-          // @ts-ignore
-          if (document.querySelector('.captcha-handler')) {
-            document.querySelector('.captcha-handler').click();
-          }
-          if (document.querySelector('.geetest_radar_tip_content')) {
-            document.querySelector('.geetest_radar_tip_content').click();
-          }
-        },
-      );
-      await new Promise(resolve => setTimeout(resolve, 10000));
-    } catch (error) {
-      console.log('error: NO CAPTCHA ENCOUNTER', error);
     }
-    try {
-      await context.waitForSelector('#produit > div.product_head');
-    } catch (error) {
+
+    async function clickCaptcha() {
+      return context.evaluateInFrame('iframe',
+        async function () {
+          const captchaHandler = document.querySelector('.captcha-handler');
+          if (captchaHandler) {
+            captchaHandler.click();
+          }
+          const radarTip = document.querySelector('.geetest_radar_tip_content')
+          if (radarTip) {
+            radarTip.click();
+          }
+          return { captchaHandler: !!captchaHandler, radarTip: !!radarTip, geetest: !!window.geetest };
+        },
+      );
+    }
+
+    async function printCaptchaStatus() {
+      const status = await context.evaluateInFrame('iframe', async function () {
+        return document.querySelector('body').getAttribute('captchastatus');
+      });
+      console.log('captchastatus was', status);
+    }
+
+    async function checkBlocked() {
+      return context.evaluateInFrame('iframe', async function () {
+        return document.querySelector('body').innerText.includes('You have been blocked.');
+      });
+    }
+
+    async function dumpBlockedText() {
+      const text = await context.evaluateInFrame('iframe', async function () {
+        return document.querySelector('body').innerText
+      });
+      console.log('block text: ', text);
+    }
+
+    async function gotoPage(retries) {
+      if (retries === 0) {
+        console.log('retries exhausted');
+        return;
+      }
+
       const responseStatus = await context.goto(`${url}`, {
         antiCaptchaOptions: {
           provider: 'anti-captcha',
@@ -86,95 +88,30 @@ module.exports = {
       console.log('URL :', responseStatus.url);
 
       await context.waitForNavigation({ timeout: 30000 });
+      await new Promise(resolve => setTimeout(resolve, 1000));
       try {
-        await context.evaluateInFrame('iframe',
-          function () {
-            // @ts-ignore
-            const code = geetest
-              .toString()
-              .replace(
-                /appendTo\("#([^"]+)"\)/,
-                'appendTo(document.getElementById("$1"))',
-              );
-            return eval(`(${code})('/captcha/geetest');`);
-          },
-        );
-
-        await new Promise(resolve => setTimeout(resolve, 500));
-        await context.evaluateInFrame('iframe',
-          function () {
-            // @ts-ignore
-            if (document.querySelector('.captcha-handler')) {
-              document.querySelector('.captcha-handler').click();
-            }
-            if (document.querySelector('.geetest_radar_tip_content')) {
-              document.querySelector('.geetest_radar_tip_content').click();
-            }
-          },
-        );
-        await new Promise(resolve => setTimeout(resolve, 15000));
-        try {
-          await context.waitForSelector('#produit > div.product_head');
-          return;
-        } catch(e) {
-          console.log('captcha not solved, trying again');
+        if (await checkBlocked()) {
+          console.log('blocked');
+          await dumpBlockedText();
+          return context.reportBlocked(responseStatus.status, 'You have been blocked');
         }
-        const captchaStatus = await context.evaluateInFrame('iframe', async function () {
-          return document.querySelector('body').getAttribute('captchastatus');
-        });
-        console.log('captcha status was ', captchaStatus);
-        if (captchaStatus === 'ok') {
-          const responseStatus = await context.goto(`${url}`, {
-            antiCaptchaOptions: {
-              provider: 'anti-captcha',
-              type: 'GEETEST',
-              autoSubmit: true,
-            },
-            firstRequestTimeout: 60000,
-            timeout: timeout,
-            waitUntil: 'load',
-            checkBlocked: false,
-          });
-          console.log('Status :', responseStatus.status);
-          console.log('URL :', responseStatus.url);
-
-          await context.waitForNavigation({ timeout: 30000 });
-          try {
-            await context.evaluateInFrame('iframe',
-              function () {
-                // @ts-ignore
-                const code = geetest
-                  .toString()
-                  .replace(
-                    /appendTo\("#([^"]+)"\)/,
-                    'appendTo(document.getElementById("$1"))',
-                  );
-                return eval(`(${code})('/captcha/geetest');`);
-              },
-            );
-
-            await new Promise(resolve => setTimeout(resolve, 500));
-            await context.evaluateInFrame('iframe',
-              function () {
-                // @ts-ignore
-                if (document.querySelector('.captcha-handler')) {
-                  document.querySelector('.captcha-handler').click();
-                }
-                if (document.querySelector('.geetest_radar_tip_content')) {
-                  document.querySelector('.geetest_radar_tip_content').click();
-                }
-              },
-            );
-            await new Promise(resolve => setTimeout(resolve, 15000));
-            await context.waitForSelector('#produit > div.product_head');
-          } catch (error) {
-            console.log('Reload and resolve captcha failed: ', error);
-          }
-        };
+        if (await hasCaptcha()) {
+          console.log('found captcha');
+          await clickCaptcha();
+          await new Promise(resolve => setTimeout(resolve, 20000));
+        } else {
+          console.log('no captcha encountered');
+        }
+        await context.waitForSelector('#produit > div.product_head', { timeout: 60000 });
+        await printCaptchaStatus();
       } catch (error) {
-        console.log('error: FAILED TO SOLVE CAPTCHA AFTER RETRY', error);
+        console.log('error: ', error);
+        return gotoPage(retries - 1);
       }
     }
+  
+    await gotoPage(3);
+
     async function autoScroll (page) {
       await page.evaluate(async () => {
         await new Promise((resolve, reject) => {
