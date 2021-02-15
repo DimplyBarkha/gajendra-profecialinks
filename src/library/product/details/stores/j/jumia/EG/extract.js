0841ee3f09780f66a9e4fe9c7e94fe9a7c52dfd3
Prakhar Singh <prakhar.s@OSMOSYS.LOCAL>
2021-02-15 06:50:02 +0530

    Removed unwanted code

--- src/library/product/details/stores/j/jumia/EG/extract.js
+++ src/library/product/details/stores/j/jumia/EG/extract.js
@@ -8,22 +8,22 @@ module.exports = {
     domain: 'jumia.com',
     zipcode: '',
   },
-  implementation: async (inputs,
-    parameters,
-    context,
-    dependencies,
-  ) => {
-    const { productDetails } = dependencies;
-    await context.evaluate(async function () {
-      // const { transform } = parameters;
-      if (document.querySelector('div[id="catalog-listing"]')) { // checking if a listing page
-        console.log('we are on listing page');
-        if (document.querySelector('div[class*="-paxs row"]') && document.querySelector('div[class*="-paxs row"] article[class*="prd"]')) {
-          console.log('we are inside listing div');
-          document.querySelector('div[class*="-paxs row"] article[class*="prd"] a[class*="core"]').click();
-        }
-      }
-    });
-    return await context.extract(productDetails, { transform });
-  },
+  // implementation: async (inputs,
+  //   parameters,
+  //   context,
+  //   dependencies,
+  // ) => {
+  //   const { productDetails } = dependencies;
+  //   await context.evaluate(async function () {
+  //     // const { transform } = parameters;
+  //     if (document.querySelector('div[id="catalog-listing"]')) { // checking if a listing page
+  //       console.log('we are on listing page');
+  //       if (document.querySelector('div[class*="-paxs row"]') && document.querySelector('div[class*="-paxs row"] article[class*="prd"]')) {
+  //         console.log('we are inside listing div');
+  //         document.querySelector('div[class*="-paxs row"] article[class*="prd"] a[class*="core"]').click();
+  //       }
+  //     }
+  //   });
+  //   return await context.extract(productDetails, { transform });
+  // },
 };
