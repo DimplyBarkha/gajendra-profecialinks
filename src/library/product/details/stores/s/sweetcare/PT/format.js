597377734843316125c61b41de7262775dd09969
sivasoarlogic <siva@soarlogic.com>
2021-02-16 11:32:41 +0530

    QAfixs

--- src/library/product/details/stores/s/sweetcare/PT/format.js
+++ src/library/product/details/stores/s/sweetcare/PT/format.js
@@ -3,8 +3,8 @@
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data, context) => {
-  const cleanUp = (data) => {
+const transform = (data) => {
+  const cleanUp = (data, context) => {
     const clean = text => text.toString()
       .replace(/\r\n|\r|\n/g, ' ')
       .replace(/&amp;nbsp;/g, ' ')
@@ -22,34 +22,84 @@ const transform = (data, context) => {
     }))));
     return data;
   };
-  const state = context.getState();
-  let index = state.index || 0;
   for (const { group } of data) {
-    for (const row of group) {
-      if (row.additionalDescBulletInfo) {
-        var arrTemp = [];
-        row.additionalDescBulletInfo.forEach(item => {
-          if (item.text) {
-            arrTemp.push(item.text);
+    for (let row of group) {
+      
+      if (row.description) {
+          let description_ar = [];
+          row.description.forEach(item => {
+            description_ar.push(item.text);
+          });
+          if (description_ar.length) {
+            row.description = [{ "text": description_ar.join(" | ").replace('SWEET MAG', '').trim(), 'xpath': row.description[0].xpath }];
           }
-        });
-        row.additionalDescBulletInfo = [{ text: '|| ' + arrTemp.join(' || ') }];
       }
-      if (row.image) {
-        row.image.forEach(item => {
-          if (item.text) {
-            item.text = item.text.replace('/160/', '/max/');
-          }
+      if (row.directions) {
+        let info = [];
+        row.directions.forEach(item => {
+          info.push(item.text);
         });
+        if (info.length) {
+          row.directions = [{ "text": info.join(" "), 'xpath': row.directions[0].xpath }];
+        }
+    }
+    if (row.image) {
+      let info = [];
+      row.image.forEach(item => {
+        info.push(item.text);
+      });
+      if (info.length) {
+        row.image = [{ "text": info[0], 'xpath': row.image[0].xpath }];
       }
-      if (row.sku) {
-        row.sku = [{ text: row.sku[index].text }];
+  }  
+  if (row.promotion) {
+    let info = [];
+    row.promotion.forEach(item => {
+      item.text = item.text.match(/poupe até.*/);
+      info.push(item.text);
+  });
+  if (info.length) {
+    row.promotion = [{ "text": '-'+info.join().replace('poupe até','').replace(' | Sweetcare®','').replace(/\s*/g,''), 'xpath': row.promotion[0].xpath }];
+  }
+}
+   if (row.price) {
+      let info = [];
+      row.price.forEach(item => {
+        item.text = item.text.replace(/\s*/g, '').trim();
+        item.text = item.text.slice(1,-1);
+        item.text = '€'+item.text;
+        info.push(item.text);
+    });
+    if (info.length) {
+      row.price = [{ "text": info[0], 'xpath': row.price[0].xpath }];
+    }
+}
+  if (row.imageAlt) {
+    let info = [];
+    row.imageAlt.forEach(item => {
+      info.push(item.text);
+    });
+    if (info.length) {
+      row.imageAlt = [{ "text": info[0], 'xpath': row.imageAlt[0].xpath }];
+    }
+}
+      if (row.descriptionBullets) {
+        row.descriptionBullets = [{'text':row.descriptionBullets.length, 'xpath':row.descriptionBullets[0].xpath}];              
+      } 
+      if (row.category) {
+        let info = [];
+        row.category.forEach(item => {
+          info.push(item.text.trim());
+        });
+        if (info.length) {
+          row.category = [];
+          info.forEach(item => {
+            row.category.push({ "text": item});
+          });
+        }
       }
     }
-    index = index + 1;
   }
-  context.setState({ index });
   return cleanUp(data);
 };
-
 module.exports = { transform };
