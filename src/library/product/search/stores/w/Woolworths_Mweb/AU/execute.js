15b733b0a0c2392d2d2a099afc4becb32e94d85e
sivasoarlogic <siva@soarlogic.com>
2021-02-16 12:21:40 +0530

    QAfixs

--- src/library/product/search/stores/w/Woolworths_Mweb/AU/execute.js
+++ src/library/product/search/stores/w/Woolworths_Mweb/AU/execute.js
@@ -1,3 +1,34 @@
+/**
+ *
+ * @param { { keywords: string, zipcode: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  console.log('params', parameters);
+  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+  await dependencies.goto({ url, zipcode: inputs.zipcode });
+
+  if (parameters.loadedSelector) {
+    await context.waitForFunction(function (sel, xp) {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 100000 }, parameters.loadedSelector, parameters.noResultsXPath);
+  }
+  console.log('Checking no results', parameters.noResultsXPath);
+  return await context.evaluate(function (xp) {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, parameters.noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/search/execute',
@@ -5,9 +36,10 @@ module.exports = {
     country: 'AU',
     store: 'Woolworths_Mweb',
     domain: 'woolworths.com.au',
-    url: null,
-    loadedSelector: null,
-    noResultsXPath: null,
+    url: 'https://www.woolworths.com.au/Shop/Search?searchTerm={searchTerms}',
+    loadedSelector: 'div.ng-trigger-staggerFadeInOut div.product-grid--tile',
+    noResultsXPath: '//span[contains(text(),"Unfortunately, we could")] | //h1[contains(text()," There are no products matching your filters. ")]',
     zipcode: "''",
   },
+  implementation,
 };
--- src/library/product/search/stores/w/Woolworths_Mweb/AU/extract.js
+++ src/library/product/search/stores/w/Woolworths_Mweb/AU/extract.js
@@ -1,10 +1,10 @@
-
+const { transform } = require('./format');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'AU',
     store: 'Woolworths_Mweb',
-    transform: null,
+    transform: transform,
     domain: 'woolworths.com.au',
     zipcode: "''",
   },
