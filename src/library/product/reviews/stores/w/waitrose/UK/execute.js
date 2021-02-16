9dcee9d441416b3e6e4d91ddb7927f2d8a9fbffa
Kamil <33379395+Rivi96@users.noreply.github.com>
2021-02-16 10:22:19 +0100

    Url creation using rpc

--- src/library/product/reviews/stores/w/waitrose/UK/execute.js
+++ src/library/product/reviews/stores/w/waitrose/UK/execute.js
@@ -1,3 +1,29 @@
+async function implementation ({ url, id }, { reviewUrl, noResultsXPath }, context, dependencies) {
+  const patternReplace = () => {
+    if (reviewUrl) {
+      let tempUrl = reviewUrl;
+      if (id) tempUrl = tempUrl.replace(/{id}/g, encodeURIComponent(id));
+      return tempUrl;
+    }
+  };
+  const destinationUrl = patternReplace();
+  await dependencies.goto({ url: destinationUrl });
+
+  const searchPage = await context.evaluate(async () => { return document.querySelectorAll('article[data-test="product-pod"]').length; });
+  if (searchPage === 1) {
+    var detailsPage = await context.evaluate(async () => {
+      if (document.querySelector('a[data-origincomponent="ProductPod"]') != null) {
+        var productLink = document.querySelector('a[data-origincomponent="ProductPod"]').getAttribute('href');
+      }
+      return productLink;
+    });
+    if (detailsPage) {
+      await context.goto('https://www.waitrose.com' + detailsPage);
+      await context.waitForNavigation();
+    }
+  }
+  return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/reviews/execute',
@@ -6,9 +32,10 @@ module.exports = {
     store: 'waitrose',
     domain: 'waitrose.com',
     loadedSelector: 'section[class="productDetailContainer___1TUHx"]',
-    noResultsXPath: '//a[contains(@class,"bv-write-review-label")]',
-    reviewUrl: null,
+    noResultsXPath: '//div[@class="bv-content-placeholder"] | //button[@aria-label="Load more"]',
+    reviewUrl: 'https://www.waitrose.com/ecom/shop/search?&searchTerm={id}',
     sortButtonSelectors: null,
     zipcode: '',
   },
+  implementation,
 };
