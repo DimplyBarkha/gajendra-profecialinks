b2c3ae6dd962f3582a2fc8c8b4a7edd4e8b7a13d
Lokanath2020 <loknath.s@osmosys.asia>
2021-02-16 14:03:11 +0530

    fixed the secondary image issue

--- src/library/product/details/stores/m/mediaexpert/PL/extract.js
+++ src/library/product/details/stores/m/mediaexpert/PL/extract.js
@@ -1,115 +1,131 @@
+/** @format */
+
 const { transform } = require('./shared');
 
 module.exports = {
-  implements: 'product/details/extract',
-  parameterValues: {
-    country: 'PL',
-    store: 'mediaexpert',
-    transform,
-    domain: 'mediaexpert.pl',
-    zipcode: '',
-    noResultsXPath: null,
-  },
-  implementation: async (inputs, { country, domain, transform: transformParam }, context, { productDetails }) => {
-    await context.evaluate(async function () {
-      const applyScroll = async function (context) {
-        let scrollTop = 0;
-        while (scrollTop !== 20000) {
-          await stall(500);
-          scrollTop += 1000;
-          window.scroll(0, scrollTop);
-          if (scrollTop === 20000) {
-            await stall(5000);
-            break;
-          }
-        }
-        function stall(ms) {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              resolve();
-            }, ms);
-          });
-        }
-      };
-      const enhancedConent = document.querySelector('div#description label.a-toggle_label');
-      if (enhancedConent) {
-        // @ts-ignore
-        enhancedConent.click();
-        await applyScroll();
-      }
-    });
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-    try {
-      await context.waitForXPath(`//div[@class="description-container" or @class='description_dyson-container' ]/div[contains(.,'Included') or contains(.,'contents') or contains(.,'Contents')]/following-sibling::*//img/@src`);
-    }
-    catch (e) {
-      console.log("No in the box url present");
-    }
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-    const videoIframes = await context.evaluate(async function () {
-      console.log(document.querySelectorAll('div#description div[style] iframe[data-component="lazyLoad"]'));
-      return document.querySelectorAll('div#description div[style] iframe[data-component="lazyLoad"]');
-    });
-    for (let i = 0; i < Object.keys(videoIframes).length; i++) {
-      const selector = 'div#description div[style]  iframe';
-      const extractedVideoLink = await extractVideosFromEnhancedContent(selector);
-      if (extractedVideoLink && extractedVideoLink.includes('youtube')) {
-        await appendVideosToDom(extractedVideoLink);
-        await removeAddedVideoFromDom(selector);
-      }
-    }
+	implements: 'product/details/extract',
+	parameterValues: {
+		country: 'PL',
+		store: 'mediaexpert',
+		transform,
+		domain: 'mediaexpert.pl',
+		zipcode: '',
+		noResultsXPath: null,
+	},
+	implementation: async (
+		inputs,
+		{ country, domain, transform: transformParam },
+		context,
+		{ productDetails }
+	) => {
+		await context.evaluate(async function () {
+			const applyScroll = async function (context) {
+				let scrollTop = 0;
+				while (scrollTop !== 20000) {
+					await stall(500);
+					scrollTop += 1000;
+					window.scroll(0, scrollTop);
+					if (scrollTop === 20000) {
+						await stall(5000);
+						break;
+					}
+				}
+				function stall(ms) {
+					return new Promise((resolve, reject) => {
+						setTimeout(() => {
+							resolve();
+						}, ms);
+					});
+				}
+			};
+			const enhancedConent = document.querySelector(
+				'div#description label.a-toggle_label'
+			);
+			if (enhancedConent) {
+				// @ts-ignore
+				enhancedConent.click();
+				await applyScroll();
+			}
+		});
+		await new Promise((resolve) => setTimeout(resolve, 10000));
+		try {
+			await context.waitForXPath(
+				`//div[@class="description-container" or @class='description_dyson-container' ]/div[contains(.,'Included') or contains(.,'contents') or contains(.,'Contents')]/following-sibling::*//img/@src`
+			);
+		} catch (e) {
+			console.log('No in the box url present');
+		}
+		await new Promise((resolve) => setTimeout(resolve, 10000));
+		const videoIframes = await context.evaluate(async function () {
+			console.log(
+				document.querySelectorAll(
+					'div#description div[style] iframe[data-component="lazyLoad"]'
+				)
+			);
+			return document.querySelectorAll(
+				'div#description div[style] iframe[data-component="lazyLoad"]'
+			);
+		});
+		for (let i = 0; i < Object.keys(videoIframes).length; i++) {
+			const selector = 'div#description div[style]  iframe';
+			const extractedVideoLink = await extractVideosFromEnhancedContent(
+				selector
+			);
+			if (extractedVideoLink && extractedVideoLink.includes('youtube')) {
+				await appendVideosToDom(extractedVideoLink);
+				await removeAddedVideoFromDom(selector);
+			}
+		}
 
-    async function extractVideosFromEnhancedContent(selector) {
-      try {
-        const videos = await context.evaluateInFrame(selector,
-          function () {
-            console.log('start of evaluate');
-            const a = document.querySelector('link[rel="canonical"]');
-            if (a) {
-              return a.getAttribute('href');
-            }
-          },
-        );
-        return videos;
-      } catch (err) {
-        console.log(err);
-        return null;
-      }
-    };
+		async function extractVideosFromEnhancedContent(selector) {
+			try {
+				const videos = await context.evaluateInFrame(selector, function () {
+					console.log('start of evaluate');
+					const a = document.querySelector('link[rel="canonical"]');
+					if (a) {
+						return a.getAttribute('href');
+					}
+				});
+				return videos;
+			} catch (err) {
+				console.log(err);
+				return null;
+			}
+		}
 
-    async function appendVideosToDom(videoLink) {
-      await context.evaluate(async function (videoLink) {
-        const newDiv = document.createElement('div');
-        newDiv.id = 'videoFromEnhancedContent';
-        newDiv.textContent = videoLink;
-        newDiv.style.display = 'none';
-        document.body.appendChild(newDiv);
-      }, videoLink);
-    }
+		async function appendVideosToDom(videoLink) {
+			await context.evaluate(async function (videoLink) {
+				const newDiv = document.createElement('div');
+				newDiv.id = 'videoFromEnhancedContent';
+				newDiv.textContent = videoLink;
+				newDiv.style.display = 'none';
+				document.body.appendChild(newDiv);
+			}, videoLink);
+		}
 
-    async function removeAddedVideoFromDom(videoSelector) {
-      await context.evaluate(function (videoSelector) {
-        if (videoSelector) {
-          const videoEle = document.querySelector(videoSelector);
-          if (videoEle) {
-            const parentEle = videoEle.parentElement;
-            return parentEle ? parentEle.removeChild(videoEle) : null;
-          }
-        }
-      }, videoSelector);
-    }
-    // async function aplusSpecial (){
-    //   const html = await response.text();
-    //   const aImage = html.match(/(style="background: url('=|"url":)"([^"]+)/);
-    //   if (!aImage) return '';
-    //   return aImage[2];
-    // }
-    // const pdp = 'div.c-group.is-container.is-offerGrid > div > div.is-carouselContainer';
-    // try{
-    //   await context.waitForSelector(pdp, { timeout: 90000 });
-    // } catch (error) {
-    //   console.log(error.message);
-    // }
-    return await context.extract(productDetails, { transform: transformParam });
-  },
+		async function removeAddedVideoFromDom(videoSelector) {
+			await context.evaluate(function (videoSelector) {
+				if (videoSelector) {
+					const videoEle = document.querySelector(videoSelector);
+					if (videoEle) {
+						const parentEle = videoEle.parentElement;
+						return parentEle ? parentEle.removeChild(videoEle) : null;
+					}
+				}
+			}, videoSelector);
+		}
+		// async function aplusSpecial (){
+		//   const html = await response.text();
+		//   const aImage = html.match(/(style="background: url('=|"url":)"([^"]+)/);
+		//   if (!aImage) return '';
+		//   return aImage[2];
+		// }
+		// const pdp = 'div.c-group.is-container.is-offerGrid > div > div.is-carouselContainer';
+		// try{
+		//   await context.waitForSelector(pdp, { timeout: 90000 });
+		// } catch (error) {
+		//   console.log(error.message);
+		// }
+		return await context.extract(productDetails, { transform: transformParam });
+	},
 };
--- src/library/product/details/stores/m/mediaexpert/PL/shared.js
+++ src/library/product/details/stores/m/mediaexpert/PL/shared.js
@@ -1,175 +1,195 @@
-
 /**
- *
+ * @format
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
+
 const transform = (data) => {
-  // Default transform function
-  const clean = text => text.toString()
-    .replace(/\r\n|\r|\n/g, ' ')
-    .replace(/&amp;nbsp;/g, ' ')
-    .replace(/&amp;#160/g, ' ')
-    .replace(/\u00A0/g, ' ')
-    .replace(/\s{2,}/g, ' ')
-    .replace(/^ +| +$|( )+/g, ' ')
-    // eslint-disable-next-line no-control-regex
-    .replace(/[\x00-\x1F]/g, '')
-    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
-    .trim();
-  function escapeUnicode(str) {
-    return str.replace(/[^\0-~]/g, function (ch) {
-      return "\\u" + ("0000" + ch.charCodeAt().toString(16)).slice(-4);
-    });
-  }
-  for (const { group } of data) {
-    for (const row of group) {
-      if (row.category) {
-        for (let i = 0; i < row.category.length; i++) {
-          row.category[i].text = row.category[i].text.replace('›', '');
-        }
-      }
+	// Default transform function
+	const clean = (text) =>
+		text
+			.toString()
+			.replace(/\r\n|\r|\n/g, ' ')
+			.replace(/&amp;nbsp;/g, ' ')
+			.replace(/&amp;#160/g, ' ')
+			.replace(/\u00A0/g, ' ')
+			.replace(/\s{2,}/g, ' ')
+			.replace(/^ +| +$|( )+/g, ' ')
+			// eslint-disable-next-line no-control-regex
+			.replace(/[\x00-\x1F]/g, '')
+			.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
+			.trim();
+	function escapeUnicode(str) {
+		return str.replace(/[^\0-~]/g, function (ch) {
+			return '\\u' + ('0000' + ch.charCodeAt().toString(16)).slice(-4);
+		});
+	}
+	for (const { group } of data) {
+		for (const row of group) {
+			if (row.category) {
+				for (let i = 0; i < row.category.length; i++) {
+					row.category[i].text = row.category[i].text.replace('›', '');
+				}
+			}
 
-      if (row.alternateImages) {
-        for (let i = 0; i < row.alternateImages.length; i++) {
-          const imageObj = row.alternateImages[i].text;
-          const image = imageObj.substring(imageObj.indexOf('media'), imageObj.indexOf("',"));
-          const imageStr = `https://www.mediaexpert.pl/${image}`;
-          row.alternateImages[i].text = imageStr;
-        }
-      }
+			if (row.alternateImages) {
+				for (let i = 0; i < row.alternateImages.length; i++) {
+					const imageObj = row.alternateImages[i].text;
+					const image = imageObj.substring(
+						imageObj.indexOf('media'),
+						imageObj.indexOf("',")
+					);
+					const imageStr = `https://www.mediaexpert.pl/${image}`;
+					row.alternateImages[i].text = imageStr;
+				}
+			}
 
-      // if (row.productData) {
-      //   let prodData = JSON.parse(escapeUnicode(row.productData[0].text))['@graph'];
-      //   if (prodData) {
-      //     prodData = prodData[0];
-      //     if (prodData) {
-      //       row.aggregateRating = prodData.aggregateRating && prodData.aggregateRating.ratingValue ? [{ text: prodData.aggregateRating.ratingValue }] : [];
-      //       prodData.sku ? row.variantId = [{ text: prodData.sku }] : row.variantId = [];
-      //       prodData.gtin13 ? row.eangtin = [{ text: prodData.gtin13 }] : row.eangtin = [];
-      //     }
-      //   }
-      // }
+			// if (row.productData) {
+			//   let prodData = JSON.parse(escapeUnicode(row.productData[0].text))['@graph'];
+			//   if (prodData) {
+			//     prodData = prodData[0];
+			//     if (prodData) {
+			//       row.aggregateRating = prodData.aggregateRating && prodData.aggregateRating.ratingValue ? [{ text: prodData.aggregateRating.ratingValue }] : [];
+			//       prodData.sku ? row.variantId = [{ text: prodData.sku }] : row.variantId = [];
+			//       prodData.gtin13 ? row.eangtin = [{ text: prodData.gtin13 }] : row.eangtin = [];
+			//     }
+			//   }
+			// }
 
-      if (row.videos) {
-        for (let i = 0; i < row.videos.length; i++) {
-          let videoStr = row.videos[i].text;
-          videoStr = videoStr.substring(0, videoStr.indexOf('?'));
-          videoStr = `https://www.youtube.com/watch?v=${videoStr}`;
-          row.videos[i].text = videoStr;
-        }
-      }
+			if (row.videos) {
+				for (let i = 0; i < row.videos.length; i++) {
+					let videoStr = row.videos[i].text;
+					videoStr = videoStr.substring(0, videoStr.indexOf('?'));
+					videoStr = `https://www.youtube.com/watch?v=${videoStr}`;
+					row.videos[i].text = videoStr;
+				}
+			}
 
-      if (row.aggregateRating && row.aggregateRating.length > 0) {
-        row.aggregateRating[0].text = row.aggregateRating[0].text.replace('.', ',');
-      }
+			if (row.aggregateRating && row.aggregateRating.length > 0) {
+				row.aggregateRating[0].text = row.aggregateRating[0].text.replace(
+					'.',
+					','
+				);
+			}
 
-      if (row.promocodePrice) {
-        row.price = row.promocodePrice;
-      }
+			if (row.promocodePrice) {
+				row.price = row.promocodePrice;
+			}
 
-      if (row.price) {
-        row.price[0].text = row.price[0].text.replace('.', ',');
-      }
+			if (row.price) {
+				row.price[0].text = row.price[0].text.replace('.', ',');
+			}
 
-      if (row.listPrice) {
-        row.listPrice[0].text = row.listPrice[0].text.replace('.', ',');
-      }
+			if (row.listPrice) {
+				row.listPrice[0].text = row.listPrice[0].text.replace('.', ',');
+			}
 
-      if (row.specifications) {
-        let specStr = '';
-        for (let i = 0; i < row.specifications.length; i++) {
-          specStr += `${row.specifications[i].text} || `;
-        }
-        specStr = specStr.substring(0, specStr.lastIndexOf('|| '));
-        row.specifications = [{ text: specStr }];
-      }
+			if (row.specifications) {
+				let specStr = '';
+				for (let i = 0; i < row.specifications.length; i++) {
+					specStr += `${row.specifications[i].text} || `;
+				}
+				specStr = specStr.substring(0, specStr.lastIndexOf('|| '));
+				row.specifications = [{ text: specStr }];
+			}
 
-      if (row.weightNet) {
-        let netWeight = row.weightNet[0].text;
-        if (netWeight.includes('/')) {
-          netWeight = netWeight.split('/')[1];
-        }
-        row.weightNet = [{ text: netWeight }];
-      }
+			if (row.weightNet) {
+				let netWeight = row.weightNet[0].text;
+				if (netWeight.includes('/')) {
+					netWeight = netWeight.split('/')[1];
+				}
+				row.weightNet = [{ text: netWeight }];
+			}
 
-      if (row.image) {
-        let imageData = row.image[0].text;
-        imageData = imageData.substring(imageData.indexOf("'/media") + 1, imageData.indexOf("',"));
-        row.image = [{ text: `https://www.mediaexpert.pl${imageData}` }];
-      }
+			if (row.image) {
+				let imageData = row.image[0].text;
+				imageData = imageData.substring(
+					imageData.indexOf("'/media") + 1,
+					imageData.indexOf("',")
+				);
+				row.image = [{ text: `https://www.mediaexpert.pl${imageData}` }];
+			}
 
-      if (!row.image && row.productSingleImage) {
-        row.image = row.productSingleImage;
-        let imageData = row.image[0].text;
-        imageData = imageData.substring(imageData.indexOf("'/media") + 1, imageData.indexOf("',"));
-        row.image = [{ text: `https://www.mediaexpert.pl${imageData}` }];
-        row.imageAlt = row.productSingleImageAltText ? row.productSingleImageAltText : [];
-        // if (row.alternateImages) {
-        //   delete row.alternateImages;
-        //   delete row.secondaryImageTotal;
-        // }
-      }
+			if (!row.image && row.productSingleImage) {
+				row.image = row.productSingleImage;
+				let imageData = row.image[0].text;
+				imageData = imageData.substring(
+					imageData.indexOf("'/media") + 1,
+					imageData.indexOf("',")
+				);
+				row.image = [{ text: `https://www.mediaexpert.pl${imageData}` }];
+				row.imageAlt = row.productSingleImageAltText
+					? row.productSingleImageAltText
+					: [];
+				// if (row.alternateImages) {
+				//   delete row.alternateImages;
+				//   delete row.secondaryImageTotal;
+				// }
+			}
 
-      // if (row.manufacturerDescription) {
-      //   let enhancedContent = '';
-      //   for (let i = 0; i < row.manufacturerDescription.length; i++) {
-      //     enhancedContent += row.manufacturerDescription[i].text + ' ';
-      //   }
-      //   row.manufacturerDescription = [{ text: enhancedContent }];
-      // }
+			// if (row.manufacturerDescription) {
+			//   let enhancedContent = '';
+			//   for (let i = 0; i < row.manufacturerDescription.length; i++) {
+			//     enhancedContent += row.manufacturerDescription[i].text + ' ';
+			//   }
+			//   row.manufacturerDescription = [{ text: enhancedContent }];
+			// }
 
-      if (row.weightNet) {
-        let weightNet = row.weightNet[0].text;
-        if (row.weightNetUom && !row.weightNet[0].text.includes('kg')) {
-          weightNet = `${weightNet} ${row.weightNetUom[0].text}`;
-        } else if (!row.weightNet[0].text.includes('kg')) {
-          weightNet = `${weightNet} kg`;
-        }
-        row.weightNet = [{ text: weightNet }];
-      }
+			if (row.weightNet) {
+				let weightNet = row.weightNet[0].text;
+				if (row.weightNetUom && !row.weightNet[0].text.includes('kg')) {
+					weightNet = `${weightNet} ${row.weightNetUom[0].text}`;
+				} else if (!row.weightNet[0].text.includes('kg')) {
+					weightNet = `${weightNet} kg`;
+				}
+				row.weightNet = [{ text: weightNet }];
+			}
 
-      // if (row.enhancedContent && !row.manufacturerDescription) {
-      //   row.manufacturerDescription = row.enhancedContent;
-      // }
+			// if (row.enhancedContent && !row.manufacturerDescription) {
+			//   row.manufacturerDescription = row.enhancedContent;
+			// }
 
-      if (!row.sku) {
-        row.sku = row.skuSecondary;
-        if (!row.sku && row.skuAtLast) {
-          row.sku = row.skuAtLast;
-        }
-      }
+			if (!row.sku) {
+				row.sku = row.skuSecondary;
+				if (!row.sku && row.skuAtLast) {
+					row.sku = row.skuAtLast;
+				}
+			}
 
-      if (!row.manufacturerImages && row.aplusImages_fallBack) {
-        row.manufacturerImages = row.aplusImages_fallBack;
-      }
+			if (!row.manufacturerImages && row.aplusImages_fallBack) {
+				row.manufacturerImages = row.aplusImages_fallBack;
+			}
 
-      if (row.videoFromEnhancedContent) {
-        if (row.videos) {
-          row.videos = row.videos.concat(row.videoFromEnhancedContent);
-        } else {
-          row.videos = row.videoFromEnhancedContent;
-        }
-      }
-      if (row.image && row.alternateImages) {
-        row.alternateImages.forEach((element, index) => {
-          if (element.text == row.image[0].text) {
-            row.alternateImages.splice(index, index + 1)
-          }
-        })
-      }
-    }
-  }
+			if (row.videoFromEnhancedContent) {
+				if (row.videos) {
+					row.videos = row.videos.concat(row.videoFromEnhancedContent);
+				} else {
+					row.videos = row.videoFromEnhancedContent;
+				}
+			}
+			if (row.image && row.alternateImages) {
+				row.alternateImages.forEach((element, index) => {
+					if (element.text == row.image[0].text) {
+						row.alternateImages.splice(index, index + 1);
+					}
+				});
+			}
+		}
+	}
 
-  data.forEach(obj => obj.group.forEach(row => Object.keys(row).forEach(function (header) {
-    if (row[header]) {
-      row[header].forEach(el => {
-        el.text = clean(el.text);
-      });
-    }
-  })));
+	data.forEach((obj) =>
+		obj.group.forEach((row) =>
+			Object.keys(row).forEach(function (header) {
+				if (row[header]) {
+					row[header].forEach((el) => {
+						el.text = clean(el.text);
+					});
+				}
+			})
+		)
+	);
 
-  return data;
+	return data;
 };
 
 module.exports = { transform };
