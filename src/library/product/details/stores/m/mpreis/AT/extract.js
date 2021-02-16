c66a22113a7732b2b5870747faf2f635b4b6b2a5
banderovskyi <roman1004b@gmail.com>
2021-02-16 13:05:16 +0200

    Cahge nutrition parsing

--- src/library/product/details/stores/m/mpreis/AT/extract.js
+++ src/library/product/details/stores/m/mpreis/AT/extract.js
@@ -11,26 +11,28 @@ module.exports = {
   implementation: async (inputs, parameters, context, dependencies) => {
     const { transform } = parameters;
     const { productDetails } = dependencies;
-    // await new Promise((resolve, reject) => setTimeout(resolve, 100000));
-    // await context.waitForSelector('.prod-info');
+    await context.evaluate(async() => {
+      let scrollTop = 0;
+      while (scrollTop <= 20000) {
+        await stall(200);
+        scrollTop += 4000;
+        window.scroll(0, scrollTop);
+        if (scrollTop === 20000) {
+          await stall(1000);
+          break;
+        }
+      }
+      function stall(ms) {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+    });
+    await context.waitForSelector('#uc-btn-accept-banner');
+    await context.click('#uc-btn-accept-banner');
     await context.evaluate(async () => {
-      // let scrollTop = 0;
-      // while (scrollTop <= 20000) {
-      //   await stall(200);
-      //   scrollTop += 2000;
-      //   window.scroll(0, scrollTop);
-      //   if (scrollTop === 30000) {
-      //     await stall(2000);
-      //     break;
-      //   }
-      // }
-      // function stall (ms) {
-      //   return new Promise((resolve) => {
-      //     setTimeout(() => {
-      //       resolve();
-      //     }, ms);
-      //   });
-      // }
       function addElementToDocument (key, value) {
         const createdElem = document.querySelector(`#${key}`);
         if (!createdElem) {
@@ -45,7 +47,10 @@ module.exports = {
       const tables = document.querySelectorAll('table.table');
       tables.forEach(table => {
         table.querySelectorAll('td').forEach(tdItem => {
-          tdItem.textContent = tdItem.textContent + ' Data'
+          if(tdItem.previousSibling.textContent.includes('Nährwerttabelle')){
+            tdItem.textContent = tdItem.textContent.replace(/</gmi, '');
+            tdItem.textContent = tdItem.textContent + ' Data'
+          }
         })
       })
 
--- src/library/product/details/stores/m/mpreis/AT/variantsExtract.js
+++ src/library/product/details/stores/m/mpreis/AT/variantsExtract.js
@@ -10,12 +10,33 @@ module.exports = {
   },
   implementation: async (inputs, parameters, context, dependencies) => {
     const { createUrl, variants } = dependencies;
+    await context.waitForSelector('#uc-btn-accept-banner');
+    await context.click('#uc-btn-accept-banner');
     await context.evaluate(function () {
       const variantsLinks = document.querySelectorAll('.panel-title a');
+      const variantsArr = [];
       variantsLinks.forEach(variantsLink => {
-        const href = variantsLink.getAttribute('href');
-        variantsLink.setAttribute('href', `${window.location.href}${href}`);
-      })
+          const variantText = variantsLink.textContent.trim();
+          if(!variantsArr.includes(`${variantText}`)){
+            const href = variantsLink.getAttribute('href');
+            variantsLink.className += ` mp-variant-link mp-variant-link-${variantText.replace(/ /, '-')}`; 
+            variantsLink.setAttribute('data-href', `${window.location.href}${href}`);
+
+            const linkNode = document.createElement('span');
+            linkNode.style.display = 'none';
+            linkNode.textContent = `${window.location.href}${href}`;
+            variantsLink.append(linkNode);
+            
+            const variantNode = document.createElement('i');
+            variantNode.style.display = 'none';
+            variantNode.textContent = variantText;
+            variantsLink.append(variantNode);
+
+            variantsArr.push(variantText);
+          }
+      });
+      console.log(variantsArr);
+
     }, createUrl);
   return await context.extract(variants);
   },
