dbfa96c36635aa9af0f72ce1bd61ebb149c52dc2
shwetakedari10 <60596662+shwetakedari10@users.noreply.github.com>
2021-02-16 13:15:07 +0530

    the core for carrefour IT has been completed and deployed in staging

--- src/library/product/details/stores/c/carrefour/IT/execute.js
+++ src/library/product/details/stores/c/carrefour/IT/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'carrefour',
     domain: 'carrefour.it',
     loadedSelector: 'div.product-images',
-    noResultsXPath: '//h2[contains(text(),"la pagina richiesta non esiste")]|//h1[contains(text(),"forse hai sbagliato corsia")]',
+    noResultsXPath: '//h2[contains(text(),"la pagina richiesta non esiste")]|//h1[contains(text(),"forse hai sbagliato corsia")]|//div[@class="slide-content"]|//div[@class="editorial-header container"]',
     zipcode: '',
   },
 };
