80655e2f4d5bc16147b7dafecda83651522b82c8
astropaula <wojciechowska.fran@gmail.com>
2021-02-16 15:19:14 +0100

    Added noresult and search page exception

--- src/library/product/reviews/stores/s/superdrug/UK/execute.js
+++ src/library/product/reviews/stores/s/superdrug/UK/execute.js
@@ -1,3 +1,56 @@
+/**
+ *
+ * @param { { id: string, url: string, zipcode: string, date: string, days: string } } inputs
+ * @param { { url: string, loadedSelector?: string, noResultsXPath: string, sortButtonSelectors: string, reviewUrl: string } } parameters
+ * @param { ImportIO.IContext } context
+ * @param { { goto: ImportIO.Action} } dependencies
+ */
+async function implementation (
+  { url, id, zipcode, date, days },
+  { reviewUrl, sortButtonSelectors, loadedSelector, noResultsXPath },
+  context,
+  dependencies,
+) {
+  const patternReplace = () => {
+    if (!reviewUrl) throw new Error('No pattern provided to generate a valid URL');
+    let tempUrl = reviewUrl;
+    if (id) tempUrl = tempUrl.replace(/{id}/g, encodeURIComponent(id));
+    if (date) tempUrl = tempUrl.replace(/{date}/g, encodeURIComponent(date));
+    if (days) tempUrl = tempUrl.replace(/{days}/g, encodeURIComponent(days));
+    return tempUrl;
+  };
+  const destinationUrl = url || patternReplace();
+
+  await dependencies.goto({ url: destinationUrl, zipcode });
+
+  const product = await context.evaluate(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    if (document.querySelector('ul[class="plp__product-container"]')) {
+      return document.querySelector('a[class="item__image ClickSearchResultEvent_Class"]').href;
+    }
+  });
+  await dependencies.goto({ url: product, zipcode });
+  if (sortButtonSelectors) {
+    const selectors = sortButtonSelectors.split('|');
+    for (const selector of selectors) {
+      await context.click(selector);
+    }
+  }
+  if (loadedSelector) {
+    await context.waitForFunction((sel, xp) => {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+
+  console.log('Checking no results', noResultsXPath);
+  return await context.evaluate((xp) => {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
+}
 
 module.exports = {
   implements: 'product/reviews/execute',
@@ -6,9 +59,10 @@ module.exports = {
     store: 'superdrug',
     domain: 'superdrug.com',
     loadedSelector: 'div[class*="pdp__BVRRContainer--container"]',
-    noResultsXPath: '//button[@id="first-to-write"]',
+    noResultsXPath: '//button[@id="first-to-write"] | //h1[contains(text(), "this product is no longer available")] | //div[@id="no_results"]',
     reviewUrl: 'https://www.superdrug.com/search?text={id}',
     sortButtonSelectors: null,
     zipcode: '',
   },
+  implementation,
 };
