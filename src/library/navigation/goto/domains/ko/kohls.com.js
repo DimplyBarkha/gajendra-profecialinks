b068f803c569bea97f101f542a3a4cb40d388bc3
sasz94 <sacha.bartlomiej@gmail.com>
2021-02-16 16:09:09 +0100

    Fixed sku & variantId

--- src/library/navigation/goto/domains/ko/kohls.com.js
+++ src/library/navigation/goto/domains/ko/kohls.com.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'kohls.com',
-    timeout: 50000,
+    timeout: 30000,
     country: 'US',
     store: 'kohls',
     zipcode: '',
