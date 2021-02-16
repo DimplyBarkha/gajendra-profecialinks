b0fef75eb0a1782d278d9375b6d9f1d650cc0750
Priyanka <priyanka.l@osmosys.asia>
2021-02-16 21:55:41 +0530

    Worked on updating goto for  rewe

--- src/library/navigation/goto/domains/sh/shop.rewe.de.js
+++ src/library/navigation/goto/domains/sh/shop.rewe.de.js
@@ -9,51 +9,29 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url }, parameterValues, context, dependencies) => {
-      const isCaptcha = async () => {
-      return await context.evaluate(async function () {
-        const captchaEl = document.evaluate('//div[@class="captcha-box"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        if (captchaEl.snapshotLength) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-    };
-
-    if (isCaptcha()) {
-      console.log('isCaptcha', true);
-
-      await context.solveCaptcha({
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.goto(url, {
+      firstRequestTimeout: 60000,
+      timeout: 60000,
+      waitUntil: 'load',
+      checkBlocked: false,
+      store: 'rewe',
+      country: 'DE',
+      domain: 'shop.rewe.de',
+      antiCaptchaOptions: {
         type: 'HCAPTCHA',
-        inputElement: `#challenge-form > input[name='r']`,
-        autoSubmit: true,
-      });
-      console.log('solved captcha, waiting for page change');
-      context.waitForNavigation();
-      console.log('Captcha vanished');
-    }
-  },
-  // implementation: async ({ url }, parameterValues, context, dependencies) => {
-  //   // eslint-disable-next-line
-  //   // const js_enabled = true; // Math.random() > 0.7;
-  //   // console.log('js_enabled', js_enabled); ;
+      },
+    });
 
-  //   const isCaptcha = async () => {
-  //     return await context.evaluate(async function () {
-  //       const captchaEl = document.evaluate('//div[@id="cf-hcaptcha-container"]//iframe', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-  //       if (captchaEl.snapshotLength) {
-  //         return 'true';
-  //       } else {
-  //         return 'false';
-  //       }
-  //     });
-  //   };
-  //   try {
-  //     if (isCaptcha) {
-  //       document.querySelector('div[id="cf-hcaptcha-container"] iframe').click();
-  //     }
-  //   } catch (err) {
-  //     console.group('Captcha not solved' + err);
-  //   }
-  // },
+    await context.waitForSelector('#cf-hcaptcha-container iframe');
+    await context.solveCaptcha({
+      type: 'HCAPTCHA',
+      inputElement: '#cf-hcaptcha-container iframe',  
+      autoSubmit: true,
+    });
+  },
 };
