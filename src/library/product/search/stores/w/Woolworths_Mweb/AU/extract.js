482af67f7aa83635152759f424657df86f71a83f
sivasoarlogic <siva@soarlogic.com>
2021-02-16 12:16:33 +0530

    QAfixs

---
+++ src/library/product/search/stores/w/Woolworths_Mweb/AU/extract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/search/extract',
+  parameterValues: {
+    country: 'AU',
+    store: 'Woolworths_Mweb',
+    transform: null,
+    domain: 'woolworths.com.au',
+    zipcode: "''",
+  },
+};
