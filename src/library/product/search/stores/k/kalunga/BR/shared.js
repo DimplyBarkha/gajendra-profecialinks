f1f0a62d35138b0017109c495068cdb012cd472d
Import-io <ihor.vasylechko-makeitease@partner.import.io>
2021-02-16 12:53:56 +0200

    search kalunga_BR fixed search urls

--- src/library/product/search/stores/k/kalunga/BR/shared.js
+++ src/library/product/search/stores/k/kalunga/BR/shared.js
@@ -11,11 +11,11 @@ const transform = (data) => {
       try {
         gr['rank'] = [{ text: index + 1 }];
         gr['rankOrganic'] = [{ text: index + 1 }];
-        if (gr && gr.price && gr.price.length) gr.price[0].text = gr.price[0].text.replace(onlyNumbers, '');
-        if (gr && gr.reviewCount && gr.reviewCount.length) {
-          gr.reviewCount[0].text = gr.reviewCount[0].text.replace(onlyNumbers, '');
-          gr['aggregateRating'] = gr.reviewCount;
+        if (gr && gr.price && gr.price.length) {
+          const findPrice = gr.price.find(el => el.text.includes('$'));
+          gr.price = [{ text: findPrice.text.replace(onlyNumbers, '') }];
         }
+        if (gr && gr.aggregateRating && gr.aggregateRating.length) gr.aggregateRating[0].text = gr.aggregateRating[0].text.replace(onlyNumbers, '');
         if (gr && gr.productUrl && gr.productUrl.length) gr.productUrl = [{ text: 'https://www.kalunga.com.br' + gr.productUrl[0].text }];
         if (gr && gr.id && gr.id.length) {
           const text = gr.id[0].text;
