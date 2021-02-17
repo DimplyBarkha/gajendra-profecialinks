d14bc2764ae3bd47eef6fc1906c09dd12e4c5476
sasz94 <sacha.bartlomiej@gmail.com>
2021-02-17 21:39:45 +0100

    Fixed videos & sku attributes

--- src/library/product/details/stores/k/kohls/US/extract.js
+++ src/library/product/details/stores/k/kohls/US/extract.js
@@ -5,18 +5,33 @@ async function implementation (inputs, parameters, context, dependencies) {
   const { productDetails } = dependencies;
 
   await context.evaluate(async () => {
-    function addElementToDom (element, id) {
-      const div = document.createElement('div');
-      div.id = id;
-      div.innerHTML = element;
-      document.body.appendChild(div);
+    function addElementToDocument (key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
     }
-
     const availability = document.querySelector('h3[class="OOSprodNotAvail"]')
       ? 'Out Of Stock'
       : 'In Stock';
 
-    addElementToDom(availability, 'availability');
+    addElementToDocument('availability', availability);
+
+    const iframeVideoEl = document.querySelector('iframe[title="Product Videos"]');
+    if (iframeVideoEl) {
+      try {
+        console.log(iframeVideoEl.contentDocument.querySelector('video[src]'));
+        addElementToDocument('iframe_video', iframeVideoEl.contentDocument.querySelector('video[src]').getAttribute('src'));
+      } catch (e) {
+        console.log('there is no video in iframe');
+      }
+    }
+
+    const skuScriptEl = document.evaluate("//script[contains(.,'productSKU\":') and not(@id)]", document, null, XPathResult.STRING_TYPE, null).stringValue;
+    if (skuScriptEl && /productSKU":"(\d+)",/.test(skuScriptEl)) {
+      addElementToDocument('sku_from_script', skuScriptEl.match(/productSKU":"(\d+)",/)[1]);
+    }
   });
 
   return await context.extract(productDetails, { transform });
