2644f1b425c214585643539f2e1e6e401f36432b
sivasoarlogic <siva@soarlogic.com>
2021-02-17 23:32:44 +0530

    timeoutupdate

--- src/library/navigation/goto/domains/ca/cabutteodyprod.tylerhost.net.js
+++ src/library/navigation/goto/domains/ca/cabutteodyprod.tylerhost.net.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'cabutteodyprod.tylerhost.net',
-    timeout: null,
+    timeout: 100000,
     country: 'CA',
     store: 'ButteCounty',
     zipcode: "''",
