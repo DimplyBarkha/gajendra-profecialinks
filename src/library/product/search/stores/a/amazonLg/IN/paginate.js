7f9b874b55b9610228204ff6bf789c1f67f60690
Abhishek Kumar Mishra <abhishekkumar_mishra@hcl.com>
2021-02-17 16:21:39 +0530

    modified pagination

--- src/library/product/search/stores/a/amazonLg/IN/paginate.js
+++ src/library/product/search/stores/a/amazonLg/IN/paginate.js
@@ -32,6 +32,6 @@ module.exports = {
     openSearchDefinition: {
       template: 'https://www.amazon.in/s?k={searchTerms}&ref=nb_sb_noss_2&page={page}',
     },
-    noResultsXPath: '//a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | /html[not(//div[contains(@data-component-type,"s-search-result") and @data-asin][not(contains(@class, "AdHolder"))])] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
+    noResultsXPath: '/html[not(//script[contains(text(),\'pageType: "Search"\')])] | //a//img[contains(@src,"503.png")] | //a[contains(@href,"ref=cs_503_link")] | //script[contains(text(),"PageNotFound")] | //span[contains(@cel_widget_id,"MAIN-TOP_BANNER_MESSAGE") and contains(., "No results")] | //img[contains(@alt,"Dogs of Amazon")] |//*[contains(text(),"Looking for something?")]',
   },
 };
