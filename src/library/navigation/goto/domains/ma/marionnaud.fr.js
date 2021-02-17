685117c9242ccf3c8bcfd0e53529d2d5068bbb1d
pavitra r <pavithra.ramasamy-odc@partner.import.io>
2021-02-18 00:19:05 +0530

    added first request timeout - akshay channa

--- src/library/navigation/goto/domains/ma/marionnaud.fr.js
+++ src/library/navigation/goto/domains/ma/marionnaud.fr.js
@@ -8,4 +8,8 @@ module.exports = {
     store: 'marionnaud',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 30000;
+    await context.goto(url, { first_request_timeout: 90000, timeout, waitUntil: 'load', checkBlocked: true });
+  },
 };
--- src/library/product/details/execute.js
+++ src/library/product/details/execute.js
@@ -17,7 +17,15 @@ const implementation = async (inputs, { loadedSelector, noResultsXPath }, contex
   await dependencies.goto({ ...inputs, url: builtUrl || url });
 
   if (loadedSelector) {
+    await context.waitForFunction(
+      (selector, xpath) => {
+        return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
+      },
+      { timeout: 10000 },
+      loadedSelector,
+      noResultsXPath,
     );
+  };
   return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
 };
 
