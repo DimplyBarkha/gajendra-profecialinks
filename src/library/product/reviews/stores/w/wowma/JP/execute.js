61eced314d2c85e46c0b4cef57b8cac6eede269a
Avinash <avinash.kadam@profecialinks.com>
2021-02-17 14:56:21 +0530

    review extractor updated

--- src/library/product/reviews/stores/w/wowma/JP/execute.js
+++ src/library/product/reviews/stores/w/wowma/JP/execute.js
@@ -5,10 +5,12 @@ module.exports = {
     country: 'JP',
     store: 'wowma',
     domain: 'wowma.jp',
-    loadedSelector: null,
-    noResultsXPath: null,
-    reviewUrl: null,
+    loadedSelector: 'section#lotReviewContents',
+    noResultsXPath: '//div[contains(@class,"NoResultPage")]',
+    reviewUrl: 'https://wowma.jp/items/{id}/reviews?order=created_desc',
     sortButtonSelectors: null,
     zipcode: '',
   },
 };
+
+
