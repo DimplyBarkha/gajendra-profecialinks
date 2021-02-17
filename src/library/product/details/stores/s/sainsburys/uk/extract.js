e3acf288b3f432eb8ca15516052a0e5d561b312b
Dhanush <dhanush.isukapalli@od.import.io>
2021-02-17 23:15:52 +0530

    sainsburys By Dhanush

--- src/library/product/details/stores/s/sainsburys/uk/extract.js
+++ src/library/product/details/stores/s/sainsburys/uk/extract.js
@@ -9,9 +9,9 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ inputstring }, { country, domain }, context, { productDetails }) => {
-    await context.waitForSelector('h2[class="pt__info__description"] a', 30000);
-    await context.click('h2[class="pt__info__description"] a');
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await context.waitForSelector('h2[class="pt__info__description"] a', 30000);
+    // await context.click('h2[class="pt__info__description"] a');
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
     await context.evaluate(() => {
       function addHiddenDiv(key, value) {
 
