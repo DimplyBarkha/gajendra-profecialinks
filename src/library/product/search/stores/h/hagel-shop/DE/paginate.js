5af596959dfd2823e1f6890d851ce5fb63e48a60
pavitra r <pavithra.ramasamy-odc@partner.import.io>
2021-02-17 17:26:53 +0530

    updated paginate.js - akshay channa

--- src/library/product/search/stores/h/hagel-shop/DE/paginate.js
+++ src/library/product/search/stores/h/hagel-shop/DE/paginate.js
@@ -4,16 +4,16 @@ module.exports = {
   parameterValues: {
     country: 'DE',
     store: 'hagel-shop',
-    nextLinkSelector: 'a[class="button next selectorgadget_selected"]',
+    // nextLinkSelector: 'a[class="button next selectorgadget_selected"]',
     // 'body > div.wrapper > div.main-container.case > div > div.main > article > div.search-result > div.category-products > div.toolbar.bottom > div > div > ul > li:last-child > a[class="button next"]',
     // div[class="toolbar bottom"] li a[class="button next"]
     mutationSelector: null,
     spinnerSelector: null,
     loadedSelector: 'div[class="search-result"]',
     noResultsXPath: '//p[@class="note-msg"]',
-    // openSearchDefinition: {
-    //   template: 'https://www.hagel-shop.de/catalogsearch/result?q={searchTerms}&p={page}',
-    // },
+    openSearchDefinition: {
+      template: 'https://www.hagel-shop.de/catalogsearch/result?followSearch=9998&verbose=true&q={searchTerms}&p={page}&limit=20'
+    },
     domain: 'hagel-shop.de',
     zipcode: '',
   },
