151d5640a5d3a3e9f886b583d5ff2eb652b20000
Kamil <33379395+Rivi96@users.noreply.github.com>
2021-02-17 09:40:33 +0100

    Execute.js fixes

--- src/library/product/reviews/stores/w/waitrose/UK/execute.js
+++ src/library/product/reviews/stores/w/waitrose/UK/execute.js
@@ -1,14 +1,22 @@
-async function implementation ({ url, id }, { reviewUrl, noResultsXPath }, context, dependencies) {
+async function implementation (
+  { url, id, zipcode, date, days },
+  { reviewUrl, sortButtonSelectors, loadedSelector, noResultsXPath },
+  context,
+  dependencies,
+) {
   const patternReplace = () => {
-    if (reviewUrl) {
-      let tempUrl = reviewUrl;
-      if (id) tempUrl = tempUrl.replace(/{id}/g, encodeURIComponent(id));
-      return tempUrl;
-    }
+    if (!reviewUrl) throw new Error('No pattern provided to generate a valid URL');
+    let tempUrl = reviewUrl;
+    if (id) tempUrl = tempUrl.replace(/{id}/g, encodeURIComponent(id));
+    if (date) tempUrl = tempUrl.replace(/{date}/g, encodeURIComponent(date));
+    if (days) tempUrl = tempUrl.replace(/{days}/g, encodeURIComponent(days));
+    return tempUrl;
   };
-  const destinationUrl = patternReplace();
-  await dependencies.goto({ url: destinationUrl });
+  const destinationUrl = url || patternReplace();
 
+  await dependencies.goto({ url: destinationUrl, zipcode });
+
+  // find and go to product page
   const searchPage = await context.evaluate(async () => { return document.querySelectorAll('article[data-test="product-pod"]').length; });
   if (searchPage === 1) {
     var detailsPage = await context.evaluate(async () => {
@@ -22,7 +30,26 @@ async function implementation ({ url, id }, { reviewUrl, noResultsXPath }, conte
       await context.waitForNavigation();
     }
   }
-  return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
+
+  if (sortButtonSelectors) {
+    const selectors = sortButtonSelectors.split('|');
+    for (const selector of selectors) {
+      await context.click(selector);
+    }
+  }
+  if (loadedSelector) {
+    await context.waitForFunction((sel, xp) => {
+      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+    }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+  }
+
+  return await context.evaluate((xp) => {
+    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+    console.log(xp, r);
+    const e = r.iterateNext();
+    console.log(e);
+    return !e;
+  }, noResultsXPath);
 }
 
 module.exports = {
