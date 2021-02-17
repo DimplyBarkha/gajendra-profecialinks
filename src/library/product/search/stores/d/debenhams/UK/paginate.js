2e6a984e018f5ef4a308c38f51b394b34f3cb134
Sweeti Kothari <sweeti@fluentcode.io>
2021-02-17 14:14:54 +0530

    search pagination change

--- src/library/product/search/stores/d/debenhams/UK/paginate.js
+++ src/library/product/search/stores/d/debenhams/UK/paginate.js
@@ -1,16 +1,17 @@
-
 module.exports = {
-  implements: 'product/search/paginate',
-  parameterValues: {
-    country: 'UK',
-    store: 'debenhams',
-    nextLinkSelector: "nav.pw-pagination.dbh-pagination-bottom button.pw-pagination__next:not([disabled])",
-    mutationSelector: null,
-    spinnerSelector: null,
-    loadedSelector: "div.t-product-list__container",
-    noResultsXPath: "//div[contains(@class, 't-generic-error')]//h3[contains(@class, 't-generic-error-title')]",
-    openSearchDefinition: null,
-    domain: 'debenhams.com',
-    zipcode: '',
-  },
+    implements: 'product/search/paginate',
+    parameterValues: {
+        country: 'UK',
+        store: 'debenhams',
+        nextLinkSelector: "nav.pw-pagination.dbh-pagination-bottom button.pw-pagination__next:not([disabled])",
+        mutationSelector: null,
+        spinnerSelector: null,
+        loadedSelector: "div.t-product-list__container",
+        noResultsXPath: "//div[contains(@class, 't-generic-error')]//h3[contains(@class, 't-generic-error-title')]",
+        openSearchDefinition: {
+            template: 'https://www.debenhams.com/search/{searchTerms}?pn={page}'
+        },
+        domain: 'debenhams.com',
+        zipcode: '',
+    },
 };
