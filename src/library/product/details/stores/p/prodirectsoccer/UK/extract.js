425aa15a7e8034d45d3479123e90ee81d43802bc
Nilesh <nilesh.b@osmosys.asia>
2021-02-17 13:14:47 +0530

    Fixed issue.

--- src/library/product/details/stores/p/prodirectsoccer/UK/extract.js
+++ src/library/product/details/stores/p/prodirectsoccer/UK/extract.js
@@ -23,6 +23,27 @@ module.exports = {
     if (popup) {
       await context.click('.taxfree-popup button');
     }
+
+    await context.evaluate(async () => {
+      try {
+        const brand = JSON.parse(document.evaluate("//script[@type='application/ld+json'][contains(.,'brand')]", document, null, XPathResult.ANY_TYPE, null)
+        && document.evaluate("//script[@type='application/ld+json'][contains(.,'brand')]", document, null, XPathResult.ANY_TYPE, null).iterateNext()
+        &&  document.evaluate("//script[@type='application/ld+json'][contains(.,'brand')]", document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent
+        && document.evaluate("//script[@type='application/ld+json'][contains(.,'brand')]", document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent.trim()).brand;
+        const imageAlt = document.evaluate("(//div[contains(@class,'grid-image-gallery__box')]//img/@alt)[1] | (//div[contains(@class,'circular-carousel')]//div[contains(@class,'product-thumb__box')]/img/@alt)[1]", document, null, XPathResult.ANY_TYPE, null)
+        && document.evaluate("(//div[contains(@class,'grid-image-gallery__box')]//img/@alt)[1] | (//div[contains(@class,'circular-carousel')]//div[contains(@class,'product-thumb__box')]/img/@alt)[1]", document, null, XPathResult.ANY_TYPE, null).iterateNext()
+        && document.evaluate("(//div[contains(@class,'grid-image-gallery__box')]//img/@alt)[1] | (//div[contains(@class,'circular-carousel')]//div[contains(@class,'product-thumb__box')]/img/@alt)[1]", document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent
+        && document.evaluate("(//div[contains(@class,'grid-image-gallery__box')]//img/@alt)[1] | (//div[contains(@class,'circular-carousel')]//div[contains(@class,'product-thumb__box')]/img/@alt)[1]", document, null, XPathResult.ANY_TYPE, null).iterateNext().textContent.trim();
+        if(brand){
+          document.querySelector('h1') ? document.querySelector('h1').setAttribute('brand',brand) : '';
+        }
+        if(imageAlt){
+          document.querySelector('h1') ? document.querySelector('h1').setAttribute('image-alt',imageAlt) : '';
+        }
+      } catch (error) {
+        console.log('Brand not found.')
+      }
+    });
     return await context.extract(productDetails, { transform });
   },
 };
