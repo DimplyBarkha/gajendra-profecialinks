79112a61979d2b4971af33a6c9f02e43e1e54000
sobha rani <amol.thukrul-od@partner.import.io>
2021-02-17 12:12:08 +0530

    fressnapf complete

---
+++ src/library/product/details/stores/s/snipes/DE/variantsExtract.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/details/variants/variantsExtract',
+  parameterValues: {
+    country: 'DE',
+    store: 'snipes',
+    transform: null,
+    domain: 'snipes.com',
+    zipcode: '',
+  },
+};
