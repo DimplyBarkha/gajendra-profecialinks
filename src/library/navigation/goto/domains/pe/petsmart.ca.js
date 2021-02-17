1f35535047ca2a1971dde62e2dfda26f439f4ae9
Shreyam <shreyam.g@osmosys.asia>
2021-02-17 20:57:15 +0530

    Updated options and proxy

--- src/library/navigation/goto/domains/pe/petsmart.ca.js
+++ src/library/navigation/goto/domains/pe/petsmart.ca.js
@@ -8,10 +8,14 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url }, parameters, context, dependencies) => {
-    url = `${url}#[!opt!]{"first_request_timeout":50000,"force200":true}[/!opt!]`;
-    context.setBlockAds(false);
-    context.setLoadAllResources(true);
-    context.setAntiFingerprint(false);
+    url = `${url}#[!opt!]{"block_ads":false,"first_request_timeout":60,"load_timeout":60,"load_all_resources":true,"discard_CSP_header":true,"force200": true}[/!opt!]`;
+    await context.setBypassCSP(true);
+    await context.setCssEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setJavaScriptEnabled(true);
+    await context.setBlockAds(false);
+    await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
     await context.goto(url, { timeout: 50000, waitUntil: 'load', checkBlocked: true, block_ads: false, load_all_resources: true, images_enabled: true });
     await context.evaluate(() => {
       if (document.querySelector("#main > div.error-page-content > div > div")) {
