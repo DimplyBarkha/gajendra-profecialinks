9748fa36bfb4aa1ed26a75900ce2b791141d1322
BorysMaciuch <borysmaciuch@gmail.com>
2021-02-17 10:16:58 +0100

    Add drive, deploy extractor

--- src/library/navigation/goto/domains/gr/groceries.asda.com.js
+++ src/library/navigation/goto/domains/gr/groceries.asda.com.js
@@ -1,10 +1,9 @@
-
 module.exports = {
-  implements: 'navigation/goto',
+  implements: "navigation/goto",
   parameterValues: {
-    domain: 'groceries.asda.com',
-    country: 'UK',
-    store: 'asda',
+    domain: "groceries.asda.com",
+    country: "UK",
+    store: "asda",
   },
   implementation: async (inputs, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 10000;
@@ -12,56 +11,98 @@ module.exports = {
     const url = `https://storelocator.asda.com/?cmpid=ahc-_-otc-storeloc-_-asdacom-_-hp-_-header-_-store`;
     const zipcode = inputs.zipcode;
     const storeId = inputs.storeID;
-    await context.goto(url, { timeout: 30000, waitUntil: 'load', checkBlocked: true, antiCaptchaOptions: { type: 'RECAPTCHA' } });
+    await context.goto(url, {
+      timeout: 30000,
+      waitUntil: "load",
+      checkBlocked: true,
+      antiCaptchaOptions: { type: "RECAPTCHA" },
+    });
     await new Promise((resolve, reject) => setTimeout(resolve, 10000));
     // await context.click('div.top-nav li:nth-child(2) a');
-    await context.setInputValue('input[class="search-input Locator-input js-locator-input"]', `${inputs.zipcode}`);
+    await context.setInputValue(
+      'input[class="search-input Locator-input js-locator-input"]',
+      `${inputs.zipcode}`
+    );
     await context.waitForSelector('button[type="submit"]');
     await context.click('button[type="submit"] img');
     await new Promise((resolve, reject) => setTimeout(resolve, 5000));
     const storeid = inputs.storeID;
-    console.log(storeid)
     const storeName = await context.evaluate(async (storeid) => {
-        //return !document.querySelector(`input[value="${storeid}"]`);
-        const query = `#js-yl-${storeid} span.LocationName`;
-        console.log(query)
-    await new Promise((resolve, reject) => setTimeout(resolve, 50000));
+      //return !document.querySelector(`input[value="${storeid}"]`);
+      const query = `#js-yl-${storeid} span.LocationName`;
+      await new Promise((resolve, reject) => setTimeout(resolve, 50000));
 
-        if(query) {
-          return document.querySelector(query) ? document.querySelector(query).textContent : '';
-        }
-      }, storeid);
-    console.log('store', storeName);
+      if (query) {
+        return document.querySelector(query)
+          ? document.querySelector(query).textContent
+          : "";
+      }
+    }, storeid);
+
+    const retailerId = await context.evaluate(async (storeid) => {
+      const query = `#js-yl-${storeid}`;
+      await new Promise((resolve, reject) => setTimeout(resolve, 50000));
+      const regex = /\d+/
+      if (query) {
+        return document.querySelector(query)
+          ? document.querySelector(query).id.match(regex)
+          : "";
+      }
+    }, storeid);
+
+    const retailerZipcode = await context.evaluate(async (storeid) => {
+      //return !document.querySelector(`input[value="${storeid}"]`);
+      const query = `#js-yl-${storeid} span.c-address-postal-code`;
+      await new Promise((resolve, reject) => setTimeout(resolve, 50000));
+
+      if (query) {
+        return document.querySelector(query)
+          ? document.querySelector(query).textContent
+          : "";
+      }
+    }, storeid);
+
     const url2 = `https://groceries.asda.com/search/${inputs.URL}`;
-    await context.goto(url2, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+    await context.goto(url2, {
+      timeout: 60000,
+      waitUntil: "load",
+      checkBlocked: true,
+    });
     await new Promise((resolve, reject) => setTimeout(resolve, 20000));
     await context.evaluate(async function () {
-      if (!document.querySelector('div.co-product')) {
-        throw Error('We cant find any product for input provided');
+      if (!document.querySelector("div.co-product")) {
+        throw Error("We cant find any product for input provided");
       }
     });
-    await context.waitForSelector('div.search-page-content__products-tab-content ul li a[data-auto-id="linkProductTitle"]');
+    await context.waitForSelector(
+      'div.search-page-content__products-tab-content ul li a[data-auto-id="linkProductTitle"]'
+    );
     const newUrl = await context.evaluate(function () {
-      const tempUrl = document.querySelector('div.search-page-content__products-tab-content ul li a[data-auto-id="linkProductTitle"]');
+      const tempUrl = document.querySelector(
+        'div.search-page-content__products-tab-content ul li a[data-auto-id="linkProductTitle"]'
+      );
       if (tempUrl) {
-        return `https://groceries.asda.com${tempUrl.getAttribute('href')}`;
+        return `https://groceries.asda.com${tempUrl.getAttribute("href")}`;
       }
     });
-    console.log(`storeId${inputs.storeID}`);
-    console.log(`postcode${inputs.zipcode}`);
     //const url = newUrl;
-    console.log('MAmatha1' + url);
-    await context.goto(newUrl, { timeout: 60000, waitUntil: 'load', checkBlocked: true });
+    await context.goto(newUrl, {
+      timeout: 60000,
+      waitUntil: "load",
+      checkBlocked: true,
+    });
     async function addHtml(html, id) {
-      const div = document.createElement('div');
-      div.setAttribute('id', id);
+      const div = document.createElement("div");
+      div.setAttribute("id", id);
       div.innerHTML = html;
       document.body.append(div);
-    };
-    await context.evaluate(addHtml, storeName, 'retailername');
-    console.log(zipcode);
+    }
+    await context.evaluate(addHtml, storeName, "retailername");
+    await context.evaluate(addHtml, retailerId, "retailerid");
+    await context.evaluate(addHtml, retailerZipcode, "retailerzipcode");
+
     // if (zipcode || storeId) {
     //   await dependencies.setZipCode(inputs);
     // }
-  }
+  },
 };
