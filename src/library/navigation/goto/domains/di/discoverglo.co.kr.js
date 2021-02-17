86eee19ecd2c7ce96ecde531e5a79084f1a9d0e2
Avinash <avinash.kadam@profecialinks.com>
2021-02-17 20:12:41 +0530

    retry goto url if status is not 200

--- src/library/navigation/goto/domains/di/discoverglo.co.kr.js
+++ src/library/navigation/goto/domains/di/discoverglo.co.kr.js
@@ -1,3 +1,52 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { timeout = 10000 } = parameters;
+  const { url, zipcode, storeId } = inputs;
+  // await context.goto(url, { timeout, waitUntil: 'load', checkBlocked: true, captureRequests: true });
+
+  let lastResponseData;
+  const run = async () => {
+    lastResponseData = await context.goto(url, {
+      timeout,
+      waitUntil: 'load',
+      checkBlocked: true,
+      captureRequests: true,
+    });
+    console.log('first responseData: ' + lastResponseData.status);
+    if (lastResponseData.status === 404 || lastResponseData.status === 410) {
+      return;
+    }
+    if (lastResponseData.status !== 200) {
+      lastResponseData = await context.goto(url, {
+        timeout,
+        waitUntil: 'load',
+        checkBlocked: true,
+        captureRequests: true,
+      });
+      console.log('retry for goto responseData: ' + lastResponseData.status);
+    }
+  };
+
+  try {
+    await run();
+  } catch (e) {
+    console.log('error in goto url');
+  }
+
+  // patch for synchronicity issue between json decoring and goto result
+  if (url.split('[!opt!]')[1] && url.split('[!opt!]')[1].includes('"type":"json"')) {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
+
+  console.log(`zipcode: ${zipcode}`);
+  if (zipcode || storeId) {
+    await dependencies.setZipCode(inputs);
+  }
+}
 
 module.exports = {
   implements: 'navigation/goto',
@@ -9,4 +58,5 @@ module.exports = {
     store: 'discoverglo',
     zipcode: '',
   },
+  implementation,
 };
