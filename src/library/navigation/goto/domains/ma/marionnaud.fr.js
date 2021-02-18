3be8bdc083a32184af7b7c6d1e6173216b30905a
pavitra r <pavithra.ramasamy-odc@partner.import.io>
2021-02-18 17:47:46 +0530

    added variantId and timeout - akshay channa

--- src/library/navigation/goto/domains/ma/marionnaud.fr.js
+++ src/library/navigation/goto/domains/ma/marionnaud.fr.js
@@ -3,13 +3,13 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'marionnaud.fr',
-    timeout: 100000,
+    timeout: 200000,
     country: 'FR',
     store: 'marionnaud',
     zipcode: '',
   },
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 30000;
-    await context.goto(url, { first_request_timeout: 90000, timeout, waitUntil: 'load', checkBlocked: true });
+    const timeout = parameters.timeout ? parameters.timeout : 90000;
+    await context.goto(url, { first_request_timeout: 150000, timeout, waitUntil: 'load', checkBlocked: true });
   },
 };
