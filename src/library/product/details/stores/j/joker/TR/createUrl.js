205030db4ffd51839800a92627bdbed038ddc3f1
krupesh.chaudhary <krupesh.chaudhary@kcsitglobal.com>
2021-02-18 13:21:48 +0530

    core joker run

--- src/library/product/details/stores/j/joker/TR/createUrl.js
+++ src/library/product/details/stores/j/joker/TR/createUrl.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'product/details/createUrl',
   parameterValues: {
     domain: 'joker.com.tr',
-    prefix: 'urun',
+    prefix: null,
     url: 'https://www.joker.com.tr/urun/{id}',
     country: 'TR',
     store: 'joker',
