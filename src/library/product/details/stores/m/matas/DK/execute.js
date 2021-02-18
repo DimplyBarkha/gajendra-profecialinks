ed273c34c8b65ec2e4afd9ce1a62bcc495cdb64b
aakash-osmosys <akash.s@osmosys.asia>
2021-02-19 04:05:58 +0530

    Updated no results xpath

--- src/library/product/details/stores/m/matas/DK/execute.js
+++ src/library/product/details/stores/m/matas/DK/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     country: 'DK',
     store: 'matas',
     domain: 'matas.dk',
-    loadedSelector: 'h1[class="product-name product-name--large"]',
-    noResultsXPath: '//span[@class="color-positive-text"]',
+    loadedSelector: 'h1.product-name.product-name--large',
+    noResultsXPath: '//span[contains(., "Beklager! Ingen resultater")]',
     zipcode: '',
   },
 };
