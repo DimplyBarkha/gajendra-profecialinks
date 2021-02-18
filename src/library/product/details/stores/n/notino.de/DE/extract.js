69e23341a4eaffdfbfb0aadbfd41031daf5a6ed8
prasath <r.rahul@olivedatacentre.com>
2021-02-18 10:38:30 -0800

    notino_de

--- src/library/product/details/stores/n/notino.de/DE/extract.js
+++ src/library/product/details/stores/n/notino.de/DE/extract.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
@@ -10,34 +11,40 @@ module.exports = {
   implementation,
 };
 async function implementation(
-  inputs,
-  parameters,
-  context,
-  dependencies,
+inputs,
+parameters,
+context,
+dependencies,
 ) {
-  const { transform } = parameters;
-  const { productDetails } = dependencies;
-  await context.evaluate(async function () {
-    function addElementToDocument(key, value) {
-      const catElement = document.createElement('div');
-      catElement.id = key;
-      catElement.textContent = value;
-      catElement.style.display = 'none';
-      document.body.appendChild(catElement);
+const { transform } = parameters;
+const { productDetails } = dependencies;
+await context.evaluate(async function () {
+  function addElementToDocument(key, value) {
+    const catElement = document.createElement('div');
+    catElement.id = key;
+    catElement.textContent = value;
+    catElement.style.display = 'none';
+    document.body.appendChild(catElement);
     }
-    var a = ''
-    // @ts-ignore
-    const c = document.querySelector('script[type="application/ld+json"]').innerText;
-    console.log(c, 'ggggggggggggggg')
-    // @ts-ignore
-    if (c.includes('OutOfStock')) {
-      a = "Out of Stock"
-    }
-    else {
-      a = "In Stock"
-    }
-    addElementToDocument('a', a);
-  });
-  return await context.extract(productDetails, { transform });
-  // return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
+
+  // @ts-ignore
+  var a=''
+  // @ts-ignore
+  const c = document.querySelector('script[type="application/ld+json"]').innerText;
+  const jsondata = JSON.parse(c);
+  const gtin = jsondata.gtin13;
+  if(c.includes('OutOfStock')){
+    a="Out of Stock"
+
+  }
+  else{
+    a= "In Stock"
+  }
+  addElementToDocument('a', a);
+  addElementToDocument('b', gtin);
+
+
+});
+return await context.extract(productDetails, { transform });
+// return await context.extract(productDetails, { transform, type: 'MERGE_ROWS' });
 }
