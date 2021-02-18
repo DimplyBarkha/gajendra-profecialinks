50b418faee71dc66c718c92264f89a76948cb7b2
kcs-mohammedbhanpurawala <mohammed.bhanpurawala@kcsitglobal.com>
2021-02-18 13:26:35 +0530

    Druni Variant Commit

--- src/library/product/details/stores/d/druni/ES/extract.js
+++ src/library/product/details/stores/d/druni/ES/extract.js
@@ -1,11 +1,100 @@
-const { transform } = require('./transform');
+const { transform } = require("./transform");
+
+async function implementation(inputs, parameters, context, dependencies) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  const data = [];
+  const variantCount = await context.evaluate(async function () {
+    return document.querySelector(".swatch-attribute")
+      ? document.querySelectorAll(".swatch-attribute .swatch-option").length
+      : 0;
+  });
+  const QuantityCount = await context.evaluate(async function () {
+    return document.querySelector("#super-product-table")
+      ? document.querySelectorAll("#super-product-table .item").length
+      : 0;
+  });
+  if (variantCount !== 0) {
+    for (let i = 0; i < variantCount; i++) {
+      await context.click(
+        `.swatch-attribute .swatch-option:nth-of-type(${i + 1})`
+      );
+      await context.evaluate(async function () {
+        function addElementToDom(element, id) {
+          const div = document.createElement("div");
+          div.id = id;
+          div.innerHTML = element;
+          document.querySelector("body").appendChild(div);
+        }
+        // const shortName = document.querySelector(".fotorama__thumb img")
+        //   ? document.querySelector(".fotorama__thumb img").src
+        //   : "";
+
+        // let alternateImages = shortName;
+        // addElementToDom(alternateImages, "alternateImages");
+      });
+      const extract = await context.extract(productDetails, { transform });
+      data.push(extract);
+      await context.evaluate(async function () {
+        const elementsIds = [];
+
+        elementsIds.forEach((elemId) => {
+          const element = document.querySelector(`div#${elemId}`);
+          if (element) {
+            element.parentNode.removeChild(element);
+          }
+        });
+      });
+    }
+    return data;
+  } else if (QuantityCount !== 0) {
+    for (let i = 0; i < QuantityCount; i++) {
+      await context.click(`#super-product-table .item:nth-of-type(${i + 1})`);
+      await context.evaluate(async function () {
+        function addElementToDom(element, id) {
+          const div = document.createElement("div");
+          div.id = id;
+          div.innerHTML = element;
+          document.querySelector("body").appendChild(div);
+        }
+        const shortName = document.querySelector(
+          "#super-product-table .special-price"
+        )
+          ? document.querySelector(
+              `#super-product-table .special-price:nth-of-type(${i + 1}`
+            ).innerText
+          : "";
+
+        let price = shortName;
+        addElementToDom(price, "price");
+      });
+      const extract = await context.extract(productDetails, { transform });
+      data.push(extract);
+      await context.evaluate(async function () {
+        const elementsIds = ["price"];
+
+        elementsIds.forEach((elemId) => {
+          const element = document.querySelector(`div#${elemId}`);
+          if (element) {
+            element.parentNode.removeChild(element);
+          }
+        });
+      });
+    }
+    return data;
+  } else {
+    return data;
+  }
+}
+
 module.exports = {
-  implements: 'product/details/extract',
+  implements: "product/details/extract",
   parameterValues: {
-    country: 'ES',
-    store: 'druni',
+    country: "ES",
+    store: "druni",
     transform: transform,
-    domain: 'druni.es',
-    zipcode: '',
+    domain: "druni.es",
+    zipcode: "",
   },
+  implementation,
 };
--- src/library/product/details/stores/d/druni/ES/transform.js
+++ src/library/product/details/stores/d/druni/ES/transform.js
@@ -34,28 +34,62 @@ const transform = (data, context) => {
       try {
         if (row.aggregateRating) {
           var baseObject = JSON.parse(row.aggregateRating[0].text);
-          row.aggregateRating = [
-            { text: baseObject.aggregateRating.ratingValue },
-          ];
+
+          var baseObj = baseObject.aggregateRating.ratingValue;
+          var finalRating = baseObj.replace(".", ",");
+          // console.log(finalRating);
+          row.aggregateRating = [{ text: finalRating }];
         }
         if (row.availabilityText) {
-          if (row.availabilityText[0].text == "box-tocart unavailable") {
+          if (row.availabilityText[0].text == "- + Añadir al carrito Añadir a favoritos") {
             // console.log("Out of stock");
-            row.availabilityText = [{ text: "Out of stock" }];
-          }
-          if(row.availabilityText[0].text == "box-tocart"){
-            // console.log("In stock");
-            row.availabilityText = [{ text: "In stock" }];
+            row.availabilityText = [{ text: "In Stock" }];
           }
         }
-        if(row.category){
-          let x = row.category[0].text
-          var rat = x.replace('window.dataLayer = window.dataLayer || []; window.dataLayer.push({"ecommerce":{"currencyCode":"EUR"},"pageType":"catalog_product_view","list":"detail"}); window.dataLayer.push(', '');
-          var semi = rat.replace(`); (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NQDSNPL');`,``);
+        if (row.category) {
+          let x = row.category[0].text;
+          var rat = x.replace(
+            'window.dataLayer = window.dataLayer || []; window.dataLayer.push({"ecommerce":{"currencyCode":"EUR"},"pageType":"catalog_product_view","list":"detail"}); window.dataLayer.push(',
+            ""
+          );
+          var semi = rat.replace(
+            `); (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NQDSNPL');`,
+            ``
+          );
           var obj = JSON.parse(semi);
           row.category = [{ text: obj.product.category }];
-          
         }
+        if (row.variantId) {
+          let x = row.variantId[0].text;
+          var rat = x.replace(
+            'window.dataLayer = window.dataLayer || []; window.dataLayer.push({"ecommerce":{"currencyCode":"EUR"},"pageType":"catalog_product_view","list":"detail"}); window.dataLayer.push(',
+            ""
+          );
+          var semi = rat.replace(
+            `); (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NQDSNPL');`,
+            ``
+          );
+          var obj = JSON.parse(semi);
+          row.variantId = [{ text: obj.product.id }];
+        }
+        // if (row.alternateImages) {
+        //   var i,j;
+        //   var objects = [];
+        //   var finalObjects = [];
+        //     for (i = 0; i < row.alternateImages.length; i++) {
+              
+        //         var obj = row.alternateImages[i].text;
+        //         var object = obj.split("?")[0];
+        //         objects.push(object);
+        //         finalObjects.push(objects.concat());
+        //         console.log("working");
+        //     console.log(objects);
+
+        //     }
+        // //     // console.log(finalObjects);
+        // //     // row.alternateImages = [{ text: object }];
+          
+        // }
       } catch (exception) {
         console.log("Error in transform", exception);
       }
