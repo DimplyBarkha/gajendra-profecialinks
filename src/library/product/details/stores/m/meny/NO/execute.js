578077d21ddc60122483d0a048fa7eb7af1b2420
wasiqe-a <wasiqe.a@osmosys.asia>
2021-02-18 18:19:48 +0530

    Fixes interaction errors

--- src/library/product/details/stores/m/meny/NO/execute.js
+++ src/library/product/details/stores/m/meny/NO/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'meny',
     domain: 'meny.no',
     loadedSelector: '#maincontent > div > div > header',
-    noResultsXPath: "//p[contains(., 'Noe gikk galt') and contains(., 'ikke funnet')]",
+    noResultsXPath: "//p[contains(., 'Noe gikk galt') and contains(., 'ikke funnet')] | //main[@id='maincontent']/div[count(*)=0]",
     zipcode: '',
   },
 };
