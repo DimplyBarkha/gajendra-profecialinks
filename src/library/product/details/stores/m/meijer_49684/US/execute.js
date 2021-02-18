ec0302eafcd77e7efc7045161e06f0788dc4d20b
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-18 15:14:55 +0530

    updated zipcode to execute.js

--- src/library/product/details/stores/m/meijer_49684/US/execute.js
+++ src/library/product/details/stores/m/meijer_49684/US/execute.js
@@ -1,4 +1,49 @@
+const implementation = async (inputs, { loadedSelector, noResultsXPath }, context, dependencies) => {
+  const { url, id } = inputs;
+  let builtUrl;
+  if (!url) {
+    if (!id) throw new Error('No id provided');
+    else builtUrl = await dependencies.createUrl(inputs);
+  }
+  await dependencies.goto({ ...inputs, url: builtUrl || url });
+  await context.evaluate(async function () {
+    try {
+      // @ts-ignore
+      document.querySelector('span[class="glyphicon glyphicon-edit js-change-store-link"]').click()
+      await new Promise(r => setTimeout(r, 4000));
+      console.log("waiting for open store input box")
+      // @ts-ignore
+      document.querySelector('a[class="StoreFlyout__changeStore"]').click()
+      await new Promise(r => setTimeout(r, 4000));
+      console.log("waiting for send store id to input link")
+      var att = document.createAttribute('value')
+      att.value = "49684"
+      document.querySelector('input[id="store-flyout-address"]').setAttributeNode(att)
+      // @ts-ignore
+      document.querySelector('button[class="StoreFlyout__search-button btn btn-primary"]').click()
+      await new Promise(r => setTimeout(r, 5000));
+      console.log("waiting for search link")
+      // @ts-ignore
+      document.querySelector('button[class="StoreFlyout__myStore btn btn-primary StoreFlyout__selectOptions"]').click()
+      // await new Promise(r => setTimeout(r, 5000));
+      console.log("waiting for submit link")
+    }
+    catch (e) {
 
+    }
+  });
+  if (loadedSelector) {
+    await context.waitForFunction(
+      (selector, xpath) => {
+        return !!(document.querySelector(selector) || document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue);
+      },
+      { timeout: 10000 },
+      loadedSelector,
+      noResultsXPath,
+    );
+  }
+  return await context.evaluate((xpath) => !document.evaluate(xpath, document, null, XPathResult.BOOLEAN_TYPE, null).booleanValue, noResultsXPath);
+};
 module.exports = {
   implements: 'product/details/execute',
   parameterValues: {
@@ -9,4 +54,5 @@ module.exports = {
     noResultsXPath: null,
     zipcode: '',
   },
+  implementation,
 };
--- src/library/product/details/stores/m/meijer_49684/US/extract.js
+++ src/library/product/details/stores/m/meijer_49684/US/extract.js
@@ -8,47 +8,10 @@ module.exports = {
     domain: 'meijer.com',
     zipcode: '',
   },
-  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+  implementation: async (inputs, parameters, context, dependencies) => {
+    const { transform } = parameters;
+    const { productDetails } = dependencies;
     await context.evaluate(async function () {
-      try{
-        // @ts-ignore
-        document.querySelector('span[class="glyphicon glyphicon-edit js-change-store-link"]').click()
-        await new Promise(r => setTimeout(r, 4000));
-        console.log("waiting for open store input box")
-        // @ts-ignore
-        document.querySelector('a[class="StoreFlyout__changeStore"]').click()
-        await new Promise(r => setTimeout(r, 4000));
-        console.log("waiting for send store id to input link")
-        var att = document.createAttribute('value')
-        att.value = "49684"
-        document.querySelector('input[id="store-flyout-address"]').setAttributeNode(att)
-        // @ts-ignore
-        document.querySelector('button[class="StoreFlyout__search-button btn btn-primary"]').click()
-        await new Promise(r => setTimeout(r, 5000));
-        console.log("waiting for search link")
-        // @ts-ignore
-        document.querySelector('button[class="StoreFlyout__myStore btn btn-primary StoreFlyout__selectOptions"]').click()
-        // await new Promise(r => setTimeout(r, 5000));
-        console.log("waiting for submit link")
-        }
-        catch(e)
-        {
-        
-        }
-      function addElementToDocument(key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
-      }
-      const getXpath = (xpath, prop) => {
-        const elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-        let result;
-        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-        else result = elem ? elem.singleNodeValue : '';
-        return result && result.trim ? result.trim() : result;
-      };
       const getAllXpath = (xpath, prop) => {
         const nodeSet = document.evaluate(
           xpath,
@@ -64,58 +27,23 @@ module.exports = {
         }
         return result;
       };
+      function addHiddenDiv(id, content) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll("div[itemprop='name']")[0];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      }
       //custom code to concatenate
-      var manufacturerDesc = getAllXpath("//div[@class='wc-rich-content-description']//text()","nodeValue");
-      // custom code to get price with $
-      // console.log('abc'+manufacturerDesc)
-      let final_manufacturerDesc;
+      var manufacturerDesc = getAllXpath("//div[@class='wc-rich-content-description']//text()", "nodeValue");
+      let final_manufacturerDesc = '';
       for (let index = 0; index < manufacturerDesc.length; index++) {
-        final_manufacturerDesc += manufacturerDesc[index]
+        final_manufacturerDesc += manufacturerDesc[index];
       }
-      console.log('Saipavan'+final_manufacturerDesc)
-      addElementToDocument('final_manufacturerDesc', final_manufacturerDesc)
-      try {
-        const size = getXpath("//div[@class='lsection mobile-product-name h6']//text()", 'nodeValue');
-        console.log('Raghav'+size);
-        var nameArr = size.split(',');
-        console.log('Sai'+nameArr);
-        console.log('Rahul'+nameArr[nameArr.length - 1]);
-        addElementToDocument('size', nameArr[nameArr.length - 1])
-      //   const price = getXpath("//div[@class='display-price']//span[@itemprop='price']/text() | (//div[@class='display-price sale-price']/text())[1]", 'nodeValue');
-      //   if (price.includes('$')) {
-      //     addElementToDocument('price', price);
-      //   }
-      //   else {
-      //     addElementToDocument('price', '$' + price);
-      //   }
-      } catch (error) {
-
-      }
-      // try {
-      //   var stock = "In Stock"
-      //   const availability = getXpath('//h2[@itemprop="availability"]/text()', 'nodeValue');
-      //   if (availability.includes("In Stock")) {
-      //     addElementToDocument('availability', stock);
-      //     console.log(stock)
-      //   }
-      //   else {
-      //     stock = "Out Of Stock"
-      //     console.log(stock)
-      //     addElementToDocument('availability', stock);
-      //   }
-      // } catch (error) {
-
-      // }
-      // try{
-      // function addclass(xpathforpagination) {
-      //     var elems = document.querySelectorAll(xpathforpagination);
-      //     elems[0].classList.add('pagination');
-      //   }
-      // }
-      // catch(e){
-
-      // }
+      console.log('Saipavan' + final_manufacturerDesc);
+      addHiddenDiv('final_manufacturerDesc', final_manufacturerDesc);
     });
-    await context.extract(productDetails, { transform: transformParam });
+    return await context.extract(productDetails, { transform });
   },
 };
