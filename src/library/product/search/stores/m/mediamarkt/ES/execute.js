3898df885a76f624d0b01d7261f73f0c9c4514b6
unknown <ramraut25@gmail.com>
2021-02-19 01:06:25 +0530

    Updated xpaths and selectors

--- src/library/product/search/stores/m/mediamarkt/ES/execute.js
+++ src/library/product/search/stores/m/mediamarkt/ES/execute.js
@@ -5,8 +5,8 @@ module.exports = {
     store: 'mediamarkt',
     domain: 'mediamarkt.es',
     url: 'https://www.mediamarkt.es/es/search.html?query={searchTerms}&searchProfile=onlineshop&channel=mmeses',
-    loadedSelector: 'div.product-wrapper',
-    noResultsXPath: '//div[contains(@id, "search_no_result")]|//div[@id="product-details"]',
+    loadedSelector: 'div[class*="ProductFlexBox"]',
+    noResultsXPath: "//div[contains(@class,'ZeroResultsView')]//p[contains(.,'Lamentablemente, no encontramos una coincidencia adecuada para tu búsqueda')]",
     zipcode: '',
   },
 };
