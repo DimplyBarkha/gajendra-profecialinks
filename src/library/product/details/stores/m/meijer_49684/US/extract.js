9af7501d99b955748713f40843f1abca2ba88a6d
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-18 17:37:12 +0530

    added wait for selector

--- src/library/product/details/stores/m/meijer_49684/US/extract.js
+++ src/library/product/details/stores/m/meijer_49684/US/extract.js
@@ -8,42 +8,53 @@ module.exports = {
     domain: 'meijer.com',
     zipcode: '',
   },
-  implementation: async (inputs, parameters, context, dependencies) => {
-    const { transform } = parameters;
-    const { productDetails } = dependencies;
-    await context.evaluate(async function () {
-      const getAllXpath = (xpath, prop) => {
-        const nodeSet = document.evaluate(
-          xpath,
-          document,
-          null,
-          XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-          null
-        );
-        const result = [];
-        for (let index = 0; index < nodeSet.snapshotLength; index++) {
-          const element = nodeSet.snapshotItem(index);
-          if (element) result.push(prop ? element[prop] : element.nodeValue);
-        }
-        return result;
-      };
-      function addHiddenDiv(id, content) {
-        const newDiv = document.createElement('div');
-        newDiv.id = id;
-        newDiv.textContent = content;
-        newDiv.style.display = 'none';
-        const originalDiv = document.querySelectorAll("div[itemprop='name']")[0];
-        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
-      }
-      //custom code to concatenate
-      var manufacturerDesc = getAllXpath("//div[@class='wc-rich-content-description']//text()", "nodeValue");
-      let final_manufacturerDesc = '';
-      for (let index = 0; index < manufacturerDesc.length; index++) {
-        final_manufacturerDesc += manufacturerDesc[index];
-      }
-      console.log('Saipavan' + final_manufacturerDesc);
-      addHiddenDiv('final_manufacturerDesc', final_manufacturerDesc);
-    });
-    return await context.extract(productDetails, { transform });
-  },
+  implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  try {
+    await context.waitForSelector("div[class='wc-rich-content-description']", { timeout: 30000 });
+  } catch (error) {
+    
+  }
+  await context.evaluate(async function () {
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(
+        xpath,
+        document,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null
+      );
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+      }
+      return result;
+    };
+    function addHiddenDiv(id, content) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll("div[itemprop='name']")[0];
+      originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+    }
+    //custom code to concatenate
+    var manufacturerDesc = getAllXpath("//div[@class='wc-rich-content-description']//text()", "nodeValue");
+    let final_manufacturerDesc = '';
+    for (let index = 0; index < manufacturerDesc.length; index++) {
+      final_manufacturerDesc += manufacturerDesc[index];
+    }
+    console.log('Saipavan' + final_manufacturerDesc);
+    addHiddenDiv('final_manufacturerDesc', final_manufacturerDesc);
+  });
+  return await context.extract(productDetails, { transform });
+}
