80a2b116c231bd6041ff47d880d60b6ab46df0d1
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-18 16:36:59 +0530

    updated execute.js

--- src/library/product/search/stores/l/loblaws/CA/execute.js
+++ src/library/product/search/stores/l/loblaws/CA/execute.js
@@ -6,12 +6,7 @@ module.exports = {
     store: 'loblaws',
     domain: 'loblaws.ca',
     url: 'https://www.loblaws.ca/search?search-bar={searchTerms}',
-<<<<<<< HEAD
-    // loadedSelector: 'li[class="product-tile-group__list__item"]',
-    // url: 'https://www.loblaws.ca/search?search-bar=Aero',
-=======
     loadedSelector: null,
     noResultsXPath: null,
->>>>>>> 925fa7fbf830ff316326e0a824cce53636541c54
   },
 };
