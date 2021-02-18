293dad267cba9c4e8b67ad135da41c7f14bff0e0
Rekha Takalkar <rekha.takalkar@profecialinks.com>
2021-02-18 12:29:49 +0530

    updated timeout

--- src/library/navigation/goto/domains/bi/bigw.com.au.js
+++ src/library/navigation/goto/domains/bi/bigw.com.au.js
@@ -3,24 +3,20 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'bigw.com.au',
-    timeout: 50000,
+    timeout: 100000,
     country: 'AU',
     store: 'bigw',
     zipcode: '',
   },
-  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
-   if (zipcode) {
-  url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true, "cookie_jar":[{"name":"invCheckPostalCode","value":${zipcode}}]}[/!opt!]`;
-} else {
-  url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
-}
-    await context.goto(url, {
-      block_ads: false,
-      load_all_resources: true,
-      images_enabled: true,
-      timeout: 50000,
-      waitUntil: 'load',
-    });
-    await context.waitForNavigation();
+  implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
+    const timeout = parameters.timeout ? parameters.timeout : 2000;
+    await context.setLoadAllResources(true);
+    await context.setAntiFingerprint(false);
+    await context.setBlockAds(false);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, first_request_timeout: 60, load_timeout: 60 });
+    console.log(zipcode);
+    if (zipcode) {
+      await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
+    }
   },
 };
