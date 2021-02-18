29cf963fdb266f21ac1188e80bc1dc2abc4a5b6e
sushma-mangalapally <sushma.mangalapally@import.io>
2021-02-18 13:41:51 -0800

    Create review extractor for sephora us

---
+++ src/library/product/reviews/stores/s/sephora/US/reviews.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  implements: 'product/reviews',
+  parameterValues: {
+    country: 'US',
+    store: 'sephora',
+    domain: 'sephora.com',
+    zipcode: '',
+    mergeType: null,
+  },
+};
