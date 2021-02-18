273842cf351409f40f553f347a9c6ed5f9d3f0cb
Rekha Takalkar <rekha.takalkar@profecialinks.com>
2021-02-18 11:24:36 +0530

    updated

--- src/library/navigation/goto/domains/bi/bigw.com.au.js
+++ src/library/navigation/goto/domains/bi/bigw.com.au.js
@@ -8,4 +8,19 @@ module.exports = {
     store: 'bigw',
     zipcode: '',
   },
+  implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
+   if (zipcode) {
+  url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true, "cookie_jar":[{"name":"invCheckPostalCode","value":${zipcode}}]}[/!opt!]`;
+} else {
+  url = `${url}#[!opt!]{"first_request_timeout":50000, "force200": true}[/!opt!]`;
+}
+    await context.goto(url, {
+      block_ads: false,
+      load_all_resources: true,
+      images_enabled: true,
+      timeout: 50000,
+      waitUntil: 'load',
+    });
+    await context.waitForNavigation();
+  },
 };
