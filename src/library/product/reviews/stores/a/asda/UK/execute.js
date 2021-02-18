71690edc219ac920caa16d50d4050c58faec8cea
Wayne E <bartekstafiej2@gmail.com>
2021-02-18 13:03:07 +0100

    Finished RD extractor (3508)

--- src/library/product/reviews/stores/a/asda/UK/execute.js
+++ src/library/product/reviews/stores/a/asda/UK/execute.js
@@ -6,9 +6,52 @@ module.exports = {
     store: 'asda',
     domain: 'asda.com',
     loadedSelector: 'main.product-detail-page.layout__main',
-    noResultsXPath: '//section[@class="layout__section page-not-found-layout__section"]',
+    noResultsXPath: '//section[@class="layout__section page-not-found-layout__section"] | //div[@class="pdp-description-reviews__no-reviews"]',
     reviewUrl: 'https://groceries.asda.com/product/{id}',
-    sortButtonSelectors: null,
+    sortButtonSelectors: 'null',
     zipcode: "''",
+  },
+  implementation: async function (
+    { id },
+    { loadedSelector, noResultsXPath },
+    context,
+    dependencies,
+  ) {
+    const url = 'https://groceries.asda.com/product/{id}'.replace(
+      '{id}',
+      encodeURIComponent(id),
+    );
+
+    await context.goto(url);
+
+    await context.waitForNavigation();
+
+    if (loadedSelector) {
+      await context.waitForFunction((sel, xp) => {
+        return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+      }, { timeout: 10000 }, loadedSelector, noResultsXPath);
+    }
+
+    // clicking in reviews section if there are results
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    await context.evaluate(() => {
+      const reviewsSection = document.querySelector('button[data-auto-id="tab-1"]');
+
+      if (reviewsSection !== null) {
+        // @ts-ignore
+        reviewsSection.click();
+      }
+    });
+
+    console.log('Checking no results', noResultsXPath);
+    return await context.evaluate((xp) => {
+      const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+      console.log(xp, r);
+      const e = r.iterateNext();
+      console.log(e);
+      return !e;
+    }, noResultsXPath);
   },
 };
--- src/library/product/reviews/stores/a/asda/UK/extract.js
+++ src/library/product/reviews/stores/a/asda/UK/extract.js
@@ -19,38 +19,50 @@ module.exports = {
     // eslint-disable-next-line no-shadow
     const { transform } = parameters;
 
-    // clicking in reviews section
+    async function extractionHelper () {
+      await context.evaluate(() => {
+        let review;
+        const reviews = document.querySelectorAll('div[data-auto-id="reviewContents"] div[class="rating-stars__stars rating-stars__stars--top"]');
+        for (let i = 0; i < reviews.length; i++) {
+          review = reviews[i].style.width.replace('%', '');
+          document.querySelectorAll(
+            'div[data-auto-id="reviewContents"] div[class="rating-stars__stars rating-stars__stars--top"]')[i]
+            .setAttribute('review', (parseFloat(review) / 20));
+        }
 
-    await context.evaluate(() => {
-      const reviewsSection = document.querySelector('button[data-auto-id="tab-1"]');
-
-      if (reviewsSection !== null) {
-        // @ts-ignore
-        reviewsSection.click();
-      }
-    });
+        const url = document.location.href;
+        document.querySelector('section[id="main-content"]').setAttribute('sku', url.match('([^\/]+$)')[1]);
+      });
+    }
 
     async function nextPage () {
       await context.evaluate(() => {
         document.querySelector('button[data-auto-id="btnright"]').click();
       });
+      ;
     }
 
-    while (true) {
-      const date = Date.now();
+    // pagination
 
-      const lastPaginationButton = await context.evaluate(() => {
-        return document.querySelector(
-          'span[class*="asda-icon asda-icon--gray asda-icon--small asda-icon--rotate270"]');
+    let pageNumber = 1;
+
+    while (true) {
+      await context.waitForSelector('div[class="pdp-description-reviews__reviews-contents-cntr"], div[class="pdp-description-reviews__no-reviews"]');
+      const lastPage = await context.evaluate(() => {
+        return parseInt(document.querySelector(
+          'button[class="asda-link asda-link--primary asda-link--button co-pagination__last-page"]').textContent);
       });
 
-      if (lastPaginationButton !== null) {
+      extractionHelper();
+
+      if (lastPage === pageNumber || pageNumber >= 15) {
         return await context.extract(productReviews, { transform }, 'MERGE_ROWS');
       } else {
         await context.extract(productReviews, { transform }, 'MERGE_ROWS');
 
         await new Promise((resolve) => setTimeout(resolve, 1500));
 
+        pageNumber++;
         nextPage();
       }
     }
