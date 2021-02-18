63ae9af1d37a5b561e46992b4052d6fbfb4f3efa
webDevpriyesh <developerpriyesh29@gmail.com>
2021-02-18 18:40:13 +0530

    video issue fixed

--- src/library/product/details/stores/c/cocopanda/DK/extract.js
+++ src/library/product/details/stores/c/cocopanda/DK/extract.js
@@ -28,9 +28,9 @@ async function implementation (
       document.body.appendChild(prodEle);
     }
 
-    var youtube = document.querySelector('iframe.videoly-box');
+    var youtube = document.getElementsByClassName('videoly-box');
     if(youtube){
-      var video_list = youtube.contentDocument.getElementsByClassName('b-video-item-tile');
+      var video_list = youtube[0].contentDocument.getElementsByClassName('b-video-item-tile');
       for(var i=0; i<video_list.length; i++){
         var video_id = video_list[i].getAttribute('data-videoid');
         addEleToDoc('custom_video_class',video_id);
