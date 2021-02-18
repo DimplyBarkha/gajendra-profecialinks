0783f8aaf49c5292970cc1f98aa43ea184217973
Rekha Takalkar <rekha.takalkar@profecialinks.com>
2021-02-18 17:04:09 +0530

    updated files

--- src/library/navigation/goto/domains/bi/bigw.com.au.js
+++ src/library/navigation/goto/domains/bi/bigw.com.au.js
@@ -9,14 +9,13 @@ module.exports = {
     zipcode: '',
   },
   implementation: async ({ url, zipcode, storeId }, parameters, context, dependencies) => {
-    const timeout = parameters.timeout ? parameters.timeout : 40000;
-    //const timeout = 10000;
-    console.log("mere time out yahaaaaaaaaaa===============",parameters);
+    const timeout = parameters.timeout ? parameters.timeout : 25000;
     await context.setLoadAllResources(true);
+    await context.setLoadImages(true);
     await context.setAntiFingerprint(false);
     await context.setBlockAds(false);
-    await context.goto(url, {timeout: timeout, waitUntil: 'load', checkBlocked: true, first_request_timeout: 60, load_timeout: 60 });
-    console.log(zipcode);
+    await context.goto(url, { timeout: timeout, waitUntil: 'load', checkBlocked: true, first_request_timeout: 60, load_timeout: 60 });
+    console.log("zzzzzzzzzzz",zipcode);
     if (zipcode) {
       await dependencies.setZipCode({ url: url, zipcode: zipcode, storeId });
     }
