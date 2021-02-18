490602d54d1fd4370039e5fc1794baa1debe7fc6
unknown <leena.gavali-odc@partner.import.io>
2021-02-18 16:23:52 +0530

    QA_core_

--- src/library/product/details/stores/m/menards/US/extract.js
+++ src/library/product/details/stores/m/menards/US/extract.js
@@ -17,17 +17,32 @@ module.exports = {
         catElement.style.display = 'none';
         document.body.appendChild(catElement);
       }
+      const getAllXpath = (xpath, prop) => {
+        const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        const result = [];
+        for (let index = 0; index < nodeSet.snapshotLength; index++) {
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
+        }
+        return result;
+      };
+
+      var second = getAllXpath('//meta[@property="og:image"][position()>1]/@content', 'nodeValue');
+      if (second.length >= 1){
+        var abc = second.join(" || ");
+        addElementToDocument('image', abc);
+    }
       // @ts-ignore
-      const manufacturer = window.qubitEcProduct[0].product.manufacturer;
-      if (manufacturer != null){
-      addElementToDocument('detail-manufacturer', manufacturer);
-      }
+      // const manufacturer = window.qubitEcProduct[0].product.manufacturer;
+      // if (manufacturer != null){
+      // addElementToDocument('detail-manufacturer', manufacturer);
+      // }
 
       // @ts-ignore
-      const category = window.qubitEcProduct[0].product.category;
-      if (category != null){
-      addElementToDocument('sub-category', category);
-      }
+      // const category = window.qubitEcProduct[0].product.category;
+      // if (category != null){
+      // addElementToDocument('sub-category', category);
+      // }
       
 
     });
