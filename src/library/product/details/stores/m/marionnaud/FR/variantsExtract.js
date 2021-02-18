a67794062f5d91bf3490cc4ac060e32b48f62255
pavitra r <pavithra.ramasamy-odc@partner.import.io>
2021-02-18 18:53:55 +0530

    updated cc for variantUrl - akshay channa

--- src/library/product/details/stores/m/marionnaud/FR/variantsExtract.js
+++ src/library/product/details/stores/m/marionnaud/FR/variantsExtract.js
@@ -8,4 +8,46 @@ module.exports = {
     domain: 'marionnaud.fr',
     zipcode: '',
   },
+  implementation,
 };
+async function implementation(
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { variants } = dependencies;
+  await context.evaluate(async function () {
+
+    function addElementToDocument (key, value) {
+      const catElement = document.createElement('div');
+      catElement.id = key;
+      catElement.textContent = value;
+      catElement.style.display = 'none';
+      document.body.appendChild(catElement);
+    }
+
+    const getAllXpath = (xpath, prop) => {
+      const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const result = [];
+      for (let index = 0; index < nodeSet.snapshotLength; index++) {
+        const element = nodeSet.snapshotItem(index);
+        if (element) result.push(prop ? element[prop] : element.nodeValue);
+      }
+      return result;
+    };
+
+    
+
+
+    var url = getAllXpath('//div[@id="productVariantDisplay"]/div/a/@href | //div[@id="buttonClickshow"]/ul/li/a/@href', 'nodeValue');
+      for(var i=0; i<url.length; i++){
+        url[i] = "https://www.marionnaud.fr"+url[i];
+        addElementToDocument("url", url[i]);
+      }
+
+
+    });
+    return await context.extract(variants, { transform });
+  }
