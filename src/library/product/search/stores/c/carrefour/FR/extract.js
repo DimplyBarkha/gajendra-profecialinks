f9f5859ad45305f555d0bcd951011bec90d793ae
Akansha Wajge <akansha.wajge@synerzip.com>
2021-02-19 11:52:41 +0530

    carrefour fr search pagination  changes

--- src/library/product/search/stores/c/carrefour/FR/extract.js
+++ src/library/product/search/stores/c/carrefour/FR/extract.js
@@ -10,148 +10,74 @@ module.exports = {
   },
   implementation,
 };
-// async function implementation (
-//   inputs,
-//   parameters,
-//   context,
-//   dependencies,
-// ) {
-//   const { transform } = parameters;
-//   const { productDetails } = dependencies;
-//   //--------------------------------------
-//    await new Promise((resolve, reject) => setTimeout(resolve, 8000));
-//    try {
-//     await context.waitForSelector('button#footer_tc_privacy_button', { timeout: 90000 });
-//     let cookieBtnClicked = 0; 
-//     await context.evaluate( () => {
-//     // await context.evaluateInFrame('div.v-content__wrap iframe', () => {
-//       let cookieButton = document.querySelector('button#footer_tc_privacy_button');
-//       if (cookieButton) {
-//         // @ts-ignore
-//         cookieButton.click();
-//         cookieBtnClicked = 1;
-//       }
-//     });
-//     console.log('cookieBtnClicked before if: ', cookieBtnClicked);
-//     if(cookieBtnClicked === 1){
-//       console.log('cookieBtnClicked: ', cookieBtnClicked);
-//     }
-//     } catch (error) {
-//       console.log('error: ', error);
-//     }
-//       try {
-//       await context.waitForXPath('//div[@class="ab-popin_content"]', { timeout: 30000 });
-//       await context.evaluateInFrame('iframe', () => {
-//         let closePopUp = document.querySelector('div.ab-popin_content button.modal__close');
-//         if (closePopUp) {
-//           // @ts-ignore
-//           closePopUp.click();
-//         }
-//       });
-//       } catch (error) {
-//         console.log('error: ', error);
-//       }
-//    await new Promise((resolve, reject) => setTimeout(resolve, 8000));
-//   //--------------------------------------
-//   await context.evaluate(async () => {
-//     try {
-//       await new Promise((resolve) => setTimeout(resolve, 6000));
-//     }catch(error){
-//       console.log(error)
-//     }
-//     async function infiniteScroll () {
-//       let prevScroll = document.documentElement.scrollTop;
-//       while (true) {
-//         window.scrollBy(0, document.documentElement.clientHeight);
-//         await new Promise(resolve => setTimeout(resolve, 1000));
-//         const currentScroll = document.documentElement.scrollTop;
-//         if (currentScroll === prevScroll) {
-//           break;
-//         }
-//         prevScroll = currentScroll;
-//       }
-//     }
-//       await infiniteScroll();
-//   })
-
-//   try {
-//     await new Promise((resolve) => setTimeout(resolve, 6000));
-//   } catch (error) {
-//     console.log('error: ', error);
-    
-//   }
-//   return await context.extract(productDetails, { transform });
-// }
-
-async function implementation (
+async function implementation(
   inputs,
   parameters,
   context,
   dependencies,
 ) {
+  const { results } = inputs;
   const { transform } = parameters;
   const { productDetails } = dependencies;
-  let nextLinkSelector = null;
-  try {
-    await new Promise(resolve => setTimeout(resolve, 5000));
-  } catch (error) {
-    console.log('All products not loaded after scrolling!!');
-  }
-  nextLinkSelector = await context.evaluate(() => 
-      !!document.querySelector('div.pagination__button-wrap button')
-    );
-    async function nextLink() {
-      nextLinkSelector = await context.evaluate(() => 
-        !!document.querySelector('div.pagination__button-wrap button')
-      );
-
-      let count = 0;
-      while(nextLinkSelector){
-        count += await context.evaluate(() => {
-          return document.querySelectorAll('li.product-grid-item').length;
-        });
-
-        if(count <= 179){
-          await context.extract(productDetails, { transform }, { type: 'APPEND' });
-          await context.click('div.pagination__button-wrap button');
-          await new Promise(resolve => setTimeout(resolve, 2000));
-          nextLinkSelector = await context.evaluate(() => 
-            !!document.querySelector('div.pagination__button-wrap button')
-          );
-        } else {
-          break;
+  await context.evaluate(async (results) => {
+    try {
+      let ul = document.querySelectorAll('ul#data-plp_produits');
+      let finalUL;
+      if (ul) {
+        finalUL = ul[1];
+      }
+      let totalProducts = finalUL.querySelectorAll('li.product-grid-item').length;
+      if (totalProducts) {
+        // await new Promise(resolve => setTimeout(resolve, 5000));
+        try {
+          await context.waitForSelector('li.product-grid-item');
+        } catch (error) {
+          console.log('All products not loaded after scrolling!!');
         }
       }
-    }
-    if(nextLinkSelector){
-      await nextLink();
-    }
-  // await context.evaluate(async function () { 
-  //   while (document.querySelector('div.pagination__button-wrap button')) {
-  //     const element = document.querySelector('div.pagination__button-wrap button');
-  //     element.scrollIntoView();
-  //     await new Promise(resolve => setTimeout(resolve, 1500));
-  //     // @ts-ignore
-  //     document.querySelector('div.pagination__button-wrap button').click();
-  //     await new Promise(resolve => setTimeout(resolve, 1500));
-  //     let ul = document.querySelectorAll('ul#data-plp_produits');
-  //     let finalUL ;
-  //     if(ul){
-  //       finalUL = ul[1];
-  //     }
-  //     let NoOFelementForBreak=finalUL.querySelectorAll("li.product-grid-item").length;
+      if (document.querySelector('div.pagination__button-wrap button')) {
+        let loadMoreBtn = true;
+        while (loadMoreBtn) {
+          document.querySelector('div.pagination__button-wrap button').click();
+          await new Promise(resolve => setTimeout(resolve, 2000));
+          let ul1 = document.querySelectorAll('ul#data-plp_produits');
+          let finalUL1;
+          if (ul1) {
+            finalUL1 = ul1[1];
+          }
+          let currentCount = finalUL1.querySelectorAll('li.product-grid-item').length;
+          console.log(currentCount)
+          // if (currentCount >= results) {
+          if (currentCount >= 150) {
+            loadMoreBtn = false;
+          }
+        }
+      }
+      //----------------
+      async function infiniteScroll() {
+        let prevScroll = document.documentElement.scrollTop;
+        while (true) {
+          window.scrollBy(0, document.documentElement.clientHeight);
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          const currentScroll = document.documentElement.scrollTop;
+          if (currentScroll === prevScroll) {
+            break;
+          }
+          prevScroll = currentScroll;
+        }
+      }
+      await infiniteScroll();
+      //--------------------
       
-  //     // @ts-ignore
-  //     if( NoOFelementForBreak=="150"){
-  //       break;
-  //     }
-  //   }
-  // });
+    } catch (e) {
+      console.log('error in load more reviews');
+    }
+  }, results);
   try {
-    await context.waitForXPath('//li[contains(@class,"product-grid-item")]');
     await new Promise(resolve => setTimeout(resolve, 5000));
   } catch (error) {
     console.log('All products not loaded after scrolling!!');
   }
+
   return await context.extract(productDetails, { transform });
 }
--- src/library/product/search/stores/c/carrefour/FR/search.js
+++ src/library/product/search/stores/c/carrefour/FR/search.js
@@ -7,4 +7,49 @@ module.exports = {
     domain: 'carrefour.fr',
     zipcode: '',
   },
+  implementation: async (inputs, { country, store, domain, zipcode }, context, { execute, extract, paginate }) => {
+    const { URL, keywords, Keywords, results = 150, Brands, query } = inputs;
+    const inputKeywords = Keywords || keywords || Brands;
+
+    // TODO: consider moving this to a reusable function
+    const length = (results) => results.reduce((acc, { group }) => acc + (Array.isArray(group) ? group.length : 0), 0);
+
+    const resultsReturned = await execute({
+      ...inputs,
+      searchURL: URL,
+      keywords: inputKeywords,
+      zipcode: inputs.zipcode || zipcode,
+      query: query,
+    });
+
+    // do the search
+
+    if (!resultsReturned) {
+      console.log('No results were returned');
+      return;
+    }
+
+    // try gettings some search results
+    const pageOne = await extract({results});
+
+    let collected = length(pageOne);
+
+    console.log(`Got initial number of results: ${collected}`);
+
+    // check we have some data
+    if (collected === 0) {
+      console.log('Was not able to collect any data on the first page');
+      return;
+    }
+
+    let page = 2;
+    while (collected < results && await paginate({ keywords: inputKeywords, page, offset: collected })) {
+      const data = await extract({results});
+      const count = length(data);
+      if (count === 0) break; // no results
+      collected += count;
+      console.log('Got more results', collected);
+      page++;
+    }
+  },
 };
