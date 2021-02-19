ff1ed8229e3d3f1e12aa67f997b83157870d1e1e
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-02-19 13:16:53 +0530

    #3509 devbuybuybabysearch

--- src/library/product/search/stores/b/Buybuybaby/US/extract.js
+++ src/library/product/search/stores/b/Buybuybaby/US/extract.js
@@ -1,11 +1,25 @@
 const { transform } = require('../../../../shared');
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await context.evaluate(async () => {
+    //  await new Promise(r => setTimeout(r, 6000));
+     await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    });
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
     country: 'US',
     store: 'Buybuybaby',
-    transform,
+    transform: transform,
     domain: 'buybuybaby.com',
-    zipcode: '',
   },
+  implementation, 
 };
