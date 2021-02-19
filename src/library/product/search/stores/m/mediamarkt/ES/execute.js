d81da263f6ffbeff776cedfc5b986ee12ee49e63
Suryakanta Sahoo <suryakanta.s@osmosys.asia>
2021-02-19 21:36:18 +0530

    fixed loading, thumbnail, gtin, searchurl

--- src/library/product/search/stores/m/mediamarkt/ES/execute.js
+++ src/library/product/search/stores/m/mediamarkt/ES/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     domain: 'mediamarkt.es',
     url: 'https://www.mediamarkt.es/es/search.html?query={searchTerms}&searchProfile=onlineshop&channel=mmeses',
     // url: 'https://www.mediamarkt.es/es/search.html?query={searchTerms}',
-    loadedSelector: 'div[class*="ProductFlexBox"]',
+    loadedSelector: 'div[class*="ProductFlexBox"] img',
     noResultsXPath: "//div[contains(@class,'ZeroResultsView')]//p[contains(.,'Lamentablemente, no encontramos una coincidencia adecuada para tu búsqueda')]",
     zipcode: '',
   },
