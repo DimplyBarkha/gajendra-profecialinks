9227f4a2d7c46ac2406810ea68ad0878f5b171c1
romancg <gromantsg@gmail.com>
2021-02-19 15:14:27 +0200

    #3494 fix price

--- src/library/product/details/stores/m/metro/UA/extract.js
+++ src/library/product/details/stores/m/metro/UA/extract.js
@@ -13,19 +13,6 @@ module.exports = {
     const { transform } = parameters;
     const { productDetails } = dependencies;
     await context.evaluate(() => {
-      function addHiddenDiv (id, text) {
-        const div = document.createElement('div');
-        div.id = id;
-        div.innerHTML = text;
-        document.body.appendChild(div);
-      }
-      // if (document.querySelector('.big-product-card__add-to-cart-btn').innerText === 'Добавить в корзину') {
-      //   addHiddenDiv('stock', 'In Stock');
-      //   //document.querySelector('.main').setAttribute('stock', 'In Stock');
-      // } else {
-      //   addHiddenDiv('stock', 'Out of Stock');
-      //   //document.querySelector('.main').setAttribute('stock', 'Out of Stock');
-      // }
       const btn = document.querySelector('.jsx-3209444046');
       if (btn) {
         btn.click();
