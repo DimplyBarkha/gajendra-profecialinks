c4bc9b553e0468fdad66295bbc69b9ad8de3ca30
Your Name <karthik.ravi-odc@partner.import.io>
2021-02-19 20:11:49 +0530

    new

--- src/library/product/details/stores/f/flaschenpost/DE/extract.js
+++ src/library/product/details/stores/f/flaschenpost/DE/extract.js
@@ -18,23 +18,47 @@ module.exports = {
     await context.evaluate(async () => {
       try {
         const usernameElements = document.querySelectorAll('#validZipcode');
-      // @ts-ignore
-      usernameElements.forEach(username => username.value = "28203");
-      // @ts-ignore
-      document.querySelector('div[class="fp-modal_input"]>button').click()
-      await new Promise(r => setTimeout(r, 6000));
-        
+        // @ts-ignore
+        usernameElements.forEach(username => username.value = "28203");
+        // @ts-ignore
+        document.querySelector('div[class="fp-modal_input"]>button').click()
+        await new Promise(r => setTimeout(r, 6000));
+
       } catch (error) {
-        
-      } 
-      function addElementToDocument(key, value) {
-        const catElement = document.createElement('div');
-        catElement.className = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
+
       }
-      
+      var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+      function addHiddenDiv(id, content, index) {
+        const newDiv = document.createElement('div');
+        newDiv.id = id;
+        newDiv.textContent = content;
+        newDiv.style.display = 'none';
+        const originalDiv = document.querySelectorAll('div[class="fp_product_details"] div[class*="fp_article"] div[class="fp_article_details"]')[index];
+        originalDiv.parentNode.insertBefore(newDiv, originalDiv);
+      }
+      var vari = getXpath('/html/body/div[1]/script[2]/text()', 'nodeValue');
+      if (vari != null) {
+        if (vari.includes("id:'")) {
+          var ids = vari.split("id:'");
+          let finalIds = [];
+          for (let j = 1; j < ids.length; j++) {
+            if (ids[j].includes("',")) {
+              finalIds.push(ids[j]);
+            }
+          }
+          for (var i = 0; i < finalIds.length; i++) {
+            var temp = finalIds[i].split("',")[0];
+            addHiddenDiv('rpc', temp, i);
+          }
+        }
+      }
+
     });
     return await context.extract(productDetails, { transform });
   },
