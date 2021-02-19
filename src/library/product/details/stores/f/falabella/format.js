8f01aa217464f5a14a4c976eaa9d2bc635b5b55f
adheer <sangeta.kumari@synerzip.com>
2021-02-19 16:01:51 +0530

    kick back issues quick image fail issue & price issues

--- src/library/product/details/stores/f/falabella/format.js
+++ src/library/product/details/stores/f/falabella/format.js
@@ -4,38 +4,42 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-    for (const { group } of data) {
-      for (const row of group) {
-        if (row.description) {
-          row.description.forEach(item => {
-            item.text = item.text.replace(/(\s?\n)+/g, ' | ').trim();
-          });
-        }
-        if (row.specifications) {
-          row.specifications.forEach(item => {
-            item.text = item.text.replace(/(\s?\n)+/g, ' ').trim();
-          });
-        }
-        if (row.alternateImages) {
-            row.alternateImages.forEach(item => {
-              item.text = item.text.replace("?wid=800&hei=800&qlt=70", '').trim();
-            });
-        }        
-        if (row.aggregateRatingText) {
-          row.aggregateRatingText.forEach(item => {
-            item.text = item.text.replace(/(\s?\n)+/g, ' | ').trim();
-          });
-        }
-        if (row.shippingInfo) {
-          row.shippingInfo.forEach(item => {
-            item.text = item.text.replace(/(\s?\n)+/g, ' | ').trim();
-          });
-        }
-
+  for (const { group } of data) {
+    for (const row of group) {
+      if (row.description) {
+        row.description.forEach(item => {
+          item.text = item.text.replace(/(\s?\n)+/g, ' | ').trim();
+        });
+      }
+      if (row.specifications) {
+        row.specifications.forEach(item => {
+          item.text = item.text.replace(/(\s?\n)+/g, ' ').trim();
+        });
+      }
+      if (row.price) {
+        row.price.forEach(item => {
+          item.text = item.text.replace(' (Precio final)', '').trim();
+          // item.text = item.text.replace('.', '').trim();
+        });
+      }
+      if (row.alternateImages) {
+        row.alternateImages.forEach(item => {
+          item.text = item.text.replace('?wid=800&hei=800&qlt=70', '').trim();
+        });
+      }
+      if (row.aggregateRatingText) {
+        row.aggregateRatingText.forEach(item => {
+          item.text = item.text.replace(/(\s?\n)+/g, ' | ').trim();
+        });
+      }
+      if (row.shippingInfo) {
+        row.shippingInfo.forEach(item => {
+          item.text = item.text.replace(/(\s?\n)+/g, ' | ').trim();
+        });
       }
     }
-    return data;
-  };
-  
-  module.exports = { transform };
-  
+  }
+  return data;
+};
+
+module.exports = { transform };
