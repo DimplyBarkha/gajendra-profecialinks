626738b13935ebfa849e9ce7a8257bd53d98fd00
Narasimharao Mannepalli <narasimharao.m@HCL.COM>
2021-02-19 17:38:15 +0530

    bjs RD

--- src/library/product/reviews/stores/b/bjs/US/execute.js
+++ src/library/product/reviews/stores/b/bjs/US/execute.js
@@ -7,7 +7,7 @@ module.exports = {
     domain: 'bjs.com',
     loadedSelector: null,
     noResultsXPath: null,
-    reviewUrl: 'https://display.powerreviews.com/m/9794/l/en_US/product/P_172661/reviews?filters=&search=&sort=Newest&image_only=false&apikey=7c12e7e9-fe30-4e7a-bcb8-8376b9117a6b',
+    reviewUrl: 'https://display.powerreviews.com/m/9794/l/en_US/product/P_{id}/reviews?filters=&search=&sort=Newest&image_only=false&apikey=7c12e7e9-fe30-4e7a-bcb8-8376b9117a6b',
     sortButtonSelectors: null,
     zipcode: '',
   },
--- src/library/product/reviews/stores/b/bjs/US/extract.js
+++ src/library/product/reviews/stores/b/bjs/US/extract.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   implements: 'product/reviews/extract',
   parameterValues: {
@@ -7,5 +6,90 @@ module.exports = {
     transform: null,
     domain: 'bjs.com',
     zipcode: '',
+  },
+
+  implementation: async ({ url }, { country, domain }, context, { productDetails }) => {
+    await context.evaluate(async function () {
+    // @ts-ignore
+      // @ts-ignore
+      // @ts-ignore
+      // @ts-ignore
+      function stall (ms) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve();
+          }, ms);
+        });
+      }
+      // const dataObj = window.dataLayer[0].product;
+      function addElementToDocument (key, value) {
+        const catElement = document.createElement('div');
+        catElement.id = key;
+        catElement.textContent = value;
+        catElement.style.display = 'none';
+        document.body.appendChild(catElement);
+      }
+      // function addHiddenDiv (node, id, content) {
+      //   const newDiv = document.createElement('div');
+      //   newDiv.id = id;
+      //   newDiv.textContent = content;
+      //   newDiv.style.display = 'none';
+      //   node.appendChild(newDiv);
+      // }
+      // const aggregateRating = document.querySelectorAll('#lb-results > div > div > ul > li > div > div > a > div > p > span > span');
+      // for (let k = 0; k < aggregateRating.length; k++) {
+      // // @ts-ignore
+      //   let singleRating = aggregateRating[k].style.width;
+      //   singleRating = singleRating.slice(0, singleRating.length - 1);
+      //   singleRating = (5 * singleRating) / 100;
+      //   singleRating = singleRating.toFixed(1);
+      //   addHiddenDiv(aggregateRating[k], 'aggregateRating', singleRating);
+      // }
+
+      // const itemContainers = document.querySelectorAll('#lb-results > div > div > ul > li');
+      // let rank = 1;
+      // for (const itemContainer of itemContainers) {
+      //   console.log(itemContainer);
+      //   const totalRank = itemContainer + rank;
+      //   addHiddenDiv(itemContainer, 'rank', totalRank);
+      //   rank++;
+      // }
+      // await stall(7000);
+      // @ts-ignore
+      // const mainDataObj = window.dataLayer[6].ecommerce.detail.products[0].dimension2;
+      // if (mainDataObj) {
+      //   addElementToDocument('pd_variantId', mainDataObj);
+      // }
+
+      // var str = document.querySelector('#snippet-google-dataLayer-push > script').innerText;
+      // if (str.length > 0) {
+      //   var refStr = 'ecommerce":{"detail":{"products":[{"dimension2":"';
+      //   var id = str.substring(str.indexOf(refStr) + refStr.length, str.indexOf('","name'));
+      //   if (id.match(/(\d{7})/g)) {
+      //     var prodid = id.match(/(\d{7})/g)[0];
+      //     addElementToDocument('pd_variantId', prodid);
+      //   }
+      // }
+
+      // await context.waitForXPath("//a[@class='product-top__main-image-link img-box compare-add-img']/img[@class='img-box__img js-lazy js-only jsOnly product-top__main-image compare-img js-lazy--loaded']/@src | //a[@class='product-top__main-image-link img-box compare-add-img']/img[@class='img-box__img js-lazy js-only jsOnly product-top__main-image compare-img js-lazy--loaded']/@src | //a[@class='product-top__main-image-link img-box compare-add-img js-pdbox']/img[@class='img-box__img js-lazy js-only jsOnly product-top__main-image compare-img js-lazy--loaded']/@src");
+      // await stall(7000);
+      // const image = document.querySelector('#product-main-img > a > img').getAttribute('src');
+      // console.log(image);
+      // if (image) {
+      //   addElementToDocument('image', image);
+      // }
+
+      // var Information = JSON.parse(document.querySelector('script[type="application/ld+json"]').innerText);
+      // addElementToDocument('sku', Information.sku);
+      // addElementToDocument('gtin', Information.gtin13);
+      // addElementToDocument('brand', Information.brand.name);
+      // addElementToDocument('image', Information.image);
+      // addElementToDocument('name', Information.name);
+
+      var reviewXpath = JSON.parse(document.querySelector('body > pre').nodeValue);
+      //const reviewXpath = getAllXpath("/html/body/pre",'nodeValue');
+      console.log("reviewXpath::::", reviewXpath);
+    });
+    await context.extract(productDetails);
   },
 };
