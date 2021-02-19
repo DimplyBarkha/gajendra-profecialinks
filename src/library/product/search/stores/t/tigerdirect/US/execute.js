a8001cc889570e5685874a1641207108cd80558d
unknown <leena.gavali-odc@partner.import.io>
2021-02-19 12:34:08 +0530

    rank_fail

--- src/library/product/search/stores/t/tigerdirect/US/execute.js
+++ src/library/product/search/stores/t/tigerdirect/US/execute.js
@@ -6,8 +6,8 @@ module.exports = {
     store: 'tigerdirect',
     domain: 'tigerdirect.com',
     url: 'https://www.tigerdirect.com/applications/SearchTools/search.asp?keywords={searchTerms}',
-    loadedSelector: null,
-    noResultsXPath: null,
+    loadedSelector: 'div[class="main-content"]',
+    noResultsXPath: '//div[@class="row"]/h1',
     zipcode: '',
   },
 };
--- src/library/product/search/stores/t/tigerdirect/US/paginate.js
+++ src/library/product/search/stores/t/tigerdirect/US/paginate.js
@@ -4,12 +4,14 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'tigerdirect',
-    nextLinkSelector: 'a[title="Next page"]',
+    // nextLinkSelector: 'a[title="Next page"]',
     mutationSelector: null,
     spinnerSelector: null,
-    loadedSelector: 'body',
-    noResultsXPath: null,
-    openSearchDefinition: null,
+    loadedSelector: 'div[class="main-content"]',
+    noResultsXPath: '//div[@class="row"]/h1',
+    openSearchDefinition: {
+      template: 'https://www.tigerdirect.com/applications/SearchTools/search.asp?page={page}&keywords={searchTerms}&sort=0&recs=10',
+      },
     domain: 'tigerdirect.com',
     zipcode: '',
   },
