cf497fb734d41a40936dfb88f516bc8eba435e5b
sasz94 <sacha.bartlomiej@gmail.com>
2021-02-19 11:17:02 +0100

    Added variant recursive extraction

--- src/library/navigation/goto/domains/ko/kohls.com.js
+++ src/library/navigation/goto/domains/ko/kohls.com.js
@@ -11,7 +11,7 @@ module.exports = {
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
     await context.goto(url, {
       timeout: 30000,
-      waitUntil: 'networkidle0',
+      waitUntil: 'load',
       checkBlocked: false,
     });
   },
--- src/library/product/details/stores/k/kohls/US/extract.js
+++ src/library/product/details/stores/k/kohls/US/extract.js
@@ -34,7 +34,125 @@ async function implementation (inputs, parameters, context, dependencies) {
     }
   });
 
-  return await context.extract(productDetails, { transform });
+  const returnVariantDataForRecursion = async () => {
+    return await context.evaluate(async () => {
+      const variantCategoryArray = [];
+      console.log('herereererere');
+      if (document.querySelectorAll('div#product-specifications div.product_boss_tmpl > div.pdp-product-color a.color-swatch').length > 0) variantCategoryArray.push({ selector: 'div#product-specifications div.product_boss_tmpl > div.pdp-product-color', length: document.querySelectorAll('div#product-specifications div.product_boss_tmpl > div.pdp-product-color a.color-swatch').length });
+     /*  if (document.querySelectorAll('div#product-specifications div.product_boss_tmpl > div.pdp-product-size a.pdp-size-swatch') && document.querySelectorAll('div#product-specifications div.product_boss_tmpl > div.pdp-product-size a.pdp-size-swatch').length > 0) variantCategoryArray.push({ selector: 'div#product-specifications div.product_boss_tmpl > div.pdp-product-size', length: document.querySelectorAll('div#product-specifications div.product_boss_tmpl > div.pdp-product-size a.pdp-size-swatch').length });
+      */ if (document.querySelectorAll('div#product-specifications div.product_boss_tmpl select > option[data-skusize]:not([class])').length > 0) variantCategoryArray.push('div#product-specifications div.product_boss_tmpl select > option[data-skusize]:not([class])');
+       return variantCategoryArray;
+    });
+  };
+
+  const clickVariant = async (selector, index, level, dropdownSelector) => {
+    await context.evaluate(async ({ selector, index }) => {
+      const variantEl = document.querySelector(selector).querySelectorAll('a')[index];
+      console.log(variantEl);
+      variantEl.click();
+    }, { selector, index });
+    if (level === 0) {
+      if (dropdownSelector) {
+        const dropdownOptions = await context.evaluate(async (selector) => {
+          const dropdownAppend = document.createElement('div');
+          dropdownAppend.id = 'added_dropdown';
+          document.body.appendChild(dropdownAppend);
+          const optionElements = Array.from(document.querySelectorAll(selector));
+          const data = [];
+          optionElements.forEach(el => {
+            data.push(el.textContent);
+          });
+          return data;
+         /*  for (const el of dropdownOptions) {
+            console.log(el.textContent);
+            dropdownAppend.textContent = el.textContent;
+            await new Promise((resolve) => setTimeout(resolve, 100));
+          } */
+        }, dropdownSelector);
+        for (const el of dropdownOptions) {
+          context.evaluate(async (text) => {
+            document.querySelector('div#added_dropdown').textContent = text;
+          }, { text: el });
+          await context.extract(productDetails, { transform });
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+      } else {
+        await context.extract(productDetails, { transform });
+      }
+    }
+  };
+
+  const loopThroughVariants = async (data, dropdownSelector) => {
+    const variantCategory = data.shift();
+    const selector = variantCategory.selector;
+    const length = variantCategory.length;
+    console.log('data length rekurencja: ', data.length);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    for (let i = 0; i < length; i++) {
+      console.log('IM ON THIS SELECTOR: ', selector);
+      console.log(`clicking varaint:${i}`);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await clickVariant(selector, i, data.length, dropdownSelector);
+      if (data.length >= 1) {
+        console.log('going below bye');
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        await loopThroughVariants([...data]);
+      }
+    }
+  };
+
+  // checking if a product has variants and navigating to them
+  const checkIfProductHasVariantsAndClickThem = async () => {
+    const variantWrappersArray = await returnVariantDataForRecursion();
+    const dropdownSelector = typeof variantWrappersArray[variantWrappersArray.length - 1] === 'string' ? variantWrappersArray.pop() : null;
+    console.log('data z variantami: ', variantWrappersArray);
+   /*  const variantWrapperElementLength = await context.evaluate(async (wrapers) => {
+      return document.querySelector(wrapers[0]).querySelectorAll('a').length;
+    }, variantWrappersArray);
+
+    const variantWrapperElementLength2 = await context.evaluate(async (wrapers) => {
+      return document.querySelector(wrapers[1]).querySelectorAll('a').length;
+    }, variantWrappersArray); */
+    console.log('length arrayki przed rekurencja: ', variantWrappersArray.length);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await loopThroughVariants(variantWrappersArray, dropdownSelector);
+
+    /* if (variantCategoryAmount > 1) {
+      const firstCategoryVariants = await context.evaluate(async () => {
+        var firstVariants = [];
+        document
+          .querySelectorAll('div#multiSkuContainer > div:first-of-type ul li')
+          .forEach((variant) => {
+            firstVariants.push(variant.innerHTML);
+          });
+        return firstVariants;
+      });
+      for (let j = 0; j < firstCategoryVariants.length; j++) {
+        await context.evaluate((j) => {
+          document
+            .querySelectorAll(
+              'div#multiSkuContainer > div:first-of-type ul li button',
+              // @ts-ignore
+            )[j].click();
+        }, j);
+        await context.waitForNavigation();
+        await clickAllVariants(
+          variantElements,
+          'div#multiSkuContainer > div:last-of-type ul li button',
+        );
+      }
+    } else if (variantCategoryAmount === 1) {
+      await clickAllVariants(
+        variantElements,
+        'div#multiSkuContainer > div:last-of-type ul li button',
+      );
+    } else {
+      await extractSingleProductData();
+    } */
+  };
+
+  await checkIfProductHasVariantsAndClickThem();
 }
 
 module.exports = {
