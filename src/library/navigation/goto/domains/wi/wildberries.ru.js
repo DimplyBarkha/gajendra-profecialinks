59e6408c279d211e0b1a98f59a6f8721ec02e764
banderovskyi <roman1004b@gmail.com>
2021-02-19 10:21:39 +0200

    Init rd extractor

---
+++ src/library/navigation/goto/domains/wi/wildberries.ru.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'wildberries.ru',
+    timeout: null,
+    jsonToTable: null,
+    country: 'RU',
+    store: 'wildberries',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/reviews/stores/w/wildberries/RU/paginate.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  implements: 'navigation/paginate',
+  parameterValues: {
+    template: null,
+    country: 'RU',
+    store: 'wildberries',
+    nextLinkSelector: null,
+    nextPageUrlSelector: null,
+    nextLinkXpath: null,
+    mutationSelector: null,
+    spinnerSelector: null,
+    loadedSelector: null,
+    loadedXpath: null,
+    noResultsXPath: null,
+    stopConditionSelectorOrXpath: null,
+    resultsDivSelector: null,
+    openSearchDefinition: null,
+    domain: 'wildberries.ru',
+    zipcode: '',
+  },
+};
