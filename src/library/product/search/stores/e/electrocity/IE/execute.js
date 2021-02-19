a98bdd04d8226d05b46bc1a920b730415cbd358a
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-19 15:39:03 +0530

    updated xpath for id and updated the url structure

--- src/library/product/search/stores/e/electrocity/IE/execute.js
+++ src/library/product/search/stores/e/electrocity/IE/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'IE',
     store: 'electrocity',
     domain: 'electrocity.ie',
-    url: 'https://www.electrocity.ie/?s="{searchTerms}"&post_type=product&dgwt_wcas=1',
+    url: 'https://www.electrocity.ie/?s={searchTerms}&post_type=product&dgwt_wcas=1',
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
