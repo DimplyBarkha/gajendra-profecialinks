7ea0ac26751e14dd30d47f7ff5cbb4fc94a9d71a
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-02-19 12:58:08 +0100

    - Improved search extractors and reduced it to 1 - Updated tests

--- src/library/product/search/stores/e/easyparapharmacie/FR/execute.js
+++ src/library/product/search/stores/e/easyparapharmacie/FR/execute.js
@@ -1,4 +1,4 @@
-
+// @ts-nocheck
 module.exports = {
   implements: 'product/search/execute',
   parameterValues: {
@@ -9,5 +9,58 @@ module.exports = {
     loadedSelector: null,
     noResultsXPath: null,
     zipcode: '',
+  },
+  implementation: async (inputs, { url, loadedSelector, noResultsXPath }, context, dependencies) => {
+    const { keywords, results } = inputs;
+    const destinationUrl = url.replace('{searchTerms}', encodeURIComponent(keywords));
+    await dependencies.goto({ ...inputs, url: destinationUrl });
+
+    await context.evaluate(async (results) => {
+      const waitForResults = async (prevResultsNum) => {
+        let resultsNum = document.querySelectorAll('div.product-container').length;
+        let iteration = 0;
+        while (prevResultsNum === resultsNum && iteration < 10) {
+          console.log(`Prev results: ${prevResultsNum}`);
+          console.log(`Results now: ${resultsNum}`);
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          resultsNum = document.querySelectorAll('div.product-container').length;
+          iteration++;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      };
+
+      let buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
+      if (buttonShowMore) {
+        let totalProducts = document.querySelectorAll('div.product-container').length;
+        while (totalProducts < results && !buttonShowMore.disabled) {
+          buttonShowMore.click();
+          await waitForResults(totalProducts);
+          totalProducts = document.querySelectorAll('div.product-container').length;
+          buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
+        }
+      }
+    }, results || 150);
+
+    if (loadedSelector) {
+      await context.waitForFunction(
+        function (sel, xp) {
+          return Boolean(
+            document.querySelector(sel) ||
+              document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext(),
+          );
+        },
+        { timeout: 10000 },
+        loadedSelector,
+        noResultsXPath,
+      );
+    }
+    console.log('Checking no results', noResultsXPath);
+    return await context.evaluate((xp) => {
+      const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+      console.log(xp, r);
+      const e = r.iterateNext();
+      console.log(e);
+      return !e;
+    }, noResultsXPath);
   },
 };
--- src/library/product/search/stores/e/easyparapharmacie/FR/extract.js
+++ src/library/product/search/stores/e/easyparapharmacie/FR/extract.js
@@ -5,53 +5,6 @@ const implementation = async (inputs, parameters, context, dependencies) => {
   const { transform } = parameters;
   const { productDetails } = dependencies;
 
-  await context.evaluate(async () => {
-    const waitForResults = async (prevResultsNum) => {
-      let resultsNum = document.querySelectorAll('div.product-container').length;
-      let iteration = 0;
-      while (prevResultsNum === resultsNum && iteration < 10) {
-        console.log(`Prev results: ${prevResultsNum}`);
-        console.log(`Results now: ${resultsNum}`);
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        resultsNum = document.querySelectorAll('div.product-container').length;
-        iteration++;
-      }
-    };
-
-    let buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
-    if (buttonShowMore) {
-      let totalProducts = document.querySelectorAll('div.product-container').length;
-      // There are 2 extractors here, because IM search needs 1000 results and
-      // there's no way to pass 'results' input to extract.js
-      // For IM search uncomment/comment 2 lines below
-      // while (totalProducts < 1000 && !buttonShowMore.disabled) {
-      while (totalProducts < 150 && !buttonShowMore.disabled) {
-        buttonShowMore.click();
-        await waitForResults(totalProducts);
-        totalProducts = document.querySelectorAll('div.product-container').length;
-        buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
-      }
-
-      // let counter = 0;
-      // const resultsPerPage = 24;
-      // // We add here 1000 because this is what IM search requires, and so far
-      // // there's no way to pass inputs to extract.js
-      // // const maxResultsForExtractor = 150 - resultsPerPage;
-      // const maxResultsForExtractor = 1000 - resultsPerPage;
-      // do {
-      //   if (Math.floor(maxResultsForExtractor / resultsPerPage) <= counter) {
-      //     break;
-      //   }
-      //   buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
-      //   // @ts-ignore
-      //   buttonShowMore.click();
-      //   await stall(1000);
-      //   counter++;
-      //   // @ts-ignore
-      // } while (buttonShowMore.disabled === false);
-    }
-  });
-
   await context.evaluate(async function () {
     const addDiv = (className, content, productDiv) => {
       const newDiv = document.createElement('div');
