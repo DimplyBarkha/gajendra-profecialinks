e9e89be23216791733c551b8f214cf95d7491bbf
sushma-mangalapally <sushma.mangalapally@import.io>
2021-02-18 16:45:17 -0800

    Set up reviews for US

--- src/library/product/reviews/stores/s/sephora/US/extract.js
+++ src/library/product/reviews/stores/s/sephora/US/extract.js
@@ -1,4 +1,25 @@
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { productReviews } = dependencies;
+  async function scrollToRec (node) {
+    await context.evaluate(async (node) => {
+      const element = document.querySelector(node) || null;
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+        await new Promise((resolve) => {
+          setTimeout(resolve, 5000);
+        });
+      }
+    }, node);
+  }
+  await scrollToRec('div#ratings-reviews-container');
 
+  return await context.extract(productReviews);
+}
 module.exports = {
   implements: 'product/reviews/extract',
   parameterValues: {
@@ -8,4 +29,5 @@ module.exports = {
     domain: 'sephora.com',
     zipcode: '',
   },
+  implementation,
 };
