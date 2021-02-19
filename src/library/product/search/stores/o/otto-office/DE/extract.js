edf762e306a3bdb1e198c7f667241e5b5492c3b0
Prateek3018 <prateek.s@osmosys.asia>
2021-02-20 00:21:59 +0530

    updated id

--- src/library/product/search/stores/o/otto-office/DE/extract.js
+++ src/library/product/search/stores/o/otto-office/DE/extract.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   implementation,
 };
-async function implementation(
+async function implementation (
   inputs,
   parameters,
   context,
@@ -19,30 +19,26 @@ async function implementation(
   const { transform } = parameters;
   const { productDetails } = dependencies;
   await context.evaluate(async function () {
-
-    function addHiddenDiv1(id, content, index) {
-      const newDiv = document.createElement("div");
+    function addHiddenDiv1 (id, content, index) {
+      const newDiv = document.createElement('div');
       newDiv.id = id;
       newDiv.textContent = content;
-      newDiv.style.display = "none";
+      newDiv.style.display = 'none';
       const originalDiv = document.querySelectorAll('a[class="oo-link fftracking-productclick"] b')[index];
       originalDiv.parentNode.insertBefore(newDiv, originalDiv);
     }
-    let rawRating = document.querySelectorAll('div[class="item-rating-stars_gold ooB-abs"]');
+    const rawRating = document.querySelectorAll('div[class="item-rating-stars_gold ooB-abs"]');
     let temp, temp1;
     for (let i = 0; i < rawRating.length; i++) {
-      temp = rawRating[i].getAttribute('style')
-      if (temp.includes("width:")) {
-        temp1 = temp.split("width:")[1];
-        temp1 = temp1.split("px")[0];
+      temp = rawRating[i].getAttribute('style');
+      if (temp.includes('width:')) {
+        temp1 = temp.split('width:')[1];
+        temp1 = temp1.split('px')[0];
         temp1 = parseInt(temp1);
         temp1 = (temp1 * 5) / 65;
         addHiddenDiv1('aggrating', temp1, i);
       }
     }
-
-
-    
   });
   return await context.extract(productDetails, { transform });
-  };
+};
