288f414217cb17eb8b12f652acb349cb4bd285bf
Shweta Kedari <shwetak@synerzipune.local>
2021-02-20 02:46:34 +0530

    added loaded selector for URL input

--- src/library/product/details/stores/c/carrefour/FR/execute.js
+++ src/library/product/details/stores/c/carrefour/FR/execute.js
@@ -5,7 +5,7 @@ module.exports = {
     country: 'FR',
     store: 'carrefour',
     domain: 'carrefour.fr',
-    loadedSelector: 'div.product-list',
+    loadedSelector: 'div.product-list , div.pdp-card__header--text',
     noResultsXPath: '//div[@class="error-block"]/div | //div[contains(@class, "search-results-head__title search-results-head__title--no-result")]',
     zipcode: '',
   },
