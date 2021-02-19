a98c81f1616f2491cc679c0e0fd248ffe472f04b
banderovskyi <roman1004b@gmail.com>
2021-02-19 14:55:34 +0200

    Change shared

--- src/library/product/reviews/stores/l/liquorland/AU/shared.js
+++ src/library/product/reviews/stores/l/liquorland/AU/shared.js
@@ -4,30 +4,51 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data) => {
-  function findKeyValue (key, data) {
-    let result = [];
-    data.forEach((dataObj) => {
-      dataObj.group.forEach((dataObjElem) => {
-        if (dataObjElem[key]) {
-          result = dataObjElem[key];
-        }
-      });
-    });
-    return result;
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+    // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
+    .trim();
+  for (const { group } of data) {
+    for (const row of group) {
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
+    }
   }
+  function checkIfReviewIsFromLast30Days (reviewDate) {
+    const timestamp = new Date().getTime() - (30 * 24 * 60 * 60 * 1000);
+    if (new Date(reviewDate).getTime() >= timestamp) {
+      return true;
+    }
+    return false;
+  }
+  data = data.filter(function (item) {
+    console.log('group length before' + item.group.length);
+    item.group = item.group.filter(function (row) {
+      if (checkIfReviewIsFromLast30Days(row.reviewDate[0].text)) {
+        return true;
+      }
+      return false;
+    });
+    console.log('group length after' + item.group.length);
+    item.rows = item.group.length;
+    if (item.group.length !== 0) {
+      return true;
+    }
+    return false;
+  });
 
   data.forEach((dataObj) => {
     dataObj.group.forEach((fieldName, index) => {
-      for (var row in fieldName) {
-        fieldName[row].forEach((elem, i) => {
-          const fieldText = fieldName[row][i].text;
-          fieldName[row][i].text = fieldText.replace(
-            /(\n \n \n \n|\n \n \n|\n \n|\r\n|\n|\r)/gm,
-            ' ',
-          );
-        });
-      }
-
       if(fieldName.reviewRating){
         const ratingValue = fieldName.reviewRating[0].value;
         if(ratingValue > 5) {
