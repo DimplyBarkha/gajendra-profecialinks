460eaea361aee1be065ed3064ee5f52f89e2fd2f
Piotr Wieleba <wieleba.piotr@gmail.com>
2021-02-19 09:57:12 +0100

    - Fixing issues with the search extractor - Creating 2 code versions for search and IM search extractors - Updating tests

--- src/library/product/search/stores/e/easyparapharmacie/FR/extract.js
+++ src/library/product/search/stores/e/easyparapharmacie/FR/extract.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const { transform } = require('../../../../shared');
 
 const implementation = async (inputs, parameters, context, dependencies) => {
@@ -5,42 +6,61 @@ const implementation = async (inputs, parameters, context, dependencies) => {
   const { productDetails } = dependencies;
 
   await context.evaluate(async () => {
+    const waitForResults = async (prevResultsNum) => {
+      let resultsNum = document.querySelectorAll('div.product-container').length;
+      let iteration = 0;
+      while (prevResultsNum === resultsNum && iteration < 10) {
+        console.log(`Prev results: ${prevResultsNum}`);
+        console.log(`Results now: ${resultsNum}`);
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        resultsNum = document.querySelectorAll('div.product-container').length;
+        iteration++;
+      }
+    };
+
     let buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
     if (buttonShowMore) {
-      let counter = 0;
-      const resultsPerPage = 24;
-      const maxResultsForExtractor = 150 - resultsPerPage;
-      do {
-        if (Math.floor(maxResultsForExtractor / resultsPerPage) <= counter) {
-          break;
-        }
-        buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
-        // @ts-ignore
+      let totalProducts = document.querySelectorAll('div.product-container').length;
+      // There are 2 extractors here, because IM search needs 1000 results and
+      // there's no way to pass 'results' input to extract.js
+      // For IM search uncomment/comment 2 lines below
+      // while (totalProducts < 1000 && !buttonShowMore.disabled) {
+      while (totalProducts < 150 && !buttonShowMore.disabled) {
         buttonShowMore.click();
-        await stall(500);
-        counter++;
-        // @ts-ignore
-      } while (buttonShowMore.disabled === false);
-    }
+        await waitForResults(totalProducts);
+        totalProducts = document.querySelectorAll('div.product-container').length;
+        buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
+      }
 
-    function stall (ms) {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, ms);
-      });
+      // let counter = 0;
+      // const resultsPerPage = 24;
+      // // We add here 1000 because this is what IM search requires, and so far
+      // // there's no way to pass inputs to extract.js
+      // // const maxResultsForExtractor = 150 - resultsPerPage;
+      // const maxResultsForExtractor = 1000 - resultsPerPage;
+      // do {
+      //   if (Math.floor(maxResultsForExtractor / resultsPerPage) <= counter) {
+      //     break;
+      //   }
+      //   buttonShowMore = document.querySelector('.ais-infinite-hits--showmore').children[0];
+      //   // @ts-ignore
+      //   buttonShowMore.click();
+      //   await stall(1000);
+      //   counter++;
+      //   // @ts-ignore
+      // } while (buttonShowMore.disabled === false);
     }
   });
 
   await context.evaluate(async function () {
-    function addDiv (className, content, productDiv) {
+    const addDiv = (className, content, productDiv) => {
       const newDiv = document.createElement('div');
       // @ts-ignore
       newDiv.classList.add(className);
       newDiv.innerHTML = content;
       // @ts-ignore
       productDiv.appendChild(newDiv);
-    }
+    };
 
     const allProducts = document.querySelectorAll('div.product-container');
     allProducts.forEach((product) => {
