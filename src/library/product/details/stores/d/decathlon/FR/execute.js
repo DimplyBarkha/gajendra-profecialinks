d1af183b0f50fc049058533458a073cffc03f88e
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-20 19:11:36 +0530

    updated the noResultsXpath

--- src/library/product/details/stores/d/decathlon/FR/execute.js
+++ src/library/product/details/stores/d/decathlon/FR/execute.js
@@ -6,7 +6,7 @@ module.exports = {
     store: 'decathlon',
     domain: 'decathlon.fr',
     loadedSelector: 'div.product-display',
-    noResultsXPath: '//h1[contains(text(),"Erreur")] | //div[@id="search-result-top"]//div[@class="title"] | //main//span[text()="No result"]',
+    noResultsXPath: `//h1[contains(text(),"Erreur")] | //div[@id="search-result-top"]//div[@class="title"] | //main//span[text()="No result"] | //*[contains(@id,"search-suggestions-banner")]//*[contains(text(),"Désolé, il n'y a aucun résultat pour")]`,
     zipcode: '',
   },
 };
