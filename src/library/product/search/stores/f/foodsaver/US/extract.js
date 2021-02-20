aa31e3df2bca6659b332d33d15f8cbfb00def080
SS <shital.sura@synerzip.com>
2021-02-20 22:31:05 +0530

    foodsaver search fixes

--- src/library/product/search/stores/f/foodsaver/US/extract.js
+++ src/library/product/search/stores/f/foodsaver/US/extract.js
@@ -1,4 +1,30 @@
-const { transform } = require('../shared');
+// const { transform } = require('../shared');
+const { transform } = require('../../../../shared');
+
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { transform } = parameters;
+  const { productDetails } = dependencies;
+  await new Promise((resolve, reject) => setTimeout(resolve, 4000));
+
+  await context.evaluate(async ()  => {
+    for (let index = 0; index < 7; index++) {
+      const morButtonSel = document.querySelector('div.show-more div.text-center button.btn-tertiary');
+      if (morButtonSel) {
+        // console.log('going to click now');
+        morButtonSel.click();
+        await new Promise((resolve, reject) => setTimeout(resolve, 6000));
+        // console.log('clicked now------÷---');
+      }        
+    }
+  });
+
+  return await context.extract(productDetails, { transform });
+}
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
@@ -8,4 +34,5 @@ module.exports = {
     domain: 'foodsaver.com',
     zipcode: '',
   },
+  implementation,
 };
