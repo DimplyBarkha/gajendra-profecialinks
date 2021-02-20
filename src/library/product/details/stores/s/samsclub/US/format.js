0ff66d27e2add27ad2dc8bee16d1ffc13e19a960
Harsha-Chirmade <harsha.chirmade@profecialinks.com>
2021-02-21 00:09:34 +0530

    updated

--- src/library/product/details/stores/s/samsclub/US/format.js
+++ src/library/product/details/stores/s/samsclub/US/format.js
@@ -24,6 +24,7 @@ const transform = (data) => {
         return data;
     };
     for (const { group } of data) {
+        var itemNo = '';
         for (const row of group) {
             if (row.alternateImages) {
                 row.alternateImages.forEach(item => {
@@ -39,7 +40,6 @@ const transform = (data) => {
                         let split1 = item.text.split("?");
                         item.text = `${split1[0]}`;
                     }
-
                 });
             }
             if (row.manufacturerDescription) {
@@ -86,23 +86,124 @@ const transform = (data) => {
             }
             if (row.variantId) {
                 row.variantId.forEach(item => {
-                    console.log("item.text",item.text);
                     if (item.text.includes('Item # ')) {
                     item.text = item.text.replace('Item # ','').trim();
+                    itemNo = item.text;
+                    console.log("item.variantId", itemNo);
                     }
                 });
             }
             if (row.price) {
                 row.price.forEach(item => {
                     if (item.text.includes('current price: ')) {
-                    item.text = item.text.replace('current price: ','').trim();
+                        item.text = item.text.replace('current price: ','').trim();
                     }
                 });
             }
             if (row.sku) {
                 row.sku.forEach(item => {
                     if (item.text.includes('Model # ')) {
-                    item.text = item.text.replace('Model # ','').trim();
+                        item.text = item.text.replace('Model # ','').trim();
+                    }
+                    else if(item.text.includes(itemNo)){
+                        let skuText = "(\"itemNumber\":\""+itemNo+"\".*?\"manufacturingInfo\":{\"model\":\".*?\")";
+                        var skuId1 = item.text.match(skuText);
+                        let skuId2 = ''
+                        if (typeof skuId1 !== 'undefined' && skuId1 != null) {
+                            skuId2 = skuId1[0].match("{\"model\":\".*?\"");
+                            if (typeof skuId2 !== 'undefined' && skuId2 != null) {
+                               item.text = skuId2[0].replace('\"model\":\"','').replace('"','').replace('}','').replace('{','');
+                            }
+                            else {
+                                item.text = '';
+                            }
+                        }
+                        else {
+                            skuText = "(\"itemNumber\":\""+itemNo+"\".*?\"vendor\":{\"stockId\":\".*?\")";
+                            console.log("item.skuText", skuText);
+                            skuId1 = item.text.match(skuText);
+                            if (typeof skuId1 !== 'undefined' && skuId1 != null) {
+                                skuId2 = skuId1[0].match("{\"stockId\":\".*?\"");
+                                console.log("item.skuId2", skuId2);
+                                if (typeof skuId2 !== 'undefined' && skuId2 != null) {
+                                    item.text = skuId2[0].replace('\"stockId\":\"','').replace('"','').replace('}','').replace('{','');
+                                }
+                                else {
+                                    item.text = '';
+                                }
+                            }
+                            else {
+                                item.text = '';
+                            }
+                        }
+                    }
+                });
+            }
+            if (row.mpc) {
+                row.mpc.forEach(item => {
+                    if (item.text.includes('Model # ')) {
+                        item.text = item.text.replace('Model # ','').trim();
+                    }
+                    else if(item.text.includes(itemNo)){
+                        let mpcText = "(\"itemNumber\":\""+itemNo+"\".*?\"manufacturingInfo\":{\"model\":\".*?\")";
+                        let mpcId1 = item.text.match(mpcText);
+                        let mpcId2 = ''
+                        if (typeof mpcId1 !== 'undefined' && mpcId1 != null) {
+                           mpcId2 = mpcId1[0].match("{\"model\":\".*?\"");
+                           if (typeof mpcId2 !== 'undefined' && mpcId2 != null) {
+                               item.text = mpcId2[0].replace('\"model\":\"','').replace('"','').replace('}','').replace('{','');
+                           }
+                           else {
+                            item.text = '';
+                        }
+                        }
+                        else {
+                            mpcText = "(\"itemNumber\":\""+itemNo+"\".*?\"vendor\":{\"stockId\":\".*?\")";
+                            console.log("item.skuText", mpcText);
+                            mpcId1 = item.text.match(mpcText);
+                            if (typeof mpcId1 !== 'undefined' && mpcId1 != null) {
+                                mpcId2 = mpcId1[0].match("{\"stockId\":\".*?\"");
+                                console.log("item.skuId2", mpcId2);
+                                if (typeof mpcId2 !== 'undefined' && mpcId2 != null) {
+                                    item.text = mpcId2[0].replace('\"stockId\":\"','').replace('"','').replace('}','').replace('{','');
+                                }
+                                else {
+                                    item.text = '';
+                                }
+                            }
+                            else {
+                                item.text = '';
+                            }
+                        }
+                    }
+                });
+            }
+            if (row.listPrice) {
+                row.listPrice.forEach(item => {
+                    if(item.text.includes(itemNo)){
+                        let listPriceText = "(\"itemNumber\":\""+itemNo+"\".*?\"manufacturingInfo\":{\"model\":.*?\")";
+                        let listPriceDetails = item.text.match(listPriceText);
+                        if (typeof listPriceDetails !== 'undefined' && listPriceDetails != null) {
+                            let listOriginal = listPriceDetails[0].match("\"startPrice\":{\"amount\":.*?\"");
+                            if (typeof listOriginal !== 'undefined' && listOriginal != null) {
+                                let listDiscount = listPriceDetails[0].match("\"finalPrice\":{\"amount\":.*?\"");
+                                if (typeof listDiscount !== 'undefined' && listDiscount != null) {
+                                   if(listOriginal[0] == listDiscount[0]){
+                                      listOriginal = '';
+                                    }
+                                    item.text = listOriginal[0].replace('\"startPrice\":{\"amount\":','').replace('\"','').replace(',','');
+                                }
+                                else {
+                                    item.text = '';
+                                }
+                            }
+                            else {
+                                item.text = '';
+                            }
+                        }
+                        else {
+                            item.text = '';
+                        }
                     }
                 });
             }
