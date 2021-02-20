28e5db39e14cbd397c3fffb8fb05dfc64e168c81
Shweta Kedari <shwetak@synerzipune.local>
2021-02-21 00:28:55 +0530

    Date filter fix

--- src/library/product/reviews/stores/c/cdiscount/FR/shared.js
+++ src/library/product/reviews/stores/c/cdiscount/FR/shared.js
@@ -4,49 +4,6 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  function checkIfReviewIsFromLast30Days (lastDate, reviewDate) {
-    console.log('lastDate' + lastDate);
-    console.log('reviewDate' + reviewDate);
-    const timestamp = new Date(lastDate).getTime() - (30 * 24 * 60 * 60 * 1000);
-    console.log('timestamp' + timestamp);
-    console.log(new Date(reviewDate).getTime());
-    if (new Date(reviewDate).getTime() >= timestamp) {
-      console.log('True');
-      return true;
-    }
-    console.log('false');
-    return false;
-  }
-
-  const state = context.getState();
-  let lastReviewDate = state.lastReviewDate || null;
-
-  data = data.filter(function (item, index, data) {
-    //console.log('group length before' + item.group.length);
-    item.group = item.group.filter(function (row) {
-      if (!lastReviewDate) {
-        lastReviewDate = row.reviewDate[0].text;
-      }
-    let dt = lastReviewDate;
-    let newLastDate = dt.split("/")[1]+'/'+dt.split("/")[0]+'/'+dt.split("/")[2];
-    let rDate = row.reviewDate[0].text
-    let newReviewDate = rDate.split("/")[1]+'/'+rDate.split("/")[0]+'/'+rDate.split("/")[2];
-    
-      if (checkIfReviewIsFromLast30Days(newLastDate, newReviewDate)) {
-        return true;
-      }
-      return false;
-    });
-    context.setState({ lastReviewDate });
-    //console.log('group length after' + item.group.length);
-    item.rows = item.group.length;
-    if (item.group.length !== 0) {
-      return true;
-    }
-    return false;
-  });
-  
-  //return data;
   const clean = text => text.toString()
     .replace(/\r\n|\r|\n/g, ' ')
     .replace(/&amp;nbsp;/g, ' ')
@@ -56,7 +13,7 @@ const transform = (data, context) => {
     .replace(/"\s{1,}/g, '"')
     .replace(/\s{1,}"/g, '"')
     .replace(/^ +| +$|( )+/g, ' ')
-  // eslint-disable-next-line no-control-regex
+    // eslint-disable-next-line no-control-regex
     .replace(/[\x00-\x1F]/g, '')
     .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
     .trim();
@@ -67,6 +24,28 @@ const transform = (data, context) => {
       }));
     }
   }
+  function checkIfReviewIsFromLast30Days (reviewDate, maxDays = 30) {
+    const timestamp = new Date().getTime() - (maxDays * 24 * 60 * 60 * 1000);
+    if (new Date(reviewDate).getTime() >= timestamp) {
+      return true;
+    }
+    return false;
+  }
+  data = data.filter(function (item) {
+    console.log('group length before' + item.group.length);
+    item.group = item.group.filter(function (row) {
+      if (row.reviewDate.length && checkIfReviewIsFromLast30Days(row.reviewDate[0].text)) {
+        return true;
+      }
+      return false;
+    });
+    console.log('group length after' + item.group.length);
+    item.rows = item.group.length;
+    if (item.group.length !== 0) {
+      return true;
+    }
+    return false;
+  });
   return data;
 };
 module.exports = { transform };
