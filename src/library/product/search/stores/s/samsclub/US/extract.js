150c777c7b2e08fa2fa0bcc63e0c4b736149b6cf
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-02-20 23:07:15 +0530

    fixing

--- src/library/product/search/stores/s/samsclub/US/extract.js
+++ src/library/product/search/stores/s/samsclub/US/extract.js
@@ -1,4 +1,5 @@
-const {transform} = require('../US/format');
+//const {transform} = require('../US/format');
+const {transform} = require('../../../../shared');
 module.exports = {
   implements: 'product/search/extract',
   parameterValues: {
