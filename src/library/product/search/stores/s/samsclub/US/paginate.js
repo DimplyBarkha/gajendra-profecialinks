4f25d2f40617fcd14b75de12dce6c66ae553cdf1
Judhisthira Sahoo <judhishtor@profecialinks.com>
2021-02-20 12:08:51 +0530

    fixing

--- src/library/product/search/stores/s/samsclub/US/paginate.js
+++ src/library/product/search/stores/s/samsclub/US/paginate.js
@@ -3,11 +3,15 @@ module.exports = {
   parameterValues: {
     country: 'US',
     store: 'samsclub',
-    nextLinkSelector: 'div.sc-pagination-control ol li.sc-pagination-next button' ,
-    mutationSelector: null,
-    spinnerSelector: null,
-    loadedSelector: 'div.sc-plp-layout',
-    openSearchDefinition: null,
+    //nextLinkSelector: 'div.sc-pagination-control ol li.sc-pagination-next button' ,
+    //mutationSelector: null,
+    //spinnerSelector: null,
+    //loadedSelector: 'div.sc-plp-layout',
+    //openSearchDefinition: null,
+    openSearchDefinition: {
+      offset: 48,
+      template: 'https://www.samsclub.com/api/node/vivaldi/v2/az/products/search?sourceType=1&sortKey=relevance&sortOrder=1&limit=48&searchTerm={searchTerms}&clubId=6384&br=true&offset={offset}#[!opt!]{"type":"json"}[/!opt!]',
+    },
     domain: 'samsclub.com',
   },
 };
