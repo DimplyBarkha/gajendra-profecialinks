4db42090505f898a0d18fb687277d44c4425379d
Roksolana <dytchakro@gmail.com>
2021-02-22 12:12:51 +0200

    first

---
+++ src/library/navigation/goto/domains/me/mega-image.ro.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  implements: 'navigation/goto',
+  parameterValues: {
+    domain: 'mega-image.ro',
+    timeout: null,
+    jsonToTable: null,
+    country: 'RO',
+    store: 'mega-image',
+    zipcode: '',
+  },
+};
---
+++ src/library/product/details/stores/m/mega-image/RO/details.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  implements: 'product/details',
+  parameterValues: {
+    country: 'RO',
+    store: 'mega-image',
+    domain: 'mega-image.ro',
+    zipcode: '',
+  },
+};
