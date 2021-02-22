2aa3533579c4d93ac87143ef7b571c3d8aa9a326
Hetal Kothari <hetal.kothari@sz.import.io>
2021-02-22 18:26:16 +0530

    UAT fixes for variants

--- src/library/product/details/stores/n/netshoes/BR/variantsExtract.js
+++ src/library/product/details/stores/n/netshoes/BR/variantsExtract.js
@@ -1,4 +1,30 @@
-
+async function implementation (
+  inputs,
+  parameters,
+  context,
+  dependencies,
+) {
+  const { createUrl, variants } = dependencies;
+  await context.evaluate(function () {
+    function addHiddenDiv (id, content, index) {
+      const newDiv = document.createElement('div');
+      newDiv.id = id;
+      newDiv.textContent = content;
+      newDiv.style.display = 'none';
+      const originalDiv = document.querySelectorAll('section[class="buy-box"] a[qa-automation="product-color"]')[index];
+      originalDiv.appendChild(newDiv);
+    }
+    const node = document.querySelectorAll('section[class="buy-box"] a[qa-automation="product-color"]')
+    if (node) {
+      console.log("Inside node");
+      for (let i = 0; i < node.length; i++) {
+        const id = node[i].getAttribute("data-sku");
+        addHiddenDiv('ii_variant', 'https://www.netshoes.com.br/' + id, i);
+      }
+    }
+  }, createUrl);
+  return await context.extract(variants);
+}
 module.exports = {
   implements: 'product/details/variants/variantsExtract',
   parameterValues: {
@@ -7,5 +33,5 @@ module.exports = {
     transform: null,
     domain: 'netshoes.com.br',
     zipcode: "''",
-  },
+  }, implementation
 };
