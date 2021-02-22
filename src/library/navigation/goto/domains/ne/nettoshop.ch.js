2ef789996b8273e7acbd9bb14dd3dc540df86b23
osmarghyadeep <arghyadep.g@partner.import.io>
2021-02-22 17:26:28 +0530

    updated the code to reduce firstRequestTimeouts

--- src/library/navigation/goto/domains/ne/nettoshop.ch.js
+++ src/library/navigation/goto/domains/ne/nettoshop.ch.js
@@ -3,7 +3,7 @@ module.exports = {
   implements: 'navigation/goto',
   parameterValues: {
     domain: 'nettoshop.ch',
-    timeout: 90000,
+    timeout: 120000,
     country: 'CH',
     store: 'nettoshop_ch_fr',
     zipcode: '',
@@ -11,12 +11,14 @@ module.exports = {
   implementation: async ({ url, zipcode }, parameters, context, dependencies) => {
     const timeout = parameters.timeout ? parameters.timeout : 100000;
     // await context.setBypassCSP(true);
-    await context.setCssEnabled(true);
-    await context.setAntiFingerprint(false);
-    await context.setJavaScriptEnabled(true);
     await context.setBlockAds(false);
+    await context.setFirstRequestTimeout(100000);
+    await context.setCssEnabled(true);
     await context.setLoadAllResources(true);
     await context.setLoadImages(true);
+    await context.setJavaScriptEnabled(true);
+    await context.setAntiFingerprint(false);
+    await context.setUseRelayProxy(false);
     const inputUrl = `${url}#[!opt!]{"discard_CSP_header":true, "block_ads": false}[/!opt!]`;
     await context.goto(inputUrl, { first_request_timeout: 60000, timeout, waitUntil: 'load', checkBlocked: false });
     // await context.goto(inputUrl, { timeout: timeout, waitUntil: 'load', checkBlocked: false });
