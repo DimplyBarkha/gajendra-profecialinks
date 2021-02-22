f550a9da7960226d11653cbd510aaf27e48e446f
Saipavan Guntraddi <saipavan.guntraddi-odc@partner.import.io>
2021-02-22 22:08:55 +0530

    updated code

--- src/library/product/details/stores/l/loblaws/CA/extract.js
+++ src/library/product/details/stores/l/loblaws/CA/extract.js
@@ -1,32 +1,21 @@
+const { cleanUp } = require('../../../../shared');
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'CA',
     store: 'loblaws',
-    transform: null,
+    transform: cleanUp,
     domain: 'loblaws.ca',
     zipcode: '',
   },
-  implementation: async ({ inputstring }, { country, domain }, context, { productDetails }) => {
-    
+  implementation: async ({ inputString }, { country, domain, transform: transformParam }, context, { productDetails }) => {
+
     await context.waitForSelector('h3[class="product-tile__details__info__name"] a', 3000);
     await context.click('h3[class="product-tile__details__info__name"] a');
     await new Promise((resolve) => setTimeout(resolve, 5000));
-    
-
-
-
-
     // delay
     // await context.waitForSelector('div.product-image-list > div > div > div > div > div > div > img', 5000)
     await context.evaluate(() => {
-      function addElementToDocument(key, value) {
-        const catElement = document.createElement('div');
-        catElement.id = key;
-        catElement.textContent = value;
-        catElement.style.display = 'none';
-        document.body.appendChild(catElement);
-      }
       function addElementToDocument(key, value) {
         const catElement = document.createElement('div');
         catElement.id = key;
@@ -38,31 +27,19 @@ module.exports = {
         const nodeSet = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
         const result = [];
         for (let index = 0; index < nodeSet.snapshotLength; index++) {
-        const element = nodeSet.snapshotItem(index);
-        if (element) result.push(prop ? element[prop] : element.nodeValue);
+          const element = nodeSet.snapshotItem(index);
+          if (element) result.push(prop ? element[prop] : element.nodeValue);
         }
         return result;
-        };
-        var xyz=[]
-      //  const pipeSeparatorDouble = (id, data) => {
-      //   var doubleSeparatorText = data.join('|');
-      //   finalDescText = doubleSeparatorText;
-      //   addElementToDocument(id, doubleSeparatorText);
-      // };
-      const v = getAllXpath('//div[@class="slick-track"]/div[not(@data-index="0")]/div//img/@src', 'nodeValue');
+      };
+      const alternateImages = getAllXpath('//div[@class="slick-track"]/div[not(@data-index="0")]/div//img/@src', 'nodeValue');
       // @ts-ignore
-      var uniq = [...new Set(v)];
-      var y=uniq.join(' | ')
-      // pipeSeparatorDouble('secondaryImages', uniq); 
-      console.log('ssssssssss',uniq)
-      addElementToDocument('secondaryImages',y)
-  
-
-
-
+      var uniqalternateImages = [...new Set(alternateImages)];
+      for (let i = 0; i < uniqalternateImages.length; i++) {
+        console.log('sai',uniqalternateImages[i])
+        addElementToDocument('uniqalternateImages', uniqalternateImages[i]);
+      }
     });
-  
-
-  await context.extract(productDetails);
-},
+    await context.extract(productDetails, { transform: transformParam });
+  },
 };
