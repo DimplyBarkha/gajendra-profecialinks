0941c8261a13899c94206d8c26f7a794b56e670a
Your Name <karthik.ravi-odc@partner.import.io>
2021-02-22 22:12:35 +0530

    new

--- src/library/product/details/stores/f/flaschenpost/DE/extract.js
+++ src/library/product/details/stores/f/flaschenpost/DE/extract.js
@@ -14,19 +14,35 @@ module.exports = {
     dependencies,
   ) => {
     const { transform } = parameters;
-    const { productDetails } = dependencies;    
+    const { productDetails } = dependencies;
+    var wantedZip = '28203';
+    await new Promise((resolve, reject) => setTimeout(resolve, 10000));
+    await context.click('a[class="fp-changeZipCode fp-footer-changeZipCode"]');
+    await context.setInputValue('[id="validZipcode"]', wantedZip);
+    await context.waitForSelector('button[class="fp-button fp-button--primary zip--button fontSizeL"]', 6000)
+    await context.click('button[class="fp-button fp-button--primary zip--button fontSizeL"]');
     await context.waitForFunction(function (xp) {
       return Boolean(document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 100000 }, '//div[@class="fp_product_details"]/div[contains(@class,"fp_article")]');
-    
-    await context.evaluate(async () => {     
-      var getXpath = (xpath, prop) => {
-        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
-        let result;
-        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
-        else result = elem ? elem.singleNodeValue : '';
-        return result && result.trim ? result.trim() : result;
-      };
+    }, { timeout: 100000 }, '//script[contains(text(),"variant")]/text()');
+    const applyScroll = async function (context) {
+      await context.evaluate(async () => {        
+        let scrollTop = 0;
+        while (scrollTop !== 20000) {
+          scrollTop += 1000;
+          window.scroll(0, scrollTop);
+          await stall(1000);
+        }
+        function stall(ms) {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              resolve();
+            }, ms);
+          });
+        }
+      });
+    };
+    await applyScroll(context);
+    await context.evaluate(async function () {
       function addHiddenDiv(id, content, index) {
         const newDiv = document.createElement('div');
         newDiv.id = id;
@@ -35,6 +51,14 @@ module.exports = {
         const originalDiv = document.querySelectorAll('div[class="fp_product_details"] div[class*="fp_article"] div[class="fp_article_details"]')[index];
         originalDiv.parentNode.insertBefore(newDiv, originalDiv);
       }
+      var getXpath = (xpath, prop) => {
+        var elem = document.evaluate(xpath, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null);
+        let result;
+        if (prop && elem && elem.singleNodeValue) result = elem.singleNodeValue[prop];
+        else result = elem ? elem.singleNodeValue : '';
+        return result && result.trim ? result.trim() : result;
+      };
+
       var vari = getXpath('//script[contains(text(),"variant")]/text()', 'nodeValue');
       console.log('pathhhhhhh', vari)
       if (vari != null) {
@@ -49,12 +73,12 @@ module.exports = {
           }
           for (var i = 0; i < finalIds.length; i++) {
             var temp = finalIds[i].split("',")[0];
-            console.log('tempppppppppp', temp)
             addHiddenDiv('rpc', temp, i);
+            console.log('tempppppppppp', temp)
           }
         }
       }
-    });    
+    });
     return await context.extract(productDetails, { transform });
   },
-}
+};
