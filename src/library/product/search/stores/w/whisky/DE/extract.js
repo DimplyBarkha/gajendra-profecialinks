c0a61e7ff806b8743b0efea52447e720b3d9779f
unknown <pavithra.ramasamy-odc@partner.import.io>
2021-02-22 18:49:34 +0530

    #2658 devwhiskysearch

--- src/library/product/search/stores/w/whisky/DE/extract.js
+++ src/library/product/search/stores/w/whisky/DE/extract.js
@@ -37,7 +37,7 @@ async function implementation(
       return result;
       };
     var price = getAllXpath('//div[@class="panel-body"]//span[@class="article-price-default article-club-hidden"]//text()', 'nodeValue');
-    var productid = getAllXpath('//div[contains(@class,"article-stock")]/@class', 'nodeValue');
+    
 
 
     if (price.length >= 1) {
@@ -46,6 +46,34 @@ async function implementation(
         addHiddenDiv("PRICEVALUE", price[i], i);
       }
     }
+
+  var productid = getAllXpath('//div[contains(@class,"article-stock")]/@class', 'nodeValue');
+  var imageid = getAllXpath('//div[@class="article-left article-thumbnail"]//a[@class="title"]/img/@data-src', 'nodeValue');
+  var id = getAllXpath('//div[@class="panel-body"]//div[@class="form-group to-basket-default"]//button/@data-datamints-enhancedga-product', 'nodeValue');
+  var j=0; //not available
+  var k=0; 
+  for(var i=0;i<productid.length;i++){
+    if(productid[i].includes("stock-state-notAvailable"))
+    {
+      if(imageid[i].includes("ximage_"))
+      {
+        imageid[i]=imageid[i].split("ximage_")[1];
+        imageid[i]=imageid[i].split("_")[0];
+        addHiddenDiv("PRODUCTID", imageid[i], i);
+      }
+       
+    }
+    else{
+      if(id[k].includes('id": "'))
+      {
+        id[k]=id[k].split('id": "')[1];
+        id[k]=id[k].split('"')[0];
+        addHiddenDiv("PRODUCTID", id[k], i);
+        k=k+1;
+      }
+      
+    }
+  }
   });
   return await context.extract(productDetails, { transform });
 }
