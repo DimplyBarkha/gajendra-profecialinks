9db143b9b22fb3fe41a85becfe1593d3654138d6
shwetakedari10 <60596662+shwetakedari10@users.noreply.github.com>
2021-02-22 18:12:43 +0530

    added saparate shared js file in costco also added upated xpath for aggreeagte rating count

--- src/library/product/reviews/stores/c/costco/US/extract.js
+++ src/library/product/reviews/stores/c/costco/US/extract.js
@@ -1,4 +1,4 @@
-const { transform } = require('../../../../shared');
+const { transform } = require('./../shared');
 
 async function implementation (
   inputs,
@@ -9,15 +9,25 @@ async function implementation (
   const { transform } = parameters;
   const { productReviews } = dependencies;
 
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   await context.evaluate(async function () {
-    const tabList = document.querySelectorAll('#tablist li');
-    if (tabList) {
-      for (let i = 0; i < tabList.length; i++) {
-        if (tabList[i].innerText === 'Reviews') {
-          tabList[i].querySelector('a').click();
-        }
-      }
+    let postal_code_continue_btn =   document.getElementById('postal-code-continue');
+    if(postal_code_continue_btn){
+      postal_code_continue_btn.click();
     }
+  });
+
+  await context.waitForSelector('#BVRRContainer', { timeout: 10000 });
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await context.evaluate(async function () {
+    // const tabList = document.querySelectorAll('#tablist li');
+    // if (tabList) {
+    //   for (let i = 0; i < tabList.length; i++) {
+    //     if (tabList[i].innerText === 'Reviews') {
+    //       tabList[i].querySelector('a').click();
+    //     }
+    //   }
+    // }
     const loadMoreButton = document.querySelector('#nav-pdp-tab-header-12 > div.row.view-more-v2 > div > input');
     if (loadMoreButton) {
       loadMoreButton.click();
@@ -31,7 +41,27 @@ async function implementation (
       console.log(error);
     }
   });
+  
+  // let count = 0;
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
+  // let nextLinkSelector = await context.evaluate(() => !!document.querySelector('li.bv-content-pagination-buttons-item.bv-content-pagination-buttons-item-next a'));
+  // while (nextLinkSelector) {
+  //   count += await context.evaluate(() => {
+  //     return document.querySelectorAll(
+  //       'ol li.bv-content-item.bv-content-top-review.bv-content-review',
+  //     ).length;
+  //   });
 
+  //   console.log('here is count', count);
+  //   if (count <= 150) {
+  //     await context.extract(productReviews, { transform }, { type: 'APPEND' });
+  //     await context.click('li.bv-content-pagination-buttons-item.bv-content-pagination-buttons-item-next a');
+  //     await new Promise(resolve => setTimeout(resolve, 1000));
+  //     nextLinkSelector = await context.evaluate(() => !!document.querySelector('li.bv-content-pagination-buttons-item.bv-content-pagination-buttons-item-next a'));
+  //   } else {
+  //     break;
+  //   }
+  // }
   return await context.extract(productReviews, { transform });
 }
 
