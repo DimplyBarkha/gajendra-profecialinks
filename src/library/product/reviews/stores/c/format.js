f9b79255a4ac53b827f0218a0899d869ed0e5a99
nilofarsh <nilofar.shaikh@synerzip.com>
2021-02-22 18:43:58 +0530

    Added max days for reviews

--- src/library/product/reviews/stores/c/format.js
+++ src/library/product/reviews/stores/c/format.js
@@ -4,44 +4,6 @@
  * @returns {ImportIO.Group[]}
  */
 const transform = (data, context) => {
-  function checkIfReviewIsFromLast30Days (lastDate, reviewDate) {
-    console.log('lastDate' + lastDate);
-    console.log('reviewDate' + reviewDate);
-    const timestamp = new Date(lastDate).getTime() - (30 * 24 * 60 * 60 * 1000);
-    console.log('timestamp' + timestamp);
-    console.log(new Date(reviewDate).getTime());
-    if (new Date(reviewDate).getTime() >= timestamp) {
-      console.log('True');
-      return true;
-    }
-    console.log('false');
-    return false;
-  }
-
-  const state = context.getState();
-  let lastReviewDate = state.lastReviewDate || null;
-
-  data = data.filter(function (item, index, data) {
-    console.log('group length before' + item.group.length);
-    item.group = item.group.filter(function (row) {
-      if (!lastReviewDate) {
-        lastReviewDate = row.reviewDate[0].text;
-      }
-      if (checkIfReviewIsFromLast30Days(lastReviewDate, row.reviewDate[0].text)) {
-        return true;
-      }
-      return false;
-    });
-
-    console.log('group length after' + item.group.length);
-    item.rows = item.group.length;
-    if (item.group.length !== 0) {
-      return true;
-    }
-    return false;
-  });
-  context.setState({ lastReviewDate });
-  //return data;
   const clean = text => text.toString()
     .replace(/\r\n|\r|\n/g, ' ')
     .replace(/&amp;nbsp;/g, ' ')
@@ -57,11 +19,39 @@ const transform = (data, context) => {
     .trim();
   for (const { group } of data) {
     for (const row of group) {
+      if (row.reviewCount) {
+        row.reviewCount.forEach((reviewCountItem) => {
+          reviewCountItem.text = reviewCountItem.text.replace(/[{()}]/gm, '').trim();
+        });
+      }
       Object.keys(row).forEach(header => row[header].forEach(el => {
         el.text = clean(el.text);
       }));
     }
   }
+  function checkIfReviewIsFromLast30Days (reviewDate, maxDays) {
+    const timestamp = new Date().getTime() - (maxDays * 24 * 60 * 60 * 1000);
+    if (new Date(reviewDate).getTime() >= timestamp) {
+      return true;
+    }
+    return false;
+  }
+  data = data.filter(function (item) {
+    console.log('group length before' + item.group.length);
+    item.group = item.group.filter(function (row) {
+      const maxDays = row.maxDays && row.maxDays[0] ? row.maxDays[0].text : 30;
+      if (row.reviewDate.length && checkIfReviewIsFromLast30Days(row.reviewDate[0].text, maxDays)) {
+        return true;
+      }
+      return false;
+    });
+    console.log('group length after' + item.group.length);
+    item.rows = item.group.length;
+    if (item.group.length !== 0) {
+      return true;
+    }
+    return false;
+  });
   return data;
 };
 module.exports = { transform };
