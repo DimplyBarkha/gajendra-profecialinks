8a2432cc9cf4c337b84f31b988007ba0071ae27f
Avinash <avinash.kadam@profecialinks.com>
2021-02-22 20:27:35 +0530

    xpath, selectors, paginate updated

--- src/library/product/details/stores/n/nicokick/US/extract.js
+++ src/library/product/details/stores/n/nicokick/US/extract.js
@@ -1,10 +1,11 @@
+const { cleanUp } = require('./../../../../shared');
 
 module.exports = {
   implements: 'product/details/extract',
   parameterValues: {
     country: 'US',
     store: 'nicokick',
-    transform: null,
+    transform: cleanUp,
     domain: 'nicokick.com',
     zipcode: '',
   },
--- src/library/product/reviews/stores/n/nicokick/shared.js
+++ src/library/product/reviews/stores/n/nicokick/shared.js
@@ -1,20 +1,36 @@
-
 /**
  *
  * @param {ImportIO.Group[]} data
  * @returns {ImportIO.Group[]}
  */
-const transform = (data) => {
+const transform = (data, context) => {
+  const clean = text => text.toString()
+    .replace(/\r\n|\r|\n/g, ' ')
+    .replace(/&amp;nbsp;/g, ' ')
+    .replace(/&amp;#160/g, ' ')
+    .replace(/\u00A0/g, ' ')
+    .replace(/\s{2,}/g, ' ')
+    .replace(/"\s{1,}/g, '"')
+    .replace(/\s{1,}"/g, '"')
+    .replace(/^ +| +$|( )+/g, ' ')
+  // eslint-disable-next-line no-control-regex
+    .replace(/[\x00-\x1F]/g, '')
+    .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, ' ')
+    .trim();
   for (const { group } of data) {
     for (const row of group) {
+
       if (row.reviewRating) {
         row.reviewRating.forEach(item => {
           item.text = parseInt(item.text, 10) / 20;
         });
       }
+
+      Object.keys(row).forEach(header => row[header].forEach(el => {
+        el.text = clean(el.text);
+      }));
     }
   }
   return data;
 };
-
 module.exports = { transform };
