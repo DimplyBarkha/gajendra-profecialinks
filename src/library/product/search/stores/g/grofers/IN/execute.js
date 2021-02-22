2bc1dcb0b293972388f34d8ed2788ad347714a89
venkatsn <venkat.sn-odc@partner.import.io>
2021-02-22 20:19:23 +0530

    grofers in

--- src/library/product/search/stores/g/grofers/IN/execute.js
+++ src/library/product/search/stores/g/grofers/IN/execute.js
@@ -1,42 +1,42 @@
-/**
- *
- * @param { { keywords: string, zipcode: string } } inputs
- * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
- * @param { ImportIO.IContext } context
- * @param { { goto: ImportIO.Action} } dependencies
- */
-async function implementation (
-  inputs,
-  parameters,
-  context,
-  dependencies,
-) {
-  console.log('params', parameters);
-  const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
-  await dependencies.goto({ url, zipcode: inputs.zipcode });
+// /**
+//  *
+//  * @param { { keywords: string, zipcode: string } } inputs
+//  * @param { { url: string, loadedSelector?: string, noResultsXPath: string } } parameters
+//  * @param { ImportIO.IContext } context
+//  * @param { { goto: ImportIO.Action} } dependencies
+//  */
+// async function implementation (
+//   inputs,
+//   parameters,
+//   context,
+//   dependencies,
+// ) {
+//   console.log('params', parameters);
+//   const url = parameters.url.replace('{searchTerms}', encodeURIComponent(inputs.keywords));
+//   await dependencies.goto({ url, zipcode: inputs.zipcode });
 
-  try {
-    await context.click('div.user-address');
-    await context.waitForSelector('div.img-loader__wrapper img[alt="Mumbai"]');
-    await context.click('div.img-loader__wrapper img[alt="Mumbai"]');
-  } catch (exception) {
-    console.log(exception);
-  }
+//   try {
+//     await context.click('div.user-address');
+//     await context.waitForSelector('div.img-loader__wrapper img[alt="Mumbai"]');
+//     await context.click('div.img-loader__wrapper img[alt="Mumbai"]');
+//   } catch (exception) {
+//     console.log(exception);
+//   }
 
-  if (parameters.loadedSelector) {
-    await context.waitForFunction(function (sel, xp) {
-      return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
-    }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
-  }
-  console.log('Checking no results', parameters.noResultsXPath);
-  return await context.evaluate(function (xp) {
-    const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-    console.log(xp, r);
-    const e = r.iterateNext();
-    console.log(e);
-    return !e;
-  }, parameters.noResultsXPath);
-}
+//   if (parameters.loadedSelector) {
+//     await context.waitForFunction(function (sel, xp) {
+//       return Boolean(document.querySelector(sel) || document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null).iterateNext());
+//     }, { timeout: 10000 }, parameters.loadedSelector, parameters.noResultsXPath);
+//   }
+//   console.log('Checking no results', parameters.noResultsXPath);
+//   return await context.evaluate(function (xp) {
+//     const r = document.evaluate(xp, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+//     console.log(xp, r);
+//     const e = r.iterateNext();
+//     console.log(e);
+//     return !e;
+//   }, parameters.noResultsXPath);
+// }
 
 module.exports = {
   implements: 'product/search/execute',
@@ -49,5 +49,5 @@ module.exports = {
     noResultsXPath: '//h1[@class="no-result__msg-main"]',
     zipcode: "''",
   },
-  implementation,
+ // implementation,
 };
